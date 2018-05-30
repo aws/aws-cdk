e16b5e617f34dd7e7acf191fc88a8a73f1ea781d
Elad Ben-Israel <benisrae@amazon.com>
2018-05-30 12:46:11 +0300

    aws-cdk-java: Initial external release

---
+++ packages/aws-cdk-java/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+outdir="src/main"
+rm -fr "${outdir}"
+for p in $(find-jsii-packages -k jsii.names.java); do
+    echo "Generating java code for module $p"
+    jsii-pacmak --target java --outdir "${outdir}" $p
+done
