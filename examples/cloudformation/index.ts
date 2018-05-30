fa06ab29e2abb08e424fd965aff17c035557da85
Elad Ben-Israel <benisrae@amazon.com>
2018-05-30 12:47:01 +0300

    examples: Initial external release
    Co-authored-by: Matthew Pirocchi <pirocchi@amazon.com>
    Co-authored-by: Paul Maddox <pmaddox@amazon.com>
    Co-authored-by: Rico Huijbers <huijbers@amazon.com>
    Co-authored-by: Breland Miley <brelandm@amazon.com>
    Co-authored-by: Romain Marcadier-Muller <rmuller@amazon.com>
    Co-authored-by: Mitch Garnaat <garnaat@amazon.com>
    Co-authored-by: Shiv Lakshminarayan <shivlaks@amazon.com>
    Co-authored-by: Max Hall <hallmaxw@amazon.com>
    Co-authored-by: Doug Schwartz <dougsch@amazon.com>
    Co-authored-by: Adam Ruka <adamruka@amazon.com>

---
+++ examples/cloudformation/index.ts
@@ -0,0 +1,18 @@
+import { App, Stack } from 'aws-cdk';
+import { sqs } from 'aws-cdk-resources';
+
+class CloudFormationExample extends Stack {
+    constructor(parent: App) {
+        super(parent);
+
+        new sqs.QueueResource(this, 'MyQueue', {
+            visibilityTimeout: 300
+        });
+    }
+}
+
+const app = new App(process.argv);
+
+new CloudFormationExample(app);
+
+process.stdout.write(app.run());
---
+++ examples/sns-sqs/index.ts
@@ -0,0 +1,49 @@
+import { App, PolicyDocument, PolicyStatement, Stack } from "aws-cdk";
+import { sns, sqs } from 'aws-cdk-resources';
+import { Topic } from 'aws-cdk-sns';
+import { Queue } from 'aws-cdk-sqs';
+
+class ACL extends Stack {
+    constructor(parent: App, name: string) {
+        super(parent, name);
+
+        const topic = new Topic(this, 'MyTopic');
+        const queue = new Queue(this, 'MyQueue', {
+            visibilityTimeoutSec: 300
+        });
+
+        topic.subscribeQueue(queue);
+    }
+}
+
+class CFN extends Stack {
+    constructor(parent: App, name: string) {
+        super(parent, name);
+
+        const topic = new sns.TopicResource(this, 'MyTopic');
+        const queue = new sqs.QueueResource(this, 'MyQueue');
+
+        new sns.SubscriptionResource(this, 'TopicToQueue', {
+            topicArn: topic.ref, // ref == arn for topics
+            endpoint: queue.queueName,
+            protocol: 'sqs'
+        });
+
+        const policyDocument = new PolicyDocument();
+        policyDocument.addStatement(new PolicyStatement()
+            .addResource(queue.queueArn)
+            .addAction('sqs:SendMessage')
+            .addServicePrincipal('sns.amazonaws.com')
+            .setCondition('ArnEquals', { 'aws:SourceArn': topic.ref }));
+
+        new sqs.QueuePolicyResource(this, 'MyQueuePolicy', {
+            policyDocument,
+            queues: [ queue.ref ]
+        });
+    }
+}
+
+const app = new App(process.argv);
+new ACL(app, 'acl');
+new CFN(app, 'cfn');
+process.stdout.write(app.run());
