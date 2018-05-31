acff7353abed5ec7b6b5a7ff18abcef66eb0bf55
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2018-05-31 10:12:06 +0200

    [aws-cdk-resources] Enable declarations file, and fix problems that uncovered

---
+++ packages/aws-cdk-resources/test/test.dates.d.ts
@@ -0,0 +1,8 @@
+import { Test } from 'nodeunit';
+declare const _default: {
+    'Dates are validated correctly'(test: Test): void;
+    'Dates are formatted as ISO8601'(test: Test): void;
+    'Dates are always formatted in UTC'(test: Test): void;
+    'Invalid dates are considered invalid'(test: Test): void;
+};
+export = _default;
