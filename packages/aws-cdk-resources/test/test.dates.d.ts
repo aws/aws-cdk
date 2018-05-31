abddaf7b50e9ae17271fc0156d430626ae74e275
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2018-05-31 10:51:20 +0200

    Add missing entry in .gitignore & remove offending files.

--- packages/aws-cdk-resources/test/test.dates.d.ts
+++
@@ -1,8 +0,0 @@
-import { Test } from 'nodeunit';
-declare const _default: {
-    'Dates are validated correctly'(test: Test): void;
-    'Dates are formatted as ISO8601'(test: Test): void;
-    'Dates are always formatted in UTC'(test: Test): void;
-    'Invalid dates are considered invalid'(test: Test): void;
-};
-export = _default;
--- packages/aws-cdk-resources/test/test.tag.d.ts
+++
@@ -1,6 +0,0 @@
-import { Test } from 'nodeunit';
-declare const _default: {
-    'can use tags while modeling S3 buckets'(test: Test): void;
-    'can use tag-like property bags while modeling S3 buckets'(test: Test): void;
-};
-export = _default;
