c79b719f99fb2583e337a4f1af3dc913d5f2949c
Rico Huijbers <huijbers@amazon.com>
2018-05-31 14:56:19 +0200

    Remove region, use NodeJS instead of Python to pick apart the JSON

--- sign.sh
+++ sign.sh
@@ -8,7 +8,6 @@ if [[ "${2:-}" == "" ]]; then
     exit 1
 fi
 
-
 tmpdir=$(mktemp -d)
 trap "shred $tmpdir/* && rm -rf $tmpdir" EXIT
 
@@ -16,11 +15,16 @@ SECRET=CDK/$1/SigningKey
 
 # Use secrets manager to obtain the key and passphrase into a JSON file
 echo "Retrieving key $SECRET..." >&2
-aws --region us-east-1 secretsmanager get-secret-value --secret-id "$SECRET" --output text --query SecretString > $tmpdir/secret.txt
-passphrase=$(python -c "import json; print(json.load(file('$tmpdir/secret.txt'))['Passphrase'])")
+aws secretsmanager get-secret-value --secret-id "$SECRET" --output text --query SecretString > $tmpdir/secret.txt
+
+value-from-secret() {
+    node -e "console.log(JSON.parse(require('fs').readFileSync('$tmpdir/secret.txt', { encoding: 'utf-8' })).$1)"
+}
+
+passphrase=$(value-from-secret Passphrase)
 
 echo "Importing key..." >&2
-gpg --homedir $tmpdir --import <(python -c "import json; print(json.load(file('$tmpdir/secret.txt'))['PrivateKey'])")
+gpg --homedir $tmpdir --import <(value-from-secret PrivateKey)
 
 while [[ "${2:-}" != "" ]]; do
     echo "Signing $2..." >&2
