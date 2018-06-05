a6ccf063318fec922e6f9b73e2c46b6d457d1c4a
Elad Ben-Israel <benisrae@amazon.com>
2018-06-06 00:32:05 +0300

    Read current version from lerna.json

--- update-version.sh
+++ update-version.sh
@@ -2,4 +2,5 @@
 
 # HACK: lerna needs a git repo to work but publish will work nevertheless
 git init
-exec node_modules/.bin/lerna publish --skip-git --skip-npm --repo-version=0.6.0-pre+${CODEBUILD_RESOLVED_SOURCE_VERSION:0:7} --yes
+version="$(node -e "console.log(require('./lerna.json').version)")"
+exec node_modules/.bin/lerna publish --skip-git --skip-npm --repo-version=${version}-pre+${CODEBUILD_RESOLVED_SOURCE_VERSION:0:7} --yes
