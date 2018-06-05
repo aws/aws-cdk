aa96170c23c767f398924556d7ffc19cda4b0de5
Elad Ben-Israel <elad.benisrael@gmail.com>
2018-06-06 01:28:06 +0300

    Add commit hash to version during build (#25)
    * Add commit hash to version during build
    
    To ensure that builds can be traced to their
    originating commit when building in CI/CD, we
    use $CODEBUILD_RESOLVED_SOURCE_VERSION to
    append a short commit hash to the version.
    
    HACK: since codebuild doesn't really clone the repo,
    and lerna looks for ".git", we need to "git init"
    before calling "lerna publish", but it works.
    
    * Read current version from lerna.json
    * Use git HEAD commit if CODEBUILD_RESOLVED_SOURCE_VERSION is not defined

---
+++ update-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# HACK: lerna needs a git repo to work but publish will work nevertheless
+git init
+
+version="$(node -e "console.log(require('./lerna.json').version)")"
+commit="${CODEBUILD_RESOLVED_SOURCE_VERSION}"
+
+# CODEBUILD_RESOLVED_SOURCE_VERSION is not defined (i.e. local build or CodePipeline build),
+# use the HEAD commit hash
+if [ -z "${commit}" ]; then
+  commit="$(git rev-parse --verify HEAD)"
+fi
+
+exec node_modules/.bin/lerna publish --skip-git --skip-npm --repo-version=${version}-pre+${commit:0:7} --yes
