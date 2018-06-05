9d3c477d279266e16968ff3164d195c26d316f28
Elad Ben-Israel <benisrae@amazon.com>
2018-06-05 10:36:37 +0300

    fix(aws-cdk-codepipeline): InvokeLambdaAction
    1. Accepts a `LambdaRef` instead of function name
    2. Add `lambda:ListFunctions` and `lambda:InvokeFunction` to pipeline role.
    3. Add `codepipeline:PutJobXxxResult` to Lambda role.
    
    In order to avoid the cyclic dependency:
    
        Pipeline => Lambda
        Lambda => Role
        Role => Pipeline
    
    We follow the recommendation in the [CodePipeline docs][1]
    and use a "*" resource for  `codepipeline:PutJobXxxResult`
    for the Lambda execution policy.
    
    [1]: https://docs.aws.amazon.com/codepipeline/latest/userguide/actions-invoke-lambda-function.html
    
    BREAKING CHANGE:
    
    - The `DefaultBounds` function is no longer exported.
    - `GitHubSourceProps.oathToken` spelling fixed to `oauthToken`
    - `BuildAction` now takes an `inputArtifact` instead of `source` (allows
        using these actions for arbitrary activity within a pipeline).
    - `InvokeLambdaProps` takes `lambda` instead of `functionName`.
    - `InvokeLambda` renamed to `InvokeLambdaAction`

--- packages/aws-cdk-codepipeline/test/test.action.ts
+++ packages/aws-cdk-codepipeline/test/test.action.ts
@@ -1,7 +1,11 @@
 import { Stack } from 'aws-cdk';
+import { expect, haveResource } from 'aws-cdk-assert';
+import { Lambda, LambdaInlineCode, LambdaRuntime } from 'aws-cdk-lambda';
 import { Test } from 'nodeunit';
-import { Action, ActionCategory, Artifact, DefaultBounds, Pipeline, Stage } from '../lib';
+import { Action, ActionArtifactBounds, ActionCategory, Artifact, InvokeLambdaAction, Pipeline, Stage } from '../lib';
 import { validateArtifactBounds, validateSourceAction } from '../lib/validation';
+
+// tslint:disable:object-literal-key-quotes
 
 class TestAction extends Action {}
 
@@ -61,7 +65,7 @@ export = {
     'standard action with artifacts'(test: Test) {
         const stage = stageForTesting();
         const action = new TestAction(stage, 'TestAction', {
-            artifactBounds: DefaultBounds(),
+            artifactBounds: defaultBounds(),
             category: ActionCategory.Source,
             provider: 'test provider',
             configuration: { blah: 'bleep' }
@@ -83,14 +87,94 @@ export = {
             runOrder: 1
         });
         test.done();
-    }
+    },
 
+    'InvokeLambdaAction can be used to invoke lambda functions from a pipeline'(test: Test) {
+        const stack = new Stack();
+
+        const lambda = new Lambda(stack, 'Function', {
+            code: new LambdaInlineCode('bla'),
+            handler: 'index.handler',
+            runtime: LambdaRuntime.NodeJS43,
+        });
+
+        const pipeline = new Pipeline(stack, 'Pipeline');
+
+        new InvokeLambdaAction(new Stage(pipeline, 'Stage'), 'InvokeAction', {
+            lambda,
+            userParameters: 'foo-bar/42'
+        });
+
+        expect(stack).to(haveResource('AWS::CodePipeline::Pipeline', {
+            "ArtifactStore": {
+              "Location": {
+                "Ref": "PipelineArtifactsBucket22248F97"
+              },
+              "Type": "S3"
+            },
+            "RoleArn": {
+              "Fn::GetAtt": [
+                "PipelineRoleD68726F7",
+                "Arn"
+              ]
+            },
+            "Stages": [
+              {
+                "Actions": [
+                  {
+                    "ActionTypeId": {
+                      "Category": "Invoke",
+                      "Owner": "AWS",
+                      "Provider": "Lambda",
+                      "Version": "1"
+                    },
+                    "Configuration": {
+                      "FunctionName": {
+                        "Ref": "Function76856677"
+                      },
+                      "UserParameters": "foo-bar/42"
+                    },
+                    "InputArtifacts": [],
+                    "Name": "InvokeAction",
+                    "OutputArtifacts": [],
+                    "RunOrder": 1
+                  }
+                ],
+                "Name": "Stage"
+              }
+            ]
+        }));
+
+        expect(stack).to(haveResource('AWS::IAM::Policy', {
+            "PolicyDocument": {
+              "Statement": [
+                {
+                  "Action": [
+                    "codepipeline:PutJobSuccessResult",
+                    "codepipeline:PutJobFailureResult"
+                  ],
+                  "Effect": "Allow",
+                  "Resource": "*"
+                }
+              ],
+              "Version": "2012-10-17"
+            },
+            "PolicyName": "FunctionServiceRoleDefaultPolicy2F49994A",
+            "Roles": [
+              {
+                "Ref": "FunctionServiceRole675BB04A"
+              }
+            ]
+        }));
+
+        test.done();
+    }
 };
 
 function boundsValidationResult(numberOfArtifacts: number, min: number, max: number): string[] {
     const stage = stageForTesting();
     const action = new TestAction(stage, 'TestAction', {
-        artifactBounds: DefaultBounds(),
+        artifactBounds: defaultBounds(),
         category: ActionCategory.Test,
         provider: 'test provider'
     });
@@ -105,4 +189,13 @@ function stageForTesting(): Stage {
     const stack = new Stack();
     const pipeline = new Pipeline(stack, 'pipeline');
     return new Stage(pipeline, 'stage');
+}
+
+function defaultBounds(): ActionArtifactBounds {
+    return {
+        minInputs: 0,
+        maxInputs: 5,
+        minOutputs: 0,
+        maxOutputs: 5
+    };
 }
