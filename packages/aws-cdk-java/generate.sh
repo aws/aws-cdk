52585f91a784aa7233f83f32a497e0e4c79d38ee
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2018-06-07 12:30:08 +0200

    Update to JSII 0.4.0-pre+ddc9b9e

--- packages/aws-cdk-java/generate.sh
+++ packages/aws-cdk-java/generate.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
-outdir="src/main/java"
+outdir="src/main"
 rm -fr "${outdir}"
 for p in $(find-jsii-packages -k jsii.names.java); do
     echo "Generating java code for module $p"
-    jsii-pacmak --target java --outdir "${outdir}" $p
+    jsii-pacmak --target java --outdir "${outdir}/java" $p
 done
+
+# Moving the "resources" folder where mvn expects it to be...
+[ -d ${outdir}/java/resources ] && mv ${outdir}/java/resources ${outdir}
 
 mkdir -p target
 cp -f ../../node_modules/jsii-java-runtime/jsii-runtime-0.4.0.jar $PWD/target
