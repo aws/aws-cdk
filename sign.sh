c998d08041e5b5fa473d8c5f8fff39f596884d6a
Rico Huijbers <rix0rrr@gmail.com>
2018-06-07 11:02:58 +0200

    Sign binaries at end of build
    Signing will not happen if the environment variable that
    determines the key to use is not set.
    
    We no longer use different artifact types--we will have one key only.
    
    Also add docs about validating the download.

--- sign.sh
+++ sign.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ "${2:-}" == "" ]]; then
-    echo "Usage: sign.sh ARTIFACTTYPE FILE [FILE...]" >&2
+if [[ "${1:-}" == "" ]]; then
+    echo "Usage: sign.sh FILE [FILE...]" >&2
     echo "">&2
     echo "Creates detached signature as FILE.sig." >&2
     exit 1
 fi
 
+if [[ "${SIGNING_KEY_SCOPE:-}" == "" ]]; then
+    echo "SIGNING_KEY_SCOPE not set; not signing artifacts." >&2
+    exit 0
+fi
+
 tmpdir=$(mktemp -d)
 trap "shred $tmpdir/* && rm -rf $tmpdir" EXIT
 
-SECRET=CDK/$1/SigningKey
+SECRET=$SIGNING_KEY_SCOPE/SigningKey
 
 # Use secrets manager to obtain the key and passphrase into a JSON file
 echo "Retrieving key $SECRET..." >&2
@@ -26,8 +31,8 @@ passphrase=$(value-from-secret Passphrase)
 echo "Importing key..." >&2
 gpg --homedir $tmpdir --import <(value-from-secret PrivateKey)
 
-while [[ "${2:-}" != "" ]]; do
-    echo "Signing $2..." >&2
+while [[ "${1:-}" != "" ]]; do
+    echo "Signing $1..." >&2
     echo $passphrase | gpg \
         --homedir $tmpdir \
         --local-user aws-cdk@amazon.com \
