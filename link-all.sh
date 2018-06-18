19cbb55b8c222aa5892eb1ea66a2786b0fac12f6
Rico Huijbers <huijbers@amazon.com>
2018-06-18 20:29:13 +0200

    Fix symlink generating script to properly link @aws-cdk scoped packages

--- link-all.sh
+++ link-all.sh
@@ -4,8 +4,19 @@
 set -euo pipefail
 root="$(cd $(dirname $0) && pwd)"
 
+mkdir -p node_modules
+
 modules="$(find ${root}/packages -name package.json | grep -v node_modules | grep -v init-templates | xargs -n1 dirname)"
 for module in ${modules}; do
-  echo "${module} => node_modules/$(basename ${module})"
-  ln -s ${module} node_modules/
+  if [[ $(basename $(dirname $module)) == @* ]]; then
+    # This is a @scope/based package
+    scope=$(basename $(dirname $module))
+    link_dir=node_modules/$(basename $(dirname $module))
+    mkdir -p $link_dir
+  else
+    link_dir=node_modules
+  fi
+
+  echo "${module} => $link_dir/$(basename $module)"
+  ln -fs ${module} $link_dir
 done
