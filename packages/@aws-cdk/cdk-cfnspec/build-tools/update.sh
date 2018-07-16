e75a7f7306348789c04668c22297da308f4a5e26
Elad Ben-Israel <benisrae@amazon.com>
2018-07-16 03:46:53 -0700

    cdk-cfnspec: Further update improvement (#345)
    * Auto-create a CHANGELOG entry which describes
      the updates to the spec (which resources were
      added, properties, property types, etc).
    * Invoke `create-missing-libraries.sh` after each
      update to initialize AWS construct libraries for
      new resource types.

--- packages/@aws-cdk/cdk-cfnspec/build-tools/update.sh
+++ packages/@aws-cdk/cdk-cfnspec/build-tools/update.sh
@@ -7,15 +7,49 @@
 
 set -euo pipefail
 
-src="spec-source"
+function update-spec() {
+    local title=$1
+    local url=$2
+    local target=$3
+    local gunzip=$4
 
-rm -f ${src}/000_CloudFormationResourceSpecification.json
-curl -L "https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json" \
-    | gunzip - > ${src}/000_CloudFormationResourceSpecification.json
+    local intermediate="$(mktemp -d)/new.json"
 
-rm -fr ${src}/000_sam.spec.json
-curl -L "https://raw.githubusercontent.com/awslabs/goformation/master/generate/sam-2016-10-31.json" \
-    > ${src}/000_sam.spec.json
+    echo >&2 "Downloading from ${url}..."
+    if ${gunzip}; then
+        curl -sL "${url}" | gunzip - > ${intermediate}
+    else
+        curl -sL "${url}" > ${intermediate}
+    fi
 
-# sort hash keys to help identify *real* changes
-sort-json ${src}/*.json
+    echo >&2 "Sorting..."
+    sort-json ${intermediate}
+
+    echo >&2 "Updaging CHANGELOG.md..."
+    touch CHANGELOG.md
+    mv CHANGELOG.md CHANGELOG.md.bak
+    rm -f CHANGELOG.md
+    node build-tools/spec-diff.js "${title}" "${target}" "${intermediate}" > CHANGELOG.md
+    echo "" >> CHANGELOG.md
+    cat CHANGELOG.md.bak >> CHANGELOG.md
+    rm CHANGELOG.md.bak
+
+    echo >&2 "Updarting source spec..."
+    rm -f ${target}
+    cp ${intermediate} ${target}
+}
+
+update-spec \
+    "CloudFormation Resource Specification" \
+    "https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json" \
+    spec-source/000_CloudFormationResourceSpecification.json \
+    true
+
+update-spec \
+    "Serverless Application Model (SAM) Resource Specification" \
+    "https://raw.githubusercontent.com/awslabs/goformation/master/generate/sam-2016-10-31.json" \
+    spec-source/000_sam.spec.json \
+    false
+
+echo >&2 "Creating missing AWS construct libraries for new resource types..."
+(cd ../../.. && ./create-missing-libraries.sh)
