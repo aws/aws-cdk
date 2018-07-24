90767cbe72f5f575c5f3fdcb5bbf198cf3c209e8
Mike Cowgill <moofish32@gmail.com>
2018-07-24 13:29:56 -0700

    Customize VpcNetwork configuration (#250)
    BREAKING CHANGE:
    
     * Improved IP space allocation to be more efficient (breaking change to existing VPC stacks)
     * Customize subnet IP space with `SubnetConfiguration` arrays
     * Defined 3 distinct subnet types (Public, Private, and Isolated)
     * Added ability to control the number of NAT Gateways and automated creation based on the existence of `Private` subnets
     * Standardized `MapPublicIpOnLaunch` for enabled in Public and disabled in other subnet types
     * Enable the ability to evenly split remaining IP space across a set of subnet configurations (e.g. allocate public space and then split remaining network space across 1 to N subnets)
     * Support existing default when no configuration data is provided

--- packages/@aws-cdk/aws-ec2/test/test.vpc.ts
+++ packages/@aws-cdk/aws-ec2/test/test.vpc.ts
@@ -1,30 +1,32 @@
 import { countResources, expect, haveResource } from '@aws-cdk/assert';
-import {  AvailabilityZoneProvider, Stack } from '@aws-cdk/cdk';
+import { AvailabilityZoneProvider, Stack } from '@aws-cdk/cdk';
 import { Test } from 'nodeunit';
-import { DefaultInstanceTenancy, OutboundTrafficMode, VpcNetwork } from '../lib';
+import { DefaultInstanceTenancy, SubnetType, VpcNetwork } from '../lib';
 
 export = {
 
-    "When creating a VPC with the default CIDR range": {
+    "When creating a VPC": {
+        "with the default CIDR range": {
 
-        "vpc.vpcId returns a token to the VPC ID"(test: Test) {
-            const stack = getTestStack();
-            const vpc = new VpcNetwork(stack, 'TheVPC');
-            test.deepEqual(vpc.vpcId.resolve(), {Ref: 'TheVPC92636AB0' } );
-            test.done();
-        },
+            "vpc.vpcId returns a token to the VPC ID"(test: Test) {
+                const stack = getTestStack();
+                const vpc = new VpcNetwork(stack, 'TheVPC');
+                test.deepEqual(vpc.vpcId.resolve(), {Ref: 'TheVPC92636AB0' } );
+                test.done();
+            },
 
-        "it uses the correct network range"(test: Test) {
-            const stack =  getTestStack();
-            new VpcNetwork(stack, 'TheVPC');
-            expect(stack).to(haveResource('AWS::EC2::VPC', {
-                CidrBlock: VpcNetwork.DEFAULT_CIDR_RANGE,
-                EnableDnsHostnames: true,
-                EnableDnsSupport: true,
-                InstanceTenancy: DefaultInstanceTenancy.Default,
-                Tags: []
-            }));
-            test.done();
+            "it uses the correct network range"(test: Test) {
+                const stack =  getTestStack();
+                new VpcNetwork(stack, 'TheVPC');
+                expect(stack).to(haveResource('AWS::EC2::VPC', {
+                    CidrBlock: VpcNetwork.DEFAULT_CIDR_RANGE,
+                    EnableDnsHostnames: true,
+                    EnableDnsSupport: true,
+                    InstanceTenancy: DefaultInstanceTenancy.Default,
+                    Tags: []
+                }));
+                test.done();
+            }
         },
 
         "with all of the properties set, it successfully sets the correct VPC properties"(test: Test) {
@@ -38,7 +40,6 @@ export = {
                 enableDnsHostnames: false,
                 enableDnsSupport: false,
                 defaultInstanceTenancy: DefaultInstanceTenancy.Dedicated,
-                outboundTraffic: OutboundTrafficMode.None,
                 tags: [tag]
             });
 
@@ -62,36 +63,183 @@ export = {
             test.done();
         },
 
-        "with outbound traffic mode None, the VPC should not contain an IGW or NAT Gateways"(test: Test) {
+        "with only isolated subnets, the VPC should not contain an IGW or NAT Gateways"(test: Test) {
             const stack = getTestStack();
-            new VpcNetwork(stack, 'TheVPC', { outboundTraffic: OutboundTrafficMode.None });
+            new VpcNetwork(stack, 'TheVPC', {
+                subnetConfiguration: [
+                    {
+                        subnetType: SubnetType.Isolated,
+                        name: 'Isolated',
+                    }
+                ]
+            });
             expect(stack).notTo(haveResource("AWS::EC2::InternetGateway"));
             expect(stack).notTo(haveResource("AWS::EC2::NatGateway"));
+            expect(stack).to(haveResource("AWS::EC2::Subnet", {
+                MapPublicIpOnLaunch: false
+            }));
             test.done();
         },
 
-        "with outbound traffic mode FromPublicSubnetsOnly, the VPC should have an IGW but no NAT Gateways"(test: Test) {
+        "with no private subnets, the VPC should have an IGW but no NAT Gateways"(test: Test) {
             const stack = getTestStack();
-            new VpcNetwork(stack, 'TheVPC', { outboundTraffic: OutboundTrafficMode.FromPublicSubnetsOnly });
+            new VpcNetwork(stack, 'TheVPC', {
+                subnetConfiguration: [
+                    {
+                        subnetType: SubnetType.Public,
+                        name: 'Public',
+                    },
+                    {
+                        subnetType: SubnetType.Isolated,
+                        name: 'Isolated',
+                    }
+                ]
+            });
             expect(stack).to(countResources('AWS::EC2::InternetGateway', 1));
             expect(stack).notTo(haveResource("AWS::EC2::NatGateway"));
             test.done();
         },
 
-        "with outbound traffic mode FromPublicAndPrivateSubnets, the VPC should have an IGW, and a NAT Gateway per AZ"(test: Test) {
+        "with no subnets defined, the VPC should have an IGW, and a NAT Gateway per AZ"(test: Test) {
             const stack = getTestStack();
             const zones = new AvailabilityZoneProvider(stack).availabilityZones.length;
-            new VpcNetwork(stack, 'TheVPC', { outboundTraffic: OutboundTrafficMode.FromPublicAndPrivateSubnets });
+            new VpcNetwork(stack, 'TheVPC', { });
             expect(stack).to(countResources("AWS::EC2::InternetGateway", 1));
             expect(stack).to(countResources("AWS::EC2::NatGateway", zones));
             test.done();
         },
 
+        "with custom subents, the VPC should have the right number of subnets, an IGW, and a NAT Gateway per AZ"(test: Test) {
+            const stack = getTestStack();
+            const zones = new AvailabilityZoneProvider(stack).availabilityZones.length;
+            new VpcNetwork(stack, 'TheVPC', {
+              cidr: '10.0.0.0/21',
+              subnetConfiguration: [
+                {
+                  cidrMask: 24,
+                  name: 'ingress',
+                  subnetType: SubnetType.Public,
+                },
+                {
+                  cidrMask: 24,
+                  name: 'application',
+                  subnetType: SubnetType.Private,
+                },
+                {
+                  cidrMask: 28,
+                  name: 'rds',
+                  subnetType: SubnetType.Isolated,
+                }
+              ],
+              maxAZs: 3
+            });
+            expect(stack).to(countResources("AWS::EC2::InternetGateway", 1));
+            expect(stack).to(countResources("AWS::EC2::NatGateway", zones));
+            expect(stack).to(countResources("AWS::EC2::Subnet", 9));
+            for (let i = 0; i < 6; i++) {
+              expect(stack).to(haveResource("AWS::EC2::Subnet", {
+                CidrBlock: `10.0.${i}.0/24`
+              }));
+            }
+            for (let i = 0; i < 3; i++) {
+              expect(stack).to(haveResource("AWS::EC2::Subnet", {
+                CidrBlock: `10.0.6.${i * 16}/28`
+              }));
+            }
+            test.done();
+        },
+        "with custom subents and natGateways = 2 there should be only to NATGW"(test: Test) {
+            const stack = getTestStack();
+            new VpcNetwork(stack, 'TheVPC', {
+              cidr: '10.0.0.0/21',
+              natGateways: 2,
+              subnetConfiguration: [
+                {
+                  cidrMask: 24,
+                  name: 'ingress',
+                  subnetType: SubnetType.Public,
+                },
+                {
+                  cidrMask: 24,
+                  name: 'application',
+                  subnetType: SubnetType.Private,
+                },
+                {
+                  cidrMask: 28,
+                  name: 'rds',
+                  subnetType: SubnetType.Isolated,
+                }
+              ],
+              maxAZs: 3
+            });
+            expect(stack).to(countResources("AWS::EC2::InternetGateway", 1));
+            expect(stack).to(countResources("AWS::EC2::NatGateway", 2));
+            expect(stack).to(countResources("AWS::EC2::Subnet", 9));
+            for (let i = 0; i < 6; i++) {
+              expect(stack).to(haveResource("AWS::EC2::Subnet", {
+                CidrBlock: `10.0.${i}.0/24`
+              }));
+            }
+            for (let i = 0; i < 3; i++) {
+              expect(stack).to(haveResource("AWS::EC2::Subnet", {
+                CidrBlock: `10.0.6.${i * 16}/28`
+              }));
+            }
+            test.done();
+        },
         "with enableDnsHostnames enabled but enableDnsSupport disabled, should throw an Error"(test: Test) {
             const stack = getTestStack();
             test.throws(() => new VpcNetwork(stack, 'TheVPC', {
                 enableDnsHostnames: true,
                 enableDnsSupport: false
+            }));
+            test.done();
+        },
+        "with public subnets MapPublicIpOnLaunch is true"(test: Test) {
+            const stack = getTestStack();
+            new VpcNetwork(stack, 'VPC', {
+                maxAZs: 1,
+                subnetConfiguration: [
+                    {
+                        cidrMask: 24,
+                        name: 'ingress',
+                        subnetType: SubnetType.Public,
+                    }
+                ],
+            });
+            expect(stack).to(countResources("AWS::EC2::Subnet", 1));
+            expect(stack).notTo(haveResource("AWS::EC2::NatGateway"));
+            expect(stack).to(haveResource("AWS::EC2::Subnet", {
+                MapPublicIpOnLaunch: true
+            }));
+            test.done();
+        },
+        "with maxAZs set to 2"(test: Test) {
+            const stack = getTestStack();
+            new VpcNetwork(stack, 'VPC', { maxAZs: 2 });
+            expect(stack).to(countResources("AWS::EC2::Subnet", 4));
+            expect(stack).to(countResources("AWS::EC2::Route", 4));
+            for (let i = 0; i < 4; i++) {
+              expect(stack).to(haveResource("AWS::EC2::Subnet", {
+                CidrBlock: `10.0.${i * 64}.0/18`
+              }));
+            }
+            expect(stack).to(haveResource("AWS::EC2::Route", {
+                DestinationCidrBlock: '0.0.0.0/0',
+                NatGatewayId: { },
+            }));
+            test.done();
+        },
+        "with natGateway set to 1"(test: Test) {
+            const stack = getTestStack();
+            new VpcNetwork(stack, 'VPC', { natGateways: 1 });
+            expect(stack).to(countResources("AWS::EC2::Subnet", 6));
+            expect(stack).to(countResources("AWS::EC2::Route", 6));
+            expect(stack).to(countResources("AWS::EC2::Subnet", 6));
+            expect(stack).to(countResources("AWS::EC2::NatGateway", 1));
+            expect(stack).to(haveResource("AWS::EC2::Route", {
+                DestinationCidrBlock: '0.0.0.0/0',
+                NatGatewayId: { },
             }));
             test.done();
         }
@@ -108,8 +256,7 @@ export = {
             test.done();
         }
 
-    }
-
+    },
 };
 
 function getTestStack(): Stack {
