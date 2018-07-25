73ebb6e1771fb4888a26b55f3189dfe7ead610fd
Elad Ben-Israel <benisrae@amazon.com>
2018-07-25 15:20:37 +0300

    Add tests

---
+++ packages/@aws-cdk/aws-lambda/test/integ.assets.file.ts
@@ -0,0 +1,23 @@
+import cdk = require('@aws-cdk/core');
+import path = require('path');
+import { Lambda, LambdaCode, LambdaRuntime } from '../lib';
+
+class TestStack extends cdk.Stack {
+    constructor(parent: cdk.App, id: string) {
+        super(parent, id);
+
+        /// !show
+        new Lambda(this, 'MyLambda', {
+            code: LambdaCode.file(path.join(__dirname, 'handler.zip')),
+            handler: 'index.main',
+            runtime: LambdaRuntime.Python36
+        });
+        /// !hide
+    }
+}
+
+const app = new cdk.App(process.argv);
+
+new TestStack(app, 'lambda-test-assets-file');
+
+process.stdout.write(app.run());
