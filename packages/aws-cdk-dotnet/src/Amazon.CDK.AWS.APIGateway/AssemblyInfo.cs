27a033e180729abd46c2cab60517097656e4972e
Matthew Pirocchi <pirocchi@amazon.com>
2018-07-26 17:08:40 +0000

    Remove generated code.

--- packages/aws-cdk-dotnet/src/Amazon.CDK.AWS.APIGateway/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using AWS.Jsii.Runtime.Deputy;
-
-[assembly: JsiiAssembly("@aws-cdk/aws-apigateway", "0.7.3-beta", "aws-cdk-aws-apigateway-0.7.3-beta.tgz")]
--- packages/aws-cdk-dotnet/src/Amazon.CDK.AWS.IAM/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using AWS.Jsii.Runtime.Deputy;
-
-[assembly: JsiiAssembly("@aws-cdk/aws-iam", "0.7.3-beta", "aws-cdk-aws-iam-0.7.3-beta.tgz")]
