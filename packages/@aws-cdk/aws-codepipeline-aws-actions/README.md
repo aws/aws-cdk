9ca84ff343161a9ec8f6eb5b985e58bdc09261b6
Adam Ruka <adamruka@amazon.com>
2018-07-27 14:27:28 -0700

    Introduce the 'aws-codepipeline-aws-actions' package.

---
+++ packages/@aws-cdk/aws-codepipeline-aws-actions/README.md
@@ -0,0 +1,23 @@
+## AWS Actions for AWS Code Pipeline
+
+This module contains all of the integrations for CodePipeline
+with other AWS services.
+
+Example usage:
+
+```ts
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import codepipelineAwsActions = require('@aws-cdk/aws-codepipeline-aws-actions');
+
+const pipeline = new codepipeline.Pipeline(this, 'MyPipeline');
+
+const sourceStage = new codepipeline.Stage(pipeline, 'Source');
+new codepipelineAwsActions.codecommit.PipelineSource(sourceStage, 'Source', {
+    // ...
+});
+
+const buildStage = new codepipeline.Stage(pipeline, 'Build');
+new codepipelineAwsActions.codebuild.PipelineBuildAction(buildStage, 'Build', {
+    // ...
+});
+```
---
+++ packages/@aws-cdk/aws-codepipeline-aws-actions/lib/index.ts
@@ -0,0 +1,20 @@
+import codebuild_ = require('@aws-cdk/aws-codebuild-codepipeline');
+
+export namespace codebuild {
+    export import PipelineBuildActionProps = codebuild_.PipelineBuildActionProps;
+    export import PipelineBuildAction = codebuild_.PipelineBuildAction;
+}
+
+import codecommit_ = require('@aws-cdk/aws-codecommit-codepipeline');
+
+export namespace codecommit {
+    export import PipelineSourceProps = codecommit_.PipelineSourceProps;
+    export import PipelineSource = codecommit_.PipelineSource;
+}
+
+import lambda_ = require('@aws-cdk/aws-lambda-codepipeline');
+
+export namespace lambda {
+    export import PipelineInvokeAction = lambda_.PipelineInvokeAction;
+    export import PipelineInvokeActionProps = lambda_.PipelineInvokeActionProps;
+}
