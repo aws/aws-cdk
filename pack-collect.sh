bf08f29c0a3648f9de155067c2948b338d4027a7
Elad Ben-Israel <benisrae@amazon.com>
2018-08-01 16:26:35 +0300

    CR fixes

--- pack-collect.sh
+++ pack-collect.sh
@@ -2,8 +2,9 @@
 # Merges all files from all "dist/" directories under "packages/" into "./pack"
 # Executed by pack.sh as a preparation for beta-bundle.sh
 set -euo pipefail
-mkdir -p pack
+target="pack"
+mkdir -p ${target}
 for dir in $(find packages -name dist | grep -v node_modules); do
-  echo $dir
-  rsync -av $dir/ pack/
+  echo "Merging ${dir} into ${target}"
+  rsync -av $dir/ ${target}/
 done
