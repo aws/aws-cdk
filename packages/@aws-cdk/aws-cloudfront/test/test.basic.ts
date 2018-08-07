64c6a32f4e568620dcd40ac18241bfa79d849a55
💅 💃 🌈 Miley <brelandm@amazon.com>
2018-08-06 17:51:27 -0700

    Adding a few more tests

--- packages/@aws-cdk/aws-cloudfront/test/test.basic.ts
+++ packages/@aws-cdk/aws-cloudfront/test/test.basic.ts
@@ -7,6 +7,94 @@ import { CloudFrontWebDistribution } from '../lib';
 // tslint:disable:object-literal-key-quotes
 
 export = {
+
+    'distribution with custom origin adds custom origin'(test: Test) {
+      const stack = new cdk.Stack();
+
+      new CloudFrontWebDistribution(stack, 'AnAmazingWebsiteProbably', {
+          originConfigs: [
+              {
+                  originHeaders: {
+                    "X-Custom-Header": "somevalue",
+                  },
+                  customOriginSource: {
+                    domainName: "myorigin.com",
+                  },
+                  behaviors: [
+                    {
+                      isDefaultBehavior: true,
+                    }
+                  ],
+              }
+          ]
+      });
+
+      expect(stack).toMatch(
+        {
+          "Resources": {
+          "AnAmazingWebsiteProbablyCFDistribution47E3983B": {
+            "Type": "AWS::CloudFront::Distribution",
+            "Properties": {
+              "DistributionConfig": {
+                "CacheBehaviors": [],
+                "DefaultCacheBehavior": {
+                  "AllowedMethods": [
+                    "GET",
+                    "HEAD"
+                  ],
+                  "CachedMethods": [
+                    "GET",
+                    "HEAD"
+                  ],
+                  "ForwardedValues": {
+                    "Cookies": {
+                      "Forward": "none"
+                    },
+                    "QueryString": false
+                  },
+                  "TargetOriginId": "origin1",
+                  "ViewerProtocolPolicy": "https-only"
+                },
+                "DefaultRootObject": "index.html",
+                "Enabled": true,
+                "HttpVersion": "http2",
+                "IPV6Enabled": true,
+                "Origins": [
+                  {
+                    "CustomOriginConfig": {
+                      "HTTPPort": 80,
+                      "HTTPSPort": 443,
+                      "OriginKeepaliveTimeout": 5,
+                      "OriginProtocolPolicy": "https-only",
+                      "OriginReadTimeout": 30,
+                      "OriginSSLProtocols": [
+                        "TLSv1.2"
+                      ]
+                    },
+                    "DomainName": "myorigin.com",
+                    "Id": "origin1",
+                    "OriginCustomHeaders": [
+                      {
+                        "HeaderName": "X-Custom-Header",
+                        "HeaderValue": "somevalue"
+                      }
+                    ]
+                  }
+                ],
+                "PriceClass": "PriceClass_100",
+                "ViewerCertificate": {
+                  "CloudFrontDefaultCertificate": true
+                }
+              }
+            }
+          }
+        }
+      }
+      );
+
+      test.done();
+    },
+
     'most basic distribution'(test: Test) {
         const stack = new cdk.Stack();
         const sourceBucket = new s3.Bucket(stack, 'Bucket');
