7b6f3c1ffff312293ab97ff670175f24db3c3b43
Breland Miley <breland@bdawg.org>
2018-08-07 14:58:16 -0700

    Fix CloudFront origin error (#514)
    * Fix CloudFront origin error
    
    When using CloudFront without an origin access identity, we were failing to
    inlclude the "empty" property S3OriginConfig - this fixes that.
    
    Also adds an integ test and a validate that verifies this.
    
    Should fix #508

---
+++ packages/@aws-cdk/aws-cloudfront/test/integ.cloudfront.ts
@@ -0,0 +1,23 @@
+
+import s3 = require('@aws-cdk/aws-s3');
+import cdk = require('@aws-cdk/cdk');
+import cloudfront = require('../lib');
+
+const app = new cdk.App(process.argv);
+
+const stack = new cdk.Stack(app, 'aws-cdk-cloudfront');
+
+const sourceBucket = new s3.Bucket(stack, 'Bucket');
+
+new cloudfront.CloudFrontWebDistribution(stack, 'MyDistribution', {
+    originConfigs: [
+        {
+            s3OriginSource: {
+                s3BucketSource: sourceBucket
+            },
+            behaviors : [ {isDefaultBehavior: true}]
+        }
+    ]
+ });
+
+process.stdout.write(app.run());
