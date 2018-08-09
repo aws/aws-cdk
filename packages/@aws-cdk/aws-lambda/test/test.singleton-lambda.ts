b8ddf16f36adca03728dd956b86df8dca07108ad
Rico Huijbers <rix0rrr@gmail.com>
2018-08-09 15:02:34 +0200

    BREAKING: remove '@aws-cdk/aws-custom-resources' (#513)
    Since this package does not correspond to an AWS service,
    it should not be named with an 'aws-' prefix.
    
    Fixes #483.

--- packages/@aws-cdk/aws-lambda/test/test.singleton-lambda.ts
+++ packages/@aws-cdk/aws-lambda/test/test.singleton-lambda.ts
@@ -1,8 +1,7 @@
 import { expect, matchTemplate } from '@aws-cdk/assert';
-import lambda = require('@aws-cdk/aws-lambda');
 import cdk = require('@aws-cdk/cdk');
 import { Test } from 'nodeunit';
-import { SingletonLambda } from '../lib';
+import lambda = require('../lib');
 
 export = {
   'can add same singleton Lambda multiple times, only instantiated once in template'(test: Test) {
@@ -11,7 +10,7 @@ export = {
 
     // WHEN
     for (let i = 0; i < 5; i++) {
-      new SingletonLambda(stack, `Singleton${i}`, {
+      new lambda.SingletonLambda(stack, `Singleton${i}`, {
         uuid: '84c0de93-353f-4217-9b0b-45b6c993251a',
         code: new lambda.LambdaInlineCode('def hello(): pass'),
         runtime: lambda.LambdaRuntime.Python27,
