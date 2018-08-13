dfb7106a9ca6bfcdb7161405e27c06ed99e9132a
Elad Ben-Israel <benisrae@amazon.com>
2018-08-13 19:09:09 +0300

    feat(aws-s3): Bucket Notifications (#201)
    Adds support for S3 bucket notifications. The `bucket.onEvent`
    method will add a notification destination for a bucket.
    
    The s3.INotificationDestination interface is used
    to allow SNS, SQS and Lambda to implement notification
    destinations. This interface inverts the control and
    allows the destination to prepare to receive notifications.
    For example, it can modify it's policy appropriately.
    
    Since CloudFormation bucket notification support require two-phase
    deployments (due to the fact PutBucketNotification will fail if
    the destination policy has not been updated, and CloudFormation
    cannot create the policy until the bucket is created). The reason this
    is a limitation in CloudFormation is that they could not model the
    1:1 relationship between the bucket and the notifications using the
    current semantics of CloudFormation.
    
    In the CDK, we can model this relationship by encapsulating the
    notifications custom resource behind a bucket. This means that users
    don't interact with this resource directly, but rather just subscribe
    to notifications on a bucket, and the resource (and accompanying handler)
    will be created as needed.

--- packages/@aws-cdk/aws-s3/lib/index.ts
+++ packages/@aws-cdk/aws-s3/lib/index.ts
@@ -1,6 +1,7 @@
 export * from './bucket';
 export * from './bucket-policy';
 export * from './rule';
+export * from './notification-dest';
 
 // AWS::S3 CloudFormation Resources:
 export * from './s3.generated';
