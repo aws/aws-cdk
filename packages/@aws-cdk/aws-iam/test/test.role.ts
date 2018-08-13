0a71f15d4494d2011ee01608437fa0df0544a51b
Elad Ben-Israel <benisrae@amazon.com>
2018-08-13 12:25:32 +0300

    feat(aws-iam): support maxSessionDuration for Role (#545)
    Allow specifying a maximum session duration for roles.
    
    Fixes #543

--- packages/@aws-cdk/aws-iam/test/test.role.ts
+++ packages/@aws-cdk/aws-iam/test/test.role.ts
@@ -117,5 +117,64 @@ export = {
         }));
 
         test.done();
+    },
+
+    'maxSessionDuration': {
+
+        'is not specified by default'(test: Test) {
+            const stack = new Stack();
+            new Role(stack, 'MyRole', { assumedBy: new ServicePrincipal('sns.amazonaws.com') });
+            expect(stack).toMatch({
+                Resources: {
+                  MyRoleF48FFE04: {
+                    Type: "AWS::IAM::Role",
+                    Properties: {
+                      AssumeRolePolicyDocument: {
+                        Statement: [
+                          {
+                            Action: "sts:AssumeRole",
+                            Effect: "Allow",
+                            Principal: {
+                              Service: "sns.amazonaws.com"
+                            }
+                          }
+                        ],
+                        Version: "2012-10-17"
+                      }
+                    }
+                  }
+                }
+            });
+            test.done();
+        },
+
+        'can be used to specify the maximum session duration for assuming the role'(test: Test) {
+            const stack = new Stack();
+
+            new Role(stack, 'MyRole', { maxSessionDurationSec: 3700, assumedBy: new ServicePrincipal('sns.amazonaws.com') });
+
+            expect(stack).to(haveResource('AWS::IAM::Role', {
+                MaxSessionDuration: 3700
+            }));
+
+            test.done();
+        },
+
+        'must be between 3600 and 43200'(test: Test) {
+            const stack = new Stack();
+
+            const assumedBy = new ServicePrincipal('bla');
+
+            new Role(stack, 'MyRole1', { assumedBy, maxSessionDurationSec: 3600 });
+            new Role(stack, 'MyRole2', { assumedBy, maxSessionDurationSec: 43200 });
+
+            const expected = (val: any) => `maxSessionDuration is set to ${val}, but must be >= 3600sec (1hr) and <= 43200sec (12hrs)`;
+            test.throws(() => new Role(stack, 'MyRole3', { assumedBy, maxSessionDurationSec: 60 }), expected(60));
+            test.throws(() => new Role(stack, 'MyRole4', { assumedBy, maxSessionDurationSec: 3599 }), expected(3599));
+            test.throws(() => new Role(stack, 'MyRole5', { assumedBy, maxSessionDurationSec: 43201 }), expected(43201));
+
+            test.done();
+        }
     }
+
 };
