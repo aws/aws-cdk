c182690ea5131b536b71dc89b1e6a1cd2e1b7664
Elad Ben-Israel <benisrae@amazon.com>
2018-08-16 17:19:49 +0300

    fix(s3): bucket notification dependencies
    Allow bucket notification destinations to specify
    dependency elements that are added to the bucket notification resource.
    
    This is to ensure that S3 will be able to validate the subscription
    when notifications are configured.

--- packages/@aws-cdk/aws-sqs/lib/policy.ts
+++ packages/@aws-cdk/aws-sqs/lib/policy.ts
@@ -1,4 +1,4 @@
-import { Construct, PolicyDocument } from '@aws-cdk/cdk';
+import { Construct, IDependable, PolicyDocument } from '@aws-cdk/cdk';
 import { QueueRef } from './queue-ref';
 import { cloudformation } from './sqs.generated';
 
@@ -12,18 +12,25 @@ export interface QueuePolicyProps {
 /**
  * Applies a policy to SQS queues.
  */
-export class QueuePolicy extends Construct {
+export class QueuePolicy extends Construct implements IDependable {
     /**
      * The IAM policy document for this policy.
      */
     public readonly document = new PolicyDocument();
 
+    /**
+     * Allows adding QueuePolicy as a dependency.
+     */
+    public readonly dependencyElements = new Array<IDependable>();
+
     constructor(parent: Construct, name: string, props: QueuePolicyProps) {
         super(parent, name);
 
-        new cloudformation.QueuePolicyResource(this, 'Resource', {
+        const resource = new cloudformation.QueuePolicyResource(this, 'Resource', {
             policyDocument: this.document,
             queues: props.queues.map(q => q.queueUrl)
         });
+
+        this.dependencyElements.push(resource);
     }
 }
