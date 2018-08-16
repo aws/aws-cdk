2d49aee4c8fc5a994983564a14aa4c3dbdd2578e
James Cooke <james@jaynne.co.uk>
2018-08-16 06:09:48 +0100

    fix(aws-rds): unable to create cluster with a single instance (#578)
    The docs state that you should be able to create a cluster with a single instance however there is a trivial error that prevents this.

--- packages/@aws-cdk/aws-rds/test/test.cluster.ts
+++ packages/@aws-cdk/aws-rds/test/test.cluster.ts
@@ -57,7 +57,37 @@ export = {
         // THEN: No error
 
         test.done();
-    }
+    },
+    'can create a cluster with a single instance'(test: Test) {
+        // GIVEN
+        const stack = testStack();
+        const vpc = new ec2.VpcNetwork(stack, 'VPC');
+
+        // WHEN
+        new DatabaseCluster(stack, 'Database', {
+            engine: DatabaseClusterEngine.Aurora,
+            instances: 1,
+            masterUser: {
+                username: new Username('admin'),
+                password: new Password('tooshort'),
+            },
+            instanceProps: {
+                instanceType: new ec2.InstanceTypePair(ec2.InstanceClass.Burstable2, ec2.InstanceSize.Small),
+                vpc
+            }
+        });
+
+        // THEN
+        expect(stack).to(haveResource('AWS::RDS::DBCluster', {
+            Engine: "aurora",
+            DBSubnetGroupName: { Ref: "DatabaseSubnets56F17B9A" },
+            MasterUsername: "admin",
+            MasterUserPassword: "tooshort",
+            VpcSecurityGroupIds: [ {"Fn::GetAtt": ["DatabaseSecurityGroup5C91FDCB", "GroupId"]}]
+        }));
+
+        test.done();
+    },
 };
 
 function testStack() {
