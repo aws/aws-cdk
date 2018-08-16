f6e1b37152cf7a7f050f9f977e716f9aa4605856
Rico Huijbers <rix0rrr@gmail.com>
2018-08-16 17:10:30 +0200

    BREAKING(aws-ec2): SecurityGroup can be used in Connections (#582)
    Refactoring of the object model for connection/security groups so that a
    SecurityGroup object can be used as the target of an .allowTo()
    statement:
    
        cluster.connections.allowTo(securityGroup)
    
    SecurityGroupRef is now abstract and needs to be constructed using
    SecurityGroupRef.import(). This is also the mechanism for importing
    a non-constructed SecurityGroup into the construct tree. 
    
    As part of the refactoring:
    
    - Get rid of IDefaultConnectable, the functionality has been folded into
      IConnectable/Connections.
    - Get rid of ISecurityGroup.
    - Rename IConnectionPeer => ISecurityGroupRule.
    - Drastically simplify implementation, get rid of recursion and classes
      to enable the recursion to terminate. All complex logic is now nicely
      contained within Connections.
    
    This change is BREAKING to connections-enabled construct writers, but
    transparent to application builders.
    
    Fixes #579.

--- packages/@aws-cdk/aws-ec2/lib/index.ts
+++ packages/@aws-cdk/aws-ec2/lib/index.ts
@@ -1,10 +1,10 @@
 export * from './auto-scaling-group';
-export * from './connection';
 export * from './connections';
 export * from './instance-types';
 export * from './load-balancer';
 export * from './machine-image';
 export * from './security-group';
+export * from './security-group-rule';
 export * from './vpc';
 export * from './vpc-ref';
 
