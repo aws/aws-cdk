24a445cfcae3c3a892906dbe73273a28ad36f53a
Rico Huijbers <huijbers@amazon.com>
2018-08-17 10:43:34 +0200

    Add unit test on assets

--- packages/aws-cdk/lib/api/toolkit-info.ts
+++ packages/aws-cdk/lib/api/toolkit-info.ts
@@ -8,6 +8,18 @@ import { BUCKET_DOMAIN_NAME_OUTPUT, BUCKET_NAME_OUTPUT  } from './bootstrap-envi
 import { waitForStack } from './util/cloudformation';
 import { SDK } from './util/sdk';
 
+export interface UploadProps {
+    s3KeyPrefix?: string,
+    s3KeySuffix?: string,
+    contentType?: string,
+}
+
+export interface Uploaded {
+    filename: string;
+    key: string;
+    changed: boolean;
+}
+
 export class ToolkitInfo {
     constructor(private readonly props: {
         sdk: SDK,
@@ -29,11 +41,7 @@ export class ToolkitInfo {
      * Uses md5 hash to render the full key and skips upload if an object
      * already exists by this key.
      */
-    public async uploadIfChanged(data: any, props: {
-        s3KeyPrefix?: string,
-        s3KeySuffix?: string,
-        contentType?: string,
-    }): Promise<{ filename: string, key: string, changed: boolean }> {
+    public async uploadIfChanged(data: any, props: UploadProps): Promise<Uploaded> {
         const s3 = await this.props.sdk.s3(this.props.environment, Mode.ForWriting);
 
         const s3KeyPrefix = props.s3KeyPrefix || '';
--- packages/aws-cdk/test/test.assets.ts
+++ packages/aws-cdk/test/test.assets.ts
@@ -1,20 +1,25 @@
+import { AssetMetadataEntry } from '@aws-cdk/cx-api';
 import { Test } from 'nodeunit';
+import { Uploaded, UploadProps } from '../lib';
 import { prepareAssets } from '../lib/assets';
 
 export = {
   async 'prepare assets'(test: Test) {
+    // GIVEN
     const stack = {
+      name: 'SomeStack',
       metadata: {
-        '/SomeStack/SomeResource': {
+        '/SomeStack/SomeResource': [{
           type: 'aws:cdk:asset',
           data: {
-            path: 'abc.zip',
+            path: __filename,
+            id: 'SomeStackSomeResource4567',
             packaging: 'file',
             s3BucketParameter: 'BucketParameter',
             s3KeyParameter: 'KeyParameter'
-          },
+          } as AssetMetadataEntry,
           trace: []
-        }
+        }]
       },
       template: {
         Resources: {
@@ -24,10 +29,31 @@ export = {
         }
       }
     };
-    const toolkit = {};
-    const params = await prepareAssets(stack, toolkit);
+    const toolkit = new FakeToolkit();
 
-    console.log(params);
+    // WHEN
+    const params = await prepareAssets(stack, toolkit as any);
+
+    // THEN
+    test.deepEqual(params, [
+      { ParameterKey: 'BucketParameter', ParameterValue: 'bucket' },
+      { ParameterKey: 'KeyParameter', ParameterValue: 'assets/SomeStackSomeResource4567/||12345.js' },
+    ]);
+
     test.done();
   }
 };
+
+class FakeToolkit {
+  public bucketUrl: string = 'https://bucket';
+  public bucketName: string = 'bucket';
+
+  public async uploadIfChanged(_data: any, props: UploadProps): Promise<Uploaded> {
+    const filename = `12345${props.s3KeySuffix}`;
+    return {
+      filename,
+      changed: true,
+      key: `${props.s3KeyPrefix}${filename}`
+    };
+  }
+}
