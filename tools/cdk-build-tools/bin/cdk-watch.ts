1a09173c33b8d0b98b9394ef754f669ec39f62a7
Romain Marcadier-Muller <romain.muller@telecomnancy.net>
2018-09-05 08:30:10 +0200

    feat(cdk-build-tools): Allow configuring jsii, jsii-pacmak and tsc (#649)
    Allows additional options to be used to customize the behavior of `cdk-build-tools` commands, making
    it significantly easier to test a new version of `jsii`, `jsii-pacmak` or `tsc` without having to make complex
    changes in the codebase or using `npm link`.
    
    Permits using environment variables to specify those options. The environment variables are using the
    `TRUMP_CASED` name of the command and option, separated with an `_` character. For example,
    one can use `CDK_BUILD_JSII` to configure a different `jsii` executable to the `cdk-build` command.
    
    The default behavior is unchanged from the current (not setting any of those will use the commands
    provided by the dependency closure of the package).

--- tools/cdk-build-tools/bin/cdk-watch.ts
+++ tools/cdk-build-tools/bin/cdk-watch.ts
@@ -1,8 +1,29 @@
+import yargs = require('yargs');
 import { shell } from '../lib/os';
 import { packageCompiler } from '../lib/package-info';
 
+interface Arguments extends yargs.Arguments {
+    jsii?: string;
+    tsc?: string;
+}
+
 async function main() {
-    await shell([packageCompiler(), '-w']);
+    const args: Arguments = yargs
+        .env('CDK_WATCH')
+        .usage('Usage: cdk-watch')
+        .option('jsii', {
+            type: 'string',
+            desc: 'Specify a different jsii executable',
+            defaultDescription: 'jsii provided by node dependencies'
+        })
+        .option('tsc', {
+            type: 'string',
+            desc: 'Specify a different tsc executable',
+            defaultDescription: 'tsc provided by node dependencies'
+        })
+        .argv as any;
+
+    await shell([packageCompiler({ jsii: args.jsii, tsc: args.tsc }), '-w']);
 }
 
 main().catch(e => {
--- tools/cdk-build-tools/lib/compile.ts
+++ tools/cdk-build-tools/lib/compile.ts
@@ -1,13 +1,13 @@
 import { ChangeDetector } from "merkle-build";
 import ignoreList = require('./ignore-list');
 import { makeExecutable, shell } from "./os";
-import { currentPackageJson, packageCompiler } from "./package-info";
+import { CompilerOverrides, currentPackageJson, packageCompiler } from "./package-info";
 import { Timers } from "./timer";
 
 /**
  * Run the compiler on the current package
  */
-export async function compileCurrentPackage(timers: Timers, force?: boolean): Promise<void> {
+export async function compileCurrentPackage(timers: Timers, compilers: CompilerOverrides = {}, force?: boolean): Promise<void> {
     // We don't need to do the rest if the folder hash didn't change
     // NOTE: This happens post-cfn2ts on purpose, since a change in cfn2ts or the spec might lead
     // to different generated sources, in which case we DO need to recompile.
@@ -25,7 +25,7 @@ export async function compileCurrentPackage(timers: Timers, force?: boolean): Pr
         return;
     }
 
-    await shell([packageCompiler()], timers);
+    await shell([packageCompiler(compilers)], timers);
 
     // Find files in bin/ that look like they should be executable, and make them so.
     const scripts = currentPackageJson().bin || {};
