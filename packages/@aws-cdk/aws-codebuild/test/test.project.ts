a20584fac96d8810288759dee33308342f8955a0
Rico Huijbers <huijbers@amazon.com>
2018-09-06 14:51:47 +0200

    feat(aws-codebuild): build script from asset
    Add a feature to CodeBuild projects to allow reading the build script
    from an asset.
    
    This is useful to decouple (potentially complex) build scripts from the
    artifacts being processed, or for simply running a long-running script
    in a serverless fashion.
    
    Fixes #639.

--- packages/@aws-cdk/aws-codebuild/test/test.project.ts
+++ packages/@aws-cdk/aws-codebuild/test/test.project.ts
@@ -1,4 +1,5 @@
 import { expect, haveResource } from '@aws-cdk/assert';
+import assets = require('@aws-cdk/assets');
 import cdk = require('@aws-cdk/cdk');
 import { Test } from 'nodeunit';
 import codebuild = require('../lib');
@@ -44,5 +45,44 @@ export = {
         }));
 
         test.done();
-    }
+    },
+
+    'construct from asset'(test: Test) {
+        // GIVEN
+        const stack = new cdk.Stack();
+
+        // WHEN
+        new codebuild.Project(stack, 'Project', {
+            buildScriptAsset: new assets.ZipDirectoryAsset(stack, 'Asset', { path: '.' }),
+            buildScriptAssetEntrypoint: 'hello.sh',
+        });
+
+        // THEN
+        expect(stack).to(haveResource('AWS::CodeBuild::Project', {
+            Environment: {
+                ComputeType: "BUILD_GENERAL1_SMALL",
+                EnvironmentVariables: [
+                    {
+                        Name: "SCRIPT_S3_BUCKET",
+                        Type: "PLAINTEXT",
+                        Value: { Ref: "AssetS3Bucket235698C0" }
+                    },
+                    {
+                        Name: "SCRIPT_S3_KEY",
+                        Type: "PLAINTEXT",
+                        Value: { "Fn::Join": [ "", [
+                            { "Fn::Select": [ 0, { "Fn::Split": [ "||", { Ref: "AssetS3VersionKeyA852DDAE" } ] } ] },
+                            { "Fn::Select": [ 1, { "Fn::Split": [ "||", { Ref: "AssetS3VersionKeyA852DDAE" } ] } ] }
+                        ] ] }
+                    }
+                ],
+            },
+            Source: {
+                // tslint:disable-next-line:max-line-length
+                BuildSpec: "{\"version\":\"0.2\",\"phases\":{\"pre_build\":[\"echo \\\"Downloading scripts from s3://${SCRIPT_S3_BUCKET}/${SCRIPT_S3_KEY}\\\"\",\"aws s3 cp s3://${SCRIPT_S3_BUCKET}/${SCRIPT_S3_KEY} /tmp\",\"mkdir -p /tmp/scriptdir\",\"unzip /tmp/$(basename $SCRIPT_S3_KEY) -d /tmp/scriptdir\"],\"build\":[\"export SCRIPT_DIR=/tmp/scriptdir\",\"echo \\\"Running hello.sh\\\"\",\"chmod +x /tmp/scriptdir/hello.sh\",\"/tmp/scriptdir/hello.sh\"]}}",
+            }
+        }));
+
+        test.done();
+    },
 };
