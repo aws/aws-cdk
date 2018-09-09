9f815f9df36965f4f4c5289fe93e60ff675e0ea7
Rico Huijbers <rix0rrr@gmail.com>
2018-09-09 15:04:08 +0200

    feat(aws-lambda): allow placing Lambda in VPC (#598)
    feat(aws-lambda): allow placing Lambda in VPC (#598)
    
    It is now possible to deploy Lambda ENIs in a VPC, so
    they can access private services.
    
    Fixes #580.

---
+++ packages/@aws-cdk/aws-iam/lib/managed-policy.ts
@@ -0,0 +1,29 @@
+import cdk = require('@aws-cdk/cdk');
+
+/**
+ * A policy managed by AWS
+ *
+ * For this managed policy, you only need to know the name to be able to use it.
+ *
+ * Some managed policy names start with "service-role/", some start with
+ * "job-function/", and some don't start with anything. Do include the
+ * prefix when constructing this object.
+ */
+export class AwsManagedPolicy {
+    constructor(private readonly managedPolicyName: string) {
+    }
+
+    /**
+     * The Arn of this managed policy
+     */
+    public get policyArn(): cdk.Arn {
+        // the arn is in the form of - arn:aws:iam::aws:policy/<policyName>
+        return cdk.Arn.fromComponents({
+            service: "iam",
+            region: "", // no region for managed policy
+            account: "aws", // the account for a managed policy is 'aws'
+            resource: "policy",
+            resourceName: this.managedPolicyName
+        });
+    }
+}
