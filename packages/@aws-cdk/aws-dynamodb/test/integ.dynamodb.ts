6d1e1d363aee832003635bdfed1c8fb430388061
Jungseok Lee <jungseoklee85@gmail.com>
2018-09-11 00:29:35 -0700

    feat(aws-dynamodb): Support server-side encryption (#684)
    This patch supports DynamoDB server-side encryption.

--- packages/@aws-cdk/aws-dynamodb/test/integ.dynamodb.ts
+++ packages/@aws-cdk/aws-dynamodb/test/integ.dynamodb.ts
@@ -1,11 +1,14 @@
 import { App, Stack } from '@aws-cdk/cdk';
-import { KeyAttributeType, Table } from '../lib';
+import { KeyAttributeType, StreamViewType, Table } from '../lib';
 
 const app = new App(process.argv);
 
 const stack = new Stack(app, 'aws-cdk-dynamodb');
 
-const table = new Table(stack, 'Table');
+const table = new Table(stack, 'Table', {
+    sseEnabled: true,
+    streamSpecification: StreamViewType.KeysOnly
+});
 
 table.addPartitionKey('hashKey', KeyAttributeType.String);
 table.addSortKey('rangeKey', KeyAttributeType.Number);
