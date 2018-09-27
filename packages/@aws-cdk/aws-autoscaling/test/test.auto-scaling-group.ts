3d48eb28dee1b15bd0884f7e9d318dc71080a071
Mike Cowgill <moofish32@gmail.com>
2018-09-27 03:11:02 -0700

    feat: Tagging support for AutoScaling/SecurityGroup (#766)
    Add support for tagging of AutoScalingGroups and Security Groups.
    
    BREAKING CHANGE: constructor signature of `TagManager` has changed.
    `initialTags` is now passed inside a props object.

--- packages/@aws-cdk/aws-autoscaling/test/test.auto-scaling-group.ts
+++ packages/@aws-cdk/aws-autoscaling/test/test.auto-scaling-group.ts
@@ -19,28 +19,35 @@ export = {
 
         expect(stack).toMatch({
             "Resources": {
-              "MyFleetInstanceSecurityGroup774E8234": {
-                "Type": "AWS::EC2::SecurityGroup",
-                "Properties": {
-                  "GroupDescription": "MyFleet/InstanceSecurityGroup",
-                  "SecurityGroupEgress": [
-                    {
-                      "CidrIp": "0.0.0.0/0",
-                      "Description": "Outbound traffic allowed by default",
-                      "FromPort": -1,
-                      "IpProtocol": "-1",
-                      "ToPort": -1
+                "MyFleetInstanceSecurityGroup774E8234": {
+                    "Type": "AWS::EC2::SecurityGroup",
+                    "Properties": {
+                        "GroupDescription": "MyFleet/InstanceSecurityGroup",
+                        "SecurityGroupEgress": [
+                            {
+                                "CidrIp": "0.0.0.0/0",
+                                "Description": "Outbound traffic allowed by default",
+                                "FromPort": -1,
+                                "IpProtocol": "-1",
+                                "ToPort": -1
+                            }
+                        ],
+                        "SecurityGroupIngress": [],
+                        "Tags": [
+                            {
+                                "Key": "Name",
+                                "Value": "MyFleet"
+                            }
+                        ],
+
+                        "VpcId": "my-vpc"
                     }
-                  ],
-                  "SecurityGroupIngress": [],
-                  "VpcId": "my-vpc"
-                }
-              },
-              "MyFleetInstanceRole25A84AB8": {
-                "Type": "AWS::IAM::Role",
-                "Properties": {
-                  "AssumeRolePolicyDocument": {
-                    "Statement": [
+                },
+                "MyFleetInstanceRole25A84AB8": {
+                    "Type": "AWS::IAM::Role",
+                    "Properties": {
+                        "AssumeRolePolicyDocument": {
+                            "Statement": [
                       {
                         "Action": "sts:AssumeRole",
                         "Effect": "Allow",
@@ -99,11 +106,19 @@ export = {
                   "LaunchConfigurationName": {
                     "Ref": "MyFleetLaunchConfig5D7F9801"
                   },
-                  "MaxSize": "1",
-                  "MinSize": "1",
-                  "VPCZoneIdentifier": [
-                    "pri1"
-                  ]
+                    "Tags": [
+                        {
+                            "Key": "Name",
+                            "PropagateAtLaunch": true,
+                            "Value": "MyFleet"
+                        }
+                    ],
+
+                    "MaxSize": "1",
+                    "MinSize": "1",
+                    "VPCZoneIdentifier": [
+                        "pri1"
+                    ]
                 }
               }
             }
@@ -123,121 +138,21 @@ export = {
         });
 
         fleet.addToRolePolicy(new cdk.PolicyStatement()
-            .addAction('*')
+            .addAction('test:SpecialName')
             .addAllResources());
 
-        expect(stack).toMatch({
-            "Resources": {
-              "MyFleetInstanceSecurityGroup774E8234": {
-                "Type": "AWS::EC2::SecurityGroup",
-                "Properties": {
-                  "GroupDescription": "MyFleet/InstanceSecurityGroup",
-                  "SecurityGroupEgress": [
+        expect(stack).to(haveResource('AWS::IAM::Policy', {
+            PolicyDocument: {
+                Statement: [
                     {
-                      "CidrIp": "0.0.0.0/0",
-                      "Description": "Outbound traffic allowed by default",
-                      "FromPort": -1,
-                      "IpProtocol": "-1",
-                      "ToPort": -1
+                        Action: "test:SpecialName",
+                        Effect: "Allow",
+                        Resource: "*"
                     }
-                  ],
-                  "SecurityGroupIngress": [],
-                  "VpcId": "my-vpc"
-                }
-              },
-              MyFleetInstanceRole25A84AB8: {
-                "Type": "AWS::IAM::Role",
-                "Properties": {
-                  "AssumeRolePolicyDocument": {
-                    "Statement": [
-                      {
-                        "Action": "sts:AssumeRole",
-                        "Effect": "Allow",
-                        "Principal": {
-                          "Service": "ec2.amazonaws.com"
-                        }
-                      }
-                    ],
-                    "Version": "2012-10-17"
-                  }
-                }
-              },
-              MyFleetInstanceRoleDefaultPolicy7B0197E7: {
-                "Type": "AWS::IAM::Policy",
-                "Properties": {
-                  "PolicyDocument": {
-                    "Statement": [
-                      {
-                        "Action": "*",
-                        "Effect": "Allow",
-                        "Resource": "*"
-                      }
-                    ],
-                    "Version": "2012-10-17"
-                  },
-                  "PolicyName": "MyFleetInstanceRoleDefaultPolicy7B0197E7",
-                  "Roles": [
-                    {
-                      "Ref": "MyFleetInstanceRole25A84AB8"
-                    }
-                  ]
-                }
-              },
-              MyFleetInstanceProfile70A58496: {
-                "Type": "AWS::IAM::InstanceProfile",
-                "Properties": {
-                  "Roles": [
-                    {
-                      "Ref": "MyFleetInstanceRole25A84AB8"
-                    }
-                  ]
-                }
-              },
-              MyFleetLaunchConfig5D7F9801: {
-                Type: "AWS::AutoScaling::LaunchConfiguration",
-                Properties: {
-                  "IamInstanceProfile": {
-                    "Ref": "MyFleetInstanceProfile70A58496"
-                  },
-                  "ImageId": "dummy",
-                  "InstanceType": "m4.micro",
-                  "SecurityGroups": [
-                    {
-                      "Fn::GetAtt": [
-                        "MyFleetInstanceSecurityGroup774E8234",
-                        "GroupId"
-                      ]
-                    }
-                  ],
-                  "UserData": {
-                    "Fn::Base64": "#!/bin/bash\n"
-                  }
-                },
-                DependsOn: [
-                  "MyFleetInstanceRole25A84AB8",
-                  "MyFleetInstanceRoleDefaultPolicy7B0197E7"
-                ]
-              },
-              MyFleetASG88E55886: {
-                Type: "AWS::AutoScaling::AutoScalingGroup",
-                UpdatePolicy: {
-                  AutoScalingScheduledAction: { IgnoreUnmodifiedGroupSizeProperties: true }
-                },
-                Properties: {
-                  DesiredCapacity: "1",
-                  LaunchConfigurationName: {
-                    Ref: "MyFleetLaunchConfig5D7F9801"
-                  },
-                  MaxSize: "1",
-                  MinSize: "1",
-                  VPCZoneIdentifier: [
-                    "pri1"
-                  ]
-                }
-              }
-            }
-        });
-
+                ],
+                Version: "2012-10-17"
+            },
+        }));
         test.done();
     },
 
@@ -351,6 +266,47 @@ export = {
                     ]
                 },
                 'most-secure'],
+        }));
+        test.done();
+    },
+    'can set tags'(test: Test) {
+        // GIVEN
+        const stack = new cdk.Stack(undefined, 'MyStack', { env: { region: 'us-east-1', account: '1234' }});
+        const vpc = mockVpc(stack);
+
+        // WHEN
+        const asg = new autoscaling.AutoScalingGroup(stack, 'MyFleet', {
+            instanceType: new ec2.InstanceTypePair(ec2.InstanceClass.M4, ec2.InstanceSize.Micro),
+            machineImage: new ec2.AmazonLinuxImage(),
+            vpc,
+            updateType: autoscaling.UpdateType.RollingUpdate,
+            rollingUpdateConfiguration: {
+                minSuccessfulInstancesPercent: 50,
+                pauseTimeSec: 345
+            },
+            tags: {superfood: 'acai'},
+        });
+        asg.tags.setTag('notsuper', 'caramel', {propagate: false});
+
+        // THEN
+        expect(stack).to(haveResource("AWS::AutoScaling::AutoScalingGroup", {
+            Tags: [
+                {
+                    Key: 'superfood',
+                    Value: 'acai',
+                    PropagateAtLaunch: true,
+                },
+                {
+                    Key: 'Name',
+                    Value: 'MyFleet',
+                    PropagateAtLaunch: true,
+                },
+                {
+                    Key: 'notsuper',
+                    Value: 'caramel',
+                    PropagateAtLaunch: false,
+                },
+            ]
         }));
         test.done();
     },
