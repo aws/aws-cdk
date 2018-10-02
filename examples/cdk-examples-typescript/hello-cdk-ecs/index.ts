298e0a796daef9f61588bf718b7ab6f69e7b91bc
Hsing-Hui Hsu <hsinghui@amazon.com>
2018-10-02 11:26:29 +0200

    Add Capabilities in Linux Parameters on ContainerDefs

--- examples/cdk-examples-typescript/hello-cdk-ecs/index.ts
+++ examples/cdk-examples-typescript/hello-cdk-ecs/index.ts
@@ -3,51 +3,60 @@ import ecs = require('@aws-cdk/aws-ecs');
 import cdk = require('@aws-cdk/cdk');
 
 class BonjourECS extends cdk.Stack {
-    constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
-        super(parent, name, props);
+  constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
+    super(parent, name, props);
 
-        // For better iteration speed, it might make sense to put this VPC into
-        // a separate stack and import it here. We then have two stacks to
-        // deploy, but VPC creation is slow so we'll only have to do that once
-        // and can iterate quickly on consuming stacks. Not doing that for now.
-        const vpc = new ec2.VpcNetwork(this, 'MyVpc', {
-            maxAZs: 2
-        });
+    // For better iteration speed, it might make sense to put this VPC into
+    // a separate stack and import it here. We then have two stacks to
+    // deploy, but VPC creation is slow so we'll only have to do that once
+    // and can iterate quickly on consuming stacks. Not doing that for now.
+    const vpc = new ec2.VpcNetwork(this, 'MyVpc', {
+      maxAZs: 2
+    });
 
-        const cluster = new ecs.Cluster(this, 'DemoCluster', {
-            vpc
-        });
+    const cluster = new ecs.EcsCluster(this, 'EcsCluster', {
+      vpc
+    });
 
-        // name, image, cpu, memory, port (with default)
-        //
-        // Include in constructs:
-        //   - networking - include SD, ALB
-        //   - logging - cloudwatch logs integration? talk to nathan about 3rd
-        //     party integrations - aggregated logging across the service
-        //     (instead of per task). Probably prometheus or elk?
-        //   - tracing aws-xray-fargate - CNCF opentracing standard - jaeger,
-        //     zipkin.
-        //   - so x-ray is a container that is hooked up to sidecars that come
-        //     with the application container itself
-        //   - autoscaling - application autoscaling (Fargate focused?)
+    // name, image, cpu, memory, port (with default)
+    //
+    // Include in constructs:
+    //   - networking - include SD, ALB
+    //   - logging - cloudwatch logs integration? talk to nathan about 3rd
+    //     party integrations - aggregated logging across the service
+    //     (instead of per task). Probably prometheus or elk?
+    //   - tracing aws-xray-fargate - CNCF opentracing standard - jaeger,
+    //     zipkin.
+    //   - so x-ray is a container that is hooked up to sidecars that come
+    //     with the application container itself
+    //   - autoscaling - application autoscaling (Fargate focused?)
 
-        const taskDefinition = new ecs.TaskDefinition(this, "MyTD", {
-            family: "ecs-task-definition",
-            placementConstraints: [{
-                type: ecs.PlacementConstraintType.DistinctInstance
-            }],
-        });
+    const taskDefinition = new ecs.EcsTaskDefinition(this, "EcsTD", {
+      family: "ecs-task-definition",
+      placementConstraints: [{
+        type: ecs.PlacementConstraintType.DistinctInstance
+      }],
+    });
 
-        taskDefinition.addContainer(new ecs.ContainerDefinition(this, 'Def', {
-            name: "web",
-            image: ecs.DockerHub.image("amazon/amazon-ecs-sample"),
-            cpu: 1024,
-            memoryMiB: 512,
-            essential: true
-        }));
+    const container = new ecs.ContainerDefinition(this, 'Container', {
+      name: "web",
+      image: ecs.DockerHub.image("amazon/amazon-ecs-sample"),
+      cpu: 1024,
+      memoryMiB: 512,
+      essential: true
+    });
 
-        cluster.runService(taskDefinition);
-    }
+    container.linuxParameters.addCapability(ecs.Capability.All);
+
+    taskDefinition.addContainer(container);
+
+    new ecs.EcsService(this, "EcsService", {
+            cluster,
+            taskDefinition,
+            desiredCount: 1,
+    });
+    // cluster.runService(taskDefinition);
+  }
 }
 
 const app = new cdk.App(process.argv);
--- packages/@aws-cdk/aws-ecs/lib/linux-parameters.ts
+++ packages/@aws-cdk/aws-ecs/lib/linux-parameters.ts
@@ -1,16 +1,84 @@
 import { cloudformation } from './ecs.generated';
 
 export class LinuxParameters {
+  public initProcessEnabled?: boolean;
 
-  public addCapability() {
-    // FIXME
+  public sharedMemorySize?: number;
+
+  private readonly addCapabilities: Capability[] = [];
+
+  private readonly dropCapabilities: Capability[] = [];
+
+  // private readonly devices: Device[] = [];
+
+  // private readonly tmpfs: Tmpfs[] = [];
+
+  /**
+   * Only works with EC2 launch type
+   */
+  public addCapability(...cap: Capability[]) {
+    this.addCapabilities.push(...cap);
   }
 
-  public dropCapability() {
-    // FIXME
+  public dropCapability(...cap: Capability[]) {
+    this.dropCapabilities.push(...cap);
   }
 
   public toLinuxParametersJson(): cloudformation.TaskDefinitionResource.LinuxParametersProperty {
-    return {};
+    return {
+      initProcessEnabled: this.initProcessEnabled,
+      sharedMemorySize: this.sharedMemorySize,
+      capabilities: {
+        add: this.addCapabilities,
+        drop: this.dropCapabilities,
+      }
+    };
   }
+}
+
+// export interface Device {
+// }
+
+// export interface Tmpfs {
+// }
+
+export enum Capability {
+  All = "ALL",
+  AuditControl = "AUDIT_CONTROL",
+  AuditWrite = "AUDIT_WRITE",
+  BlockSuspend = "BLOCK_SUSPEND",
+  Chown = "CHOWN",
+  DacOverride = "DAC_OVERRIDE",
+  DacReadSearch = "DAC_READ_SEARCH",
+  Fowner = "FOWNER",
+  Fsetid = "FSETID",
+  IpcLock = "IPC_LOCK",
+  IpcOwner = "IPC_OWNER",
+  Kill = "KILL",
+  Lease = "LEASE",
+  LinuxImmutable = "LINUX_IMMUTABLE",
+  MacAdmin = "MAC_ADMIN",
+  MacOverride = "MAC_OVERRIDE",
+  Mknod = "MKNOD",
+  NetAdmin = "NET_ADMIN",
+  NetBindService = "NET_BIND_SERVICE",
+  NetBroadcast = "NET_BROADCAST",
+  NetRaw = "NET_RAW",
+  Setfcap = "SETFCAP",
+  Setgid = "SETGID",
+  Setpcap = "SETPCAP",
+  Setuid = "SETUID",
+  SysAdmin = "SYS_ADMIN",
+  SysBoot = "SYS_BOOT",
+  SysChroot = "SYS_CHROOT",
+  SysModule = "SYS_MODULE",
+  SysNice = "SYS_NICE",
+  SysPacct = "SYS_PACCT",
+  SysPtrace = "SYS_PTRACE",
+  SysRawio = "SYS_RAWIO",
+  SysResource = "SYS_RESOURCE",
+  SysTime = "SYS_TIME",
+  SysTtyConfig = "SYS_TTY_CONFIG",
+  Syslog = "SYSLOG",
+  WakeAlarm = "WAKE_ALARM"
 }
