e8aa186b0b8beaab1ae1dfeffbab9d75faca1c77
Hsing-Hui Hsu <hsinghui@amazon.com>
2018-10-04 11:51:43 +0200

    Integ test for Fargate service
    Notes:
    - Loadbalancer needs to be specified to be "internetFacing"
    - Cannot modify LoadBalancer on service once service has been created;
      can get around this by modifying ID of LB
    - Listener must be set to open

--- packages/@aws-cdk/aws-ecs/test/fargate/integ.lb-awsvpc-nw.ts
+++ packages/@aws-cdk/aws-ecs/test/fargate/integ.lb-awsvpc-nw.ts
@@ -7,6 +7,7 @@ const app = new cdk.App(process.argv);
 const stack = new cdk.Stack(app, 'aws-ecs-integ');
 
 const vpc = new ec2.VpcNetwork(stack, 'Vpc', { maxAZs: 2 });
+
 const cluster = new ecs.FargateCluster(stack, 'EcsCluster', { vpc });
 
 const taskDefinition = new ecs.FargateTaskDefinition(stack, 'TaskDef', {
@@ -18,7 +19,7 @@ const container = taskDefinition.addContainer('web', {
   memoryLimitMiB: 1024,
 });
 container.addPortMappings({
-  containerPort: 8080,
+  containerPort: 80,
   protocol: ecs.Protocol.Tcp
 });
 
@@ -27,13 +28,13 @@ const service = new ecs.FargateService(stack, "Service", {
   taskDefinition,
 });
 
-const lb = new elbv2.ApplicationLoadBalancer(stack, 'LB', { vpc });
-const listener = lb.addListener('PublicListener', { port: 80 });
+const lb = new elbv2.ApplicationLoadBalancer(stack, 'LB', { vpc, internetFacing: true });
+const listener = lb.addListener('PublicListener', { port: 80, open: true });
 listener.addTargets('ECS', {
   port: 80,
   targets: [service]
 });
 
-new cdk.Output(stack, 'LoadBalancerDNS', { value: lb.dnsName });
+new cdk.Output(stack, 'LoadBalancerDNS', { value: lb.dnsName, });
 
 process.stdout.write(app.run());
