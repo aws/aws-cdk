049557ebbea8ecdd7a28116b819b5d16b70b61de
Hsing-Hui Hsu <hsinghui@amazon.com>
2018-10-05 15:14:27 +0200

    Add integ test for load balanced service in bridge mode

---
+++ packages/@aws-cdk/aws-ecs/test/ecs/integ.lb-bridge-nw.ts
@@ -0,0 +1,43 @@
+
+import ec2 = require('@aws-cdk/aws-ec2');
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
+import cdk = require('@aws-cdk/cdk');
+import ecs = require('../../lib');
+
+const app = new cdk.App(process.argv);
+const stack = new cdk.Stack(app, 'aws-ecs-integ-ecs');
+
+const vpc = new ec2.VpcNetwork(stack, 'Vpc', { maxAZs: 2 });
+
+const cluster = new ecs.EcsCluster(stack, 'EcsCluster', { vpc });
+
+const taskDefinition = new ecs.EcsTaskDefinition(stack, 'TaskDef', {
+  // networkMode defaults to "bridge"
+  // memoryMiB: '1GB',
+  // cpu: '512'
+});
+
+const container = taskDefinition.addContainer('web', {
+  image: ecs.DockerHub.image("amazon/amazon-ecs-sample"),
+  memoryLimitMiB: 1024,
+});
+container.addPortMappings({
+  containerPort: 80,
+  protocol: ecs.Protocol.Tcp
+});
+
+const service = new ecs.EcsService(stack, "Service", {
+  cluster,
+  taskDefinition,
+});
+
+const lb = new elbv2.ApplicationLoadBalancer(stack, 'LB', { vpc, internetFacing: true });
+const listener = lb.addListener('PublicListener', { port: 80, open: true });
+listener.addTargets('ECS', {
+  port: 80,
+  targets: [service]
+});
+
+new cdk.Output(stack, 'LoadBalancerDNS', { value: lb.dnsName, });
+
+process.stdout.write(app.run());
