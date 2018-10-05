b73bdad4fed4e831114826291c2cc88e05986403
Hsing-Hui Hsu <hsinghui@amazon.com>
2018-10-05 15:13:11 +0200

    Clean up fargate integ test

--- packages/@aws-cdk/aws-ecs/test/fargate/integ.lb-awsvpc-nw.ts
+++ packages/@aws-cdk/aws-ecs/test/fargate/integ.lb-awsvpc-nw.ts
@@ -4,23 +4,21 @@ import cdk = require('@aws-cdk/cdk');
 import ecs = require('../../lib');
 
 const app = new cdk.App(process.argv);
-const stack = new cdk.Stack(app, 'aws-ecs-integ-fargate');
+const stack = new cdk.Stack(app, 'aws-ecs-integ');
 
 const vpc = new ec2.VpcNetwork(stack, 'Vpc', { maxAZs: 2 });
 
-const cluster = new ecs.FargateCluster(stack, 'EcsCluster', { vpc });
+const cluster = new ecs.FargateCluster(stack, 'FargateCluster', { vpc });
 
 const taskDefinition = new ecs.FargateTaskDefinition(stack, 'TaskDef', {
   memoryMiB: '1GB',
   cpu: '512'
 });
+
 const container = taskDefinition.addContainer('web', {
   image: ecs.DockerHub.image("amazon/amazon-ecs-sample"),
 });
-container.addPortMappings({
-  containerPort: 80,
-  protocol: ecs.Protocol.Tcp
-});
+
 container.addPortMappings({
   containerPort: 80,
   protocol: ecs.Protocol.Tcp
@@ -33,7 +31,7 @@ const service = new ecs.FargateService(stack, "Service", {
 
 const lb = new elbv2.ApplicationLoadBalancer(stack, 'LB', { vpc, internetFacing: true });
 const listener = lb.addListener('PublicListener', { port: 80, open: true });
-listener.addTargets('ECS', {
+listener.addTargets('Fargate', {
   port: 80,
   targets: [service]
 });
