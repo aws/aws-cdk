a559684b6e8853144daa5cd7e0d0e5cc1b88cf1b
Elad Ben-Israel <benisrae@amazon.com>
2018-10-12 11:31:34 +0300

    fix(toolkit): multi-stack apps cannot be synthesized or deployed (#911)
    Due to a recent cx protocol change (#868), some toolkit commands stopped
    respecting the "selected" stacks (the ones specified in the command line).
    
    "cdk synth" would always return the first stack, and "cdk deploy" would
    always deploy all stacks.
    
    Since we have test coverage gaps in the toolkit (#294), we did not
    discover this before we released.
    
    This change includes an initial set of integration tests for the 
    toolkit. At the moment they should be manually executed when toolkit
    changes are made, but we will execute them in a pipeline.
    
    Fixes #910

---
+++ packages/aws-cdk/integ-tests/test-cdk-deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+scriptdir=$(cd $(dirname $0) && pwd)
+source ${scriptdir}/common.bash
+# ----------------------------------------------------------
+
+setup
+
+stack_arn=$(cdk deploy cdk-toolkit-integration-test-2)
+echo "Stack deployed successfully"
+
+# verify that we only deployed a single stack (there's a single ARN in the output)
+assert_lines "${stack_arn}" 1
+
+# verify the number of resources in the stack
+response_json=$(mktemp).json
+aws cloudformation describe-stack-resources --stack-name ${stack_arn} > ${response_json}
+resource_count=$(node -e "console.log(require('${response_json}').StackResources.length)")
+if [ "${resource_count}" -ne 2 ]; then
+    fail "stack has ${resource_count} resources, and we expected two"
+fi
+
+# destroy
+cdk destroy -f cdk-toolkit-integration-test-2
+
+echo "✅  success"
