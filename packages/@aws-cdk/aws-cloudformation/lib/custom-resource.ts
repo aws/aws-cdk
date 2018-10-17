458419add066b2613f7e7cc4f5183e46422c356d
Elad Ben-Israel <benisrae@amazon.com>
2018-10-17 09:39:46 +0300

    feat(aws-cloudformation): allow specifying custom resource type (#943)
    Adds the `resourceType` property to `CustomResource`
    which allows specifying the type name for the resource.
    
    Custom resource type names must start with "Custom::" and
    have some naming restrictions, which are validated.

--- packages/@aws-cdk/aws-cloudformation/lib/custom-resource.ts
+++ packages/@aws-cdk/aws-cloudformation/lib/custom-resource.ts
@@ -32,6 +32,27 @@ export interface CustomResourceProps {
    * Properties to pass to the Lambda
    */
   properties?: Properties;
+
+  /**
+   * For custom resources, you can specify AWS::CloudFormation::CustomResource
+   * (the default) as the resource type, or you can specify your own resource
+   * type name. For example, you can use "Custom::MyCustomResourceTypeName".
+   *
+   * Custom resource type names must begin with "Custom::" and can include
+   * alphanumeric characters and the following characters: _@-. You can specify
+   * a custom resource type name up to a maximum length of 60 characters. You
+   * cannot change the type during an update.
+   *
+   * Using your own resource type names helps you quickly differentiate the
+   * types of custom resources in your stack. For example, if you had two custom
+   * resources that conduct two different ping tests, you could name their type
+   * as Custom::PingTester to make them easily identifiable as ping testers
+   * (instead of using AWS::CloudFormation::CustomResource).
+   *
+   * @see
+   * https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-cfn-customresource.html#aws-cfn-resource-type-name
+   */
+  resourceType?: string;
 }
 
 /**
@@ -57,6 +78,10 @@ export class CustomResource extends cloudformation.CustomResource {
     });
 
     this.userProperties = props.properties;
+
+    if (props.resourceType) {
+      this.useCustomResourceType(props.resourceType);
+    }
   }
 
   /**
@@ -65,6 +90,23 @@ export class CustomResource extends cloudformation.CustomResource {
   protected renderProperties(properties: any): {[key: string]: any}  {
     const props = super.renderProperties(properties);
     return Object.assign(props, uppercaseProperties(this.userProperties || {}));
+  }
+
+  private useCustomResourceType(resourceType: string) {
+    if (!resourceType.startsWith('Custom::')) {
+      throw new Error(`Custom resource type must begin with "Custom::" (${resourceType})`);
+    }
+
+    const typeName = resourceType.substr(resourceType.indexOf('::') + 2);
+    if (typeName.length > 60) {
+      throw new Error(`Custom resource type length > 60 (${resourceType})`);
+    }
+
+    if (!/^[a-z0-9_@-]+$/i.test(typeName)) {
+      throw new Error(`Custom resource type name can only include alphanumeric characters and _@- (${typeName})`);
+    }
+
+    this.addOverride('Type', resourceType);
   }
 
 }
