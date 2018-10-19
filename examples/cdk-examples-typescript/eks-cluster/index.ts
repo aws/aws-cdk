bfe3569f2887211022506dbca535b498ed0f9396
Yandy Ramirez <yandyr@gmail.com>
2018-10-19 12:53:19 -0400

    work on example file, not currently working

--- examples/cdk-examples-typescript/eks-cluster/index.ts
+++ examples/cdk-examples-typescript/eks-cluster/index.ts
@@ -1,6 +1,7 @@
-import ec2 = require('@aws-cdk/aws-ec2');
-import eks = require('@aws-cdk/aws-eks');
-import cdk = require( '@aws-cdk/cdk');
+import { UpdateType } from "@aws-cdk/aws-autoscaling";
+import ec2 = require("@aws-cdk/aws-ec2");
+import eks = require("@aws-cdk/aws-eks");
+import cdk = require("@aws-cdk/cdk");
 
 const app = new cdk.App();
 
@@ -13,22 +14,24 @@ const app = new cdk.App();
  * they must not be isolated, as instances later need to
  * have outbound internet access to contact the API Server
  */
-const network = new cdk.Stack(app, 'Network');
-const vpc = new ec2.VpcNetwork(network, 'VPC', {
-  cidr: '10.244.0.0/16',
+const networkStack = new cdk.Stack(app, "Network");
+
+const vpc = new ec2.VpcNetwork(networkStack, "VPC", {
+  cidr: "10.244.0.0/16",
   maxAZs: 3,
   natGateways: 0,
   subnetConfiguration: [
     {
-      name: 'pub',
+      name: "pub",
       cidrMask: 24,
       subnetType: ec2.SubnetType.Public
     }
   ],
   tags: {
-    env: '${env}'
+    env: "${env}"
   }
 });
+const vpcExport = vpc.export();
 
 /**
  * This stack creates the EKS Cluster with the imported VPC
@@ -38,17 +41,54 @@ const vpc = new ec2.VpcNetwork(network, 'VPC', {
  * version can be specified, only 1.10 supported now
  * will become useful when more versions are supported
  */
-const clusterStack = new cdk.Stack(app, 'Cluster');
-const importedVpc = ec2.VpcNetworkRef.import(
+const clusterStack = new cdk.Stack(app, "Cluster");
+
+const clusterVpc = ec2.VpcNetworkRef.import(
   clusterStack,
-  'ImportedVpc',
-  vpc.export()
+  "ClusterVpc",
+  vpcExport
 );
-new eks.Cluster(clusterStack, 'Cluster', {
-  vpc: importedVpc,
+const cluster = new eks.Cluster(clusterStack, "Cluster", {
+  vpc: clusterVpc,
   vpcPlacement: {
     subnetsToUse: ec2.SubnetType.Public
   }
 });
+cluster.connections.allowFromAnyIPv4(new ec2.TcpPort(443));
+
+const grp1 = new eks.Nodes(cluster, "NodeGroup1");
+
+grp1.addNodes({
+  vpc: clusterVpc,
+  instanceType: new ec2.InstanceTypePair(
+    ec2.InstanceClass.M5,
+    ec2.InstanceSize.Large
+  ),
+  machineImage: new ec2.GenericLinuxImage(eks.nodeAmi.normal),
+  minSize: 3,
+  desiredCapacity: 3,
+  maxSize: 6,
+  updateType: UpdateType.RollingUpdate,
+  keyName: "aws-dev-key",
+  vpcPlacement: cluster.vpcPlacement
+});
+grp1.nodeGroup.connections.allowFromAnyIPv4(new ec2.TcpPort(22));
+
+// const grp2 = new eks.Nodes(cluster, "NodeGroup2");
+// grp2.addNodes({
+//   vpc: clusterVpc,
+//   instanceType: new ec2.InstanceTypePair(
+//     ec2.InstanceClass.T2,
+//     ec2.InstanceSize.Large
+//   ),
+//   machineImage: new ec2.GenericLinuxImage(eks.nodeAmi.normal),
+//   minSize: 2,
+//   desiredCapacity: 4,
+//   maxSize: 8,
+//   updateType: UpdateType.RollingUpdate,
+//   keyName: "aws-dev-key",
+//   vpcPlacement: cluster.vpcPlacement
+// });
+// grp2.nodeGroup.connections.allowFromAnyIPv4(new ec2.TcpPort(22));
 
 app.run();
