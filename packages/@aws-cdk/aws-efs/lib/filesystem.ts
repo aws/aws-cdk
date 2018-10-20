d811cf517e8e9fa7440ed44f97d8a93bdb28f78d
Richard Boyd <rhboyd@amazon.com>
2018-10-20 12:11:08 -0400

    updated new indentation and PR comments

--- packages/@aws-cdk/aws-efs/lib/filesystem.ts
+++ packages/@aws-cdk/aws-efs/lib/filesystem.ts
@@ -1,5 +1,5 @@
-import {Construct, Output} from '@aws-cdk/cdk';
-import {cloudformation, FileSystemId} from './efs.generated';
+import {Construct} from '@aws-cdk/cdk';
+import {cloudformation} from './efs.generated';
 
 /**
  * Properties of a reference to an Elastic File System.
@@ -8,78 +8,102 @@ import {cloudformation, FileSystemId} from './efs.generated';
  * @see FileSystemRef#export
  */
 export interface FileSystemRefProps {
-    /**
-     * The physical, human-readable Id of the EFS we're referencing.
-     * The EFS must be in the same account and region as the root Stack.
-     */
-    fileSystemId: FileSystemId;
+  /**
+   * The physical, human-readable Id of the EFS we're referencing.
+   * The EFS must be in the same account and region as the root Stack.
+   */
+  fileSystemId: string;
 }
 
 /* tslint:disable:max-line-length */
 export interface FileSystemProps {
-    /**
-     * The throughput, measured in MiB/s, that you want to provision for a file system that
-     * you're creating. The limit on throughput is 1024 MiB/s. You can get these limits
-     * increased by contacting AWS Support. Must be greater than or equal to 1.0MiB/s
-     *
-     * https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-elasticfilesystem-filesystem-provisionedthroughputinmibps
-     * @default 100
-     */
-    throughputMiB?: number;
+  /**
+   * The throughput, measured in MiB/s, that you want to provision for a file system that
+   * you're creating. The limit on throughput is 1024 MiB/s. You can get these limits
+   * increased by contacting AWS Support. Must be greater than or equal to 1.0MiB/s
+   *
+   * https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-elasticfilesystem-filesystem-provisionedthroughputinmibps
+   * @default 100
+   */
+  throughputMiB?: number;
 
-    /**
-     * The throughput mode for the file system to be created. There are two throughput modes to
-     * choose from for your file system: bursting and provisioned. You can decrease your file
-     * system's throughput in Provisioned Throughput mode or change between the throughput modes
-     * as long as it’s been more than 24 hours since the last decrease or throughput mode change.
-     *
-     * https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-elasticfilesystem-filesystem-throughputmode
-     * @default ThroughputMode.Bursting
-     */
-    throughputMode?: ThroughputMode
+  /**
+   * The throughput mode for the file system to be created. There are two throughput modes to
+   * choose from for your file system: bursting and provisioned. You can decrease your file
+   * system's throughput in Provisioned Throughput mode or change between the throughput modes
+   * as long as it’s been more than 24 hours since the last decrease or throughput mode change.
+   *
+   * https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-elasticfilesystem-filesystem-throughputmode
+   * @default ThroughputMode.Bursting
+   */
+  throughputMode?: ThroughputMode
 
-    /**
-     * The PerformanceMode of the file system. We recommend generalPurpose performance mode for
-     * most file systems. File systems using the maxIO performance mode can scale to higher levels
-     * of aggregate throughput and operations per second with a tradeoff of slightly higher
-     * latencies for most file operations. This can't be changed after the file system has
-     * been created.
-     *
-     * https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-efs-filesystem-performancemode
-     * @default PerformanceMode.GeneralPurpose
-     */
-    performanceMode?: PerformanceMode
+  /**
+   * The PerformanceMode of the file system. We recommend generalPurpose performance mode for
+   * most file systems. File systems using the maxIO performance mode can scale to higher levels
+   * of aggregate throughput and operations per second with a tradeoff of slightly higher
+   * latencies for most file operations. This can't be changed after the file system has
+   * been created.
+   *
+   * https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-efs-filesystem-performancemode
+   * @default PerformanceMode.GeneralPurpose
+   */
+  performanceMode?: PerformanceMode
+
+  /**
+   * A boolean value that, if true, creates an encrypted file system. For more information, see
+   * CreateFileSystem in the Amazon Elastic File System User Guide.
+   *
+   * https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-efs-filesystem-encrypted
+   * @default true
+   */
+  encrypted?: boolean
 }
 /* tslint:enable:max-line-length */
+
+export abstract class FileSystemRef extends Construct {
+  public static import(parent: Construct, id: string, props: FileSystemRefProps) {
+    return new ImportedFileSystem(parent, id, props);
+  }
+
+  public abstract readonly fileSystemId: string;
+
+  public export(): FileSystemRefProps {
+    return {
+      fileSystemId: this.fileSystemId
+    };
+  }
+}
 
 /**
  * Provides a Elastic File System (EFS).
  */
-export class FileSystem extends Construct {
+export class FileSystem extends FileSystemRef {
 
-    public readonly fileSystemId: FileSystemId;
+  public readonly fileSystemId: string;
 
-    private readonly fileSystem: cloudformation.FileSystemResource;
+  private readonly fileSystem: cloudformation.FileSystemResource;
 
-    constructor(parent: Construct, id: string, props: FileSystemProps = {}) {
-        super(parent, id);
+  constructor(parent: Construct, id: string, props: FileSystemProps = {}) {
+    super(parent, id);
 
-        const throughput = props.throughputMiB === undefined ? 100 : props.throughputMiB;
-        if (throughput < 1.0) {
-            throw new Error(`Provisioned throughput can\'t be set to a value less than 1.0 MiB/s, provided value: ${props.throughputMiB} `);
-        }
-        const throughputMode = props.throughputMode || ThroughputMode.Bursting;
-        const performanceMode = props.performanceMode || PerformanceMode.GeneralPurpose;
-
-        this.fileSystem = new cloudformation.FileSystemResource(this, 'Resource', {
-            encrypted: true,
-            performanceMode,
-            provisionedThroughputInMibps: throughput,
-            throughputMode
-        });
-
-        this.fileSystemId = this.fileSystem.ref;
+    const throughput = props.throughputMiB === undefined ? 100 : props.throughputMiB;
+    if (throughput < 1.0) {
+      throw new Error(`Provisioned throughput can\'t be set to a value less than 1.0 MiB/s, provided value: ${props.throughputMiB}`);
     }
+    const throughputMode = props.throughputMode || ThroughputMode.Bursting;
+    const performanceMode = props.performanceMode || PerformanceMode.GeneralPurpose;
+    const encrypted = props.encrypted || true;
+
+    this.fileSystem = new cloudformation.FileSystemResource(this, 'Resource', {
+      encrypted,
+      performanceMode,
+      provisionedThroughputInMibps: throughput,
+      throughputMode
+    });
+
+    this.fileSystemId = this.fileSystem.ref;
+  }
 }
 
 /**
@@ -89,8 +113,8 @@ export class FileSystem extends Construct {
  * been more than 24 hours since the last decrease or throughput mode change.
  */
 export enum ThroughputMode {
-    Bursting = 'bursting',
-    Provisioned = 'provisioned',
+  Bursting = 'bursting',
+  Provisioned = 'provisioned',
 }
 
 /**
@@ -100,31 +124,14 @@ export enum ThroughputMode {
  * most file operations. This can't be changed after the file system has been created.
  */
 export enum PerformanceMode {
-    GeneralPurpose = 'generalPurpose',
-    MaxIO = 'maxIO',
-}
-
-export abstract class FileSystemRef extends Construct {
-    public static import(parent: Construct, id: string, props: FileSystemRefProps) {
-      return new ImportedFileSystem(parent, id, props);
-    }
-
-    public abstract readonly fileSystemId: FileSystemId;
-    constructor(parent: Construct, id: string) {
-      super(parent, id);
-    }
-
-    public export(): FileSystemRefProps {
-      return {
-        fileSystemId: new FileSystemId(new Output(this, 'FileSystemId', { value: this.fileSystemId }).makeImportValue())
-      };
-    }
+  GeneralPurpose = 'generalPurpose',
+  MaxIO = 'maxIO',
 }
 
 export class ImportedFileSystem extends FileSystemRef {
-    public readonly fileSystemId: FileSystemId;
-    constructor(parent: Construct, id: string, props: FileSystemRefProps) {
-      super(parent, id);
-      this.fileSystemId = props.fileSystemId;
-    }
+  public readonly fileSystemId: string;
+  constructor(parent: Construct, id: string, props: FileSystemRefProps) {
+    super(parent, id);
+    this.fileSystemId = props.fileSystemId;
   }
+}
--- packages/@aws-cdk/aws-efs/test/test.efs.ts
+++ packages/@aws-cdk/aws-efs/test/test.efs.ts
@@ -1,172 +1,131 @@
-import {App, Stack} from '@aws-cdk/cdk';
-import {Test} from 'nodeunit';
-import {FileSystem, ImportedFileSystem, PerformanceMode, ThroughputMode} from '../lib/filesystem';
+import { expect, haveResource } from '@aws-cdk/assert';
+import cdk = require('@aws-cdk/cdk');
+import { Test } from 'nodeunit';
+import { FileSystem, ImportedFileSystem, PerformanceMode, ThroughputMode } from '../lib/filesystem';
 
 export = {
-    'default properties': {
-        'defaults for all properties'(test: Test) {
-            const app = new TestApp();
-            new FileSystem(app.stack, 'MyFileSystem');
+  'default properties'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem');
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+      Encrypted: true,
+      PerformanceMode: 'generalPurpose',
+      ProvisionedThroughputInMibps: 100,
+      ThroughputMode: 'bursting',
+      encrypted: true
+    }));
 
-            const template = app.synthesizeTemplate();
+    test.done();
+  },
+  'when specifying throughput'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      throughputMiB: 10,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+      Encrypted: true,
+      PerformanceMode: 'generalPurpose',
+      ProvisionedThroughputInMibps: 10,
+      ThroughputMode: 'bursting',
+      encrypted: true
+    }));
 
-            test.deepEqual(template, {
-                Resources: {
-                    MyFileSystem2010A7C5: {
-                        Type: 'AWS::EFS::FileSystem',
-                        Properties: {
-                            Encrypted: true,
-                            PerformanceMode: 'generalPurpose',
-                            ProvisionedThroughputInMibps: 100,
-                            ThroughputMode: 'bursting'
-                        }
-                    }
-                }
-            });
-            test.done();
-        }
-    },
-    'when specifying throughput'(test: Test) {
-        const app = new TestApp();
-        new FileSystem(app.stack, 'MyFileSystem', {
-            throughputMiB: 10,
-        });
-        const template = app.synthesizeTemplate();
+    test.done();
+  },
+  'when specifying throughput mode'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      throughputMode: ThroughputMode.Provisioned,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+      Encrypted: true,
+      PerformanceMode: 'generalPurpose',
+      ProvisionedThroughputInMibps: 100,
+      ThroughputMode: 'provisioned',
+      encrypted: true
+    }));
 
-        test.deepEqual(template, {
-            Resources: {
-                MyFileSystem2010A7C5: {
-                    Type: 'AWS::EFS::FileSystem',
-                    Properties: {
-                        Encrypted: true,
-                        PerformanceMode: 'generalPurpose',
-                        ProvisionedThroughputInMibps: 10,
-                        ThroughputMode: 'bursting'
-                    }
-                }
-            }
-        });
-        test.done();
-    },
-    'when specifying throughput mode'(test: Test) {
-        const app = new TestApp();
-        new FileSystem(app.stack, 'MyFileSystem', {
-            throughputMode: ThroughputMode.Provisioned,
-        });
-        const template = app.synthesizeTemplate();
+    test.done();
+  },
+  'when specifying performance mode'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      performanceMode: PerformanceMode.MaxIO,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+      Encrypted: true,
+      PerformanceMode: 'maxIO',
+      ProvisionedThroughputInMibps: 100,
+      ThroughputMode: 'bursting',
+      encrypted: true
+    }));
 
-        test.deepEqual(template, {
-            Resources: {
-                MyFileSystem2010A7C5: {
-                    Type: 'AWS::EFS::FileSystem',
-                    Properties: {
-                        Encrypted: true,
-                        PerformanceMode: 'generalPurpose',
-                        ProvisionedThroughputInMibps: 100,
-                        ThroughputMode: 'provisioned'
-                    }
-                }
-            }
-        });
-        test.done();
-    },
-    'when specifying performance mode'(test: Test) {
-        const app = new TestApp();
-        new FileSystem(app.stack, 'MyFileSystem', {
-            performanceMode: PerformanceMode.MaxIO,
-        });
-        const template = app.synthesizeTemplate();
+    test.done();
+  },
+  'when specifying encryption mode'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      encrypted: false,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+      Encrypted: true,
+      PerformanceMode: 'generalPurpose',
+      ProvisionedThroughputInMibps: 100,
+      ThroughputMode: 'bursting',
+      encrypted: false
+    }));
 
-        test.deepEqual(template, {
-            Resources: {
-                MyFileSystem2010A7C5: {
-                    Type: 'AWS::EFS::FileSystem',
-                    Properties: {
-                        Encrypted: true,
-                        PerformanceMode: 'maxIO',
-                        ProvisionedThroughputInMibps: 100,
-                        ThroughputMode: 'bursting'
-                    }
-                }
-            }
-        });
-        test.done();
-    },
-    'when specifying every property'(test: Test) {
-        const app = new TestApp();
-        new FileSystem(app.stack, 'MyFileSystem', {
-            throughputMiB: 10,
-            throughputMode: ThroughputMode.Provisioned,
-            performanceMode: PerformanceMode.MaxIO,
-        });
-        const template = app.synthesizeTemplate();
+    test.done();
+  },
+  'when specifying every property'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      throughputMiB: 10,
+      throughputMode: ThroughputMode.Provisioned,
+      performanceMode: PerformanceMode.MaxIO,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+      Encrypted: true,
+      PerformanceMode: 'maxIO',
+      ProvisionedThroughputInMibps: 10,
+      ThroughputMode: 'provisioned',
+      encrypted: true
+    }));
 
-        test.deepEqual(template, {
-            Resources: {
-                MyFileSystem2010A7C5: {
-                    Type: 'AWS::EFS::FileSystem',
-                    Properties: {
-                        Encrypted: true,
-                        PerformanceMode: 'maxIO',
-                        ProvisionedThroughputInMibps: 10,
-                        ThroughputMode: 'provisioned'
-                    }
-                }
-            }
-        });
-        test.done();
-    },
-    'invalid throughput is rejected'(test: Test) {
-        const app = new TestApp();
+    test.done();
+  },
+  'invalid throughput is rejected'(test: Test) {
+    const stack = new cdk.Stack();
+    test.throws(
+      () => new FileSystem(stack, 'MyFileSystem', {throughputMiB: -2}),
+      Error,
+      'Provisioned throughput can\'t be set to a value less than 1.0 MiB/s');
 
-        test.throws(
-            () => new FileSystem(app.stack, 'MyFileSystem', {throughputMiB: -2}),
-            Error,
-            'Provisioned throughput can\'t be set to a value less than 1.0 MiB/s');
+    test.done();
+  },
+  'valid throughput on boundary is accepted'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      throughputMiB: 1,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+      Encrypted: true,
+      PerformanceMode: 'generalPurpose',
+      ProvisionedThroughputInMibps: 1,
+      ThroughputMode: 'bursting',
+      encrypted: true
+    }));
 
-        test.done();
-    },
-    'valid throughput on boundary is accepted'(test: Test) {
-        const app = new TestApp();
-        new FileSystem(app.stack, 'MyFileSystem', {
-            throughputMiB: 1,
-        });
-        const template = app.synthesizeTemplate();
+    test.done();
+  },
+  'retrieve FileSystemId'(test: Test) {
+    const stack = new cdk.Stack();
+    const fileSystem = new FileSystem(stack, 'MyFileSystem', {
+      throughputMiB: 1,
+    });
 
-        test.deepEqual(template, {
-            Resources: {
-                MyFileSystem2010A7C5: {
-                    Type: 'AWS::EFS::FileSystem',
-                    Properties: {
-                        Encrypted: true,
-                        PerformanceMode: 'generalPurpose',
-                        ProvisionedThroughputInMibps: 1,
-                        ThroughputMode: 'bursting'
-                    }
-                }
-            }
-        });
-        test.done();
-    },
-    'retrieve FileSystemId'(test: Test) {
-        const app = new TestApp();
-        const fileSystem = new FileSystem(app.stack, 'MyFileSystem', {
-            throughputMiB: 1,
-        });
-
-        const newApp = new TestApp();
-        // const newFS = new ImportedFileSystem(newApp.stack, 'MyFileSystem', {fileSystemId: fileSystem.fileSystemId});
-        ImportedFileSystem.import(newApp.stack, 'MyReferencedFileSystem', {fileSystemId: fileSystem.fileSystemId});
-        test.done();
-    }
+    // const newFS = new ImportedFileSystem(newApp.stack, 'MyFileSystem', {fileSystemId: fileSystem.fileSystemId});
+    ImportedFileSystem.import(stack, 'MyReferencedFileSystem', {fileSystemId: fileSystem.fileSystemId});
+    test.done();
+  }
 };
-
-class TestApp {
-    private readonly app = new App();
-    // tslint:disable-next-line:member-ordering
-    public readonly stack: Stack = new Stack(this.app, 'MyStack');
-
-    public synthesizeTemplate() {
-        return this.app.synthesizeStack(this.stack.name).template;
-    }
-}
