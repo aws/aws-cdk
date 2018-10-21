e8f995928fc41c4886a263d32b1454d150229abd
Richard Boyd <rhboyd@amazon.com>
2018-10-20 23:09:49 -0400

    added CR Feedback

--- packages/@aws-cdk/aws-efs/test/test.efs.ts
+++ packages/@aws-cdk/aws-efs/test/test.efs.ts
@@ -1,8 +1,124 @@
-import { Test, testCase } from 'nodeunit';
+import { expect, haveResource } from '@aws-cdk/assert';
+import cdk = require('@aws-cdk/cdk');
+import { Test } from 'nodeunit';
+import { FileSystem, ImportedFileSystem, PerformanceMode, ThroughputMode } from '../lib/filesystem';
 
-export = testCase({
-  notTested(test: Test) {
-    test.ok(true, 'No tests are specified for this package.');
+export = {
+  'default properties'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem');
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+        Encrypted: true,
+        PerformanceMode: 'generalPurpose',
+        ProvisionedThroughputInMibps: 100,
+        ThroughputMode: 'bursting'
+      }));
+
+    test.done();
+  },
+  'when specifying throughput'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      throughputMiB: 10,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+        Encrypted: true,
+        PerformanceMode: 'generalPurpose',
+        ProvisionedThroughputInMibps: 10,
+        ThroughputMode: 'bursting'
+      }));
+
+    test.done();
+  },
+  'when specifying throughput mode'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      throughputMode: ThroughputMode.Provisioned,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+        Encrypted: true,
+        PerformanceMode: 'generalPurpose',
+        ProvisionedThroughputInMibps: 100,
+        ThroughputMode: 'provisioned'
+      }));
+
+    test.done();
+  },
+  'when specifying performance mode'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      performanceMode: PerformanceMode.MaxIO,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+        Encrypted: true,
+        PerformanceMode: 'maxIO',
+        ProvisionedThroughputInMibps: 100,
+        ThroughputMode: 'bursting'
+      }));
+
+    test.done();
+  },
+  'when specifying encryption mode'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      encrypted: false,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+        Encrypted: true,
+        PerformanceMode: 'generalPurpose',
+        ProvisionedThroughputInMibps: 100,
+        ThroughputMode: 'bursting'
+      }));
+
+    test.done();
+  },
+  'when specifying every property'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      throughputMiB: 10,
+      throughputMode: ThroughputMode.Provisioned,
+      performanceMode: PerformanceMode.MaxIO,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+        Encrypted: true,
+        PerformanceMode: 'maxIO',
+        ProvisionedThroughputInMibps: 10,
+        ThroughputMode: 'provisioned'
+      }));
+
+    test.done();
+  },
+  'invalid throughput is rejected'(test: Test) {
+    const stack = new cdk.Stack();
+    test.throws(
+      () => new FileSystem(stack, 'MyFileSystem', {throughputMiB: -2}),
+      Error,
+      'Provisioned throughput can\'t be set to a value less than 1.0 MiB/s');
+
+    test.done();
+  },
+  'valid throughput on boundary is accepted'(test: Test) {
+    const stack = new cdk.Stack();
+    new FileSystem(stack, 'MyFileSystem', {
+      throughputMiB: 1,
+    });
+    expect(stack).to(haveResource('AWS::EFS::FileSystem', {
+        Encrypted: true,
+        PerformanceMode: 'generalPurpose',
+        ProvisionedThroughputInMibps: 1,
+        ThroughputMode: 'bursting'
+      }));
+
+    test.done();
+  },
+  'retrieve FileSystemId'(test: Test) {
+    const stack = new cdk.Stack();
+    const fileSystem = new FileSystem(stack, 'MyFileSystem', {
+      throughputMiB: 1,
+    });
+
+    // const newFS = new ImportedFileSystem(newApp.stack, 'MyFileSystem', {fileSystemId: fileSystem.fileSystemId});
+    ImportedFileSystem.import(stack, 'MyReferencedFileSystem', {fileSystemId: fileSystem.fileSystemId});
     test.done();
   }
-});
+};
