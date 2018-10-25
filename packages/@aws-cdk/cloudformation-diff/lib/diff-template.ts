611b4f4043b38feb0a11458726a26e56fc84f6f2
Rico Huijbers <rix0rrr@gmail.com>
2018-10-25 11:49:34 +0200

    fix(cloudformation-diff): track replacements (#1003)
    Track second-order effects of resource replacements through references.
    
    If a resource gets replaced and it's referenced by some other resource
    in an immutable field, that field gets replaced as well, and so on.
    
    ALSO IN THIS COMMIT
    
    - Configure lerna to not update package-lock.json on every bootstrap.
    
    Fixes #1001

--- packages/@aws-cdk/cloudformation-diff/lib/diff-template.ts
+++ packages/@aws-cdk/cloudformation-diff/lib/diff-template.ts
@@ -39,18 +39,40 @@ const DIFF_HANDLERS: HandlerRegistry = {
  *      the template +newTemplate+.
  */
 export function diffTemplate(currentTemplate: { [key: string]: any }, newTemplate: { [key: string]: any }): types.TemplateDiff {
-  const differences: types.ITemplateDiff = {};
-  const unknown: { [key: string]: types.Difference<any> } = {};
-  for (const key of unionOf(Object.keys(currentTemplate), Object.keys(newTemplate)).sort()) {
-    const oldValue = currentTemplate[key];
-    const newValue = newTemplate[key];
-    if (deepEqual(oldValue, newValue)) { continue; }
-    const handler: DiffHandler = DIFF_HANDLERS[key]
-                   || ((_diff, oldV, newV) => unknown[key] = impl.diffUnknown(oldV, newV));
-    handler(differences, oldValue, newValue);
+  // We're going to modify this in-place
+  newTemplate = deepCopy(newTemplate);
+
+  while (true) {
+    const differences: types.ITemplateDiff = {};
+    const unknown: { [key: string]: types.Difference<any> } = {};
+    for (const key of unionOf(Object.keys(currentTemplate), Object.keys(newTemplate)).sort()) {
+      const oldValue = currentTemplate[key];
+      const newValue = newTemplate[key];
+      if (deepEqual(oldValue, newValue)) { continue; }
+      const handler: DiffHandler = DIFF_HANDLERS[key]
+                    || ((_diff, oldV, newV) => unknown[key] = impl.diffUnknown(oldV, newV));
+      handler(differences, oldValue, newValue);
+
+    }
+    if (Object.keys(unknown).length > 0) { differences.unknown = new types.DifferenceCollection(unknown); }
+
+    // Propagate replacements for replaced resources
+    let didPropagateReferenceChanges = false;
+    if (differences.resources) {
+      differences.resources.forEach((logicalId, change) => {
+        if (change.changeImpact === types.ResourceImpact.WILL_REPLACE) {
+          if (propagateReplacedReferences(newTemplate, logicalId)) {
+            didPropagateReferenceChanges = true;
+          }
+        }
+      });
+    }
+
+    // We're done only if we didn't have to propagate any more replacements.
+    if (!didPropagateReferenceChanges) {
+      return new types.TemplateDiff(differences);
+    }
   }
-  if (Object.keys(unknown).length > 0) { differences.unknown = new types.DifferenceCollection(unknown); }
-  return new types.TemplateDiff(differences);
 }
 
 /**
@@ -58,4 +80,68 @@ export function diffTemplate(currentTemplate: { [key: string]: any }, newTemplat
  */
 export function diffResource(oldValue: types.Resource, newValue: types.Resource): types.ResourceDifference {
   return impl.diffResource(oldValue, newValue);
+}
+
+/**
+ * Replace all references to the given logicalID on the given template, in-place
+ *
+ * Returns true iff any references were replaced.
+ */
+function propagateReplacedReferences(template: object, logicalId: string): boolean {
+  let ret = false;
+
+  function recurse(obj: any) {
+    if (Array.isArray(obj)) {
+      obj.forEach(recurse);
+    }
+
+    if (typeof obj === 'object' && obj !== null) {
+      if (!replaceReference(obj)) {
+        Object.values(obj).forEach(recurse);
+      }
+    }
+  }
+
+  function replaceReference(obj: any) {
+    const keys = Object.keys(obj);
+    if (keys.length !== 1) { return false; }
+    const key = keys[0];
+
+    if (key === 'Ref') {
+      if (obj.Ref === logicalId) {
+        obj.Ref = logicalId + '(replaced)';
+        ret = true;
+      }
+      return true;
+    }
+
+    if (key.startsWith('Fn::')) {
+      if (Array.isArray(obj[key]) && obj[key].length > 0 && obj[key][0] === logicalId) {
+        obj[key][0] = logicalId + '(replaced)';
+        ret = true;
+      }
+      return true;
+    }
+
+    return false;
+  }
+
+  recurse(template);
+  return ret;
+}
+
+function deepCopy(x: any): any {
+  if (Array.isArray(x)) {
+    return x.map(deepCopy);
+  }
+
+  if (typeof x === 'object' && x !== null) {
+    const ret: any = {};
+    for (const key of Object.keys(x)) {
+      ret[key] = deepCopy(x[key]);
+    }
+    return ret;
+  }
+
+  return x;
 }
