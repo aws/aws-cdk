be9f4471bacab69f9c90de72eb7ce716075e9d51
Rico Huijbers <huijbers@amazon.com>
2018-10-26 11:05:10 +0200

    feat(aws-ecs): add Task AutoScaling to Service
    This uses the @aws-cdk/aws-applicationautoscaling library to add
    Task AutoScaling facilities to ECS and Fargate services.

--- packages/@aws-cdk/aws-ecs/lib/index.ts
+++ packages/@aws-cdk/aws-ecs/lib/index.ts
@@ -1,23 +1,26 @@
 export * from './base/base-cluster';
-export * from './ecs/ecs-cluster';
-export * from './fargate/fargate-cluster';
-
 export * from './base/base-service';
-export * from './ecs/ecs-service';
-export * from './fargate/fargate-service';
-
 export * from './base/base-task-definition';
-export * from './ecs/ecs-task-definition';
-export * from './fargate/fargate-task-definition';
+export * from './base/scalable-task-count';
+
 export * from './container-definition';
 export * from './container-image';
+
+export * from './ecs/ecs-cluster';
+export * from './ecs/ecs-service';
+export * from './ecs/ecs-task-definition';
+
+export * from './fargate/fargate-cluster';
+export * from './fargate/fargate-service';
+export * from './fargate/fargate-task-definition';
+
 export * from './linux-parameters';
 export * from './load-balanced-ecs-service';
 export * from './load-balanced-fargate-service';
 export * from './load-balanced-fargate-service-applet';
 
-export * from './log-drivers/log-driver';
 export * from './log-drivers/aws-log-driver';
+export * from './log-drivers/log-driver';
 
 // AWS::ECS CloudFormation Resources:
 //
