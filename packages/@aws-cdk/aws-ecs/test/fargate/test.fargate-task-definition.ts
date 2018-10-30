35ebd9ca8e3785f5bb771f27ab9f7a947463601e
Hsing-Hui Hsu <hsinghui@amazon.com>
2018-10-30 01:44:04 -0700

    WIP unit tests Fargate Task Definition

---
+++ packages/@aws-cdk/aws-ecs/test/fargate/test.fargate-task-definition.ts
@@ -0,0 +1,28 @@
+import { expect, haveResource } from '@aws-cdk/assert';
+import cdk = require('@aws-cdk/cdk');
+import { Test } from 'nodeunit';
+import ecs = require('../../lib');
+
+export = {
+  "When creating an Fargate TaskDefinition": {
+    "with only required properties set, it correctly sets default properties"(test: Test) {
+      // GIVEN
+      const stack = new cdk.Stack();
+      new ecs.FargateTaskDefinition(stack, 'FargateTaskDef');
+
+      // THEN
+      expect(stack).to(haveResource("AWS::ECS::TaskDefinition", {
+        Family: "FargateTaskDef",
+        ContainerDefinitions: [],
+        Volumes: [],
+        NetworkMode: ecs.NetworkMode.AwsVpc,
+        RequiresCompatibilities: [ecs.Compatibilities.Fargate],
+        Cpu: "256",
+        Memory: "512",
+      }));
+
+      // test error if no container defs?
+      test.done();
+    },
+  }
+};
