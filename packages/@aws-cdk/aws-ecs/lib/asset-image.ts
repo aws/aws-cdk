45a82c880696e617658b9bc49b410431330814f6
Rico Huijbers <huijbers@amazon.com>
2018-11-01 10:24:40 +0100

    Fix asset tests

--- packages/@aws-cdk/aws-ecs/lib/asset-image.ts
+++ packages/@aws-cdk/aws-ecs/lib/asset-image.ts
@@ -100,8 +100,8 @@ class AdoptRegistry extends cdk.Construct {
     const fn = new lambda.SingletonFunction(this, 'Function', {
       runtime: lambda.Runtime.NodeJS810,
       lambdaPurpose: 'AdoptEcrRegistry',
-      handler: 'index.handler',
-      code: lambda.Code.inline(`exports.handler = ${trivialMinify(adoptRegistryHandler.toString())}`),
+      handler: 'handler.handler',
+      code: lambda.Code.asset(path.join(__dirname, 'adopt-registry')),
       uuid: 'dbc60def-c595-44bc-aa5c-28c95d68f62c',
       timeout: 300
     });
@@ -119,114 +119,4 @@ class AdoptRegistry extends cdk.Construct {
 
     this.repositoryUri = resource.getAtt('RepositoryUri').toString();
   }
-}
-
-// tslint:disable:no-console
-async function adoptRegistryHandler(event: any, context: any) {
-  try {
-    const AWS = require('aws-sdk');
-    const ecr = new AWS.ECR();
-
-    console.log(JSON.stringify(event));
-
-    const markerStatement = {
-      Sid: event.StackId,
-      Effect: "Deny",
-      Action: "OwnedBy:CDKStack",
-      Principal: "*"
-    };
-
-    function repoName(props: any) {
-      return props.RepositoryArn.split('/').slice(1).join('/');
-    }
-
-    // The repository must already exist
-    async function getAdopter(name: string): Promise<any> {
-      try {
-        const policyResponse = await ecr.getRepositoryPolicy({ repositoryName: name }).promise();
-        const policy = JSON.parse(policyResponse.policyText);
-        // Search the policy for an adopter marker
-        return (policy.Statement || []).find((x: any) => x.Action === markerStatement.Action) || {};
-      } catch (e) {
-        if (e.code !== 'RepositoryPolicyNotFoundException') { throw e; }
-        return {};
-      }
-    }
-
-    const repo = repoName(event.ResourceProperties);
-    const adopter = await getAdopter(repo);
-    if (event.RequestType === 'Delete') {
-      if (adopter.Sid !== markerStatement.Sid) {
-        throw new Error(`This repository is already owned by another stack: ${adopter.Sid}`);
-      }
-      console.log('Deleting', repo);
-      const ids = (await ecr.listImages({ repositoryName: repo }).promise()).imageIds;
-      try {
-        await ecr.batchDeleteImage({ repositoryName: repo, imageIds: ids }).promise();
-        await ecr.deleteRepository({ repositoryName: repo }).promise();
-      } catch (e) {
-        if (e.code !== 'RepositoryPolicyNotFoundException') { throw e; }
-      }
-    }
-
-    if (event.RequestType === 'Create' || event.RequestType === 'Update') {
-      if (adopter.Sid !== undefined && adopter.Sid !== markerStatement.Sid) {
-        throw new Error(`This repository is already owned by another stack: ${adopter.Sid}`);
-      }
-      console.log('Adopting', repo);
-      await ecr.setRepositoryPolicy({ repositoryName: repo, policyText: JSON.stringify({
-        Version: '2008-10-17',
-        Statement: [markerStatement]
-      }) }).promise();
-    }
-
-    const arn = event.ResourceProperties.RepositoryArn.split(':');
-    await respond("SUCCESS", "OK", repo, {
-      RepositoryUri: `${arn[4]}.dkr.ecr.${arn[3]}.amazonaws.com/${repoName(event.ResourceProperties)}`
-    });
-  } catch (e) {
-    console.log(e);
-    await respond("FAILED", e.message, context.logStreamName, {});
-  }
-
-  function respond(responseStatus: string, reason: string, physId: string, data: any) {
-    const responseBody = JSON.stringify({
-      Status: responseStatus,
-      Reason: reason,
-      PhysicalResourceId: physId,
-      StackId: event.StackId,
-      RequestId: event.RequestId,
-      LogicalResourceId: event.LogicalResourceId,
-      NoEcho: false,
-      Data: data
-    });
-
-    console.log('Responding', JSON.stringify(responseBody));
-
-    const parsedUrl = require('url').parse(event.ResponseURL);
-    const requestOptions = {
-      hostname: parsedUrl.hostname,
-      path: parsedUrl.path,
-      method: "PUT",
-      headers: { "content-type": "", "content-length": responseBody.length }
-    };
-
-    return new Promise<void>((resolve, reject) => {
-      try {
-        const request = require('https').request(requestOptions, resolve);
-        request.on("error", reject);
-        request.write(responseBody);
-        request.end();
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
-}
-
-/**
- * Trivial minification by changing TypeScript's 4-space indentation to 1-space
- */
-function trivialMinify(s: string) {
-  return s.replace(/^ {4}/mg, ' ');
 }
---
+++ packages/@aws-cdk/aws-ecs/test/test.asset-image.ts
@@ -0,0 +1,30 @@
+import { expect, MatchStyle } from '@aws-cdk/assert';
+import cdk = require('@aws-cdk/cdk');
+import { Test } from 'nodeunit';
+import ecs = require('../lib');
+
+export = {
+  'test instantiating Asset Image'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+
+    // WHEN
+    new ecs.AssetImage(stack, 'Image', {
+      directory: '.'
+    });
+
+    // THEN
+    expect(stack).toMatch({
+      ImageRepositoryC2BE7AD4: {
+        Type: "String",
+        Description: "Repository ARN for asset \"Image\""
+      },
+      ImageTagE17D8A6B: {
+        Type: "String",
+        Description: "Tag for asset \"Image\""
+      },
+    }, MatchStyle.SUPERSET);
+
+    test.done();
+  }
+};
