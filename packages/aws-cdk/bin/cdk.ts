471ecf134a9eb74c7bbda4de106d7b11c463073b
Rico Huijbers <rix0rrr@gmail.com>
2018-11-15 15:10:03 +0100

    chore: change "already bootstrapped" message (#1180)
    Change the wording of the "already bootstrapped" message to be more emotionally neutral.

--- packages/aws-cdk/bin/cdk.ts
+++ packages/aws-cdk/bin/cdk.ts
@@ -281,8 +281,8 @@ async function initCommandLine() {
       success(' ⏳  Bootstrapping environment %s...', colors.blue(environment.name));
       try {
         const result = await bootstrapEnvironment(environment, aws, toolkitStackName, roleArn);
-        const message = result.noOp ? ' ✅  Environment %s was already fully bootstrapped!'
-                      : ' ✅  Successfully bootstrapped environment %s!';
+        const message = result.noOp ? ' ✅  Environment %s bootstrapped (no changes).'
+                      : ' ✅  Environment %s bootstrapped.';
         success(message, colors.blue(environment.name));
       } catch (e) {
         error(' ❌  Environment %s failed bootstrapping: %s', colors.blue(environment.name), e);
