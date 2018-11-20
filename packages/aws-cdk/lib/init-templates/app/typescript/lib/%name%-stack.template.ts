402cbc4ed6ecf64f3c71e3d6abacf5b72808e515
Rico Huijbers <rix0rrr@gmail.com>
2018-11-20 11:07:05 +0100

    fix(cdk init): update 'app' init template (#1209)
    To avoid people having to clear out their template from the generated
    topic and queue after they run 'cdk init app', make the 'app' template an
    empty CDK project.
    
    The original code has been moved to 'cdk init sample-app' (aliases: 'cdk
    init sample', 'cdk init example').
    
    Fixes #1124.
    
    Also change the content of the templates. We recommend people write reusable constructs in 'lib'
    directories, and they should put stacks there as well.
    
    Change both init templates to reflect this change.
    
    Fixes #1128.
    
    The 'lib' init template used 'QueueArn' but that no long exists.
    
    Fixes #1214.

---
+++ packages/aws-cdk/lib/init-templates/app/typescript/lib/%name%-stack.template.ts
@@ -0,0 +1,9 @@
+import cdk = require('@aws-cdk/cdk');
+
+export class %name.PascalCased%Stack extends cdk.Stack {
+  constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
+    super(parent, name, props);
+
+    // The code that defines your stack goes here
+  }
+}
