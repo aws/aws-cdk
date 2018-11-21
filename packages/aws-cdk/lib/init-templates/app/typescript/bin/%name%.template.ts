045cf464fbbd7f334f8729c105a77b2c42dda731
Rico Huijbers <rix0rrr@gmail.com>
2018-11-21 11:28:17 +0100

    Temporarily revert "fix(cdk init): update 'app' init template (#1209)"
    This reverts commit 402cbc4ed6ecf64f3c71e3d6abacf5b72808e515 for the
    upcoming release.

--- packages/aws-cdk/lib/init-templates/app/typescript/bin/%name%.template.ts
+++ packages/aws-cdk/lib/init-templates/app/typescript/bin/%name%.template.ts
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
+import sns = require('@aws-cdk/aws-sns');
+import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/cdk');
-import { %name.PascalCased%Stack } from '../lib/%name%-stack';
+
+class %name.PascalCased%Stack extends cdk.Stack {
+  constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
+    super(parent, name, props);
+
+    const queue = new sqs.Queue(this, '%name.PascalCased%Queue', {
+      visibilityTimeoutSec: 300
+    });
+
+    const topic = new sns.Topic(this, '%name.PascalCased%Topic');
+
+    topic.subscribeQueue(queue);
+  }
+}
 
 const app = new cdk.App();
+
 new %name.PascalCased%Stack(app, '%name.PascalCased%Stack');
+
 app.run();
