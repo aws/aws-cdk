15b9810aa6e0b1ddfd50269bd11b20b1e686898a
Rico Huijbers <rix0rrr@gmail.com>
2018-11-26 13:53:02 +0100

    feat: add support for secrets in CDK apps (#1208)
    Adds constructs that evaluates to SSM Parameter Store and Secrets
    Manager values. These can be used to retrieve secret values in
    CDK apps.
    
    Fixes #656.

---
+++ packages/@aws-cdk/aws-secretsmanager/test/test.secret-string.ts
@@ -0,0 +1,35 @@
+import cdk = require('@aws-cdk/cdk');
+import { Test } from 'nodeunit';
+import secretsmanager = require('../lib');
+
+export = {
+  'can reference Secrets Manager Value'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+
+    // WHEN
+    const ref = new secretsmanager.SecretString(stack, 'Ref', {
+      secretId: 'SomeSecret',
+    });
+
+    // THEN
+    test.equal(cdk.resolve(ref.value), '{{resolve:secretsmanager:SomeSecret:SecretString:::}}');
+
+    test.done();
+  },
+
+  'can reference jsonkey in Secrets Manager Value'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+
+    // WHEN
+    const ref = new secretsmanager.SecretString(stack, 'Ref', {
+      secretId: 'SomeSecret',
+    });
+
+    // THEN
+    test.equal(cdk.resolve(ref.jsonFieldValue('subkey')), '{{resolve:secretsmanager:SomeSecret:SecretString:subkey::}}');
+
+    test.done();
+  },
+};
---
+++ packages/@aws-cdk/aws-ssm/lib/parameter-store-string.ts
@@ -0,0 +1,59 @@
+import cdk = require('@aws-cdk/cdk');
+
+/**
+ * Properties for a ParameterStoreValue
+ */
+export interface ParameterStoreStringProps {
+  /**
+   * The name of the parameter store value
+   */
+  parameterName: string;
+
+  /**
+   * The version number of the value you wish to retrieve.
+   */
+  version: number;
+}
+
+/**
+ * References a secret value in AWS Systems Manager Parameter Store
+ *
+ * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/dynamic-references.html
+ */
+export class ParameterStoreString extends cdk.DynamicReference {
+  constructor(parent: cdk.Construct, id: string, props: ParameterStoreStringProps) {
+    super(parent, id, {
+      service: cdk.DynamicReferenceService.Ssm,
+      referenceKey: `${props.parameterName}:${props.version}`,
+    });
+  }
+}
+
+/**
+ * Properties for a ParameterStoreValue
+ */
+export interface ParameterStoreSecureStringProps {
+  /**
+   * The name of the parameter store secure string value
+   */
+  parameterName: string;
+
+  /**
+   * The version number of the value you wish to retrieve.
+   */
+  version: number;
+}
+
+/**
+ * References a secret value in AWS Systems Manager Parameter Store
+ *
+ * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/dynamic-references.html
+ */
+export class ParameterStoreSecureString extends cdk.DynamicReference {
+  constructor(parent: cdk.Construct, id: string, props: ParameterStoreStringProps) {
+    super(parent, id, {
+      service: cdk.DynamicReferenceService.SsmSecure,
+      referenceKey: `${props.parameterName}:${props.version}`,
+    });
+  }
+}
