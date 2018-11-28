aa00144035154f2bb178ca12da0daea50354d217
Adam Ruka <adamruka85@gmail.com>
2018-11-28 00:03:11 -0800

    feat(aws-ecr): add an ECR Repository source CodePipeline Action. (#1255)

--- packages/@aws-cdk/aws-ecr/lib/repository-ref.ts
+++ packages/@aws-cdk/aws-ecr/lib/repository-ref.ts
@@ -1,5 +1,8 @@
+import codepipeline = require('@aws-cdk/aws-codepipeline-api');
+import events = require('@aws-cdk/aws-events');
 import iam = require('@aws-cdk/aws-iam');
 import cdk = require('@aws-cdk/cdk');
+import { CommonPipelineSourceActionProps, PipelineSourceAction } from './pipeline-action';
 
 /**
  * An ECR repository
@@ -43,6 +46,44 @@ export abstract class RepositoryRef extends cdk.Construct {
     // Calculate this from the ARN
     const parts = cdk.ArnUtils.parse(this.repositoryArn);
     return `${parts.account}.dkr.ecr.${parts.region}.amazonaws.com/${parts.resourceName}`;
+  }
+
+  /**
+   * Convenience method for creating a new {@link PipelineSourceAction},
+   * and adding it to the given Stage.
+   *
+   * @param stage the Pipeline Stage to add the new Action to
+   * @param name the name of the newly created Action
+   * @param props the optional construction properties of the new Action
+   * @returns the newly created {@link PipelineSourceAction}
+   */
+  public addToPipeline(stage: codepipeline.IStage, name: string, props: CommonPipelineSourceActionProps = {}):
+      PipelineSourceAction {
+    return new PipelineSourceAction(this, name, {
+      stage,
+      repository: this,
+      ...props,
+    });
+  }
+
+  public onImagePushed(name: string, target?: events.IEventRuleTarget, imageTag?: string): events.EventRule {
+    return new events.EventRule(this, name, {
+      targets: target ? [target] : undefined,
+      eventPattern: {
+        source: ['aws.ecr'],
+        detail: {
+          eventName: [
+            'PutImage',
+          ],
+          requestParameters: {
+            repositoryName: [
+              this.repositoryName,
+            ],
+            imageTag: imageTag ? [imageTag] : undefined,
+          },
+        },
+      },
+    });
   }
 
   /**
