20e9020811488177f26b63d31e6b5c87f0d62087
Elad Ben-Israel <benisrae@amazon.com>
2019-01-03 22:16:49 +0200

    chore: awslint (#1468)
    Introducing awslint: a linter for the AWS Construct Library APIs. The goal of the linter is to enforce the API design guidelines across the AWS Construct Library.
    
    This commit includes a set of initial rules for the guidelines defined under design/aws-guidelines.md. 
    
    - The `awslint` npm script has been added to all modules through pkglint.
    - All violations have been captured through "exclude"s in `package.json` files so we can continue to lint from here.

---
+++ tools/awslint/lib/rules/construct.ts
@@ -0,0 +1,51 @@
+import reflect = require('jsii-reflect');
+import { Linter, MethodSignatureParameterExpectation } from '../linter';
+import { CONSTRUCT_FQN, isConstruct } from '../util';
+
+interface ConstructLinterContext  {
+  readonly construct: reflect.ClassType;
+}
+
+export const constructLinter = new Linter<ConstructLinterContext>(assembly => assembly.classes
+  .filter(t => isConstruct(t))
+  .map(construct => ({ construct })));
+
+constructLinter.add({
+  code: 'construct-ctor',
+  message: 'signature of all construct constructors should be "scope, id, props"',
+  eval: e => {
+    // only applies to non abstract classes
+    if (e.ctx.construct.abstract) {
+      return;
+    }
+
+    const initializer = e.ctx.construct.initializer;
+    if (!e.assert(initializer, e.ctx.construct.fqn)) {
+      return;
+    }
+
+    const expectedParams = new Array<MethodSignatureParameterExpectation>();
+
+    expectedParams.push({
+      name: 'scope',
+      type: CONSTRUCT_FQN
+    });
+
+    expectedParams.push({
+      name: 'id',
+      type: 'string'
+    });
+
+    // it's okay for a construct not to have a "props" argument so we only
+    // assert the "props" argument if there are more than two parameters
+    if (initializer.parameters.length > 2) {
+      expectedParams.push({
+        name: 'props',
+      });
+    }
+
+    e.assertSignature(initializer, {
+      parameters: expectedParams
+    });
+  }
+});
