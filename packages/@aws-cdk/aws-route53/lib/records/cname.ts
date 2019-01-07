db373d93b96a84fc6794824e6d242c6d6906af21
Elad Ben-Israel <benisrae@amazon.com>
2019-01-07 16:33:50 +0200

    CR comments + tslint fix

--- packages/@aws-cdk/aws-route53/lib/records/cname.ts
+++ packages/@aws-cdk/aws-route53/lib/records/cname.ts
@@ -4,10 +4,26 @@ import { CfnRecordSet } from '../route53.generated';
 import { determineFullyQualifiedDomainName } from './_util';
 
 export interface CnameRecordProps {
+  /**
+   * The hosted zone in which to define the new TXT record.
+   */
   zone: IHostedZone;
+
+  /**
+   * The domain name for this record set.
+   */
   recordName: string;
+
+  /**
+   * The value for this record set.
+   */
   recordValue: string;
-  /** @default 1800 seconds */
+
+  /**
+   * The resource record cache time to live (TTL) in seconds.
+   *
+   * @default 1800 seconds
+   */
   ttl?: number;
 }
 
--- packages/@aws-cdk/aws-route53/test/integ.route53.ts
+++ packages/@aws-cdk/aws-route53/test/integ.route53.ts
@@ -1,6 +1,6 @@
 import ec2 = require('@aws-cdk/aws-ec2');
 import cdk = require('@aws-cdk/cdk');
-import { PrivateHostedZone, PublicHostedZone, TXTRecord, CnameRecord } from '../lib';
+import { CnameRecord, PrivateHostedZone, PublicHostedZone, TxtRecord } from '../lib';
 
 const app = new cdk.App();
 
@@ -16,7 +16,7 @@ const publicZone = new PublicHostedZone(stack, 'PublicZone', {
   zoneName: 'cdk.test'
 });
 
-new TXTRecord(privateZone, 'TXT', {
+new TxtRecord(privateZone, 'TXT', {
   zone: privateZone,
   recordName: '_foo',
   recordValue: 'Bar!',
