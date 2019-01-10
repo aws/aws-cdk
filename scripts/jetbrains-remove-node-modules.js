e32e0da59a4f23e4ffdc8b8b497ac1b28a8c84db
Aaron Costley <costleya@amazon.com>
2019-01-10 14:59:35 -0800

    fix(scripts): JetBrains exclusion script is flexible.
    * The JetBrains IDEs exclusion list assumed your iml file was named aws-cdk. It now uses the root directory's name instead.
    * IntelliJ stores the iml file at root. The script now accounts for that difference.
    * For uncommon setups, the script can take an optional path arguement to opt-out of conventional file discovery, and be explicit.

--- scripts/jetbrains-remove-node-modules.js
+++ scripts/jetbrains-remove-node-modules.js
@@ -19,12 +19,14 @@ function getAllNodeModulesPaths(dir) {
     return nodeModulesPaths;
 }
 
+// Should be run at the root directory
 if (!fs.existsSync('lerna.json')) {
     throw new Error('This script should be run from the root of the repo.');
 }
 
 const nodeModulesPaths = getAllNodeModulesPaths('.');
 
+// Hardcoded exclusions for this project (in addition to node_modules)
 const exclusions = nodeModulesPaths.map(path => `<excludeFolder url="file://$MODULE_DIR$/${path}" />`);
 exclusions.push('<excludeFolder url="file://$MODULE_DIR$/.tmp" />');
 exclusions.push('<excludeFolder url="file://$MODULE_DIR$/docs" />');
@@ -33,13 +35,26 @@ exclusions.push('<excludeFolder url="file://$MODULE_DIR$/tmp" />');
 
 exclusionsString = exclusions.join(os.EOL);
 
-const exclusionInfo = fs.readFileSync('.idea/aws-cdk.iml');
+// Let filename be passed in as an override
+let fileName = process.argv[2] || process.cwd().split('/').slice(-1).pop() + '.iml';
+
+// Jetbrains IDEs store iml in .idea except for IntelliJ, which uses root.
+if (fs.existsSync('.idea/' + fileName)) {
+    fileName = '.idea/' + fileName;
+} else if (!fs.existsSync(fileName)) {
+    throw new Error('iml file not found in .idea or at root. Please pass in a path explicitly as the first argument.');
+}
+
+// Keep the contents. We are only updating exclusions.
+const exclusionInfo = fs.readFileSync(fileName);
 
 const toWrite = exclusionInfo.toString().replace(/<content url="file:\/\/\$MODULE_DIR\$">(?:\s.+)+\/content>/m, `<content url="file://$MODULE_DIR$">${os.EOL}${exclusionsString}${os.EOL}</content>`);
 
 console.log(os.EOL + 'Writing to file...');
 
-fs.unlinkSync('.idea/aws-cdk.iml');
-fs.writeFileSync('.idea/aws-cdk.iml', toWrite);
+// "Delete" the file first to avoid strange concurrent use errors.
+fs.unlinkSync(fileName);
+
+fs.writeFileSync(fileName, toWrite);
 
 console.log('Done!');
