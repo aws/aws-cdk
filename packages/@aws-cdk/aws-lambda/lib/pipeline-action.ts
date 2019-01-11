7d89da97b610bda262d43c5ca9c779b57f4c97ba
Radoslaw Smogura <smogurar@amazon.com>
2019-01-11 13:31:24 -0800

    Use spread operator for lambda action, test
    * use spread operator for lambda invoke action;
    * revert `integ.pipeline-cfn.expected.json`

--- packages/@aws-cdk/aws-lambda/lib/pipeline-action.ts
+++ packages/@aws-cdk/aws-lambda/lib/pipeline-action.ts
@@ -83,9 +83,7 @@ export interface PipelineInvokeActionProps extends CommonPipelineInvokeActionPro
 export class PipelineInvokeAction extends codepipeline.Action {
   constructor(scope: cdk.Construct, id: string, props: PipelineInvokeActionProps) {
     super(scope, id, {
-      stage: props.stage,
-      role: props.role,
-      runOrder: props.runOrder,
+      ...props,
       category: codepipeline.ActionCategory.Invoke,
       provider: 'Lambda',
       artifactBounds: codepipeline.defaultBounds(),
