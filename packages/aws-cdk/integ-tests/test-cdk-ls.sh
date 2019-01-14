cf8abc3a7c756951e4a3fc45f670a3f08ef2b7c8
Rico Huijbers <rix0rrr@gmail.com>
2019-01-14 14:34:27 +0100

    Fix toolkit integ 'ls' test

--- packages/aws-cdk/integ-tests/test-cdk-ls.sh
+++ packages/aws-cdk/integ-tests/test-cdk-ls.sh
@@ -7,9 +7,11 @@ source ${scriptdir}/common.bash
 setup
 
 assert "cdk ls" <<HERE
+cdk-toolkit-integration-iam-test
+cdk-toolkit-integration-order-providing
 cdk-toolkit-integration-test-1
 cdk-toolkit-integration-test-2
-cdk-toolkit-integration-iam-test
+cdk-toolkit-integration-order-consuming
 HERE
 
 echo "✅  success"
