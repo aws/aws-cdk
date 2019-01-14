0b3c23c550078016615edc0d0bb9743fa2e19dfc
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2019-01-14 15:22:39 -0800

    fix: Report stack metadata in assertions
    `expect(stack).at(path)` is broken because it relies on construct tree metadata
    to determine the logical ID for a given path, however the metadata is hard-coded
    to an empty object. This fixes the behavior by correctly collecting the metadata
    from the construct tree and recording it on the `SynthesizedStack`.

--- packages/@aws-cdk/assert/lib/expect.ts
+++ packages/@aws-cdk/assert/lib/expect.ts
@@ -21,7 +21,7 @@ export function expect(stack: api.SynthesizedStack | cdk.Stack, skipValidation =
     sstack = {
       name: 'test',
       template: stack.toCloudFormation(),
-      metadata: {},
+      metadata: collectStackMetadata(stack.node),
       environment: {
         name: 'test',
         account: 'test',
@@ -37,4 +37,15 @@ export function expect(stack: api.SynthesizedStack | cdk.Stack, skipValidation =
 
 function isStackClassInstance(x: api.SynthesizedStack | cdk.Stack): x is cdk.Stack {
   return 'toCloudFormation' in x;
+}
+
+function collectStackMetadata(root: cdk.ConstructNode): api.StackMetadata {
+  const result: api.StackMetadata = {};
+  for (const construct of root.findAll(cdk.ConstructOrder.DepthFirst)) {
+    for (const entry of construct.node.metadata) {
+      result[construct.node.path] = result[construct.node.path] || [];
+      result[construct.node.path].push(entry);
+    }
+  }
+  return result;
 }
