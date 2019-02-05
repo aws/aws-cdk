0ff013fef9eae61ea9d3fe038e1ed07aff105d3f
Elad Ben-Israel <benisrae@amazon.com>
2019-02-05 16:29:45 +0200

    install root modules via npm ci

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ export NODE_OPTIONS="--max-old-space-size=4096 ${NODE_OPTIONS:-}"
 
 echo "============================================================================================="
 echo "installing repo-global dependencies..."
-npm i --no-package-lock --global-style
+npm ci --global-style
 
 # Now that we have lerna available...
 export PATH=node_modules/.bin:$PATH
