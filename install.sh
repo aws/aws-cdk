67c3b9f78540e22f63d4dafed7a7ec7686fd99c0
Elad Ben-Israel <benisrae@amazon.com>
2019-02-05 16:48:26 +0200

    upgrade npm if npm ci is not supported

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 export NODE_OPTIONS="--max-old-space-size=4096 ${NODE_OPTIONS:-}"
 
+if ! npm ci --help; then
+  echo "upgrading npm, because "npm ci" is not supported"
+  npm i -g npm
+fi
 
 echo "============================================================================================="
 echo "installing repo-global dependencies..."
