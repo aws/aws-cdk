b10851b059ea1813da19fa7e5526d4beb9cc4fc8
Elad Ben-Israel <benisrae@amazon.com>
2019-02-11 16:56:02 +0200

    add note about role being assumable by ec2.amazonaws.com

--- packages/@aws-cdk/aws-autoscaling/lib/auto-scaling-group.ts
+++ packages/@aws-cdk/aws-autoscaling/lib/auto-scaling-group.ts
@@ -162,6 +162,15 @@ export interface AutoScalingGroupProps extends CommonAutoScalingGroupProps {
 
   /**
    * An IAM role to associate with the instance profile assigned to this Auto Scaling Group.
+   *
+   * The role must be assumable by the service principal `ec2.amazonaws.com`:
+   *
+   * @example
+   *
+   *    const role = new iam.Role(this, 'MyRole', {
+   *      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+   *    });
+   *
    * @default A role will automatically be created, it can be accessed via the `role` property
    */
   role?: iam.IRole;
