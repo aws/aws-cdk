8713ac69c6a83fcd704016e3c1e35e8de8ada469
Rico Huijbers <rix0rrr@gmail.com>
2019-02-13 14:59:06 +0100

    feat(decdk) prototype for declarative CDK (decdk) (#1618)
    A prototype for a tool that reads CloudFormation-like JSON/YAML templates which can contain both normal CloudFormation resources (AWS::S3::Bucket) and also reference AWS CDK resources (@aws-cdk/aws-s3.Bucket).
    
    See README for details.

---
+++ tools/decdk/test/schema.test.ts
@@ -0,0 +1,71 @@
+import { exec as execAsync } from 'child_process';
+import reflect = require('jsii-reflect');
+import path = require('path');
+import { promisify } from 'util';
+import { SchemaContext, schemaForInterface } from '../lib/jsii2schema';
+
+const exec = promisify(execAsync);
+const fixturedir = path.join(__dirname, 'fixture');
+
+// tslint:disable:no-console
+
+let typesys: reflect.TypeSystem;
+
+beforeAll(async () => {
+  // jsii-compile the fixtures module
+  await exec(require.resolve('jsii/bin/jsii'), { cwd: fixturedir });
+
+  // load the resulting file system
+  typesys = new reflect.TypeSystem();
+  await typesys.loadFile(path.join(fixturedir, '.jsii'));
+  await typesys.load(path.dirname(require.resolve('@aws-cdk/cdk/.jsii')));
+});
+
+test('schemaForInterface: interface with primitives', () => {
+  // GIVEN
+  const defs = { };
+  const ctx = SchemaContext.root(defs);
+
+  // WHEN
+  const ref = schemaForInterface(typesys.findFqn('fixture.InterfaceWithPrimitives'), ctx);
+
+  // THEN
+  expect(ref).toStrictEqual({ $ref: '#/definitions/fixture.InterfaceWithPrimitives' });
+  expect(ctx.definitions).toStrictEqual({
+    'fixture.InterfaceWithPrimitives': {
+      type: 'object',
+      title: 'InterfaceWithPrimitives',
+      additionalProperties: false,
+      properties: {
+        arrayOfStrings: {
+          type: 'array',
+          items: { type: 'string' },
+          description: 'Array of strings.'
+        },
+        mapOfNumbers: {
+          type: 'object',
+          additionalProperties: { type: 'number' }
+        },
+        numberProperty: {
+          type: 'number',
+          description: 'A property of type number.'
+        },
+        stringProperty: {
+          type: 'string',
+          description: 'A property of type string.'
+        },
+        optionalBoolean: {
+          type: 'boolean',
+          description: 'Optional boolean'
+        }
+      },
+      required: [
+        'arrayOfStrings',
+        'mapOfNumbers',
+        'numberProperty',
+        'stringProperty'
+      ],
+      comment: 'fixture.InterfaceWithPrimitives'
+    }
+  });
+});
