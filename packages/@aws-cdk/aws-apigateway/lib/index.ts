46236d90edb544e9f9861eb8d0f694aa1228727e
John Shaskin <guitarjohn@gmail.com>
2019-02-12 23:57:04 -0700

    feat(apigateway): add support for MethodResponse to aws-apigateway. (#1572)

--- packages/@aws-cdk/aws-apigateway/lib/index.ts
+++ packages/@aws-cdk/aws-apigateway/lib/index.ts
@@ -7,6 +7,8 @@ export * from './stage';
 export * from './integrations';
 export * from './lambda-api';
 export * from './vpc-link';
+export * from './methodresponse';
+export * from './model';
 
 // AWS::ApiGateway CloudFormation Resources:
 export * from './apigateway.generated';
