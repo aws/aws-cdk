b456cc4403f573646f494ab7ca43c1e1d2e12e50
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2019-02-27 22:35:50 +0100

    Call addVpnConnection in integ test

--- packages/@aws-cdk/aws-ec2/test/integ.vpn.ts
+++ packages/@aws-cdk/aws-ec2/test/integ.vpn.ts
@@ -4,20 +4,21 @@ import ec2 = require('../lib');
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'aws-cdk-ec2-vpn');
 
-new ec2.VpcNetwork(stack, 'MyVpc', {
+const vpc = new ec2.VpcNetwork(stack, 'MyVpc', {
   cidr: '10.10.0.0/16',
   vpnConnections: {
     Dynamic: { // Dynamic routing
       ip: '52.85.255.164'
-    },
-    Static: { // Static routing
-      ip: '52.85.255.197',
-      staticRoutes: [
-        '192.168.10.0/24',
-        '192.168.20.0/24'
-      ]
     }
   }
+});
+
+vpc.addVpnConnection('Static', { // Static routing
+  ip: '52.85.255.197',
+  staticRoutes: [
+    '192.168.10.0/24',
+    '192.168.20.0/24'
+  ]
 });
 
 app.run();
