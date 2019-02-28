0e12e2a4a14f4092f03278d60bcd2f7f7bf73133
Elad Ben-Israel <benisrae@amazon.com>
2019-02-28 14:01:51 +0200

    interm

--- packages/@aws-cdk/cx-api/lib/artifacts.ts
+++ packages/@aws-cdk/cx-api/lib/artifacts.ts
@@ -1,24 +1,38 @@
 export const AWS_ENV_REGEX = /aws\:\/\/([0-9]+|unknown-account)\/([a-z\-0-9]+)/;
 
 export enum ArtifactType {
-  CloudFormationStack = 'aws:cloudformation:stack',
-  DockerImage = 'aws:docker',
-  File = 'aws:file'
+  AwsCloudFormationStack = 'aws:cloudformation:stack',
+  AwsEcrDockerImage = 'aws:ecr:image',
+  AwsS3Object = 'aws:s3:object'
 }
 
-export interface Artifact {
+export interface ArtifactMetadata {
   type: ArtifactType;
   environment: string; // format: aws://account/region
-  properties?: { [name: string]: any };
   metadata?: { [path: string]: any };
   dependencies?: string[];
   missing?: { [key: string]: any };
-
-  /**
-   * Build instructions for this artifact (for example, lambda-builders, zip directory, docker build, etc)
-   */
-  build?: any;
+  properties?: { [name: string]: any };
 }
+
+export interface AwsCloudformationStackArtifact extends ArtifactMetadata {
+  type: ArtifactType.AwsCloudFormationStack,
+  properties: {
+    templateFile: string;
+    parameters: {
+      [name: string]: string
+    }
+  }
+}
+
+export interface AwsS3ObjectArtifact extends ArtifactMetadata {
+  type: ArtifactType.AwsS3Object,
+  properties: {
+    file: string;
+  }
+}
+
+export type Artifact = AwsCloudformationStackArtifact | AwsS3ObjectArtifact;
 
 export function validateArtifact(artifcat: Artifact) {
   if (!AWS_ENV_REGEX.test(artifcat.environment)) {
