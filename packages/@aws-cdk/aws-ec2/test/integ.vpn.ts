219c0064405ba1e33e01f28f1a26b36656912f91
Jonathan Goldwasser <jogold@users.noreply.github.com>
2019-03-04 12:06:57 +0100

    feat(ec2): add support for vpn connections (#1899)
    Add support for Site-to-Site VPN connections to VPC networks.
    
    When VPN connections are specified, a VPN gateway is automatically
    created and attached to the VPC. By default, routes are propagated on the
    route tables associated with the private subnets. Propagation to routes
    tables associated with public and/or isolated subnets is supported.
    
    Update VPC context provider to also import vpnGatewayId.
    
    References awslabs/jsii#231

---
+++ packages/@aws-cdk/aws-ec2/test/integ.vpn.ts
@@ -0,0 +1,29 @@
+import cdk = require('@aws-cdk/cdk');
+import ec2 = require('../lib');
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'aws-cdk-ec2-vpn');
+
+const vpc = new ec2.VpcNetwork(stack, 'MyVpc', {
+  cidr: '10.10.0.0/16',
+  vpnConnections: {
+    Dynamic: { // Dynamic routing
+      ip: '52.85.255.164',
+      tunnelOptions: [
+        {
+          preSharedKey: 'secretkey1234'
+        }
+      ]
+    }
+  }
+});
+
+vpc.addVpnConnection('Static', { // Static routing
+  ip: '52.85.255.197',
+  staticRoutes: [
+    '192.168.10.0/24',
+    '192.168.20.0/24'
+  ]
+});
+
+app.run();
