d95058fb1949ee1d2bee238a39d2a5485f95d5fa
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2019-03-05 17:25:30 +0100

    WIP

--- packages/@aws-cdk/region-info/build-tools/generate.sh
+++ packages/@aws-cdk/region-info/build-tools/generate.sh
@@ -21,14 +21,4 @@ tsc                                     \
 echo "⌛️ Generating the static data..."
 node build-tools/generate-static-data.js
 
-echo "⏳ Generating JSON schema for RegionInfo..."
-typescript-json-schema                  \
-  lib/user-data.ts                      \
-  'IUserSuppliedRegionInfo'             \
-  --out schema/region-info.schema.json  \
-  --refs              true              \
-  --required          true              \
-  --strictNullChecks  true              \
-  --topRef            true
-
 echo "🍻 Pre-Build Complete!"
