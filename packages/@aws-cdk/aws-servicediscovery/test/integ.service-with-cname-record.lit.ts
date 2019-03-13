71434152c3798967e4ab6958eff70da55efd752b
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-03-13 02:29:22 -0700

    feat(servicediscovery): AWS Cloud Map construct library (#1804)
    Add a construct library for AWS Cloud Map.
    
    Partially based on work by @jogold.

---
+++ packages/@aws-cdk/aws-servicediscovery/test/integ.service-with-cname-record.lit.ts
@@ -0,0 +1,21 @@
+import cdk = require('@aws-cdk/cdk');
+import servicediscovery = require('../lib');
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'aws-servicediscovery-integ');
+
+const namespace = new servicediscovery.PublicDnsNamespace(stack, 'Namespace', {
+  name: 'foobar.com',
+});
+
+const service = namespace.createService('Service', {
+  name: 'foo',
+  dnsRecordType: servicediscovery.DnsRecordType.CNAME,
+  dnsTtlSec: 30
+});
+
+service.registerCnameInstance({
+  instanceCname: 'service.pizza',
+});
+
+app.run();
