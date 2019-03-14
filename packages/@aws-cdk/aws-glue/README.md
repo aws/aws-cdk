d74c2761717f0c89e1079c7f428eca772c87d3c2
Sam Goodwin <samgood@amazon.com>
2019-03-13 17:43:45 -0700

    feat(glue): add L2 resources for `Database` and `Table` (#1988)

--- packages/@aws-cdk/aws-glue/README.md
+++ packages/@aws-cdk/aws-glue/README.md
@@ -1,2 +1,187 @@
 ## The CDK Construct Library for AWS Glue
 This module is part of the [AWS Cloud Development Kit](https://github.com/awslabs/aws-cdk) project.
+
+### Database
+
+A `Database` is a logical grouping of `Tables` in the Glue Catalog.
+
+```ts
+new glue.Database(stack, 'MyDatabase', {
+  databaseName: 'my_database'
+});
+```
+
+By default, a S3 bucket is created and the Database is stored under  `s3://<bucket-name>/`, but you can manually specify another location:
+
+```ts
+new glue.Database(stack, 'MyDatabase', {
+  databaseName: 'my_database',
+  locationUri: 's3://explicit-bucket/some-path/'
+});
+```
+
+### Table
+
+A Glue table describes a table of data in S3: its structure (column names and types), location of data (S3 objects with a common prefix in a S3 bucket), and format for the files (Json, Avro, Parquet, etc.):
+
+```ts
+new glue.Table(stack, 'MyTable', {
+  database: myDatabase,
+  tableName: 'my_table',
+  columns: [{
+    name: 'col1',
+    type: glue.Schema.string,
+  }, {
+    name: 'col2',
+    type: glue.Schema.array(Schema.string),
+    comment: 'col2 is an array of strings' // comment is optional
+  }]
+  dataFormat: glue.DataFormat.Json
+});
+```
+
+By default, a S3 bucket will be created to store the table's data but you can manually pass the `bucket` and `s3Prefix`:
+
+```ts
+new glue.Table(stack, 'MyTable', {
+  bucket: myBucket,
+  s3Prefix: 'my-table/'
+  ...
+});
+```
+
+#### Partitions
+
+To improve query performance, a table can specify `partitionKeys` on which data is stored and queried separately. For example, you might partition a table by `year` and `month` to optimize queries based on a time window:
+
+```ts
+new glue.Table(stack, 'MyTable', {
+  database: myDatabase,
+  tableName: 'my_table',
+  columns: [{
+    name: 'col1',
+    type: glue.Schema.string
+  }],
+  partitionKeys: [{
+    name: 'year',
+    type: glue.Schema.smallint
+  }, {
+    name: 'month',
+    type: glue.Schema.smallint
+  }],
+  dataFormat: glue.DataFormat.Json
+});
+```
+
+### [Encryption](https://docs.aws.amazon.com/athena/latest/ug/encryption.html)
+
+You can enable encryption on a Table's data:
+* `Unencrypted` - files are not encrypted. The default encryption setting.
+* [S3Managed](https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingServerSideEncryption.html) - Server side encryption (`SSE-S3`) with an Amazon S3-managed key.
+```ts
+new glue.Table(stack, 'MyTable', {
+  encryption: glue.TableEncryption.S3Managed
+  ...
+});
+```
+* [Kms](https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingKMSEncryption.html) - Server-side encryption (`SSE-KMS`) with an AWS KMS Key managed by the account owner.
+
+```ts
+// KMS key is created automatically
+new glue.Table(stack, 'MyTable', {
+  encryption: glue.TableEncryption.Kms
+  ...
+});
+
+// with an explicit KMS key
+new glue.Table(stack, 'MyTable', {
+  encryption: glue.TableEncryption.Kms,
+  encryptionKey: new kms.EncryptionKey(stack, 'MyKey')
+  ...
+});
+```
+* [KmsManaged](https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingKMSEncryption.html) - Server-side encryption (`SSE-KMS`), like `Kms`, except with an AWS KMS Key managed by the AWS Key Management Service.
+```ts
+new glue.Table(stack, 'MyTable', {
+  encryption: glue.TableEncryption.KmsManaged
+  ...
+});
+```
+* [ClientSideKms](https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingClientSideEncryption.html#client-side-encryption-kms-managed-master-key-intro) - Client-side encryption (`CSE-KMS`) with an AWS KMS Key managed by the account owner.
+```ts
+// KMS key is created automatically
+new glue.Table(stack, 'MyTable', {
+  encryption: glue.TableEncryption.ClientSideKms
+  ...
+});
+
+// with an explicit KMS key
+new glue.Table(stack, 'MyTable', {
+  encryption: glue.TableEncryption.ClientSideKms,
+  encryptionKey: new kms.EncryptionKey(stack, 'MyKey')
+  ...
+});
+```
+
+*Note: you cannot provide a `Bucket` when creating the `Table` if you wish to use server-side encryption (`Kms`, `KmsManaged` or `S3Managed`)*.
+
+### Types
+
+A table's schema is a collection of columns, each of which have a `name` and a `type`. Types are recursive structures, consisting of primitive and complex types:
+
+```ts
+new glue.Table(stack, 'MyTable', {
+  columns: [{
+    name: 'primitive_column',
+    type: glue.Schema.string
+  }, {
+    name: 'array_column',
+    type: glue.Schema.array(glue.Schema.integer),
+    comment: 'array<integer>'
+  }, {
+    name: 'map_column',
+    type: glue.Schema.map(
+      glue.Schema.string,
+      glue.Schema.timestamp),
+    comment: 'map<string,string>'
+  }, {
+    name: 'struct_column',
+    type: glue.Schema.struct([{
+      name: 'nested_column',
+      type: glue.Schema.date,
+      comment: 'nested comment'
+    }]),
+    comment: "struct<nested_column:date COMMENT 'nested comment'>"
+  }],
+  ...
+```
+
+#### Primitive
+
+Numeric:
+* `bigint`
+* `float`
+* `integer`
+* `smallint`
+* `tinyint`
+
+Date and Time:
+* `date`
+* `timestamp`
+
+String Types:
+
+* `string`
+* `decimal`
+* `char`
+* `varchar`
+
+Misc:
+* `boolean`
+* `binary`
+
+#### Complex
+
+* `array` - array of some other type
+* `map` - map of some primitive key type to any value type.
+* `struct` - nested structure containing individually named and typed columns.
---
+++ packages/@aws-cdk/aws-glue/lib/table.ts
@@ -0,0 +1,425 @@
+import iam = require('@aws-cdk/aws-iam');
+import kms = require('@aws-cdk/aws-kms');
+import s3 = require('@aws-cdk/aws-s3');
+import cdk = require('@aws-cdk/cdk');
+import { DataFormat } from './data-format';
+import { IDatabase } from './database';
+import { CfnTable } from './glue.generated';
+import { Column } from './schema';
+
+export interface ITable extends cdk.IConstruct {
+  readonly tableArn: string;
+  readonly tableName: string;
+
+  export(): TableImportProps;
+}
+
+/**
+ * Encryption options for a Table.
+ *
+ * @see https://docs.aws.amazon.com/athena/latest/ug/encryption.html
+ */
+export enum TableEncryption {
+  Unencrypted = 'Unencrypted',
+
+  /**
+   * Server side encryption (SSE) with an Amazon S3-managed key.
+   *
+   * @see https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingServerSideEncryption.html
+   */
+  S3Managed = 'SSE-S3',
+
+  /**
+   * Server-side encryption (SSE) with an AWS KMS key managed by the account owner.
+   *
+   * @see https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingKMSEncryption.html
+   */
+  Kms = 'SSE-KMS',
+
+  /**
+   * Server-side encryption (SSE) with an AWS KMS key managed by the KMS service.
+   */
+  KmsManaged = 'SSE-KMS-MANAGED',
+
+  /**
+   * Client-side encryption (CSE) with an AWS KMS key managed by the account owner.
+   *
+   * @see https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingClientSideEncryption.html
+   */
+  ClientSideKms = 'CSE-KMS'
+}
+
+export interface TableImportProps {
+  tableArn: string;
+  tableName: string;
+}
+
+export interface TableProps {
+  /**
+   * Name of the table.
+   */
+  tableName: string;
+
+  /**
+   * Description of the table.
+   *
+   * @default generated
+   */
+  description?: string;
+
+  /**
+   * Database in which to store the table.
+   */
+  database: IDatabase;
+
+  /**
+   * S3 bucket in which to store data.
+   *
+   * @default one is created for you
+   */
+  bucket?: s3.IBucket;
+
+  /**
+   * S3 prefix under which table objects are stored.
+   *
+   * @default data/
+   */
+  s3Prefix?: string;
+
+  /**
+   * Columns of the table.
+   */
+  columns: Column[];
+
+  /**
+   * Partition columns of the table.
+   *
+   * @default table is not partitioned
+   */
+  partitionKeys?: Column[]
+
+  /**
+   * Storage type of the table's data.
+   */
+  dataFormat: DataFormat;
+
+  /**
+   * Indicates whether the table's data is compressed or not.
+   *
+   * @default false
+   */
+  compressed?: boolean;
+
+  /**
+   * The kind of encryption to secure the data with.
+   *
+   * You can only provide this option if you are not explicitly passing in a bucket.
+   *
+   * If you choose `SSE-KMS`, you *can* provide an un-managed KMS key with `encryptionKey`.
+   * If you choose `CSE-KMS`, you *must* provide an un-managed KMS key with `encryptionKey`.
+   *
+   * @default Unencrypted
+   */
+  encryption?: TableEncryption;
+
+  /**
+   * External KMS key to use for bucket encryption.
+   *
+   * The `encryption` property must be `SSE-KMS` or `CSE-KMS`.
+   *
+   * @default key is managed by KMS.
+   */
+  encryptionKey?: kms.IEncryptionKey;
+
+  /**
+   * Indicates whether the table data is stored in subdirectories.
+   *
+   * @default false
+   */
+  storedAsSubDirectories?: boolean;
+}
+
+/**
+ * A Glue table.
+ */
+export class Table extends cdk.Construct implements ITable {
+  /**
+   * Creates a Table construct that represents an external table.
+   *
+   * @param scope The scope creating construct (usually `this`).
+   * @param id The construct's id.
+   * @param props A `TableImportProps` object. Can be obtained from a call to `table.export()` or manually created.
+   */
+  public static import(scope: cdk.Construct, id: string, props: TableImportProps): ITable {
+    return new ImportedTable(scope, id, props);
+  }
+
+  /**
+   * Database this table belongs to.
+   */
+  public readonly database: IDatabase;
+
+  /**
+   * The type of encryption enabled for the table.
+   */
+  public readonly encryption: TableEncryption;
+
+  /**
+   * The KMS key used to secure the data if `encryption` is set to `CSE-KMS` or `SSE-KMS`. Otherwise, `undefined`.
+   */
+  public readonly encryptionKey?: kms.IEncryptionKey;
+
+  /**
+   * S3 bucket in which the table's data resides.
+   */
+  public readonly bucket: s3.IBucket;
+
+  /**
+   * S3 Key Prefix under which this table's files are stored in S3.
+   */
+  public readonly s3Prefix: string;
+
+  /**
+   * Name of this table.
+   */
+  public readonly tableName: string;
+
+  /**
+   * ARN of this table.
+   */
+  public readonly tableArn: string;
+
+  /**
+   * Format of this table's data files.
+   */
+  public readonly dataFormat: DataFormat;
+
+  /**
+   * This table's columns.
+   */
+  public readonly columns: Column[];
+
+  /**
+   * This table's partition keys if the table is partitioned.
+   */
+  public readonly partitionKeys?: Column[];
+
+  constructor(scope: cdk.Construct, id: string, props: TableProps) {
+    super(scope, id);
+
+    this.database = props.database;
+    this.dataFormat = props.dataFormat;
+    this.s3Prefix = props.s3Prefix || 'data/';
+
+    validateSchema(props.columns, props.partitionKeys);
+    this.columns = props.columns;
+    this.partitionKeys = props.partitionKeys;
+
+    const {bucket, encryption, encryptionKey} = createBucket(this, props);
+    this.bucket = bucket;
+    this.encryption = encryption;
+    this.encryptionKey = encryptionKey;
+
+    const tableResource = new CfnTable(this, 'Table', {
+      catalogId: props.database.catalogId,
+
+      databaseName: props.database.databaseName,
+
+      tableInput: {
+        name: props.tableName,
+        description: props.description || `${props.tableName} generated by CDK`,
+
+        partitionKeys: renderColumns(props.partitionKeys),
+
+        parameters: {
+          has_encrypted_data: this.encryption !== TableEncryption.Unencrypted
+        },
+        storageDescriptor: {
+          location: `s3://${this.bucket.bucketName}/${this.s3Prefix}`,
+          compressed: props.compressed === undefined ? false : props.compressed,
+          storedAsSubDirectories: props.storedAsSubDirectories === undefined ? false : props.storedAsSubDirectories,
+          columns: renderColumns(props.columns),
+          inputFormat: props.dataFormat.inputFormat.className,
+          outputFormat: props.dataFormat.outputFormat.className,
+          serdeInfo: {
+            serializationLibrary: props.dataFormat.serializationLibrary.className
+          },
+        },
+
+        tableType: 'EXTERNAL_TABLE'
+      }
+    });
+
+    this.tableName = tableResource.tableName;
+    this.tableArn = `${this.database.databaseArn}/${this.tableName}`;
+  }
+
+  public export(): TableImportProps {
+    return {
+      tableName: new cdk.Output(this, 'TableName', { value: this.tableName }).makeImportValue().toString(),
+      tableArn: new cdk.Output(this, 'TableArn', { value: this.tableArn }).makeImportValue().toString(),
+    };
+  }
+
+  /**
+   * Grant read permissions to the table and the underlying data stored in S3 to an IAM principal.
+   *
+   * @param identity the principal
+   */
+  public grantRead(identity: iam.IPrincipal): void {
+    this.grant(identity, {
+      permissions: readPermissions,
+      kmsActions: ['kms:Decrypt']
+    });
+    this.bucket.grantRead(identity, this.s3Prefix);
+  }
+
+  /**
+   * Grant write permissions to the table and the underlying data stored in S3 to an IAM principal.
+   *
+   * @param identity the principal
+   */
+  public grantWrite(identity: iam.IPrincipal): void {
+    this.grant(identity, {
+      permissions: writePermissions,
+      kmsActions: ['kms:Encrypt', 'kms:GenerateDataKey']
+    });
+    this.bucket.grantWrite(identity, this.s3Prefix);
+  }
+
+  /**
+   * Grant read and write permissions to the table and the underlying data stored in S3 to an IAM principal.
+   *
+   * @param identity the principal
+   */
+  public grantReadWrite(identity: iam.IPrincipal): void {
+    this.grant(identity, {
+      permissions: readPermissions.concat(writePermissions),
+      kmsActions: ['kms:Decrypt', 'kms:Encrypt', 'kms:GenerateDataKey']
+    });
+    this.bucket.grantReadWrite(identity, this.s3Prefix);
+  }
+
+  private grant(identity: iam.IPrincipal, props: {
+    permissions: string[];
+    // CSE-KMS needs to grant its own KMS policies because the bucket is unaware of the key.
+    // TODO: we wouldn't need this if kms.EncryptionKey exposed grant methods.
+    kmsActions?: string[];
+  }) {
+    identity.addToPolicy(new iam.PolicyStatement()
+      .addResource(this.tableArn)
+      .addActions(...props.permissions));
+    if (this.encryption === TableEncryption.ClientSideKms) {
+      identity.addToPolicy(new iam.PolicyStatement()
+        .addResource(this.encryptionKey!.keyArn)
+        .addActions(...props.kmsActions!));
+    }
+  }
+}
+
+function validateSchema(columns: Column[], partitionKeys?: Column[]): void {
+  if (columns.length === 0) {
+    throw new Error('you must specify at least one column for the table');
+  }
+  // Check there is at least one column and no duplicated column names or partition keys.
+  const names = new Set<string>();
+  (columns.concat(partitionKeys || [])).forEach(column => {
+    if (names.has(column.name)) {
+      throw new Error(`column names and partition keys must be unique, but 'p1' is duplicated`);
+    }
+    names.add(column.name);
+  });
+}
+
+// map TableEncryption to bucket's SSE configuration (s3.BucketEncryption)
+const encryptionMappings = {
+  [TableEncryption.S3Managed]: s3.BucketEncryption.S3Managed,
+  [TableEncryption.KmsManaged]: s3.BucketEncryption.KmsManaged,
+  [TableEncryption.Kms]: s3.BucketEncryption.Kms,
+  [TableEncryption.ClientSideKms]: s3.BucketEncryption.Unencrypted,
+  [TableEncryption.Unencrypted]: s3.BucketEncryption.Unencrypted,
+};
+
+// create the bucket to store a table's data depending on the `encryption` and `encryptionKey` properties.
+function createBucket(table: Table, props: TableProps) {
+  const encryption = props.encryption || TableEncryption.Unencrypted;
+  let bucket = props.bucket;
+
+  if (bucket && (encryption !== TableEncryption.Unencrypted && encryption !== TableEncryption.ClientSideKms)) {
+    throw new Error('you can not specify encryption settings if you also provide a bucket');
+  }
+
+  let encryptionKey: kms.IEncryptionKey | undefined;
+  if (encryption === TableEncryption.ClientSideKms && props.encryptionKey === undefined) {
+    // CSE-KMS should behave the same as SSE-KMS - use the provided key or create one automatically
+    // Since Bucket only knows about SSE, we repeat the logic for CSE-KMS at the Table level.
+    encryptionKey = new kms.EncryptionKey(table, 'Key');
+  } else {
+    encryptionKey = props.encryptionKey;
+  }
+
+  // create the bucket if none was provided
+  if (!bucket) {
+    if (encryption === TableEncryption.ClientSideKms) {
+      bucket = new s3.Bucket(table, 'Bucket');
+    } else {
+      bucket = new s3.Bucket(table, 'Bucket', {
+        encryption: encryptionMappings[encryption],
+        encryptionKey
+      });
+      encryptionKey = bucket.encryptionKey;
+    }
+  }
+
+  return {
+    bucket,
+    encryption,
+    encryptionKey
+  };
+}
+
+const readPermissions = [
+  'glue:BatchDeletePartition',
+  'glue:BatchGetPartition',
+  'glue:GetPartition',
+  'glue:GetPartitions',
+  'glue:GetTable',
+  'glue:GetTables',
+  'glue:GetTableVersions'
+];
+
+const writePermissions = [
+  'glue:BatchCreatePartition',
+  'glue:BatchDeletePartition',
+  'glue:CreatePartition',
+  'glue:DeletePartition',
+  'glue:UpdatePartition'
+];
+
+function renderColumns(columns?: Array<Column | Column>) {
+  if (columns === undefined) {
+    return undefined;
+  }
+  return columns.map(column => {
+    return {
+      name: column.name,
+      type: column.type.inputString,
+      comment: column.comment
+    };
+  });
+}
+
+class ImportedTable extends cdk.Construct implements ITable {
+  public readonly tableArn: string;
+  public readonly tableName: string;
+
+  constructor(scope: cdk.Construct, id: string, private readonly props: TableImportProps) {
+    super(scope, id);
+    this.tableArn = props.tableArn;
+    this.tableName = props.tableName;
+  }
+
+  public export(): TableImportProps {
+    return this.props;
+  }
+}
