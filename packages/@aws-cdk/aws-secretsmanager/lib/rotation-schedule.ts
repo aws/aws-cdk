bf79c82890ce8baf50b5de5185a6809a212875e4
Jonathan Goldwasser <jogold@users.noreply.github.com>
2019-03-20 19:10:51 +0100

    feat(secretsmanager/rds): support credential rotation (#2052)
    Add construct for rotation schedule, secret target attachment and RDS rotation
    single user.

---
+++ packages/@aws-cdk/aws-secretsmanager/lib/rotation-schedule.ts
@@ -0,0 +1,49 @@
+import lambda = require('@aws-cdk/aws-lambda');
+import cdk = require('@aws-cdk/cdk');
+import { ISecret } from './secret';
+import { CfnRotationSchedule } from './secretsmanager.generated';
+
+/**
+ * Options to add a rotation schedule to a secret.
+ */
+export interface RotationScheduleOptions {
+  /**
+   * THe Lambda function that can rotate the secret.
+   */
+  rotationLambda: lambda.IFunction;
+
+  /**
+   * Specifies the number of days after the previous rotation before
+   * Secrets Manager triggers the next automatic rotation.
+   *
+   * @default 30
+   */
+  automaticallyAfterDays?: number;
+}
+
+/**
+ * Construction properties for a RotationSchedule.
+ */
+export interface RotationScheduleProps extends RotationScheduleOptions {
+  /**
+   * The secret to rotate.
+   */
+  secret: ISecret;
+}
+
+/**
+ * A rotation schedule.
+ */
+export class RotationSchedule extends cdk.Construct {
+  constructor(scope: cdk.Construct, id: string, props: RotationScheduleProps) {
+    super(scope, id);
+
+    new CfnRotationSchedule(this, 'Resource', {
+      secretId: props.secret.secretArn,
+      rotationLambdaArn: props.rotationLambda.functionArn,
+      rotationRules: {
+        automaticallyAfterDays: props.automaticallyAfterDays || 30
+      }
+    });
+  }
+}
