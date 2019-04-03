361a92eb2d278c9620f1fe6774d5adb75fa88a7b
Rico Huijbers <rix0rrr@gmail.com>
2019-04-03 14:10:29 +0200

    Remove dynamodb global

--- packages/@aws-cdk/aws-dynamodb-global/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./aws-dynamodb-global";
-export * from "./lambda-global-dynamodb";
-export * from "./multi-dynamodb-stack";
--- packages/@aws-cdk/aws-dynamodb-global/test/test.dynamodb.global.d.ts
+++
@@ -1,7 +0,0 @@
-import { Test } from 'nodeunit';
-declare const _default: {
-    'default stack': {
-        'default'(test: Test): void;
-    };
-};
-export = _default;
