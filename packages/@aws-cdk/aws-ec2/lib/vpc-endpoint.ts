c0fcd02bca90e7ded90ecdbfd5448217c6c3f8bd
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2019-04-08 17:49:01 +0200

    Error message

--- packages/@aws-cdk/aws-ec2/lib/vpc-endpoint.ts
+++ packages/@aws-cdk/aws-ec2/lib/vpc-endpoint.ts
@@ -157,7 +157,7 @@ export class GatewayVpcEndpoint extends VpcEndpoint implements IGatewayVpcEndpoi
     const routeTableIds = [...new Set(Array().concat(...subnets.map(s => props.vpc.selectSubnets(s).routeTableIds)))];
 
     if (routeTableIds.length === 0) {
-      throw new Error(`Can't add an endpoint to VPC; route table IDs are not available`);
+      throw new Error(`Can't add a gateway endpoint to VPC; route table IDs are not available`);
     }
 
     const endpoint = new CfnVPCEndpoint(this, 'Resource', {
