e1e012cc703d67b0ce90e45c19e3410889b8d8df
Yandy Ramirez <yandyr@gmail.com>
2019-04-14 12:35:22 -0400

    doc: document virtual service class

--- packages/@aws-cdk/aws-appmesh/lib/virtual-service.ts
+++ packages/@aws-cdk/aws-appmesh/lib/virtual-service.ts
@@ -13,19 +13,56 @@ export interface VirtualServiceBaseProps {
    *
    * @example service.domain.local
    */
-  readonly virtualServiceName?: string;
+  readonly virtualServiceName: string;
+  /**
+   * The name of the VirtualRouter which this VirtualService uses as provider
+   */
   readonly virtualRouterName: string;
 }
 
+/**
+ * The properties applied to the VirtualService being created
+ */
 export interface VirtualServiceProps extends VirtualServiceBaseProps {
+  /**
+   * The AppMesh mesh name for whiich the VirtualService belongs to
+   */
   readonly meshName: string;
 }
 
+/**
+ * VirtualService represents a service inside an AppMesh
+ *
+ * @see https://docs.aws.amazon.com/app-mesh/latest/userguide/virtual_services.html
+ */
 export class VirtualService extends cdk.Construct {
+  /**
+   * The AppMesh mesh name for whiich the VirtualService belongs to
+   *
+   * @type {string}
+   * @memberof VirtualService
+   */
   public readonly meshName: string;
+  /**
+   * The name of the VirtualRouter which this VirtualService uses as provider
+   *
+   * @type {string}
+   * @memberof VirtualService
+   */
   public readonly virtualRouterName: string;
+  /**
+   * The name of the VirtualService, it is recommended this follows the fully-qualified domain name format.
+   *
+   * @type {string}
+   * @memberof VirtualService
+   */
   public readonly virtualServiceName: string;
-  public readonly virtaulServiceMeshName: string;
+  /**
+   * The Amazon Resource Name (ARN) for the virtual service
+   *
+   * @type {string}
+   * @memberof VirtualService
+   */
   public readonly virtaulServiceArn: string;
 
   constructor(scope: cdk.Construct, id: string, props: VirtualServiceProps) {
@@ -35,7 +72,7 @@ export class VirtualService extends cdk.Construct {
     this.virtualRouterName = props.virtualRouterName;
 
     this.node.apply(new cdk.Tag(NAME_TAG, this.node.path));
-    const name = props.virtualServiceName || id;
+    const name = props.virtualServiceName;
 
     const svc = new CfnVirtualService(this, 'VirtualService', {
       meshName: this.meshName,
@@ -51,6 +88,5 @@ export class VirtualService extends cdk.Construct {
 
     this.virtaulServiceArn = svc.virtualServiceArn;
     this.virtualServiceName = svc.virtualServiceName;
-    this.virtaulServiceMeshName = svc.virtualServiceMeshName;
   }
 }
