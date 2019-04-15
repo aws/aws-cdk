28e2475df704053c95548f2ecbb40bee43f2eedf
Elad Ben-Israel <benisrae@amazon.com>
2019-04-15 18:57:15 +0300

    Stylistic changes

--- packages/@aws-cdk/aws-dynamodb-global/lib/aws-dynamodb-global.ts
+++ packages/@aws-cdk/aws-dynamodb-global/lib/aws-dynamodb-global.ts
@@ -38,19 +38,20 @@ export class GlobalTable extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string, props: GlobalTableProps) {
     super(scope, id);
     this._regionalTables = [];
+    
     if (props.streamSpecification != null && props.streamSpecification !== dynamodb.StreamViewType.NewAndOldImages) {
-      throw(new TypeError("dynamoProps.streamSpecification MUST be set to dynamodb.StreamViewType.NewAndOldImages"));
+      throw new Error("dynamoProps.streamSpecification MUST be set to dynamodb.StreamViewType.NewAndOldImages");
     }
-    // Need to set this streamSpecification, otherwise global tables don't work
+    
+    // need to set this streamSpecification, otherwise global tables don't work
     // And no way to set a default value in an interface
     const stackProps = {
       ...props,
       streamSpecification: dynamodb.StreamViewType.NewAndOldImages
     };
-    /**
-     * Here we loop through the configured regions.
-     * In each region we'll deploy a separate stack with a DynamoDB Table with identical properties in the individual stacks
-     */
+    
+    // here we loop through the configured regions.
+    // in each region we'll deploy a separate stack with a DynamoDB Table with identical properties in the individual stacks
     for (const reg of props.regions) {
       const regionalStack = new cdk.Stack(this, id + "-" + reg, { env: { region: reg } });
       const regionalTable = new dynamodb.Table(regionalStack, id + '-GlobalTable-' + reg, stackProps);
