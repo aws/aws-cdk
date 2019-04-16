1865a88b1a99e4d7f0dbbaf9f710abdc6baa8a46
Romain Marcadier-Muller <romain.muller@telecomnancy.net>
2019-04-16 12:50:09 +0200

    chore: Drop @aws-cdk/aws-serverless source (#2128)
    The package at version 0.27.0 was effectively marked as deprecated on
    NPM.JS with an appropriate message, instructing users to use
    @aws-cdk/aws-sam instead.
    
    As a consequence, this beacon is no longer needed and can be dropped.

--- packages/@aws-cdk/aws-serverless/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '@aws-cdk/aws-sam';
-
-// tslint:disable-next-line: no-console
-console.error('WARNING: The @aws-cdk/serverless package was renamed to @aws-cdk/sam. Please update your dependencies!');
