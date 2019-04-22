b9bc4805937fb0b83014494d3990d991956d76a9
Elad Ben-Israel <benisrae@amazon.com>
2019-04-22 15:35:40 +0300

    remove cycle

--- packages/@aws-cdk/aws-codecommit/test/integ.codecommit-events.ts
+++ packages/@aws-cdk/aws-codecommit/test/integ.codecommit-events.ts
@@ -1,4 +1,3 @@
-import targets = require('@aws-cdk/aws-events-targets');
 import sns = require('@aws-cdk/aws-sns');
 import cdk = require('@aws-cdk/cdk');
 import codecommit = require('../lib');
@@ -9,6 +8,13 @@ const stack = new cdk.Stack(app, 'aws-cdk-codecommit-events');
 const repo = new codecommit.Repository(stack, 'Repo', { repositoryName: 'aws-cdk-codecommit-events' });
 const topic = new sns.Topic(stack, 'MyTopic');
 
-repo.onReferenceCreated('OnReferenceCreated', new targets.SnsTopicTarget(topic));
+// we can't use @aws-cdk/aws-events-targets.SnsTopic here because it will
+// create a cyclic dependency with codebuild, so we just fake it
+repo.onReferenceCreated('OnReferenceCreated', {
+  asEventRuleTarget: () => ({
+    arn: topic.topicArn,
+    id: 'MyTopic'
+  })
+});
 
 app.run();
