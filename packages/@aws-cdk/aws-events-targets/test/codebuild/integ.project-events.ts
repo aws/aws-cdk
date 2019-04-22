5b1018c1791697d619de1d96fdc02515f65a4a20
Elad Ben-Israel <benisrae@amazon.com>
2019-04-22 23:22:14 +0300

    feat(aws-events-targets): centralized module for cloudwatch event targets (#2343)
    Start moving all implementations of `IEventRuleTarget` into a centralized
    module to adhere with the new AWS Construct Library guideline for integrations.
    
    This change includes the implementation of the following event rule targets:
    
    - `targets.SnsTopic`
    - `targets.CodeBuildProject`
    
    BREAKING CHANGE: `sns.Topic` no longer implements `IEventRuleTarget`. Use `@aws-cdk/aws-events-targets.SnsTopic` instead.
    * `codebuild.Project` no longer implements `IEventRuleTarget`. Use `@aws-cdk/aws-events-targets.CodeBuildProject`

--- packages/@aws-cdk/aws-events-targets/test/codebuild/integ.project-events.ts
+++ packages/@aws-cdk/aws-events-targets/test/codebuild/integ.project-events.ts
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
+import codebuild = require('@aws-cdk/aws-codebuild');
 import codecommit = require('@aws-cdk/aws-codecommit');
 import sns = require('@aws-cdk/aws-sns');
 import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/cdk');
-import { CodeCommitSource, Project } from '../lib';
+import targets = require('../../lib');
 
 const app = new cdk.App();
 
 const stack = new cdk.Stack(app, 'aws-cdk-codebuild-events');
 
 const repo = new codecommit.Repository(stack, 'MyRepo', { repositoryName: 'aws-cdk-codebuild-events' });
-const project = new Project(stack, 'MyProject', {
-  source: new CodeCommitSource({ repository: repo }),
+const project = new codebuild.Project(stack, 'MyProject', {
+  source: new codebuild.CodeCommitSource({ repository: repo }),
 });
 
 const queue = new sqs.Queue(stack, 'MyQueue');
@@ -21,12 +22,12 @@ topic.subscribeQueue(queue);
 
 // this will send an email with the JSON event for every state change of this
 // build project.
-project.onStateChange('StateChange', topic);
+project.onStateChange('StateChange', new targets.SnsTopic(topic));
 
 // this will send an email with the message "Build phase changed to <phase>".
 // The phase will be extracted from the "completed-phase" field of the event
 // details.
-project.onPhaseChange('PhaseChange').addTarget(topic, {
+project.onPhaseChange('PhaseChange').addTarget(new targets.SnsTopic(topic), {
   textTemplate: `Build phase changed to <phase>`,
   pathsMap: {
     phase: '$.detail.completed-phase'
@@ -34,8 +35,8 @@ project.onPhaseChange('PhaseChange').addTarget(topic, {
 });
 
 // trigger a build when a commit is pushed to the repo
-const onCommitRule = repo.onCommit('OnCommit', project, 'master');
-onCommitRule.addTarget(topic, {
+const onCommitRule = repo.onCommit('OnCommit', new targets.CodeBuildProject(project), 'master');
+onCommitRule.addTarget(new targets.SnsTopic(topic), {
   textTemplate: 'A commit was pushed to the repository <repo> on branch <branch>',
   pathsMap: {
     branch: '$.detail.referenceName',
