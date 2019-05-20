4d17b28e8f2aaee1b1a49b312a398dcdf8efc7a1
Simon-Pierre Gingras <spgingras@poka.io>
2019-05-20 06:13:42 -0700

    fix(lambda): compare Runtimes by value instead of identity (#2543)
    Different Runtime objects that represent the same runtime now compare as equal. This is important when using Layers, which declare compatible runtimes.

---
+++ packages/@aws-cdk/aws-lambda/test/test.runtime.ts
@@ -0,0 +1,70 @@
+import {Test, testCase} from 'nodeunit';
+import {RuntimeFamily} from "../lib";
+import lambda = require('../lib');
+
+export = testCase({
+  'runtimes are equal for different instances'(test: Test) {
+    // GIVEN
+    const runtime1 = new lambda.Runtime('python3.7', RuntimeFamily.Python, {supportsInlineCode: true});
+    const runtime2 = new lambda.Runtime('python3.7', RuntimeFamily.Python, {supportsInlineCode: true});
+
+    // WHEN
+    const result = runtime1.equals(runtime2);
+
+    // THEN
+    test.strictEqual(result, true, 'Runtimes should be equal');
+
+    test.done();
+  },
+  'runtimes are equal for same instance'(test: Test) {
+    // GIVEN
+    const runtime = new lambda.Runtime('python3.7', RuntimeFamily.Python, {supportsInlineCode: true});
+
+    // WHEN
+    const result = runtime.equals(runtime);
+
+    // THEN
+    test.strictEqual(result, true, 'Runtimes should be equal');
+
+    test.done();
+  },
+  'unequal when name changes'(test: Test) {
+    // GIVEN
+    const runtime1 = new lambda.Runtime('python3.7', RuntimeFamily.Python, {supportsInlineCode: true});
+    const runtime2 = new lambda.Runtime('python3.6', RuntimeFamily.Python, {supportsInlineCode: true});
+
+    // WHEN
+    const result = runtime1.equals(runtime2);
+
+    // THEN
+    test.strictEqual(result, false, 'Runtimes should be unequal when name changes');
+
+    test.done();
+  },
+  'unequal when family changes'(test: Test) {
+    // GIVEN
+    const runtime1 = new lambda.Runtime('python3.7', RuntimeFamily.Python, {supportsInlineCode: true});
+    const runtime2 = new lambda.Runtime('python3.7', RuntimeFamily.Java, {supportsInlineCode: true});
+
+    // WHEN
+    const result = runtime1.equals(runtime2);
+
+    // THEN
+    test.strictEqual(result, false, 'Runtimes should be unequal when family changes');
+
+    test.done();
+  },
+  'unequal when supportsInlineCode changes'(test: Test) {
+    // GIVEN
+    const runtime1 = new lambda.Runtime('python3.7', RuntimeFamily.Python, {supportsInlineCode: true});
+    const runtime2 = new lambda.Runtime('python3.7', RuntimeFamily.Python, {supportsInlineCode: false});
+
+    // WHEN
+    const result = runtime1.equals(runtime2);
+
+    // THEN
+    test.strictEqual(result, false, 'Runtimes should be unequal when supportsInlineCode changes');
+
+    test.done();
+  },
+});
