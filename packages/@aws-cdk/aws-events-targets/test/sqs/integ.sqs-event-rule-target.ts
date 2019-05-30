30b04ab38af0622cf39e0abed2035b8e433b238b
Matteo Madeddu <matteo.madeddu@gmail.com>
2019-05-30 16:04:03 +0200

    fix typo in package.json files. fix permission issue in test https://github.com/awslabs/aws-cdk/pull/2683\#discussion_r288987667

--- packages/@aws-cdk/aws-events-targets/test/sqs/integ.sqs-event-rule-target.ts
+++ packages/@aws-cdk/aws-events-targets/test/sqs/integ.sqs-event-rule-target.ts
@@ -1,13 +1,11 @@
 import events = require('@aws-cdk/aws-events');
-import iam = require('@aws-cdk/aws-iam');
-import { PolicyStatementEffect } from '@aws-cdk/aws-iam';
 import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/cdk');
 import targets = require('../../lib');
 
 // ---------------------------------
-// Define a rule that triggers an event every 1min.
-// Connect the event with a queue. This means that the queue should have
+// Define a rule that triggers an SNS topic every 1min.
+// Connect the topic with a queue. This means that the queue should have
 // a message sent to it every minute.
 
 const app = new cdk.App();
@@ -19,21 +17,6 @@ const event = new events.Rule(stack, 'EveryMinute', {
 });
 
 const queue = new sqs.Queue(stack, 'MyQueue');
-const policy = new iam.PolicyStatement(PolicyStatementEffect.Allow);
-policy.addActions(
-  "sqs:SendMessage",
-  "sqs:SendMessageBatch",
-  "sqs:GetQueueAttributes",
-  "sqs:GetQueueUrl"
-);
-policy.addResource(queue.queueArn);
-policy.addCondition("ArnEquals", {
-  "aws:SourceArn": {
-    Ref: "EveryMinute2BBCEA8F"
-  }
-});
-policy.addPrincipal(new iam.ServicePrincipal("events"));
-queue.addToResourcePolicy(policy);
-event.addTarget(new targets.SqsQueue(queue);
+event.addTarget(new targets.SqsQueue(queue));
 
 app.run();
