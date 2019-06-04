9dd544b7488952327feadefb12aa5624ef9bc82f
Alberto Galimberti <alberto.galimberti@docebo.com>
2019-06-04 14:36:27 +0200

    feat(aws-stepfunctions-tasks): allow specifying waitForTaskToken suffix in resourceArn (#2658)
    * InvokeFunction props is now optional
    * Payload can have multiple nesting levels
    * Unit test
    * Integration test
    * Invoke lambda via SFN integrated service ARN

--- packages/@aws-cdk/aws-stepfunctions-tasks/lib/invoke-function.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/lib/invoke-function.ts
@@ -1,6 +1,7 @@
 import iam = require('@aws-cdk/aws-iam');
 import lambda = require('@aws-cdk/aws-lambda');
 import sfn = require('@aws-cdk/aws-stepfunctions');
+import { FieldUtils } from '../../aws-stepfunctions/lib/fields';
 
 /**
  * Properties for InvokeFunction
@@ -9,7 +10,7 @@ export interface InvokeFunctionProps {
   /**
    * The JSON that you want to provide to your Lambda function as input.
    */
-  readonly payload?: { [key: string]: string };
+  readonly payload?: { [key: string]: any };
 
   /**
    * Whether to pause the workflow until a task token is returned
@@ -17,6 +18,14 @@ export interface InvokeFunctionProps {
    * @default false
    */
   readonly waitForTaskToken?: boolean;
+
+  /**
+   * Whether to invoke lambda via integrated service ARN "arn:aws:states:::lambda:invoke"
+   * or via Function ARN.
+   *
+   * @default false
+   */
+  readonly invokeAsIntegratedService?: boolean;
 }
 
 /**
@@ -28,16 +37,28 @@ export interface InvokeFunctionProps {
 export class InvokeFunction implements sfn.IStepFunctionsTask {
 
   private readonly waitForTaskToken: boolean;
+  private readonly invokeAsIntegratedService: boolean;
 
-  constructor(private readonly lambdaFunction: lambda.IFunction, private readonly props: InvokeFunctionProps) {
+  constructor(private readonly lambdaFunction: lambda.IFunction, private readonly props: InvokeFunctionProps = {}) {
     this.waitForTaskToken = props.waitForTaskToken === true;
+
+    // Invoke function as integrated service if flag is in props, or if waitForTaskToken property is true
+    this.invokeAsIntegratedService = props.invokeAsIntegratedService === true || this.waitForTaskToken;
+
+    if (this.waitForTaskToken && !FieldUtils.containsTaskToken(props.payload)) {
+      throw new Error('Task Token is missing in payload');
+    }
   }
 
   public bind(_task: sfn.Task): sfn.StepFunctionsTaskProperties {
+    const resourceArn = this.invokeAsIntegratedService
+      ? 'arn:aws:states:::lambda:invoke' + this.waitForTaskToken ? '.waitForTaskToken' : ''
+      : this.lambdaFunction.functionArn;
+
+    const includeParameters = this.invokeAsIntegratedService || this.props.payload;
+
     return {
-      resourceArn: this.waitForTaskToken
-        ? 'arn:aws:states:::lambda:invoke.waitForTaskToken'
-        : this.lambdaFunction.functionArn,
+      resourceArn,
       policyStatements: [new iam.PolicyStatement()
         .addResource(this.lambdaFunction.functionArn)
         .addActions("lambda:InvokeFunction")
@@ -45,9 +66,11 @@ export class InvokeFunction implements sfn.IStepFunctionsTask {
       metricPrefixSingular: 'LambdaFunction',
       metricPrefixPlural: 'LambdaFunctions',
       metricDimensions: { LambdaFunctionArn: this.lambdaFunction.functionArn },
-      parameters: {
-        FunctionName: this.lambdaFunction.functionName,
-        ...this.props.payload && { Payload: this.props.payload },
+      ...includeParameters && {
+        parameters: {
+          ...this.invokeAsIntegratedService && { FunctionName: this.lambdaFunction.functionName },
+          ...this.props.payload && { Payload: this.props.payload },
+        }
       }
     };
   }
--- packages/@aws-cdk/aws-stepfunctions-tasks/test/invoke-function.test.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/test/invoke-function.test.ts
@@ -14,8 +14,69 @@ test('Lambda function can be used in a Task', () => {
     handler: 'index.hello',
     runtime: lambda.Runtime.Python27,
   });
+  const task = new sfn.Task(stack, 'Task', { task: new tasks.InvokeFunction(fn) });
+  new sfn.StateMachine(stack, 'SM', {
+    definition: task
+  });
+
+  // THEN
+  expect(stack).toHaveResource('AWS::StepFunctions::StateMachine', {
+    DefinitionString: {
+      "Fn::Join": ["", [
+        "{\"StartAt\":\"Task\",\"States\":{\"Task\":{\"End\":true,\"Type\":\"Task\",\"Resource\":\"",
+        { "Fn::GetAtt": ["Fn9270CBC0", "Arn"] },
+        "\"}}}"
+      ]]
+    },
+  });
+});
+
+test('Lambda function can be used in a Task with Task Token', () => {
+  // GIVEN
+  const stack = new Stack();
+
+  // WHEN
+  const fn = new lambda.Function(stack, 'Fn', {
+    code: lambda.Code.inline('hello'),
+    handler: 'index.hello',
+    runtime: lambda.Runtime.Python27,
+  });
   const task = new sfn.Task(stack, 'Task', {
-    task: new tasks.InvokeFunction(fn, { waitForTaskToken: false })
+    task: new tasks.InvokeFunction(fn, {
+      waitForTaskToken: true,
+      payload: {
+        "token.$": "$$.Task.Token"
+      }
+    })
+  });
+  new sfn.StateMachine(stack, 'SM', {
+    definition: task
+  });
+
+  // THEN
+  expect(stack).toHaveResource('AWS::StepFunctions::StateMachine', {
+    DefinitionString: {
+      "Fn::Join": ["", [
+        "{\"StartAt\":\"Task\",\"States\":{\"Task\":{\"End\":true,\"Type\":\"Task\",\"Resource\":\"",
+        { "Fn::GetAtt": ["Fn9270CBC0", "Arn"] },
+        "\"}}}"
+      ]]
+    },
+  });
+});
+
+test('Lambda function can be used in a Task with integrated service ARN', () => {
+  // GIVEN
+  const stack = new Stack();
+
+  // WHEN
+  const fn = new lambda.Function(stack, 'Fn', {
+    code: lambda.Code.inline('hello'),
+    handler: 'index.hello',
+    runtime: lambda.Runtime.Python27,
+  });
+  const task = new sfn.Task(stack, 'Task', {
+    task: new tasks.InvokeFunction(fn, { invokeAsIntegratedService: true })
   });
   new sfn.StateMachine(stack, 'SM', {
     definition: task
@@ -31,16 +92,44 @@ test('Lambda function can be used in a Task', () => {
           {
             Ref: "Fn9270CBC0"
           },
-          "\"},\"Type\":\"Task\",\"Resource\":\"",
-          {
-            "Fn::GetAtt": [
-              "Fn9270CBC0",
-              "Arn"
-            ]
-          },
-          "\"}}}"
+          "\"},\"Type\":\"Task\",\"Resource\":\".waitForTaskToken\"}}}"
         ]
       ]
+    },
+  });
+});
+
+test('Lambda function can be used in a Task with integrated service ARN and with Task Token', () => {
+  // GIVEN
+  const stack = new Stack();
+
+  // WHEN
+  const fn = new lambda.Function(stack, 'Fn', {
+    code: lambda.Code.inline('hello'),
+    handler: 'index.hello',
+    runtime: lambda.Runtime.Python27,
+  });
+  const task = new sfn.Task(stack, 'Task', {
+    task: new tasks.InvokeFunction(fn, {
+      invokeAsIntegratedService: true,
+      waitForTaskToken: true,
+      payload: {
+        "token.$": "$$.Task.Token"
+      }
+    })
+  });
+  new sfn.StateMachine(stack, 'SM', {
+    definition: task
+  });
+
+  // THEN
+  expect(stack).toHaveResource('AWS::StepFunctions::StateMachine', {
+    DefinitionString: {
+      "Fn::Join": ["", [
+        "{\"StartAt\":\"Task\",\"States\":{\"Task\":{\"End\":true,\"Type\":\"Task\",\"Resource\":\"",
+        { "Fn::GetAtt": ["Fn9270CBC0", "Arn"] },
+        "\"}}}"
+      ]]
     },
   });
 });
