adf12a69967db24b7af64fc28cb83b6f10a20a29
Piradeep Kandasamy <piradeep@amazon.com>
2019-06-06 15:03:46 -0700

    Remove LoadBalancedFargateServiceApplet as Applets are no longer supported

--- packages/@aws-cdk/aws-ecs-patterns/lib/index.ts
+++ packages/@aws-cdk/aws-ecs-patterns/lib/index.ts
@@ -4,7 +4,6 @@ export * from './base/queue-worker-service-base';
 
 export * from './ecs/load-balanced-ecs-service';
 export * from './fargate/load-balanced-fargate-service';
-export * from './fargate/load-balanced-fargate-service-applet';
 export * from './base/load-balanced-service-base';
 
 export * from './ecs/scheduled-ecs-task';
