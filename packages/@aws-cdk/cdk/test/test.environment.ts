eb1b908b969205557577c24878e800bd68d42433
Elad Ben-Israel <benisrae@amazon.com>
2019-06-13 20:56:49 +0300

    fix(core): use default account/region when environment is not specified
    The PR #2728 caused a regression on how the framework behaves when a
    stack's environment (account and/or region) are not specified.
    
    The behavior is: if account and/or region are not specified at the stack
    level (or set to Aws.accountId and Aws.region, respectively), the cloud
    assembly manifest of this stack will have an environment specification
    based on the defaults passed in from the CLI.
    
    This is a temporary solution which fixes #2853 but doesn't fully implement
    the behavior we eventually want (which is documented in #2866).

--- packages/@aws-cdk/cdk/test/test.environment.ts
+++ packages/@aws-cdk/cdk/test/test.environment.ts
@@ -1,6 +1,7 @@
 import { DEFAULT_ACCOUNT_CONTEXT_KEY, DEFAULT_REGION_CONTEXT_KEY } from '@aws-cdk/cx-api';
+import cxapi = require('@aws-cdk/cx-api');
 import { Test } from 'nodeunit';
-import { App, Stack, Token } from '../lib';
+import { App, Aws, Stack, Token } from '../lib';
 
 export = {
   'By default, environment region and account are not defined and resolve to intrinsics'(test: Test) {
@@ -39,5 +40,143 @@ export = {
     test.deepEqual(stack2.resolve(stack2.region), { Ref: 'AWS::Region' });
 
     test.done();
+  },
+
+  'environment defaults': {
+    'default-account-unknown-region'(test: Test) {
+      // GIVEN
+      const app = new App();
+
+      // WHEN
+      app.node.setContext(cxapi.DEFAULT_ACCOUNT_CONTEXT_KEY, 'my-default-account');
+      const stack = new Stack(app, 'stack');
+
+      // THEN
+      test.deepEqual(stack.resolve(stack.account), { Ref: 'AWS::AccountId' }); // TODO: after we implement #2866 this should be 'my-default-account'
+      test.deepEqual(stack.resolve(stack.region), { Ref: 'AWS::Region' });
+      test.deepEqual(app.synth().getStack(stack.stackName).environment, {
+        account: 'my-default-account',
+        region: 'unknown-region',
+        name: 'aws://my-default-account/unknown-region'
+      });
+
+      test.done();
+    },
+
+    'default-account-explicit-region'(test: Test) {
+      // GIVEN
+      const app = new App();
+
+      // WHEN
+      app.node.setContext(cxapi.DEFAULT_ACCOUNT_CONTEXT_KEY, 'my-default-account');
+      const stack = new Stack(app, 'stack', { env: { region: 'explicit-region' }});
+
+      // THEN
+      test.deepEqual(stack.resolve(stack.account), { Ref: 'AWS::AccountId' }); // TODO: after we implement #2866 this should be 'my-default-account'
+      test.deepEqual(stack.resolve(stack.region), 'explicit-region');
+      test.deepEqual(app.synth().getStack(stack.stackName).environment, {
+        account: 'my-default-account',
+        region: 'explicit-region',
+        name: 'aws://my-default-account/explicit-region'
+      });
+
+      test.done();
+    },
+
+    'explicit-account-explicit-region'(test: Test) {
+      // GIVEN
+      const app = new App();
+
+      // WHEN
+      app.node.setContext(cxapi.DEFAULT_ACCOUNT_CONTEXT_KEY, 'my-default-account');
+      const stack = new Stack(app, 'stack', { env: {
+        account: 'explicit-account',
+        region: 'explicit-region'
+      }});
+
+      // THEN
+      test.deepEqual(stack.resolve(stack.account), 'explicit-account');
+      test.deepEqual(stack.resolve(stack.region), 'explicit-region');
+      test.deepEqual(app.synth().getStack(stack.stackName).environment, {
+        account: 'explicit-account',
+        region: 'explicit-region',
+        name: 'aws://explicit-account/explicit-region'
+      });
+
+      test.done();
+    },
+
+    'default-account-default-region'(test: Test) {
+      // GIVEN
+      const app = new App();
+
+      // WHEN
+      app.node.setContext(cxapi.DEFAULT_ACCOUNT_CONTEXT_KEY, 'my-default-account');
+      app.node.setContext(cxapi.DEFAULT_REGION_CONTEXT_KEY, 'my-default-region');
+      const stack = new Stack(app, 'stack');
+
+      // THEN
+      test.deepEqual(stack.resolve(stack.account), { Ref: 'AWS::AccountId' }); // TODO: after we implement #2866 this should be 'my-default-account'
+      test.deepEqual(stack.resolve(stack.region), { Ref: 'AWS::Region' });     // TODO: after we implement #2866 this should be 'my-default-region'
+      test.deepEqual(app.synth().getStack(stack.stackName).environment, {
+        account: 'my-default-account',
+        region: 'my-default-region',
+        name: 'aws://my-default-account/my-default-region'
+      });
+
+      test.done();
+    },
+
+    'token-account-token-region-no-defaults'(test: Test) {
+      // GIVEN
+      const app = new App();
+
+      // WHEN
+      app.node.setContext(cxapi.DEFAULT_ACCOUNT_CONTEXT_KEY, 'my-default-account');
+      app.node.setContext(cxapi.DEFAULT_REGION_CONTEXT_KEY, 'my-default-region');
+      const stack = new Stack(app, 'stack', {
+        env: {
+          account: Aws.accountId,
+          region: Aws.region
+        }
+      });
+
+      // THEN
+      test.deepEqual(stack.resolve(stack.account), { Ref: 'AWS::AccountId' });
+      test.deepEqual(stack.resolve(stack.region), { Ref: 'AWS::Region' });
+      test.deepEqual(app.synth().getStack(stack.stackName).environment, {
+        account: 'my-default-account',
+        region: 'my-default-region',
+        name: 'aws://my-default-account/my-default-region'
+      });
+
+      test.done();
+    },
+
+    'token-account-token-region-with-defaults'(test: Test) {
+      // GIVEN
+      const app = new App();
+
+      // WHEN
+      const stack = new Stack(app, 'stack', {
+        env: {
+          account: Aws.accountId,
+          region: Aws.region
+        }
+      });
+
+      // THEN
+      test.deepEqual(stack.resolve(stack.account), { Ref: 'AWS::AccountId' });
+      test.deepEqual(stack.resolve(stack.region), { Ref: 'AWS::Region' });
+      test.deepEqual(app.synth().getStack(stack.stackName).environment, {
+        account: 'unknown-account',
+        region: 'unknown-region',
+        name: 'aws://unknown-account/unknown-region'
+      });
+
+      test.done();
+    }
+
+
   },
 };
