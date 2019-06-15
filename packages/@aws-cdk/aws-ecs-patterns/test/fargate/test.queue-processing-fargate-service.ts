23fa6a69d4996d9bb3c908f169c77cc946cf76ec
Adam Ruka <adamruka@amazon.com>
2019-06-14 17:09:00 -0700

    Fix @aws-ecs-patterns.

--- packages/@aws-cdk/aws-ecs-patterns/test/fargate/test.queue-processing-fargate-service.ts
+++ packages/@aws-cdk/aws-ecs-patterns/test/fargate/test.queue-processing-fargate-service.ts
@@ -69,7 +69,9 @@ export = {
     const vpc = new ec2.Vpc(stack, 'VPC');
     const cluster = new ecs.Cluster(stack, 'Cluster', { vpc });
     cluster.addCapacity('DefaultAutoScalingGroup', { instanceType: new ec2.InstanceType('t2.micro') });
-    const queue = new sqs.Queue(stack, 'fargate-test-queue', { queueName: 'fargate-test-sqs-queue'});
+    const queue = new sqs.Queue(stack, 'fargate-test-queue', {
+      queueName: cdk.PhysicalName.of('fargate-test-sqs-queue'),
+    });
 
     // WHEN
     new ecsPatterns.QueueProcessingFargateService(stack, 'Service', {
