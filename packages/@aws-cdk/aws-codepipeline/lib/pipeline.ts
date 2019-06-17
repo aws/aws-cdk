c70dc53abb35a3f93e191ca42e4c17d3dc3054da
Adam Ruka <adamruka@amazon.com>
2019-06-17 11:27:33 -0700

    refactor(codepipeline): rename `name` in `StageProps` to `stageName`. (#2882)
    BREAKING CHANGE: the property designating the name of the stage when creating a CodePipeline is now called `stageName` instead of `name`

--- packages/@aws-cdk/aws-codepipeline/lib/pipeline.ts
+++ packages/@aws-cdk/aws-codepipeline/lib/pipeline.ts
@@ -38,7 +38,7 @@ export interface StageProps {
   /**
    * The physical, human-readable name to assign to this Pipeline Stage.
    */
-  readonly name: string;
+  readonly stageName: string;
 
   /**
    * The list of Actions to create this Stage with.
@@ -279,8 +279,8 @@ export class Pipeline extends PipelineBase {
    */
   public addStage(props: StageAddToPipelineProps): IStage {
     // check for duplicate Stages and names
-    if (this.stages.find(s => s.stageName === props.name)) {
-      throw new Error(`Stage with duplicate name '${props.name}' added to the Pipeline`);
+    if (this.stages.find(s => s.stageName === props.stageName)) {
+      throw new Error(`Stage with duplicate name '${props.stageName}' added to the Pipeline`);
     }
 
     const stage = new Stage(props, this);
