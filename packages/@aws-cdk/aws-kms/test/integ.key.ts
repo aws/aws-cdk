c616422849c711555fa94fd8c662e9816b0f1ae4
Elad Ben-Israel <benisrae@amazon.com>
2019-06-18 11:52:02 +0300

    refactor: clean up API for removal policy across the library (#2893)
    - Normalize how deletion policy is configured for stateful resources across the AWS Construct Library. - Simplify the model by removing the future-proofing of "RemovalPolicy.Forbid".
    - When `RemovalPolicy.Destory` is used, explicitly specify `DeletionPolicy: Delete` in CloudFormation (as oppose to relying on the fact the "Delete" is the default).
    
    BREAKING CHANGE: multiple modules have been changed to use `cdk.RemovalPolicy`
    to configure the resource's removal policy.
    * **core:** `applyRemovalPolicy` is now `CfnResource.applyRemovalPolicy`.
    * **core:** `RemovalPolicy.Orphan` has been renamed to `Retain`.
    * **core:** `RemovalPolicy.Forbid` has been removed, use `Retain`.
    * **ecr:** `RepositoryProps.retain` is now `removalPolicy`, and defaults to `Retain` instead of remove since ECR is a stateful resource
    * **kms:** `KeyProps.retain` is now `removalPolicy`
    * **logs:** `LogGroupProps.retainLogGroup` is now `removalPolicy`
    * **logs:** `LogStreamProps.retainLogStream` is now `removalPolicy`
    * **rds:** `DatabaseClusterProps.deleteReplacePolicy` is now `removalPolicy`
    * **rds:** `DatabaseInstanceNewProps.deleteReplacePolicy` is now `removalPolicy`

--- packages/@aws-cdk/aws-kms/test/integ.key.ts
+++ packages/@aws-cdk/aws-kms/test/integ.key.ts
@@ -1,13 +1,13 @@
 import { PolicyStatement } from '@aws-cdk/aws-iam';
 import iam = require('@aws-cdk/aws-iam');
-import { App, Stack } from '@aws-cdk/cdk';
+import { App, RemovalPolicy, Stack } from '@aws-cdk/cdk';
 import { Key } from '../lib';
 
 const app = new App();
 
 const stack = new Stack(app, `aws-cdk-kms-1`);
 
-const key = new Key(stack, 'MyKey', { retain: false });
+const key = new Key(stack, 'MyKey', { removalPolicy: RemovalPolicy.Destroy });
 
 key.addToResourcePolicy(new PolicyStatement({
   resources: ['*'],
