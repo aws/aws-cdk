d9003466b4a222835c670f74a13e6b967e59443b
Rico Huijbers <rix0rrr@gmail.com>
2019-06-18 12:06:31 +0200

    refactor(events): clean up Events APIs (#2840)
    Biggest change is modeling the schedule types.
    
    BREAKING CHANGE:
    
    * **events**: `EventPattern.detail` is now a map.
    * **events**: `scheduleExpression: string` is now `schedule: Schedule`.

--- packages/@aws-cdk/aws-events-targets/test/codebuild/codebuild.test.ts
+++ packages/@aws-cdk/aws-events-targets/test/codebuild/codebuild.test.ts
@@ -8,7 +8,9 @@ test('use codebuild project as an eventrule target', () => {
   // GIVEN
   const stack = new Stack();
   const project = new codebuild.PipelineProject(stack, 'MyProject');
-  const rule = new events.Rule(stack, 'Rule', { scheduleExpression: 'rate(1 min)' });
+  const rule = new events.Rule(stack, 'Rule', {
+    schedule: events.Schedule.expression('rate(1 min)')
+  });
 
   // WHEN
   rule.addTarget(new targets.CodeBuildProject(project));
