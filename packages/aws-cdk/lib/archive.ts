5056d4c49f7ce32f93c0a3ce199fb2195ceb1201
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2019-06-19 16:50:55 +0200

    feat(toolkit): ensure consistent zip files
    Zip files were not consistent across deploys resulting in unnecessary S3 uploads and stack updates.
    
    Ensure consistency by appending files in series (guarantees file ordering in the zip) and reseting
    dates (guarantees same hash for same content).
    
    Closes #1997, Closes #2759

--- packages/aws-cdk/lib/archive.ts
+++ packages/aws-cdk/lib/archive.ts
@@ -1,23 +1,39 @@
 import archiver = require('archiver');
 import crypto = require('crypto');
 import fs = require('fs-extra');
+import glob = require('glob');
+import path = require('path');
 
 export function zipDirectory(directory: string, outputFile: string): Promise<void> {
   return new Promise((ok, fail) => {
     const output = fs.createWriteStream(outputFile);
     const archive = archiver('zip');
+
     // The below options are needed to support following symlinks when building zip files:
-    // -  nodir: This will prevent symlinks themselves from being copied into the zip.
+    // - nodir: This will prevent symlinks themselves from being copied into the zip.
     // - follow: This will follow symlinks and copy the files within.
     const globOptions = {
       dot: true,
       nodir: true,
       follow: true,
-      cwd: directory
+      cwd: directory,
     };
-    archive.glob('**', globOptions);
-    archive.pipe(output);
-    archive.finalize();
+    const files = glob.sync('**', globOptions); // The output here is already sorted
+
+    output.on('open', async () => {
+      archive.pipe(output);
+
+      await files.reduce(async (acc, file) => { // Append files serially to ensure file order
+        await acc;
+        const data = await fs.readFile(path.join(directory, file));
+        archive.append(data, {
+          name: file,
+          date: new Date(0), // reset dates to get the same hash for the same content
+        });
+      }, Promise.resolve());
+
+      archive.finalize();
+    });
 
     archive.on('warning', fail);
     archive.on('error', fail);
