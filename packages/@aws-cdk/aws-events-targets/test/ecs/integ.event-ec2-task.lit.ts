86c7bb3d4f11ad5c04d41f3429c5223b09b5041a
Rico Huijbers <rix0rrr@gmail.com>
2019-06-21 15:08:56 +0200

    Update usage sites

--- packages/@aws-cdk/aws-events-targets/test/ecs/integ.event-ec2-task.lit.ts
+++ packages/@aws-cdk/aws-events-targets/test/ecs/integ.event-ec2-task.lit.ts
@@ -25,7 +25,7 @@ class EventStack extends cdk.Stack {
     taskDefinition.addContainer('TheContainer', {
       image: ecs.ContainerImage.fromAsset(path.resolve(__dirname, 'eventhandler-image')),
       memoryLimitMiB: 256,
-      logging: new ecs.AwsLogDriver(this, 'TaskLogging', { streamPrefix: 'EventDemo' })
+      logging: new ecs.AwsLogDriver({ streamPrefix: 'EventDemo' })
     });
 
     // An Rule that describes the event trigger (in this case a scheduled run)
