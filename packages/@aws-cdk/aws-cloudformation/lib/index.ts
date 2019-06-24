6f4f689aacc1714e697e0094fa6fdcee5ad0bee9
Elad Ben-Israel <benisrae@amazon.com>
2019-06-24 17:13:35 +0300

    refactor(cloudformation): extract "custom-resources" module (#3027)
    Extract the AwsCustomResource construct to a separate module called @aws-cdk/custom-resources, dedicated to special custom resources such as this one.
    
    Also, stop bundling the entire AWS SDK and instead extract `apis/metadata.json` during build time which fixes #2951.
    
    BREAKING CHANGE: The `AwsCustomResource` class was moved to a new module called @aws-cdk/custom-resource

--- packages/@aws-cdk/aws-cloudformation/lib/index.ts
+++ packages/@aws-cdk/aws-cloudformation/lib/index.ts
@@ -1,6 +1,5 @@
 export * from './cloud-formation-capabilities';
 export * from './custom-resource';
-export * from './aws-custom-resource';
 
 // AWS::CloudFormation CloudFormation Resources:
 export * from './cloudformation.generated';
