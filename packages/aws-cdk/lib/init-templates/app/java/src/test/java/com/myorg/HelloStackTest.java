3b905f21cfba4088dd04ae9835f4c173f1be8e28
Elad Ben-Israel <benisrae@amazon.com>
2019-06-25 16:38:33 +0300

    fix(cli): fix java app template (#3061)
    follow up on #3058 - since we renamed the core package namesapce to "software.amazon.awscdk.core"

--- packages/aws-cdk/lib/init-templates/app/java/src/test/java/com/myorg/HelloStackTest.java
+++ packages/aws-cdk/lib/init-templates/app/java/src/test/java/com/myorg/HelloStackTest.java
@@ -1,6 +1,6 @@
 package com.myorg;
 
-import software.amazon.awscdk.App;
+import software.amazon.awscdk.core.App;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
