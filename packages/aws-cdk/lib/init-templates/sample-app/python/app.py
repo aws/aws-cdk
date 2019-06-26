e5d18b7895173b49bb55f90114eee17612cfd2d5
shivlaks <32604953+shivlaks@users.noreply.github.com>
2019-06-25 23:41:17 -0700

    fix(cli): fix Python sample-app template (#3071)
    follow up on #3058 - since we renamed the core package namesapce to "software.amazon.awscdk.core"
    
    Fixes #3069

--- packages/aws-cdk/lib/init-templates/sample-app/python/app.py
+++ packages/aws-cdk/lib/init-templates/sample-app/python/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from aws_cdk import cdk
+from aws_cdk import core
 
 from hello.hello_stack import MyStack
 
 
-app = cdk.App()
+app = core.App()
 MyStack(app, "hello-cdk-1", env={'region': 'us-east-2'})
 MyStack(app, "hello-cdk-2", env={'region': 'us-west-2'})
 
-app.run()
+app.synth()
