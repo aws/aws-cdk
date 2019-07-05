a2c10af7a9943eadab213331a9728f2364698de8
Mitch Garnaat <mitch@garnaat.com>
2019-07-04 18:45:20 -0700

    Pass kwargs along to superclass (#3205)

--- packages/aws-cdk/lib/init-templates/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
+++ packages/aws-cdk/lib/init-templates/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
@@ -4,6 +4,6 @@ from aws_cdk import core
 class %name.PascalCased%Stack(core.Stack):
 
     def __init__(self, app: core.App, id: str, **kwargs) -> None:
-        super().__init__(app, id)
+        super().__init__(app, id, **kwargs)
 
         # The code that defines your stack goes here
