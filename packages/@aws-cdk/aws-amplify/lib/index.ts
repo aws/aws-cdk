d004a4a1781d9633beefb74bbbcaf7043c9f6ccc
Simon Thulbourn <sthulb@users.noreply.github.com>
2019-07-09 19:50:53 +0100

    adds shared and branch resources

--- packages/@aws-cdk/aws-amplify/lib/index.ts
+++ packages/@aws-cdk/aws-amplify/lib/index.ts
@@ -1,4 +1,6 @@
 // AWS::Amplify CloudFormation Resources:
 export * from './amplify.generated';
 export * from './app';
+export * from './branch';
 export * from './domain';
+export * from './shared';
