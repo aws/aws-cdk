a488e5cdb9e198933e923768fbfa007077e910e1
Jonathan Goldwasser <jogold@users.noreply.github.com>
2019-07-23 16:26:29 +0200

    fix(s3): fail early with bad notification filters (#3397)
    Avoid CF deploy time errors when specifying multiple prefixes or suffixes in notification filters.
    
    Closes #3347
    Possibly something to fix in v2.0 (#3398)

--- packages/@aws-cdk/aws-s3/test/test.notification.ts
+++ packages/@aws-cdk/aws-s3/test/test.notification.ts
@@ -21,4 +21,76 @@ export = {
 
     test.done();
   },
+
+  'can specify prefix and suffix filter rules'(test: Test) {
+    const stack = new cdk.Stack();
+
+    const bucket = new s3.Bucket(stack, 'MyBucket');
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED, {
+      bind: () => ({
+        arn: 'ARN',
+        type: s3.BucketNotificationDestinationType.TOPIC
+      }),
+    }, { prefix: 'images/', suffix: '.png' });
+
+    expect(stack).to(haveResource('Custom::S3BucketNotifications', {
+      NotificationConfiguration: {
+        TopicConfigurations: [
+          {
+            Events: [
+              's3:ObjectCreated:*'
+            ],
+            Filter: {
+              Key: {
+                FilterRules: [
+                  {
+                    Name: 'suffix',
+                    Value: '.png'
+                  },
+                  {
+                    Name: 'prefix',
+                    Value: 'images/'
+                  }
+                ]
+              }
+            },
+            TopicArn: 'ARN'
+          }
+        ]
+      }
+    }));
+
+    test.done();
+  },
+
+  'throws with multiple prefix rules in a filter'(test: Test) {
+    const stack = new cdk.Stack();
+
+    const bucket = new s3.Bucket(stack, 'MyBucket');
+
+    test.throws(() => bucket.addEventNotification(s3.EventType.OBJECT_CREATED, {
+      bind: () => ({
+        arn: 'ARN',
+        type: s3.BucketNotificationDestinationType.TOPIC
+      }),
+    }, { prefix: 'images/'}, { prefix: 'archive/' }), /prefix rule/);
+
+    test.done();
+  },
+
+  'throws with multiple suffix rules in a filter'(test: Test) {
+    const stack = new cdk.Stack();
+
+    const bucket = new s3.Bucket(stack, 'MyBucket');
+
+    test.throws(() => bucket.addEventNotification(s3.EventType.OBJECT_CREATED, {
+      bind: () => ({
+        arn: 'ARN',
+        type: s3.BucketNotificationDestinationType.TOPIC
+      }),
+    }, { suffix: '.png'}, { suffix: '.zip' }), /suffix rule/);
+
+    test.done();
+  }
 };
