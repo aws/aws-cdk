78491fdabf73047fd1fb1b78f22db6b54455676b
aereal <aereal@aereal.org>
2019-07-24 18:56:22 +0900

    refactor(toolkit): awaits all of files to be appended

--- packages/aws-cdk/lib/archive.ts
+++ packages/aws-cdk/lib/archive.ts
@@ -4,6 +4,15 @@ import fs = require('fs-extra');
 import glob = require('glob');
 import path = require('path');
 
+const appendFiles = async (directory: string, files: string[], archive: archiver.Archiver): Promise<void> => {
+  await Promise.all(files.map(async file => {
+    archive.append(fs.createReadStream(path.join(directory, file)), {
+      name: file,
+      date: new Date('1980-01-01T00:00:00.000Z'), // reset dates to get the same hash for the same content
+    });
+  }));
+};
+
 export function zipDirectory(directory: string, outputFile: string): Promise<void> {
   return new Promise((ok, fail) => {
     // The below options are needed to support following symlinks when building zip files:
@@ -24,17 +33,12 @@ export function zipDirectory(directory: string, outputFile: string): Promise<voi
     archive.on('error', fail);
     archive.pipe(output);
 
-    files.forEach(file => { // Append files serially to ensure file order
-      archive.append(fs.createReadStream(path.join(directory, file)), {
-        name: file,
-        date: new Date('1980-01-01T00:00:00.000Z'), // reset dates to get the same hash for the same content
-      });
+    appendFiles(directory, files, archive).then(() => {
+      archive.finalize();
+
+      // archive has been finalized and the output file descriptor has closed, resolve promise
+      output.once('close', () => ok());
     });
-
-    archive.finalize();
-
-    // archive has been finalized and the output file descriptor has closed, resolve promise
-    output.once('close', () => ok());
   });
 }
 
