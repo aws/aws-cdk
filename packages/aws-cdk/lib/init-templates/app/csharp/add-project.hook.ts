86d4047b71f79aa0bc7e48fe96725cd13927d947
Geoff Weinhold <gweinhold@gmail.com>
2019-07-27 21:26:50 -0700

    Templatized csharp init to a base skeleton.

--- packages/aws-cdk/lib/init-templates/app/csharp/add-project.hook.ts
+++ packages/aws-cdk/lib/init-templates/app/csharp/add-project.hook.ts
@@ -3,8 +3,8 @@ import * as path from 'path';
 import { InvokeHook } from '../../../init';
 
 export const invoke: InvokeHook = async (targetDirectory: string) => {
-  const slnPath = path.join(targetDirectory, "src", "HelloCdk.sln");
-  const csprojPath = path.join(targetDirectory, "src", "HelloCdk", "HelloCdk.csproj");
+  const slnPath = path.join(targetDirectory, "src", "%name.PascalCased%.sln");
+  const csprojPath = path.join(targetDirectory, "src", "%name.PascalCased%", "%name.PascalCased%.csproj");
 
   const child = child_process.spawn('dotnet', [ 'sln', slnPath, 'add', csprojPath ], {
     // Need this for Windows where we want .cmd and .bat to be found as well.
@@ -26,7 +26,7 @@ export const invoke: InvokeHook = async (targetDirectory: string) => {
       if (code === 0) {
         resolve(Buffer.concat(stdout).toString('utf-8'));
       } else {
-        reject(new Error(`Could not add project HelloCdk.csproj to solution HelloCdk.sln. Error code: ${code}`));
+        reject(new Error(`Could not add project %name.PascalCased%.csproj to solution %name.PascalCased%.sln. Error code: ${code}`));
       }
     });
   });
