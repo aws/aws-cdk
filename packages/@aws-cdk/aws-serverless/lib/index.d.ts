c26d04551bdad702afff9af4fb38a6112ba279e3
Yandy Ramirez <yandyr@gmail.com>
2019-07-28 22:12:21 -0400

    delete packages no longer in main repo

--- packages/@aws-cdk/aws-serverless/lib/index.d.ts
+++
@@ -1,1 +0,0 @@
-export * from '@aws-cdk/aws-sam';
--- packages/@aws-cdk/aws-serverless/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("@aws-cdk/aws-sam"));
-// tslint:disable-next-line: no-console
-console.error('WARNING: The @aws-cdk/serverless package was renamed to @aws-cdk/sam. Please update your dependencies!');
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLHNDQUFpQztBQUVqQyx1Q0FBdUM7QUFDdkMsT0FBTyxDQUFDLEtBQUssQ0FBQyx3R0FBd0csQ0FBQyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0ICogZnJvbSAnQGF3cy1jZGsvYXdzLXNhbSc7XG5cbi8vIHRzbGludDpkaXNhYmxlLW5leHQtbGluZTogbm8tY29uc29sZVxuY29uc29sZS5lcnJvcignV0FSTklORzogVGhlIEBhd3MtY2RrL3NlcnZlcmxlc3MgcGFja2FnZSB3YXMgcmVuYW1lZCB0byBAYXdzLWNkay9zYW0uIFBsZWFzZSB1cGRhdGUgeW91ciBkZXBlbmRlbmNpZXMhJyk7XG4iXX0=
