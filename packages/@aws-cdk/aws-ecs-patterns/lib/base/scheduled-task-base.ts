adf9cf6ccd2343b733f7367eb6397440f6a0d7f6
Piradeep Kandasamy <piradeep@amazon.com>
2019-07-29 14:26:06 -0700

    fix(aws-ecs-patterns): update ecs-patterns to be consistent across constructs (#3404)
    Update ecs-patterns to be consistent across constructs

--- packages/@aws-cdk/aws-ecs-patterns/lib/base/scheduled-task-base.ts
+++ packages/@aws-cdk/aws-ecs-patterns/lib/base/scheduled-task-base.ts
@@ -1,19 +1,26 @@
-import ecs = require('@aws-cdk/aws-ecs');
-import { ICluster } from '@aws-cdk/aws-ecs';
-import events = require('@aws-cdk/aws-events');
-import eventsTargets = require('@aws-cdk/aws-events-targets');
-import cdk = require('@aws-cdk/core');
+import { Schedule } from "@aws-cdk/aws-applicationautoscaling";
+import { AwsLogDriver, ContainerImage, ICluster, Secret, TaskDefinition } from "@aws-cdk/aws-ecs";
+import { Rule } from "@aws-cdk/aws-events";
+import { EcsTask } from "@aws-cdk/aws-events-targets";
+import { Construct } from "@aws-cdk/core";
 
+/**
+ * The properties for the base ScheduledEc2Task or ScheduledFargateTask task.
+ */
 export interface ScheduledTaskBaseProps {
   /**
-   * The cluster where your service will be deployed.
+   * The name of the cluster that hosts the service.
    */
-  readonly cluster: ecs.ICluster;
+  readonly cluster: ICluster;
 
   /**
-   * The image to start.
+   * The image used to start a container.
+   *
+   * This string is passed directly to the Docker daemon.
+   * Images in the Docker Hub registry are available by default.
+   * Other repositories are specified with either repository-url/image:tag or repository-url/image@digest.
    */
-  readonly image: ecs.ContainerImage;
+  readonly image: ContainerImage;
 
   /**
    * The schedule or rate (frequency) that determines when CloudWatch Events
@@ -22,17 +29,19 @@ export interface ScheduledTaskBaseProps {
    *
    * @see http://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html
    */
-  readonly schedule: events.Schedule;
+  readonly schedule: Schedule;
 
   /**
-   * The CMD value to pass to the container. A string with commands delimited by commas.
+   * The command that is passed to the container.
    *
-   * @default none
+   * If you provide a shell command as a single string, you have to quote command-line arguments.
+   *
+   * @default - CMD value built into container image.
    */
   readonly command?: string[];
 
   /**
-   * Number of desired copies of running tasks.
+   * The desired number of instantiations of the task definition to keep running on the service.
    *
    * @default 1
    */
@@ -50,37 +59,46 @@ export interface ScheduledTaskBaseProps {
    *
    * @default - No secret environment variables.
    */
-  readonly secrets?: { [key: string]: ecs.Secret };
+  readonly secrets?: { [key: string]: Secret };
 }
 
 /**
- * A scheduled task base that will be initiated off of cloudwatch events.
+ * The base class for ScheduledEc2Task and ScheduledFargateTask tasks.
  */
-export abstract class ScheduledTaskBase extends cdk.Construct {
+export abstract class ScheduledTaskBase extends Construct {
+  /**
+   * The name of the cluster that hosts the service.
+   */
   public readonly cluster: ICluster;
+  /**
+   * The desired number of instantiations of the task definition to keep running on the service.
+   */
   public readonly desiredTaskCount: number;
-  public readonly eventRule: events.Rule;
+  public readonly eventRule: Rule;
 
-  constructor(scope: cdk.Construct, id: string, props: ScheduledTaskBaseProps) {
+  /**
+   * Constructs a new instance of the ScheduledTaskBase class.
+   */
+  constructor(scope: Construct, id: string, props: ScheduledTaskBaseProps) {
     super(scope, id);
 
     this.cluster = props.cluster;
     this.desiredTaskCount = props.desiredTaskCount || 1;
 
     // An EventRule that describes the event trigger (in this case a scheduled run)
-    this.eventRule = new events.Rule(this, 'ScheduledEventRule', {
+    this.eventRule = new Rule(this, 'ScheduledEventRule', {
       schedule: props.schedule,
     });
   }
 
   /**
-   * Create an ecs task using the task definition provided and add it to the scheduled event rule
+   * Create an ECS task using the task definition provided and add it to the scheduled event rule.
    *
    * @param taskDefinition the TaskDefinition to add to the event rule
    */
-  protected addTaskDefinitionToEventTarget(taskDefinition: ecs.TaskDefinition): eventsTargets.EcsTask {
+  protected addTaskDefinitionToEventTarget(taskDefinition: TaskDefinition): EcsTask {
     // Use the EcsTask as the target of the EventRule
-    const eventRuleTarget = new eventsTargets.EcsTask( {
+    const eventRuleTarget = new EcsTask( {
       cluster: this.cluster,
       taskDefinition,
       taskCount: this.desiredTaskCount
@@ -96,7 +114,7 @@ export abstract class ScheduledTaskBase extends cdk.Construct {
    *
    * @param prefix the Cloudwatch logging prefix
    */
-  protected createAWSLogDriver(prefix: string): ecs.AwsLogDriver {
-    return new ecs.AwsLogDriver({ streamPrefix: prefix });
+  protected createAWSLogDriver(prefix: string): AwsLogDriver {
+    return new AwsLogDriver({ streamPrefix: prefix });
   }
 }
