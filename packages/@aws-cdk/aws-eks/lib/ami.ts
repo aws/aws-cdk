af43c242ee1fc6437283fc65907f1468728ead64
Pahud Hsieh <pahudnet@gmail.com>
2019-08-04 08:08:08 +0800

    Update ami.ts
    - 1.10 was deprecated and users can no longer create 1.10 cluster 
    - add HKG(ap-east-1) AMIs 
    check doc history for details https://docs.aws.amazon.com/eks/latest/userguide/update-cluster.html

--- packages/@aws-cdk/aws-eks/lib/ami.ts
+++ packages/@aws-cdk/aws-eks/lib/ami.ts
@@ -61,25 +61,11 @@ export function nodeTypeForInstanceType(instanceType: ec2.InstanceType) {
  * @see https://docs.aws.amazon.com/eks/latest/userguide/eks-optimized-ami.html
  */
 const EKS_AMI: {[version: string]: {[type: string]: {[region: string]: string}}} = {
-  '1.10': parseTable(`
-    US East (Ohio) (us-east-2) 	ami-0523ec257fff1261d 	ami-006382264ad5fc773
-    US East (N. Virginia) (us-east-1) 	ami-01c1c96b9aa69de37 	ami-0f1b9925c9ace2043
-    US West (Oregon) (us-west-2) 	ami-021dd1fb7ba7e6e51 	ami-0c3c59a4a7bea5678
-    Asia Pacific (Mumbai) (ap-south-1) 	ami-04c2ed5ff15a580f4 	ami-0ea42cc96a8375851
-    Asia Pacific (Tokyo) (ap-northeast-1) 	ami-02ffa4511b4baa5fa 	ami-00f64026212ad62c0
-    Asia Pacific (Seoul) (ap-northeast-2) 	ami-06295f3e6390dae00 	ami-060b7aed71dfaa5f5
-    Asia Pacific (Singapore) (ap-southeast-1) 	ami-07f8ccb046b3ce697 	ami-029b65710f075da3d
-    Asia Pacific (Sydney) (ap-southeast-2) 	ami-03ebcd449b224e0a3 	ami-0f13220d49a34e787
-    EU (Frankfurt) (eu-central-1) 	ami-0c40973ffcf8bca40 	ami-08d32cef88aa48343
-    EU (Ireland) (eu-west-1) 	ami-06a96b4cfd627430b 	ami-058a8eec818dc3910
-    EU (London) (eu-west-2) 	ami-03356e704fb004162 	ami-0cd6f7f1f7ceedc27
-    EU (Paris) (eu-west-3) 	ami-0eb77a4ca7135122b 	ami-01e3c54f23b6b02b3
-    EU (Stockholm) (eu-north-1) 	ami-028df8ba9b8603bdd 	ami-0d1ee923abbb8cf6c
-    `),
   '1.11': parseTable(`
     US East (Ohio) (us-east-2) 	ami-03c6648b74285020f 	ami-0b87186dda80931ee
     US East (N. Virginia) (us-east-1) 	ami-0a5f5d5b0f6f58199 	ami-07207754196c1a8fc
     US West (Oregon) (us-west-2) 	ami-057d1c0dcb254a878 	ami-052da6a4e0ae156ad
+    Asia Pacific (Hong Kong) (ap-east-1) 	ami-006e7a52666ab7494 	N/A*
     Asia Pacific (Mumbai) (ap-south-1) 	ami-00f1adebe5ab9a431 	ami-04645af6384529c5d
     Asia Pacific (Tokyo) (ap-northeast-1) 	ami-0a0b6606652f9b3b9 	ami-0a8f4e1f9bf09a81f
     Asia Pacific (Seoul) (ap-northeast-2) 	ami-0c84b3f055cda1afb 	ami-01db6bb089f6adfcf
@@ -95,6 +81,7 @@ const EKS_AMI: {[version: string]: {[type: string]: {[region: string]: string}}}
     US East (Ohio) (us-east-2) 	ami-0fe61ae4c397e710d 	ami-067d88fb64d3d7990
     US East (N. Virginia) (us-east-1) 	ami-0e380e0a62d368837 	ami-06e46a15650294dfa
     US West (Oregon) (us-west-2) 	ami-0355c210cb3f58aa2 	ami-084e8e620163aa50e
+    Asia Pacific (Hong Kong) (ap-east-1) 	ami-03885e0a7493b073f 	N/A*
     Asia Pacific (Mumbai) (ap-south-1) 	ami-01b6a163133c31994 	ami-09ad3a49fb13389a0
     Asia Pacific (Tokyo) (ap-northeast-1) 	ami-0a9b3f8b4b65b402b 	ami-0cd09d7293f31df8a
     Asia Pacific (Seoul) (ap-northeast-2) 	ami-069f6a654a8795f72 	ami-006549812c03748cb
@@ -110,6 +97,7 @@ const EKS_AMI: {[version: string]: {[type: string]: {[region: string]: string}}}
     US East (Ohio) (us-east-2) 	ami-0485258c2d1c3608f 	ami-0ccac9d9b57864000
     US East (N. Virginia) (us-east-1) 	ami-0f2e8e5663e16b436 	ami-0017d945a10387606
     US West (Oregon) (us-west-2) 	ami-03a55127c613349a7 	ami-08335952e837d087b
+    Asia Pacific (Hong Kong) (ap-east-1) 	ami-032850771ac6f8ae2 	N/A*
     Asia Pacific (Mumbai) (ap-south-1) 	ami-0a9b1c1807b1a40ab 	ami-005b754faac73f0cc
     Asia Pacific (Tokyo) (ap-northeast-1) 	ami-0fde798d17145fae1 	ami-04cf69bbd6c0fae0b
     Asia Pacific (Seoul) (ap-northeast-2) 	ami-07fd7609df6c8e39b 	ami-0730e699ed0118737
