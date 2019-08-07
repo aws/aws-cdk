b247fd9f203546fc07311434297302d2c1f6a681
Elliot Murphy <statik@users.noreply.github.com>
2019-08-06 20:53:42 -0400

    Also redact AWS_SECURITY_TOKEN (#3560)
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- packages/aws-cdk/lib/commands/doctor.ts
+++ packages/aws-cdk/lib/commands/doctor.ts
@@ -72,6 +72,6 @@ function displayCdkEnvironmentVariables() {
 
 function anonymizeAwsVariable(name: string, value: string) {
   if (name === 'AWS_ACCESS_KEY_ID') { return value.substr(0, 4) + '<redacted>'; }  // Show ASIA/AKIA key type, but hide identifier
-  if (name === 'AWS_SECRET_ACCESS_KEY' || name === 'AWS_SESSION_TOKEN') { return '<redacted>'; }
+  if (name === 'AWS_SECRET_ACCESS_KEY' || name === 'AWS_SESSION_TOKEN' || name === 'AWS_SECURITY_TOKEN') { return '<redacted>'; }
   return value;
 }
