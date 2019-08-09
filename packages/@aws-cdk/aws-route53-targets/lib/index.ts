206097de478a3054f044e15decf614b8399b51a9
Jimmy Gaussen <jimmygaussen@gmail.com>
2019-08-09 08:44:43 +0200

    chore(route53): fix export ordering

--- packages/@aws-cdk/aws-route53-targets/lib/index.ts
+++ packages/@aws-cdk/aws-route53-targets/lib/index.ts
@@ -1,4 +1,4 @@
 export * from './api-gateway-domain-name';
+export * from './classic-load-balancer-target';
 export * from './cloudfront-target';
 export * from './load-balancer-target';
-export * from './classic-load-balancer-target';
