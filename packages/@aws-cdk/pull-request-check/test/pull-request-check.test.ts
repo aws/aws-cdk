de1d2e08349cbd73b5907db78e104888713fefe4
hupe1980 <hello@webappsolutions.de>
2019-08-10 14:21:42 +0200

    chore(pull-request-check): Update unittests

---
+++ packages/@aws-cdk/pull-request-check/test/pull-request-check.test.ts
@@ -0,0 +1,48 @@
+import { BuildSpec, ComputeType, LinuxBuildImage } from '@aws-cdk/aws-codebuild';
+import { Repository } from '@aws-cdk/aws-codecommit';
+import { Stack } from '@aws-cdk/core';
+import { toMatchCdkSnapshot } from '@cloudcomponents/jest-cdk-snapshot';
+
+import { PullRequestCheck } from '../lib';
+
+expect.extend({ toMatchCdkSnapshot });
+
+test('default setup', () => {
+  // GIVEN
+  const stack = new Stack();
+
+  const repository = new Repository(stack, 'Repository', {
+    repositoryName: 'MyRepositoryName',
+    description: 'Some description.'
+  });
+
+  // WHEN
+  new PullRequestCheck(stack, 'PullRequestCheck', {
+    repository,
+    buildSpec: BuildSpec.fromSourceFilename('buildspecs/prcheck.yml')
+  });
+
+  // THEN
+  expect(stack).toMatchCdkSnapshot();
+});
+
+test('custom setup', () => {
+  // GIVEN
+  const stack = new Stack();
+
+  const repository = new Repository(stack, 'Repository', {
+    repositoryName: 'MyRepositoryName',
+    description: 'Some description.'
+  });
+
+  // WHEN
+  new PullRequestCheck(stack, 'PullRequestCheck', {
+    repository,
+    buildSpec: BuildSpec.fromSourceFilename('buildspecs/prcheck.yml'),
+    computeType: ComputeType.LARGE,
+    buildImage: LinuxBuildImage.UBUNTU_14_04_PYTHON_3_7_1
+  });
+
+  // THEN
+  expect(stack).toMatchCdkSnapshot();
+});
