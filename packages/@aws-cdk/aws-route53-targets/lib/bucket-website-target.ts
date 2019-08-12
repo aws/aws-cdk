55d4b41d07d38908cfd1f0aecb58a3fbfeecc739
Jimmy Gaussen <jimmy.gaussen@logicgram.fr>
2019-08-12 11:42:41 +0200

    feat(route53-targets): s3 bucket website target support * adds region-info S3_STATIC_WEBSITE_ZONE_53_HOSTED_ZONE_ID

---
+++ packages/@aws-cdk/aws-route53-targets/lib/bucket-website-target.ts
@@ -0,0 +1,33 @@
+import route53 = require('@aws-cdk/aws-route53');
+import s3 = require('@aws-cdk/aws-s3');
+import regionInfo = require('@aws-cdk/region-info');
+import {Stack, Token} from '@aws-cdk/core';
+
+
+/**
+ * Use a S3 as an alias record target
+ */
+export class BucketWebsiteTarget implements route53.IAliasRecordTarget {
+    constructor(private readonly bucket: s3.Bucket) {
+    }
+
+    public bind(_record: route53.IRecordSet): route53.AliasRecordTargetConfig {
+        const {region} = Stack.of(this.bucket.stack);
+
+        // FIXME
+        if (Token.isUnresolved(region)) {
+            throw new Error(`Unresolved region token: ${region}`);
+        }
+
+        const hostedZoneId = regionInfo.Fact.find(region, regionInfo.FactName.S3_STATIC_WEBSITE_ZONE_53_HOSTED_ZONE_ID);
+
+        if (!hostedZoneId) {
+            throw new Error(`Bucket website target is not supported for the "${region}" region`);
+        }
+
+        return {
+            hostedZoneId,
+            dnsName: this.bucket.bucketWebsiteUrl,
+        };
+    }
+}
