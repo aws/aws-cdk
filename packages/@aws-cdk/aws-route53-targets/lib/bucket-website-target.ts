6b8a3d1b7d66d675e8533d6e2197186388f568c5
Jimmy Gaussen <jimmy.gaussen@logicgram.fr>
2019-08-12 15:22:49 +0200

    chore: fix indent

--- packages/@aws-cdk/aws-route53-targets/lib/bucket-website-target.ts
+++ packages/@aws-cdk/aws-route53-targets/lib/bucket-website-target.ts
@@ -7,26 +7,26 @@ import regionInfo = require('@aws-cdk/region-info');
  * Use a S3 as an alias record target
  */
 export class BucketWebsiteTarget implements route53.IAliasRecordTarget {
-    constructor(private readonly bucket: s3.Bucket) {
+  constructor(private readonly bucket: s3.Bucket) {
+  }
+
+  public bind(_record: route53.IRecordSet): route53.AliasRecordTargetConfig {
+    const {region} = Stack.of(this.bucket.stack);
+
+    // FIXME
+    if (Token.isUnresolved(region)) {
+      throw new Error(`Unresolved region token: ${region}`);
     }
 
-    public bind(_record: route53.IRecordSet): route53.AliasRecordTargetConfig {
-        const {region} = Stack.of(this.bucket.stack);
+    const hostedZoneId = regionInfo.Fact.find(region, regionInfo.FactName.S3_STATIC_WEBSITE_ZONE_53_HOSTED_ZONE_ID);
 
-        // FIXME
-        if (Token.isUnresolved(region)) {
-            throw new Error(`Unresolved region token: ${region}`);
-        }
-
-        const hostedZoneId = regionInfo.Fact.find(region, regionInfo.FactName.S3_STATIC_WEBSITE_ZONE_53_HOSTED_ZONE_ID);
-
-        if (!hostedZoneId) {
-            throw new Error(`Bucket website target is not supported for the "${region}" region`);
-        }
-
-        return {
-            hostedZoneId,
-            dnsName: this.bucket.bucketWebsiteUrl,
-        };
+    if (!hostedZoneId) {
+      throw new Error(`Bucket website target is not supported for the "${region}" region`);
     }
+
+    return {
+      hostedZoneId,
+      dnsName: this.bucket.bucketWebsiteUrl,
+    };
+  }
 }
