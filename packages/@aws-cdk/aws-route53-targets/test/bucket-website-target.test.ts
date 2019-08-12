8d321382b6094650f4ee2ef2518800add03a3f69
Jimmy Gaussen <jimmy.gaussen@logicgram.fr>
2019-08-12 15:29:14 +0200

    chore: fix tests

--- packages/@aws-cdk/aws-route53-targets/test/bucket-website-target.test.ts
+++ packages/@aws-cdk/aws-route53-targets/test/bucket-website-target.test.ts
@@ -6,7 +6,8 @@ import targets = require('../lib');
 
 test('use S3 bucket website as record target', () => {
   // GIVEN
-  const stack = new Stack();
+  const app = new App();
+  const stack = new Stack(app, 'test', {env: {region: 'us-east-1'}});
 
   const bucketWebsite = new s3.Bucket(stack, 'Bucket');
 
@@ -20,7 +21,30 @@ test('use S3 bucket website as record target', () => {
 
   // THEN
   expect(stack).toHaveResource('AWS::Route53::RecordSet', {
+    AliasTarget: {
+      DNSName: { "Fn::GetAtt": [ "Bucket83908E77", "WebsiteURL"] },
+      HostedZoneId: "Z3AQBSTGFYJSTF"
+    },
   });
+});
+
+test('throws if region agnostic', () => {
+  // GIVEN
+  const stack = new Stack();
+
+  const bucketWebsite = new s3.Bucket(stack, 'Bucket');
+
+  // WHEN
+  const zone = new route53.PublicHostedZone(stack, 'HostedZone', { zoneName: 'test.public' });
+
+  // THEN
+  expect(() => {
+    new route53.ARecord(zone, 'Alias', {
+      zone,
+      recordName: '_foo',
+      target: route53.RecordTarget.fromAlias(new targets.BucketWebsiteTarget(bucketWebsite))
+    });
+  }).toThrow(/Cannot use an S3 record alias in region-agnostic stacks/);
 });
 
 test('throws if bucket website hosting is unavailable (cn-northwest-1)', () => {
