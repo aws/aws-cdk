6711225e5c89d43808897e79bc6e0655bfc9644f
hupe1980 <hello@webappsolutions.de>
2019-08-15 11:15:42 +0200

    Revert "feat(events-targets): Add unit test"
    This reverts commit e7dbd2ec179ce6c7ae5be0da5b855343ceb614e5.

--- packages/@aws-cdk/aws-events-targets/test/codebuild/codebuild.test.ts
+++ packages/@aws-cdk/aws-events-targets/test/codebuild/codebuild.test.ts
@@ -16,93 +16,54 @@ test('use codebuild project as an eventrule target', () => {
   rule.addTarget(new targets.CodeBuildProject(project));
 
   // THEN
-  expect(stack).to(
-    haveResource('AWS::Events::Rule', {
-      Targets: [
+  expect(stack).to(haveResource('AWS::Events::Rule', {
+    Targets: [
+      {
+        Arn: {
+          "Fn::GetAtt": [
+            "MyProject39F7B0AE",
+            "Arn"
+          ]
+        },
+        Id: "Target0",
+        RoleArn: {
+          "Fn::GetAtt": [
+            "MyProjectEventsRole5B7D93F5",
+            "Arn"
+          ]
+        }
+      }
+    ]
+  }));
+
+  expect(stack).to(haveResource('AWS::IAM::Role', {
+    AssumeRolePolicyDocument: {
+      Statement: [
         {
-          Arn: {
-            'Fn::GetAtt': ['MyProject39F7B0AE', 'Arn']
-          },
-          Id: 'Target0',
-          RoleArn: {
-            'Fn::GetAtt': ['MyProjectEventsRole5B7D93F5', 'Arn']
+          Action: "sts:AssumeRole",
+          Effect: "Allow",
+          Principal: { Service: { "Fn::Join": [ "", [ "events.", { Ref: "AWS::URLSuffix" } ] ] } }
+        }
+      ],
+      Version: "2012-10-17"
+    }
+  }));
+
+  expect(stack).to(haveResource('AWS::IAM::Policy', {
+    PolicyDocument: {
+      Statement: [
+        {
+          Action: "codebuild:StartBuild",
+          Effect: "Allow",
+          Resource: {
+            "Fn::GetAtt": [
+              "MyProject39F7B0AE",
+              "Arn"
+            ]
           }
         }
-      ]
-    })
-  );
-
-  expect(stack).to(
-    haveResource('AWS::IAM::Role', {
-      AssumeRolePolicyDocument: {
-        Statement: [
-          {
-            Action: 'sts:AssumeRole',
-            Effect: 'Allow',
-            Principal: {
-              Service: {
-                'Fn::Join': ['', ['events.', { Ref: 'AWS::URLSuffix' }]]
-              }
-            }
-          }
-        ],
-        Version: '2012-10-17'
-      }
-    })
-  );
-
-  expect(stack).to(
-    haveResource('AWS::IAM::Policy', {
-      PolicyDocument: {
-        Statement: [
-          {
-            Action: 'codebuild:StartBuild',
-            Effect: 'Allow',
-            Resource: {
-              'Fn::GetAtt': ['MyProject39F7B0AE', 'Arn']
-            }
-          }
-        ],
-        Version: '2012-10-17'
-      }
-    })
-  );
-});
-
-test('specifying event for codebuild project target', () => {
-  // GIVEN
-  const stack = new Stack();
-  const project = new codebuild.PipelineProject(stack, 'MyProject');
-  const rule = new events.Rule(stack, 'Rule', {
-    schedule: events.Schedule.expression('rate(1 hour)')
-  });
-
-  // WHEN
-  const eventInput = {
-    buildspecOverride: 'buildspecs/hourly.yml'
-  };
-
-  rule.addTarget(
-    new targets.CodeBuildProject(project, {
-      event: events.RuleTargetInput.fromObject(eventInput)
-    })
-  );
-
-  // THEN
-  expect(stack).to(
-    haveResource('AWS::Events::Rule', {
-      Targets: [
-        {
-          Arn: {
-            'Fn::GetAtt': ['MyProject39F7B0AE', 'Arn']
-          },
-          Id: 'Target0',
-          Input: JSON.stringify(eventInput),
-          RoleArn: {
-            'Fn::GetAtt': ['MyProjectEventsRole5B7D93F5', 'Arn']
-          }
-        }
-      ]
-    })
-  );
+      ],
+      Version: "2012-10-17"
+    }
+  }));
 });
