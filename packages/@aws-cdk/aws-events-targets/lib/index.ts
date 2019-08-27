5c187501b2e7cd58b2ef1154b3b954556fb9ab87
Jonathan Goldwasser <jogold@users.noreply.github.com>
2019-08-27 09:31:58 +0200

    feat(events): add target to make AWS API calls (#3720)
    Add a AwsApi rule target to make AWS API calls.
    
    Comparable to the AwsCustomResource in terms of API and IAM permissions.
    
    Closes #2538

--- packages/@aws-cdk/aws-events-targets/lib/index.ts
+++ packages/@aws-cdk/aws-events-targets/lib/index.ts
@@ -2,6 +2,7 @@ export * from './codepipeline';
 export * from './sns';
 export * from './sqs';
 export * from './codebuild';
+export * from './aws-api';
 export * from './lambda';
 export * from './ecs-task-properties';
 export * from './ecs-task';
