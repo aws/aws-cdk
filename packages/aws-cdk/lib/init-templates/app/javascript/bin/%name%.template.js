6d5b342914a2704de9465aeee59fc2ffd763271d
Romain Marcadier-Muller <rmuller@amazon.com>
2019-08-28 23:17:45 +0200

    fix(init-templates): remove dependency on @types/node (#3840)
    * fix(init-templates): remove dependency on @types/node
    
    That was not required anymore since we no longer require passing the
    command line arguments to the `app.run()` call. This dependency has the
    potential of causing collisions with the ES libraries included by the
    TypeScript compiler, and specifying a correct version can be a delicate
    exercise, especially as those templates have to keep working in the
    future.
    
    Additionally, pinned the TypeScript compiler version to the current
    minor, as recommended by the TypeScript maintainers (TypeScript does not
    honor SemVer, but guarantees no breaking change between stable releases,
    which they indicate by issuing a new major version).
    
    Fixes #3839
    
    * also pin typescript compiler version to current minor

--- packages/aws-cdk/lib/init-templates/app/javascript/bin/%name%.template.js
+++ packages/aws-cdk/lib/init-templates/app/javascript/bin/%name%.template.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-// @ts-ignore: Cannot find declaration file
-require('source-map-support/register');
 const cdk = require('@aws-cdk/core');
 const { %name.PascalCased%Stack } = require('../lib/%name%-stack');
 
