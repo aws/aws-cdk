0c2ed36483cd6881e5a04ef3147b70d28e42fe72
Elad Ben-Israel <benisrae@amazon.com>
2019-08-28 11:51:30 +0300

    updates
    * accept multiple arguments
    * output to stdout
    * print usage if needed
    * improve code style and readability

--- scripts/stability-report.sh
+++ scripts/stability-report.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# generates a csv that includes a report of module stability and cfn-only status
-# usage: repo-root$ scripts/stability-report.sh
+# outputs a csv that includes a report of module stability and cfn-only status
+# usage: [repo_root]$ scripts/stability-report.sh
 set -euo pipefail
-outfile="stability.csv"
-echo "module,stability,cfn-only" > ${outfile}
-find . -name package.json | grep -v node_modules | xargs -n1 node scripts/stability.js >> ${outfile}
-
+echo "module,stability,cfn-only"
+find . -name package.json | grep -v node_modules | xargs scripts/stability.js
--- scripts/stability.js
+++ scripts/stability.js
@@ -1,9 +1,28 @@
+#!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
-const packageJsonPath = process.argv[2] || path.join(process.cwd(), 'package.json');
-const package = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-const libdir = path.join(path.dirname(packageJsonPath), 'lib');
-const libsrc = fs.existsSync(libdir) ? fs.readdirSync(libdir).filter(x => !x.endsWith('.generated.ts') && !x.endsWith('.js') && x !== 'index.ts') : [ 'dummy' ];
 
-console.log(`${package.name.replace(/^@aws-cdk\//g, '')},${package.stability || 'unknown'},${libsrc.length === 0 ? 'cfn-only' : ''}`);
+const dirs = process.argv.slice(2);
+if (dirs.length === 0) {
+  console.error(`usage: ${path.basename(process.argv[1])} module1/package.json module2/package.json module3/package.json ...`);
+  process.exit(1);
+}
 
+for (const packageJsonPath of dirs) {
+  const moduledir = path.dirname(packageJsonPath);
+  const metadata = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+
+  const components = [
+    metadata.name.replace(/^@aws-cdk\//g, ''), // module
+    metadata.stability || 'unknown',           // stability
+    cfnOnly(moduledir) ? 'cfn-only' : ''       // cfn-only
+  ]
+
+  console.log(components.join(','));
+}
+
+function cfnOnly(dir) {
+  const libdir = path.join(dir, 'lib');
+  const libsrc = fs.existsSync(libdir) ? fs.readdirSync(libdir).filter(x => !x.endsWith('.generated.ts') && !x.endsWith('.js') && x !== 'index.ts') : [ 'dummy' ];
+  return libsrc.length === 0;
+}
