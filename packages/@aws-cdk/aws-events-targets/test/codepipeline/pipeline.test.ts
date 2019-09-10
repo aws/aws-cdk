bafe760301b0caad6614b44cacf7fd6234c2bc9e
Adam Ruka <adamruka@amazon.com>
2019-09-09 22:40:18 -0700

    feat(events): allow passing a role to the CodePipeline target (#4006)
    Fixes #3999

--- packages/@aws-cdk/aws-events-targets/test/codepipeline/pipeline.test.ts
+++ packages/@aws-cdk/aws-events-targets/test/codepipeline/pipeline.test.ts
@@ -1,79 +1,120 @@
-import { expect, haveResource } from '@aws-cdk/assert';
+import { expect, haveResource, haveResourceLike } from '@aws-cdk/assert';
 import codepipeline = require('@aws-cdk/aws-codepipeline');
 import events = require('@aws-cdk/aws-events');
-import { Construct, Stack } from '@aws-cdk/core';
+import iam = require('@aws-cdk/aws-iam');
+import { CfnElement, Construct, Stack } from '@aws-cdk/core';
 import targets = require('../../lib');
 
-test('use codebuild project as an eventrule target', () => {
-  // GIVEN
-  const stack = new Stack();
-  const pipeline = new codepipeline.Pipeline(stack, 'Pipeline');
+describe('CodePipeline event target', () => {
+  let stack: Stack;
+  let pipeline: codepipeline.Pipeline;
+  let pipelineArn: any;
 
-  const srcArtifact = new codepipeline.Artifact('Src');
-  const buildArtifact = new codepipeline.Artifact('Bld');
-  pipeline.addStage({
-    stageName: 'Source',
-    actions: [new TestAction({
-      actionName: 'Hello',
-      category: codepipeline.ActionCategory.SOURCE,
-      provider: 'x',
-      artifactBounds: { minInputs: 0, maxInputs: 0 , minOutputs: 1, maxOutputs: 1, },
-      outputs: [srcArtifact]})]
+  beforeEach(() => {
+    stack = new Stack();
+    pipeline = new codepipeline.Pipeline(stack, 'Pipeline');
+    const srcArtifact = new codepipeline.Artifact('Src');
+    const buildArtifact = new codepipeline.Artifact('Bld');
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [new TestAction({
+        actionName: 'Hello',
+        category: codepipeline.ActionCategory.SOURCE,
+        provider: 'x',
+        artifactBounds: { minInputs: 0, maxInputs: 0 , minOutputs: 1, maxOutputs: 1, },
+        outputs: [srcArtifact]})]
+    });
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [new TestAction({
+        actionName: 'Hello',
+        category: codepipeline.ActionCategory.BUILD,
+        provider: 'y',
+        inputs: [srcArtifact],
+        outputs: [buildArtifact],
+        artifactBounds: { minInputs: 1, maxInputs: 1 , minOutputs: 1, maxOutputs: 1, }})]
+    });
+    pipelineArn = {
+      "Fn::Join": [ "", [
+        "arn:",
+        { Ref: "AWS::Partition" },
+        ":codepipeline:",
+        { Ref: "AWS::Region" },
+        ":",
+        { Ref: "AWS::AccountId" },
+        ":",
+        { Ref: "PipelineC660917D" }]
+      ]
+    };
   });
-  pipeline.addStage({
-    stageName: 'Build',
-    actions: [new TestAction({
-      actionName: 'Hello',
-      category: codepipeline.ActionCategory.BUILD,
-      provider: 'y',
-      inputs: [srcArtifact],
-      outputs: [buildArtifact],
-      artifactBounds: { minInputs: 1, maxInputs: 1 , minOutputs: 1, maxOutputs: 1, }})]
+
+  describe('when added to an event rule as a target', () => {
+    let rule: events.Rule;
+
+    beforeEach(() => {
+      rule = new events.Rule(stack, 'rule', {
+        schedule: events.Schedule.expression('rate(1 minute)'),
+      });
+    });
+
+    describe('with default settings', () => {
+      beforeEach(() => {
+        rule.addTarget(new targets.CodePipeline(pipeline));
+      });
+
+      test("adds the pipeline's ARN and role to the targets of the rule", () => {
+        expect(stack).to(haveResource('AWS::Events::Rule', {
+          Targets: [
+            {
+              Arn: pipelineArn,
+              Id: "Target0",
+              RoleArn: { "Fn::GetAtt": [ "PipelineEventsRole46BEEA7C", "Arn" ] },
+            },
+          ],
+        }));
+      });
+
+      test("creates a policy that has StartPipeline permissions on the pipeline's ARN", () => {
+        expect(stack).to(haveResource('AWS::IAM::Policy', {
+          PolicyDocument: {
+            Statement: [
+              {
+                Action: "codepipeline:StartPipelineExecution",
+                Effect: "Allow",
+                Resource: pipelineArn,
+              }
+            ],
+            Version: "2012-10-17"
+          }
+        }));
+      });
+    });
+
+    describe('with an explicit event role', () => {
+      beforeEach(() => {
+        const role = new iam.Role(stack, 'MyExampleRole', {
+          assumedBy: new iam.AnyPrincipal(),
+        });
+        const roleResource = role.node.defaultChild as CfnElement;
+        roleResource.overrideLogicalId('MyRole'); // to make it deterministic in the assertion below
+
+        rule.addTarget(new targets.CodePipeline(pipeline, {
+          eventRole: role,
+        }));
+      });
+
+      test("points at the given event role in the rule's targets", () => {
+        expect(stack).to(haveResourceLike('AWS::Events::Rule', {
+          Targets: [
+            {
+              Arn: pipelineArn,
+              RoleArn: { "Fn::GetAtt": ["MyRole", "Arn"] },
+            },
+          ],
+        }));
+      });
+    });
   });
-
-  const rule = new events.Rule(stack, 'rule', {
-    schedule: events.Schedule.expression('rate(1 minute)'),
-  });
-
-  // WHEN
-  rule.addTarget(new targets.CodePipeline(pipeline));
-
-  const pipelineArn = {
-    "Fn::Join": [ "", [
-      "arn:",
-      { Ref: "AWS::Partition" },
-      ":codepipeline:",
-      { Ref: "AWS::Region" },
-      ":",
-      { Ref: "AWS::AccountId" },
-      ":",
-      { Ref: "PipelineC660917D" }]
-    ]
-  };
-
-  // THEN
-  expect(stack).to(haveResource('AWS::Events::Rule', {
-    Targets: [
-      {
-        Arn: pipelineArn,
-        Id: "Target0",
-        RoleArn: { "Fn::GetAtt": [ "PipelineEventsRole46BEEA7C", "Arn" ] }
-      }
-    ]
-  }));
-
-  expect(stack).to(haveResource('AWS::IAM::Policy', {
-    PolicyDocument: {
-      Statement: [
-        {
-          Action: "codepipeline:StartPipelineExecution",
-          Effect: "Allow",
-          Resource: pipelineArn,
-        }
-      ],
-      Version: "2012-10-17"
-    }
-  }));
 });
 
 class TestAction implements codepipeline.IAction {
