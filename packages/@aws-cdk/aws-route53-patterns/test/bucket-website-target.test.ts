a13ca6d211cd0cde7fcd9ad3937906cd70fafb62
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2019-09-16 14:02:32 +0200

    feat(route53): Domain redirect pattern (#3946)
    This implements an HTTPS redirect using a CloudFront distribution and an S3 bucket. (fix #3893)

---
+++ packages/@aws-cdk/aws-route53-patterns/test/bucket-website-target.test.ts
@@ -0,0 +1,106 @@
+import '@aws-cdk/assert/jest';
+import { Certificate } from '@aws-cdk/aws-certificatemanager';
+import { HostedZone } from '@aws-cdk/aws-route53';
+import { App, Stack } from '@aws-cdk/core';
+import { HttpsRedirect } from '../lib';
+
+test('create HTTPS redirect', () => {
+  // GIVEN
+  const app = new App();
+  const stack = new Stack(app, 'test', { env: { region: 'us-east-1' } });
+
+  // WHEN
+  new HttpsRedirect(stack, 'Redirect', {
+    recordNames: ['foo.example.com', 'baz.example.com'],
+    targetDomain: 'bar.example.com',
+    zone: HostedZone.fromHostedZoneAttributes(stack, 'HostedZone', {
+      hostedZoneId: 'ID',
+      zoneName: 'example.com',
+    })
+  });
+
+  // THEN
+  expect(stack).toHaveResource('AWS::S3::Bucket', {
+    WebsiteConfiguration: {
+      RedirectAllRequestsTo: {
+        HostName: "bar.example.com",
+        Protocol: "https"
+      }
+    }
+  });
+  expect(stack).toHaveResourceLike('AWS::CloudFront::Distribution', {
+    DistributionConfig: {
+      Aliases: ['foo.example.com', 'baz.example.com']
+    }
+  });
+  expect(stack).toHaveResource('AWS::Route53::RecordSet', {
+    Name: 'foo.example.com.',
+    HostedZoneId: 'ID',
+  });
+  expect(stack).toHaveResource('AWS::Route53::RecordSet', {
+    Name: 'baz.example.com.',
+    HostedZoneId: 'ID',
+  });
+});
+
+test('create HTTPS redirect for apex', () => {
+  // GIVEN
+  const app = new App();
+  const stack = new Stack(app, 'test', { env: { region: 'us-east-1' } });
+
+  // WHEN
+  new HttpsRedirect(stack, 'Redirect', {
+    targetDomain: 'bar.example.com',
+    zone: HostedZone.fromHostedZoneAttributes(stack, 'HostedZone', {
+      hostedZoneId: 'ID',
+      zoneName: 'example.com',
+    })
+  });
+
+  // THEN
+  expect(stack).toHaveResource('AWS::S3::Bucket', {
+    WebsiteConfiguration: {
+      RedirectAllRequestsTo: {
+        HostName: "bar.example.com",
+        Protocol: "https"
+      }
+    }
+  });
+  expect(stack).toHaveResource('AWS::Route53::RecordSet', {
+    Name: 'example.com.',
+  });
+});
+
+test('create HTTPS redirect with existing cert', () => {
+  // GIVEN
+  const app = new App();
+  const stack = new Stack(app, 'test', { env: { region: 'us-east-1' } });
+
+  // WHEN
+  new HttpsRedirect(stack, 'Redirect', {
+    recordNames: ['foo.example.com'],
+    certificate: Certificate.fromCertificateArn(stack, 'Certificate', 'someArn'),
+    targetDomain: 'bar.example.com',
+    zone: HostedZone.fromHostedZoneAttributes(stack, 'HostedZone', {
+      hostedZoneId: 'ID',
+      zoneName: 'example.com',
+    })
+  });
+
+  // THEN
+  expect(stack).toHaveResource('AWS::S3::Bucket', {
+    WebsiteConfiguration: {
+      RedirectAllRequestsTo: {
+        HostName: "bar.example.com",
+        Protocol: "https"
+      }
+    }
+  });
+  expect(stack).toHaveResourceLike('AWS::CloudFront::Distribution', {
+    DistributionConfig: {
+      ViewerCertificate: {
+        AcmCertificateArn: "someArn"
+      }
+    }
+  });
+});
