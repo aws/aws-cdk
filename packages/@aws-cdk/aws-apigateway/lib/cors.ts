f415bf9d8dc3fc85352b7a22f892943cabdf5ae3
Elad Ben-Israel <benisrae@amazon.com>
2019-09-26 00:05:52 +0300

    document api

--- packages/@aws-cdk/aws-apigateway/lib/cors.ts
+++ packages/@aws-cdk/aws-apigateway/lib/cors.ts
@@ -1,22 +1,51 @@
 export interface CorsOptions {
   /**
+   * Specifies the response status code returned from the OPTIONS method.
+   *
    * @default 204
    */
   readonly statusCode?: number;
 
-  readonly allowOrigin: string;
+  /**
+   * The Access-Control-Allow-Origin response header indicates whether the
+   * response can be shared with requesting code from the given origin.
+   *
+   * Specifies the list of origins that are allowed to make requests to this resource.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+   */
+  readonly allowOrigins: string[];
 
   /**
+   * The Access-Control-Allow-Headers response header is used in response to a
+   * preflight request which includes the Access-Control-Request-Headers to
+   * indicate which HTTP headers can be used during the actual request.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers
    * @default
    */
   readonly allowHeaders?: string[];
 
   /**
+   * The Access-Control-Allow-Methods response header specifies the method or
+   * methods allowed when accessing the resource in response to a preflight request.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Methods
    * @default
    */
   readonly allowMethods?: string[];
 
   /**
+   * The Access-Control-Allow-Credentials response header tells browsers whether
+   * to expose the response to frontend JavaScript code when the request's
+   * credentials mode (Request.credentials) is "include".
+   *
+   * When a request's credentials mode (Request.credentials) is "include",
+   * browsers will only expose the response to frontend JavaScript code if the
+   * Access-Control-Allow-Credentials value is true.
+   *
+   * Credentials are cookies, authorization headers or TLS client certificates.
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Credentials
    * @default false
    */
   readonly allowCredentials?: boolean;
