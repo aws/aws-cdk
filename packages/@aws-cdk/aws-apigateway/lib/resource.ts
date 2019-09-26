30e328e9643a296e5b3c0e8c324835ccb41e19fe
Elad Ben-Israel <benisrae@amazon.com>
2019-09-26 17:00:45 +0300

    document addCorsPreflight

--- packages/@aws-cdk/aws-apigateway/lib/resource.ts
+++ packages/@aws-cdk/aws-apigateway/lib/resource.ts
@@ -89,7 +89,17 @@ export interface IResource extends IResourceBase {
   addMethod(httpMethod: string, target?: Integration, options?: MethodOptions): Method;
 
   /**
-   * TODO
+   * Adds an OPTIONS method to this resource which responds to Cross-Origin
+   * Resource Sharing (CORS) preflight requests.
+   *
+   * Cross-Origin Resource Sharing (CORS) is a mechanism that uses additional
+   * HTTP headers to tell browsers to give a web application running at one
+   * origin, access to selected resources from a different origin. A web
+   * application executes a cross-origin HTTP request when it requests a
+   * resource that has a different origin (domain, protocol, or port) from its
+   * own.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
    */
   addCorsPreflight(options: CorsOptions): Method;
 }
