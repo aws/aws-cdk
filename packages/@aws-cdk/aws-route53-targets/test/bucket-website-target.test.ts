07041765661ba2cc1af15e18e8c5403d31fc70d3
Jimmy Gaussen <jimmy.gaussen@logicgram.fr>
2019-09-26 11:15:04 +0200

    chore: match test bucketName with generated records

--- packages/@aws-cdk/aws-route53-targets/test/bucket-website-target.test.ts
+++ packages/@aws-cdk/aws-route53-targets/test/bucket-website-target.test.ts
@@ -4,18 +4,21 @@ import s3 = require('@aws-cdk/aws-s3');
 import { App, Stack } from '@aws-cdk/core';
 import targets = require('../lib');
 
+const [recordName, zoneName] = ['foo', 'test.public'];
+const bucketName = [recordName, zoneName].join('.');
+
 test('use S3 bucket website as record target', () => {
   // GIVEN
   const app = new App();
   const stack = new Stack(app, 'test', {env: {region: 'us-east-1'}});
 
-  const bucketWebsite = new s3.Bucket(stack, 'Bucket');
+  const bucketWebsite = new s3.Bucket(stack, 'Bucket', { bucketName });
 
   // WHEN
-  const zone = new route53.PublicHostedZone(stack, 'HostedZone', { zoneName: 'test.public' });
+  const zone = new route53.PublicHostedZone(stack, 'HostedZone', { zoneName });
   new route53.ARecord(zone, 'Alias', {
     zone,
-    recordName: '_foo',
+    recordName,
     target: route53.RecordTarget.fromAlias(new targets.BucketWebsiteTarget(bucketWebsite))
   });
 
@@ -33,13 +36,13 @@ test('use S3 bucket website as record target (fromBucketName)', () => {
   const app = new App();
   const stack = new Stack(app, 'test', {env: {region: 'us-east-1'}});
 
-  const bucketWebsite = s3.Bucket.fromBucketName(stack, 'Bucket', 'test');
+  const bucketWebsite = s3.Bucket.fromBucketName(stack, 'Bucket', bucketName);
 
   // WHEN
-  const zone = new route53.PublicHostedZone(stack, 'HostedZone', { zoneName: 'test.public' });
+  const zone = new route53.PublicHostedZone(stack, 'HostedZone', { zoneName });
   new route53.ARecord(zone, 'Alias', {
     zone,
-    recordName: '_foo',
+    recordName,
     target: route53.RecordTarget.fromAlias(new targets.BucketWebsiteTarget(bucketWebsite))
   });
 
@@ -56,16 +59,16 @@ test('throws if region agnostic', () => {
   // GIVEN
   const stack = new Stack();
 
-  const bucketWebsite = new s3.Bucket(stack, 'Bucket');
+  const bucketWebsite = new s3.Bucket(stack, 'Bucket', { bucketName });
 
   // WHEN
-  const zone = new route53.PublicHostedZone(stack, 'HostedZone', { zoneName: 'test.public' });
+  const zone = new route53.PublicHostedZone(stack, 'HostedZone', { zoneName });
 
   // THEN
   expect(() => {
     new route53.ARecord(zone, 'Alias', {
       zone,
-      recordName: '_foo',
+      recordName,
       target: route53.RecordTarget.fromAlias(new targets.BucketWebsiteTarget(bucketWebsite))
     });
   }).toThrow(/Cannot use an S3 record alias in region-agnostic stacks/);
@@ -79,13 +82,13 @@ test('throws if bucket website hosting is unavailable (cn-northwest-1)', () => {
   const bucketWebsite = new s3.Bucket(stack, 'Bucket');
 
   // WHEN
-  const zone = new route53.PublicHostedZone(stack, 'HostedZone', { zoneName: 'test.public' });
+  const zone = new route53.PublicHostedZone(stack, 'HostedZone', { zoneName });
 
   // THEN
   expect(() => {
     new route53.ARecord(zone, 'Alias', {
       zone,
-      recordName: '_foo',
+      recordName,
       target: route53.RecordTarget.fromAlias(new targets.BucketWebsiteTarget(bucketWebsite))
     });
   }).toThrow(/Bucket website target is not supported/);
