9717a9910fb8f3b185d52d47271b7bea5879e01a
Rico Huijbers <rix0rrr@gmail.com>
2019-09-26 10:13:09 +0200

    Update ec2-service.ts

--- packages/@aws-cdk/aws-ecs/lib/ec2/ec2-service.ts
+++ packages/@aws-cdk/aws-ecs/lib/ec2/ec2-service.ts
@@ -74,7 +74,7 @@ export interface Ec2ServiceProps extends BaseServiceOptions {
    * Specifies whether to propagate the tags from the task definition or the service to the tasks in the service.
    * Tags can only be propagated to the tasks within the service during service creation.
    *
-   * @deprecate
+   * @deprecated Use `propagateTags` instead.
    * @default PropagatedTagSource.NONE
    */
   readonly propagateTaskTagsFrom?: PropagatedTagSource;
