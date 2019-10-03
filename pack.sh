ec56ad9e70635e94206f7035c81378c8bd820201
Rico Huijbers <rix0rrr@gmail.com>
2019-10-03 18:47:56 +0200

    chore: use new parallel build feature of jsii
    Improve build times by using the new "build-all-at-once" feature of
    jsii-pacmak.
    
    Goes together with https://github.com/aws/jsii/pull/849.

--- pack.sh
+++ pack.sh
@@ -2,7 +2,7 @@
 # Runs "npm package" in all modules. This will produce a "dist/" directory in each module.
 # Then, calls pack-collect.sh to merge all outputs into a root ./pack directory, which is
 # later read by bundle-beta.sh.
-set -e
+set -eu
 export PATH=$PWD/node_modules/.bin:$PATH
 export NODE_OPTIONS="--max-old-space-size=4096 ${NODE_OPTIONS:-}"
 root=$PWD
@@ -11,15 +11,33 @@ distdir="$PWD/dist"
 rm -fr ${distdir}
 mkdir -p ${distdir}
 
-scopes=$(lerna ls 2>/dev/null | grep -v "(private)" | cut -d" " -f1 | xargs -n1 -I{} echo "--scope {}" | tr "\n" " ")
+# Split out jsii and non-jsii packages. Jsii packages will be built all at once.
+# Non-jsii packages will be run individually.
+echo "Collecting package list..." >&2
+scripts/list-packages $TMPDIR/jsii.txt $TMPDIR/nonjsii.txt
 
-# Run the "cdk-package" script in all modules. For jsii modules, this invokes jsii-pacmak which generates and builds multi-language
-# outputs. For non-jsii module, it will just run "npm pack" and place the output in dist/npm
-# (which is similar to how pacmak outputs it).
-lerna run ${scopes} --sort --concurrency=1 --stream package
+# Return lerna scopes from a package list
+function lerna_scopes() {
+  while [[ "${1:-}" != "" ]]; do
+    echo "--scope $1 "
+    shift
+  done
+}
 
-# Collect dist/ from all modules into the root dist/
-for dir in $(find packages -name dist | grep -v node_modules); do
+echo "Packaging jsii modules" >&2
+
+# Jsii packaging (all at once using jsii-pacmak)
+jsii-pacmak \
+  --verbose \
+  --outdir $distdir/ \
+  $(cat $TMPDIR/jsii.txt)
+
+# Non-jsii packaging, which means running 'package' in every individual
+# module and rsync'ing the result to the shared dist directory.
+echo "Packaging non-jsii modules" >&2
+lerna run $(lerna_scopes $(cat $TMPDIR/nonjsii.txt)) --sort --concurrency=1 --stream package
+
+for dir in $(find packages -name dist | grep -v node_modules | grep -v run-wrappers); do
   echo "Merging ${dir} into ${distdir}"
   rsync -av $dir/ ${distdir}/
 done
