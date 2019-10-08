e2b9b474e70bba7e13dc544720aa66da95c1d5b8
Jimmy Gaussen <jimmy.gaussen@gmail.com>
2019-10-08 20:32:43 +0200

    fix: export health-check files

--- packages/@aws-cdk/aws-route53/lib/index.ts
+++ packages/@aws-cdk/aws-route53/lib/index.ts
@@ -3,6 +3,10 @@ export * from './hosted-zone-provider';
 export * from './hosted-zone-ref';
 export * from './record-set';
 export * from './alias-record-target';
+export * from './heath-check/health-check';
+export * from './heath-check/alarm-health-check';
+export * from './heath-check/calculated-health-check';
+export * from './heath-check/endpoint-health-check';
 
 // AWS::Route53 CloudFormation Resources:
 export * from './route53.generated';
