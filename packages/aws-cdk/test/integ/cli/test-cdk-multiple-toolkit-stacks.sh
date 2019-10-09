8aebcea1b34a38a8cb071103c2d7d1bcbfc23961
Selva <selvak26@gmail.com>
2019-10-09 11:20:28 -0600

    feat(cli): add tags to CDKToolkit stack through bootstrap cli command (#4320)
    * feat(cli): add tags to bootstrap cli(#4227)
    
    * feat(cli): add unit test and integration test and default value for tags in bootstrap cli(#4227)
    
    * feat(cli): reverted test case(#4227)
    
    * feat(cli): fix review comments on toolkit stack tags,removed unwanted test scenarios(#4227)
    
    * feat(cli): fix test cases for bootstrap cli tags(#4227)
    
    * feat(cli): add integration test for tags based on peer review(#4227)
    
    * feat(cli): change toolkit tag integration test message and condition(#4227)

--- packages/aws-cdk/test/integ/cli/test-cdk-multiple-toolkit-stacks.sh
+++ packages/aws-cdk/test/integ/cli/test-cdk-multiple-toolkit-stacks.sh
@@ -10,12 +10,21 @@ toolkit_stack_name_1="toolkit-stack-1-${RANDOM}"
 toolkit_stack_name_2="toolkit-stack-2-${RANDOM}"
 
 # deploy two toolkit stacks into the same environment (see #1416)
-cdk bootstrap --toolkit-stack-name ${toolkit_stack_name_1}
-cdk bootstrap --toolkit-stack-name ${toolkit_stack_name_2}
+cdk bootstrap --toolkit-stack-name ${toolkit_stack_name_1} --tags Foo=Bar
+cdk bootstrap --toolkit-stack-name ${toolkit_stack_name_2} --tags Foo=Bar
 
 # just check that the new stack exists
 aws cloudformation describe-stack-resources --stack-name ${toolkit_stack_name_1}
 aws cloudformation describe-stack-resources --stack-name ${toolkit_stack_name_2}
+
+# get tags from the new stack
+tag_stack_1=$(aws cloudformation describe-stacks --stack-name ${toolkit_stack_name_1} --query "Stacks[0].Tags[?Key=='Foo'].Value" --output text)
+tag_stack_2=$(aws cloudformation describe-stacks --stack-name ${toolkit_stack_name_2} --query "Stacks[0].Tags[?Key=='Foo'].Value" --output text)
+
+# check if tag is not equal to bar
+if [ "${tag_stack_1}" != "Bar" ] || [ "${tag_stack_2}" != "Bar" ]; then
+    fail "toolkit tags test expect Bar but got ${tag_stack_1} and ${tag_stack_2}"
+fi
 
 # clean up
 aws cloudformation delete-stack --stack-name ${toolkit_stack_name_1}
