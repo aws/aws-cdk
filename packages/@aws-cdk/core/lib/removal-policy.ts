831b56ccc029e577fd514cfa2075b5d2cae8a153
Noah Litov <noahlitov@gmail.com>
2019-10-18 14:40:02 -0700

    update for suggested changes

--- packages/@aws-cdk/core/lib/removal-policy.ts
+++ packages/@aws-cdk/core/lib/removal-policy.ts
@@ -16,7 +16,8 @@ export interface RemovalPolicyOptions {
   /**
    * The default policy to apply in case the removal policy is not defined.
    *
-   * @default RemovalPolicy.Destroy
+   * The default value is resource specific. To determine the default value for a resoure,
+   * please consult that specific resource's documentation.
    */
   readonly default?: RemovalPolicy;
 
