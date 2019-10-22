c2479f2828c65f3fd94856ec0a1be947c2426951
Rico Huijbers <rix0rrr@gmail.com>
2019-10-22 12:02:50 +0200

    fix(cli): add Cloud Assembly backwards compat tests
    Make sure that the CLI will continue to be able to load old cloud
    assemblies. We do this by using the CLI feature that it can directly
    load Cloud Assemblies through its `--app` argument, hard copying
    a couple of cloud assemblies into the test set and trying to synth
    those.
    
    In addition to adding a test for loading v0.36.0 assemblies, also
    add tests for 2 common context provider requests (AZs and VPC),
    to make sure new CLIs will continue to be able to handle these
    context lookup requests.
    
    Fixes #4475, pre-emptively adds a regression test for #4544.

---
+++ packages/aws-cdk/test/integ/cli/test-backwards-compat.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -euo pipefail
+scriptdir=$(cd $(dirname $0) && pwd)
+source ${scriptdir}/common.bash
+# ----------------------------------------------------------
+
+tmpdir=$(dirname $(mktemp -u))
+casmdir=$tmpdir/cdk-integ-cx
+stderrfile=$casmdir/cdk.err
+
+# Copy the files from a cloud assembly source dir into the
+# tempdir. Evaluate .js files found their (interpret them
+# as templates).
+function prepare_cloud_assembly() {
+    echo "ASSEMBLY ${1}"
+    rm -rf $casmdir
+    mkdir -p $casmdir
+    cp -R $1/* $casmdir
+
+    # Execute templates to produce file with the same name
+    # but without .js extension
+    shopt -s nullglob
+    for template in $casmdir/*.js; do
+        node $template > ${template%.js}
+    done
+}
+
+# Assert that there was no providerError in CDK's stderr
+# Because we rely on the app/framework to actually error in case the
+# provider fails, we inspect the logs here.
+function assert_no_error() {
+    if grep '$providerError' $stderrfile; then
+        cat $stderrfile >&2
+        echo "There was an error executing the context provider!" >&2
+        exit 1
+    fi
+}
+
+# Echo the TEST_ACCOUNT, TEST_REGION vars to make bash abort if they're not set.
+echo "Running backwards compatibility test (account ${TEST_ACCOUNT}, region ${TEST_REGION})"
+
+for assembly in ${scriptdir}/cloud-assemblies/*; do
+    prepare_cloud_assembly $assembly
+
+    rm -f cdk.context.json
+    cdk -a $casmdir -v synth > /dev/null 2> $stderrfile
+
+    assert_no_error
+done
+
+echo "✅  success"
