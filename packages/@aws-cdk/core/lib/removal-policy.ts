decc217a8c8cf0c1aad9b6e0a40c5c6cbbc65541
Noah Litov <noahlitov@gmail.com>
2019-10-21 20:03:49 -0700

    Readded @default tag

--- packages/@aws-cdk/core/lib/removal-policy.ts
+++ packages/@aws-cdk/core/lib/removal-policy.ts
@@ -16,7 +16,7 @@ export interface RemovalPolicyOptions {
   /**
    * The default policy to apply in case the removal policy is not defined.
    *
-   * The default value is resource specific. To determine the default value for a resoure,
+   * @default - Default value is resource specific. To determine the default value for a resoure,
    * please consult that specific resource's documentation.
    */
   readonly default?: RemovalPolicy;
