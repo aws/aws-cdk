62dc470249f00cfcb61e0405e7c3613adb5025e3
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2019-10-28 15:12:22 +0100

    feat(cli): add --no-fail option to diff
    Use `cdk diff --no-fail` to exit with 0 even when there is a diff.
    
    To avoid a CLI breaking change, the default remains to fail on diff.
    
    Related to #4650 and #4708

--- packages/aws-cdk/test/integ/cli/test-cdk-diff.sh
+++ packages/aws-cdk/test/integ/cli/test-cdk-diff.sh
@@ -6,11 +6,14 @@ source ${scriptdir}/common.bash
 
 setup
 
-function cdk_diff() {
-    cdk diff $1 2>&1 || true
-}
+cdk diff --no-fail ${STACK_NAME_PREFIX}-test-1 2>&1 | grep "AWS::SNS::Topic"
+cdk diff --no-fail ${STACK_NAME_PREFIX}-test-2 2>&1 | grep "AWS::SNS::Topic"
 
-cdk_diff ${STACK_NAME_PREFIX}-test-1 | grep "AWS::SNS::Topic"
-cdk_diff ${STACK_NAME_PREFIX}-test-2 | grep "AWS::SNS::Topic"
+failed=0
+cdk diff ${STACK_NAME_PREFIX}-test-1 2>&1 || failed=1
+
+if [ $failed -ne 1 ]; then
+  fail 'cdk diff with a diff does not fail'
+fi
 
 echo "✅  success"
