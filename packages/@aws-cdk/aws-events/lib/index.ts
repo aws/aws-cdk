67b552abb46b33ea005ac6c6878ee8386be914ff
c.p <github@deh.li>
2019-10-29 10:18:36 -0400

    feat(aws-events): Adds EventBus resources (#4609)
    Adds the L2 resource for `CfnEventBus`

--- packages/@aws-cdk/aws-events/lib/index.ts
+++ packages/@aws-cdk/aws-events/lib/index.ts
@@ -2,6 +2,7 @@ export * from './input';
 export * from './rule';
 export * from './rule-ref';
 export * from './target';
+export * from './event-bus';
 export * from './event-pattern';
 export * from './schedule';
 export * from './on-event-options';
