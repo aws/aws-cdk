5a20a5ffa23cca554134e2e8d7b31581e60d1844
hacker65536 <s.hacker65536@gmail.com>
2019-10-30 07:13:56 +0000

    chore: checkfiles with yarn install

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ export NODE_OPTIONS="--max-old-space-size=4096 ${NODE_OPTIONS:-}"
 
 echo "============================================================================================="
 echo "installing..."
-yarn install
+yarn install --check-files
 
 fail() {
   echo "❌  Last command failed. Scroll up to see errors in log (search for '!!!!!!!!')."
