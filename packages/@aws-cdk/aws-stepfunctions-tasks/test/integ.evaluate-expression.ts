cece309fea958c83d137bf45449caedb0b895bac
Jonathan Goldwasser <jogold@users.noreply.github.com>
2019-11-04 15:42:45 +0100

    feat(stepfunctions): add `EvaluateExpression` task (#4602)
    Use the EvaluateExpression to perform simple operations referencing state paths. The
    expression referenced in the task will be evaluated in a Lambda function
    (eval()). This allows you to not have to write Lambda code for simple operations.

---
+++ packages/@aws-cdk/aws-stepfunctions-tasks/test/integ.evaluate-expression.ts
@@ -0,0 +1,30 @@
+import sfn = require('@aws-cdk/aws-stepfunctions');
+import cdk = require('@aws-cdk/core');
+import tasks = require('../lib');
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'aws-stepfunctions-integ');
+
+const sum = new sfn.Task(stack, 'Sum', {
+  task: new tasks.EvaluateExpression({
+    expression: '$.a + $.b',
+  }),
+  resultPath: '$.c'
+});
+
+const multiply = new sfn.Task(stack, 'Multiply', {
+  task: new tasks.EvaluateExpression({
+    expression: '$.c * 2'
+  }),
+  resultPath: '$.waitSeconds'
+});
+
+new sfn.StateMachine(stack, 'StateMachine', {
+  definition: sum
+    .next(multiply)
+    .next(new sfn.Wait(stack, 'Wait', {
+      time: sfn.WaitTime.secondsPath('$.waitSeconds')
+    })),
+});
+
+app.synth();
