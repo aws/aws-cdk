0a7b0a9bf5178de873baff2f52ee55d83f87300d
Shiv Lakshminarayan <shivlaks@amazon.com>
2019-11-07 03:33:47 -0800

    change type of attribute bag to take in any K-V pair

--- packages/@aws-cdk/core/lib/tree.ts
+++ packages/@aws-cdk/core/lib/tree.ts
@@ -15,11 +15,10 @@ export interface ITreeInspector {
  * Inspector that maintains an attribute bag
  */
 export class TreeInspector implements ITreeInspector {
-
   /**
    * Represents the bag of attributes as key-value pairs.
    */
-  public readonly attributes: Record<string, any> = {};
+  public readonly attributes: { [key: string]: any } = {};
 
   /**
    * Adds attribute to bag. Keys should be added by convention to prevent conflicts
