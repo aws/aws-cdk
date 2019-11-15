4ec50d99d86f25d2faae1b75c650a514f4958fac
Jerry Kindall <52084730+Jerry-AWS@users.noreply.github.com>
2019-11-14 16:35:37 -0800

    Add message about what the script is doing

--- packages/aws-cdk/lib/init-templates/app/python/source.bat
+++ packages/aws-cdk/lib/init-templates/app/python/source.bat
@@ -9,4 +9,5 @@ rem On Windows, this command just runs this batch file (the argument is ignored)
 rem
 rem Now we don't need to document a Windows command for activating a virtualenv.
 
+echo Executing .env\Scripts\activate.bat for you
 .env\Scripts\activate.bat
