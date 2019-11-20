1f8b784666ec4ed820ee209d02c11d07f4b3341d
👨🏼‍💻 Romain Marcadier-Muller <rmuller@amazon.com>
2019-11-20 16:26:29 +0100

    complete integration tests up

--- packages/aws-cdk/test/integ/init/test-python.sh
+++ packages/aws-cdk/test/integ/init/test-python.sh
@@ -5,17 +5,22 @@
 set -eu
 scriptdir=$(cd $(dirname $0) && pwd)
 source ${scriptdir}/common.bash
+
 header Python
+
 #------------------------------------------------------------------
 
-for template in app sample-app; do
-    echo "Trying template $template"
+if [[ "${1:-}" == "" ]]; then
+    templates="app sample-app"
+else
+    templates="$@"
+fi
+
+for template in $templates; do
+    echo "Trying Python template $template"
+
     setup
 
-    cdk init -l python $template
-
-    source .env/bin/activate
-    type -p pip
-    pip install -r requirements.txt
+    cdk init -l python -t $template
     cdk synth
 done
