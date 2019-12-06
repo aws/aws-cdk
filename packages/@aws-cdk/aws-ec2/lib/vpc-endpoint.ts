0b69c3b4486ddd2dd38385178afa0bb1a1f09398
Chris McKnight <cmckni3@gmail.com>
2019-12-06 07:24:58 -0600

    fix(ec2): Fix CODEBUILD_FIPS interface endpoint (#5315)

--- packages/@aws-cdk/aws-ec2/lib/vpc-endpoint.ts
+++ packages/@aws-cdk/aws-ec2/lib/vpc-endpoint.ts
@@ -210,7 +210,7 @@ export class InterfaceVpcEndpointAwsService implements IInterfaceVpcEndpointServ
   public static readonly CLOUDFORMATION = new InterfaceVpcEndpointAwsService('cloudformation');
   public static readonly CLOUDTRAIL = new InterfaceVpcEndpointAwsService('cloudtrail');
   public static readonly CODEBUILD = new InterfaceVpcEndpointAwsService('codebuild');
-  public static readonly CODEBUILD_FIPS = new InterfaceVpcEndpointAwsService('codebuil-fips');
+  public static readonly CODEBUILD_FIPS = new InterfaceVpcEndpointAwsService('codebuild-fips');
   public static readonly CODECOMMIT = new InterfaceVpcEndpointAwsService('codecommit');
   public static readonly CODECOMMIT_FIPS = new InterfaceVpcEndpointAwsService('codecommit-fips');
   public static readonly CODEPIPELINE = new InterfaceVpcEndpointAwsService('codepipeline');
