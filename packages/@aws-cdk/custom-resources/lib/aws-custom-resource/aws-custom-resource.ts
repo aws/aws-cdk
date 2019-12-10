8ee12cd2ce8eca66444b15cb22b592ce3a8df581
Jonathan Goldwasser <jogold@users.noreply.github.com>
2019-12-10 20:45:00 +0100

    chore: nodejs12.x runtime for AWS API constructs (#5283)
    The `nodejs12.x` runtime comes with AWS SDK v2.536.0, allowing users to make calls to more
    recent services/actions.

--- packages/@aws-cdk/custom-resources/lib/aws-custom-resource/aws-custom-resource.ts
+++ packages/@aws-cdk/custom-resources/lib/aws-custom-resource/aws-custom-resource.ts
@@ -174,7 +174,7 @@ export class AwsCustomResource extends cdk.Construct implements iam.IGrantable {
 
     const provider = new lambda.SingletonFunction(this, 'Provider', {
       code: lambda.Code.fromAsset(path.join(__dirname, 'runtime')),
-      runtime: lambda.Runtime.NODEJS_10_X,
+      runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
       uuid: '679f53fa-c002-430c-b0da-5b7982bd2287',
       lambdaPurpose: 'AWS',
