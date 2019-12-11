075bbb32cbd25289b1c79db7325865470d2cfa21
Shiv Lakshminarayan <shivlaks@amazon.com>
2019-12-11 13:59:32 -0800

    fix(init-templates): use pytest for Python sample-app init template (#5325)
    * fix(init-templates): use pytest for Python sample-app init template
    
    Fixes #5313
    
    * "remove redundant call to get_template"

--- packages/aws-cdk/lib/init-templates/sample-app/python/tests/unit/test_%name%_stack.template.py
+++ packages/aws-cdk/lib/init-templates/sample-app/python/tests/unit/test_%name%_stack.template.py
@@ -1,11 +1,19 @@
-import unittest
+import json
+import pytest
 
 from aws_cdk import core
+from %name%.%name%_stack import %name.PascalCased%Stack
 
-from hello.hello_construct import HelloConstruct
 
-class Test%name.PascalCased%Stack(unittest.TestCase):
+def get_template():
+    app = core.App()
+    %name.PascalCased%Stack(app, "%name%")
+    return json.dumps(app.synth().get_stack("%name%").template)
 
-    def setUp(self):
-        self.app = core.App()
-        self.stack = core.Stack(self.app, "TestStack")
+
+def test_sqs_queue_created():
+    assert("AWS::SQS::Queue" in get_template())
+
+
+def test_sns_topic_created():
+    assert("AWS::SNS::Topic" in get_template())
