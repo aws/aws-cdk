840927506480ed2e8102897c30d73e3ce8517a08
Vincent Lesierse <vincent@lesierse.com>
2019-12-12 10:50:40 +0100

    feat(eks): Add HelmRelease construct

--- packages/@aws-cdk/aws-eks/lib/index.ts
+++ packages/@aws-cdk/aws-eks/lib/index.ts
@@ -1,6 +1,7 @@
 export * from './cluster';
 export * from './aws-auth-mapping';
 export * from './k8s-resource';
+export * from './helm-release';
 export * from './aws-auth';
 
 // AWS::EKS CloudFormation Resources:
