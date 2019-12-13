3e24e3bb8f4c96436db81b64580d3264388c9df6
Rico Huijbers <rix0rrr@gmail.com>
2019-12-13 19:46:56 +0100

    refactor(elbv2): introduce ListenerCertificate (#5405)
    Finally model certificates in a consistent way, using an
    `IListenerCertificate` interface (it has to be an interface
    to be able to maintain backwards compatibility with the
    erroneously introduced `INetworkListenerCertificateProps` interface)
    and an implemention of it called `ListenerCertificate`.
    
    `ListenerCertificate` can currently be created from an ACM certificate,
    and in the future should also be creatable from an IAM certificate.
    
    Make it the same for ALB and NLBs.
    
    Fixes #5330.

--- packages/@aws-cdk/aws-elasticloadbalancingv2/lib/alb/application-listener-certificate.ts
+++ packages/@aws-cdk/aws-elasticloadbalancingv2/lib/alb/application-listener-certificate.ts
@@ -1,5 +1,6 @@
 import cdk = require('@aws-cdk/core');
 import { CfnListenerCertificate } from '../elasticloadbalancingv2.generated';
+import { IListenerCertificate } from '../shared/listener-certificate';
 import { IApplicationListener } from './application-listener';
 
 /**
@@ -15,8 +16,20 @@ export interface ApplicationListenerCertificateProps {
    * ARNs of certificates to attach
    *
    * Duplicates are not allowed.
+   *
+   * @deprecated Use `certificates` instead.
+   * @default - One of 'certificates' and 'certificateArns' is required.
    */
-  readonly certificateArns: string[];
+  readonly certificateArns?: string[];
+
+  /**
+   * Certificates to attach
+   *
+   * Duplicates are not allowed.
+   *
+   * @default - One of 'certificates' and 'certificateArns' is required.
+   */
+  readonly certificates?: IListenerCertificate[];
 }
 
 /**
@@ -26,9 +39,18 @@ export class ApplicationListenerCertificate extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string, props: ApplicationListenerCertificateProps) {
     super(scope, id);
 
+    if (!props.certificateArns && !props.certificates) {
+      throw new Error(`At least one of 'certificateArns' or 'certificates' is required`);
+    }
+
+    const certificates = [
+      ...(props.certificates || []).map(c => ({ certificateArn: c.certificateArn })),
+      ...(props.certificateArns || []).map(certificateArn => ({ certificateArn })),
+    ];
+
     new CfnListenerCertificate(this, 'Resource', {
       listenerArn: props.listener.listenerArn,
-      certificates: props.certificateArns.map(certificateArn => ({ certificateArn })),
+      certificates,
     });
   }
 }
