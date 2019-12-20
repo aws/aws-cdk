6dc0cbed79fee62482921ccb53f1ee8d814e3ac5
Thomas Klinger <Thomas.Klinger@otto.de>
2019-12-20 22:18:50 +0100

    refactor(securityhub): replace old nodeunit tests

--- packages/@aws-cdk/aws-securityhub/test/test.hub.ts
+++ packages/@aws-cdk/aws-securityhub/test/test.hub.ts
@@ -2,16 +2,18 @@ import '@aws-cdk/assert/jest';
 import { App, Stack } from '@aws-cdk/core';
 import { Hub } from '../lib';
 
-test('create Hub', () => {
-    // GIVEN
-    const app = new App();
-    const stack = new Stack(app, 'test');
+describe('SecurityHub', () => {
+    it('creates a Hub', () => {
+        // GIVEN
+        const app = new App();
+        const stack = new Stack(app, 'test');
 
-    // WHEN
-    new Hub(stack, 'Hub', {});
+        // WHEN
+        new Hub(stack, 'Hub', {});
 
-    // THEN
-    expect(stack).toHaveResource('AWS::SecurityHub::Hub', {
+        // THEN
+        expect(stack).toHaveResource('AWS::SecurityHub::Hub', {
 
+        });
     });
 });
