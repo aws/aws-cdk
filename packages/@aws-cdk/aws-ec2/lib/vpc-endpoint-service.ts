7d0878dcd03668d49b66b5d26d015b086e1c98bc
James Fleming <flemjame@amazon.com>
2019-12-30 20:03:22 +0000

    Removing tests and adding IVpcEndpointService

--- packages/@aws-cdk/aws-ec2/lib/vpc-endpoint-service.ts
+++ packages/@aws-cdk/aws-ec2/lib/vpc-endpoint-service.ts
@@ -3,6 +3,16 @@ import { Construct, IResource, Resource } from '@aws-cdk/core';
 import { CfnVPCEndpointService, CfnVPCEndpointServicePermissions } from './ec2.generated';
 
 /**
+ * A load balancer that can host a VPC Endpoint Service
+ */
+export interface IVpcEndpointLoadBalancer {
+  /**
+   * The ARN of the load balancer that hosts the VPC Endpoint Service
+   */
+  readonly loadBalancerArn: string;
+}
+
+/**
  * A VPC endpoint service.
  */
 export interface IVpcEndpointService extends IResource {
@@ -23,7 +33,7 @@ export class VpcEndpointService extends Resource implements IVpcEndpointService 
    * One or more network load balancer ARNs to host the service.
    * @attribute
    */
-  public readonly networkLoadBalancerArns: string[];
+  public readonly vpcEndpointServiceLoadBalancers: IVpcEndpointLoadBalancer[];
 
   /**
    * Whether to require manual acceptance of new connections to the service.
@@ -42,16 +52,16 @@ export class VpcEndpointService extends Resource implements IVpcEndpointService 
   constructor(scope: Construct, id: string, props: VpcEndpointServiceProps) {
     super(scope, id);
 
-    if (props.networkLoadBalancerArns === undefined || props.networkLoadBalancerArns.length === 0) {
-      throw new Error("VPC Endpoint Service must have at least one `Network Load Balancer ARN` specified.");
+    if (props.vpcEndpointServiceLoadBalancers === undefined || props.vpcEndpointServiceLoadBalancers.length === 0) {
+      throw new Error("VPC Endpoint Service must have at least one load balancer specified.");
     }
 
-    this.networkLoadBalancerArns = props.networkLoadBalancerArns;
+    this.vpcEndpointServiceLoadBalancers = props.vpcEndpointServiceLoadBalancers;
     this.acceptanceRequired = props.acceptanceRequired !== undefined ? props.acceptanceRequired : true;
     this.whitelistedPrincipals = props.whitelistedPrincipals !== undefined ? props.whitelistedPrincipals : [];
 
     this.endpointService = new CfnVPCEndpointService(this, id, {
-      networkLoadBalancerArns: this.networkLoadBalancerArns,
+      networkLoadBalancerArns: this.vpcEndpointServiceLoadBalancers.map(lb => lb.loadBalancerArn),
       acceptanceRequired: this.acceptanceRequired
     });
 
@@ -76,9 +86,9 @@ export interface VpcEndpointServiceProps {
     readonly vpcEndpointServiceName?: string;
 
     /**
-     * One or more network load balancer ARNs to host the service.
+     * One or more load balancers to host the service.
      */
-    readonly networkLoadBalancerArns: string[];
+    readonly vpcEndpointServiceLoadBalancers: IVpcEndpointLoadBalancer[];
 
     /**
      * Whether to require manual acceptance of new connections to the service.
