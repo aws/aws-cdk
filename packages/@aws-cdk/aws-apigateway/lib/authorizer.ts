cfdda1fff0a39ace67dcb757c1fc9526501b3e9c
Elad Ben-Israel <benisrae@amazon.com>
2019-12-30 15:59:55 +0200

    moved Authorizer to authorizer.ts

--- packages/@aws-cdk/aws-apigateway/lib/authorizer.ts
+++ packages/@aws-cdk/aws-apigateway/lib/authorizer.ts
@@ -1,4 +1,19 @@
+import { Resource } from '@aws-cdk/core';
 import { AuthorizationType } from './method';
+import { RestApi } from './restapi';
+
+/**
+ * Base class for all custom authorizers
+ */
+export abstract class Authorizer extends Resource implements IAuthorizer {
+  public readonly abstract authorizerId: string;
+  public readonly authorizationType = AuthorizationType.CUSTOM;
+
+  /**
+   * Called when the authorizer is used from a specific REST API.
+   */
+  public abstract _attachToApi(restApi: RestApi): void;
+}
 
 /**
  * Represents an API Gateway authorizer.
