4f102bfd15008841a0de8aa75a2b94eaa666feae
Ahmed Sedek <ahmed.magdy.g.s@gmail.com>
2019-12-30 13:42:02 +0100

    Dummy change

--- packages/@aws-cdk/aws-cloudwatch/lib/metric-types.ts
+++ packages/@aws-cdk/aws-cloudwatch/lib/metric-types.ts
@@ -1,4 +1,3 @@
-
 /**
  * Interface for metrics
  */
@@ -16,6 +15,7 @@ export interface IMetric {
 
 /**
  * Metric dimension
+ *
  */
 export interface Dimension {
   /**
@@ -33,44 +33,44 @@ export interface Dimension {
  * Statistic to use over the aggregation period
  */
 export enum Statistic {
-  SAMPLE_COUNT = 'SampleCount',
-  AVERAGE = 'Average',
-  SUM = 'Sum',
-  MINIMUM = 'Minimum',
-  MAXIMUM = 'Maximum',
+  SAMPLE_COUNT = "SampleCount",
+  AVERAGE = "Average",
+  SUM = "Sum",
+  MINIMUM = "Minimum",
+  MAXIMUM = "Maximum"
 }
 
 /**
  * Unit for metric
  */
 export enum Unit {
-  SECONDS = 'Seconds',
-  MICROSECONDS = 'Microseconds',
-  MILLISECONDS = 'Milliseconds',
-  BYTES = 'Bytes',
-  KILOBYTES = 'Kilobytes',
-  MEGABYTES = 'Megabytes',
-  GIGABYTES = 'Gigabytes',
-  TERABYTES = 'Terabytes',
-  BITS = 'Bits',
-  KILOBITS = 'Kilobits',
-  MEGABITS = 'Megabits',
-  GIGABITS = 'Gigabits',
-  TERABITS = 'Terabits',
-  PERCENT = 'Percent',
-  COUNT = 'Count',
-  BYTES_PER_SECOND = 'Bytes/Second',
-  KILOBYTES_PER_SECOND = 'Kilobytes/Second',
-  MEGABYTES_PER_SECOND = 'Megabytes/Second',
-  GIGABYTES_PER_SECOND = 'Gigabytes/Second',
-  TERABYTES_PER_SECOND = 'Terabytes/Second',
-  BITS_PER_SECOND = 'Bits/Second',
-  KILOBITS_PER_SECOND = 'Kilobits/Second',
-  MEGABITS_PER_SECOND = 'Megabits/Second',
-  GIGABITS_PER_SECOND = 'Gigabits/Second',
-  TERABITS_PER_SECOND = 'Terabits/Second',
-  COUNT_PER_SECOND = 'Count/Second',
-  NONE = 'None'
+  SECONDS = "Seconds",
+  MICROSECONDS = "Microseconds",
+  MILLISECONDS = "Milliseconds",
+  BYTES = "Bytes",
+  KILOBYTES = "Kilobytes",
+  MEGABYTES = "Megabytes",
+  GIGABYTES = "Gigabytes",
+  TERABYTES = "Terabytes",
+  BITS = "Bits",
+  KILOBITS = "Kilobits",
+  MEGABITS = "Megabits",
+  GIGABITS = "Gigabits",
+  TERABITS = "Terabits",
+  PERCENT = "Percent",
+  COUNT = "Count",
+  BYTES_PER_SECOND = "Bytes/Second",
+  KILOBYTES_PER_SECOND = "Kilobytes/Second",
+  MEGABYTES_PER_SECOND = "Megabytes/Second",
+  GIGABYTES_PER_SECOND = "Gigabytes/Second",
+  TERABYTES_PER_SECOND = "Terabytes/Second",
+  BITS_PER_SECOND = "Bits/Second",
+  KILOBITS_PER_SECOND = "Kilobits/Second",
+  MEGABITS_PER_SECOND = "Megabits/Second",
+  GIGABITS_PER_SECOND = "Gigabits/Second",
+  TERABITS_PER_SECOND = "Terabits/Second",
+  COUNT_PER_SECOND = "Count/Second",
+  NONE = "None"
 }
 
 /**
