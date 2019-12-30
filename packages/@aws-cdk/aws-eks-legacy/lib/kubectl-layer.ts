6a3946d940207316d3fb0e56416428152999aa25
Elad Ben-Israel <benisrae@amazon.com>
2019-12-30 16:58:52 +0200

    fix(eks): failures when creating or updating clusters
    There were two causes of timeouts for EKS cluster creation: create time which is longer than the AWS Lambda timeout (15min) and lack of retry when applying kubectl after the cluster has been created.
    
    The change fixes the first issue by leveraging the custom resource provider framework to implement the cluster resource as an async resource. The custom resource providers are now bundled as nested stacks so they don't take up too many resources from users, and are also reused by multiple clusters within the same stack. This required that the creation role will not be the same as the lambda role, so we define this role separately and assume it within the providers.
    
    The second issue is fixed by adding 3 retries to "kubectl apply".
    
    **Backwards compatibility**: as described in #5544, since the resource provider handler of `Cluster` and `KubernetesResource` has been changed, this change requires a replacement of existing clusters (deployment fails with "service token cannot be changed" error). Since this can be disruptive to users, this change includes an exact copy of the previous version under a new module called `@aws-cdk/aws-eks-legacy`, which can be used as a drop-in replacement until users decide to upgrade to the new version. Using the legacy cluster will emit a synthesis warning that this module will no longer be released as part of the CDK starting March 1st, 2020.
    
    - Fixes #4087
    - Fixes #4695
    - Fixes #5259
    - Fixes #5501
    
    ---
    
    BREAKING CHANGE: (in experimental module) the providers behind the AWS EKS module have been rewritten to address multiple stability issues. Since this change requires cluster replacement, the old version of this module is available under `@aws-cdk/aws-eks-legacy`. Please read https://github.com/aws/aws-cdk/issues/5544 carefully for upgrade instructions.

---
+++ packages/@aws-cdk/aws-eks-legacy/lib/kubectl-layer.ts
@@ -0,0 +1,78 @@
+import * as lambda from '@aws-cdk/aws-lambda';
+import { CfnResource, Construct, Stack, Token } from '@aws-cdk/core';
+import * as crypto from 'crypto';
+
+const KUBECTL_APP_ARN = 'arn:aws:serverlessrepo:us-east-1:903779448426:applications/lambda-layer-kubectl';
+const KUBECTL_APP_VERSION = '1.13.7';
+
+export interface KubectlLayerProps {
+  /**
+   * The semantic version of the kubectl AWS Lambda Layer SAR app to use.
+   *
+   * @default '1.13.7'
+   */
+  readonly version?: string;
+}
+
+/**
+ * An AWS Lambda layer that includes kubectl and the AWS CLI.
+ *
+ * @see https://github.com/aws-samples/aws-lambda-layer-kubectl
+ */
+export class KubectlLayer extends Construct implements lambda.ILayerVersion {
+
+  /**
+   * Gets or create a singleton instance of this construct.
+   */
+  public static getOrCreate(scope: Construct, props: KubectlLayerProps = {}): KubectlLayer {
+    const stack = Stack.of(scope);
+    const id = 'kubectl-layer-' + (props.version ? props.version : "8C2542BC-BF2B-4DFE-B765-E181FD30A9A0");
+    const exists = stack.node.tryFindChild(id) as KubectlLayer;
+    if (exists) {
+      return exists;
+    }
+
+    return new KubectlLayer(stack, id, props);
+  }
+
+  /**
+   * The ARN of the AWS Lambda layer version.
+   */
+  public readonly layerVersionArn: string;
+
+  /**
+   * All runtimes are compatible.
+   */
+  public readonly compatibleRuntimes?: lambda.Runtime[] = undefined;
+
+  constructor(scope: Construct, id: string, props: KubectlLayerProps = {}) {
+    super(scope, id);
+
+    const uniqueId = crypto.createHash('md5').update(this.node.path).digest("hex");
+    const version = props.version || KUBECTL_APP_VERSION;
+
+    this.stack.templateOptions.transforms = [ 'AWS::Serverless-2016-10-31' ]; // required for AWS::Serverless
+    const resource = new CfnResource(this, 'Resource', {
+      type: 'AWS::Serverless::Application',
+      properties: {
+        Location: {
+          ApplicationId: KUBECTL_APP_ARN,
+          SemanticVersion: version
+        },
+        Parameters: {
+          LayerName: `kubectl-${uniqueId}`
+        }
+      }
+    });
+
+    this.layerVersionArn = Token.asString(resource.getAtt('Outputs.LayerVersionArn'));
+  }
+
+  public get stack() {
+    return Stack.of(this);
+  }
+
+  public addPermission(_id: string, _permission: lambda.LayerVersionPermission): void {
+    return;
+  }
+}
