997300d854d8c3c9acc747704c3e838c8c46cd6b
Michael <6798695+gruenem@users.noreply.github.com>
2020-01-08 14:37:48 +0100

    Python3.8 doesn't support inline code yet

--- packages/@aws-cdk/aws-lambda/lib/runtime.ts
+++ packages/@aws-cdk/aws-lambda/lib/runtime.ts
@@ -39,7 +39,7 @@ export class Runtime {
   public static readonly PYTHON_2_7 =     new Runtime('python2.7',      RuntimeFamily.PYTHON, { supportsInlineCode: true });
   public static readonly PYTHON_3_6 =     new Runtime('python3.6',      RuntimeFamily.PYTHON, { supportsInlineCode: true });
   public static readonly PYTHON_3_7 =     new Runtime('python3.7',      RuntimeFamily.PYTHON, { supportsInlineCode: true });
-  public static readonly PYTHON_3_8 =     new Runtime('python3.8',      RuntimeFamily.PYTHON, { supportsInlineCode: true });
+  public static readonly PYTHON_3_8 =     new Runtime('python3.8',      RuntimeFamily.PYTHON);
   public static readonly JAVA_8 =        new Runtime('java8',          RuntimeFamily.JAVA);
   public static readonly JAVA_11 =        new Runtime('java11',          RuntimeFamily.JAVA);
   public static readonly DOTNET_CORE_1 =  new Runtime('dotnetcore1.0',  RuntimeFamily.DOTNET_CORE);
