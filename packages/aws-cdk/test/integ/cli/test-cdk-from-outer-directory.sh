6e46efee0570881852fbc484a201bafa0edf96e7
epolon <epolon@amazon.com>
2020-01-13 15:50:14 +0200

    added cleanup to remove temp directory

--- packages/aws-cdk/test/integ/cli/test-cdk-from-outer-directory.sh
+++ packages/aws-cdk/test/integ/cli/test-cdk-from-outer-directory.sh
@@ -7,6 +7,13 @@ source ${scriptdir}/common.bash
 setup
 
 temp_dir=$(mktemp -d)
+
+function cleanup() {
+  rm -rf ${temp_dir}
+}
+
+trap cleanup EXIT INT
+
 pushd ${temp_dir}
 set +e
 output="$(cdk diff ${STACK_NAME_PREFIX}-test-1 2>&1)"
