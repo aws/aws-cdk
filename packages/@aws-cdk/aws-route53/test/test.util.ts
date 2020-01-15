898af4596acc6cdd79f3bc70f1c1ba098cde0cf5
Don Buchanan <42761041+Don-CA@users.noreply.github.com>
2020-01-15 16:29:03 +1100

    determineFullyQualifiedDomainName ignores trailing dot in hostedZone.zoneName

--- packages/@aws-cdk/aws-route53/test/test.util.ts
+++ packages/@aws-cdk/aws-route53/test/test.util.ts
@@ -1,5 +1,5 @@
+import * as cdk from '@aws-cdk/core';
 import { Test } from 'nodeunit';
-
 import * as util from '../lib/util';
 
 export = {
@@ -7,9 +7,67 @@ export = {
     test.throws(() => util.validateZoneName('zone.name.'), /trailing dot/);
     test.done();
   },
+
   'accepts a valid domain name'(test: Test) {
     const domainName = 'amazonaws.com';
     util.validateZoneName(domainName);
     test.done();
-  }
+  },
+
+  'providedName ending with a dot returns the name'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+
+    // WHEN
+    const providedName = 'test.domain.com.';
+    const qualified = util.determineFullyQualifiedDomainName(providedName, {
+        hostedZoneId: 'fakeId',
+        zoneName: 'ignored',
+        hostedZoneArn: 'arn:aws:route53:::hostedzone/fakeId',
+        stack,
+        node: stack.node,
+    });
+
+    // THEN
+    test.equal(qualified, 'test.domain.com.');
+    test.done();
+  },
+
+  'providedName that matches zoneName returns providedName with a dot'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+
+    // WHEN
+    const providedName = 'test.domain.com';
+    const qualified = util.determineFullyQualifiedDomainName(providedName, {
+        hostedZoneId: 'fakeId',
+        zoneName: 'domain.com.',
+        hostedZoneArn: 'arn:aws:route53:::hostedzone/fakeId',
+        stack,
+        node: stack.node,
+    });
+
+    // THEN
+    test.equal(qualified, 'test.domain.com.');
+    test.done();
+  },
+
+  'providedName that does not matches zoneName concatenates providedName and zoneName'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+
+    // WHEN
+    const providedName = 'test';
+    const qualified = util.determineFullyQualifiedDomainName(providedName, {
+        hostedZoneId: 'fakeId',
+        zoneName: 'domain.com.',
+        hostedZoneArn: 'arn:aws:route53:::hostedzone/fakeId',
+        stack,
+        node: stack.node,
+    });
+
+    // THEN
+    test.equal(qualified, 'test.domain.com.');
+    test.done();
+  },
 };
