3f5730f08ebedcb47aecee8141930563599fc793
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2020-01-17 14:54:10 +0100

    improve and document integ test

--- packages/@aws-cdk/aws-lambda-destinations/test/integ.lambda-chain.ts
+++ packages/@aws-cdk/aws-lambda-destinations/test/integ.lambda-chain.ts
@@ -1,6 +1,18 @@
 import * as lambda from '@aws-cdk/aws-lambda';
-import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
+import { App, CfnOutput, Construct, Stack, StackProps } from '@aws-cdk/core';
 import * as destinations from '../lib';
+
+// Test success case with:
+// 1. Invoke first function in the chain
+//   aws lambda invoke --function-name <first function name> --invocation-type Event --payload '"OK"' response.json
+// 2. Check logs of third function (should show 'Event: "OK"')
+//   aws logs filter-log-events --log-group-name /aws/lambda/<third function name>
+//
+// Test failure case with:
+// 1. Invoke first function in the chain
+//   aws lambda invoke --function-name <first function name> --invocation-type Event --payload '"error"' response.json
+// 2. Check logs of error function (should show 'Event: {"errorType": "Error", "errorMessage": "UnkownError", "trace":"..."}')
+//   aws logs filter-log-events --log-group-name /aws/lambda/<error function name>
 
 class TestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,7 +22,8 @@ class TestStack extends Stack {
       runtime: lambda.Runtime.NODEJS_10_X,
       handler: 'index.handler',
       code: lambda.Code.fromInline(`exports.handler = async (event) => {
-        console.log(event);
+        console.log('Event: %j', event);
+        if (event === 'error') throw new Error('UnkownError');
         return event;
       };`),
     };
@@ -18,14 +31,21 @@ class TestStack extends Stack {
     const first = new lambda.Function(this, 'First', lambdaProps);
     const second = new lambda.Function(this, 'Second', lambdaProps);
     const third = new lambda.Function(this, 'Third', lambdaProps);
+    const error = new lambda.Function(this, 'Error', lambdaProps);
 
     first.configureAsyncInvoke({
       onSuccess: new destinations.LambdaDestination(second, { responseOnly: true }),
+      onFailure: new destinations.LambdaDestination(error, { responseOnly: true }),
+      retryAttempts: 0
     });
 
     second.configureAsyncInvoke({
       onSuccess: new destinations.LambdaDestination(third, { responseOnly: true }),
     });
+
+    new CfnOutput(this, 'FirstFunctionName', { value: first.functionName });
+    new CfnOutput(this, 'ThirdFunctionName', { value: third.functionName });
+    new CfnOutput(this, 'ErrorFunctionName', { value: error.functionName });
   }
 }
 
