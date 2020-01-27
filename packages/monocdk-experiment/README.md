fb8bf18fabaa0ea295bce3cf42c92deecd5197bc
Elad Ben-Israel <benisrae@amazon.com>
2020-01-28 00:08:35 +0200

    chore: mono-cdk experiment
    An experiment to bundle the entire CDK into a single monolithic module called `monocdk-experiment`.
    
    This module depends on all other modules as a dev-dependency and during build copies all the lib/** files
    from all modules into an src/ directory and rewrites the "import" statements for internal references.
    
    The goal is to release this to npm as an experiment in monolithic packaging.

---
+++ packages/monocdk-experiment/README.md
@@ -0,0 +1,5 @@
+# MonoCDK Experiment
+
+[![experimental](http://badges.github.io/stability-badges/dist/experimental.svg)](http://github.com/badges/stability-badges)
+
+An experiment to bundle all of the CDK into a single module.
---
+++ packages/monocdk-experiment/gen.js
@@ -0,0 +1,84 @@
+// generates the mono-cdk module by copying @aws-cdk/lib/** to src/
+// and rewriting the interdepedent "import" statements.
+const fs = require('fs-extra');
+const path = require('path');
+const glob = require('glob');
+
+async function main() {
+  const srcdir = path.resolve('src');
+  const reexports = [];
+
+  await fs.rmdir(srcdir, { recursive: true });
+  await fs.mkdir(srcdir);
+
+  const root = path.resolve(__dirname, 'node_modules', '@aws-cdk');
+  const modules = await fs.readdir(root);
+
+  for (const dir of modules) {
+    const module = path.resolve(root, dir);
+    const meta = JSON.parse(await fs.readFile(path.join(module, 'package.json'), 'utf-8'));
+
+    if (meta.deprecated) {
+      console.error(`skipping deprecated ${meta.name}`);
+      continue;
+    }
+
+    if (!meta.jsii) {
+      console.error(`skipping non-jsii ${meta.name}`);
+      continue;
+    }
+
+    const basename = path.basename(module);
+    const source = `${module}/lib`;
+    const target = `${srcdir}/${basename}/lib`;
+    console.log(`${source} => ${target}`);
+    await fs.copy(source, target);
+
+    await fs.writeFile(path.join(path.dirname(target), 'index.ts'), 'export * from "./lib"\n');
+
+    const namespace = basename.replace(/-/g, '_');
+    reexports.push(`import * as ${namespace} from './${basename}/lib'; export { ${namespace} };`)
+  }
+
+  await fs.writeFile(path.join(srcdir, 'index.ts'), reexports.join('\n'));
+
+  const sourceFiles = await findSources(srcdir);
+  for (const source of sourceFiles) {
+    await rewriteImports(srcdir, source);
+  }
+}
+
+async function findSources(srcdir) {
+  return new Promise((ok, ko) => glob('**/*.ts', { cwd: srcdir }, (err, results) => {
+    if (err) { return ko(err); }
+    return ok(results);
+  }));
+}
+
+async function rewriteImports(srcdir, relativeSource) {
+  const absoluteSource = path.join(srcdir, relativeSource);
+  const source = await fs.readFile(absoluteSource, 'utf-8');
+
+  const match = /from ['"]@aws-cdk\/(.+)['"]/.exec(source);
+  if (!match) {
+    return;
+  }
+
+  const left = source.substring(0, match.index);
+  const right = source.substring(match.index + match[0].length);
+
+  const submodule = match[1];
+  const moduleDir = path.join(srcdir, submodule);
+  const rel = path.relative(path.dirname(absoluteSource), moduleDir)
+
+  const newSource = `${left}from '${rel}'${right}`;
+  await fs.writeFile(absoluteSource, newSource);
+
+  // call recursively until exhausted
+  await rewriteImports(srcdir, relativeSource);
+}
+
+main().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
