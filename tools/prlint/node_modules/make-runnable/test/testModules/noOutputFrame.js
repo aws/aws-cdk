aadfada84ce153a6c7867b08a5296fb291b78713
epolon <epolon@amazon.com>
2020-01-27 13:13:45 +0200

    added make runnable

---
+++ tools/prlint/node_modules/make-runnable/test/testModules/noOutputFrame.js
@@ -0,0 +1,7 @@
+module.exports = {
+	lookMa: 'no frame!'
+};
+
+require('../../custom')({
+	printOutputFrame: false
+});
---
+++ tools/prlint/node_modules/make-runnable/utils/mergeObjects.js
@@ -0,0 +1,6 @@
+module.exports = function mergeObjects(obj1, obj2) {
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+}
