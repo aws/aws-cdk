fcb26bfafa5549e8795c75f7561e1323fb1363e3
epolon <epolon@amazon.com>
2020-01-27 13:19:58 +0200

    debug logging

--- .github/actions/prlinter/index.js
+++ .github/actions/prlinter/index.js
@@ -3,7 +3,23 @@ const github = require('@actions/github');
 const linter = require('prlint')
 
 try {
+
+    const number = github.context.issue.number;
+
+    console.log(`PR: ${number}`);
+
+    console.log('Validating...');
+
     linter.mandatoryChanges(github.context.issue.number);
+
+    console.log('Success!')
+
+
 } catch (error) {
+
+    console.log(`Failed: ${error.message}`)
+
     core.setFailed(error.message);
+
+    console.log('Set Failed')
 }
