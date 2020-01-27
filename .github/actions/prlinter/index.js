366656ad5aacc0a8cc0d155cf27a0b825db06a74
epolon <epolon@amazon.com>
2020-01-27 13:26:05 +0200

    make async

--- .github/actions/prlinter/index.js
+++ .github/actions/prlinter/index.js
@@ -2,24 +2,29 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const linter = require('prlint')
 
-try {
+async function run() {
 
-    const number = github.context.issue.number;
+    try {
 
-    console.log(`PR: ${number}`);
-
-    console.log('Validating...');
-
-    await linter.mandatoryChanges(github.context.issue.number);
-
-    console.log('Success!')
-
-
-} catch (error) {
-
-    console.log(`Failed: ${error.message}`)
-
-    core.setFailed(error.message);
-
-    console.log('Set Failed')
+        const number = github.context.issue.number;
+    
+        console.log(`PR: ${number}`);
+    
+        console.log('Validating...');
+    
+        await linter.mandatoryChanges(github.context.issue.number);
+    
+        console.log('Success!')
+    
+    
+    } catch (error) {
+    
+        console.log(`Failed: ${error.message}`)
+    
+        core.setFailed(error.message);
+    
+        console.log('Set Failed')
+    }    
 }
+
+run()
