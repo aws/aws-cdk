785faf82921a77a532c8696d9bc55a42f765c747
Rico Huijbers <rix0rrr@gmail.com>
2020-02-14 10:42:14 +0100

    chore: make VPC-import integ test isolated
    The "VPC import" CLI integ test creates a VPC with a tag of `custom-tag:
    bazinga!` and then tries to import it.
    
    If multiple instances of this test run at the same time, multiple VPCs
    will be created and the lookup will fail since the query must produce
    a unique VPC. What's worse, the VPC won't even be cleaned up anymore
    and all future test runs will fail.
    
    Make the tag value include a unique token so that the query will
    always look up the VPC just created. Also try to recover and delete
    the stack we just created if the test fails.

--- packages/aws-cdk/test/integ/cli/test-vpc-lookup.sh
+++ packages/aws-cdk/test/integ/cli/test-vpc-lookup.sh
@@ -12,10 +12,20 @@ echo "Setup complete!"
 
 # verify we can synth the importing stack now
 echo "Verifying we can now import that VPC"
-ENABLE_VPC_TESTING="IMPORT" cdk synth -v ${STACK_NAME_PREFIX}-import-vpc
+FAILED=false
+if ! ENABLE_VPC_TESTING="IMPORT" cdk synth -v ${STACK_NAME_PREFIX}-import-vpc; then
+  # Don't stop here (still clean up) but remember that we failed
+  echo "[MARK] Importing failed! The error will be just above this line." >&2
+  FAILED=true
+fi
 
 # destroy
 echo "Cleaning up..."
 ENABLE_VPC_TESTING="DEFINE" cdk destroy -f ${STACK_NAME_PREFIX}-define-vpc
 
-echo "✅  success"
+if $FAILED; then
+  echo "Importing test failed. Disregard the cleanup log, search upward for the line saying '[MARK]'." >&2
+  exit 1
+else
+  echo "✅  success"
+fi
