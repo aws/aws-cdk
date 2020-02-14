8cfc8604d1cdef95631ae79365ee409f0f817f4f
Rico Huijbers <rix0rrr@gmail.com>
2020-02-14 11:16:43 +0100

    Cleanup beforehand

--- packages/aws-cdk/test/integ/cli/test-vpc-lookup.sh
+++ packages/aws-cdk/test/integ/cli/test-vpc-lookup.sh
@@ -6,26 +6,19 @@ source ${scriptdir}/common.bash
 
 setup
 
+echo "Making sure we are clean before starting."
+ENABLE_VPC_TESTING="DEFINE" cdk destroy -f ${STACK_NAME_PREFIX}-define-vpc
+
 echo "Setting up: creating a VPC with known tags"
 ENABLE_VPC_TESTING="DEFINE" cdk deploy ${STACK_NAME_PREFIX}-define-vpc
 echo "Setup complete!"
 
 # verify we can synth the importing stack now
 echo "Verifying we can now import that VPC"
-FAILED=false
-if ! ENABLE_VPC_TESTING="IMPORT" cdk synth -v ${STACK_NAME_PREFIX}-import-vpc; then
-  # Don't stop here (still clean up) but remember that we failed
-  echo "[MARK] Importing failed! The error will be just above this line." >&2
-  FAILED=true
-fi
+ENABLE_VPC_TESTING="IMPORT" cdk synth -v ${STACK_NAME_PREFIX}-import-vpc
 
 # destroy
 echo "Cleaning up..."
 ENABLE_VPC_TESTING="DEFINE" cdk destroy -f ${STACK_NAME_PREFIX}-define-vpc
 
-if $FAILED; then
-  echo "Importing test failed. Disregard the cleanup log, search upward for the line saying '[MARK]'." >&2
-  exit 1
-else
-  echo "✅  success"
-fi
+echo "✅  success"
