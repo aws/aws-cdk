8d7e516c3cd5f5ca0cad27af8c1fe522c081faaa
Brian Rogers <brian.andrew.rogers@gmail.com>
2020-02-17 15:19:40 -0500

    Clarify documentation on dead letter queue objects

--- packages/@aws-cdk/aws-lambda/lib/sns-dlq.ts
+++ packages/@aws-cdk/aws-lambda/lib/sns-dlq.ts
@@ -2,7 +2,7 @@ import * as sns from '@aws-cdk/aws-sns';
 import { DLQDestinationConfig, IEventSourceDLQ } from "./dlq";
 
 /**
- * An SNS destination configuration
+ * An SNS dead letter queue destination configuration
  */
 export class SnsDLQ implements IEventSourceDLQ {
     constructor(private readonly topic: sns.ITopic) {
--- packages/@aws-cdk/aws-lambda/lib/sqs-dlq.ts
+++ packages/@aws-cdk/aws-lambda/lib/sqs-dlq.ts
@@ -2,7 +2,7 @@ import * as sqs from '@aws-cdk/aws-sqs';
 import { DLQDestinationConfig, IEventSourceDLQ } from "./dlq";
 
 /**
- * An SQS destination configuration
+ * An SQS dead letter queue destination configuration
  */
 export class SqsDLQ implements IEventSourceDLQ {
     constructor(private readonly queue: sqs.IQueue) {
