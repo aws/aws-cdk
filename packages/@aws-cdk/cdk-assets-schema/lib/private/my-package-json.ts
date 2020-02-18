00b4429ef8da38d802523663e33fe862a657d955
Rico Huijbers <rix0rrr@gmail.com>
2020-02-18 12:43:08 +0100

    Disable linter checks.

--- packages/@aws-cdk/cdk-assets-schema/lib/private/my-package-json.ts
+++ packages/@aws-cdk/cdk-assets-schema/lib/private/my-package-json.ts
@@ -1,5 +1,8 @@
 import * as path from 'path';
 
+// tslint:disable:no-var-requires
+// eslint-disable @typescript-eslint/no-require-imports
+
 /**
  * Get my package JSON.
  *
@@ -14,13 +17,9 @@ export function loadMyPackageJson(): any {
     } catch (e) {
       if (e.code !== 'MODULE_NOT_FOUND') { throw e; }
 
-      const parent = path.dirname(dir);
-      if (parent === dir) { throw new Error(`No package.json found upward of ${__dirname}`); }
-      dir = parent;
+      const newdir = path.dirname(dir);
+      if (newdir === dir) { throw new Error(`No package.json found upward of ${__dirname}`); }
+      dir = newdir;
     }
   }
 }
-
-// tslint:disable:no-var-requires
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-// tslint:enable:no-var-requires
