90de2df61a0c654020fb8af8fb09a5f872be2ab8
Pahud <pahudnet@gmail.com>
2020-02-18 08:22:35 +0800

    add more tests

--- packages/@aws-cdk/aws-cloud9/test/test.cloud9.ts
+++ packages/@aws-cdk/aws-cloud9/test/test.cloud9.ts
@@ -1,17 +1,52 @@
-import { expect, haveResource } from '@aws-cdk/assert';
+import { countResources, expect, haveOutput, haveResource } from '@aws-cdk/assert';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
 import { Test } from 'nodeunit';
 import * as cloud9 from '../lib';
 
 export = {
-  'default cloud9 ec2 environment'(test: Test) {
+  'with no properties set, it correctly sets default properties'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+    // const vpc = mockVpc(stack);
+
+    // WHEN
+    new cloud9.EnvironmentEC2(stack, 'C9Env');
+
+    // THEN
+    expect(stack).to(haveResource("AWS::Cloud9::EnvironmentEC2", {
+      InstanceType: "t2.micro",
+      SubnetId: {
+        Ref: "C9EnvVpcPublicSubnet1Subnet6CAA08D7"
+      }
+    }
+    ));
+    expect(stack).to(countResources("AWS::Cloud9::EnvironmentEC2", 1));
+    expect(stack).to(haveResource("AWS::EC2::VPC", {
+      CidrBlock: "10.0.0.0/16",
+      EnableDnsHostnames: true,
+      EnableDnsSupport: true,
+      InstanceTenancy: "default",
+      Tags: [
+        {
+          Key: "Name",
+          Value: "C9Env/Vpc"
+        }
+      ]
+    }
+    ));
+    test.done();
+  },
+
+  'with only vpc set, it correctly sets default properties'(test: Test) {
     // GIVEN
     const stack = new cdk.Stack();
     const vpc = mockVpc(stack);
 
     // WHEN
-    new cloud9.EnvironmentEC2(stack, 'C9Env', { vpc });
+    new cloud9.EnvironmentEC2(stack, 'C9Env', {
+      vpc,
+     });
 
     // THEN
     expect(stack).to(haveResource("AWS::Cloud9::EnvironmentEC2", {
@@ -20,6 +55,73 @@ export = {
     }
     ));
 
+    test.done();
+  },
+
+  'allow specifying instance type'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const vpc = mockVpc(stack);
+
+    // WHEN
+    new cloud9.EnvironmentEC2(stack, 'C9Env', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.C5, ec2.InstanceSize.XLARGE)
+    });
+
+    // THEN
+    expect(stack).to(haveResource("AWS::Cloud9::EnvironmentEC2", {
+      InstanceType: "c5.xlarge",
+      SubnetId: "pub1"
+    }
+    ));
+    test.done();
+  },
+
+  'when subnetId and vpc both specified, select subnetId'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const vpc = mockVpc(stack);
+
+    // WHEN
+    new cloud9.EnvironmentEC2(stack, 'C9Env', {
+      vpc,
+      subnetId: 'anotherId'
+    });
+
+    // THEN
+    expect(stack).to(haveResource("AWS::Cloud9::EnvironmentEC2", {
+      InstanceType: "t2.micro",
+      SubnetId: "anotherId"
+    }
+    ));
+    test.done();
+  },
+
+  'multiple environments with default properties'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+
+    // WHEN
+    for (let i = 0; i < 2; i++) {
+      new cloud9.EnvironmentEC2(stack, `C9Env${i}`);
+    }
+
+    // THEN
+    expect(stack).to(countResources("AWS::Cloud9::EnvironmentEC2", 2));
+    expect(stack).to(haveResource("AWS::EC2::VPC", {
+      CidrBlock: "10.0.0.0/16",
+      EnableDnsHostnames: true,
+      EnableDnsSupport: true,
+      InstanceTenancy: "default",
+      Tags: [
+        {
+          Key: "Name",
+          Value: "C9Env1/Vpc"
+        }
+      ]
+    }
+    ));
     test.done();
   },
 
@@ -46,7 +148,7 @@ export = {
     const vpc = mockVpc(stack);
 
     // WHEN
-    new cloud9.EnvironmentEC2(stack, 'C9Env', {
+    const c9env = new cloud9.EnvironmentEC2(stack, 'C9Env', {
       vpc,
       environmentEc2Name: 'MyCustomEnvName',
       description: 'my test env',
@@ -54,6 +156,7 @@ export = {
       subnetId: 'foo'
     });
 
+    new cdk.CfnOutput(stack, 'URL', { value: c9env.ideUrl});
     // THEN
     expect(stack).to(haveResource("AWS::Cloud9::EnvironmentEC2", {
       InstanceType: "t3.large",
@@ -62,6 +165,9 @@ export = {
       SubnetId: "foo"
     }
     ));
+    expect(stack).to(haveOutput({
+      outputName: 'URL'
+    }));
 
     test.done();
   },
