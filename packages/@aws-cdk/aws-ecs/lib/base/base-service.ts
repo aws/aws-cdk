768c1e788ec830b8fc29dfb62dde82552d34d232
Efe Karakus <efekarakus@gmail.com>
2020-02-19 11:19:00 -0800

    fix(aws-ecs): propagate dnsTtl property part of cloudMapOptions (#6370)
    Fixes #6223

--- packages/@aws-cdk/aws-ecs/lib/base/base-service.ts
+++ packages/@aws-cdk/aws-ecs/lib/base/base-service.ts
@@ -514,7 +514,8 @@ export abstract class BaseService extends Resource
       namespace: sdNamespace,
       name: options.name,
       dnsRecordType: dnsRecordType!,
-      customHealthCheck: { failureThreshold: options.failureThreshold || 1 }
+      customHealthCheck: { failureThreshold: options.failureThreshold || 1 },
+      dnsTtl: options.dnsTtl,
     });
 
     const serviceArn = cloudmapService.serviceArn;
