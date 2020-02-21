33929cf45c030084d0914dd5a165f0192617d6a6
robertd <robert.djurasaj@gmail.com>
2020-02-20 23:21:59 -0700

    remove extra space

--- packages/@aws-cdk/aws-apigateway/lib/restapi.ts
+++ packages/@aws-cdk/aws-apigateway/lib/restapi.ts
@@ -1,6 +1,6 @@
 import { IVpcEndpoint } from '@aws-cdk/aws-ec2';
 import * as iam from '@aws-cdk/aws-iam';
-import { Aws, CfnOutput, Construct,  IResource as IResourceBase, Resource, Stack } from '@aws-cdk/core';
+import { Aws, CfnOutput, Construct, IResource as IResourceBase, Resource, Stack } from '@aws-cdk/core';
 import { ApiKey, IApiKey } from './api-key';
 import { CfnAccount, CfnRestApi } from './apigateway.generated';
 import { CorsOptions } from './cors';
