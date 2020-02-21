ddbf5656a8a28cfcce1d2a0724df80d45874b5b2
Adam Plumer <plumer@amazon.com>
2020-02-21 14:23:16 -0500

    fixup! feat(apigateway): add lambda request authorizer construct

--- packages/@aws-cdk/aws-apigateway/lib/authorizers/identity-source.ts
+++ packages/@aws-cdk/aws-apigateway/lib/authorizers/identity-source.ts
@@ -1,5 +1,3 @@
-import { Token } from '@aws-cdk/core';
-
 /**
  * Represents an identity source.
  *
@@ -8,40 +6,48 @@ import { Token } from '@aws-cdk/core';
  */
 export class IdentitySource {
   /**
+   * Provides a properly formatted header identity source.
    * @param headerName the name of the header the `IdentitySource` will represent.
-   * @returns a new `IdentitySource` representing `headerName` from the request.
+   *
+   * @returns a header identity source.
    */
-  public static header(headerName: string): IdentitySource {
+  public static header(headerName: string): string {
     return IdentitySource.toString(headerName, 'method.request.header');
   }
 
   /**
+   * Provides a properly formatted query string identity source.
    * @param queryString the name of the query string the `IdentitySource` will represent.
-   * @returns a new `IdentitySource` representing `queryString` from the request.
+   *
+   * @returns a query string identity source.
    */
-  public static queryString(queryString: string): IdentitySource {
+  public static queryString(queryString: string): string {
     return IdentitySource.toString(queryString, 'method.request.querystring');
   }
 
   /**
+   * Provides a properly formatted API Gateway stage variable identity source.
    * @param stageVariable the name of the stage variable the `IdentitySource` will represent.
-   * @returns a new `IdentitySource` representing `stageVariable` from the API Gateway.
+   *
+   * @returns an API Gateway stage variable identity source.
    */
-  public static stageVariable(stageVariable: string): IdentitySource {
+  public static stageVariable(stageVariable: string): string {
     return IdentitySource.toString(stageVariable, 'stageVariables');
   }
 
   /**
+   * Provides a properly formatted request context identity source.
    * @param context the name of the context variable the `IdentitySource` will represent.
-   * @returns a new `IdentitySource` representing `context` from the request context.
+   *
+   * @returns a request context identity source.
    */
-  public static context(context: string): IdentitySource {
+  public static context(context: string): string {
     return IdentitySource.toString(context, 'context');
   }
 
   private static toString(source: string, type: string) {
-    if (!Token.isUnresolved(source) && source === '') {
-      throw new Error(`IdentitySources cannot be empty. Received: ${source}`);
+    if (!source.trim()) {
+      throw new Error(`IdentitySources must be a non-empty string.`);
     }
 
     return `${type}.${source}`;
