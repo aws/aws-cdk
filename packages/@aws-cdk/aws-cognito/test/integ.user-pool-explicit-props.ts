58f75f2584c72ffb523b3c9678cbdbb71cb344f7
Niranjan Jayakar <16217941+nija-at@users.noreply.github.com>
2020-02-27 14:56:44 +0000

    chore(cognito): fix broken integration test (#6484)
    It turns out that the Cognito service has validations that,
    1. the user invitation email body and sms message must have the
    template strings '{username}' and '{####}' - which would hold the
    username and temporary password,
    2. the verification email body and sms message should have the template
    string '{####}' which would hold the verification code.
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/@aws-cdk/aws-cognito/test/integ.user-pool-explicit-props.ts
+++ packages/@aws-cdk/aws-cognito/test/integ.user-pool-explicit-props.ts
@@ -8,14 +8,14 @@ new UserPool(stack, 'myuserpool', {
   userPoolName: 'MyUserPool',
   userInvitation: {
     emailSubject: 'invitation email subject from the integ test',
-    emailBody: 'invitation email body from the integ test',
-    smsMessage: 'invitation sms message from the integ test'
+    emailBody: 'invitation email body from the integ test for {username}. Temp password is {####}.',
+    smsMessage: 'invitation sms message from the integ test for {username}. Temp password is {####}.',
   },
   selfSignUpEnabled: true,
   userVerification: {
-    emailBody: 'verification email body from the integ test',
+    emailBody: 'verification email body from the integ test. Code is {####}.',
     emailSubject: 'verification email subject from the integ test',
-    smsMessage: 'verification sms message from the integ test'
+    smsMessage: 'verification sms message from the integ test. Code is {####}.',
   },
   signInAliases: {
     username: true,
