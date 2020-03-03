d04951b036b0626e8a5102d88d609a8205bdf357
epolon <epolon@amazon.com>
2020-03-04 00:14:13 +0200

    added commit message validation

--- tools/prlint/index.js
+++ tools/prlint/index.js
@@ -18,7 +18,7 @@ function createGitHubClient() {
     } else {
         console.log("Creating un-authenticated GitHub Client")
     }
-    
+
     return new GitHub({'token': token});
 }
 
@@ -63,10 +63,10 @@ async function mandatoryChanges(number) {
     }
 
     const gh = createGitHubClient();
-    
+
     const issues = gh.getIssues(OWNER, REPO);
     const repo = gh.getRepo(OWNER, REPO);
-    
+
     console.log(`⌛  Fetching PR number ${number}`)
     const issue = (await issues.getIssue(number)).data;
 
@@ -80,7 +80,55 @@ async function mandatoryChanges(number) {
     fixContainsTest(issue, files);
 
     console.log("✅  Success")
-        
+
+}
+
+async function commitMessage(number) {
+
+    if (!number) {
+        throw new Error('Must provide a PR number')
+    }
+
+    const gh = createGitHubClient();
+
+    const issues = gh.getIssues(OWNER, REPO);
+    const repo = gh.getRepo(OWNER, REPO);
+
+    console.log(`⌛  Fetching PR number ${number}`)
+    const issue = (await issues.getIssue(number)).data;
+
+    const commitMessageSection = issue.body.match(/## Commit Message([\s|\S]*)## End Commit Message/);
+
+    if (!commitMessageSection) {
+        throw new LinterError("Your PR description doesn't specify the commit message properly. See for details.")
+    }
+
+    const commitMessage = commitMessageSection[1];
+
+    const paragraphs = commitMessage.split(/\r?\n/).filter(function (l) {
+        return l.length > 0;
+    });
+
+    const title = paragraphs[0];
+
+    console.log("⌛  Validating...");
+
+    // the title of the commit should be equal (kind of) to the title of the PR.
+    // so that the final commit on master will conform to conventional commits.
+    if (!title.startsWith(issue.title)) {
+        throw new LinterError("First line of '## Commit Message' section"
+            + ` must start with the PR title: '${issue.title}'`)
+    }
+
+    // if the commit contains breaking changes, make sure its the last paragraph so
+    // that it is correctly generated in the changelog
+    for (i = 0; i < paragraphs.length; i++) {
+        if (i != paragraphs.length - 1 && paragraphs[i].startsWith("BREAKING CHANGE:")) {
+            throw new LinterError("'BREAKING CHANGE:' must be specified as the last paragraph");
+        }
+    }
+
+    console.log("✅  Success")
 }
 
 // we don't use the 'export' prefix because github actions
@@ -88,6 +136,7 @@ async function mandatoryChanges(number) {
 // TODO need to verify this.
 module.exports.mandatoryChanges = mandatoryChanges
 module.exports.LinterError = LinterError
+module.exports.commitMessage = commitMessage
 
 require('make-runnable/custom')({
     printOutputFrame: false
