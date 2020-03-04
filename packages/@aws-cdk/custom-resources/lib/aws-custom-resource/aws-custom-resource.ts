96671baac998577a9db47dcf6fa1838580aeb30f
epolon <epolon@amazon.com>
2020-03-04 16:32:00 +0200

    not sure who this happened

--- packages/@aws-cdk/custom-resources/lib/aws-custom-resource/aws-custom-resource.ts
+++ packages/@aws-cdk/custom-resources/lib/aws-custom-resource/aws-custom-resource.ts
@@ -17,6 +17,32 @@ export type AwsSdkMetadata = {[key: string]: any};
 const awsSdkMetadata: AwsSdkMetadata = metadata;
 
 /**
+ * Physical ID of the custom resource.
+ */
+export class PhysicalResourceId {
+
+  /**
+   * Extract the physical resource id from the path (dot notation) to the data in the API call response.
+   */
+  public static fromResponse(responsePath: string): PhysicalResourceId {
+    return new PhysicalResourceId(responsePath, undefined);
+  }
+
+  /**
+   * Explicit physical resource id.
+   */
+  public static of(id: string): PhysicalResourceId {
+    return new PhysicalResourceId(undefined, id);
+  }
+
+  /**
+   * @param responsePath Path to a response data element to be used as the physical id.
+   * @param id Literal string to be used as the physical id.
+   */
+  private constructor(public readonly responsePath?: string, public readonly id?: string) { }
+}
+
+/**
  * An AWS SDK call.
  */
 export interface AwsSdkCall {
@@ -43,22 +69,12 @@ export interface AwsSdkCall {
   readonly parameters?: any;
 
   /**
-   * The path to the data in the API call response to use as the physical
-   * resource id. Either `physicalResourceId` or `physicalResourceIdPath`
-   * must be specified for onCreate or onUpdate calls.
-   *
-   * @default - no path
-   */
-  readonly physicalResourceIdPath?: string;
-
-  /**
-   * The physical resource id of the custom resource for this call. Either
-   * `physicalResourceId` or `physicalResourceIdPath` must be specified for
-   * onCreate or onUpdate calls.
+   * The physical resource id of the custom resource for this call.
+   * Mandatory for onCreate or onUpdate calls.
    *
    * @default - no physical resource id
    */
-  readonly physicalResourceId?: string;
+  readonly physicalResourceId?: PhysicalResourceId;
 
   /**
    * The regex pattern to use to catch API errors. The `code` property of the
@@ -67,7 +83,7 @@ export interface AwsSdkCall {
    *
    * @default - do not catch errors
    */
-  readonly catchErrorPattern?: string;
+  readonly ignoreErrorCodesMatching?: string;
 
   /**
    * API version to use for the service
@@ -97,6 +113,61 @@ export interface AwsSdkCall {
    * @default - return all data
    */
   readonly outputPath?: string;
+}
+
+/**
+ * Options for the auto-generation of policies based on the configured SDK calls.
+ */
+export interface SdkCallsPolicyOptions {
+
+  /**
+   * The resources that the calls will have access to.
+   *
+   * It is best to use specific resource ARN's when possible. However, you can also use `AwsCustomResourcePolicy.ANY_RESOURCE`
+   * to allow access to all resources. For example, when `onCreate` is used to create a resource which you don't
+   * know the physical name of in advance.
+   *
+   * Note that will apply to ALL SDK calls.
+   */
+  readonly resources: string[]
+
+}
+
+/**
+ * The IAM Policy that will be applied to the different calls.
+ */
+export class AwsCustomResourcePolicy {
+
+  /**
+   * Use this constant to configure access to any resource.
+   */
+  public static readonly ANY_RESOURCE = ['*'];
+
+  /**
+   * Explicit IAM Policy Statements.
+   *
+   * @param statements the statements to propagate to the SDK calls.
+   */
+  public static fromStatements(statements: iam.PolicyStatement[]) {
+    return new AwsCustomResourcePolicy(statements, undefined);
+  }
+
+  /**
+   * Generate IAM Policy Statements from the configured SDK calls.
+   *
+   * Each SDK call with be translated to an IAM Policy Statement in the form of: `call.service:call.action` (e.g `s3:PutObject`).
+   *
+   * @param options options for the policy generation
+   */
+  public static fromSdkCalls(options: SdkCallsPolicyOptions) {
+    return new AwsCustomResourcePolicy([], options.resources);
+  }
+
+  /**
+   * @param statements statements for explicit policy.
+   * @param resources resources for auto-generated from SDK calls.
+   */
+  private constructor(public readonly statements: iam.PolicyStatement[], public readonly resources?: string[]) {}
 }
 
 /**
@@ -134,8 +205,7 @@ export interface AwsCustomResourceProps {
   readonly onDelete?: AwsSdkCall;
 
   /**
-   * The IAM policy statements to allow the different calls. Use only if
-   * resource restriction is needed.
+   * The policy to apply to the resource.
    *
    * The custom resource also implements `iam.IGrantable`, making it possible
    * to use the `grantXxx()` methods.
@@ -144,9 +214,10 @@ export interface AwsCustomResourceProps {
    * to note the that function's role will eventually accumulate the
    * permissions/grants from all resources.
    *
-   * @default - extract the permissions from the calls
+   * @see Policy.fromStatements
+   * @see Policy.fromSdkCalls
    */
-  readonly policyStatements?: iam.PolicyStatement[];
+  readonly policy: AwsCustomResourcePolicy;
 
   /**
    * The execution role for the Lambda function implementing this custom
@@ -174,9 +245,21 @@ export interface AwsCustomResourceProps {
  *
  */
 export class AwsCustomResource extends cdk.Construct implements iam.IGrantable {
+
+  private static breakIgnoreErrorsCircuit(sdkCalls: Array<AwsSdkCall | undefined>, caller: string) {
+
+    for (const call of sdkCalls) {
+      if (call?.ignoreErrorCodesMatching) {
+        throw new Error(`\`${caller}\`` + ' cannot be called along with `ignoreErrorCodesMatching`.');
+      }
+    }
+
+  }
+
   public readonly grantPrincipal: iam.IPrincipal;
 
   private readonly customResource: CustomResource;
+  private readonly props: AwsCustomResourceProps;
 
   // 'props' cannot be optional, even though all its properties are optional.
   // this is because at least one sdk call must be provided.
@@ -188,10 +271,18 @@ export class AwsCustomResource extends cdk.Construct implements iam.IGrantable {
     }
 
     for (const call of [props.onCreate, props.onUpdate]) {
-      if (call && !call.physicalResourceId && !call.physicalResourceIdPath) {
-        throw new Error('Either `physicalResourceId` or `physicalResourceIdPath` must be specified for onCreate and onUpdate calls.');
+      if (call && !call.physicalResourceId) {
+        throw new Error('`physicalResourceId` must be specified for onCreate and onUpdate calls.');
       }
     }
+
+    for (const call of [props.onCreate, props.onUpdate, props.onDelete]) {
+      if (call?.physicalResourceId?.responsePath) {
+        AwsCustomResource.breakIgnoreErrorsCircuit([call], "PhysicalResourceId.fromResponse");
+      }
+    }
+
+    this.props = props;
 
     const provider = new lambda.SingletonFunction(this, 'Provider', {
       code: lambda.Code.fromAsset(path.join(__dirname, 'runtime')),
@@ -204,19 +295,22 @@ export class AwsCustomResource extends cdk.Construct implements iam.IGrantable {
     });
     this.grantPrincipal = provider.grantPrincipal;
 
-    if (props.policyStatements) {
-      for (const statement of props.policyStatements) {
+    if (props.policy.statements.length !== 0) {
+      // Use custom statements provided by the user
+      for (const statement of props.policy.statements) {
         provider.addToRolePolicy(statement);
       }
-    } else { // Derive statements from AWS SDK calls
+    } else {
+      // Derive statements from AWS SDK calls
       for (const call of [props.onCreate, props.onUpdate, props.onDelete]) {
         if (call) {
           provider.addToRolePolicy(new iam.PolicyStatement({
             actions: [awsSdkToIamAction(call.service, call.action)],
-            resources: ['*']
+            resources: props.policy.resources
           }));
         }
       }
+
     }
 
     const create = props.onCreate || props.onUpdate;
@@ -239,9 +333,14 @@ export class AwsCustomResource extends cdk.Construct implements iam.IGrantable {
    * Use `Token.asXxx` to encode the returned `Reference` as a specific type or
    * use the convenience `getDataString` for string attributes.
    *
+   * Note that you cannot use this method if `ignoreErrorCodesMatching`
+   * is configured for any of the SDK calls. This is because in such a case,
+   * the response data might not exist, and will cause a CloudFormation deploy time error.
+   *
    * @param dataPath the path to the data
    */
   public getData(dataPath: string) {
+    AwsCustomResource.breakIgnoreErrorsCircuit([this.props.onCreate, this.props.onUpdate], "getData");
     return this.customResource.getAtt(dataPath);
   }
 
@@ -250,11 +349,17 @@ export class AwsCustomResource extends cdk.Construct implements iam.IGrantable {
    *
    * Example for S3 / listBucket : 'Buckets.0.Name'
    *
+   * Note that you cannot use this method if `ignoreErrorCodesMatching`
+   * is configured for any of the SDK calls. This is because in such a case,
+   * the response data might not exist, and will cause a CloudFormation deploy time error.
+   *
    * @param dataPath the path to the data
    */
   public getDataString(dataPath: string): string {
+    AwsCustomResource.breakIgnoreErrorsCircuit([this.props.onCreate, this.props.onUpdate], "getDataString");
     return this.customResource.getAttString(dataPath);
   }
+
 }
 
 /**
