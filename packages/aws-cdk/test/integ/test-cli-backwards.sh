94c623d9517315a54960338e65a1fbb27b0e90dd
epolon <epolon@amazon.com>
2020-03-14 19:45:08 +0200

    use GITHUB_TOKEN if possible when making api calls to GitHub

--- packages/aws-cdk/test/integ/test-cli-backwards.sh
+++ packages/aws-cdk/test/integ/test-cli-backwards.sh
@@ -11,12 +11,23 @@ function cleanup {
     popd
 }
 
+function download_latest_release_json {
+
+    github_headers=""
+    if [[ "${GITHUB_TOKEN:-}" != "" ]]; then
+        github_headers="Authorization: token ${GITHUB_TOKEN}"
+    fi
+
+    curl -Ss --dump-header headers.txt -H "$github_headers" https://api.github.com/repos/aws/aws-cdk/releases/latest > release.json
+}
+
 trap cleanup INT EXIT
 
-curl -Ss https://api.github.com/repos/aws/aws-cdk/releases/latest > release.json
+download_latest_release_json
 latest_version=$(node -p "require('./release.json').name")
 
-echo "Checking out latest version"
+echo "Checking out integration tests from version ${latest_version}"
 pushd ${repo_root} git checkout ${latest_version} -- test/integ/cli && popd
 
+echo "Running integration tests from version ${latest_version}"
 # ${integdir}/cli/test.sh
