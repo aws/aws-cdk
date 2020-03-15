5776c5ef02e73b58c6f71b44e60b39daffe292b9
epolon <epolon@amazon.com>
2020-03-15 15:55:14 +0200

    futile attempt to make an ugly method prettier + docstring

--- packages/@aws-cdk/cx-protocol/lib/protocol.ts
+++ packages/@aws-cdk/cx-protocol/lib/protocol.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import { ArtifactMetadataEntryType, ArtifactType, AssemblyManifest, Tag } from './assembly-manifest';
+import { ArtifactMetadataEntryType, ArtifactType, AssemblyManifest } from './assembly-manifest';
 
 /**
  * Protocol utility class.
@@ -24,21 +24,30 @@ export class Manifest {
         return manifest;
     }
 
+    /**
+     * This requires some explaining...
+     *
+     * We previously used `{ Key, Value }` for the object that represents a stack tag. (Notice the casing)
+     * @link https://github.com/aws/aws-cdk/blob/v1.27.0/packages/aws-cdk/lib/api/cxapp/stacks.ts#L427.
+     *
+     * When that object moved to this package, it had to be JSII compliant, which meant the property
+     * names must be `camelCased`, and not `PascalCased`. This meant it no longer matches the structure in the `manifest.json` file.
+     * In order to support current manifest files, we have to translate the `PascalCased` representation to the new `camelCased` one.
+     *
+     * Note that the serialization itself still writes `PascalCased` because it relates to how CloudFormation expects it.
+     *
+     * Ideally, we would start writing the `camelCased` and translate to how CloudFormation expects it when needed. But this requires nasty
+     * backwards-compatibility code and it just doesn't seem to be worth the effort.
+     */
     private static patchStackTags(manifest: AssemblyManifest) {
-        if (manifest.artifacts) {
-            for (const artifact of Object.values(manifest.artifacts)) {
-                if (artifact.type === ArtifactType.AWS_CLOUDFORMATION_STACK) {
-                    if (artifact.metadata) {
-                        for (const metaentry of Object.values(artifact.metadata)) {
-                            for (const metadata of metaentry) {
-                                if (metadata.type === ArtifactMetadataEntryType.STACK_TAGS && metadata.data) {
-                                    const fixedTags = new Array<Tag>();
-                                    for (const tag of metadata.data as any[]) {
-                                        fixedTags.push({ key: tag.Key, value: tag.Value });
-                                    }
-                                    Object.assign(metadata.data, fixedTags);
-                                }
-                            }
+        for (const artifact of Object.values(manifest.artifacts || [])) {
+            if (artifact.type === ArtifactType.AWS_CLOUDFORMATION_STACK) {
+                for (const metadataEntries of Object.values(artifact.metadata || [])) {
+                    for (const metadataEntry of metadataEntries) {
+                        if (metadataEntry.type === ArtifactMetadataEntryType.STACK_TAGS && metadataEntry.data) {
+                            Object.assign(metadataEntry.data, (metadataEntry.data as any[]).map(t => {
+                                return { key: t.Key, value: t.Value };
+                             }));
                         }
                     }
                 }
