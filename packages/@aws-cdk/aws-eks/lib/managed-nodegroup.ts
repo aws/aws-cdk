3075be08d773553db5031abf6dd31123f984b978
Pahud Hsieh <pahudnet@gmail.com>
2020-03-18 07:25:28 +0800

    Update packages/@aws-cdk/aws-eks/lib/managed-nodegroup.ts
    Co-Authored-By: Elad Ben-Israel <benisrae@amazon.com>

--- packages/@aws-cdk/aws-eks/lib/managed-nodegroup.ts
+++ packages/@aws-cdk/aws-eks/lib/managed-nodegroup.ts
@@ -49,7 +49,7 @@ export interface RemoteAccess {
   /**
    * The Amazon EC2 SSH key that provides access for SSH communication with the worker nodes in the managed node group.
    */
-  readonly ec2SshKey: string,
+  readonly ec2SshKey: string;
   /**
    * The security groups that are allowed SSH access (port 22) to the worker nodes. If you specify an Amazon EC2 SSH key but do not
    * specify a source security group when you create a managed node group, then port 22 on the worker nodes is opened to the internet (0.0.0.0/0).
