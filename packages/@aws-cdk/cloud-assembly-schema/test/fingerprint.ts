ed67e327c672d551d80496a2cb189cece8aa11bc
epolon <epolon@amazon.com>
2020-03-19 23:06:47 +0200

    fix version

--- packages/@aws-cdk/cloud-assembly-schema/test/fingerprint.ts
+++ packages/@aws-cdk/cloud-assembly-schema/test/fingerprint.ts
@@ -2,15 +2,15 @@ import * as crypto from 'crypto';
 
 export function hashObject(obj: any) {
 
-    function filterDocStrings(name: string, value: string) {
-      if (name) {
-        return value;
-      }
-      return "";
-    }
+    // function filterDocStrings(name: string, value: string) {
+    //   if (name) {
+    //     return value;
+    //   }
+    //   return "";
+    // }
 
     const hash = crypto.createHash('sha256');
-    hash.update(JSON.stringify(obj, filterDocStrings));
+    hash.update(JSON.stringify(obj));
     return `${hash.digest('hex')}`;
 
   }
