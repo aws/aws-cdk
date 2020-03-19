8dd6dbee6d90a22c65ff9fc7ebaa5c658db2a772
epolon <epolon@amazon.com>
2020-03-19 23:14:21 +0200

    filter out description keys in schema

--- packages/@aws-cdk/cloud-assembly-schema/test/fingerprint.ts
+++ packages/@aws-cdk/cloud-assembly-schema/test/fingerprint.ts
@@ -2,15 +2,15 @@ import * as crypto from 'crypto';
 
 export function hashObject(obj: any) {
 
-    // function filterDocStrings(name: string, value: string) {
-    //   if (name) {
-    //     return value;
-    //   }
-    //   return "";
-    // }
+    function filterDocStrings(name: string, value: any) {
+      if (name === 'description' && typeof(value) === 'string') {
+        return "";
+      }
+      return value;
+    }
 
     const hash = crypto.createHash('sha256');
-    hash.update(JSON.stringify(obj));
+    hash.update(JSON.stringify(obj, filterDocStrings));
     return `${hash.digest('hex')}`;
 
   }
