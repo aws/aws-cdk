6e23783a47a04c9b62c3521c05e36677953f0ffa
epolon <epolon@amazon.com>
2020-03-19 22:37:58 +0200

    refactor

--- packages/@aws-cdk/cloud-assembly-schema/scripts/bump-schema-version.js
+++ packages/@aws-cdk/cloud-assembly-schema/scripts/bump-schema-version.js
@@ -1,18 +1,21 @@
-/* eslint-disable */
 const jsonpatch = require('fast-json-patch');
 const semver = require('semver');
 const fs = require('fs');
 const path = require('path');
 const fingerprint = require('../test/fingerprint');
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-var schema = require('../schema/cloud-assembly.schema.json');
+const versionPath = '../schema/cloud-assembly.version.json';
+const expectedPath = '../test/schema.expected.json';
+const schemaPath = '../schema/cloud-assembly.schema.json';
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-var version = require('../schema/cloud-assembly.version.json');
+var schema = require(schemaPath);
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-var expected = require('../test/schema.expected.json');
+var version = require(versionPath);
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+var expected = require(expectedPath);
 
 function applyPatch(document, patch, out) {
     const patched = jsonpatch.applyPatch(document, patch).newDocument;
@@ -26,10 +29,10 @@ if (currentHash != expectedHash) {
 
     applyPatch(version,
         [{ op:"replace", path: "/version", value: semver.inc(version.version, 'major') }],
-        path.join(__dirname, '../schema/cloud-assembly.version.json'))
+        path.join(__dirname, versionPath))
 
     applyPatch(expected,
         [{ op:"replace", path: "/hash", value: currentHash }],
-        path.join(__dirname, '../test/schema.expected.json'))
+        path.join(__dirname, expectedPath))
 
 }
