c3d780053da2ae96a97bd2a32aae4cdc110f4854
Niranjan Jayakar <nija@amazon.com>
2020-03-20 10:30:08 +0000

    chore(apigateway): improve error msg when authorizer has no attached RestApi
    closes #6798

--- packages/@aws-cdk/aws-apigateway/test/authorizers/test.lambda.ts
+++ packages/@aws-cdk/aws-apigateway/test/authorizers/test.lambda.ts
@@ -267,5 +267,22 @@ export = {
     expect(stack).notTo(haveResource('AWS::Lambda::Permission'));
 
     test.done();
-  }
+  },
+
+  'request authorizer throws when not attached to a rest api'(test: Test) {
+    const stack = new Stack();
+    const func = new lambda.Function(stack, 'myfunction', {
+      handler: 'handler',
+      code: lambda.Code.fromInline('foo'),
+      runtime: lambda.Runtime.NODEJS_12_X,
+    });
+    const auth = new RequestAuthorizer(stack, 'myauthorizer', {
+      handler: func,
+      identitySources: [ IdentitySource.header('myheader') ],
+    });
+
+    test.throws(() => stack.resolve(auth.authorizerArn), /must be attached to a RestApi/);
+
+    test.done();
+  },
 };
