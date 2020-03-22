a73810af429ebfdf7695717a8b2ebe2722a352d6
epolon <epolon@amazon.com>
2020-03-22 13:02:54 +0200

    mid work

--- packages/aws-cdk/test/integ/cli/test.sh
+++ packages/aws-cdk/test/integ/cli/test.sh
@@ -7,9 +7,26 @@ header CLI Integration Tests
 
 prepare_fixture
 
+VERSION_UNDER_TEST=${VERSION_UNDER_TEST:-1.30.0}
+
+function get_skip_jusitification {
+  test=$1
+  version=$2
+  echo $(node -p "require('../cli.exclusions.js').forTest(${test}, ${VERSION_UNDER_TEST}).justification")
+}
+
 for test in $(cd ${scriptdir} && ls test-*.sh); do
   echo "============================================================================================"
+
+  skip_jusitification=$(get_skip_jusitification ${test})
+
+  if [ ${skip_jusitification} != "undefined" ]; then
+    echo ${test} - skipped (${skip_jusitification})
+    continue
+  fi
+
   echo "${test}"
   echo "============================================================================================"
-  /bin/bash ${scriptdir}/${test}
+  # /bin/bash ${scriptdir}/${test}
 done
+
