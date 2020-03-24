29bac260e9784c38db7d29f57da9e2ca3f054e4e
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-24 15:31:37 -0700

    fix(aws-ecs-patterns): revert commit f31f4e1

--- packages/@aws-cdk/aws-ecs-patterns/lib/base/application-load-balanced-service-base.ts
+++ packages/@aws-cdk/aws-ecs-patterns/lib/base/application-load-balanced-service-base.ts
@@ -351,15 +351,13 @@ export abstract class ApplicationLoadBalancedServiceBase extends cdk.Construct {
         throw new Error('A Route53 hosted domain zone name is required to configure the specified domain name');
       }
 
-      if (internetFacing) {
-        const record = new ARecord(this, "DNS", {
-          zone: props.domainZone,
-          recordName: props.domainName,
-          target: RecordTarget.fromAlias(new LoadBalancerTarget(loadBalancer)),
-        });
+      const record = new ARecord(this, "DNS", {
+        zone: props.domainZone,
+        recordName: props.domainName,
+        target: RecordTarget.fromAlias(new LoadBalancerTarget(loadBalancer)),
+      });
 
-        domainName = record.domainName;
-      }
+      domainName = record.domainName;
     }
 
     if (loadBalancer instanceof ApplicationLoadBalancer) {
