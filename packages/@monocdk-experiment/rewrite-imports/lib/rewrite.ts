817043c846c4e920aa72be0364a93bf6ce65ef4b
Elad Ben-Israel <benisrae@amazon.com>
2020-03-25 15:52:23 +0200

    chore(monocdk): export "core" types without a namespace
    Move all the types from "@aws-cdk/core" to the root namespace of `monocdk-experiment`.
    
    Before:
    
        import { Construct } from 'monocdk-experiment/core';
        // or
        import { core } from 'monocdk-experiment';
        const Construct = core.Construct;
    
    After:
    
        import { Construct } from 'monocdk-experiment';
    
    
    Update the @monocdk-experiment/rewrite-imports tool to use the new "barrel imports" and respect the fact that core types are non-namespace.
    
    Update @monocdk-experiment/assert accordingly.

---
+++ packages/@monocdk-experiment/rewrite-imports/lib/rewrite.ts
@@ -0,0 +1,23 @@
+const exclude = [
+  '@aws-cdk/cloudformation-diff',
+  '@aws-cdk/assert'
+];
+
+export function rewriteFile(source: string) {
+  const output = new Array<string>();
+  for (const line of source.split('\n')) {
+    output.push(rewriteLine(line));
+  }
+  return output.join('\n');
+}
+
+export function rewriteLine(line: string) {
+  for (const skip of exclude) {
+    if (line.includes(skip)) {
+      return line;
+    }
+  }
+  return line
+    .replace(/(["']?)@aws-cdk\/core(["']?)/g, '$1monocdk-experiment$2')        // monocdk-experiment => monocdk-experiment
+    .replace(/(["']?)@aws-cdk\/(.+?)(["']?)/g, '$1monocdk-experiment/$2$3');  // monocdk-experiment/foobar => monocdk-experiment/foobar;
+}
