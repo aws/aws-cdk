5c597bea2a3adf17e48258b0353c2c7c2dda489c
Elad Ben-Israel <benisrae@amazon.com>
2020-03-25 21:31:57 +0200

    chore(monocdk): export "core" types without a namespace (#7007)
    Move all the types from "@aws-cdk/core" to the root namespace of `monocdk-experiment`.
    
    Before:
    
        import { Construct } from 'monocdk-experiment/core';
        // or
        import { core } from 'monocdk-experiment';
        const Construct = core.Construct;
    
    After:
    
        import { Construct } from 'monocdk-experiment';
    
    
    Update the @monocdk-experiment/rewrite-imports tool to use the new "barrel imports" and respect the fact that core types are non-namespace.
    
    Update @monocdk-experiment/assert accordingly.

--- packages/@monocdk-experiment/assert/clone.sh
+++ packages/@monocdk-experiment/assert/clone.sh
@@ -13,7 +13,7 @@ for file in ${files}; do
   cp $src/$file .
 done
 
-npx rewrite-imports {lib,test}/*.ts jest.ts
+npx rewrite-imports **/*.ts
 
 # symlink the full staged monocdk from the staging directory to node_modules
 rm -fr node_modules/monocdk-experiment
