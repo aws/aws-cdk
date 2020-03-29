fecba547e6e09745f2a33c1be57ee22bc7ed10c8
epolon <epolon@amazon.com>
2020-03-29 21:21:03 +0300

    revert align script

--- scripts/align-version.sh
+++ scripts/align-version.sh
@@ -12,20 +12,9 @@ cd ${scriptdir}/..
 files="$(find . -name package.json | grep -v node_modules | xargs)"
 ${scriptdir}/align-version.js ${files}
 
-CLOUD_ASSEMBLY_PACKAGE=packages/@aws-cdk/cloud-assembly-schema
-CLOUD_ASSEMBLY_VERSION_FILE=${CLOUD_ASSEMBLY_PACKAGE}/schema/cloud-assembly.version.json
-pushd ${CLOUD_ASSEMBLY_PACKAGE}
-yarn run align-schema-version
-popd
-
 # validation
 marker=$(node -p "require('./scripts/get-version-marker')")
 if find . -name package.json | grep -v node_modules | xargs grep "[^0-9]${marker}"; then
   echo "ERROR: unexpected version marker ${marker} in a package.json file"
-  exit 1
-fi
-
-if grep "[^0-9]${marker}" -v ${CLOUD_ASSEMBLY_VERSION_FILE} | xargs grep "[^0-9]${marker}"; then
-  echo "ERROR: unexpected version marker ${marker} in a ${CLOUD_ASSEMBLY_VERSION_FILE} file"
   exit 1
 fi
