1685b0433e88ef145012f061edc0688a5bf351ac
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2020-03-30 17:24:21 +0200

    feat(backup): Vault, Plan and Selection
    Add L2 constructs for AWS Backup: BackupVault, BackupPlan and BackupSelection.
    
    Ready-made plans and rules are provided.
    
    All backupable resources inside a Construct can be easily added to a selection.
    
    Credits to @arnulfojr for starting the work on this in #4954.

--- packages/@aws-cdk/aws-backup/README.md
+++ packages/@aws-cdk/aws-backup/README.md
@@ -17,8 +17,73 @@
 ---
 <!--END STABILITY BANNER-->
 
-This module is part of the [AWS Cloud Development Kit](https://github.com/aws/aws-cdk) project.
+AWS Backup is a fully managed backup service that makes it easy to centralize and automate the backup of data across AWS services in the cloud and on premises. Using AWS Backup, you can configure backup policies and monitor backup activity for your AWS resources in one place.
+
+### Backup plan and selection
+
+In AWS Backup, a *backup plan* is a policy expression that defines when and how you want to back up your AWS resources, such as Amazon DynamoDB tables or Amazon Elastic File System (Amazon EFS) file systems. You can assign resources to backup plans, and AWS Backup automatically backs up and retains backups for those resources according to the backup plan. You can create multiple backup plans if you have workloads with different backup requirements.
+
+This module provides ready-made backup plans (similar to the console exprience):
 
 ```ts
-import backup = require('@aws-cdk/aws-backup');
+import * as backup from '@aws-cdk/aws-backup';
+
+// Daily, weekly and monthly with 5 year retention
+const plan = backup.BackupPlan.dailyWeeklyMonthly5YearRetention(this, 'Plan');
 ```
+
+Assigning resources to a plan can be done with `addSelection()`:
+
+```ts
+plan.addSelection('Selection', {
+  resources: [
+    backup.BackupResource.fromDynamoDbTable(myTable), // A DynamoDB table
+    backup.BackupResource.fromTag('stage', 'prod'), // All resources that are tagged stage=prod in the region/account
+    backup.BackupResource.fromConstruct(myCoolConstruct), // All backupable resources in `myCoolConstruct`
+  ]
+})
+```
+If not specified, a new IAM role with managed policies for backup and restore
+will be created for the selection. The `BackupSelection` implements `IGrantable`.
+
+To add rules to a plan, use `addRule()`:
+
+```ts
+plan.addRule(new BackupPlanRule({
+  completionWindow: Duration.hours(2),
+  startWindow: Duration.hours(1),
+  scheduleExpression: events.Schedule.cron({ // Only cron expressions are supported
+    day: '15',
+    hour: '3',
+    minute: '30'
+  }),
+  moveToColdStorageAfter: Duration.days(30)
+}));
+```
+
+Ready-made rules are also available:
+
+```ts
+plan.addRule(BackupPlanRule.daily());
+plan.addRule(BackupPlanRule.weekly());
+```
+
+By default a new [vault](#Backup-vault) is created when creating a plan.
+It is also possible specify a vault either at the plan level or at the
+rule level.
+
+```ts
+const plan = backup.BackupPlan.daily35DayRetention(this, 'Plan', myVault); // Use `myVault` for all plan rules
+plan.addRule(BackupPlanRule.monthly1Year(otherVault)); // Use `otherVault` for this specific rule
+```
+
+### Backup vault
+In AWS Backup, a *backup vault* is a container that you organize your backups in. You can use backup vaults to set the AWS Key Management Service (AWS KMS) encryption key that is used to encrypt backups in the backup vault and to control access to the backups in the backup vault. If you require different encryption keys or access policies for different groups of backups, you can optionally create multiple backup vaults.
+
+```ts
+const vault = new BackupVault(stack, 'Vault', {
+  encryptionKey: myKey, // Custom encryption key
+  notificationTopic: myTopic, // Send all vault events to this SNS topic
+});
+```
+A vault has a default `RemovalPolicy` set to `RETAIN`.
---
+++ packages/@aws-cdk/aws-backup/lib/selection.ts
@@ -0,0 +1,135 @@
+import * as iam from '@aws-cdk/aws-iam';
+import { Construct, Lazy, Resource } from '@aws-cdk/core';
+import { CfnBackupSelection } from './backup.generated';
+import { BackupableResourcesCollector } from './backupable-resources-collector';
+import { IBackupPlan } from './plan';
+import { BackupResource, TagOperation } from './resource';
+
+/**
+ * Options for a BackupSelection
+ */
+export interface BackupSelectionOptions {
+  /**
+   * The resources to backup.
+   * Use the helper static methods defined on `BackupResource`.
+   */
+  readonly resources: BackupResource[];
+
+  /**
+   * The name for this selection
+   *
+   * @default - a CDK generated name
+   */
+  readonly backupSelectionName?: string;
+
+  /**
+   * The role that AWS Backup uses to authenticate when restoring the target
+   * resource
+   *
+   * @default - a new role will be created
+   */
+  readonly role?: iam.IRole;
+}
+
+/**
+ * Properties for a BackupSelection
+ */
+export interface BackupSelectionProps extends BackupSelectionOptions {
+  /**
+   * The backup plan for this selection
+   */
+  readonly backupPlan: IBackupPlan;
+}
+
+/**
+ * A backup selection
+ */
+export class BackupSelection extends Resource implements iam.IGrantable {
+  /**
+   * The identifier of the backup plan.
+   *
+   * @attribute
+   */
+  public readonly backupPlanId: string;
+
+  /**
+   * The identifier of the backup selection.
+   *
+   * @attribute
+   */
+  public readonly selectionId: string;
+
+  /**
+   * The principal to grant permissions to
+   */
+  public readonly grantPrincipal: iam.IPrincipal;
+
+  constructor(scope: Construct, id: string, props: BackupSelectionProps) {
+    super(scope, id);
+
+    const role = props.role || new iam.Role(this, 'Role', {
+      assumedBy: new iam.ServicePrincipal('backup.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSBackupServiceRolePolicyForBackup'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSBackupServiceRolePolicyForRestores'),
+      ]
+    });
+    this.grantPrincipal = role;
+
+    const selection = new CfnBackupSelection(this, 'Resource', {
+      backupPlanId: props.backupPlan.backupPlanId,
+      backupSelection: {
+        iamRoleArn: role.roleArn,
+        selectionName: props.backupSelectionName || this.node.id,
+        listOfTags: formatListOfTags(props.resources),
+        resources: formatResources(props.resources),
+      }
+    });
+
+    this.backupPlanId = selection.attrBackupPlanId;
+    this.selectionId = selection.attrSelectionId;
+  }
+}
+
+function formatListOfTags(backupResources: BackupResource[]): CfnBackupSelection.ConditionResourceTypeProperty[] | undefined {
+  const listOfTags: CfnBackupSelection.ConditionResourceTypeProperty[] = [];
+
+  for (const resource of backupResources) {
+    if (resource.tagCondition) {
+      listOfTags.push({
+        conditionKey: resource.tagCondition.key,
+        conditionType: resource.tagCondition.operation || TagOperation.STRING_EQUALS,
+        conditionValue: resource.tagCondition.value,
+      });
+    }
+  }
+
+  return listOfTags.length !== 0 ? listOfTags : undefined;
+}
+
+function formatResources(backupResources: BackupResource[]): string[] {
+  const _resources: string[] = [];
+  const aspects: BackupableResourcesCollector[] = [];
+
+  // Create all aspects first and then loop over them because we cannot
+  // add items to a token list
+  for (const resource of backupResources) {
+    if (resource.resource) {
+      _resources.push(resource.resource);
+    }
+
+    if (resource.construct) {
+      const backupResourcesCollector = new BackupableResourcesCollector();
+      resource.construct.node.applyAspect(backupResourcesCollector);
+      aspects.push(backupResourcesCollector);
+    }
+  }
+
+  return Lazy.listValue({ produce: () => {
+    const resources: string[] = [..._resources];
+    for (const aspect of aspects) {
+      resources.push(...aspect.resources);
+    }
+    return resources;
+  }});
+}
