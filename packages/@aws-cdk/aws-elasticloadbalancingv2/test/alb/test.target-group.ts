43ed6a40a217a39b8a7e8d9669e1026516b041a2
flemjame-at-amazon <57235867+flemjame-at-amazon@users.noreply.github.com>
2020-03-31 12:04:04 -0400

    feat(elbv2): health checks for Lambda targets (#7023)
    * Initial commit without addition of tests
    
    * Add tests
    
    * Need to add port
    
    * Fix test
    
    * Add note to README
    
    Co-authored-by: Rico Huijbers <rix0rrr@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/@aws-cdk/aws-elasticloadbalancingv2/test/alb/test.target-group.ts
+++ packages/@aws-cdk/aws-elasticloadbalancingv2/test/alb/test.target-group.ts
@@ -1,3 +1,4 @@
+import { expect, haveResource } from '@aws-cdk/assert';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
 import { Test } from 'nodeunit';
@@ -49,6 +50,51 @@ export = {
     test.throws(() => {
       tg.addTarget(new elbv2.InstanceTarget('i-1234'));
     }, /Cannot add a non-self registering target to an imported TargetGroup/);
+
+    test.done();
+  },
+
+  'HealthCheck fields set if provided'(test: Test) {
+    // GIVEN
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, 'Stack');
+    const vpc = new ec2.Vpc(stack, "VPC", {});
+    const alb = new elbv2.ApplicationLoadBalancer(stack, 'ALB', {vpc});
+    const listener = new elbv2.ApplicationListener(stack, "Listener", {
+      port: 80,
+      loadBalancer: alb,
+      open: false
+    });
+
+    // WHEN
+    const ipTarget = new elbv2.IpTarget("10.10.12.12");
+    listener.addTargets("TargetGroup", {
+      targets: [ipTarget],
+      port: 80,
+      healthCheck: {
+        enabled: true,
+        healthyHttpCodes: "255",
+        interval: cdk.Duration.seconds(255),
+        timeout: cdk.Duration.seconds(192),
+        healthyThresholdCount: 29,
+        unhealthyThresholdCount: 27,
+        path: "/arbitrary"
+      }
+    });
+
+    // THEN
+    expect(stack).to(haveResource('AWS::ElasticLoadBalancingV2::TargetGroup', {
+      HealthCheckEnabled : true,
+      HealthCheckIntervalSeconds : 255,
+      HealthCheckPath : "/arbitrary",
+      HealthCheckTimeoutSeconds : 192,
+      HealthyThresholdCount : 29,
+      Matcher : {
+        HttpCode : "255"
+      },
+      Port: 80,
+      UnhealthyThresholdCount : 27,
+      }));
 
     test.done();
   }
