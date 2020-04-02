aa3a214a06e5be688fe672c7dbb2f42cf2e270dc
Shiv Lakshminarayan <shivlaks@amazon.com>
2020-04-01 19:41:43 -0700

    chore: fix integration tests for cdk deploy with outputs

--- packages/aws-cdk/test/integ/cli/test-cdk-deploy-wildcard-with-outputs.sh
+++ packages/aws-cdk/test/integ/cli/test-cdk-deploy-wildcard-with-outputs.sh
@@ -6,8 +6,18 @@ source ${scriptdir}/common.bash
 
 setup
 
+# create directory in /tmp
+outputs_folder=${integ_test_dir}/outputs
+mkdir -p ${outputs_folder}
+
+# set up outputs
 outputs_file=${integ_test_dir}/outputs/outputs.json
+expected_file=${integ_test_dir}/outputs/expected.json
+touch $expected_file
+
+# add prefixes as stacks are keyed on their stack name in the outputs file
 expected_outputs=${scriptdir}/cdk-deploy-wildcard-with-outputs-expected.json
+sed "s|outputs|$STACK_NAME_PREFIX\-outputs|g" "$expected_outputs" > "$expected_file"
 
 # deploy all outputs stacks
 cdk deploy ${STACK_NAME_PREFIX}-outputs-test-\* --outputs-file ${outputs_file}
@@ -15,13 +25,13 @@ echo "Stacks deployed successfully"
 
 # verify generated outputs file
 generated_outputs_file="$(cat ${outputs_file})"
-expected_outputs_file="$(cat ${expected_outputs})"
+expected_outputs_file="$(cat ${expected_file})"
 if [[ "${generated_outputs_file}" != "${expected_outputs_file}" ]]; then
     fail "unexpected outputs. Expected: ${expected_outputs_file} Actual: ${generated_outputs_file}"
 fi 
 
 # destroy
-rm ${outputs_file}
+rm -rf ${outputs_folder}
 cdk destroy -f ${STACK_NAME_PREFIX}-outputs-test-1
 cdk destroy -f ${STACK_NAME_PREFIX}-outputs-test-2
 
--- packages/aws-cdk/test/integ/cli/test-cdk-deploy-with-outputs.sh
+++ packages/aws-cdk/test/integ/cli/test-cdk-deploy-with-outputs.sh
@@ -6,21 +6,31 @@ source ${scriptdir}/common.bash
 
 setup
 
-outputs_file=${integ_test_dir}/outputs/outputs.json
-expected_outputs=${scriptdir}/cdk-deploy-with-outputs-expected.json
+# create directory in /tmp
+outputs_folder=${integ_test_dir}/outputs
+mkdir -p ${outputs_folder}
 
-cdk deploy -v ${STACK_NAME_PREFIX}-outputs-test-1 --outputs-file ${outputs_file}
+# set up outputs
+outputs_file=${outputs_folder}/outputs.json
+expected_file=${outputs_folder}/expected.json
+touch $expected_file
+
+# add prefixes as stacks are keyed on their stack name in the outputs file
+expected_outputs=${scriptdir}/cdk-deploy-with-outputs-expected.json
+sed "s|outputs|$STACK_NAME_PREFIX\-outputs|g" "$expected_outputs" > "$expected_file"
+
+cdk deploy ${STACK_NAME_PREFIX}-outputs-test-1 --outputs-file ${outputs_file}
 echo "Stack deployed successfully"
 
 # verify generated outputs file
 generated_outputs_file="$(cat ${outputs_file})"
-expected_outputs_file="$(cat ${expected_outputs})"
+expected_outputs_file="$(cat ${expected_file})"
 if [[ "${generated_outputs_file}" != "${expected_outputs_file}" ]]; then
     fail "unexpected outputs. Expected: ${expected_outputs_file} Actual: ${generated_outputs_file}"
 fi 
 
 # destroy
-rm ${outputs_file}
+rm -rf ${outputs_folder}
 cdk destroy -f ${STACK_NAME_PREFIX}-outputs-test-1
 
 echo "✅  success"
