2f7394c225b74f94407b8fe4f22546d383375244
epolon <epolon@amazon.com>
2020-04-02 21:01:43 +0300

    fix path to scripts

--- packages/aws-cdk/scripts/integ-cli-regression.sh
+++ packages/aws-cdk/scripts/integ-cli-regression.sh
@@ -8,7 +8,7 @@ scriptdir=$(cd $(dirname $0) && pwd)
 repo_root=$(realpath ${scriptdir}/..)
 
 echo "Running regression tests against local code"
-${repo_root}/test/integ/run-against-repo test-cli-regression-against-current-code.sh
+${repo_root}/test/integ/run-against-repo ${repo_root}/test/integ/test-cli-regression-against-current-code.sh
 
 echo "Running regression tests against local CLI and published framework"
-${repo_root}/test/integ/run-against-repo test-cli-regression-against-latest-release.sh
+${repo_root}/test/integ/run-against-repo ${repo_root}/test/integ/test-cli-regression-against-latest-release.sh
