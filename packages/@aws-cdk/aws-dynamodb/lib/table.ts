8e7dc796f08113d980339a5a504f6af4423c64ca
jsstand <jsstand@amazon.com>
2020-04-05 00:42:21 -0700

    fix: Removed trailing whitespace

--- packages/@aws-cdk/aws-dynamodb/lib/table.ts
+++ packages/@aws-cdk/aws-dynamodb/lib/table.ts
@@ -100,9 +100,9 @@ export interface TableOptions {
   readonly serverSideEncryption?: boolean;
 
   /**
-   * The AWS KMS customer master key (CMK) that should be used for the AWS KMS encryption. 
-   * To specify a CMK, use its key ID, Amazon Resource Name (ARN), alias name, or alias ARN. 
-   * Note that you should only provide this parameter if the key is different from the default 
+   * The AWS KMS customer master key (CMK) that should be used for the AWS KMS encryption.
+   * To specify a CMK, use its key ID, Amazon Resource Name (ARN), alias name, or alias ARN.
+   * Note that you should only provide this parameter if the key is different from the default
    * DynamoDB customer master key alias/aws/dynamodb.
    * @default - KMS Master Key server-side encryption is enabled with an customer owned master key
    */
