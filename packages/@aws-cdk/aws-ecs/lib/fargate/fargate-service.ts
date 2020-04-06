32b5ed50830ac65cb21180de8882d4d729a2a620
Rico Huijbers <rix0rrr@gmail.com>
2020-04-06 13:40:14 +0200

    fix(ecs): default Service throws in a VPC without private subnets (#7188)
    People like to create VPCs without private subnets, as the NAT
    gateways (or NAT instances) can be expensive.
    
    The default settings of an ECS Service is to create themselves
    in a PRIVATE subnet, instead of the "default selection order" of
    Private -> Isolated -> Public. Now use the default selection
    (by not supplying a `subnetType` at all).
    
    Fixes #7062.

--- packages/@aws-cdk/aws-ecs/lib/fargate/fargate-service.ts
+++ packages/@aws-cdk/aws-ecs/lib/fargate/fargate-service.ts
@@ -28,7 +28,7 @@ export interface FargateServiceProps extends BaseServiceOptions {
   /**
    * The subnets to associate with the service.
    *
-   * @default - Private subnets.
+   * @default - Public subnets if `assignPublicIp` is set, otherwise the first available one of Private, Isolated, Public, in that order.
    */
   readonly vpcSubnets?: ec2.SubnetSelection;
 
