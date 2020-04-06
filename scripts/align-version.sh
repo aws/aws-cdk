10748cd5cc1b9ea9c11d460f98773386b2f91fb2
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2020-04-06 19:29:23 +0200

    -E

--- scripts/align-version.sh
+++ scripts/align-version.sh
@@ -14,7 +14,7 @@ ${scriptdir}/align-version.js ${files}
 
 # validation
 marker=$(node -p "require('./scripts/get-version-marker').replace(/\./g, '\.')")
-if find . -name package.json | grep -v node_modules | xargs grep -P "\"[^0-9]?${marker}\""; then
+if find . -name package.json | grep -v node_modules | xargs grep -E "\"[^0-9]?${marker}\""; then
   echo "ERROR: unexpected version marker ${marker} in a package.json file"
   exit 1
 fi
