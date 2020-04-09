f68821d28b50dbc509c99855bb8c63e2ee60a991
robertd <robert.djurasaj@gmail.com>
2020-04-08 20:50:28 -0600

    feat(ecs): add Fargate 1.4.0 support

--- packages/@aws-cdk/aws-ecs/lib/fargate/fargate-service.ts
+++ packages/@aws-cdk/aws-ecs/lib/fargate/fargate-service.ts
@@ -158,9 +158,18 @@ export class FargateService extends BaseService implements IFargateService {
  */
 export enum FargatePlatformVersion {
   /**
-   * The latest, recommended platform version
+   * The latest, recommended platform version.
    */
   LATEST = 'LATEST',
+
+  /**
+   * Version 1.4.0
+   *
+   * Supports EFS endpoints, CAP_SYS_PTRACE Linux capability,
+   * network performance metrics in CloudWatch Container Insights,
+   * consolidated 20 GB ephemeral volume.
+   */
+  VERSION1_4 = '1.4.0',
 
   /**
    * Version 1.3.0
