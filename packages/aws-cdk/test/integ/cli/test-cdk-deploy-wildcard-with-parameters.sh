b96375082d5fe7c50b27077eeee1f870fc93aef2
Shiv Lakshminarayan <shivlaks@amazon.com>
2020-04-15 16:44:17 -0700

    chore(cli): fix wildcard deploy with parameters integ test

--- packages/aws-cdk/test/integ/cli/test-cdk-deploy-wildcard-with-parameters.sh
+++ packages/aws-cdk/test/integ/cli/test-cdk-deploy-wildcard-with-parameters.sh
@@ -8,12 +8,12 @@ setup
 
 # STACK_NAME_PREFIX is used in OtherTopicNameParam to allow multiple instances
 # of this test to run in parallel, othewise they will attempt to create the same SNS topic.
-stack_arns=$(cdk deploy ${STACK_NAME_PREFIX}-param-test-\* --parameters "${STACK_NAME_PREFIX}-param-test-1:TopicNameParam=${STACK_NAME_PREFIX}bazinga" --parameters "${STACK_NAME_PREFIX}-param-test-2:OtherTopicNameParam=${STACK_NAME_PREFIX}ThatsMySpot")
+stack_arns=$(/Users/shivlaks/code/blankslate/aws-cdk/packages/aws-cdk/bin/cdk deploy ${STACK_NAME_PREFIX}-param-test-\* --parameters "${STACK_NAME_PREFIX}-param-test-1:TopicNameParam=${STACK_NAME_PREFIX}bazinga" --parameters "${STACK_NAME_PREFIX}-param-test-2:OtherTopicNameParam=${STACK_NAME_PREFIX}ThatsMySpot" --parameters "${STACK_NAME_PREFIX}-param-test-3:DisplayNameParam=${STACK_NAME_PREFIX}HeyThere" --parameters "${STACK_NAME_PREFIX}-param-test-3:OtherDisplayNameParam=${STACK_NAME_PREFIX}AnotherOne")
 echo "Stack deployed successfully"
 
 # verify that we only deployed a single stack (there's a single ARN in the output)
 lines="$(echo "${stack_arns}" | wc -l)"
-if [ "${lines}" -ne 2 ]; then
+if [ "${lines}" -ne 3 ]; then
     echo "-- output -----------"
     echo "${stack_arns}"
     echo "---------------------"
