fedbc8932935b09122596710d5bcf1d9d27ff2bb
Niranjan Jayakar <nija@amazon.com>
2020-04-16 17:52:45 +0100

    some docs

--- packages/@aws-cdk/custom-resources/lib/provider-framework/waiter-state-machine.ts
+++ packages/@aws-cdk/custom-resources/lib/provider-framework/waiter-state-machine.ts
@@ -2,26 +2,44 @@ import { Grant, IGrantable, PolicyStatement, Role, ServicePrincipal } from '@aws
 import { IFunction } from '@aws-cdk/aws-lambda';
 import { CfnResource, Construct, Duration, Stack } from '@aws-cdk/core';
 
-export interface StateMachineProps {
+export interface WaiterStateMachineProps {
+  /**
+   * The main handler that notifies if the waiter to decide 'complete' or 'incomplete'.
+   */
   readonly isCompleteHandler: IFunction;
 
+  /**
+   * The handler to call if the waiter times out and is incomplete.
+   */
   readonly timeoutHandler: IFunction;
 
+  /**
+   * The interval to wait between attempts.
+   */
   readonly interval: Duration;
 
+  /**
+   * Number of attempts.
+   */
   readonly maxAttempts: number;
 
+  /**
+   * Backoff between attempts.
+   */
   readonly backoffRate: number;
 }
 
 /**
  * A very simple StateMachine construct highly customized to the provider framework.
  * This is so that this package does not need to depend on aws-stepfunctions module.
+ *
+ * The state machine continuously calls the isCompleteHandler, until it succeeds or times out.
+ * The handler is called `maxAttempts` times with an `interval` duration and a `backoffRate` rate.
  */
 export class WaiterStateMachine extends Construct {
   public readonly stateMachineArn: string;
 
-  constructor(scope: Construct, id: string, props: StateMachineProps) {
+  constructor(scope: Construct, id: string, props: WaiterStateMachineProps) {
     super(scope, id);
 
     const role = new Role(this, 'Role', {
