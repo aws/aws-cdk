6fe7c87e3e3317864a9f3bd88e8e69f78380aff5
James Fleming <flemjame@amazon.com>
2020-04-20 21:41:45 +0000

    Trailing whitespace

--- packages/@aws-cdk/aws-rds/lib/cluster.ts
+++ packages/@aws-cdk/aws-rds/lib/cluster.ts
@@ -332,7 +332,7 @@ export class DatabaseCluster extends DatabaseClusterBase {
 
   /**
    * The subnets used by the DB subnet group.
-   * 
+   *
    * @default - the Vpc default strategy if not specified.
    */
   private readonly vpcSubnets?: ec2.SubnetSelection;
