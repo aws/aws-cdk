29dcb0a6f872ddcbe315cdd09a68f50ecc3d5826
Niranjan Jayakar <nija@amazon.com>
2020-04-21 15:46:14 +0100

    chore(efs): align construct library to latest standards
    - Drop the `Efs` prefix to the exported types.
    - Change the type of provisioned throughput property to use `Size`.
    
    BREAKING CHANGE: Exported types no longer have the `Efs` prefix.
    * **efs:** `provisionedThroughputInMibps` property is renamed to
    `provisionedThroughputInSeconds` and has the type `Size`.

--- packages/@aws-cdk/aws-efs/lib/efs-file-system.ts
+++ packages/@aws-cdk/aws-efs/lib/efs-file-system.ts
@@ -1,15 +1,16 @@
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as kms from '@aws-cdk/aws-kms';
-import {Construct, Resource, Tag} from '@aws-cdk/core';
-import {CfnFileSystem, CfnMountTarget} from './efs.generated';
+import { Construct, IResource, Resource, Size, Tag } from '@aws-cdk/core';
+import { CfnFileSystem, CfnMountTarget } from './efs.generated';
 
-// tslint:disable: max-line-length
+// tslint:disable:max-line-length
 /**
  * EFS Lifecycle Policy, if a file is not accessed for given days, it will move to EFS Infrequent Access.
  *
  * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-elasticfilesystem-filesystem-lifecyclepolicies
  */
-export enum EfsLifecyclePolicyProperty {
+// tslint:enable
+export enum LifecyclePolicyProperty {
   /**
    * After 7 days of not being accessed.
    */
@@ -41,7 +42,7 @@ export enum EfsLifecyclePolicyProperty {
  *
  * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-efs-filesystem-performancemode
  */
-export enum EfsPerformanceMode {
+export enum PerformanceMode {
   /**
    * This is the general purpose performance mode for most file systems.
    */
@@ -59,7 +60,7 @@ export enum EfsPerformanceMode {
  *
  * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-elasticfilesystem-filesystem-throughputmode
  */
-export enum EfsThroughputMode {
+export enum ThroughputMode {
   /**
    *  This mode on Amazon EFS scales as the size of the file system in the standard storage class grows.
    */
@@ -74,7 +75,7 @@ export enum EfsThroughputMode {
 /**
  * Interface to implement AWS File Systems.
  */
-export interface IEfsFileSystem extends ec2.IConnectable {
+export interface IFileSystem extends ec2.IConnectable, IResource {
   /**
    * The ID of the file system, assigned by Amazon EFS.
    *
@@ -86,7 +87,7 @@ export interface IEfsFileSystem extends ec2.IConnectable {
 /**
  * Properties of EFS FileSystem.
  */
-export interface EfsFileSystemProps {
+export interface FileSystemProps {
 
   /**
    * VPC to launch the file system in.
@@ -133,51 +134,36 @@ export interface EfsFileSystemProps {
    *
    * @default - none
    */
-  readonly lifecyclePolicy?: EfsLifecyclePolicyProperty;
+  readonly lifecyclePolicy?: LifecyclePolicyProperty;
 
   /**
    * Enum to mention the performance mode of the file system.
    *
    * @default - GENERAL_PURPOSE
    */
-  readonly performanceMode?: EfsPerformanceMode;
+  readonly performanceMode?: PerformanceMode;
 
   /**
    * Enum to mention the throughput mode of the file system.
    *
    * @default - BURSTING
    */
-  readonly throughputMode?: EfsThroughputMode;
+  readonly throughputMode?: ThroughputMode;
 
   /**
-   * Provisioned throughput for the file system. This is a required property if the throughput mode is set to PROVISIONED.
-   * Valid values are 1-1024.
+   * Provisioned throughput for the file system.
+   * This is a required property if the throughput mode is set to PROVISIONED.
+   * Valid values are 1MiB/s -> 1GiB/s
    *
    * @default - None, errors out
    */
-  readonly provisionedThroughputInMibps?: number;
-}
-
-/**
- *  A new or imported EFS File System.
- */
-abstract class EfsFileSystemBase extends Resource implements IEfsFileSystem {
-
-  /**
-   * The security groups/rules used to allow network connections to the file system.
-   */
-  public abstract readonly connections: ec2.Connections;
-
-  /**
-   * @attribute
-   */
-  public abstract readonly fileSystemId: string;
+  readonly provisionedThroughputPerSecond?: Size;
 }
 
 /**
  * Properties that describe an existing EFS file system.
  */
-export interface EfsFileSystemAttributes {
+export interface FileSystemAttributes {
   /**
    * The security group of the file system
    */
@@ -199,17 +185,17 @@ export interface EfsFileSystemAttributes {
  *
  * @resource AWS::EFS::FileSystem
  */
-export class EfsFileSystem extends EfsFileSystemBase {
+export class FileSystem extends Resource implements IFileSystem {
 
   /**
    * Import an existing File System from the given properties.
    */
-  public static fromEfsFileSystemAttributes(scope: Construct, id: string, attrs: EfsFileSystemAttributes): IEfsFileSystem {
-    class Import extends EfsFileSystemBase implements IEfsFileSystem {
+  public static fromFileSystemAttributes(scope: Construct, id: string, attrs: FileSystemAttributes): IFileSystem {
+    class Import extends Resource implements IFileSystem {
       public readonly fileSystemId = attrs.fileSystemID;
       public readonly connections = new ec2.Connections({
         securityGroups: [attrs.securityGroup],
-        defaultPort: ec2.Port.tcp(EfsFileSystem.DEFAULT_PORT),
+        defaultPort: ec2.Port.tcp(FileSystem.DEFAULT_PORT),
       });
     }
 
@@ -231,37 +217,32 @@ export class EfsFileSystem extends EfsFileSystemBase {
    */
   public readonly fileSystemId: string;
 
-  private readonly efsFileSystem: CfnFileSystem;
-
   /**
    * Constructor for creating a new EFS FileSystem.
    */
-  constructor(scope: Construct, id: string, props: EfsFileSystemProps) {
+  constructor(scope: Construct, id: string, props: FileSystemProps) {
     super(scope, id);
 
-    if (props.throughputMode === EfsThroughputMode.PROVISIONED) {
-      if (props.provisionedThroughputInMibps === undefined) {
+    if (props.throughputMode === ThroughputMode.PROVISIONED) {
+      if (props.provisionedThroughputPerSecond === undefined) {
         throw new Error('Property provisionedThroughputInMibps is required when throughputMode is PROVISIONED');
-      } else if (!Number.isInteger(props.provisionedThroughputInMibps)) {
-        throw new Error('Invalid input for provisionedThroughputInMibps');
-      } else if (props.provisionedThroughputInMibps < 1 || props.provisionedThroughputInMibps > 1024) {
-        this.node.addWarning('Valid values for throughput are 1-1024 MiB/s. You can get this limit increased by contacting AWS Support.');
+      } else if (props.provisionedThroughputPerSecond.toMebibytes() > 1024) {
+        throw new Error('Valid values for throughput are 1MiB/s - 1GiB/s. You can get this limit increased by contacting AWS Support.');
       }
     }
 
-    this.efsFileSystem = new CfnFileSystem(this, 'Resource', {
+    const filesystem = new CfnFileSystem(this, 'Resource', {
       encrypted: props.encrypted,
       kmsKeyId: (props.kmsKey ? props.kmsKey.keyId : undefined),
       lifecyclePolicies: (props.lifecyclePolicy ? Array.of({
-        transitionToIa: EfsLifecyclePolicyProperty[props.lifecyclePolicy],
+        transitionToIa: LifecyclePolicyProperty[props.lifecyclePolicy],
       } as CfnFileSystem.LifecyclePolicyProperty) : undefined),
       performanceMode: props.performanceMode,
       throughputMode: props.throughputMode,
-      provisionedThroughputInMibps: props.provisionedThroughputInMibps,
+      provisionedThroughputInMibps: props.provisionedThroughputPerSecond ? props.provisionedThroughputPerSecond.toMebibytes() : undefined,
     });
 
-    this.fileSystemId = this.efsFileSystem.ref;
-    this.node.defaultChild = this.efsFileSystem;
+    this.fileSystemId = filesystem.ref;
     Tag.add(this, 'Name', props.fileSystemName || this.node.path);
 
     const securityGroup = (props.securityGroup || new ec2.SecurityGroup(this, 'EfsSecurityGroup', {
@@ -270,7 +251,7 @@ export class EfsFileSystem extends EfsFileSystemBase {
 
     this.connections = new ec2.Connections({
       securityGroups: [securityGroup],
-      defaultPort: ec2.Port.tcp(EfsFileSystem.DEFAULT_PORT),
+      defaultPort: ec2.Port.tcp(FileSystem.DEFAULT_PORT),
     });
 
     const subnets = props.vpc.selectSubnets(props.vpcSubnets);
