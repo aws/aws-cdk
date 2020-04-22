634da9342800641b65bf2e0809e8aa530f54d2fb
Niranjan Jayakar <nija@amazon.com>
2020-04-22 17:56:05 +0100

    Currently deployment logical id salting are only part of Methods, Models
    and Resources. Properties of the `RestApi` such as policy, parameters,
    binaryMediaTypes, etc. are not currently added to this salting.
    
    As a consequence, any change to the RestApi property does not trigger a
    new deployment, as customers would expect it would.
    
    The solution adds the serialized CloudFormation JSON of `CfnRestApi` to
    the logical id salting, such that change to any of the properties here
    will trigger a new deployment.
    
    **Note:** With this change, any change to the properties of `RestApi`
    will trigger a new deployment.
    
    fixes #5354

--- packages/@aws-cdk/aws-apigateway/test/test.deployment.ts
+++ packages/@aws-cdk/aws-apigateway/test/test.deployment.ts
@@ -43,7 +43,7 @@ export = {
             Name: 'api',
           },
         },
-        deployment33381975: {
+        deployment33381975bba46c5132329b81e7befcbbba5a0e75: {
           Type: 'AWS::ApiGateway::Deployment',
           Properties: {
             RestApiId: {
@@ -97,7 +97,7 @@ export = {
             Name: 'api',
           },
         },
-        deployment33381975: {
+        deployment33381975bba46c5132329b81e7befcbbba5a0e75: {
           Type: 'AWS::ApiGateway::Deployment',
           Properties: {
             RestApiId: {
@@ -133,30 +133,33 @@ export = {
     test.done();
   },
 
-  '"addToLogicalId" will "salt" the logical ID of the deployment resource'(test: Test) {
+  'logical ID of the deployment resource is salted'(test: Test) {
     // GIVEN
     const stack = new Stack();
     const api = new apigateway.RestApi(stack, 'api', { deploy: false, cloudWatchRole: false });
     const deployment = new apigateway.Deployment(stack, 'deployment', { api });
     api.root.addMethod('GET');
 
-    // default logical ID (with no "salt")
-    test.ok(synthesize().Resources.deployment33381975);
+    const resources = synthesize().Resources;
+    test.ok(resources.deployment33381975bba46c5132329b81e7befcbbba5a0e75,
+      `resource deployment33381975bba46c5132329b81e7befcbbba5a0e75 not found, instead found ${Object.keys(resources)}`);
 
     // adding some salt
     deployment.addToLogicalId({ foo: 123 }); // add some data to the logical ID
 
     // the logical ID changed
     const template = synthesize();
-    test.ok(!template.Resources.deployment33381975, 'old resource id deleted');
-    test.ok(template.Resources.deployment33381975427670fa9e4148dc851927485bdf36a5, 'new resource is created');
+    test.ok(!template.Resources.deployment33381975bba46c5132329b81e7befcbbba5a0e75, 'old resource id is not deleted');
+    test.ok(template.Resources.deployment33381975075f46a4503208d69fcffed2f263c48c,
+      `new resource deployment33381975075f46a4503208d69fcffed2f263c48c is not created, instead found ${Object.keys(template.Resources)}`);
 
     // tokens supported, and are resolved upon synthesis
     const value = 'hello hello';
     deployment.addToLogicalId({ foo: Lazy.stringValue({ produce: () => value }) });
 
     const template2 = synthesize();
-    test.ok(template2.Resources.deployment33381975a12dfe81474913364dc31c06e37f9449);
+    test.ok(template2.Resources.deployment33381975b6d7672e4c9afd0b741e41d07739786b,
+      `resource deployment33381975b6d7672e4c9afd0b741e41d07739786b not found, instead found ${Object.keys(template2.Resources)}`);
 
     test.done();
 
@@ -214,10 +217,10 @@ export = {
 
     // THEN
     expect(stack1).to(haveResource('AWS::ApiGateway::Stage', {
-      DeploymentId: { Ref: 'myapiDeploymentB7EF8EB7e0b8372768854261d2d1218739e0a307' },
+      DeploymentId: { Ref: 'myapiDeploymentB7EF8EB74c5295c27fa87ff13f4d04e13f67662d' },
     }));
     expect(stack2).to(haveResource('AWS::ApiGateway::Stage', {
-      DeploymentId: { Ref: 'myapiDeploymentB7EF8EB77c517352b0f7ab73c333e36585c8f1f3' },
+      DeploymentId: { Ref: 'myapiDeploymentB7EF8EB7b50d305057ba109c118e4aafd4509355' },
     }));
     test.done();
   },
