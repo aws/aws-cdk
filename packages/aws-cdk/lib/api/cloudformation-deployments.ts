d162dca98574b9ed77914232810ed4df7c93c3b9
Rico Huijbers <rix0rrr@gmail.com>
2020-04-24 11:09:24 +0200

    fix(cli): can't bootstrap environment not in app
    It used to be that if we had an `--app` argument, we would always glob
    arguments to `cdk bootstrap <ENV>` through the environments of stacks
    in the app.
    
    This makes it super hard/annoying to run `cdk bootstrap
    aws://1234/us-somewhere` in a CI/CD project; you have to add a stack
    there first and compile before you're allowed to do that, which is
    kinda silly.
    
    Change behavior to only glob environment from the environments in the
    app if it looks like the user is supplying a glob (if it contains `*`).
    
    If the user is supplying a concrete environment name, just use it
    directly.
    
    Also in this commit:
    
    - refactor: lots of places where we were passing around a pair
    of `(account, region)`. Replace those by passing a `cxapi.Environment`
    instead (most of the changes in this PR).
    - refactor: the old and new bootstrapping experience had a lot of
    copy/pasta between them. Refactored to make them share code.
    - (prerelease) feat: add a version check to the bootstrapping operation,
    so that users won't accidentally downgrade a bootstrap stack to an older
    version (This happened to 2 devs already, and is an easy mistake to
    make. Protect against it.)

--- packages/aws-cdk/lib/api/cloudformation-deployments.ts
+++ packages/aws-cdk/lib/api/cloudformation-deployments.ts
@@ -172,9 +172,9 @@ export class CloudFormationDeployments {
     if (!stack.environment) {
       throw new Error(`The stack ${stack.displayName} does not have an environment`);
     }
-    const resolvedEnvironment = await this.sdkProvider.resolveEnvironment(stack.environment.account, stack.environment.region);
+    const resolvedEnvironment = await this.sdkProvider.resolveEnvironment(stack.environment);
 
-    const stackSdk = await this.sdkProvider.forEnvironment(stack.environment.account, stack.environment.region, mode);
+    const stackSdk = await this.sdkProvider.forEnvironment(stack.environment, mode);
 
     return {
       stackSdk,
