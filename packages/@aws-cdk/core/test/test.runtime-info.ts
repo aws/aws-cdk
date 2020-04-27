0bb089f25ebe058a23d08477ac7e8399e7a66619
Elad Ben-Israel <benisrae@amazon.com>
2020-04-27 22:33:00 +0300

    use a mock version to improve testability

--- packages/@aws-cdk/core/test/test.runtime-info.ts
+++ packages/@aws-cdk/core/test/test.runtime-info.ts
@@ -7,11 +7,12 @@ import { collectRuntimeInformation } from '../lib/private/runtime-info';
 export = {
   'version reporting includes @aws-solutions-konstruk libraries'(test: Test) {
     const pkgdir = fs.mkdtempSync(path.join(os.tmpdir(), 'runtime-info-konstruk-fixture'));
-
+    const mockVersion = '1.2.3';
+    
     fs.writeFileSync(path.join(pkgdir, 'index.js'), 'module.exports = \'this is foo\';');
     fs.writeFileSync(path.join(pkgdir, 'package.json'), JSON.stringify({
       name: '@aws-solutions-konstruk/foo',
-      version: '0.0.0',
+      version: mockVersion,
     }));
 
     // eslint-disable-next-line @typescript-eslint/no-require-imports, import/no-extraneous-dependencies
@@ -25,7 +26,7 @@ export = {
       '@aws-cdk/core': version,
       '@aws-cdk/cx-api': version,
       '@aws-cdk/cloud-assembly-schema': version,
-      '@aws-solutions-konstruk/foo': '0.0.0',
+      '@aws-solutions-konstruk/foo': mockVersion,
       'jsii-runtime': `node.js/${process.version}`,
     });
     test.done();
