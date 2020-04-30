f8eafa1c34b207c7100d6805c17c2fc95177a590
Elad Ben-Israel <benisrae@amazon.com>
2020-04-30 19:48:19 +0300

    add coverage to for the compatibility layer

--- packages/@aws-cdk/assets/lib/staging.ts
+++ packages/@aws-cdk/assets/lib/staging.ts
@@ -1,5 +1,5 @@
-import { AssetStaging, Construct, SymlinkFollowMode } from '@aws-cdk/core';
-import { FollowMode } from './fs/follow-mode';
+import { AssetStaging, Construct } from '@aws-cdk/core';
+import { toSymlinkFollow } from './compat';
 import { FingerprintOptions } from './fs/options';
 
 /**
@@ -25,20 +25,5 @@ export class Staging extends AssetStaging {
       extraHash: props.extraHash,
       follow: toSymlinkFollow(props.follow),
     });
-  }
-}
-
-function toSymlinkFollow(follow?: FollowMode): SymlinkFollowMode | undefined {
-  if (!follow) {
-    return undefined;
-  }
-
-  switch (follow) {
-    case FollowMode.NEVER: return SymlinkFollowMode.NEVER;
-    case FollowMode.ALWAYS: return SymlinkFollowMode.ALWAYS;
-    case FollowMode.BLOCK_EXTERNAL: return SymlinkFollowMode.BLOCK_EXTERNAL;
-    case FollowMode.EXTERNAL: return SymlinkFollowMode.EXTERNAL;
-    default:
-      throw new Error(`unknown follow mode: ${follow}`);
   }
 }
