33ae03240172c3f5b1efcf283b415f9848000c67
Barun Ray <ray.barunray@gmail.com>
2020-05-05 17:03:10 +0530

    feat(sns): add support for subscription DLQ in SNS

--- packages/@aws-cdk/aws-sns-subscriptions/lib/subscription.ts
+++ packages/@aws-cdk/aws-sns-subscriptions/lib/subscription.ts
@@ -1,4 +1,5 @@
 import * as sns from '@aws-cdk/aws-sns';
+import { IQueue } from '@aws-cdk/aws-sqs';
 
 /**
  * Options to subscribing to an SNS topic
@@ -10,4 +11,12 @@ export interface SubscriptionProps {
    * @default - all messages are delivered
    */
   readonly filterPolicy?: { [attribute: string]: sns.SubscriptionFilter };
+
+  /**
+   * Queue used when `deadLetterQueue` is enabled.
+   * Default queue is created when not specified.
+   *
+   * @default - SQS queue with 14 day retention period if `deadLetterQueueEnabled` is `true`
+   */
+  readonly deadLetterQueue?: IQueue;
 }
