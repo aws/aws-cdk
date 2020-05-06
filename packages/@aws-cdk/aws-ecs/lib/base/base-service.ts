6be4d24461822bc9cf5badfeef2a14288c07cf5d
Efe Karakus <karakuse@amazon.com>
2020-05-06 09:30:33 -0700

    refactor(ecs): update @deprecated comment for configureAwsVpcNetworking

--- packages/@aws-cdk/aws-ecs/lib/base/base-service.ts
+++ packages/@aws-cdk/aws-ecs/lib/base/base-service.ts
@@ -586,7 +586,7 @@ export abstract class BaseService extends Resource
 
   /**
    * This method is called to create a networkConfiguration.
-   * @deprecated use configureAwsVpcNetworkingWithMultipleSecurityGroups instead.
+   * @deprecated use configureAwsVpcNetworkingWithSecurityGroups instead.
    */
   // tslint:disable-next-line:max-line-length
   protected configureAwsVpcNetworking(vpc: ec2.IVpc, assignPublicIp?: boolean, vpcSubnets?: ec2.SubnetSelection, securityGroup?: ec2.ISecurityGroup) {
