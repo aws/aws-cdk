f3057cd0adfe46c42d2291d91a5d26217c6ba246
Shiv Lakshminarayan <shivlaks@amazon.com>
2020-05-06 15:30:38 -0700

    feat(stepfunctions): custom state as an escape hatch
    Custom State which enables the capability to provide Amazon States Language (ASL) JSON as an escape hatch. Useful when there are capabilities that are offered through Step Functions such as service integrations, and state properties but there isn't support through the CDK yet.
    
    It enables the usage of all service integrations we don't currently support.

---
+++ packages/@aws-cdk/aws-stepfunctions/test/custom-state.test.ts
@@ -0,0 +1,34 @@
+import '@aws-cdk/assert/jest';
+import * as cdk from '@aws-cdk/core';
+import * as stepfunctions from '../lib';
+
+describe('Custom State', () => {
+  test('maintains the state Json provided during construction', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const stateJson = {
+      Type: 'Task',
+      Resource: 'arn:aws:states:::dynamodb:putItem',
+      Parameters: {
+        TableName: 'MyTable',
+        Item: {
+          id: {
+            S: 'MyEntry',
+          },
+        },
+      },
+      ResultPath: null,
+    };
+
+    // WHEN
+    const customState = new stepfunctions.CustomState(stack, 'Custom', {
+      stateJson,
+    });
+
+    // THEN
+    expect(customState.toStateJson()).toStrictEqual({
+      ...stateJson,
+      End: true,
+    });
+  });
+});
