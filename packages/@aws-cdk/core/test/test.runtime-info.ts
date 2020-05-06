61a7e7bae8fd81337279f00c4ba3ab15ea378e87
Rico Huijbers <rix0rrr@gmail.com>
2020-05-06 16:53:34 +0200

    Whoopsie, use actual version

--- packages/@aws-cdk/core/test/test.runtime-info.ts
+++ packages/@aws-cdk/core/test/test.runtime-info.ts
@@ -26,7 +26,7 @@ export = {
       '@aws-cdk/core': version,
       '@aws-cdk/cx-api': version,
       '@aws-cdk/cloud-assembly-schema': version,
-      '@aws-cdk/cdk-assets-schema': '0.0.0',
+      '@aws-cdk/cdk-assets-schema': version,
       '@aws-solutions-konstruk/foo': mockVersion,
       'jsii-runtime': `node.js/${process.version}`,
     });
