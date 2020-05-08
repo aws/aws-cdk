f4f3bd12b26a238d8ba61715ce49a2463c8dbca0
Rico Huijbers <rix0rrr@gmail.com>
2020-05-08 12:02:50 +0200

    chore: change CLI integ tests from Bash to TypeScript
    Converted most tests from Bash to TypeScript. It's much more comfortable
    to write complex tests in a programming language than bash. Plus, `jest`
    is a better test runner than bash, too.
    
    Even though tests are now written in TypeScript, this does not
    conceptually change their SUT! They are still testing the CLI via
    running it as a subprocess, they are NOT reaching directly into the CLI
    code to test its private methods. It's just that setup/teardown/error
    handling/assertions/AWS calls are much more convenient to do in a real
    programming language.
    
    Compilation of the tests is done as part of the normal package build, at
    which point it is using the dependencies brought in by the containing
    `aws-cdk` package's `package.json`.
    
    When run in a non-develompent repo (as done during integ tests or canary
    runs), the required dependencies are brought in just-in-time via
    `test-jest.sh`. Using a separate `package.json` just for the tests
    makes it a package in its own right for `yarn`, and I didn't want
    to go there.

---
+++ packages/aws-cdk/test/integ/cli/aws-helpers.ts
@@ -0,0 +1,143 @@
+import * as AWS from 'aws-sdk';
+import { log } from './cdk-helpers';
+
+interface Env {
+  account: string;
+  region: string;
+}
+
+export let testEnv = async (): Promise<Env> => {
+  const response = await new AWS.STS().getCallerIdentity().promise();
+
+  const ret: Env = {
+    account: response.Account!,
+    region: process.env.AWS_REGION ?? process.env.AWS_DEFAULT_REGION ?? 'us-east-1',
+  };
+
+  testEnv = () => Promise.resolve(ret);
+  return ret;
+};
+
+export let cloudFormation = makeAwsCaller(AWS.CloudFormation);
+export let sns = makeAwsCaller(AWS.SNS);
+export let iam = makeAwsCaller(AWS.IAM);
+export let lambda = makeAwsCaller(AWS.Lambda);
+export let sts = makeAwsCaller(AWS.STS);
+
+/**
+ * Perform an AWS call from nothing
+ *
+ * Create the correct client, do the call and resole the promise().
+ */
+async function awsCall<
+  A extends AWS.Service,
+  B extends keyof ServiceCalls<A>,
+>(ctor: new (config: any) => A, call: B, request: First<ServiceCalls<A>[B]>): Promise<Second<ServiceCalls<A>[B]>> {
+  const env = await testEnv();
+  const cfn = new ctor({ region: env.region });
+  const response = cfn[call](request);
+  try {
+    return await response.promise();
+  } catch (e) {
+    const newErr = new Error(`${call}(${JSON.stringify(request)}): ${e.message}`);
+    (newErr as any).code = e.code;
+    throw newErr;
+  }
+}
+
+/**
+ * Factory function to invoke 'awsCall' for specific services.
+ *
+ * Not strictly necessary but calling this replaces a whole bunch of annoying generics you otherwise have to type:
+ *
+ * ```ts
+ * export function cloudFormation<
+ *   C extends keyof ServiceCalls<AWS.CloudFormation>,
+ * >(call: C, request: First<ServiceCalls<AWS.CloudFormation>[C]>): Promise<Second<ServiceCalls<AWS.CloudFormation>[C]>> {
+ *   return awsCall(AWS.CloudFormation, call, request);
+ * }
+ * ```
+ */
+function makeAwsCaller<A extends AWS.Service>(ctor: new (config: any) => A) {
+  return <B extends keyof ServiceCalls<A>>(call: B, request: First<ServiceCalls<A>[B]>): Promise<Second<ServiceCalls<A>[B]>> => {
+    return awsCall(ctor, call, request);
+  };
+}
+
+type ServiceCalls<T> = NoNayNever<SimplifiedService<T>>;
+// Map ever member in the type to the important AWS call overload, or to 'never'
+type SimplifiedService<T> = {[k in keyof T]: AwsCallIO<T[k]>};
+// Remove all 'never' types from an object type
+type NoNayNever<T> = Pick<T, {[k in keyof T]: T[k] extends never ? never : k }[keyof T]>;
+
+// Because of the overloads an AWS handler type looks like this:
+//
+//   {
+//      (params: INPUTSTRUCT, callback?: ((err: AWSError, data: {}) => void) | undefined): Request<OUTPUT, ...>;
+//      (callback?: ((err: AWS.AWSError, data: {}) => void) | undefined): AWS.Request<...>;
+//   }
+//
+// Get the first overload and extract the input and output struct types
+type AwsCallIO<T> =
+  T extends {
+    (args: infer INPUT, callback?: ((err: AWS.AWSError, data: any) => void) | undefined): AWS.Request<infer OUTPUT, AWS.AWSError>;
+    (callback?: ((err: AWS.AWSError, data: {}) => void) | undefined): AWS.Request<any, any>;
+  } ? [INPUT, OUTPUT] : never;
+
+type First<T> = T extends [any, any] ? T[0] : never;
+type Second<T> = T extends [any, any] ? T[1] : never;
+
+export async function deleteStacks(...stackNames: string[]) {
+  if (stackNames.length === 0) { return; }
+
+  for (const stackName of stackNames) {
+    await cloudFormation('deleteStack', {
+      StackName: stackName,
+    });
+  }
+
+  await retry(`Deleting ${stackNames}`, afterSeconds(300), async () => {
+    for (const stackName of stackNames) {
+      if (await stackExists(stackName)) {
+        throw new Error(`Delete of '${stackName}' not complete yet`);
+      }
+    }
+  });
+}
+
+export async function stackExists(stackName: string) {
+  try {
+    await cloudFormation('describeStacks', { StackName: stackName });
+    return true;
+  } catch (e) {
+    if (e.message.indexOf('does not exist') > -1) { return false; }
+    throw e;
+  }
+}
+
+export function afterSeconds(seconds: number): Date {
+  return new Date(Date.now() + seconds * 1000);
+}
+
+export async function retry<A>(operation: string, deadline: Date, block: () => Promise<A>): Promise<A> {
+  let i = 0;
+  log(`💈 ${operation}`);
+  while (true) {
+    try {
+      i++;
+      const ret = await block();
+      log(`💈 ${operation}: succeeded after ${i} attempts`);
+      return ret;
+    } catch (e) {
+      if (Date.now() > deadline.getTime()) {
+        throw new Error(`${operation}: did not succeed after ${i} attempts: ${e}`);
+      }
+      log(`⏳ ${operation} (${e.message})`);
+      await sleep(5000);
+    }
+  }
+}
+
+export async function sleep(ms: number) {
+  return new Promise(ok => setTimeout(ok, ms));
+}
---
+++ packages/aws-cdk/test/integ/cli/cli.integtest.ts
@@ -0,0 +1,463 @@
+import { promises as fs } from 'fs';
+import * as path from 'path';
+import { afterSeconds, cloudFormation, iam, lambda, retry, sns, sts } from './aws-helpers';
+import { cdk, cdkDeploy, cdkDestroy, cleanupOldStacks, fullStackName, INTEG_TEST_DIR,
+  log, prepareAppFixture, STACK_NAME_PREFIX } from './cdk-helpers';
+
+jest.setTimeout(600 * 1000);
+
+beforeEach(async () => {
+  await prepareAppFixture();
+  await cleanupOldStacks();
+});
+
+afterEach(async () => {
+  await cleanupOldStacks();
+});
+
+test('VPC Lookup', async () => {
+  log('Making sure we are clean before starting.');
+  await cdkDestroy('define-vpc', { modEnv: { ENABLE_VPC_TESTING: 'DEFINE' }});
+
+  log('Setting up: creating a VPC with known tags');
+  await cdkDeploy('define-vpc', { modEnv: { ENABLE_VPC_TESTING: 'DEFINE' }});
+  log('Setup complete!');
+
+  log('Verifying we can now import that VPC');
+  await cdkDeploy('import-vpc', { modEnv: { ENABLE_VPC_TESTING: 'IMPORT' }});
+});
+
+test('Two ways of shoing the version', async () => {
+  const version1 = await cdk(['version']);
+  const version2 = await cdk(['--version']);
+
+  expect(version1).toEqual(version2);
+});
+
+test('Termination protection', async () => {
+  await cdkDeploy('termination-protection');
+
+  // Try a destroy that should fail
+  await expect(cdkDestroy('termination-protection')).rejects.toThrow('exited with error');
+
+  await cloudFormation('updateTerminationProtection', {
+    EnableTerminationProtection: false,
+    StackName: fullStackName('termination-protection'),
+  });
+});
+
+test('cdk synth', async () => {
+  await expect(cdk(['synth', fullStackName('test-1')])).resolves.toEqual(
+    `Resources:
+  topic69831491:
+    Type: AWS::SNS::Topic
+    Metadata:
+      aws:cdk:path: ${STACK_NAME_PREFIX}-test-1/topic/Resource`);
+
+  await expect(cdk(['synth', fullStackName('test-2')])).resolves.toEqual(
+    `Resources:
+  topic152D84A37:
+    Type: AWS::SNS::Topic
+    Metadata:
+      aws:cdk:path: ${STACK_NAME_PREFIX}-test-2/topic1/Resource
+  topic2A4FB547F:
+    Type: AWS::SNS::Topic
+    Metadata:
+      aws:cdk:path: ${STACK_NAME_PREFIX}-test-2/topic2/Resource`);
+});
+
+test('ssm parameter provider error', async () => {
+  await expect(cdk(['synth',
+    fullStackName('missing-ssm-parameter'),
+    '-c', 'test:ssm-parameter-name=/does/not/exist',
+  ], {
+    allowErrExit: true,
+  })).resolves.toContain('SSM parameter not available in account');
+});
+
+test('automatic ordering', async () => {
+  // Deploy the consuming stack which will include the producing stack
+  await cdkDeploy('order-consuming');
+
+  // Destroy the providing stack which will include the consuming stack
+  await cdkDestroy('order-providing');
+});
+
+test('context setting', async () => {
+  await fs.writeFile(path.join(INTEG_TEST_DIR, 'cdk.context.json'), JSON.stringify({
+    contextkey: 'this is the context value',
+  }));
+  try {
+    await expect(cdk(['context'])).resolves.toContain('this is the context value');
+
+    // Test that deleting the contextkey works
+    await cdk(['context', '--reset', 'contextkey']);
+    await expect(cdk(['context'])).resolves.not.toContain('this is the context value');
+
+    // Test that forced delete of the context key does not throw
+    await cdk(['context', '-f', '--reset', 'contextkey']);
+
+  } finally {
+    await fs.unlink(path.join(INTEG_TEST_DIR, 'cdk.context.json'));
+  }
+});
+
+test('deploy', async () => {
+  const stackArn = await cdkDeploy('test-2', { captureStderr: false });
+
+  // verify the number of resources in the stack
+  const response = await cloudFormation('describeStackResources', {
+    StackName: stackArn,
+  });
+  expect(response.StackResources?.length).toEqual(2);
+});
+
+test('deploy all', async () => {
+  const arns = await cdkDeploy('test-*', { captureStderr: false });
+
+  // verify that we only deployed a single stack (there's a single ARN in the output)
+  expect(arns.split('\n').length).toEqual(2);
+});
+
+test('nested stack with parameters', async () => {
+// STACK_NAME_PREFIX is used in MyTopicParam to allow multiple instances
+// of this test to run in parallel, othewise they will attempt to create the same SNS topic.
+  const stackArn = await cdkDeploy('with-nested-stack-using-parameters', {
+    options: ['--parameters', 'MyTopicParam=${STACK_NAME_PREFIX}ThereIsNoSpoon'],
+    captureStderr: false,
+  });
+
+  // verify that we only deployed a single stack (there's a single ARN in the output)
+  expect(stackArn.split('\n').length).toEqual(1);
+
+  // verify the number of resources in the stack
+  const response = await cloudFormation('describeStackResources', {
+    StackName: stackArn,
+  });
+  expect(response.StackResources?.length).toEqual(1);
+});
+
+test('deploy without execute', async () => {
+  const stackArn = await cdkDeploy('test-2', {
+    options: ['--no-execute'],
+    captureStderr: false,
+  });
+  // verify that we only deployed a single stack (there's a single ARN in the output)
+  expect(stackArn.split('\n').length).toEqual(1);
+
+  const response = await cloudFormation('describeStacks', {
+    StackName: stackArn,
+  });
+
+  expect(response.Stacks?.[0].StackStatus).toEqual('REVIEW_IN_PROGRESS');
+});
+
+test('security related changes without a CLI are expected to fail', async () => {
+  // redirect /dev/null to stdin, which means there will not be tty attached
+  // since this stack includes security-related changes, the deployment should
+  // immediately fail because we can't confirm the changes
+  await expect(cdkDeploy('iam-test', {
+    options: ['<', '/dev/null'], // H4x, this only works because I happen to know we pass shell: true.
+  })).rejects.toThrow('exited with error');
+});
+
+test('deploy wildcard with outputs', async () => {
+  const outputsFile = path.join(INTEG_TEST_DIR, 'outputs', 'outputs.json');
+  await fs.mkdir(path.dirname(outputsFile), { recursive: true });
+
+  await cdkDeploy(['outputs-test-*'], {
+    options: ['--outputs-file', outputsFile],
+  });
+
+  const outputs = JSON.parse((await fs.readFile(outputsFile, { encoding: 'utf-8' })).toString());
+  expect(outputs).toEqual({
+    [`${STACK_NAME_PREFIX}-outputs-test-1`]: {
+      TopicName: 'MyTopic',
+    },
+    [`${STACK_NAME_PREFIX}-outputs-test-2`]: {
+      TopicName: 'MyOtherTopic',
+    },
+  });
+});
+
+test('deploy with parameters', async () => {
+  const stackArn = await cdkDeploy('param-test-1', {
+    options: [
+      '--parameters', `TopicNameParam=${STACK_NAME_PREFIX}bazinga`,
+    ],
+    captureStderr: false,
+  });
+
+  const response = await cloudFormation('describeStacks', {
+    StackName: stackArn,
+  });
+
+  expect(response.Stacks?.[0].Parameters).toEqual([
+    {
+      ParameterKey: 'TopicNameParam',
+      ParameterValue: `${STACK_NAME_PREFIX}bazinga`,
+    },
+  ]);
+});
+
+test('deploy with wildcard and parameters', async () => {
+  await cdkDeploy('param-test-*', {
+    options: [
+      '--parameters', `${STACK_NAME_PREFIX}-param-test-1:TopicNameParam=${STACK_NAME_PREFIX}bazinga`,
+      '--parameters', `${STACK_NAME_PREFIX}-param-test-2:OtherTopicNameParam=${STACK_NAME_PREFIX}ThatsMySpot`,
+      '--parameters', `${STACK_NAME_PREFIX}-param-test-3:DisplayNameParam=${STACK_NAME_PREFIX}HeyThere`,
+      '--parameters', `${STACK_NAME_PREFIX}-param-test-3:OtherDisplayNameParam=${STACK_NAME_PREFIX}AnotherOne`,
+    ],
+  });
+});
+
+test('deploy with parameters multi', async () => {
+  const paramVal1 = `${STACK_NAME_PREFIX}bazinga`;
+  const paramVal2 = `${STACK_NAME_PREFIX}=jagshemash`;
+
+  const stackArn = await cdkDeploy('param-test-3', {
+    options: [
+      '--parameters', `DisplayNameParam=${paramVal1}`,
+      '--parameters', `OtherDisplayNameParam=${paramVal2}`,
+    ],
+    captureStderr: false,
+  });
+
+  const response = await cloudFormation('describeStacks', {
+    StackName: stackArn,
+  });
+
+  expect(response.Stacks?.[0].Parameters).toEqual([
+    {
+      ParameterKey: 'DisplayNameParam',
+      ParameterValue: paramVal1,
+    },
+    {
+      ParameterKey: 'OtherDisplayNameParam',
+      ParameterValue: paramVal2,
+    },
+  ]);
+});
+
+test('deploy with notification ARN', async () => {
+  const topicName = `${STACK_NAME_PREFIX}-test-topic`;
+
+  const response = await sns('createTopic', { Name: topicName });
+  const topicArn = response.TopicArn!;
+  try {
+    await cdkDeploy('test-2', {
+      options: ['--notification-arns', topicArn],
+    });
+
+    // verify that the stack we deployed has our notification ARN
+    const describeResponse = await cloudFormation('describeStacks', {
+      StackName: fullStackName('test-2'),
+    });
+    expect(describeResponse.Stacks?.[0].NotificationARNs).toEqual([topicArn]);
+  } finally {
+    await sns('deleteTopic', {
+      TopicArn: topicArn,
+    });
+  }
+});
+
+test('deploy with role', async () => {
+  const roleName = `${STACK_NAME_PREFIX}-test-role`;
+
+  await deleteRole();
+
+  const createResponse = await iam('createRole', {
+    RoleName: roleName,
+    AssumeRolePolicyDocument: JSON.stringify({
+      Version: '2012-10-17',
+      Statement: [{
+        Action: 'sts:AssumeRole',
+        Principal: { Service: 'cloudformation.amazonaws.com' },
+        Effect: 'Allow',
+      }, {
+        Action: 'sts:AssumeRole',
+        Principal: { AWS: (await sts('getCallerIdentity', {})).Arn },
+        Effect: 'Allow',
+      }],
+    }),
+  });
+  const roleArn = createResponse.Role.Arn;
+  try {
+    await iam('putRolePolicy', {
+      RoleName: roleName,
+      PolicyName: 'DefaultPolicy',
+      PolicyDocument: JSON.stringify({
+        Version: '2012-10-17',
+        Statement: [{
+          Action: '*',
+          Resource: '*',
+          Effect: 'Allow',
+        }],
+      }),
+    });
+
+    await retry('Trying to assume fresh role', afterSeconds(300), async () => {
+      await sts('assumeRole', {
+        RoleArn: roleArn,
+        RoleSessionName: 'testing',
+      });
+    });
+
+    await cdkDeploy('test-2', {
+      options: ['--role-arn', roleArn],
+    });
+
+  } finally {
+    deleteRole();
+  }
+
+  async function deleteRole() {
+    try {
+      // tslint:disable-next-line: forin
+      for (const policyName of (await iam('listRolePolicies', { RoleName: roleName })).PolicyNames) {
+        await iam('deleteRolePolicy', {
+          RoleName: roleName,
+          PolicyName: policyName,
+        });
+      }
+      await iam('deleteRole', { RoleName: roleName });
+    } catch (e) {
+      if (e.message.indexOf('cannot be found') > -1) { return; }
+      throw e;
+    }
+  }
+});
+
+test('cdk diff', async () => {
+  const diff1 = await cdk(['diff', fullStackName('test-1')]);
+  expect(diff1).toContain('AWS::SNS::Topic');
+
+  const diff2 = await cdk(['diff', fullStackName('test-2')]);
+  expect(diff2).toContain('AWS::SNS::Topic');
+
+  // We can make it fail by passing --fail
+  await expect(cdk(['diff', '--fail', fullStackName('test-1')]))
+    .rejects.toThrow('exited with error');
+});
+
+test('deploy stack with docker asset', async () => {
+  await cdkDeploy('docker');
+});
+
+test('deploy and test stack with lambda asset', async () => {
+  const stackArn = await cdkDeploy('lambda', { captureStderr: false });
+
+  const response = await cloudFormation('describeStacks', {
+    StackName: stackArn,
+  });
+  const lambdaArn = response.Stacks?.[0].Outputs?.[0].OutputValue;
+  if (lambdaArn === undefined) {
+    throw new Error('Stack did not have expected Lambda ARN output');
+  }
+
+  const output = await lambda('invoke', {
+    FunctionName: lambdaArn,
+  });
+
+  expect(JSON.stringify(output.Payload)).toContain('dear asset');
+});
+
+test('cdk ls', async () => {
+  const listing = await cdk(['ls'], { captureStderr: false });
+
+  const expectedStacks = [
+    'conditional-resource',
+    'docker',
+    'docker-with-custom-file',
+    'failed',
+    'iam-test',
+    'lambda',
+    'missing-ssm-parameter',
+    'order-providing',
+    'outputs-test-1',
+    'outputs-test-2',
+    'param-test-1',
+    'param-test-2',
+    'param-test-3',
+    'termination-protection',
+    'test-1',
+    'test-2',
+    'with-nested-stack',
+    'with-nested-stack-using-parameters',
+    'order-consuming',
+  ];
+
+  for (const stack of expectedStacks) {
+    expect(listing).toContain(fullStackName(stack));
+  }
+});
+
+test('deploy stack without resource', async () => {
+  // Deploy the stack without resources
+  await cdkDeploy('conditional-resource', { modEnv: { NO_RESOURCE: 'TRUE' }});
+
+  // This should have succeeded but not deployed the stack.
+  await expect(cloudFormation('describeStacks', { StackName: fullStackName('conditional-resource') }))
+    .rejects.toThrow('conditional-resource does not exist');
+
+  // Deploy the stack with resources
+  await cdkDeploy('conditional-resource');
+
+  // Then again WITHOUT resources (this should destroy the stack)
+  await cdkDeploy('conditional-resource', { modEnv: { NO_RESOURCE: 'TRUE' } });
+
+  await expect(cloudFormation('describeStacks', { StackName: fullStackName('conditional-resource') }))
+    .rejects.toThrow('conditional-resource does not exist');
+});
+
+test('IAM diff', async () => {
+  const output = await cdk(['diff', fullStackName('iam-test')]);
+
+  // Roughly check for a table like this:
+  //
+  // ┌───┬─────────────────┬────────┬────────────────┬────────────────────────────┬───────────┐
+  // │   │ Resource        │ Effect │ Action         │ Principal                  │ Condition │
+  // ├───┼─────────────────┼────────┼────────────────┼────────────────────────────┼───────────┤
+  // │ + │ ${SomeRole.Arn} │ Allow  │ sts:AssumeRole │ Service:ec2.amazon.aws.com │           │
+  // └───┴─────────────────┴────────┴────────────────┴────────────────────────────┴───────────┘
+
+  expect(output).toContain('${SomeRole.Arn}');
+  expect(output).toContain('sts:AssumeRole');
+  expect(output).toContain('ec2.amazon.aws.com');
+});
+
+test('fast deploy', async () => {
+  // we are using a stack with a nested stack because CFN will always attempt to
+  // update a nested stack, which will allow us to verify that updates are actually
+  // skipped unless --force is specified.
+  const stackArn = await cdkDeploy('with-nested-stack', { captureStderr: false });
+  const changeSet1 = await getLatestChangeSet();
+
+  // Deploy the same stack again, there should be no new change set created
+  await cdkDeploy('with-nested-stack');
+  const changeSet2 = await getLatestChangeSet();
+  expect(changeSet2.ChangeSetId).toEqual(changeSet1.ChangeSetId);
+
+  // Deploy the stack again with --force, now we should create a changeset
+  await cdkDeploy('with-nested-stack', { options: ['--force'] });
+  const changeSet3 = await getLatestChangeSet();
+  expect(changeSet3.ChangeSetId).not.toEqual(changeSet2.ChangeSetId);
+
+  // Deploy the stack again with tags, expected to create a new changeset
+  // even though the resources didn't change.
+  await cdkDeploy('with-nested-stack', { options: ['--tags', 'key=value'] });
+  const changeSet4 = await getLatestChangeSet();
+  expect(changeSet4.ChangeSetId).not.toEqual(changeSet3.ChangeSetId);
+
+  async function getLatestChangeSet() {
+    const response = await cloudFormation('describeStacks', { StackName: stackArn });
+    if (!response.Stacks?.[0]) { throw new Error('Did not get a ChangeSet at all'); }
+    log(`Found Change Set ${response.Stacks?.[0].ChangeSetId}`);
+    return response.Stacks?.[0];
+  }
+});
+
+test('failed deploy does not hang', async () => {
+  // this will hang if we introduce https://github.com/aws/aws-cdk/issues/6403 again.
+  await expect(cdkDeploy('failed')).rejects.toThrow('exited with error');
+});
