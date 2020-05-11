5b282b330de52bfe2e62b0f1e65b626d02a909c0
Rico Huijbers <rix0rrr@gmail.com>
2020-05-11 13:38:12 +0200

    chore: fix termination protection
    Termination Protection was broken in the merge of #6366.
    
    Restore the line that had disappeared from the merge and add a unit
    test to detect this earlier in the future.

--- packages/@aws-cdk/core/lib/stack-synthesizers/_shared.ts
+++ packages/@aws-cdk/core/lib/stack-synthesizers/_shared.ts
@@ -45,6 +45,7 @@ export function addStackArtifactToAssembly(
 
   const properties: cxschema.AwsCloudFormationStackProperties = {
     templateFile: stack.templateFile,
+    terminationProtection: stack.terminationProtection,
     ...stackProps,
     ...stackNameProperty,
   };
