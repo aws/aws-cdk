7a0d70dbbff4d105bc10c3cb35c822816507e9ea
Niranjan Jayakar <nija@amazon.com>
2020-05-12 17:39:48 +0100

    switch to use fs-extra

--- tools/cfn2ts/test/index.test.ts
+++ tools/cfn2ts/test/index.test.ts
@@ -1,20 +1,15 @@
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 import * as os from 'os';
 import * as path from 'path';
-import * as util from 'util';
 import generate from '../lib';
-
-const mkdtemp = util.promisify(fs.mkdtemp);
-const exists = util.promisify(fs.exists);
-const readFile = util.promisify(fs.readFile);
 
 test('filterResourcePrefix picks the prefix as expected', async () => {
   const out = await tmpdir();
   await generate('AWS::Cognito', out, {
     filterResourcePrefix: 'UserPool',
   });
-  expect(await exists(path.join(out, 'cognito.generated.ts'))).toBeTruthy();
-  const data = await readFile(path.join(out, 'cognito.generated.ts'), { encoding: 'utf8' });
+  expect(await fs.pathExists(path.join(out, 'cognito.generated.ts'))).toBeTruthy();
+  const data = await fs.readFile(path.join(out, 'cognito.generated.ts'), { encoding: 'utf8' });
   const userPoolMatches = data.match(/CfnUserPool/);
   const identityPoolMatches = data.match(/CfnIdentityPool/);
   expect(userPoolMatches).not.toBeNull();
@@ -22,5 +17,5 @@ test('filterResourcePrefix picks the prefix as expected', async () => {
 });
 
 async function tmpdir() {
-  return await mkdtemp(path.join(os.tmpdir(), 'cfn2ts-test'));
+  return await fs.mkdtemp(path.join(os.tmpdir(), 'cfn2ts-test'));
 }
