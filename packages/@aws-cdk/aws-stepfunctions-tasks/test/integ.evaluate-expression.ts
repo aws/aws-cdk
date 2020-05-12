bde8d706cd6162323eb84ef4f848a563cd430ffb
Niranjan Jayakar <nija@amazon.com>
2020-05-12 10:44:22 +0100

    fix(stepfunctions-tasks): EvaluateExpression is limited to expressions that contain state paths (#7774)
    fixes #7655

--- packages/@aws-cdk/aws-stepfunctions-tasks/test/integ.evaluate-expression.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/test/integ.evaluate-expression.ts
@@ -2,6 +2,13 @@ import * as sfn from '@aws-cdk/aws-stepfunctions';
 import * as cdk from '@aws-cdk/core';
 import * as tasks from '../lib';
 
+/*
+ * Stack verification steps:
+ * * aws stepfunctions start-execution --input '{"a": 3, "b": 4}' --state-machine-arn <StateMachineARN>
+ * * aws stepfunctions describe-execution --execution-arn <execution-arn>
+ * * The output here should contain `status: "SUCCEEDED"` and `output: "{ a: 3, b: 4, c: 7, d: 14, now: <current date> }"
+ */
+
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'aws-stepfunctions-integ');
 
@@ -16,15 +23,27 @@ const multiply = new sfn.Task(stack, 'Multiply', {
   task: new tasks.EvaluateExpression({
     expression: '$.c * 2',
   }),
-  resultPath: '$.waitSeconds',
+  resultPath: '$.d',
 });
 
-new sfn.StateMachine(stack, 'StateMachine', {
+const now = new sfn.Task(stack, 'Now', {
+  task: new tasks.EvaluateExpression({
+    expression: '(new Date()).toUTCString()',
+  }),
+  resultPath: '$.now',
+});
+
+const statemachine = new sfn.StateMachine(stack, 'StateMachine', {
   definition: sum
     .next(multiply)
     .next(new sfn.Wait(stack, 'Wait', {
-      time: sfn.WaitTime.secondsPath('$.waitSeconds'),
-    })),
+      time: sfn.WaitTime.secondsPath('$.d'),
+    }))
+    .next(now),
+});
+
+new cdk.CfnOutput(stack, 'StateMachineARN', {
+  value: statemachine.stateMachineArn,
 });
 
 app.synth();
