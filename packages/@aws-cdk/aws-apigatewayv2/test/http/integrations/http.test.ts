9ac6aa0b908fd562a1d637e5a67b60a4a95b4fc6
Hyeonsoo David Lee <hyeonsoo.david.lee@gmail.com>
2020-05-15 18:55:29 +0900

    feat(apigatewayv2): HTTP API - configure CORS preflight (#7923)
    - ADD interface CorsPreflightOptions
    - ADD HttpApiProps.corsPreflight
    - MOD HttpApi.constructor
    - MOD README.md
    
    Closes #7922

--- packages/@aws-cdk/aws-apigatewayv2/test/http/integrations/http.test.ts
+++ packages/@aws-cdk/aws-apigatewayv2/test/http/integrations/http.test.ts
@@ -1,5 +1,6 @@
+import { ABSENT } from '@aws-cdk/assert';
 import '@aws-cdk/assert/jest';
-import { Stack } from '@aws-cdk/core';
+import { Duration, Stack } from '@aws-cdk/core';
 import { HttpApi, HttpMethod, HttpProxyIntegration, HttpRoute, HttpRouteKey } from '../../../lib';
 
 describe('HttpProxyIntegration', () => {
@@ -36,6 +37,38 @@ describe('HttpProxyIntegration', () => {
 
     expect(stack).toHaveResource('AWS::ApiGatewayV2::Integration', {
       IntegrationMethod: 'PATCH',
+    });
+  });
+
+  test('CORS Configuration is correctly configured.', () => {
+    const stack = new Stack();
+    new HttpApi(stack, 'HttpApi', {
+      corsPreflight: {
+        allowCredentials: true,
+        allowHeaders: ['Authorization'],
+        allowMethods: [HttpMethod.GET, HttpMethod.HEAD, HttpMethod.OPTIONS, HttpMethod.POST],
+        allowOrigins: ['*'],
+        maxAge: Duration.seconds(36400),
+      },
+    });
+
+    expect(stack).toHaveResource('AWS::ApiGatewayV2::Api', {
+      CorsConfiguration: {
+        AllowCredentials: true,
+        AllowHeaders: ['Authorization'],
+        AllowMethods: ['GET', 'HEAD', 'OPTIONS', 'POST'],
+        AllowOrigins: ['*'],
+        MaxAge: 36400,
+      },
+    });
+  });
+
+  test('CorsConfiguration is ABSENT when not specified.', () => {
+    const stack = new Stack();
+    new HttpApi(stack, 'HttpApi');
+
+    expect(stack).toHaveResource('AWS::ApiGatewayV2::Api', {
+      CorsConfiguration: ABSENT,
     });
   });
 });
