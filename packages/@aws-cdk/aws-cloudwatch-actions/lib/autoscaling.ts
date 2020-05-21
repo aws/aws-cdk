400ffbd0167172407a72a7bf762f76ffcd74f809
Shiv Lakshminarayan <shivlaks@amazon.com>
2020-05-21 05:06:47 -0700

    chore(cloudwatch-actions): clear linter exclusions (#8046)
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-cloudwatch-actions/lib/autoscaling.ts
+++ packages/@aws-cdk/aws-cloudwatch-actions/lib/autoscaling.ts
@@ -9,6 +9,10 @@ export class AutoScalingAction implements cloudwatch.IAlarmAction {
   constructor(private readonly stepScalingAction: autoscaling.StepScalingAction) {
   }
 
+  /**
+   * Returns an alarm action configuration to use an AutoScaling StepScalingAction
+   * as an alarm action
+   */
   public bind(_scope: cdk.Construct, _alarm: cloudwatch.IAlarm): cloudwatch.AlarmActionConfig {
     return { alarmActionArn: this.stepScalingAction.scalingPolicyArn };
   }
