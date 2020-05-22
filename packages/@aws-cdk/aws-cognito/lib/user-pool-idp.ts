05a9e5321405afb435d3952f8b7c01915521f219
Niranjan Jayakar <nija@amazon.com>
2020-05-22 16:00:56 +0100

    drop apple

--- packages/@aws-cdk/aws-cognito/lib/user-pool-idp.ts
+++ packages/@aws-cdk/aws-cognito/lib/user-pool-idp.ts
@@ -127,68 +127,6 @@ export class UserPoolAmazonIdentityProvider extends Resource implements IUserPoo
 }
 
 /**
- * Properties to initialize UserPoolAppleIdentityProvider
- */
-export interface UserPoolAppleIdentityProviderProps {
-  /**
-   * The user pool to which this construct provides identities.
-   */
-  readonly userPool: IUserPool;
-
-  /**
-   * The Services id received when the 'Sign in with Apple' client was created.
-   */
-  readonly servicesId: string;
-  /**
-   * The team id received when the 'Sign in with Apple' client was created.
-   */
-  readonly teamId: string;
-  /**
-   * The key id received when the 'Sign in with Apple' client was created.
-   */
-  readonly keyId: string;
-  /**
-   * The private key received when the 'Sign in with Apple' client was created.
-   */
-  readonly privateKey: string;
-  /**
-   * The types of user profile data to obtain for the Amazon profile.
-   * @see https://developer.amazon.com/docs/login-with-amazon/customer-profile.html
-   * @default [ public_profile, email ]
-   */
-  readonly scopes?: string[];
-}
-
-/**
- * Represents a identity provider that integrates with 'Login with Amazon'
- * @resource AWS::Cognito::UserPoolIdentityProvider
- */
-export class UserPoolAppleIdentityProvider extends Resource implements IUserPoolIdentityProvider {
-  public readonly providerName: string;
-
-  constructor(scope: Construct, id: string, props: UserPoolAppleIdentityProviderProps) {
-    super(scope, id);
-
-    const scopes = props.scopes ?? [ 'public_profile', 'email' ];
-
-    const resource = new CfnUserPoolIdentityProvider(this, 'Resource', {
-      userPoolId: props.userPool.userPoolId,
-      providerName: 'SignInWithApple', // must be 'SignInWithApple' when the type is 'SignInWithApple'
-      providerType: 'SignInWithApple',
-      providerDetails: {
-        client_id: props.servicesId,
-        team_id: props.teamId,
-        key_id: props.keyId,
-        private_key: props.privateKey,
-        authorize_scopes: scopes.join(' '),
-      },
-    });
-
-    this.providerName = super.getResourceNameAttribute(resource.ref);
-  }
-}
-
-/**
  * Options to integrate with the various social identity providers.
  */
 export class UserPoolIdentityProvider {
@@ -206,14 +144,6 @@ export class UserPoolIdentityProvider {
    */
   public static amazon(scope: Construct, id: string, options: UserPoolAmazonIdentityProviderProps) {
     return new UserPoolAmazonIdentityProvider(scope, id, options);
-  }
-
-  /**
-   * Federate with 'Sign in with Apple'
-   * @see https://developer.apple.com/sign-in-with-apple/
-   */
-  public static apple(scope: Construct, id: string, options: UserPoolAppleIdentityProviderProps) {
-    return new UserPoolAppleIdentityProvider(scope, id, options);
   }
 
   private constructor() {}
