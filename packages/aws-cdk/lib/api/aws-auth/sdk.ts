2cad3ccfdc611370b178f4f9ab6f1f11a9e4fcea
Rico Huijbers <rix0rrr@gmail.com>
2020-05-25 15:24:43 +0200

    fix(cli): paper cuts (#8164)
    Fixing a number of small paper cuts in the CLI. Specifically:
    
    - When using `--cloudformation-execution-policies` or `--trust`, the
      positional argument that follows (typically an environment name) would
      be ignored, because of the way we configure yargs. Make it so that the
      options takes a single argument, and must be repeated for multiple
      arguments, making it a lot easier to use.
    
    - When a stack fails to create and is destroyed before being redeployed,
      the `deployStack()` routine would forget that the stack had been
      deleted and attempt to create a change set to update the stack, which
      would promptly fail. Remember we deleted the stack, so that we'll
      create a changeset to create a new one.
    
    - When a stack fails to create the first time, and the next deploy uses
      the same template, the "skip deploy" optimization we introduced to
      speed up deployment of stacks with nested stacks incorrectly skips
      the deployment.
    
    - Wrap the SDK objects, and when an AWS fails output information about
      the call that failed. Due to a lack of stack traces in NodeJS, it
      would otherwise be very hard to figure out where the error was
      happening.
    
    - Using the SDK wrapper, when the error looks like it's an error in 
      assuming a role, replace it with an error message that describes the
      most probable cause: missing role/failure to bootstrap.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk/lib/api/aws-auth/sdk.ts
+++ packages/aws-cdk/lib/api/aws-auth/sdk.ts
@@ -64,27 +64,27 @@ export class SDK implements ISDK {
   }
 
   public cloudFormation(): AWS.CloudFormation {
-    return new AWS.CloudFormation(this.config);
+    return wrapServiceErrorHandling(new AWS.CloudFormation(this.config));
   }
 
   public ec2(): AWS.EC2 {
-    return new AWS.EC2(this.config);
+    return wrapServiceErrorHandling(new AWS.EC2(this.config));
   }
 
   public ssm(): AWS.SSM {
-    return new AWS.SSM(this.config);
+    return wrapServiceErrorHandling(new AWS.SSM(this.config));
   }
 
   public s3(): AWS.S3 {
-    return new AWS.S3(this.config);
+    return wrapServiceErrorHandling(new AWS.S3(this.config));
   }
 
   public route53(): AWS.Route53 {
-    return new AWS.Route53(this.config);
+    return wrapServiceErrorHandling(new AWS.Route53(this.config));
   }
 
   public ecr(): AWS.ECR {
-    return new AWS.ECR(this.config);
+    return wrapServiceErrorHandling(new AWS.ECR(this.config));
   }
 
   public async currentAccount(): Promise<Account> {
@@ -103,4 +103,113 @@ export class SDK implements ISDK {
   }
 }
 
+/**
+ * Return a wrapping object for the underlying service object
+ *
+ * Responds to failures in the underlying service calls, in two different
+ * ways:
+ *
+ * - When errors are encountered, log the failing call and the error that
+ *   it triggered (at debug level). This is necessary because the lack of
+ *   stack traces in NodeJS otherwise makes it very hard to suss out where
+ *   a certain AWS error occurred.
+ * - The JS SDK has a funny business of wrapping any credential-based error
+ *   in a super-generic (and in our case wrong) exception. If we then use a
+ *   'ChainableTemporaryCredentials' and the target role doesn't exist,
+ *   the error message that shows up by default is super misleading
+ *   (https://github.com/aws/aws-sdk-js/issues/3272). We can fix this because
+ *   the exception contains the "inner exception", so we unwrap and throw
+ *   the correct error ("cannot assume role").
+ *
+ * The wrapping business below is slightly more complicated than you'd think
+ * because we must hook into the `promise()` method of the object that's being
+ * returned from the methods of the object that we wrap, so there's two
+ * levels of wrapping going on, and also some exceptions to the wrapping magic.
+ */
+function wrapServiceErrorHandling<A extends object>(serviceObject: A): A {
+  const classObject = serviceObject.constructor.prototype;
+
+  return new Proxy(serviceObject, {
+    get(obj: A, prop: string) {
+      const real = (obj as any)[prop];
+      // Things we don't want to intercept:
+      // - Anything that's not a function.
+      // - 'constructor', s3.upload() will use this to do some magic and we need the underlying constructor.
+      // - Any method that's not on the service class (do not intercept 'makeRequest' and other helpers).
+      if (prop === 'constructor' || !classObject.hasOwnProperty(prop) || !isFunction(real)) { return real; }
+
+      // NOTE: This must be a function() and not an () => {
+      // because I need 'this' to be dynamically bound and not statically bound.
+      // If your linter complains don't listen to it!
+      return function(this: any) {
+        // Call the underlying function. If it returns an object with a promise()
+        // method on it, wrap that 'promise' method.
+        const args = [].slice.call(arguments, 0);
+        const response = real.apply(this, args);
+
+        // Don't intercept unless the return value is an object with a '.promise()' method.
+        if (typeof response !== 'object' || !response) { return response; }
+        if (!('promise' in response)) { return response; }
+
+        // Return an object with the promise method replaced with a wrapper which will
+        // do additional things to errors.
+        return Object.assign(Object.create(response), {
+          promise() {
+            return response.promise().catch((e: Error) => {
+              e = makeDetailedException(e);
+              debug(`Call failed: ${prop}(${JSON.stringify(args[0])}) => ${e.message}`);
+              return Promise.reject(e); // Re-'throw' the new error
+            });
+          },
+        });
+      };
+    },
+  });
+}
+
 const CURRENT_ACCOUNT_KEY = Symbol('current_account_key');
+
+function isFunction(x: any): x is (...args: any[]) => any {
+  return x && {}.toString.call(x) === '[object Function]';
+}
+
+/**
+ * Extract a more detailed error out of a generic error if we can
+ */
+function makeDetailedException(e: Error): Error {
+  // This is the super-generic "something's wrong" error that the JS SDK wraps other errors in.
+  // https://github.com/aws/aws-sdk-js/blob/f0ac2e53457c7512883d0677013eacaad6cd8a19/lib/event_listeners.js#L84
+  if (e.message.startsWith('Missing credentials in config')) {
+    const original = (e as any).originalError;
+    if (original) {
+      // When the SDK does a 'util.copy', they lose the Error-ness of the inner error
+      // (they copy the Error's properties into a plain object) so make it an Error object again.
+      e = Object.assign(new Error(), original);
+    }
+  }
+
+  // At this point, the error might still be a generic "ChainableTemporaryCredentials failed"
+  // error which wraps the REAL error (AssumeRole failed). We're going to replace the error
+  // message with one that's more likely to help users, and tell them the most probable
+  // fix (bootstrapping). The underlying service call failure will be appended below.
+  if (e.message === 'Could not load credentials from ChainableTemporaryCredentials') {
+    e.message = 'Could not assume role in target account (did you bootstrap the environment with the right \'--trust\'s?)';
+  }
+
+  // Replace the message on this error with a concatenation of all inner error messages.
+  // Must more clear what's going on that way.
+  e.message = allChainedExceptionMessages(e);
+  return e;
+}
+
+/**
+ * Return the concatenated message of all exceptions in the AWS exception chain
+ */
+function allChainedExceptionMessages(e: Error | undefined) {
+  const ret = new Array<string>();
+  while (e) {
+    ret.push(e.message);
+    e = (e as any).originalError;
+  }
+  return ret.join(': ');
+}
