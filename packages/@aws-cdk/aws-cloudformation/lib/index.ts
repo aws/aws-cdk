659127fd6eb1aa8c02793cd50f89be2740ad6d1a
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2020-06-07 22:54:33 +0200

    restore cloudformation package to original state

--- packages/@aws-cdk/aws-cloudformation/lib/index.ts
+++ packages/@aws-cdk/aws-cloudformation/lib/index.ts
@@ -1,8 +1,6 @@
 export * from './cloud-formation-capabilities';
 export * from './custom-resource';
 export * from './nested-stack';
-export * from './resource-provider';
-export * from './cfncli-layer';
 
 // AWS::CloudFormation CloudFormation Resources:
 export * from './cloudformation.generated';
