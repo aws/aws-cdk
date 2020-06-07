420e6c9e741d7fd000ff7b3a766b124a72909d26
Elad Ben-Israel <benisrae@amazon.com>
2020-06-07 12:46:38 +0300

    update stack `env` description

--- packages/@aws-cdk/core/lib/stack.ts
+++ packages/@aws-cdk/core/lib/stack.ts
@@ -28,39 +28,65 @@ export interface StackProps {
    * The AWS environment (account/region) where this stack will be deployed.
    *
    * Set the `region`/`account` fields of `env` to either a concrete value to
-   * select the indicated environment (recommended for production stacks), or
-   * to the values of environment variables
+   * select the indicated environment (recommended for production stacks), or to
+   * the values of environment variables
    * `CDK_DEFAULT_REGION`/`CDK_DEFAULT_ACCOUNT` to let the target environment
    * depend on the AWS credentials/configuration that the CDK CLI is executed
    * under (recommended for development stacks).
    *
-   * If the `Stack` is instantiated inside a `Stage` construct, any undefined
-   * `region`/`account` fields from `env` will default to the same field on
-   * the encompassing `Stage`, if configured there.
+   * If the `Stack` is instantiated inside a `Stage`, any undefined
+   * `region`/`account` fields from `env` will default to the same field on the
+   * encompassing `Stage`, if configured there.
    *
-   * If either of `region`/`account` is not set nor inherited from `Stage`,
-   * the Stack will be *environment-agnostic*.
-   *
-   * Environment-agnostic stacks can be deployed to any environment, may not be
-   * able to take advantage of all features of the CDK. For example, they will
-   * not be able to use environmental context lookups, will not automatically
-   * translate Service Principals to the right format based on the environment's
-   * AWS partition, and other such enhancements.
+   * If either `region` or `account` are not set nor inherited from `Stage`, the
+   * Stack will be considered "*environment-agnostic*"". Environment-agnostic
+   * stacks can be deployed to any environment but may not be able to take
+   * advantage of all features of the CDK. For example, they will not be able to
+   * use environmental context lookups such as `ec2.Vpc.fromLookup` and will not
+   * automatically translate Service Principals to the right format based on the
+   * environment's AWS partition, and other such enhancements.
    *
    * @example
    *
-   * // Use a concrete account and region to deploy this stack to
+   * // Use a concrete account and region to deploy this stack to:
+   * // `.account` and `.region` will simply return these values.
    * new MyStack(app, 'Stack1', {
-   *   env: { account: '123456789012', region: 'us-east-1' },
+   *   env: {
+   *     account: '123456789012',
+   *     region: 'us-east-1'
+   *   },
    * });
    *
-   * // Use the CLI's current credentials to determine the target environment
+   * // Use the CLI's current credentials to determine the target environment:
+   * // `.account` and `.region` will reflect the account+region the CLI
+   * // is configured to use (based on the user CLI credentials)
    * new MyStack(app, 'Stack2', {
-   *   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+   *   env: {
+   *     account: process.env.CDK_DEFAULT_ACCOUNT,
+   *     region: process.env.CDK_DEFAULT_REGION
+   *   },
    * });
    *
-   * @default - The environment of the containing `Stage` if available, otherwise create
-   * the stack will be environment-agnostic.
+   * // Define multiple stacks stage associated with an environment
+   * const myStage = new Stage(app, 'MyStage', {
+   *   env: {
+   *     account: '123456789012',
+   *     region: 'us-east-1'
+   *   }
+   * });
+   *
+   * // both of these stavks will use the stage's account/region:
+   * // `.account` and `.region` will resolve to the concrete values as above
+   * new MyStack(myStage, 'Stack1');
+   * new YourStack(myStage, 'Stack1');
+   *
+   * // Define an environment-agnostic stack:
+   * // `.account` and `.region` will resolve to `{ "Ref": "AWS::AccountId" }` and `{ "Ref": "AWS::Region" }` respectively.
+   * // which will only resolve to actual values by CloudFormation during deployment.
+   * new MyStack(app, 'Stack1');
+   *
+   * @default - The environment of the containing `Stage` if available,
+   * otherwise create the stack will be environment-agnostic.
    */
   readonly env?: Environment;
 
