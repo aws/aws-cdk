3546251b85e76121efb889c01c84621fd9799522
Eduardo de Moura Rodrigues <eduardomourar@users.noreply.github.com>
2020-06-11 09:04:02 +0200

    feat(eks): timeout option helm charts (#8338)
    This creates an additional option called `timeout` that will be passed down whenever deploying helm chart to an EKS cluster.
    
    In order to allow the timeout parameter to work while performing helm commands, the provider framework has to honor the maximum timeout of 15 minutes from target process (lambda in this case).
    
    closes #8215 
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-eks/lib/helm-chart.ts
+++ packages/@aws-cdk/aws-eks/lib/helm-chart.ts
@@ -1,4 +1,4 @@
-import { Construct, CustomResource, Stack } from '@aws-cdk/core';
+import { Construct, CustomResource, Duration, Stack } from '@aws-cdk/core';
 import { Cluster } from './cluster';
 
 /**
@@ -47,6 +47,12 @@ export interface HelmChartOptions {
    * @default - Helm will not wait before marking release as successful
    */
   readonly wait?: boolean;
+
+  /**
+   * Amount of time to wait for any individual Kubernetes operation. Maximum 15 minutes.
+   * @default Duration.minutes(5)
+   */
+  readonly timeout?: Duration;
 }
 
 /**
@@ -68,7 +74,7 @@ export interface HelmChartProps extends HelmChartOptions {
  */
 export class HelmChart extends Construct {
   /**
-   * The CloudFormation reosurce type.
+   * The CloudFormation resource type.
    */
   public static readonly RESOURCE_TYPE = 'Custom::AWSCDK-EKS-HelmChart';
 
@@ -78,6 +84,11 @@ export class HelmChart extends Construct {
     const stack = Stack.of(this);
 
     const provider = props.cluster._kubectlProvider;
+
+    const timeout = props.timeout?.toSeconds();
+    if (timeout && timeout > 900) {
+      throw new Error('Helm chart timeout cannot be higher than 15 minutes.');
+    }
 
     new CustomResource(this, 'Resource', {
       serviceToken: provider.serviceToken,
@@ -89,6 +100,7 @@ export class HelmChart extends Construct {
         Chart: props.chart,
         Version: props.version,
         Wait: props.wait || false,
+        Timeout: timeout,
         Values: (props.values ? stack.toJsonString(props.values) : undefined),
         Namespace: props.namespace || 'default',
         Repository: props.repository,
