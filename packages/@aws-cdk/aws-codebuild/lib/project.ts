4e7ed9d3c1d55396a985b8af00286adbca2c392a
Ajay Karpur <akarpur@amazon.com>
2020-06-11 15:02:51 -0700

    add docstrings to every LinuxGpuBuildImage constant

--- packages/@aws-cdk/aws-codebuild/lib/project.ts
+++ packages/@aws-cdk/aws-codebuild/lib/project.ts
@@ -1435,43 +1435,57 @@ export class LinuxBuildImage implements IBuildImage {
  * @see https://aws.amazon.com/releasenotes/available-deep-learning-containers-images/
  */
 export class LinuxGpuBuildImage implements IBuildImage {
-  /** Tensorflow GPU images from AWS Deep Learning Containers. */
+  /** Tensorflow 1.14.0 GPU image from AWS Deep Learning Containers. */
   public static readonly DLC_TENSORFLOW_1_14_0 = LinuxGpuBuildImage.DlcImage('tensorflow-training',
     '1.14.0-gpu-py36-cu100-ubuntu16.04');
+  /** Tensorflow 1.15.0 GPU image from AWS Deep Learning Containers. */
   public static readonly DLC_TENSORFLOW_1_15_0 = LinuxGpuBuildImage.DlcImage('tensorflow-training',
     '1.15.0-gpu-py36-cu100-ubuntu18.04');
+  /** Tensorflow 1.15.2 GPU training image from AWS Deep Learning Containers. */
   public static readonly DLC_TENSORFLOW_1_15_2_TRAINING = LinuxGpuBuildImage.DlcImage('tensorflow-training',
     '1.15.2-gpu-py37-cu100-ubuntu18.04');
+  /** Tensorflow 1.15.2 GPU inference image from AWS Deep Learning Containers. */
   public static readonly DLC_TENSORFLOW_1_15_2_INFERENCE = LinuxGpuBuildImage.DlcImage('tensorflow-inference',
     '1.15.2-gpu-py36-cu100-ubuntu18.04');
+  /** Tensorflow 2.0.0 GPU image from AWS Deep Learning Containers. */
   public static readonly DLC_TENSORFLOW_2_0_0 = LinuxGpuBuildImage.DlcImage('tensorflow-training',
     '2.0.0-gpu-py36-cu100-ubuntu18.04');
+  /** Tensorflow 2.0.1 GPU image from AWS Deep Learning Containers. */
   public static readonly DLC_TENSORFLOW_2_0_1 = LinuxGpuBuildImage.DlcImage('tensorflow-training',
     '2.0.1-gpu-py36-cu100-ubuntu18.04');
+  /** Tensorflow 2.1.0 GPU training image from AWS Deep Learning Containers. */
   public static readonly DLC_TENSORFLOW_2_1_0_TRAINING = LinuxGpuBuildImage.DlcImage('tensorflow-training',
     '2.1.0-gpu-py36-cu101-ubuntu18.04');
+  /** Tensorflow 2.1.0 GPU inference image from AWS Deep Learning Containers. */
   public static readonly DLC_TENSORFLOW_2_1_0_INFERENCE = LinuxGpuBuildImage.DlcImage('tensorflow-inference',
     '2.1.0-gpu-py36-cu101-ubuntu18.04');
+  /** Tensorflow 2.2.0 GPU training image from AWS Deep Learning Containers. */
   public static readonly DLC_TENSORFLOW_2_2_0_TRAINING = LinuxGpuBuildImage.DlcImage('tensorflow-training',
     '2.2.0-gpu-py37-cu101-ubuntu18.04');
 
-  /** PyTorch GPU images from AWS Deep Learning Containers. */
+  /** PyTorch 1.2.0 GPU image from AWS Deep Learning Containers. */
   public static readonly DLC_PYTORCH_1_2_0 = LinuxGpuBuildImage.DlcImage('pytorch-training',
     '1.2.0-gpu-py36-cu100-ubuntu16.04');
+  /** PyTorch 1.3.1 GPU image from AWS Deep Learning Containers. */
   public static readonly DLC_PYTORCH_1_3_1 = LinuxGpuBuildImage.DlcImage('pytorch-training',
     '1.3.1-gpu-py36-cu101-ubuntu16.04');
+  /** PyTorch 1.4.0 GPU training image from AWS Deep Learning Containers. */
   public static readonly DLC_PYTORCH_1_4_0_TRAINING = LinuxGpuBuildImage.DlcImage('pytorch-training',
     '1.4.0-gpu-py36-cu101-ubuntu16.04');
+  /** PyTorch 1.4.0 GPU inference image from AWS Deep Learning Containers. */
   public static readonly DLC_PYTORCH_1_4_0_INFERENCE = LinuxGpuBuildImage.DlcImage('pytorch-inference',
     '1.4.0-gpu-py36-cu101-ubuntu16.04');
+  /** PyTorch 1.5.0 GPU training image from AWS Deep Learning Containers. */
   public static readonly DLC_PYTORCH_1_5_0_TRAINING = LinuxGpuBuildImage.DlcImage('pytorch-training',
     '1.5.0-gpu-py36-cu101-ubuntu16.04');
+  /** PyTorch 1.5.0 GPU inference image from AWS Deep Learning Containers. */
   public static readonly DLC_PYTORCH_1_5_0_INFERENCE = LinuxGpuBuildImage.DlcImage('pytorch-inference',
     '1.5.0-gpu-py36-cu101-ubuntu16.04');
 
-  /** MXNet GPU images from AWS Deep Learning Containers. */
+  /** MXNet 1.4.1 GPU image from AWS Deep Learning Containers. */
   public static readonly DLC_MXNET_1_4_1 = LinuxGpuBuildImage.DlcImage('mxnet-training',
     '1.4.1-gpu-py36-cu100-ubuntu16.04');
+  /** MXNet 1.6.0 GPU image from AWS Deep Learning Containers. */
   public static readonly DLC_MXNET_1_6_0 = LinuxGpuBuildImage.DlcImage('mxnet-training',
     '1.6.0-gpu-py36-cu101-ubuntu16.04');
 
