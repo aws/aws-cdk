152ac3af35a16102f23d54ecd2eb21657e5b6a1e
Pahud Hsieh <pahudnet@gmail.com>
2020-06-17 05:04:42 +0800

    feat(globalaccelerator): support Accelerator, Listener and EndpointGroup (#8221)
    feat(globalaccelerator): support Accelerator, Listener and EndpointGroup
    
    - Add `Accelerator`, `Listener` and `EndpointGroup` resources
    - support `addEndpoint`, `addLoadBalancer`, `addElasticIpAddress` and `addEc2Instance` to add any supported endpoint into the `EndpointGroup`
    
    Closes #5527 
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-globalaccelerator/test/integ.globalaccelerator.ts
@@ -0,0 +1,48 @@
+
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as cdk from '@aws-cdk/core';
+import * as ga from '../lib';
+import * as testfixture from './util';
+
+class GaStack extends testfixture.TestStack {
+  constructor(scope: cdk.Construct, id: string) {
+    super(scope, id);
+
+    const vpc = new ec2.Vpc(this, 'VPC', { maxAzs: 3, natGateways: 1});
+    const accelerator = new ga.Accelerator(this, 'Accelerator');
+    const listener = new ga.Listener(this, 'Listener', {
+      accelerator,
+      portRanges: [
+        {
+          fromPort: 80,
+          toPort: 80,
+        },
+      ],
+    });
+    const endpointGroup = new ga.EndpointGroup(this, 'Group', { listener });
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'ALB', { vpc, internetFacing: true  });
+    const nlb = new elbv2.NetworkLoadBalancer(this, 'NLB', { vpc, internetFacing: true });
+    const eip = new ec2.CfnEIP(this, 'ElasticIpAddress');
+    const instances = new Array<ec2.Instance>();
+
+    for ( let i = 0; i < 2; i++) {
+      instances.push(new ec2.Instance(this, `Instance${i}`, {
+        vpc,
+        machineImage: new ec2.AmazonLinuxImage(),
+        instanceType: new ec2.InstanceType('t3.small'),
+      }));
+    }
+
+    endpointGroup.addLoadBalancer('AlbEndpoint', alb);
+    endpointGroup.addLoadBalancer('NlbEndpoint', nlb);
+    endpointGroup.addElasticIpAddress('EipEndpoint', eip);
+    endpointGroup.addEc2Instance('InstanceEndpoint', instances[0]);
+    endpointGroup.addEndpoint('InstanceEndpoint2', instances[1].instanceId);
+
+  }
+}
+
+const app = new cdk.App();
+
+new GaStack(app, 'integ-globalaccelerator');
