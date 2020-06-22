fce68aa25ddcde1d280237b579efe92fbca7f3a9
Calvin Combs <comcalvi@amazon.com>
2020-06-22 17:13:58 -0400

    commiting before pulling

--- packages/@aws-cdk/cloudformation-include/lib/file-utils.ts
+++ packages/@aws-cdk/cloudformation-include/lib/file-utils.ts
@@ -1,6 +1,12 @@
 import * as fs from 'fs';
+import * as YAML from 'yaml';
 
 export function readJsonSync(filePath: string): any {
   const fileContents = fs.readFileSync(filePath);
   return JSON.parse(fileContents.toString());
+}
+
+export function readYamlSync(filePath: string): any {
+  const fileContents = fs.readFileSync(filePath);
+  return YAML.parse(fileContents.toString());
 }
