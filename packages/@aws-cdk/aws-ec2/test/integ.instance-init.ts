6f7a2f3280bbd6bd84aacbe5a6a66a55842cb6cf
Rico Huijbers <rix0rrr@gmail.com>
2020-07-03 10:36:56 +0200

    Add integ test

---
+++ packages/@aws-cdk/aws-ec2/test/integ.instance-init.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import * as ec2 from '../lib';
+import * as cdk from '@aws-cdk/core';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'integ-init', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
+
+const vpc = ec2.Vpc.fromLookup(stack, 'VPC', { isDefault: true });
+
+new ec2.Instance(stack, 'Instance', {
+  vpc,
+  vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+  instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+  machineImage: new ec2.AmazonLinuxImage(),
+  init: ec2.CloudFormationInit.fromElements(
+    ec2.InitCommand.shellCommand('echo hello'),
+  ),
+});
+
+app.synth();
