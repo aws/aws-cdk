ac32eb8081749923c365b58d89ea1274e5defbeb
epolon <epolon@amazon.com>
2020-07-06 11:59:21 +0300

    remove duplicate function

--- packages/aws-cdk/test/integ/helpers.js
+++ packages/aws-cdk/test/integ/helpers.js
@@ -32,37 +32,6 @@ module.exports.fetchSupplantVersion = async function(base) {
 
 }
 
-module.exports.fetchIntegrationTests = async function(base) {
-
-  const baseWithoutPre = base.split('-')[0];
-
-  const token = process.env.GITHUB_TOKEN;
-
-  if (!token) {
-    throw new Error('GITHUB_TOKEN must be set');
-  }
-
-  const github = new Octokit({ auth: token  });
-
-  const releases = await github.repos.listReleases({
-    owner: 'aws',
-    repo: 'aws-cdk',
-  });
-
-  // this returns a list in decsending order, newest releases first
-  for (const release of releases.data) {
-
-    const version = release.name.replace('v', '');
-
-    if (semver.lt(version, baseWithoutPre)) {
-      return version;
-    }
-  }
-
-  return undefined;
-
-}
-
 require('make-runnable/custom')({
   printOutputFrame: false,
 })
