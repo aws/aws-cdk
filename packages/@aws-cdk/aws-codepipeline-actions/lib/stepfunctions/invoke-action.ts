4360d285b326ae0db1ec74ae53cb874da6d28ead
Madhumitran Loganathan <madhulog@amazon.com>
2020-07-07 14:58:05 -0700

    feat: allow stepfunctions invoke action
    this feature enables developers using the cdk
    to create stepfunction invoke action in their stacks.
    
    verified the changes using unit tests.
    
    Reviewed by: sainsbm

---
+++ packages/@aws-cdk/aws-codepipeline-actions/lib/stepfunctions/invoke-action.ts
@@ -0,0 +1,149 @@
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as iam from '@aws-cdk/aws-iam';
+import * as stepfunction from '@aws-cdk/aws-stepfunctions';
+import { Construct} from '@aws-cdk/core';
+import { Action } from '../action';
+ 
+/**
+ * Represents the input type for the StateMachine.
+ */
+export enum StateMachineInputType {
+  /**
+   * When specified, the value in the Input field is passed 
+   * directly to the state machine input.
+   */
+  LITERAL = 'Literal',
+
+  /**
+   * The contents of a file in the input artifact specified by the Input field 
+   * is used as the input for the state machine execution. 
+   * Input artifact and FilePath is required when InputType is set to FilePath.
+   */
+  FILEPATH = 'FilePath',
+}
+
+/**
+ * Construction properties of the {@link StepFunctionsInvokeAction StepFunction Invoke Action}.
+ */
+export interface StepFunctionsInvokeActionProps extends codepipeline.CommonAwsActionProps {
+  /**
+   * The optional input Artifact of the Action.
+   * If InputType is set to FilePath, this artifact is required 
+   * and is used to source the input for the state machine execution.
+   * 
+   * @default the Action will not have any inputs
+   * @see https://docs.aws.amazon.com/codepipeline/latest/userguide/action-reference-StepFunctions.html#action-reference-StepFunctions-example
+   */
+  readonly input?: codepipeline.Artifact;
+ 
+  /**
+   * The optional output Artifact of the Action.
+   *
+   * @default the Action will not have any outputs
+   */
+  readonly output?: codepipeline.Artifact;
+ 
+  /**
+   * The state machine to invoke.
+   */
+  readonly stateMachine: stepfunction.IStateMachine;
+ 
+  /**
+   * Optional StateMachine InputType
+   * InputType can be Literal or FilePath
+   *
+   * @default - Literal
+   */
+  readonly stateMachineInputType?: StateMachineInputType;
+ 
+  /**y
+   * When InputType is set to Literal (default), this field is optional.
+   * If provided, the Input field is used directly as the input for the state machine execution.
+   * Otherwise, the state machine is invoked with an empty JSON object {}.
+   * 
+   * When InputType is set to FilePath, this field is required.
+   * An input artifact is also required when InputType is set to FilePath.
+   * 
+   * @default - none
+   */
+  readonly stateMachineInput?: string | object;
+ 
+ 
+  /**
+   * Prefix (optinoal)
+   *
+   * By default, the action execution ID is used as the state machine execution name. 
+   * If a prefix is provided, it is prepended to the action execution ID with a hyphen and 
+   * together used as the state machine execution name.
+   */
+  readonly executionPrefixName?: string;
+}
+ 
+/**
+ * CodePipeline invoke Action that is provided by an AWS Step Function.
+ */
+export class StepFunctionsInvokeAction extends Action {
+  private readonly props: StepFunctionsInvokeActionProps;
+ 
+  constructor(props: StepFunctionsInvokeActionProps) {
+    super({
+      ...props,
+      resource: props.stateMachine,
+      category: codepipeline.ActionCategory.INVOKE,
+      provider: 'StepFunctions',
+      artifactBounds: {
+        minInputs: 0,
+        maxInputs: 1,
+        minOutputs: 0,
+        maxOutputs: 1,
+      },
+    });
+
+    //StateMachineInput is required when the StateMachineInputType is set as FilePath
+    if ( props.stateMachineInputType == StateMachineInputType.FILEPATH && (!props.stateMachineInput)) {
+      throw new Error('Input type FilePath was specified, but no filepath was provided');
+    }
+
+    //Input Artifact is required when the StateMachineInputType is set as FilePath
+    if ( props.stateMachineInputType == StateMachineInputType.FILEPATH && (!this.actionProperties.inputs)) {
+      throw new Error('Input type FilePath was specified, but no input artifact was provided');
+    }
+ 
+    this.props = props;
+  }
+ 
+  protected bound(_scope: Construct, _stage: codepipeline.IStage, options: codepipeline.ActionBindOptions):
+  codepipeline.ActionConfig {
+    // allow pipeline to invoke this step function
+    options.role.addToPolicy(new iam.PolicyStatement({
+      actions: ['states:StartExecution','states:DescribeStateMachine'],
+      resources: [this.props.stateMachine.stateMachineArn],
+    }));
+ 
+    // allow action executions to be inspected and invoked
+    options.role.addToPolicy(new iam.PolicyStatement({
+      actions: ['states:DescribeExecution'],
+      //TODO: find statemachinename from arn. ARN is part of interface IStateMachine which doesn't have the name property. Confirm with Matt
+      resources: [`arn:aws:states:*:*:execution:${this.props.stateMachine.stateMachineArn}:${this.props.executionPrefixName || ''}*`],
+    }));
+ 
+    this.props.stateMachine.grantStartExecution(options.role);
+ 
+    // allow the Role access to the Bucket, if there are any inputs/outputs
+    if ((this.actionProperties.inputs ?? []).length > 0) {
+      options.bucket.grantRead(options.role);
+    }
+    if ((this.actionProperties.outputs || []).length > 0) {
+      options.bucket.grantWrite(options.role);
+    }
+
+    return {
+      configuration: {
+        StateMachineArn: this.props.stateMachine.stateMachineArn,
+        Input: this.props.stateMachineInput,
+        InputType: this.props.stateMachineInputType,
+        ExecutionNamePrefix: this.props.executionPrefixName,
+      },
+    };
+  }
+}
---
+++ packages/@aws-cdk/aws-codepipeline-actions/test/stepfunctions/test.stepfunctions-invoke-actions.ts
@@ -0,0 +1,175 @@
+import { expect, haveResourceLike, not } from '@aws-cdk/assert';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as stepfunction from '@aws-cdk/aws-stepfunctions';
+import { Stack } from '@aws-cdk/core';
+import { Test } from 'nodeunit';
+import * as cpactions from '../../lib';
+import { StateMachineInputType } from '../../lib';
+
+export = {
+  'StepFunctions Invoke Action': {
+    'Verify stepfunction configuration properties are set to specific values'(test: Test) {
+      const stack = new Stack();
+
+      //when
+      minimalPipeline(stack, undefined);
+
+      //then
+      expect(stack).to(haveResourceLike('AWS::CodePipeline::Pipeline', {
+        'Stages': [
+          //Must have a source stage
+          {
+            'Actions': [
+              {
+                ActionTypeId: {
+                  Category: 'Source',
+                  Owner: 'AWS',
+                  Provider: 'S3',
+                  Version: '1',
+                },
+                'Configuration': {
+                  S3Bucket: {
+                    Ref: 'MyBucketF68F3FF0',
+                  },
+                  S3ObjectKey: 'some/path/to',
+                },
+              },
+              
+            ],
+          },
+          // Must have stepfunction invoke action configuration
+          {
+            'Actions': [
+              {
+                ActionTypeId: {
+                  Category: 'Invoke',
+                  Owner: 'AWS',
+                  Provider: 'StepFunctions',
+                  Version: '1',
+                },
+                'Configuration': {
+                  StateMachineArn: {
+                    Ref: 'SimpleStateMachineE8E2CF40',
+                  },
+                  InputType: 'Literal',
+                },
+              },
+            ],
+          },
+        ],
+      }));
+
+      expect(stack).to(not(haveResourceLike('AWS::Events::Rule')));
+
+      test.done();
+    }, 
+
+    'Allows the pipeline to invoke this stepfunction'(test: Test) {
+      const stack = new Stack();
+
+      minimalPipeline(stack, undefined);
+      
+      expect(stack).to(haveResourceLike('AWS::IAM::Policy', {
+        'PolicyDocument': {
+          'Statement': [
+            {
+              'Action': ['states:StartExecution','states:DescribeStateMachine'],
+              'Resource': {
+                'Ref': 'SimpleStateMachineE8E2CF40',
+              },
+              'Effect': 'Allow',
+            },
+          ],
+        },
+      }));
+
+      expect(stack).to(haveResourceLike('AWS::IAM::Role'));
+
+      test.done();
+    }, 
+
+    'does not allow passing empty StateMachineInput when the StateMachineInputType is FilePath'(test: Test) {
+      const stack = new Stack();
+
+      const startState = new stepfunction.Pass(stack, 'StartState');
+
+      const simpleStateMachine  = new stepfunction.StateMachine(stack, 'SimpleStateMachine', {
+        definition: startState,
+      });
+
+      test.throws(() => {
+        new cpactions.StepFunctionsInvokeAction({
+          actionName: 'Invoke',
+          stateMachine: simpleStateMachine,
+          stateMachineInputType: StateMachineInputType.FILEPATH,
+          stateMachineInput: '',
+        });
+      });
+
+      test.done();
+    },
+
+    'should throw an exception when the StateMachineInputType is FilePath and no input artifact is provided'(test: Test) {
+      const stack = new Stack();
+
+      const startState = new stepfunction.Pass(stack, 'StartState');
+
+      const simpleStateMachine  = new stepfunction.StateMachine(stack, 'SimpleStateMachine', {
+        definition: startState,
+      });
+
+      test.throws(() => {
+        new cpactions.StepFunctionsInvokeAction({
+          actionName: 'Invoke',
+          stateMachine: simpleStateMachine,
+          stateMachineInputType: StateMachineInputType.FILEPATH,
+          stateMachineInput: 'assets/input.json',
+        });
+      });
+
+      test.done();
+    },
+  },
+};
+
+interface MinimalPipelineOptions {
+  readonly trigger?: cpactions.S3Trigger;
+
+  readonly bucket?: s3.IBucket;
+
+  readonly bucketKey?: string;
+}
+
+function minimalPipeline(stack: Stack, options: MinimalPipelineOptions = {}): codepipeline.IStage {
+  const sourceOutput = new codepipeline.Artifact();
+  const startState = new stepfunction.Pass(stack, 'StartState');
+  const simpleStateMachine  = new stepfunction.StateMachine(stack, 'SimpleStateMachine', {
+    definition: startState,
+  }); 
+  const pipeline = new codepipeline.Pipeline(stack, 'MyPipeline');
+  const sourceStage = pipeline.addStage({
+    stageName: 'Source',
+    actions: [
+      new cpactions.S3SourceAction({
+        actionName: 'Source',
+        bucket: options.bucket || new s3.Bucket(stack, 'MyBucket'),
+        bucketKey: options.bucketKey || 'some/path/to',
+        output: sourceOutput,
+        trigger: options.trigger,
+      }),
+    ],
+  });
+  pipeline.addStage({
+    stageName: 'Invoke',
+    actions: [
+      new cpactions.StepFunctionsInvokeAction({
+        actionName: 'Invoke',
+        stateMachine: simpleStateMachine,
+        stateMachineInputType: StateMachineInputType.LITERAL,
+        stateMachineInput: '{}',
+      }),
+    ],
+  });
+  return sourceStage;
+}
