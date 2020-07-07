002c093df3c78504ff24a37dc0c3d7951bebe2d3
Hyeonsoo David Lee <hyeonsoo.david.lee@gmail.com>
2020-07-07 23:07:46 +0900

    fix(rds): Add TargetGroupName to AWS::RDS::DBProxyTargetGroup

--- packages/@aws-cdk/aws-rds/lib/proxy.ts
+++ packages/@aws-cdk/aws-rds/lib/proxy.ts
@@ -422,16 +422,13 @@ export class DatabaseProxy extends cdk.Resource
       throw new Error('Cannot specify both dbInstanceIdentifiers and dbClusterIdentifiers');
     }
 
-    const proxyTargetGroup = new CfnDBProxyTargetGroup(this, 'ProxyTargetGroup', {
+    new CfnDBProxyTargetGroup(this, 'ProxyTargetGroup', {
       dbProxyName: this.dbProxyName,
       dbInstanceIdentifiers,
       dbClusterIdentifiers,
       connectionPoolConfigurationInfo: toConnectionPoolConfigurationInfo(props),
+      targetGroupName: 'default',
     });
-
-    // Currently(2020-07-04), this property must be set to default.
-    // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-rds-dbproxytargetgroup.html#TargetGroupName-fn::getatt
-    proxyTargetGroup.addOverride('Properties.TargetGroupName', 'default');
   }
 
   /**
