1bc8dfa4641438b716b75ddef8c129d792863396
Bryan Pan <bryanpan342@gmail.com>
2020-07-08 16:08:51 -0700

    chore(appsync): reorganize code base to improve readability (#8933)
    **[ISSUE]**
    aws-appsync library only had one file to host all the code. Hard to read and navigate because close to 2000 lines.
    
    **[APPROACH]**
    Segmented in the following manner:
    - Data Sources
    - Resolvers
    - Keys
    - GraphQL 
    - Mapping Templates
    
    **[NOTE]**
    File names and contents are subject to change as I continue to add to this construct.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-appsync/lib/index.ts
+++ packages/@aws-cdk/aws-appsync/lib/index.ts
@@ -1,3 +1,7 @@
 // AWS::AppSync CloudFormation Resources:
 export * from './appsync.generated';
+export * from './key';
+export * from './dataSource';
+export * from './mappingTemplate';
+export * from './resolver';
 export * from './graphqlapi';
