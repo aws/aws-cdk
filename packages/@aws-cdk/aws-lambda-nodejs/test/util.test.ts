1eaf9b2d2c0f766cfe10f05010e72f499043386e
Rico Huijbers <rix0rrr@gmail.com>
2020-07-10 18:06:14 +0200

    Fixing assumptions on source tree layout

--- packages/@aws-cdk/aws-lambda-nodejs/test/util.test.ts
+++ packages/@aws-cdk/aws-lambda-nodejs/test/util.test.ts
@@ -3,17 +3,17 @@ import { findUp } from '../lib/util';
 
 test('findUp', () => {
   // Starting at process.cwd()
-  expect(findUp('README.md')).toMatch(/aws-lambda-nodejs$/);
+  expect(findUp('README.md')).toEqual(path.resolve(__dirname, '..'));
 
   // Non existing file
   expect(findUp('non-existing-file.unknown')).toBe(undefined);
 
   // Starting at a specific path
-  expect(findUp('util.test.ts', path.join(__dirname, 'integ-handlers'))).toMatch(/aws-lambda-nodejs\/test$/);
+  expect(findUp('util.test.ts', path.join(__dirname, 'integ-handlers'))).toEqual(__dirname);
 
   // Non existing file starting at a non existing relative path
   expect(findUp('not-to-be-found.txt', 'non-existing/relative/path')).toBe(undefined);
 
   // Starting at a relative path
-  expect(findUp('util.test.ts', 'test/integ-handlers')).toMatch(/aws-lambda-nodejs\/test$/);
+  expect(findUp('util.test.ts', 'test/integ-handlers')).toEqual(__dirname);
 });
