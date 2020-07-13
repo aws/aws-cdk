358d177c3fe55e3e741b722be856214df75cd556
Elad Ben-Israel <benisrae@amazon.com>
2020-07-13 19:03:54 +0000

    implement "prepare" using aspects

--- packages/@aws-cdk/pipelines/lib/stage.ts
+++ packages/@aws-cdk/pipelines/lib/stage.ts
@@ -1,6 +1,6 @@
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as cpactions from '@aws-cdk/aws-codepipeline-actions';
-import { Stage } from '@aws-cdk/core';
+import { Stage, Aspects } from '@aws-cdk/core';
 import * as cxapi from '@aws-cdk/cx-api';
 import { Construct } from 'constructs';
 import { AssetType, DeployCdkStackAction } from './actions';
@@ -55,6 +55,8 @@ export class CdkStage extends Construct {
     this.pipelineStage = props.pipelineStage;
     this.cloudAssemblyArtifact = props.cloudAssemblyArtifact;
     this.host = props.host;
+
+    Aspects.of(this).apply({ visit: () => this.prepare() });
   }
 
   /**
@@ -176,7 +178,7 @@ export class CdkStage extends Construct {
    * after creation, nor is there a way to specify relative priorities, which
    * is a limitation that we should take away in the base library.
    */
-  protected prepare() {
+  private prepare() {
     // FIXME: Make sure this only gets run once. There seems to be an issue in the reconciliation
     // loop that may trigger this more than once if it throws an error somewhere, and the exception
     // that gets thrown here will then override the actual failure.
