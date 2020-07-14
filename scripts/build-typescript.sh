68dfe2d9d93acf56032327f9335d9172a6e8198c
Elad Ben-Israel <benisrae@amazon.com>
2020-07-14 13:15:50 +0300

    chore: disable api compatibility checks

--- scripts/build-typescript.sh
+++ scripts/build-typescript.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+scriptdir="$(cd $(dirname $0) && pwd)"
+${scriptdir}/generate-aggregate-tsconfig.sh > ${scriptdir}/../tsconfig.json
+cd ${scriptdir}/..
 node_modules/.bin/tsc -b "$@"
