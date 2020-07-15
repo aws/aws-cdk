a3e69ba806514fd41eba83477b4687a3958d6088
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2020-07-15 15:53:47 +0300

    fix(core): add unit test for app context

--- packages/@aws-cdk/core/test/test.app.ts
+++ packages/@aws-cdk/core/test/test.app.ts
@@ -350,6 +350,22 @@ export = {
 
     test.done();
   },
+
+  'application support any type in context'(test: Test) {
+    const app = new App({
+      context: {
+        isString: "string",
+        isNumber: 10,
+        isObject: { isString: "string", isNumber: 10 },
+      },
+    });
+
+    test.assert(app.node.tryGetContext('isString') === "string", "expect string value.")
+    test.assert(app.node.tryGetContext('isNumber') === 10, "expect number value.")
+    test.deepEqual(app.node.tryGetContext('isObject'), { isString: "string", isNumber: 10 })
+
+    test.done();
+  }
 };
 
 class MyConstruct extends Construct {
