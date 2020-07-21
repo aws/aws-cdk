26c2b5738237b6d5887751c3487b7262c3dcd967
Calvin Combs <comcalvi@amazon.com>
2020-07-21 17:28:31 -0400

    removed unneeded blank line

--- packages/@aws-cdk/core/lib/index.ts
+++ packages/@aws-cdk/core/lib/index.ts
@@ -58,7 +58,6 @@ export * from './custom-resource-provider';
 export * from './cfn-capabilities';
 export * from './cloudformation.generated';
 
-
 // WARNING: Should not be exported, but currently is because of a bug. See the
 // class description for more information.
 export * from './private/intrinsic';
