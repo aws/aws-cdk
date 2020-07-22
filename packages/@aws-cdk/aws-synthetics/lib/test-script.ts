eac77be1358034a5c7c3caaac2215925c9879685
Kaizen Conroy <kcc2@williams.edu>
2020-07-22 15:24:43 -0400

    update test class

--- packages/@aws-cdk/aws-synthetics/lib/test-script.ts
+++ packages/@aws-cdk/aws-synthetics/lib/test-script.ts
@@ -1,10 +1,10 @@
-import { Code } from './code';
 import * as s3 from '@aws-cdk/aws-s3';
 import { Construct } from '@aws-cdk/core';
+import { Code } from './code';
 
 export class Test {
   /**
-   * @returns `CustomTest` associated with the specified Code object
+   * @returns `Test` associated with the specified Code object
    * @param code The script you want the canary to run
    * @param handler The handler of the code
    */
@@ -18,17 +18,35 @@ export class Test {
   }
 
   /**
-   * TODO
+   * Use this method to access the template for heartBeat monitoring. This test will hit the specified url and report back
+   * information on the health of the website.
+   *
+   * @returns `Test` with the heartBeat template as the Code
+   * @param url The url of the website you want to test
+   *
+   * TODO: implement
    */
   //public static heartBeat(url: string){}
 
   /**
-   * TODO
+   * Use this method to access the template for api Endpoint monitoring. This test will hit the endpoint specified and report
+   * back what it receives.
+   *
+   * @param url The url of the api endpoint you want to test
+   * @param options Options to customize the api endpoint template
+   *
+   * TODO: implement
    */
   //public static apiEndpoint(url: string, options: apiOptions){}
 
   /**
-   * TODO
+   * Use this method to access the template for a broken link checker. This test will check a specified number of links and report
+   * back the first broken link found.
+   *
+   * @param url The starting url to check
+   * @param options Options to customzie the template
+   *
+   * TODO: implement
    */
   //public static brokenLink(url: string, options: linkOptions){}
 
@@ -52,16 +70,17 @@ export interface TestConfig {
   readonly handler: string;
 }
 
-// export class CustomTest extends Test {
-//   constructor(public readonly code: Code, public readonly handler: string){
-//     super();
-
-//   }
-
-
-// }
-
+/**
+ * Options for specifying a custom test
+ */
 export interface CustomOptions {
+  /**
+   * The code of the canary script
+   */
   code: Code,
+
+  /**
+   * The handler for the code
+   */
   handler: string,
 }
