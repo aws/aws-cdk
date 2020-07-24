ede2020b0f11a8af9759b099fbb9e1a9e73dce22
Bryan Pan <bryanpan342@gmail.com>
2020-07-23 20:55:26 -0700

    preliminary changes

--- packages/@aws-cdk/aws-appsync/lib/schema.ts
+++ packages/@aws-cdk/aws-appsync/lib/schema.ts
@@ -3,12 +3,16 @@ import { Construct } from '@aws-cdk/core';
 import { SchemaType } from './schema-util';
 
 export interface SchemaProps {
-
+  custom?: string;
 }
 
 export class Schema extends Construct {
-  constructor(scope: Construct, id: string, props: SchemaProps) {
-    super(scope, id);
 
+  public readonly custom?: string;
+
+  constructor(scope: Construct, id: string, props?: SchemaProps) {
+    super(scope, id);
+    this.custom = props?.custom;
   }
+  
 }
