226bcf8ef387cfd5bc46bdb3ffb7d9685cc55659
Pahud <pahudnet@gmail.com>
2020-07-28 22:40:22 +0800

    minor

--- packages/@aws-cdk/aws-ecs/test/ec2/integ.capacity-provider.ts
+++ packages/@aws-cdk/aws-ecs/test/ec2/integ.capacity-provider.ts
@@ -6,39 +6,54 @@ import { CapacityProvider } from '../../lib/capacity-provider';
 
 const app = new cdk.App();
 
-const stack = new cdk.Stack(app, 'integ-capacity-provider2');
+const env = {
+  region: process.env.CDK_DEFAULT_REGION,
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+};
 
-const vpc = new ec2.Vpc(stack, 'Vpc', { maxAzs: 3, natGateways: 1});
+const stack = new cdk.Stack(app, 'integ-capacity-provider2', { env });
+
+// const vpc = new ec2.Vpc(stack, 'Vpc', { maxAzs: 3, natGateways: 1});
+const vpc = ec2.Vpc.fromLookup(stack, 'Vpc', { isDefault: true })
 
 const asg = new AutoScalingGroup(stack, 'ASG', {
-  vpc,
+  vpc,  
   machineImage: new ecs.EcsOptimizedAmi(),
   instanceType: new ec2.InstanceType('t3.large'),
   maxCapacity: 5,
   minCapacity: 1,
-});
-
-const asgSpot = new AutoScalingGroup(stack, 'ASGSpot', {
-  vpc,
-  machineImage: new ecs.EcsOptimizedAmi(),
-  instanceType: new ec2.InstanceType('t3.large'),
-  maxCapacity: 5,
-  minCapacity: 1,
-  spotPrice: '0.1088',
-});
-
-const cp = new CapacityProvider (stack, 'CP', {
-  autoscalingGroup: asg,
-  managedScaling: true,
-  managedTerminationProtection: true,
-});
-
-const cpSpot = new CapacityProvider(stack, 'CPSpot', {
-  autoscalingGroup: asgSpot,
 });
 
 const cluster = new ecs.Cluster(stack, 'Cluster', {
   vpc,
 });
 
-cluster.addCapacityProvider(cp, cpSpot);
+cluster.addAutoScalingGroup(asg)
+
+const cp = new CapacityProvider(stack, 'CP', {
+  autoscalingGroup: asg,
+  managedScaling: true,
+  managedTerminationProtection: true,
+});
+
+// cluster.addCapacityProvider(cp);
+
+
+
+// const asgSpot = new AutoScalingGroup(stack, 'ASGSpot', {
+//   vpc,
+//   machineImage: new ecs.EcsOptimizedAmi(),
+//   instanceType: new ec2.InstanceType('t3.large'),
+//   maxCapacity: 5,
+//   minCapacity: 1,
+//   spotPrice: '0.1088',
+// });
+
+
+
+// const cpSpot = new CapacityProvider(stack, 'CPSpot', {
+//   autoscalingGroup: asgSpot,
+// });
+
+
+
