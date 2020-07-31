a27b00ade9675f77654c33fa93e2ae632f3ec315
Nick Lynch <nlynch@amazon.com>
2020-07-31 10:45:44 +0100

    Replacing default VPC with a new 2-AZ one

--- packages/@aws-cdk/aws-cloudfront-origins/test/integ.load-balancer-origin.ts
+++ packages/@aws-cdk/aws-cloudfront-origins/test/integ.load-balancer-origin.ts
@@ -5,14 +5,9 @@ import * as cdk from '@aws-cdk/core';
 import * as origins from '../lib';
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'cloudfront-load-balancer-origin', {
-  env: {
-    account: process.env.CDK_INTEG_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_INTEG_REGION || process.env.CDK_DEFAULT_REGION,
-  },
-});
+const stack = new cdk.Stack(app, 'cloudfront-load-balancer-origin');
 
-const vpc = ec2.Vpc.fromLookup(stack, 'VPC', { isDefault: true });
+const vpc = new ec2.Vpc(stack, 'Vpc', { maxAzs: 2 });
 const loadbalancer = new elbv2.ApplicationLoadBalancer(stack, 'LB', { vpc, internetFacing: true });
 
 new cloudfront.Distribution(stack, 'Distribution', {
