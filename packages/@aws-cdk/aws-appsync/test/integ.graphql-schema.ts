72d8be4eb70ebb4e24ca9a368f7f253fbb3fc49a
Bryan Pan <bryanpan342@gmail.com>
2020-08-05 17:21:36 -0700

    new implementation

--- packages/@aws-cdk/aws-appsync/test/integ.graphql-schema.ts
+++ packages/@aws-cdk/aws-appsync/test/integ.graphql-schema.ts
@@ -16,66 +16,47 @@ const api = new appsync.GraphQLApi(stack, 'code-first-api', {
   schemaDefinition: appsync.SchemaDefinition.CODE,
 });
 
-const t_id = appsync.AttributeType.id('id').required();
+const t_id_r = appsync.AttributeType.id().required();
+const t_string = appsync.AttributeType.string();
+const t_int = appsync.AttributeType.int();
+const t_float = appsync.AttributeType.float();
+const t_string_l = appsync.AttributeType.string().list();
 
-// Planet Props
-const t_name = appsync.AttributeType.string('name');
-const t_diameter = appsync.AttributeType.int('diameter');
-const t_rotationPeriod = appsync.AttributeType.int('rotationPeriod');
-const t_orbitalPeriod = appsync.AttributeType.int('orbitalPeriod');
-const t_gravity = appsync.AttributeType.string('gravity');
-const t_population = appsync.AttributeType.float('population');
-const t_climates= appsync.AttributeType.string('climates').list();
-const t_terrains = appsync.AttributeType.string('terrains').list();
-const t_surfaceWater = appsync.AttributeType.float('surfaceWater');
-const t_created = appsync.AttributeType.string('created');
-const t_edited = appsync.AttributeType.string('edited');
-
-// Species Props
-const t_classification = appsync.AttributeType.string('classification');
-const t_designation = appsync.AttributeType.string('designation');
-const t_averageHeight = appsync.AttributeType.float('averageHeight');
-const t_averageLifespan = appsync.AttributeType.int('averageLifespan');
-const t_eyeColors = appsync.AttributeType.string('eyeColors').list();
-const t_hairColors = appsync.AttributeType.string('hairColors').list();
-const t_skinColors = appsync.AttributeType.string('skinColors').list();
-const t_language = appsync.AttributeType.string('language');
-
-const t_planet = api.addType('Planet', {
-  definition: [
-    t_name,
-    t_diameter,
-    t_rotationPeriod,
-    t_orbitalPeriod,
-    t_gravity,
-    t_population,
-    t_climates,
-    t_terrains,
-    t_surfaceWater,
-    t_created,
-    t_edited,
-    t_id,
-  ],
+const planet = api.addType('Planet', {
+  definition: {
+    name: t_string,
+    diameter: t_int,
+    rotationPeriod: t_int,
+    orbitalPeriod: t_int,
+    gravity: t_string,
+    population: t_string_l,
+    climates: t_string_l,
+    terrains: t_string_l,
+    surfaceWater: t_float,
+    created: t_string,
+    edited: t_string,
+    id: t_id_r,
+  },
 });
 
 api.addType('Species', {
-  definition: [
-    t_name,
-    t_classification,
-    t_designation,
-    t_averageHeight,
-    t_averageLifespan,
-    t_eyeColors,
-    t_hairColors,
-    t_skinColors,
-    t_language,
-    appsync.AttributeType.object('homeworld', t_planet),
-    t_created,
-    t_edited,
-    t_id,
-  ],
+  definition: {
+    name: t_string,
+    classification: t_string,
+    designation: t_string,
+    averageHeight: t_float,
+    averageLifespan: t_int,
+    eyeColors: t_string_l,
+    hairColors: t_string_l,
+    skinColors: t_string_l,
+    language: t_string,
+    homeworld: appsync.AttributeType.object(planet),
+    created: t_string,
+    edited: t_string,
+    id: t_id_r,
+  },
 });
 
-api.appendToSchema('type Query {\n  getTests: [test]\n}', '\n');
+api.appendToSchema('type Query {\n  getPlanets: [Planet]\n}', '\n');
 
 app.synth();
