6a399574400f892a31fad25972dbadcd37997cd0
joelzhong <luckily.zhong@gmail.com>
2020-08-07 22:14:06 +0800

    aws-codestarnotifications 1. fix dependency cycle 2. fix for yarn build failure

--- packages/@aws-cdk/aws-codestarnotifications/lib/notification-rule.ts
+++ packages/@aws-cdk/aws-codestarnotifications/lib/notification-rule.ts
@@ -5,100 +5,6 @@ import * as cdk from '@aws-cdk/core';
 import { CfnNotificationRule } from './codestarnotifications.generated';
 
 /**
- * The options for AWS Codebuild、AWS Codepipeline and AWS Codecommit notification integration
- */
-export interface AddNotificationRuleOptions {
-
-  /**
-   * The name for the notification rule.
-   * Notification rule names must be unique in your AWS account.
-   */
-  readonly notificationRuleName: string;
-
-  /**
-   * The status of the notification rule.
-   * If the status is set to DISABLED, notifications aren't sent for the notification rule.
-   *
-   * @default Status.ENABLED
-   */
-  readonly status?: Status;
-
-  /**
-   * The level of detail to include in the notifications for this resource.
-   * BASIC will include only the contents of the event as it would appear in AWS CloudWatch.
-   * FULL will include any supplemental information provided by AWS CodeStar Notifications and/or the service for the resource for which the notification is created.
-   *
-   * @default DetailType.FULL
-   */
-  readonly detailType?: DetailType;
-
-  /**
-   * A list of Amazon Resource Names (ARNs) of Amazon SNS topics and AWS Chatbot clients to associate with the notification rule.
-   *
-   * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-codestarnotifications-notificationrule-target.html
-   */
-  readonly targets: INotificationTarget[];
-
-  /**
-   * A list of event types associated with this notification rule.
-   * For a complete list of event types and IDs, see Notification concepts in the Developer Tools Console User Guide.
-   *
-   * @see https://docs.aws.amazon.com/dtconsole/latest/userguide/concepts.html#concepts-api
-   */
-  readonly events: Events[];
-}
-
-/**
- * Properties for a new notification rule
- */
-export interface NotificationRuleProps {
-
-  /**
-   * The name for the notification rule.
-   * Notification rule names must be unique in your AWS account.
-   */
-  readonly notificationRuleName: string;
-
-  /**
-   * The status of the notification rule.
-   * If the status is set to DISABLED, notifications aren't sent for the notification rule.
-   *
-   * @default Status.ENABLED
-   */
-  readonly status?: Status;
-
-  /**
-   * The level of detail to include in the notifications for this resource.
-   * BASIC will include only the contents of the event as it would appear in AWS CloudWatch.
-   * FULL will include any supplemental information provided by AWS CodeStar Notifications and/or the service for the resource for which the notification is created.
-   *
-   * @default DetailType.FULL
-   */
-  readonly detailType?: DetailType;
-
-  /**
-   * A list of Amazon Resource Names (ARNs) of Amazon SNS topics and AWS Chatbot clients to associate with the notification rule.
-   *
-   * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-codestarnotifications-notificationrule-target.html
-   */
-  readonly targets: INotificationTarget[];
-
-  /**
-   * A list of event types associated with this notification rule.
-   * For a complete list of event types and IDs, see Notification concepts in the Developer Tools Console User Guide.
-   *
-   * @see https://docs.aws.amazon.com/dtconsole/latest/userguide/concepts.html#concepts-api
-   */
-  readonly events: Events[];
-
-  /**
-   * The Amazon Resource Name (ARN) of the resource to associate with the notification rule.
-   * Supported resources include pipelines in AWS CodePipeline, repositories in AWS CodeCommit, and build projects in AWS CodeBuild.
-   */
-  readonly resource: string;
-}
-
-/**
  * The list of event types for AWS Codecommit
  */
 export enum RepositoryEvent {
@@ -429,6 +335,61 @@ export class SNSTopicNotificationTarget implements INotificationTarget {
       targetAddress: this.topic.topicArn,
     };
   }
+}
+
+/**
+ * The options for AWS Codebuild、AWS Codepipeline and AWS Codecommit notification integration
+ */
+export type AddNotificationRuleOptions = Omit<NotificationRuleProps, 'resource'>;
+
+/**
+ * Properties for a new notification rule
+ */
+export interface NotificationRuleProps {
+
+  /**
+   * The name for the notification rule.
+   * Notification rule names must be unique in your AWS account.
+   */
+  readonly notificationRuleName: string;
+
+  /**
+   * The status of the notification rule.
+   * If the status is set to DISABLED, notifications aren't sent for the notification rule.
+   *
+   * @default Status.ENABLED
+   */
+  readonly status?: Status;
+
+  /**
+   * The level of detail to include in the notifications for this resource.
+   * BASIC will include only the contents of the event as it would appear in AWS CloudWatch.
+   * FULL will include any supplemental information provided by AWS CodeStar Notifications and/or the service for the resource for which the notification is created.
+   *
+   * @default DetailType.FULL
+   */
+  readonly detailType?: DetailType;
+
+  /**
+   * A list of Amazon Resource Names (ARNs) of Amazon SNS topics and AWS Chatbot clients to associate with the notification rule.
+   *
+   * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-codestarnotifications-notificationrule-target.html
+   */
+  readonly targets: INotificationTarget[];
+
+  /**
+   * A list of event types associated with this notification rule.
+   * For a complete list of event types and IDs, see Notification concepts in the Developer Tools Console User Guide.
+   *
+   * @see https://docs.aws.amazon.com/dtconsole/latest/userguide/concepts.html#concepts-api
+   */
+  readonly events: Events[];
+
+  /**
+   * The Amazon Resource Name (ARN) of the resource to associate with the notification rule.
+   * Supported resources include pipelines in AWS CodePipeline, repositories in AWS CodeCommit, and build projects in AWS CodeBuild.
+   */
+  readonly resource: string;
 }
 
 /**
