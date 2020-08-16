f52c25b70a0779c145797f52b09e28334728849f
Josh Kellendonk <joshkellendonk@gmail.com>
2020-08-16 15:16:48 -0600

    Add back my layer bundling test

--- packages/@aws-cdk/aws-lambda-python/test/bundling.test.ts
+++ packages/@aws-cdk/aws-lambda-python/test/bundling.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/order */
 import * as fs from 'fs';
 import { Code, Runtime } from '@aws-cdk/aws-lambda';
-import { bundle, hasDependencies } from '../lib/bundling';
+import { bundle, hasDependencies, IMAGE_LAYER_DIR, IMAGE_FUNCTION_DIR, bundleLayer } from '../lib/bundling';
 import { DependenciesLocation } from '../lib';
 import * as child_process from 'child_process';
 import * as cdk from '@aws-cdk/core';
@@ -59,7 +59,7 @@ test('Bundling with requirements.txt installed', () => {
       image: { image: MOCK_BUILD_IMAGE_ID },
     }),
     assetHashType: cdk.AssetHashType.BUNDLE,
-    exclude: ['*.pyc'],
+    exclude: expect.arrayContaining(['*.pyc']),
   }));
 });
 
@@ -93,11 +93,12 @@ test('Bundling Python 2.7 with requirements.txt installed', () => {
       image: { image: MOCK_BUILD_IMAGE_ID },
     }),
     assetHashType: cdk.AssetHashType.BUNDLE,
-    exclude: ['*.pyc'],
+    exclude: expect.arrayContaining(['*.pyc']),
   }));
 });
 
-test('Bundling can dependencies can be switched off', () => {
+test('Bundling dependencies can be switched off', () => {
+  // GIVEN
   existsSyncMock.mockImplementation((p: fs.PathLike) => {
     if (/requirements.txt/.test(p.toString())) {
       return true;
@@ -105,14 +106,64 @@ test('Bundling can dependencies can be switched off', () => {
     return existsSyncOriginal(p);
   });
 
+  // WHEN
   bundle({
     entry: '/project/folder',
     runtime: Runtime.PYTHON_3_7,
     dependenciesLocation: DependenciesLocation.NONE,
   });
 
-  // Does not install dependencies when instructed not to.
+  // THEN
   expect(Code.fromAsset).toHaveBeenCalledWith('/project/folder');
+});
+
+test('Bundling dependencies into a layer', () => {
+  // GIVEN
+  const MOCK_BUILD_IMAGE_ID = '1234567890abcdef';
+  spawnSyncMock.mockImplementation(() => ({
+    status: 0,
+    stderr: Buffer.from('stderr'),
+    stdout: Buffer.from(`sha256:${MOCK_BUILD_IMAGE_ID}`),
+    pid: 123,
+    output: ['stdout', 'stderr'],
+    signal: null,
+  }));
+
+  existsSyncMock.mockImplementation((p: fs.PathLike) => {
+    if (/requirements.txt/.test(p.toString())) {
+      return true;
+    }
+    return existsSyncOriginal(p);
+  });
+
+  const opts = {
+    entry: '/project/folder',
+    runtime: Runtime.PYTHON_2_7,
+    dependenciesLocation: DependenciesLocation.LAYER,
+  };
+
+  // WHEN
+  bundle(opts);
+  bundleLayer(opts);
+
+  // THEN
+  expect(Code.fromAsset).toHaveBeenCalledWith('/project/folder', expect.objectContaining({
+    bundling: expect.objectContaining({
+      image: { image: MOCK_BUILD_IMAGE_ID },
+      command: ['sh', '-c', `cp -r ${IMAGE_LAYER_DIR}/. /asset-output/`],
+    }),
+    assetHashType: cdk.AssetHashType.BUNDLE,
+    exclude: expect.arrayContaining(['*.pyc']),
+  }));
+
+  expect(Code.fromAsset).toHaveBeenCalledWith('/project/folder', expect.objectContaining({
+    bundling: expect.objectContaining({
+      image: { image: MOCK_BUILD_IMAGE_ID },
+      command: ['sh', '-c', `cp -r ${IMAGE_FUNCTION_DIR}/. /asset-output/`],
+    }),
+    assetHashType: cdk.AssetHashType.BUNDLE,
+    exclude: expect.arrayContaining(['*.pyc']),
+  }));
 });
 
 describe('Dependency detection', () => {
