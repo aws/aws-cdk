17d1366dedcdea573b818b4a22b7f9cff09eb43f
Meno Abels <meno.abels@adviser.com>
2020-08-17 08:10:29 +0200

    feat(addServiceRoleProps): enable the passing of roleprops on creation of serviceaccount
        Enable to set the role-name on creation of a eks serviceaccount to fullfill
        the name bounded external IAM Rules to gain access to some external resources
    
        ----
    
        *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-eks/lib/service-account.ts
+++ packages/@aws-cdk/aws-eks/lib/service-account.ts
@@ -1,6 +1,132 @@
-import { AddToPrincipalPolicyResult, IPrincipal, IRole, OpenIdConnectPrincipal, PolicyStatement, PrincipalPolicyFragment, Role  } from '@aws-cdk/aws-iam';
-import { CfnJson, Construct  } from '@aws-cdk/core';
+import { AddToPrincipalPolicyResult, IManagedPolicy, IPrincipal, IRole, OpenIdConnectPrincipal, PolicyDocument, PolicyStatement, PrincipalPolicyFragment, Role  } from '@aws-cdk/aws-iam';
+import { CfnJson, Construct, Duration } from '@aws-cdk/core';
 import { Cluster } from './cluster';
+
+/**
+ * Options for `ServiceAccountRoleProps`
+ * very same to RoleProps
+ */
+export interface ServiceAccountRoleProps {
+  /**
+   * The IAM principal (i.e. `new ServicePrincipal('sns.amazonaws.com')`)
+   * which can assume this role.
+   *
+   * You can later modify the assume role policy document by accessing it via
+   * the `assumeRolePolicy` property.
+   * @default No assumedBy required
+   */
+  readonly assumedBy?: IPrincipal;
+
+  /**
+   * ID that the role assumer needs to provide when assuming this role
+   *
+   * If the configured and provided external IDs do not match, the
+   * AssumeRole operation will fail.
+   *
+   * @deprecated see {@link externalIds}
+   *
+   * @default No external ID required
+   */
+  readonly externalId?: string;
+
+  /**
+   * List of IDs that the role assumer needs to provide one of when assuming this role
+   *
+   * If the configured and provided external IDs do not match, the
+   * AssumeRole operation will fail.
+   *
+   * @default No external ID required
+   */
+  readonly externalIds?: string[];
+
+  /**
+   * A list of managed policies associated with this role.
+   *
+   * You can add managed policies later using
+   * `addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName(policyName))`.
+   *
+   * @default - No managed policies.
+   */
+  readonly managedPolicies?: IManagedPolicy[];
+
+  /**
+   * A list of named policies to inline into this role. These policies will be
+   * created with the role, whereas those added by ``addToPolicy`` are added
+   * using a separate CloudFormation resource (allowing a way around circular
+   * dependencies that could otherwise be introduced).
+   *
+   * @default - No policy is inlined in the Role resource.
+   */
+  readonly inlinePolicies?: { [name: string]: PolicyDocument };
+
+  /**
+   * The path associated with this role. For information about IAM paths, see
+   * Friendly Names and Paths in IAM User Guide.
+   *
+   * @default /
+   */
+  readonly path?: string;
+
+  /**
+   * AWS supports permissions boundaries for IAM entities (users or roles).
+   * A permissions boundary is an advanced feature for using a managed policy
+   * to set the maximum permissions that an identity-based policy can grant to
+   * an IAM entity. An entity's permissions boundary allows it to perform only
+   * the actions that are allowed by both its identity-based policies and its
+   * permissions boundaries.
+   *
+   * @link https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-iam-role.html#cfn-iam-role-permissionsboundary
+   * @link https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_boundaries.html
+   *
+   * @default - No permissions boundary.
+   */
+  readonly permissionsBoundary?: IManagedPolicy;
+
+  /**
+   * A name for the IAM role. For valid values, see the RoleName parameter for
+   * the CreateRole action in the IAM API Reference.
+   *
+   * IMPORTANT: If you specify a name, you cannot perform updates that require
+   * replacement of this resource. You can perform updates that require no or
+   * some interruption. If you must replace the resource, specify a new name.
+   *
+   * If you specify a name, you must specify the CAPABILITY_NAMED_IAM value to
+   * acknowledge your template's capabilities. For more information, see
+   * Acknowledging IAM Resources in AWS CloudFormation Templates.
+   *
+   * @default - AWS CloudFormation generates a unique physical ID and uses that ID
+   * for the group name.
+   */
+  readonly roleName?: string;
+
+  /**
+   * The maximum session duration that you want to set for the specified role.
+   * This setting can have a value from 1 hour (3600sec) to 12 (43200sec) hours.
+   *
+   * Anyone who assumes the role from the AWS CLI or API can use the
+   * DurationSeconds API parameter or the duration-seconds CLI parameter to
+   * request a longer session. The MaxSessionDuration setting determines the
+   * maximum duration that can be requested using the DurationSeconds
+   * parameter.
+   *
+   * If users don't specify a value for the DurationSeconds parameter, their
+   * security credentials are valid for one hour by default. This applies when
+   * you use the AssumeRole* API operations or the assume-role* CLI operations
+   * but does not apply when you use those operations to create a console URL.
+   *
+   * @link https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use.html
+   *
+   * @default Duration.hours(1)
+   */
+  readonly maxSessionDuration?: Duration;
+
+  /**
+   * A description of the role. It can be up to 1000 characters long.
+   *
+   * @default - No description.
+   */
+  readonly description?: string;
+}
 
 /**
  * Options for `ServiceAccount`
@@ -17,6 +143,12 @@ export interface ServiceAccountOptions {
    * @default "default"
    */
   readonly namespace?: string;
+
+  /**
+   * The namespace of the service account.
+   * @default "default"
+   */
+  readonly role?: ServiceAccountRoleProps;
 }
 
 /**
@@ -72,7 +204,10 @@ export class ServiceAccount extends Construct implements IPrincipal {
     const principal = new OpenIdConnectPrincipal(cluster.openIdConnectProvider).withConditions({
       StringEquals: conditions,
     });
-    this.role = new Role(this, 'Role', { assumedBy: principal });
+    this.role = new Role(this, 'Role', {
+      ...props.role,
+      assumedBy: principal,
+    });
 
     this.assumeRoleAction = this.role.assumeRoleAction;
     this.grantPrincipal = this.role.grantPrincipal;
