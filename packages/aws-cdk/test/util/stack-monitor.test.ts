4fec4b2a8b9f2dd3330ca38ba323f25e29939d4c
Rico Huijbers <rix0rrr@gmail.com>
2020-08-19 21:51:29 +0200

    chore(cli): fix bugs introduced in stack monitor (#9830)
    In #9795, I was under the impression that the `DescribeStackEvents`
    would list events in chronological order.
    
    This was a misunderstanding resulting from not reading the documentation--
    in fact, the events are returned in reverse chronological order.
    
    The paging logic was therefore wrong and the events would be fed into
    the activity printer in the wrong order, leading to incorrect progress
    reporting.
    
    This is a critical bug and must be fixed immediately!
    
    While I was in there, fixed two other paper cuts around the
    printer at the same time:
    
    - Events occurring near the start of the monitor would be missed. This
      was because time-based filtering would be employed based on the
      client machine's local clock, which might be out of sync with the
      remote clock. Use the server's clock instead, by using the
      `CreationTime` of the ChangeSet as the start time.
    
    - Events occurring near the end of the monitor would be missed. This
      was because `DescribeStackStatus` might return a completion state
      before the monitor had polled the events that led up to the completion.
      Fix by doing a final poll after the stop call has been received,
      so we are guaranteed to have seen all events.
    
    The actual claim in that PR was incorrect. We were *not* quadratic (we
    wouldn't list all pages on every tick), we were just more linear than
    necessary (we *would* list all pages on the *first* tick... which is still
    a lot). The current PR properly fixes the issue by stopping paging
    not just if we've already seen the events, but also if the events are
    starting to fall outside the time window we are interested in.
    
    This *actually* fixes #9470.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk/test/util/stack-monitor.test.ts
+++ packages/aws-cdk/test/util/stack-monitor.test.ts
@@ -9,37 +9,150 @@ beforeEach(() => {
   printer = new FakePrinter();
 });
 
-test('retain page token between ticks', async () => {
-  let finished = false;
-  sdk.stubCloudFormation({
-    describeStackEvents: (jest.fn() as jest.Mock<AWS.CloudFormation.DescribeStackEventsOutput, [AWS.CloudFormation.DescribeStackEventsInput]>)
-      // First call, return a page token
-      .mockImplementationOnce((request) => {
-        expect(request.NextToken).toBeUndefined();
-        return { NextToken: 'token' };
-      })
-      // Second call, expect page token, return no page
-      .mockImplementationOnce(request => {
-        expect(request.NextToken).toEqual('token');
-        return { };
-      })
-      // Third call, ensure we still get the same page token
-      .mockImplementationOnce(request => {
-        expect(request.NextToken).toEqual('token');
-        finished = true;
-        return { };
-      }),
-  });
+test('continue to the next page if it exists', async () => {
+  await testMonitorWithEventCalls([
+    (request) => {
+      expect(request.NextToken).toBeUndefined();
+      return {
+        StackEvents: [event(102)],
+        NextToken: 'some-token',
+      };
+    },
+    (request) => {
+      expect(request.NextToken).toBe('some-token');
+      return {
+        StackEvents: [event(101)],
+      };
+    },
+  ]);
 
-  const monitor = new StackActivityMonitor(sdk.cloudFormation(), 'StackName', printer).start();
+  // Printer sees them in chronological order
+  expect(printer.eventIds).toEqual(['101', '102']);
+});
+
+test('do not page further if we already saw the last event', async () => {
+  await testMonitorWithEventCalls([
+    (request) => {
+      expect(request.NextToken).toBeUndefined();
+      return {
+        StackEvents: [event(101)],
+      };
+    },
+    (request) => {
+      expect(request.NextToken).toBeUndefined();
+      return {
+        StackEvents: [event(102), event(101)],
+        NextToken: 'some-token',
+      };
+    },
+    (request) => {
+      // Did not use the token
+      expect(request.NextToken).toBeUndefined();
+      return {};
+    },
+  ]);
+
+  // Seen in chronological order
+  expect(printer.eventIds).toEqual(['101', '102']);
+});
+
+test('do not page further if the last event is too old', async () => {
+  await testMonitorWithEventCalls([
+    (request) => {
+      expect(request.NextToken).toBeUndefined();
+      return {
+        StackEvents: [event(101), event(95)],
+        NextToken: 'some-token',
+      };
+    },
+    (request) => {
+      // Start again from the top
+      expect(request.NextToken).toBeUndefined();
+      return {};
+    },
+  ]);
+
+  // Seen only the new one
+  expect(printer.eventIds).toEqual(['101']);
+});
+
+test('do a final request after the monitor is stopped', async () => {
+  await testMonitorWithEventCalls([
+    // Before stop
+    (request) => {
+      expect(request.NextToken).toBeUndefined();
+      return {
+        StackEvents: [event(101)],
+      };
+    },
+  ],
+  // After stop
+  [
+    (request) => {
+      expect(request.NextToken).toBeUndefined();
+      return {
+        StackEvents: [event(102), event(101)],
+      };
+    },
+  ]);
+
+  // Seen both
+  expect(printer.eventIds).toEqual(['101', '102']);
+});
+
+const T0 = 1597837230504;
+
+// Events 0-99 are before we started paying attention
+const T100 = T0 + 100 * 1000;
+
+function event(nr: number): AWS.CloudFormation.StackEvent {
+  return {
+    EventId: `${nr}`,
+    StackId: 'StackId',
+    StackName: 'StackName',
+    Timestamp: new Date(T0 + nr * 1000),
+  };
+}
+
+async function testMonitorWithEventCalls(
+  beforeStopInvocations: Array<(x: AWS.CloudFormation.DescribeStackEventsInput) => AWS.CloudFormation.DescribeStackEventsOutput>,
+  afterStopInvocations: Array<(x: AWS.CloudFormation.DescribeStackEventsInput) => AWS.CloudFormation.DescribeStackEventsOutput> = [],
+) {
+  let describeStackEvents = (jest.fn() as jest.Mock<AWS.CloudFormation.DescribeStackEventsOutput, [AWS.CloudFormation.DescribeStackEventsInput]>);
+
+  let finished = false;
+
+  for (const invocation of beforeStopInvocations) {
+    const invocation_ = invocation; // Capture loop variable in local because of closure semantics
+    const isLast = invocation === beforeStopInvocations[beforeStopInvocations.length - 1];
+    describeStackEvents = describeStackEvents.mockImplementationOnce(request => {
+      const ret = invocation_(request);
+      if (isLast) {
+        finished = true;
+      }
+      return ret;
+    });
+  }
+  for (const invocation of afterStopInvocations) {
+    describeStackEvents = describeStackEvents.mockImplementationOnce(invocation);
+  }
+  describeStackEvents.mockImplementation(() => { return {}; });
+
+  sdk.stubCloudFormation({ describeStackEvents });
+
+  const monitor = new StackActivityMonitor(sdk.cloudFormation(), 'StackName', printer, undefined, new Date(T100)).start();
   await waitForCondition(() => finished);
   await monitor.stop();
-});
+}
 
 
 class FakePrinter implements IActivityPrinter {
   public updateSleep: number = 0;
   public readonly activities: StackActivity[] = [];
+
+  public get eventIds() {
+    return this.activities.map(a => a.event.EventId);
+  }
 
   public addActivity(activity: StackActivity): void {
     this.activities.push(activity);
