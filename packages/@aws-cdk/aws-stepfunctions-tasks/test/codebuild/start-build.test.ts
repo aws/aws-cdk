171f1ad61a6f80c176dae77378e9745c5abb9279
DaWyz <alban.escalier@singlepoint.ie>
2020-08-20 00:06:34 +0000

    Reduce start-build scope
    Only allowing to pass the CodeBuild project and environment variables for now. The rest to come in future iteration.

--- packages/@aws-cdk/aws-stepfunctions-tasks/test/codebuild/start-build.test.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/test/codebuild/start-build.test.ts
@@ -61,118 +61,13 @@ test('Task with all the parameters', () => {
   const task = new CodeBuildStartBuild(stack, 'Task', {
     project: codebuildProject,
     integrationPattern: sfn.IntegrationPattern.RUN_JOB,
-    artifactsOverride: {
-      artifactIdentifier: 'id',
-      encryptionDisabled: false,
-      location: 'string',
-      name: 'string',
-      namespaceType: 'BUILD_ID',
-      overrideArtifactName: false,
-      packaging: 'ZIP',
-      path: 'string',
-      type: 'S3',
-    },
-    buildspecOverride: 'string',
-    buildStatusConfigOverride: {
-      context: 'string',
-      targetUrl: 'string',
-    },
-    cacheOverride: {
-      location: 'string',
-      modes: ['LOCAL_CUSTOM_CACHE'],
-      type: 'S3',
-    },
-    certificateOverride: 'string',
-    computeTypeOverride: codebuild.ComputeType.MEDIUM,
-    debugSessionEnabled: false,
-    encryptionKeyOverride: 'string',
-    environmentTypeOverride: 'LINUX_CONTAINER',
-    environmentVariablesOverride: [
-      {
-        name: 'string',
+    environmentVariablesOverride: {
+      env: {
         type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
-        value: 'string',
-      },
-    ],
-    gitCloneDepthOverride: 0,
-    gitSubmodulesConfigOverride: {
-      fetchSubmodules: false,
-    },
-    idempotencyToken: 'string',
-    imageOverride: 'string',
-    imagePullCredentialsTypeOverride: codebuild.ImagePullPrincipalType.CODEBUILD,
-    insecureSslOverride: false,
-    logsConfigOverride: {
-      cloudWatchLogs: {
-        groupName: 'string',
-        status: 'ENABLED',
-        streamName: 'string',
-      },
-      s3Logs: {
-        encryptionDisabled: false,
-        location: 'string',
-        status: 'ENABLED',
+        value: 'prod',
       },
     },
-    privilegedModeOverride: false,
-    queuedTimeoutInMinutesOverride: 60,
-    registryCredentialOverride: {
-      credential: 'string',
-      credentialProvider: 'SECRETS_MANAGER',
-    },
-    reportBuildStatusOverride: false,
-    secondaryArtifactsOverride: [
-      {
-        artifactIdentifier: 'string',
-        encryptionDisabled: false,
-        location: 'string',
-        name: 'string',
-        namespaceType: 'BUILD_ID',
-        overrideArtifactName: false,
-        packaging: 'ZIP',
-        path: 'string',
-        type: 'S3',
-      },
-    ],
-    secondarySourcesOverride: [
-      {
-        auth: {
-          resource: 'string',
-          type: 'OAUTH',
-        },
-        buildspec: 'string',
-        buildStatusConfig: {
-          context: 'string',
-          targetUrl: 'string',
-        },
-        gitCloneDepth: 0,
-        gitSubmodulesConfig: {
-          fetchSubmodules: false,
-        },
-        insecureSsl: false,
-        location: 'string',
-        reportBuildStatus: false,
-        sourceIdentifier: 'string',
-        type: 'S3',
-      },
-    ],
-    secondarySourcesVersionOverride: [
-      {
-        sourceIdentifier: 'string',
-        sourceVersion: 'string',
-      },
-    ],
-    serviceRoleOverride: 'string',
-    sourceAuthOverride: {
-      resource: 'string',
-      type: 'OAUTH',
-    },
-    sourceLocationOverride: 'string',
-    sourceTypeOverride: 'S3',
-    sourceVersion: 'string',
-    timeoutInMinutesOverride: 60,
   });
-
 
   // THEN
   expect(stack.resolve(task.toStateJson())).toEqual({
@@ -194,116 +89,13 @@ test('Task with all the parameters', () => {
       ProjectName: {
         Ref: 'ProjectC78D97AD',
       },
-      ArtifactsOverride: {
-        ArtifactIdentifier: 'id',
-        EncryptionDisabled: false,
-        Location: 'string',
-        Name: 'string',
-        NamespaceType: 'BUILD_ID',
-        OverrideArtifactName: false,
-        Packaging: 'ZIP',
-        Path: 'string',
-        Type: 'S3',
-      },
-      BuildspecOverride: 'string',
-      BuildStatusConfigOverride: {
-        Context: 'string',
-        TargetUrl: 'string',
-      },
-      CacheOverride: {
-        Location: 'string',
-        Modes: ['LOCAL_CUSTOM_CACHE'],
-        Type: 'S3',
-      },
-      CertificateOverride: 'string',
-      ComputeTypeOverride: codebuild.ComputeType.MEDIUM,
-      DebugSessionEnabled: false,
-      EncryptionKeyOverride: 'string',
-      EnvironmentTypeOverride: 'LINUX_CONTAINER',
       EnvironmentVariablesOverride: [
         {
-          Name: 'string',
+          Name: 'env',
           Type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
-          Value: 'string',
+          Value: 'prod',
         },
       ],
-      GitCloneDepthOverride: 0,
-      GitSubmodulesConfigOverride: {
-        FetchSubmodules: false,
-      },
-      IdempotencyToken: 'string',
-      ImageOverride: 'string',
-      ImagePullCredentialsTypeOverride: 'CODEBUILD',
-      InsecureSslOverride: false,
-      LogsConfigOverride: {
-        CloudWatchLogs: {
-          GroupName: 'string',
-          Status: 'ENABLED',
-          StreamName: 'string',
-        },
-        S3Logs: {
-          EncryptionDisabled: false,
-          Location: 'string',
-          Status: 'ENABLED',
-        },
-      },
-      PrivilegedModeOverride: false,
-      QueuedTimeoutInMinutesOverride: 60,
-      RegistryCredentialOverride: {
-        Credential: 'string',
-        CredentialProvider: 'SECRETS_MANAGER',
-      },
-      ReportBuildStatusOverride: false,
-      SecondaryArtifactsOverride: [
-        {
-          ArtifactIdentifier: 'string',
-          EncryptionDisabled: false,
-          Location: 'string',
-          Name: 'string',
-          NamespaceType: 'BUILD_ID',
-          OverrideArtifactName: false,
-          Packaging: 'ZIP',
-          Path: 'string',
-          Type: 'S3',
-        },
-      ],
-      SecondarySourcesOverride: [
-        {
-          Auth: {
-            Resource: 'string',
-            Type: 'OAUTH',
-          },
-          Buildspec: 'string',
-          BuildStatusConfig: {
-            Context: 'string',
-            TargetUrl: 'string',
-          },
-          GitCloneDepth: 0,
-          GitSubmodulesConfig: {
-            FetchSubmodules: false,
-          },
-          InsecureSsl: false,
-          Location: 'string',
-          ReportBuildStatus: false,
-          SourceIdentifier: 'string',
-          Type: 'S3',
-        },
-      ],
-      SecondarySourcesVersionOverride: [
-        {
-          SourceIdentifier: 'string',
-          SourceVersion: 'string',
-        },
-      ],
-      ServiceRoleOverride: 'string',
-      SourceAuthOverride: {
-        Resource: 'string',
-        Type: 'OAUTH',
-      },
-      SourceLocationOverride: 'string',
-      SourceTypeOverride: 'S3',
-      SourceVersion: 'string',
-      TimeoutInMinutesOverride: 60,
     },
   });
 });
@@ -313,14 +105,12 @@ test('supports tokens', () => {
   const task = new CodeBuildStartBuild(stack, 'Task', {
     project: codebuildProject,
     integrationPattern: sfn.IntegrationPattern.RUN_JOB,
-    gitCloneDepthOverride: sfn.JsonPath.numberAt('$.gitCloneDepth'),
-    environmentVariablesOverride: [
-      {
-        name: 'ZONE',
+    environmentVariablesOverride: {
+      ZONE: {
         type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
         value: sfn.JsonPath.stringAt('$.envVariables.zone'),
       },
-    ],
+    },
   });
 
   // THEN
@@ -340,11 +130,10 @@ test('supports tokens', () => {
     },
     End: true,
     Parameters: {
-      'ProjectName': {
+      ProjectName: {
         Ref: 'ProjectC78D97AD',
       },
-      'GitCloneDepthOverride.$': '$.gitCloneDepth',
-      'EnvironmentVariablesOverride': [
+      EnvironmentVariablesOverride: [
         {
           'Name': 'ZONE',
           'Type': codebuild.BuildEnvironmentVariableType.PLAINTEXT,
