92f3658e917170eb1779043b273da6eec82498d2
Josh Kellendonk <joshkellendonk@gmail.com>
2020-08-26 20:17:19 -0600

    Change the dependency removal integ to synth twice

--- packages/@aws-cdk/aws-lambda-python/test/integ.function.requirements.removed.ts
+++ packages/@aws-cdk/aws-lambda-python/test/integ.function.requirements.removed.ts
@@ -12,58 +12,57 @@ import * as lambda from '../lib';
  */
 
 class TestStack extends Stack {
+  public readonly dependenciesAssetHash: string;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // Create a working directory for messing around with requirements.txt
-    const workDir = fs.mkdtempSync(path.join(os.tmpdir(), 'cdk-integ'));
-    fs.copyFileSync(path.join(__dirname, 'lambda-handler-requirements', 'index.py'), path.join(workDir, 'index.py'));
-    const requirementsTxtPath = path.join(workDir, 'requirements.txt');
-
-    // Write a requirements.txt with an extraneous dependency (colorama)
-    const beforeDeps = 'requests==2.23.0\npillow==6.2.2\ncolorama==0.4.3\n';
-    fs.writeFileSync(requirementsTxtPath, beforeDeps);
-
-    const fnBefore = new lambda.PythonFunction(this, 'before', {
+    const fn = new lambda.PythonFunction(this, 'function', {
       entry: workDir,
       runtime: Runtime.PYTHON_2_7,
     });
 
-    new CfnOutput(this, 'FunctionBefore', {
-      value: fnBefore.functionName,
+    new CfnOutput(this, 'Function', {
+      value: fn.functionName,
     });
 
-    // Then, write a requirements.txt without the extraneous dependency
-    const afterDeps = 'requests==2.23.0\npillow==6.2.2\n';
-    fs.writeFileSync(requirementsTxtPath, afterDeps);
-
-    const fnAfter = new lambda.PythonFunction(this, 'after', {
-      entry: workDir,
-      runtime: Runtime.PYTHON_2_7,
-    });
-
-    new CfnOutput(this, 'FunctionAfter', {
-      value: fnAfter.functionName,
-    });
-
-    // Ensure that the fnBefore and fnAfter assets are different
-    const fnBeforeAssetHash = (fnBefore.node.findAll(ConstructOrder.POSTORDER)
+    // Find the asset hash of the dependencies
+    this.dependenciesAssetHash = (fn.node.findAll(ConstructOrder.POSTORDER)
       .find(c => c.node.path.endsWith('Dependencies/Code')) as any)
       .assetHash;
-    const fnAfterAssetHash = (fnAfter.node.findAll(ConstructOrder.POSTORDER)
-      .find(c => c.node.path.endsWith('Dependencies/Code')) as any)
-      .assetHash;
-
-    if (!fnBeforeAssetHash || !fnAfterAssetHash) {
-      throw new Error('The asset hashes are not both defined');
-    }
-
-    if (fnBeforeAssetHash === fnAfterAssetHash) {
-      throw new Error('Despite removing a dependency, the assets for the two lambdas are the same');
-    }
   }
 }
 
+// This is a special integration test that synths twice to show that docker
+// picks up a change in requirements.txt
+
+// Create a working directory for messing around with requirements.txt
+const workDir = fs.mkdtempSync(path.join(os.tmpdir(), 'cdk-integ'));
+fs.copyFileSync(path.join(__dirname, 'lambda-handler-requirements', 'index.py'), path.join(workDir, 'index.py'));
+const requirementsTxtPath = path.join(workDir, 'requirements.txt');
+
+// Write a requirements.txt with an extraneous dependency (colorama)
+const beforeDeps = 'requests==2.23.0\npillow==6.2.2\ncolorama==0.4.3\n';
+fs.writeFileSync(requirementsTxtPath, beforeDeps);
+
+// Synth the first time
 const app = new App();
-new TestStack(app, 'cdk-integ-lambda-python-requirements-removed');
+const stack1 = new TestStack(app, 'cdk-integ-lambda-python-requirements-removed');
 app.synth();
+
+// Then, write a requirements.txt without the extraneous dependency and synth again
+const afterDeps = 'requests==2.23.0\npillow==6.2.2\n';
+fs.writeFileSync(requirementsTxtPath, afterDeps);
+
+// Synth the same stack a second time with different requirements.txt contents
+const app2 = new App();
+const stack2 = new TestStack(app2, 'cdk-integ-lambda-python-requirements-removed');
+app2.synth();
+
+if (!stack1.dependenciesAssetHash || !stack2.dependenciesAssetHash) {
+  throw new Error('The asset hashes are not both truthy');
+}
+
+if (stack1.dependenciesAssetHash === stack2.dependenciesAssetHash) {
+  throw new Error('Removing a dependency did not change the asset hash');
+}
