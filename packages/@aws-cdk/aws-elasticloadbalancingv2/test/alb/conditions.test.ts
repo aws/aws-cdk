5b6107a959acda11c95d62857c03e084ef171d54
EC2 Default User <ec2-user@ip-172-31-24-24.cn-northwest-1.compute.internal>
2020-09-01 12:01:02 +0000

    format code

--- packages/@aws-cdk/aws-elasticloadbalancingv2/test/alb/conditions.test.ts
+++ packages/@aws-cdk/aws-elasticloadbalancingv2/test/alb/conditions.test.ts
@@ -3,15 +3,15 @@ import * as elbv2 from '../../lib';
 
 describe('tests', () => {
   test('pathPatterns length should be less than or equal to 5', () => {
-    
+
     //GIVEN
-    const array = ['/u1', '/u2', '/u3', '/u4', '/u5', '/u6'];  
-      
-	// THEN
-	expect(() => {
-	  elbv2.ListenerCondition.pathPatterns(array);
-	}).toThrow(/A rule can only have '5' condition values/);
-	
+    const array = ['/u1', '/u2', '/u3', '/u4', '/u5', '/u6'];
+
+    // THEN
+    expect(() => {
+      elbv2.ListenerCondition.pathPatterns(array);
+    }).toThrow(/A rule can only have '5' condition values/);
+
   });
-  
+
 });
