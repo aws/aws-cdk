155f6f4d1090c13456a6ed488e0bb498813a0c8b
EC2 Default User <ec2-user@ip-172-31-24-24.cn-northwest-1.compute.internal>
2020-09-01 12:36:03 +0000

    add a positive case

--- packages/@aws-cdk/aws-elasticloadbalancingv2/test/alb/conditions.test.ts
+++ packages/@aws-cdk/aws-elasticloadbalancingv2/test/alb/conditions.test.ts
@@ -5,11 +5,15 @@ describe('tests', () => {
   test('pathPatterns length should be less than or equal to 5', () => {
 
     //GIVEN
-    const array = ['/u1', '/u2', '/u3', '/u4', '/u5', '/u6'];
+    const array1 = ['/u1', '/u2', '/u3', '/u4', '/u5'];
+    const array2 = ['/u1', '/u2', '/u3', '/u4', '/u5', '/u6'];
+    const listenerCondition = elbv2.ListenerCondition.pathPatterns(array1)
 
     // THEN
+    expect(listenerCondition.renderRawCondition()["pathPatternConfig"]["values"]).toEqual(array1);
+
     expect(() => {
-      elbv2.ListenerCondition.pathPatterns(array);
+      elbv2.ListenerCondition.pathPatterns(array2);
     }).toThrow(/A rule can only have '5' condition values/);
 
   });
