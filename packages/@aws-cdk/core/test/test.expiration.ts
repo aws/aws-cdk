c409d93232166c475ceb974503da166a64b34f0c
Bryan Pan <bryanpan342@gmail.com>
2020-09-04 13:32:13 -0700

    address suggestions for expiration

--- packages/@aws-cdk/core/test/test.expiration.ts
+++ packages/@aws-cdk/core/test/test.expiration.ts
@@ -10,19 +10,19 @@ export = nodeunit.testCase({
 
   'at specified date'(test: nodeunit.Test) {
     const date = new Date('Sun, 26 Jan 2020 00:53:20 GMT');
-    test.equal(Expiration.atDate(new Date('Sun, 26 Jan 2020 00:53:20 GMT')).value, 'Sun, 26 Jan 2020 00:53:20 GMT');
-    test.equal(Expiration.atDate(new Date(1580000000000)).value, 'Sun, 26 Jan 2020 00:53:20 GMT');
-    test.equal(Expiration.atDate(new Date(date)).value, 'Sun, 26 Jan 2020 00:53:20 GMT');
+    test.equal(Expiration.atDate(new Date('Sun, 26 Jan 2020 00:53:20 GMT')).date.toUTCString(), 'Sun, 26 Jan 2020 00:53:20 GMT');
+    test.equal(Expiration.atDate(new Date(1580000000000)).date.toUTCString(), 'Sun, 26 Jan 2020 00:53:20 GMT');
+    test.equal(Expiration.atDate(new Date(date)).date.toUTCString(), 'Sun, 26 Jan 2020 00:53:20 GMT');
     test.done();
   },
 
   'at time stamp'(test: nodeunit.Test) {
-    test.equal(Expiration.atDate(new Date(1580000000000)).value, 'Sun, 26 Jan 2020 00:53:20 GMT');
+    test.equal(Expiration.atDate(new Date(1580000000000)).date.toUTCString(), 'Sun, 26 Jan 2020 00:53:20 GMT');
     test.done();
   },
 
   'after'(test: nodeunit.Test) {
-    test.ok(Math.abs(new Date(Expiration.after(Duration.minutes(10)).value).getTime() - (Date.now() + 600000)) < 15000);
+    test.ok(Math.abs(new Date(Expiration.after(Duration.minutes(10)).date.toUTCString()).getTime() - (Date.now() + 600000)) < 15000);
     test.done();
   },
 
