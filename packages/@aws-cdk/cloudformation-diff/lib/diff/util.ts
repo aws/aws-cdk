2c7559e0f2d4c04537e4bc7c1fe598994d3cdf3f
Adam Ruka <adamruka@amazon.com>
2020-09-16 11:04:19 -0700

    fix(diff): allow strings to be passed for boolean properties (#10378)
    In CloudFormation, it's legal to pass as string where a boolean value is expected.
    Take that into account when performing diffs,
    and allow that case in `@aws-cdk/cloudformation-include`.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/cloudformation-diff/lib/diff/util.ts
+++ packages/@aws-cdk/cloudformation-diff/lib/diff/util.ts
@@ -15,6 +15,12 @@
  */
 export function deepEqual(lvalue: any, rvalue: any): boolean {
   if (lvalue === rvalue) { return true; }
+  // CloudFormation allows passing strings into boolean-typed fields
+  if (((typeof lvalue === 'string' && typeof rvalue === 'boolean') ||
+      (typeof lvalue === 'boolean' && typeof rvalue === 'string')) &&
+      lvalue.toString() === rvalue.toString()) {
+    return true;
+  }
   // allows a numeric 10 and a literal "10" to be equivalent;
   // this is consistent with CloudFormation.
   if (((typeof lvalue === 'string') || (typeof rvalue === 'string')) && (parseFloat(lvalue) === parseFloat(rvalue))) {
