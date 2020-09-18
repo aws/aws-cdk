82aa74b6febb1556d83575a88ff041cfbdd9f9d4
epolon <epolon@amazon.com>
2020-09-18 19:15:50 +0300

    first working version

--- packages/@aws-cdk/aws-s3-notifications/lib/lambda.ts
+++ packages/@aws-cdk/aws-s3-notifications/lib/lambda.ts
@@ -13,21 +13,35 @@ export class LambdaDestination implements s3.IBucketNotificationDestination {
   public bind(_scope: Construct, bucket: s3.IBucket): s3.BucketNotificationDestinationConfig {
     const permissionId = `AllowBucketNotificationsFrom${bucket.node.uniqueId}`;
 
-    if (this.fn.permissionsNode.tryFindChild(permissionId) === undefined) {
-      this.fn.addPermission(permissionId, {
-        sourceAccount: Stack.of(bucket).account,
-        principal: new iam.ServicePrincipal('s3.amazonaws.com'),
-        sourceArn: bucket.bucketArn,
-        // the bucket stack already has a dependency on the lambda stack because
-        // of the notification resource. therefore adding the permission to the lambda
-        // stack would create a circular dependency, so we add it to the bucket stack.
-        scope: Stack.of(bucket),
-      });
-    }
+    const permissionProps: lambda.Permission = {
+      sourceAccount: Stack.of(bucket).account,
+      principal: new iam.ServicePrincipal('s3.amazonaws.com'),
+      sourceArn: bucket.bucketArn,
+    };
 
-    // if we have a permission resource for this relationship, add it as a dependency
-    // to the bucket notifications resource, so it will be created first.
-    const permission = this.fn.permissionsNode.tryFindChild(permissionId) as CfnResource | undefined;
+    let permission = undefined;
+
+    if (Stack.of(this.fn) !== Stack.of(bucket)) {
+      if (Stack.of(bucket).node.tryFindChild(permissionId) === undefined) {
+        this.fn.addPermission(permissionId, {
+          ...permissionProps,
+          // the bucket stack already has a dependency on the lambda stack because
+          // of the notification resource. therefore adding the permission to the lambda
+          // stack would create a circular dependency, so we add it to the bucket stack.
+          scope: Stack.of(bucket),
+        });
+        permission = Stack.of(bucket).node.tryFindChild(permissionId) as CfnResource | undefined;
+      }
+    } else {
+      if (this.fn.permissionsNode.tryFindChild(permissionId) === undefined) {
+        this.fn.addPermission(permissionId, {
+          ...permissionProps,
+        });
+        // if we have a permission resource for this relationship, add it as a dependency
+        // to the bucket notifications resource, so it will be created first.
+        permission = this.fn.permissionsNode.tryFindChild(permissionId) as CfnResource | undefined;
+      }
+    }
 
     return {
       type: s3.BucketNotificationDestinationType.LAMBDA,
