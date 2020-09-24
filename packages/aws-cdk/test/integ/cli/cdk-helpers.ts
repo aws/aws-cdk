d117ebae82e28f099a4683c92599af800ab249ae
Rico Huijbers <rix0rrr@gmail.com>
2020-09-24 10:19:42 +0200

    core(integ): run all CDK integ tests with `-v`
    Now that we suppress output of non-failing tests, it becomes
    all the more important to have detailed information for failing tests.

--- packages/aws-cdk/test/integ/cli/cdk-helpers.ts
+++ packages/aws-cdk/test/integ/cli/cdk-helpers.ts
@@ -178,7 +178,7 @@ export class TestFixture {
   }
 
   public async cdk(args: string[], options: CdkCliOptions = {}) {
-    return this.shell(['cdk', ...args], {
+    return this.shell(['cdk', '-v', ...args], {
       ...options,
       modEnv: {
         AWS_REGION: this.aws.region,
