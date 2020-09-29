3201d7dbd67a9658e57440ed8423b20dba191b8a
Joe Snell <joepsnell@gmail.com>
2020-09-29 09:46:48 -0500

    get first test working

--- packages/@aws-cdk/aws-glue/test/connection.test.ts
+++ packages/@aws-cdk/aws-glue/test/connection.test.ts
@@ -1,8 +1,7 @@
-import { expect } from '@aws-cdk/assert';
+import { expect, haveResource } from '@aws-cdk/assert';
 import { Stack } from '@aws-cdk/core';
 import { Test } from 'nodeunit';
 import * as glue from '../lib';
-import * as ec2 from '@aws-cdk/aws-ec2';
 
 export = {
   'creates a connection'(test: Test) {
@@ -12,22 +11,31 @@ export = {
         connectionProperties: {},
     });
 
-    expect(stack).toMatch({});
+    expect(stack).to(haveResource('AWS::Glue::Connection', {
+      "CatalogId": {
+        "Ref": "AWS::AccountId"
+      },
+      "ConnectionInput": {
+        "ConnectionProperties": {},
+        "ConnectionType": "JDBC",
+        "PhysicalConnectionRequirements": {}
+      }
+    }));
 
     test.done();
   },
 
-  'creates a connection with vpc'(test: Test) {
-    const stack = new Stack();
-    const vpc = new ec2.Vpc(stack, 'Vpc')
+  // 'creates a connection with vpc'(test: Test) {
+  //   const stack = new Stack();
+  //   const vpc = new ec2.Vpc(stack, 'Vpc')
 
-    new glue.Connection(stack, 'Connection', {
-        connectionProperties: {},
-        vpc
-    });
+  //   new glue.Connection(stack, 'Connection', {
+  //       connectionProperties: {},
+  //       vpc
+  //   });
 
-    expect(stack).toMatch({});
+  //   expect(stack).toMatch({});
 
-    test.done();
-  },
+  //   test.done();
+  // },
 };
