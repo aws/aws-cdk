ad63456e78b84733a913b9570b5c913bc047f62b
Niranjan Jayakar <nija@amazon.com>
2020-10-01 08:57:55 +0100

    rds

--- packages/@aws-cdk/aws-rds/lib/proxy.ts
+++ packages/@aws-cdk/aws-rds/lib/proxy.ts
@@ -2,6 +2,7 @@ import * as ec2 from '@aws-cdk/aws-ec2';
 import * as iam from '@aws-cdk/aws-iam';
 import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
 import * as cdk from '@aws-cdk/core';
+import { Construct } from 'constructs';
 import { IDatabaseCluster } from './cluster-ref';
 import { IEngine } from './engine';
 import { IDatabaseInstance } from './instance';
@@ -327,7 +328,7 @@ export class DatabaseProxy extends cdk.Resource
    * Import an existing database proxy.
    */
   public static fromDatabaseProxyAttributes(
-    scope: cdk.Construct,
+    scope: Construct,
     id: string,
     attrs: DatabaseProxyAttributes,
   ): IDatabaseProxy {
@@ -367,7 +368,7 @@ export class DatabaseProxy extends cdk.Resource
 
   private readonly resource: CfnDBProxy;
 
-  constructor(scope: cdk.Construct, id: string, props: DatabaseProxyProps) {
+  constructor(scope: Construct, id: string, props: DatabaseProxyProps) {
     super(scope, id, { physicalName: props.dbProxyName || id });
 
     const role = props.role || new iam.Role(this, 'IAMRole', {
