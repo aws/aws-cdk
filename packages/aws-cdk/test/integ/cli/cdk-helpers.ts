7f6b139924cf6d1ed17c4485f1fdd09edba42cc9
epolon <epolon@amazon.com>
2020-10-07 15:20:16 +0300

    use version

--- packages/aws-cdk/test/integ/cli/cdk-helpers.ts
+++ packages/aws-cdk/test/integ/cli/cdk-helpers.ts
@@ -10,11 +10,7 @@ const REGIONS = process.env.AWS_REGIONS
   ? process.env.AWS_REGIONS.split(',')
   : [process.env.AWS_REGION ?? process.env.AWS_DEFAULT_REGION ?? 'us-east-1'];
 
-const FRAMEWORK_VERSION = process.env.FRAMEWORK_VERSION
-
-if (!FRAMEWORK_VERSION) {
-  throw new Error('FRAMEWORK_VERSION env variable is required for running integration tests');
-}
+const FRAMEWORK_VERSION = process.env.FRAMEWORK_VERSION;
 
 process.stdout.write(`Using regions: ${REGIONS}\n`);
 
@@ -64,7 +60,7 @@ export function withCdkApp<A extends TestContext & AwsContext>(block: (context: 
 
     let success = true;
     try {
-      await fixture.shell(['npm', 'install',
+      let modules = [
         '@aws-cdk/core',
         '@aws-cdk/aws-sns',
         '@aws-cdk/aws-iam',
@@ -72,7 +68,12 @@ export function withCdkApp<A extends TestContext & AwsContext>(block: (context: 
         '@aws-cdk/aws-ssm',
         '@aws-cdk/aws-ecr-assets',
         '@aws-cdk/aws-cloudformation',
-        '@aws-cdk/aws-ec2'].map(module => `${module}@${FRAMEWORK_VERSION}`));
+        '@aws-cdk/aws-ec2',
+      ];
+      if (FRAMEWORK_VERSION) {
+        modules = modules.map(module => `${module}@${FRAMEWORK_VERSION}`);
+      }
+      await fixture.shell(['npm', 'install', ...modules]);
 
       await ensureBootstrapped(fixture);
 
