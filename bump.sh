128e88159b6a1a72f32b89e46c2ac8dc1126ed11
epolon <epolon@amazon.com>
2020-10-09 00:56:26 +0300

    extract candidate bump logic to a separate script

--- bump.sh
+++ bump.sh
@@ -15,39 +15,19 @@
 set -euo pipefail
 version=${1:-minor}
 
-PRE_RELEASE_TAG=${PRE_RELEASE_TAG:-}
-SKIP_COMMIT=${SKIP_COMMIT:-false}
-SKIP_CHANGELOG=${SKIP_CHANGELOG:-false}
-
 echo "Starting ${version} version bump"
 
 # /bin/bash ./install.sh
 
-args="--release-as ${version}"
-
-if [ ! -z ${PRE_RELEASE_TAG} ]; then
-  args="${args} --prerelease=${PRE_RELEASE_TAG}"
-fi
-
-if [ ${SKIP_COMMIT} = "true" ]; then
-  args="${args} --skip.commit"
-fi
-
-if [ ${SKIP_CHANGELOG} = "true" ]; then
-  args="${args} --skip.changelog"
-fi
-
 # Generate CHANGELOG and create a commit (see .versionrc.json)
-npx standard-version ${args}
+npx standard-version --release-as ${version}
 
-if [ ${SKIP_COMMIT} = "false" ]; then
-  # I am sorry.
-  #
-  # I've gone diving through the code of `conventional-changelog` to see if there
-  # was a way to configure the string and ultimately I decided that a 'sed' was the simpler
-  # way to go.
-  sed -i.tmp -e 's/BREAKING CHANGES$/BREAKING CHANGES TO EXPERIMENTAL FEATURES/' CHANGELOG.md
-  rm CHANGELOG.md.tmp
-  git add CHANGELOG.md
-  git commit --amend --no-edit
-fi
+# I am sorry.
+#
+# I've gone diving through the code of `conventional-changelog` to see if there
+# was a way to configure the string and ultimately I decided that a 'sed' was the simpler
+# way to go.
+sed -i.tmp -e 's/BREAKING CHANGES$/BREAKING CHANGES TO EXPERIMENTAL FEATURES/' CHANGELOG.md
+rm CHANGELOG.md.tmp
+git add CHANGELOG.md
+git commit --amend --no-edit
