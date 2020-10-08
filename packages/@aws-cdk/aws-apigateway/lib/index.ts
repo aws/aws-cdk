548429967562d1445e76ca4306618bae564b9d26
Carlos Yuste <carlos.yuste@col.vueling.com>
2020-10-08 10:35:52 +0200

    Revert "import metrics in index"
    This reverts commit d8d4bab8d278164cee2e77f274fb140559188b1b.

--- packages/@aws-cdk/aws-apigateway/lib/index.ts
+++ packages/@aws-cdk/aws-apigateway/lib/index.ts
@@ -27,5 +27,3 @@ export * from './gateway-response';
 export * from './apigateway.generated';
 // AWS::ApiGatewayV2 CloudFormation resources:
 export * from './apigatewayv2';
-
-import './apigateway-augmentations.generated';
