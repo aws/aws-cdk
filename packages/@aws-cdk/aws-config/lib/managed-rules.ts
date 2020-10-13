7f4f96eb46faf6ba1140be2f0a4ab9e7b78cb943
Shiv Lakshminarayan <shivlaks@amazon.com>
2020-10-13 16:56:31 -0700

    feat(config): convenience class with static constants for referencing AWS managed rules (#10834)
    follow-up with some feedback from the API review:
    * added a class `ManagedRuleIdentifiers` with static constants for managed rule identifiers
    * updated `README` and tests
    * tried to use inclusive wording(blocked/ blocked list as opposed to blacklist)
    
    we decided not to go with an enum like class as this is a breaking change that we don't
    want to force on users as well as the limited value from wrapping a string around a class.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-config/lib/managed-rules.ts
+++ packages/@aws-cdk/aws-config/lib/managed-rules.ts
@@ -2,7 +2,7 @@ import * as iam from '@aws-cdk/aws-iam';
 import * as sns from '@aws-cdk/aws-sns';
 import { Duration, Lazy, Stack } from '@aws-cdk/core';
 import { Construct } from 'constructs';
-import { ManagedRule, ResourceType, RuleProps, RuleScope } from './rule';
+import { ManagedRule, ManagedRuleIdentifiers, ResourceType, RuleProps, RuleScope } from './rule';
 
 /**
  * Construction properties for a AccessKeysRotated
@@ -28,7 +28,7 @@ export class AccessKeysRotated extends ManagedRule {
   constructor(scope: Construct, id: string, props: AccessKeysRotatedProps = {}) {
     super(scope, id, {
       ...props,
-      identifier: 'ACCESS_KEYS_ROTATED',
+      identifier: ManagedRuleIdentifiers.ACCESS_KEYS_ROTATED,
       inputParameters: {
         ...props.maxAge
           ? {
@@ -76,7 +76,7 @@ export class CloudFormationStackDriftDetectionCheck extends ManagedRule {
   constructor(scope: Construct, id: string, props: CloudFormationStackDriftDetectionCheckProps = {}) {
     super(scope, id, {
       ...props,
-      identifier: 'CLOUDFORMATION_STACK_DRIFT_DETECTION_CHECK',
+      identifier: ManagedRuleIdentifiers.CLOUDFORMATION_STACK_DRIFT_DETECTION_CHECK,
       inputParameters: {
         cloudformationRoleArn: Lazy.stringValue({ produce: () => this.role.roleArn }),
       },
@@ -121,7 +121,7 @@ export class CloudFormationStackNotificationCheck extends ManagedRule {
 
     super(scope, id, {
       ...props,
-      identifier: 'CLOUDFORMATION_STACK_NOTIFICATION_CHECK',
+      identifier: ManagedRuleIdentifiers.CLOUDFORMATION_STACK_NOTIFICATION_CHECK,
       inputParameters: props.topics && props.topics.reduce(
         (params, topic, idx) => ({ ...params, [`snsTopic${idx + 1}`]: topic.topicArn }),
         {},
