d9667c4c86d1c1b047384cbc297616b14bf03f17
Niranjan Jayakar <nija@amazon.com>
2020-10-19 12:38:18 +0100

    chore: merge forward script fails when branch is already checked out
    The [AutoPullRequest] CodeBuild job already checks out the base branch.
    This causes the `git checkout -b` command to fail since the branch
    already exists.
    
    Update the script so that branch creation occurs only if it does not
    already exist.
    
    [AutoPullRequest]: https://github.com/aws/cdk-ops/blob/21acd7ff37ad9ab14d8c3f9b41980e9f62f64503/lib/cdk-pipeline.ts#L284-L290

--- scripts/merge-forward.sh
+++ scripts/merge-forward.sh
@@ -4,5 +4,9 @@
 set -exo pipefail
 
 git fetch --all
-git checkout -b v2-main origin/v2-main
+
+# create 'v2-main' if it does not already exist
+(git branch --list | grep 'v2-main') || git checkout -b v2-main origin/v2-main
+
+git checkout v2-main
 git merge origin/master --no-edit
