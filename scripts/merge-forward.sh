f93d98d1738d2906a5afb4b4f3ae26c7b152fc6d
Niranjan Jayakar <nija@amazon.com>
2020-10-19 13:13:34 +0100

    use checkout -B instead

--- scripts/merge-forward.sh
+++ scripts/merge-forward.sh
@@ -4,9 +4,5 @@
 set -exo pipefail
 
 git fetch --all
-
-# create 'v2-main' if it does not already exist
-(git branch --list | grep 'v2-main') || git checkout -b v2-main origin/v2-main
-
-git checkout v2-main
+git checkout -B v2-main origin/v2-main
 git merge origin/master --no-edit
