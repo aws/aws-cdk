bf3dfca7785a03da4c6ec40d46b63da38a248370
Niranjan Jayakar <nija@amazon.com>
2020-10-21 16:01:29 +0100

    adjustments

--- scripts/merge-forward.sh
+++ scripts/merge-forward.sh
@@ -5,4 +5,18 @@ set -exo pipefail
 
 git fetch --all
 git checkout -B v2-main origin/v2-main
-git merge origin/master --no-edit
+
+if [ -z $PR_VALIDATION ]; then
+  from='origin/master'
+  unset -e
+else
+  from=$(git rev-parse HEAD)
+fi
+
+git merge $from --no-edit
+
+if [ $? -ne 0 ]; then
+  echo "::error ::Merge conflicts detected against v2-main branch. Try to avoid."
+  echo "::error ::See above for conflicting files. If unavoidable, add the 'pr/exempt-merge-conflict' label"
+  exit 1
+fi
