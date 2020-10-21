9e1b5663c62a165658f42d0f7be947dc6edb1cb4
Ayush Goyal <goyalayu@amazon.com>
2020-10-21 22:44:42 +0530

    feat(apigatewayv2): VpcLink and HttpProxyPrivateIntegration

---
+++ packages/@aws-cdk/aws-apigatewayv2-integrations/lib/http/index.ts
@@ -0,0 +1,4 @@
+export * from './base-types';
+export * from './alb-integration';
+export * from './nlb-integration';
+export * from './service-discovery-integration';
---
+++ packages/@aws-cdk/aws-apigatewayv2-integrations/lib/http/nlb-integration.ts
@@ -0,0 +1,39 @@
+import { HttpRouteIntegrationBindOptions, HttpRouteIntegrationConfig } from '@aws-cdk/aws-apigatewayv2';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import { HttpPrivateIntegrationOptions } from './base-types';
+import { HttpPrivateIntegration } from './http-private';
+
+/**
+ * Properties to initialize `HttpNlbIntegration`.
+ */
+export interface HttpNlbIntegrationProps extends HttpPrivateIntegrationOptions {
+  /**
+   * The listener to the netwwork load balancer used for the integration
+   */
+  readonly listener: elbv2.NetworkListener;
+}
+
+/**
+ * The Network Load Balancer integration resource for HTTP API
+ */
+export class HttpNlbIntegration extends HttpPrivateIntegration {
+  constructor(private readonly props: HttpNlbIntegrationProps) {
+    super();
+  }
+
+  public bind(options: HttpRouteIntegrationBindOptions): HttpRouteIntegrationConfig {
+    const vpcLink = this._configureVpcLink(options, {
+      vpcLink: this.props.vpcLink,
+      vpc: this.props.listener.loadBalancer.vpc,
+    });
+
+    return {
+      method: this.props.method ?? this.httpMethod,
+      payloadFormatVersion: this.payloadFormatVersion,
+      type: this.integrationType,
+      connectionType: this.connectionType,
+      connectionId: vpcLink.vpcLinkId,
+      uri: this.props.listener.listenerArn,
+    };
+  }
+}
