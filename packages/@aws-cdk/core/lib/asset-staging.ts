405f0f8d49ff226bcf67d6b7545e6a413e221273
Rico Huijbers <rix0rrr@gmail.com>
2020-10-22 14:17:56 +0200

    Move cache

--- packages/@aws-cdk/core/lib/asset-staging.ts
+++ packages/@aws-cdk/core/lib/asset-staging.ts
@@ -8,41 +8,13 @@ import * as minimatch from 'minimatch';
 import { AssetHashType, AssetOptions } from './assets';
 import { BundlingOptions } from './bundling';
 import { FileSystem, FingerprintOptions } from './fs';
+import { Cache } from './private/cache';
 import { Stack } from './stack';
 import { Stage } from './stage';
 
 // v2 - keep this import as a separate section to reduce merge conflict when forward merging with the v2 branch.
 // eslint-disable-next-line
 import { Construct as CoreConstruct } from './construct-compat';
-
-/**
- * A simple cache class.
- *
- * Must be declared at the top of the file because we're going to use it statically in the
- * AssetStaging class.
- */
-class Cache<A> {
-  private cache = new Map<string, A>();
-
-  /**
-   * Clears the cache
-   */
-  public clear() {
-    this.cache.clear();
-  }
-
-  /**
-   * Get a value from the cache or calculate it
-   */
-  public obtain(cacheKey: string, calcFn: () => A): A {
-    let value = this.cache.get(cacheKey);
-    if (value) { return value; }
-
-    value = calcFn();
-    this.cache.set(cacheKey, value);
-    return value;
-  }
-}
 
 /**
  * A previously staged asset
