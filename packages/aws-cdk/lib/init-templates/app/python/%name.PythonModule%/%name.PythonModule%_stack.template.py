656ba505177b277e4c073cc6100af37cca45a8e8
Eric Z. Beard <ericzbeard@gmail.com>
2020-10-26 15:55:35 -0700

    Fix init templates

--- packages/aws-cdk/lib/init-templates/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
+++ packages/aws-cdk/lib/init-templates/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
@@ -3,7 +3,7 @@ from aws_cdk import core
 
 class %name.PascalCased%Stack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
--- packages/aws-cdk/lib/init-templates/sample-app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
+++ packages/aws-cdk/lib/init-templates/sample-app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
@@ -9,8 +9,8 @@ from aws_cdk import (
 
 class %name.PascalCased%Stack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
         queue = sqs.Queue(
             self, "%name.PascalCased%Queue",
