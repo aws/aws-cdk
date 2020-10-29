8928287216a18b3b1d97821912ba572f04acd536
Josh Kellendonk <joshkellendonk@gmail.com>
2020-10-29 13:51:20 -0600

    docs: improve description of allowAllOutbound

--- packages/@aws-cdk/cx-api/lib/context/security-group.ts
+++ packages/@aws-cdk/cx-api/lib/context/security-group.ts
@@ -9,8 +9,9 @@ export interface SecurityGroupContextResponse {
   readonly securityGroupId: string;
 
   /**
-   * Whether the security group of the load balancer allows all outbound
-   * traffic.
+   * Whether the security group allows all outbound traffic. This will be true
+   * when the security group has all-protocol egress permissions to access both
+   * `0.0.0.0/0` and `::/0`.
    */
   readonly allowAllOutbound: boolean;
 }
