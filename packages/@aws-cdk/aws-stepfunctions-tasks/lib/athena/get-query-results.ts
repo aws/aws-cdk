6f67f181aa9dd26e159da44e360af9aa9961dddc
Takumi Akiyama <t.akiym@gmail.com>
2020-11-03 04:08:36 +0900

    fix(stepfunctions-tasks): Athena* APIs have incorrect supported integration patterns (#11188)
    The changes made by #11045 seem to support `WAIT_FOR_TASK_TOKEN (.waitForTaskToken)` but according to the documentation, only `Request Response` and `Run a job (.sync)` are supported: https://docs.aws.amazon.com/step-functions/latest/dg/connect-athena.html
    
    closes #11246
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-stepfunctions-tasks/lib/athena/get-query-results.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/lib/athena/get-query-results.ts
@@ -40,7 +40,6 @@ export class AthenaGetQueryResults extends sfn.TaskStateBase {
 
   private static readonly SUPPORTED_INTEGRATION_PATTERNS: sfn.IntegrationPattern[] = [
     sfn.IntegrationPattern.REQUEST_RESPONSE,
-    sfn.IntegrationPattern.WAIT_FOR_TASK_TOKEN,
   ];
 
   protected readonly taskMetrics?: sfn.TaskMetricsConfig;
