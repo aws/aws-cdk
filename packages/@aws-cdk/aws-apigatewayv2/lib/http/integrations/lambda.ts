4e2deddfe5f35790cc8a5201bd98bf78dbeecc6e
Elad Ben-Israel <benisrae@amazon.com>
2020-11-03 14:28:14 +0200

    fixup

--- packages/@aws-cdk/aws-apigatewayv2/lib/http/integrations/lambda.ts
+++ packages/@aws-cdk/aws-apigatewayv2/lib/http/integrations/lambda.ts
@@ -30,7 +30,7 @@ export class LambdaProxyIntegration implements IHttpRouteIntegration {
 
   public bind(options: HttpRouteIntegrationBindOptions): HttpRouteIntegrationConfig {
     const route = options.route;
-    this.props.handler.addPermission(`${Names.uniqueId(route)}-Permission`, {
+    this.props.handler.addPermission(`${Names.nodeUniqueId(route.node)}-Permission`, {
       scope: options.scope,
       principal: new ServicePrincipal('apigateway.amazonaws.com'),
       sourceArn: Stack.of(route).formatArn({
