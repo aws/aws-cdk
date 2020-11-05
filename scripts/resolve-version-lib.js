ef74a5959f5a2c8c7f471822dc248a3ddcbf60cf
Elad Ben-Israel <benisrae@amazon.com>
2020-11-05 16:28:31 +0200

    chore: multiple release branches without merge conflicts (#11307)
    As we prepare for 2.0, we need to release the CDK concurrently in multiple version lines (1.x and 2.0.0-alpha.x).
    
    In order to avoid merge conflicts of `lerna.json` and `CHANGELOG.md` between the v1 and v2 branches, we extracted the version number from `lerna.json` to `version.vNNN.json` and changelog to `CHANGELOG.vNNN.json` (1.0 is still CHANGELOG.md because it is tracked externally).
    
    A new file called `release.json` has been introduced and includes *static* information about which version line this branch serves. This allows us to avoid merge conflicts caused by
    version bumps between release branches.
    
    This change also cleans up some of the scripts related to versioning and bumps. The main bump script is now implemented in `scripts/bump.js` and interacts with `standard-version` as a library instead of through the CLI. To that end, the `.versionrc.json` file was also removed.
    
    See CONTRIBUTING for more details about how this works.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ scripts/resolve-version-lib.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+const path = require('path');
+const fs = require('fs');
+
+//=============================================================
+// UNIT TESTS: tools/script-tests/test/resolve-version.test.js 
+//=============================================================
+
+function resolveVersion(rootdir) {
+  const ALLOWED_RELEASE_TYPES = [ 'alpha', 'rc', 'stable' ];
+  const MIN_MAJOR = 1, MAX_MAJOR = 2; // extra safety: update to allow new major versions
+  
+  //
+  // parse release.json
+  //
+  const releaseFile = path.join(rootdir, 'release.json');
+  const releaseConfig = require(releaseFile);
+  const majorVersion = releaseConfig.majorVersion;
+  const releaseType = releaseConfig.releaseType;
+  if (!majorVersion) { throw new Error(`"majorVersion"" must be defined in ${releaseFile}`); }
+  if (!releaseType) { throw new Error(`"releaseType" must be defined in ${releaseFile}`); }
+  if (typeof(majorVersion) !== 'number') { throw new Error(`majorVersion=${majorVersion} must be a number`); }
+  if (majorVersion < MIN_MAJOR || majorVersion > MAX_MAJOR) { throw new Error(`majorVersion=${majorVersion} is an unsupported major version (should be between ${MIN_MAJOR} and ${MAX_MAJOR})`); }
+  if (!ALLOWED_RELEASE_TYPES.includes(releaseType)) { throw new Error(`releaseType=${releaseType} is not allowed. Allowed values: ${ALLOWED_RELEASE_TYPES.join(',')}`); }
+  
+  //
+  // resolve and check that we have a version file
+  //
+  
+  const versionFile = `version.v${majorVersion}.json`;
+  const versionFilePath = path.join(rootdir, versionFile);
+  if (!fs.existsSync(versionFilePath)) {
+    throw new Error(`unable to find version file ${versionFile} for major version ${majorVersion}`);
+  }
+  
+  //
+  // validate that current version matches the requirements
+  //
+  
+  const currentVersion = require(versionFilePath).version;
+  console.error(`current version: ${currentVersion}`);
+  if (!currentVersion.startsWith(`${majorVersion}.`)) { 
+    throw new Error(`current version "${currentVersion}" does not use the expected major version ${majorVersion}`); 
+  }
+  if (releaseType === 'stable') {
+    if (currentVersion.includes('-')) {
+      throw new Error(`found pre-release tag in version specified in ${versionFile} is ${currentVersion} but "releaseType"" is set to "stable"`);
+    }
+  } else {
+    if (!currentVersion.includes(`-${releaseType}.`)) {
+      throw new Error(`could not find pre-release tag "${releaseType}" in current version "${currentVersion}" defined in ${versionFile}`);
+    }
+  }
+  
+  //
+  // determine changelog file name
+  //
+  
+  const changelogFile = majorVersion === 1 
+    ? 'CHANGELOG.md' 
+    : `CHANGELOG.v${majorVersion}.md`;
+  
+  //
+  // export all of it
+  //
+  
+  return {
+    version: currentVersion,
+    versionFile: versionFile,
+    changelogFile: changelogFile,
+    prerelease: releaseType !== 'stable' ? releaseType : undefined,
+    marker: '0.0.0',
+  };
+}
+
+module.exports = resolveVersion;
