128bd4aac85efda6586061d69ba70f84b597a144
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2020-11-12 11:43:59 +0100

    fix(core): heavy source fingerpinting when bundling is skipped
    If the asset uses OUTPUT or BUNDLE it's generally because its source is
    a very large directory. This is the case for the `NodejsFunction` which
    mounts the project root (along with the `node_modules` folder!).
    
    Use a custom hash in this case when skipping bundling. Otherwise running
    `cdk ls` can result in heavy fingerprinting operations (again this is
    the case for the `NodejsFunction`).
    
    Regression introduced in #11008
    (https://github.com/aws/aws-cdk/pull/11008/files#diff-62eef996be8abeb157518522c3cbf84a33dd4751c103304df87b04eb6d7bbab6L160)

--- packages/@aws-cdk/core/test/staging.test.ts
+++ packages/@aws-cdk/core/test/staging.test.ts
@@ -731,6 +731,7 @@ nodeunitShim({
     test.equal(asset.sourcePath, directory);
     test.equal(asset.stagedPath, directory);
     test.equal(asset.relativeStagedPath(stack), directory);
+    test.equal(asset.assetHash, '729a2fdb899011ee9f3f226f4cfc03b1c2a83b97ff9c4105192430e6bc18bff3');
 
     test.done();
   },
