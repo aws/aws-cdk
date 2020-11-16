79076a6c85f2e771491c9b8b9d4d4c12e47d5c5f
Kendra Neil <kneil@amazon.com>
2020-11-15 17:48:09 -0800

    fix(aws-custom-resource): aws-custom-resource fails when  is missing
        This change makes getting the metadata a function instead of a const, fixes https://github.com/aws/aws-cdk/issues/7284.
    
        Since this change only impacted the way in which awsSdkMetadata was provided to awsSdkToIamAction no new tests were warranted, but I reran yarn build and all tests stull passed.

--- packages/@aws-cdk/custom-resources/lib/aws-custom-resource/aws-custom-resource.ts
+++ packages/@aws-cdk/custom-resources/lib/aws-custom-resource/aws-custom-resource.ts
@@ -7,16 +7,10 @@ import * as cdk from '@aws-cdk/core';
 import { Construct } from 'constructs';
 import { PHYSICAL_RESOURCE_ID_REFERENCE, flatten } from './runtime';
 
-// don't use "require" since the typescript compiler emits errors since this
-// file is not listed in tsconfig.json.
-const metadata = JSON.parse(fs.readFileSync(path.join(__dirname, 'sdk-api-metadata.json'), 'utf-8'));
-
 /**
  * AWS SDK service metadata.
  */
 export type AwsSdkMetadata = {[key: string]: any};
-
-const awsSdkMetadata: AwsSdkMetadata = metadata;
 
 /**
  * Reference to the physical resource id that can be passed to the AWS operation as a parameter.
@@ -435,6 +429,7 @@ export class AwsCustomResource extends cdk.Construct implements iam.IGrantable {
  */
 function awsSdkToIamAction(service: string, action: string): string {
   const srv = service.toLowerCase();
+  const awsSdkMetadata = getAwsSdkMetadata();
   const iamService = (awsSdkMetadata[srv] && awsSdkMetadata[srv].prefix) || srv;
   const iamAction = action.charAt(0).toUpperCase() + action.slice(1);
   return `${iamService}:${iamAction}`;
@@ -454,4 +449,8 @@ function encodeBooleans(object: object) {
         return v;
     }
   });
+}
+
+function getAwsSdkMetadata(): AwsSdkMetadata {
+  return JSON.parse(fs.readFileSync(path.join(__dirname, 'sdk-api-metadata.json'), 'utf-8'));
 }
