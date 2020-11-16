7cc66733282ac15824e7aa9dbb4b87906641938d
Nathan Peck <peckn@amazon.com>
2020-11-16 22:02:43 +0000

    Adding the test app content

---
+++ packages/@aws-cdk-containers/ecs-service-extensions/test/test-apps/greeter/index.js
@@ -0,0 +1,30 @@
+var request = require('request-promise-native');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const GREETING_URL = process.env.GREETING_URL;
+const NAME_URL = process.env.NAME_URL;
+
+const os = require('os');
+const hostname = os.hostname();
+
+if (!GREETING_URL) {
+  throw new Error('Process requires that environment variable GREETING_URL be passed');
+}
+
+if (!NAME_URL) {
+  throw new Error('Process requires that environment variable NAME_URL be passed');
+}
+
+app.get('*', async function (req, res) {
+  res.send(`From ${hostname}: ` + await request(GREETING_URL) + ' ' + await request(NAME_URL));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+// This causes the process to respond to "docker stop" faster
+process.on('SIGTERM', function () {
+  console.log('Received SIGTERM, shutting down');
+  app.close();
+});
---
+++ packages/@aws-cdk-containers/ecs-service-extensions/test/test-apps/name/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const os = require('os');
+const hostname = os.hostname();
+
+var names = [
+  'James', 'Mary',
+  'John', 'Patricia',
+  'Robert', 'Jennifer',
+  'Michael', 'Linda',
+  'William', 'Elizabeth',
+  'David', 'Barbara',
+  'Richard', 'Susan',
+  'Joseph', 'Jessica',
+  'Thomas', 'Sarah',
+  'Charles', 'Karen'
+];
+
+app.get('*', function (req, res) {
+  res.send(names[Math.floor(Math.random() * names.length)] + ` (${hostname})`);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+// This causes the process to respond to "docker stop" faster
+process.on('SIGTERM', function () {
+  console.log('Received SIGTERM, shutting down');
+  app.close();
+});
