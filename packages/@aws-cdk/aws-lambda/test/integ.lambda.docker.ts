ded2120fff3ec333284a43ed63616b9e1ede770d
Niranjan Jayakar <nija@amazon.com>
2020-11-18 16:17:25 +0000

    integ test

---
+++ packages/@aws-cdk/aws-lambda/test/integ.lambda.docker.ts
@@ -0,0 +1,34 @@
+import * as path from 'path';
+import { App, Aspects, IAspect, Stack } from '@aws-cdk/core';
+import { IConstruct } from 'constructs';
+import { CfnFunction, Code, Function, Runtime } from '../lib';
+
+class TestStack extends Stack {
+  constructor(scope: App, id: string) {
+    super(scope, id, {
+      env: { region: 'sa-east-1' }, // the feature is available only in sa-east-1 during private beta. Remove after launch.
+    });
+
+    new Function(this, 'MyLambda', {
+      code: Code.fromAssetImage(path.join(__dirname, 'docker-lambda-handler')),
+      handler: 'app.handler',
+      runtime: Runtime.NODEJS_12_X,
+    });
+  }
+}
+
+class PrivateResourceAspect implements IAspect {
+  visit(construct: IConstruct): void {
+    if (construct instanceof CfnFunction) {
+      (construct as any).cfnResourceType = 'AWSLambdaBeta::Lambda::Function';
+    }
+  }
+}
+
+const app = new App();
+const stack = new TestStack(app, 'lambda-ecr-docker');
+
+// the feature is available as an CFN private resource during private beta. Remove after launch.
+Aspects.of(stack).add(new PrivateResourceAspect());
+
+app.synth();
