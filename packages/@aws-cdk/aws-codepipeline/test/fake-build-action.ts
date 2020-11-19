e24048ffb1a04bc458d4c220bc4b632379d50c91
Rico Huijbers <rix0rrr@gmail.com>
2020-11-19 16:22:08 +0100

    Add accidentally moved files back

---
+++ packages/@aws-cdk/aws-codepipeline/test/fake-build-action.ts
@@ -0,0 +1,55 @@
+import * as iam from '@aws-cdk/aws-iam';
+import { IResource } from '@aws-cdk/core';
+import { Construct } from 'constructs';
+import * as codepipeline from '../lib';
+
+export interface FakeBuildActionProps extends codepipeline.CommonActionProps {
+  input: codepipeline.Artifact;
+
+  output?: codepipeline.Artifact;
+
+  extraInputs?: codepipeline.Artifact[];
+
+  owner?: string;
+
+  role?: iam.IRole;
+
+  account?: string;
+
+  region?: string;
+
+  customConfigKey?: string;
+
+  resource?: IResource;
+}
+
+export class FakeBuildAction extends codepipeline.Action {
+  private readonly customConfigKey: string | undefined;
+
+  constructor(props: FakeBuildActionProps) {
+    super({
+      ...props,
+      category: codepipeline.ActionCategory.BUILD,
+      provider: 'Fake',
+      artifactBounds: { minInputs: 1, maxInputs: 3, minOutputs: 0, maxOutputs: 1 },
+      inputs: [props.input, ...props.extraInputs || []],
+      outputs: props.output ? [props.output] : undefined,
+    });
+    this.customConfigKey = props.customConfigKey;
+  }
+
+  public testChangeInputs(inputs: codepipeline.Artifact[]) {
+    super.changeInputs(inputs);
+  }
+
+  public bound(_scope: Construct, _stage: codepipeline.IStage, _options: codepipeline.ActionBindOptions): codepipeline.ActionConfig {
+    return {
+      boundAction: {
+        action: this,
+        configuration: () => ({
+          CustomConfigKey: this.customConfigKey,
+        }),
+      },
+    };
+  }
+}
---
+++ packages/@aws-cdk/aws-codepipeline/test/fake-source-action.ts
@@ -0,0 +1,45 @@
+import { Lazy } from '@aws-cdk/core';
+import { Construct } from 'constructs';
+import * as codepipeline from '../lib';
+
+export interface IFakeSourceActionVariables {
+  readonly firstVariable: string;
+}
+
+export interface FakeSourceActionProps extends codepipeline.CommonActionProps {
+  readonly output: codepipeline.Artifact;
+
+  readonly extraOutputs?: codepipeline.Artifact[];
+
+  readonly region?: string;
+}
+
+export class FakeSourceAction extends codepipeline.Action {
+  public readonly variables: IFakeSourceActionVariables;
+
+  constructor(props: FakeSourceActionProps) {
+    super({
+      ...props,
+      category: codepipeline.ActionCategory.SOURCE,
+      provider: 'Fake',
+      artifactBounds: { minInputs: 0, maxInputs: 0, minOutputs: 1, maxOutputs: 4 },
+      outputs: [props.output, ...props.extraOutputs || []],
+    });
+    this.variables = {
+      firstVariable: Lazy.stringValue({ produce: () => `#{${this.actionProperties.variablesNamespace}.FirstVariable}` }),
+    };
+  }
+
+  public testChangeOutputs(outputs: codepipeline.Artifact[]) {
+    super.changeOutputs(outputs);
+  }
+
+  public bound(_scope: Construct, _stage: codepipeline.IStage, _options: codepipeline.ActionBindOptions): codepipeline.ActionConfig {
+    return {
+      boundAction: {
+        action: this,
+        configuration: () => undefined,
+      },
+    };
+  }
+}
