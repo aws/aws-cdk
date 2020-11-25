cf52975e2021d1da6b350fbb5c446cd2ad99460c
Nick Lynch <nlynch@amazon.com>
2020-11-25 11:19:28 +0000

    Slightly better

--- scripts/merge-forward.sh
+++ scripts/merge-forward.sh
@@ -9,8 +9,6 @@ git checkout -B v2-main origin/v2-main
 # Some package rules differ between v1 and v2, most notably which packages can be public vs private.
 # These differences are fixable via 'pkglint', so we run that and commit the delta (if any).
 yarn install --frozen-lockfile
-# Build pkglint and its dependencies (e.g., eslint-plugin-cdk), then run.
-cd tools/pkglint && ../../scripts/buildup && cd ../../
 yarn pkglint
 # Commit the difference, if there is one.
 git diff --quiet || git commit -am 'automatic pkglint fixes'
