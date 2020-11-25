450f993cbb767f1118011909d9596be4cbc01c6e
epolon <epolon@amazon.com>
2020-11-25 12:03:47 +0200

    add kms key to integ test

--- packages/@aws-cdk/aws-elasticsearch/test/integ.elasticsearch.ts
+++ packages/@aws-cdk/aws-elasticsearch/test/integ.elasticsearch.ts
@@ -1,4 +1,6 @@
 import { EbsDeviceVolumeType } from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
+import * as kms from '@aws-cdk/aws-kms';
 import { App, Stack, StackProps } from '@aws-cdk/core';
 import { Construct } from 'constructs';
 import * as es from '../lib';
@@ -6,6 +8,8 @@ import * as es from '../lib';
 class TestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const key = new kms.Key(this, 'Key');
 
     new es.Domain(this, 'Domain', {
       version: es.ElasticsearchVersion.V7_1,
@@ -20,7 +24,19 @@ class TestStack extends Stack {
       nodeToNodeEncryption: true,
       encryptionAtRest: {
         enabled: true,
+        // this is important as it affects the permissions needed
+        // to perform domain operations
+        kmsKey: key,
       },
+      // test the access policies custom resource works
+      accessPolicies: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: ['es:ESHttp*'],
+          principals: [new iam.AnyPrincipal()],
+          resources: ['*'],
+        }),
+      ],
     });
   }
 }
