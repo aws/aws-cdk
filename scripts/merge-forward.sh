c1740b3bcc4184fdee2c7792eed2c030d330c648
Nick Lynch <nlynch@amazon.com>
2020-11-25 11:57:06 +0000

    chore: fix merge-forward pkglint (#11676)
    Install dependencies and pkglint properly before running pkglint on all packages.

--- scripts/merge-forward.sh
+++ scripts/merge-forward.sh
@@ -8,7 +8,9 @@ git checkout -B v2-main origin/v2-main
 
 # Some package rules differ between v1 and v2, most notably which packages can be public vs private.
 # These differences are fixable via 'pkglint', so we run that and commit the delta (if any).
+yarn install --frozen-lockfile
 yarn pkglint
+# Commit the difference, if there is one.
 git diff --quiet || git commit -am 'automatic pkglint fixes'
 
 git merge origin/master --no-edit
