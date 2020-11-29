1669244157db1537a5907837fe181065628b5b05
Jonathan Goldwasser <jogold@users.noreply.github.com>
2020-11-29 10:38:31 +0100

    fix(lambda-nodejs): automatic entry finding with compilerOptions.outDir (#11729)
    When the defining file is a `.ts` that is emitted into an
    [`outDir`](https://www.typescriptlang.org/tsconfig#outDir) extracting
    the defining filename with the stacktrace returns the original path
    because of the sourcemap.
    
    So, if the `.ts` file is defined in `src` and emitted into `lib`
    using the stacktrace returns the path in `src`. When shipping a
    construct library with npm the `src` is not published and the
    automatic entry finding fails.
    
    The `src`/`lib` architecture is the one in https://github.com/projen/projen.
    
    Use the `CallSite` API to avoid this problem.
    
    Inspired by https://github.com/sindresorhus/callsites and
    https://github.com/sindresorhus/caller-callsite.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-lambda-nodejs/lib/util.ts
+++ packages/@aws-cdk/aws-lambda-nodejs/lib/util.ts
@@ -3,46 +3,33 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 
-// From https://github.com/errwischt/stacktrace-parser/blob/master/src/stack-trace-parser.js
-const STACK_RE = /^\s*at (?:((?:\[object object\])?[^\\/]+(?: \[as \S+\])?) )?\(?(.*?):(\d+)(?::(\d+))?\)?\s*$/i;
-
-/**
- * A parsed stack trace line
- */
-export interface StackTrace {
-  readonly file: string;
-  readonly methodName?: string;
-  readonly lineNumber: number;
-  readonly column: number;
+export interface CallSite {
+  getThis(): any;
+  getTypeName(): string;
+  getFunctionName(): string;
+  getMethodName(): string;
+  getFileName(): string;
+  getLineNumber(): number;
+  getColumnNumber(): number;
+  getFunction(): Function;
+  getEvalOrigin(): string;
+  isNative(): boolean;
+  isToplevel(): boolean;
+  isEval(): boolean;
+  isConstructor(): boolean;
 }
 
 /**
- * Parses the stack trace of an error
+ * Get callsites from the V8 stack trace API
+ *
+ * https://github.com/sindresorhus/callsites
  */
-export function parseStackTrace(error?: Error): StackTrace[] {
-  const err = error || new Error();
-
-  if (!err.stack) {
-    return [];
-  }
-
-  const lines = err.stack.split('\n');
-
-  const stackTrace: StackTrace[] = [];
-
-  for (const line of lines) {
-    const results = STACK_RE.exec(line);
-    if (results) {
-      stackTrace.push({
-        file: results[2],
-        methodName: results[1],
-        lineNumber: parseInt(results[3], 10),
-        column: parseInt(results[4], 10),
-      });
-    }
-  }
-
-  return stackTrace;
+export function callsites(): CallSite[] {
+  const _prepareStackTrace = Error.prepareStackTrace;
+  Error.prepareStackTrace = (_, stack) => stack;
+  const stack = new Error().stack?.slice(1);
+  Error.prepareStackTrace = _prepareStackTrace;
+  return stack as unknown as CallSite[];
 }
 
 /**
