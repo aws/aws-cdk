7656d215a3a26866ab7476db86284756c8003049
Dominic Fezzie <fezzid@amazon.com>
2020-12-07 20:41:17 -0800

    update ecs service extension to use new service discovery class

--- packages/@aws-cdk-containers/ecs-service-extensions/lib/extensions/appmesh.ts
+++ packages/@aws-cdk-containers/ecs-service-extensions/lib/extensions/appmesh.ts
@@ -279,7 +279,9 @@ export class AppMeshExtension extends ServiceExtension {
     this.virtualNode = new appmesh.VirtualNode(this.scope, `${this.parentService.id}-virtual-node`, {
       mesh: this.mesh,
       virtualNodeName: this.parentService.id,
-      cloudMapService: service.cloudMapService,
+      serviceDiscovery: service.cloudMapService? appmesh.ServiceDiscovery.cloudmap({
+        service: service.cloudMapService,
+      }): undefined,
       listeners: [addListener(this.protocol, containerextension.trafficPort)],
     });
 
