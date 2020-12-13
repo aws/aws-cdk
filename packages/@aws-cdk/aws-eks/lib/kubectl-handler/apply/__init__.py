b28c15ad26176b1bf6da1f8d1a2b259be54ad559
Elad Ben-Israel <benisrae@amazon.com>
2020-12-13 21:57:20 +0200

    fix(eks): k8s resources accidentally deleted due to logical ID change (#12053)
    The `KubernetesManifest` construct used `kubectl apply` for both CREATE and UPDATE operations. This means that if, for example, two manifests had resources with the same k8s name (`metadata.name`), the second manifest created will not fail, but rather override the resource definition.
    
    As a consequence, if the logical ID of a `KubernetesManifest` resource was changed (without a change in the physical name), CFN would perform a replacement process which involves a CREATE of the new resource and then a DELETE of the old one. Since the CREATE operation was implemented through `apply`, it succeeded (with no-op) but then the DELETE operation would delete the resource. The result is that the resource was deleted.
    
    The solution is to use `kubectl create --save-config` instead of `kubectl apply` for CREATE operations. This yields the desired CREATE semantics (dah!).
    
    Now, if a `KubernetesManifest` resource is defined with a K8S object name that already exists, the CREATE operation will fail as expected. The logical ID change scenario (resource replacement), would also issue a CREATE operation first which will fail.
    
    To change logical IDs of `KubernetesManifest` resources, users will have to either delete the old resource or change its physical name.
    
    Since this is quite hard to test (due to multi-phase deployments and failure modes), this was tested manually:
    
    1. Defined a manifest with logical name X1 and physical name Y1 -> CREATE was issued
    2. Changed logical name to X2 (physical remains Y1) -> update failed because CFN issues a CREATE operation first (#10397)
    3. Changed also the physical name to Y2 -> deploy succeeded, new resource created, old resource pruned.
    
    This fixes #10397
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-eks/lib/kubectl-handler/apply/__init__.py
+++ packages/@aws-cdk/aws-eks/lib/kubectl-handler/apply/__init__.py
@@ -40,7 +40,10 @@ def apply_handler(event, context):
 
     logger.info("manifest written to: %s" % manifest_file)
 
-    if request_type == 'Create' or request_type == 'Update':
+    if request_type == 'Create':
+        # --save-config will allow us to use "apply" later
+        kubectl('create', manifest_file, '--save-config')
+    elif request_type == 'Update':
         opts = []
         if prune_label is not None:
             opts = ['--prune', '-l', prune_label]
