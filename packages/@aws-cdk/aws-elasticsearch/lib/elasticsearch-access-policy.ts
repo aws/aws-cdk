934aad47cf49d3f63f113408b6bef0e9947f63e5
epolon <epolon@amazon.com>
2020-12-14 16:41:44 +0200

    move some code around and fixup tests

--- packages/@aws-cdk/aws-elasticsearch/lib/elasticsearch-access-policy.ts
+++ packages/@aws-cdk/aws-elasticsearch/lib/elasticsearch-access-policy.ts
@@ -1,5 +1,4 @@
 import * as iam from '@aws-cdk/aws-iam';
-import * as kms from '@aws-cdk/aws-kms';
 import * as cdk from '@aws-cdk/core';
 import * as cr from '@aws-cdk/custom-resources';
 
@@ -21,14 +20,6 @@ export interface ElasticsearchAccessPolicyProps {
    * The access policy statements for the Elasticsearch cluster
    */
   readonly accessPolicies: iam.PolicyStatement[];
-
-  /**
-   * The key used for cluster encryption at rest.
-   * Needed in order to grant kms permissions on this key for the operation to succeed.
-   *
-   * @default - no key.
-   */
-  readonly kmsKey?: kms.IKey;
 }
 
 /**
@@ -56,19 +47,5 @@ export class ElasticsearchAccessPolicy extends cr.AwsCustomResource {
       policy: cr.AwsCustomResourcePolicy.fromSdkCalls({ resources: [props.domainArn] }),
     });
 
-    if (props.kmsKey) {
-
-      // https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/encryption-at-rest.html
-
-      // these permissions are documented as required during domain creation.
-      // while not strictly documented for updates as well, it stands to reason that an update
-      // operation might require these in case the cluster uses a kms key.
-      // empircal evidence shows this is indeed required: https://github.com/aws/aws-cdk/issues/11412
-      this.grantPrincipal.addToPrincipalPolicy(new iam.PolicyStatement({
-        actions: ['kms:List*', 'kms:Describe*', 'kms:CreateGrant'],
-        resources: [props.kmsKey.keyArn],
-        effect: iam.Effect.ALLOW,
-      }));
-    }
   }
 }
--- packages/@aws-cdk/aws-elasticsearch/test/elasticsearch-access-policy.test.ts
+++ packages/@aws-cdk/aws-elasticsearch/test/elasticsearch-access-policy.test.ts
@@ -1,7 +1,5 @@
 import '@aws-cdk/assert/jest';
-import * as assert from '@aws-cdk/assert';
 import * as iam from '@aws-cdk/aws-iam';
-import * as kms from '@aws-cdk/aws-kms';
 import { App, Stack } from '@aws-cdk/core';
 import { ElasticsearchAccessPolicy } from '../lib/elasticsearch-access-policy';
 
@@ -12,44 +10,6 @@ beforeEach(() => {
   app = new App();
   stack = new Stack(app, 'Stack', {
     env: { account: '1234', region: 'testregion' },
-  });
-});
-
-test('grants kms permissions if needed', () => {
-
-  const key = new kms.Key(stack, 'Key');
-
-  new ElasticsearchAccessPolicy(stack, 'ElasticsearchAccessPolicy', {
-    domainName: 'TestDomain',
-    domainArn: 'test:arn',
-    accessPolicies: [new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: ['es:ESHttp*'],
-      principals: [new iam.Anyone()],
-      resources: ['test:arn'],
-    })],
-    kmsKey: key,
-  });
-
-  const resources = assert.expect(stack).value.Resources;
-  expect(resources.AWS679f53fac002430cb0da5b7982bd2287ServiceRoleDefaultPolicyD28E1A5E.Properties.PolicyDocument).toEqual({
-    Statement: [
-      {
-        Action: [
-          'kms:List*',
-          'kms:Describe*',
-          'kms:CreateGrant',
-        ],
-        Effect: 'Allow',
-        Resource: {
-          'Fn::GetAtt': [
-            'Key961B73FD',
-            'Arn',
-          ],
-        },
-      },
-    ],
-    Version: '2012-10-17',
   });
 });
 
