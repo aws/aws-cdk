17d84ad2fb8a27bf688c93d35efab600db92795d
Elad Ben-Israel <benisrae@amazon.com>
2020-12-14 20:12:56 +0200

    fix(eks): failure to deploy cluster since aws-auth configmap exists (#12068)
    The change in #12053 introduced a regression which causes failures in creating new clusters. Since we changed the KubernetesManifest resource to use `kubectl create` in CREATE operations, the attempt to create the `aws-auth` config map is failing because this config map is already created by the cluster.
    
    This change adds an `override` to `KubernetesManifest` which will cause CREATE to be performed using `apply` instead, which practically allows overriding/adopting existing K8s resources.
    
    
    ----
    
    
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-eks/lib/kubectl-handler/apply/__init__.py
+++ packages/@aws-cdk/aws-eks/lib/kubectl-handler/apply/__init__.py
@@ -23,6 +23,7 @@ def apply_handler(event, context):
     manifest_text = props['Manifest']
     role_arn      = props['RoleArn']
     prune_label   = props.get('PruneLabel', None)
+    overwrite     = props.get('Overwrite', False)
 
     # "log in" to the cluster
     subprocess.check_call([ 'aws', 'eks', 'update-kubeconfig',
@@ -41,8 +42,14 @@ def apply_handler(event, context):
     logger.info("manifest written to: %s" % manifest_file)
 
     if request_type == 'Create':
-        # --save-config will allow us to use "apply" later
-        kubectl('create', manifest_file, '--save-config')
+        # if "overwrite" is enabled, then we use "apply" for CREATE operations
+        # which technically means we can determine the desired state of an
+        # existing resource.
+        if overwrite:
+            kubectl('apply', manifest_file)
+        else:
+            # --save-config will allow us to use "apply" later
+            kubectl('create', manifest_file, '--save-config')
     elif request_type == 'Update':
         opts = []
         if prune_label is not None:
