6b08996f8ac74d75fbf6b87c7caad0455b5a7c83
Nick Lynch <nlynch@amazon.com>
2020-12-16 11:36:02 +0000

    fix(cloudfront): cross-region EdgeFunction does not work within a Stage
    The creation of the cross-region stack was always defaulting to the root,
    resulting in potential cross-stage dependencies.
    
    fixes #12092

--- packages/@aws-cdk/aws-cloudfront/lib/experimental/edge-function.ts
+++ packages/@aws-cdk/aws-cloudfront/lib/experimental/edge-function.ts
@@ -201,8 +201,8 @@ export class EdgeFunction extends Resource implements lambda.IVersion {
   }
 
   private edgeStack(stackId?: string): Stack {
-    const stage = this.node.root;
-    if (!stage || !Stage.isStage(stage)) {
+    const stage = Stage.of(this);
+    if (!stage) {
       throw new Error('stacks which use EdgeFunctions must be part of a CDK app or stage');
     }
     const region = this.env.region;
