289b59fae08d61d84bf07db361c308bdd52b4e40
daisuke-yoshimoto <yoshimoto@intra-mart.jp>
2020-12-17 17:17:47 +0900

    Fix test case

--- packages/@aws-cdk/aws-efs/test/efs-file-system.test.ts
+++ packages/@aws-cdk/aws-efs/test/efs-file-system.test.ts
@@ -22,7 +22,7 @@ test('default file system is created correctly', () => {
     DeletionPolicy: 'Retain',
     UpdateReplacePolicy: 'Retain',
   }, ResourcePart.CompleteDefinition));
-  expectCDK(stack).to(haveResource('AWS::EFS::MountTarget'));
+  expectCDK(stack).notTo(haveResource('AWS::EFS::MountTarget'));
   expectCDK(stack).to(haveResource('AWS::EC2::SecurityGroup'));
 });
 
