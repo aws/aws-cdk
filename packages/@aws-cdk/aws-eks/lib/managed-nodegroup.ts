481e6f12eabb6647127c740a8d1d4a4b8649ccd4
Pahud Hsieh <pahudnet@gmail.com>
2020-12-18 11:25:57 +0800

    Update packages/@aws-cdk/aws-eks/lib/managed-nodegroup.ts
    Co-authored-by: Eli Polonsky <Eli.polonsky@gmail.com>

--- packages/@aws-cdk/aws-eks/lib/managed-nodegroup.ts
+++ packages/@aws-cdk/aws-eks/lib/managed-nodegroup.ts
@@ -285,7 +285,7 @@ export class Nodegroup extends Resource implements INodegroup {
     }
 
     if (props.instanceType) {
-      Annotations.of(this).addWarning('instanceType will be deprecated, remember to use instanceTypes instead.');
+      Annotations.of(this).addWarning(`'instanceType' is deprecated and will be removed in the next major version. please use 'instanceTypes' instead`);
     }
     const instanceTypes = props.instanceTypes ?? (props.instanceType ? [props.instanceType] : Nodegroup.DEFAULT_INSTANCE_TYPES);
     const determinedAmiType = determineAmiTypes(instanceTypes);
