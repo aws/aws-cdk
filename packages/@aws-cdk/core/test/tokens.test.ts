3145e6a910436f49c25f8cf9fc60a39133337435
Rico Huijbers <rix0rrr@gmail.com>
2020-12-21 17:09:33 +0100

    Disregard CDK_DISABLE_STACK_TRACES completely for the purposes of Tokens

--- packages/@aws-cdk/core/test/tokens.test.ts
+++ packages/@aws-cdk/core/test/tokens.test.ts
@@ -656,10 +656,8 @@ nodeunitShim({
     }
 
     const previousValue = process.env.CDK_DEBUG;
-    const previousDisable = process.env.CDK_DISABLE_STACK_TRACE;
 
     process.env.CDK_DEBUG = 'true';
-    delete process.env.CDK_DISABLE_STACK_TRACE;
     const x = showMeInTheStackTrace();
     let message;
     try {
@@ -668,7 +666,6 @@ nodeunitShim({
       message = e.message;
     } finally {
       process.env.CDK_DEBUG = previousValue;
-      process.env.CDK_DISABLE_STACK_TRACE = previousDisable;
     }
 
     test.ok(message && message.includes('showMeInTheStackTrace'));
