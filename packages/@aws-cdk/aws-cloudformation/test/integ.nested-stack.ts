ef9d14af207e052b303eaf809d36084c28059ba6
Adam Ruka <adamruka@amazon.com>
2020-12-29 14:43:24 -0800

    Ignore asset changes in a snapshot test in @aws-cdk/aws-cloudformation.

--- packages/@aws-cdk/aws-cloudformation/test/integ.nested-stack.ts
+++ packages/@aws-cdk/aws-cloudformation/test/integ.nested-stack.ts
@@ -1,3 +1,5 @@
+/// !cdk-integ pragma:ignore-assets
+
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as sns from '@aws-cdk/aws-sns';
 import * as sns_subscriptions from '@aws-cdk/aws-sns-subscriptions';
