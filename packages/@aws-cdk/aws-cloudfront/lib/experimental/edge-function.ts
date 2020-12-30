bdb02a47046003093bad9be03f5e5f12b36eb80f
Adam Ruka <adamruka@amazon.com>
2020-12-29 17:07:04 -0800

    Change the calculateFunctionHash() in @aws-cloudfront that is copied from @aws-lambda.

--- packages/@aws-cdk/aws-cloudfront/lib/experimental/edge-function.ts
+++ packages/@aws-cdk/aws-cloudfront/lib/experimental/edge-function.ts
@@ -252,11 +252,44 @@ function addEdgeLambdaToRoleTrustStatement(role: iam.IRole) {
 // This should be DRY'ed up (exported by @aws-lambda) before this is marked as stable.
 function calculateFunctionHash(fn: lambda.Function) {
   const stack = Stack.of(fn);
+
   const functionResource = fn.node.defaultChild as CfnResource;
+
   // render the cloudformation resource from this function
   const config = stack.resolve((functionResource as any)._toCloudFormation());
+  // config is of the shape: { Resources: { LogicalId: { Type: 'Function', Properties: { ... } }}}
+  const resources = config.Resources;
+  const logicalId = Object.keys(resources)[0];
+  const resourceAttributes = resources[logicalId];
+  // sort the keys, so that the order in the string returned from JSON.stringify() is stable
+  // as new properties are added to the CfnFunction class
+  const stringifiedConfig = JSON.stringify(config, [
+    'Resources', logicalId,
+    // 'Type' and 'Properties' are before 'DependsOn' today, so leave it that way
+    'Type', 'Properties',
+    // sort the resource attributes
+    // 'Environment' winds up at the end today, so preserve that
+    ...allKeysRecursively(resourceAttributes, ['Environment']).sort(),
+    'Environment',
+    // for legacy reasons, we do _not_ sort the env variable keys
+    ...allKeysRecursively(resourceAttributes?.Properties?.Environment),
+  ]);
 
   const hash = crypto.createHash('md5');
-  hash.update(JSON.stringify(config));
+  hash.update(stringifiedConfig);
   return hash.digest('hex');
+}
+
+function allKeysRecursively(input: any, blacklist: string[] = []): string[] {
+  if (typeof(input) !== 'object') {
+    return [];
+  }
+  const ret = new Array<string>();
+  for (const [key, val] of Object.entries(input)) {
+    if (val != null && blacklist.indexOf(key) === -1) {
+      ret.push(key);
+      ret.push(...allKeysRecursively(val, blacklist));
+    }
+  }
+  return ret;
 }
