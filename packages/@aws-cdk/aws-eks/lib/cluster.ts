ccee9e723ff14008c8d06f2974013303f901caac
Dirk Nilius <dirk.nilius@cgi.com>
2021-01-06 08:40:23 +0100

    fix(aws-eks): fixed the aws-node-termination-handler helm chart node selector
    fixes #12368
    
    The former implementation was using the string format for submitting Helm values in the CLI ("parent.child=value"). This does not work here as JavaScript does not expand the string. We need to define the values is the correct hierarchy.

--- packages/@aws-cdk/aws-eks/lib/cluster.ts
+++ packages/@aws-cdk/aws-eks/lib/cluster.ts
@@ -1448,7 +1448,9 @@ export class Cluster extends ClusterBase {
         repository: 'https://aws.github.io/eks-charts',
         namespace: 'kube-system',
         values: {
-          'nodeSelector.lifecycle': LifecycleLabel.SPOT,
+          nodeSelector:{
+            lifecycle: LifecycleLabel.SPOT,
+          }
         },
       });
     }
