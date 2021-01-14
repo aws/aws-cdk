a50f7bc6eb9dbf4d3c084fddadebac67fc10315b
Elad Ben-Israel <benisrae@amazon.com>
2021-01-14 14:08:07 +0200

    more fixes

--- packages/@aws-cdk/aws-elasticsearch/lib/elasticsearch-access-policy.ts
+++ packages/@aws-cdk/aws-elasticsearch/lib/elasticsearch-access-policy.ts
@@ -1,5 +1,4 @@
 import * as iam from '@aws-cdk/aws-iam';
-import * as cdk from '@aws-cdk/core';
 import * as cr from '@aws-cdk/custom-resources';
 
 // keep this import separate from other imports to reduce chance for merge conflicts with v2-main
