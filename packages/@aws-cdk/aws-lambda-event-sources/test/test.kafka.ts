410ac08651a6ed15a8c5bd4e3e26279c06839931
Jan Brauer <jan.brauer@superluminar.io>
2021-01-14 14:49:01 +0100

    Add test for SelfManagedKafkaEventSource

--- packages/@aws-cdk/aws-lambda-event-sources/test/test.kafka.ts
+++ packages/@aws-cdk/aws-lambda-event-sources/test/test.kafka.ts
@@ -9,7 +9,7 @@ import { TestFunction } from './test-function';
 /* eslint-disable quote-props */
 
 export = {
-  'sufficiently complex example'(test: Test) {
+  'MSK: sufficiently complex example'(test: Test) {
     // GIVEN
     const stack = new cdk.Stack();
     const fn = new TestFunction(stack, 'Fn');
@@ -66,6 +66,77 @@ export = {
         'Ref': 'Fn9270CBC0',
       },
       'BatchSize': 100,
+      'StartingPosition': 'TRIM_HORIZON',
+      'Topics': [
+        kafkaTopic,
+      ],
+      'SourceAccessConfigurations': [
+        {
+          'Type': 'BASIC_AUTH',
+          'URI': {
+            'Ref': 'SecretA720EF05',
+          },
+        },
+      ],
+    }));
+
+    test.done();
+  },
+
+  'self managed Kafka: sufficiently complex example'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const fn = new TestFunction(stack, 'Fn');
+    const kafkaTopic = 'some-topic';
+    const secret = new Secret(stack, 'Secret', { secretName: 'AmazonMSK_KafkaSecret' });
+    const bootstrapServers = ['kafka-broker:9092'];
+
+    // WHEN
+    fn.addEventSource(new sources.SelfManagedKafkaEventSource(
+      bootstrapServers,
+      kafkaTopic,
+      secret,
+      {
+        startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+      }));
+
+    // THEN
+    expect(stack).to(haveResource('AWS::IAM::Policy', {
+      'PolicyDocument': {
+        'Statement': [
+          {
+            'Action': [
+              'secretsmanager:GetSecretValue',
+              'secretsmanager:DescribeSecret',
+            ],
+            'Effect': 'Allow',
+            'Resource': {
+              'Ref': 'SecretA720EF05',
+            },
+          },
+        ],
+        'Version': '2012-10-17',
+      },
+      'PolicyName': 'FnServiceRoleDefaultPolicyC6A839BF',
+      'Roles': [
+        {
+          'Ref': 'FnServiceRoleB9001A96',
+        },
+      ],
+    }));
+
+    expect(stack).to(haveResource('AWS::Lambda::EventSourceMapping', {
+      'FunctionName': {
+        'Ref': 'Fn9270CBC0',
+      },
+      'BatchSize': 100,
+      'SelfManagedEventSource': {
+        'Endpoints': {
+          'KafkaBootstrapServers': [
+            'kafka-broker:9092',
+          ],
+        },
+      },
       'StartingPosition': 'TRIM_HORIZON',
       'Topics': [
         kafkaTopic,
