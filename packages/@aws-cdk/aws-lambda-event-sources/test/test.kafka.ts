1f510d64a7f0c6b16d2372d9034c6bd05323afcd
Jan Brauer <jan.brauer@superluminar.io>
2021-01-14 14:05:32 +0100

    Add test for ManagedKafkaEventSource

---
+++ packages/@aws-cdk/aws-lambda-event-sources/test/test.kafka.ts
@@ -0,0 +1,84 @@
+import { expect, haveResource } from '@aws-cdk/assert';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { Secret } from '@aws-cdk/aws-secretsmanager';
+import * as cdk from '@aws-cdk/core';
+import { Test } from 'nodeunit';
+import * as sources from '../lib';
+import { TestFunction } from './test-function';
+
+/* eslint-disable quote-props */
+
+export = {
+  'sufficiently complex example'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const fn = new TestFunction(stack, 'Fn');
+    const clusterArn = 'some-arn';
+    const kafkaTopic = 'some-topic';
+    const secret = new Secret(stack, 'Secret', { secretName: 'AmazonMSK_KafkaSecret' });
+
+    // WHEN
+    fn.addEventSource(new sources.ManagedKafkaEventSource(
+      clusterArn,
+      kafkaTopic,
+      secret,
+      {
+        startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+      }));
+
+    // THEN
+    // expect(stack).to(haveResource('AWS::IAM::Policy', {
+    //   'PolicyDocument': {
+    //     'Statement': [
+    //       {
+    //         'Action': 'dynamodb:ListStreams',
+    //         'Effect': 'Allow',
+    //         'Resource': '*',
+    //       },
+    //       {
+    //         'Action': [
+    //           'dynamodb:DescribeStream',
+    //           'dynamodb:GetRecords',
+    //           'dynamodb:GetShardIterator',
+    //         ],
+    //         'Effect': 'Allow',
+    //         'Resource': {
+    //           'Fn::GetAtt': [
+    //             'TD925BC7E',
+    //             'StreamArn',
+    //           ],
+    //         },
+    //       },
+    //     ],
+    //     'Version': '2012-10-17',
+    //   },
+    //   'PolicyName': 'FnServiceRoleDefaultPolicyC6A839BF',
+    //   'Roles': [{
+    //     'Ref': 'FnServiceRoleB9001A96',
+    //   }],
+    // }));
+
+    expect(stack).to(haveResource('AWS::Lambda::EventSourceMapping', {
+      'EventSourceArn': clusterArn,
+      'FunctionName': {
+        'Ref': 'Fn9270CBC0',
+      },
+      'BatchSize': 100,
+      'StartingPosition': 'TRIM_HORIZON',
+      'Topics': [
+        kafkaTopic,
+      ],
+      'SourceAccessConfigurations': [
+        {
+          'Type': 'BASIC_AUTH',
+          'URI': {
+            'Ref': 'SecretA720EF05',
+          },
+        },
+      ],
+    }));
+
+    test.done();
+  },
+
+}
