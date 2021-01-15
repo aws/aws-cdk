38578de90ececab5a32ef5d3b0f5fe5ea11150f5
Marilynn Zayan <mzzayan@amazon.com>
2021-01-15 14:20:25 -0500

    update test rule for event bus policy

--- packages/@aws-cdk/aws-events/test/test.rule.ts
+++ packages/@aws-cdk/aws-events/test/test.rule.ts
@@ -713,7 +713,7 @@ export = {
       const eventBusPolicyStack = app.node.findChild(`EventBusPolicy-${sourceAccount}-us-west-2-${targetAccount}`) as cdk.Stack;
       expect(eventBusPolicyStack).to(haveResourceLike('AWS::Events::EventBusPolicy', {
         'Action': 'events:PutEvents',
-        'StatementId': `Allow-account-${sourceAccount}`,
+        'StatementId': `Allow-account-${sourceAccount}-${cdk.Names.uniqueId(app)}`,
         'Principal': sourceAccount,
       }));
 
