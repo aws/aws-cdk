468a2f5211cfe201063cbcac7835b7dec86c048e
Elad Ben-Israel <benisrae@amazon.com>
2021-01-17 10:51:28 +0200

    remove old imports

--- packages/@aws-cdk/aws-s3/lib/notifications-resource/notifications-resource-handler.ts
+++ packages/@aws-cdk/aws-s3/lib/notifications-resource/notifications-resource-handler.ts
@@ -2,10 +2,6 @@ import * as iam from '@aws-cdk/aws-iam';
 import * as cdk from '@aws-cdk/core';
 import { Construct } from 'constructs';
 
-// keep this import separate from other imports to reduce chance for merge conflicts with v2-main
-// eslint-disable-next-line no-duplicate-imports, import/order
-import { Construct } from '@aws-cdk/core';
-
 /**
  * A Lambda-based custom resource handler that provisions S3 bucket
  * notifications for a bucket.
