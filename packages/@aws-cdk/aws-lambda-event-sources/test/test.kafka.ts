f94133c7a162b3820fe937cddd5651d33edc96d4
Jan Brauer <jan.brauer@superluminar.io>
2021-01-18 14:25:46 +0100

    Remove obsolete eslint rule - and fix it

--- packages/@aws-cdk/aws-lambda-event-sources/test/test.kafka.ts
+++ packages/@aws-cdk/aws-lambda-event-sources/test/test.kafka.ts
@@ -26,53 +26,53 @@ export = {
 
     // THEN
     expect(stack).to(haveResource('AWS::IAM::Policy', {
-      'PolicyDocument': {
-        'Statement': [
+      PolicyDocument: {
+        Statement: [
           {
-            'Action': [
+            Action: [
               'secretsmanager:GetSecretValue',
               'secretsmanager:DescribeSecret',
             ],
-            'Effect': 'Allow',
-            'Resource': {
-              'Ref': 'SecretA720EF05',
+            Effect: 'Allow',
+            Resource: {
+              Ref: 'SecretA720EF05',
             },
           },
           {
-            'Action': [
+            Action: [
               'kafka:DescribeCluster',
               'kafka:GetBootstrapBrokers',
               'kafka:ListScramSecrets',
             ],
-            'Effect': 'Allow',
-            'Resource': 'some-arn',
+            Effect: 'Allow',
+            Resource: 'some-arn',
           },
         ],
-        'Version': '2012-10-17',
+        Version: '2012-10-17',
       },
-      'PolicyName': 'FnServiceRoleDefaultPolicyC6A839BF',
-      'Roles': [
+      PolicyName: 'FnServiceRoleDefaultPolicyC6A839BF',
+      Roles: [
         {
-          'Ref': 'FnServiceRoleB9001A96',
+          Ref: 'FnServiceRoleB9001A96',
         },
       ],
     }));
 
     expect(stack).to(haveResource('AWS::Lambda::EventSourceMapping', {
-      'EventSourceArn': clusterArn,
-      'FunctionName': {
-        'Ref': 'Fn9270CBC0',
+      EventSourceArn: clusterArn,
+      FunctionName: {
+        Ref: 'Fn9270CBC0',
       },
-      'BatchSize': 100,
-      'StartingPosition': 'TRIM_HORIZON',
-      'Topics': [
+      BatchSize: 100,
+      StartingPosition: 'TRIM_HORIZON',
+      Topics: [
         kafkaTopic,
       ],
-      'SourceAccessConfigurations': [
+      SourceAccessConfigurations: [
         {
-          'Type': 'BASIC_AUTH',
-          'URI': {
-            'Ref': 'SecretA720EF05',
+          Type: 'BASIC_AUTH',
+          URI: {
+            Ref: 'SecretA720EF05',
           },
         },
       ],
@@ -100,50 +100,50 @@ export = {
 
     // THEN
     expect(stack).to(haveResource('AWS::IAM::Policy', {
-      'PolicyDocument': {
-        'Statement': [
+      PolicyDocument: {
+        Statement: [
           {
-            'Action': [
+            Action: [
               'secretsmanager:GetSecretValue',
               'secretsmanager:DescribeSecret',
             ],
-            'Effect': 'Allow',
-            'Resource': {
-              'Ref': 'SecretA720EF05',
+            Effect: 'Allow',
+            Resource: {
+              Ref: 'SecretA720EF05',
             },
           },
         ],
-        'Version': '2012-10-17',
+        Version: '2012-10-17',
       },
-      'PolicyName': 'FnServiceRoleDefaultPolicyC6A839BF',
-      'Roles': [
+      PolicyName: 'FnServiceRoleDefaultPolicyC6A839BF',
+      Roles: [
         {
-          'Ref': 'FnServiceRoleB9001A96',
+          Ref: 'FnServiceRoleB9001A96',
         },
       ],
     }));
 
     expect(stack).to(haveResource('AWS::Lambda::EventSourceMapping', {
-      'FunctionName': {
-        'Ref': 'Fn9270CBC0',
+      FunctionName: {
+        Ref: 'Fn9270CBC0',
       },
-      'BatchSize': 100,
-      'SelfManagedEventSource': {
-        'Endpoints': {
-          'KafkaBootstrapServers': [
+      BatchSize: 100,
+      SelfManagedEventSource: {
+        Endpoints: {
+          KafkaBootstrapServers: [
             'kafka-broker:9092',
           ],
         },
       },
-      'StartingPosition': 'TRIM_HORIZON',
-      'Topics': [
+      StartingPosition: 'TRIM_HORIZON',
+      Topics: [
         kafkaTopic,
       ],
-      'SourceAccessConfigurations': [
+      SourceAccessConfigurations: [
         {
-          'Type': 'BASIC_AUTH',
-          'URI': {
-            'Ref': 'SecretA720EF05',
+          Type: 'BASIC_AUTH',
+          URI: {
+            Ref: 'SecretA720EF05',
           },
         },
       ],
