dc5a34627f3ba3280e61978b6032c6a41064d1dc
Curtis Eppel <curtis.eppel@cultureamp.com>
2021-01-19 19:52:42 +1100

    Add integration test for MSK cluster

---
+++ packages/@aws-cdk/aws-msk/test/integ.cluster.ts
@@ -0,0 +1,20 @@
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as msk from '../lib';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'aws-cdk-msk-integ');
+
+const vpc = new ec2.Vpc(stack, 'VPC', { maxAzs: 2 });
+
+const cluster = new msk.Cluster(stack, 'Cluster', {
+  clusterName: 'integ-test',
+  brokerNodeGroupProps: { vpc },
+});
+
+cluster.connections.allowToAnyIpv4(
+  ec2.Port.tcp(9094),
+  'Brokers open to the world'
+);
+
+app.synth();
