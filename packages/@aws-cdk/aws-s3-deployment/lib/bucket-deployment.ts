ed3e6abdbfb9e9a9cf2bbc9e401b8e1bba872acd
Bradley Walters <oss@walters.app>
2021-01-21 14:26:36 -0700

    fix(s3-deployment): User metadata keys have redundant triple `x-amz` prefix (#12414)
    Without this fix, keys are prefixed with x-amzn-meta- by the TypeScript code,
    then the Python code runs and prefixes metadata keys again with x-amz-meta-.
    Then, the Python shells out to aws-cli which makes a request to the S3 service.
    There, the keys are prefixed *yet again* with x-amz-meta- _unconditionally_.
    
    Thus no matter what keys the user specified, the keys in S3 would be:
    x-amz-meta-x-amz-meta-x-amzn-meta-<user input>.
    
    This issue was originally reported as #8459.
    
    #10678 attempted to fix this issue, and fixed the Python, but missed the TS.
    It also suffers from the S3 service adding the prefix unconditionally.
    
    After this change, neither the TypeScript code nor the Python code will attempt
    to prepend to the metadata key. Instead we rely on the S3 service to do it.
    
    BREAKING CHANGE: User metadata keys of bucket objects will change from `x-amz-meta-x-amz-meta-x-amzn-meta-mykey` to `x-amz-meta-mykey`.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-s3-deployment/lib/bucket-deployment.ts
+++ packages/@aws-cdk/aws-s3-deployment/lib/bucket-deployment.ts
@@ -257,10 +257,7 @@ export class BucketDeployment extends CoreConstruct {
  */
 
 function mapUserMetadata(metadata: UserDefinedObjectMetadata) {
-  const mapKey = (key: string) =>
-    key.toLowerCase().startsWith('x-amzn-meta-')
-      ? key.toLowerCase()
-      : `x-amzn-meta-${key.toLowerCase()}`;
+  const mapKey = (key: string) => key.toLowerCase();
 
   return Object.keys(metadata).reduce((o, key) => ({ ...o, [mapKey(key)]: metadata[key] }), {});
 }
@@ -358,7 +355,7 @@ export class Expires {
 export interface UserDefinedObjectMetadata {
   /**
    * Arbitrary metadata key-values
-   * Keys must begin with `x-amzn-meta-` (will be added automatically if not provided)
+   * The `x-amz-meta-` prefix will automatically be added to keys.
    * @see https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingMetadata.html#UserMetadata
    */
   readonly [key: string]: string;
