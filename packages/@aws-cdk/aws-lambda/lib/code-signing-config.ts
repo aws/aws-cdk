b2b326331f4a6c51a1d98560885354325dee73f1
小室　雅春 <>
2021-01-28 07:53:05 +0900

    modify lambda code signing config

--- packages/@aws-cdk/aws-lambda/lib/code-signing-config.ts
+++ packages/@aws-cdk/aws-lambda/lib/code-signing-config.ts
@@ -1,37 +1,54 @@
-import { Resource } from '@aws-cdk/core';
+import { IResource, Resource } from '@aws-cdk/core';
 import { Construct } from 'constructs';
 import { CfnCodeSigningConfig } from './lambda.generated';
+import { SigningProfile } from '@aws-cdk/aws-signer';
 
 export enum UntrustedArtifactOnDeployment {
   ENFORCE = 'enforce',
   WARN = 'warn',
 }
 
-export interface CodeSigningConfigOptions {
-  signingProfileVersionArn: string[],
+export interface ICodeSigningConfig extends IResource {
+  /**
+   * The ARN of Code Signing Config
+   * @Attribute CodeSigningConfigArn
+   */
+  readonly codeSigningConfigArn: string;
+
+  /**
+   * The id of Code Signing Config
+   * @Attribute CodeSigningConfigId
+   */
+  readonly codeSigningConfigId: string;
+}
+
+export interface CodeSigningConfigProps {
+  signingProfile: SigningProfile,
   untrustedArtifactOnDeployment?: UntrustedArtifactOnDeployment,
   description?: string
 }
 
-export class CodeSigningConfig extends Resource {
-  public readonly codeSigningConfigArn: string;
+export class CodeSigningConfig extends Resource implements ICodeSigningConfig{
+  readonly codeSigningConfigArn: string;
+  readonly codeSigningConfigId: string;
 
-  constructor(scope: Construct, id: string, props: CodeSigningConfigOptions) {
+  constructor(scope: Construct, id: string, props: CodeSigningConfigProps) {
     super(scope, id);
 
-    if (props.signingProfileVersionArn.length > 20) {
+    if (props.signingProfile.length > 20) {
       throw new Error('Signing profile version arn is up to 20');
     }
 
     const resource: CfnCodeSigningConfig = new CfnCodeSigningConfig(this, 'Resource', {
       allowedPublishers: {
-        signingProfileVersionArns: props.signingProfileVersionArn,
+        signingProfileVersionArns: props.signingProfile.signingProfileVersionArn,
       },
       codeSigningPolicies: {
         untrustedArtifactOnDeployment: props.untrustedArtifactOnDeployment
       },
       description: props.description
     });
-    this.codeSigningConfigArn = resource.ref;
+    this.codeSigningConfigArn = resource.attrCodeSigningConfigArn;
+    this.codeSigningConfigId = resource.attrCodeSigningConfigId;
   }
 }
