2b6d2b61b8a31af26b37eef7884f94a677defd0e
Matthew Bonig <matthew.bonig@gmail.com>
2021-01-28 07:36:56 -0700

    so many commas

--- packages/@aws-cdk/aws-codedeploy/test/server/test.deployment-group.ts
+++ packages/@aws-cdk/aws-codedeploy/test/server/test.deployment-group.ts
@@ -64,17 +64,17 @@ export = {
               [
                 '#!/bin/bash\nPKG_CMD=`which yum 2>/dev/null`\nif [ -z "$PKG_CMD" ]; then\nPKG_CMD=apt-get\nelse\nPKG=CMD=yum\nfi\n$PKG_CMD update -y\n$PKG_CMD install -y ruby2.0\nif [ $? -ne 0 ]; then\n$PKG_CMD install -y ruby\nfi\n$PKG_CMD install -y awscli\nTMP_DIR=`mktemp -d`\ncd $TMP_DIR\naws s3 cp s3://aws-codedeploy-',
                 {
-                  'Ref': 'AWS::Region'
+                  'Ref': 'AWS::Region',
                 },
                 '/latest/install . --region ',
                 {
-                  'Ref': 'AWS::Region'
+                  'Ref': 'AWS::Region',
                 },
                 '\nchmod +x ./install\n./install auto\nrm -fr $TMP_DIR',
-              ]
-            ]
-          }
-        }
+              ],
+            ],
+          },
+        },
       }));
 
       test.done();
@@ -102,13 +102,13 @@ export = {
               [
                 '<powershell>Set-Variable -Name TEMPDIR -Value (New-TemporaryFile).DirectoryName\naws s3 cp s3://aws-codedeploy-',
                 {
-                  'Ref': 'AWS::Region'
+                  'Ref': 'AWS::Region',
                 },
                 '/latest/codedeploy-agent.msi $TEMPDIR\\codedeploy-agent.msi\ncd $TEMPDIR\n.\\codedeploy-agent.msi /quiet /l c:\\temp\\host-agent-install-log.txt</powershell>',
-              ]
-            ]
-          }
-        }
+              ],
+            ],
+          },
+        },
       }));
 
       test.done();
