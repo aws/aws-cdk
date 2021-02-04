f10b46ff30dcfaae84cd613f818e5f1d52da79ad
Masaharu Komuro <s1c3s4a4z3l6n6l1@hedrall.slack.com>
2021-02-05 06:29:12 +0900

    make signingProfiles to list
    Co-authored-by: Niranjan Jayakar <nija@amazon.com>

--- packages/@aws-cdk/aws-lambda/lib/code-signing-config.ts
+++ packages/@aws-cdk/aws-lambda/lib/code-signing-config.ts
@@ -23,7 +23,7 @@ export interface ICodeSigningConfig extends IResource {
 }
 
 export interface CodeSigningConfigProps {
-  signingProfile: SigningProfile,
+  signingProfiles: ISigningProfile[],
   untrustedArtifactOnDeployment?: UntrustedArtifactOnDeployment,
   description?: string
 }
