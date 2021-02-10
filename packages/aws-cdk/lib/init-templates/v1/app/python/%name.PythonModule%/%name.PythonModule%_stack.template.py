d644e0b00d9af9ce50b89f9b1f1eeab30b9f3e3c
Jerry Kindall <52084730+Jerry-AWS@users.noreply.github.com>
2021-02-10 08:42:39 -0800

    use cdk to reference objects from core module

--- packages/aws-cdk/lib/init-templates/v1/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
+++ packages/aws-cdk/lib/init-templates/v1/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
@@ -1,8 +1,8 @@
 from aws_cdk import core, core as cdk
 
-class %name.PascalCased%Stack(core.Stack):
+class %name.PascalCased%Stack(cdk.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
