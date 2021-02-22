195b4bce31921410432d5531eacd0c607ac8aee9
Jerry Kindall <jkindall@amazon.com>
2021-02-22 15:05:24 -0800

    Add comments explaining secondary import

--- packages/aws-cdk/lib/init-templates/v1/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
+++ packages/aws-cdk/lib/init-templates/v1/app/python/%name.PythonModule%/%name.PythonModule%_stack.template.py
@@ -1,4 +1,11 @@
-from aws_cdk import core, core as cdk
+from aws_cdk import core as cdk
+
+# For consistency with other languages, `cdk` is the preferred import name for
+# the CDK's core module.  The following line also imports it as `core` for use
+# with examples from the CDK Developer's Guide, which are in the process of
+# being updated to use `cdk`.  You may delete this import if you don't need it.
+from aws_cdk import core
+
 
 class %name.PascalCased%Stack(cdk.Stack):
 
--- packages/aws-cdk/lib/init-templates/v1/app/python/app.template.py
+++ packages/aws-cdk/lib/init-templates/v1/app/python/app.template.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
-from aws_cdk import core, core as cdk
+from aws_cdk import core as cdk
+
+# For consistency with TypeScript code, `cdk` is the preferred import name for
+# the CDK's core module.  The following line also imports it as `core` for use
+# with examples from the CDK Developer's Guide, which are in the process of
+# being updated to use `cdk`.  You may delete this import if you don't need it.
+from aws_cdk import core
 
 from %name.PythonModule%.%name.PythonModule%_stack import %name.PascalCased%Stack
 
