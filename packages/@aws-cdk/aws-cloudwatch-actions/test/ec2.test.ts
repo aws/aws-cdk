ac3ca30dcda608a11cdd4c695b0573281d7b0831
Carlos Yuste <carlos.yuste@col.vueling.com>
2021-02-25 10:39:13 +0100

    feat(cw-actions) Add ec2 actions

---
+++ packages/@aws-cdk/aws-cloudwatch-actions/test/ec2.test.ts
@@ -0,0 +1,36 @@
+import '@aws-cdk/assert/jest';
+import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
+import { Stack } from '@aws-cdk/core';
+import * as actions from '../lib';
+
+test('can use instance reboot as alarm action', () => {
+  // GIVEN
+  const stack = new Stack();
+  //const topic = new sns.Topic(stack, 'Topic');
+  const alarm = new cloudwatch.Alarm(stack, 'Alarm', {
+    metric: new cloudwatch.Metric({ namespace: 'AWS', metricName: 'Henk' }),
+    evaluationPeriods: 3,
+    threshold: 100,
+  });
+
+  // WHEN
+  alarm.addAlarmAction(new actions.Ec2Action(actions.Ec2InstanceActions.REBOOT));
+
+  // THEN
+  expect(stack).toHaveResource('AWS::CloudWatch::Alarm', {
+    AlarmActions: [
+      {
+        'Fn::Join': [
+          '',
+          [
+            'arn:aws:automate:',
+            {
+              Ref: 'AWS::Region',
+            },
+            ':ec2:reboot',
+          ],
+        ],
+      },
+    ],
+  });
+});
