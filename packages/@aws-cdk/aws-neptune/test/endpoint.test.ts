c36683701d88eb0c53fdd2add66b10c47c05f56b
Sainath Mallidi <sainathdreams@gmail.com>
2021-03-01 07:32:26 -0800

    feat(neptune): high level constructs for db clusters and instances (#12763)
    - This change adds higher level constructs for Neptune clusters
    - Adds higher-level constructs for
      - AWS::Neptune::DBCluster
      - AWS::Neptune::DBInstance
      - AWS::Neptune::DBClusterParameterGroup
      - AWS::Neptune::DBParameterGroup
      - AWS::Neptune::DBSubnetGroup
    
    fixes aws#12762
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-neptune/test/endpoint.test.ts
@@ -0,0 +1,15 @@
+import { Endpoint } from '../lib';
+
+describe('Endpoint', () => {
+  test('accepts valid port string numbers', () => {
+    // GIVEN
+    for (const port of [1, 50, 65535]) {
+      // WHEN
+      const endpoint = new Endpoint('127.0.0.1', port);
+
+      // THEN
+      expect(endpoint.port).toBe(port);
+    }
+  });
+
+});
