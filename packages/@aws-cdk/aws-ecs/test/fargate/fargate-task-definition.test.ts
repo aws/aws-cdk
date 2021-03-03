2665b2c31ff204d78c5ea3fa88f45b66e81b7ce8
rli1994hi <rli1994hi@gmail.com>
2021-03-03 09:42:58 -0800

    fix(events): imported ECS Task Definition cannot be used as target (#13293)
    ### Note
    
    Fixes #12811
    
    This change allows creating `EcsTask` from an imported task definition. It
    does so by changing the `taskDefinition` type in `EcsTaskProps` from concrete class
    `TaskDefinition` to interface `ITaskDefinition`.
    
    ### Testing
    `buildup` aws-events-targets
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-ecs/test/fargate/fargate-task-definition.test.ts
+++ packages/@aws-cdk/aws-ecs/test/fargate/fargate-task-definition.test.ts
@@ -5,7 +5,7 @@ import { nodeunitShim, Test } from 'nodeunit-shim';
 import * as ecs from '../../lib';
 
 nodeunitShim({
-  'When creating an Fargate TaskDefinition': {
+  'When creating a Fargate TaskDefinition': {
     'with only required properties set, it correctly sets default properties'(test: Test) {
       // GIVEN
       const stack = new cdk.Stack();
@@ -110,6 +110,92 @@ nodeunitShim({
       test.throws(() => {
         taskDefinition.addPlacementConstraint(ecs.PlacementConstraint.memberOf('attribute:ecs.instance-type =~ t2.*'));
       }, /Cannot set placement constraints on tasks that run on Fargate/);
+
+      test.done();
+    },
+  },
+
+  'When importing from an existing Fargate TaskDefinition': {
+    'can succeed using TaskDefinition Arn'(test: Test) {
+      // GIVEN
+      const stack = new cdk.Stack();
+      const expectTaskDefinitionArn = 'TD_ARN';
+
+      // WHEN
+      const taskDefinition = ecs.FargateTaskDefinition.fromFargateTaskDefinitionArn(stack, 'FARGATE_TD_ID', expectTaskDefinitionArn);
+
+      // THEN
+      test.equal(taskDefinition.taskDefinitionArn, expectTaskDefinitionArn);
+      test.done();
+    },
+
+    'can succeed using attributes'(test: Test) {
+      // GIVEN
+      const stack = new cdk.Stack();
+      const expectTaskDefinitionArn = 'TD_ARN';
+      const expectNetworkMode = ecs.NetworkMode.AWS_VPC;
+      const expectTaskRole = new iam.Role(stack, 'TaskRole', {
+        assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      });
+
+      // WHEN
+      const taskDefinition = ecs.FargateTaskDefinition.fromFargateTaskDefinitionAttributes(stack, 'TD_ID', {
+        taskDefinitionArn: expectTaskDefinitionArn,
+        networkMode: expectNetworkMode,
+        taskRole: expectTaskRole,
+      });
+
+      // THEN
+      test.equal(taskDefinition.taskDefinitionArn, expectTaskDefinitionArn);
+      test.equal(taskDefinition.compatibility, ecs.Compatibility.FARGATE);
+      test.ok(taskDefinition.isFargateCompatible);
+      test.equal(taskDefinition.isEc2Compatible, false);
+      test.equal(taskDefinition.networkMode, expectNetworkMode);
+      test.equal(taskDefinition.taskRole, expectTaskRole);
+
+      test.done();
+    },
+
+    'returns a Fargate TaskDefinition that will throw an error when trying to access its networkMode but its networkMode is undefined'(test: Test) {
+      // GIVEN
+      const stack = new cdk.Stack();
+      const expectTaskDefinitionArn = 'TD_ARN';
+      const expectTaskRole = new iam.Role(stack, 'TaskRole', {
+        assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      });
+
+      // WHEN
+      const taskDefinition = ecs.FargateTaskDefinition.fromFargateTaskDefinitionAttributes(stack, 'TD_ID', {
+        taskDefinitionArn: expectTaskDefinitionArn,
+        taskRole: expectTaskRole,
+      });
+
+      // THEN
+      test.throws(() => {
+        taskDefinition.networkMode;
+      }, 'This operation requires the networkMode in ImportedTaskDefinition to be defined. ' +
+        'Add the \'networkMode\' in ImportedTaskDefinitionProps to instantiate ImportedTaskDefinition');
+
+      test.done();
+    },
+
+    'returns a Fargate TaskDefinition that will throw an error when trying to access its taskRole but its taskRole is undefined'(test: Test) {
+      // GIVEN
+      const stack = new cdk.Stack();
+      const expectTaskDefinitionArn = 'TD_ARN';
+      const expectNetworkMode = ecs.NetworkMode.AWS_VPC;
+
+      // WHEN
+      const taskDefinition = ecs.FargateTaskDefinition.fromFargateTaskDefinitionAttributes(stack, 'TD_ID', {
+        taskDefinitionArn: expectTaskDefinitionArn,
+        networkMode: expectNetworkMode,
+      });
+
+      // THEN
+      test.throws(() => {
+        taskDefinition.taskRole;
+      }, 'This operation requires the taskRole in ImportedTaskDefinition to be defined. ' +
+        'Add the \'taskRole\' in ImportedTaskDefinitionProps to instantiate ImportedTaskDefinition');
 
       test.done();
     },
