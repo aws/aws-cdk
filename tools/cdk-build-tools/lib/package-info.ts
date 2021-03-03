a2f8060bbff4da8f1fbd9e9449a869ac67ac5e5e
Niranjan Jayakar <nija@amazon.com>
2021-03-03 15:15:45 +0000

    build to run strip-deprecated

--- tools/cdk-build-tools/lib/package-info.ts
+++ tools/cdk-build-tools/lib/package-info.ts
@@ -83,7 +83,7 @@ export interface CompilerOverrides {
  */
 export function packageCompiler(compilers: CompilerOverrides): string[] {
   if (isJsii()) {
-    return [compilers.jsii || require.resolve('jsii/bin/jsii'), '--silence-warnings=reserved-word'];
+    return [compilers.jsii || require.resolve('jsii/bin/jsii'), '--silence-warnings=reserved-word', '--strip-deprecated'];
   } else {
     return [compilers.tsc || require.resolve('typescript/bin/tsc'), '--build'];
   }
