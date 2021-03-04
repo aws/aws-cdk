8d121d47bf496643b9caf6fb2d2c99b822abb3cd
Elad Ben-Israel <benisrae@amazon.com>
2021-03-04 11:47:02 +0200

    chore: build go bindings for monocdk
    Add `go` configuration to the `monocdk` package.
    
    To be able to generate go bindings a few tweaks to the current codebase were needed:
    
    BREAKING CHANGE: The deprecated `Duration.toISOString()` method is no longer available. Please use `Duration.toIsoString()` instead. Unfortunteally this API needs to be broken to disambiguate the two methods in the Go generated code.

--- packages/@aws-cdk/core/lib/cfn-json.ts
+++ packages/@aws-cdk/core/lib/cfn-json.ts
@@ -74,7 +74,7 @@ export class CfnJson extends CoreConstruct implements IResolvable {
     return this.jsonString;
   }
 
-  public resolve(_: IResolveContext): any {
+  public resolve(_context: IResolveContext): any {
     return this.value;
   }
 }
