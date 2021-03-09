cec0e2ecb433850f745f6c2f39d2b08e90a4c95f
Niranjan Jayakar <nija@amazon.com>
2021-03-09 11:09:23 +0000

    eks

--- packages/@aws-cdk/aws-eks/lib/cluster.ts
+++ packages/@aws-cdk/aws-eks/lib/cluster.ts
@@ -1153,9 +1153,9 @@ export class Cluster extends ClusterBase {
    * The nodes will automatically be configured with the right VPC and AMI
    * for the instance type and Kubernetes version.
    *
-   * Note that if you specify `updateType: RollingUpdate` or `updateType: ReplacingUpdate`, your nodes might be replaced at deploy
+   * Note that if you specify `updatePolicy: UpdatePolicy.rollingUpdate()` or `updateType: UpdatePolicy.replacingUpdate`, your nodes might be replaced at deploy
    * time without notice in case the recommended AMI for your machine image type has been updated by AWS.
-   * The default behavior for `updateType` is `None`, which means only new instances will be launched using the new AMI.
+   * The default behavior for `updatePolicy` is `None`, which means only new instances will be launched using the new AMI.
    *
    * Spot instances will be labeled `lifecycle=Ec2Spot` and tainted with `PreferNoSchedule`.
    * In addition, the [spot interrupt handler](https://github.com/awslabs/ec2-spot-labs/tree/master/ec2-spot-eks-solution/spot-termination-handler)
@@ -1178,7 +1178,7 @@ export class Cluster extends ClusterBase {
           cpuArch: cpuArchForInstanceType(options.instanceType),
           kubernetesVersion: this.version.version,
         }),
-      updateType: options.updateType,
+      updatePolicy: options.updatePolicy,
       instanceType: options.instanceType,
     });
 
