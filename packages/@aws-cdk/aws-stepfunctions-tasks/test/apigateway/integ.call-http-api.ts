ce3c1fa8d4cfe693150b7cfab948bd88404f370b
Ayush Goyal <goyalayu@amazon.com>
2021-03-11 00:08:21 +0530

    feat(stepfunctions-tasks): Support calling ApiGateway REST and HTTP APIs (#13033)
    feat(stepfunctions-tasks): Support calling APIGW REST and HTTP APIs
    
    Taking ownership of the original PR #11565 by @Sumeet-Badyal 
    
    API as per documentation here:
    https://docs.aws.amazon.com/step-functions/latest/dg/connect-api-gateway.html
    https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
    
    
    closes #11566
    closes #11565
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-stepfunctions-tasks/test/apigateway/integ.call-http-api.ts
@@ -0,0 +1,48 @@
+import * as apigatewayv2 from '@aws-cdk/aws-apigatewayv2';
+import * as integrations from '@aws-cdk/aws-apigatewayv2-integrations';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as sfn from '@aws-cdk/aws-stepfunctions';
+import * as cdk from '@aws-cdk/core';
+import { AuthType, HttpMethod, CallApiGatewayHttpApiEndpoint } from '../../lib';
+
+/*
+ * Stack verification steps:
+ * * aws stepfunctions start-execution --state-machine-arn <deployed state machine arn> : should return execution arn
+ *
+ * * aws stepfunctions describe-execution --execution-arn <execution-arn generated before> --query 'status': should return status as SUCCEEDED
+ * * aws stepfunctions describe-execution --execution-arn <execution-arn generated before> --query 'output': should return the string \"hello, world!\"
+ */
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'CallHttpApiInteg');
+const httpApi = new apigatewayv2.HttpApi(stack, 'MyHttpApi');
+
+const handler = new lambda.Function(stack, 'HelloHandler', {
+  runtime: lambda.Runtime.NODEJS_10_X,
+  handler: 'index.handler',
+  code: new lambda.InlineCode('exports.handler = async function(event, context) { return { statusCode: 200, body: "hello, world!" }; };'),
+});
+httpApi.addRoutes({
+  path: '/',
+  integration: new integrations.LambdaProxyIntegration({
+    handler,
+  }),
+});
+
+const callEndpointJob = new CallApiGatewayHttpApiEndpoint(stack, 'Call APIGW', {
+  api: httpApi,
+  method: HttpMethod.GET,
+  authType: AuthType.IAM_ROLE,
+  outputPath: sfn.JsonPath.stringAt('$.ResponseBody'),
+});
+
+const chain = sfn.Chain.start(callEndpointJob);
+
+const sm = new sfn.StateMachine(stack, 'StateMachine', {
+  definition: chain,
+  timeout: cdk.Duration.seconds(30),
+});
+
+new cdk.CfnOutput(stack, 'stateMachineArn', {
+  value: sm.stateMachineArn,
+});
