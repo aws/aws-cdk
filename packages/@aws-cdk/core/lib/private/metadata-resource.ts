82c2340b629d9a1da59a9d3ba03f0d8ff79dd0bc
Nick Lynch <nlynch@amazon.com>
2021-03-10 11:51:11 +0000

    chore(core): update CDK Metadata to report construct-level details (#13423)
    See CDK RFC 253 (aws/aws-cdk-rfcs#254) for background and details.
    
    Currently -- if a user has not opted out -- an AWS::CDK::Metadata resource
    is added to each generated stack template with details about each loaded module
    and version that matches an Amazon-specific allow list.
    
    This modules list is used to:
    
    - Track what library versions customers are using so they can be contacted in
      the event of a severe (security) issue with a library.
    - Get business metrics on the adoption of CDK and its libraries.
    
    This modules list is sometimes inaccurate (a module may be loaded into memory
    without actually being used) and too braod to support CDK v2.
    
    This feature (mostly) implements the specification proposed in RFC 253 to
    include metadata about what constructs are present in each stack, rather than
    modules loaded into memory. The allow-list is still used to ensure only CDK/AWS
    constructs are reported on.
    
    Implementation notes:
    - The format of the Analytics property has changed slightly since the RFC. See
      the service-side code for justification and latest spec.
    - How to handle the jsii runtime information was left un-spec'd. I've chosen to
      create a psuedo-Construct to add to the list as the simplest solution.
    - `runtime-info.test.ts` leaps through some serious hoops to work equally well
      for both v1 and v2, and to fail somewhat gracefully locally if `tsc` was used
      to compile the module instead of `jsii`. Critques of this approach welcome!
    - I removed an annoyance from `resolve-version-lib.js` that produced error
      messages when running unit tests.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/core/lib/private/metadata-resource.ts
+++ packages/@aws-cdk/core/lib/private/metadata-resource.ts
@@ -1,4 +1,4 @@
-import * as cxapi from '@aws-cdk/cx-api';
+import * as zlib from 'zlib';
 import { RegionInfo } from '@aws-cdk/region-info';
 import { CfnCondition } from '../cfn-condition';
 import { Fn } from '../cfn-fn';
@@ -8,41 +8,12 @@ import { Construct } from '../construct-compat';
 import { Lazy } from '../lazy';
 import { Stack } from '../stack';
 import { Token } from '../token';
-import { collectRuntimeInformation } from './runtime-info';
+import { ConstructInfo, constructInfoFromStack } from './runtime-info';
 
 /**
  * Construct that will render the metadata resource
  */
 export class MetadataResource extends Construct {
-  /**
-   * Clear the modules cache
-   *
-   * The next time the MetadataResource is rendered, it will do a lookup of the
-   * modules from the NodeJS module cache again.
-   *
-   * Used only for unit tests.
-   */
-  public static clearModulesCache() {
-    this._modulesPropertyCache = undefined;
-  }
-
-  /**
-   * Cached version of the _modulesProperty() accessor
-   *
-   * No point in calculating this fairly expensive list more than once.
-   */
-  private static _modulesPropertyCache?: string;
-
-  /**
-   * Calculate the modules property
-   */
-  private static modulesProperty(): string {
-    if (this._modulesPropertyCache === undefined) {
-      this._modulesPropertyCache = formatModules(collectRuntimeInformation());
-    }
-    return this._modulesPropertyCache;
-  }
-
   constructor(scope: Stack, id: string) {
     super(scope, id);
 
@@ -51,7 +22,7 @@ export class MetadataResource extends Construct {
       const resource = new CfnResource(this, 'Default', {
         type: 'AWS::CDK::Metadata',
         properties: {
-          Modules: Lazy.string({ produce: () => MetadataResource.modulesProperty() }),
+          Analytics: Lazy.string({ produce: () => formatAnalytics(constructInfoFromStack(scope)) }),
         },
       });
 
@@ -76,17 +47,81 @@ function makeCdkMetadataAvailableCondition() {
     .map(ri => Fn.conditionEquals(Aws.REGION, ri.name)));
 }
 
-function formatModules(runtime: cxapi.RuntimeInfo): string {
-  const modules = new Array<string>();
+/** Convenience type for arbitrarily-nested map */
+class Trie extends Map<string, Trie> { }
 
-  // inject toolkit version to list of modules
-  const cliVersion = process.env[cxapi.CLI_VERSION_ENV];
-  if (cliVersion) {
-    modules.push(`aws-cdk=${cliVersion}`);
-  }
+/**
+ * Formats a list of construct fully-qualified names (FQNs) and versions into a (possibly compressed) prefix-encoded string.
+ *
+ * The list of ConstructInfos is logically formatted into:
+ * ${version}!${fqn} (e.g., "1.90.0!aws-cdk-lib.Stack")
+ * and then all of the construct-versions are grouped with common prefixes together, grouping common parts in '{}' and separating items with ','.
+ *
+ * Example:
+ * [1.90.0!aws-cdk-lib.Stack, 1.90.0!aws-cdk-lib.Construct, 1.90.0!aws-cdk-lib.service.Resource, 0.42.1!aws-cdk-lib-experiments.NewStuff]
+ * Becomes:
+ * 1.90.0!aws-cdk-lib.{Stack,Construct,service.Resource},0.42.1!aws-cdk-lib-experiments.NewStuff
+ *
+ * The whole thing is then either included directly as plaintext as:
+ * v2:plaintext:{prefixEncodedList}
+ * Or is compressed and base64-encoded, and then formatted as:
+ * v2:deflate64:{prefixEncodedListCompressedAndEncoded}
+ *
+ * Exported/visible for ease of testing.
+ */
+export function formatAnalytics(infos: ConstructInfo[]) {
+  const trie = new Trie();
+  infos.forEach(info => insertFqnInTrie(`${info.version}!${info.fqn}`, trie));
 
-  for (const key of Object.keys(runtime.libraries).sort()) {
-    modules.push(`${key}=${runtime.libraries[key]}`);
+  const plaintextEncodedConstructs = prefixEncodeTrie(trie);
+  const compressedConstructs = zlib.gzipSync(Buffer.from(plaintextEncodedConstructs)).toString('base64');
+
+  return `v2:deflate64:${compressedConstructs}`;
+}
+
+/**
+ * Splits after non-alphanumeric characters (e.g., '.', '/') in the FQN
+ * and insert each piece of the FQN in nested map (i.e., simple trie).
+ */
+function insertFqnInTrie(fqn: string, trie: Trie) {
+  for (const fqnPart of fqn.replace(/[^a-z0-9]/gi, '$& ').split(' ')) {
+    const nextLevelTreeRef = trie.get(fqnPart) ?? new Trie();
+    trie.set(fqnPart, nextLevelTreeRef);
+    trie = nextLevelTreeRef;
   }
-  return modules.join(',');
+  return trie;
+}
+
+/**
+ * Prefix-encodes a "trie-ish" structure, using '{}' to group and ',' to separate siblings.
+ *
+ * Example input:
+ * ABC,ABD,AEF
+ *
+ * Example trie:
+ * A --> B --> C
+ *  |     \--> D
+ *  \--> E --> F
+ *
+ * Becomes:
+ * A{B{C,D},EF}
+ */
+function prefixEncodeTrie(trie: Trie) {
+  let prefixEncoded = '';
+  let isFirstEntryAtLevel = true;
+  [...trie.entries()].forEach(([key, value]) => {
+    if (!isFirstEntryAtLevel) {
+      prefixEncoded += ',';
+    }
+    isFirstEntryAtLevel = false;
+    prefixEncoded += key;
+    if (value.size > 1) {
+      prefixEncoded += '{';
+      prefixEncoded += prefixEncodeTrie(value);
+      prefixEncoded += '}';
+    } else {
+      prefixEncoded += prefixEncodeTrie(value);
+    }
+  });
+  return prefixEncoded;
 }
