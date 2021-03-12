78bb27702f1b6bdebfc3e2956205aba227d17f8c
robertd <robert.djurasaj@gmail.com>
2021-03-12 00:17:09 -0700

    feat(region-info): add ap-northeast-3 region

--- packages/@aws-cdk/region-info/build-tools/aws-entities.ts
+++ packages/@aws-cdk/region-info/build-tools/aws-entities.ts
@@ -4,36 +4,62 @@
  * Not in the list ==> no built-in data for that region.
  */
 export const AWS_REGIONS = [
-  'us-east-2',
+  /** Africa (Cape Town) */
+  'af-south-1',
+  /** Asia Pacific (Hong Kong) */
+  'ap-east-1',
+  /** Asia Pacific (Tokyo) */
+  'ap-northeast-1',
+  /** Asia Pacific (Seoul) */
+  'ap-northeast-2',
+  /** Asia Pacific (Osaka) */
+  'ap-northeast-3',
+  /** Asia Pacific (Mumbai) */
+  'ap-south-1',
+  /** Asia Pacific (Singapore) */
+  'ap-southeast-1',
+  /** Asia Pacific (Sydney) */
+  'ap-southeast-2',
+  /** Canada (Central) */
+  'ca-central-1',
+  /** China (Beijing) */
+  'cn-north-1',
+  /** China (Ningxia) */
+  'cn-northwest-1',
+  /** Europe (Frankfurt) */
+  'eu-central-1',
+  /** Europe (Stockholm) */
+  'eu-north-1',
+  /** Europe (Milan) */
+  'eu-south-1',
+  /** Europe (Ireland) */
+  'eu-west-1',
+  /** Europe (London) */
+  'eu-west-2',
+  /** Europe (Paris) */
+  'eu-west-3',
+  /** Middle East (Bahrain) */
+  'me-south-1',
+  /** South America (São Paulo) */
+  'sa-east-1',
+  /** US East (N. Virginia) */
   'us-east-1',
-  'us-west-1',
-  'us-west-2',
+  /** US East (Ohio) */
+  'us-east-2',
+  /** AWS GovCloud (US-East) */
   'us-gov-east-1',
+  /** AWS GovCloud (US-West) */
   'us-gov-west-1',
   'us-iso-east-1',
   'us-isob-east-1',
-  'af-south-1',
-  'ap-east-1',
-  'ap-south-1',
-  'ap-northeast-2',
-  'ap-southeast-1',
-  'ap-southeast-2',
-  'ap-northeast-1',
-  'ca-central-1',
-  'cn-north-1',
-  'cn-northwest-1',
-  'eu-central-1',
-  'eu-west-1',
-  'eu-west-2',
-  'eu-west-3',
-  'eu-north-1',
-  'eu-south-1',
-  'me-south-1',
-  'sa-east-1',
+  /** US West (N. California) */
+  'us-west-1',
+  /** US West (Oregon) */
+  'us-west-2',
 ].sort();
 
 /**
- * Possibly non-exaustive list of all service names, used to locate service principals.
+ * Possibly non-exhaustive list of all service names, used to locate service principals.
  *
  * Not in the list ==> default service principal mappings.
  */
