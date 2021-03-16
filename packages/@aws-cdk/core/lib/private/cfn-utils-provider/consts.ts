a246d26ae3c2e842a22dbe22f17a904156534f7e
Rico Huijbers <rix0rrr@gmail.com>
2021-03-16 17:13:55 +0100

    fix(core): `toJsonString()` cannot handle list intrinsics (#13544)
    The previous attempt at a fix missed one important case: the types
    of the values involved in the `{ Fn::Join }` expression didn't actually
    match up. They all needed to be strings, but the previous implentation
    just dropped list-typed values in there.
    
    Unfortunately, there is no way to do it correctly with just string
    manipulation in CloudFormation (`{ Fn::Join }` etc), so we'll have
    to resort to using a Custom Resource if we encounter list values.
    
    Actually fixes #13465.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/core/lib/private/cfn-utils-provider/consts.ts
+++ packages/@aws-cdk/core/lib/private/cfn-utils-provider/consts.ts
@@ -5,5 +5,10 @@ export const enum CfnUtilsResourceType {
   /**
    * CfnJson
    */
-  CFN_JSON = 'Custom::AWSCDKCfnJson'
+  CFN_JSON = 'Custom::AWSCDKCfnJson',
+
+  /**
+   * CfnJsonStringify
+   */
+  CFN_JSON_STRINGIFY = 'Custom::AWSCDKCfnJsonStringify',
 }
