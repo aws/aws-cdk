c490390f9ffcd5a9c21508a472ab1c6ca60ff3c6
Mitchell Valine <mitchellvaline@yahoo.com>
2021-03-16 09:29:30 -0700

    make policy document optional

--- packages/@aws-cdk/aws-sns/lib/policy.ts
+++ packages/@aws-cdk/aws-sns/lib/policy.ts
@@ -14,8 +14,9 @@ export interface TopicPolicyProps {
   readonly topics: ITopic[];
   /**
    * IAM policy document to apply to topic(s).
+   * @default empty policy document
    */
-  readonly policyDocument: PolicyDocument;
+  readonly policyDocument?: PolicyDocument;
 
 }
 
@@ -26,15 +27,21 @@ export class TopicPolicy extends Resource {
   /**
    * The IAM policy document for this policy.
    */
-  public readonly document: PolicyDocument;
+  public readonly document = new PolicyDocument({
+    // statements must be unique, so we use the statement index.
+    // potantially SIDs can change as a result of order change, but this should
+    // not have an impact on the policy evaluation.
+    // https://docs.aws.amazon.com/sns/latest/dg/AccessPolicyLanguage_SpecialInfo.html
+    assignSids: true,
+  });
 
   constructor(scope: Construct, id: string, props: TopicPolicyProps) {
     super(scope, id);
 
-    this.document = props.policyDocument;
+    this.document = props.policyDocument ?? this.document;
 
     new CfnTopicPolicy(this, 'Resource', {
-      policyDocument: props.policyDocument,
+      policyDocument: this.document,
       topics: props.topics.map(t => t.topicArn),
     });
   }
--- packages/@aws-cdk/aws-sns/test/test.sns.ts
+++ packages/@aws-cdk/aws-sns/test/test.sns.ts
@@ -276,7 +276,7 @@ export = {
     test.done();
   },
 
-  'TopicPolicy can be created'(test: Test) {
+  'TopicPolicy passed document'(test: Test) {
     // GIVEN
     const stack = new cdk.Stack();
     const topic = new sns.Topic(stack, 'MyTopic');
@@ -318,6 +318,52 @@ export = {
       },
     });
 
+    test.done();
+  },
+
+  'Add statements to policy'(test: Test) {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const topic = new sns.Topic(stack, 'MyTopic');
+
+    // WHEN
+    const topicPolicy = new sns.TopicPolicy(stack, 'TopicPolicy', {
+      topics: [topic],
+    });
+    topicPolicy.document.addStatements(new iam.PolicyStatement({
+      actions: ['service:statement0'],
+      principals: [new iam.ArnPrincipal('arn')],
+    }));
+
+    // THEN
+    expect(stack).toMatch({
+      'Resources': {
+        'MyTopic86869434': {
+          'Type': 'AWS::SNS::Topic',
+        },
+        'TopicPolicyA24B096F': {
+          'Type': 'AWS::SNS::TopicPolicy',
+          'Properties': {
+            'PolicyDocument': {
+              'Statement': [
+                {
+                  'Action': 'service:statement0',
+                  'Effect': 'Allow',
+                  'Principal': { 'AWS': 'arn' },
+                  'Sid': '0',
+                },
+              ],
+              'Version': '2012-10-17',
+            },
+            'Topics': [
+              {
+                'Ref': 'MyTopic86869434',
+              },
+            ],
+          },
+        },
+      },
+    });
     test.done();
   },
 
