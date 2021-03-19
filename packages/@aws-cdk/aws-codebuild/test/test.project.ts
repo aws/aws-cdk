136ce123ba1b0d5dc5429ae63f4dc4c771920377
Adam Ruka <adamruka@amazon.com>
2021-03-19 16:19:41 -0700

    fix(codebuild): take the account & region of an imported Project from its ARN
    This is needed to correctly use CodeBuild in CodePipeline
    (which needs to know whether the Project is from a different account/region).
    
    Fixes #13694

--- packages/@aws-cdk/aws-codebuild/test/test.project.ts
+++ packages/@aws-cdk/aws-codebuild/test/test.project.ts
@@ -982,6 +982,7 @@ export = {
 
       test.done();
     },
+
     'can override build timeout'(test: Test) {
       // GIVEN
       const stack = new cdk.Stack();
@@ -999,6 +1000,20 @@ export = {
       expect(stack).to(haveResourceLike('AWS::CodeBuild::Project', {
         TimeoutInMinutes: 30,
       }));
+
+      test.done();
+    },
+  },
+
+  'can be imported': {
+    'by ARN'(test: Test) {
+      const stack = new cdk.Stack();
+      const project = codebuild.Project.fromProjectArn(stack, 'Project',
+        'arn:aws:codebuild:us-west-2:123456789012:project/My-Project');
+
+      test.equal(project.projectName, 'My-Project');
+      test.equal(project.env.account, '123456789012');
+      test.equal(project.env.region, 'us-west-2');
 
       test.done();
     },
