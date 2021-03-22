19d5a8d8fc135cb43bfcf1740f57991c2002f127
Rico Huijbers <rix0rrr@gmail.com>
2021-03-22 12:24:18 +0100

    feat(init-templates): app template comes with hint comments for 'env' (#13696)
    How the `env` parameter is supposed to be used is still confusing to
    a lot of users.
    
    Add uncommentable lines to the init templates showing and describing
    the 3 alternatives.
    
    Fixes #12321.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk/lib/init-templates/v2/app/python/app.template.py
+++ packages/aws-cdk/lib/init-templates/v2/app/python/app.template.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 
 import aws_cdk_lib as core
 
@@ -6,6 +7,22 @@ from %name.PythonModule%.%name.PythonModule%_stack import %name.PascalCased%Stac
 
 
 app = core.App()
-%name.PascalCased%Stack(app, "%name.PascalCased%Stack")
+%name.PascalCased%Stack(app, "%name.PascalCased%Stack",
+    # If you don't specify 'env', this stack will be environment-agnostic.
+    # Account/Region-dependent features and context lookups will not work,
+    # but a single synthesized template can be deployed anywhere.
+
+    # Uncomment the next line to specialize this stack for the AWS Account
+    # and Region that are implied by the current CLI configuration.
+
+    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+    # Uncomment the next line if you know exactly what Account and Region you
+    # want to deploy the stack to. */
+
+    #env=core.Environment(account='123456789012', region='us-east-1'),
+
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    )
 
 app.synth()
