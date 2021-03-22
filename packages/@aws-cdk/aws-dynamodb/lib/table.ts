591d31e30f6b3d30c3e2876f458df2add6934b27
Sordie <35031535+Sordie@users.noreply.github.com>
2021-03-22 18:25:35 +0000

    fix(dynamodb): typo

--- packages/@aws-cdk/aws-dynamodb/lib/table.ts
+++ packages/@aws-cdk/aws-dynamodb/lib/table.ts
@@ -13,7 +13,7 @@ import * as perms from './perms';
 import { ReplicaProvider } from './replica-provider';
 import { EnableScalingProps, IScalableTableAttribute } from './scalable-attribute-api';
 import { ScalableTableAttribute } from './scalable-table-attribute';
-import { TableTimeToLiveProvider } from './tabel-time-to-live-provider';
+import { TableTimeToLiveProvider } from './table-time-to-live-provider';
 
 // keep this import separate from other imports to reduce chance for merge conflicts with v2-main
 // eslint-disable-next-line no-duplicate-imports, import/order
