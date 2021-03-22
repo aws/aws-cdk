e53964e8462b4a452a976bf82436ac357b563dea
Niranjan Jayakar <nija@amazon.com>
2021-03-22 18:49:42 +0000

    fix(events,applicationautoscaling): specifying a schedule rate in seconds results in an error (#13689)
    A previous change - b1449a17 - introduced a validation that should have
    been applied only when Duration is specified as a token. Instead, it was
    applied for non token values as well.
    
    Adjusting the validation so it only applies to when the duration is a
    token.
    
    fixes #13566
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-applicationautoscaling/lib/schedule.ts
+++ packages/@aws-cdk/aws-applicationautoscaling/lib/schedule.ts
@@ -17,11 +17,11 @@ export abstract class Schedule {
    * Construct a schedule from an interval and a time unit
    */
   public static rate(duration: Duration): Schedule {
-    const validDurationUnit = ['minute', 'minutes', 'hour', 'hours', 'day', 'days'];
-    if (!validDurationUnit.includes(duration.unitLabel())) {
-      throw new Error("Allowed unit for scheduling is: 'minute', 'minutes', 'hour', 'hours', 'day' or 'days'");
-    }
     if (duration.isUnresolved()) {
+      const validDurationUnit = ['minute', 'minutes', 'hour', 'hours', 'day', 'days'];
+      if (!validDurationUnit.includes(duration.unitLabel())) {
+        throw new Error("Allowed units for scheduling are: 'minute', 'minutes', 'hour', 'hours', 'day' or 'days'");
+      }
       return new LiteralSchedule(`rate(${duration.formatTokenToNumber()})`);
     }
     if (duration.toSeconds() === 0) {
