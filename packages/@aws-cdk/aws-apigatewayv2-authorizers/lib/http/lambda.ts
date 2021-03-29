f29571682405160efe5156e0652fa8642ef1a907
Kyle Roach <kroach.work@gmail.com>
2021-03-29 00:28:04 -0400

    feat: allow returning iam policy with simple responses

--- packages/@aws-cdk/aws-apigatewayv2-authorizers/lib/http/lambda.ts
+++ packages/@aws-cdk/aws-apigatewayv2-authorizers/lib/http/lambda.ts
@@ -19,6 +19,9 @@ export enum HttpLambdaResponseType {
 
   /** Returns an IAM Policy */
   IAM,
+
+  /** Returns an IAM Policy, with simple responses enabled */
+  IAM_SIMPLE,
 }
 
 /**
@@ -75,7 +78,10 @@ export class HttpLambdaAuthorizer implements IHttpRouteAuthorizer {
       const id = this.props.authorizerName && !Token.isUnresolved(this.props.authorizerName) ?
         this.props.authorizerName : 'LambdaAuthorizer';
 
-      const enableSimpleResponses = this.props.responseTypes.includes(HttpLambdaResponseType.SIMPLE) || undefined;
+      const enableSimpleResponses =
+        this.props.responseTypes.includes(HttpLambdaResponseType.SIMPLE) ||
+        this.props.responseTypes.includes(HttpLambdaResponseType.IAM_SIMPLE) ||
+        undefined;
 
       this.authorizer = new HttpAuthorizer(options.scope, id, {
         httpApi: options.route.httpApi,
--- packages/@aws-cdk/aws-apigatewayv2-authorizers/test/http/lambda.test.ts
+++ packages/@aws-cdk/aws-apigatewayv2-authorizers/test/http/lambda.test.ts
@@ -1,4 +1,5 @@
 import '@aws-cdk/assert/jest';
+import { ABSENT } from '@aws-cdk/assert';
 import { HttpApi, HttpIntegrationType, HttpRouteIntegrationBindOptions, IHttpRouteIntegration, PayloadFormatVersion } from '@aws-cdk/aws-apigatewayv2';
 import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
 import { Duration, Stack } from '@aws-cdk/core';
@@ -73,7 +74,48 @@ describe('HttpLambdaAuthorizer', () => {
       Name: 'LambdaAuthorizer',
       AuthorizerType: 'REQUEST',
       AuthorizerResultTtlInSeconds: 300,
+      EnableSimpleResponses: ABSENT,
       AuthorizerPayloadFormatVersion: '1.0',
+      IdentitySource: [
+        '$request.header.Authorization',
+      ],
+    });
+
+    expect(stack).toHaveResource('AWS::ApiGatewayV2::Route', {
+      AuthorizationType: 'CUSTOM',
+    });
+  });
+
+  test('default iam simple', () => {
+    // GIVEN
+    const stack = new Stack();
+    const api = new HttpApi(stack, 'HttpApi');
+
+    const handler = new Function(stack, 'auth-function', {
+      runtime: Runtime.NODEJS_12_X,
+      code: Code.fromInline('exports.handler = () => {return true}'),
+      handler: 'index.handler',
+    });
+
+    const authorizer = new HttpLambdaAuthorizer({
+      responseTypes: [HttpLambdaResponseType.IAM_SIMPLE],
+      handler,
+    });
+
+    // WHEN
+    api.addRoutes({
+      integration: new DummyRouteIntegration(),
+      path: '/books',
+      authorizer,
+    });
+
+    // THEN
+    expect(stack).toHaveResource('AWS::ApiGatewayV2::Authorizer', {
+      Name: 'LambdaAuthorizer',
+      AuthorizerType: 'REQUEST',
+      AuthorizerResultTtlInSeconds: 300,
+      AuthorizerPayloadFormatVersion: '1.0',
+      EnableSimpleResponses: true,
       IdentitySource: [
         '$request.header.Authorization',
       ],
