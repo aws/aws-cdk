2f9824419ab6842f46f465fb717f1c9cf8efdff0
Matt Simpson <m.simpson@outlook.com>
2021-03-29 01:28:02 +0000

    feat(aws-events):api destinations

--- packages/@aws-cdk/aws-events-targets/lib/api-destination.ts
+++ packages/@aws-cdk/aws-events-targets/lib/api-destination.ts
@@ -1,7 +1,4 @@
 import * as events from '@aws-cdk/aws-events';
-import * as iam from '@aws-cdk/aws-iam';
-import { Names } from '@aws-cdk/core';
-import { singletonEventRole } from './util';
 
 /**
  * Customize the EventBridge Api Destinations Target
@@ -22,22 +19,20 @@ export interface ApiDestinationProps {
    * @default none
    */
   readonly headerParameters?: { [key: string]: string };
-    /**
+  /**
    * The path parameter values to be used to populate API Gateway REST API
    * or EventBridge Api destinations path wildcards ("*")
    *
    * @default none
    */
   readonly pathParameterValues?: string[]
-    /**
+  /**
    * The query string keys/values that need to be sent as part of request invoking the API Gateway REST API
    *  or EventBridge Api destination.
    *
    * @default none
    */
   readonly queryStringParameters?: { [key: string]: string };
-
-
 }
 
 /**
@@ -54,11 +49,11 @@ export class ApiDestination implements events.IRuleTarget {
    * Returns a RuleTarget that can be used to trigger API destinations
    * from an EventBridge event.
    */
-  public bind(rule: events.IRule, _id?: string): events.RuleTargetConfig {
+  public bind(_rule: events.IRule, _id?: string): events.RuleTargetConfig {
     const httpParameters: events.CfnRule.HttpParametersProperty = {
-        headerParameters: this.props.headerParameters,
-        pathParameterValues: this.props.pathParameterValues,
-        queryStringParameters: this.props.queryStringParameters,
+      headerParameters: this.props.headerParameters,
+      pathParameterValues: this.props.pathParameterValues,
+      queryStringParameters: this.props.queryStringParameters,
     };
 
     return {
--- packages/@aws-cdk/aws-events-targets/test/api-destination/api-destination.test.ts
+++ packages/@aws-cdk/aws-events-targets/test/api-destination/api-destination.test.ts
@@ -4,45 +4,43 @@ import { Stack } from '@aws-cdk/core';
 import * as targets from '../../lib';
 
 test('use aws batch job as an eventrule target', () => {
-    // GIVEN
-    const stack = new Stack();
+  // GIVEN
+  const stack = new Stack();
 
-    const connection = new events.Connection(stack, 'Connection', {
-        authorizationType: 'BASIC',
-        authParameters: {
-            BasicAuthParameters: {
-                Password: 'password',
-                Username: 'username',
-            },
+  const connection = new events.Connection(stack, 'Connection', {
+    authorizationType: events.AuthorizationType.BASIC,
+    authParameters: {
+      basicAuthParameters: {
+        password: 'password',
+        username: 'username',
+      },
+    },
+    description: 'ConnectionDescription',
+    connectionName: 'testConnection',
+  });
+
+  const rule = new events.Rule(stack, 'Rule', {
+    schedule: events.Schedule.expression('rate(1 min)'),
+  });
+
+  // WHEN
+  rule.addTarget(new targets.ApiDestination(connection));
+
+  // THEN
+  expect(stack).to(haveResource('AWS::Events::Rule', {
+    ScheduleExpression: 'rate(1 min)',
+    State: 'ENABLED',
+    Targets: [
+      {
+        Arn: {
+          'Fn::GetAtt': [
+            'Connection07624BCD',
+            'Arn',
+          ],
         },
-        description: 'ConnectionDescription',
-        name: 'testConnection',
-    });
-
-    const rule = new events.Rule(stack, 'Rule', {
-        schedule: events.Schedule.expression('rate(1 min)'),
-    });
-
-    // WHEN
-    rule.addTarget(new targets.ApiDestination(connection));
-
-    // THEN
-    expect(stack).to(haveResource('AWS::Events::Rule', {
-        ScheduleExpression: 'rate(1 min)',
-        State: 'ENABLED',
-        Targets: [
-            {
-                Arn: {
-                    Ref: 'MyConnection',
-                },
-                Id: 'Target0',
-                RoleArn: {
-                    'Fn::GetAtt': [
-                        'MyJobEventsRoleCF43C336',
-                        'Arn',
-                    ],
-                },
-            },
-        ],
-    }));
+        Id: 'Target0',
+        HttpParameters: {},
+      },
+    ],
+  }));
 });
