e6b7047f52cc5d44fbbea0f905f509eee6b8a7a1
Rico Huijbers <rix0rrr@gmail.com>
2021-03-31 19:05:35 +0200

    chore: fail early on impossible shrinkwrap (#13899)
    If the **resolved** version of a package (the one we force using Yarn)
    does not match the **required** version of a package (the one in
    `package.json`), then NPM ignores the resolved version and installs
    the package from the `requires` list anyway, *even* if it's processing
    a shrinkwrap file.
    
    Unfortunately, sometimes we have to force this situation, such as with
    the `netmask` issue, where the required version is declared at `^1.0.6`
    but we need to force it to be resolved to `2.0.1`.
    
    Detect this situation early and bail out, so that we don't try to ship something
    that doesn't have the version resolution we expected to ship.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- tools/yarn-cling/test/cling.test.ts
+++ tools/yarn-cling/test/cling.test.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import { generateShrinkwrap } from "../lib";
+import { checkRequiredVersions, generateShrinkwrap } from '../lib';
 
 test('generate lock for fixture directory', async () => {
   const lockFile = await generateShrinkwrap({
@@ -9,27 +9,27 @@ test('generate lock for fixture directory', async () => {
 
   expect(lockFile).toEqual({
     lockfileVersion: 1,
-    name: "package1",
+    name: 'package1',
     requires: true,
-    version: "1.1.1",
+    version: '1.1.1',
     dependencies: {
       package2: {
-        version: "2.2.2",
+        version: '2.2.2',
       },
       registrydependency1: {
         dependencies: {
           registrydependency2: {
-            integrity: "sha512-pineapple",
-            resolved: "https://registry.bla.com/stuff",
-            version: "2.3.999",
+            integrity: 'sha512-pineapple',
+            resolved: 'https://registry.bla.com/stuff',
+            version: '2.3.999',
           },
         },
-        integrity: "sha512-banana",
+        integrity: 'sha512-banana',
         requires: {
-          registrydependency2: "^2.3.4",
+          registrydependency2: '^2.3.4',
         },
-        resolved: "https://registry.bla.com/stuff",
-        version: "1.2.999",
+        resolved: 'https://registry.bla.com/stuff',
+        version: '1.2.999',
       },
     },
   });
@@ -43,26 +43,48 @@ test('generate hoisted lock for fixture directory', async () => {
 
   expect(lockFile).toEqual({
     lockfileVersion: 1,
-    name: "package1",
+    name: 'package1',
     requires: true,
-    version: "1.1.1",
+    version: '1.1.1',
     dependencies: {
       package2: {
-        version: "2.2.2",
+        version: '2.2.2',
       },
       registrydependency1: {
-        integrity: "sha512-banana",
+        integrity: 'sha512-banana',
         requires: {
-          registrydependency2: "^2.3.4",
+          registrydependency2: '^2.3.4',
         },
-        resolved: "https://registry.bla.com/stuff",
-        version: "1.2.999",
+        resolved: 'https://registry.bla.com/stuff',
+        version: '1.2.999',
       },
       registrydependency2: {
-        integrity: "sha512-pineapple",
-        resolved: "https://registry.bla.com/stuff",
-        version: "2.3.999",
+        integrity: 'sha512-pineapple',
+        resolved: 'https://registry.bla.com/stuff',
+        version: '2.3.999',
       },
     },
   });
+});
+
+test('fail when requires cannot be satisfied', async () => {
+  const lockFile = {
+    lockfileVersion: 1,
+    name: 'package1',
+    requires: true,
+    version: '1.1.1',
+    dependencies: {
+      package1: {
+        version: '2.2.2',
+        requires: {
+          package2: '^3.3.3', // <- this needs to be adjusted
+        },
+      },
+      package2: {
+        version: '4.4.4',
+      },
+    },
+  };
+
+  expect(() => checkRequiredVersions(lockFile)).toThrow(/This can never/);
 });
