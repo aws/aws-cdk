8abd7f78db99ca1cc70581ba13e5fda28d996241
Rico Huijbers <rix0rrr@gmail.com>
2021-04-06 11:16:57 +0200

    chore(cli): change Java init templates to be less repetitive
    The Java init templates were recently fixed to use `new` instead
    of the builder, because the builder would instantiate the wrong class
    (`Stack` instead of `MyStack`) (#13988).
    
    However, the structure was changed to have 3 different `new` calls
    in 3 different comment blocks.
    
    Change it back to the old structure: there is one instantiation, and
    uncommenting just passes or doesn't pass the `env` property. Except
    this time we use `new` instead of the builder.

--- packages/aws-cdk/lib/init-templates/v2/app/java/src/main/java/com/myorg/%name.PascalCased%App.template.java
+++ packages/aws-cdk/lib/init-templates/v2/app/java/src/main/java/com/myorg/%name.PascalCased%App.template.java
@@ -1,7 +1,8 @@
 package com.myorg;
 
-import software.amazon.awscdk.lib.App;
-import software.amazon.awscdk.lib.Environment;
+import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
 
 import java.util.Arrays;
 
@@ -9,37 +10,33 @@ public class %name.PascalCased%App {
     public static void main(final String[] args) {
         App app = new App();
 
-        // If you don't specify 'env', this stack will be environment-agnostic.
-        // Account/Region-dependent features and context lookups will not work,
-        // but a single synthesized template can be deployed anywhere.
-        new %name.PascalCased%Stack(app, "%name.PascalCased%Stack");
-
-        // Replace the above stack intialization with the following to specialize
-        // this stack for the AWS Account and Region that are implied by the current
-        // CLI configuration.
-        /*
         new %name.PascalCased%Stack(app, "%name.PascalCased%Stack", StackProps.builder()
+                // If you don't specify 'env', this stack will be environment-agnostic.
+                // Account/Region-dependent features and context lookups will not work,
+                // but a single synthesized template can be deployed anywhere.
+
+                // Uncomment the next block to specialize this stack for the AWS Account
+                // and Region that are implied by the current CLI configuration.
+                /*
                 .env(Environment.builder()
                         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
                         .region(System.getenv("CDK_DEFAULT_REGION"))
                         .build())
-                .build());
-        */
+                */
 
-        // Replace the above stack initialization with the following if you know exactly
-        // what Account and Region you want to deploy the stack to.
-        /*
-        new %name.PascalCased%Stack(app, "%name.PascalCased%Stack", StackProps.builder()
+                // Uncomment the next block if you know exactly what Account and Region you
+                // want to deploy the stack to.
+                /*
                 .env(Environment.builder()
                         .account("123456789012")
                         .region("us-east-1")
                         .build())
+                */
 
+                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                 .build());
-        */
-
-        // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 
         app.synth();
     }
 }
+
