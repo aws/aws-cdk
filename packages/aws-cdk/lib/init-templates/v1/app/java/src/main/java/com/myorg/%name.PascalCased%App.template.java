b8c8e70b9024bee0422110615b2b3327e98182b5
Mitchell Valine <mitchellvaline@gmail.com>
2021-04-05 19:04:44 -0700

    Make comments cleaner

--- packages/aws-cdk/lib/init-templates/v1/app/java/src/main/java/com/myorg/%name.PascalCased%App.template.java
+++ packages/aws-cdk/lib/init-templates/v1/app/java/src/main/java/com/myorg/%name.PascalCased%App.template.java
@@ -10,31 +10,36 @@ public class %name.PascalCased%App {
     public static void main(final String[] args) {
         App app = new App();
 
-        new %name.PascalCased%Stack(app, "%name.PascalCased%Stack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+        // If you don't specify 'env', this stack will be environment-agnostic.
+        // Account/Region-dependent features and context lookups will not work,
+        // but a single synthesized template can be deployed anywhere.
+        new %name.PascalCased%Stack(app, "%name.PascalCased%Stack");
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
+        // Replace the above stack intialization with the following to specialize
+        // this stack for the AWS Account and Region that are implied by the current
+        // CLI configuration.
+        /*
+        new %name.PascalCased%Stack(app, "%name.PascalCased%Stack", StackProps.builder()
                 .env(Environment.builder()
                         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
                         .region(System.getenv("CDK_DEFAULT_REGION"))
                         .build())
-                */
+                .build());
+        */
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
+        // Replace the above stack initialization with the following if you know exactly
+        // what Account and Region you want to deploy the stack to.
+        /*
+        new %name.PascalCased%Stack(app, "%name.PascalCased%Stack", StackProps.builder()
                 .env(Environment.builder()
                         .account("123456789012")
                         .region("us-east-1")
                         .build())
-                */
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 
                 .build());
+        */
+
+        // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 
         app.synth();
     }
