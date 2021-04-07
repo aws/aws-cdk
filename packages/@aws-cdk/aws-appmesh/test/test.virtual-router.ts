b1ecd3d49d7ebf97a54a80d06779ef0f0b113c16
Rico Huijbers <rix0rrr@gmail.com>
2021-04-07 12:20:55 +0200

    chore: introduce `@aws-cdk/assert-internal`, `aws-cdk-migration` (#13700)
    `@aws-cdk/assert` will be a publish-only package: we no longer use it to test our own packages.
    
    Instead, our own packages are tested using `@aws-cdk/assert-internal`. `@aws-cdk/assert` will
    be built by cloning the `assert-internal` package, and made to depend on either `@aws-cdk/core`
    on the v1 branch, and `aws-cdk-lib` on the v2 branch.
    
    ----
    
    The `rewrite-imports` script has been moved to a top-level tools package, `aws-cdk-migration`.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-appmesh/test/test.virtual-router.ts
+++ packages/@aws-cdk/aws-appmesh/test/test.virtual-router.ts
@@ -1,4 +1,4 @@
-import { expect, haveResourceLike } from '@aws-cdk/assert';
+import { expect, haveResourceLike } from '@aws-cdk/assert-internal';
 import * as cdk from '@aws-cdk/core';
 import { Test } from 'nodeunit';
 
--- packages/@aws-cdk/aws-cloudfront/test/cache-policy.test.ts
+++ packages/@aws-cdk/aws-cloudfront/test/cache-policy.test.ts
@@ -1,4 +1,4 @@
-import '@aws-cdk/assert/jest';
+import '@aws-cdk/assert-internal/jest';
 import { App, Aws, Duration, Stack } from '@aws-cdk/core';
 import { CachePolicy, CacheCookieBehavior, CacheHeaderBehavior, CacheQueryStringBehavior } from '../lib';
 
