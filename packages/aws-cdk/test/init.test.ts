aa2ad18dfd96463222ca8db51700fa89a5299e81
Rico Huijbers <rix0rrr@gmail.com>
2021-04-08 16:20:17 +0200

    Print more

--- packages/aws-cdk/test/init.test.ts
+++ packages/aws-cdk/test/init.test.ts
@@ -128,11 +128,11 @@ describe('constructs version', () => {
 
     // convert dir name from aws-cdk-test-xyz to AwsCdkTestXyz
     const slnName = path.basename(workDir).split('-').map(s => `${s[0].toUpperCase()}${s.slice(1)}`).join('');
-    // eslint-disable-next-line no-console
-    console.log('Was expecting to see', slnName, 'in', workDir);
     const csprojFile = path.join(workDir, 'src', slnName, `${slnName}.csproj`);
 
     expect(await fs.pathExists(csprojFile)).toBeTruthy();
+    // eslint-disable-next-line no-console
+    console.log('Was expecting to see', slnName, 'in', workDir, 'as', csprojFile);
     const csproj = (await fs.readFile(csprojFile, 'utf8')).split(/\r?\n/);
 
     expect(csproj).toContainEqual(expect.stringMatching(/\<PackageReference Include="Constructs" Version="\[10\..*,11\..*\)"/));
