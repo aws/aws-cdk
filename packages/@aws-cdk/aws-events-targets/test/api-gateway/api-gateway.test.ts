ae1f56a359283aa565f414da967b093ca8b15d1c
hedrall <masaharu73@gmail.com>
2021-04-08 16:25:34 +0900

    fix to use assert-internal

--- packages/@aws-cdk/aws-events-targets/test/api-gateway/api-gateway.test.ts
+++ packages/@aws-cdk/aws-events-targets/test/api-gateway/api-gateway.test.ts
@@ -1,4 +1,4 @@
-import '@aws-cdk/assert/jest';
+import '@aws-cdk/assert-internal/jest';
 import * as api from '@aws-cdk/aws-apigateway';
 import * as events from '@aws-cdk/aws-events';
 import * as iam from '@aws-cdk/aws-iam';
