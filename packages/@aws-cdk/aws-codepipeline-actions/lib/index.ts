2d434ddaed882032678fea9c922421a9b4d941f2
Adam Ruka <adamruka@amazon.com>
2021-04-15 11:04:52 -0700

    feat(codepipeline): introduce the Action abstract class (#14009)
    Add an officially supported class to the API of the CodePipeline module
    that contains some logic common to all implementations of the `IAction` interface,
    like dealing with variable namespaces, or methods like `onStateChange()`.
    
    There was previously a class like that in the codepipeline-actions module,
    but it was marked experimental, and had some strong opinions on the construction way of the subclasses
    (required passing the `actionProperties` through a `super()` call in the constructor,
    which is not very flexible).
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-codepipeline-actions/lib/index.ts
+++ packages/@aws-cdk/aws-codepipeline-actions/lib/index.ts
@@ -17,4 +17,4 @@ export * from './s3/deploy-action';
 export * from './s3/source-action';
 export * from './stepfunctions/invoke-action';
 export * from './servicecatalog/deploy-action';
-export * from './action'; // for some reason, JSII fails building the module without exporting this class
+export * from './action';
