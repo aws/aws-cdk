8f4921c9f87dfac09b64f3da1e2be189052777e2
Sandro Manke <neuronist@gmail.com>
2021-04-15 17:41:38 +0200

    feat(ec2): make it possible to override mapPublicIpOnLaunch for public Subnets (#14194)
    This PR enables to override the Auto Assign Public IP Parameter, so you can also turn it off for public subnets.

--- packages/@aws-cdk/aws-ec2/lib/vpc.ts
+++ packages/@aws-cdk/aws-ec2/lib/vpc.ts
@@ -838,6 +838,12 @@ export interface VpcProps {
    *      },
    *      {
    *        cidrMask: 24,
+   *        name: 'public-other',
+   *        subnetType: ec2.SubnetType.PUBLIC,
+   *        mapPublicIpOnLaunch: false,
+   *      },
+   *      {
+   *        cidrMask: 24,
    *        name: 'application',
    *        subnetType: ec2.SubnetType.PRIVATE,
    *      },
@@ -958,6 +964,13 @@ export interface SubnetConfiguration {
    * @default false
    */
   readonly reserved?: boolean;
+
+  /**
+   * Controls if a public IP is associated to an instance at launch
+   *
+   * @default true in Subnet.Public, false in Subnet.Private or Subnet.Isolated.
+   */
+   readonly mapPublicIpOnLaunch?: boolean;
 }
 
 /**
@@ -1386,7 +1399,7 @@ export class Vpc extends VpcBase {
         availabilityZone: zone,
         vpcId: this.vpcId,
         cidrBlock: this.networkBuilder.addSubnet(cidrMask),
-        mapPublicIpOnLaunch: (subnetConfig.subnetType === SubnetType.PUBLIC),
+        mapPublicIpOnLaunch: (subnetConfig.subnetType === SubnetType.PUBLIC && subnetConfig.mapPublicIpOnLaunch !== false),
       };
 
       let subnet: Subnet;
