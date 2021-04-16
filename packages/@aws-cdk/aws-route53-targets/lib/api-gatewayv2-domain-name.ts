460f9b226bd9ebe380953017eb6568da268ab4c6
NetaNir <neta1nir@gmail.com>
2021-04-16 13:37:00 -0700

    feat(stepfunction-tasks/route53-targets): add apigwv2 interface to the integration packages

--- packages/@aws-cdk/aws-route53-targets/lib/api-gatewayv2-domain-name.ts
+++ packages/@aws-cdk/aws-route53-targets/lib/api-gatewayv2-domain-name.ts
@@ -5,7 +5,7 @@ import * as route53 from '@aws-cdk/aws-route53';
  * Defines an API Gateway V2 domain name as the alias target.
  */
 export class ApiGatewayv2Domain implements route53.IAliasRecordTarget {
-  constructor(private readonly domainName: apigv2.IDomainName) { }
+  constructor(private readonly domainName: ApiGatewayDomainName) { }
 
   public bind(_record: route53.IRecordSet): route53.AliasRecordTargetConfig {
     return {
@@ -13,4 +13,20 @@ export class ApiGatewayv2Domain implements route53.IAliasRecordTarget {
       hostedZoneId: this.domainName.regionalHostedZoneId,
     };
   }
+}
+
+/**
+ * Represents an APIGatewayV2 DomainName
+ */
+export interface ApiGatewayDomainName {
+  /**
+    * The region-specific Amazon Route 53 Hosted Zone ID of the regional endpoint.
+    */
+  readonly regionalHostedZoneId: string;
+
+  /**
+   * The domain name associated with the regional endpoint for this custom domain name.
+   */
+  readonly regionalDomainName: string;
+
 }
