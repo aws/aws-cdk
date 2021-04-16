1fbfbe0eda74ca0af776f5034a413b677bf29cc3
Alex Johnson <alexjo5000@gmail.com>
2021-04-16 12:09:35 -0700

    feat(appmesh): add Connection Pools for VirtualNode and VirtualGateway (#13917)
    Adds connection pools to VirtualNodes and VirtualGateways. VirtualNodes support listeners for HTTP, HTTP2, gRPC and TPC. VirtualGateways support only HTTP, HTTP2 and gRPC (no TCP). There are connection pool configurations for each of these 7 node/protocol combinations.
    
    * HTTP connection pools need these properties: maxConnections, maxPendingRequests
    * HTTP2 connection pools need these properties: maxRequests
    * gRPC connection pools need these properties: maxRequests
    * TCP connection pools need these properties: maxConnections
    
    Since connection pool properties differ by protocol so it's important to enforce the correct fields in CDK. To accomplish this  I've added interfaces per protocol type and squashed API config `ConnectionPool > [Protocol] > properties` to the equivalent `ConnectionPool > properties` CDK config. This is ok because the user will never mismatch protocol types. From [docs (see connection pool section)](https://docs.aws.amazon.com/app-mesh/latest/userguide/virtual_gateways.html):
    > The connectionPool and portMapping protocols must be the same. If your listener protocol is grpc or http2, specify maxRequests only. If your listener protocol is http, you can specify both maxConnections and maxPendingRequests. 
    
    ```ts
    new appmesh.VirtualNode(stack, 'test-node', {
      mesh,
      listeners: [
        appmesh.VirtualNodeListener.tcp({
          port: 80,
          connectionPool: {
            maxConnections: 100,
          },
        }),
      ],
    });
    ```
    
    Pre-addressing the `ConnectionPoolConfig` placeholder interface & lint ignore statement: I’ve added this so I can treat the connection pool types as sub-classes while taking advantage of the interface object literal pattern. Cleaner UX compared to using classes.
    
    BREAKING CHANGE: HTTP2 `VirtualNodeListener`s must be now created with `Http2VirtualNodeListenerOptions`
    * **appmesh**: HTTP2 `VirtualGatewayListener`s must be now created with `Http2VirtualGatewayListenerOptions`
    
    Closes #11647
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-appmesh/lib/private/utils.ts
+++ packages/@aws-cdk/aws-appmesh/lib/private/utils.ts
@@ -39,3 +39,29 @@ const HEALTH_CHECK_PROPERTY_THRESHOLDS: {[key in (keyof AppMeshHealthCheck)]?: [
   timeoutMillis: [2000, 60000],
   unhealthyThreshold: [2, 10],
 };
+
+/**
+ * Generated Connection pool config
+ */
+export interface ConnectionPoolConfig {
+  /**
+   * The maximum connections in the pool
+   *
+   * @default - none
+   */
+  readonly maxConnections?: number;
+
+  /**
+   * The maximum pending requests in the pool
+   *
+   * @default - none
+   */
+  readonly maxPendingRequests?: number;
+
+  /**
+   * The maximum requests in the pool
+   *
+   * @default - none
+   */
+  readonly maxRequests?: number;
+}
