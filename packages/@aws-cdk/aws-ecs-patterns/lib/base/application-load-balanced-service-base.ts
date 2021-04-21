3fd25a9b11c7dd168667754a6ac538e37159a2d4
yiluncui <75712077+yiluncui@users.noreply.github.com>
2021-04-21 03:49:08 -0700

    fix(aws-ecs-patterns, aws-elasticloadbalancingv2): Pass TargetGroup P… (#14092)
    Pass TargetGroup ProtocolVersion as parameters to higher level constructs (#14091)
    
    This change allows users to optionally provide ProtocolVersion when
    adding targets to ALB listeners as well as specifying ProtocolVersion
    into ApplicationLoadBalancedService patterns in ecs-patterns.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-ecs-patterns/lib/base/application-load-balanced-service-base.ts
+++ packages/@aws-cdk/aws-ecs-patterns/lib/base/application-load-balanced-service-base.ts
@@ -5,7 +5,7 @@ import {
   ICluster, LogDriver, PropagatedTagSource, Secret,
 } from '@aws-cdk/aws-ecs';
 import {
-  ApplicationListener, ApplicationLoadBalancer, ApplicationProtocol, ApplicationTargetGroup,
+  ApplicationListener, ApplicationLoadBalancer, ApplicationProtocol, ApplicationProtocolVersion, ApplicationTargetGroup,
   IApplicationLoadBalancer, ListenerCertificate, ListenerAction, AddApplicationTargetsProps,
 } from '@aws-cdk/aws-elasticloadbalancingv2';
 import { IRole } from '@aws-cdk/aws-iam';
@@ -116,6 +116,13 @@ export interface ApplicationLoadBalancedServiceBaseProps {
    * set by default to HTTPS.
    */
   readonly protocol?: ApplicationProtocol;
+
+  /**
+   * The protocol version to use
+   *
+   * @default ApplicationProtocolVersion.HTTP1
+   */
+  readonly protocolVersion?: ApplicationProtocolVersion;
 
   /**
    * The domain name for the service, e.g. "api.example.com."
@@ -412,6 +419,7 @@ export abstract class ApplicationLoadBalancedServiceBase extends CoreConstruct {
 
     const targetProps: AddApplicationTargetsProps = {
       protocol: props.targetProtocol ?? ApplicationProtocol.HTTP,
+      protocolVersion: props.protocolVersion,
     };
 
     this.listener = loadBalancer.addListener('PublicListener', {
