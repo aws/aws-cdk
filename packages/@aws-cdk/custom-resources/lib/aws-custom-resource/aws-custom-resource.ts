a97807307933588ffce5c2636c8815afed277bea
Erik Karlsson <erik.karlsson@educations.com>
2021-04-23 15:03:11 +0200

    feat(custom-resources):  AwsSdkCall can assume Role for cross-account custom resources (#13916)
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*
    #13915 
    
    PR related to my feature request in #13915

--- packages/@aws-cdk/custom-resources/lib/aws-custom-resource/aws-custom-resource.ts
+++ packages/@aws-cdk/custom-resources/lib/aws-custom-resource/aws-custom-resource.ts
@@ -130,6 +130,17 @@ export interface AwsSdkCall {
    * @default - return all data
    */
   readonly outputPath?: string;
+
+  /**
+   * Used for running the SDK calls in underlying lambda with a different role
+   * Can be used primarily for cross-account requests to for example connect
+   * hostedzone with a shared vpc
+   *
+   * Example for Route53 / associateVPCWithHostedZone
+   *
+   * @default - run without assuming role
+   */
+  readonly assumedRoleArn?: string;
 }
 
 /**
@@ -355,10 +366,16 @@ export class AwsCustomResource extends CoreConstruct implements iam.IGrantable {
     } else {
       // Derive statements from AWS SDK calls
       for (const call of [props.onCreate, props.onUpdate, props.onDelete]) {
-        if (call) {
+        if (call && call.assumedRoleArn == null) {
           const statement = new iam.PolicyStatement({
             actions: [awsSdkToIamAction(call.service, call.action)],
             resources: props.policy.resources,
+          });
+          statements.push(statement);
+        } else if (call && call.assumedRoleArn != null) {
+          const statement = new iam.PolicyStatement({
+            actions: ['sts:AssumeRole'],
+            resources: [call.assumedRoleArn],
           });
           statements.push(statement);
         }
