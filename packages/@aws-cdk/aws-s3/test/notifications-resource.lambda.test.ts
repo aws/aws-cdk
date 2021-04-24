2fab734686fd011a6ff4b4ec30b4ca41d2bd7384
Michael Brewer <michael.brewer@gyft.com>
2021-04-23 23:33:12 -0700

    test(s3): add missing semi

--- packages/@aws-cdk/aws-s3/test/notifications-resource.lambda.test.ts
+++ packages/@aws-cdk/aws-s3/test/notifications-resource.lambda.test.ts
@@ -2,7 +2,7 @@ import { spawnSync } from 'child_process';
 import * as path from 'path';
 
 test('lambda python pytest', () => {
-  const testScript = path.join(__dirname, '..', 'lib', 'notifications-resource', 'lambda-source', 'test.sh')
+  const testScript = path.join(__dirname, '..', 'lib', 'notifications-resource', 'lambda-source', 'test.sh');
   const result = spawnSync(testScript, { stdio: 'inherit' });
   expect(result.status).toBe(0);
 });
