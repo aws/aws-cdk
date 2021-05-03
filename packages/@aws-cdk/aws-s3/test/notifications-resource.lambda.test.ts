0614d14493dd427a681aa280eae0015c538e9588
Michael Brewer <michael.brewer@gyft.com>
2021-05-03 01:42:30 -0700

    chore: review changes
    Co-authored-by: Eli Polonsky <Eli.polonsky@gmail.com>

--- packages/@aws-cdk/aws-s3/test/notifications-resource.lambda.test.ts
+++ packages/@aws-cdk/aws-s3/test/notifications-resource.lambda.test.ts
@@ -1,7 +1,7 @@
 import { spawnSync } from 'child_process';
 import * as path from 'path';
 
-test('lambda python pytest', () => {
+test('notifications handler', () => {
   const testScript = path.join(__dirname, '..', 'lib', 'notifications-resource', 'lambda-source', 'test.sh');
   const result = spawnSync(testScript, { stdio: 'inherit' });
   expect(result.status).toBe(0);
