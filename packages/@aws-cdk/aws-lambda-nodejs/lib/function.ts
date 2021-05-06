7dcb44d6469c16fb7cb71b20e01f3f10f42aa7bb
Otavio Macedo <otaviom@amazon.com>
2021-05-06 17:04:04 +0100

    chore(lambda-nodejs): more informative error message when the handler file cannot be automatically found

--- packages/@aws-cdk/aws-lambda-nodejs/lib/function.ts
+++ packages/@aws-cdk/aws-lambda-nodejs/lib/function.ts
@@ -155,7 +155,7 @@ function findEntry(id: string, entry?: string): string {
     return jsHandlerFile;
   }
 
-  throw new Error('Cannot find entry file.');
+  throw new Error(`Cannot find handler file ${jsHandlerFile}`);
 }
 
 /**
