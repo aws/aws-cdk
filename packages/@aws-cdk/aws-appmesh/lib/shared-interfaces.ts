b1251139290b9b0deeb4b833315e69fa630340d7
AWS CDK Automation <43080478+aws-cdk-automation@users.noreply.github.com>
2021-05-11 10:34:27 +0100

    chore: forward merge 'master' into 'v2-main' (#14601)
    * chore(cloudfront): remove the use of calculateFunctionHash (#14583)
    
    `calculateFunctionHash()` was used to compute the 'refresh token' of the
    custom resource for the EdgeFunction construct.
    
    This method is private to the lambda module and is deemed to be changed.
    Instead, use the lambda function version's logical id.
    
    The logical id of the version includes computing the function hash (among
    others) and is a more reliable determinant of whether the underlying
    function version changed.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*
    
    * feat(cloudwatch): validate parameters for a metric dimensions (closes #3116) (#14365)
    
    As per #3116, the changes in this PR validate metric dimension values (length, and checking if the value is null or undefined) and throw errors if the values are not valid. I've also corrected a comment in the metric-types.ts to use the correct method name
    
    * feat(appmesh): change HealthChecks to use protocol-specific union-like classes (#14432)
    
    BREAKING CHANGE: HealthChecks require use of static factory methods
    
    fixes https://github.com/aws/aws-cdk/issues/11640
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*
    
    Co-authored-by: Niranjan Jayakar <nija@amazon.com>
    Co-authored-by: OksanaH <34384274+OksanaH@users.noreply.github.com>
    Co-authored-by: Dominic Fezzie <fezzid@amazon.com>
    Co-authored-by: Elad Ben-Israel <benisrae@amazon.com>

--- packages/@aws-cdk/aws-appmesh/lib/shared-interfaces.ts
+++ packages/@aws-cdk/aws-appmesh/lib/shared-interfaces.ts
@@ -56,69 +56,14 @@ export interface TcpTimeout {
 
 /**
  * Enum of supported AppMesh protocols
+ *
+ * @deprecated not for use outside package
  */
 export enum Protocol {
   HTTP = 'http',
   TCP = 'tcp',
   HTTP2 = 'http2',
   GRPC = 'grpc',
-}
-
-/**
- * Properties used to define healthchecks when creating virtual nodes.
- * All values have a default if only specified as {} when creating.
- * If property not set, then no healthchecks will be defined.
- */
-export interface HealthCheck {
-  /**
-   * Number of successful attempts before considering the node UP
-   *
-   * @default 2
-   */
-  readonly healthyThreshold?: number;
-
-  /**
-   * Interval in milliseconds to re-check
-   *
-   * @default 5 seconds
-   */
-  readonly interval?: cdk.Duration;
-
-  /**
-   * The path where the application expects any health-checks, this can also be the application path.
-   *
-   * @default /
-   */
-  readonly path?: string;
-
-  /**
-   * The TCP port number for the healthcheck
-   *
-   * @default - same as corresponding port mapping
-   */
-  readonly port?: number;
-
-  /**
-   * The protocol to use for the healthcheck, for convinience a const enum has been defined.
-   * Protocol.HTTP or Protocol.TCP
-   *
-   * @default - same as corresponding port mapping
-   */
-  readonly protocol?: Protocol;
-
-  /**
-   * Timeout in milli-seconds for the healthcheck to be considered a fail.
-   *
-   * @default 2 seconds
-   */
-  readonly timeout?: cdk.Duration;
-
-  /**
-   * Number of failed attempts before considering the node DOWN.
-   *
-   * @default 2
-   */
-  readonly unhealthyThreshold?: number;
 }
 
 /**
