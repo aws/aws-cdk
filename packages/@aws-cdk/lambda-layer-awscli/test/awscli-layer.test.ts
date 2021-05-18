66151b2997383be016cd4066927302fd6ecf83f6
Niranjan Jayakar <nija@amazon.com>
2021-05-18 17:01:18 +0100

    revert(lambda-layer-awscli): add awscli v2.2.5 (#14487) (#14744)
    This reverts commit 704946a9df25d73f1ebcb47f9df9c73d75d82acc.
    
    The addition of the new lambda layer increases the size of the bundled
    monocdk package. This causes our pipeline to fail during our integration
    test.
    
    Including the awscliv2 lambda layer, the artifact size for monocdk is
    86MB. It's not clear if the limitation is one from npm or a limitation
    imposed by verdaccio; the latter is used by our release pipeline to test
    unreleased npm modules.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/lambda-layer-awscli/test/awscli-layer.test.ts
+++ packages/@aws-cdk/lambda-layer-awscli/test/awscli-layer.test.ts
@@ -1,26 +1,13 @@
 import { Stack } from '@aws-cdk/core';
-import { AwsCliLayer, AwsCliV2Layer } from '../lib';
+import { AwsCliLayer } from '../lib';
 import '@aws-cdk/assert-internal/jest';
 
-test('synthesized to a v1 layer version', () => {
+test('synthesized to a layer version', () => {
   //GIVEN
   const stack = new Stack();
 
   // WHEN
   new AwsCliLayer(stack, 'MyLayer');
-
-  // THEN
-  expect(stack).toHaveResource('AWS::Lambda::LayerVersion', {
-    Description: '/opt/awscli/aws',
-  });
-});
-
-test('synthesized to a v2 layer version', () => {
-  //GIVEN
-  const stack = new Stack();
-
-  // WHEN
-  new AwsCliV2Layer(stack, 'MyLayer');
 
   // THEN
   expect(stack).toHaveResource('AWS::Lambda::LayerVersion', {
