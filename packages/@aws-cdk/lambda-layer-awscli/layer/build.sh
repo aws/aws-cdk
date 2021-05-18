8adabffc6be02c6b90c4e35e61402a42d0559cda
kirintw <cy91244@gmail.com>
2021-05-18 15:36:56 +0800

    chore(lambda-layer-awscli): add awscli v2.2.5 (#14487)
    Update awscli to version 2.2.5, fixes #13993.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/lambda-layer-awscli/layer/build.sh
+++ packages/@aws-cdk/lambda-layer-awscli/layer/build.sh
@@ -3,16 +3,21 @@ set -euo pipefail
 
 cd $(dirname $0)
 
-echo ">> Building AWS Lambda layer inside a docker image..."
+echo ">> Building AWS Lambda layer inside docker images..."
 
-TAG='aws-lambda-layer'
+V1_TAG="lambda-awscli-layer"
+V2_TAG="lambda-awscli-v2-layer"
 
-docker build -t ${TAG} .
+docker build -t "${V1_TAG}" . -f Dockerfile
+docker build -t "${V2_TAG}" . -f v2.Dockerfile
 
-echo ">> Extrating layer.zip from the build container..."
-CONTAINER=$(docker run -d ${TAG} false)
-docker cp ${CONTAINER}:/layer.zip ../lib/layer.zip
+echo ">> Extrating layer zip files from the build containers..."
+V1_CONTAINER=$(docker run -d ${V1_TAG} false)
+V2_CONTAINER=$(docker run -d ${V2_TAG} false)
+docker cp "${V1_CONTAINER}:/layer.zip" ../lib/layer.zip
+docker cp "${V2_CONTAINER}:/layer_v2.zip" ../lib/layer_v2.zip
 
-echo ">> Stopping container..."
-docker rm -f ${CONTAINER}
-echo ">> lib/layer.zip is ready"
+echo ">> Stopping containers..."
+docker rm -f "${V1_CONTAINER}"
+docker rm -f "${V2_CONTAINER}"
+echo ">> lib/layer.zip & lib/layer_v2.zip is ready"
