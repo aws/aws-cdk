435ab6d1a6ade3031aba0585abadeb04e65c73f1
Niranjan Jayakar <nija@amazon.com>
2021-05-21 15:58:11 +0100

    fix(lambda): changing reserved concurrency fails lambda version deployment (#14586)
    Function hash calculations include properties such as
    `ReservedConcurrentExecutions`, `Tags` and `DependsOn` which do not
    affect lambda versions.
    
    The change is to only use specific properties in the `Properties` section
    of `AWS::Lambda::Function` for hash calculation.
    
    We record and classify the current set of properties for the resource
    type `AWS::Lambda::Function`. Any unclassified property will error out.
    To support escape hatches, a `Function.classifyVersionProperty()` API
    is introduced.
    
    However, this change causes logical for the `Version` resource to
    change. Since the lambda service does not allow duplicate versions,
    users will see deployment failures without additional changes to the
    `Function` resouce. Hence, keep all this logic behind a feature flag that
    users can opt into.
    
    fixes #11537
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-lambda/lib/function.ts
+++ packages/@aws-cdk/aws-lambda/lib/function.ts
@@ -383,6 +383,23 @@ export class Function extends FunctionBase {
     return this._currentVersion;
   }
 
+  /** @internal */
+  public static _VER_PROPS: { [key: string]: boolean } = {};
+
+  /**
+   * Record whether specific properties in the `AWS::Lambda::Function` resource should
+   * also be associated to the Version resource.
+   * See 'currentVersion' section in the module README for more details.
+   * @param propertyName The property to classify
+   * @param locked whether the property should be associated to the version or not.
+   */
+  public static classifyVersionProperty(propertyName: string, locked: boolean) {
+    this._VER_PROPS[propertyName] = locked;
+  }
+
+  /**
+   * Import a lambda function into the CDK using its ARN
+   */
   public static fromFunctionArn(scope: Construct, id: string, functionArn: string): IFunction {
     return Function.fromFunctionAttributes(scope, id, { functionArn });
   }
