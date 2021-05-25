2a6a56a744804640083830771b229ba8502402ba
Ben Chaimberg <chaimber@amazon.com>
2021-05-24 18:31:52 -0700

    add dummy test

---
+++ packages/@aws-cdk/aws-kinesisfirehose/test/delivery-stream.test.ts
@@ -0,0 +1,38 @@
+import '@aws-cdk/assert-internal/jest';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as redshift from '@aws-cdk/aws-redshift';
+import * as cdk from '@aws-cdk/core';
+import * as kinesisfirehose from '../lib';
+
+describe('delivery stream', () => {
+  let stack: cdk.Stack;
+
+  beforeEach(() => {
+    stack = new cdk.Stack();
+  });
+
+  test('default function', () => {
+    const vpc = new ec2.Vpc(stack, 'Vpc');
+    const database = 'my_db';
+    const cluster = new redshift.Cluster(stack, 'Cluster', {
+      vpc: vpc,
+      masterUser: {
+        masterUsername: 'master',
+      },
+      defaultDatabaseName: database,
+    });
+
+    new kinesisfirehose.DeliveryStream(stack, 'Delivery Stream', {
+      destination: new kinesisfirehose.RedshiftDestination({
+        cluster: cluster,
+        user: {
+          username: 'firehose',
+        },
+        database: database,
+        tableName: 'table',
+      }),
+    });
+
+    expect(stack).toHaveResource('AWS::KinesisFirehose::DeliveryStream');
+  });
+});
