e948e7a86c79785faa23dfe18783482b33d52f94
Ben Chaimberg <chaimber@amazon.com>
2021-05-26 13:57:17 -0700

    feat(migration): add constructs migration to rewrite script

--- packages/aws-cdk-migration/lib/rewrite.ts
+++ packages/aws-cdk-migration/lib/rewrite.ts
@@ -3,7 +3,7 @@ import * as ts from 'typescript';
 /**
  * Re-writes "hyper-modular" CDK imports (most packages in `@aws-cdk/*`) to the
  * relevant "mono" CDK import path. The re-writing will only modify the imported
- * library path, presrving the existing quote style, etc...
+ * library path, preserving the existing quote style, etc...
  *
  * Syntax errors in the source file being processed may cause some import
  * statements to not be re-written.
@@ -23,21 +23,44 @@ import * as ts from 'typescript';
  */
 export function rewriteImports(sourceText: string, fileName: string = 'index.ts'): string {
   const sourceFile = ts.createSourceFile(fileName, sourceText, ts.ScriptTarget.ES2018);
+  const program = ts.createProgram([fileName], {});
+  const typeChecker = program.getTypeChecker();
 
   const replacements = new Array<{ original: ts.Node, updatedLocation: string }>();
 
   const visitor = <T extends ts.Node>(node: T): ts.VisitResult<T> => {
-    const moduleSpecifier = getModuleSpecifier(node);
-    const newTarget = moduleSpecifier && updatedLocationOf(moduleSpecifier.text);
+    const { location: moduleSpecifier, value } = getModuleSpecifier(node) ?? {};
+    if (moduleSpecifier) {
+      if (moduleSpecifier.text === '@aws-cdk/core' && value) {
+        let lookForName: string | undefined;
+        let lookForQualifier: string | undefined;
+        if (Array.isArray(value)) {
+          lookForName = value.find(({ property }) => property.text === 'Construct')?.alias.text;
+        } else if (ts.isIdentifier(value)) {
+          lookForName = 'Construct';
+          lookForQualifier = value.text;
+        }
+        lookForName ?? '' + lookForQualifier ?? '';
+      }
 
-    if (moduleSpecifier != null && newTarget != null) {
-      replacements.push({ original: moduleSpecifier, updatedLocation: newTarget });
+      const newTarget = updatedLocationOf(moduleSpecifier.text);
+      if (newTarget != null) {
+        replacements.push({ original: moduleSpecifier, updatedLocation: newTarget });
+      }
+
+      // eslint-disable-next-line no-console
+      console.log(node);
+      const symbol = typeChecker.getSymbolAtLocation(node);
+      if (symbol) {
+        // eslint-disable-next-line no-console
+        console.log(typeChecker.getFullyQualifiedName(symbol));
+      }
     }
 
     return node;
   };
 
-  sourceFile.statements.forEach(node => ts.visitNode(node, visitor));
+  ts.forEachChild(sourceFile, node => visitor(node));
 
   let updatedSourceText = sourceText;
   // Applying replacements in reverse order, so node positions remain valid.
@@ -49,43 +72,66 @@ export function rewriteImports(sourceText: string, fileName: string = 'index.ts'
   }
 
   return updatedSourceText;
+}
 
-  function getModuleSpecifier(node: ts.Node): ts.StringLiteral | undefined {
-    if (ts.isImportDeclaration(node)) {
-      // import style
-      const moduleSpecifier = node.moduleSpecifier;
-      if (ts.isStringLiteral(moduleSpecifier)) {
-        // import from 'location';
-        // import * as name from 'location';
-        return moduleSpecifier;
-      } else if (ts.isBinaryExpression(moduleSpecifier) && ts.isCallExpression(moduleSpecifier.right)) {
-        // import { Type } = require('location');
-        return getModuleSpecifier(moduleSpecifier.right);
+interface ImportName {
+  property: ts.Identifier;
+  alias: ts.Identifier;
+}
+
+interface Import {
+  location: ts.StringLiteral;
+  value?: ts.Identifier | ImportName[];
+}
+
+function getModuleSpecifier(node: ts.Node): Import | undefined {
+  if (ts.isImportDeclaration(node) && ts.isStringLiteral(node.moduleSpecifier)) {
+    // import { Type } from 'location';
+    // import * as name from 'location';
+    const location = node.moduleSpecifier;
+    if (node.importClause && node.importClause.namedBindings) {
+      if (ts.isNamespaceImport(node.importClause.namedBindings)) {
+        return {
+          location: location,
+          value: node.importClause.namedBindings.name,
+        };
+      } else if (ts.isNamedImports(node.importClause.namedBindings)) {
+        return {
+          location: location,
+          value: node.importClause.namedBindings.elements.map((element) => {
+            return { property: element.propertyName ?? element.name, alias: element.name };
+          }),
+        };
       }
-    } else if (
-      ts.isImportEqualsDeclaration(node)
-      && ts.isExternalModuleReference(node.moduleReference)
-      && ts.isStringLiteral(node.moduleReference.expression)
-    ) {
-      // import name = require('location');
-      return node.moduleReference.expression;
-    } else if (
-      (ts.isCallExpression(node))
-      && ts.isIdentifier(node.expression)
-      && node.expression.escapedText === 'require'
-      && node.arguments.length === 1
-    ) {
-      // require('location');
-      const argument = node.arguments[0];
-      if (ts.isStringLiteral(argument)) {
-        return argument;
-      }
-    } else if (ts.isExpressionStatement(node) && ts.isCallExpression(node.expression)) {
-      // require('location'); // This is an alternate AST version of it
-      return getModuleSpecifier(node.expression);
     }
-    return undefined;
+  } else if (
+    ts.isImportEqualsDeclaration(node)
+    && ts.isExternalModuleReference(node.moduleReference)
+    && ts.isStringLiteral(node.moduleReference.expression)
+  ) {
+    // import name = require('location');
+    return {
+      location: node.moduleReference.expression,
+      value: node.name,
+    };
+  } else if (
+    (ts.isCallExpression(node))
+    && ts.isIdentifier(node.expression)
+    && node.expression.escapedText === 'require'
+    && node.arguments.length === 1
+  ) {
+    // require('location');
+    const argument = node.arguments[0];
+    if (ts.isStringLiteral(argument)) {
+      return {
+        location: argument,
+      };
+    }
+  } else if (ts.isExpressionStatement(node) && ts.isCallExpression(node.expression)) {
+    // require('location'); // This is an alternate AST version of it
+    return getModuleSpecifier(node.expression);
   }
+  return undefined;
 }
 
 const EXEMPTIONS = new Set([
--- packages/aws-cdk-migration/test/rewrite.test.ts
+++ packages/aws-cdk-migration/test/rewrite.test.ts
@@ -38,7 +38,7 @@ describe(rewriteImports, () => {
   // something before
   import * as s3 from '@aws-cdk/aws-s3';
   import * as cfndiff from '@aws-cdk/cloudformation-diff';
-  import { Construct } from "@aws-cdk/core";
+  import { Stack } from "@aws-cdk/core";
   // something after
 
   console.log('Look! I did something!');`, 'subject.ts');
@@ -47,7 +47,7 @@ describe(rewriteImports, () => {
   // something before
   import * as s3 from 'aws-cdk-lib/aws-s3';
   import * as cfndiff from '@aws-cdk/cloudformation-diff';
-  import { Construct } from "aws-cdk-lib";
+  import { Stack } from "aws-cdk-lib";
   // something after
 
   console.log('Look! I did something!');`);
@@ -58,7 +58,6 @@ describe(rewriteImports, () => {
   // something before
   import s3 = require('@aws-cdk/aws-s3');
   import cfndiff = require('@aws-cdk/cloudformation-diff');
-  import { Construct } = require("@aws-cdk/core");
   // something after
 
   console.log('Look! I did something!');`, 'subject.ts');
@@ -67,7 +66,6 @@ describe(rewriteImports, () => {
   // something before
   import s3 = require('aws-cdk-lib/aws-s3');
   import cfndiff = require('@aws-cdk/cloudformation-diff');
-  import { Construct } = require("aws-cdk-lib");
   // something after
 
   console.log('Look! I did something!');`);
@@ -87,5 +85,68 @@ describe(rewriteImports, () => {
     // something after
 
     console.log('Look! I did something!');`);
+  });
+
+  test('correctly rewrites namespaced Construct by moving to constructs', () => {
+    const output = rewriteImports(`
+  // something before
+  import * as cdk from '@aws-cdk/core';
+  // something after
+
+  function f(c: cdk.Construct) {
+  }
+  console.log('Look! I did something!');`, 'subject.ts');
+
+    expect(output).toBe(`
+  // something before
+  import * as cdk from 'aws-cdk-lib';
+  import * as constructs from 'constructs';
+  // something after
+
+  function f(c: constructs.Construct) {
+  }
+  console.log('Look! I did something!');`);
+  });
+
+  test('correctly rewrites barrel Construct by moving to constructs', () => {
+    const output = rewriteImports(`
+  // something before
+  import { Construct, Stack } from '@aws-cdk/core';
+  // something after
+
+  function f(c: Construct) {
+  }
+  console.log('Look! I did something!');`, 'subject.ts');
+
+    expect(output).toBe(`
+  // something before
+  import { Stack } from 'aws-cdk-lib';
+  import { Construct } from 'constructs';
+  // something after
+
+  function f(c: Construct) {
+  }
+  console.log('Look! I did something!');`);
+  });
+
+  test('correctly rewrites aliased barrel Construct by moving to constructs', () => {
+    const output = rewriteImports(`
+  // something before
+  import { Construct as CoreConstruct, Stack } from '@aws-cdk/core';
+  // something after
+
+  function f(c: CoreConstruct) {
+  }
+  console.log('Look! I did something!');`, 'subject.ts');
+
+    expect(output).toBe(`
+  // something before
+  import { Stack } from 'aws-cdk-lib';
+  import { Construct as CoreConstruct } from 'constructs';
+  // something after
+
+  function f(c: CoreConstruct) {
+  }
+  console.log('Look! I did something!');`);
   });
 });
