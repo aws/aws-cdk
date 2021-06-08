be4718253436c3bac3ab2c94a3000d1d6dae1d87
joelzhong <luckily.zhong@gmail.com>
2021-06-08 14:59:43 +0800

    aws-codebuild changes: 1. add integ test fo notificaiton integration 2. move back enum ProjectEvent to project.ts

--- packages/@aws-cdk/aws-codebuild/lib/project.ts
+++ packages/@aws-cdk/aws-codebuild/lib/project.ts
@@ -19,7 +19,6 @@ import { CodePipelineArtifacts } from './codepipeline-artifacts';
 import { IFileSystemLocation } from './file-location';
 import { NoArtifacts } from './no-artifacts';
 import { NoSource } from './no-source';
-import { ProjectEvent } from './notification-events';
 import { runScriptLinuxBuildSpec, S3_BUCKET_ENV, S3_KEY_ENV } from './private/run-script-linux-build-spec';
 import { LoggingOptions } from './project-logs';
 import { renderReportGroupArn } from './report-group-utils';
@@ -1975,4 +1974,40 @@ export enum BuildEnvironmentVariableType {
    * An environment variable stored in AWS Secrets Manager.
    */
   SECRETS_MANAGER = 'SECRETS_MANAGER'
+}
+
+/**
+ * The list of event types for AWS Codebuild
+ * @see https://docs.aws.amazon.com/dtconsole/latest/userguide/concepts.html#events-ref-buildproject
+ */
+enum ProjectEvent {
+  /**
+   * Trigger notification when project build state failed
+   */
+  BUILD_STATE_FAILED = 'codebuild-project-build-state-failed',
+
+  /**
+   * Trigger notification when project build state succeeded
+   */
+  BUILD_STATE_SUCCEEDED = 'codebuild-project-build-state-succeeded',
+
+  /**
+   * Trigger notification when project build state in progress
+   */
+  BUILD_STATE_IN_PROGRESS = 'codebuild-project-build-state-in-progress',
+
+  /**
+   * Trigger notification when project build state stopped
+   */
+  BUILD_STATE_STOPPED = 'codebuild-project-build-state-stopped',
+
+  /**
+   * Trigger notification when project build phase failure
+   */
+  BUILD_PHASE_FAILRE = 'codebuild-project-build-phase-failure',
+
+  /**
+   * Trigger notification when project build phase success
+   */
+  BUILD_PHASE_SUCCESS = 'codebuild-project-build-phase-success',
 }
