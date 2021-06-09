e4fc515755ebba7e90ad2cf0e270334edfeaf452
Ben Chaimberg <chaimber@amazon.com>
2021-06-09 11:15:22 -0700

    feat(migration): add constructs migration to rewrite script (#14916)
    Add support to the rewrite-imports script for removing the construct compatibility layer. This change will rewrite all imports of `Construct` that come from `@aws-cdk/core` to the one that comes from `constructs`. In particular, the following forms are supported:
    - namespaced: `import * as cdk from '@aws-cdk/core'`
      - adds a new import: `import * as constructs from 'constructs'`;
    - barrel: `import { Construct } from '@aws-cdk/core'`
      - adds a new import: `import { Construct } from 'constructs'`
      - removes old import: `import { App, Construct, Stack } from '@aws-cdk/core'` => `import { App, Stack } from '@aws-cdk/core'`;
    - barrel with alias: `import { Construct as CoreConstruct } from '@aws-cdk/core'`
      - adds a new import: `import { Construct as CoreConstruct } from 'constructs'`
      - removes old import, as above
    
    As always, the unit tests are authoritative!
    
    I also removed some invalid import styles but let me know if they are actually valid and I just missed something!
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-migration/test/rewrite.test.ts
+++ packages/aws-cdk-migration/test/rewrite.test.ts
@@ -7,7 +7,7 @@ describe(rewriteImports, () => {
     import '@aws-cdk/aws-s3/hello';
     // something after
 
-    console.log('Look! I did something!');`, 'subhect.ts');
+    console.log('Look! I did something!');`, 'subject.ts');
 
     expect(output).toBe(`
     // something before
@@ -23,7 +23,7 @@ describe(rewriteImports, () => {
     require('@aws-cdk/aws-s3/hello');
     // something after
 
-    console.log('Look! I did something!');`, 'subhect.ts');
+    console.log('Look! I did something!');`, 'subject.ts');
 
     expect(output).toBe(`
     // something before
@@ -38,7 +38,7 @@ describe(rewriteImports, () => {
   // something before
   import * as s3 from '@aws-cdk/aws-s3';
   import * as cfndiff from '@aws-cdk/cloudformation-diff';
-  import { Construct } from "@aws-cdk/core";
+  import { Stack } from "@aws-cdk/core";
   // something after
 
   console.log('Look! I did something!');`, 'subject.ts');
@@ -47,7 +47,7 @@ describe(rewriteImports, () => {
   // something before
   import * as s3 from 'aws-cdk-lib/aws-s3';
   import * as cfndiff from '@aws-cdk/cloudformation-diff';
-  import { Construct } from "aws-cdk-lib";
+  import { Stack } from "aws-cdk-lib";
   // something after
 
   console.log('Look! I did something!');`);
@@ -58,7 +58,6 @@ describe(rewriteImports, () => {
   // something before
   import s3 = require('@aws-cdk/aws-s3');
   import cfndiff = require('@aws-cdk/cloudformation-diff');
-  import { Construct } = require("@aws-cdk/core");
   // something after
 
   console.log('Look! I did something!');`, 'subject.ts');
@@ -67,7 +66,6 @@ describe(rewriteImports, () => {
   // something before
   import s3 = require('aws-cdk-lib/aws-s3');
   import cfndiff = require('@aws-cdk/cloudformation-diff');
-  import { Construct } = require("aws-cdk-lib");
   // something after
 
   console.log('Look! I did something!');`);
@@ -79,7 +77,7 @@ describe(rewriteImports, () => {
     import '@aws-cdk/assert/jest';
     // something after
 
-    console.log('Look! I did something!');`, 'subhect.ts');
+    console.log('Look! I did something!');`, 'subject.ts');
 
     expect(output).toBe(`
     // something before
@@ -88,4 +86,94 @@ describe(rewriteImports, () => {
 
     console.log('Look! I did something!');`);
   });
+
+  test('correctly rewrites import namespaced Construct by moving to constructs', () => {
+    const output = rewriteImports(`
+  // something before
+  import * as cdk from '@aws-cdk/core';
+  // something after
+
+  ${constructFileBody('cdk.Construct')}
+  console.log('Look! I did something!');`, 'subject.ts');
+
+    expect(output).toBe(`
+  // something before
+  import * as cdk from 'aws-cdk-lib';
+  import * as constructs from 'constructs';
+  // something after
+
+  ${constructFileBody('constructs.Construct')}
+  console.log('Look! I did something!');`);
+  });
+
+  test('correctly rewrites require namespaced Construct by moving to constructs', () => {
+    const output = rewriteImports(`
+  // something before
+  import cdk = require('@aws-cdk/core');
+  // something after
+
+  ${constructFileBody('cdk.Construct')}
+  console.log('Look! I did something!');`, 'subject.ts');
+
+    expect(output).toBe(`
+  // something before
+  import cdk = require('aws-cdk-lib');
+  import constructs = require('constructs');
+  // something after
+
+  ${constructFileBody('constructs.Construct')}
+  console.log('Look! I did something!');`);
+  });
+
+  test('correctly rewrites barrel Construct by moving to constructs', () => {
+    const output = rewriteImports(`
+  // something before
+  import { Construct, Stack } from '@aws-cdk/core';
+  // something after
+
+  ${constructFileBody('Construct')}
+  console.log('Look! I did something!');`, 'subject.ts');
+
+    expect(output).toBe(`
+  // something before
+  import { Stack } from 'aws-cdk-lib';
+  import { Construct } from 'constructs';
+  // something after
+
+  ${constructFileBody('Construct')}
+  console.log('Look! I did something!');`);
+  });
+
+  test('correctly rewrites aliased barrel Construct by moving to constructs', () => {
+    const output = rewriteImports(`
+  // something before
+  import { App, Construct as CoreConstruct } from '@aws-cdk/core';
+  // something after
+
+  ${constructFileBody('CoreConstruct')}
+  console.log('Look! I did something!');`, 'subject.ts');
+
+    expect(output).toBe(`
+  // something before
+  import { App } from 'aws-cdk-lib';
+  import { Construct as CoreConstruct } from 'constructs';
+  // something after
+
+  ${constructFileBody('CoreConstruct')}
+  console.log('Look! I did something!');`);
+  });
 });
+
+function constructFileBody(identifier: string) {
+  return `
+interface I {
+    c: ${identifier};
+}
+class C {
+    constructor(c: ${identifier}) {}
+}
+function f(c: ${identifier}) {
+    new ${identifier}(c as ${identifier}, 'id')
+}
+`;
+}
