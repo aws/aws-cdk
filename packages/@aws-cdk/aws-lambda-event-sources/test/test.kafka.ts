bd76454b10db5ac44b605a214052dcb5022adbbf
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-06-11 08:53:15 -0600

    fix(lambda-event-sources): kafka event source expects credentials even when accessed via vpc (#14804)
    Fixes https://github.com/aws/aws-cdk/issues/14038
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-lambda-event-sources/test/test.kafka.ts
+++ packages/@aws-cdk/aws-lambda-event-sources/test/test.kafka.ts
@@ -15,6 +15,59 @@ export = {
       const fn = new TestFunction(stack, 'Fn');
       const clusterArn = 'some-arn';
       const kafkaTopic = 'some-topic';
+
+      // WHEN
+      fn.addEventSource(new sources.ManagedKafkaEventSource(
+        {
+          clusterArn,
+          topic: kafkaTopic,
+          startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+        }));
+
+      // THEN
+      expect(stack).to(haveResource('AWS::IAM::Policy', {
+        PolicyDocument: {
+          Statement: [
+            {
+              Action: [
+                'kafka:DescribeCluster',
+                'kafka:GetBootstrapBrokers',
+                'kafka:ListScramSecrets',
+              ],
+              Effect: 'Allow',
+              Resource: clusterArn,
+            },
+          ],
+          Version: '2012-10-17',
+        },
+        PolicyName: 'FnServiceRoleDefaultPolicyC6A839BF',
+        Roles: [
+          {
+            Ref: 'FnServiceRoleB9001A96',
+          },
+        ],
+      }));
+
+      expect(stack).to(haveResource('AWS::Lambda::EventSourceMapping', {
+        EventSourceArn: clusterArn,
+        FunctionName: {
+          Ref: 'Fn9270CBC0',
+        },
+        BatchSize: 100,
+        StartingPosition: 'TRIM_HORIZON',
+        Topics: [
+          kafkaTopic,
+        ],
+      }));
+
+      test.done();
+    },
+    'with secret'(test: Test) {
+      // GIVEN
+      const stack = new cdk.Stack();
+      const fn = new TestFunction(stack, 'Fn');
+      const clusterArn = 'some-arn';
+      const kafkaTopic = 'some-topic';
       const secret = new Secret(stack, 'Secret', { secretName: 'AmazonMSK_KafkaSecret' });
 
       // WHEN
@@ -84,7 +137,7 @@ export = {
     },
   },
 
-  'self managed kafka': {
+  'self-managed kafka': {
     'default'(test: Test) {
       // GIVEN
       const stack = new cdk.Stack();
@@ -153,9 +206,84 @@ export = {
 
       test.done();
     },
+    'without vpc, secret must be set'(test: Test) {
+      const stack = new cdk.Stack();
+      const fn = new TestFunction(stack, 'Fn');
+      const kafkaTopic = 'some-topic';
+      const bootstrapServers = ['kafka-broker:9092'];
+
+      test.throws(() => {
+        fn.addEventSource(new sources.SelfManagedKafkaEventSource(
+          {
+            bootstrapServers: bootstrapServers,
+            topic: kafkaTopic,
+            startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+          }));
+      }, /secret must be set/);
+
+      test.done();
+    },
 
     VPC: {
       'correctly rendered in the stack'(test: Test) {
+        // GIVEN
+        const stack = new cdk.Stack();
+        const fn = new TestFunction(stack, 'Fn');
+        const kafkaTopic = 'some-topic';
+        const bootstrapServers = ['kafka-broker:9092'];
+        const sg = SecurityGroup.fromSecurityGroupId(stack, 'SecurityGroup', 'sg-0123456789');
+        const vpc = new Vpc(stack, 'Vpc');
+
+        // WHEN
+        fn.addEventSource(new sources.SelfManagedKafkaEventSource(
+          {
+            bootstrapServers: bootstrapServers,
+            topic: kafkaTopic,
+            startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+            vpc: vpc,
+            vpcSubnets: { subnetType: SubnetType.PRIVATE },
+            securityGroup: sg,
+          }));
+
+        // THEN
+        expect(stack).notTo(haveResource('AWS::IAM::Policy'));
+        expect(stack).to(haveResource('AWS::Lambda::EventSourceMapping', {
+          FunctionName: {
+            Ref: 'Fn9270CBC0',
+          },
+          BatchSize: 100,
+          SelfManagedEventSource: {
+            Endpoints: {
+              KafkaBootstrapServers: bootstrapServers,
+            },
+          },
+          StartingPosition: 'TRIM_HORIZON',
+          Topics: [
+            kafkaTopic,
+          ],
+          SourceAccessConfigurations: [
+            {
+              Type: 'VPC_SECURITY_GROUP',
+              URI: 'sg-0123456789',
+            },
+            {
+              Type: 'VPC_SUBNET',
+              URI: {
+                Ref: 'VpcPrivateSubnet1Subnet536B997A',
+              },
+            },
+            {
+              Type: 'VPC_SUBNET',
+              URI: {
+                Ref: 'VpcPrivateSubnet2Subnet3788AAA1',
+              },
+            },
+          ],
+        }));
+
+        test.done();
+      },
+      'with secret'(test: Test) {
         // GIVEN
         const stack = new cdk.Stack();
         const fn = new TestFunction(stack, 'Fn');
