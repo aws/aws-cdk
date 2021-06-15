9dc9f97f8e69c297d547e7d54a6cb802abbc1293
Dillon <dponzo18@gmail.com>
2021-06-15 18:39:25 -0400

    feat(servicecatalog): initial implementation of the Portfolio construct (#15099)
    This is the first minimal release of an L2 construct for a service catalog portfolio. The portfolio
    construct includes methods to give access to IAM Principals as well as share across accounts. In a future
    PR, functionality to associate with a Product and add constraints will be added.
    
    Testing done
    ------------------
    * `yarn build && yarn lint`
    * `yarn rosetta:extract --strict`
    * `yarn test`
    * `yarn integ`
    
    Co-authored-by: Aidan Crank <arcrank@gmail.com>
    Co-authored-by: Jackie Wang <wanjacki@amazon.com>
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-servicecatalog/test/integ.portfolio.ts
@@ -0,0 +1,25 @@
+import * as iam from '@aws-cdk/aws-iam';
+import { App, Stack } from '@aws-cdk/core';
+import * as servicecatalog from '../lib';
+
+const app = new App();
+const stack = new Stack(app, 'integ-servicecatalog-portfolio');
+
+const role = new iam.Role(stack, 'TestRole', {
+  assumedBy: new iam.AccountRootPrincipal(),
+});
+
+const group = new iam.Group(stack, 'TestGroup');
+
+const portfolio = new servicecatalog.Portfolio(stack, 'TestPortfolio', {
+  displayName: 'TestPortfolio',
+  providerName: 'TestProvider',
+  description: 'This is our Service Catalog Portfolio',
+});
+
+portfolio.giveAccessToRole(role);
+portfolio.giveAccessToGroup(group);
+
+portfolio.shareWithAccount('123456789012');
+
+app.synth();
