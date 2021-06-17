aa2ce8e17f88418b76f1098881d0422321ef411a
Niranjan Jayakar <nija@amazon.com>
2021-06-17 15:52:37 +0100

    make sed portable

--- packages/@aws-cdk/assertions/vendor-in.sh
+++ packages/@aws-cdk/assertions/vendor-in.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+set -e
+
+porta_sed() {
+  local lookup=$1
+  local replacement=$2
+  local file=$3
+
+  # inplace replacement of sed is not portable across BSD and GNU. Use backup extension and then delete.
+  sed -i'.del' "s^${lookup}^${replacement}^g" $file
+  rm $file.del
+}
+export -f porta_sed
+
 echo "⏳ Vendoring in modules..."
 
 scriptdir=$(cd $(dirname $0) && pwd)
@@ -15,11 +28,11 @@ rsync -a ../cfnspec/spec/ $dest/cfnspec/spec
 
 # cloudformation-diff
 rsync -a --exclude '*.d.ts' --exclude '*.js' ../cloudformation-diff/lib/ $dest/cloudformation-diff
-find $dest/cloudformation-diff -name '*.ts' -exec sed -i '' "s^'@aws-cdk/cfnspec'^'../../cfnspec/lib'^g" '{}' \;
+find $dest/cloudformation-diff -name '*.ts' | xargs -n1 bash -c 'porta_sed "$@"' _ '@aws-cdk/cfnspec' '../../cfnspec/lib'
 
 # assert-internal
 rsync -a --exclude '*.d.ts' --exclude '*.js' ../assert-internal/lib/ $dest/assert
-find $dest/assert -name '*.ts' -exec sed -i '' "s^'@aws-cdk/cloudformation-diff'^'../../cloudformation-diff'^g" '{}' \;
+find $dest/assert -name '*.ts' | xargs -n1 bash -c 'porta_sed "$@"' _ '@aws-cdk/cloudformation-diff' '../../cloudformation-diff'
 
 # readme
 cat > $dest/README.md <<EOF
