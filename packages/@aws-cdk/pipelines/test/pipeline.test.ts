a09e8728eafb44e058ceac6e6f7845caa4e93e6a
Otavio Macedo <otaviomacedo@protonmail.com>
2021-06-18 13:25:38 +0100

    chore(pipelines): added abstractions for managing compliance between different categories of the same test (#15174)
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/pipelines/test/pipeline.test.ts
+++ packages/@aws-cdk/pipelines/test/pipeline.test.ts
@@ -16,6 +16,7 @@ import * as cpa from '@aws-cdk/aws-codepipeline-actions';
 import { Stack, Stage, StageProps, SecretValue, Tags } from '@aws-cdk/core';
 import { Construct } from 'constructs';
 import * as cdkp from '../lib';
+import { behavior } from './helpers/compliance';
 import { BucketStack, PIPELINE_ENV, stackTemplate, TestApp, TestGitHubNpmPipeline } from './testutil';
 
 let app: TestApp;
@@ -32,26 +33,29 @@ afterEach(() => {
   app.cleanup();
 });
 
-test('references stack template in subassembly', () => {
-  // WHEN
-  pipeline.addApplicationStage(new OneStackApp(app, 'App'));
+behavior('references stack template in subassembly', (tests) => {
+  tests.legacy(() => {
+    // WHEN
+    pipeline.addApplicationStage(new OneStackApp(app, 'App'));
 
-  // THEN
-  expect(pipelineStack).toHaveResourceLike('AWS::CodePipeline::Pipeline', {
-    Stages: arrayWith({
-      Name: 'App',
-      Actions: arrayWith(
-        objectLike({
-          Name: 'Stack.Prepare',
-          InputArtifacts: [objectLike({})],
-          Configuration: objectLike({
-            StackName: 'App-Stack',
-            TemplatePath: stringLike('*::assembly-App/*.template.json'),
+    // THEN
+    expect(pipelineStack).toHaveResourceLike('AWS::CodePipeline::Pipeline', {
+      Stages: arrayWith({
+        Name: 'App',
+        Actions: arrayWith(
+          objectLike({
+            Name: 'Stack.Prepare',
+            InputArtifacts: [objectLike({})],
+            Configuration: objectLike({
+              StackName: 'App-Stack',
+              TemplatePath: stringLike('*::assembly-App/*.template.json'),
+            }),
           }),
-        }),
-      ),
-    }),
+        ),
+      }),
+    });
   });
+
 });
 
 test('obvious error is thrown when stage contains no stacks', () => {
