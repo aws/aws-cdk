0f53cb44ae1a1949cafcfd475ed4d93fbbfe989f
Ayato Tokubi <tokubi.ayato@gmail.com>
2021-06-22 13:00:40 +0100

    fix(iam): remove incorrect normalization of principal

--- packages/@aws-cdk/aws-msk/test/cluster.test.ts
+++ packages/@aws-cdk/aws-msk/test/cluster.test.ts
@@ -218,7 +218,7 @@ describe('MSK Cluster', () => {
                   },
                 },
                 Effect: 'Allow',
-                Principal: '*',
+                Principal: { AWS: '*' },
                 Resource: '*',
               },
             ],
