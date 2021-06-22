06c03e13c2fe5734e0c60513d3192a1c60e077cb
Jonathan Goldwasser <jonathan.goldwasser@gmail.com>
2021-06-22 09:32:58 +0200

    fix(amplify): cannot remove basic auth
    Removing the `BasicAuthConfig` property from the template doesn't remove
    the basic auth. Explicitely set `EnableBasicAuth` to `false` instead.
    
    Closes #15028

--- packages/@aws-cdk/aws-amplify/test/app.test.ts
+++ packages/@aws-cdk/aws-amplify/test/app.test.ts
@@ -43,6 +43,9 @@ test('create an app connected to a GitHub repository', () => {
     },
     OauthToken: 'secret',
     Repository: 'https://github.com/aws/aws-cdk',
+    BasicAuthConfig: {
+      EnableBasicAuth: false,
+    },
   });
 
   expect(stack).toHaveResource('AWS::IAM::Role', {
