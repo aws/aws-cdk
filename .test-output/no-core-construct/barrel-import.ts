483aa25093590a5ac4981fa0e8aa637d47cf2046
Reagan Elm <reagan.elm@gmail.com>
2021-06-23 09:09:47 -0400

    chore(nodejs-lambda): update documentation

---
+++ .test-output/no-core-construct/barrel-import.ts
@@ -0,0 +1,10 @@
+import * as cdk from '@aws-cdk/core'
+import * as constructs from 'constructs';
+import * as somethingElse from 'somewhereElse';
+
+const x: constructs.Construct;
+const y: somethingElse.Things;
+function fn1(z: constructs.Construct) {}
+
+const p: constructs.IConstruct;
+function fn2(p: constructs.IConstruct) {}
