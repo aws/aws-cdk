6c535dadba52742cd08ccb903af84684a14782ce
Otavio Macedo <otaviomacedo@protonmail.com>
2021-06-23 17:02:48 +0100

    chore(pipelines): Moved pipelines unit tests over to the new test API (#15250)
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/pipelines/test/build-role-policy-statements.test.ts
+++ packages/@aws-cdk/pipelines/test/build-role-policy-statements.test.ts
@@ -4,6 +4,7 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import { PolicyStatement } from '@aws-cdk/aws-iam';
 import { Stack } from '@aws-cdk/core';
 import * as cdkp from '../lib';
+import { behavior } from './helpers/compliance';
 import { PIPELINE_ENV, TestApp, TestGitHubNpmPipeline } from './testutil';
 
 let app: TestApp;
@@ -22,33 +23,35 @@ afterEach(() => {
   app.cleanup();
 });
 
-test('Build project includes codeartifact policy statements for role', () => {
-  // WHEN
-  new TestGitHubNpmPipeline(pipelineStack, 'Cdk', {
-    sourceArtifact,
-    cloudAssemblyArtifact,
-    synthAction: cdkp.SimpleSynthAction.standardNpmSynth({
+behavior('Build project includes codeartifact policy statements for role', (suite) => {
+  suite.legacy(() => {
+    // WHEN
+    new TestGitHubNpmPipeline(pipelineStack, 'Cdk', {
       sourceArtifact,
       cloudAssemblyArtifact,
-      rolePolicyStatements: [
-        new PolicyStatement({
-          actions: ['codeartifact:*', 'sts:GetServiceBearerToken'],
-          resources: ['arn:my:arn'],
-        }),
-      ],
-    }),
-  });
-
-  // THEN
-  expect(pipelineStack).toHaveResourceLike('AWS::IAM::Policy', {
-    PolicyDocument: {
-      Statement: arrayWith(deepObjectLike({
-        Action: [
-          'codeartifact:*',
-          'sts:GetServiceBearerToken',
+      synthAction: cdkp.SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        rolePolicyStatements: [
+          new PolicyStatement({
+            actions: ['codeartifact:*', 'sts:GetServiceBearerToken'],
+            resources: ['arn:my:arn'],
+          }),
         ],
-        Resource: 'arn:my:arn',
-      })),
-    },
+      }),
+    });
+
+    // THEN
+    expect(pipelineStack).toHaveResourceLike('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: arrayWith(deepObjectLike({
+          Action: [
+            'codeartifact:*',
+            'sts:GetServiceBearerToken',
+          ],
+          Resource: 'arn:my:arn',
+        })),
+      },
+    });
   });
 });
