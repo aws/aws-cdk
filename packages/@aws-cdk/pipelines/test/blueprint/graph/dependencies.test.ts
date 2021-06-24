9d14afe4a3db0d572a53b14b7eba9bc4c0b68efa
Elad Ben-Israel <benisrae@amazon.com>
2021-06-24 16:16:12 +0300

    add a few tests

--- packages/@aws-cdk/pipelines/test/blueprint/graph/dependencies.test.ts
+++ packages/@aws-cdk/pipelines/test/blueprint/graph/dependencies.test.ts
@@ -1,6 +1,5 @@
-import { Graph, GraphNode } from '../../../lib/helpers-internal';
-
-class PlainNode extends GraphNode<any> { }
+import { GraphNode } from '../../../lib/helpers-internal';
+import { mkGraph, nodeNames } from './util';
 
 describe('with nested graphs', () => {
   const graph = mkGraph('G', G => {
@@ -51,38 +50,3 @@ describe('with nested graphs', () => {
     ]);
   });
 });
-
-function mkGraph(name: string, block: (b: GraphBuilder) => void) {
-  const graph = new Graph(name);
-  block({
-    graph(name2, deps, block2) {
-      const innerG = mkGraph(name2, block2);
-      innerG.dependOn(...deps);
-      graph.add(innerG);
-      return innerG;
-    },
-    node(name2, deps) {
-      const innerN = new PlainNode(name2);
-      innerN.dependOn(...deps ?? []);
-      graph.add(innerN);
-      return innerN;
-    },
-  });
-  return graph;
-}
-
-
-interface GraphBuilder {
-  graph(name: string, deps: GraphNode<any>[], block: (b: GraphBuilder) => void): Graph<any>;
-  node(name: string, deps?: GraphNode<any>[]): GraphNode<any>;
-}
-
-
-function nodeNames(n: GraphNode<any>): string;
-function nodeNames(ns: GraphNode<any>[]): string[];
-function nodeNames(ns: GraphNode<any>[][]): string[][];
-function nodeNames(n: any): any {
-  if (n instanceof GraphNode) { return n.id; }
-  if (Array.isArray(n)) { return n.map(nodeNames); }
-  throw new Error('oh no');
-}
