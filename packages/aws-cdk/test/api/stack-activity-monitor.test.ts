3c8342766d3a80e3196139ac1b69ed90430aa864
ryparker <ryan.parker3@outlook.com>
2021-06-28 13:59:15 -0500

    test(stack-activity-monitor.test.ts) transformed expected timestampts from CST to GMT, to match CI machine's local time

--- packages/aws-cdk/test/api/stack-activity-monitor.test.ts
+++ packages/aws-cdk/test/api/stack-activity-monitor.test.ts
@@ -23,7 +23,7 @@ test('prints 0/4 progress report, when addActivity is called with an "IN_PROGRES
     });
   });
 
-  expect(output[0].trim()).toStrictEqual(`0/4 |10:17:06 PM | ${reset('IN_PROGRESS         ')} | AWS::CloudFormation::Stack | ${reset(bold('stack1'))}`);
+  expect(output[0].trim()).toStrictEqual(`0/4 |3:17:06 AM | ${reset('IN_PROGRESS         ')} | AWS::CloudFormation::Stack | ${reset(bold('stack1'))}`);
 });
 
 test('prints 1/4 progress report, when addActivity is called with an "UPDATE_COMPLETE" ResourceStatus', () => {
@@ -47,7 +47,7 @@ test('prints 1/4 progress report, when addActivity is called with an "UPDATE_COM
     });
   });
 
-  expect(output[0].trim()).toStrictEqual(`1/4 |10:17:06 PM | ${green('UPDATE_COMPLETE     ')} | AWS::CloudFormation::Stack | ${green(bold('stack1'))}`);
+  expect(output[0].trim()).toStrictEqual(`1/4 |3:17:06 AM | ${green('UPDATE_COMPLETE     ')} | AWS::CloudFormation::Stack | ${green(bold('stack1'))}`);
 });
 
 test('prints 1/4 progress report, when addActivity is called with an "UPDATE_COMPLETE_CLEAN_IN_PROGRESS" ResourceStatus', () => {
@@ -71,7 +71,7 @@ test('prints 1/4 progress report, when addActivity is called with an "UPDATE_COM
     });
   });
 
-  expect(output[0].trim()).toStrictEqual(`1/4 |10:17:06 PM | ${green('UPDATE_COMPLETE_CLEA')} | AWS::CloudFormation::Stack | ${green(bold('stack1'))}`);
+  expect(output[0].trim()).toStrictEqual(`1/4 |3:17:06 AM | ${green('UPDATE_COMPLETE_CLEA')} | AWS::CloudFormation::Stack | ${green(bold('stack1'))}`);
 });
 
 
@@ -96,7 +96,7 @@ test('prints 1/4 progress report, when addActivity is called with an "ROLLBACK_C
     });
   });
 
-  expect(output[0].trim()).toStrictEqual(`1/4 |10:17:06 PM | ${yellow('ROLLBACK_COMPLETE_CL')} | AWS::CloudFormation::Stack | ${yellow(bold('stack1'))}`);
+  expect(output[0].trim()).toStrictEqual(`1/4 |3:17:06 AM | ${yellow('ROLLBACK_COMPLETE_CL')} | AWS::CloudFormation::Stack | ${yellow(bold('stack1'))}`);
 });
 
 test('prints 0/4 progress report, when addActivity is called with an "UPDATE_FAILED" ResourceStatus', () => {
@@ -120,7 +120,7 @@ test('prints 0/4 progress report, when addActivity is called with an "UPDATE_FAI
     });
   });
 
-  expect(output[0].trim()).toStrictEqual(`0/4 |10:17:06 PM | ${red('UPDATE_FAILED       ')} | AWS::CloudFormation::Stack | ${red(bold('stack1'))}`);
+  expect(output[0].trim()).toStrictEqual(`0/4 |3:17:06 AM | ${red('UPDATE_FAILED       ')} | AWS::CloudFormation::Stack | ${red(bold('stack1'))}`);
 });
 
 
@@ -169,9 +169,9 @@ test('does not print "Failed Resources:" list, when all deployments are successf
   });
 
   expect(output.length).toStrictEqual(3);
-  expect(output[0].trim()).toStrictEqual(`0/2 |10:17:06 PM | ${reset('IN_PROGRESS         ')} | AWS::CloudFormation::Stack | ${reset(bold('stack1'))}`);
-  expect(output[1].trim()).toStrictEqual(`1/2 |10:17:06 PM | ${green('UPDATE_COMPLETE     ')} | AWS::CloudFormation::Stack | ${green(bold('stack1'))}`);
-  expect(output[2].trim()).toStrictEqual(`2/2 |10:17:06 PM | ${green('UPDATE_COMPLETE     ')} | AWS::CloudFormation::Stack | ${green(bold('stack2'))}`);
+  expect(output[0].trim()).toStrictEqual(`0/2 |3:17:06 AM | ${reset('IN_PROGRESS         ')} | AWS::CloudFormation::Stack | ${reset(bold('stack1'))}`);
+  expect(output[1].trim()).toStrictEqual(`1/2 |3:17:06 AM | ${green('UPDATE_COMPLETE     ')} | AWS::CloudFormation::Stack | ${green(bold('stack1'))}`);
+  expect(output[2].trim()).toStrictEqual(`2/2 |3:17:06 AM | ${green('UPDATE_COMPLETE     ')} | AWS::CloudFormation::Stack | ${green(bold('stack2'))}`);
 });
 
 test('prints "Failed Resources:" list, when at least one deployment fails', () => {
@@ -208,8 +208,8 @@ test('prints "Failed Resources:" list, when at least one deployment fails', () =
   });
 
   expect(output.length).toStrictEqual(4);
-  expect(output[0].trim()).toStrictEqual(`0/2 |10:17:06 PM | ${reset('IN_PROGRESS         ')} | AWS::CloudFormation::Stack | ${reset(bold('stack1'))}`);
-  expect(output[1].trim()).toStrictEqual(`0/2 |10:17:06 PM | ${red('UPDATE_FAILED       ')} | AWS::CloudFormation::Stack | ${red(bold('stack1'))}`);
+  expect(output[0].trim()).toStrictEqual(`0/2 |3:17:06 AM | ${reset('IN_PROGRESS         ')} | AWS::CloudFormation::Stack | ${reset(bold('stack1'))}`);
+  expect(output[1].trim()).toStrictEqual(`0/2 |3:17:06 AM | ${red('UPDATE_FAILED       ')} | AWS::CloudFormation::Stack | ${red(bold('stack1'))}`);
   expect(output[2].trim()).toStrictEqual('Failed resources:');
-  expect(output[3].trim()).toStrictEqual(`10:17:06 PM | ${red('UPDATE_FAILED       ')} | AWS::CloudFormation::Stack | ${red(bold('stack1'))}`);
+  expect(output[3].trim()).toStrictEqual(`3:17:06 AM | ${red('UPDATE_FAILED       ')} | AWS::CloudFormation::Stack | ${red(bold('stack1'))}`);
 });
