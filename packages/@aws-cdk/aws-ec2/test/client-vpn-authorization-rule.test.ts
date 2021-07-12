3bc25310803528fcdea426290d3c8cdadc1dbdf3
Douglas Naphas <douglasnaphas@gmail.com>
2021-07-11 23:42:32 -0400

    Remove non-constructor setup and tests
    https://github.com/aws/aws-cdk/issues/13810
    
    I am only testing the logic in the constructor that makes sure only one
    of ClientVpnEndoint (deprecated, typo) and ClientVpnEndpoint are
    supplied. This commit removes setup and tests that I had copied over
    from another test, code that does not relate to the behavior under test.

--- packages/@aws-cdk/aws-ec2/test/client-vpn-authorization-rule.test.ts
+++ packages/@aws-cdk/aws-ec2/test/client-vpn-authorization-rule.test.ts
@@ -1,144 +1,11 @@
 import '@aws-cdk/assert-internal/jest';
-import { SamlMetadataDocument, SamlProvider } from '@aws-cdk/aws-iam';
 import { Stack } from '@aws-cdk/core';
-import * as ec2 from '../lib';
 import { Connections, IClientVpnEndpoint } from '../lib';
 import { ClientVpnAuthorizationRule } from '../lib/client-vpn-authorization-rule';
-import { ClientVpnUserBasedAuthentication } from '../lib/client-vpn-endpoint';
 
 let stack: Stack;
-let vpc: ec2.IVpc;
 beforeEach(() => {
   stack = new Stack();
-  vpc = new ec2.Vpc(stack, 'Vpc');
-});
-
-test('client vpn endpoint', () => {
-  const samlProvider = new SamlProvider(stack, 'Provider', {
-    metadataDocument: SamlMetadataDocument.fromXml('xml'),
-  });
-
-  vpc.addClientVpnEndpoint('Endpoint', {
-    cidr: '10.100.0.0/16',
-    serverCertificateArn: 'server-certificate-arn',
-    clientCertificateArn: 'client-certificate-arn',
-    clientConnectionHandler: {
-      functionArn: 'function-arn',
-      functionName: 'AWSClientVPN-function-name',
-    },
-    dnsServers: ['8.8.8.8', '8.8.4.4'],
-    userBasedAuthentication: ClientVpnUserBasedAuthentication.federated(samlProvider),
-  });
-
-  expect(stack).toHaveResource('AWS::EC2::ClientVpnEndpoint', {
-    AuthenticationOptions: [
-      {
-        MutualAuthentication: {
-          ClientRootCertificateChainArn: 'client-certificate-arn',
-        },
-        Type: 'certificate-authentication',
-      },
-      {
-        FederatedAuthentication: {
-          SAMLProviderArn: {
-            Ref: 'Provider2281708E',
-          },
-        },
-        Type: 'federated-authentication',
-      },
-    ],
-    ClientCidrBlock: '10.100.0.0/16',
-    ConnectionLogOptions: {
-      CloudwatchLogGroup: {
-        Ref: 'VpcEndpointLogGroup96A18897',
-      },
-      Enabled: true,
-    },
-    ServerCertificateArn: 'server-certificate-arn',
-    ClientConnectOptions: {
-      Enabled: true,
-      LambdaFunctionArn: 'function-arn',
-    },
-    DnsServers: ['8.8.8.8', '8.8.4.4'],
-    SecurityGroupIds: [
-      {
-        'Fn::GetAtt': ['VpcEndpointSecurityGroup7B25EFDC', 'GroupId'],
-      },
-    ],
-    VpcId: {
-      Ref: 'Vpc8378EB38',
-    },
-  });
-
-  expect(stack).toCountResources('AWS::EC2::ClientVpnTargetNetworkAssociation', 2);
-
-  expect(stack).toHaveResource('AWS::EC2::ClientVpnTargetNetworkAssociation', {
-    ClientVpnEndpointId: {
-      Ref: 'VpcEndpoint6FF034F6',
-    },
-    SubnetId: {
-      Ref: 'VpcPrivateSubnet1Subnet536B997A',
-    },
-  });
-
-  expect(stack).toHaveResource('AWS::EC2::ClientVpnTargetNetworkAssociation', {
-    ClientVpnEndpointId: {
-      Ref: 'VpcEndpoint6FF034F6',
-    },
-    SubnetId: {
-      Ref: 'VpcPrivateSubnet2Subnet3788AAA1',
-    },
-  });
-
-  expect(stack).toHaveOutput({
-    outputName: 'VpcEndpointSelfServicePortalUrl760AFE23',
-    outputValue: {
-      'Fn::Join': [
-        '',
-        [
-          'https://self-service.clientvpn.amazonaws.com/endpoints/',
-          {
-            Ref: 'VpcEndpoint6FF034F6',
-          },
-        ],
-      ],
-    },
-  });
-
-  expect(stack).toHaveResource('AWS::EC2::ClientVpnAuthorizationRule', {
-    ClientVpnEndpointId: {
-      Ref: 'VpcEndpoint6FF034F6',
-    },
-    TargetNetworkCidr: {
-      'Fn::GetAtt': ['Vpc8378EB38', 'CidrBlock'],
-    },
-    AuthorizeAllGroups: true,
-  });
-});
-
-test('client vpn endpoint with custom authorization rules', () => {
-  const endpoint = vpc.addClientVpnEndpoint('Endpoint', {
-    cidr: '10.100.0.0/16',
-    serverCertificateArn: 'server-certificate-arn',
-    clientCertificateArn: 'client-certificate-arn',
-    authorizeAllUsersToVpcCidr: false,
-  });
-
-  endpoint.addAuthorizationRule('Rule', {
-    cidr: '10.0.10.0/32',
-    groupId: 'group-id',
-  });
-
-  expect(stack).toCountResources('AWS::EC2::ClientVpnAuthorizationRule', 1);
-
-  expect(stack).toHaveResource('AWS::EC2::ClientVpnAuthorizationRule', {
-    ClientVpnEndpointId: {
-      Ref: 'VpcEndpoint6FF034F6',
-    },
-    TargetNetworkCidr: '10.0.10.0/32',
-    AccessGroupId: 'group-id',
-    AuthorizeAllGroups: false,
-  });
 });
 
 describe('ClientVpnAuthorizationRule constructor', () => {
@@ -153,7 +20,7 @@ describe('ClientVpnAuthorizationRule constructor', () => {
     };
     new ClientVpnAuthorizationRule(stack, 'Rule', { cidr: '10.0.10.0/32', clientVpnEndpoint });
     expect(stack).toCountResources('AWS::EC2::ClientVpnAuthorizationRule', 1);
-    expect(stack.node.children.length).toBe(2);
+    expect(stack.node.children.length).toBe(1);
   });
   test('either clientVpnEndoint (deprecated, typo) or clientVpnEndpoint is required', () => {
     expect(() => {
@@ -198,6 +65,6 @@ describe('ClientVpnAuthorizationRule constructor', () => {
     expect(() => {
       new ClientVpnAuthorizationRule(stack, 'InvalidRule', { cidr: '10.0.10.0/32' });
     }).toThrow();
-    expect(stack.node.children.length).toBe(1);
+    expect(stack.node.children.length).toBe(0);
   });
 });
