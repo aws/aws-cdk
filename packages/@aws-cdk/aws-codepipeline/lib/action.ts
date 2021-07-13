6efc354a9467c83ca42b29cd1c466b41d685c430
Rico Huijbers <rix0rrr@gmail.com>
2021-07-13 16:33:44 +0200

    Revert "Pull the Construct thread a bit more"
    This reverts commit 89aed647746427a9218f441fb18c7a214abda8dd.

--- packages/@aws-cdk/aws-codepipeline/lib/action.ts
+++ packages/@aws-cdk/aws-codepipeline/lib/action.ts
@@ -7,7 +7,7 @@ import { Artifact } from './artifact';
 
 // keep this import separate from other imports to reduce chance for merge conflicts with v2-main
 // eslint-disable-next-line no-duplicate-imports, import/order
-import { Construct } from 'constructs';
+import { Construct } from '@aws-cdk/core';
 
 export enum ActionCategory {
   SOURCE = 'Source',
