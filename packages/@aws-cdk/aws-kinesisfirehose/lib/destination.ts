8401fb4f7f88e11d4de608207b6e59de973f069f
Madeline Kusters <mkusters@amazon.com>
2021-07-14 10:04:26 -0700

    feat(aws-kinesisfirehose): DeliveryStream API and basic S3 destination

---
+++ packages/@aws-cdk/aws-kinesisfirehose/lib/destination.ts
@@ -0,0 +1,99 @@
+import * as logs from '@aws-cdk/aws-logs';
+import { Construct } from 'constructs';
+import { IDeliveryStream } from './delivery-stream';
+import { CfnDeliveryStream } from './kinesisfirehose.generated';
+
+/**
+ * A Kinesis Data Firehose delivery stream destination configuration.
+ */
+export interface DestinationConfig {
+  /**
+   * Schema-less properties that will be injected directly into `CfnDeliveryStream`.
+   */
+  readonly properties: object;
+}
+
+/**
+ * Options when binding a destination to a delivery stream.
+ */
+export interface DestinationBindOptions {
+  /**
+   * The delivery stream.
+   */
+  readonly deliveryStream: IDeliveryStream;
+}
+
+/**
+ * A Kinesis Data Firehose delivery stream destination.
+ */
+export interface IDestination {
+  /**
+   * Binds this destination to the Kinesis Data Firehose delivery stream.
+   *
+   * Implementers should use this method to bind resources to the stack and initialize values using the provided stream.
+   */
+  bind(scope: Construct, options: DestinationBindOptions): DestinationConfig;
+}
+
+/**
+ * Generic properties for defining a delivery stream destination.
+ */
+export interface DestinationProps {
+  /**
+   * If true, log errors when data transformation or data delivery fails.
+   *
+   * If `logGroup` is provided, this will be implicitly set to `true`.
+   *
+   * @default true - errors are logged.
+   */
+  readonly logging?: boolean;
+
+  /**
+   * The CloudWatch log group where log streams will be created to hold error logs.
+   *
+   * @default - if `logging` is set to `true`, a log group will be created for you.
+   */
+  readonly logGroup?: logs.ILogGroup;
+}
+
+/**
+ * Abstract base class that destination types can extend to benefit from methods that create generic configuration.
+ */
+export abstract class DestinationBase implements IDestination {
+  private logGroups: { [logGroupId: string]: logs.ILogGroup } = {};
+
+  constructor(protected readonly props: DestinationProps = {}) { }
+
+  abstract bind(scope: Construct, options: DestinationBindOptions): DestinationConfig;
+
+  protected createLoggingOptions(
+    scope: Construct,
+    deliveryStream: IDeliveryStream,
+    streamId: string,
+  ): CfnDeliveryStream.CloudWatchLoggingOptionsProperty | undefined {
+    return this._createLoggingOptions(scope, deliveryStream, streamId, 'LogGroup', this.props.logging, this.props.logGroup);
+  }
+
+  private _createLoggingOptions(
+    scope: Construct,
+    deliveryStream: IDeliveryStream,
+    streamId: string,
+    logGroupId: string,
+    logging?: boolean,
+    propsLogGroup?: logs.ILogGroup,
+  ): CfnDeliveryStream.CloudWatchLoggingOptionsProperty | undefined {
+    if (logging === false && propsLogGroup) {
+      throw new Error('logging cannot be set to false when logGroup is provided');
+    }
+    if (logging !== false || propsLogGroup) {
+      this.logGroups[logGroupId] = this.logGroups[logGroupId] ?? propsLogGroup ?? new logs.LogGroup(scope, logGroupId);
+      this.logGroups[logGroupId].grantWrite(deliveryStream);
+      return {
+        enabled: true,
+        logGroupName: this.logGroups[logGroupId].logGroupName,
+        logStreamName: this.logGroups[logGroupId].addStream(streamId).logStreamName,
+      };
+    }
+    return undefined;
+  }
+}
---
+++ packages/@aws-cdk/aws-kinesisfirehose/test/destination.test.ts
@@ -0,0 +1,158 @@
+import '@aws-cdk/assert-internal/jest';
+import * as iam from '@aws-cdk/aws-iam';
+import * as logs from '@aws-cdk/aws-logs';
+import * as cdk from '@aws-cdk/core';
+import { Construct } from 'constructs';
+import * as firehose from '../lib';
+
+describe('destination', () => {
+  let stack: cdk.Stack;
+  let deliveryStreamRole: iam.IRole;
+  let deliveryStream: firehose.IDeliveryStream;
+
+  const deliveryStreamRoleArn = 'arn:aws:iam::111122223333:role/DeliveryStreamRole';
+
+  beforeEach(() => {
+    stack = new cdk.Stack();
+    deliveryStreamRole = iam.Role.fromRoleArn(stack, 'Delivery Stream Role', deliveryStreamRoleArn);
+    deliveryStream = firehose.DeliveryStream.fromDeliveryStreamAttributes(stack, 'Delivery Stream', {
+      deliveryStreamName: 'mydeliverystream',
+      role: deliveryStreamRole,
+    });
+  });
+
+  describe('createLoggingOptions', () => {
+    class LoggingDestination extends firehose.DestinationBase {
+      public bind(scope: Construct, options: firehose.DestinationBindOptions): firehose.DestinationConfig {
+        return {
+          properties: {
+            testDestinationConfig: {
+              loggingConfig: this.createLoggingOptions(scope, options.deliveryStream, 'streamId'),
+            },
+          },
+        };
+      }
+    }
+
+    test('creates resources and configuration by default', () => {
+      const testDestination = new LoggingDestination();
+
+      const testDestinationConfig = testDestination.bind(stack, { deliveryStream });
+
+      expect(stack).toHaveResource('AWS::Logs::LogGroup');
+      expect(stack).toHaveResource('AWS::Logs::LogStream');
+      expect(stack.resolve(testDestinationConfig)).toStrictEqual({
+        properties: {
+          testDestinationConfig: {
+            loggingConfig: {
+              enabled: true,
+              logGroupName: {
+                Ref: 'LogGroupF5B46931',
+              },
+              logStreamName: {
+                Ref: 'LogGroupstreamId3B940622',
+              },
+            },
+          },
+        },
+      });
+    });
+    test('does not create resources or configuration if disabled', () => {
+      const testDestination = new LoggingDestination({ logging: false });
+
+      const testDestinationConfig = testDestination.bind(stack, { deliveryStream });
+
+      expect(stack.resolve(testDestinationConfig)).toStrictEqual({
+        properties: {
+          testDestinationConfig: {},
+        },
+      });
+    });
+
+    test('creates configuration if log group provided', () => {
+      const testDestination = new LoggingDestination({ logGroup: new logs.LogGroup(stack, 'Log Group') });
+
+      const testDestinationConfig = testDestination.bind(stack, { deliveryStream });
+
+      expect(stack.resolve(testDestinationConfig)).toMatchObject({
+        properties: {
+          testDestinationConfig: {
+            loggingConfig: {
+              enabled: true,
+            },
+          },
+        },
+      });
+    });
+
+    test('throws error if logging disabled but log group provided', () => {
+      const testDestination = new LoggingDestination({ logging: false, logGroup: new logs.LogGroup(stack, 'Log Group') });
+
+      expect(() => testDestination.bind(stack, { deliveryStream })).toThrowError('logging cannot be set to false when logGroup is provided');
+    });
+
+    test('uses provided log group', () => {
+      const testDestination = new LoggingDestination({ logGroup: new logs.LogGroup(stack, 'Log Group') });
+
+      const testDestinationConfig = testDestination.bind(stack, { deliveryStream });
+
+      expect(stack).toCountResources('AWS::Logs::LogGroup', 1);
+      expect(stack.resolve(testDestinationConfig)).toMatchObject({
+        properties: {
+          testDestinationConfig: {
+            loggingConfig: {
+              enabled: true,
+              logGroupName: {
+                Ref: 'LogGroupD9735569',
+              },
+              logStreamName: {
+                Ref: 'LogGroupstreamIdA1293DC2',
+              },
+            },
+          },
+        },
+      });
+    });
+
+    test('re-uses log group if called multiple times', () => {
+      const testDestination = new class extends firehose.DestinationBase {
+        public bind(scope: Construct, options: firehose.DestinationBindOptions): firehose.DestinationConfig {
+          return {
+            properties: {
+              testDestinationConfig: {
+                loggingConfig: this.createLoggingOptions(scope, options.deliveryStream, 'streamId'),
+                anotherLoggingConfig: this.createLoggingOptions(scope, options.deliveryStream, 'anotherStreamId'),
+              },
+            },
+          };
+        }
+      }();
+
+      const testDestinationConfig = testDestination.bind(stack, { deliveryStream });
+
+      expect(stack).toCountResources('AWS::Logs::LogGroup', 1);
+      expect(stack.resolve(testDestinationConfig)).toMatchObject({
+        properties: {
+          testDestinationConfig: {
+            loggingConfig: {
+              logGroupName: {
+                Ref: 'LogGroupF5B46931',
+              },
+              logStreamName: {
+                Ref: 'LogGroupstreamId3B940622',
+              },
+            },
+            anotherLoggingConfig: {
+              logGroupName: {
+                Ref: 'LogGroupF5B46931',
+              },
+              logStreamName: {
+                Ref: 'LogGroupanotherStreamIdF2754481',
+              },
+            },
+          },
+        },
+      });
+    });
+  });
+});
