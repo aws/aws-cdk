bae37a5c9a344a0a53864d248f23e5c4ad8a72b0
Elad Ben-Israel <benisrae@amazon.com>
2021-07-18 10:03:02 +0300

    fixups

--- packages/@aws-cdk/aws-codepipeline/lib/pipeline.ts
+++ packages/@aws-cdk/aws-codepipeline/lib/pipeline.ts
@@ -490,7 +490,7 @@ export class Pipeline extends PipelineBase {
     validateNamespaceName(richAction.actionProperties.variablesNamespace);
 
     // bind the Action (type h4x)
-    const actionConfig = richAction.bind(actionScope as CoreConstruct, stage, {
+    const actionConfig = richAction.bind(actionScope, stage, {
       role: actionRole ? actionRole : this.role,
       bucket: crossRegionInfo.artifactBucket,
     });
