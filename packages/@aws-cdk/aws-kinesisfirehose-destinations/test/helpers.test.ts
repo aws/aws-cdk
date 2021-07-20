a307108f0e185f053330a3b6af38a6d935fb7721
Ben Chaimberg <chaimber@amazon.com>
2021-07-20 14:38:10 -0700

    feat(kinesisfirehose): destinations can provide resources to be depended on by the delivery stream (#15676)
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-kinesisfirehose-destinations/test/helpers.test.ts
+++ packages/@aws-cdk/aws-kinesisfirehose-destinations/test/helpers.test.ts
@@ -13,12 +13,12 @@ describe('createLoggingOptions', () => {
     destinationRole = iam.Role.fromRoleArn(stack, 'Delivery Stream Role', 'arn:aws:iam::111122223333:role/DestinationRole');
   });
 
-  test('creates resources and configuration by default', () => {
-    const loggingOptions = createLoggingOptions(stack, { streamId: 'streamId', role: destinationRole });
+  it('creates resources and configuration by default', () => {
+    const loggingOutput = createLoggingOptions(stack, { streamId: 'streamId', role: destinationRole });
 
     expect(stack).toHaveResource('AWS::Logs::LogGroup');
     expect(stack).toHaveResource('AWS::Logs::LogStream');
-    expect(stack.resolve(loggingOptions)).toStrictEqual({
+    expect(stack.resolve(loggingOutput?.loggingOptions)).toStrictEqual({
       enabled: true,
       logGroupName: {
         Ref: 'LogGroupF5B46931',
@@ -28,29 +28,30 @@ describe('createLoggingOptions', () => {
       },
     });
   });
-  test('does not create resources or configuration if disabled', () => {
-    const loggingOptions = createLoggingOptions(stack, { logging: false, streamId: 'streamId', role: destinationRole });
 
-    expect(stack.resolve(loggingOptions)).toBeUndefined();
+  it('does not create resources or configuration if disabled', () => {
+    const loggingOutput = createLoggingOptions(stack, { logging: false, streamId: 'streamId', role: destinationRole });
+
+    expect(stack.resolve(loggingOutput)).toBeUndefined();
   });
 
-  test('creates configuration if log group provided', () => {
-    const loggingOptions = createLoggingOptions(stack, { logGroup: new logs.LogGroup(stack, 'Log Group'), streamId: 'streamId', role: destinationRole });
+  it('creates configuration if log group provided', () => {
+    const loggingOutput = createLoggingOptions(stack, { logGroup: new logs.LogGroup(stack, 'Log Group'), streamId: 'streamId', role: destinationRole });
 
-    expect(stack.resolve(loggingOptions)).toMatchObject({
+    expect(stack.resolve(loggingOutput?.loggingOptions)).toMatchObject({
       enabled: true,
     });
   });
 
-  test('throws error if logging disabled but log group provided', () => {
+  it('throws error if logging disabled but log group provided', () => {
     expect(() => createLoggingOptions(stack, { logging: false, logGroup: new logs.LogGroup(stack, 'Log Group'), streamId: 'streamId', role: destinationRole })).toThrowError('logging cannot be set to false when logGroup is provided');
   });
 
-  test('uses provided log group', () => {
-    const loggingOptions = createLoggingOptions(stack, { logGroup: new logs.LogGroup(stack, 'Log Group'), streamId: 'streamId', role: destinationRole });
+  it('uses provided log group', () => {
+    const loggingOutput = createLoggingOptions(stack, { logGroup: new logs.LogGroup(stack, 'Log Group'), streamId: 'streamId', role: destinationRole });
 
     expect(stack).toCountResources('AWS::Logs::LogGroup', 1);
-    expect(stack.resolve(loggingOptions)).toMatchObject({
+    expect(stack.resolve(loggingOutput?.loggingOptions)).toMatchObject({
       enabled: true,
       logGroupName: {
         Ref: 'LogGroupD9735569',
@@ -61,12 +62,12 @@ describe('createLoggingOptions', () => {
     });
   });
 
-  test('re-uses log group if called multiple times', () => {
-    const loggingOptions = createLoggingOptions(stack, { streamId: 'streamId', role: destinationRole });
-    const anotherLoggingOptions = createLoggingOptions(stack, { streamId: 'anotherStreamId', role: destinationRole });
+  it('re-uses log group if called multiple times', () => {
+    const loggingOutput = createLoggingOptions(stack, { streamId: 'streamId', role: destinationRole });
+    const anotherLoggingOutput = createLoggingOptions(stack, { streamId: 'anotherStreamId', role: destinationRole });
 
     expect(stack).toCountResources('AWS::Logs::LogGroup', 1);
-    expect(stack.resolve(loggingOptions)).toMatchObject({
+    expect(stack.resolve(loggingOutput?.loggingOptions)).toMatchObject({
       logGroupName: {
         Ref: 'LogGroupF5B46931',
       },
@@ -74,7 +75,7 @@ describe('createLoggingOptions', () => {
         Ref: 'LogGroupstreamId3B940622',
       },
     });
-    expect(stack.resolve(anotherLoggingOptions)).toMatchObject({
+    expect(stack.resolve(anotherLoggingOutput?.loggingOptions)).toMatchObject({
       logGroupName: {
         Ref: 'LogGroupF5B46931',
       },
@@ -82,5 +83,34 @@ describe('createLoggingOptions', () => {
         Ref: 'LogGroupanotherStreamIdF2754481',
       },
     });
+  });
+
+  it('grants log group write permissions to destination role', () => {
+    const logGroup = new logs.LogGroup(stack, 'Log Group');
+
+    createLoggingOptions(stack, { logGroup, streamId: 'streamId', role: destinationRole });
+
+    expect(stack).toHaveResourceLike('AWS::IAM::Policy', {
+      Roles: [stack.resolve(destinationRole.roleName)],
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: [
+              'logs:CreateLogStream',
+              'logs:PutLogEvents',
+            ],
+            Effect: 'Allow',
+            Resource: stack.resolve(logGroup.logGroupArn),
+          },
+        ],
+      },
+    });
+  });
+
+  it('returns log group grant as dependable', () => {
+    const loggingOutput = createLoggingOptions(stack, { logging: true, streamId: 'streamId', role: destinationRole });
+
+    expect(loggingOutput?.dependables?.length).toBe(1);
+    expect(loggingOutput?.dependables[0]).toBeInstanceOf(iam.Grant);
   });
 });
