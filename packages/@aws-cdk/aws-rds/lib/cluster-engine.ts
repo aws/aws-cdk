2a7d4b774afb4d9d1c09221aefae363470bf4bb2
Jordie Bodlay <jordie@jordie.org>
2021-07-22 02:10:01 +1000

    chore(rds): add AuroraPostgresEngineVersion 12.6 (#15686)
    Version 12.6 of the Aurora PostgreSQL cluster engine has been released as per https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/AuroraPostgreSQL.Updates.20180305.html
    
    <img width="1238" alt="Screen Shot 2021-07-21 at 6 39 16 pm" src="https://user-images.githubusercontent.com/712360/126459465-4c47f792-64ed-4963-aace-c21b72de61cd.png">
    
    Can also be seen via aws CLI:
    
    ![Screen Shot 2021-07-21 at 6 38 17 pm](https://user-images.githubusercontent.com/712360/126459517-176804ac-5d72-47e6-8de7-e669e359650c.png)
    
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-rds/lib/cluster-engine.ts
+++ packages/@aws-cdk/aws-rds/lib/cluster-engine.ts
@@ -459,6 +459,8 @@ export class AuroraPostgresEngineVersion {
   public static readonly VER_11_9 = AuroraPostgresEngineVersion.of('11.9', '11', { s3Import: true, s3Export: true });
   /** Version "12.4". */
   public static readonly VER_12_4 = AuroraPostgresEngineVersion.of('12.4', '12', { s3Import: true, s3Export: true });
+  /** Version "12.6". */
+  public static readonly VER_12_6 = AuroraPostgresEngineVersion.of('12.6', '12', { s3Import: true, s3Export: true });
 
   /**
    * Create a new AuroraPostgresEngineVersion with an arbitrary version.
