689cd8f0f8b5e1a58128059143443470311cea4c
Ben Chaimberg <chaimber@amazon.com>
2021-07-22 13:32:25 -0700

    feat(core): lazy mappings will only synthesize if keys are unresolved (#15617)
    This feature adds new static methods to the CfnMapping construct that
    allow the creation of "lazy" mappings. A lazy mapping will only create
    a Mappings section in the synthesized CFN template if some "find"
    operation on the mapping was not able to return a value since one or
    more of the lookup keys were unresolved.
    
    Usage:
    ```ts
    // Register the mapping as a lazy mapping.
    CfnMapping.registerLazyMap('UNIQUEMAPPINGID', {
      TopLevelKey: {
        SecondLevelKey: 'value',
      },
    });
    
    // Later, find a value from the mapping.  Since the keys are both
    // resolved, this returns a resolved value and does not create a
    // CfnMapping.
    CfnMapping.findInLazyMap(scope, 'UNIQUEMAPPINGID', 'TopLevelKey', 'SecondLevelKey');
    // > 'value'
    
    // If we try to find a value from the mapping using unresolved keys, a
    // CfnMapping is created and a Fn::FindInMap is returned.
    CfnMapping.findInLazyMap(scope, 'UNIQUEMAPPINGID', 'TopLevelKey', Aws.REGION);
    // > { Fn::FindInMap: [ 'UNIQUEMAPPINGID', 'TopLevelKey', { Ref: 'AWS::Region' } ] }
    ```
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/core/test/mappings.test.ts
+++ packages/@aws-cdk/core/test/mappings.test.ts
@@ -1,5 +1,7 @@
+import { ArtifactMetadataEntryType } from '@aws-cdk/cloud-assembly-schema';
+import { CloudAssembly } from '@aws-cdk/cx-api';
 import { nodeunitShim, Test } from 'nodeunit-shim';
-import { Aws, CfnMapping, CfnResource, Fn, Stack } from '../lib';
+import { App, Aws, CfnMapping, CfnResource, Fn, Stack } from '../lib';
 import { toCloudFormation } from './util';
 
 nodeunitShim({
@@ -78,6 +80,13 @@ nodeunitShim({
     const expected = { 'Fn::FindInMap': ['mapping', 'instanceCount', { Ref: 'AWS::Region' }] };
     test.deepEqual(stack.resolve(v1), expected);
     test.deepEqual(stack.resolve(v2), expected);
+    test.deepEqual(toCloudFormation(stack).Mappings, {
+      mapping: {
+        instanceCount: {
+          'us-east-1': 12,
+        },
+      },
+    });
     test.done();
   },
 
@@ -99,6 +108,13 @@ nodeunitShim({
     test.deepEqual(stack.resolve(v), {
       'Fn::FindInMap': ['mapping', { Ref: 'AWS::Region' }, 'size'],
     });
+    test.deepEqual(toCloudFormation(stack).Mappings, {
+      mapping: {
+        'us-east-1': {
+          size: 12,
+        },
+      },
+    });
     test.done();
   },
 
@@ -119,6 +135,135 @@ nodeunitShim({
     // THEN
     test.throws(() => mapping.findInMap('not-found', Aws.REGION), /Mapping doesn't contain top-level key 'not-found'/);
     test.deepEqual(stack.resolve(v), { 'Fn::FindInMap': ['mapping', 'size', { Ref: 'AWS::Region' }] });
+    test.deepEqual(toCloudFormation(stack).Mappings, {
+      mapping: {
+        size: {
+          'us-east-1': 12,
+        },
+      },
+    });
     test.done();
   },
 });
+
+describe('lazy mapping', () => {
+  let stack: Stack;
+  let mapping: CfnMapping;
+  const backing = {
+    TopLevelKey1: {
+      SecondLevelKey1: [1, 2, 3],
+      SecondLevelKey2: { Hello: 'World' },
+    },
+  };
+
+  beforeEach(() => {
+    stack = new Stack();
+    mapping = new CfnMapping(stack, 'Lazy Mapping', {
+      mapping: backing,
+      lazy: true,
+    });
+  });
+
+  it('does not create CfnMapping if findInMap keys can be resolved', () => {
+    const retrievedValue = mapping.findInMap('TopLevelKey1', 'SecondLevelKey1');
+
+    expect(stack.resolve(retrievedValue)).toStrictEqual([1, 2, 3]);
+    expect(toCloudFormation(stack)).toStrictEqual({});
+  });
+
+  it('does not create CfnMapping if findInMap is not called', () => {
+    expect(toCloudFormation(stack)).toStrictEqual({});
+  });
+
+  it('creates CfnMapping if top level key cannot be resolved', () => {
+    const retrievedValue = mapping.findInMap(Aws.REGION, 'SecondLevelKey1');
+
+    expect(stack.resolve(retrievedValue)).toStrictEqual({ 'Fn::FindInMap': ['LazyMapping', { Ref: 'AWS::Region' }, 'SecondLevelKey1'] });
+    expect(toCloudFormation(stack)).toStrictEqual({
+      Mappings: {
+        LazyMapping: backing,
+      },
+    });
+  });
+
+  it('creates CfnMapping if second level key cannot be resolved', () => {
+    const retrievedValue = mapping.findInMap('TopLevelKey1', Aws.REGION);
+
+    expect(stack.resolve(retrievedValue)).toStrictEqual({ 'Fn::FindInMap': ['LazyMapping', 'TopLevelKey1', { Ref: 'AWS::Region' }] });
+    expect(toCloudFormation(stack)).toStrictEqual({
+      Mappings: {
+        LazyMapping: backing,
+      },
+    });
+  });
+
+  it('throws if keys can be resolved but are not found in backing', () => {
+    expect(() => mapping.findInMap('NonExistentKey', 'SecondLevelKey1'))
+      .toThrowError(/Mapping doesn't contain top-level key .*/);
+    expect(() => mapping.findInMap('TopLevelKey1', 'NonExistentKey'))
+      .toThrowError(/Mapping doesn't contain second-level key .*/);
+  });
+});
+
+describe('eager by default', () => {
+  const backing = {
+    TopLevelKey1: {
+      SecondLevelKey1: [1, 2, 3],
+      SecondLevelKey2: { Hello: 'World' },
+    },
+  };
+
+  let app: App;
+  let stack: Stack;
+  let mapping: CfnMapping;
+
+  beforeEach(() => {
+    app = new App();
+    stack = new Stack(app, 'Stack');
+    mapping = new CfnMapping(stack, 'Lazy Mapping', {
+      mapping: backing,
+    });
+  });
+
+  it('emits warning if no findInMap called', () => {
+    const assembly = app.synth();
+
+    expect(getInfoAnnotations(assembly)).toStrictEqual([{
+      path: '/Stack/Lazy Mapping',
+      message: 'Consider making this CfnMapping a lazy mapping by providing `lazy: true`: either no findInMap was called or every findInMap could be immediately resolved without using Fn::FindInMap',
+    }]);
+  });
+
+  it('emits warning if every findInMap resolves immediately', () => {
+    mapping.findInMap('TopLevelKey1', 'SecondLevelKey1');
+
+    const assembly = app.synth();
+
+    expect(getInfoAnnotations(assembly)).toStrictEqual([{
+      path: '/Stack/Lazy Mapping',
+      message: 'Consider making this CfnMapping a lazy mapping by providing `lazy: true`: either no findInMap was called or every findInMap could be immediately resolved without using Fn::FindInMap',
+    }]);
+  });
+
+  it('does not emit warning if a findInMap could not resolve immediately', () => {
+    mapping.findInMap('TopLevelKey1', Aws.REGION);
+
+    const assembly = app.synth();
+
+    expect(getInfoAnnotations(assembly)).toStrictEqual([]);
+  });
+});
+
+function getInfoAnnotations(casm: CloudAssembly) {
+  const result = new Array<{ path: string, message: string }>();
+  for (const stack of Object.values(casm.manifest.artifacts ?? {})) {
+    for (const [path, md] of Object.entries(stack.metadata ?? {})) {
+      for (const x of md) {
+        if (x.type === ArtifactMetadataEntryType.INFO) {
+          result.push({ path, message: x.data as string });
+        }
+      }
+    }
+  }
+  return result;
+}
