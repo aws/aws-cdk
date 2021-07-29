a6ca92f88028f87ceeca74b0d5eb084d34efaa6d
James Fleming <flemjame@amazon.com>
2021-07-29 14:08:04 +0000

    Only verify the layers property, and add some helper test functions

--- packages/@aws-cdk/aws-lambda/test/lambda-insights.test.ts
+++ packages/@aws-cdk/aws-lambda/test/lambda-insights.test.ts
@@ -1,7 +1,31 @@
 import '@aws-cdk/assert-internal/jest';
-import { ResourcePart } from '@aws-cdk/assert-internal';
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '../lib';
+
+/**
+ * Boilerplate code to create a Function with a given insights version
+ */
+function functionWithInsightsVersion(stack: cdk.Stack, id: string, insightsVersion: lambda.LambdaInsightsVersion): lambda.IFunction {
+  return new lambda.Function(stack, id, {
+    code: new lambda.InlineCode('foo'),
+    handler: 'index.handler',
+    runtime: lambda.Runtime.NODEJS_10_X,
+    insightsVersion,
+  });
+}
+
+/**
+ * Check if the function's Role has the Lambda Insights IAM policy
+ */
+function verifyRoleHasCorrectPolicies(stack: cdk.Stack): any {
+  return expect(stack).toHaveResource('AWS::IAM::Role', {
+    ManagedPolicyArns:
+      [
+        { 'Fn::Join': ['', ['arn:', { Ref: 'AWS::Partition' }, ':iam::aws:policy/service-role/AWSLambdaBasicExecutionRole']] },
+        { 'Fn::Join': ['', ['arn:', { Ref: 'AWS::Partition' }, ':iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy']] },
+      ],
+  });
+}
 
 // This test code has app.synth() because the lambda-insights code has functions that are only run on synthesis
 describe('lambda-insights', () => {
@@ -9,32 +33,13 @@ describe('lambda-insights', () => {
     const app = new cdk.App();
     const stack = new cdk.Stack(app, 'Stack', {});
     const layerArn = 'arn:aws:lambda:us-east-1:580247275435:layer:LambdaInsightsExtension:14';
-    new lambda.Function(stack, 'MyLambda', {
-      code: new lambda.InlineCode('foo'),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_10_X,
-      insightsVersion: lambda.LambdaInsightsVersion.fromInsightVersionArn(layerArn),
-    });
+    functionWithInsightsVersion(stack, 'MyLambda', lambda.LambdaInsightsVersion.fromInsightVersionArn(layerArn));
 
-    expect(stack).toHaveResource('AWS::IAM::Role', {
-      ManagedPolicyArns:
-        [
-          { 'Fn::Join': ['', ['arn:', { Ref: 'AWS::Partition' }, ':iam::aws:policy/service-role/AWSLambdaBasicExecutionRole']] },
-          { 'Fn::Join': ['', ['arn:', { Ref: 'AWS::Partition' }, ':iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy']] },
-        ],
-    });
+    verifyRoleHasCorrectPolicies(stack);
 
     expect(stack).toHaveResource('AWS::Lambda::Function', {
-      Properties:
-      {
-        Code: { ZipFile: 'foo' },
-        Handler: 'index.handler',
-        Role: { 'Fn::GetAtt': ['MyLambdaServiceRole4539ECB6', 'Arn'] },
-        Runtime: 'nodejs10.x',
-        Layers: [layerArn],
-      },
-      DependsOn: ['MyLambdaServiceRole4539ECB6'],
-    }, ResourcePart.CompleteDefinition);
+      Layers: [layerArn],
+    });
 
     // On synthesis it should not throw an error
     expect(() => app.synth()).not.toThrow();
@@ -45,32 +50,13 @@ describe('lambda-insights', () => {
     const stack = new cdk.Stack(app, 'Stack', {
       env: { account: '123456789012', region: 'us-east-1' },
     });
-    new lambda.Function(stack, 'MyLambda', {
-      code: new lambda.InlineCode('foo'),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_10_X,
-      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_54_0,
-    });
+    functionWithInsightsVersion(stack, 'MyLambda', lambda.LambdaInsightsVersion.VERSION_1_0_54_0);
 
-    expect(stack).toHaveResource('AWS::IAM::Role', {
-      ManagedPolicyArns:
-        [
-          { 'Fn::Join': ['', ['arn:', { Ref: 'AWS::Partition' }, ':iam::aws:policy/service-role/AWSLambdaBasicExecutionRole']] },
-          { 'Fn::Join': ['', ['arn:', { Ref: 'AWS::Partition' }, ':iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy']] },
-        ],
-    });
+    verifyRoleHasCorrectPolicies(stack);
 
     expect(stack).toHaveResource('AWS::Lambda::Function', {
-      Properties:
-      {
-        Code: { ZipFile: 'foo' },
-        Handler: 'index.handler',
-        Role: { 'Fn::GetAtt': ['MyLambdaServiceRole4539ECB6', 'Arn'] },
-        Runtime: 'nodejs10.x',
-        Layers: ['arn:aws:lambda:us-east-1:580247275435:layer:LambdaInsightsExtension:2'],
-      },
-      DependsOn: ['MyLambdaServiceRole4539ECB6'],
-    }, ResourcePart.CompleteDefinition);
+      Layers: ['arn:aws:lambda:us-east-1:580247275435:layer:LambdaInsightsExtension:2'],
+    });
 
     // On synthesis it should not throw an error
     expect(() => app.synth()).not.toThrow();
@@ -83,12 +69,7 @@ describe('lambda-insights', () => {
     });
 
     // AF-SOUTH-1 exists, 1.0.54.0 exists, but 1.0.54.0 isn't supported in AF-SOUTH-1
-    new lambda.Function(stack, 'BadVersion', {
-      code: new lambda.InlineCode('foo'),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_10_X,
-      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_54_0,
-    });
+    functionWithInsightsVersion(stack, 'BadVersion', lambda.LambdaInsightsVersion.VERSION_1_0_54_0);
 
     // On synthesis it should throw an error
     expect(() => app.synth()).toThrow();
@@ -98,33 +79,20 @@ describe('lambda-insights', () => {
     const app = new cdk.App();
     const stack = new cdk.Stack(app, 'Stack', {});
 
-    new lambda.Function(stack, 'MyLambda', {
-      code: new lambda.InlineCode('foo'),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_10_X,
-      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_54_0,
-    });
+    functionWithInsightsVersion(stack, 'MyLambda', lambda.LambdaInsightsVersion.VERSION_1_0_54_0);
 
     // Should be looking up a mapping
     expect(stack).toHaveResource('AWS::Lambda::Function', {
-      Properties:
-      {
-        Code: { ZipFile: 'foo' },
-        Handler: 'index.handler',
-        Role: { 'Fn::GetAtt': ['MyLambdaServiceRole4539ECB6', 'Arn'] },
-        Runtime: 'nodejs10.x',
-        Layers: [{
-          'Fn::FindInMap': [
-            'LambdaInsightsVersions10540',
-            {
-              Ref: 'AWS::Region',
-            },
-            'arn',
-          ],
-        }],
-      },
-      DependsOn: ['MyLambdaServiceRole4539ECB6'],
-    }, ResourcePart.CompleteDefinition);
+      Layers: [{
+        'Fn::FindInMap': [
+          'LambdaInsightsVersions10540',
+          {
+            Ref: 'AWS::Region',
+          },
+          'arn',
+        ],
+      }],
+    });
 
     // On synthesis it should not throw an error
     expect(() => app.synth()).not.toThrow();
@@ -135,59 +103,34 @@ describe('lambda-insights', () => {
     const app = new cdk.App();
     const stack = new cdk.Stack(app, 'Stack', {});
 
-    new lambda.Function(stack, 'MyLambda1', {
-      code: new lambda.InlineCode('foo'),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_10_X,
-      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_54_0,
-    });
+    functionWithInsightsVersion(stack, 'MyLambda1', lambda.LambdaInsightsVersion.VERSION_1_0_54_0);
+    functionWithInsightsVersion(stack, 'MyLambda2', lambda.LambdaInsightsVersion.VERSION_1_0_54_0);
 
-    new lambda.Function(stack, 'MyLambda2', {
-      code: new lambda.InlineCode('foo'),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_10_X,
-      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_54_0,
+    expect(stack).toHaveResource('AWS::Lambda::Function', {
+      Role: { 'Fn::GetAtt': ['MyLambda1ServiceRole69A7E1EA', 'Arn'] },
+      Layers: [{
+        'Fn::FindInMap': [
+          'LambdaInsightsVersions10540',
+          {
+            Ref: 'AWS::Region',
+          },
+          'arn',
+        ],
+      }],
     });
 
     expect(stack).toHaveResource('AWS::Lambda::Function', {
-      Properties:
-      {
-        Code: { ZipFile: 'foo' },
-        Handler: 'index.handler',
-        Role: { 'Fn::GetAtt': ['MyLambda1ServiceRole69A7E1EA', 'Arn'] },
-        Runtime: 'nodejs10.x',
-        Layers: [{
-          'Fn::FindInMap': [
-            'LambdaInsightsVersions10540',
-            {
-              Ref: 'AWS::Region',
-            },
-            'arn',
-          ],
-        }],
-      },
-      DependsOn: ['MyLambda1ServiceRole69A7E1EA'],
-    }, ResourcePart.CompleteDefinition);
-
-    expect(stack).toHaveResource('AWS::Lambda::Function', {
-      Properties:
-      {
-        Code: { ZipFile: 'foo' },
-        Handler: 'index.handler',
-        Role: { 'Fn::GetAtt': ['MyLambda2ServiceRoleD09B370C', 'Arn'] },
-        Runtime: 'nodejs10.x',
-        Layers: [{
-          'Fn::FindInMap': [
-            'LambdaInsightsVersions10540',
-            {
-              Ref: 'AWS::Region',
-            },
-            'arn',
-          ],
-        }],
-      },
-      DependsOn: ['MyLambda2ServiceRoleD09B370C'],
-    }, ResourcePart.CompleteDefinition);
+      Role: { 'Fn::GetAtt': ['MyLambda2ServiceRoleD09B370C', 'Arn'] },
+      Layers: [{
+        'Fn::FindInMap': [
+          'LambdaInsightsVersions10540',
+          {
+            Ref: 'AWS::Region',
+          },
+          'arn',
+        ],
+      }],
+    });
 
     // On synthesis it should not throw an error
     expect(() => app.synth()).not.toThrow();
