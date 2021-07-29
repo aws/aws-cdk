81208a0cea1bbff8549977ea46dec1cd73442e5c
Steven Guggenheimer <sguggenh@amazon.com>
2021-07-29 08:54:57 -0500

    Change getOriginShield method name to renderOriginShield
    Co-authored-by: Nick Lynch <nlynch@amazon.com>

--- packages/@aws-cdk/aws-cloudfront/lib/origin.ts
+++ packages/@aws-cdk/aws-cloudfront/lib/origin.ts
@@ -188,7 +188,7 @@ export abstract class OriginBase implements IOrigin {
   /**
    * Takes origin shield region and converts to CfnDistribution.OriginShieldProperty
    */
-  private getOriginShield(originShieldRegion:string = ''): CfnDistribution.OriginShieldProperty | undefined {
+  private renderOriginShield(originShieldRegion?: string): CfnDistribution.OriginShieldProperty | undefined {
     if (originShieldRegion) {
       if (!(/^\w\w\-[a-z]*\-\d$/).test(originShieldRegion)) throw new Error(`originShieldRegion ${originShieldRegion} is not a valid AWS region.`);
       return {
