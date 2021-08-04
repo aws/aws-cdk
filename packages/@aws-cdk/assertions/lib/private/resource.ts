04a068f5f2eec86271543d96f089783b5de698b3
Niranjan Jayakar <nija@amazon.com>
2021-08-04 11:35:59 +0100

    refactor step 2

--- packages/@aws-cdk/assertions/lib/private/resource.ts
+++ packages/@aws-cdk/assertions/lib/private/resource.ts
@@ -4,75 +4,84 @@ import { StackInspector } from '../vendored/assert';
 
 export function findResources(inspector: StackInspector, type: string, props: any = {}): { [key: string]: any }[] {
   const matcher = Matcher.isMatcher(props) ? props : Match.objectLike(props);
-  let results: { [key: string]: any }[] = [];
 
-  eachEntryInSection(
-    inspector.value.Resources,
-    (resource) => resource.Type === type,
+  const section: { [key: string] : any } = inspector.value.Resources ?? {};
+  const filteredSection = Object.entries(section)
+    .filter(([_, v]) => v.Type === type)
+    .reduce((agg, [k, v]) => { return { ...agg, [k]: v }; }, {});
+  const result = matchUp(filteredSection, matcher);
 
-    (resource) => {
-      const result = matcher.test(resource);
-      if (!result.hasFailed()) {
-        results.push(resource);
-      }
-    },
-  );
+  if (!result.match) {
+    return [];
+  }
 
-  return results;
+  return result.matches;
 }
 
 export function hasResource(inspector: StackInspector, type: string, props: any): string | void {
   const matcher = Matcher.isMatcher(props) ? props : Match.objectLike(props);
-  let closestResult: MatchResult | undefined = undefined;
-  let count: number = 0;
 
-  let match = false;
-  eachEntryInSection(
-    inspector.value.Resources,
-    (resource) => resource.Type === type,
+  const section: { [key: string] : any } = inspector.value.Resources ?? {};
+  const filteredSection = Object.entries(section)
+    .filter(([_, v]) => v.Type === type)
+    .reduce((agg, [k, v]) => { return { ...agg, [k]: v }; }, {});
+  const result = matchUp(filteredSection, matcher);
 
-    (resource) => {
-      if (match) { return; }
-      count++;
-      const result = matcher.test(resource);
-      if (!result.hasFailed()) {
-        match = true;
-      }
-      if (closestResult === undefined || closestResult.failCount > result.failCount) {
-        closestResult = result;
-      }
-    },
-  );
-
-  if (match) {
+  if (result.match) {
     return;
   }
 
-  if (closestResult === undefined) {
+  if (result.closestResult === undefined) {
     return `No resource with type ${type} found`;
   }
 
   return [
-    `${count} resources with type ${type} found, but none match as expected.`,
+    `${result.count} resources with type ${type} found, but none match as expected.`,
     formatMessage(
-      closestResult,
-      // typescript thinks 'closestResult' is never at this point but is not.
-      // Not clear why. Force cast.
-      (closestResult as unknown as MatchResult).target,
+      result.closestResult,
+      result.closestResult.target,
     ),
   ].join('\n');
 }
 
+type MatchSuccess = { match: true, matches: any[] };
+type MatchFailure = { match: false, closestResult?: MatchResult, count: number };
+
+function matchUp(section: any, matcher: Matcher): MatchSuccess | MatchFailure {
+  let closestResult: MatchResult | undefined = undefined;
+  let matching: any[] = [];
+  let count = 0;
+
+  eachEntryInSection(
+    section,
+
+    (entry) => {
+      const result = matcher.test(entry);
+      if (!result.hasFailed()) {
+        matching.push(entry);
+      } else {
+        count++;
+        if (closestResult === undefined || closestResult.failCount > result.failCount) {
+          closestResult = result;
+        }
+      }
+    },
+  );
+
+  if (matching.length > 0) {
+    return { match: true, matches: matching };
+  } else {
+    return { match: false, closestResult, count };
+  }
+}
+
 function eachEntryInSection(
   section: any,
-  predicate: undefined | ((entry: any) => boolean),
-  cb: (resource: {[key: string]: any}) => void): void {
+  cb: (entry: {[key: string]: any}) => void): void {
 
   for (const logicalId of Object.keys(section ?? {})) {
     const resource: { [key: string]: any } = section[logicalId];
-    if (predicate === undefined || predicate(resource)) {
-      cb(resource);
-    }
+    cb(resource);
   }
 }
 
