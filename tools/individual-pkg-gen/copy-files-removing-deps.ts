dba34213eadbee0da72712586a6bd39c78138bb2
Nick Lynch <nlynch@amazon.com>
2021-08-04 17:09:49 +0100

    chore: rewrite jsii targets for experimental packages during transformation (#15890)
    As part of #15722, the decision was made to change the package.json files for
    experimental packages to add 'alpha' identifiers. This ends up causing issues
    for our ubergen-ed packages (i.e., aws-cdk-lib), which then try to include L1s
    for these packages under the 'alpha' namespaces. Instead, switch (back) to
    adding the alpha identifiers as part of the package transformation step.
    
    Reverted the change to the pkglint rules, and re-ran `yarn pkglint` to update
    the package.json files.
    
    I also did some light refactoring, since the method was a bit hard to read.
    
    fixes #15845
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- tools/individual-pkg-gen/copy-files-removing-deps.ts
+++ tools/individual-pkg-gen/copy-files-removing-deps.ts
@@ -4,9 +4,9 @@ import * as fs from 'fs-extra';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const lerna_project = require('@lerna/project');
 
-copyFilesRemovingDependencies();
+transformPackages();
 
-function copyFilesRemovingDependencies(): void {
+function transformPackages(): void {
   // there is a lerna.json in the individual-packages directory, where this script executes
   const project = new lerna_project.Project(__dirname);
   const packages = project.getPackagesSync();
@@ -34,147 +34,157 @@ function copyFilesRemovingDependencies(): void {
       const source = path.join(srcDir, sourceFileName);
       const destination = path.join(destDir, sourceFileName);
 
-      let fileProcessed = false;
-      if (sourceFileName === 'package.json') {
-        const packageJson = fs.readJsonSync(source);
-        if (packageJson.name === pkg.name) {
-          const pkgUnscopedName = `${pkg.name.substring('@aws-cdk/'.length)}`;
-
-          packageJson.name = `@aws-cdk-lib-alpha/${pkgUnscopedName}`;
-          packageJson.repository.directory = `packages/individual-packages/${pkgUnscopedName}`;
-
-          // disable awslint (some rules are hard-coded to @aws-cdk/core)
-          packageJson.awslint = {
-            exclude: ['*:*'],
-          };
-
-          // add a pkglint exemption for the 'package name = dir name' rule
-          const pkglint = packageJson.pkglint || {};
-          pkglint.exclude = [
-            ...(pkglint.exclude || []),
-            'naming/package-matches-directory',
-            // the experimental packages need the "real" assert dependency
-            'assert/assert-dependency',
-          ];
-          packageJson.pkglint = pkglint;
-
-          // turn off the L1 generation, which uses @aws-cdk/ modules
-          if (packageJson.scripts?.gen === 'cfn2ts') {
-            delete packageJson.scripts.gen;
-          }
-
-          /* ****** handle dependencies ****** */
-
-          // regular dependencies
-          const alphaDependencies: { [dep: string]: string } = {};
-          const constructsAndCdkLibDevDeps: { [dep: string]: string } = {};
-          for (const dependency of Object.keys(packageJson.dependencies || {})) {
-            // all 'regular' dependencies on alpha modules will be converted to
-            // a pair of devDependency on '0.0.0' and peerDependency on '^0.0.0',
-            // and the package will have no regular dependencies anymore
-            switch (dependency) {
-              // @core corresponds to aws-cdk-lib
-              case '@aws-cdk/core':
-                constructsAndCdkLibDevDeps['aws-cdk-lib'] = pkg.version;
-                break;
-              case 'constructs':
-                constructsAndCdkLibDevDeps.constructs = packageJson.dependencies.constructs;
-                break;
-              default:
-                if (alphaPackages[dependency]) {
-                  alphaDependencies[alphaPackages[dependency]] = pkg.version;
-                }
-            }
-          }
-          packageJson.dependencies = undefined;
-
-          // devDependencies
-          const alphaDevDependencies: { [dep: string]: string } = {};
-          const devDependencies: { [dep: string]: string } = {};
-          for (const v1DevDependency of Object.keys(packageJson.devDependencies || {})) {
-            switch (v1DevDependency) {
-              case '@aws-cdk/assert-internal':
-              case '@aws-cdk/assert':
-                devDependencies['@aws-cdk/assert'] = packageJson.devDependencies[v1DevDependency];
-                break;
-              default:
-                if (alphaPackages[v1DevDependency]) {
-                  alphaDevDependencies[alphaPackages[v1DevDependency]] = pkg.version;
-                } else if (!v1DevDependency.startsWith('@aws-cdk/')) {
-                  devDependencies[v1DevDependency] = packageJson.devDependencies[v1DevDependency];
-                }
-            }
-          }
-          const finalPackageJson = { ...packageJson };
-          // we save the devDependencies in a temporary _package.json
-          finalPackageJson.devDependencies = {
-            ...devDependencies,
-            ...constructsAndCdkLibDevDeps,
-            ...alphaDevDependencies,
-            ...alphaDependencies,
-          };
-          packageJson.devDependencies = {
-            ...alphaDevDependencies,
-            ...alphaDependencies,
-          };
-
-          // peer dependencies
-          finalPackageJson.peerDependencies = {
-            ...(Object.entries(alphaDependencies)
-              // for other alpha dependencies, we need to depend on exact versions
-              // (because of the braking changes between them)
-              .reduce((acc, [depName, depVersion]) => {
-                acc[depName] = depVersion;
-                return acc;
-              }, {} as { [dep: string]: string })),
-            ...(Object.entries(constructsAndCdkLibDevDeps)
-              .reduce((acc, [depName, depVersion]) => {
-                acc[depName] = `${depVersion.startsWith('^') ? '' : '^'}${depVersion}`;
-                return acc;
-              }, {} as { [dep: string]: string })),
-          };
-          packageJson.peerDependencies = undefined;
-
-          fileProcessed = true;
-          fs.writeJsonSync(destination, packageJson, { spaces: 2 });
-          fs.writeJsonSync(path.join(destDir, '_package.json'), finalPackageJson, { spaces: 2 });
-        }
+      if (srcDir === pkg.location && sourceFileName === 'package.json') {
+        // Only transform packageJsons at the package root, not in any nested packages.
+        transformPackageJson(pkg, source, destination, alphaPackages);
       } else if (sourceFileName === '.gitignore') {
         // ignore everything, otherwise there are uncommitted files present in testing,
         // because the module's .gitignore file has entries like !.eslintrc.js
-        fileProcessed = true;
         const gitIgnoreContents = fs.readFileSync(source);
         fs.outputFileSync(destination, Buffer.concat([gitIgnoreContents, Buffer.from('\n*\n')]));
       } else if (sourceFileName === '.eslintrc.js') {
         // Change the default configuration of the import/no-extraneous-dependencies rule
         // (as the unstable packages don't use direct dependencies,
         // but instead a combination of devDependencies + peerDependencies)
-        fileProcessed = true;
         const esLintRcContents = fs.readFileSync(source);
         fs.outputFileSync(destination, Buffer.concat([esLintRcContents,
           Buffer.from("\nbaseConfig.rules['import/no-extraneous-dependencies'] = ['error', " +
               '{ devDependencies: true, peerDependencies: true } ];\n')]));
       } else if (sourceFileName.endsWith('.ts') && !sourceFileName.endsWith('.d.ts')) {
-        fileProcessed = true;
         const sourceCode = fs.readFileSync(source).toString();
         const sourceCodeOutput = awsCdkMigration.rewriteImports(sourceCode, sourceFileName, {
           customModules: alphaPackages,
         });
         fs.outputFileSync(destination, sourceCodeOutput);
-      }
-
-      if (fileProcessed) {
-        continue;
-      }
-
-      const stat = fs.statSync(source);
-      if (stat.isDirectory()) {
-        copyOrTransformFiles(pkg, source, destination);
       } else {
-        fs.copySync(source, destination);
+        const stat = fs.statSync(source);
+        if (stat.isDirectory()) {
+          copyOrTransformFiles(pkg, source, destination);
+        } else {
+          fs.copySync(source, destination);
+        }
       }
     }
   }
+}
+
+function transformPackageJson(pkg: any, source: string, destination: string, alphaPackages: { [dep: string]: string; }) {
+  const packageJson = fs.readJsonSync(source);
+  const pkgUnscopedName = `${pkg.name.substring('@aws-cdk/'.length)}`;
+
+  packageJson.name = `@aws-cdk-lib-alpha/${pkgUnscopedName}`;
+  packageJson.repository.directory = `packages/individual-packages/${pkgUnscopedName}`;
+
+  // disable awslint (some rules are hard-coded to @aws-cdk/core)
+  packageJson.awslint = {
+    exclude: ['*:*'],
+  };
+
+  // add a pkglint exemption for the 'package name = dir name' rule
+  const pkglint = packageJson.pkglint || {};
+  pkglint.exclude = [
+    ...(pkglint.exclude || []),
+    'naming/package-matches-directory',
+    // the experimental packages need the "real" assert dependency
+    'assert/assert-dependency',
+  ];
+  packageJson.pkglint = pkglint;
+
+  // turn off the L1 generation, which uses @aws-cdk/ modules
+  if (packageJson.scripts?.gen === 'cfn2ts') {
+    delete packageJson.scripts.gen;
+  }
+
+  const jsiiTargets = packageJson.jsii.targets;
+  jsiiTargets.dotnet.namespace = jsiiTargets.dotnet.namespace.replace(
+    /^Amazon\.CDK\./, 'Amazon.CDK.Alpha.');
+  jsiiTargets.java.package = jsiiTargets.java.package.replace(
+    /^software\.amazon\.awscdk\./, 'software.amazon.awscdk.alpha.');
+  jsiiTargets.java.maven.artifactId = jsiiTargets.java.maven.artifactId.startsWith('cdk-')
+    ? jsiiTargets.java.maven.artifactId.replace(/cdk-/, 'cdk-alpha.')
+    : 'cdk-alpha.aws-' + jsiiTargets.java.maven.artifactId;
+  jsiiTargets.python.distName = jsiiTargets.python.distName.replace(
+    /^aws-cdk\./, 'aws-cdk.alpha.');
+  jsiiTargets.python.module = jsiiTargets.python.module.replace(
+    /^aws_cdk\./, 'aws_cdk.alpha.');
+
+  const finalPackageJson = transformPackageJsonDependencies(packageJson, pkg, alphaPackages);
+
+  fs.writeJsonSync(destination, packageJson, { spaces: 2 });
+  fs.writeJsonSync(path.join(path.dirname(destination), '_package.json'), finalPackageJson, { spaces: 2 });
+}
+
+function transformPackageJsonDependencies(packageJson: any, pkg: any, alphaPackages: { [dep: string]: string; }) {
+  // regular dependencies
+  const alphaDependencies: { [dep: string]: string; } = {};
+  const constructsAndCdkLibDevDeps: { [dep: string]: string; } = {};
+  for (const dependency of Object.keys(packageJson.dependencies || {})) {
+    // all 'regular' dependencies on alpha modules will be converted to
+    // a pair of devDependency on '0.0.0' and peerDependency on '^0.0.0',
+    // and the package will have no regular dependencies anymore
+    switch (dependency) {
+      // @core corresponds to aws-cdk-lib
+      case '@aws-cdk/core':
+        constructsAndCdkLibDevDeps['aws-cdk-lib'] = pkg.version;
+        break;
+      case 'constructs':
+        constructsAndCdkLibDevDeps.constructs = packageJson.dependencies.constructs;
+        break;
+      default:
+        if (alphaPackages[dependency]) {
+          alphaDependencies[alphaPackages[dependency]] = pkg.version;
+        }
+    }
+  }
+  packageJson.dependencies = undefined;
+
+  // devDependencies
+  const alphaDevDependencies: { [dep: string]: string; } = {};
+  const devDependencies: { [dep: string]: string; } = {};
+  for (const v1DevDependency of Object.keys(packageJson.devDependencies || {})) {
+    switch (v1DevDependency) {
+      case '@aws-cdk/assert-internal':
+      case '@aws-cdk/assert':
+        devDependencies['@aws-cdk/assert'] = packageJson.devDependencies[v1DevDependency];
+        break;
+      default:
+        if (alphaPackages[v1DevDependency]) {
+          alphaDevDependencies[alphaPackages[v1DevDependency]] = pkg.version;
+        } else if (!v1DevDependency.startsWith('@aws-cdk/')) {
+          devDependencies[v1DevDependency] = packageJson.devDependencies[v1DevDependency];
+        }
+    }
+  }
+  const finalPackageJson = { ...packageJson };
+  // we save the devDependencies in a temporary _package.json
+  finalPackageJson.devDependencies = {
+    ...devDependencies,
+    ...constructsAndCdkLibDevDeps,
+    ...alphaDevDependencies,
+    ...alphaDependencies,
+  };
+  packageJson.devDependencies = {
+    ...alphaDevDependencies,
+    ...alphaDependencies,
+  };
+
+  // peer dependencies
+  finalPackageJson.peerDependencies = {
+    ...(Object.entries(alphaDependencies)
+      // for other alpha dependencies, we need to depend on exact versions
+      // (because of the braking changes between them)
+      .reduce((acc, [depName, depVersion]) => {
+        acc[depName] = depVersion;
+        return acc;
+      }, {} as { [dep: string]: string; })),
+    ...(Object.entries(constructsAndCdkLibDevDeps)
+      .reduce((acc, [depName, depVersion]) => {
+        acc[depName] = `${depVersion.startsWith('^') ? '' : '^'}${depVersion}`;
+        return acc;
+      }, {} as { [dep: string]: string; })),
+  };
+  packageJson.peerDependencies = undefined;
+  return finalPackageJson;
 }
 
 function shouldIgnoreFile(name: string): boolean {
