fd5ecb05129f83235fc8f1c7e83a8fa88279e515
Niranjan Jayakar <nija@amazon.com>
2021-08-04 11:55:28 +0100

    refactor step 3

--- packages/@aws-cdk/assertions/lib/private/resource.ts
+++ packages/@aws-cdk/assertions/lib/private/resource.ts
@@ -1,15 +1,18 @@
 import { Match } from '../match';
-import { Matcher, MatchResult } from '../matcher';
+import { Matcher } from '../matcher';
 import { StackInspector } from '../vendored/assert';
+import { formatFailure, matchSection } from './section';
+
+// Partial type for CloudFormation Resource
+type Resource = {
+  Type: string;
+}
 
 export function findResources(inspector: StackInspector, type: string, props: any = {}): { [key: string]: any }[] {
   const matcher = Matcher.isMatcher(props) ? props : Match.objectLike(props);
 
-  const section: { [key: string] : any } = inspector.value.Resources ?? {};
-  const filteredSection = Object.entries(section)
-    .filter(([_, v]) => v.Type === type)
-    .reduce((agg, [k, v]) => { return { ...agg, [k]: v }; }, {});
-  const result = matchUp(filteredSection, matcher);
+  const section: { [key: string] : Resource } = inspector.value.Resources ?? {};
+  const result = matchSection(filterType(section, type), matcher);
 
   if (!result.match) {
     return [];
@@ -21,11 +24,8 @@ export function findResources(inspector: StackInspector, type: string, props: an
 export function hasResource(inspector: StackInspector, type: string, props: any): string | void {
   const matcher = Matcher.isMatcher(props) ? props : Match.objectLike(props);
 
-  const section: { [key: string] : any } = inspector.value.Resources ?? {};
-  const filteredSection = Object.entries(section)
-    .filter(([_, v]) => v.Type === type)
-    .reduce((agg, [k, v]) => { return { ...agg, [k]: v }; }, {});
-  const result = matchUp(filteredSection, matcher);
+  const section: { [key: string]: Resource } = inspector.value.Resources ?? {};
+  const result = matchSection(filterType(section, type), matcher);
 
   if (result.match) {
     return;
@@ -36,65 +36,13 @@ export function hasResource(inspector: StackInspector, type: string, props: any)
   }
 
   return [
-    `${result.count} resources with type ${type} found, but none match as expected.`,
-    formatMessage(
-      result.closestResult,
-      result.closestResult.target,
-    ),
+    `${result.analyzedCount} resources with type ${type} found, but none match as expected.`,
+    formatFailure(result.closestResult),
   ].join('\n');
 }
 
-type MatchSuccess = { match: true, matches: any[] };
-type MatchFailure = { match: false, closestResult?: MatchResult, count: number };
-
-function matchUp(section: any, matcher: Matcher): MatchSuccess | MatchFailure {
-  let closestResult: MatchResult | undefined = undefined;
-  let matching: any[] = [];
-  let count = 0;
-
-  eachEntryInSection(
-    section,
-
-    (entry) => {
-      const result = matcher.test(entry);
-      if (!result.hasFailed()) {
-        matching.push(entry);
-      } else {
-        count++;
-        if (closestResult === undefined || closestResult.failCount > result.failCount) {
-          closestResult = result;
-        }
-      }
-    },
-  );
-
-  if (matching.length > 0) {
-    return { match: true, matches: matching };
-  } else {
-    return { match: false, closestResult, count };
-  }
-}
-
-function eachEntryInSection(
-  section: any,
-  cb: (entry: {[key: string]: any}) => void): void {
-
-  for (const logicalId of Object.keys(section ?? {})) {
-    const resource: { [key: string]: any } = section[logicalId];
-    cb(resource);
-  }
-}
-
-function formatMessage(closestResult: MatchResult, closestResource: {}): string {
-  return [
-    'The closest result is:',
-    reindent(JSON.stringify(closestResource, undefined, 2)),
-    'with the following mismatches:',
-    ...closestResult.toHumanStrings().map(s => `\t${s}`),
-  ].join('\n');
-}
-
-function reindent(x: string, indent: number = 2): string {
-  const pad = ' '.repeat(indent);
-  return pad + x.split('\n').join(`\n${pad}`);
+function filterType(section: { [key: string]: Resource }, type: string): { [key: string]: Resource } {
+  return Object.entries(section)
+    .filter(([_, v]) => v.Type === type)
+    .reduce((agg, [k, v]) => { return { ...agg, [k]: v }; }, {});
 }
