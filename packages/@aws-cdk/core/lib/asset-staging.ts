0ce5ba79a37a90b1602881acb5c03d22fbe0c808
akuntsch <alex.kuntsch+github@mailbox.org>
2021-08-11 11:08:20 +0200

    fix(core): asset bundling fails for non-existent user (#15313)
    Currently, when not explicitly passing a user in BundlingOptions, `os.userInfo()` is called as a default fallback. Even though the user is only used and needed when using docker bundling, it is still called when using local bundling. This can be problematic e.g. when running cdk scripts inside a docker container where the user does not exist inside the container (see #15415).
    
    Fixes #15415.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/core/lib/asset-staging.ts
+++ packages/@aws-cdk/core/lib/asset-staging.ts
@@ -428,16 +428,6 @@ export class AssetStaging extends CoreConstruct {
     // Chmod the bundleDir to full access.
     fs.chmodSync(bundleDir, 0o777);
 
-    let user: string;
-    if (options.user) {
-      user = options.user;
-    } else { // Default to current user
-      const userInfo = os.userInfo();
-      user = userInfo.uid !== -1 // uid is -1 on Windows
-        ? `${userInfo.uid}:${userInfo.gid}`
-        : '1000:1000';
-    }
-
     // Always mount input and output dir
     const volumes = [
       {
@@ -457,6 +447,16 @@ export class AssetStaging extends CoreConstruct {
 
       localBundling = options.local?.tryBundle(bundleDir, options);
       if (!localBundling) {
+        let user: string;
+        if (options.user) {
+          user = options.user;
+        } else { // Default to current user
+          const userInfo = os.userInfo();
+          user = userInfo.uid !== -1 // uid is -1 on Windows
+            ? `${userInfo.uid}:${userInfo.gid}`
+            : '1000:1000';
+        }
+
         options.image.run({
           command: options.command,
           user,
