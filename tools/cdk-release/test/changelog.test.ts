357152640335deb79a7d78cea5b685ccbe784c9b
Nick Lynch <nlynch@amazon.com>
2021-08-13 17:18:49 +0100

    chore(cdk-release): track and bump separate alpha version (#16043)
    As part of the project to release our alpha modules as independent modules, this
    change extends our `cdk-release` tool to support tracking and bumping two
    versions: the primary, stable version, and an additional, optional alpha
    version.
    
    If the local `version.vX.json` file has an `alphaVersion` property, this version
    will be bumped alongside the main (stable) version. If the main version is also
    a prerelease, then the alphaVersion is simply incremented (e.g., `2.0.0-alpha.0`
    -> `2.0.0-alpha.1`); if the main version is stable, the alpha is incremented to
    match (e.g., the `2.1.0` release will get an alpha of `2.1.0-alpha.0`). If no
    `alphaVersion` is present, it is simply ignored.
    
    This is still only part of the work to release the alpha modules. Remaining:
    - Create the indepedent module CHANGELOGs, and aggregate into the main CHANGELOG
    - Change the `align-versions` script to read and apply the alphaVersion if the
      module is an alpha module.
    - Create the first alpha version in the v2-main branch.
    
    *Implementation details:*
    
    I was frustrated by the generic 'Updater' and 'UpdaterModule' pattern that I
    needed to work around, so I decided to just simplify by removing the generic
    capabilities that we're not using. This involved deleting a lot of code that
    wasn't doing much. Lots of refactoring ensured. Similarly, there simply weren't
    nearly enough tests in this module yet; I added at least a few more to improve
    coverage in anticipation of the next tasks (which will require even more tests).
    My apologies to the reviewer(s) for the muddled diff, but I strongly believe
    these simplifications will make it easier to maintain `cdk-release` going
    forward.
    
    related #15591
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- tools/cdk-release/test/changelog.test.ts
+++ tools/cdk-release/test/changelog.test.ts
@@ -1,10 +1,9 @@
-import { ConventionalCommit, filterCommits, getConventionalCommitsFromGitHistory } from '../lib/conventional-commits';
-import { changelog } from '../lib/lifecycles/changelog';
-import { ReleaseOptions } from '../lib/types';
+import { ConventionalCommit } from '../lib/conventional-commits';
+import { changelog, ChangelogOptions } from '../lib/lifecycles/changelog';
 
 describe('Changelog generation', () => {
-  const args: ReleaseOptions = {
-    releaseAs: 'minor',
+  const args: ChangelogOptions = {
+    changelogFile: 'CHANGELOG.md',
     dryRun: true,
     silent: true,
     includeDateInChangelog: false,
@@ -53,52 +52,6 @@ describe('Changelog generation', () => {
 
 `);
   });
-
-  test("correctly skips experimental modules, even with 'BREAKING CHANGES'", async () => {
-    const commits: ConventionalCommit[] = [
-      buildCommit({
-        type: 'feat',
-        scope: 'scope',
-        subject: 'super important feature',
-      }),
-      buildCommit({
-        type: 'fix',
-        scope: 'example-construct-library', // really hope we don't stabilize this one
-        subject: 'hairy bugfix',
-        notes: [
-          {
-            title: 'BREAKING CHANGE',
-            text: 'this is a breaking change',
-          },
-        ],
-      }),
-    ];
-
-    const changelogContents = await invokeChangelogFrom1_23_0to1_24_0({
-      ...args,
-      stripExperimentalChanges: true,
-    }, commits);
-
-    expect(changelogContents).toBe(
-      `## [1.24.0](https://github.com/aws/aws-cdk/compare/v1.23.0...v1.24.0)
-
-### Features
-
-* **scope:** super important feature
-
-`);
-  });
-
-  test('makes it so that no Git commits are queried if Changelog generation is skipped', async () => {
-    const commits = await getConventionalCommitsFromGitHistory({
-      ...args,
-      skip: {
-        changelog: true,
-      },
-    }, '3.9.2');
-
-    expect(commits).toHaveLength(0);
-  });
 });
 
 interface PartialCommit extends Partial<ConventionalCommit> {
@@ -115,7 +68,6 @@ function buildCommit(commit: PartialCommit): ConventionalCommit {
   };
 }
 
-async function invokeChangelogFrom1_23_0to1_24_0(args: ReleaseOptions, commits: ConventionalCommit[]): Promise<string> {
-  const changelogResult = await changelog(args, '1.23.0', '1.24.0', filterCommits(args, commits));
-  return changelogResult.contents;
+async function invokeChangelogFrom1_23_0to1_24_0(args: ChangelogOptions, commits: ConventionalCommit[]): Promise<string> {
+  return (await changelog(args, '1.23.0', '1.24.0', commits)).contents;
 }
