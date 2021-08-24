44d8ba9547ee7c2ce0badabfebd0a89d626412da
Otavio Macedo <otaviom@amazon.com>
2021-08-24 10:19:49 +0100

    chore: added more documentation to the transform.sh script

--- scripts/transform.sh
+++ scripts/transform.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
+# To run this script in development, first build the following packages:
+#     packages/@aws-cdk/assert
+#     packages/aws-cdk-lib
+#     tools/individual-pkg-gen
+
 set -euo pipefail
 scriptdir=$(cd $(dirname $0) && pwd)
 
 # Creates a symlink in each individual package's node_modules folder pointing
 # to the root folder's node_modules/.bin. This allows yarn to find the executables
 # it needs (e.g., jsii-rosetta) for the build.
+#
+# The reason yarn doesn't find the executables in the first place is that they are
+# not dependencies of each individual package -- nor should they be. They can't be
+# found in the lerna workspace, either, since it only includes the individual
+# packages. A possible solution that doesn't involve using this workaround function
+# is to add more packages to the workspace, which includes at least awslint and all
+# the tools/* packages, but potentially more, such as cloudformation-diff,
+# cloud-assembly-schema and others, which are dependencies of one or more of the
+# tools packages.
+#
+# In addition, we need to copy aws-cdk-lib inside individual-packages/ and remove
+# all @aws-cdk/* packages from its devDependencies section. Otherwise, the build
+# process for the experimental packages will include 250+ packages altogether,
+# making the build time unworkable.
+#
+# Finally, we will need to restrict the build command only to the packages inside
+# individual-packages/, which may be achieved with yarn 2 commands such as
+# yarn workspaces foreach <commandName>, available with the workspace-tools plugin.
 createSymlinks() {
   find "$1" ! -path "$1" -type d -maxdepth 1 \
     -exec mkdir -p {}/node_modules \; \
