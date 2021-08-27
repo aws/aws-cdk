1f892cac4e060a962b5923ddb086551ba8d4642e
Robert Djurasaj <robertd@users.noreply.github.com>
2021-08-27 04:18:44 -0600

    feat(aws-cloudfront-origins): add custom headers to S3Origin (#16161)
    Closes #16160.
    
    @njlynch Can you please review this when time permits and see if it needs any further polishing.
    
    cc @nwitte-rocketloans
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-cloudfront-origins/lib/s3-origin.ts
+++ packages/@aws-cdk/aws-cloudfront-origins/lib/s3-origin.ts
@@ -11,14 +11,7 @@ import { Construct } from '@aws-cdk/core';
 /**
  * Properties to use to customize an S3 Origin.
  */
-export interface S3OriginProps {
-  /**
-   * An optional path that CloudFront appends to the origin domain name when CloudFront requests content from the origin.
-   * Must begin, but not end, with '/' (e.g., '/production/images').
-   *
-   * @default '/'
-   */
-  readonly originPath?: string;
+export interface S3OriginProps extends cloudfront.OriginProps {
   /**
    * An optional Origin Access Identity of the origin identity cloudfront will use when calling your s3 bucket.
    *
