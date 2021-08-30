a6d9862725cbdc836d9400cb6af5b696d1d4754e
Jonathan Goldwasser <jonathan.goldwasser@gmail.com>
2021-08-30 16:47:04 +0200

    doc

--- packages/@aws-cdk/core/lib/fs/fingerprint.ts
+++ packages/@aws-cdk/core/lib/fs/fingerprint.ts
@@ -16,6 +16,8 @@ const CRLF = `${CR}${LF}`;
 /**
  * Produces fingerprint based on the contents of a single file or an entire directory tree.
  *
+ * Line endings are converted from CRLF to LF.
+ *
  * The fingerprint will also include:
  * 1. An extra string if defined in `options.extra`.
  * 2. The symlink follow mode value.
