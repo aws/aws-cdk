65d2c9533d2f3ae789fb714e9f0c2897c48a7baa
Calvin Combs <comcalvi@amazon.com>
2021-09-02 10:44:13 -0700

    removed unneeded default

--- packages/aws-cdk/bin/cdk.ts
+++ packages/aws-cdk/bin/cdk.ts
@@ -105,7 +105,7 @@ async function parseCommandLineArguments() {
       .option('outputs-file', { type: 'string', alias: 'O', desc: 'Path to file where stack outputs will be written as JSON', requiresArg: true })
       .option('previous-parameters', { type: 'boolean', default: true, desc: 'Use previous values for existing parameters (you must specify all parameters on every deployment if this is disabled)' })
       .option('progress', { type: 'string', choices: [StackActivityProgress.BAR, StackActivityProgress.EVENTS], desc: 'Display mode for stack activity events' })
-      .option('rollback', { type: 'boolean', default: undefined, desc: 'Rollback stack to stable state on failure (iterate more rapidly with --no-rollback or -R)' })
+      .option('rollback', { type: 'boolean', desc: 'Rollback stack to stable state on failure (iterate more rapidly with --no-rollback or -R)' })
       // Hack to get '-R' as an alias for '--no-rollback', suggested by: https://github.com/yargs/yargs/issues/1729
       .option('R', { type: 'boolean', hidden: true })
       .middleware(yargsNegativeAlias('R', 'rollback'), true)
