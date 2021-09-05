18c9a638e78889c2ae809f58883c28fea3d1a67d
DaWyz <DaWyz@users.noreply.github.com>
2021-09-04 21:45:24 -0700

    feat(events-targets): Add DLQ support for EventBus target

--- packages/@aws-cdk/aws-events-targets/lib/event-bus.ts
+++ packages/@aws-cdk/aws-events-targets/lib/event-bus.ts
@@ -1,9 +1,12 @@
 import * as events from '@aws-cdk/aws-events';
 import * as iam from '@aws-cdk/aws-iam';
-import { singletonEventRole } from './util';
+import * as sqs from '@aws-cdk/aws-sqs';
+import { singletonEventRole, addToDeadLetterQueueResourcePolicy } from './util';
 
 /**
  * Configuration properties of an Event Bus event
+ *
+ * Cannot extend TargetBaseProps. Retry policy is not supported for Event bus targets.
  */
 export interface EventBusProps {
   /**
@@ -12,25 +15,39 @@ export interface EventBusProps {
    * @default a new role is created.
    */
   readonly role?: iam.IRole;
+
+  /**
+   * The SQS queue to be used as deadLetterQueue.
+   * Check out the [considerations for using a dead-letter queue](https://docs.aws.amazon.com/eventbridge/latest/userguide/rule-dlq.html#dlq-considerations).
+   *
+   * The events not successfully delivered are automatically retried for a specified period of time,
+   * depending on the retry policy of the target.
+   * If an event is not delivered before all retry attempts are exhausted, it will be sent to the dead letter queue.
+   *
+   * @default - no dead-letter queue
+   */
+  readonly deadLetterQueue?: sqs.IQueue;
 }
 
 /**
  * Notify an existing Event Bus of an event
  */
 export class EventBus implements events.IRuleTarget {
-  private readonly role?: iam.IRole;
-
-  constructor(private readonly eventBus: events.IEventBus, props: EventBusProps = {}) {
-    this.role = props.role;
-  }
+  constructor(private readonly eventBus: events.IEventBus, private readonly props: EventBusProps = {}) { }
 
   bind(rule: events.IRule, _id?: string): events.RuleTargetConfig {
-    if (this.role) {
-      this.role.addToPrincipalPolicy(this.putEventStatement());
+    if (this.props.role) {
+      this.props.role.addToPrincipalPolicy(this.putEventStatement());
     }
-    const role = this.role ?? singletonEventRole(rule, [this.putEventStatement()]);
+    const role = this.props.role ?? singletonEventRole(rule, [this.putEventStatement()]);
+
+    if (this.props.deadLetterQueue) {
+      addToDeadLetterQueueResourcePolicy(rule, this.props.deadLetterQueue);
+    }
+
     return {
       arn: this.eventBus.eventBusArn,
+      deadLetterConfig: this.props.deadLetterQueue ? { arn: this.props.deadLetterQueue?.queueArn } : undefined,
       role,
     };
   }
