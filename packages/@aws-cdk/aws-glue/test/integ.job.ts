f01c0be40a44c58d41c7bf35691b839790a74e25
Ahmed Kamel <humanzz@hotmail.com>
2021-09-08 15:17:19 +0100

    update integ.job.ts

--- packages/@aws-cdk/aws-glue/test/integ.job.ts
+++ packages/@aws-cdk/aws-glue/test/integ.job.ts
@@ -2,37 +2,20 @@ import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
 import * as glue from '../lib';
 
+/**
+ * To verify the ability to run those jobs
+ *
+ * Run the job using
+ *   `aws glue start-job-run --job-name <job name>`
+ */
 const app = new cdk.App();
 
 const stack = new cdk.Stack(app, 'aws-glue-job');
 
 const script = glue.Code.fromAsset(path.join(__dirname, 'job-script/hello_world.py'));
 
-new glue.Job(stack, 'MinimalGlueEtlJob', {
-  executable: glue.JobExecutable.pythonEtl({
-    glueVersion: glue.GlueVersion.V2_0,
-    pythonVersion: glue.PythonVersion.THREE,
-    script,
-  }),
-});
-
-new glue.Job(stack, 'MinimalGlueStreamingJob', {
-  executable: glue.JobExecutable.pythonStreaming({
-    glueVersion: glue.GlueVersion.V2_0,
-    pythonVersion: glue.PythonVersion.THREE,
-    script,
-  }),
-});
-
-new glue.Job(stack, 'MinimalPythonShellJob', {
-  executable: glue.JobExecutable.pythonShell({
-    glueVersion: glue.GlueVersion.V2_0,
-    pythonVersion: glue.PythonVersion.THREE,
-    script,
-  }),
-});
-
-const etlJob = new glue.Job(stack, 'Job', {
+const etlJob = new glue.Job(stack, 'EtlJob', {
+  jobName: 'EtlJob',
   executable: glue.JobExecutable.pythonEtl({
     glueVersion: glue.GlueVersion.V2_0,
     pythonVersion: glue.PythonVersion.THREE,
@@ -48,11 +31,50 @@ const etlJob = new glue.Job(stack, 'Job', {
     arg1: 'value1',
     arg2: 'value2',
   },
+  sparkUI: {
+    enabled: true,
+  },
+  continuousLogging: {
+    enabled: true,
+    quiet: true,
+    logStreamPrefix: 'EtlJob',
+  },
+  tags: {
+    key: 'value',
+  },
+});
+etlJob.metricSuccess();
+
+new glue.Job(stack, 'StreamingJob', {
+  jobName: 'StreamingJob',
+  executable: glue.JobExecutable.pythonStreaming({
+    glueVersion: glue.GlueVersion.V2_0,
+    pythonVersion: glue.PythonVersion.THREE,
+    script,
+  }),
+  defaultArguments: {
+    arg1: 'value1',
+    arg2: 'value2',
+  },
   tags: {
     key: 'value',
   },
 });
 
-etlJob.metricSuccess();
+new glue.Job(stack, 'ShellJob', {
+  jobName: 'ShellJob',
+  executable: glue.JobExecutable.pythonShell({
+    glueVersion: glue.GlueVersion.V2_0,
+    pythonVersion: glue.PythonVersion.THREE,
+    script,
+  }),
+  defaultArguments: {
+    arg1: 'value1',
+    arg2: 'value2',
+  },
+  tags: {
+    key: 'value',
+  },
+});
 
 app.synth();
