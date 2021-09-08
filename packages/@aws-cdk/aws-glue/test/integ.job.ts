ea32eabd1242f4e37373d2f445bb6dd161ea902e
Ahmed Kamel <humanzz@hotmail.com>
2021-09-08 18:31:41 +0100

    update integ test verification documentation

--- packages/@aws-cdk/aws-glue/test/integ.job.ts
+++ packages/@aws-cdk/aws-glue/test/integ.job.ts
@@ -3,10 +3,19 @@ import * as cdk from '@aws-cdk/core';
 import * as glue from '../lib';
 
 /**
- * To verify the ability to run those jobs
+ * To verify the ability to run jobs created in this test
  *
  * Run the job using
- *   `aws glue start-job-run --job-name <job name>`
+ *   `aws glue start-job-run --region us-east-1 --job-name <job name>`
+ * This will return a runId
+ *
+ * Get the status of the job run using
+ *   `aws glue get-job-run --region us-east-1 --job-name <job name> --run-id <runId>`
+ *
+ * For example, to test the ShellJob
+ * - Run: `aws glue start-job-run --region us-east-1 --job-name ShellJob`
+ * - Get Status: `aws glue get-job-run --region us-east-1 --job-name ShellJob --run-id <runId output by the above command>`
+ * - Check output: `aws logs get-log-events --region us-east-1 --log-group-name "/aws-glue/python-jobs/output" --log-stream-name "<runId>>` which should show "hello world"
  */
 const app = new cdk.App();
 
