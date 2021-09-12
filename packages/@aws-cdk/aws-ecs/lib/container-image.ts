9da275e7ad876d5682c050692d125073c800551d
Elad Ben-Israel <benisrae@amazon.com>
2021-09-12 14:30:03 +0300

    docs(assets): tarball path should be absolute (#15750)
    Improve docs to indicate that the path to a tarball should be absolute and not relative.
    
    Fixes #15721 
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-ecs/lib/container-image.ts
+++ packages/@aws-cdk/aws-ecs/lib/container-image.ts
@@ -59,7 +59,8 @@ export abstract class ContainerImage {
    * Use this method if the container image has already been created by another process (e.g. jib)
    * and you want to add it as a container image asset.
    *
-   * @param tarballFile Path to the tarball (relative to the directory).
+   * @param tarballFile Absolute path to the tarball. You can use language-specific idioms (such as `__dirname` in Node.js)
+   *                    to create an absolute path based on the current script running directory.
    */
   public static fromTarball(tarballFile: string): ContainerImage {
     return {
