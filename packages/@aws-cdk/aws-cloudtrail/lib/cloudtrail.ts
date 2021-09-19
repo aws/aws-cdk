7e1e3ddcb6b29960be1387b1559f0dadfad3f6b5
Vincent Turato <vincentturato@gmail.com>
2021-09-19 16:19:57 -0700

    use ManagementEventSources enum

--- packages/@aws-cdk/aws-cloudtrail/lib/cloudtrail.ts
+++ packages/@aws-cdk/aws-cloudtrail/lib/cloudtrail.ts
@@ -431,7 +431,7 @@ export interface AddEventSelectorOptions {
    *
    * @default []
    */
-  readonly excludeManagementEventSources?: string[];
+  readonly excludeManagementEventSources?: ManagementEventSources[];
 }
 
 /**
