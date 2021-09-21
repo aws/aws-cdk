a4de461bf98dd412681d76a56b52eac77ff4ee60
kaizen3031593 <zen.conroy@gmail.com>
2021-09-21 12:42:04 -0400

    jest config change coverage threshold

--- packages/@aws-cdk/cfnspec/jest.config.js
+++ packages/@aws-cdk/cfnspec/jest.config.js
@@ -1,2 +1,10 @@
 const baseConfig = require('../../../tools/cdk-build-tools/config/jest.config');
-module.exports = baseConfig;
+module.exports = {
+  ...baseConfig,
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      branches: 50,
+    }
+  }
+};
