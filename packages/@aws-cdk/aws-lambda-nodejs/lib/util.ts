c017ed111608da6380ee2e28fcfd40e04515add6
Mike <mike@catalystcode.io>
2021-09-23 10:26:04 -0500

    fixes #15847

--- packages/@aws-cdk/aws-lambda-nodejs/lib/util.ts
+++ packages/@aws-cdk/aws-lambda-nodejs/lib/util.ts
@@ -35,19 +35,34 @@ export function callsites(): CallSite[] {
  * Find a file by walking up parent directories
  */
 export function findUp(name: string, directory: string = process.cwd()): string | undefined {
+  return findUpMultiple([name], directory)[0];
+}
+
+/**
+ * Find the lowest of multiple files by walking up parent directories. If
+ * multiple files exist at the same level, they will all be returned.
+ */
+export function findUpMultiple(names: string[], directory: string = process.cwd()): string[] {
   const absoluteDirectory = path.resolve(directory);
 
-  const file = path.join(directory, name);
-  if (fs.existsSync(file)) {
-    return file;
+  const files = [];
+  for (const name of names) {
+    const file = path.join(directory, name);
+    if (fs.existsSync(file)) {
+      files.push(file);
+    }
+  }
+
+  if (files.length > 0) {
+    return files;
   }
 
   const { root } = path.parse(absoluteDirectory);
   if (absoluteDirectory === root) {
-    return undefined;
+    return [];
   }
 
-  return findUp(name, path.dirname(absoluteDirectory));
+  return findUpMultiple(names, path.dirname(absoluteDirectory));
 }
 
 /**
