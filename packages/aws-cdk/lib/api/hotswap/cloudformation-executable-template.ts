ca36d4f9d8db18a38414af26dd6d4ce35acaf230
Adam Ruka <adamruka@amazon.com>
2021-09-29 15:41:06 -0700

    Move Cfn intrinsics implementations into a separate class.

--- packages/aws-cdk/lib/api/hotswap/cloudformation-executable-template.ts
+++ packages/aws-cdk/lib/api/hotswap/cloudformation-executable-template.ts
@@ -40,6 +40,68 @@ export class CloudFormationExecutableTemplate {
   }
 
   public async evaluateCfnExpression(cfnExpression: any): Promise<any> {
+    const self = this;
+    class CfnIntrinsics {
+      public evaluateIntrinsic(intrinsic: Intrinsic): any {
+        const intrinsicFunc = (this as any)[intrinsic.name];
+        if (!intrinsicFunc) {
+          throw new Error(`CloudFormation function ${intrinsic.name} is not supported`);
+        }
+
+        const argsAsArray = Array.isArray(intrinsic.args) ? intrinsic.args : [intrinsic.args];
+
+        return intrinsicFunc.apply(this, argsAsArray);
+      }
+
+      async 'Fn::Join'(separator: string, args: any[]): Promise<string> {
+        const evaluatedArgs = await self.evaluateCfnExpression(args);
+        return evaluatedArgs.join(separator);
+      }
+
+      async 'Fn::Split'(separator: string, args: any): Promise<string> {
+        const evaluatedArgs = await self.evaluateCfnExpression(args);
+        return evaluatedArgs.split(separator);
+      }
+
+      async 'Fn::Select'(index: number, args: any[]): Promise<string> {
+        const evaluatedArgs = await self.evaluateCfnExpression(args);
+        return evaluatedArgs[index];
+      }
+
+      async 'Ref'(logicalId: string): Promise<string> {
+        const refTarget = await self.findRefTarget(logicalId);
+        if (refTarget) {
+          return refTarget;
+        } else {
+          throw new Error(`Reference target '${logicalId}' was not found`);
+        }
+      }
+
+      async 'Fn::GetAtt'(logicalId: string, attributeName: string): Promise<string> {
+        // ToDo handle the 'logicalId.attributeName' form of Fn::GetAtt
+        const attrValue = await self.findGetAttTarget(logicalId, attributeName);
+        if (attrValue) {
+          return attrValue;
+        } else {
+          throw new Error(`Trying to evaluate Fn::GetAtt of '${logicalId}.${attributeName}' but not in context!`);
+        }
+      }
+
+      async 'Fn::Sub'(template: string, explicitPlaceholders?: { [variable: string]: string }): Promise<string> {
+        const placeholders = explicitPlaceholders
+          ? { ...self.context, ...(await self.evaluateCfnExpression(explicitPlaceholders)) }
+          : self.context;
+
+        return template.replace(/\${([^}]*)}/g, (_: string, key: string) => {
+          if (key in placeholders) {
+            return placeholders[key];
+          } else {
+            throw new Error(`Fn::Sub target '${key}' was not found`);
+          }
+        });
+      }
+    }
+
     if (cfnExpression == null) {
       return cfnExpression;
     }
@@ -51,7 +113,7 @@ export class CloudFormationExecutableTemplate {
     if (typeof cfnExpression === 'object') {
       const intrinsic = this.parseIntrinsic(cfnExpression);
       if (intrinsic) {
-        return this.evaluateIntrinsic(intrinsic);
+        return new CfnIntrinsics().evaluateIntrinsic(intrinsic);
       } else {
         const ret: { [key: string]: any } = {};
         for (const [key, val] of Object.entries(cfnExpression)) {
@@ -64,54 +126,6 @@ export class CloudFormationExecutableTemplate {
     return cfnExpression;
   }
 
-  async 'Fn::Join'(separator: string, args: any[]): Promise<string> {
-    const evaluatedArgs = await this.evaluateCfnExpression(args);
-    return evaluatedArgs.join(separator);
-  }
-
-  async 'Fn::Split'(separator: string, args: any): Promise<string> {
-    const evaluatedArgs = await this.evaluateCfnExpression(args);
-    return evaluatedArgs.split(separator);
-  }
-
-  async 'Fn::Select'(index: number, args: any[]): Promise<string> {
-    const evaluatedArgs = await this.evaluateCfnExpression(args);
-    return evaluatedArgs[index];
-  }
-
-  async 'Ref'(logicalId: string): Promise<string> {
-    const refTarget = await this.findRefTarget(logicalId);
-    if (refTarget) {
-      return refTarget;
-    } else {
-      throw new Error(`Reference target '${logicalId}' was not found`);
-    }
-  }
-
-  async 'Fn::GetAtt'(logicalId: string, attributeName: string): Promise<string> {
-    // ToDo handle the 'logicalId.attributeName' form of Fn::GetAtt
-    const attrValue = await this.findGetAttTarget(logicalId, attributeName);
-    if (attrValue) {
-      return attrValue;
-    } else {
-      throw new Error(`Trying to evaluate Fn::GetAtt of '${logicalId}.${attributeName}' but not in context!`);
-    }
-  }
-
-  async 'Fn::Sub'(template: string, explicitPlaceholders?: { [variable: string]: string }): Promise<string> {
-    const placeholders = explicitPlaceholders
-      ? { ...this.context, ...(await this.evaluateCfnExpression(explicitPlaceholders)) }
-      : this.context;
-
-    return template.replace(/\${([^}]*)}/g, (_: string, key: string) => {
-      if (key in placeholders) {
-        return placeholders[key];
-      } else {
-        throw new Error(`Fn::Sub target '${key}' was not found`);
-      }
-    });
-  }
-
   private parseIntrinsic(x: any): Intrinsic | undefined {
     const keys = Object.keys(x);
     if (keys.length === 1 && (keys[0].startsWith('Fn::') || keys[0] === 'Ref')) {
@@ -121,17 +135,6 @@ export class CloudFormationExecutableTemplate {
       };
     }
     return undefined;
-  }
-
-  private evaluateIntrinsic(intrinsic: Intrinsic): any {
-    const intrinsicFunc = (this as any)[intrinsic.name];
-    if (!intrinsicFunc) {
-      throw new Error(`CloudFormation function ${intrinsic.name} is not supported`);
-    }
-
-    const argsAsArray = Array.isArray(intrinsic.args) ? intrinsic.args : [intrinsic.args];
-
-    return intrinsicFunc.apply(this, argsAsArray);
   }
 
   private async findRefTarget(logicalId: string): Promise<string | undefined> {
