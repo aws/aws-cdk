020f1c40a9289ea7726e9339b7efcea9529417ee
kaizen3031593 <zen.conroy@gmail.com>
2021-09-30 16:02:43 -0400

    add documentation for validateReleaseLabel

--- packages/@aws-cdk/aws-stepfunctions-tasks/lib/emr/emr-create-cluster.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/lib/emr/emr-create-cluster.ts
@@ -386,6 +386,9 @@ export class EmrCreateCluster extends sfn.TaskStateBase {
 
   /**
    * Validates the release label string is in proper format.
+   * Release labels are in the form emr-x.x.x. For example, emr-5.33.0.
+   *
+   * @see https://docs.aws.amazon.com/emr/latest/ReleaseGuide/emr-release-components.html
    */
   private validateReleaseLabel(releaseLabel: string): string {
     const prefix = releaseLabel.substr(0, 4);
