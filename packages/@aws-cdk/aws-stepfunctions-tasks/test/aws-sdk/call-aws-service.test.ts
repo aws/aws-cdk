7bd6b0b34efb1da364b1d6deb3820954fa1334fe
Jonathan Goldwasser <jonathan.goldwasser@gmail.com>
2021-10-01 11:47:09 +0200

    README

--- packages/@aws-cdk/aws-stepfunctions-tasks/test/aws-sdk/call-aws-service.test.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/test/aws-sdk/call-aws-service.test.ts
@@ -1,3 +1,4 @@
+import '@aws-cdk/assert-internal/jest';
 import * as sfn from '@aws-cdk/aws-stepfunctions';
 import * as cdk from '@aws-cdk/core';
 import * as tasks from '../../lib';
@@ -21,6 +22,10 @@ test('CallAwsService task', () => {
     iamResources: ['*'],
   });
 
+  new sfn.StateMachine(stack, 'StateMachine', {
+    definition: task,
+  });
+
   // THEN
   expect(stack.resolve(task.toStateJson())).toEqual({
     Type: 'Task',
@@ -40,6 +45,65 @@ test('CallAwsService task', () => {
     Parameters: {
       'Bucket': 'my-bucket',
       'Key.$': '$.key',
+    },
+  });
+
+  expect(stack).toHaveResource('AWS::IAM::Policy', {
+    PolicyDocument: {
+      Statement: [
+        {
+          Action: 's3:getObject',
+          Effect: 'Allow',
+          Resource: '*',
+        },
+      ],
+      Version: '2012-10-17',
+    },
+  });
+});
+
+test('with custom IAM action', () => {
+  // WHEN
+  const task = new tasks.CallAwsService(stack, 'ListBuckets', {
+    service: 's3',
+    action: 'listBuckets',
+    iamResources: ['*'],
+    iamAction: 's3:ListAllMyBuckets',
+  });
+
+  new sfn.StateMachine(stack, 'StateMachine', {
+    definition: task,
+  });
+
+  // THEN
+  expect(stack.resolve(task.toStateJson())).toEqual({
+    Type: 'Task',
+    Resource: {
+      'Fn::Join': [
+        '',
+        [
+          'arn:',
+          {
+            Ref: 'AWS::Partition',
+          },
+          ':states:::aws-sdk:s3:listBuckets',
+        ],
+      ],
+    },
+    End: true,
+    Parameters: {},
+  });
+
+  expect(stack).toHaveResource('AWS::IAM::Policy', {
+    PolicyDocument: {
+      Statement: [
+        {
+          Action: 's3:ListAllMyBuckets',
+          Effect: 'Allow',
+          Resource: '*',
+        },
+      ],
+      Version: '2012-10-17',
     },
   });
 });
