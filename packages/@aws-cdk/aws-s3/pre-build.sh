827be466e0ff0714f2bc01e9f8b794a00cf73d02
Rico Huijbers <rix0rrr@gmail.com>
2021-10-04 14:27:10 +0200

    Remove useless file

--- packages/@aws-cdk/aws-s3/pre-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Bundle `node_modules/yaml` into the auto-delete-object handler
-tgt=lib/auto-delete-objects-handler/node_modules/yaml
-if [[ ! -d $tgt ]]; then
-  mkdir -p $tgt
-  cp -R node_modules/yaml/ $tgt/
-fi
