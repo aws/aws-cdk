b4d07f0b76f3ae983b120c482c7ca1a69acff827
Adam Ruka <adamruka@amazon.com>
2021-10-19 03:01:47 -0700

    fix(cfn-diff): correctly handle Date strings in diff (#16591)
    Turns out, `parseFloat()` in JavaScript is even crazier than we thought,
    and returns nonsense like `2021` for a string containing a Date like `'2021-10-25'`.
    For that reason, add an explicit check that the string parsed looks like a number before calling `parseFloat()`.
    
    Fixes #16444
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/cloudformation-diff/lib/diff-template.ts
+++ packages/@aws-cdk/cloudformation-diff/lib/diff-template.ts
@@ -99,13 +99,17 @@ function calculateTemplateDiff(currentTemplate: { [key: string]: any }, newTempl
   for (const key of unionOf(Object.keys(currentTemplate), Object.keys(newTemplate)).sort()) {
     const oldValue = currentTemplate[key];
     const newValue = newTemplate[key];
-    if (deepEqual(oldValue, newValue)) { continue; }
+    if (deepEqual(oldValue, newValue)) {
+      continue;
+    }
     const handler: DiffHandler = DIFF_HANDLERS[key]
                   || ((_diff, oldV, newV) => unknown[key] = impl.diffUnknown(oldV, newV));
     handler(differences, oldValue, newValue);
 
   }
-  if (Object.keys(unknown).length > 0) { differences.unknown = new types.DifferenceCollection(unknown); }
+  if (Object.keys(unknown).length > 0) {
+    differences.unknown = new types.DifferenceCollection(unknown);
+  }
 
   return new types.TemplateDiff(differences);
 }
