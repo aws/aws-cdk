99f376afac5156f0ccfe4508a2ff79045aa2981a
Jericho Tolentino <68654047+jericht@users.noreply.github.com>
2021-10-20 04:17:58 -0500

    feat(ec2): add aspect to require imdsv2 (#16051)
    Partially fixes: https://github.com/aws/aws-cdk/issues/5137
    Related PR: https://github.com/aws/aws-cdk/pull/16052
    
    **Note:** This PR and the above related PR have common code that has been duplicated across these two PRs because I decided it made more sense for these Aspects to be in the same package with the constructs they work with. However, it means I had to duplicate some of the base class code across the two PRs. Looking for an opinion on what's better here:
    - Should we keep it as is (2 PRs) so these Aspects are cleanly separated? or,
    - Does it make sense to either combine them in some way (e.g. a new package `@aws-cdk/aspects`) or have one reference the other (maybe the AutoScalingGroup aspect can reference the code in this PR since it already depends on this package).
    
    ### Changes
    Adds an aspect that can enable/disable IMDSv1 on Instances and Launch Templates.
    
    ### Testing
    Added unit tests
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-ec2/test/aspects/require-imdsv2-aspect.test.ts
@@ -0,0 +1,205 @@
+import {
+  countResources,
+  expect as expectCDK,
+  haveResourceLike,
+} from '@aws-cdk/assert-internal';
+import '@aws-cdk/assert-internal/jest';
+import * as cdk from '@aws-cdk/core';
+import {
+  CfnLaunchTemplate,
+  Instance,
+  InstanceRequireImdsv2Aspect,
+  InstanceType,
+  LaunchTemplate,
+  LaunchTemplateRequireImdsv2Aspect,
+  MachineImage,
+  Vpc,
+} from '../../lib';
+
+describe('RequireImdsv2Aspect', () => {
+  let app: cdk.App;
+  let stack: cdk.Stack;
+  let vpc: Vpc;
+
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new cdk.Stack(app, 'Stack');
+    vpc = new Vpc(stack, 'Vpc');
+  });
+
+  test('suppresses warnings', () => {
+    // GIVEN
+    const aspect = new LaunchTemplateRequireImdsv2Aspect({
+      suppressWarnings: true,
+    });
+    const errmsg = 'ERROR';
+    const visitMock = jest.spyOn(aspect, 'visit').mockImplementation((node) => {
+      // @ts-ignore
+      aspect.warn(node, errmsg);
+    });
+    const construct = new cdk.Construct(stack, 'Construct');
+
+    // WHEN
+    aspect.visit(construct);
+
+    // THEN
+    expect(visitMock).toHaveBeenCalled();
+    expect(construct.node.metadataEntry).not.toContainEqual({
+      data: expect.stringContaining(errmsg),
+      type: 'aws:cdk:warning',
+      trace: undefined,
+    });
+  });
+
+  describe('InstanceRequireImdsv2Aspect', () => {
+    test('requires IMDSv2', () => {
+      // GIVEN
+      const instance = new Instance(stack, 'Instance', {
+        vpc,
+        instanceType: new InstanceType('t2.micro'),
+        machineImage: MachineImage.latestAmazonLinux(),
+      });
+      const aspect = new InstanceRequireImdsv2Aspect();
+
+      // WHEN
+      cdk.Aspects.of(stack).add(aspect);
+      app.synth();
+
+      // THEN
+      const launchTemplate = instance.node.tryFindChild('LaunchTemplate') as LaunchTemplate;
+      expect(launchTemplate).toBeDefined();
+      expectCDK(stack).to(haveResourceLike('AWS::EC2::LaunchTemplate', {
+        LaunchTemplateName: stack.resolve(launchTemplate.launchTemplateName),
+        LaunchTemplateData: {
+          MetadataOptions: {
+            HttpTokens: 'required',
+          },
+        },
+      }));
+      expectCDK(stack).to(haveResourceLike('AWS::EC2::Instance', {
+        LaunchTemplate: {
+          LaunchTemplateName: stack.resolve(launchTemplate.launchTemplateName),
+        },
+      }));
+    });
+
+    test('does not toggle when Instance has a LaunchTemplate', () => {
+      // GIVEN
+      const instance = new Instance(stack, 'Instance', {
+        vpc,
+        instanceType: new InstanceType('t2.micro'),
+        machineImage: MachineImage.latestAmazonLinux(),
+      });
+      instance.instance.launchTemplate = {
+        launchTemplateName: 'name',
+        version: 'version',
+      };
+      const aspect = new InstanceRequireImdsv2Aspect();
+
+      // WHEN
+      cdk.Aspects.of(stack).add(aspect);
+
+      // THEN
+      // Aspect normally creates a LaunchTemplate for the Instance to toggle IMDSv1,
+      // so we can assert that one was not created
+      expectCDK(stack).to(countResources('AWS::EC2::LaunchTemplate', 0));
+      expect(instance.node.metadataEntry).toContainEqual({
+        data: expect.stringContaining('Cannot toggle IMDSv1 because this Instance is associated with an existing Launch Template.'),
+        type: 'aws:cdk:warning',
+        trace: undefined,
+      });
+    });
+
+    test('suppresses Launch Template warnings', () => {
+      // GIVEN
+      const instance = new Instance(stack, 'Instance', {
+        vpc,
+        instanceType: new InstanceType('t2.micro'),
+        machineImage: MachineImage.latestAmazonLinux(),
+      });
+      instance.instance.launchTemplate = {
+        launchTemplateName: 'name',
+        version: 'version',
+      };
+      const aspect = new InstanceRequireImdsv2Aspect({
+        suppressLaunchTemplateWarning: true,
+      });
+
+      // WHEN
+      aspect.visit(instance);
+
+      // THEN
+      expect(instance.node.metadataEntry).not.toContainEqual({
+        data: expect.stringContaining('Cannot toggle IMDSv1 because this Instance is associated with an existing Launch Template.'),
+        type: 'aws:cdk:warning',
+        trace: undefined,
+      });
+    });
+  });
+
+  describe('LaunchTemplateRequireImdsv2Aspect', () => {
+    test('warns when LaunchTemplateData is a CDK token', () => {
+      // GIVEN
+      const launchTemplate = new LaunchTemplate(stack, 'LaunchTemplate');
+      const cfnLaunchTemplate = launchTemplate.node.tryFindChild('Resource') as CfnLaunchTemplate;
+      cfnLaunchTemplate.launchTemplateData = fakeToken();
+      const aspect = new LaunchTemplateRequireImdsv2Aspect();
+
+      // WHEN
+      aspect.visit(launchTemplate);
+
+      // THEN
+      expect(launchTemplate.node.metadataEntry).toContainEqual({
+        data: expect.stringContaining('LaunchTemplateData is a CDK token.'),
+        type: 'aws:cdk:warning',
+        trace: undefined,
+      });
+    });
+
+    test('warns when MetadataOptions is a CDK token', () => {
+      // GIVEN
+      const launchTemplate = new LaunchTemplate(stack, 'LaunchTemplate');
+      const cfnLaunchTemplate = launchTemplate.node.tryFindChild('Resource') as CfnLaunchTemplate;
+      cfnLaunchTemplate.launchTemplateData = {
+        metadataOptions: fakeToken(),
+      } as CfnLaunchTemplate.LaunchTemplateDataProperty;
+      const aspect = new LaunchTemplateRequireImdsv2Aspect();
+
+      // WHEN
+      aspect.visit(launchTemplate);
+
+      // THEN
+      expect(launchTemplate.node.metadataEntry).toContainEqual({
+        data: expect.stringContaining('LaunchTemplateData.MetadataOptions is a CDK token.'),
+        type: 'aws:cdk:warning',
+        trace: undefined,
+      });
+    });
+
+    test('requires IMDSv2', () => {
+      // GIVEN
+      new LaunchTemplate(stack, 'LaunchTemplate');
+      const aspect = new LaunchTemplateRequireImdsv2Aspect();
+
+      // WHEN
+      cdk.Aspects.of(stack).add(aspect);
+
+      // THEN
+      expectCDK(stack).to(haveResourceLike('AWS::EC2::LaunchTemplate', {
+        LaunchTemplateData: {
+          MetadataOptions: {
+            HttpTokens: 'required',
+          },
+        },
+      }));
+    });
+  });
+});
+
+function fakeToken(): cdk.IResolvable {
+  return {
+    creationStack: [],
+    resolve: (_c) => {},
+    toString: () => '',
+  };
+}
