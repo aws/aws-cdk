438f6ecdaae31bcf7539b1d8aa75fb4014f0e0fd
Rico Huijbers <rix0rrr@gmail.com>
2021-10-25 17:07:09 +0200

    Review changes

--- packages/aws-cdk/test/integ/cli/cli.integtest.ts
+++ packages/aws-cdk/test/integ/cli/cli.integtest.ts
@@ -612,40 +612,36 @@ integTest('IAM diff', withDefaultFixture(async (fixture) => {
   expect(output).toContain('ec2.amazonaws.com');
 }));
 
-if (MAJOR_VERSION === '1') {
-  // Broken for v2 until https://github.com/aws/aws-cdk/pull/17099 has been merged
+integTest('fast deploy', withDefaultFixture(async (fixture) => {
+  // we are using a stack with a nested stack because CFN will always attempt to
+  // update a nested stack, which will allow us to verify that updates are actually
+  // skipped unless --force is specified.
+  const stackArn = await fixture.cdkDeploy('with-nested-stack', { captureStderr: false });
+  const changeSet1 = await getLatestChangeSet();
 
-  integTest('fast deploy', withDefaultFixture(async (fixture) => {
-    // we are using a stack with a nested stack because CFN will always attempt to
-    // update a nested stack, which will allow us to verify that updates are actually
-    // skipped unless --force is specified.
-    const stackArn = await fixture.cdkDeploy('with-nested-stack', { captureStderr: false });
-    const changeSet1 = await getLatestChangeSet();
+  // Deploy the same stack again, there should be no new change set created
+  await fixture.cdkDeploy('with-nested-stack');
+  const changeSet2 = await getLatestChangeSet();
+  expect(changeSet2.ChangeSetId).toEqual(changeSet1.ChangeSetId);
 
-    // Deploy the same stack again, there should be no new change set created
-    await fixture.cdkDeploy('with-nested-stack');
-    const changeSet2 = await getLatestChangeSet();
-    expect(changeSet2.ChangeSetId).toEqual(changeSet1.ChangeSetId);
+  // Deploy the stack again with --force, now we should create a changeset
+  await fixture.cdkDeploy('with-nested-stack', { options: ['--force'] });
+  const changeSet3 = await getLatestChangeSet();
+  expect(changeSet3.ChangeSetId).not.toEqual(changeSet2.ChangeSetId);
 
-    // Deploy the stack again with --force, now we should create a changeset
-    await fixture.cdkDeploy('with-nested-stack', { options: ['--force'] });
-    const changeSet3 = await getLatestChangeSet();
-    expect(changeSet3.ChangeSetId).not.toEqual(changeSet2.ChangeSetId);
+  // Deploy the stack again with tags, expected to create a new changeset
+  // even though the resources didn't change.
+  await fixture.cdkDeploy('with-nested-stack', { options: ['--tags', 'key=value'] });
+  const changeSet4 = await getLatestChangeSet();
+  expect(changeSet4.ChangeSetId).not.toEqual(changeSet3.ChangeSetId);
 
-    // Deploy the stack again with tags, expected to create a new changeset
-    // even though the resources didn't change.
-    await fixture.cdkDeploy('with-nested-stack', { options: ['--tags', 'key=value'] });
-    const changeSet4 = await getLatestChangeSet();
-    expect(changeSet4.ChangeSetId).not.toEqual(changeSet3.ChangeSetId);
-
-    async function getLatestChangeSet() {
-      const response = await fixture.aws.cloudFormation('describeStacks', { StackName: stackArn });
-      if (!response.Stacks?.[0]) { throw new Error('Did not get a ChangeSet at all'); }
-      fixture.log(`Found Change Set ${response.Stacks?.[0].ChangeSetId}`);
-      return response.Stacks?.[0];
-    }
-  }));
-}
+  async function getLatestChangeSet() {
+    const response = await fixture.aws.cloudFormation('describeStacks', { StackName: stackArn });
+    if (!response.Stacks?.[0]) { throw new Error('Did not get a ChangeSet at all'); }
+    fixture.log(`Found Change Set ${response.Stacks?.[0].ChangeSetId}`);
+    return response.Stacks?.[0];
+  }
+}));
 
 integTest('failed deploy does not hang', withDefaultFixture(async (fixture) => {
   // this will hang if we introduce https://github.com/aws/aws-cdk/issues/6403 again.
