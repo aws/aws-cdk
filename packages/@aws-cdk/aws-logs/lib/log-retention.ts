c1fd7969c1e116eb0dd97bba1e3a3b2f4416d6da
Rico Huijbers <rix0rrr@gmail.com>
2021-10-27 15:17:59 +0200

    Make lazy rendering a feature of the tag manager

--- packages/@aws-cdk/aws-logs/lib/log-retention.ts
+++ packages/@aws-cdk/aws-logs/lib/log-retention.ts
@@ -130,7 +130,6 @@ export class LogRetention extends CoreConstruct {
  */
 class LogRetentionFunction extends CoreConstruct implements cdk.ITaggable {
   public readonly functionArn: cdk.Reference;
-  readonly cfnFunction: cdk.CfnResource;
 
   tags: cdk.TagManager = new cdk.TagManager(cdk.TagType.KEY_VALUE, 'AWS::Lambda::Function');
 
@@ -157,7 +156,7 @@ class LogRetentionFunction extends CoreConstruct implements cdk.ITaggable {
     }));
 
     // Lambda function
-    this.cfnFunction = new cdk.CfnResource(this, 'Resource', {
+    const resource = new cdk.CfnResource(this, 'Resource', {
       type: 'AWS::Lambda::Function',
       properties: {
         Handler: 'index.handler',
@@ -167,27 +166,19 @@ class LogRetentionFunction extends CoreConstruct implements cdk.ITaggable {
           S3Key: asset.s3ObjectKey,
         },
         Role: role.roleArn,
+        Tags: this.tags.renderedTags,
       },
     });
-    this.functionArn = this.cfnFunction.getAtt('Arn');
+    this.functionArn = resource.getAtt('Arn');
 
     // Function dependencies
     role.node.children.forEach((child) => {
       if (cdk.CfnResource.isCfnResource(child)) {
-        this.cfnFunction.addDependsOn(child);
+        resource.addDependsOn(child);
       }
       if (cdk.Construct.isConstruct(child) && child.node.defaultChild && cdk.CfnResource.isCfnResource(child.node.defaultChild)) {
-        this.cfnFunction.addDependsOn(child.node.defaultChild);
+        resource.addDependsOn(child.node.defaultChild);
       }
     });
-  }
-
-  prepare() {
-    // Because we're using a CfnResource to avoid a cyclic dependency on aws-lambda
-    // we lose the automatic application of tags. We have to add them again manually.
-    // This has to be done in the prepare phase, after Tag Aspects have applied.
-    if (this.tags.hasTags()) {
-      this.cfnFunction.addPropertyOverride('Tags', this.tags.renderTags());
-    }
   }
 }
