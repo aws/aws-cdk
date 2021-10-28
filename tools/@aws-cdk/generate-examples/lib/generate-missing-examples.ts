aceefa9f71c76bfe9ea78c626dc494a76de892a2
kaizen3031593 <zen.conroy@gmail.com>
2021-10-28 09:12:14 -0400

    introduce exampleFragment type

--- tools/@aws-cdk/generate-examples/lib/generate-missing-examples.ts
+++ tools/@aws-cdk/generate-examples/lib/generate-missing-examples.ts
@@ -8,7 +8,7 @@ export async function generateMissingExamples(assemblyLocations: string[]) {
     await typesystem.load(path.join(assemblyLocation, '.jsii'));
     for (const aClass of typesystem.classes) {
       const classType = aClass.spec;
-      const a = generateClassExample(typesystem, classType).join('');
+      const a = generateClassExample(typesystem, classType);
       console.log(a);
     }
   }
