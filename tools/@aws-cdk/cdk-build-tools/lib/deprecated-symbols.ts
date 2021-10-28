8168ab8e46bb0f5663c4c671eda904d75543332b
Niranjan Jayakar <nija@amazon.com>
2021-10-28 12:24:36 +0100

    describeDeprecated()

--- tools/@aws-cdk/cdk-build-tools/lib/deprecated-symbols.ts
+++ tools/@aws-cdk/cdk-build-tools/lib/deprecated-symbols.ts
@@ -1,15 +1,36 @@
 /* eslint-disable jest/no-export */
+
+export function describeDeprecated(name: string, fn: jest.EmptyFunction) {
+  describe(name, () => {
+    let deprecated: string | undefined;
+    beforeEach(() => {
+      deprecated = quiet();
+    });
+    afterEach(() => {
+      reset(deprecated);
+    });
+    fn();
+  });
+}
+
 export function testDeprecated(name: string, fn: () => any, timeout?: number) {
   test(name, () => {
-    const deprecated = process.env.JSII_DEPRECATED;
-
-    process.env.JSII_DEPRECATED = 'quiet';
+    const deprecated = quiet();
     fn();
-
-    if (deprecated === undefined) {
-      delete process.env.JSII_DEPRECATED;
-    } else {
-      process.env.JSII_DEPRECATED = deprecated;
-    }
+    reset(deprecated);
   }, timeout);
+}
+
+function quiet(): string | undefined {
+  const deprecated = process.env.JSII_DEPRECATED;
+  process.env.JSII_DEPRECATED = 'quiet';
+  return deprecated;
+}
+
+function reset(deprecated: string | undefined) {
+  if (deprecated === undefined) {
+    delete process.env.JSII_DEPRECATED;
+  } else {
+    process.env.JSII_DEPRECATED = deprecated;
+  }
 }
