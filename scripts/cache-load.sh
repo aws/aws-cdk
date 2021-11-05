d4e24716aae8a5c9cfc648dd78c1ff6f6a3c4cba
Rico Huijbers <rix0rrr@gmail.com>
2021-11-05 22:03:49 +0100

    chore: enable rosetta cache for builds (#17336)
    Avail ourselves of the new build cache feature in cdklabs/cdk-ops#1776.
    Adds two new things:
    
    **A persistently cached directory**
    
    The directory `$HOME/.s3buildcache` will be stored and restored in the
    S3 bucket, if configured. The build can assume that files it puts in
    there will be availble on the next build (and on the corresponding
    PR build).
    
    **Cache rosetta tablet**
    
    If there is a file in the persistent cache directory for Rosetta,
    pass it to `jsii-rosetta` as an input.
    
    Afterwards, store whatever tablet the build produced back into the
    cache directory.
    
    The latter will only impact the persistent cache if done on a build
    that is actually configured to store the cache back, which is only
    the main pipeline build.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ scripts/cache-load.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# If the environment variable S3_BUILD_CACHE is set, download and extract the
+# tarball it points to into the directory $HOME/.s3buildcache.
+#
+set -eu
+
+cachedir=$HOME/.s3buildcache
+mkdir -p $cachedir
+
+if [[ "${S3_BUILD_CACHE:-}" = "" ]]; then
+    exit 0
+fi
+
+echo "🧳 Build cache enabled: ${S3_BUILD_CACHE}"
+if ! aws s3 ls ${S3_BUILD_CACHE} > /dev/null; then
+    echo "🧳⚠️  Cache not found."
+    exit 0
+fi
+
+if ! (cd $cachedir && aws s3 cp ${S3_BUILD_CACHE} - | tar xzv); then
+    echo "🧳⚠️  Something went wrong fetching the cache. Continuing anyway."
+fi
