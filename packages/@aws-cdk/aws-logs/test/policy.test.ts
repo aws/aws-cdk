8ef2c0a52229ec898085e10660e53c4104796bce
Ryan Parker <ryan.parker3@outlook.com>
2021-11-08 13:05:44 -0800

    fixed physicalName error and added a simple test

---
+++ packages/@aws-cdk/aws-logs/test/policy.test.ts
@@ -0,0 +1,34 @@
+import '@aws-cdk/assert-internal/jest';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+import { Stack } from '@aws-cdk/core';
+import { LogGroup } from '../lib';
+
+describe('resource policy', () => {
+  test('simple instantiation', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    const logGroup = new LogGroup(stack, 'LogGroup');
+
+    logGroup.addToResourcePolicy(new PolicyStatement({
+      actions: ['logs:CreateLogStream'],
+      resources: ['*'],
+    }));
+
+    // THEN
+    expect(stack).toHaveResource('AWS::Logs::ResourcePolicy', {
+      PolicyName: 'LogGroupPolicy643B329C',
+      PolicyDocument: JSON.stringify({
+        Statement: [
+          {
+            Action: 'logs:CreateLogStream',
+            Effect: 'Allow',
+            Resource: '*',
+          },
+        ],
+        Version: '2012-10-17',
+      }),
+    });
+  });
+});
