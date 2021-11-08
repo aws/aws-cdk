3a1398b6a116ad4dd60f371c5b55eab160053ea1
Ryan Parker <ryan.parker3@outlook.com>
2021-11-08 13:50:51 -0800

    test: testing if ResourcePolicy can be created directly

--- packages/@aws-cdk/aws-logs/test/policy.test.ts
+++ packages/@aws-cdk/aws-logs/test/policy.test.ts
@@ -1,10 +1,10 @@
 import '@aws-cdk/assert-internal/jest';
-import { PolicyStatement } from '@aws-cdk/aws-iam';
+import { PolicyStatement, ServicePrincipal } from '@aws-cdk/aws-iam';
 import { Stack } from '@aws-cdk/core';
-import { LogGroup } from '../lib';
+import { LogGroup, ResourcePolicy } from '../lib';
 
 describe('resource policy', () => {
-  test('simple instantiation', () => {
+  test('ResourcePolicy is added to stack, when .addToResourcePolicy() is provided a valid Statement', () => {
     // GIVEN
     const stack = new Stack();
     const logGroup = new LogGroup(stack, 'LogGroup');
@@ -28,6 +28,25 @@ describe('resource policy', () => {
         ],
         Version: '2012-10-17',
       }),
+    });
+  });
+
+  test('ResourcePolicy is added to stack, when created manually/directly', () => {
+    // GIVEN
+    const stack = new Stack();
+    const logGroup = new LogGroup(stack, 'LogGroup');
+
+    // WHEN
+    const resourcePolicy = new ResourcePolicy(stack, 'ResourcePolicy');
+    resourcePolicy.document.addStatements(new PolicyStatement({
+      actions: ['logs:CreateLogStream', 'logs:PutLogEvents'],
+      principals: [new ServicePrincipal('es.amazonaws.com')],
+      resources: [logGroup.logGroupArn],
+    }));
+
+    // THEN
+    expect(stack).toHaveResource('AWS::Logs::ResourcePolicy', {
+      PolicyName: 'ResourcePolicy',
     });
   });
 });
