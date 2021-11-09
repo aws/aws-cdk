229767afdac3d004f0971e8a68fd197097a899a3
Christopher Rybicki <rybickic@amazon.com>
2021-11-08 19:48:49 -0800

    feat(cli): improve asset publishing times by up to 30%

--- packages/aws-cdk/lib/util/asset-publishing.ts
+++ packages/aws-cdk/lib/util/asset-publishing.ts
@@ -27,6 +27,8 @@ export async function publishAssets(manifest: cdk_assets.AssetManifest, sdk: Sdk
 }
 
 class PublishingAws implements cdk_assets.IAws {
+  private sdkCache: Map<String, ISDK> = new Map();
+
   constructor(
     /**
      * The base SDK to work with
@@ -66,16 +68,32 @@ class PublishingAws implements cdk_assets.IAws {
   /**
    * Get an SDK appropriate for the given client options
    */
-  private sdk(options: cdk_assets.ClientOptions): Promise<ISDK> {
+  private async sdk(options: cdk_assets.ClientOptions): Promise<ISDK> {
+    // if this was `JSON.stringify(options)`, extra fields might
+    // accidentally be included in the cache key
+    const cacheKey = JSON.stringify({
+      region: options.region,
+      assumeRuleArn: options.assumeRoleArn,
+      assumeRoleExternalId: options.assumeRoleExternalId,
+    });
+
+    const maybeSdk = this.sdkCache.get(cacheKey);
+    if (maybeSdk) {
+      return Promise.resolve(maybeSdk);
+    }
+
     const env = {
       ...this.targetEnv,
       region: options.region ?? this.targetEnv.region, // Default: same region as the stack
     };
 
-    return this.aws.forEnvironment(env, Mode.ForWriting, {
+    const sdk = await this.aws.forEnvironment(env, Mode.ForWriting, {
       assumeRoleArn: options.assumeRoleArn,
       assumeRoleExternalId: options.assumeRoleExternalId,
     });
+    this.sdkCache.set(cacheKey, sdk);
+
+    return sdk;
   }
 }
 
