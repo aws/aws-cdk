277b5ef8ee4b1d5e206c53ec3500ff7e472abdcd
Rico Huijbers <rix0rrr@gmail.com>
2021-11-15 10:57:16 +0100

    Reduce duplication in tests

--- tools/@aws-cdk/generate-examples/test/generate.test.ts
+++ tools/@aws-cdk/generate-examples/test/generate.test.ts
@@ -1,12 +1,12 @@
 import * as reflect from 'jsii-reflect';
 
 import { generateAssignmentStatement } from '../lib/generate';
-import { AssemblyFixture, DUMMY_ASSEMBLY_TARGETS } from './testutil';
+import { AssemblyFixture, DUMMY_ASSEMBLY_TARGETS, MultipleSources } from './testutil';
 
 describe('generateClassAssignment ', () => {
-  test('generates example for class with static methods', async () => {
-    const assembly = await AssemblyFixture.fromSource(
-      {
+  test('generates example for class with static methods',
+    expectedDocTest({
+      sources: {
         'index.ts': `
         export class ClassA {
           public static firstMethod() { return new ClassA(); }
@@ -15,24 +15,18 @@ describe('generateClassAssignment ', () => {
           private constructor() {}
         }`,
       },
-      {
-        name: 'my_assembly',
-        jsii: DUMMY_ASSEMBLY_TARGETS,
-      },
-    );
+      typeName: 'ClassA',
+      expected: [
+        'import * as my_assembly from \'my_assembly\';',
+        '',
+        'const classA = my_assembly.ClassA.firstMethod();',
+      ],
+    }),
+  );
 
-    const ts = new reflect.TypeSystem();
-    await ts.load(assembly.directory);
-
-    const type = ts.findClass('my_assembly.ClassA');
-    expect(generateAssignmentStatement(type)?.toString()).toEqual('import * as my_assembly from \'my_assembly\';\n\nconst classA = my_assembly.ClassA.firstMethod();');
-
-    await assembly.cleanup();
-  });
-
-  test('generates example for class with static properties', async () => {
-    const assembly = await AssemblyFixture.fromSource(
-      {
+  test('generates example for class with static properties',
+    expectedDocTest({
+      sources: {
         'index.ts': `
         export class ClassA {
           public static readonly FIRST_PROPERTY = new ClassA();
@@ -41,47 +35,35 @@ describe('generateClassAssignment ', () => {
           private constructor() {}
         }`,
       },
-      {
-        name: 'my_assembly',
-        jsii: DUMMY_ASSEMBLY_TARGETS,
-      },
-    );
+      typeName: 'ClassA',
+      expected: [
+        'import * as my_assembly from \'my_assembly\';',
+        '',
+        'const classA = my_assembly.ClassA.FIRST_PROPERTY;',
+      ],
+    }),
+  );
 
-    const ts = new reflect.TypeSystem();
-    await ts.load(assembly.directory);
-
-    const type = ts.findClass('my_assembly.ClassA');
-    expect(generateAssignmentStatement(type)?.toString()).toEqual('import * as my_assembly from \'my_assembly\';\n\nconst classA = my_assembly.ClassA.FIRST_PROPERTY;');
-
-    await assembly.cleanup();
-  });
-
-  test('generates example for class instantiation', async () => {
-    const assembly = await AssemblyFixture.fromSource(
-      {
+  test('generates example for class instantiation',
+    expectedDocTest({
+      sources: {
         'index.ts': `
         export class ClassA {
           public constructor(public readonly a: string, public readonly b: string) {}
         }`,
       },
-      {
-        name: 'my_assembly',
-        jsii: DUMMY_ASSEMBLY_TARGETS,
-      },
-    );
+      typeName: 'ClassA',
+      expected: [
+        'import * as my_assembly from \'my_assembly\';',
+        '',
+        'const classA = new my_assembly.ClassA(\'a\', \'b\');',
+      ],
+    }),
+  );
 
-    const ts = new reflect.TypeSystem();
-    await ts.load(assembly.directory);
-
-    const type = ts.findClass('my_assembly.ClassA');
-    expect(generateAssignmentStatement(type)?.toString()).toEqual('import * as my_assembly from \'my_assembly\';\n\nconst classA = new my_assembly.ClassA(\'a\', \'b\');');
-
-    await assembly.cleanup();
-  });
-
-  test('generates example for more complicated class instantiation', async () => {
-    const assembly = await AssemblyFixture.fromSource(
-      {
+  test('generates example for more complicated class instantiation',
+    expectedDocTest({
+      sources: {
         'index.ts': `
         export class ClassA {
           public constructor(public readonly scope: string, public readonly id: string, public readonly props: ClassAProps) {}
@@ -104,39 +86,29 @@ describe('generateClassAssignment ', () => {
         }
         `,
       },
-      {
-        name: 'my_assembly',
-        jsii: DUMMY_ASSEMBLY_TARGETS,
-      },
-    );
-
-    const ts = new reflect.TypeSystem();
-    await ts.load(assembly.directory);
-
-    const type = ts.findClass('my_assembly.ClassA');
-    expect(generateAssignmentStatement(type)?.toString()?.split('\n')).toEqual([
-      'import * as my_assembly from \'my_assembly\';',
-      '',
-      'declare const prop5: any;',
-      'declare const property: my_assembly.Property;',
-      '',
-      'const classA = new my_assembly.ClassA(this, \'MyClassA\', {',
-      '  prop1: 123,',
-      '  prop2: property,',
-      '  prop3: [\'prop3\'],',
-      '  prop4: \'prop4\',',
-      '',
-      '  // the properties below are optional',
-      '  prop5: prop5,',
-      '  prop6: false,',
-      '  prop7: {',
-      '    prop7Key: \'prop7\',',
-      '  },',
-      '});',
-    ]);
-
-    await assembly.cleanup();
-  });
+      typeName: 'ClassA',
+      expected: [
+        'import * as my_assembly from \'my_assembly\';',
+        '',
+        'declare const prop5: any;',
+        'declare const property: my_assembly.Property;',
+        '',
+        'const classA = new my_assembly.ClassA(this, \'MyClassA\', {',
+        '  prop1: 123,',
+        '  prop2: property,',
+        '  prop3: [\'prop3\'],',
+        '  prop4: \'prop4\',',
+        '',
+        '  // the properties below are optional',
+        '  prop5: prop5,',
+        '  prop6: false,',
+        '  prop7: {',
+        '    prop7Key: \'prop7\',',
+        '  },',
+        '});',
+      ],
+    }),
+  );
 
   test('returns undefined if class has no statics and private initializer', async () => {
     const assembly = await AssemblyFixture.fromSource(
@@ -161,9 +133,9 @@ describe('generateClassAssignment ', () => {
     await assembly.cleanup();
   });
 
-  test('optional properties are added in the correct spot', async () => {
-    const assembly = await AssemblyFixture.fromSource(
-      {
+  test('optional properties are added in the correct spot',
+    expectedDocTest({
+      sources: {
         'index.ts': `
         export class ClassA {
           public constructor(public readonly scope: string, public readonly id: string, public readonly props: ClassAProps) {}
@@ -174,34 +146,24 @@ describe('generateClassAssignment ', () => {
         }
         `,
       },
-      {
-        name: 'my_assembly',
-        jsii: DUMMY_ASSEMBLY_TARGETS,
-      },
-    );
+      typeName: 'ClassA',
+      expected: [
+        'import * as my_assembly from \'my_assembly\';',
+        '',
+        'const classA = new my_assembly.ClassA(this, \'MyClassA\', {',
+        '  prop1: 123,',
+        '',
+        '  // the properties below are optional',
+        '  prop2: 123,',
+        '});',
+      ],
+    }),
+  );
 
-    const ts = new reflect.TypeSystem();
-    await ts.load(assembly.directory);
-
-    const type = ts.findClass('my_assembly.ClassA');
-    expect(generateAssignmentStatement(type)?.toString()?.split('\n')).toEqual([
-      'import * as my_assembly from \'my_assembly\';',
-      '',
-      'const classA = new my_assembly.ClassA(this, \'MyClassA\', {',
-      '  prop1: 123,',
-      '',
-      '  // the properties below are optional',
-      '  prop2: 123,',
-      '});',
-    ]);
-
-    await assembly.cleanup();
-  });
-
-  test('comment added when all properties are optional', async () => {
-    const comment = 'all optional props';
-    const assembly = await AssemblyFixture.fromSource(
-      {
+  test(
+    'comment added when all properties are optional',
+    expectedDocTest({
+      sources: {
         'index.ts': `
         export class ClassA {
           public constructor(public readonly scope: string, public readonly id: string, public readonly props: ClassAProps = {}) {}
@@ -212,25 +174,120 @@ describe('generateClassAssignment ', () => {
         }
         `,
       },
+      typeName: 'ClassA',
+      expected: [
+        'import * as my_assembly from \'my_assembly\';',
+        '',
+        'const classA = new my_assembly.ClassA(this, \'MyClassA\', /* all optional props */ {',
+        '  prop1: 123,',
+        '  prop2: 123,',
+        '});',
+      ],
+    }),
+  );
+});
+
+test(
+  'generate example for struct',
+  expectedDocTest({
+    sources: {
+      'index.ts': `
+      export interface SomeStruct {
+        readonly required: string;
+        readonly optional?: number;
+      }
+      `,
+    },
+    typeName: 'SomeStruct',
+    expected: [
+      'import * as my_assembly from \'my_assembly\';',
+      '',
+      'const someStruct: my_assembly.SomeStruct = {',
+      '  required: \'required\',',
+      '',
+      '  // the properties below are optional',
+      '  optional: 123,',
+      '};',
+    ],
+  }),
+);
+
+test(
+  'rendering an enum value',
+  expectedDocTest({
+    sources: {
+      'index.ts': `
+        export interface SomeStruct {
+          readonly someEnum: MyEnum;
+        }
+        export enum MyEnum {
+          VALUE1 = 1,
+          VALUE2 = 2,
+        }
+      `,
+    },
+    typeName: 'SomeStruct',
+    expected: [
+      'import * as my_assembly from \'my_assembly\';',
+      '',
+      'const someStruct: my_assembly.SomeStruct = {',
+      '  someEnum: my_assembly.MyEnum.VALUE1,',
+      '};',
+    ],
+  }),
+);
+
+test('rendering types in namespaces', expectedDocTest({
+  sources: {
+    'index.ts': `export namespace spacename {
+        export interface SomeStruct {
+          readonly someEnum: MyEnum;
+        }
+        export enum MyEnum {
+          VALUE1 = 1,
+          VALUE2 = 2,
+        }
+      }
+    `,
+  },
+  typeName: 'spacename.SomeStruct',
+  expected: [
+    'import * as my_assembly from \'my_assembly\';',
+    '',
+    'const someStruct: my_assembly.spacename.SomeStruct = {',
+    '  someEnum: my_assembly.spacename.MyEnum.VALUE1,',
+    '};',
+  ],
+}));
+
+interface DocTest {
+  readonly sources: MultipleSources;
+  readonly typeName: string;
+  readonly expected: string[];
+}
+
+function expectedDocTest(testParams: DocTest) {
+  return async () => {
+    const assembly = await AssemblyFixture.fromSource(
+      testParams.sources,
       {
         name: 'my_assembly',
         jsii: DUMMY_ASSEMBLY_TARGETS,
       },
     );
+    try {
+      const ts = new reflect.TypeSystem();
+      await ts.load(assembly.directory);
 
-    const ts = new reflect.TypeSystem();
-    await ts.load(assembly.directory);
-
-    const type = ts.findClass('my_assembly.ClassA');
-    expect(generateAssignmentStatement(type)?.toString()?.split('\n')).toEqual([
-      'import * as my_assembly from \'my_assembly\';',
-      '',
-      `const classA = new my_assembly.ClassA(this, \'MyClassA\', /* ${comment} */ {`,
-      '  prop1: 123,',
-      '  prop2: 123,',
-      '});',
-    ]);
-
-    await assembly.cleanup();
-  });
-});
+      const type = ts.findFqn(`my_assembly.${testParams.typeName}`);
+      // eslint-disable-next-line no-console
+      console.log(type.spec);
+      if (!type.isClassType() && !type.isInterfaceType()) {
+        throw new Error('Expecting class or interface');
+      }
+      expect(generateAssignmentStatement(type)?.toString()?.split('\n')).toEqual(testParams.expected);
+    } finally {
+      await assembly.cleanup();
+    }
+  };
+}
