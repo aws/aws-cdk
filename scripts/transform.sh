3537b94c4617bca20270236667d7c31e79880f85
Nick Lynch <nlynch@amazon.com>
2021-11-16 10:38:39 +0000

    chore: renaming to TRANSFORM_CONCURRENCY

--- scripts/transform.sh
+++ scripts/transform.sh
@@ -59,6 +59,6 @@ cd "$individual_packages_folder"
 createSymlinks "$individual_packages_folder"
 
 if [ "$skip_build" != "true" ]; then
-  CONCURRENCY=${CONCURRENCY:-4}
-  PHASE=transform yarn lerna run --stream --concurrency ${CONCURRENCY} $runtarget
+  TRANSFORM_CONCURRENCY=${TRANSFORM_CONCURRENCY:-4}
+  PHASE=transform yarn lerna run --stream --concurrency ${TRANSFORM_CONCURRENCY} $runtarget
 fi
