927ce96e77fa7a349eb811aad77fda85dac02f01
Niranjan Jayakar <nija@amazon.com>
2021-11-17 10:49:26 +0000

    fix incorrect merge resolutions

--- packages/@aws-cdk/core/test/private/tree-metadata.test.ts
+++ packages/@aws-cdk/core/test/private/tree-metadata.test.ts
@@ -338,7 +338,7 @@ describe('tree metadata', () => {
 
     const treenode = app.node.findChild('Tree');
 
-    const warn = treenode.node.metadataEntry.find((md) => {
+    const warn = treenode.node.metadata.find((md) => {
       return md.type === cxschema.ArtifactMetadataEntryType.WARN
         && /Forcing an inspect error/.test(md.data as string)
         && /mycfnresource/.test(md.data as string);
