9f00cdc31e6b927ea4d3b0af5d42ffbb7a7923d7
Markus <mail@markussiebert.com>
2021-11-19 23:20:20 +0100

    fix comma dangle

--- packages/@aws-cdk/aws-docdb/test/cluster.test.ts
+++ packages/@aws-cdk/aws-docdb/test/cluster.test.ts
@@ -333,7 +333,7 @@ describe('DatabaseCluster', () => {
 
     // THEN
     expectCDK(stack).to(haveResourceLike('AWS::SecretsManager::Secret', {
-      Namr: '/myapp/mydocdb/masteruser'
+      Name: '/myapp/mydocdb/masteruser',
     }));
   });
 
