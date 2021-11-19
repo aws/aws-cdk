34af5988b7c1ff003d10612150191803f762a79f
nom3ad <19239479+nom3ad@users.noreply.github.com>
2021-11-20 05:15:43 +0530

    fix(opensearch): correctly validate ebs configuration against instance types  (#16911)
    Currently, the following valid cluster configurations are not possible to be created with CDK.
    
    ```ts
    new es.Domain(stack, 'Domain1', {
        version: es.EngineVersion.OPENSEARCH_1_0,
        ebs: { enabled: false },
        capacity: {
            masterNodes: 3,
            masterNodeInstanceType: 'c5.2xlarge.search',
            dataNodeInstanceType: 'i3.2xlarge.search'
        }
    });
    
    
    new es.Domain(stack, 'Domain2', {
        version: es.EngineVersion.OPENSEARCH_1_0,
        ebs: { enabled: false, },
        capacity: {
            masterNodes: 3,
            masterNodeInstanceType: 'c6g.large.search',
            dataNodeInstanceType: 'r6gd.large.search'
        }
    });
    ```
    Throws error:
    `EBS volumes are required when using instance types other than r3, i3 or r6gd.`
    
    Here, EBS validation is being checked on master nodes instead of just the data nodes -  which causes the above failure.
    
    This PR applies fix on both `elasticsearch` and `opensearchservice` modules
    
    Fixes #11898 
    
    ----
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-opensearchservice/lib/domain.ts
+++ packages/@aws-cdk/aws-opensearchservice/lib/domain.ts
@@ -1320,9 +1320,8 @@ export class Domain extends DomainBase implements IDomain, ec2.IConnectable {
       return instanceTypes.some(isInstanceType);
     };
 
-    function isEveryInstanceType(...instanceTypes: string[]): Boolean {
-      return instanceTypes.some(t => dedicatedMasterType.startsWith(t))
-        && instanceTypes.some(t => instanceType.startsWith(t));
+    function isEveryDatanodeInstanceType(...instanceTypes: string[]): Boolean {
+      return instanceTypes.some(t => instanceType.startsWith(t));
     };
 
     // Validate feature support for the given Elasticsearch/OpenSearch version, per
@@ -1396,7 +1395,7 @@ export class Domain extends DomainBase implements IDomain, ec2.IConnectable {
 
     // Only R3, I3 and r6gd support instance storage, per
     // https://aws.amazon.com/opensearch-service/pricing/
-    if (!ebsEnabled && !isEveryInstanceType('r3', 'i3', 'r6gd')) {
+    if (!ebsEnabled && !isEveryDatanodeInstanceType('r3', 'i3', 'r6gd')) {
       throw new Error('EBS volumes are required when using instance types other than r3, i3 or r6gd.');
     }
 
