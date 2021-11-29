b9898e9cc1278ad44f6bc2d3eba298afc1200312
robertd <robert.djurasaj@gmail.com>
2021-11-29 09:44:05 -0700

    feat(ec2): add vt1 instances

--- packages/@aws-cdk/aws-ec2/lib/instance-types.ts
+++ packages/@aws-cdk/aws-ec2/lib/instance-types.ts
@@ -585,6 +585,11 @@ export enum InstanceClass {
    * Macintosh instances built on Apple Mac mini computers, 1st generation with Intel procesors
    */
   MAC1 = 'mac1',
+
+  /**
+   * Multi-stream video transcoding instances for resolutions up to 4K UHD, 1st generation
+   */
+  VT1 = 'vt1',
 }
 
 /**
