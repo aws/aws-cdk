2fc68954cfbc3c65694e767b00a2318f9cc4a501
Rico Huijbers <rix0rrr@gmail.com>
2021-12-14 14:44:36 +0100

    fix(cli): asset publishing broken cross account (#18007)
    In #17668, cross-account S3 asset publishing was broken.
    
    The reason is that the `account()` function was always broken, using the
    default account instead of the target account. However, previously this
    function was only called in an irrecoverable situation anyway, and its
    failure would be rare.
    
    The recent change also calls this function for logging purposes in
    a happy-case scenario, but then triggers an error during the logging.
    
    Fix the invocation to use the right account.
    
    Fixes #17988.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/cdk-assets/test/fake-listener.ts
@@ -0,0 +1,16 @@
+import { IPublishProgressListener, EventType, IPublishProgress } from '../lib/progress';
+
+export class FakeListener implements IPublishProgressListener {
+  public readonly messages = new Array<string>();
+
+  constructor(private readonly doAbort = false) {
+  }
+
+  public onPublishEvent(_type: EventType, event: IPublishProgress): void {
+    this.messages.push(event.message);
+
+    if (this.doAbort) {
+      event.abort();
+    }
+  }
+}
