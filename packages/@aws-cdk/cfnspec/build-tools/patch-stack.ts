5e60cb8837d49df7233790d08f8ef77e1a0a56b1
Rico Huijbers <rix0rrr@gmail.com>
2021-12-29 14:04:57 +0100

    chore(cfnspec): consume CloudFormation specification in parts
    Whenever there are errors in the CloudFormation specification, we
    currently have to fail the build and can't consume anything.
    
    To ensure we make some progress, apply the following strategy instead:
    
    - Split the spec into fragments, on a per-service basis.
    - Consume those per-service spec updates that are valid; if updates
      are invalid, we will leave them at the old version.
    
    This will produce an always-building spec, of which certain parts
    may be outdated. Report the outdated parts in the CHANGELOG.
    
    Notifying the CloudFormation team about spec errors is an out-of-band
    process, and out of scope of this PR.
    
    As a side effect of this work, formalize the spec manipulation we do
    with JSON and patch files into a mini-standard called "JSON Patch
    Stacks", and add some tools to operate on them.

---
+++ packages/@aws-cdk/cfnspec/build-tools/patch-stack.ts
@@ -0,0 +1,232 @@
+
+/**
+ * Apply a JSON patch stack into the given target file
+ *
+ * The sources can be taken from one or more directories.
+ */
+import * as path from 'path';
+import * as fastJsonPatch from 'fast-json-patch';
+import * as fs from 'fs-extra';
+
+export interface PatchOptions {
+  readonly quiet?: boolean;
+}
+
+/**
+ * Apply a stack to an existing model
+ */
+export async function loadStack(sourceDirectory: string, options: PatchOptions = {}) {
+  const targetObject: any = {};
+
+  const files = await fs.readdir(sourceDirectory);
+  for (const file of files.sort()) {
+    const fullFile = path.join(sourceDirectory, file);
+    const relName = path.relative(process.cwd(), fullFile);
+
+    let data;
+    if ((await fs.stat(fullFile)).isDirectory()) {
+      data = await loadStack(fullFile, options);
+    } else if (file.endsWith('.json')) {
+      data = await fs.readJson(fullFile);
+    } else {
+      continue;
+    }
+
+    if (file.indexOf('patch') === -1) {
+      if (!options.quiet) {
+        // eslint-disable-next-line no-console
+        console.log(relName);
+      }
+
+      // Copy properties from current object into structure, adding/overwriting whatever is found
+      merge(targetObject, data, []);
+    } else {
+      // Apply the loaded file as a patch onto the current structure
+      patch(targetObject, data, (x) => {
+        if (!options.quiet) {
+          // eslint-disable-next-line no-console
+          console.log(`${relName}: ${x}`);
+        }
+      });
+    }
+  }
+  return targetObject;
+}
+
+/**
+ * Combine a number of stacks and write a file
+ *
+ * Stacks are loaded individually, and merged afterwards.
+ */
+export async function resolveStack(targetFile: string, sourceDirectory: string, options: PatchOptions = {}) {
+  const stack = await loadStack(sourceDirectory, options);
+  await writeSorted(targetFile, stack);
+}
+
+export async function writeSorted(targetFile: string, data: any) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const sortJson = require('sort-json');
+  await fs.mkdirp(path.dirname(targetFile));
+  data = sortJson(data);
+  await fs.writeJson(targetFile, data, { spaces: 2 });
+}
+
+function printSorted(data: any) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const sortJson = require('sort-json');
+  data = sortJson(data);
+  process.stdout.write(JSON.stringify(data, undefined, 2));
+}
+
+function merge(target: any, fragment: any, jsonPath: string[]) {
+  if (!fragment) { return; }
+  for (const key of Object.keys(fragment)) {
+    if (key.startsWith('$')) { continue; }
+
+    if (key in target) {
+      const specVal = target[key];
+      const fragVal = fragment[key];
+      if (typeof specVal !== typeof fragVal) {
+        // eslint-disable-next-line max-len
+        throw new Error(`Attempted to merge ${JSON.stringify(fragVal)} into incompatible ${JSON.stringify(specVal)} at path ${jsonPath.join('/')}/${key}`);
+      }
+      if (typeof specVal !== 'object' && specVal !== fragVal) {
+        // eslint-disable-next-line max-len
+        throw new Error(`Conflict when attempting to merge ${JSON.stringify(fragVal)} into ${JSON.stringify(specVal)} at path ${jsonPath.join('/')}/${key}`);
+      }
+      merge(specVal, fragVal, [...jsonPath, key]);
+    } else {
+      target[key] = fragment[key];
+    }
+  }
+}
+
+function patch(target: any, fragment: any, log: (x: string) => void) {
+  if (!fragment) { return; }
+
+  const patches = findPatches(target, fragment);
+  for (const p of patches) {
+    log(p.description ?? '');
+
+    try {
+      fastJsonPatch.applyPatch(target, p.operations);
+    } catch (e) {
+      throw new Error(`error applying patch: ${JSON.stringify(p, undefined, 2)}: ${e.message}`);
+    }
+  }
+}
+
+interface Patch {
+  readonly description?: string;
+  readonly operations: Operation[];
+}
+
+type Operation =
+  | { readonly op: 'add'; readonly path: string; readonly value: any }
+  | { readonly op: 'remove'; readonly path: string }
+  | { readonly op: 'replace'; readonly path: string; readonly value: any }
+  | { readonly op: 'copy'; readonly path: string; readonly from: string }
+  | { readonly op: 'move'; readonly path: string; readonly from: string }
+  | { readonly op: 'test'; readonly path: string; readonly value: any }
+  ;
+
+/**
+ * Find the sets of patches to apply in a document
+ *
+ * Adjusts paths to be root-relative, which makes it possible to have paths
+ * point outside the patch scope.
+ */
+function findPatches(data: any, patchSource: any): Patch[] {
+  const ret: Patch[] = [];
+  recurse(data, patchSource, []);
+  return ret;
+
+  function recurse(actualData: any, fragment: any, jsonPath: string[]) {
+    if (!fragment) { return; }
+
+    if ('patch' in fragment) {
+      ret.push({
+        description: fragment.patch.description,
+        operations: fragment.patch.operations.map((op: any) => adjustPaths(op, jsonPath)),
+      });
+    } else if ('patch:each' in fragment) {
+      const p = fragment['patch:each'];
+      if (typeof actualData !== 'object') {
+        throw new Error(`Patch ${p.description}: expecting object in data, found '${actualData}'`);
+      }
+      for (const key in actualData) {
+        ret.push({
+          description: `${key}: ${p.description}`,
+          operations: p.operations.map((op: any) => adjustPaths(op, [...jsonPath, key])),
+        });
+      }
+    } else {
+      for (const key of Object.keys(fragment)) {
+        recurse(actualData[key], fragment[key], [...jsonPath, key]);
+      }
+    }
+  }
+
+  function adjustPaths(op: any, jsonPath: string[]): Operation {
+    return {
+      ...op,
+      ...op.path ? { path: adjustPath(op.path, jsonPath) } : undefined,
+      ...op.from ? { from: adjustPath(op.from, jsonPath) } : undefined,
+    };
+  }
+
+  /**
+   * Adjust path
+   *
+   * '$/' means from the root, otherwise interpret as relative path.
+   */
+  function adjustPath(originalPath: string, jsonPath: string[]): string {
+    if (typeof originalPath !== 'string') {
+      throw new Error(`adjustPath: expected string, got ${JSON.stringify(originalPath)}`);
+    }
+    if (originalPath.startsWith('$/')) {
+      return originalPath.substr(1);
+    }
+    return jsonPath.map(p => `/${p}`).join('') + originalPath;
+  }
+}
+
+
+/**
+ * Run this file as a CLI tool, to apply a patch stack from the command line
+ *
+ * Usage: patch-stack <TARGET_FILE> <DIR> [DIR [...]]
+ */
+async function main(args: string[]) {
+  const quiet = eatArg('-q', args) || eatArg('--quiet', args);
+
+  if (args.length < 1) {
+    throw new Error('Usage: patch-stack <DIR> [<FILE>]');
+  }
+
+  const [dir, targetFile] = args;
+
+  if (targetFile) {
+    await resolveStack(targetFile, dir, { quiet });
+  } else {
+    printSorted(await loadStack(dir, { quiet }));
+  }
+}
+
+function eatArg(arg: string, args: string[]) {
+  for (let i = 0; i < args.length && args[i] !== '--'; i++) {
+    if (args[i] === arg) {
+      args.splice(i, 1);
+      return true;
+    }
+  }
+  return false;
+}
+
+if (require.main === module) {
+  main(process.argv.slice(2)).catch(e => {
+    process.exitCode = 1;
+    // eslint-disable-next-line no-console
+    console.error(e.message);
+  });
+}
