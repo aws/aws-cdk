8b87d5e534b24f547d7e4afa7b0575d6c25834fe
Ben Limmer <ben@benlimmer.com>
2022-01-03 04:30:32 -0700

    docs(cli): improve upgrade message (#18195)
    This PR relates to #18024. It adds information on upgrading between major versions to the upgrade banner.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk/test/version.test.ts
+++ packages/aws-cdk/test/version.test.ts
@@ -4,6 +4,7 @@ import { promisify } from 'util';
 import * as fs from 'fs-extra';
 import * as sinon from 'sinon';
 import * as logging from '../lib/logging';
+import * as npm from '../lib/util/npm';
 import { latestVersionIfHigher, VersionCheckTTL, displayVersionMessage } from '../lib/version';
 
 jest.setTimeout(10_000);
@@ -77,9 +78,61 @@ test('No Version specified for storage in the TTL file', async () => {
 });
 
 test('Skip version check if environment variable is set', async () => {
-  process.stdout.isTTY = true;
-  process.env.CDK_DISABLE_VERSION_CHECK = '1';
+  sinon.stub(process, 'stdout').value({ ...process.stdout, isTTY: true });
+  sinon.stub(process, 'env').value({ ...process.env, CDK_DISABLE_VERSION_CHECK: '1' });
   const printStub = sinon.stub(logging, 'print');
   await displayVersionMessage();
   expect(printStub.called).toEqual(false);
+});
+
+describe('version message', () => {
+  let previousIsTty: true | undefined;
+  beforeAll(() => {
+    previousIsTty = process.stdout.isTTY;
+    process.stdout.isTTY = true;
+  });
+
+  afterAll(() => {
+    process.stdout.isTTY = previousIsTty;
+  });
+
+  test('Prints a message when a new version is available', async () => {
+    // Given the current version is 1.0.0 and the latest version is 1.1.0
+    const currentVersion = '1.0.0';
+    jest.spyOn(npm, 'getLatestVersionFromNpm').mockResolvedValue('1.1.0');
+    const printSpy = jest.spyOn(logging, 'print');
+
+    // When displayVersionMessage is called
+    await displayVersionMessage(currentVersion, new VersionCheckTTL(tmpfile(), 0));
+
+    // Then the new version message is printed to stdout
+    expect(printSpy).toHaveBeenCalledWith(expect.stringContaining('1.1.0'));
+  });
+
+  test('Includes major upgrade documentation when available', async() => {
+    // Given the current version is 1.0.0 and the latest version is 2.0.0
+    const currentVersion = '1.0.0';
+    jest.spyOn(npm, 'getLatestVersionFromNpm').mockResolvedValue('2.0.0');
+    const printSpy = jest.spyOn(logging, 'print');
+
+    // When displayVersionMessage is called
+    await displayVersionMessage(currentVersion, new VersionCheckTTL(tmpfile(), 0));
+
+    // Then the V1 -> V2 documentation is printed
+    expect(printSpy).toHaveBeenCalledWith(expect.stringContaining('Information about upgrading from version 1.x to version 2.x is available here: https://docs.aws.amazon.com/cdk/v2/guide/migrating-v2.html'));
+  });
+
+  test('Does not include major upgrade documentation when unavailable', async() => {
+    // Given current version is 99.0.0 and the latest version is 100.0.0
+    const currentVersion = '99.0.0';
+    jest.spyOn(npm, 'getLatestVersionFromNpm').mockResolvedValue('100.0.0');
+    const printSpy = jest.spyOn(logging, 'print');
+
+    // When displayVersionMessage is called
+    await displayVersionMessage(currentVersion, new VersionCheckTTL(tmpfile(), 0));
+
+    // Then no upgrade documentation is printed
+    expect(printSpy).toHaveBeenCalledWith(expect.stringContaining('100.0.0'));
+    expect(printSpy).not.toHaveBeenCalledWith(expect.stringContaining('Information about upgrading from 99.x to 100.x'));
+  });
 });
