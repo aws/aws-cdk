204d0a51ecdf56e946163f78f31a57c97b9c7b4a
dyoshikawa <34151621+dyoshikawa@users.noreply.github.com>
2022-01-04 12:28:51 +0900

    feat(iot): Action to send messages to SQS queues (#18087)
    Fixes #17699
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-iot-actions/lib/index.ts
+++ packages/@aws-cdk/aws-iot-actions/lib/index.ts
@@ -5,3 +5,5 @@ export * from './common-action-props';
 export * from './firehose-stream-action';
 export * from './lambda-function-action';
 export * from './s3-put-object-action';
+export * from './sqs-queue-action';
+
