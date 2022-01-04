f35b9f2181ddc3ad61369048a1a49f6bee58ebe7
Rico Huijbers <rix0rrr@gmail.com>
2022-01-04 12:41:35 +0100

    chore(cfnspec): consume CloudFormation specification in parts (#18210)
    Whenever there are errors in the CloudFormation specification, we
    currently have to fail the build and can't consume anything.
    
    To ensure we make some progress, apply the following strategy instead:
    
    - Split the spec into fragments, on a per-service basis.
    - Consume those per-service spec updates that are valid; if updates
      are invalid, we will leave them at the old version.
    
    This will produce an always-building spec, of which certain parts
    may be outdated. Report the outdated parts in the CHANGELOG.
    
    Notifying the CloudFormation team about spec errors is an out-of-band
    process, and out of scope of this PR.
    
    As a side effect of this work, formalize the spec manipulation we do
    with JSON and patch files into a mini-standard called "JSON Patch
    Stacks", and add some tools to operate on them.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/cfnspec/build-tools/update.sh
+++ packages/@aws-cdk/cfnspec/build-tools/update.sh
@@ -13,51 +13,67 @@ rm -f CHANGELOG.md.new
 function update-spec() {
     local title=$1
     local url=$2
-    local target=$3
+    local targetdir=$3
     local gunzip=$4
+    local split=$5
 
-    local intermediate="$(mktemp -d)/new.json"
+    local tmpdir="$(mktemp -d)"
+    local newspec="${tmpdir}/new_proposed.json"
+    local newcombined="${tmpdir}/new.json"
+    local oldcombined="${tmpdir}/old.json"
 
     # fail if the spec has changes, otherwise we won't be able to determine the diff
-    if [ -n "$(git status --porcelain ${target})" ]; then
-        echo "The file ${target} has changes, revert them before cfn-update"
+    if [ -n "$(git status --porcelain ${targetdir})" ]; then
+        echo "The directory ${targetdir} has changes, revert them before cfn-update"
         exit 1
     fi
 
     echo >&2 "Downloading from ${url}..."
     if ${gunzip}; then
-        curl -sL "${url}" | gunzip - > ${intermediate}
+        curl -sL "${url}" | gunzip - > ${newspec}
     else
-        curl -sL "${url}" > ${intermediate}
+        curl -sL "${url}" > ${newspec}
     fi
 
-    echo >&2 "Sorting..."
-    sort-json ${intermediate}
+    # Calculate the old and new combined specs, so we can do a diff on the changes
+    echo >&2 "Updating source spec..."
+
+    node build-tools/patch-set.js --quiet "${targetdir}" "${oldcombined}"
+    if ${split}; then
+        node build-tools/split-spec-by-service.js "${newspec}" "${targetdir}"
+    else
+        cp "${newspec}" "${targetdir}/spec.json"
+        sort-json "${targetdir}/spec.json"
+    fi
+    node build-tools/patch-set.js --quiet "${targetdir}" "${newcombined}"
 
     echo >&2 "Updating CHANGELOG.md..."
-    node build-tools/spec-diff.js "${title}" "${target}" "${intermediate}" >> CHANGELOG.md.new
-    echo "" >> CHANGELOG.md.new
+    node build-tools/spec-diff.js "${title}" "${oldcombined}" "${newcombined}" >> CHANGELOG.md.new
 
-    echo >&2 "Updating source spec..."
-    rm -f ${target}
-    cp ${intermediate} ${target}
+    echo "" >> CHANGELOG.md.new
 }
+
+# First run a build to get a complete version of the old spec
 
 update-spec \
     "CloudFormation Resource Specification" \
     "https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json" \
-    spec-source/000_CloudFormationResourceSpecification.json \
-    true
+    spec-source/specification/000_cfn/000_official \
+    true true
 
 echo >&2 "Recording new version..."
 rm -f cfn.version
-node -p "require('${scriptdir}/../spec-source/000_CloudFormationResourceSpecification.json').ResourceSpecificationVersion" > cfn.version
+node -p "require('${scriptdir}/../spec-source/specification/000_cfn/000_official/001_Version.json').ResourceSpecificationVersion" > cfn.version
+
+
+echo >&2 "Reporting outdated specs..."
+node build-tools/report-issues spec-source/specification/000_cfn/000_official/ outdated >> CHANGELOG.md.new
 
 update-spec \
     "Serverless Application Model (SAM) Resource Specification" \
     "https://raw.githubusercontent.com/awslabs/goformation/master/generate/sam-2016-10-31.json" \
-    spec-source/000_sam.spec.json \
-    false
+    spec-source/specification/100_sam/000_official \
+    false false
 
 npm run build
 
