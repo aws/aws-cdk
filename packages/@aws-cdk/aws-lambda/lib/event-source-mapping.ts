ad4f2f1cfe465ee3f169739297f6d8ed5844a359
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-01-08 12:39:17 +1300

    fix(lambda): add new line on comment

--- packages/@aws-cdk/aws-lambda/lib/event-source-mapping.ts
+++ packages/@aws-cdk/aws-lambda/lib/event-source-mapping.ts
@@ -36,7 +36,8 @@ export class SourceAccessConfigurationType {
   public static readonly SASL_SCRAM_512_AUTH = new SourceAccessConfigurationType('SASL_SCRAM_512_AUTH');
 
   /**
-   * The Secrets Manager ARN of your secret key containing the certificate chain (X.509 PEM), private key (PKCS#8 PEM), and private key password (optional) used for mutual TLS authentication of your MSK/Apache Kafka brokers.
+   * The Secrets Manager ARN of your secret key containing the certificate chain (X.509 PEM), private key (PKCS#8 PEM), 
+   * and private key password (optional) used for mutual TLS authentication of your MSK/Apache Kafka brokers.
    */
   public static readonly CLIENT_CERTIFICATE_TLS_AUTH = new SourceAccessConfigurationType('CLIENT_CERTIFICATE_TLS_AUTH'),
 
