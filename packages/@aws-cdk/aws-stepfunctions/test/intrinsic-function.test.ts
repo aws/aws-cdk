66ef4c5fda3bf58f28504a9b07a578024dcc6fec
w-haibara <hwhaibarawataru@gmail.com>
2022-01-14 01:15:55 +0900

    fix typo

--- packages/@aws-cdk/aws-stepfunctions/test/intrinsic-function.test.ts
+++ packages/@aws-cdk/aws-stepfunctions/test/intrinsic-function.test.ts
@@ -8,7 +8,7 @@ describe('States.Format()', () => {
   test('format string with nuber, string', () => {
     expect(IntrinsicFunction.StatesForamt('1{}2{}3', 'str', 123)).
       toEqual('States.Format(\'1{}2{}3\', \'str\', 123)');
-  });
+  }),
   test('format string with nuber, string and JsonPath', () => {
     expect(IntrinsicFunction.StatesForamt('1{}2{}3{}4', 'str', 123, '$.path')).
       toEqual('States.Format(\'1{}2{}3{}4\', \'str\', 123, $.path)');
