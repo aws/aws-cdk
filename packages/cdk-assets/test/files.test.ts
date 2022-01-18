3520c09ac4dda940a36ea245f1af1c8bcf219dc6
Rico Huijbers <rix0rrr@gmail.com>
2022-01-18 22:50:22 +0100

    chore: prevent empty zip uploads (#18487)
    Due to something we haven't completely figured out yet, our asset
    packaging sometimes produces empty zip files, leading to an error
    like this uploading code Lambda:
    
    ```
    Uploaded file must be a non-empty zip
    ```
    
    Do the following to address this:
    
    * If any empty zip files already ended up in S3, do not regard this as a
      cache hit. Rebuild the asset and upload it again. We do this by
      checking the item's size: this may be overly pessimistic, but if it is
      we're probably not wasting a lot of time rebuilding and
      uploading a tiny file.
    * If a fresh asset build is producing an empty zip file, loudly complain
      and ask the user to come to our bug tracker, so we can figure out
      where these spurious issues are coming from.
    
    Relates to #18459.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/cdk-assets/test/files.test.ts
+++ packages/cdk-assets/test/files.test.ts
@@ -2,12 +2,19 @@ jest.mock('child_process');
 
 import { Manifest } from '@aws-cdk/cloud-assembly-schema';
 import * as mockfs from 'mock-fs';
-import { AssetManifest, AssetPublishing } from '../lib';
+import { AssetPublishing, AssetManifest } from '../lib';
 import { FakeListener } from './fake-listener';
 import { mockAws, mockedApiFailure, mockedApiResult, mockUpload } from './mock-aws';
 import { mockSpawn } from './mock-child_process';
 
 const ABS_PATH = '/simple/cdk.out/some_external_file';
+
+const DEFAULT_DESTINATION = {
+  region: 'us-north-50',
+  assumeRoleArn: 'arn:aws:role',
+  bucketName: 'some_bucket',
+  objectKey: 'some_key',
+};
 
 let aws: ReturnType<typeof mockAws>;
 beforeEach(() => {
@@ -21,19 +28,12 @@ beforeEach(() => {
           source: {
             path: 'some_file',
           },
-          destinations: {
-            theDestination: {
-              region: 'us-north-50',
-              assumeRoleArn: 'arn:aws:role',
-              bucketName: 'some_bucket',
-              objectKey: 'some_key',
-            },
-          },
+          destinations: { theDestination: DEFAULT_DESTINATION },
         },
       },
     }),
     '/simple/cdk.out/some_file': 'FILE_CONTENTS',
-    [ABS_PATH]: 'FILE_CONTENTS',
+    [ABS_PATH]: 'ZIP_FILE_THAT_IS_DEFINITELY_NOT_EMPTY',
     '/abs/cdk.out/assets.json': JSON.stringify({
       version: Manifest.version(),
       files: {
@@ -41,14 +41,7 @@ beforeEach(() => {
           source: {
             path: '/simple/cdk.out/some_file',
           },
-          destinations: {
-            theDestination: {
-              region: 'us-north-50',
-              assumeRoleArn: 'arn:aws:role',
-              bucketName: 'some_other_bucket',
-              objectKey: 'some_key',
-            },
-          },
+          destinations: { theDestination: { ...DEFAULT_DESTINATION, bucketName: 'some_other_bucket' } },
         },
       },
     }),
@@ -59,14 +52,7 @@ beforeEach(() => {
           source: {
             executable: ['sometool'],
           },
-          destinations: {
-            theDestination: {
-              region: 'us-north-50',
-              assumeRoleArn: 'arn:aws:role',
-              bucketName: 'some_external_bucket',
-              objectKey: 'some_key',
-            },
-          },
+          destinations: { theDestination: { ...DEFAULT_DESTINATION, bucketName: 'some_external_bucket' } },
         },
       },
     }),
@@ -77,32 +63,31 @@ beforeEach(() => {
           source: {
             path: 'plain_text.txt',
           },
-          destinations: {
-            theDestination: {
-              region: 'us-north-50',
-              assumeRoleArn: 'arn:aws:role',
-              bucketName: 'some_bucket',
-              objectKey: 'some_key.txt',
-            },
-          },
+          destinations: { theDestination: { ...DEFAULT_DESTINATION, objectKey: 'some_key.txt' } },
         },
         theImageAsset: {
           source: {
             path: 'image.png',
           },
-          destinations: {
-            theDestination: {
-              region: 'us-north-50',
-              assumeRoleArn: 'arn:aws:role',
-              bucketName: 'some_bucket',
-              objectKey: 'some_key.png',
-            },
-          },
+          destinations: { theDestination: { ...DEFAULT_DESTINATION, objectKey: 'some_key.png' } },
         },
       },
     }),
     '/types/cdk.out/plain_text.txt': 'FILE_CONTENTS',
     '/types/cdk.out/image.png': 'FILE_CONTENTS',
+    '/emptyzip/cdk.out/assets.json': JSON.stringify({
+      version: Manifest.version(),
+      files: {
+        theTextAsset: {
+          source: {
+            path: 'empty_dir',
+            packaging: 'zip',
+          },
+          destinations: { theDestination: DEFAULT_DESTINATION },
+        },
+      },
+    }),
+    '/emptyzip/cdk.out/empty_dir': { }, // Empty directory
   });
 
   aws = mockAws();
@@ -114,6 +99,7 @@ afterEach(() => {
 
 test('pass destination properties to AWS client', async () => {
   const pub = new AssetPublishing(AssetManifest.fromPath('/simple/cdk.out'), { aws, throwOnError: false });
+  aws.mockS3.listObjectsV2 = mockedApiResult({});
 
   await pub.publish();
 
@@ -127,6 +113,7 @@ test('Do nothing if file already exists', async () => {
   const pub = new AssetPublishing(AssetManifest.fromPath('/simple/cdk.out'), { aws });
 
   aws.mockS3.listObjectsV2 = mockedApiResult({ Contents: [{ Key: 'some_key' }] });
+  aws.mockS3.upload = mockUpload();
   await pub.publish();
 
   expect(aws.mockS3.listObjectsV2).toHaveBeenCalledWith(expect.objectContaining({
@@ -134,7 +121,20 @@ test('Do nothing if file already exists', async () => {
     Prefix: 'some_key',
     MaxKeys: 1,
   }));
+  expect(aws.mockS3.upload).not.toHaveBeenCalled();
 });
+
+test('tiny file does not count as cache hit', async () => {
+  const pub = new AssetPublishing(AssetManifest.fromPath('/simple/cdk.out'), { aws });
+
+  aws.mockS3.listObjectsV2 = mockedApiResult({ Contents: [{ Key: 'some_key', Size: 5 }] });
+  aws.mockS3.upload = mockUpload();
+
+  await pub.publish();
+
+  expect(aws.mockS3.upload).toHaveBeenCalled();
+});
+
 
 test('upload file if new (list returns other key)', async () => {
   const pub = new AssetPublishing(AssetManifest.fromPath('/simple/cdk.out'), { aws });
@@ -310,6 +310,18 @@ test('correctly identify asset path if path is absolute', async () => {
   expect(true).toBeTruthy(); // No exception, satisfy linter
 });
 
+test('empty directory prints failures', async () => {
+  const progressListener = new FakeListener();
+  const pub = new AssetPublishing(AssetManifest.fromPath('/emptyzip/cdk.out'), { aws, progressListener });
+
+  aws.mockS3.listObjectsV2 = mockedApiResult({ Contents: undefined });
+  aws.mockS3.upload = mockUpload(); // Should not be hit
+
+  await pub.publish();
+
+  expect(progressListener.messages).toContainEqual(expect.stringContaining('EMPTY ZIP FILE'));
+});
+
 describe('external assets', () => {
   let pub: AssetPublishing;
   beforeEach(() => {
@@ -330,7 +342,7 @@ describe('external assets', () => {
 
   test('upload external asset correctly', async () => {
     aws.mockS3.listObjectsV2 = mockedApiResult({ Contents: undefined });
-    aws.mockS3.upload = mockUpload('FILE_CONTENTS');
+    aws.mockS3.upload = mockUpload('ZIP_FILE_THAT_IS_DEFINITELY_NOT_EMPTY');
     const expectAllSpawns = mockSpawn({ commandLine: ['sometool'], stdout: ABS_PATH });
 
     await pub.publish();
