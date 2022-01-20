ae241811220ee59f9e743ee79ce321bcfd5e312f
Mischa Spiegelmock <me@mish.dev>
2022-01-20 20:42:17 +0200

    Allow setting mainFields for esbuild

--- packages/@aws-cdk/aws-lambda-nodejs/lib/bundling.ts
+++ packages/@aws-cdk/aws-lambda-nodejs/lib/bundling.ts
@@ -211,6 +211,7 @@ export class Bundling implements cdk.BundlingOptions {
       ...this.props.banner ? [`--banner:js=${JSON.stringify(this.props.banner)}`] : [],
       ...this.props.footer ? [`--footer:js=${JSON.stringify(this.props.footer)}`] : [],
       ...this.props.charset ? [`--charset=${this.props.charset}`] : [],
+      ...this.props.mainFields ? [`--main-fields=${this.props.mainFields}`] : [],
     ];
 
     let depsCommand = '';
