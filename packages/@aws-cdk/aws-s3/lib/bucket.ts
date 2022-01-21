4af39cb6fc2c0d8bdb960a894d755c264e5feb11
Peter Woodworth <44349620+peterwoodworth@users.noreply.github.com>
2022-01-21 15:53:09 -0800

    docs(s3): remove vestigial documentation (#18604)
    fixes #18575 
    Models documentation for [similar](https://docs.aws.amazon.com/cdk/api/v1/docs/@aws-cdk_aws-ec2.VpcAttributes.html) [structs](https://docs.aws.amazon.com/cdk/api/v1/docs/@aws-cdk_aws-lambda.FunctionAttributes.html)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-s3/lib/bucket.ts
+++ packages/@aws-cdk/aws-s3/lib/bucket.ts
@@ -356,9 +356,7 @@ export interface IBucket extends IResource {
 }
 
 /**
- * A reference to a bucket. The easiest way to instantiate is to call
- * `bucket.export()`. Then, the consumer can use `Bucket.import(this, ref)` and
- * get a `Bucket`.
+ * A reference to a bucket outside this stack
  */
 export interface BucketAttributes {
   /**
