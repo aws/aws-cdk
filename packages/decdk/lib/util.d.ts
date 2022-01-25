d4e72bf83651a89b631c953e57d47e85a24ad85f
Flavio Leggio <flavioleggio@gmail.com>
2022-01-25 21:07:28 +0100

    remove decdk files

--- packages/decdk/lib/util.d.ts
+++
@@ -1,7 +0,0 @@
-import * as jsiiReflect from 'jsii-reflect';
-/**
- * Reads a YAML/JSON template file.
- */
-export declare function readTemplate(templateFile: string): Promise<any>;
-export declare function loadTypeSystem(validate?: boolean): Promise<jsiiReflect.TypeSystem>;
-export declare function stackNameFromFileName(fileName: string): string;
