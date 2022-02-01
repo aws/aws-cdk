b693e3d5a80ebe2aebbc9af8ab6a5be4227c1dbb
Elad Ben-Israel <benisrae@amazon.com>
2022-02-01 21:22:27 +0200

    revert: "chore(cloudfront): encryption and enforceSSL on distribution s3 loggingBucket (#18264)" (#18772)
    #18271 resulted in the definition of a new bucket policy, which broke existing users that already had an implicit bucket policy created by AWS (see [docs](https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/AWS-logs-and-resource-policy.html#AWS-logs-infrastructure-S3)).
    
    Reverts commit ad7374a7475ea4eeb33720f0f07cdd10cd9c9702 in the meantime until we figure out the longer term solution.
    
    Fixes #18676
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-cloudfront/lib/web-distribution.ts
+++ packages/@aws-cdk/aws-cloudfront/lib/web-distribution.ts
@@ -954,10 +954,7 @@ export class CloudFrontWebDistribution extends cdk.Resource implements IDistribu
     }
 
     if (props.loggingConfig) {
-      this.loggingBucket = props.loggingConfig.bucket || new s3.Bucket(this, 'LoggingBucket', {
-        encryption: s3.BucketEncryption.S3_MANAGED,
-        enforceSSL: true,
-      });
+      this.loggingBucket = props.loggingConfig.bucket || new s3.Bucket(this, 'LoggingBucket');
       distributionConfig = {
         ...distributionConfig,
         logging: {
