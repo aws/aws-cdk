5bd11d1a462e55d50880a201df5f33fc8a3b3c17
Rico Huijbers <rix0rrr@gmail.com>
2022-02-02 11:27:13 +0100

    chore(lambda-layer-awscli): add update mechanism for AWS CLI (#18780)
    We bundle the AWS CLI. This adds a mechanism to keep up with new updates
    of the CLI. It adds a script that should be executed periodically to
    create a PR with a version update.
    
    We had to install and bump to Python v3 because newer versions of the
    AWS CLI have ended support for Python 2.7.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/lambda-layer-awscli/layer/build.sh
+++ packages/@aws-cdk/lambda-layer-awscli/layer/build.sh
@@ -3,11 +3,13 @@ set -euo pipefail
 
 cd $(dirname $0)
 
-echo ">> Building AWS Lambda layer inside a docker image..."
+version=$(cat ../awscli.version)
+
+echo ">> Building AWS Lambda layer inside a docker image for CLI version ${version}..."
 
 TAG='aws-lambda-layer'
 
-docker build -t ${TAG} .
+docker build -t ${TAG} . --build-arg AWSCLI_VERSION=${version}
 
 echo ">> Extrating layer.zip from the build container..."
 CONTAINER=$(docker run -d ${TAG} false)
