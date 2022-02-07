9b62b014ecfb056533599c5f23d952d2dc515e69
Rico Huijbers <rix0rrr@gmail.com>
2022-02-07 15:55:13 +0100

    Fix unit test

--- packages/@aws-cdk/aws-iam/test/role.from-role-arn.test.ts
+++ packages/@aws-cdk/aws-iam/test/role.from-role-arn.test.ts
@@ -565,7 +565,7 @@ test('Role.fromRoleName', () => {
   const stack = new Stack(app, 'Stack', { env: { region: 'asdf', account: '1234' } });
   const role = Role.fromRoleName(stack, 'MyRole', 'MyRole');
 
-  expect(stack.resolve(role.roleArn)).toEqual({ 'Fn::Join': ['', ['arn:', { Ref: 'AWS::Partition' }, ':iam:1234:role/MyRole']] });
+  expect(stack.resolve(role.roleArn)).toEqual({ 'Fn::Join': ['', ['arn:', { Ref: 'AWS::Partition' }, ':iam::1234:role/MyRole']] });
 });
 
 function somePolicyStatement() {
