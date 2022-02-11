9ac83589892f9a7cfada97ad9562c4751d0daeef
Rico Huijbers <rix0rrr@gmail.com>
2022-02-11 14:57:38 +0100

    Move an enum

--- packages/@aws-cdk/aws-codepipeline-actions/lib/cloudformation/stackset-types.ts
+++ packages/@aws-cdk/aws-codepipeline-actions/lib/cloudformation/stackset-types.ts
@@ -203,30 +203,6 @@ export interface StackInstancesBindResult {
 }
 
 /**
- * Describes whether AWS CloudFormation StackSets automatically deploys to AWS Organizations accounts that are added to a target organization or
- * organizational unit (OU).
- */
-export enum StackSetOrganizationsAutoDeployment {
-  /**
-   * StackSets automatically deploys additional stack instances to AWS Organizations accounts that are added to a target organization or
-   * organizational unit (OU) in the specified Regions. If an account is removed from a target organization or OU, AWS CloudFormation StackSets
-   * deletes stack instances from the account in the specified Regions.
-   */
-  ENABLED = 'Enabled',
-
-  /**
-   * StackSets does not automatically deploy additional stack instances to AWS Organizations accounts that are added to a target organization or
-   * organizational unit (OU) in the specified Regions.
-   */
-  DISABLED = 'Disabled',
-
-  /**
-   * Stack resources are retained when an account is removed from a target organization or OU.
-   */
-  ENABLED_WITH_STACK_RETENTION = 'EnabledWithStackRetention'
-}
-
-/**
  * Base parameters for the StackSet
  */
 export abstract class StackSetParameters {
@@ -459,6 +435,31 @@ export interface OrganizationsDeploymentProps {
    */
   readonly autoDeployment?: StackSetOrganizationsAutoDeployment;
 }
+
+/**
+ * Describes whether AWS CloudFormation StackSets automatically deploys to AWS Organizations accounts that are added to a target organization or
+ * organizational unit (OU).
+ */
+export enum StackSetOrganizationsAutoDeployment {
+  /**
+   * StackSets automatically deploys additional stack instances to AWS Organizations accounts that are added to a target organization or
+   * organizational unit (OU) in the specified Regions. If an account is removed from a target organization or OU, AWS CloudFormation StackSets
+   * deletes stack instances from the account in the specified Regions.
+   */
+  ENABLED = 'Enabled',
+
+  /**
+   * StackSets does not automatically deploy additional stack instances to AWS Organizations accounts that are added to a target organization or
+   * organizational unit (OU) in the specified Regions.
+   */
+  DISABLED = 'Disabled',
+
+  /**
+   * Stack resources are retained when an account is removed from a target organization or OU.
+   */
+  ENABLED_WITH_STACK_RETENTION = 'EnabledWithStackRetention'
+}
+
 
 /**
  * Properties for configuring self-managed permissions
