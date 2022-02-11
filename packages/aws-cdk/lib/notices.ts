7c873c32e43ea776327e8f9ed0980cf620236ade
Otavio Macedo <otaviom@amazon.com>
2022-02-11 14:04:04 +0000

    Added cache

--- packages/aws-cdk/lib/notices.ts
+++ packages/aws-cdk/lib/notices.ts
@@ -1,11 +1,13 @@
 import * as https from 'https';
+import * as path from 'path';
+import * as fs from 'fs-extra';
 import * as semver from 'semver';
-import { print } from './logging';
+import { print, debug } from './logging';
+import { cdkCacheDir } from './util/directories';
 import { versionNumber } from './version';
 
 export async function displayNotices() {
-
-  const dataSource = new WebsiteNoticeDataSource();
+  const dataSource = new CachedDataSource(path.join(cdkCacheDir(), 'notices.json'), new WebsiteNoticeDataSource());
   const filter = new NoticeFilter({
     cliVersion: versionNumber(),
     temporarilySuppressed: false,
@@ -18,8 +20,7 @@ export async function displayNotices() {
     .filter(notice => filter.apply(notice))
     .map(notice => formatter.apply(notice));
 
-
-  if (notices.length > 0) {
+  if (individualMessages.length > 0) {
     const finalMessage = `NOTICES
     
 ${individualMessages.join('\n\n')}
@@ -66,12 +67,63 @@ export class WebsiteNoticeDataSource implements NoticeDataSource {
             }
           });
         }
+        // Otherwise, we just ignore the result and move on
       });
     });
   }
 }
 
+interface CachedNotices {
+  expiration: number,
+  notices: Notice[],
+}
+
+const TIME_TO_LIVE = 60 * 60 * 1000; // 1 hour
+
 // TODO Caching can be implemented as a decorator around NoticeDataSource
+export class CachedDataSource implements NoticeDataSource {
+  constructor(
+    private readonly fileName: string,
+    private readonly dataSource: NoticeDataSource) {
+  }
+
+  async fetch(): Promise<Notice[]> {
+    const cachedData = await this.loadCachedData();
+    const notices = cachedData.notices;
+    const expiration = cachedData.expiration ?? 0;
+
+    if (Date.now() > expiration) {
+      const freshData = {
+        expiration: Date.now() + TIME_TO_LIVE,
+        notices: await this.dataSource.fetch(),
+      };
+      await this.storeCachedData(freshData);
+      return freshData.notices;
+    } else {
+      return notices;
+    }
+  }
+
+  private async loadCachedData(): Promise<CachedNotices> {
+    try {
+      return await fs.readJSON(this.fileName) as CachedNotices;
+    } catch (e) {
+      debug(`Failed to load notices from cached: ${e}`);
+      return {
+        expiration: 0,
+        notices: [],
+      };
+    }
+  }
+
+  private async storeCachedData(cached: CachedNotices): Promise<void> {
+    try {
+      await fs.writeJSON(this.fileName, cached);
+    } catch (e) {
+      debug(`Failed to store notices in the cache: ${e}`);
+    }
+  }
+}
 
 export interface NoticeFilterProps {
   permanentlySuppressed: boolean,
