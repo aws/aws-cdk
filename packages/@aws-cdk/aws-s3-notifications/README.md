7db9ee06cacbea9a5b2122f04669062790e11ac0
Markus <mail@markussiebert.com>
2022-02-16 13:59:38 +0100

    fix: notifications allowed with imported kms keys

--- packages/@aws-cdk/aws-s3-notifications/README.md
+++ packages/@aws-cdk/aws-s3-notifications/README.md
@@ -26,6 +26,19 @@ const topic = new sns.Topic(this, 'Topic');
 bucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT, new s3n.SnsDestination(topic));
 ```
 
+The following example shows how to send a notification to an SQS queue
+when an object is created in an S3 bucket:
+
+```ts
+import * as sqs from '@aws-cdk/aws-sqs';
+
+const bucket = new s3.Bucket(this, 'Bucket');
+const queue = new sqs.Queue(stack, 'Queue');
+
+
+bucket.addObjectCreatedNotification(s3.EventType.OBJECT_CREATED_PUT, new s3n.SqsDestination(topic));
+```
+
 The following example shows how to send a notification to a Lambda function when an object is created in an S3 bucket:
 
 ```ts
--- packages/@aws-cdk/aws-s3-notifications/lib/sqs.ts
+++ packages/@aws-cdk/aws-s3-notifications/lib/sqs.ts
@@ -1,6 +1,11 @@
 import * as iam from '@aws-cdk/aws-iam';
+import * as kms from '@aws-cdk/aws-kms';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as sqs from '@aws-cdk/aws-sqs';
+import { Annotations } from '@aws-cdk/core';
+
+// keep this import separate from other imports to reduce chance for merge conflicts with v2-main
+// eslint-disable-next-line no-duplicate-imports, import/order
 import { Construct } from '@aws-cdk/core';
 
 /**
@@ -24,11 +29,16 @@ export class SqsDestination implements s3.IBucketNotificationDestination {
     // if this queue is encrypted, we need to allow S3 to read messages since that's how
     // it verifies that the notification destination configuration is valid.
     if (this.queue.encryptionMasterKey) {
-      this.queue.encryptionMasterKey.addToResourcePolicy(new iam.PolicyStatement({
+      const statement = new iam.PolicyStatement({
         principals: [new iam.ServicePrincipal('s3.amazonaws.com')],
         actions: ['kms:GenerateDataKey*', 'kms:Decrypt'],
         resources: ['*'],
-      }), /* allowNoOp */ false);
+      });
+      if (this.queue.encryptionMasterKey instanceof kms.Key) {
+        this.queue.encryptionMasterKey.addToResourcePolicy(statement, /* allowNoOp */ false);
+      } else {
+        Annotations.of(this.queue.encryptionMasterKey).addWarning(`Can not change key policy of imported kms key. Ensure hat your key policy contains the following permissions: \n${JSON.stringify(statement.toJSON(), null, 2)}`);
+      }
     }
 
     return {
