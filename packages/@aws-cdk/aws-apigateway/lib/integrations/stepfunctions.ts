14974f960977f0b249a221bb7bfee75867ead737
Roger Chi <roger@rogerchi.com>
2022-03-06 23:55:27 -0500

    fix(aws-apigateway): missing comma to make failure response payload valid json (#19253)
    The failure response from a StepFunctionsRestApi integration is not valid JSON, it is missing a comma.  This fixes the issue by adding a comma.  Fixes #19252
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-apigateway/lib/integrations/stepfunctions.ts
+++ packages/@aws-cdk/aws-apigateway/lib/integrations/stepfunctions.ts
@@ -203,7 +203,7 @@ function integrationResponse() {
           '#if($input.path(\'$.status\').toString().equals("FAILED"))', 
             '#set($context.responseOverride.status = 500)', 
             '{',
-              '"error": "$input.path(\'$.error\')"',
+              '"error": "$input.path(\'$.error\')",',
               '"cause": "$input.path(\'$.cause\')"',
             '}',
           '#else',
