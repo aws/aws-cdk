12e45f039bd6336a4f2deddda4f6ea00cb655da0
Rico Huijbers <rix0rrr@gmail.com>
2022-03-08 14:01:42 -0800

    Add comma

--- packages/@aws-cdk/pipelines/lib/codepipeline/codebuild-step.ts
+++ packages/@aws-cdk/pipelines/lib/codepipeline/codebuild-step.ts
@@ -233,6 +233,9 @@ export class CodeBuildStep extends ShellStep {
    * The variable must be set in the shell of the CodeBuild step when
    * it finishes its `post_build` phase.
    *
+   * ```ts
+   * ```
+   *
    * @param variableName the name of the variable for reference.
    * @example
    * // Access the output of one CodeBuildStep in another CodeBuildStep
@@ -245,7 +248,7 @@ export class CodeBuildStep extends ShellStep {
    * const step2 = new pipelines.CodeBuildStep('Step2', {
    *   env: {
    *     IMPORTED_VAR: step1.exportedVariable('MY_VAR'),
-   *   }
+   *   },
    *   commands: ['echo $IMPORTED_VAR'],
    * });
    */
