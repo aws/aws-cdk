7b9f85b5779d2df2d509f85264a43af84e371184
Kaizen Conroy <36202692+kaizen3031593@users.noreply.github.com>
2022-03-25 00:23:30 -0400

    chore: fix tests that use node.metadataentry (#19545)
    Does this for everything except the tests in core that are specifically testing `Aspects`. There's less of a chance that those tests are disturbed by adding annotations elsewhere in the CDK.
    
    Closes #19544 
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-events/test/rule.test.ts
+++ packages/@aws-cdk/aws-events/test/rule.test.ts
@@ -30,33 +30,27 @@ describe('rule', () => {
 
   test('rule displays warning when minutes are not included in cron', () => {
     const stack = new cdk.Stack();
-    const rule = new Rule(stack, 'MyRule', {
+    new Rule(stack, 'MyRule', {
       schedule: Schedule.cron({
         hour: '8',
         day: '1',
       }),
     });
-    expect(rule.node.metadataEntry).toEqual([{
-      type: 'aws:cdk:warning',
-      data: 'cron: If you don\'t pass \'minute\', by default the event runs every minute. Pass \'minute: \'*\'\' if that\'s what you intend, or \'minute: 0\' to run once per hour instead.',
-      trace: undefined,
-    }]);
-    Annotations.fromStack(stack).hasWarning('/Default/MyRule', "cron: If you don't pass 'minute', by default the event runs every minute. Pass 'minute: '*'' if that's what you intend, or 'minute: 0' to run once per hour instead.");
 
+    Annotations.fromStack(stack).hasWarning('/Default/MyRule', "cron: If you don't pass 'minute', by default the event runs every minute. Pass 'minute: '*'' if that's what you intend, or 'minute: 0' to run once per hour instead.");
   });
 
   test('rule does not display warning when minute is set to * in cron', () => {
     const stack = new cdk.Stack();
-    const rule = new Rule(stack, 'MyRule', {
+    new Rule(stack, 'MyRule', {
       schedule: Schedule.cron({
         minute: '*',
         hour: '8',
         day: '1',
       }),
     });
-    expect(rule.node.metadataEntry).toEqual([]);
-    const annotations = Annotations.fromStack(stack).findWarning('*', Match.anyValue());
-    expect(annotations.length).toBe(0);
+
+    Annotations.fromStack(stack).hasNoWarning('/Default/MyRule', Match.anyValue());
   });
 
   test('can get rule name', () => {
