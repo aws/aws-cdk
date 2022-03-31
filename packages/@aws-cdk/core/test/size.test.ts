ad5c3abab0ba450738285e7b5fc77be41d0c1c25
Rico Huijbers <rix0rrr@gmail.com>
2022-03-31 16:27:12 +0200

    chore(core): improve token error for Duration and Size
    The old error message was not up to CDK standards (not clearly
    describing the error and not suggesting a course of action).
    
    This should be more actionable.

--- packages/@aws-cdk/core/test/size.test.ts
+++ packages/@aws-cdk/core/test/size.test.ts
@@ -13,7 +13,7 @@ describe('size', () => {
     expect(stack.resolve(lazySize.toKibibytes())).toEqual(1337);
     expect(
       () => stack.resolve(lazySize.toMebibytes())).toThrow(
-      /Unable to perform time unit conversion on un-resolved token/,
+      /Size must be specified as 'Size.mebibytes\(\)' here/,
     );
 
 
