ef404774c1109b5e6f8cd64aee2789aa1c23d8cd
Cory Hall <43035978+corymhall@users.noreply.github.com>
2022-03-31 16:13:13 -0400

    feat(elbv2): use `addAction()` on an imported application listener (#19293)
    This PR adds the ability to call `addAction()` on an imported
    `IApplicationListener`.
    
    Fixes #10902
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-elasticloadbalancingv2/test/alb/listener.test.ts
+++ packages/@aws-cdk/aws-elasticloadbalancingv2/test/alb/listener.test.ts
@@ -707,6 +707,85 @@ describe('tests', () => {
     });
   });
 
+  test('Can add actions to an imported listener', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const stack2 = new cdk.Stack();
+    const vpc = new ec2.Vpc(stack, 'VPC');
+    const lb = new elbv2.ApplicationLoadBalancer(stack, 'LoadBalancer', {
+      vpc,
+    });
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    // WHEN
+    listener.addAction('Default', {
+      action: elbv2.ListenerAction.fixedResponse(404, {
+        contentType: 'text/plain',
+        messageBody: 'Not Found',
+      }),
+    });
+
+    const importedListener = elbv2.ApplicationListener.fromApplicationListenerAttributes(stack2, 'listener', {
+      listenerArn: 'listener-arn',
+      defaultPort: 443,
+      securityGroup: ec2.SecurityGroup.fromSecurityGroupId(stack2, 'SG', 'security-group-id', {
+        allowAllOutbound: false,
+      }),
+    });
+    importedListener.addAction('Hello', {
+      action: elbv2.ListenerAction.fixedResponse(503),
+      conditions: [elbv2.ListenerCondition.pathPatterns(['/hello'])],
+      priority: 10,
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::ElasticLoadBalancingV2::Listener', {
+      DefaultActions: [
+        {
+          FixedResponseConfig: {
+            ContentType: 'text/plain',
+            MessageBody: 'Not Found',
+            StatusCode: '404',
+          },
+          Type: 'fixed-response',
+        },
+      ],
+    });
+
+    Template.fromStack(stack2).hasResourceProperties('AWS::ElasticLoadBalancingV2::ListenerRule', {
+      ListenerArn: 'listener-arn',
+      Priority: 10,
+      Actions: [
+        {
+          FixedResponseConfig: {
+            StatusCode: '503',
+          },
+          Type: 'fixed-response',
+        },
+      ],
+    });
+  });
+
+  test('actions added to an imported listener must have a priority', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+
+    const importedListener = elbv2.ApplicationListener.fromApplicationListenerAttributes(stack, 'listener', {
+      listenerArn: 'listener-arn',
+      defaultPort: 443,
+      securityGroup: ec2.SecurityGroup.fromSecurityGroupId(stack, 'SG', 'security-group-id', {
+        allowAllOutbound: false,
+      }),
+    });
+    expect(() => {
+      importedListener.addAction('Hello', {
+        action: elbv2.ListenerAction.fixedResponse(503),
+      });
+    }).toThrow(/priority must be set for actions added to an imported listener/);
+  });
+
   testDeprecated('Can add redirect responses', () => {
     // GIVEN
     const stack = new cdk.Stack();
