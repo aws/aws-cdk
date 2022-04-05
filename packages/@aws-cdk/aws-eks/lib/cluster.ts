d16f5268d9eebd47b09342b436001d2c1f2e0c37
robertd <robert.djurasaj@gmail.com>
2022-04-04 22:11:26 -0600

    deprecate 1.18

--- packages/@aws-cdk/aws-eks/lib/cluster.ts
+++ packages/@aws-cdk/aws-eks/lib/cluster.ts
@@ -796,6 +796,7 @@ export class KubernetesVersion {
 
   /**
    * Kubernetes version 1.18
+   * @deprecated Use newer version of EKS
    */
   public static readonly V1_18 = KubernetesVersion.of('1.18');
 
