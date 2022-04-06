42fb1744f289d446b109a67a0066e9d1f5b4434a
epolon <epolon@amazon.com>
2022-04-06 15:04:20 +0300

    forward merge

--- tools/@aws-cdk/pkglint/lib/rules.ts
+++ tools/@aws-cdk/pkglint/lib/rules.ts
@@ -14,6 +14,7 @@ import {
   findInnerPackages,
   monoRepoRoot,
 } from './util';
+import { Bundle } from '@aws-cdk/node-bundle';
 
 const PKGLINT_VERSION = require('../package.json').version; // eslint-disable-line @typescript-eslint/no-require-imports
 const AWS_SERVICE_NAMES = require('./aws-service-official-names.json'); // eslint-disable-line @typescript-eslint/no-require-imports
