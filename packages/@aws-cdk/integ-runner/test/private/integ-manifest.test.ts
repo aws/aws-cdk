32f2a3ff8777c4a0b7efbd834a553eb04f5d964c
Otavio Macedo <otaviomacedo@protonmail.com>
2022-04-07 15:37:42 +0100

    chore(integ-runner): add a manifest writer

---
+++ packages/@aws-cdk/integ-runner/test/private/integ-manifest.test.ts
@@ -0,0 +1,41 @@
+import * as os from 'os';
+import { IntegManifest, Manifest } from '@aws-cdk/cloud-assembly-schema';
+import * as fs from 'fs-extra';
+import { IntegManifestWriter } from '../../lib/runner/private/integ-manifest';
+
+describe(IntegManifestWriter, () => {
+  const tmpDir = fs.mkdtempSync(os.tmpdir());
+  const manifest: IntegManifest = {
+    version: 'does not matter',
+    testCases: {
+      test1: {
+        stacks: ['MyStack'],
+        diffAssets: false,
+      },
+      test2: {
+        stacks: ['MyOtherStack'],
+        diffAssets: true,
+      },
+    },
+  };
+
+  beforeEach(() => {
+    fs.emptyDirSync(tmpDir);
+  });
+
+  it('writes manifests to default location in the specified directory', () => {
+    IntegManifestWriter.write(manifest, tmpDir);
+
+    const loaded = Manifest.loadIntegManifest(`${tmpDir}/integ.json`);
+
+    expect(loaded).toEqual({ ...manifest, version: Manifest.version() });
+  });
+
+  it('writes manifests to the chosen location', () => {
+    IntegManifestWriter.write(manifest, `${tmpDir}/custom-name.json`);
+
+    const loaded = Manifest.loadIntegManifest(`${tmpDir}/custom-name.json`);
+
+    expect(loaded).toEqual({ ...manifest, version: Manifest.version() });
+  });
+});
