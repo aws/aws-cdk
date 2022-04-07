6c839bfc3452c6bf628cdea7140c37262b80a4b7
peterwoodworth <woodwoop@amazon.com>
2022-04-07 15:44:30 -0700

    style

--- packages/@aws-cdk/aws-apigatewayv2/test/websocket/integ.stage.ts
+++ packages/@aws-cdk/aws-apigatewayv2/test/websocket/integ.stage.ts
@@ -3,11 +3,9 @@ import * as cdk from '@aws-cdk/core';
 import * as apigw from '../../lib';
 
 const app = new cdk.App();
-
 const stack = new cdk.Stack(app, 'aws-cdk-aws-apigatewayv2-websocket-stage');
 
 const webSocketApi = new apigw.WebSocketApi(stack, 'WebSocketApi');
-
 new apigw.WebSocketStage(stack, 'WebSocketStage', {
   webSocketApi,
   stageName: 'dev',
