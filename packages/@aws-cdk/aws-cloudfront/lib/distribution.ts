285f3ead394ea5b2be66a1af2462d1926c6fea99
Yasunori Kirimoto <yasunorikirimoto@gmail.com>
2022-04-08 00:26:25 +0900

    Add sslSupportMethod
    - Add test for sslSupportMethod
    - Corrected comment
    
    closes #19476

--- packages/@aws-cdk/aws-cloudfront/lib/distribution.ts
+++ packages/@aws-cdk/aws-cloudfront/lib/distribution.ts
@@ -225,8 +225,8 @@ export interface DistributionProps {
     * The SSL method CloudFront will use for your distribution.
     *
     * Server Name Indication (SNI) - is an extension to the TLS computer networking protocol by which a client indicates
-    *  which hostname it is attempting to connect to at the start of the handshaking process. This allows a server to present
-    *  multiple certificates on the same IP address and TCP port number and hence allows multiple secure (HTTPS) websites
+    * which hostname it is attempting to connect to at the start of the handshaking process. This allows a server to present
+    * multiple certificates on the same IP address and TCP port number and hence allows multiple secure (HTTPS) websites
     * (or any other service over TLS) to be served by the same IP address without requiring all those sites to use the same certificate.
     *
     * CloudFront can use SNI to host multiple distributions on the same IP - which a large majority of clients will support.
@@ -236,7 +236,7 @@ export interface DistributionProps {
     *
     * See the CloudFront SSL for more details about pricing : https://aws.amazon.com/cloudfront/custom-ssl-domains/
     *
-    * @default - SSLMethod.SNI
+    * @default SSLMethod.SNI
     */
   readonly sslSupportMethod?: SSLMethod;
 }
