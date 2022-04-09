6ccde8710d227b1830a89f96a15572352bd863b8
WinterYukky <WinterYukky@gmail.com>
2022-04-09 14:55:00 +0000

    test(rum): all value set pattern

--- packages/@aws-cdk/aws-rum/test/integ.app-monitor.ts
+++ packages/@aws-cdk/aws-rum/test/integ.app-monitor.ts
@@ -1,3 +1,4 @@
+import * as identitypool from '@aws-cdk/aws-cognito-identitypool';
 import { App, Stack } from '@aws-cdk/core';
 import * as rum from '../lib';
 
@@ -5,7 +6,24 @@ const app = new App();
 
 const stack = new Stack(app, 'integ-app-monitor');
 
-new rum.AppMonitor(stack, 'MyAppMonitor', {
+new rum.AppMonitor(stack, 'DefaultValueAppMonitor', {
   domain: 'my-website.com',
 });
+
+const myIdentityPool = new identitypool.IdentityPool(stack, 'MyIdentityPool');
+new rum.AppMonitor(stack, 'CustomValueAppMonitor', {
+  domain: 'my-website2.com',
+  appMonitorName: 'my-app-monitor',
+  identityPool: myIdentityPool,
+  role: myIdentityPool.unauthenticatedRole,
+  persistence: true,
+  allowCookies: true,
+  enableXRay: true,
+  excludedPages: ['/exclude'],
+  favoritePages: ['/favorite'],
+  includedPages: [],
+  sessionSampleRate: 10,
+  telemetries: [],
+});
+
 app.synth();
