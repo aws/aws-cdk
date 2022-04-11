b2e0a80af18d1fc5056f7148b28f08a2674b210e
Otavio Macedo <otaviomacedo@protonmail.com>
2022-04-11 09:17:21 +0100

    Simplified snapshot dir logic and removed the stack

--- packages/@aws-cdk/integ-tests/lib/test-case.ts
+++ packages/@aws-cdk/integ-tests/lib/test-case.ts
@@ -1,6 +1,5 @@
-import * as path from 'path';
 import { IntegManifest, TestCase } from '@aws-cdk/cloud-assembly-schema';
-import { attachCustomSynthesis, ISynthesisSession, Stack, StackProps } from '@aws-cdk/core';
+import { attachCustomSynthesis, ISynthesisSession } from '@aws-cdk/core';
 import * as fs from 'fs-extra';
 import { IntegManifestWriter } from './manifest-writer';
 
@@ -8,41 +7,24 @@ import { IntegManifestWriter } from './manifest-writer';
 // eslint-disable-next-line no-duplicate-imports, import/order
 import { Construct } from '@aws-cdk/core';
 
-export interface IntegTestCaseProps extends StackProps, TestCase {
-  testFileName: string;
+export interface IntegTestCaseProps extends TestCase {
 }
 
-// TODO Find a better name for this class
-class IntegTestCaseConstruct extends Construct {
+export class IntegTestCase extends Construct {
   constructor(scope: Construct, id: string, props: IntegTestCaseProps) {
     super(scope, id);
 
     attachCustomSynthesis(this, {
-      onSynthesize: (_session: ISynthesisSession) => {
-
-        // TODO This code snippet is duplicated from integ-runner. Consider refactoring
-        const parsed = path.parse(props.testFileName);
-        const directory = parsed.dir;
-        const testName = parsed.name.slice(6);
-        const snapshotDir = path.join(directory, `${testName}.integ.snapshot`);
-
-        const { testFileName, ...testCase } = props;
+      onSynthesize: (session: ISynthesisSession) => {
+        const snapshotDir = session.assembly.outdir;
         const manifest: IntegManifest = {
           version: '',
-          testCases: { [id]: testCase },
+          testCases: { [id]: props },
         };
 
         fs.ensureDirSync(snapshotDir);
         IntegManifestWriter.write(manifest, snapshotDir);
       },
     });
-  }
-}
-
-export class IntegTestCase extends Stack {
-  constructor(scope: Construct, id: string, props: IntegTestCaseProps) {
-    super(scope, id, props);
-
-    new IntegTestCaseConstruct(this, 'integ', props);
   }
 }
