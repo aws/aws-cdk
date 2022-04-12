697fdbe71642c93492c38e834e654ed736a9edb4
Cory Hall <43035978+corymhall@users.noreply.github.com>
2022-04-12 16:43:47 -0400

    feat(integ-runner): integ-runner enhancements (#19865)
    This PR adds a couple of enhancements to the `integ-runner`
    
    1. Add `--profiles` option which allows you to provide a list of
       AWS profiles to use. This will be used in combination with the
       `--parallel-regions` option to run the integration tests in parallel
       across multiple accounts + regions.
    2. Adds `--from-file` option that allows you to store a list of tests to
       run (or exclude) in a file, rather than passing test names as
       arguments.
    3. Adds `--exclude` option, which when provided will excluded the tests
       provided as arguments
    4. Adds some additional metrics. Now each test will show the total time
       in the logs. Also, if `--verbose` option is provided a summary will
       be printed which will contain metrics for the entire test.
    ```bash
      --- Integration test metrics ---
    Profile mb-dev + Region us-east-2 total time: 349.866
      test/integ.bundling.js: 58.75
      test/integ.lambda-insights-mapping.js: 61.012
      test/integ.assets.lit.js: 61.121
      test/integ.lambda.docker.js: 81.936
      test/integ.log-retention.js: 87.047
    Profile mb-dev + Region us-west-2 total time: 359.775
      test/integ.layer-version.lit.js: 72.61
      test/integ.autoscaling.lit.js: 82.82
      test/integ.current-version.js: 84.982
      test/integ.lambda.js: 119.363
    Profile mb-dev + Region us-east-1 total time: 378.713
      test/integ.assets.file.js: 67.915
      test/integ.runtime.inlinecode.js: 68.211
    ```
    
    There are also a couple of small refactors.
    1. Split up `workers.test.ts` into `snapshot-worker.test.ts` and `integ-worker.test.ts`
    2. Refactored the `extract_workers` functions. There used to be two functions `snapshotTestBatch` and `integTestBatch` which called `singleThreadedXXX` functions. Since each request is essentially single threaded I've moved the `singleThreadedXXX` functions into `extract_worker` and renamed them to be `integTestWorker` and `snapshotTestWorker`
    
    ----
    
    ### All Submissions:
    
    * [x] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/master/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/master/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/master/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/integ-runner/lib/workers/common.ts
+++ packages/@aws-cdk/integ-runner/lib/workers/common.ts
@@ -3,10 +3,50 @@ import * as logger from '../logger';
 import { IntegTestConfig } from '../runner/integ-tests';
 
 /**
+ * Represents integration tests metrics for a given worker
+ */
+export interface IntegRunnerMetrics {
+  /**
+   * The region the test was run in
+   */
+  readonly region: string;
+
+  /**
+   * The total duration of the worker.
+   * This will be the sum of all individual test durations
+   */
+  readonly duration: number;
+
+  /**
+   * Contains the duration of individual tests that the
+   * worker executed.
+   *
+   * Map of testName to duration.
+   */
+  readonly tests: { [testName: string]: number };
+
+  /**
+   * The profile that was used to run the test
+   *
+   * @default - default profile
+   */
+  readonly profile?: string;
+}
+
+/**
  * Integration test results
  */
 export interface IntegBatchResponse {
-  failedTests: IntegTestConfig[];
+  /**
+   * List of failed tests
+   */
+  readonly failedTests: IntegTestConfig[];
+
+  /**
+   * List of Integration test metrics. Each entry in the
+   * list represents metrics from a single worker (account + region).
+   */
+  readonly metrics: IntegRunnerMetrics[];
 }
 
 /**
@@ -92,6 +132,11 @@ export interface Diagnostic {
   readonly message: string;
 
   /**
+   * The time it took to run the test
+   */
+  readonly duration?: number;
+
+  /**
    * The reason for the diagnostic
    */
   readonly reason: DiagnosticReason;
@@ -111,18 +156,25 @@ export function printSummary(total: number, failed: number): void {
 export function printResults(diagnostic: Diagnostic): void {
   switch (diagnostic.reason) {
     case DiagnosticReason.SNAPSHOT_SUCCESS:
-      logger.success('  %s No Change!', diagnostic.testName);
+      logger.success('  %s No Change! %s', diagnostic.testName, chalk.gray(`${diagnostic.duration}s`));
       break;
     case DiagnosticReason.TEST_SUCCESS:
-      logger.success('  %s Test Succeeded!', diagnostic.testName);
+      logger.success('  %s Test Succeeded! %s', diagnostic.testName, chalk.gray(`${diagnostic.duration}s`));
       break;
     case DiagnosticReason.NO_SNAPSHOT:
-      logger.error('  %s - No Snapshot!', diagnostic.testName);
+      logger.error('  %s - No Snapshot! %s', diagnostic.testName, chalk.gray(`${diagnostic.duration}s`));
       break;
     case DiagnosticReason.SNAPSHOT_FAILED:
-      logger.error('  %s - Snapshot changed!\n%s', diagnostic.testName, diagnostic.message);
+      logger.error('  %s - Snapshot changed! %s\n%s', diagnostic.testName, chalk.gray(`${diagnostic.duration}s`), diagnostic.message);
       break;
     case DiagnosticReason.TEST_FAILED:
-      logger.error('  %s - Failed!\n%s', diagnostic.testName, diagnostic.message);
+      logger.error('  %s - Failed! %s\n%s', diagnostic.testName, chalk.gray(`${diagnostic.duration}s`), diagnostic.message);
   }
+}
+
+/**
+ * Flatten a list of lists into a list of elements
+ */
+export function flatten<T>(xs: T[][]): T[] {
+  return Array.prototype.concat.apply([], xs);
 }
