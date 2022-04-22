51ead1676489d0d3a741b07cb3d012493f8bc0fe
Rico Huijbers <rix0rrr@gmail.com>
2022-04-22 15:27:06 +0200

    chore: minor test improvements
    - Remove gross duplication in a test
    - Have objectMatcher tell us what keys ARE there instead of just what
      keys AREN'T there
    - Add a test to confirm that `addEventSourceMapping()` doesn't
      accidentally produce non-stable logical IDs

--- packages/@aws-cdk/assertions/lib/match.ts
+++ packages/@aws-cdk/assertions/lib/match.ts
@@ -300,7 +300,7 @@ class ObjectMatch extends Matcher {
         result.recordFailure({
           matcher: this,
           path: [`/${patternKey}`],
-          message: 'Missing key',
+          message: `Missing key '${patternKey}' among ${Object.keys(actual).map(k => `'${k}'`).join(', ') || '(none)'}`,
         });
         continue;
       }
