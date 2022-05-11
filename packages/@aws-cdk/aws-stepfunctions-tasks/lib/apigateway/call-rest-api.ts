a89daf36cffcead7342b369dc67b89900c09949c
Tejas M R <matblockgreninja@gmail.com>
2022-05-11 21:26:35 +0530

    Fixed props.method is read-only property
    Fixed props.method is read-only property by creating another constant called method to hold updated props.method

--- packages/@aws-cdk/aws-stepfunctions-tasks/lib/apigateway/call-rest-api.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/lib/apigateway/call-rest-api.ts
@@ -58,15 +58,12 @@ export class CallApiGatewayRestApiEndpoint extends CallApiGatewayEndpointBase {
 
     this.apiEndpoint = this.getApiEndpoint();
     
-    if (props.method.toUpperCase() === 'ANY') {
-      props.method = '*';
-    }
-
+    const method: string = (props.method.toUpperCase() === 'ANY') ? '*' : props.method;
     this.arnForExecuteApi =  cdk.Stack.of(props.api).formatArn({
       service: 'execute-api',
       resource: props.api.restApiId,
       arnFormat: cdk.ArnFormat.SLASH_RESOURCE_NAME,
-      resourceName: `${props.stageName}/${props.method}${props.apiPath}`,
+      resourceName: `${props.stageName}/${method}${props.apiPath}`,
     });
     
     this.stageName = props.stageName;
