52beb515158304a808582a252a284ba4ce942d6f
Christopher Rybicki <rybickic@amazon.com>
2022-05-19 13:34:22 -0700

    chore(codepipeline): revert "cannot deploy pipeline stack with crossAccountKeys twice"

--- packages/@aws-cdk/core/lib/names.ts
+++ packages/@aws-cdk/core/lib/names.ts
@@ -1,37 +1,6 @@
 import { Construct, Node } from 'constructs';
 import { ConstructNode } from './construct-compat';
-import { unresolved } from './private/encoding';
-import { makeUniqueResourceName } from './private/unique-resource-name';
 import { makeUniqueId } from './private/uniqueid';
-import { Stack } from './stack';
-
-
-/**
- * Options for creating a unique resource name.
-*/
-export interface UniqueResourceNameOptions {
-
-  /**
-   * The maximum length of the unique resource name.
-   *
-   * @default - 256
-   */
-  readonly maxLength?: number;
-
-  /**
-   * The separator used between the path components.
-   *
-   * @default - none
-   */
-  readonly separator?: string;
-
-  /**
-   * Non-alphanumeric characters allowed in the unique resource name.
-   *
-   * @default - none
-   */
-  readonly allowedSpecialCharacters?: string;
-}
 
 /**
  * Functions for devising unique names for constructs. For example, those can be
@@ -41,8 +10,7 @@ export class Names {
   /**
    * Returns a CloudFormation-compatible unique identifier for a construct based
    * on its path. The identifier includes a human readable portion rendered
-   * from the path components and a hash suffix. uniqueId is not unique if multiple
-   * copies of the stack are deployed. Prefer using uniqueResourceName().
+   * from the path components and a hash suffix.
    *
    * @param construct The construct
    * @returns a unique id based on the construct path
@@ -66,30 +34,6 @@ export class Names {
   public static nodeUniqueId(node: ConstructNode): string {
     const components = node.scopes.slice(1).map(c => c.node.id);
     return components.length > 0 ? makeUniqueId(components) : '';
-  }
-
-  /**
-   * Returns a CloudFormation-compatible unique identifier for a construct based
-   * on its path. This function finds the stackName of the parent stack (non-nested)
-   * to the construct, and the ids of the components in the construct path.
-   *
-   * The user can define allowed special characters, a separator between the elements,
-   * and the maximum length of the resource name. The name includes a human readable portion rendered
-   * from the path components, with or without user defined separators, and a hash suffix.
-   * If the resource name is longer than the maximum length, it is trimmed in the middle.
-   *
-   * @param construct The construct
-   * @param options Options for defining the unique resource name
-   * @returns a unique resource name based on the construct path
-   */
-  public static uniqueResourceName(construct: Construct, options: UniqueResourceNameOptions) {
-    const node = Node.of(construct);
-
-    const componentsPath = node.scopes.slice(node.scopes.indexOf(node.scopes.reverse()
-      .find(component => (Stack.isStack(component) && !unresolved(component.stackName)))!,
-    )).map(component => Stack.isStack(component) && !unresolved(component.stackName) ? component.stackName : Node.of(component).id);
-
-    return makeUniqueResourceName(componentsPath, options);
   }
 
   private constructor() {}
