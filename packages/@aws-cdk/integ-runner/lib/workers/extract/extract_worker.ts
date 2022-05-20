947626d4a1157dea5eb193d64a806ec9deff3975
Rico Huijbers <rix0rrr@gmail.com>
2022-05-20 16:03:14 +0200

    chore(integ-runner): `--inspect-failures` option (#20399)
    When the integ runner fails to validate a snapshot, add an option
    to retain the tempdir with the "new actual" snapshot so that a human
    can go look around in it.
    
    Also make the `--verbose` flag print out a command that will reproduce
    the running of the CDK app directly from the command-line, so that
    it can be debugged more easily.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/integ-runner/lib/workers/extract/extract_worker.ts
+++ packages/@aws-cdk/integ-runner/lib/workers/extract/extract_worker.ts
@@ -1,7 +1,7 @@
 import * as workerpool from 'workerpool';
 import { IntegSnapshotRunner, IntegTestRunner } from '../../runner';
 import { IntegTestConfig } from '../../runner/integration-tests';
-import { DiagnosticReason, IntegTestWorkerConfig, formatAssertionResults } from '../common';
+import { DiagnosticReason, IntegTestWorkerConfig, SnapshotVerificationOptions, Diagnostic, formatAssertionResults } from '../common';
 import { IntegTestBatchRequest } from '../integ-test-worker';
 
 /**
@@ -84,7 +84,7 @@ export function integTestWorker(request: IntegTestBatchRequest): IntegTestWorker
  * if there is an existing snapshot, and if there is will
  * check if there are any changes
  */
-export function snapshotTestWorker(test: IntegTestConfig): IntegTestWorkerConfig[] {
+export function snapshotTestWorker(test: IntegTestConfig, options: SnapshotVerificationOptions = {}): IntegTestWorkerConfig[] {
   const failedTests = new Array<IntegTestWorkerConfig>();
   const runner = new IntegSnapshotRunner({ fileName: test.fileName, directory: test.directory });
   const start = Date.now();
@@ -98,12 +98,12 @@ export function snapshotTestWorker(test: IntegTestConfig): IntegTestWorkerConfig
       });
       failedTests.push(test);
     } else {
-      const { diagnostics, destructiveChanges } = runner.testSnapshot();
+      const { diagnostics, destructiveChanges } = runner.testSnapshot(options);
       if (diagnostics.length > 0) {
         diagnostics.forEach(diagnostic => workerpool.workerEmit({
           ...diagnostic,
           duration: (Date.now() - start) / 1000,
-        }));
+        } as Diagnostic));
         failedTests.push({
           fileName: test.fileName,
           directory: test.directory,
@@ -115,7 +115,7 @@ export function snapshotTestWorker(test: IntegTestConfig): IntegTestWorkerConfig
           testName: runner.testName,
           message: 'Success',
           duration: (Date.now() - start) / 1000,
-        });
+        } as Diagnostic);
       }
     }
   } catch (e) {
@@ -125,7 +125,7 @@ export function snapshotTestWorker(test: IntegTestConfig): IntegTestWorkerConfig
       testName: runner.testName,
       reason: DiagnosticReason.SNAPSHOT_FAILED,
       duration: (Date.now() - start) / 1000,
-    });
+    } as Diagnostic);
   }
 
   return failedTests;
