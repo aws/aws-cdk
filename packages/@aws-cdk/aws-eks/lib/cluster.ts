48e6e04ea625a35d7de8eb64a9a801b3a49e5031
Greg Cockburn <gergnz@gmail.com>
2022-05-26 07:50:11 +1000

    add support for c7g, graviton3 processors

--- packages/@aws-cdk/aws-eks/lib/cluster.ts
+++ packages/@aws-cdk/aws-eks/lib/cluster.ts
@@ -2285,6 +2285,7 @@ function nodeTypeForInstanceType(instanceType: ec2.InstanceType) {
 
 function cpuArchForInstanceType(instanceType: ec2.InstanceType) {
   return INSTANCE_TYPES.graviton2.includes(instanceType.toString().substring(0, 3)) ? CpuArch.ARM_64 :
+    INSTANCE_TYPES.graviton3.includes(instanceType.toString().substring(0, 3)) ? CpuArch.ARM_64 :
     INSTANCE_TYPES.graviton.includes(instanceType.toString().substring(0, 2)) ? CpuArch.ARM_64 :
       CpuArch.X86_64;
 }
