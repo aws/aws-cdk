a9d84cdf3bd00b23a6a294cf597277b93cd1e3c1
Joe Flateau <joe@joeflateau.net>
2022-05-26 09:21:35 -0400

    feat(aws-ecr-assets): support the --platform option when building docker images (#20439)
    This PR adds support for specifying the desired build platform when building docker images (ie: build an arm64 image on an amd64/x86_64 host). Closes #12472 
    
    This PR does NOT touch Lambda builders, only ECR assets. #16770 attempted to implement support for ECR and Lambda but was abandoned. Meanwhile #16858 implemented lambda platform support. This implements the ECR side
    
    I have run `yarn integ`
    
    ----
    
    ### All Submissions:
    
    * [x] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/master/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/master/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [x] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/master/INTEGRATION_TESTS.md)?
    	* [x] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-ecr-assets/test/integ.assets-docker.ts
+++ packages/@aws-cdk/aws-ecr-assets/test/integ.assets-docker.ts
@@ -19,10 +19,18 @@ const asset2 = new assets.DockerImageAsset(stack, 'DockerImage2', {
   directory: path.join(__dirname, 'demo-image'),
 });
 
+const asset3 = new assets.DockerImageAsset(stack, 'DockerImage3', {
+  directory: path.join(__dirname, 'demo-image'),
+  platform: assets.Platform.LINUX_ARM64,
+});
+
 const user = new iam.User(stack, 'MyUser');
 asset.repository.grantPull(user);
 asset2.repository.grantPull(user);
+asset3.repository.grantPull(user);
 
 new cdk.CfnOutput(stack, 'ImageUri', { value: asset.imageUri });
+new cdk.CfnOutput(stack, 'ImageUri2', { value: asset2.imageUri });
+new cdk.CfnOutput(stack, 'ImageUri3', { value: asset3.imageUri });
 
 app.synth();
