cf27661b4b3d5666eb7274a9d2d790739b135168
robertd <robert.djurasaj@gmail.com>
2022-06-03 08:16:46 -0600

    add optional props and default values

--- packages/@aws-cdk/lambda-layer-kubectl/lib/kubectl-layer.ts
+++ packages/@aws-cdk/lambda-layer-kubectl/lib/kubectl-layer.ts
@@ -1,5 +1,3 @@
-// import * as crypto from 'crypto';
-// import * as fs from 'fs';
 import * as path from 'path';
 import * as lambda from '@aws-cdk/aws-lambda';
 import { Construct } from 'constructs';
@@ -9,24 +7,29 @@ import { Construct } from 'constructs';
  */
 export interface KubectlLayerProps {
   /**
-   * Kubectl Version which is actually a version of EKS cluster.
+   * Kubectl Version which is actually a version of k8s the EKS cluster is running.
    */
   readonly kubectlVersion: string;
+
+  /**
+   * Helm Charts version
+   */
+  readonly helmVersion: string;
 }
 
 /**
  * An AWS Lambda layer that includes `kubectl` and `helm`.
  */
 export class KubectlLayer extends lambda.LayerVersion {
-  constructor(scope: Construct, id: string, props: KubectlLayerProps) {
+  constructor(scope: Construct, id: string, props?: KubectlLayerProps) {
     super(scope, id, {
       code: lambda.Code.fromDockerBuild(path.join(__dirname, '..', 'layer'), {
         buildArgs: {
-          KUBECTL_VERSION: props.kubectlVersion,
-          HELM_VERSION: '3.8.2',
+          KUBECTL_VERSION: props?.kubectlVersion ?? '1.20.0',
+          HELM_VERSION: props?.helmVersion ?? '3.8.2',
         },
       }),
-      description: `/opt/kubectl/kubectl (${props.kubectlVersion}) and /opt/helm/helm`,
+      description: `/opt/kubectl/kubectl and /opt/helm/helm`,
     });
   }
 }
--- packages/@aws-cdk/lambda-layer-kubectl/test/kubectl-layer.test.ts
+++ packages/@aws-cdk/lambda-layer-kubectl/test/kubectl-layer.test.ts
@@ -7,7 +7,7 @@ test('synthesized to a layer version', () => {
   const stack = new Stack();
 
   // WHEN
-  new KubectlLayer(stack, 'MyLayer', { kubectlVersion: '1.22' });
+  new KubectlLayer(stack, 'MyLayer', { kubectlVersion: '1.22.6' });
 
   // THEN
   Template.fromStack(stack).hasResourceProperties('AWS::Lambda::LayerVersion', {
