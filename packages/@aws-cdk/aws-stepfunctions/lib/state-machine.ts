98ee140f0b238453016609d088fe497dffed13ea
Julian Kahnert <mail@juliankahnert.de>
2022-06-08 22:32:45 +0200

    fix lints II

--- packages/@aws-cdk/aws-stepfunctions/lib/state-machine.ts
+++ packages/@aws-cdk/aws-stepfunctions/lib/state-machine.ts
@@ -150,12 +150,12 @@ abstract class StateMachineBase extends Resource implements IStateMachine {
   /**
    * Import a state machine via resource name
    */
-  public static fromStateMachineName(scope: Construct, id: string, resourceName: string): IStateMachine {
+  public static fromStateMachineName(scope: Construct, id: string, stateMachineName: string): IStateMachine {
     const stateMachineArn = Stack.of(this).formatArn({
       service: 'states',
       resource: 'stateMachine',
       arnFormat: ArnFormat.COLON_RESOURCE_NAME,
-      resourceName,
+      resourceName: stateMachineName,
     });
     return this.fromStateMachineArn(scope, id, stateMachineArn);
   }
