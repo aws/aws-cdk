17b4b5601dea82eebfc92059eda7e1740c458c2f
Cory Hall <43035978+corymhall@users.noreply.github.com>
2022-06-20 12:01:45 -0400

    fix(events-targets): cloudwatch logs requires specific input template (#20748)
    The CloudWatch logs log group target requires a very specific input
    template. It does not support `input` or `inputPath` and if
    `inputTemplate` is specified it must be in the format of
    
    `{"timestamp": <time>, "message": <message>}`
    
    where both the values for `timestamp` and `message` are strings.
    
    This requirement is not very well documented, the only reference I could
    find is in a `Note` on this
    [page](https://docs.aws.amazon.com/eventbridge/latest/APIReference/API_PutTargets.html).
    
    This PR adds a new property `logEvent` and deprecates the `event`
    property to ensure that if the user adds an event input that it uses the
    correct format. While working on this PR is started by adding some
    validation if the user provides the `event` property and then went down
    the route of providing the new `logEvent` property. Since I already did
    the work of creating the validation for `event` I've kept it, but I'm
    open to just removing that logic since it is validating a `deprecated`
    property.
    
    I've also added an integration test that asserts that the expected
    message is written to the log group.
    
    fixes #19451
    
    
    ----
    
    ### All Submissions:
    
    * [x] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [x] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [x] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-events-targets/test/logs/log-group.test.ts
+++ packages/@aws-cdk/aws-events-targets/test/logs/log-group.test.ts
@@ -4,7 +4,8 @@ import * as logs from '@aws-cdk/aws-logs';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 import * as targets from '../../lib';
-
+import { LogGroupTargetInput } from '../../lib';
+import { testDeprecated } from '@aws-cdk/cdk-build-tools';
 
 test('use log group as an event rule target', () => {
   // GIVEN
@@ -54,7 +55,54 @@ test('use log group as an event rule target', () => {
   });
 });
 
-test('use log group as an event rule target with rule target input', () => {
+testDeprecated('use log group as an event rule target with rule target input', () => {
+  // GIVEN
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app);
+  const logGroup = new logs.LogGroup(stack, 'MyLogGroup', {
+    logGroupName: '/aws/events/MyLogGroup',
+  });
+  const rule1 = new events.Rule(stack, 'Rule', {
+    schedule: events.Schedule.rate(cdk.Duration.minutes(1)),
+  });
+
+  // WHEN
+  rule1.addTarget(new targets.CloudWatchLogGroup(logGroup, {
+    event: events.RuleTargetInput.fromObject({
+      message: events.EventField.fromPath('$'),
+    }),
+  }));
+
+
+  // THEN
+  expect(() => {
+    app.synth();
+  }).toThrow(/CloudWatchLogGroup targets only support input templates in the format/);
+});
+
+testDeprecated('cannot use both logEvent and event', () => {
+  // GIVEN
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app);
+  const logGroup = new logs.LogGroup(stack, 'MyLogGroup', {
+    logGroupName: '/aws/events/MyLogGroup',
+  });
+  const rule1 = new events.Rule(stack, 'Rule', {
+    schedule: events.Schedule.rate(cdk.Duration.minutes(1)),
+  });
+
+  // THEN
+  expect(() => {
+    rule1.addTarget(new targets.CloudWatchLogGroup(logGroup, {
+      event: events.RuleTargetInput.fromObject({
+        message: events.EventField.fromPath('$'),
+      }),
+      logEvent: LogGroupTargetInput.fromObject(),
+    }));
+  }).toThrow(/Only one of "event" or "logEvent" can be specified/);
+});
+
+test('logEvent with defaults', () => {
   // GIVEN
   const stack = new cdk.Stack();
   const logGroup = new logs.LogGroup(stack, 'MyLogGroup', {
@@ -66,8 +114,66 @@ test('use log group as an event rule target with rule target input', () => {
 
   // WHEN
   rule1.addTarget(new targets.CloudWatchLogGroup(logGroup, {
-    event: events.RuleTargetInput.fromObject({
-      data: events.EventField.fromPath('$'),
+    logEvent: LogGroupTargetInput.fromObject(),
+  }));
+
+  // THEN
+  Template.fromStack(stack).hasResourceProperties('AWS::Events::Rule', {
+    ScheduleExpression: 'rate(1 minute)',
+    State: 'ENABLED',
+    Targets: [
+      {
+        Arn: {
+          'Fn::Join': [
+            '',
+            [
+              'arn:',
+              {
+                Ref: 'AWS::Partition',
+              },
+              ':logs:',
+              {
+                Ref: 'AWS::Region',
+              },
+              ':',
+              {
+                Ref: 'AWS::AccountId',
+              },
+              ':log-group:',
+              {
+                Ref: 'MyLogGroup5C0DAD85',
+              },
+            ],
+          ],
+        },
+        Id: 'Target0',
+        InputTransformer: {
+          InputPathsMap: {
+            'time': '$.time',
+            'detail-type': '$.detail-type',
+          },
+          InputTemplate: '{"timestamp":<time>,"message":<detail-type>}',
+        },
+      },
+    ],
+  });
+});
+
+test('can use logEvent', () => {
+  // GIVEN
+  const stack = new cdk.Stack();
+  const logGroup = new logs.LogGroup(stack, 'MyLogGroup', {
+    logGroupName: '/aws/events/MyLogGroup',
+  });
+  const rule1 = new events.Rule(stack, 'Rule', {
+    schedule: events.Schedule.rate(cdk.Duration.minutes(1)),
+  });
+
+  // WHEN
+  rule1.addTarget(new targets.CloudWatchLogGroup(logGroup, {
+    logEvent: LogGroupTargetInput.fromObject({
+      timestamp: events.EventField.time,
+      message: events.EventField.fromPath('$'),
     }),
   }));
 
@@ -103,16 +209,17 @@ test('use log group as an event rule target with rule target input', () => {
         Id: 'Target0',
         InputTransformer: {
           InputPathsMap: {
-            f1: '$',
+            time: '$.time',
+            f2: '$',
           },
-          InputTemplate: '{"data":<f1>}',
+          InputTemplate: '{"timestamp":<time>,"message":<f2>}',
         },
       },
     ],
   });
 });
 
-test('specifying retry policy and dead letter queue', () => {
+testDeprecated('specifying retry policy and dead letter queue', () => {
   // GIVEN
   const stack = new cdk.Stack();
   const logGroup = new logs.LogGroup(stack, 'MyLogGroup', {
@@ -127,7 +234,8 @@ test('specifying retry policy and dead letter queue', () => {
   // WHEN
   rule1.addTarget(new targets.CloudWatchLogGroup(logGroup, {
     event: events.RuleTargetInput.fromObject({
-      data: events.EventField.fromPath('$'),
+      timestamp: events.EventField.time,
+      message: events.EventField.fromPath('$'),
     }),
     retryAttempts: 2,
     maxEventAge: cdk.Duration.hours(2),
@@ -174,9 +282,10 @@ test('specifying retry policy and dead letter queue', () => {
         Id: 'Target0',
         InputTransformer: {
           InputPathsMap: {
-            f1: '$',
+            time: '$.time',
+            f2: '$',
           },
-          InputTemplate: '{"data":<f1>}',
+          InputTemplate: '{"timestamp":<time>,"message":<f2>}',
         },
         RetryPolicy: {
           MaximumEventAgeInSeconds: 7200,
