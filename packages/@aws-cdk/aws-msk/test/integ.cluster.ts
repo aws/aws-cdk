b0f900df2c2269ceaec4ae4236ed5d753018a744
Mehatab Shaikh <88490687+mehatab@users.noreply.github.com>
2022-06-30 20:39:09 +0530

    removed iam auth cluster

--- packages/@aws-cdk/aws-msk/test/integ.cluster.ts
+++ packages/@aws-cdk/aws-msk/test/integ.cluster.ts
@@ -15,21 +15,8 @@ const cluster = new msk.Cluster(stack, 'Cluster', {
   removalPolicy: cdk.RemovalPolicy.DESTROY,
 });
 
-// with sasl/iam auth
-const cluster2 = new msk.Cluster(stack, 'Cluster2', {
-  clusterName: 'integ-test-sasl-iam-auth',
-  kafkaVersion: msk.KafkaVersion.V2_8_1,
-  vpc,
-  clientAuthentication: msk.ClientAuthentication.sasl({
-    iam: true,
-  }),
-  removalPolicy: cdk.RemovalPolicy.DESTROY,
-});
-
-
 // Test lazy instance of the AwsCustomResource
 new cdk.CfnOutput(stack, 'BootstrapBrokers', { value: cluster.bootstrapBrokersTls });
 new cdk.CfnOutput(stack, 'BootstrapBrokers2', { value: cluster.bootstrapBrokersTls });
-new cdk.CfnOutput(stack, 'BootstrapBrokers3', { value: cluster2.bootstrapBrokersSaslIam });
 
 app.synth();
