4af8b7342c211c867b480717b1d743a09fc3ef5f
corymhall <43035978+corymhall@users.noreply.github.com>
2022-07-06 18:44:03 +0000

    fix(cli): CLI errors when run as a non-existent user
    The CDK home directory was being created in the users home directory,
    but this causes an error if for some reason the user does not have a
    home directory. Since the cdk home directory is only being used to
    create a cache directory, this PR updates the location from being the
    users home directory to being the os tmp directory.
    
    I don't think there
    is a strong reason why this directory needs to live in the users home
    directory, but if there is I could also change this to have some
    fallback logic instead.
    
    fix #7937

--- packages/aws-cdk/lib/util/directories.ts
+++ packages/aws-cdk/lib/util/directories.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 export function cdkHomeDir() {
   return process.env.CDK_HOME
     ? path.resolve(process.env.CDK_HOME)
-    : path.join((os.userInfo().homedir ?? os.homedir()).trim() || '/', '.cdk');
+    : path.join(fs.realpathSync(os.tmpdir()).trim(), '.cdk');
 }
 
 export function cdkCacheDir() {
