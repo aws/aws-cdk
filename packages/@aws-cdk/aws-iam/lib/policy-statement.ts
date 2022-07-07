8759ac1f218c45e3908bbcd9d0898d1957da28a9
Rico Huijbers <rix0rrr@gmail.com>
2022-07-07 23:53:06 +0200

    fix(pipelines): cannot publish assets to more than 35 environments (#21010)
    We used to use `Lazy.list()` to render the list of Role ARNs that the
    Asset Publishing Role needs to assume.
    
    The Lazy list cannot be split up, meaning the overflow rendering logic
    we recently added cannot apply to this policy. Turn the lazy list into
    mutating a policy statement in-place, so the policy overflow logic can
    split the policy statement into multiple managed policies.
    
    Also in this PR:
    
    - To keep the tests behavior consistent (on whether arrays get rendered to the `Resource` field in statements or not, regardless of exactly which code path is taken), change the behavior of `PolicyStatement`, have it eagerly dedupe in memory.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-iam/lib/policy-statement.ts
+++ packages/@aws-cdk/aws-iam/lib/policy-statement.ts
@@ -69,20 +69,20 @@ export class PolicyStatement {
     return ret;
   }
 
-  private readonly _action = new Array<string>();
-  private readonly _notAction = new Array<string>();
+  private readonly _action = new OrderedSet<string>();
+  private readonly _notAction = new OrderedSet<string>();
   private readonly _principal: { [key: string]: any[] } = {};
   private readonly _notPrincipal: { [key: string]: any[] } = {};
-  private readonly _resource = new Array<string>();
-  private readonly _notResource = new Array<string>();
+  private readonly _resource = new OrderedSet<string>();
+  private readonly _notResource = new OrderedSet<string>();
   private readonly _condition: { [key: string]: any } = { };
   private _sid?: string;
   private _effect: Effect;
   private principalConditionsJson?: string;
 
   // Hold on to those principals
-  private readonly _principals = new Array<IPrincipal>();
-  private readonly _notPrincipals = new Array<IPrincipal>();
+  private readonly _principals = new OrderedSet<IPrincipal>();
+  private readonly _notPrincipals = new OrderedSet<IPrincipal>();
   private _frozen = false;
 
   constructor(props: PolicyStatementProps = {}) {
@@ -193,12 +193,15 @@ export class PolicyStatement {
    */
   public addPrincipals(...principals: IPrincipal[]) {
     this.assertNotFrozen('addPrincipals');
-    this._principals.push(...principals);
-    if (Object.keys(principals).length > 0 && Object.keys(this._notPrincipal).length > 0) {
+    if (principals.length > 0 && this._notPrincipals.length > 0) {
       throw new Error('Cannot add \'Principals\' to policy statement if \'NotPrincipals\' have been added');
     }
     for (const principal of principals) {
       this.validatePolicyPrincipal(principal);
+    }
+
+    const added = this._principals.push(...principals);
+    for (const principal of added) {
       const fragment = principal.policyFragment;
       mergePrincipal(this._principal, fragment.principalJson);
       this.addPrincipalConditions(fragment.conditions);
@@ -215,12 +218,15 @@ export class PolicyStatement {
    */
   public addNotPrincipals(...notPrincipals: IPrincipal[]) {
     this.assertNotFrozen('addNotPrincipals');
-    this._notPrincipals.push(...notPrincipals);
-    if (Object.keys(notPrincipals).length > 0 && Object.keys(this._principal).length > 0) {
+    if (notPrincipals.length > 0 && this._principals.length > 0) {
       throw new Error('Cannot add \'NotPrincipals\' to policy statement if \'Principals\' have been added');
     }
     for (const notPrincipal of notPrincipals) {
       this.validatePolicyPrincipal(notPrincipal);
+    }
+
+    const added = this._notPrincipals.push(...notPrincipals);
+    for (const notPrincipal of added) {
       const fragment = notPrincipal.policyFragment;
       mergePrincipal(this._notPrincipal, fragment.principalJson);
       this.addPrincipalConditions(fragment.conditions);
@@ -426,14 +432,14 @@ export class PolicyStatement {
    */
   public toStatementJson(): any {
     return normalizeStatement({
-      Action: this._action,
-      NotAction: this._notAction,
+      Action: this._action.direct(),
+      NotAction: this._notAction.direct(),
       Condition: this._condition,
       Effect: this.effect,
       Principal: this._principal,
       NotPrincipal: this._notPrincipal,
-      Resource: this._resource,
-      NotResource: this._notResource,
+      Resource: this._resource.direct(),
+      NotResource: this._notResource.direct(),
       Sid: this.sid,
     });
   }
@@ -503,7 +509,7 @@ export class PolicyStatement {
    */
   public validateForResourcePolicy(): string[] {
     const errors = this.validateForAnyPolicy();
-    if (Object.keys(this._principal).length === 0 && Object.keys(this._notPrincipal).length === 0) {
+    if (this._principals.length === 0 && this._notPrincipals.length === 0) {
       errors.push('A PolicyStatement used in a resource-based policy must specify at least one IAM principal.');
     }
     return errors;
@@ -516,10 +522,10 @@ export class PolicyStatement {
    */
   public validateForIdentityPolicy(): string[] {
     const errors = this.validateForAnyPolicy();
-    if (Object.keys(this._principal).length > 0 || Object.keys(this._notPrincipal).length > 0) {
+    if (this._principals.length > 0 || this._notPrincipals.length > 0) {
       errors.push('A PolicyStatement used in an identity-based policy cannot specify any IAM principals.');
     }
-    if (Object.keys(this._resource).length === 0 && Object.keys(this._notResource).length === 0) {
+    if (this._resource.length === 0 && this._notResource.length === 0) {
       errors.push('A PolicyStatement used in an identity-based policy must specify at least one resource.');
     }
     return errors;
@@ -529,42 +535,42 @@ export class PolicyStatement {
    * The Actions added to this statement
    */
   public get actions() {
-    return [...this._action];
+    return this._action.copy();
   }
 
   /**
    * The NotActions added to this statement
    */
   public get notActions() {
-    return [...this._notAction];
+    return this._notAction.copy();
   }
 
   /**
    * The Principals added to this statement
    */
   public get principals(): IPrincipal[] {
-    return [...this._principals];
+    return this._principals.copy();
   }
 
   /**
    * The NotPrincipals added to this statement
    */
   public get notPrincipals(): IPrincipal[] {
-    return [...this._notPrincipals];
+    return this._notPrincipals.copy();
   }
 
   /**
    * The Resources added to this statement
    */
   public get resources() {
-    return [...this._resource];
+    return this._resource.copy();
   }
 
   /**
    * The NotResources added to this statement
    */
   public get notResources() {
-    return [...this._notResource];
+    return this._notResource.copy();
   }
 
   /**
@@ -821,4 +827,54 @@ export function deriveEstimateSizeOptions(scope: IConstruct): EstimateSizeOption
   }
 
   return { actionEstimate, arnEstimate };
+}
+
+/**
+ * A class that behaves both as a set and an array
+ *
+ * Used for the elements of a PolicyStatement. In practice they behave as sets,
+ * but we have thousands of snapshot tests in existence that will rely on a
+ * particular order so we can't just change the type to `Set<>` wholesale without
+ * causing a lot of churn.
+ */
+class OrderedSet<A> {
+  private readonly set = new Set<A>();
+  private readonly array = new Array<A>();
+
+  /**
+   * Add new elements to the set
+   *
+   * @param xs the elements to be added
+   *
+   * @returns the elements actually added
+   */
+  public push(...xs: readonly A[]): A[] {
+    const ret = new Array<A>();
+    for (const x of xs) {
+      if (this.set.has(x)) {
+        continue;
+      }
+      this.set.add(x);
+      this.array.push(x);
+      ret.push(x);
+    }
+    return ret;
+  }
+
+  public get length() {
+    return this.array.length;
+  }
+
+  public copy(): A[] {
+    return [...this.array];
+  }
+
+  /**
+   * Direct (read-only) access to the underlying array
+   *
+   * (Saves a copy)
+   */
+  public direct(): readonly A[] {
+    return this.array;
+  }
 }
