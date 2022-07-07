42c6233c6bf0c3900f251ff503de8978c08e7688
corymhall <43035978+corymhall@users.noreply.github.com>
2022-07-07 12:24:06 +0000

    refactor to use fallback logic. First look for a home directory and then create a tmp dir.

--- packages/aws-cdk/lib/util/directories.ts
+++ packages/aws-cdk/lib/util/directories.ts
@@ -2,10 +2,28 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 
+/**
+ * Return a location that will be used as the CDK home directory.
+ * Currently the only thing that is placed here is the cache.
+ *
+ * First try to use the users home directory (i.e. /home/someuser/),
+ * but if that directory does not exist for some reason create a tmp directory.
+ *
+ * Typically it wouldn't make sense to create a one time use tmp directory for
+ * the purpose of creating a cache, but since this only applies to users that do
+ * not have a home directory (some CI systems?) this should be fine.
+ */
 export function cdkHomeDir() {
+  const tmpDir = fs.realpathSync(os.tmpdir());
+  let home;
+  try {
+    home = path.join((os.userInfo().homedir ?? os.homedir()).trim(), '.cdk');
+  } catch {
+    home = fs.mkdtempSync(path.join(tmpDir, '.cdk')).trim();
+  }
   return process.env.CDK_HOME
     ? path.resolve(process.env.CDK_HOME)
-    : path.join(fs.realpathSync(os.tmpdir()).trim(), '.cdk');
+    : home;
 }
 
 export function cdkCacheDir() {
