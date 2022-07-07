1c5db275e79864a67f29c9a6846d9ce43b9b6edf
Cory Hall <43035978+corymhall@users.noreply.github.com>
2022-07-07 14:53:37 -0400

    feat(integ-tests): expose adding IAM policies to the assertion provider (#20769)
    Currently the `AwsApiCall` construct will try and automatically create
    the correct IAM policy based on the service and api call being used. The
    assumption was that in most cases this would work, but it turns out that
    in the first couple use cases we've seen this is not the case.
    
    This PR adds another method `addToRolePolicy` on the
    `AssertionsProvider` construct and then makes the provider a `public`
    attribute on `AwsApICall`. This allows you to add additional policies.
    
    
    ----
    
    ### All Submissions:
    
    * [ ] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/integ-tests/lib/assertions/sdk.ts
+++ packages/@aws-cdk/integ-tests/lib/assertions/sdk.ts
@@ -10,6 +10,20 @@ import { AssertionsProvider, SDK_RESOURCE_TYPE_PREFIX } from './providers';
  */
 export interface IAwsApiCall extends IConstruct {
   /**
+   * access the AssertionsProvider. This can be used to add additional IAM policies
+   * the the provider role policy
+   *
+   * @example
+   * declare const apiCall: AwsApiCall;
+   * apiCall.provider.addToRolePolicy({
+   *   Effect: 'Allow',
+   *   Action: ['s3:GetObject'],
+   *   Resource: ['*'],
+   * });
+   */
+  readonly provider: AssertionsProvider;
+
+  /**
    * Returns the value of an attribute of the custom resource of an arbitrary
    * type. Attributes are returned from the custom resource provider through the
    * `Data` map where the key is the attribute name.
@@ -110,7 +124,7 @@ export class AwsApiCall extends Construct implements IAwsApiCall {
   private flattenResponse: string = 'false';
   private readonly name: string;
 
-  protected provider: AssertionsProvider;
+  public readonly provider: AssertionsProvider;
 
   constructor(scope: Construct, id: string, props: AwsApiCallProps) {
     super(scope, id);
