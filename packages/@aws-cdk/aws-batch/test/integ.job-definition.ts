c5b01061510f8c7ccd2a4388f64b4caf763cc547
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-07-13 15:07:01 +0300

    test: Try creating an integration test

---
+++ packages/@aws-cdk/aws-batch/test/integ.job-definition.ts
@@ -0,0 +1,49 @@
+/* eslint-disable quotes */
+import { ContainerImage } from "@aws-cdk/aws-ecs";
+import { App, Stack } from "@aws-cdk/core";
+import { ExpectedResult, IntegTest } from "@aws-cdk/integ-tests";
+import { Construct } from "constructs";
+import { JobDefinition } from "../lib";
+
+const jobDefinitionName = "aws-batch-with-fallback-env";
+
+class SampleStack extends Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new JobDefinition(this, "batch", {
+      jobDefinitionName,
+      container: {
+        image: ContainerImage.fromRegistry("docker/whalesay"),
+      },
+    });
+  }
+}
+
+// Beginning of the test suite
+const app = new App();
+
+const integ = new IntegTest(app, "IntegTest-aws-batch-default-env-variables", {
+  testCases: [
+    new SampleStack(app, "SampleStack-aws-batch-default-env-variables"),
+  ],
+});
+const awsApiCallResponse = integ.assertions.awsApiCall(
+  "Batch",
+  "describeJobDefinitions",
+  {
+    jobDefinitionName,
+  },
+);
+
+awsApiCallResponse.assertAtPath(
+  "jobDefinitions.0.containerProperties.environment",
+  ExpectedResult.arrayWith([
+    {
+      name: "AWS_REGION",
+      value: "",
+    },
+  ]),
+);
+
+app.synth();
