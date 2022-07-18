d579e8622dbad114a9d4560fa34c6490a1418557
Otavio Macedo <otaviomacedo@protonmail.com>
2022-07-18 15:33:29 +0100

    fix(core): CustomResourceProvider assets are staged in node_modules (#20953)
    Instead of using the source code directory as a staging directory (which, from the point of view of the consumer, is inside the `node_modules` directory), create a temporary directory for staging.
    
    Fixes https://github.com/aws/aws-cdk/issues/17460.
    
    ----
    
    ### All Submissions:
    
    * [x] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/pipelines/test/integ.newpipeline-with-vpc.ts
+++ packages/@aws-cdk/pipelines/test/integ.newpipeline-with-vpc.ts
@@ -51,7 +51,5 @@ const app = new App({
     '@aws-cdk/core:newStyleStackSynthesis': '1',
   },
 });
-new PipelineStack(app, 'PipelineStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
+new PipelineStack(app, 'PipelineStack');
 app.synth();
