c25d3975d303259c16f33153dd9a9bda943986e8
Rico Huijbers <rix0rrr@gmail.com>
2022-07-19 20:16:13 +0200

    chore: handle rejected CFN spec updates (#21236)
    Retain rejected spec updates and add a script that can report on the
    rejections. 
    
    We will add this to a cron job that will create tickets if there are rejected spec updates.
    
    ----
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/cfnspec/build-tools/report-issues.ts
+++ packages/@aws-cdk/cfnspec/build-tools/report-issues.ts
@@ -1,10 +1,16 @@
 /* eslint-disable no-console */
 /**
  * Report on spec fragment files that are being held back.
+ *
+ * Report formats:
+ *
+ * - 'outdated'/'changelog': print for changelog format
+ * - 'rejected': print validation errors, exit with error code 1 if there are any
  */
 
 import * as path from 'path';
 import * as fs from 'fs-extra';
+import { CfnSpecValidator, formatErrorInContext } from './validate-cfn';
 
 async function main(args: string[]) {
   if (args.length < 2) {
@@ -23,17 +29,42 @@ async function main(args: string[]) {
 
     const json = await fs.readJson(path.join(dir, file));
     const fragmentVersion = json.$version;
+    const serviceName = file.replace(/^000_/, '').replace(/\.json$/, '').replace('_', '::');
 
     switch (format) {
       case 'outdated':
+      case 'changelog':
         if (fragmentVersion !== officialVersion) {
           if (!headerPrinted) {
             console.log('## Unapplied changes');
             console.log('');
             headerPrinted = true;
           }
-          const serviceName = file.replace(/^000_/, '').replace(/\.json$/, '').replace('_', '::');
           console.log(`* ${serviceName} is at ${fragmentVersion}`);
+        }
+        break;
+
+      case 'rejected':
+        if (fragmentVersion !== officialVersion) {
+          // Read the 'rejected' file, parse it (which we expect to fail)
+          // and print the failures.
+          const rejectedFileName = `.${file.replace(/.json$/, '.rejected.json')}`;
+          const rejectedPath = path.join(dir, rejectedFileName);
+          if (!await fs.pathExists(rejectedPath)) {
+            // If for whatever reason the file doesn't exist, ignore
+            continue;
+          }
+          const rejectedSpec = await fs.readJson(rejectedPath);
+
+          const errors = CfnSpecValidator.validate(rejectedSpec);
+
+          console.warn('='.repeat(70));
+          console.warn(' '.repeat(Math.floor(35 - serviceName.length / 2)) + serviceName);
+          console.warn('='.repeat(70));
+          for (const error of errors) {
+            console.warn(formatErrorInContext(error));
+            process.exitCode = 1;
+          }
         }
         break;
 
