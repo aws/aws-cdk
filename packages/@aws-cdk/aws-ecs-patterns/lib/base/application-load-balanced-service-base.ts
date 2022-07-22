fa3725d33ec3f167bf47feac16e8bc1a39bf8da7
Mike Dreyfus <mdreyfus@twitch.tv>
2022-07-21 23:47:35 -0700

    error message cleanup

--- packages/@aws-cdk/aws-ecs-patterns/lib/base/application-load-balanced-service-base.ts
+++ packages/@aws-cdk/aws-ecs-patterns/lib/base/application-load-balanced-service-base.ts
@@ -444,7 +444,7 @@ export abstract class ApplicationLoadBalancedServiceBase extends Construct {
 
     if (props.idleTimeout) {
       if (props.idleTimeout > Duration.seconds(4000) || props.idleTimeout < Duration.seconds(1)) {
-        throw new Error( `IdleTime must be between 1 and 4000 seconds  ${props.idleTimeout}`);
+        throw new Error( 'IdleTime must be between 1 and 4000 seconds');
       }
     }
 
