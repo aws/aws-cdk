aws/aws-cdk

commit 17f1ec881ba8fb300bd4cf8674a87640ab05c31a

feat(core): cache fingerprints of large assets (#21321)
When fingerprinting large assets, hashing the asset can take quite a
long time - over a second for a 300MB asset, for example. This can add
up, particularly when generating multiple stacks in a single build, or
when running test suites that bundle assets multiple times, and is not
avoidable by asset caching (since it's computing the cache key).

This change caches the result of digesting individual files based on the
inode, mtime, and size of the input file.

This feature improved the runtime of one of our slowest tests by ~10%.

closes: #21297

Note: No README entries were added, because this sub-subsystem was already not documented in the README.


----

### All Submissions:

* [x] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)

### New Features

* [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)? N/A

packages/@aws-cdk/core/lib/fs/fingerprint.ts
@@ -1,6 +1,7 @@
 import * as crypto from 'crypto';
 import * as fs from 'fs';
 import * as path from 'path';
+import { Cache } from '../private/cache';
 import { IgnoreStrategy } from './ignore';
 import { FingerprintOptions, IgnoreMode, SymlinkFollowMode } from './options';
 import { shouldFollow } from './utils';
@@ -12,6 +13,17 @@ const CTRL_ETX = '\x03';
 const CR = '\r';
 const LF = '\n';
 const CRLF = `${CR}${LF}`;
+
+const fingerprintCache = new Cache<string>();
+
+/**
+ * Files are fingerprinted only the first time they are encountered, to save
+ * time hashing large files. This function clears this cache, should it be
+ * necessary for some reason.
+ */
+export function clearLargeFileFingerprintCache() {
+  fingerprintCache.clear();
+}
 
 /**
  * Produces fingerprint based on the contents of a single file or an entire directory tree.
@@ -84,6 +96,36 @@ export function fingerprint(fileOrDirectory: string, options: FingerprintOptions
 }
 
 export function contentFingerprint(file: string): string {
+  // On windows it's particularly important to pass bigint: true to ensure that
+  // floating-point inaccuracies don't result in false matches. ( see
+  // https://github.com/nodejs/node/issues/12115#issuecomment-438741212 )
+  //
+  // Note that even if we do get a inode collision somehow, it's unlikely that
+  // both mtime and size would have a false-positive as well.
+
+  // Note: Node introduced statSync's bigint argument around Node 12. We check
+  // here to make sure that we're using node 12 or higher and bypass the cache
+  // otherwise.
+
+  if (Number(process.versions.node.split('.')[0]) < 12) {
+    return contentFingerprintMiss(file);
+  }
+
+  // We also must suppress typescript typechecks as we are using a version of
+  // @types/node that only supports node 10 declarations.
+  // @ts-ignore
+  const stats = fs.statSync(file, { bigint: true });
+  const cacheKey = JSON.stringify({
+    mtime_unix: stats.mtime.getUTCDate(),
+    mtime_ms: stats.mtime.getUTCMilliseconds(),
+    inode: stats.ino.toString(),
+    size: stats.size.toString(),
+  });
+
+  return fingerprintCache.obtain(cacheKey, () => contentFingerprintMiss(file));
+}
+
+function contentFingerprintMiss(file: string): string {
   const hash = crypto.createHash('sha256');
   const buffer = Buffer.alloc(BUFFER_SIZE);
   // eslint-disable-next-line no-bitwise

packages/@aws-cdk/core/test/fs/fs-fingerprint.test.ts
@@ -124,7 +124,6 @@ describe('fs fingerprint', () => {
       // THEN
       expect(original).not.toEqual(afterChange);
       expect(afterRevert).toEqual(original);
-
     });
 
     test('does not change with the contents of un-followed symlink referent', () => {
@@ -176,6 +175,51 @@ describe('fs fingerprint', () => {
 
       fs.unlinkSync(crlf);
 
+    });
+  });
+
+  // The fingerprint cache is only enabled for node v12 and higher as older
+  // versions can have false positive inode comparisons due to floating point
+  // rounding error.
+  const describe_nodev12 = Number(process.versions.node.split('.')[0]) < 12 ? describe.skip : describe;
+  describe_nodev12('fingerprint cache', () => {
+    const testString = 'hello world';
+    const testFile = path.join(__dirname, 'inode-fp.1');
+    // This always-false ternary is just to help typescript infer the type properly
+    let openSyncSpy = false ? jest.spyOn(fs, 'openSync') : undefined;
+
+    // Create a very large test file
+    beforeAll(() => {
+      const file = fs.openSync(testFile, 'w');
+      fs.writeSync(file, testString);
+      fs.closeSync(file);
+      openSyncSpy = jest.spyOn(fs, 'openSync');
+    });
+
+    afterAll(() => {
+      fs.unlinkSync(testFile);
+      openSyncSpy?.mockRestore();
+    });
+
+    test('caches fingerprint results', () => {
+      const hash1 = FileSystem.fingerprint(testFile, {});
+      const hash2 = FileSystem.fingerprint(testFile, {});
+
+      expect(hash1).toEqual(hash2);
+      expect(openSyncSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('considers mtime', () => {
+      const hash1 = FileSystem.fingerprint(testFile, {});
+
+      const file = fs.openSync(testFile, 'r+');
+      fs.writeSync(file, 'foobar');
+      fs.closeSync(file);
+
+      const hash2 = FileSystem.fingerprint(testFile, {});
+
+      expect(hash1).not.toEqual(hash2);
+      expect(openSyncSpy).toHaveBeenCalledTimes(3);
     });
   });
 

packages/@aws-cdk/core/lib/asset-staging.ts
@@ -7,6 +7,7 @@ import * as fs from 'fs-extra';
 import { AssetHashType, AssetOptions, FileAssetPackaging } from './assets';
 import { BundlingOptions, BundlingOutput } from './bundling';
 import { FileSystem, FingerprintOptions } from './fs';
+import { clearLargeFileFingerprintCache } from './fs/fingerprint';
 import { Names } from './names';
 import { Cache } from './private/cache';
 import { Stack } from './stack';
@@ -83,6 +84,7 @@ export class AssetStaging extends Construct {
    */
   public static clearAssetHashCache() {
     this.assetCache.clear();
+    clearLargeFileFingerprintCache();
   }
 
   /**

commit 24ec9b0c8ddcb695328146ab4cc7ff0b25cea3f1

fix(core): asset fingerprint cache invalidation incorrectly uses mtime (#21374)
Instead of using the complete mtime value, it only accounted for the day-of-month
and fractional seconds part of the timestamp, which is not the intention.

The issue was introduced in #21321

packages/@aws-cdk/core/lib/fs/fingerprint.ts
@@ -103,21 +103,12 @@ export function contentFingerprint(file: string): string {
   // Note that even if we do get a inode collision somehow, it's unlikely that
   // both mtime and size would have a false-positive as well.
 
-  // Note: Node introduced statSync's bigint argument around Node 12. We check
-  // here to make sure that we're using node 12 or higher and bypass the cache
-  // otherwise.
-
-  if (Number(process.versions.node.split('.')[0]) < 12) {
-    return contentFingerprintMiss(file);
-  }
-
   // We also must suppress typescript typechecks as we are using a version of
   // @types/node that only supports node 10 declarations.
-  // @ts-ignore
   const stats = fs.statSync(file, { bigint: true });
   const cacheKey = JSON.stringify({
-    mtime_unix: stats.mtime.getUTCDate(),
-    mtime_ms: stats.mtime.getUTCMilliseconds(),
+    mtime_unix: stats.mtime.toUTCString(),
+    mtime_ms: stats.mtimeMs.toString(),
     inode: stats.ino.toString(),
     size: stats.size.toString(),
   });

packages/@aws-cdk/integ-runner/lib/runner/snapshot-test-runner.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import { Writable, WritableOptions } from 'stream';
-import { StringDecoder, NodeStringDecoder } from 'string_decoder';
+import { StringDecoder } from 'string_decoder';
 import { diffTemplate, formatDifferences, ResourceDifference, ResourceImpact } from '@aws-cdk/cloudformation-diff';
 import { Diagnostic, DiagnosticReason, DestructiveChange, SnapshotVerificationOptions } from '../workers/common';
 import { canonicalizeTemplate } from './private/canonicalize-assets';
@@ -229,7 +229,7 @@ export class IntegSnapshotRunner extends IntegRunner {
 
 class StringWritable extends Writable {
   public data: string;
-  private _decoder: NodeStringDecoder;
+  private _decoder: StringDecoder;
   constructor(options: WritableOptions) {
     super(options);
     this._decoder = new StringDecoder();

packages/cdk-assets/lib/private/shell.ts
@@ -25,22 +25,22 @@ export async function shell(command: string[], options: ShellOptions = {}): Prom
 
   return new Promise<string>((resolve, reject) => {
     if (options.input) {
-      child.stdin.write(options.input);
-      child.stdin.end();
+      child.stdin!.write(options.input);
+      child.stdin!.end();
     }
 
     const stdout = new Array<any>();
     const stderr = new Array<any>();
 
     // Both write to stdout and collect
-    child.stdout.on('data', chunk => {
+    child.stdout!.on('data', chunk => {
       if (!options.quiet) {
         process.stdout.write(chunk);
       }
       stdout.push(chunk);
     });
 
-    child.stderr.on('data', chunk => {
+    child.stderr!.on('data', chunk => {
       if (!options.quiet) {
         process.stderr.write(chunk);
       }
@@ -50,12 +50,12 @@ export async function shell(command: string[], options: ShellOptions = {}): Prom
 
     child.once('error', reject);
 
-    child.once('close', code => {
+    child.once('close', (code, signal) => {
       if (code === 0) {
         resolve(Buffer.concat(stdout).toString('utf-8'));
       } else {
         const out = Buffer.concat(stderr).toString('utf-8').trim();
-        reject(new ProcessFailed(code, `${renderCommandLine(command)} exited with error code ${code}: ${out}`));
+        reject(new ProcessFailed(code, signal, `${renderCommandLine(command)} exited with ${code != null ? 'error code' : 'signal'} ${code ?? signal}: ${out}`));
       }
     });
   });
@@ -64,7 +64,7 @@ export async function shell(command: string[], options: ShellOptions = {}): Prom
 class ProcessFailed extends Error {
   public readonly code = 'PROCESS_FAILED';
 
-  constructor(public readonly exitCode: number, message: string) {
+  constructor(public readonly exitCode: number | null, public readonly signal: NodeJS.Signals | null, message: string) {
     super(message);
   }
 }

tools/@aws-cdk/pkglint/lib/rules.ts
@@ -879,11 +879,11 @@ export class NodeCompatibility extends ValidationRule {
 
   public validate(pkg: PackageJson): void {
     const atTypesNode = pkg.getDevDependency('@types/node');
-    if (atTypesNode && !atTypesNode.startsWith('^10.')) {
+    if (atTypesNode && !atTypesNode.startsWith('^14.')) {
       pkg.report({
         ruleName: this.name,
-        message: `packages must support node version 10 and up, but ${atTypesNode} is declared`,
-        fix: () => pkg.addDevDependency('@types/node', '^10.17.5'),
+        message: `packages must support node version 14 and up, but ${atTypesNode} is declared`,
+        fix: () => pkg.addDevDependency('@types/node', '^14.18.22'),
       });
     }
   }

packages/@aws-cdk/core/test/fs/fs-fingerprint.test.ts
@@ -216,6 +216,10 @@ describe('fs fingerprint', () => {
       fs.writeSync(file, 'foobar');
       fs.closeSync(file);
 
+      // Update mtime to a value that is guaranteed to be different even if the tests run... fast!
+      const fileStat = fs.statSync(testFile, { bigint: true });
+      fs.utimesSync(testFile, fileStat.atime, new Date(1337));
+
       const hash2 = FileSystem.fingerprint(testFile, {});
 
       expect(hash1).not.toEqual(hash2);

packages/aws-cdk/test/api/util/display.test.ts
@@ -29,7 +29,7 @@ describe('Rewritable Block Tests', () => {
 
   test('displayLines writes maximum lines based on lines length if rows is undefined', () => {
     const lines = Array.from(Array(5).keys()).map(line => line.toString());
-    process.stderr.rows = undefined;
+    process.stderr.rows = undefined as any;
     const output = stderr.inspectSync(() => {
       block.displayLines(lines);
     });

packages/aws-cdk/test/diff.test.ts
@@ -1,5 +1,5 @@
 import { Writable } from 'stream';
-import { NodeStringDecoder, StringDecoder } from 'string_decoder';
+import { StringDecoder } from 'string_decoder';
 import * as cxschema from '@aws-cdk/cloud-assembly-schema';
 import { CloudFormationStackArtifact } from '@aws-cdk/cx-api';
 import { CloudFormationDeployments } from '../lib/api/cloudformation-deployments';
@@ -236,18 +236,19 @@ describe('nested stacks', () => {
     });
 
     // THEN
-    const plainTextOutput = buffer.data.replace(/\x1B\[[0-?]*[ -/]*[@-~]/g, '');
+    const plainTextOutput = buffer.data.replace(/\x1B\[[0-?]*[ -/]*[@-~]/g, '')
+      .replace(/[ \t]+$/mg, '');
     expect(plainTextOutput.trim()).toEqual(`Stack Parent
 Resources
-[~] AWS::CloudFormation::Stack AdditionChild 
+[~] AWS::CloudFormation::Stack AdditionChild
  └─ [~] Resources
      └─ [~] .SomeResource:
          └─ [+] Added: .Properties
-[~] AWS::CloudFormation::Stack DeletionChild 
+[~] AWS::CloudFormation::Stack DeletionChild
  └─ [~] Resources
      └─ [~] .SomeResource:
          └─ [-] Removed: .Properties
-[~] AWS::CloudFormation::Stack ChangedChild 
+[~] AWS::CloudFormation::Stack ChangedChild
  └─ [~] Resources
      └─ [~] .SomeResource:
          └─ [~] .Properties:
@@ -261,7 +262,7 @@ Resources
 
 class StringWritable extends Writable {
   public data: string;
-  private readonly _decoder: NodeStringDecoder;
+  private readonly _decoder: StringDecoder;
 
   constructor(options: any = {}) {
     super(options);

packages/aws-cdk/test/version.test.ts
@@ -86,9 +86,8 @@ test('Skip version check if environment variable is set', async () => {
 });
 
 describe('version message', () => {
-  let previousIsTty: true | undefined;
+  const previousIsTty = process.stdout.isTTY;
   beforeAll(() => {
-    previousIsTty = process.stdout.isTTY;
     process.stdout.isTTY = true;
   });
 