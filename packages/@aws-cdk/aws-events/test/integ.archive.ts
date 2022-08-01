c2c97ff10638d969d57e0b7bf1dc06a5d99992db
Joshua Weber <josh@joshuaw.de>
2022-08-01 19:10:48 +0200

    :white_check_mark: Adds the IntegTest structure to the integration test

--- packages/@aws-cdk/aws-events/test/integ.archive.ts
+++ packages/@aws-cdk/aws-events/test/integ.archive.ts
@@ -1,4 +1,5 @@
 import { App, Stack } from '@aws-cdk/core';
+import { IntegTest } from '@aws-cdk/integ-tests';
 import { Archive, CfnArchive, EventBus } from '../lib';
 
 const app = new App();
@@ -14,5 +15,9 @@ const archive = new Archive(stack, 'Archive', {
   },
 });
 (archive.node.defaultChild as CfnArchive).overrideLogicalId('MyCustomArchive');
+
+new IntegTest(app, 'ArchiveTest', {
+  testCases: [stack],
+});
 
 app.synth();
