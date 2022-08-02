13835b3236c6a171780e1ae8843261269bb53074
Yakir Zana <yakirz@wix.com>
2022-08-02 11:42:52 +0000

    Fix integ test

--- packages/@aws-cdk/aws-lambda/test/integ.lambda.docker-arm64.ts
+++ packages/@aws-cdk/aws-lambda/test/integ.lambda.docker-arm64.ts
@@ -1,23 +1,19 @@
 import * as path from 'path';
 import { App, Stack } from '@aws-cdk/core';
+import * as integ from '@aws-cdk/integ-tests';
 import { Architecture, DockerImageCode, DockerImageFunction } from '../lib';
-
-class TestStack extends Stack {
-  constructor(scope: App, id: string) {
-    super(scope, id);
-
-    /**
-     * The platform will be set to ARM_64 automatically based on the `architecture` property.
-     */
-    new DockerImageFunction(this, 'MyLambda', {
-      code: DockerImageCode.fromImageAsset(path.join(__dirname, 'docker-arm64-handler')),
-      architecture: Architecture.ARM_64,
-    });
-  }
-}
 
 const app = new App();
 
-new TestStack(app, 'lambda-ecr-docker-arm64');
+const stack = new Stack(app, 'lambda-ecr-docker-arm64');
+
+new DockerImageFunction(stack, 'MyLambda', {
+  code: DockerImageCode.fromImageAsset(path.join(__dirname, 'docker-arm64-handler')),
+  architecture: Architecture.ARM_64,
+});
+
+new integ.IntegTest(app, 'lambda-docker-arm64', {
+  testCases: [stack],
+});
 
 app.synth();
