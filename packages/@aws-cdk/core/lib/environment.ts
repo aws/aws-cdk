ddb4bcf11f89eea8d448ad9da0b16f33f6af72ee
Momo Kornher <kornherm@amazon.co.uk>
2022-08-03 17:57:08 +0100

    docs(core): reference correct properties for Cloudformation pseudo parameters
    These have been refactored to all caps but not all doc references got updated.

--- packages/@aws-cdk/core/lib/environment.ts
+++ packages/@aws-cdk/core/lib/environment.ts
@@ -5,28 +5,28 @@ export interface Environment {
   /**
    * The AWS account ID for this environment.
    *
-   * This can be either a concrete value such as `585191031104` or `Aws.accountId` which
+   * This can be either a concrete value such as `585191031104` or `Aws.ACCOUNT_ID` which
    * indicates that account ID will only be determined during deployment (it
    * will resolve to the CloudFormation intrinsic `{"Ref":"AWS::AccountId"}`).
    * Note that certain features, such as cross-stack references and
    * environmental context providers require concerete region information and
    * will cause this stack to emit synthesis errors.
    *
-   * @default Aws.accountId which means that the stack will be account-agnostic.
+   * @default Aws.ACCOUNT_ID which means that the stack will be account-agnostic.
    */
   readonly account?: string;
 
   /**
    * The AWS region for this environment.
    *
-   * This can be either a concrete value such as `eu-west-2` or `Aws.region`
+   * This can be either a concrete value such as `eu-west-2` or `Aws.Region`
    * which indicates that account ID will only be determined during deployment
    * (it will resolve to the CloudFormation intrinsic `{"Ref":"AWS::Region"}`).
    * Note that certain features, such as cross-stack references and
-   * environmental context providers require concerete region information and
+   * environmental context providers require concrete region information and
    * will cause this stack to emit synthesis errors.
    *
-   * @default Aws.region which means that the stack will be region-agnostic.
+   * @default Aws.Region which means that the stack will be region-agnostic.
    */
   readonly region?: string;
 }
