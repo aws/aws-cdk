fe6bf6f455c2e2146a5c9d7ed0c5aeb3e79ba537
Niko Virtala <niko.virtala@hey.com>
2022-08-04 13:46:47 +0300

    update integration test with assertion

--- packages/@aws-cdk/aws-appsync/test/integ.log-retention.ts
+++ packages/@aws-cdk/aws-appsync/test/integ.log-retention.ts
@@ -1,23 +1,40 @@
 import { join } from 'path';
 import { RetentionDays } from '@aws-cdk/aws-logs';
 import { App, Stack } from '@aws-cdk/core';
-import { IntegTest } from '@aws-cdk/integ-tests';
+import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests';
 import { GraphqlApi, LogConfig, Schema } from '../lib';
 
 const app = new App();
 const stack = new Stack(app, 'AppSyncIntegLogRetention');
 
+
+const retentionTime = RetentionDays.ONE_WEEK;
 const logConfig: LogConfig = {
-  retention: RetentionDays.ONE_WEEK,
+  retention: retentionTime,
 };
 
-new GraphqlApi(stack, 'GraphqlApi', {
+const api = new GraphqlApi(stack, 'GraphqlApi', {
   authorizationConfig: {},
   name: 'IntegLogRetention',
   schema: Schema.fromAsset(join(__dirname, 'appsync.test.graphql')),
   logConfig,
 });
 
-new IntegTest(app, 'Integ', { testCases: [stack] });
+const integ = new IntegTest(app, 'Integ', { testCases: [stack] });
+
+const describe = integ.assertions.awsApiCall('CloudWatchLogs',
+  'describeLogGroups',
+  {
+    logGroupNamePrefix: api.logGroup.logGroupName,
+  });
+
+describe.expect(ExpectedResult.objectLike({
+  logGroups: [
+    {
+      logGroupName: api.logGroup.logGroupName,
+      retentionInDays: retentionTime,
+    },
+  ],
+}));
 
 app.synth();
