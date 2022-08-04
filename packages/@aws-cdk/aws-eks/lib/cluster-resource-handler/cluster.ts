aws/aws-cdk

commit b36593569484a8881a26476b5cebfbb275505e1a

fix(eks): cannot disable cluster logging once it has been enabled (#21185)
### Description

Amazon EKS Loggings was first introduced in #18112 and added Fargate logging support in #20707. However, disabled loggings was not take into consideration at the very beginning of the design.

With this PR, enabled clusterLogging support for enablement/disablement. Also, added test cases for no logging configured, partial logging configured, full logging configured.

Fixes: #19898

### Test Results

#### Tool versions

```sh
$ node --version
v16.16.0
```

```sh
$ yarn --version
1.22.19
```

#### yarn build

```sh
$ yarn build
yarn run v1.22.19
$ cdk-build
...

Build times for @aws-cdk/aws-eks: Total time (59.5s) | /home/ec2-user/aws-cdk/tools/@aws-cdk/cdk-build-tools/node_modules/jsii/bin/jsii (35.0s) | cfn2ts (0.6s)
Done in 59.94s.
```

#### yarn test

```sh
$ yarn test

yarn run v1.22.19
$ cdk-test
PASS test/cluster.test.js (33.488 s)
PASS test/nodegroup.test.js (10.465 s)
PASS test/helm-chart.test.js (5.964 s)
PASS test/fargate.test.js (5.391 s)
PASS test/k8s-manifest.test.js
PASS test/service-account.test.js
PASS test/user-data.test.js
PASS test/awsauth.test.js
PASS test/alb-controller.test.js
PASS test/k8s-patch.test.js
...

=============================== Coverage summary ===============================
Statements   : 93.08% ( 888/954 )
Branches     : 89.47% ( 442/494 )
Functions    : 94.03% ( 142/151 )
Lines        : 94.83% ( 881/929 )
================================================================================

Test Suites: 13 passed, 13 total
Tests:       277 passed, 277 total
Snapshots:   0 total
Time:        77.227 s, estimated 91 s
Ran all test suites.

Verifying integration test snapshots...

  UNCHANGED  integ.eks-oidc-provider 27.294s
  UNCHANGED  integ.fargate-cluster 35.153s
  UNCHANGED  integ.eks-cluster-private-endpoint 35.223s
  UNCHANGED  integ.eks-helm-asset 35.602s
  UNCHANGED  integ.eks-bottlerocket-ng 35.981s

packages/@aws-cdk/aws-eks/lib/cluster-resource-handler/cluster.ts
@@ -290,6 +290,10 @@ function parseProps(props: any): aws.EKS.CreateClusterRequest {
     parsed.logging.clusterLogging[0].enabled = parsed.logging.clusterLogging[0].enabled === 'true';
   }
 
+  if (typeof (parsed.logging?.clusterLogging[1].enabled) === 'string') {
+    parsed.logging.clusterLogging[1].enabled = parsed.logging.clusterLogging[1].enabled === 'false';
+  }
+
   return parsed;
 
 }

packages/@aws-cdk/aws-eks/lib/cluster.ts
@@ -1285,7 +1285,7 @@ export class Cluster extends ClusterBase {
 
   private readonly version: KubernetesVersion;
 
-  private readonly logging?: { [key: string]: [ { [key: string]: any } ] };
+  private readonly logging?: { [key: string]: [ { [key: string]: any }, { [key: string]: any } ] };
 
   /**
    * A dummy CloudFormation resource that is used as a wait barrier which
@@ -1347,11 +1347,27 @@ export class Cluster extends ClusterBase {
     // Get subnetIds for all selected subnets
     const subnetIds = Array.from(new Set(flatten(selectedSubnetIdsPerGroup)));
 
+    // The value of clusterLoggingTypeDisabled should be invert of props.clusterLogging.
+    let clusterLoggingTypeDisabled: ClusterLoggingTypes[] = [];
+
+    // Find out type(s) to disable.
+    Object.values(ClusterLoggingTypes).forEach(function (key) {
+      let clusterLoggingTypeEnabled = Object.values(props.clusterLogging ? Object.values(props.clusterLogging) : []);
+      if (!Object.values(clusterLoggingTypeEnabled).includes(key)) {
+        clusterLoggingTypeDisabled.push(key);
+      };
+    });
+
+    // Leave it untouched as undefined if (props.clusterLogging === undefined).
     this.logging = props.clusterLogging ? {
       clusterLogging: [
         {
           enabled: true,
           types: Object.values(props.clusterLogging),
+        },
+        {
+          enabled: false,
+          types: Object.values(clusterLoggingTypeDisabled),
         },
       ],
     } : undefined;

packages/@aws-cdk/aws-eks/lib/cluster-resource.ts
@@ -25,7 +25,7 @@ export interface ClusterResourceProps {
   readonly onEventLayer?: lambda.ILayerVersion;
   readonly clusterHandlerSecurityGroup?: ec2.ISecurityGroup;
   readonly tags?: { [key: string]: string };
-  readonly logging?: { [key: string]: [ { [key: string]: any } ] };
+  readonly logging?: { [key: string]: [ { [key: string]: any }, { [key: string]: any } ] };
 }
 
 /**

packages/@aws-cdk/aws-eks/test/cluster-resource-provider.test.ts
@@ -566,6 +566,10 @@ describe('cluster resource provider', () => {
                   types: ['api'],
                   enabled: true,
                 },
+                {
+                  types: ['audit', 'authenticator', 'controllerManager', 'scheduler'],
+                  enabled: false,
+                },
               ],
             },
           }, {
@@ -580,6 +584,10 @@ describe('cluster resource provider', () => {
                 {
                   types: ['api'],
                   enabled: true,
+                },
+                {
+                  types: ['audit', 'authenticator', 'controllerManager', 'scheduler'],
+                  enabled: false,
                 },
               ],
             },
@@ -622,6 +630,10 @@ describe('cluster resource provider', () => {
                   types: ['api', 'audit', 'authenticator', 'controllerManager', 'scheduler'],
                   enabled: true,
                 },
+                {
+                  types: [],
+                  enabled: false,
+                },
               ],
             },
             resourcesVpcConfig: {
@@ -643,6 +655,10 @@ describe('cluster resource provider', () => {
                 {
                   types: ['api', 'audit', 'authenticator', 'controllerManager', 'scheduler'],
                   enabled: true,
+                },
+                {
+                  types: [],
+                  enabled: false,
                 },
               ],
             },

packages/@aws-cdk/aws-eks/test/cluster.test.ts
@@ -3156,4 +3156,98 @@ describe('cluster', () => {
       },
     });
   });
+
+  test('create a cluster without logging configure', () => {
+    // GIVEN
+    const { stack } = testFixture();
+
+    // WHEN
+    new eks.Cluster(stack, 'Cluster', {
+      version: CLUSTER_VERSION,
+    });
+
+    // THEN
+    Template.fromStack(stack).resourceCountIs('Custom::AWSCDK-EKS-Cluster::Config::logging', 0);
+  });
+
+  test('create a cluster with partial logging configure', () => {
+    // GIVEN
+    const { stack } = testFixture();
+
+    // WHEN
+    new eks.Cluster(stack, 'Cluster', {
+      version: CLUSTER_VERSION,
+      clusterLogging: [
+        eks.ClusterLoggingTypes.API,
+        eks.ClusterLoggingTypes.AUTHENTICATOR,
+        eks.ClusterLoggingTypes.SCHEDULER,
+      ],
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('Custom::AWSCDK-EKS-Cluster', {
+      Config: {
+        logging: {
+          clusterLogging: [
+            {
+              enabled: true,
+              types: [
+                'api',
+                'authenticator',
+                'scheduler',
+              ],
+            },
+            {
+              enabled: false,
+              types: [
+                'audit',
+                'controllerManager',
+              ],
+            },
+          ],
+        },
+      },
+    });
+  });
+
+  test('create a cluster with all logging configure', () => {
+    // GIVEN
+    const { stack } = testFixture();
+
+    // WHEN
+    new eks.Cluster(stack, 'Cluster', {
+      version: CLUSTER_VERSION,
+      clusterLogging: [
+        eks.ClusterLoggingTypes.API,
+        eks.ClusterLoggingTypes.AUDIT,
+        eks.ClusterLoggingTypes.AUTHENTICATOR,
+        eks.ClusterLoggingTypes.CONTROLLER_MANAGER,
+        eks.ClusterLoggingTypes.SCHEDULER,
+      ],
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('Custom::AWSCDK-EKS-Cluster', {
+      Config: {
+        logging: {
+          clusterLogging: [
+            {
+              enabled: true,
+              types: [
+                'api',
+                'audit',
+                'authenticator',
+                'controllerManager',
+                'scheduler',
+              ],
+            },
+            {
+              enabled: false,
+              types: [],
+            },
+          ],
+        },
+      },
+    });
+  });
 });

packages/@aws-cdk/aws-eks/test/fargate.test.ts
@@ -459,7 +459,21 @@ describe('fargate', () => {
 
   });
 
-  test('supports cluster logging with FargateCluster', () => {
+  test('supports cluster logging without FargateCluster', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+
+    new eks.FargateCluster(stack, 'FargateCluster', {
+      version: CLUSTER_VERSION,
+    });
+
+    //THEN
+    Template.fromStack(stack).resourceCountIs('Custom::AWSCDK-EKS-Cluster::Config::logging', 0);
+  });
+
+  test('supports cluster partial logging enabled with FargateCluster', () => {
     // GIVEN
     const stack = new Stack();
 
@@ -479,7 +493,63 @@ describe('fargate', () => {
       Config: {
         logging: {
           clusterLogging: [
-            { enabled: true, types: ['api', 'authenticator', 'scheduler'] },
+            {
+              enabled: true,
+              types: [
+                'api',
+                'authenticator',
+                'scheduler',
+              ],
+            },
+            {
+              enabled: false,
+              types: [
+                'audit',
+                'controllerManager',
+              ],
+            },
+          ],
+        },
+      },
+    });
+  });
+
+  test('supports cluster all logging enabled with FargateCluster', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+
+    new eks.FargateCluster(stack, 'FargateCluster', {
+      version: CLUSTER_VERSION,
+      clusterLogging: [
+        eks.ClusterLoggingTypes.API,
+        eks.ClusterLoggingTypes.AUDIT,
+        eks.ClusterLoggingTypes.AUTHENTICATOR,
+        eks.ClusterLoggingTypes.CONTROLLER_MANAGER,
+        eks.ClusterLoggingTypes.SCHEDULER,
+      ],
+    });
+
+    //THEN
+    Template.fromStack(stack).hasResourceProperties('Custom::AWSCDK-EKS-Cluster', {
+      Config: {
+        logging: {
+          clusterLogging: [
+            {
+              enabled: true,
+              types: [
+                'api',
+                'audit',
+                'authenticator',
+                'controllerManager',
+                'scheduler',
+              ],
+            },
+            {
+              enabled: false,
+              types: [],
+            },
           ],
         },
       },

commit 7e1b1ddba51fa31ce3a3db022171478d4ff320b3

fix(eks): missing question marks cause update cluster setting failure
introduced by #21185

Fixes: #21436

packages/@aws-cdk/aws-eks/lib/cluster-resource-handler/cluster.ts
@@ -286,11 +286,11 @@ function parseProps(props: any): aws.EKS.CreateClusterRequest {
     parsed.resourcesVpcConfig.endpointPublicAccess = parsed.resourcesVpcConfig.endpointPublicAccess === 'true';
   }
 
-  if (typeof (parsed.logging?.clusterLogging[0].enabled) === 'string') {
+  if (typeof (parsed.logging?.clusterLogging[0]?.enabled) === 'string') {
     parsed.logging.clusterLogging[0].enabled = parsed.logging.clusterLogging[0].enabled === 'true';
   }
 
-  if (typeof (parsed.logging?.clusterLogging[1].enabled) === 'string') {
+  if (typeof (parsed.logging?.clusterLogging[1]?.enabled) === 'string') {
     parsed.logging.clusterLogging[1].enabled = parsed.logging.clusterLogging[1].enabled === 'false';
   }
 