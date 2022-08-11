c004a719f7d330185411da40297cf4fbbc65391e
Adam Ruka <adamruka85@gmail.com>
2022-08-11 03:31:01 -0700

    feat(s3): introduce a `fromCfnBucket()` method (#20081)
    This PR adds a new static method to the `Bucket` class in the S3 module,
    `fromCfnBucket()`, that allows creating an L2 `IBucket` from a `CfnBucket` instance.
    This is useful when migrating from CloudFormation to CDK.
    
    ----
    
    ### All Submissions:
    
    * [x] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/master/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/master/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/master/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-s3/test/bucket-policy.test.ts
+++ packages/@aws-cdk/aws-s3/test/bucket-policy.test.ts
@@ -2,6 +2,7 @@ import { Template } from '@aws-cdk/assertions';
 import { AnyPrincipal, PolicyStatement } from '@aws-cdk/aws-iam';
 import { RemovalPolicy, Stack, App } from '@aws-cdk/core';
 import * as s3 from '../lib';
+import { CfnBucketPolicy } from '../lib';
 
 // to make it easy to copy & paste from output:
 /* eslint-disable quote-props */
@@ -149,5 +150,49 @@ describe('bucket policy', () => {
     }));
 
     expect(() => app.synth()).toThrow(/A PolicyStatement used in a resource-based policy must specify at least one IAM principal/);
+  });
+
+  describe('fromCfnBucketPolicy()', () => {
+    const stack = new Stack();
+
+    test('correctly extracts the Document and Bucket from the L1', () => {
+      const cfnBucket = new s3.CfnBucket(stack, 'CfnBucket');
+      const cfnBucketPolicy = bucketPolicyForBucketNamed(cfnBucket.ref);
+      const bucketPolicy = s3.BucketPolicy.fromCfnBucketPolicy(cfnBucketPolicy);
+
+      expect(bucketPolicy.document).not.toBeUndefined();
+      expect(bucketPolicy.document.isEmpty).toBeFalsy();
+
+      expect(bucketPolicy.bucket).not.toBeUndefined();
+      expect(bucketPolicy.bucket.policy).not.toBeUndefined();
+      expect(bucketPolicy.bucket.policy?.document.isEmpty).toBeFalsy();
+    });
+
+    test('correctly references a bucket by name', () => {
+      const cfnBucketPolicy = bucketPolicyForBucketNamed('hardcoded-name');
+      const bucketPolicy = s3.BucketPolicy.fromCfnBucketPolicy(cfnBucketPolicy);
+
+      expect(bucketPolicy.bucket).not.toBeUndefined();
+      expect(bucketPolicy.bucket.bucketName).toBe('hardcoded-name');
+    });
+
+    function bucketPolicyForBucketNamed(name: string): CfnBucketPolicy {
+      return new s3.CfnBucketPolicy(stack, `CfnBucketPolicy-${name}`, {
+        policyDocument: {
+          'Statement': [
+            {
+              'Action': 's3:*',
+              'Effect': 'Deny',
+              'Principal': {
+                'AWS': '*',
+              },
+              'Resource': '*',
+            },
+          ],
+          'Version': '2012-10-17',
+        },
+        bucket: name,
+      });
+    }
   });
 });
