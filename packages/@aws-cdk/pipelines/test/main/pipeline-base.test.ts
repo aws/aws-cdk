5590a0b79535728f300c24e37c2512820aca17b6
Dan Vicarel <dvic@derploid.com>
2022-08-14 02:35:47 -0400

    Fix new PipelineBase.isPipeline unit test
    - Assertions now use correct types
    - Test Pipeline object is now less ceremonious and just uses an anonymous type, and passes the  required `synth` prop to PipelineBase constructor
    - Also fixed some styling/casing errors and inconsistencies

--- packages/@aws-cdk/pipelines/test/main/pipeline-base.test.ts
+++ packages/@aws-cdk/pipelines/test/main/pipeline-base.test.ts
@@ -1,29 +1,29 @@
-import { Construct } from "constructs";
 import * as cdk from '@aws-cdk/core';
-import { App } from "../../../core/lib";
-import { PipelineBase } from "../../lib";
+import { Construct } from 'constructs';
+import { App } from '../../../core/lib';
+import { PipelineBase, IFileSetProducer, FileSet } from '../../lib';
+import { PIPELINE_ENV } from '../testhelpers';
 
 describe('pipeline base', () => {
 
   test('PipelineBase.isPipeline indicates that a construct extends PipelineBase', () => {
-
-    class TestPipeline extends PipelineBase {
-
-      constructor(scope: Construct, id: string) {
-        super(scope, id, { synth: {} });
-      }
-
-      protected doBuildPipeline(): void { }
-    }
-
     const app = new App();
-    const stack = new cdk.Stack(app, "test-app");
-    const pipeline = new TestPipeline(app, "test-pipeline");
-    const construct = new Construct(stack, 'Construct');
+    const pipelineStack = new cdk.Stack(app, 'PipelineStack', { env: PIPELINE_ENV });
+    const construct = new Construct(pipelineStack, 'Construct');
+    const pipeline = new class extends PipelineBase {
+      constructor() {
+        super(pipelineStack, 'Pipeline', {
+          synth: new class implements IFileSetProducer {
+            public readonly primaryOutput = new FileSet('PrimaryOutput');
+          }(),
+        });
+      }
+      protected doBuildPipeline(): void { }
+    }();
 
-    expect(PipelineBase.isPipeline(stack)).toBeUndefined();
-    expect(PipelineBase.isPipeline(pipeline)).toBeDefined();
-    expect(PipelineBase.isPipeline(construct)).toBeUndefined();
+    expect(PipelineBase.isPipeline(pipelineStack)).toStrictEqual(false);
+    expect(PipelineBase.isPipeline(construct)).toStrictEqual(false);
+    expect(PipelineBase.isPipeline(pipeline)).toStrictEqual(true);
   });
 
 });
