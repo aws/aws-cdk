80567fa7d0fa2163674d212077fea62290dc3ece
Kyle Laker <kyle@laker.email>
2022-08-16 00:09:41 -0400

    feat(cloudfront): create distributions with HTTP/3 (#21613)
    This adds support for CloudFront distributions that use HTTP/3.
    
    News post: https://aws.amazon.com/about-aws/whats-new/2022/08/amazon-cloudfront-supports-http-3-quic/
    CloudFormation: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-distribution-distributionconfig.html#cfn-cloudfront-distribution-distributionconfig-httpversion
    
    ----
    
    ### All Submissions:
    
    * [X] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [X] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [X] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-cloudfront/test/integ.distribution-http-version.ts
@@ -0,0 +1,30 @@
+import * as cdk from '@aws-cdk/core';
+import { IntegTest } from '@aws-cdk/integ-tests';
+import * as cloudfront from '../lib';
+import { TestOrigin } from './test-origin';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'integ-distribution-http-version');
+
+new cloudfront.Distribution(stack, 'Http11', {
+  defaultBehavior: { origin: new TestOrigin('www.example.com') },
+  httpVersion: cloudfront.HttpVersion.HTTP1_1,
+});
+new cloudfront.Distribution(stack, 'Http2', {
+  defaultBehavior: { origin: new TestOrigin('www.example.com') },
+  httpVersion: cloudfront.HttpVersion.HTTP2,
+});
+new cloudfront.Distribution(stack, 'Http2and3', {
+  defaultBehavior: { origin: new TestOrigin('www.example.com') },
+  httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
+});
+new cloudfront.Distribution(stack, 'Http3', {
+  defaultBehavior: { origin: new TestOrigin('www.example.com') },
+  httpVersion: cloudfront.HttpVersion.HTTP3,
+});
+
+new IntegTest(app, 'DistributionHttpVersion', {
+  testCases: [stack],
+});
+
+app.synth();
