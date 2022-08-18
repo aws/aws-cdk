4535e9f19d0f5899898a7313291b8d4c1718f2ed
Kyle Roach <kroach.work@gmail.com>
2022-08-18 13:25:04 -0400

    test: add seperate integ test

---
+++ packages/@aws-cdk/aws-logs/test/integ.metricfilter-dimensions.ts
@@ -0,0 +1,34 @@
+import { App, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
+import { IntegTest } from '@aws-cdk/integ-tests';
+import { FilterPattern, LogGroup, MetricFilter } from '../lib';
+
+class TestStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const logGroup = new LogGroup(this, 'LogGroup', {
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    /// !show
+    new MetricFilter(this, 'MetricFilter', {
+      logGroup,
+      metricNamespace: 'MyApp',
+      metricName: 'Latency',
+      filterPattern: FilterPattern.exists('$.latency'),
+      metricValue: '$.latency',
+      dimensions: {
+        ErrorCode: '$.errorCode',
+      },
+    });
+    /// !hide
+  }
+}
+
+const app = new App();
+const testCase = new TestStack(app, 'aws-cdk-metricfilter-dimensions-integ');
+
+new IntegTest(app, 'metricfilter-dimensions', {
+  testCases: [testCase],
+});
+app.synth();
