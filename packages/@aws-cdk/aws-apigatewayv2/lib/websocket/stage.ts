07b694eedd735cd69f2010a13c64bbeacf182086
Doug Thomae <douglas.thomae7130@gmail.com>
2022-08-26 14:47:46 -0700

    Added log configuration to http and websocket api stages

--- packages/@aws-cdk/aws-apigatewayv2/lib/websocket/stage.ts
+++ packages/@aws-cdk/aws-apigatewayv2/lib/websocket/stage.ts
@@ -1,8 +1,10 @@
-import { Grant, IGrantable } from '@aws-cdk/aws-iam';
-import { Stack } from '@aws-cdk/core';
+import { CfnAccount } from '@aws-cdk/aws-apigateway';
+import { Grant, IGrantable, Role, ServicePrincipal, ManagedPolicy } from '@aws-cdk/aws-iam';
+import { CfnLogGroup } from '@aws-cdk/aws-logs';
+import { RemovalPolicy, Stack, PhysicalName } from '@aws-cdk/core';
 import { Construct } from 'constructs';
 import { CfnStage } from '../apigatewayv2.generated';
-import { StageOptions, IApi, IStage, StageAttributes } from '../common';
+import { StageOptions, IApi, IStage, StageAttributes, defaultAccessLogFormat } from '../common';
 import { StageBase } from '../common/base';
 import { IWebSocketApi } from './api';
 
@@ -37,6 +39,14 @@ export interface WebSocketStageProps extends StageOptions {
    * The name of the stage.
    */
   readonly stageName: string;
+
+  /**
+   * The logging level for data trace logging.  This is available only for
+   * websocket apis.  Allowed values are OFF, INFO or ERROR.
+   *
+   * @default - OFF. No data trace logs will be generated.
+   */
+  readonly dataTraceLoggingLevel?: string;
 }
 
 /**
@@ -87,14 +97,83 @@ export class WebSocketStage extends StageBase implements IWebSocketStage {
     this.api = props.webSocketApi;
     this.stageName = this.physicalName;
 
+    /**
+     * In CfnStage the rate limits, the data trace logging control and the detailed
+     * metrics are all lumped together in the default route settings, but they're
+     * conceptually orthogonal.  I'm following the pattern that was started by
+     * breaking out the rate limits to break out the conceptually distinct
+     * throttle settings, data trace logging settings and access log settings.
+     */
+    const rateLimits = !props.throttle ? undefined : {
+      throttlingBurstLimit: !props.throttle.burstLimit ? undefined : props.throttle.burstLimit,
+      throttlingRateLimit: !props.throttle.rateLimit ? undefined : props.throttle.rateLimit,
+    };
+
+    /**
+     * The data trace log settings for CfnStage use two parameters, dataTraceEnabled to
+     * turn on the logging and a logging level.  However, specifing a logging level of
+     * OFF is the same as disabling data tracing and setting it to INFO or ERROR implies
+     * that data tracing is on, so the value of the logging flag is being deduced from
+     * the value of the log level.
+     */
+    const dataTraceLoggingSettings =
+      (!props.dataTraceLoggingLevel || props.dataTraceLoggingLevel === 'OFF') ?
+        undefined :
+        {
+          dataTraceEnabled: true,
+          loggingLevel: props.dataTraceLoggingLevel,
+        };
+
+    let destinationArn: string | undefined = undefined;
+    if (props.accessLogEnabled) {
+      if (!props.accessLogGroupArn) {
+        // We need to set up the right permissions to create the log group.
+        const iamRoleForLogGroup = new Role(this, `IAMRoleForAccessLog-${this.stageName}`, {
+          roleName: PhysicalName.GENERATE_IF_NEEDED,
+          assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+        });
+
+        iamRoleForLogGroup.node.addDependency(this.api);
+
+        iamRoleForLogGroup.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonAPIGatewayPushToCloudWatchLogs'));
+
+        // It's required to register the iam role that is used to create the log group with the account
+        // if an api gateway has never been created in this account/region before.  Otherwise, this is
+        // a no-op.
+        const account = new CfnAccount(this, 'account', {
+          cloudWatchRoleArn: iamRoleForLogGroup.roleArn,
+        });
+        account.node.addDependency(this.api);
+
+        // Setting up some reasonable defaults for the retention policy and removal policy.
+        // If the user wants something different they should create their own log group.
+        const accessLogsLogGroup = new CfnLogGroup(this, `AccessLoggingGroup-${this.stageName}`, {
+          retentionInDays: 30,
+        });
+        accessLogsLogGroup.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+        destinationArn = accessLogsLogGroup.attrArn;
+      } else {
+        destinationArn = props.accessLogGroupArn;
+      }
+    }
+
+    const accessLogSettings = !props.accessLogEnabled ? undefined : {
+      destinationArn,
+      format: !props.accessLogFormat ? defaultAccessLogFormat : props.accessLogFormat,
+    };
+
+    const defaultRouteSettings =
+      !dataTraceLoggingSettings && !rateLimits ?
+        undefined :
+        { ...dataTraceLoggingSettings, ...rateLimits };
+
     new CfnStage(this, 'Resource', {
       apiId: props.webSocketApi.apiId,
       stageName: this.physicalName,
       autoDeploy: props.autoDeploy,
-      defaultRouteSettings: !props.throttle ? undefined : {
-        throttlingBurstLimit: props.throttle?.burstLimit,
-        throttlingRateLimit: props.throttle?.rateLimit,
-      },
+      defaultRouteSettings,
+      accessLogSettings,
     });
 
     if (props.domainMapping) {
