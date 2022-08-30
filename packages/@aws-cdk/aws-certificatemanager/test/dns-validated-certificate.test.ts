5970af4fddf77e75048cb8be04530b37dee138e4
josephedward <josephedwardwork@gmail.com>
2022-08-30 10:38:52 -0400

    less than or equal to 64 characters

--- packages/@aws-cdk/aws-certificatemanager/test/dns-validated-certificate.test.ts
+++ packages/@aws-cdk/aws-certificatemanager/test/dns-validated-certificate.test.ts
@@ -237,10 +237,8 @@ test('throws when domain name is longer than 64 characters', () => {
       domainName: 'example.com'.repeat(7),
       hostedZone: exampleDotComZone,
     });
-
   }).toThrow(/Domain name must be 64 characters or less/);
-});
-
+}),
 
 test('test transparency logging settings is passed to the custom resource', () => {
   const stack = new Stack();
