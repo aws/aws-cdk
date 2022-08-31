f593a8e395480b22a47cc63613b1f12256a3cd1a
Cory Hall <43035978+corymhall@users.noreply.github.com>
2022-08-31 17:26:56 -0400

    chore(integ-runner): use newStyleSynthesis by default (#21856)
    This PR updates the `integ-runner` to _not_ set `@aws-cdk/core:newStyleStackSynthesis: false` by default. 
    Previously this was done to cut down on the merge conflicts when the forward-merge was done 
    between v1 and v2, but since we are now only committing to v2 it makes sense to test with 
    `newStyleStackSynthesis` enabled.
    
    This change required re-running _all_ integration tests since using this flag changes the template. I've separated out some of the important changes into their own commits to make it easier to review.
    
    - `chore(integ-runner): use newStyleSynthesis by default` - this contains the actual change to the `integ-runner`
    - `integ test shapshot updates` - this contains the snapshot updates for most of the tests
    - The commits that mention a specific module contain fixes for broken integration tests
    
    ----
    
    ### All Submissions:
    
    * [ ] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-route53/test/delete-existing-record-set.integ.snapshot/asset.421f670fab8460498f88755d8ad58385e2dec77a5f71a197a0f6452f8ba3c98e/index.ts
@@ -0,0 +1,68 @@
+import { Route53 } from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
+
+interface ResourceProperties {
+  HostedZoneId: string;
+  RecordName: string;
+  RecordType: string;
+}
+
+export async function handler(event: AWSLambda.CloudFormationCustomResourceEvent) {
+  const resourceProps = event.ResourceProperties as unknown as ResourceProperties;
+
+  // Only delete the existing record when the new one gets created
+  if (event.RequestType !== 'Create') {
+    return;
+  }
+
+  const route53 = new Route53({ apiVersion: '2013-04-01' });
+
+  const listResourceRecordSets = await route53.listResourceRecordSets({
+    HostedZoneId: resourceProps.HostedZoneId,
+    StartRecordName: resourceProps.RecordName,
+    StartRecordType: resourceProps.RecordType,
+  }).promise();
+
+  const existingRecord = listResourceRecordSets.ResourceRecordSets
+    .find(r => r.Name === resourceProps.RecordName && r.Type === resourceProps.RecordType);
+
+  if (!existingRecord) {
+    // There is no existing record, we can safely return
+    return;
+  }
+
+  const changeResourceRecordSets = await route53.changeResourceRecordSets({
+    HostedZoneId: resourceProps.HostedZoneId,
+    ChangeBatch: {
+      Changes: [{
+        Action: 'DELETE',
+        ResourceRecordSet: removeUndefinedAndEmpty({
+          Name: existingRecord.Name,
+          Type: existingRecord.Type,
+          TTL: existingRecord.TTL,
+          AliasTarget: existingRecord.AliasTarget,
+          ResourceRecords: existingRecord.ResourceRecords,
+        }),
+      }],
+    },
+  }).promise();
+
+  await route53.waitFor('resourceRecordSetsChanged', { Id: changeResourceRecordSets.ChangeInfo.Id }).promise();
+
+  return {
+    PhysicalResourceId: `${existingRecord.Name}-${existingRecord.Type}`,
+  };
+}
+
+// https://github.com/aws/aws-sdk-js/issues/3411
+// https://github.com/aws/aws-sdk-js/issues/3506
+function removeUndefinedAndEmpty<T>(obj: T): T {
+  const ret: { [key: string]: any } = {};
+
+  for (const [k, v] of Object.entries(obj)) {
+    if (v && (!Array.isArray(v) || v.length !== 0)) {
+      ret[k] = v;
+    }
+  }
+
+  return ret as T;
+}
