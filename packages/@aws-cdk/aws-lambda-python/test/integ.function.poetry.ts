bb9f2ad16cc82a2b6669793bdf6db436d305f2a1
corymhall <43035978+corymhall@users.noreply.github.com>
2022-09-07 14:11:01 +0000

    fix(lambda-python): bundling with poetry is broken
    It looks like something was changed in the base image and there is no
    longer write access to the `/tmp` directory which causes bundling with
    poetry to fail (see linked issue). This PR updates the Dockerfile to
    create a new cache location for both `pip` and `poetry` and switches to
    using a virtualenv for python so that it is no longer using root.
    
    To test this I executed the `integ.function.poetry` integration test
    both before (to reproduce the error) and after the fix. I'm actually not
    sure why our integration tests didn't start failing in the pipeline. The
    only thing I can think of is that we are caching the docker images and
    it just hasn't pulled down a newer one that has this issue.
    
    fixes #21867

--- packages/@aws-cdk/aws-lambda-python/test/integ.function.poetry.ts
+++ packages/@aws-cdk/aws-lambda-python/test/integ.function.poetry.ts
@@ -1,9 +1,7 @@
-// disabling update workflow because we don't want to include the assets in the snapshot
-// python bundling changes the asset hash pretty frequently
-/// !cdk-integ pragma:disable-update-workflow
 import * as path from 'path';
 import { Runtime } from '@aws-cdk/aws-lambda';
 import { App, CfnOutput, Stack, StackProps } from '@aws-cdk/core';
+import { IntegTest } from '@aws-cdk/integ-tests';
 import { Construct } from 'constructs';
 import * as lambda from '../lib';
 
@@ -20,7 +18,7 @@ class TestStack extends Stack {
       entry: path.join(__dirname, 'lambda-handler-poetry'),
       runtime: Runtime.PYTHON_3_9,
     });
-    new CfnOutput(this, 'InlineFunctionName', {
+    new CfnOutput(this, 'inlineFunctionName', {
       value: pythonFunctionInline.functionName,
     });
 
@@ -35,5 +33,13 @@ class TestStack extends Stack {
 }
 
 const app = new App();
-new TestStack(app, 'cdk-integ-lambda-python');
+const testCase = new TestStack(app, 'cdk-integ-lambda-python');
+
+new IntegTest(app, 'poetry', {
+  testCases: [testCase],
+  // disabling update workflow because we don't want to include the assets in the snapshot
+  // python bundling changes the asset hash pretty frequently
+  stackUpdateWorkflow: false,
+});
+
 app.synth();
