fe134aa62c6db1c9894b404aef7c7d04d045d8af
Elena Naum <naume@amazon.com>
2022-09-07 14:53:59 +0200

    Comments updated

--- packages/@aws-cdk/aws-secretsmanager/lib/secret.ts
+++ packages/@aws-cdk/aws-secretsmanager/lib/secret.ts
@@ -390,7 +390,7 @@ abstract class SecretBase extends Resource implements ISecret {
       resource: this,
     });
 
-    // @see https://docs.aws.amazon.com/kms/latest/developerguide/services-secrets-manager.html#asm-authz
+    // @see https://docs.aws.amazon.com/kms/latest/developerguide/services-secrets-manager.html
     const principal = new kms.ViaServicePrincipal(`secretsmanager.${Stack.of(this).region}.amazonaws.com`, grantee.grantPrincipal);
     this.encryptionKey?.grantEncryptDecrypt(principal);
     this.encryptionKey?.grant(principal, 'kms:CreateGrant', 'kms:DescribeKey');
@@ -646,7 +646,7 @@ export class Secret extends SecretBase {
       ? parseSecretNameForOwnedSecret(this, this.secretArn, props.secretName)
       : parseSecretName(this, this.secretArn);
 
-    // @see https://docs.aws.amazon.com/kms/latest/developerguide/services-secrets-manager.html
+    // @see https://docs.aws.amazon.com/kms/latest/developerguide/services-secrets-manager.html#asm-authz
     const principal =
       new kms.ViaServicePrincipal(`secretsmanager.${Stack.of(this).region}.amazonaws.com`, new iam.AccountPrincipal(Stack.of(this).account));
     this.encryptionKey?.grantEncryptDecrypt(principal);
