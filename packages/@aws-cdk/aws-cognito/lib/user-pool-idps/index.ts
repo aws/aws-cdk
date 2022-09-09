59e8b74453ebd875120c4d244e7326fe1f7e3a24
Kevin Fleming <kvnflm@gmail.com>
2022-09-09 10:06:18 +0900

    feat(cognito): add SAML user pool identity provider (#21879)
    Adds a construct for a SAML user pool identity provider.
    
    I based much of this off of https://github.com/aws/aws-cdk/pull/20241, as the OIDC and SAML identity pool providers share e.g. the length limitations on provider names.
    
    For the integration test, you have to specify a valid SAML metadata URL or XML document, or the stack won't be created. I used a sample URL from the [samling](https://fujifish.github.io/samling/samling.html) project, but this could be changed if anyone has a better suggestion.
    
    ----
    
    ### All Submissions:
    
    * [X] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [X] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [X] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-cognito/lib/user-pool-idps/index.ts
+++ packages/@aws-cdk/aws-cognito/lib/user-pool-idps/index.ts
@@ -4,3 +4,4 @@ export * from './amazon';
 export * from './facebook';
 export * from './google';
 export * from './oidc';
+export * from './saml';
---
+++ packages/@aws-cdk/aws-cognito/test/integ.user-pool-idp.saml.ts
@@ -0,0 +1,40 @@
+import { App, CfnOutput, RemovalPolicy, Stack } from '@aws-cdk/core';
+import { IntegTest } from '@aws-cdk/integ-tests';
+import { Construct } from 'constructs';
+import { UserPool, UserPoolIdentityProviderSaml, UserPoolIdentityProviderSamlMetadata } from '../lib';
+
+class TestStack extends Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    const userpool = new UserPool(this, 'pool', {
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    new UserPoolIdentityProviderSaml(this, 'cdk', {
+      userPool: userpool,
+      name: 'cdk',
+      metadata: UserPoolIdentityProviderSamlMetadata.url('https://fujifish.github.io/samling/public/metadata.xml'),
+    });
+
+    const client = userpool.addClient('client');
+
+    const domain = userpool.addDomain('domain', {
+      cognitoDomain: {
+        domainPrefix: 'cdk-test-pool',
+      },
+    });
+
+    new CfnOutput(this, 'SignInLink', {
+      value: domain.signInUrl(client, {
+        redirectUri: 'https://example.com',
+      }),
+    });
+  }
+}
+
+const app = new App();
+const testCase = new TestStack(app, 'integ-user-pool-identity-provider-saml-stack');
+
+new IntegTest(app, 'integ-user-pool-identity-provider-saml-test', {
+  testCases: [testCase],
+});
