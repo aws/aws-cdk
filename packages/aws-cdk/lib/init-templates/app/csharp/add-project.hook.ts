8a7d16b8658dcae29bbe9ee6f13be6f33624bb13
Kendra Neil <53584728+TheRealAmazonKendra@users.noreply.github.com>
2022-09-08 20:58:34 -0700

    add more context to error message

--- packages/aws-cdk/lib/init-templates/app/csharp/add-project.hook.ts
+++ packages/aws-cdk/lib/init-templates/app/csharp/add-project.hook.ts
@@ -5,5 +5,10 @@ import { shell } from '../../../os';
 export const invoke: InvokeHook = async (targetDirectory: string) => {
   const slnPath = path.join(targetDirectory, 'src', '%name.PascalCased%.sln');
   const csprojPath = path.join(targetDirectory, 'src', '%name.PascalCased%', '%name.PascalCased%.csproj');
-  await shell(['dotnet', 'sln', slnPath, 'add', csprojPath]);
+  try {
+    await shell(['dotnet', 'sln', slnPath, 'add', csprojPath]);
+  } catch (e) {
+    throw new Error(`Could not add project %name.PascalCased%.csproj to solution %name.PascalCased%.sln. ${e.message}`);
+  }
+
 };
--- packages/aws-cdk/lib/init-templates/app/fsharp/add-project.hook.ts
+++ packages/aws-cdk/lib/init-templates/app/fsharp/add-project.hook.ts
@@ -5,5 +5,9 @@ import { shell } from '../../../os';
 export const invoke: InvokeHook = async (targetDirectory: string) => {
   const slnPath = path.join(targetDirectory, 'src', '%name.PascalCased%.sln');
   const fsprojPath = path.join(targetDirectory, 'src', '%name.PascalCased%', '%name.PascalCased%.fsproj');
-  await shell(['dotnet', 'sln', slnPath, 'add', fsprojPath]);
+  try {
+    await shell(['dotnet', 'sln', slnPath, 'add', fsprojPath]);
+  } catch (e) {
+    throw new Error(`Could not add project %name.PascalCased%.fsproj to solution %name.PascalCased%.sln. ${e.message}`);
+  }
 };
