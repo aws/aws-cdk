191e6b07ca9bf885fcd117397f2a5e923b73353c
Cory Hall <43035978+corymhall@users.noreply.github.com>
2022-09-16 23:57:52 -0400

    fix(integ-tests): can't enable lookups when creating an IntegTest (#22075)
    We were not exposing the `enableLookups` property when creating an `IntegTest`. This also updates `integ-runner` to ensure that we are correctly utilizing `enableLooups`. There was an undiscovered (because you couldn't set `enableLookups=true` :) ) bug which set the dummy context on _every_ command (i.e. deploy, destroy) when it should have only been set when synthing the snapshot.
    
    
    ----
    
    ### All Submissions:
    
    * [ ] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/integ-runner/test/runner/integ-test-runner.test.ts
+++ packages/@aws-cdk/integ-runner/test/runner/integ-test-runner.test.ts
@@ -71,7 +71,11 @@ describe('IntegTest runIntegTests', () => {
       requireApproval: 'never',
       pathMetadata: false,
       assetMetadata: false,
-      context: expect.any(Object),
+      context: expect.not.objectContaining({
+        'vpc-provider:account=12345678:filter.isDefault=true:region=test-region:returnAsymmetricSubnets=true': expect.objectContaining({
+          vpcId: 'vpc-60900905',
+        }),
+      }),
       profile: undefined,
       versionReporting: false,
       lookups: false,
@@ -84,7 +88,11 @@ describe('IntegTest runIntegTests', () => {
       assetMetadata: false,
       output: 'cdk-integ.out.test-with-snapshot',
       profile: undefined,
-      context: expect.any(Object),
+      context: expect.not.objectContaining({
+        'vpc-provider:account=12345678:filter.isDefault=true:region=test-region:returnAsymmetricSubnets=true': expect.objectContaining({
+          vpcId: 'vpc-60900905',
+        }),
+      }),
       versionReporting: false,
       lookups: false,
       rollback: false,
@@ -94,7 +102,11 @@ describe('IntegTest runIntegTests', () => {
       app: 'node xxxxx.test-with-snapshot.js',
       pathMetadata: false,
       assetMetadata: false,
-      context: expect.any(Object),
+      context: expect.not.objectContaining({
+        'vpc-provider:account=12345678:filter.isDefault=true:region=test-region:returnAsymmetricSubnets=true': expect.objectContaining({
+          vpcId: 'vpc-60900905',
+        }),
+      }),
       versionReporting: false,
       profile: undefined,
       force: true,
@@ -169,7 +181,7 @@ describe('IntegTest runIntegTests', () => {
       requireApproval: 'never',
       pathMetadata: false,
       assetMetadata: false,
-      context: expect.objectContaining({
+      context: expect.not.objectContaining({
         'vpc-provider:account=12345678:filter.isDefault=true:region=test-region:returnAsymmetricSubnets=true': expect.objectContaining({
           vpcId: 'vpc-60900905',
         }),
@@ -186,7 +198,7 @@ describe('IntegTest runIntegTests', () => {
       env: expect.objectContaining({
         CDK_INTEG_ACCOUNT: '12345678',
         CDK_INTEG_REGION: 'test-region',
-        CDK_CONTEXT_JSON: expect.anything(),
+        CDK_CONTEXT_JSON: expect.stringContaining('"vpcId":"vpc-60900905"'),
       }),
       output: 'test-with-snapshot-assets-diff.integ.snapshot',
     });
@@ -194,7 +206,7 @@ describe('IntegTest runIntegTests', () => {
       app: 'node xxxxx.test-with-snapshot-assets-diff.js',
       pathMetadata: false,
       assetMetadata: false,
-      context: expect.objectContaining({
+      context: expect.not.objectContaining({
         'vpc-provider:account=12345678:filter.isDefault=true:region=test-region:returnAsymmetricSubnets=true': expect.objectContaining({
           vpcId: 'vpc-60900905',
         }),
@@ -292,7 +304,11 @@ describe('IntegTest runIntegTests', () => {
       pathMetadata: false,
       assetMetadata: false,
       versionReporting: false,
-      context: expect.any(Object),
+      context: expect.not.objectContaining({
+        'vpc-provider:account=12345678:filter.isDefault=true:region=test-region:returnAsymmetricSubnets=true': expect.objectContaining({
+          vpcId: 'vpc-60900905',
+        }),
+      }),
       profile: 'test-profile',
       rollback: false,
       lookups: false,
@@ -304,7 +320,11 @@ describe('IntegTest runIntegTests', () => {
       pathMetadata: false,
       assetMetadata: false,
       versionReporting: false,
-      context: expect.any(Object),
+      context: expect.not.objectContaining({
+        'vpc-provider:account=12345678:filter.isDefault=true:region=test-region:returnAsymmetricSubnets=true': expect.objectContaining({
+          vpcId: 'vpc-60900905',
+        }),
+      }),
       profile: 'test-profile',
       force: true,
       all: true,
