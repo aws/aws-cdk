960b19e995ceaf79bcef3e807397c146d9111760
Kendra Neil <53584728+TheRealAmazonKendra@users.noreply.github.com>
2022-09-16 17:34:07 -0700

    output test

--- tools/@aws-cdk/prlint/index.ts
+++ tools/@aws-cdk/prlint/index.ts
@@ -6,6 +6,14 @@ async function run() {
   const token: string = process.env.GITHUB_TOKEN!;
   const client = github.getOctokit(token).rest.pulls;
 
+  const reviews = await (client.listReviews({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.issue.number,
+  }));
+
+  console.log(reviews);
+
   const prLinter = new linter.PRLinter({
     client,
     owner: github.context.repo.owner,
