bf9a1ef1383f1feed7754da195770c9c0fc123ad
Kendra Neil <53584728+TheRealAmazonKendra@users.noreply.github.com>
2022-09-20 08:58:58 -0700

    fix(api-gateway): SpecRestApi ignores disableExecuteApiEndpoint property (#22133)
    In this [PR](https://github.com/aws/aws-cdk/pull/14526) the disableExecuteApiEndpoint property was added to RestApiBaseProps, which is used by both RestApi and SpecRestApi. The property is propagated to the resulting CfnRestApi when specified for a RestApi, but is ignored when specified for a SpecRestApi.
    
    Closes #21295.
    
    
    ----
    
    ### All Submissions:
    
    * [ ] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-apigateway/test/integ.spec-restapi.ts
@@ -0,0 +1,92 @@
+import * as path from 'path';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as cdk from '@aws-cdk/core';
+import { IntegTest } from '@aws-cdk/integ-tests';
+import * as apigateway from '../lib';
+
+class Test extends cdk.Stack {
+  constructor(scope: cdk.App, id: string) {
+    super(scope, id);
+
+    const api = new apigateway.SpecRestApi(this, 'my-api', {
+      apiDefinition: apigateway.ApiDefinition.fromAsset(path.join(__dirname, 'sample-definition.yaml')),
+      disableExecuteApiEndpoint: true,
+      retainDeployments: true,
+      cloudWatchRole: true,
+      deployOptions: {
+        cacheClusterEnabled: true,
+        stageName: 'beta',
+        description: 'beta stage',
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        dataTraceEnabled: true,
+        methodOptions: {
+          '/api/appliances/GET': {
+            cachingEnabled: true,
+          },
+        },
+      },
+    });
+
+    const handler = new lambda.Function(this, 'MyHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromInline(`exports.handler = ${handlerCode}`),
+      handler: 'index.handler',
+    });
+
+    const v1 = api.root.addResource('v1');
+
+    const integration = new apigateway.LambdaIntegration(handler);
+
+    const toys = v1.addResource('toys');
+    const getToysMethod: apigateway.Method = toys.addMethod('GET', integration, { apiKeyRequired: true });
+    toys.addMethod('POST');
+    toys.addMethod('PUT');
+
+    const appliances = v1.addResource('appliances');
+    appliances.addMethod('GET');
+
+    const books = v1.addResource('books');
+    books.addMethod('GET', integration);
+    books.addMethod('POST', integration);
+
+    function handlerCode(event: any, _: any, callback: any) {
+      return callback(undefined, {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(event),
+      });
+    }
+
+    const key = api.addApiKey('ApiKey');
+    const plan = api.addUsagePlan('UsagePlan', {
+      name: 'Basic',
+      apiKey: key,
+      description: 'Free tier monthly usage plan',
+      throttle: { rateLimit: 5 },
+      quota: {
+        limit: 10000,
+        period: apigateway.Period.MONTH,
+      },
+    });
+    plan.addApiStage({
+      stage: api.deploymentStage,
+      throttle: [
+        {
+          method: getToysMethod,
+          throttle: {
+            rateLimit: 10,
+            burstLimit: 2,
+          },
+        },
+      ],
+    });
+  }
+}
+
+const app = new cdk.App();
+
+const testCase = new Test(app, 'test-apigateway-spec-restapi');
+new IntegTest(app, 'apigateway-spec-restapi', {
+  testCases: [testCase],
+});
