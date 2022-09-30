ed548516cf9a81b70a751c0131fceda5aca1bfbe
Rico Huijbers <rix0rrr@gmail.com>
2022-09-30 16:37:45 +0200

    Don't need this argument

--- packages/@aws-cdk/aws-servicecatalog/lib/private/product-stack-synthesizer.ts
+++ packages/@aws-cdk/aws-servicecatalog/lib/private/product-stack-synthesizer.ts
@@ -29,6 +29,6 @@ export class ProductStackSynthesizer extends cdk.StackSynthesizer {
     }
     // Synthesize the template, but don't emit as a cloud assembly artifact.
     // It will be registered as an S3 asset of its parent instead.
-    this.synthesizeTemplate(this.stack, session);
+    this.synthesizeTemplate(session);
   }
 }
