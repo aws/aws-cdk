0a097a4f95a63a5f2402da134ae232be0d73f22c
Rico Huijbers <rix0rrr@gmail.com>
2022-09-30 17:23:00 +0200

    Remove override which hides super.bind()

--- packages/@aws-cdk/aws-servicecatalog/lib/private/product-stack-synthesizer.ts
+++ packages/@aws-cdk/aws-servicecatalog/lib/private/product-stack-synthesizer.ts
@@ -6,15 +6,6 @@ import * as cdk from '@aws-cdk/core';
  * Interoperates with the StackSynthesizer of the parent stack.
  */
 export class ProductStackSynthesizer extends cdk.StackSynthesizer {
-  private stack?: cdk.Stack;
-
-  public bind(stack: cdk.Stack): void {
-    if (this.stack !== undefined) {
-      throw new Error('A Stack Synthesizer can only be bound once, create a new instance to use with a different Stack');
-    }
-    this.stack = stack;
-  }
-
   public addFileAsset(_asset: cdk.FileAssetSource): cdk.FileAssetLocation {
     throw new Error('Service Catalog Product Stacks cannot use Assets');
   }
@@ -24,9 +15,6 @@ export class ProductStackSynthesizer extends cdk.StackSynthesizer {
   }
 
   public synthesize(session: cdk.ISynthesisSession): void {
-    if (!this.stack) {
-      throw new Error('You must call bindStack() first');
-    }
     // Synthesize the template, but don't emit as a cloud assembly artifact.
     // It will be registered as an S3 asset of its parent instead.
     this.synthesizeTemplate(session);
