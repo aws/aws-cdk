6e16ffe8e49f6a5d4ba076ba3d66f564daded96b
Mitchell Valine <mitchellvaline@yahoo.com>
2022-10-01 22:00:44 -0700

    fix(step-functions): arn is not valid across partitions (#22314)
    There were a number of arns with hardcoded formatting in the stepfunctions-tasks module that would cause policies to be invalid across partitions. Updated these to use our standard `Stack.formatArn` functionality.
    
    ----
    
    ### All Submissions:
    
    * [ ] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-stepfunctions-tasks/lib/athena/start-query-execution.ts
+++ packages/@aws-cdk/aws-stepfunctions-tasks/lib/athena/start-query-execution.ts
@@ -106,7 +106,15 @@ export class AthenaStartQueryExecution extends sfn.TaskStateBase {
           's3:ListBucketMultipartUploads',
           's3:ListMultipartUploadParts',
           's3:PutObject'],
-        resources: [this.props.resultConfiguration?.outputLocation?.bucketName ? `arn:aws:s3:::${this.props.resultConfiguration?.outputLocation?.bucketName}/${this.props.resultConfiguration?.outputLocation?.objectKey}/*` : '*'], // Need S3 location where data is stored or Athena throws an Unable to verify/create output bucket https://docs.aws.amazon.com/athena/latest/ug/security-iam-athena.html
+        resources: [
+          this.props.resultConfiguration?.outputLocation?.bucketName
+            ? cdk.Stack.of(this).formatArn({
+              service: 's3',
+              resource: this.props.resultConfiguration?.outputLocation?.bucketName,
+              resourceName: this.props.resultConfiguration?.outputLocation?.objectKey,
+            })
+            : '*',
+        ],
       }),
     );
 
