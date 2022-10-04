0c18d14a62bc92f548909b36f3c1b709433973ba
Chris Garvis <cgarvis@gmail.com>
2022-10-04 14:34:38 -0400

    Update packages/@aws-cdk/aws-rds/lib/cluster-engine.ts
    Co-authored-by: Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>

--- packages/@aws-cdk/aws-rds/lib/cluster-engine.ts
+++ packages/@aws-cdk/aws-rds/lib/cluster-engine.ts
@@ -417,7 +417,7 @@ export class AuroraMysqlEngineVersion {
   /** Version "8.0.mysql_aurora.3.02.0". */
   public static readonly VER_3_02_0 = AuroraMysqlEngineVersion.builtIn_8_0('3.02.0');
   /** Version "8.0.mysql_aurora.3.02.1". */
-  public static readonly VER_3_02_0 = AuroraMysqlEngineVersion.builtIn_8_0('3.02.1');
+  public static readonly VER_3_02_1 = AuroraMysqlEngineVersion.builtIn_8_0('3.02.1');
 
   /**
    * Create a new AuroraMysqlEngineVersion with an arbitrary version.
