15f34379bc18623e38fc7ebdcc27ecdc4eddaf77
Michael Ohde <ohd@amazon.com>
2022-10-05 16:42:04 -0700

    include snapshot of integ test properly

--- packages/@aws-cdk/aws-appsync/test/integ.appsync-none.ts
+++ packages/@aws-cdk/aws-appsync/test/integ.appsync-none.ts
@@ -1,6 +1,6 @@
-/// !cdk-integ *
-
+import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
+import { IntegTest } from '@aws-cdk/integ-tests';
 import * as appsync from '../lib';
 
 const app = new cdk.App();
@@ -8,10 +8,15 @@ const stack = new cdk.Stack(app, 'stack');
 
 const api = new appsync.GraphqlApi(stack, 'NoneAPI', {
   name: 'NoneAPI',
+  schema: appsync.Schema.fromAsset(path.join(__dirname, 'appsync.none.graphql')),
 });
 
 api.addNoneDataSource('NoneDS', {
   name: cdk.Lazy.string({ produce(): string { return 'NoneDS'; } }),
+});
+
+new IntegTest(app, 'api', {
+  testCases: [stack],
 });
 
 app.synth();
