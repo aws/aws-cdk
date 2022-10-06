f7245309885476e85362d1668f198beb132bcad3
Rico Huijbers <rix0rrr@gmail.com>
2022-10-06 13:28:03 +0200

    New files have appeared

---
+++ packages/@aws-cdk/aws-eks/test/eks-cluster.integ.snapshot/asset.b9db8e64e56b84987288e77a56bf3c0fb982931aa35cb2dcff4bc8a115ae87b3/consts.ts
@@ -0,0 +1,14 @@
+/**
+ * Supported resource type.
+ */
+export const enum CfnUtilsResourceType {
+  /**
+   * CfnJson
+   */
+  CFN_JSON = 'Custom::AWSCDKCfnJson',
+
+  /**
+   * CfnJsonStringify
+   */
+  CFN_JSON_STRINGIFY = 'Custom::AWSCDKCfnJsonStringify',
+}
