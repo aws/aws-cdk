eb1fe6213855c3f26e2c762fd9a1e2009bb2e06f
corymhall <43035978+corymhall@users.noreply.github.com>
2022-10-06 18:46:47 +0000

    fix(apigateway): CORS OPTIONS method should not require auth
    When you create a RestApi and you provide `defaultCorsPreflightOptions`
    we automatically create a CORS OPTIONS method for each method. If you
    also provide `defaultMethodOptions` then those default options get
    passed through to the CORS OPTION method as well. In the case of
    authentication options this should not be the case.
    
    This PR explicitly sets the authentication related options to NONE
    values which overrides whatever is provided in `defaultMethodOptions`.
    
    I've updated an integration tests to assert that an OPTIONS call is
    successful (I also tested before the fix to assert that it failed).
    
    fixes #8615

--- packages/@aws-cdk/aws-apigateway/test/authorizers/integ.token-authorizer.lit.ts
+++ packages/@aws-cdk/aws-apigateway/test/authorizers/integ.token-authorizer.lit.ts
@@ -1,15 +1,10 @@
 import * as path from 'path';
 import * as lambda from '@aws-cdk/aws-lambda';
-import { App, Stack } from '@aws-cdk/core';
-import { MockIntegration, PassthroughBehavior, RestApi, TokenAuthorizer } from '../../lib';
+import { App, Stack, Duration } from '@aws-cdk/core';
+import { IntegTest, ExpectedResult, Match } from '@aws-cdk/integ-tests';
+import { MockIntegration, PassthroughBehavior, RestApi, TokenAuthorizer, Cors } from '../../lib';
 
-/*
- * Stack verification steps:
- * * `curl -s -o /dev/null -w "%{http_code}" <url>` should return 401
- * * `curl -s -o /dev/null -w "%{http_code}" -H 'Authorization: deny' <url>` should return 403
- * * `curl -s -o /dev/null -w "%{http_code}" -H 'Authorization: allow' <url>` should return 200
- */
-
+/// !show
 const app = new App();
 const stack = new Stack(app, 'TokenAuthorizerInteg');
 
@@ -19,11 +14,20 @@ const authorizerFn = new lambda.Function(stack, 'MyAuthorizerFunction', {
   code: lambda.AssetCode.fromAsset(path.join(__dirname, 'integ.token-authorizer.handler')),
 });
 
-const restapi = new RestApi(stack, 'MyRestApi', { cloudWatchRole: true });
-
 const authorizer = new TokenAuthorizer(stack, 'MyAuthorizer', {
   handler: authorizerFn,
 });
+
+const restapi = new RestApi(stack, 'MyRestApi', {
+  cloudWatchRole: true,
+  defaultMethodOptions: {
+    authorizer,
+  },
+  defaultCorsPreflightOptions: {
+    allowOrigins: Cors.ALL_ORIGINS,
+  },
+});
+
 
 restapi.root.addMethod('ANY', new MockIntegration({
   integrationResponses: [
@@ -37,5 +41,89 @@ restapi.root.addMethod('ANY', new MockIntegration({
   methodResponses: [
     { statusCode: '200' },
   ],
-  authorizer,
 });
+/// !hide
+
+const integ = new IntegTest(app, 'apigw-token-auth', {
+  testCases: [stack],
+});
+const hostName = `${restapi.restApiId}.execute-api.${stack.region}.${stack.urlSuffix}`;
+const testFunc = new lambda.Function(stack, 'InvokeFunction', {
+  memorySize: 250,
+  timeout: Duration.seconds(10),
+  code: lambda.Code.fromInline(`
+const https = require('https');
+const options = {
+  hostname: '${hostName}',
+  path: '/${restapi.deploymentStage.stageName}',
+};
+exports.handler = async function(event) {
+  console.log(event);
+  options.method = event.method;
+  if ('authorization' in event) {
+    options.headers = {
+      Authorization: event.authorization,
+    };
+  }
+  let dataString = '';
+  const response = await new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      res.on('data', data => {
+        dataString += data;
+      })
+      res.on('end', () => {
+        resolve({
+          statusCode: res.statusCode,
+          body: dataString,
+        });
+      })
+    });
+    req.on('error', err => {
+      reject({
+        statusCode: 500,
+        body: JSON.stringify({
+          cause: 'Something went wrong',
+          error: err,
+        })
+      });
+    });
+    req.end();
+  });
+  return response;
+}
+`),
+  handler: 'index.handler',
+  runtime: lambda.Runtime.NODEJS_16_X,
+});
+
+const invokeGet = integ.assertions.invokeFunction({
+  functionName: testFunc.functionName,
+  payload: JSON.stringify({
+    method: 'GET',
+    authorization: 'allow',
+  }),
+});
+invokeGet.expect(ExpectedResult.objectLike({
+  Payload: Match.stringLikeRegexp('200'),
+}));
+
+const invokeGetDeny = integ.assertions.invokeFunction({
+  functionName: testFunc.functionName,
+  payload: JSON.stringify({
+    method: 'GET',
+    authorization: 'deny',
+  }),
+});
+invokeGetDeny.expect(ExpectedResult.objectLike({
+  Payload: Match.stringLikeRegexp('User is not authorized to access this resource with an explicit deny'),
+}));
+
+const invokeOptions = integ.assertions.invokeFunction({
+  functionName: testFunc.functionName,
+  payload: JSON.stringify({
+    method: 'OPTIONS',
+  }),
+});
+invokeOptions.expect(ExpectedResult.objectLike({
+  Payload: Match.stringLikeRegexp('204'),
+}));
