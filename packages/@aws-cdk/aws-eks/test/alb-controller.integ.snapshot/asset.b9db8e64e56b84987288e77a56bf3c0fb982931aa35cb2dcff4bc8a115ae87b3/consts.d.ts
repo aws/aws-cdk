0abdd1e3ee8995055093b5fc2aac0d71bb0e5f1e
Rico Huijbers <rix0rrr@gmail.com>
2022-10-07 12:48:29 +0200

    fix(custom-resources): provider can fail if S3 has brown-outs (#22362)
    The Custom Resource Provider will write a response object back to S3, but if S3 is having connection trouble, the calls will not be retried and fail.
    
    Add a simple retry loop around it.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-eks/test/alb-controller.integ.snapshot/asset.b9db8e64e56b84987288e77a56bf3c0fb982931aa35cb2dcff4bc8a115ae87b3/consts.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Supported resource type.
+ */
+export declare const enum CfnUtilsResourceType {
+    /**
+     * CfnJson
+     */
+    CFN_JSON = "Custom::AWSCDKCfnJson",
+    /**
+     * CfnJsonStringify
+     */
+    CFN_JSON_STRINGIFY = "Custom::AWSCDKCfnJsonStringify"
+}
