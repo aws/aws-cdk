0f0a89eba3ac0949188b972574fa9884e9a7ecd1
Madeline Kusters <mkusters@amazon.com>
2022-10-10 22:35:31 -0700

    include requirements.txt in the directory that is used to generate the assetHash, so that the LayerVersion will get updated when the version of awscli being used is updated

--- packages/@aws-cdk/lambda-layer-awscli/layer/download-fallback.sh
+++ packages/@aws-cdk/lambda-layer-awscli/layer/download-fallback.sh
@@ -7,11 +7,12 @@ version=$(echo $lineWithPackageVersion | cut -d '"' -f 4)
 echo "Downloading @aws-cdk/asset-awscli-v1@$version from npm"
 npm pack @aws-cdk/asset-awscli-v1@$version -q
 
-echo "Extracting layer.zip from aws-cdk-asset-awscli-v1-$version.tgz"
-tar -zxvf aws-cdk-asset-awscli-v1-$version.tgz package/lib/layer.zip
+echo "Extracting layer.zip and requirements.txt from aws-cdk-asset-awscli-v1-$version.tgz"
+tar -zxvf aws-cdk-asset-awscli-v1-$version.tgz package/lib/layer.zip package/layer/requirements.txt
 
-echo "Copying layer.zip to ./layer"
+echo "Moving layer.zip and requirements.txt to desired destinations"
 mv ./package/lib/layer.zip ./lib/
+mv ./package/layer/requirements.txt ./layer
 
 echo "Cleaning up"
 rm aws-cdk-asset-awscli-v1-$version.tgz
