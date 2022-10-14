ac0c76ac1cd2009742f2f135abb02a80dbd869e7
Juliana Motira <motiraj@amazon.com>
2022-10-14 13:03:07 -0700

    Add integ test

--- packages/@aws-cdk/aws-rds/test/integ.proxy.ts
+++ packages/@aws-cdk/aws-rds/test/integ.proxy.ts
@@ -28,4 +28,24 @@ new rds.DatabaseProxy(stack, 'dbProxy', {
   vpc,
 });
 
+const sqlserverInstance = new rds.DatabaseInstance(stack, 'sqlserverInstance', {
+  engine: rds.DatabaseInstanceEngine.sqlServerEx({
+    version: rds.SqlServerEngineVersion.VER_15,
+  }),
+  instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM),
+  credentials: rds.Credentials.fromUsername('master', {
+    excludeCharacters: '"@/\\',
+  }),
+  vpc,
+  removalPolicy: RemovalPolicy.DESTROY,
+});
+
+new rds.DatabaseProxy(stack, 'sqlserverProxy', {
+  borrowTimeout: cdk.Duration.seconds(30),
+  maxConnectionsPercent: 50,
+  secrets: [dbInstance.secret!],
+  proxyTarget: rds.ProxyTarget.fromInstance(sqlserverInstance),
+  vpc,
+});
+
 app.synth();
