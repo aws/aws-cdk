20722ddde629086267196cbac38463228d274591
Madeline Kusters <mkusters@amazon.com>
2022-10-17 14:21:18 -0700

    create a new package with a new interface

---
+++ packages/@aws-cdk/interfaces/lib/lambda-layer-asset.ts
@@ -0,0 +1,9 @@
+import { IConstruct } from 'constructs';
+
+/**
+ * A Construct that can be used as the asset for defining a Lambda Layer
+*/
+export interface ILambdaLayerAsset extends IConstruct {
+
+
+}
