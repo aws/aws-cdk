98627e5ea80c3379ad4db64a994226a5bbbbffb4
Madeline Kusters <mkusters@amazon.com>
2022-10-17 15:47:03 -0700

    Add content to the interface

--- packages/@aws-cdk/interfaces/lib/lambda-layer-asset.ts
+++ packages/@aws-cdk/interfaces/lib/lambda-layer-asset.ts
@@ -2,8 +2,15 @@ import { IConstruct } from 'constructs';
 
 /**
  * A Construct that can be used as the asset for defining a Lambda Layer
-*/
+ */
 export interface ILambdaLayerAsset extends IConstruct {
+  /**
+   * The ARN of the bucket containing the asset
+   */
+  readonly bucketArn: string;
 
-
+  /**
+   * The object key
+   */
+  readonly key: string;
 }
