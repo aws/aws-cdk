44c34df571d11f80ca805c247cbf2000d6c64d20
Madeline Kusters <mkusters@amazon.com>
2022-10-17 15:45:40 -0700

    allow passing of custom awscli

--- packages/@aws-cdk/lambda-layer-awscli/lib/awscli-layer.ts
+++ packages/@aws-cdk/lambda-layer-awscli/lib/awscli-layer.ts
@@ -1,15 +1,28 @@
 import * as path from 'path';
 import * as lambda from '@aws-cdk/aws-lambda';
 import { FileSystem } from '@aws-cdk/core';
+import { ILambdaLayerAsset } from '@aws-cdk/interfaces';
 import { Construct } from 'constructs';
+
+/**
+ * AwsCliLayer construct props
+ */
+export interface AwsCliLayerProps {
+  /**
+   * Use this property to supply your own version of the AWS CLI
+   *
+   * @default - An asset containing AWS CLI v1 will be used.
+   */
+  readonly awsCliAsset?: ILambdaLayerAsset;
+}
 
 /**
  * An AWS Lambda layer that includes the AWS CLI.
  */
 export class AwsCliLayer extends lambda.LayerVersion {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props: AwsCliLayerProps = {}) {
     super(scope, id, {
-      code: lambda.Code.fromAsset(path.join(__dirname, 'layer.zip'), {
+      code: props.awsCliAsset ? lambda.Code.fromLambdaLayerAsset(props.awsCliAsset) : lambda.Code.fromAsset(path.join(__dirname, 'layer.zip'), {
         // we hash the layer directory (it contains the tools versions and Dockerfile) because hashing the zip is non-deterministic
         assetHash: FileSystem.fingerprint(path.join(__dirname, '../layer')),
       }),
--- packages/@aws-cdk/lambda-layer-awscli/test/awscli-layer.test.ts
+++ packages/@aws-cdk/lambda-layer-awscli/test/awscli-layer.test.ts
@@ -1,5 +1,8 @@
 import { Template } from '@aws-cdk/assertions';
+import * as s3 from '@aws-cdk/aws-s3';
 import { Stack } from '@aws-cdk/core';
+import { ILambdaLayerAsset } from '@aws-cdk/interfaces';
+import { Construct } from 'constructs';
 import { AwsCliLayer } from '../lib';
 
 test('synthesized to a layer version', () => {
@@ -14,3 +17,28 @@ test('synthesized to a layer version', () => {
     Description: '/opt/awscli/aws',
   });
 });
+
+test('accepts custom aws cli asset', () => {
+  //GIVEN
+  const stack = new Stack();
+
+  // WHEN
+  new AwsCliLayer(stack, 'MyLayer', {
+    awsCliAsset: new MyAwsCliAsset(stack, 'CustomAwsCliAsset'),
+  });
+
+  // THEN
+  Template.fromStack(stack).hasResourceProperties('AWS::Lambda::LayerVersion', {
+    Content: {},
+  });
+});
+
+class MyAwsCliAsset extends Construct implements ILambdaLayerAsset {
+  bucketArn: string;
+  key: string = 'FakeObjectKey';
+  constructor(scope: Construct, id: string) {
+    super (scope, id);
+    const bucket = new s3.Bucket(this, 'CustomAwsCliBucket');
+    this.bucketArn = bucket.bucketArn;
+  }
+}
