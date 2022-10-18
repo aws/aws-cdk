8c8a4f990008ec9fdecf74bd79f27757b401dcb7
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2022-10-18 15:57:10 -0700

    feat(sagemaker): add EndpointConfig L2 construct
    This is the second of three PRs to complete the implementation of RFC
    431:
    https://github.com/aws/aws-cdk-rfcs/issues/431
    
    related to #2809
    
    Co-authored-by: Matt McClean <mmcclean@amazon.com>
    Co-authored-by: Long Yao <yl1984108@gmail.com>
    Co-authored-by: Drew Jetter <60628154+jetterdj@users.noreply.github.com>
    Co-authored-by: Murali Ganesh <59461079+foxpro24@users.noreply.github.com>
    Co-authored-by: Abilash Rangoju <988529+rangoju@users.noreply.github.com>

--- packages/@aws-cdk/aws-sagemaker/lib/private/util.ts
+++ packages/@aws-cdk/aws-sagemaker/lib/private/util.ts
@@ -1,4 +1,5 @@
 import * as crypto from 'crypto';
+import * as cdk from '@aws-cdk/core';
 
 /**
  * Generates a hash from the provided string for the purposes of avoiding construct ID collision
@@ -10,4 +11,16 @@ export function hashcode(s: string): string {
   const hash = crypto.createHash('md5');
   hash.update(s);
   return hash.digest('hex');
+}
+
+/**
+ * Whether two strings probably contain the same environment dimension (region or account).
+ *
+ * Used to compare either accounts or regions, and also returns true if both
+ * are unresolved (in which case both are expted to be "current region" or "current account").
+ * @param dim1 The first dimension to compare
+ * @param dim2 The second dimension to compare
+ */
+export function sameEnvDimension(dim1: string, dim2: string): boolean {
+  return [cdk.TokenComparison.SAME, cdk.TokenComparison.BOTH_UNRESOLVED].includes(cdk.Token.compareStrings(dim1, dim2));
 }
