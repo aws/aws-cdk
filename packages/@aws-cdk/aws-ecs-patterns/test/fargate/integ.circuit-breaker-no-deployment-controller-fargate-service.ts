02192e10f77b6fbfff61649e43b704834d6ef9ec
Fergus Dixon <fergus.dixon@validusrm.com>
2022-10-19 09:29:20 +0100

    create new test

--- packages/@aws-cdk/aws-ecs-patterns/test/fargate/integ.circuit-breaker-no-deployment-controller-fargate-service.ts
+++ packages/@aws-cdk/aws-ecs-patterns/test/fargate/integ.circuit-breaker-no-deployment-controller-fargate-service.ts
@@ -1,35 +1,24 @@
 import { Vpc } from '@aws-cdk/aws-ec2';
 import { Cluster, ContainerImage } from '@aws-cdk/aws-ecs';
-import { ApplicationProtocol } from '@aws-cdk/aws-elasticloadbalancingv2';
-import * as route53 from '@aws-cdk/aws-route53';
 import { App, Stack } from '@aws-cdk/core';
 import * as integ from '@aws-cdk/integ-tests';
 import { ApplicationLoadBalancedFargateService } from '../../lib';
 
 const app = new App();
-const stack = new Stack(app, 'aws-ecs-integ-alb-fg-https');
+const stack = new Stack(app, 'aws-ecs-integ-circuit-breaker-no-dc');
 const vpc = new Vpc(stack, 'Vpc', { maxAzs: 2 });
 const cluster = new Cluster(stack, 'Cluster', { vpc });
 
-// Loadbalancer with HTTPS
 new ApplicationLoadBalancedFargateService(stack, 'myService', {
   cluster,
   memoryLimitMiB: 512,
   taskImageOptions: {
     image: ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
   },
-  protocol: ApplicationProtocol.HTTPS,
-  enableECSManagedTags: true,
-  enableExecuteCommand: true,
-  domainName: 'test.example.com',
-  domainZone: route53.HostedZone.fromHostedZoneAttributes(stack, 'HostedZone', {
-    hostedZoneId: 'fakeId',
-    zoneName: 'example.com.',
-  }),
-  redirectHTTP: true,
+  circuitBreaker: { rollback: true, useExplicitEcsDeploymentController: false },
 });
 
-new integ.IntegTest(app, 'albFargateServiceTest', {
+new integ.IntegTest(app, 'circuitBreakerNoDeploymentControllerFargateTest', {
   testCases: [stack],
 });
 
