aws/aws-cdk

commit 1beb7bdc2e25b383b9e5af2324ec3c202f9ab0d6

feat(codedeploy): CodeDeploy deployment config constructs for Lambda and ECS (#22159)
CloudFormation now supports Lambda and ECS in the `AWS::CodeDeploy::DeploymentConfig` resource type.  This PR adds L2 constructs specific to ECS and Lambda for that resource type.

----

### All Submissions:

* [x] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)

### New Features

* [x] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
	* [x] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?

*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

packages/@aws-cdk/aws-codedeploy/lib/base-deployment-config.ts
@@ -1,0 +1,171 @@
+import { ArnFormat, Resource, Stack } from '@aws-cdk/core';
+import { Construct } from 'constructs';
+import { CfnDeploymentConfig } from './codedeploy.generated';
+import { MinimumHealthyHosts } from './host-health-config';
+import { TrafficRouting } from './traffic-routing-config';
+import { arnForDeploymentConfig, validateName } from './utils';
+
+/**
+ * The base class for ServerDeploymentConfig, EcsDeploymentConfig,
+ * and LambdaDeploymentConfig deployment configurations.
+ */
+export interface IBaseDeploymentConfig {
+  /**
+   * The physical, human-readable name of the Deployment Configuration.
+   * @attribute
+   */
+  readonly deploymentConfigName: string;
+
+  /**
+   * The ARN of the Deployment Configuration.
+   * @attribute
+   */
+  readonly deploymentConfigArn: string;
+}
+
+/**
+ * Construction properties of {@link BaseDeploymentConfig}.
+ */
+export interface BaseDeploymentConfigOptions {
+  /**
+   * The physical, human-readable name of the Deployment Configuration.
+   * @default - automatically generated name
+   */
+  readonly deploymentConfigName?: string;
+}
+
+/**
+ * The compute platform of a deployment configuration
+ */
+export enum ComputePlatform {
+  /**
+   * The deployment will target EC2 instances or on-premise servers
+   */
+  SERVER = 'Server',
+
+  /**
+   * The deployment will target a Lambda function
+   */
+  LAMBDA = 'Lambda',
+
+  /**
+   * The deployment will target an ECS server
+   */
+  ECS = 'ECS'
+}
+
+/**
+ * Complete base deployment config properties that are required to be supplied by the implementation
+ * of the BaseDeploymentConfig class.
+ */
+export interface BaseDeploymentConfigProps extends BaseDeploymentConfigOptions {
+  /**
+   * The destination compute platform for the deployment.
+   *
+   * @default ComputePlatform.Server
+   */
+  readonly computePlatform?: ComputePlatform;
+
+  /**
+   * The configuration that specifies how traffic is shifted during a deployment.
+   * Only applicable to ECS and Lambda deployments, and must not be specified for Server deployments.
+   * @default None
+   */
+  readonly trafficRouting?: TrafficRouting;
+
+  /**
+   * Minimum number of healthy hosts.
+   * @default None
+   */
+  readonly minimumHealthyHosts?: MinimumHealthyHosts;
+}
+
+/**
+ * The base class for ServerDeploymentConfig, EcsDeploymentConfig,
+ * and LambdaDeploymentConfig deployment configurations.
+ *
+ * @resource AWS::CodeDeploy::DeploymentConfig
+ */
+export abstract class BaseDeploymentConfig extends Resource implements IBaseDeploymentConfig {
+  /**
+   * Import a custom Deployment Configuration for a Deployment Group defined outside the CDK.
+   *
+   * @param scope the parent Construct for this new Construct
+   * @param id the logical ID of this new Construct
+   * @param deploymentConfigName the name of the referenced custom Deployment Configuration
+   * @returns a Construct representing a reference to an existing custom Deployment Configuration
+   */
+  protected static fromDeploymentConfigName(scope: Construct, id: string, deploymentConfigName: string): IBaseDeploymentConfig {
+    ignore(id);
+    const arn = Stack.of(scope).formatArn({
+      service: 'codedeploy',
+      resource: 'deploymentconfig',
+      resourceName: deploymentConfigName,
+      arnFormat: ArnFormat.COLON_RESOURCE_NAME,
+    });
+    return {
+      deploymentConfigName: deploymentConfigName,
+      deploymentConfigArn: arn,
+    };
+  }
+
+  /**
+   * This method should be used only for static references to predefined deployment configurations,
+   * like EcsDeploymentConfig.ALL_AT_ONCE
+   * @param name the name of the referenced custom Deployment Configuration
+   * @returns a reference to an existing custom Deployment Configuration
+   */
+  protected static deploymentConfig(name: string): IBaseDeploymentConfig {
+    return {
+      deploymentConfigName: name,
+      deploymentConfigArn: arnForDeploymentConfig(name),
+    };
+  }
+
+  /**
+   * The name of the deployment config
+   * @attribute
+   */
+  public readonly deploymentConfigName: string;
+
+  /**
+   * The arn of the deployment config
+   * @attribute
+   */
+  public readonly deploymentConfigArn: string;
+
+  public constructor(scope: Construct, id: string, props?: BaseDeploymentConfigProps) {
+    super(scope, id, {
+      physicalName: props?.deploymentConfigName,
+    });
+
+    // Traffic routing is not applicable to Server-based deployment configs
+    if (props?.trafficRouting && (props?.computePlatform === undefined || props?.computePlatform === ComputePlatform.SERVER)) {
+      throw new Error('Traffic routing config must not be specified for a Server-base deployment configuration');
+    }
+
+    // Minimum healthy hosts is only applicable to Server-based deployment configs
+    if (props?.minimumHealthyHosts && props?.computePlatform && props?.computePlatform !== ComputePlatform.SERVER) {
+      throw new Error('Minimum healthy hosts config must only be specified for a Server-base deployment configuration');
+    }
+
+    const resource = new CfnDeploymentConfig(this, 'Resource', {
+      deploymentConfigName: this.physicalName,
+      computePlatform: props?.computePlatform,
+      trafficRoutingConfig: props?.trafficRouting?.bind(this),
+      minimumHealthyHosts: props?.minimumHealthyHosts?._json,
+    });
+
+    this.deploymentConfigName = this.getResourceNameAttribute(resource.ref);
+    this.deploymentConfigArn = this.getResourceArnAttribute(arnForDeploymentConfig(resource.ref), {
+      service: 'codedeploy',
+      resource: 'deploymentconfig',
+      resourceName: this.physicalName,
+      arnFormat: ArnFormat.COLON_RESOURCE_NAME,
+    });
+
+    this.node.addValidation({ validate: () => validateName('Deployment config', this.physicalName) });
+  }
+}
+
+function ignore(_x: any) { return; }

packages/@aws-cdk/aws-codedeploy/lib/ecs/deployment-config.ts
@@ -1,52 +1,69 @@
 import { Construct } from 'constructs';
-import { arnForDeploymentConfig } from '../utils';
+import { BaseDeploymentConfig, BaseDeploymentConfigOptions, ComputePlatform, IBaseDeploymentConfig } from '../base-deployment-config';
+import { TrafficRouting } from '../traffic-routing-config';
 
 /**
  * The Deployment Configuration of an ECS Deployment Group.
+ *
+ * If you're managing the Deployment Configuration alongside the rest of your CDK resources,
+ * use the {@link EcsDeploymentConfig} class.
+ *
+ * If you want to reference an already existing deployment configuration,
+ * or one defined in a different CDK Stack,
+ * use the {@link EcsDeploymentConfig#fromEcsDeploymentConfigName} method.
+ *
  * The default, pre-defined Configurations are available as constants on the {@link EcsDeploymentConfig} class
  * (for example, `EcsDeploymentConfig.AllAtOnce`).
- *
- * Note: CloudFormation does not currently support creating custom ECS configs outside
- * of using a custom resource. You can import custom deployment config created outside the
- * CDK or via a custom resource with {@link EcsDeploymentConfig#fromEcsDeploymentConfigName}.
  */
-export interface IEcsDeploymentConfig {
-  readonly deploymentConfigName: string;
-  readonly deploymentConfigArn: string;
+export interface IEcsDeploymentConfig extends IBaseDeploymentConfig {
+}
+
+/**
+ * Construction properties of {@link EcsDeploymentConfig}.
+ */
+export interface EcsDeploymentConfigProps extends BaseDeploymentConfigOptions {
+  /**
+   * The configuration that specifies how traffic is shifted from the 'blue'
+   * target group to the 'green' target group during a deployment.
+   * @default AllAtOnce
+   */
+  readonly trafficRouting?: TrafficRouting;
 }
 
 /**
  * A custom Deployment Configuration for an ECS Deployment Group.
  *
- * Note: This class currently stands as namespaced container of the default configurations
- * until CloudFormation supports custom ECS Deployment Configs. Until then it is closed
- * (private constructor) and does not extend {@link Construct}
- *
  * @resource AWS::CodeDeploy::DeploymentConfig
  */
-export class EcsDeploymentConfig {
-  public static readonly ALL_AT_ONCE = deploymentConfig('CodeDeployDefault.ECSAllAtOnce');
+export class EcsDeploymentConfig extends BaseDeploymentConfig implements IEcsDeploymentConfig {
+  /** CodeDeploy predefined deployment configuration that shifts all traffic to the updated ECS task set at once. */
+  public static readonly ALL_AT_ONCE = EcsDeploymentConfig.deploymentConfig('CodeDeployDefault.ECSAllAtOnce');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic every minute until all traffic is shifted. */
+  public static readonly LINEAR_10PERCENT_EVERY_1MINUTES = EcsDeploymentConfig.deploymentConfig('CodeDeployDefault.ECSLinear10PercentEvery1Minutes');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic every three minutes until all traffic is shifted. */
+  public static readonly LINEAR_10PERCENT_EVERY_3MINUTES = EcsDeploymentConfig.deploymentConfig('CodeDeployDefault.ECSLinear10PercentEvery3Minutes');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic in the first increment. The remaining 90 percent is deployed five minutes later. */
+  public static readonly CANARY_10PERCENT_5MINUTES = EcsDeploymentConfig.deploymentConfig('CodeDeployDefault.ECSCanary10Percent5Minutes');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic in the first increment. The remaining 90 percent is deployed 15 minutes later. */
+  public static readonly CANARY_10PERCENT_15MINUTES = EcsDeploymentConfig.deploymentConfig('CodeDeployDefault.ECSCanary10Percent15Minutes');
 
   /**
    * Import a custom Deployment Configuration for an ECS Deployment Group defined outside the CDK.
    *
-   * @param _scope the parent Construct for this new Construct
-   * @param _id the logical ID of this new Construct
+   * @param scope the parent Construct for this new Construct
+   * @param id the logical ID of this new Construct
    * @param ecsDeploymentConfigName the name of the referenced custom Deployment Configuration
    * @returns a Construct representing a reference to an existing custom Deployment Configuration
    */
-  public static fromEcsDeploymentConfigName(_scope: Construct, _id: string, ecsDeploymentConfigName: string): IEcsDeploymentConfig {
-    return deploymentConfig(ecsDeploymentConfigName);
+  public static fromEcsDeploymentConfigName(scope: Construct, id: string, ecsDeploymentConfigName: string): IEcsDeploymentConfig {
+    return this.fromDeploymentConfigName(scope, id, ecsDeploymentConfigName);
   }
 
-  private constructor() {
-    // nothing to do until CFN supports custom ECS deployment configurations
+  public constructor(scope: Construct, id: string, props?: EcsDeploymentConfigProps) {
+    super(scope, id, {
+      ...props,
+      computePlatform: ComputePlatform.ECS,
+      trafficRouting: props?.trafficRouting ?? TrafficRouting.allAtOnce(),
+    });
   }
-}
-
-function deploymentConfig(name: string): IEcsDeploymentConfig {
-  return {
-    deploymentConfigName: name,
-    deploymentConfigArn: arnForDeploymentConfig(name),
-  };
 }

packages/@aws-cdk/aws-codedeploy/lib/lambda/deployment-config.ts
@@ -1,18 +1,21 @@
 import { Construct } from 'constructs';
-import { arnForDeploymentConfig } from '../utils';
+import { BaseDeploymentConfig, BaseDeploymentConfigOptions, ComputePlatform, IBaseDeploymentConfig } from '../base-deployment-config';
+import { TrafficRouting } from '../traffic-routing-config';
 
 /**
  * The Deployment Configuration of a Lambda Deployment Group.
+ *
+ * If you're managing the Deployment Configuration alongside the rest of your CDK resources,
+ * use the {@link LambdaDeploymentConfig} class.
+ *
+ * If you want to reference an already existing deployment configuration,
+ * or one defined in a different CDK Stack,
+ * use the {@link LambdaDeploymentConfig#fromLambdaDeploymentConfigName} method.
+ *
  * The default, pre-defined Configurations are available as constants on the {@link LambdaDeploymentConfig} class
  * (`LambdaDeploymentConfig.AllAtOnce`, `LambdaDeploymentConfig.Canary10Percent30Minutes`, etc.).
- *
- * Note: CloudFormation does not currently support creating custom lambda configs outside
- * of using a custom resource. You can import custom deployment config created outside the
- * CDK or via a custom resource with {@link LambdaDeploymentConfig#import}.
  */
-export interface ILambdaDeploymentConfig {
-  readonly deploymentConfigName: string;
-  readonly deploymentConfigArn: string;
+export interface ILambdaDeploymentConfig extends IBaseDeploymentConfig {
 }
 
 /**
@@ -29,45 +32,71 @@ export interface LambdaDeploymentConfigImportProps {
 }
 
 /**
+ * Construction properties of {@link LambdaDeploymentConfig}.
+ */
+export interface LambdaDeploymentConfigProps extends BaseDeploymentConfigOptions {
+  /**
+   * The configuration that specifies how traffic is shifted from the 'blue'
+   * target group to the 'green' target group during a deployment.
+   * @default AllAtOnce
+   */
+  readonly trafficRouting?: TrafficRouting;
+}
+
+/**
  * A custom Deployment Configuration for a Lambda Deployment Group.
- *
- * Note: This class currently stands as namespaced container of the default configurations
- * until CloudFormation supports custom Lambda Deployment Configs. Until then it is closed
- * (private constructor) and does not extend {@link Construct}
- *
  * @resource AWS::CodeDeploy::DeploymentConfig
  */
-export class LambdaDeploymentConfig {
-  public static readonly ALL_AT_ONCE = deploymentConfig('CodeDeployDefault.LambdaAllAtOnce');
-  public static readonly CANARY_10PERCENT_30MINUTES = deploymentConfig('CodeDeployDefault.LambdaCanary10Percent30Minutes');
-  public static readonly CANARY_10PERCENT_5MINUTES = deploymentConfig('CodeDeployDefault.LambdaCanary10Percent5Minutes');
-  public static readonly CANARY_10PERCENT_10MINUTES = deploymentConfig('CodeDeployDefault.LambdaCanary10Percent10Minutes');
-  public static readonly CANARY_10PERCENT_15MINUTES = deploymentConfig('CodeDeployDefault.LambdaCanary10Percent15Minutes');
-  public static readonly LINEAR_10PERCENT_EVERY_10MINUTES = deploymentConfig('CodeDeployDefault.LambdaLinear10PercentEvery10Minutes');
-  public static readonly LINEAR_10PERCENT_EVERY_1MINUTE = deploymentConfig('CodeDeployDefault.LambdaLinear10PercentEvery1Minute');
-  public static readonly LINEAR_10PERCENT_EVERY_2MINUTES = deploymentConfig('CodeDeployDefault.LambdaLinear10PercentEvery2Minutes');
-  public static readonly LINEAR_10PERCENT_EVERY_3MINUTES = deploymentConfig('CodeDeployDefault.LambdaLinear10PercentEvery3Minutes');
+export class LambdaDeploymentConfig extends BaseDeploymentConfig implements ILambdaDeploymentConfig {
+  /** CodeDeploy predefined deployment configuration that shifts all traffic to the updated Lambda function at once. */
+  public static readonly ALL_AT_ONCE = LambdaDeploymentConfig.deploymentConfig('CodeDeployDefault.LambdaAllAtOnce');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic in the first increment. The remaining 90 percent is deployed 30 minutes later. */
+  public static readonly CANARY_10PERCENT_30MINUTES = LambdaDeploymentConfig.deploymentConfig('CodeDeployDefault.LambdaCanary10Percent30Minutes');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic in the first increment. The remaining 90 percent is deployed five minutes later. */
+  public static readonly CANARY_10PERCENT_5MINUTES = LambdaDeploymentConfig.deploymentConfig('CodeDeployDefault.LambdaCanary10Percent5Minutes');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic in the first increment. The remaining 90 percent is deployed 10 minutes later. */
+  public static readonly CANARY_10PERCENT_10MINUTES = LambdaDeploymentConfig.deploymentConfig('CodeDeployDefault.LambdaCanary10Percent10Minutes');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic in the first increment. The remaining 90 percent is deployed 15 minutes later. */
+  public static readonly CANARY_10PERCENT_15MINUTES = LambdaDeploymentConfig.deploymentConfig('CodeDeployDefault.LambdaCanary10Percent15Minutes');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic every 10 minutes until all traffic is shifted. */
+  public static readonly LINEAR_10PERCENT_EVERY_10MINUTES = LambdaDeploymentConfig.deploymentConfig('CodeDeployDefault.LambdaLinear10PercentEvery10Minutes');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic every minute until all traffic is shifted. */
+  public static readonly LINEAR_10PERCENT_EVERY_1MINUTE = LambdaDeploymentConfig.deploymentConfig('CodeDeployDefault.LambdaLinear10PercentEvery1Minute');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic every two minutes until all traffic is shifted. */
+  public static readonly LINEAR_10PERCENT_EVERY_2MINUTES = LambdaDeploymentConfig.deploymentConfig('CodeDeployDefault.LambdaLinear10PercentEvery2Minutes');
+  /** CodeDeploy predefined deployment configuration that shifts 10 percent of traffic every three minutes until all traffic is shifted. */
+  public static readonly LINEAR_10PERCENT_EVERY_3MINUTES = LambdaDeploymentConfig.deploymentConfig('CodeDeployDefault.LambdaLinear10PercentEvery3Minutes');
 
   /**
-   * Import a custom Deployment Configuration for a Lambda Deployment Group defined outside the CDK.
+   * Import a Deployment Configuration for a Lambda Deployment Group defined outside the CDK.
+   *
+   * @param scope the parent Construct for this new Construct
+   * @param id the logical ID of this new Construct
+   * @param lambdaDeploymentConfigName the name of the Lambda Deployment Configuration to import
+   * @returns a Construct representing a reference to an existing Lambda Deployment Configuration
+   */
+  public static fromLambdaDeploymentConfigName(scope: Construct, id: string, lambdaDeploymentConfigName: string): ILambdaDeploymentConfig {
+    return this.fromDeploymentConfigName(scope, id, lambdaDeploymentConfigName);
+  }
+
+  /**
+   * Import a Deployment Configuration for a Lambda Deployment Group defined outside the CDK.
    *
    * @param _scope the parent Construct for this new Construct
    * @param _id the logical ID of this new Construct
    * @param props the properties of the referenced custom Deployment Configuration
    * @returns a Construct representing a reference to an existing custom Deployment Configuration
+   * @deprecated use `fromLambdaDeploymentConfigName`
    */
-  public static import(_scope:Construct, _id: string, props: LambdaDeploymentConfigImportProps): ILambdaDeploymentConfig {
-    return deploymentConfig(props.deploymentConfigName);
+  public static import(_scope: Construct, _id: string, props: LambdaDeploymentConfigImportProps): ILambdaDeploymentConfig {
+    return this.fromLambdaDeploymentConfigName(_scope, _id, props.deploymentConfigName);
   }
 
-  private constructor() {
-    // nothing to do until CFN supports custom lambda deployment configurations
+  public constructor(scope: Construct, id: string, props?: LambdaDeploymentConfigProps) {
+    super(scope, id, {
+      ...props,
+      computePlatform: ComputePlatform.LAMBDA,
+      trafficRouting: props?.trafficRouting ?? TrafficRouting.allAtOnce(),
+    });
   }
-}
-
-function deploymentConfig(name: string): ILambdaDeploymentConfig {
-  return {
-    deploymentConfigName: name,
-    deploymentConfigArn: arnForDeploymentConfig(name),
-  };
 }

commit 3b6bfde51896c80e3effbddd47606a7d1e025285

fix: breaking change to deployment config props
Change all `deploymentConfig` static method implementations on
`EcsDeploymentConfig`, `LambdaDeploymentConfig`, and
`ServerDeploymentConfig` to return their corresponding specific
interfaces instead of `IBaseDeploymentConfig`. This reverts breaking
changes to Java users introduced in #22159

packages/@aws-cdk/aws-codedeploy/lib/base-deployment-config.ts
@@ -110,19 +110,6 @@ export abstract class BaseDeploymentConfig extends Resource implements IBaseDepl
   }
 
   /**
-   * This method should be used only for static references to predefined deployment configurations,
-   * like EcsDeploymentConfig.ALL_AT_ONCE
-   * @param name the name of the referenced custom Deployment Configuration
-   * @returns a reference to an existing custom Deployment Configuration
-   */
-  protected static deploymentConfig(name: string): IBaseDeploymentConfig {
-    return {
-      deploymentConfigName: name,
-      deploymentConfigArn: arnForDeploymentConfig(name),
-    };
-  }
-
-  /**
    * The name of the deployment config
    * @attribute
    */

packages/@aws-cdk/aws-codedeploy/lib/ecs/deployment-config.ts
@@ -1,6 +1,7 @@
 import { Construct } from 'constructs';
 import { BaseDeploymentConfig, BaseDeploymentConfigOptions, ComputePlatform, IBaseDeploymentConfig } from '../base-deployment-config';
 import { TrafficRouting } from '../traffic-routing-config';
+import { deploymentConfig } from '../utils';
 
 /**
  * The Deployment Configuration of an ECS Deployment Group.
@@ -57,6 +58,10 @@ export class EcsDeploymentConfig extends BaseDeploymentConfig implements IEcsDep
    */
   public static fromEcsDeploymentConfigName(scope: Construct, id: string, ecsDeploymentConfigName: string): IEcsDeploymentConfig {
     return this.fromDeploymentConfigName(scope, id, ecsDeploymentConfigName);
+  }
+
+  private static deploymentConfig(name: string): IEcsDeploymentConfig {
+    return deploymentConfig(name);
   }
 
   public constructor(scope: Construct, id: string, props?: EcsDeploymentConfigProps) {

packages/@aws-cdk/aws-codedeploy/lib/lambda/deployment-config.ts
@@ -1,6 +1,7 @@
 import { Construct } from 'constructs';
 import { BaseDeploymentConfig, BaseDeploymentConfigOptions, ComputePlatform, IBaseDeploymentConfig } from '../base-deployment-config';
 import { TrafficRouting } from '../traffic-routing-config';
+import { deploymentConfig } from '../utils';
 
 /**
  * The Deployment Configuration of a Lambda Deployment Group.
@@ -90,6 +91,10 @@ export class LambdaDeploymentConfig extends BaseDeploymentConfig implements ILam
    */
   public static import(_scope: Construct, _id: string, props: LambdaDeploymentConfigImportProps): ILambdaDeploymentConfig {
     return this.fromLambdaDeploymentConfigName(_scope, _id, props.deploymentConfigName);
+  }
+
+  private static deploymentConfig(name: string): ILambdaDeploymentConfig {
+    return deploymentConfig(name);
   }
 
   public constructor(scope: Construct, id: string, props?: LambdaDeploymentConfigProps) {

packages/@aws-cdk/aws-codedeploy/lib/server/deployment-config.ts
@@ -1,6 +1,7 @@
 import { Construct } from 'constructs';
 import { BaseDeploymentConfig, BaseDeploymentConfigOptions, IBaseDeploymentConfig } from '../base-deployment-config';
 import { MinimumHealthyHosts } from '../host-health-config';
+import { deploymentConfig } from '../utils';
 
 /**
  * The Deployment Configuration of an EC2/on-premise Deployment Group.
@@ -61,6 +62,10 @@ export class ServerDeploymentConfig extends BaseDeploymentConfig implements ISer
     id: string,
     serverDeploymentConfigName: string): IServerDeploymentConfig {
     return this.fromDeploymentConfigName(scope, id, serverDeploymentConfigName);
+  }
+
+  private static deploymentConfig(name: string): IServerDeploymentConfig {
+    return deploymentConfig(name);
   }
 
   constructor(scope: Construct, id: string, props: ServerDeploymentConfigProps) {

packages/@aws-cdk/aws-codedeploy/lib/utils.ts
@@ -1,5 +1,6 @@
 import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
 import { Aws, Token } from '@aws-cdk/core';
+import { IBaseDeploymentConfig } from './base-deployment-config';
 import { CfnDeploymentGroup } from './codedeploy.generated';
 import { AutoRollbackConfig } from './rollback-config';
 
@@ -24,6 +25,13 @@ CfnDeploymentGroup.AlarmConfigurationProperty | undefined {
       enabled: true,
       ignorePollAlarmFailure,
     };
+}
+
+export function deploymentConfig(name: string): IBaseDeploymentConfig {
+  return {
+    deploymentConfigName: name,
+    deploymentConfigArn: arnForDeploymentConfig(name),
+  };
 }
 
 enum AutoRollbackEvent {