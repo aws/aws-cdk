0b48d6046de401c2009d04b40dd8748f3e6e7c9a
Calvin Combs <comcalvi@amazon.com>
2022-10-21 12:59:58 -0700

    integ test finalized

--- packages/@aws-cdk/aws-eks/test/eks-service-account-sdk-call.integ.snapshot/asset.dcbc2e9181e895e14522d2a67351d9654790e35567e370f54120423dd4604b40/index.py
+++ packages/@aws-cdk/aws-eks/test/eks-service-account-sdk-call.integ.snapshot/asset.dcbc2e9181e895e14522d2a67351d9654790e35567e370f54120423dd4604b40/index.py
@@ -1,11 +1,9 @@
 import json
 import logging
-#import urllib3
 import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-#http = urllib3.PoolManager()
 
 def handler(event, context):
   print(json.dumps(event))
@@ -16,13 +14,6 @@ def handler(event, context):
   s3_bucket_name = 'amazingly-made-sdk-call-created-eks-bucket'
 
   if request_type in ['Create', 'Update']:
-    #logger.info(f'Sending request to {url}')
-    # this should a substantial retry because it has to wait for the ELB to actually
-    # be functioning
-    #response = http.request('GET', url, retries=urllib3.Retry(10, backoff_factor=1))
-    #if response.status != 200:
-    #  raise RuntimeError(f'Request failed: {response.status} ({response.reason})')
-    #return {'Data': {'Value': response.data.decode('utf-8')}}
     logger.info(f'making sdk call to create bucket with name {s3_bucket_name}')
 
     s3 = boto3.client('s3')
