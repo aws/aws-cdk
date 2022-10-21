e2e02f1402a38cd78c725bf71eed798e2105a64d
Calvin Combs <comcalvi@amazon.com>
2022-10-21 10:03:38 -0700

    add missing js

---
+++ packages/@aws-cdk/aws-eks/test/docker-app/app/sdk-call.js
@@ -0,0 +1,11 @@
+const sdk = require('aws-sdk');
+sdk.config.update({region: 'us-east-2'});
+
+var s3 = new sdk.S3();
+s3.createBucket({ Bucket: 'aaamazingly-made-sdk-call-created-eks-bucket'}, function(err) {
+  if (!err) {
+    console.log('Bucket was created');
+  } else {
+    throw new Error('failed to create s3 bucket with error: ' + err);
+  }
+});
