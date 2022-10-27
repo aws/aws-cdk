bdb3598be507914143be45758d999a06a3d9001f
Kaizen Conroy <zen.conroy@gmail.com>
2022-10-27 16:38:34 -0400

    chore(lambda-layer-kubectl): depend on awscdk-asset-kubectl-v20 and remove zip files

--- packages/@aws-cdk/lambda-layer-kubectl/lib/kubectl-layer.ts
+++ packages/@aws-cdk/lambda-layer-kubectl/lib/kubectl-layer.ts
@@ -1,6 +1,6 @@
 import * as crypto from 'crypto';
 import * as fs from 'fs';
-import * as path from 'path';
+import { ASSET_FILE, DOCKER_DIR } from '@aws-cdk/asset-kubectl-v20';
 import * as lambda from '@aws-cdk/aws-lambda';
 import { Construct } from 'constructs';
 
@@ -10,9 +10,9 @@ import { Construct } from 'constructs';
 export class KubectlLayer extends lambda.LayerVersion {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
-      code: lambda.Code.fromAsset(path.join(__dirname, 'layer.zip'), {
+      code: lambda.Code.fromAsset(ASSET_FILE, {
         // we hash the Dockerfile (it contains the tools versions) because hashing the zip is non-deterministic
-        assetHash: hashFile(path.join(__dirname, '..', 'layer', 'Dockerfile')),
+        assetHash: hashFile(DOCKER_DIR),
       }),
       description: '/opt/kubectl/kubectl and /opt/helm/helm',
     });
