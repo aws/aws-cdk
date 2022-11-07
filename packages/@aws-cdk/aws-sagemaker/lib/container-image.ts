ebe1a704e49d4a08899e7216777abbec06f98f97
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2022-11-07 13:05:46 -0800

    Apply suggestion to reformat ECR image name examples
    Co-authored-by: Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>

--- packages/@aws-cdk/aws-sagemaker/lib/container-image.ts
+++ packages/@aws-cdk/aws-sagemaker/lib/container-image.ts
@@ -12,8 +12,8 @@ export interface ContainerImageConfig {
    * The image name. Images in Amazon ECR repositories can be specified by either using the full registry/repository:tag or
    * registry/repository@digest.
    *
-   * For example, 012345678910.dkr.ecr.<region-name>.amazonaws.com/<repository-name>:latest or
-   * 012345678910.dkr.ecr.<region-name>.amazonaws.com/<repository-name>@sha256:94afd1f2e64d908bc90dbca0035a5b567EXAMPLE.
+   * For example, `012345678910.dkr.ecr.<region-name>.amazonaws.com/<repository-name>:latest` or
+   * `012345678910.dkr.ecr.<region-name>.amazonaws.com/<repository-name>@sha256:94afd1f2e64d908bc90dbca0035a5b567EXAMPLE`.
    */
   readonly imageName: string;
 }
