9972bd8423cfa24f2d269922a891d368e660587c
epolon <epolon@amazon.com>
2022-11-08 00:09:22 +0200

    revert

--- packages/@aws-cdk/aws-eks/test/integ.eks-service-account-sdk-call.js.snapshot/asset.54dbb3f011fe4c24c9876a0e58b7c3a9105f6b00d3f7d011c87fc70dc6300472/sdk-call.js
+++ packages/@aws-cdk/aws-eks/test/integ.eks-service-account-sdk-call.js.snapshot/asset.54dbb3f011fe4c24c9876a0e58b7c3a9105f6b00d3f7d011c87fc70dc6300472/sdk-call.js
@@ -2,8 +2,7 @@ const sdk = require('aws-sdk');
 sdk.config.update({region: 'us-east-1'});
 
 var s3 = new sdk.S3();
-const bucketName = process.env.BUCKET_NAME;
-s3.createBucket({ Bucket: bucketName }, function(err) {
+s3.createBucket({ Bucket: 'amazingly-made-sdk-call-created-eks-bucket' }, function(err) {
   if (!err) {
     console.log('Bucket was created');
   } else {
