7f7a46fbe0613e19e99342a7123bd0d774f9d706
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2022-11-10 14:58:32 -0800

    Fix util.ts doc typo

--- packages/@aws-cdk/aws-sagemaker/lib/private/util.ts
+++ packages/@aws-cdk/aws-sagemaker/lib/private/util.ts
@@ -17,7 +17,7 @@ export function hashcode(s: string): string {
  * Whether two strings probably contain the same environment dimension (region or account).
  *
  * Used to compare either accounts or regions, and also returns true if both
- * are unresolved (in which case both are expted to be "current region" or "current account").
+ * are unresolved (in which case both are expected to be "current region" or "current account").
  * @param dim1 The first dimension to compare
  * @param dim2 The second dimension to compare
  */
