1eb6c1214c4fcb8c83851e40a57cf12c68292b2a
Momo Kornher <kornherm@amazon.co.uk>
2022-11-16 15:45:17 +0000

    feat(integ-runner): support config file

--- packages/@aws-cdk/integ-runner/test/cli.test.ts
+++ packages/@aws-cdk/integ-runner/test/cli.test.ts
@@ -1,5 +1,18 @@
+import * as fs from 'fs';
+import * as os from 'os';
 import * as path from 'path';
-import { main } from '../lib/cli';
+import { main, parseCliArgs } from '../lib/cli';
+
+let stdoutMock: jest.SpyInstance;
+let stderrMock: jest.SpyInstance;
+beforeEach(() => {
+  stdoutMock = jest.spyOn(process.stdout, 'write').mockImplementation(() => { return true; });
+  stderrMock = jest.spyOn(process.stderr, 'write').mockImplementation(() => { return true; });
+});
+afterEach(() => {
+  stdoutMock.mockRestore();
+  stderrMock.mockRestore();
+});
 
 describe('CLI', () => {
   const currentCwd = process.cwd();
@@ -8,14 +21,6 @@ describe('CLI', () => {
   });
   afterAll(() => {
     process.chdir(currentCwd);
-  });
-
-  let stdoutMock: jest.SpyInstance;
-  beforeEach(() => {
-    stdoutMock = jest.spyOn(process.stdout, 'write').mockImplementation(() => { return true; });
-  });
-  afterEach(() => {
-    stdoutMock.mockRestore();
   });
 
   test('find by default pattern', async () => {
@@ -40,5 +45,42 @@ describe('CLI', () => {
         '',
       ].join('\n'),
     ]]);
+  });
+
+  test('list only shows explicitly provided tests', async () => {
+    await main(['xxxxx.integ-test1.js', 'xxxxx.integ-test2.js', '--list', '--directory=test/test-data', '--test-regex="^xxxxx\..*\.js$"']);
+
+    expect(stdoutMock.mock.calls).toEqual([[
+      [
+        'xxxxx.integ-test1.js',
+        'xxxxx.integ-test2.js',
+        '',
+      ].join('\n'),
+    ]]);
+  });
+});
+
+describe('CLI config file', () => {
+  const configFile = 'integ.config.json';
+  const withConfig = (settings: any, fileName = configFile) => {
+    fs.writeFileSync(fileName, JSON.stringify(settings, null, 2), { encoding: 'utf-8' });
+  };
+
+  const currentCwd = process.cwd();
+  beforeEach(() => {
+    process.chdir(os.tmpdir());
+  });
+  afterEach(() => {
+    process.chdir(currentCwd);
+  });
+
+  test('options are read from config file', async () => {
+    // WHEN
+    withConfig({ list: true, app: 'echo' });
+    const options = parseCliArgs();
+
+    // THEN
+    expect(options.list).toBe(true);
+    expect(options.app).toBe('echo');
   });
 });
