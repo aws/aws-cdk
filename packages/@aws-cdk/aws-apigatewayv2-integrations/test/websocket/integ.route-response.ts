8c996cf2d5e5d1357cea925eda2b9d25e1e4c629
Emil Rowland <rowland.emil@gmail.com>
2022-11-18 22:59:15 +0000

    Added integration test

---
+++ packages/@aws-cdk/aws-apigatewayv2-integrations/test/websocket/integ.route-response.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import { WebSocketApi } from '@aws-cdk/aws-apigatewayv2';
+import * as cdk from '@aws-cdk/core';
+import { IntegTest } from '@aws-cdk/integ-tests';
+import { WebSocketMockIntegration } from '../../lib';
+
+const app = new cdk.App();
+
+const stack = new cdk.Stack(app, 'aws-cdk-aws-apigatewayv2-websockets');
+
+const api = new WebSocketApi(stack, 'MyWebsocketApi');
+api.addRoute('test', {
+  integration: new WebSocketMockIntegration('SendMessageIntegration'),
+  shouldReturnResponse: true,
+});
+
+new IntegTest(app, 'Integ', { testCases: [stack] });
+
+app.synth();
