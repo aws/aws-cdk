12aaf6f89b672d865fe661821f6a8e9edf8fae3b
guessi <guessi@gmail.com>
2022-11-18 08:17:13 +0800

    chore(eks): revert changes for integs

--- packages/@aws-cdk/aws-eks/test/integ.alb-controller.ts
+++ packages/@aws-cdk/aws-eks/test/integ.alb-controller.ts
@@ -20,7 +20,7 @@ class EksClusterAlbControllerStack extends Stack {
       vpc,
       ...getClusterVersionConfig(this),
       albController: {
-        version: eks.AlbControllerVersion.V2_4_5,
+        version: eks.AlbControllerVersion.V2_4_1,
       },
     });
 
--- packages/@aws-cdk/aws-eks/test/integ.eks-inference.ts
+++ packages/@aws-cdk/aws-eks/test/integ.eks-inference.ts
@@ -17,7 +17,7 @@ class EksClusterInferenceStack extends Stack {
       vpc,
       ...getClusterVersionConfig(this),
       albController: {
-        version: eks.AlbControllerVersion.V2_4_5,
+        version: eks.AlbControllerVersion.V2_4_1,
       },
     });
 
