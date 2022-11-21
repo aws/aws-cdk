5a1c7780280d88b19892641106414374509c611f
Tatsuya Yamamoto <the.kinnikuman@gmail.com>
2022-11-22 07:44:41 +0900

    Update packages/@aws-cdk/aws-lambda-nodejs/test/integ-handlers/dependencies-sdk-v3.ts
    Co-authored-by: Jonathan Goldwasser <jogold@users.noreply.github.com>

--- packages/@aws-cdk/aws-lambda-nodejs/test/integ-handlers/dependencies-sdk-v3.ts
+++ packages/@aws-cdk/aws-lambda-nodejs/test/integ-handlers/dependencies-sdk-v3.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-// @ts-ignore
 import { S3Client } from '@aws-sdk/client-s3'; // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved
 
 const s3 = new S3Client();
