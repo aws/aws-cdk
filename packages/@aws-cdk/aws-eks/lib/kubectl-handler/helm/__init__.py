ecf0272722c428116ca391919d67fe88e918559c
Paul Fouquet <pfouquet@linz.govt.nz>
2022-11-24 10:48:15 +1300

    fix: use 'us-east-1' region as deployment when using public ECR

--- packages/@aws-cdk/aws-eks/lib/kubectl-handler/helm/__init__.py
+++ packages/@aws-cdk/aws-eks/lib/kubectl-handler/helm/__init__.py
@@ -2,58 +2,58 @@ import json
 import logging
 import os
 import re
-import subprocess
 import shutil
+import subprocess
 import tempfile
 import zipfile
-from urllib.parse import urlparse, unquote
+from urllib.parse import unquote, urlparse
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # these are coming from the kubectl layer
-os.environ['PATH'] = '/opt/helm:/opt/awscli:' + os.environ['PATH']
+os.environ["PATH"] = "/opt/helm:/opt/awscli:" + os.environ["PATH"]
 
-outdir = os.environ.get('TEST_OUTDIR', '/tmp')
-kubeconfig = os.path.join(outdir, 'kubeconfig')
+outdir = os.environ.get("TEST_OUTDIR", "/tmp")
+kubeconfig = os.path.join(outdir, "kubeconfig")
+
 
 def get_chart_asset_from_url(chart_asset_url):
-    chart_zip = os.path.join(outdir, 'chart.zip')
+    chart_zip = os.path.join(outdir, "chart.zip")
     shutil.rmtree(chart_zip, ignore_errors=True)
-    subprocess.check_call(['aws', 's3', 'cp', chart_asset_url, chart_zip])
-    chart_dir = os.path.join(outdir, 'chart')
+    subprocess.check_call(["aws", "s3", "cp", chart_asset_url, chart_zip])
+    chart_dir = os.path.join(outdir, "chart")
     shutil.rmtree(chart_dir, ignore_errors=True)
     os.mkdir(chart_dir)
-    with zipfile.ZipFile(chart_zip, 'r') as zip_ref:
+    with zipfile.ZipFile(chart_zip, "r") as zip_ref:
         zip_ref.extractall(chart_dir)
     return chart_dir
 
-def helm_handler(event, context):
-    logger.info(json.dumps(dict(event, ResponseURL='...')))
 
-    request_type = event['RequestType']
-    props = event['ResourceProperties']
+def helm_handler(event, context):
+    logger.info(json.dumps(dict(event, ResponseURL="...")))
+
+    request_type = event["RequestType"]
+    props = event["ResourceProperties"]
 
     # resource properties
-    cluster_name     = props['ClusterName']
-    role_arn         = props['RoleArn']
-    release          = props['Release']
-    chart            = props.get('Chart', None)
-    chart_asset_url  = props.get('ChartAssetURL', None)
-    version          = props.get('Version', None)
-    wait             = props.get('Wait', False)
-    timeout          = props.get('Timeout', None)
-    namespace        = props.get('Namespace', None)
-    create_namespace = props.get('CreateNamespace', None)
-    repository       = props.get('Repository', None)
-    values_text      = props.get('Values', None)
+    cluster_name = props["ClusterName"]
+    role_arn = props["RoleArn"]
+    release = props["Release"]
+    chart = props.get("Chart", None)
+    chart_asset_url = props.get("ChartAssetURL", None)
+    version = props.get("Version", None)
+    wait = props.get("Wait", False)
+    timeout = props.get("Timeout", None)
+    namespace = props.get("Namespace", None)
+    create_namespace = props.get("CreateNamespace", None)
+    repository = props.get("Repository", None)
+    values_text = props.get("Values", None)
 
     # "log in" to the cluster
-    subprocess.check_call([ 'aws', 'eks', 'update-kubeconfig',
-        '--role-arn', role_arn,
-        '--name', cluster_name,
-        '--kubeconfig', kubeconfig
-    ])
+    subprocess.check_call(
+        ["aws", "eks", "update-kubeconfig", "--role-arn", role_arn, "--name", cluster_name, "--kubeconfig", kubeconfig]
+    )
 
     if os.path.isfile(kubeconfig):
         os.chmod(kubeconfig, 0o600)
@@ -62,33 +62,33 @@ def helm_handler(event, context):
     values_file = None
     if not request_type == "Delete" and not values_text is None:
         values = json.loads(values_text)
-        values_file = os.path.join(outdir, 'values.yaml')
+        values_file = os.path.join(outdir, "values.yaml")
         with open(values_file, "w") as f:
             f.write(json.dumps(values, indent=2))
 
-    if request_type == 'Create' or request_type == 'Update':
+    if request_type == "Create" or request_type == "Update":
         # Ensure chart or chart_asset_url are set
         if chart == None and chart_asset_url == None:
-            raise RuntimeError(f'chart or chartAsset must be specified')
+            raise RuntimeError(f"chart or chartAsset must be specified")
 
         if chart_asset_url != None:
-            assert(chart==None)
-            assert(repository==None)
-            assert(version==None)
-            if not chart_asset_url.startswith('s3://'):
-              raise RuntimeError(f'ChartAssetURL must point to as s3 location but is {chart_asset_url}')
+            assert chart == None
+            assert repository == None
+            assert version == None
+            if not chart_asset_url.startswith("s3://"):
+                raise RuntimeError(f"ChartAssetURL must point to as s3 location but is {chart_asset_url}")
             # future work: support versions from s3 assets
             chart = get_chart_asset_from_url(chart_asset_url)
 
-        if repository is not None and repository.startswith('oci://'):
+        if repository is not None and repository.startswith("oci://"):
             tmpdir = tempfile.TemporaryDirectory()
             chart_dir = get_chart_from_oci(tmpdir.name, release, repository, version)
             chart = chart_dir
 
-        helm('upgrade', release, chart, repository, values_file, namespace, version, wait, timeout, create_namespace)
+        helm("upgrade", release, chart, repository, values_file, namespace, version, wait, timeout, create_namespace)
     elif request_type == "Delete":
         try:
-            helm('uninstall', release, namespace=namespace, timeout=timeout)
+            helm("uninstall", release, namespace=namespace, timeout=timeout)
         except Exception as e:
             logger.info("delete error: %s" % e)
 
@@ -96,34 +96,32 @@ def helm_handler(event, context):
 def get_oci_cmd(repository, version):
     # Generates OCI command based on pattern. Public ECR vs Private ECR are treated differently.
     cmnd = []
-    private_ecr_pattern = '\d+.dkr.ecr.[a-z]+-[a-z]+-\d.amazonaws.com'
-    public_ecr = 'public.ecr.aws'
+    private_ecr_pattern = "\d+.dkr.ecr.[a-z]+-[a-z]+-\d.amazonaws.com"
+    public_ecr = "public.ecr.aws"
 
-    registry = repository.rsplit('/', 1)[0].replace('oci://', '')
+    registry = repository.rsplit("/", 1)[0].replace("oci://", "")
 
     if re.fullmatch(private_ecr_pattern, registry) is not None:
         logger.info("Found AWS private repository")
-        region = registry.replace('.amazonaws.com', '').split('.')[-1]
+        region = registry.replace(".amazonaws.com", "").split(".")[-1]
         cmnd = [
-            f"aws ecr get-login-password --region {region} | " \
+            f"aws ecr get-login-password --region {region} | "
             f"helm registry login --username AWS --password-stdin {registry}; helm pull {repository} --version {version} --untar"
-            ]
+        ]
     elif registry.startswith(public_ecr):
-        logger.info("Found AWS public repository, will use default region as deployment")
-        region = os.environ.get('AWS_REGION', 'us-east-1')
-
+        logger.info("Found AWS public repository, will use us-east-1 region as deployment")
         cmnd = [
-            f"aws ecr-public get-login-password --region {region} | " \
+            "aws ecr-public get-login-password --region us-east-1 | "
             f"helm registry login --username AWS --password-stdin {public_ecr}; helm pull {repository} --version {version} --untar"
-            ]
+        ]
     else:
         logger.error("OCI repository format not recognized, falling back to helm pull")
-        cmnd = ['helm', 'pull', repository, '--version', version, '--untar']
+        cmnd = ["helm", "pull", repository, "--version", version, "--untar"]
 
     return cmnd
 
 
-def get_chart_from_oci(tmpdir, release, repository = None, version = None):
+def get_chart_from_oci(tmpdir, release, repository=None, version=None):
 
     cmnd = get_oci_cmd(repository, version)
 
@@ -138,37 +136,48 @@ def get_chart_from_oci(tmpdir, release, repository = None, version = None):
             return os.path.join(tmpdir, release)
         except subprocess.CalledProcessError as exc:
             output = exc.output
-            if b'Broken pipe' in output:
+            if b"Broken pipe" in output:
                 retry = retry - 1
                 logger.info("Broken pipe, retries left: %s" % retry)
             else:
                 raise Exception(output)
-    raise Exception(f'Operation failed after {maxAttempts} attempts: {output}')
+    raise Exception(f"Operation failed after {maxAttempts} attempts: {output}")
 
 
-def helm(verb, release, chart = None, repo = None, file = None, namespace = None, version = None, wait = False, timeout = None, create_namespace = None):
+def helm(
+    verb,
+    release,
+    chart=None,
+    repo=None,
+    file=None,
+    namespace=None,
+    version=None,
+    wait=False,
+    timeout=None,
+    create_namespace=None,
+):
     import subprocess
 
-    cmnd = ['helm', verb, release]
+    cmnd = ["helm", verb, release]
     if not chart is None:
         cmnd.append(chart)
-    if verb == 'upgrade':
-        cmnd.append('--install')
+    if verb == "upgrade":
+        cmnd.append("--install")
     if create_namespace:
-        cmnd.append('--create-namespace')
+        cmnd.append("--create-namespace")
     if not repo is None:
-        cmnd.extend(['--repo', repo])
+        cmnd.extend(["--repo", repo])
     if not file is None:
-        cmnd.extend(['--values', file])
+        cmnd.extend(["--values", file])
     if not version is None:
-        cmnd.extend(['--version', version])
+        cmnd.extend(["--version", version])
     if not namespace is None:
-        cmnd.extend(['--namespace', namespace])
+        cmnd.extend(["--namespace", namespace])
     if wait:
-        cmnd.append('--wait')
+        cmnd.append("--wait")
     if not timeout is None:
-        cmnd.extend(['--timeout', timeout])
-    cmnd.extend(['--kubeconfig', kubeconfig])
+        cmnd.extend(["--timeout", timeout])
+    cmnd.extend(["--kubeconfig", kubeconfig])
 
     maxAttempts = 3
     retry = maxAttempts
@@ -179,9 +188,9 @@ def helm(verb, release, chart = None, repo = None, file = None, namespace = None
             return
         except subprocess.CalledProcessError as exc:
             output = exc.output
-            if b'Broken pipe' in output:
+            if b"Broken pipe" in output:
                 retry = retry - 1
                 logger.info("Broken pipe, retries left: %s" % retry)
             else:
                 raise Exception(output)
-    raise Exception(f'Operation failed after {maxAttempts} attempts: {output}')
+    raise Exception(f"Operation failed after {maxAttempts} attempts: {output}")
