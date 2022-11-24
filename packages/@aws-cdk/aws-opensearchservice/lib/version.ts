f2cbff674e1c5b028376f99178b76ece279f7d54
Agusti Tomas <agusti.tomas@avantir.com>
2022-11-24 20:15:23 +0100

    docs(opensearch/version): Clarify availability of 2.3

--- packages/@aws-cdk/aws-opensearchservice/lib/version.ts
+++ packages/@aws-cdk/aws-opensearchservice/lib/version.ts
@@ -71,7 +71,14 @@ export class EngineVersion {
   /** AWS OpenSearch 1.3 */
   public static readonly OPENSEARCH_1_3 = EngineVersion.openSearch('1.3');
 
-  /** AWS OpenSearch 2.3 */
+  /**
+   * AWS OpenSearch 2.3
+   *
+   * OpenSearch 2.3 is now available on Amazon OpenSearch Service across 26
+   * regions globally. Please refer to the AWS Region Table for more
+   * information about Amazon OpenSearch Service availability:
+   * https://aws.amazon.com/about-aws/global-infrastructure/regional-product-services/
+   * */
   public static readonly OPENSEARCH_2_3 = EngineVersion.openSearch('2.3');
 
   /**
