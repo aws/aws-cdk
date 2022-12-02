95c52d27ba088c5cda6c07a2f29430bc211f6d0f
Steve Houel <houes@amazon.com>
2022-12-02 08:06:01 -0800

    Adding GameSessionQueue construct

--- packages/@aws-cdk/aws-gamelift/lib/index.ts
+++ packages/@aws-cdk/aws-gamelift/lib/index.ts
@@ -6,6 +6,7 @@ export * from './game-server-group';
 export * from './ingress-rule';
 export * from './fleet-base';
 export * from './build-fleet';
+export * from './game-session-queue';
 
 // AWS::GameLift CloudFormation Resources:
 export * from './gamelift.generated';
