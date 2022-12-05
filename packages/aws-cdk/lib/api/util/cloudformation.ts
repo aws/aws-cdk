1fae95158d7631dc2f7457a64aac9faeaa8f1920
🧑🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2022-12-05 12:34:42 +0100

    fix(toolkit): endless wait if CDKToolkit stack is `REVIEW_IN_PROGRESS`
    If a `cdk bootstrap` operation is interrupted after the ChangeSet has
    been created, but before it is executed, the CDKToolkit stack will be
    left in the `REVIEW_IN_PROGRESS` state, which is a stable state until
    something or someone either executes or deletes the pending ChangeSet.
    
    This resulted in an endless (and silent, unless `cdk bootstrap` is
    executed in verbose mode) wait, as this was considered an "in progress"
    (aka unstable) state. This changes `REVIEW_IN_PROGRESS` to be treated as
    stable, while still debug-logging an indication that we are "ignoring"
    the existing ChangeSet.

--- packages/aws-cdk/lib/api/util/cloudformation.ts
+++ packages/aws-cdk/lib/api/util/cloudformation.ts
@@ -371,6 +371,14 @@ export async function stabilizeStack(cfn: CloudFormation, stackName: string) {
     if (status.isInProgress) {
       debug('Stack %s has an ongoing operation in progress and is not stable (%s)', stackName, status);
       return undefined;
+    } else if (status.isReviewInProgress) {
+      // This may happen if a stack creation operation is interrupted before the ChangeSet execution starts. Recovering
+      // from this would requiring manual intervention (deleting or executing the pending ChangeSet), abd failing to do
+      // so will result in an endless wait here (the ChangeSet wont delete or execute itself). Instead of blocking
+      // "forever" we proceed as if the stack was existing and stable. If there is a concurrent operation that just
+      // hasn't finished proceeding just yet, either this operation or the concurrent one may fail due to the other one
+      // having made progress. Which is file. I guess.
+      debug('Stack %s is in REVIEW_IN_PROGRESS state. Considering this is a stable status (%s)', stackName, status);
     }
 
     return stack;
