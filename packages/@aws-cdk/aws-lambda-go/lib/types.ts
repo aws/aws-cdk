338d563bffba2954ec51a9864d06221a12a99bb6
🧑🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2022-12-07 10:38:47 +0100

    fix example code with bad namespace

--- packages/@aws-cdk/aws-lambda-go/lib/types.ts
+++ packages/@aws-cdk/aws-lambda-go/lib/types.ts
@@ -107,10 +107,10 @@ export interface BundlingOptions {
    * use the Google proxy by default. To recreate that behavior, do the following:
    *
    * ```ts
-   * new go.GoFunction(this, 'GoFunction', {
+   * new lambda.GoFunction(this, 'GoFunction', {
    *   entry: 'app/cmd/api',
    *   bundling: {
-   *     goProxies: [go.GoFunction.GOOGLE_GOPROXY, 'direct'],
+   *     goProxies: [lambda.GoFunction.GOOGLE_GOPROXY, 'direct'],
    *   },
    * });
    * ```
