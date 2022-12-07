f8280001c5e0fc5f35f1852eeb9dc09d2186dc55
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2022-12-07 12:18:31 +0000

    feat(events): EventBus policy (#23243)
    Allow users to set the resource policy for an `EventBus`.
    
    Closes https://github.com/aws/aws-cdk/issues/23021.
    
    ----
    
    ### All Submissions:
    
    * [ ] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Construct Runtime Dependencies:
    
    * [ ] This PR adds new construct runtime dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-construct-runtime-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-events/test/integ.eventbus.ts
@@ -0,0 +1,26 @@
+import * as iam from '@aws-cdk/aws-iam';
+import { App, Stack } from '@aws-cdk/core';
+import { IntegTest } from '@aws-cdk/integ-tests';
+import { EventBus } from '../lib';
+
+const app = new App();
+const stack = new Stack(app, 'Stack', {
+  env: {
+    region: 'us-east-1',
+  },
+});
+const bus = new EventBus(stack, 'Bus');
+
+bus.addToResourcePolicy(new iam.PolicyStatement({
+  effect: iam.Effect.ALLOW,
+  principals: [new iam.AccountPrincipal(stack.account)],
+  actions: ['events:PutEvents'],
+  sid: '123',
+  resources: [bus.eventBusArn],
+}));
+
+new IntegTest(app, 'IntegTest-BatchDefaultEnvVarsStack', {
+  testCases: [stack],
+});
+
+app.synth();
