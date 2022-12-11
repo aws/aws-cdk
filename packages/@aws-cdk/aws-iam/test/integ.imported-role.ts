a6279cfaf72e7d058a7f129771402fab7ba53af8
Daniel Bartholomae <daniel@bartholomae.name>
2022-12-11 18:15:49 +0100

    test(iam): add integ test assertion

--- packages/@aws-cdk/aws-iam/test/integ.imported-role.ts
+++ packages/@aws-cdk/aws-iam/test/integ.imported-role.ts
@@ -2,6 +2,7 @@ import { App, Stack } from '@aws-cdk/core';
 import {
   IAM_IMPORTED_ROLE_STACK_SAFE_DEFAULT_POLICY_NAME,
 } from '@aws-cdk/cx-api';
+import * as integ from '@aws-cdk/integ-tests';
 import { PolicyStatement, Role, ServicePrincipal } from '../lib';
 
 const app = new App({ context: { [IAM_IMPORTED_ROLE_STACK_SAFE_DEFAULT_POLICY_NAME]: true } });
@@ -17,7 +18,15 @@ const roleInFirstStack = Role.fromRoleName(firstStack, 'Role', role.roleName);
 roleInFirstStack.addToPolicy(new PolicyStatement({ resources: ['firstQueue'], actions: ['sqs:SendMessage'] }));
 
 const secondStack = new Stack(app, 'integ-iam-imported-role-2');
+secondStack.addDependency(firstStack, 'So that this stack can be tested after both are deployed.');
+
 const roleInSecondStack = Role.fromRoleName(secondStack, 'Role', role.roleName);
 roleInSecondStack.addToPolicy(new PolicyStatement({ resources: ['secondQueue'], actions: ['sqs:SendMessage'] }));
+
+const test = new integ.IntegTest(app, 'ImportedRoleTest', {
+  testCases: [secondStack],
+});
+
+test.assertions.awsApiCall('IAM', 'listRolePolicies', { RoleName: role.roleName }).assertAtPath('PolicyNames', integ.ExpectedResult.arrayWith([integ.Match.stringLikeRegexp('.*'), integ.Match.stringLikeRegexp('.*')]));
 
 app.synth();
