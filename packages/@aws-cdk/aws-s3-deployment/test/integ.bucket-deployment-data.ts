8a4a158f59acf35bc9476f7a8923c3c45197d8f0
Cory Hall <43035978+corymhall@users.noreply.github.com>
2022-12-14 12:06:55 -0500

    feat(s3-deployment): add additional sources with `addSource` (#23321)
    PR #22857 is introducing a use case where we need to be able to add additional sources after the `BucketDeployment` resource is created.
    
    This PR adds an `addSource` method and changes all the sources evaluation within the construct to be lazy.
    
    
    ----
    
    ### All Submissions:
    
    * [ ] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Construct Runtime Dependencies:
    
    * [ ] This PR adds new construct runtime dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-construct-runtime-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-s3-deployment/test/integ.bucket-deployment-data.ts
+++ packages/@aws-cdk/aws-s3-deployment/test/integ.bucket-deployment-data.ts
@@ -10,12 +10,13 @@ const file1 = Source.data('file1.txt', 'boom');
 const file2 = Source.data('path/to/file2.txt', `bam! ${bucket.bucketName}`);
 const file3 = Source.jsonData('my/config.json', { website_url: bucket.bucketWebsiteUrl });
 
-new BucketDeployment(stack, 'DeployMeHere', {
+const deployment = new BucketDeployment(stack, 'DeployMeHere', {
   destinationBucket: bucket,
-  sources: [file1, file2, file3],
+  sources: [file1, file2],
   destinationKeyPrefix: 'deploy/here/',
   retainOnDelete: false, // default is true, which will block the integration test cleanup
 });
+deployment.addSource(file3);
 
 new CfnOutput(stack, 'BucketName', { value: bucket.bucketName });
 
