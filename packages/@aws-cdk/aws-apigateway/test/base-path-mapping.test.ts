2b94aaf82e3b603d60951a43ef17baf5c0128273
Varun Wachaspati <varunwachaspati@gmail.com>
2022-12-15 21:21:30 +0530

    Add unit tests

--- packages/@aws-cdk/aws-apigateway/test/base-path-mapping.test.ts
+++ packages/@aws-cdk/aws-apigateway/test/base-path-mapping.test.ts
@@ -53,7 +53,7 @@ describe('BasePathMapping', () => {
     });
   });
 
-  test('throw error for invalid basePath property', () => {
+  test('specify multi-level basePath property', () => {
     // GIVEN
     const stack = new cdk.Stack();
     const api = new apigw.RestApi(stack, 'MyApi');
@@ -65,7 +65,31 @@ describe('BasePathMapping', () => {
     });
 
     // WHEN
-    const invalidBasePath = '/invalid-/base-path';
+    new apigw.BasePathMapping(stack, 'MyBasePath', {
+      restApi: api,
+      domainName: domain,
+      basePath: 'api/v1/example',
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::ApiGateway::BasePathMapping', {
+      BasePath: 'api/v1/example',
+    });
+  });
+
+  test('throw error for invalid character basePath property', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const api = new apigw.RestApi(stack, 'MyApi');
+    api.root.addMethod('GET'); // api must have atleast one method.
+    const domain = new apigw.DomainName(stack, 'MyDomain', {
+      domainName: 'example.com',
+      certificate: acm.Certificate.fromCertificateArn(stack, 'cert', 'arn:aws:acm:us-east-1:1111111:certificate/11-3336f1-44483d-adc7-9cd375c5169d'),
+      endpointType: apigw.EndpointType.REGIONAL,
+    });
+
+    // WHEN
+    const invalidBasePath = 'invalid-base-path?';
 
     // THEN
     expect(() => {
@@ -75,6 +99,78 @@ describe('BasePathMapping', () => {
         basePath: invalidBasePath,
       });
     }).toThrowError(/base path may only contain/);
+  });
+
+  test('throw error for basePath starting with /', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const api = new apigw.RestApi(stack, 'MyApi');
+    api.root.addMethod('GET'); // api must have atleast one method.
+    const domain = new apigw.DomainName(stack, 'MyDomain', {
+      domainName: 'example.com',
+      certificate: acm.Certificate.fromCertificateArn(stack, 'cert', 'arn:aws:acm:us-east-1:1111111:certificate/11-3336f1-44483d-adc7-9cd375c5169d'),
+      endpointType: apigw.EndpointType.REGIONAL,
+    });
+
+    // WHEN
+    const invalidBasePath = '/invalid-base-path';
+
+    // THEN
+    expect(() => {
+      new apigw.BasePathMapping(stack, 'MyBasePath', {
+        restApi: api,
+        domainName: domain,
+        basePath: invalidBasePath,
+      });
+    }).toThrowError(/A base path cannot start or end with/);
+  });
+
+  test('throw error for basePath ending with /', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const api = new apigw.RestApi(stack, 'MyApi');
+    api.root.addMethod('GET'); // api must have atleast one method.
+    const domain = new apigw.DomainName(stack, 'MyDomain', {
+      domainName: 'example.com',
+      certificate: acm.Certificate.fromCertificateArn(stack, 'cert', 'arn:aws:acm:us-east-1:1111111:certificate/11-3336f1-44483d-adc7-9cd375c5169d'),
+      endpointType: apigw.EndpointType.REGIONAL,
+    });
+
+    // WHEN
+    const invalidBasePath = 'invalid-base-path/';
+
+    // THEN
+    expect(() => {
+      new apigw.BasePathMapping(stack, 'MyBasePath', {
+        restApi: api,
+        domainName: domain,
+        basePath: invalidBasePath,
+      });
+    }).toThrowError(/A base path cannot start or end with/);
+  });
+
+  test('throw error for basePath containing more than one consecutive /', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const api = new apigw.RestApi(stack, 'MyApi');
+    api.root.addMethod('GET'); // api must have atleast one method.
+    const domain = new apigw.DomainName(stack, 'MyDomain', {
+      domainName: 'example.com',
+      certificate: acm.Certificate.fromCertificateArn(stack, 'cert', 'arn:aws:acm:us-east-1:1111111:certificate/11-3336f1-44483d-adc7-9cd375c5169d'),
+      endpointType: apigw.EndpointType.REGIONAL,
+    });
+
+    // WHEN
+    const invalidBasePath = 'in//valid-base-path';
+
+    // THEN
+    expect(() => {
+      new apigw.BasePathMapping(stack, 'MyBasePath', {
+        restApi: api,
+        domainName: domain,
+        basePath: invalidBasePath,
+      });
+    }).toThrowError(/A base path cannot have more than one consecutive \//);
   });
 
   test('specify stage property', () => {
