abe9934ca9b46dd161c8c69d282a8a5dd7b5b08b
Carl Fürstenberg <azatoth@gmail.com>
2022-12-21 01:35:57 +0100

    fix(autoscaling): Allow adding AutoScalingGroup to multiple target groups (#23044)
    While this doesn't solve the underlying problem, which will require an API breakage in `scaleOnRequestCount()` (see
    https://github.com/aws/aws-cdk/issues/5667#issuecomment-636657482).
    
    As `scaleOnRequestCount()` seems to be the only part that is affected by this, we'll only issue a validation error if we have multiple `targetGroupArns` and `scaleOnRequestCount()` has been called.
    
    closes #5667
    
    
    ----
    
    ### All Submissions:
    
    * [x] Have you followed the guidelines in our [Contributing guide?](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md)
    
    ### Adding new Unconventional Dependencies:
    
    * [ ] This PR adds new unconventional dependencies following the process described [here](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md/#adding-new-unconventional-dependencies)
    
    ### New Features
    
    * [ ] Have you added the new feature to an [integration test](https://github.com/aws/aws-cdk/blob/main/INTEGRATION_TESTS.md)?
    	* [ ] Did you use `yarn integ` to deploy the infrastructure and generate the snapshot (i.e. `yarn integ` without `--dry-run`)?
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-autoscaling/test/integ.asg-w-elbv2.ts
+++ packages/@aws-cdk/aws-autoscaling/test/integ.asg-w-elbv2.ts
@@ -1,40 +1,97 @@
 #!/usr/bin/env node
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import { ListenerAction } from '@aws-cdk/aws-elasticloadbalancingv2';
 import * as cdk from '@aws-cdk/core';
+import * as integ from '@aws-cdk/integ-tests';
+import { Construct } from 'constructs';
 import * as autoscaling from '../lib';
 
+class ElbV2AsgStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 2,
+    });
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'Fleet', {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage(),
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true,
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    listener.addTargets('Target', {
+      port: 80,
+      targets: [asg],
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+    asg.scaleOnRequestCount('AModestLoad', {
+      targetRequestsPerSecond: 1,
+    });
+  }
+}
+
+class ElbV2AsgAtgStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 2,
+    });
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'alb', {
+      vpc,
+      internetFacing: true,
+    });
+    const listener = alb.addListener('Listener', {
+      port: 80,
+      open: true,
+    });
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'Asg', {
+      vpc,
+      instanceType: new ec2.InstanceType('t3.micro'),
+      machineImage: new ec2.AmazonLinuxImage(),
+    });
+
+    const atg1 = new elbv2.ApplicationTargetGroup(this, 'ATG1', {
+      port: 443,
+      vpc,
+    });
+    const atg2 = new elbv2.ApplicationTargetGroup(this, 'ATG2', {
+      port: 443,
+      vpc,
+    });
+
+    listener.addAction('tgs', {
+      action: ListenerAction.weightedForward([
+        { targetGroup: atg1, weight: 1 },
+        { targetGroup: atg2, weight: 1 },
+      ]),
+    });
+
+    asg.attachToApplicationTargetGroup(atg1);
+    asg.attachToApplicationTargetGroup(atg2);
+  }
+}
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'aws-cdk-asg-integ');
 
-const vpc = new ec2.Vpc(stack, 'VPC', {
-  maxAzs: 2,
+new integ.IntegTest(app, 'LambdaTest', {
+  testCases: [
+    new ElbV2AsgStack(app, 'aws-cdk-asg-integ'),
+    new ElbV2AsgAtgStack(app, 'aws-cdk-asg-atg-integ'),
+  ],
 });
-
-const asg = new autoscaling.AutoScalingGroup(stack, 'Fleet', {
-  vpc,
-  instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
-  machineImage: new ec2.AmazonLinuxImage(),
-});
-
-const lb = new elbv2.ApplicationLoadBalancer(stack, 'LB', {
-  vpc,
-  internetFacing: true,
-});
-
-const listener = lb.addListener('Listener', {
-  port: 80,
-});
-
-listener.addTargets('Target', {
-  port: 80,
-  targets: [asg],
-});
-
-listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
-
-asg.scaleOnRequestCount('AModestLoad', {
-  targetRequestsPerSecond: 1,
-});
-
-app.synth();
