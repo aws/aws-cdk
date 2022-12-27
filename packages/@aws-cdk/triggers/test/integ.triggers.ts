eeea19114bdf53e6a37684e147c11d06b0001a50
Derk Schooltink <derk.schooltink@sqills.com>
2022-12-27 14:17:57 +0100

    1. Expand success check to incorporate all 2xx-3xx response codes 2. Upgrade aws-sdk to allow for httpOptions#timeout property

--- packages/@aws-cdk/triggers/test/integ.triggers.ts
+++ packages/@aws-cdk/triggers/test/integ.triggers.ts
@@ -1,8 +1,8 @@
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as sns from '@aws-cdk/aws-sns';
-import { App, Stack } from '@aws-cdk/core';
-import * as integ from '@aws-cdk/integ-tests';
+import { App, Duration, Stack } from '@aws-cdk/core';
 import * as triggers from '../lib';
+import { InvocationType } from '../lib';
 
 const app = new App();
 const stack = new Stack(app, 'MyStack');
@@ -10,13 +10,28 @@ const stack = new Stack(app, 'MyStack');
 const topic1 = new sns.Topic(stack, 'Topic1');
 const topic2 = new sns.Topic(stack, 'Topic2');
 
-const trigger = new triggers.TriggerFunction(stack, 'MyFunction', {
-  runtime: lambda.Runtime.NODEJS_14_X,
+const triggerFunction = new triggers.TriggerFunction(stack, 'MyTriggerFunction', {
+  runtime: lambda.Runtime.NODEJS_16_X,
   handler: 'index.handler',
   code: lambda.Code.fromInline('exports.handler = function() { console.log("hi"); };'),
   executeBefore: [topic1],
 });
 
+const func = new lambda.Function(stack, 'MyLambdaFunction', {
+  runtime: lambda.Runtime.NODEJS_16_X,
+  handler: 'index.handler',
+  timeout: Duration.minutes(15),
+  code: lambda.Code.fromInline('exports.handler = async function() { await setTimeout(() => {console.log("hi")}, 3*60*1000); };'),
+});
+
+const trigger = new triggers.Trigger(stack, 'MyTrigger', {
+  handler: func,
+  invocationType: InvocationType.EVENT,
+  timeout: Duration.minutes(1),
+  executeAfter: [topic1],
+});
+
+triggerFunction.executeAfter(topic2);
 trigger.executeAfter(topic2);
 
 new triggers.TriggerFunction(stack, 'MySecondFunction', {
@@ -25,8 +40,8 @@ new triggers.TriggerFunction(stack, 'MySecondFunction', {
   code: lambda.Code.fromInline('exports.handler = function() { console.log("hello"); };'),
 });
 
-new integ.IntegTest(app, 'TriggerTest', {
-  testCases: [stack],
+new triggers.Trigger(stack, 'MyDefaultPropTrigger', {
+  handler: func,
 });
 
 app.synth();
