df55a0af7b7457df7c7df8dd1f6f70c3bf40629d
Calvin Combs <comcalvi@amazon.com>
2023-01-03 10:52:25 -0800

    fixed bad test

--- packages/aws-cdk/test/api/deploy-stack.test.ts
+++ packages/aws-cdk/test/api/deploy-stack.test.ts
@@ -100,25 +100,26 @@ test("calls tryHotswapDeployment() if 'hotswap' is `HotswapMode.CLASSIC`", async
   expect(sdk.appendCustomUserAgent).toHaveBeenCalledWith('cdk-hotswap/fallback');
 });
 
-test("foo foo calls tryHotswapDeployment() if 'hotswap' is `HotswapMode.HOTSWAP_ONLY`", async () => {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  //const hotswapDeployments = require('../../lib/api/hotswap-deployments');
-  //jest.spyOn(hotswapDeployments, tryHotswapDeployment).mockReturnValue({blah: 'foo'});
-
-  /*
-  jest.mock('../../lib/api/hotswap-deployments', () => {
-    const original = jest.requireActual('../../lib/api/hotswap-deployments');
-    return {
-      ...original,
-      tryHotswapDeployment: jest.fn(() => { throw new Error('wow this is ugly to write...'); }),
-    };
-  });
-  */
+test("calls tryHotswapDeployment() if 'hotswap' is `HotswapMode.HOTSWAP_ONLY`", async () => {
+  cfnMocks.describeStacks = jest.fn()
+    // we need the first call to return something in the Stacks prop,
+    // otherwise the access to `stackId` will fail
+    .mockImplementation(() => ({
+      Stacks: [
+        {
+          StackStatus: 'CREATE_COMPLETE',
+          StackStatusReason: 'It is magic',
+          EnableTerminationProtection: false,
+        },
+      ],
+    }));
+  sdk.stubCloudFormation(cfnMocks as any);
   // WHEN
   const deployStackResult = await deployStack({
     ...standardDeployStackArguments(),
     hotswap: HotswapMode.HOTSWAP_ONLY,
     extraUserAgent: 'extra-user-agent',
+    force: true, // otherwise, deployment would be skipped
   });
 
   // THEN
