5ea37fc19b8a3e21e2df6528f80dc63bb2e3c3a2
Momo Kornher <kornherm@amazon.co.uk>
2023-01-05 14:38:54 +0100

    feat(integ-runner): example language integ tests should do something

--- packages/@aws-cdk/integ-runner/test/language-tests/integ.typescript-test.ts
+++ packages/@aws-cdk/integ-runner/test/language-tests/integ.typescript-test.ts
@@ -1,9 +1,36 @@
-import { App, Stack } from '@aws-cdk/core';
-import { IntegTest } from '@aws-cdk/integ-tests';
+/**
+ * This test cannot use service modules since it would introduce a circular dependency
+ * Using CfnResource etc. is a workaround to still test something useful
+ */
+import * as cdk from "@aws-cdk/core";
+import { ExpectedResult, IntegTest } from "@aws-cdk/integ-tests";
 
-const app = new App();
-const stack = new Stack(app);
+const app = new cdk.App();
 
-new IntegTest(app, 'Integ', { testCases: [stack] });
+const stack = new cdk.Stack(app, "TypeScriptStack");
+const queue = new cdk.CfnResource(stack, "Queue", {
+  type: "AWS::SQS::Queue",
+  properties: {
+    FifoQueue: true
+  }
+});
+
+const assertionStack = new cdk.Stack(app, "TypeScriptAssertions");
+assertionStack.addDependency(stack);
+
+const integ = new IntegTest(app, "TypeScript", {
+  testCases: [stack],
+  assertionStack,
+});
+
+integ.assertions
+  .awsApiCall("SQS", "getQueueAttributes", {
+    QueueUrl: stack.exportValue(queue.getAtt("QueueUrl", cdk.ResolutionTypeHint.STRING)),
+    AttributeNames: ["QueueArn"],
+  })
+  .assertAtPath(
+    "Attributes.QueueArn",
+    ExpectedResult.stringLikeRegexp(".*\\.fifo$")
+  );
 
 app.synth();
--- packages/@aws-cdk/integ-runner/test/language-tests/integ_python-test.py
+++ packages/@aws-cdk/integ-runner/test/language-tests/integ_python-test.py
@@ -1,9 +1,17 @@
 import aws_cdk as cdk
-from aws_cdk.integ_tests_alpha import IntegTest
+import aws_cdk.aws_sqs as sqs
+from aws_cdk.integ_tests_alpha import IntegTest, ExpectedResult
 
 app = cdk.App()
-stack = cdk.Stack(app)
+stack = cdk.Stack(app, 'PythonStack')
 
-IntegTest(app, "Integ", test_cases=[stack])
+queue = sqs.Queue(stack, 'Queue', fifo=True)
+
+integ = IntegTest(app, "Python", test_cases=[stack])
+
+integ.assertions.aws_api_call('SQS', 'getQueueAttributes', {
+    "QueueUrl": queue.queue_url,
+    "AttributeNames": ["QueueArn"]
+}).assert_at_path('Attributes.QueueArn', ExpectedResult.string_like_regexp(".*\\.fifo$"))
 
 app.synth()
