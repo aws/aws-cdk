6a39e0abecc1dd7c2a4e1bde10c38136f41e1ec9
Momo Kornher <kornherm@amazon.co.uk>
2023-01-05 14:38:54 +0100

    fixup do useful integ tests

--- packages/@aws-cdk/integ-runner/test/language-tests/integ.typescript-test.ts
+++ packages/@aws-cdk/integ-runner/test/language-tests/integ.typescript-test.ts
@@ -1,9 +1,24 @@
-import { App, Stack } from '@aws-cdk/core';
-import { IntegTest } from '@aws-cdk/integ-tests';
+import { App, Stack } from "@aws-cdk/core";
+import { ExpectedResult, IntegTest } from "@aws-cdk/integ-tests";
+import * as sqs from "@aws-cdk/aws-sqs";
 
 const app = new App();
-const stack = new Stack(app);
+const stack = new Stack(app, "TypeScriptStack");
 
-new IntegTest(app, 'Integ', { testCases: [stack] });
+const queue = new sqs.Queue(stack, "Queue", {
+  fifo: true,
+});
+
+const integ = new IntegTest(app, "TypeScript", { testCases: [stack] });
+
+integ.assertions
+  .awsApiCall("SQS", "getQueueAttributes", {
+    QueueUrl: queue.queueUrl,
+    AttributeNames: ["QueueArn"],
+  })
+  .assertAtPath(
+    "Attributes.QueueArn",
+    ExpectedResult.stringLikeRegexp(".*\\.fifo$")
+  );
 
 app.synth();
