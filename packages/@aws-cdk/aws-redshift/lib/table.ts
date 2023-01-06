8669223784b5832b80b36d05cf3d9467ebb94735
Rizbir Khan <rizbir.khan@cgi.com>
2023-01-06 12:22:36 +0000

    addition: assigning enums to string variables

--- packages/@aws-cdk/aws-redshift/lib/table.ts
+++ packages/@aws-cdk/aws-redshift/lib/table.ts
@@ -361,28 +361,28 @@ export enum ColumnEncoding {
    * Amazon Redshift assigns an optimal encoding based on the column data.
    * This is the default.
    */
-  AUTO,
+  AUTO = 'AUTO',
 
   /**
    * The column is not compressed.
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_Raw_encoding.html
    */
-  RAW,
+  RAW = 'RAW',
 
   /**
    * The column is compressed using the AZ64 algorithm.
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/az64-encoding.html
    */
-  AZ64,
+  AZ64 = 'AZ64',
 
   /**
    * The column is compressed using a separate dictionary for each block column value on disk.
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_Byte_dictionary_encoding.html
    */
-  BYTEDICT,
+  BYTEDICT = 'BYTEDICT',
 
   /**
    * The column is compressed based on the difference between values in the column.
@@ -390,7 +390,7 @@ export enum ColumnEncoding {
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_Delta_encoding.html
    */
-  DELTA,
+  DELTA = 'DELTA',
 
   /**
    * The column is compressed based on the difference between values in the column.
@@ -398,14 +398,14 @@ export enum ColumnEncoding {
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_Delta_encoding.html
    */
-  DELTA32K,
+  DELTA32K = 'DELTA32K',
 
   /**
    * The column is compressed using the LZO algorithm.
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/lzo-encoding.html
    */
-  LZO,
+  LZO = 'LZO',
 
   /**
    * The column is compressed to a smaller storage size than the original data type.
@@ -413,7 +413,7 @@ export enum ColumnEncoding {
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_MostlyN_encoding.html
    */
-  MOSTLY8,
+  MOSTLY8 = 'MOSTLY8',
 
   /**
    * The column is compressed to a smaller storage size than the original data type.
@@ -421,7 +421,7 @@ export enum ColumnEncoding {
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_MostlyN_encoding.html
    */
-  MOSTLY16,
+  MOSTLY16 = 'MOSTLY16',
 
   /**
    * The column is compressed to a smaller storage size than the original data type.
@@ -429,33 +429,33 @@ export enum ColumnEncoding {
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_MostlyN_encoding.html
    */
-  MOSTLY32,
+  MOSTLY32 = 'MOSTLY32',
 
   /**
    * The column is compressed by recording the number of occurrences of each value in the column.
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_Runlength_encoding.html
    */
-  RUNLENGTH,
+  RUNLENGTH = 'RUNLENGTH',
 
   /**
    * The column is compressed by recording the first 245 unique words and then using a 1-byte index to represent each word.
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_Text255_encoding.html
    */
-  TEXT255,
+  TEXT255 = 'TEXT255',
 
   /**
    * The column is compressed by recording the first 32K unique words and then using a 2-byte index to represent each word.
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/c_Text255_encoding.html
    */
-  TEXT32K,
+  TEXT32K = 'TEXT32K',
 
   /**
    * The column is compressed using the ZSTD algorithm.
    *
    * @see https://docs.aws.amazon.com/redshift/latest/dg/zstd-encoding.html
    */
-  ZSTD,
+  ZSTD = 'ZSTD',
 }
