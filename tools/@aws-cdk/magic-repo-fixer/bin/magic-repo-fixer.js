79e5e46ffcdcbec6cd43eff5d1cda321248befe6
Mitchell Valine <mitchellvaline@gmail.com>
2023-01-06 14:06:57 -0800

    rename magic-repo-fixer to remodel

--- tools/@aws-cdk/magic-repo-fixer/bin/magic-repo-fixer.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cli_1 = require("../lib/cli");
-(0, cli_1.main)().catch(console.error);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFnaWMtcmVwby1maXhlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm1hZ2ljLXJlcG8tZml4ZXIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxvQ0FBa0M7QUFFbEMsSUFBQSxVQUFJLEdBQUUsQ0FBQyxLQUFLLENBQUMsT0FBTyxDQUFDLEtBQUssQ0FBQyxDQUFDIn0=
