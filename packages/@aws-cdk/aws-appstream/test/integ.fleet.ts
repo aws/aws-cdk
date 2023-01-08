4e144584bb7ee72d04fd314466bc7aba4038b073
Jacco Kulman <jaccokulman@binx.io>
2023-01-08 11:57:57 +0100

    fix linting issues

--- packages/@aws-cdk/aws-appstream/test/integ.fleet.ts
+++ packages/@aws-cdk/aws-appstream/test/integ.fleet.ts
@@ -5,7 +5,7 @@ import * as appstream from '../lib';
 /*
  * Before this test can run you need to visit the AppStream console once and click fleets.
  * This will create the necessary service role.
- * 
+ *
  * Stack verification steps:
  * * aws appstream describe-fleets --region us-east-1
  * The command should return a json with an array Fleets containing one with a Name MyFleet and a State of RUNNING.
@@ -17,16 +17,16 @@ class TestStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const fleet = new appstream.CfnFleet(this, "MyFleet", {
-      instanceType: "stream.standard.small",
-      name: "MyFleet",
+    const fleet = new appstream.CfnFleet(this, 'MyFleet', {
+      instanceType: 'stream.standard.small',
+      name: 'MyFleet',
       computeCapacity: {
         desiredInstances: 1,
       },
-      imageName: "AppStream-AmazonLinux2-09-21-2022",
+      imageName: 'AppStream-AmazonLinux2-09-21-2022',
     });
     fleet.cfnOptions.creationPolicy = {
-        startFleet: true
+      startFleet: true,
     };
     // the following code is commented because the AmazonAppStreamServiceAccess might already exist in the account
     // const role = new iam.Role(this, "AmazonAppStreamServiceAccess", {
