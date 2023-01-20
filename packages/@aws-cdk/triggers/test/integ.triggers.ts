25d99e227401ace072be4de86a71509a04b4d39b
Calvin Combs <66279577+comcalvi@users.noreply.github.com>
2023-01-19 17:56:23 -0800

    fix(lambda): lambda functions that use triggers error when invoked (#23728)
    Reverts #23062.
    
    #23062 introduced #23407, which causes lambda functions that use triggers to fail to invoke with either this error
    
    ```
    submit response to cloudformation {
      Status: 'FAILED',
      Reason: `TypeError [ERR_INVALID_ARG_TYPE]: The "msecs" argument must be of type number. Received type string ('120000')\n` +
        '    at new NodeError (internal/errors.js:322:7)\n' +
        '    at validateNumber (internal/validators.js:129:11)\n' +
        '    at getTimerDuration (internal/timers.js:384:3)\n' +
        '    at ClientRequest.setTimeout (_http_client.js:865:11)\n' +
        '    at features.constructor.handleRequest (/var/runtime/node_modules/aws-sdk/lib/http/node.js:82:12)\n' +
        '    at executeSend (/var/runtime/node_modules/aws-sdk/lib/event_listeners.js:370:29)\n' +
        '    at Request.SEND (/var/runtime/node_modules/aws-sdk/lib/event_listeners.js:384:9)\n' +
        '    at Request.callListeners (/var/runtime/node_modules/aws-sdk/lib/sequential_executor.js:102:18)\n' +
        '    at Request.emit (/var/runtime/node_modules/aws-sdk/lib/sequential_executor.js:78:10)\n' +
        '    at Request.emit (/var/runtime/node_modules/aws-sdk/lib/request.js:686:14)',
      StackId: 'arn:aws:cloudformation:us-east-1:***:stack/TestStack/83f77790-806c-11ed-8956-0a55d38b49ed',
      RequestId: '86a74312-347d-40c4-873a-09eed5b8eddd',
      PhysicalResourceId: 'AWSCDK::CustomResourceProviderFramework::CREATE_FAILED',
      LogicalResourceId: 'Trigger',
      NoEcho: undefined,
      Data: undefined
    }
    ```
    
    or
    
    ```
    Error: Trigger handler failed with status code 202 at handler (/var/task/index.js:53:15) at processTicksAndRejections (internal/process/task_queues.js:95:5) at async Runtime.handler (/var/task/__entrypoint__.js:32:24) (RequestId: ab252a7a-e06b-4fcf-b2b4-f59b2dd88734)
    ```
    
    Reverting for now, since people are unable to upgrade. 
    
    To unblock this revert, I'm disabling the integration test `dependencies-pnpm`, because when running it locally (and in this PR build) the logical ID of the version changes every run. This has been reproduced by others locally, so it's being disabled until we can resolve that issue.
    
    Fixes #23407

--- packages/@aws-cdk/triggers/test/integ.triggers.ts
+++ packages/@aws-cdk/triggers/test/integ.triggers.ts
@@ -1,8 +1,8 @@
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as sns from '@aws-cdk/aws-sns';
-import { App, Duration, Stack } from '@aws-cdk/core';
+import { App, Stack } from '@aws-cdk/core';
+import * as integ from '@aws-cdk/integ-tests';
 import * as triggers from '../lib';
-import { InvocationType } from '../lib';
 
 const app = new App();
 const stack = new Stack(app, 'MyStack');
@@ -10,34 +10,23 @@ const stack = new Stack(app, 'MyStack');
 const topic1 = new sns.Topic(stack, 'Topic1');
 const topic2 = new sns.Topic(stack, 'Topic2');
 
-const triggerFunction = new triggers.TriggerFunction(stack, 'MyTriggerFunction', {
-  runtime: lambda.Runtime.NODEJS_16_X,
+const trigger = new triggers.TriggerFunction(stack, 'MyFunction', {
+  runtime: lambda.Runtime.NODEJS_14_X,
   handler: 'index.handler',
   code: lambda.Code.fromInline('exports.handler = function() { console.log("hi"); };'),
   executeBefore: [topic1],
 });
 
-const func = new lambda.Function(stack, 'MyLambdaFunction', {
-  runtime: lambda.Runtime.NODEJS_16_X,
-  handler: 'index.handler',
-  timeout: Duration.minutes(15),
-  code: lambda.Code.fromInline('exports.handler = function() { await setTimeout(3*60*1000, "hi"); };'),
-});
-
-const trigger = new triggers.Trigger(stack, 'MyTrigger', {
-  handler: func,
-  invocationType: InvocationType.EVENT,
-  timeout: Duration.minutes(1),
-  executeAfter: [topic1],
-});
-
-triggerFunction.executeAfter(topic2);
 trigger.executeAfter(topic2);
 
 new triggers.TriggerFunction(stack, 'MySecondFunction', {
   runtime: lambda.Runtime.NODEJS_16_X,
   handler: 'index.handler',
   code: lambda.Code.fromInline('exports.handler = function() { console.log("hello"); };'),
+});
+
+new integ.IntegTest(app, 'TriggerTest', {
+  testCases: [stack],
 });
 
 app.synth();
