7be1cc521e83017b1d4dcb4b3678263ce8d6b5bc
Andreas Sieferlinger <webratz@users.noreply.github.com>
2023-01-25 13:25:38 +0100

    Update packages/@aws-cdk/core/test/docker-stub-cp.sh
    Co-authored-by: Momo Kornher <mail@moritzkornher.de>

--- packages/@aws-cdk/core/test/docker-stub-cp.sh
+++ packages/@aws-cdk/core/test/docker-stub-cp.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 # stub for the `docker` executable. it is used as CDK_DOCKER when executing unit
 # tests in `test.staging.ts` This variant is specific for tests that use the docker copy method for files, instead of bind mounts
 
-echo "$@" >> /tmp/docker-stub.input.concat
-echo "$@" > /tmp/docker-stub.input
+echo "$@" >> /tmp/docker-stub-cp.input.concat
+echo "$@" > /tmp/docker-stub-cp.input
 
 # create a fake zip to emulate created files, fetch the target path from the "docker cp" command
 if echo "$@" | grep "cp"| grep "/asset-output"; then
