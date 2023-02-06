99ee382c729a8df6e0559f5bd7b62613c5ef4b78
Calvin Combs <comcalvi@amazon.com>
2023-02-06 10:52:31 -0800

    docstring

--- packages/aws-cdk/lib/api/deploy-stack.ts
+++ packages/aws-cdk/lib/api/deploy-stack.ts
@@ -175,7 +175,7 @@ export interface DeployStackOptions {
    * A 'hotswap' deployment will attempt to short-circuit CloudFormation
    * and update the affected resources like Lambda functions directly.
    *
-   * @default - undefined for regular deployments, `HotswapMode.CLASSIC` for 'watch' deployments
+   * @default - undefined for regular deployments, `HotswapMode.HOTSWA_ONLY` for 'watch' deployments
    */
   readonly hotswap?: HotswapMode;
 
