cbebe667f882c66ea764d2391d2c75c28daaa89d
Mitchell Valine <mitchellvaline@gmail.com>
2023-02-09 13:39:31 -0800

    fix build

--- tools/@aws-cdk/remodel/lib/cli.ts
+++ tools/@aws-cdk/remodel/lib/cli.ts
@@ -1,11 +1,19 @@
-import yargs from 'yargs/yargs';
-import * as path from 'path';
-import * as fs from 'fs-extra';
 import * as cp from 'child_process';
-import { findLibrariesToPackage, verifyDependencies, copySubmoduleExports, transformPackage } from '@aws-cdk/ubergen';
-import type { Config, Export, LibraryReference, PackageJson as ubgPkgJson } from '@aws-cdk/ubergen';
+import * as path from 'path';
+import {
+  findLibrariesToPackage,
+  verifyDependencies,
+  copySubmoduleExports,
+  transformPackage,
+  Config,
+  Export,
+  LibraryReference,
+  PackageJson as UbgPkgJson,
+} from '@aws-cdk/ubergen';
+import * as fs from 'fs-extra';
+import yargs from 'yargs/yargs';
 
-interface PackageJson extends ubgPkgJson {
+interface PackageJson extends UbgPkgJson {
   readonly scripts: { [key: string]: string };
 }
 
@@ -15,7 +23,7 @@ const exec = (cmd: string, opts?: cp.ExecOptions) => new Promise((ok, ko) => {
       return ko(err);
     }
 
-    return ok({stdout, stderr});
+    return ok({ stdout, stderr });
   });
 
   proc.stdout?.pipe(process.stdout);
@@ -46,34 +54,33 @@ export async function main() {
         .option('tmp-dir', {
           type: 'string',
           desc: 'temporary intermediate directory, removed unless --no-clean is specified',
-        })
-    )
-    .argv;
+        }),
+    ).argv;
 
-    const { 'tmp-dir': tmpDir, REPO_ROOT: repoRoot, clean } = args;
+  const { 'tmp-dir': tmpDir, REPO_ROOT: repoRoot, clean } = args;
 
-    const targetDir = path.resolve(tmpDir ?? await fs.mkdtemp('remodel-'));
+  const targetDir = path.resolve(tmpDir ?? await fs.mkdtemp('remodel-'));
 
-    if (fs.existsSync(targetDir)){
-      await fs.remove(targetDir);
-    }
-    await fs.mkdir(targetDir);
+  if (fs.existsSync(targetDir)) {
+    await fs.remove(targetDir);
+  }
+  await fs.mkdir(targetDir);
 
-    // Clone all source files from the current repo to our new working
-    // directory. The entire copy including the .git directory ensures git can
-    // be aware of all source file moves if needed via `git move`.
-    await exec(`git clone ${repoRoot} ${targetDir}`);
+  // Clone all source files from the current repo to our new working
+  // directory. The entire copy including the .git directory ensures git can
+  // be aware of all source file moves if needed via `git move`.
+  await exec(`git clone ${repoRoot} ${targetDir}`);
 
-    const templateDir = path.join(__dirname, '..', 'lib', 'template');
+  const templateDir = path.join(__dirname, '..', 'lib', 'template');
 
-    await copyTemplateFiles(templateDir, targetDir);
-    await makeAwsCdkLib(targetDir);
+  await copyTemplateFiles(templateDir, targetDir);
+  await makeAwsCdkLib(targetDir);
 
-    // await runBuild(targetDir);
+  // await runBuild(targetDir);
 
-    if (clean) {
-      await fs.remove(path.resolve(targetDir));
-    }
+  if (clean) {
+    await fs.remove(path.resolve(targetDir));
+  }
 }
 
 async function copyTemplateFiles(src: string, target: string) {
@@ -83,22 +90,15 @@ async function copyTemplateFiles(src: string, target: string) {
 async function makeAwsCdkLib(target: string) {
   const awsCdkLibDir = path.join(target, 'packages', 'aws-cdk-lib');
   const pkgJsonPath = path.join(awsCdkLibDir, 'package.json');
-  const libRoot = path.join(awsCdkLibDir, 'lib');
+  const libRoot = awsCdkLibDir;
+  // const libRoot = path.join(awsCdkLibDir, 'lib');
   const pkgJson: PackageJson = await fs.readJson(pkgJsonPath);
 
-  // Update lib root for ubergen so we can nest in 'lib'
-  const newPkgJson = {
-    ...pkgJson,
-    ubergen: {
-      ...pkgJson.ubergen,
-      libRoot,
-    }
-  };
-  const pkgJsonExports = newPkgJson.exports ?? {};
-  await fs.writeFile(pkgJsonPath, JSON.stringify(newPkgJson, null, 2));
+  const pkgJsonExports = pkgJson.exports ?? {};
 
 
-  const excludeBundled = [
+  // Local packages that remain unbundled as dev dependencies
+  const localDevDeps = [
     'cdk-build-tools',
     'pkglint',
     'ubergen',
@@ -108,14 +108,27 @@ async function makeAwsCdkLib(target: string) {
     monoPackageRoot: awsCdkLibDir,
     rootPath: target,
     uberPackageJsonPath: pkgJsonPath,
-    excludedPackages: ['@aws-cdk/example-construct-library', ...excludeBundled],
+    excludedPackages: ['@aws-cdk/example-construct-library', ...localDevDeps],
   };
 
-  const devDependencies = newPkgJson?.devDependencies ?? {};
-  const packagesToBundle = await findLibrariesToPackage(newPkgJson, ubgConfig);
-  const deprecatedPackages = await findDeprecatedPackages(newPkgJson, ubgConfig);
-    
-  await verifyDependencies(newPkgJson, packagesToBundle, ubgConfig);
+  const devDependencies = pkgJson?.devDependencies ?? {};
+  const allPackages = await findAllPackages(ubgConfig);
+  const deprecatedPackages = await getDeprecatedPackages(allPackages, ubgConfig);
+  const experimentalPackages = await getExperimentalPackages(allPackages);
+  const deprecatedPackagesName = getPackageNames(deprecatedPackages);
+  const experimentalPackagesName = getPackageNames(experimentalPackages);
+
+  // Get all packages to bundle excluding deprecated and experimental ones
+  const packagesToBundle = await findLibrariesToPackage(pkgJson, {
+    ...ubgConfig,
+    excludedPackages: [
+      ...ubgConfig.excludedPackages,
+      ...deprecatedPackagesName,
+      ...experimentalPackagesName,
+    ],
+  });
+
+  await verifyDependencies(pkgJson, packagesToBundle, ubgConfig);
 
   const indexStatements = new Array<string>();
   const lazyExports = new Array<string>();
@@ -127,22 +140,8 @@ async function makeAwsCdkLib(target: string) {
 
     // May already exist as a result of codegen
     await createIfNoDir(libDir);
-
-    // Only copy stable modules source code, L1s are built during codegen
-    if (library.packageJson.stability !== 'experimental') {
-      await transformPackage(library, newPkgJson, libDir, packagesToBundle, ubgConfig.monoPackageRoot);
-    }
-
-    if (library.shortName === 'core') {
-      indexStatements.push(`export * from './${library.shortName}';`);
-      lazyExports.unshift(`export * from './${library.shortName}';`);
-    } else {
-      const exportName = library.shortName.replace(/-/g, '_');
-
-      indexStatements.push(`export * as ${exportName} from './${library.shortName}';`);
-      lazyExports.push(`Object.defineProperty(exports, '${exportName}', { get: function () { return require('./${library.shortName}'); } });`);
-    }
-    copySubmoduleExports(pkgJsonExports, library, library.shortName);
+    await transformPackage(library, pkgJson, libDir, packagesToBundle, ubgConfig.monoPackageRoot);
+    copyToExports(library, indexStatements, lazyExports, pkgJsonExports);
   }
 
   // make the exports.ts file pass linting
@@ -153,7 +152,6 @@ async function makeAwsCdkLib(target: string) {
 
   // Filter out packages we are bundling from dev dependencies
   const packagesToBundleName = packagesToBundle.map(p => p.packageJson.name);
-  const deprecatedPackagesName = deprecatedPackages.map(p => p.packageJson.name);
 
   // Filter out all of the stuff we don't want in devDeps anymore
   const filteredDevDepsEntries = Object.entries(devDependencies)
@@ -161,12 +159,38 @@ async function makeAwsCdkLib(target: string) {
       ([p]) => !(
         packagesToBundleName.includes(p)
         || deprecatedPackagesName.includes(p)
+        || experimentalPackagesName.includes(p)
         || p === '@aws-cdk/ubergen'
-      )
+      ),
     );
   const filteredDevDeps = Object.fromEntries(filteredDevDepsEntries);
 
   const newPkgJsonExports = formatPkgJsonExports(pkgJsonExports);
+
+  // Move all source files into 'lib' to make working on package easier
+  const rootFiles = await fs.readdir(awsCdkLibDir);
+  // Don't move these to 'lib'
+  const excludeNesting = [
+    'tsconfig.json',
+    '.eslintrc.js',
+    '.gitignore',
+    '.npmignore',
+    'LICENSE',
+    'NOTICE',
+    'package.json',
+    'README.md',
+    'tsconfig.json',
+    'scripts',
+  ];
+
+  await Promise.all(rootFiles.map((file: string) => {
+    if (excludeNesting.includes(file)) {
+      return Promise.resolve();
+    }
+
+    const old = path.join(awsCdkLibDir, file);
+    return fs.move(old, path.join(awsCdkLibDir, 'lib', file));
+  }));
 
   await fs.writeFile(pkgJsonPath, JSON.stringify({
     ...pkgJson,
@@ -179,69 +203,53 @@ async function makeAwsCdkLib(target: string) {
     },
     scripts: {
       ...pkgJson.scripts,
-      'gen': 'ts-node scripts/gen.ts',
-      'build': 'yarn gen && cdk-build',
+      gen: 'ts-node scripts/gen.ts',
+      build: 'yarn gen && cdk-build',
     },
     devDependencies: {
       ...filteredDevDeps,
       '@aws-cdk/cfn2ts': '0.0.0',
     },
   }, null, 2));
-
-  const rootFiles = await fs.readdir(awsCdkLibDir);
-  await Promise.all(rootFiles.map((file: string) => {
-    if (file.endsWith('.ts')) {
-      return fs.unlink(path.join(awsCdkLibDir, file));
-    }
-
-    return Promise.resolve();
-  }));
-
-  // await cleanupPackages(packagesToBundle);
-  // await cleanupPackages(deprecatedPackages);
 }
 
-// async function cleanupPackages(libraries: readonly LibraryReference[]) {
-//   for (const library of libraries) {
-//     await fs.remove(library.root);
-//   }
-// }
+function copyToExports(library: LibraryReference, indexStatements: string[], lazyExports: string[], pkgJsonExports: Record<string, Export>) {
+  if (library.shortName === 'core') {
+    indexStatements.push(`export * from './${library.shortName}';`);
+    lazyExports.unshift(`export * from './${library.shortName}';`);
+  } else {
+    const exportName = library.shortName.replace(/-/g, '_');
+
+    indexStatements.push(`export * as ${exportName} from './${library.shortName}';`);
+    lazyExports.push(`Object.defineProperty(exports, '${exportName}', { get: function () { return require('./${library.shortName}'); } });`);
+  }
+  copySubmoduleExports(pkgJsonExports, library, library.shortName);
+}
 
 function formatPkgJsonExports(exports: Record<string, Export>): Record<string, Export> {
   const format = (str: string) => {
     const split = str.split(/.(.*)/s);
     const newVal = ['./lib', split[1]].join('');
     return newVal;
-  }
+  };
 
-  const dontFormat = ['./package.json', './.jsii', './warnings.jsii.js'];
+  const dontFormat = ['./package.json', './.jsii', './.warnings.jsii.js'];
   const entries = Object.entries(exports).map(([k, v]) => {
     if (typeof v === 'string') {
       const newValue = dontFormat.includes(v) ? v : format(v);
-      return [k, newValue]
+      return [k, newValue];
     }
 
-    const nested = Object.entries(v).map(([k, v]) => {
-      if (v) {
-        return [k, format(v)]
-      }
-
-      else return [k, v];
+    const nested = Object.entries(v).map(([nk, nv]) => {
+      if (nv) {
+        return [nk, format(nv)];
+      } else {return [nk, nv];}
     });
 
     return [k, Object.fromEntries(nested)];
   });
 
   return Object.fromEntries(entries);
-}
-
-export async function runBuild(target: string) {
-  const awsCdkLibDir = path.join(target, 'packages', 'aws-cdk-lib');
-
-  await exec('yarn install', { cwd: target });
-
-  await exec('npx lerna run build --scope aws-cdk-lib --include-dependencies', { cwd: awsCdkLibDir });
-  await exec('yarn gen', { cwd: awsCdkLibDir });
 }
 
 async function findAllPackages(config: Config): Promise<LibraryReference[]> {
@@ -255,23 +263,29 @@ async function findAllPackages(config: Config): Promise<LibraryReference[]> {
         packageJson,
         root: path.join(librariesRoot, dir),
         shortName: packageJson.name.slice('@aws-cdk/'.length),
-      }
-    })
+      };
+    }),
   );
 }
 
-async function findDeprecatedPackages(pkgJson: PackageJson, config: Config): Promise<LibraryReference[]> {
+async function getDeprecatedPackages(pkgs: LibraryReference[], config: Config) {
+  const pkgJson: PackageJson = await fs.readJson(config.uberPackageJsonPath);
   const deprecatedPackages = pkgJson.ubergen?.deprecatedPackages;
-  const allPackages = await findAllPackages(config);
-
-  return allPackages.filter(p => {
-    if (config.excludedPackages.includes(p.packageJson.name)) return false;
-    else if (
+  return pkgs.filter(p => {
+    if (
       deprecatedPackages
-      && deprecatedPackages.some(packageName => packageName === p.packageJson.name)
+      && deprecatedPackages.some((packageName: string) => packageName === p.packageJson.name)
     ) return true;
     return p.packageJson.deprecated;
   });
+}
+
+function getExperimentalPackages(pkgs: LibraryReference[]) {
+  return pkgs.filter(p => p.packageJson.stability === 'experimental');
+}
+
+function getPackageNames(pkgs: LibraryReference[]) {
+  return pkgs.map(p => p.packageJson.name);
 }
 
 async function createIfNoDir(dir: string) {
