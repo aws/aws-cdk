be60dca4e8784ca8ab0f40d1533befaeeb6efb24
Madeline Kusters <80541297+madeline-k@users.noreply.github.com>
2023-02-09 07:48:19 -0800

    wip

--- tools/@aws-cdk/remodel/lib/template/packages/aws-cdk-lib/scripts/gen.ts
+++ tools/@aws-cdk/remodel/lib/template/packages/aws-cdk-lib/scripts/gen.ts
@@ -1,35 +1,42 @@
 import { generateAll } from '@aws-cdk/cfn2ts';
+import { ModuleDefinition } from '@aws-cdk/pkglint';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
-const srcDir = path.join(__dirname, '..', 'lib');
-const modulesGenerated = await generateAll(srcDir, {
-  coreImport: '../../core',
-});
-
 const awsCdkLibDir = path.join(__dirname, '..');
-
-const pkgJsonPath = path.join(awsCdkLibDir, 'package.json');
-const pkgJson = fs.readJsonSync(pkgJsonPath);
-modulesGenerated.forEach((module) => {
-  // Add export to the package.json if it's not there yet.
-  if (!pkgJson.exports[`./${module.moduleName}`]) {
-    pkgJson.exports[`./${module.moduleName}`] = `./lib/${module.moduleName}/index.js`;
-  }
+const srcDir = path.join(awsCdkLibDir, 'lib');
+generateAll(srcDir, {
+  coreImport: '../../core',
+}).then(modulesGenerated => {
+  packageJson(modulesGenerated);
+  index(modulesGenerated);
 });
-fs.writeJsonSync(pkgJsonPath, pkgJson, { spaces: 2 });
 
-const topLevelIndexFilePath = path.join(awsCdkLibDir, 'index.ts');
-const topLevelIndexFileEntries: string[] = [];
-if (fs.existsSync(topLevelIndexFilePath)) {
-  topLevelIndexFileEntries.push(...(fs.readFileSync(topLevelIndexFilePath)).toString('utf-8').split('\n'));
+function packageJson(modulesGenerated: ModuleDefinition[]) {
+  const pkgJsonPath = path.join(awsCdkLibDir, 'package.json');
+  const pkgJson = fs.readJsonSync(pkgJsonPath);
+  modulesGenerated.forEach((module) => {
+    // Add export to the package.json if it's not there yet.
+    if (!pkgJson.exports[`./${module.moduleName}`]) {
+      pkgJson.exports[`./${module.moduleName}`] = `./lib/${module.moduleName}/index.js`;
+    }
+  });
+  fs.writeJsonSync(pkgJsonPath, pkgJson, { spaces: 2 });
 }
 
-modulesGenerated.forEach((module) => {
-  // Add export to top-level index.ts if it's not there yet.
-  if (!topLevelIndexFileEntries.find(e => e.includes(module.moduleName))) {
-    topLevelIndexFileEntries.push(`export * as ${module.submoduleName} from './lib/${module.moduleName}';`);
+function index(modulesGenerated: ModuleDefinition[]) {
+  const topLevelIndexFilePath = path.join(awsCdkLibDir, 'index.ts');
+  const topLevelIndexFileEntries: string[] = [];
+  if (fs.existsSync(topLevelIndexFilePath)) {
+    topLevelIndexFileEntries.push(...(fs.readFileSync(topLevelIndexFilePath)).toString('utf-8').split('\n'));
   }
-});
 
-fs.writeFileSync(topLevelIndexFilePath, topLevelIndexFileEntries.join('\n'));
+  modulesGenerated.forEach((module) => {
+    // Add export to top-level index.ts if it's not there yet.
+    if (!topLevelIndexFileEntries.find(e => e.includes(module.moduleName))) {
+      topLevelIndexFileEntries.push(`export * as ${module.submoduleName} from './lib/${module.moduleName}';`);
+    }
+  });
+
+  fs.writeFileSync(topLevelIndexFilePath, topLevelIndexFileEntries.join('\n'));
+}
