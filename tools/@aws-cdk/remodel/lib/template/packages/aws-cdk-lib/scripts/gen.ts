1220c988e92c5b17076c575fd438cb47c121e77c
Madeline Kusters <80541297+madeline-k@users.noreply.github.com>
2023-02-09 15:32:47 -0800

    refactor

--- tools/@aws-cdk/remodel/lib/template/packages/aws-cdk-lib/scripts/gen.ts
+++ tools/@aws-cdk/remodel/lib/template/packages/aws-cdk-lib/scripts/gen.ts
@@ -5,46 +5,40 @@ import * as path from 'path';
 
 const awsCdkLibDir = path.join(__dirname, '..');
 const srcDir = path.join(awsCdkLibDir, 'lib');
+const pkgJsonPath = path.join(awsCdkLibDir, 'package.json');
+const topLevelIndexFilePath = path.join(srcDir, 'index.ts');
+
+
 
 main()
-  .then(() => process.exit(0))
-  // eslint-ignore-next-line no-console
-  .catch(console.error)
+.then(() => process.exit(0))
+// eslint-ignore-next-line no-console
+.catch(console.error)
 
 async function main() {
   const generated = await generateAll(srcDir, {
     coreImport: '../../core',
   });
 
-  addExportsToPackageJson(generated);
-  addModulesToIndexFile(generated);
+  updatePackageJsonAndIndexFiles(generated);
 }
 
-function addExportsToPackageJson(modules: ModuleDefinition[]) {
-  const pkgJsonPath = path.join(awsCdkLibDir, 'package.json');
+function updatePackageJsonAndIndexFiles(modules: ModuleDefinition[]) {
   const pkgJson = fs.readJsonSync(pkgJsonPath);
-  modules.forEach((module) => {
-    // Add export to the package.json if it's not there yet.
-    if (!pkgJson.exports[`./${module.moduleName}`]) {
-      pkgJson.exports[`./${module.moduleName}`] = `./${module.moduleName}/index.js`;
-    }
-  });
-  fs.writeJsonSync(pkgJsonPath, pkgJson, { spaces: 2 });
-}
-
-function addModulesToIndexFile(modules: ModuleDefinition[]) {
-  const topLevelIndexFilePath = path.join(srcDir, 'index.ts');
-  const topLevelIndexFileEntries: string[] = [];
+  const topLevelIndexFileEntries = new Array<string>();
   if (fs.existsSync(topLevelIndexFilePath)) {
     topLevelIndexFileEntries.push(...(fs.readFileSync(topLevelIndexFilePath)).toString('utf-8').split('\n'));
   }
 
   modules.forEach((module) => {
-    // Add export to top-level index.ts if it's not there yet.
+    if (!pkgJson.exports[`./${module.moduleName}`]) {
+      pkgJson.exports[`./${module.moduleName}`] = `./${module.moduleName}/index.js`;
+    }
     if (!topLevelIndexFileEntries.find(e => e.includes(module.moduleName))) {
       topLevelIndexFileEntries.push(`export * as ${module.submoduleName} from './${module.moduleName}';`);
-    }
+    }  
   });
 
+  fs.writeJsonSync(pkgJsonPath, pkgJson, { spaces: 2 });
   fs.writeFileSync(topLevelIndexFilePath, topLevelIndexFileEntries.join('\n'));
 }
