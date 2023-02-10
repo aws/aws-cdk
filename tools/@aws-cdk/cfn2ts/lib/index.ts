1931ef25565255034f90fcf8b521f368ca6b0f63
Madeline Kusters <80541297+madeline-k@users.noreply.github.com>
2023-02-09 16:02:25 -0800

    feat(remodel): Create or add to top-level index.ts and package.json exports during gen (#23978)
    * feat: add new modules to the package.json exports during gen step
    
    * feat(remodel): Create or add to top-level index.ts and package.json exports during gen
    
    * put package.json exports back (I removed them to test)
    
    * remove one to many newlines
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * revert package.json changes
    
    * better names
    
    * revert packageJson changes
    
    * refactor
    
    * remove lib
    
    * remove 'lib' and, remove extra interface
    
    * remove extra import
    
    * refactor
    
    * whitespace and use pkglint barrel import
    
    * use async
    
    * we still need ./lib/ for the package.json exports
    
    * remove whitespace

--- tools/@aws-cdk/cfn2ts/lib/index.ts
+++ tools/@aws-cdk/cfn2ts/lib/index.ts
@@ -40,14 +40,17 @@ export default async function(scopes: string | string[], outPath: string, option
   }
 }
 
-export async function generateAll(outPath: string, options: CodeGeneratorOptions) {
+export async function generateAll(outPath: string, options: CodeGeneratorOptions): Promise<pkglint.ModuleDefinition[]> {
   const scopes = cfnSpec.namespaces();
+  const modules = new Array<pkglint.ModuleDefinition>();
 
   for (const scope of scopes) {
     const spec = cfnSpec.filteredSpecification(s => s.startsWith(`${scope}::`));
     const module = pkglint.createModuleDefinitionFromCfnNamespace(scope);
     const packagePath = path.join(outPath, module.moduleName);
     const libPath = path.join(packagePath, 'lib');
+
+    modules.push(module);
 
     if (Object.keys(spec.ResourceTypes).length === 0) {
       throw new Error(`No resource was found for scope ${scope}`);
@@ -98,6 +101,8 @@ export async function generateAll(outPath: string, options: CodeGeneratorOptions
       await fs.writeJson(path.join(packagePath, '.jsiirc.json'), jsiirc, { spaces: 2 });
     }
   }
+
+  return modules;
 }
 
 /**
