42ab770eb314b6481547380cecd325edc9e200c7
Greg Huang <debin.huang@gmail.com>
2023-02-17 10:09:32 +0000

    change integ testing

--- packages/@aws-cdk/aws-servicecatalogappregistry/test/integ.application.ts
+++ packages/@aws-cdk/aws-servicecatalogappregistry/test/integ.application.ts
@@ -1,5 +1,6 @@
 import * as iam from '@aws-cdk/aws-iam';
 import * as cdk from '@aws-cdk/core';
+import * as integ from '@aws-cdk/integ-tests';
 import * as appreg from '../lib';
 
 const app = new cdk.App();
@@ -38,6 +39,10 @@ const myRole = new iam.Role(stack, 'MyRole', {
 });
 application.shareApplication({
   roles: [myRole],
+});
+
+new integ.IntegTest(app, 'LambdaTest', {
+  testCases: [stack],
 });
 
 app.synth();
