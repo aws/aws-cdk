ae44fc486e7d3ef6e0e049ecf31884e6a4691887
Kaizen Conroy <zen.conroy@gmail.com>
2023-03-02 15:20:37 -0500

    initial project structure

---
+++ packages/@aws-cdk/core-synthesizer/lib/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
+const core = require("@aws-cdk/core");
+function main() {
+    const a = new core.App();
+    // eslint-disable-next-line no-console
+    console.log(a.toString());
+}
+exports.main = main;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxzQ0FBc0M7QUFFdEMsU0FBZ0IsSUFBSTtJQUNsQixNQUFNLENBQUMsR0FBRyxJQUFJLElBQUksQ0FBQyxHQUFHLEVBQUUsQ0FBQztJQUN6QixzQ0FBc0M7SUFDdEMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxDQUFDLENBQUMsUUFBUSxFQUFFLENBQUMsQ0FBQztBQUM1QixDQUFDO0FBSkQsb0JBSUMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgKiBhcyBjb3JlIGZyb20gJ0Bhd3MtY2RrL2NvcmUnO1xuXG5leHBvcnQgZnVuY3Rpb24gbWFpbigpIHtcbiAgY29uc3QgYSA9IG5ldyBjb3JlLkFwcCgpO1xuICAvLyBlc2xpbnQtZGlzYWJsZS1uZXh0LWxpbmUgbm8tY29uc29sZVxuICBjb25zb2xlLmxvZyhhLnRvU3RyaW5nKCkpO1xufSJdfQ==
---
+++ packages/@aws-cdk/core-synthesizer/lib/index.ts
@@ -0,0 +1,7 @@
+import * as core from '@aws-cdk/core';
+
+export function main() {
+  const a = new core.App();
+  // eslint-disable-next-line no-console
+  console.log(a.toString());
+}
