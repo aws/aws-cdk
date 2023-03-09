1d05ba9e2fcec7329a9e7809f626a4b4bb232e88
Jung Lee <jungle@amazon.com>
2023-03-09 15:52:21 -0800

    Updated test snapshots after deploying with actual accounts, and replaced accountIds with variable

--- packages/@aws-cdk/aws-servicecatalogappregistry/test/integ.application-associator.cross-account-stack-association-enabled.ts
+++ packages/@aws-cdk/aws-servicecatalogappregistry/test/integ.application-associator.cross-account-stack-association-enabled.ts
@@ -2,8 +2,13 @@ import * as cdk from '@aws-cdk/core';
 import * as integ from '@aws-cdk/integ-tests';
 import * as appreg from '../lib';
 
+// When doing a deployment for these stacks, specify stack `env` properties and use
+// the same `account` for `localStack` and the TargetApplication stack, and a separate
+// account for `crossAccountStack`.
+// The accounts should be in the same AWS Organization with cross-account sharing enabled.
+
 const app = new cdk.App();
-const localStack = new cdk.Stack(app, 'integ-servicecatalogappregistry-first-resource');
+const localStack = new cdk.Stack(app, 'integ-servicecatalogappregistry-local-resource');
 
 new appreg.ApplicationAssociator(app, 'RegisterCdkApplication', {
   applications: [appreg.TargetApplication.createApplicationStack({
@@ -13,10 +18,10 @@ new appreg.ApplicationAssociator(app, 'RegisterCdkApplication', {
   })],
 });
 
-new cdk.Stack(app, 'integ-servicecatalogappregistry-second-resource');
+const crossAccountStack = new cdk.Stack(app, 'integ-servicecatalogappregistry-cross-account-resource');
 
 new integ.IntegTest(app, 'ApplicationAssociatorTest', {
-  testCases: [localStack],
+  testCases: [localStack, crossAccountStack],
 });
 
 app.synth();
