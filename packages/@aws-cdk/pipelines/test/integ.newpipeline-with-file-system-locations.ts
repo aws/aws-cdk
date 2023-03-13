89bed018df52f3f262b603b54cce905d99fbfdea
Lassi Raatikainen <raatikainenlassi@gmail.com>
2023-03-13 09:09:25 +0000

    fix integration test by adding vpc and privileged params to CodePipeline

--- packages/@aws-cdk/pipelines/test/integ.newpipeline-with-file-system-locations.ts
+++ packages/@aws-cdk/pipelines/test/integ.newpipeline-with-file-system-locations.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as s3_assets from '@aws-cdk/aws-s3-assets';
 import * as sqs from '@aws-cdk/aws-sqs';
 import { App, Stack, StackProps, Stage, StageProps, Aws } from '@aws-cdk/core';
@@ -11,14 +12,23 @@ class TestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 1,
+      natGateways: 1,
+    });
+
     const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
       codeBuildDefaults: {
+        vpc: vpc,
         fileSystemLocations: [codebuild.FileSystemLocation.efs({
           identifier: 'myidentifier',
           location: `fs-c8d04839.efs.${Aws.REGION}.amazonaws.com:/mnt`,
           mountOptions: 'nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2',
           mountPoint: '/media',
         })],
+        buildEnvironment: {
+          privileged: true,
+        },
       },
       synth: new pipelines.ShellStep('Synth', {
         input: pipelines.CodePipelineSource.gitHub('aws/aws-cdk', 'v2-main'),
@@ -50,7 +60,11 @@ class AppStage extends Stage {
   }
 }
 
-const app = new App();
+const app = new App({
+  context: {
+    '@aws-cdk/core:newStyleStackSynthesis': '1',
+  },
+});
 
 const stack = new TestStack(app, 'PipelinesFileSystemLocations');
 
