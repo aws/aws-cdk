99f24d8978bab0f2281f61a49b737ac54ebd565a
Rico Huijbers <rix0rrr@gmail.com>
2023-03-21 11:05:00 +0100

    chore: detect property type renames to ensure no accidental backwards breakage
    Upstream service teams may rename property types in the resource
    specification.
    
    This is strictly speaking not breaking from the point of view of the
    spec, because the names of the property types don't appear anywhere in
    the code a user would normally type (i.e., in a CloudFormation
    template).
    
    However, CDK generates classes for these types, and so the name *does*
    matter and changing it is breaking.
    
    To detect these instances, we check that during an upgrade, all old
    property type names are still present. If not, the reason is probably
    that they renamed a type.
    
    Note that this is not a 100% guaranteed to catch all scenarios (I'm sure
    you can think of changes that would be breaking and still pass this
    check), but it's at least very likely to catch honest mistakes in
    commonly expected scenarios.

--- packages/@aws-cdk/cfnspec/build-tools/update.sh
+++ packages/@aws-cdk/cfnspec/build-tools/update.sh
@@ -10,6 +10,8 @@ scriptdir=$(cd $(dirname $0) && pwd)
 
 rm -f CHANGELOG.md.new
 
+
+# update-spec <TITLE> <SOURCE> <TARGETDIR> <IS_GZIPPED> <SHOULD_SPLIT> [<SVC> [...]]
 function update-spec() {
     local title=$1
     local url=$2
@@ -67,6 +69,8 @@ update-spec \
     "${1:-https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json}" \
     spec-source/specification/000_cfn/000_official \
     true true
+
+exit 1
 
 update-spec \
     "CloudFormation Resource Specification (us-west-2)" \
