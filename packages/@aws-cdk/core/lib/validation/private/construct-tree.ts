99aba79578fedf47348d24ed982a26b5af67e33a
corymhall <43035978+corymhall@users.noreply.github.com>
2023-03-22 13:41:20 +0000

    refactoring trace cache methods

--- packages/@aws-cdk/core/lib/validation/private/construct-tree.ts
+++ packages/@aws-cdk/core/lib/validation/private/construct-tree.ts
@@ -110,17 +110,102 @@ export class ConstructTree {
   }
 
   /**
+   * Get the stack trace from the construct node metadata.
+   * The stack trace only gets recorded if the node is a `CfnResource`,
+   * but the stack trace will have entries for all types of parent construct
+   * scopes
+   */
+  private getTraceMetadata(size: number, node?: Node): string[] {
+    if (node) {
+      const construct = this.getConstructByPath(node.path);
+      if (construct) {
+        let trace;
+        if (CfnResource.isCfnResource(construct)) {
+          trace = construct.node.metadata.find(meta => !!meta.trace)?.trace ?? [];
+        } else {
+          trace = construct.node.defaultChild?.node.metadata.find(meta => !!meta.trace)?.trace ?? [];
+        }
+        // the top item is never pointing to anything relevant
+        trace.shift();
+        // take just the items we need and reverse it since we are
+        // displaying to trace bottom up
+        return Object.create(trace.slice(0, size));
+      }
+    }
+    return [];
+  }
+
+  /**
    * Get a ConstructTrace from the cache for a given construct
    *
-   * @param constructPath construct path
-   * @returns ConstructTrace if available
+   * Construct the stack trace of constructs. This will start with the
+   * root of the tree and go down to the construct that has the violation
    */
-  public getTrace(constructPath: string, trace?: ConstructTrace): ConstructTrace | undefined {
-    const constructTrace = this._traceCache.get(constructPath);
-    if (!constructTrace && trace) {
-      this._traceCache.set(constructPath, trace);
+  public getTrace(node: Node, locations?: string[]): ConstructTrace | undefined {
+    const trace = this._traceCache.get(node.path);
+    if (trace) {
+      return trace;
     }
-    return constructTrace ?? trace;
+
+    const size = this.nodeSize(node);
+
+    // the first time through the node will
+    // be the root of the tree. We need to go
+    // down the tree until we get to the bottom which
+    // will be the resource with the violation and it
+    // will contain the trace info
+    let child = node;
+    if (!locations) {
+      do {
+        if (child.children) {
+          child = this.getChild(child.children);
+        }
+      } while (child.children);
+    }
+    const metadata = (locations ?? this.getTraceMetadata(size, child));
+    const thisLocation = metadata.pop();
+
+    let constructTrace: ConstructTrace = {
+      id: node.id,
+      path: node.path,
+      // the "child" trace will be the "parent" node
+      // since we are going bottom up
+      child: node.children
+        ? this.getTrace(this.getChild(node.children), metadata)
+        : undefined,
+      construct: node.constructInfo?.fqn,
+      libraryVersion: node.constructInfo?.version,
+      location: thisLocation ?? "Run with '--debug' to include location info",
+    };
+    this._traceCache.set(constructTrace.path, constructTrace);
+    return constructTrace;
+  }
+
+  /**
+   * Each node will only have a single child so just
+   * return that
+   */
+  private getChild(children: { [key: string]: Node }): Node {
+    return Object.values(children)[0];
+  }
+
+  /**
+   * Get the size of a Node
+   */
+  private nodeSize(node: Node): number {
+    let size = 1;
+    if (!node.children) {
+      return size;
+    }
+    let children: Node | undefined = this.getChild(node.children);
+    do {
+      size++;
+      children = children.children
+        ? this.getChild(children.children)
+        : undefined;
+    } while (children);
+
+    return size;
   }
 
   /**
--- packages/@aws-cdk/core/lib/validation/private/trace.ts
+++ packages/@aws-cdk/core/lib/validation/private/trace.ts
@@ -1,6 +1,4 @@
 import { ConstructTree, ConstructTrace } from './construct-tree';
-import { CfnResource } from '../../cfn-resource';
-import { Node } from '../../private/tree-metadata';
 
 const STARTER_LINE = '└── ';
 const VERTICAL_LINE = '│';
@@ -61,105 +59,9 @@ export class ReportTrace {
     if (constructPath) {
       const treeNode = this.tree.getTreeNode(constructPath);
       if (treeNode) {
-        return this.getConstructTrace(treeNode);
+        return this.tree.getTrace(treeNode);
       }
     }
     return;
-  }
-  /**
-   * Get the stack trace from the construct node metadata.
-   * The stack trace only gets recorded if the node is a `CfnResource`,
-   * but the stack trace will have entries for all types of parent construct
-   * scopes
-   */
-  private getTraceMetadata(size: number, node?: Node): string[] {
-    if (node) {
-      const construct = this.tree.getConstructByPath(node.path);
-      if (construct) {
-        let trace;
-        if (CfnResource.isCfnResource(construct)) {
-          trace = construct.node.metadata.find(meta => !!meta.trace)?.trace ?? [];
-        } else {
-          trace = construct.node.defaultChild?.node.metadata.find(meta => !!meta.trace)?.trace ?? [];
-        }
-        // the top item is never pointing to anything relevant
-        trace.shift();
-        // take just the items we need and reverse it since we are
-        // displaying to trace bottom up
-        return Object.create(trace.slice(0, size));
-      }
-    }
-    return [];
-  }
-
-  /**
-   * Construct the stack trace of constructs. This will start with the
-   * root of the tree and go down to the construct that has the violation
-   */
-  private getConstructTrace(node: Node, locations?: string[]): ConstructTrace {
-    const trace = this.tree.getTrace(node.path);
-    if (trace) {
-      return trace;
-    }
-
-    const size = this.nodeSize(node);
-
-    // the first time through the node will
-    // be the root of the tree. We need to go
-    // down the tree until we get to the bottom which
-    // will be the resource with the violation and it
-    // will contain the trace info
-    let child = node;
-    if (!locations) {
-      do {
-        if (child.children) {
-          child = this.getChild(child.children);
-        }
-      } while (child.children);
-    }
-    const metadata = (locations ?? this.getTraceMetadata(size, child));
-    const thisLocation = metadata.pop();
-
-    let constructTrace: ConstructTrace = {
-      id: node.id,
-      path: node.path,
-      // the "child" trace will be the "parent" node
-      // since we are going bottom up
-      child: node.children
-        ? this.getConstructTrace(this.getChild(node.children), metadata)
-        : undefined,
-      construct: node.constructInfo?.fqn,
-      libraryVersion: node.constructInfo?.version,
-      location: thisLocation ?? "Run with '--debug' to include location info",
-    };
-    this.tree.getTrace(constructTrace.path, constructTrace);
-    return constructTrace;
-  }
-
-  /**
-   * Each node will only have a single child so just
-   * return that
-   */
-  private getChild(children: { [key: string]: Node }): Node {
-    return Object.values(children)[0];
-  }
-
-  /**
-   * Get the size of a Node
-   */
-  private nodeSize(node: Node): number {
-    let size = 1;
-    if (!node.children) {
-      return size;
-    }
-    let children: Node | undefined = this.getChild(node.children);
-    do {
-      size++;
-      children = children.children
-        ? this.getChild(children.children)
-        : undefined;
-    } while (children);
-
-    return size;
   }
 }
