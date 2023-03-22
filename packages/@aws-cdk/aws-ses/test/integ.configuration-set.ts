a4f70cc9a87f010ccb4e96228c3e5c96e1ec77d9
Jonathan Goldwasser <jonathan.goldwasser@gmail.com>
2023-03-22 16:37:50 +0100

    feat(ses): event destinations for configuration sets
    Add a L2 for `ConfigurationSetEventDestination`.

--- packages/@aws-cdk/aws-ses/test/integ.configuration-set.ts
+++ packages/@aws-cdk/aws-ses/test/integ.configuration-set.ts
@@ -1,13 +1,30 @@
+import * as sns from '@aws-cdk/aws-sns';
 import { App, Stack, StackProps } from '@aws-cdk/core';
 import * as integ from '@aws-cdk/integ-tests';
 import { Construct } from 'constructs';
 import * as ses from '../lib';
+import { CloudWatchDimensionSource, EventDestination } from '../lib';
 
 class TestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new ses.ConfigurationSet(this, 'ConfigurationSet');
+    const configurationSet = new ses.ConfigurationSet(this, 'ConfigurationSet');
+
+    const topic = new sns.Topic(this, 'Topic');
+
+    configurationSet.addEventDestination('Sns', {
+      destination: EventDestination.snsTopic(topic),
+    });
+
+    configurationSet.addEventDestination('CloudWatch', {
+      destination: EventDestination.cloudWatchDimensions([{
+        source: CloudWatchDimensionSource.MESSAGE_TAG,
+        name: 'ses:from-domain',
+        default: 'no_domain',
+      }]),
+    });
+
   }
 }
 
