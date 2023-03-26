4f489a118a097134a7d02bdd71d3151070bebfc2
WinterYukky <49480575+WinterYukky@users.noreply.github.com>
2023-03-26 04:32:12 +0000

    test(custom-resource): add runtime switch test

--- packages/@aws-cdk/custom-resources/lib/aws-custom-resource/runtime/index.ts
+++ packages/@aws-cdk/custom-resources/lib/aws-custom-resource/runtime/index.ts
@@ -1,6 +1,6 @@
 export { PHYSICAL_RESOURCE_ID_REFERENCE } from './shared';
 
-const runtime = process.env.AWS_EXECUTION_ENV?.split('AWS_Lambda_')[1];
+const env = process.env.AWS_EXECUTION_ENV;
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const runtimeModule = runtime && runtime >= 'nodejs18.x' ? require('./aws-sdk-v3-handler') : require('./aws-sdk-v2-handler');
-export const handler = runtimeModule.handler;
+const runtime = env && env >= 'AWS_Lambda_nodejs18.x' ? require('./aws-sdk-v3-handler') : require('./aws-sdk-v2-handler');
+export const handler = runtime.handler;
