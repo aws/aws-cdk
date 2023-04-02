b2c2b18fe687e22be446ce1caefc67ca5b23ebcf
pepito-j <pepitoj@amazon.com>
2023-04-02 08:26:39 +0000

    Updated integration tests to obfuscate AWS Account ID within the CFN template

--- packages/@aws-cdk/aws-codebuild/test/integ.project-suppress-resource-policy.ts
+++ packages/@aws-cdk/aws-codebuild/test/integ.project-suppress-resource-policy.ts
@@ -23,7 +23,12 @@ void (async () => {
   // Role must exist so that CodeBuild can assume it during stack events
   const fakeRole = new iam.Role(stack, 'FakeImportedRole', { assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com') });
   const importedRole = iam.Role.fromRoleArn(stack, 'SomeRole', `arn:aws:iam::${accountId}:role/${fakeRole.roleName}`);
-  new codebuild.PipelineProject(stack, 'BuildProjectRemovedResourcePolicy', {
+
+  const logGroup1 = new logs.LogGroup(stack, 'my-log-group-label', {
+    logGroupName: 'my-log-group-name',
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+  });
+  const projectNoResourcePolicy = new codebuild.PipelineProject(stack, 'BuildProjectRemovedResourcePolicy', {
     projectName: 'build-project-removed-resource-policy',
     role: importedRole,
     buildSpec: codebuild.BuildSpec.fromObject({
@@ -36,16 +41,17 @@ void (async () => {
     }),
     logging: {
       cloudWatch: {
-        logGroup: new logs.LogGroup(stack, 'my-log-group-label', {
-          logGroupName: 'my-log-group-name',
-          removalPolicy: cdk.RemovalPolicy.DESTROY,
-        }),
+        logGroup: logGroup1,
         suppressResourcePolicy: true,
       },
     },
   });
 
-  new codebuild.PipelineProject(stack, 'BuildProjectWithResourcePolicy', {
+  const logGroup2 = new logs.LogGroup(stack, 'my-log-group-label2', {
+    logGroupName: 'my-log-group-name2',
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+  });
+  const projectWithResourcePolicy = new codebuild.PipelineProject(stack, 'BuildProjectWithResourcePolicy', {
     projectName: 'build-project-with-resource-policy',
     role: importedRole,
     buildSpec: codebuild.BuildSpec.fromObject({
@@ -58,14 +64,28 @@ void (async () => {
     }),
     logging: {
       cloudWatch: {
-        logGroup: new logs.LogGroup(stack, 'my-log-group-label2', {
-          logGroupName: 'my-log-group-name2',
-          removalPolicy: cdk.RemovalPolicy.DESTROY,
-        }),
-        suppressResourcePolicy: false,
+        logGroup: logGroup2,
+        // suppressResourcePolicy: false,
       },
     },
   });
+  /**
+   * Obfuscating personal account ID.
+   * Generated template within snapshot hardcodes in the AWS Account ID from STS and we can't work around it with normal means
+   * (i.e. cannot just use 'stack.account' within the fromRoleArn method as this doesn't trigger the ResourcePolicy resource being added in).
+   * Instead, mimic what's being auto-created by copying what's expected into the template and then override with AWS::AccountId.
+   */
+  (projectNoResourcePolicy.node.defaultChild as codebuild.CfnProject).addPropertyOverride('ServiceRole', `arn:aws:iam::${fakeRole.principalAccount}:role/${fakeRole.roleName}`);
+  (projectWithResourcePolicy.node.defaultChild as codebuild.CfnProject).addPropertyOverride('ServiceRole', `arn:aws:iam::${fakeRole.principalAccount}:role/${fakeRole.roleName}`);
+
+  const copyPolicyDoc = cdk.Fn.join('', [
+    '{\"Statement\":[{\"Action\":[\"logs:CreateLogStream\",\"logs:PutLogEvents\"],\"Effect\":\"Allow\",\"Principal\":{\"AWS\":\"',
+    fakeRole.principalAccount!,
+    '\"},\"Resource\":\"',
+    logGroup2.logGroupArn,
+    '\"}],\"Version\":\"2012-10-17\"}',
+  ]);
+  (logGroup2.node.tryFindChild('Policy')?.node.defaultChild as logs.CfnResourcePolicy).addPropertyOverride('PolicyDocument', copyPolicyDoc);
 
   new IntegTest(app, 'AssetBuildSpecTest', { testCases: [stack] });
 
