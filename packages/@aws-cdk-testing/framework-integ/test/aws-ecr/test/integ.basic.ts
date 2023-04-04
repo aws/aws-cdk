f153006efa0ea7e01a2df5be838c96f99d919a89
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2023-04-04 12:27:31 -0700

    fix(ecr): policytext errors when includes resource (#24401)
    ECR does not allow resource to be included in private repository resource policies.
    CFN largely swallows the error message.
    Most resources require or at least allow a resource in their policies, so we should at least warn.
    
    See issue https://github.com/aws/aws-cdk/issues/24314

--- packages/@aws-cdk-testing/framework-integ/test/aws-ecr/test/integ.basic.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecr/test/integ.basic.ts
@@ -1,12 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'aws-ecr-integ-stack');
 
 const repo = new ecr.Repository(stack, 'Repo');
 repo.addLifecycleRule({ maxImageCount: 5 });
+repo.addToResourcePolicy(new iam.PolicyStatement({
+  actions: ['ecr:GetDownloadUrlForLayer'],
+  principals: [new iam.AnyPrincipal()],
+}));
 
 new cdk.CfnOutput(stack, 'RepositoryURI', {
   value: repo.repositoryUri,
--- packages/aws-cdk-lib/aws-ecr/lib/repository.ts
+++ packages/aws-cdk-lib/aws-ecr/lib/repository.ts
@@ -4,6 +4,7 @@ import * as events from '../../aws-events';
 import * as iam from '../../aws-iam';
 import * as kms from '../../aws-kms';
 import {
+  Annotations,
   ArnFormat,
   IResource,
   Lazy,
@@ -424,7 +425,7 @@ export interface OnCloudTrailImagePushedOptions extends events.OnEventOptions {
  */
 export interface OnImageScanCompletedOptions extends events.OnEventOptions {
   /**
-   * Only watch changes to the image tags spedified.
+   * Only watch changes to the image tags specified.
    * Leave it undefined to watch the full repository.
    *
    * @default - Watch the changes to the repository with all image tags
@@ -660,7 +661,17 @@ export class Repository extends RepositoryBase {
     this.node.addValidation({ validate: () => this.policyDocument?.validateForResourcePolicy() ?? [] });
   }
 
+  /**
+   * Add a policy statement to the repository's resource policy.
+   *
+   * While other resources policies in AWS either require or accept a resource section,
+   * Cfn for ECR does not allow us to specify a resource policy.
+   * It will fail if a resource section is present at all.
+   */
   public addToResourcePolicy(statement: iam.PolicyStatement): iam.AddToResourcePolicyResult {
+    if (statement.resources) {
+      Annotations.of(this).addWarning('ECR resource policy does not allow resource statements.');
+    }
     if (this.policyDocument === undefined) {
       this.policyDocument = new iam.PolicyDocument();
     }
