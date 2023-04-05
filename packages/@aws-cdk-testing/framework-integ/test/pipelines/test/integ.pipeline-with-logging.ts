972f5382b836217c748d0e26594d05bb6fbc1639
Yoga Yu <yoga.yu.169@gmail.com>
2023-04-05 19:38:48 +0800

    fix: add inte test back

---
+++ packages/@aws-cdk-testing/framework-integ/test/pipelines/test/integ.pipeline-with-logging.ts
@@ -0,0 +1,56 @@
+import { GitHubTrigger } from 'aws-cdk-lib/aws-codepipeline-actions/lib';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as cdk from 'aws-cdk-lib/core';
+import { Stack } from 'aws-cdk-lib/core';
+import * as integ from '@aws-cdk/integ-tests';
+import { ExpectedResult } from '@aws-cdk/integ-tests';
+import * as pipelines from 'aws-cdk-lib/pipelines';
+
+/*
+ * Stack verification steps:
+ * * aws codebuild batch-get-projects --names <synth codebuild project name> : should return correct cloudwatchlog settings
+ */
+const app = new cdk.App({
+  context: {
+    '@aws-cdk/core:newStyleStackSynthesis': '1',
+  },
+});
+const stack = new Stack(app, 'LoggingPipelineStack');
+
+const logGroup = new logs.LogGroup(stack, 'LogGroup', {
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+});
+
+const pipeline = new pipelines.CodePipeline(stack, 'Pipeline', {
+  codeBuildDefaults: {
+    cloudWatchLogging: {
+      logGroup: logGroup,
+      prefix: 'prefix',
+    },
+  },
+  synth: new pipelines.CodeBuildStep('Synth', {
+    input: pipelines.CodePipelineSource.gitHub('yo-ga/cdk-playground', 'main', {
+      trigger: GitHubTrigger.NONE,
+    }),
+    commands: [
+      'npm ci',
+      'npm run build',
+      'npx cdk synth',
+    ],
+  }),
+});
+
+const testCase = new integ.IntegTest(app, 'PipelineLoggingTest', {
+  testCases: [stack],
+});
+
+pipeline.buildPipeline();
+testCase.assertions
+  .awsApiCall('CodeBuild', 'batchGetProjects', {
+    names: [pipeline.synthProject.projectName],
+  })
+  .assertAtPath('projects.0.logsConfig.cloudWatchLogs.status', ExpectedResult.stringLikeRegexp('ENABLED'))
+  .assertAtPath('projects.0.logsConfig.cloudWatchLogs.logGroup', ExpectedResult.stringLikeRegexp(logGroup.logGroupName))
+  .assertAtPath('projects.0.logsConfig.cloudWatchLogs.streamName', ExpectedResult.stringLikeRegexp('prefix'));
+
+app.synth();
