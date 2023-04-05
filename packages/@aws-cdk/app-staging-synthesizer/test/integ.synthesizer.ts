477f81fe1397037bcd57a568125cb2c1ad12cdff
Kaizen Conroy <zen.conroy@gmail.com>
2023-04-05 15:11:19 -0400

    rename

--- packages/@aws-cdk/app-staging-synthesizer/test/integ.synthesizer.ts
+++ packages/@aws-cdk/app-staging-synthesizer/test/integ.synthesizer.ts
@@ -1,12 +1,19 @@
 import * as path from 'path';
 import { App, Stack } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { AppStagingSynthesizer } from '../lib';
+import { AppStagingSynthesizer, BootstrapRole } from '../lib';
+import * as integ from '@aws-cdk/integ-tests-alpha';
 
 const app = new App();
 
 const stack = new Stack(app, 'app-scoped-staging-test', {
-  synthesizer: AppStagingSynthesizer.stackPerEnv({ appId: 'app-id' }),
+  synthesizer: AppStagingSynthesizer.stackPerEnv({
+    appId: 'app-id',
+    qualifier: 'hnb659fds',
+    bootstrapRoles: {
+      cloudFormationExecutionRole: BootstrapRole.fromRoleArn('arn:aws:iam::489318732371:role/cdk-hnb659fds-cfn-exec-role-489318732371-us-east-2'),
+    },
+  }),
   env: {
     account: '489318732371',
     region: 'us-east-2',
@@ -17,6 +24,10 @@ new lambda.Function(stack, 'lambda', {
   code: lambda.AssetCode.fromAsset(path.join(__dirname, 'assets')),
   handler: 'index.handler',
   runtime: lambda.Runtime.PYTHON_3_9,
+});
+
+new integ.IntegTest(app, 'integ-tests', {
+  testCases: [stack],
 });
 
 app.synth();
