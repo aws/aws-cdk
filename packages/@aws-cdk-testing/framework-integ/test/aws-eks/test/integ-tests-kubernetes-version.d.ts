5587d48339d5470063bce86325c0b8d548851090
Yoga Yu <yoga.yu.169@gmail.com>
2023-04-07 07:05:01 +0800

    chore(cdk-testing): Remove compiled file and update gitignore for integration testing (#24959)
    After the remodeling for integration testing and v2 cdk, there are some files added. However, they are ignored by the original rule. Try to remove unnecessary files and add the rules.
    
    Closes #24947. 
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ-tests-kubernetes-version.d.ts
+++
@@ -1,7 +0,0 @@
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { Construct } from 'constructs';
-import * as eks from 'aws-cdk-lib/aws-eks';
-export declare function getClusterVersionConfig(scope: Construct): {
-    version: eks.KubernetesVersion;
-    kubectlLayer: lambda.ILayerVersion;
-};
--- packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ-handlers/util.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function add(a: number, b: number): number;
-export declare function mult(a: number, b: number): number;
