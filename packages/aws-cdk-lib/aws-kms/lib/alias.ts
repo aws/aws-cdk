694057bbbc2b172a7d49283d0b5f09d080cb1133
daschaa <josh@joshuaw.de>
2023-04-13 22:06:11 +0200

    ✨ (kms): add required aliasname prefix to aliasnames that don't start with a token

--- packages/aws-cdk-lib/aws-kms/lib/alias.ts
+++ packages/aws-cdk-lib/aws-kms/lib/alias.ts
@@ -1,5 +1,5 @@
 import * as iam from '../../aws-iam';
-import { RemovalPolicy, Resource, Stack, Token } from '../../core';
+import { RemovalPolicy, Resource, Stack, Token, Tokenization } from '../../core';
 import { Construct } from 'constructs';
 import { IKey } from './key';
 import { CfnAlias } from './kms.generated';
@@ -195,6 +195,20 @@ export class Alias extends AliasBase {
       }
 
       if (!aliasName.match(/^[a-zA-Z0-9:/_-]{1,256}$/)) {
+        throw new Error('Alias name must be between 1 and 256 characters in a-zA-Z0-9:/_-');
+      }
+    } else if (Tokenization.reverseString(aliasName).firstValue) {
+      const valueInToken = Tokenization.reverseString(aliasName).firstValue;
+
+      if (!valueInToken.startsWith(REQUIRED_ALIAS_PREFIX)) {
+        aliasName = REQUIRED_ALIAS_PREFIX + aliasName;
+      }
+
+      if (valueInToken.toLocaleLowerCase().startsWith(DISALLOWED_PREFIX)) {
+        throw new Error(`Alias cannot start with ${DISALLOWED_PREFIX}: ${aliasName}`);
+      }
+
+      if (!valueInToken.match(/^[a-zA-Z0-9:/_-]{1,256}$/)) {
         throw new Error('Alias name must be between 1 and 256 characters in a-zA-Z0-9:/_-');
       }
     }
