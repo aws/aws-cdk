d5c0bd8152b9a3a9997399024966bf50ef829f5a
Andrea Culot <95755271+andclt@users.noreply.github.com>
2023-04-13 10:17:08 +0200

    docs(lambda): Update nodejs18.x doc (#25037)
    Node18 is available in `me-central-1`, `eu-central-2` and `eu-south-2`.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-lambda/lib/runtime.ts
+++ packages/aws-cdk-lib/aws-lambda/lib/runtime.ts
@@ -87,7 +87,7 @@ export class Runtime {
   public static readonly NODEJS_16_X = new Runtime('nodejs16.x', RuntimeFamily.NODEJS, { supportsInlineCode: true });
 
   /**
-   * The NodeJS 18.x runtime (nodejs18.x) supported in all commercial regions except me-central-1, eu-central-2 and eu-south-2.
+   * The NodeJS 18.x runtime (nodejs18.x)
    */
   public static readonly NODEJS_18_X = new Runtime('nodejs18.x', RuntimeFamily.NODEJS, { supportsInlineCode: true });
 
