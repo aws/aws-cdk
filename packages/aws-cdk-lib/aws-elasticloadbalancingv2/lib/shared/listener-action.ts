d09c7d98c13281eaa2a70877edc39b13a3372825
WinterYukky <49480575+WinterYukky@users.noreply.github.com>
2023-04-20 22:39:33 +0900

    fix(elasticloadbalancingv2): can not set sessionTimeout (#24457)
    ## Summary
    Application LoadBalancer can not set `sessionTimeout` on `authenticateOidc` except in `defaultActions`. 
    This PR fixes this bug.
    
    ## Cause of the bug
    This is because the CDK uses the same structures for [ListenerRule.AuthenticateOidcConfig](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-elasticloadbalancingv2-listenerrule-authenticateoidcconfig.html) and [Listener.AuthenticateOidcConfig](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-elasticloadbalancingv2-listener-authenticateoidcconfig.html). These structures have almost the same structure, but validation fails during synthesize because the data type of `sessionTimeout` is different for String and Integer.
    
    ```diff
      AuthenticationRequestExtraParams: 
        Key : Value
      AuthorizationEndpoint: String
      ClientId: String
      ClientSecret: String
      Issuer: String
      OnUnauthenticatedRequest: String
      Scope: String
      SessionCookieName: String
    - SessionTimeout: String
    + SessionTimeout: Integer
      TokenEndpoint: String
      UseExistingClientSecret: Boolean
      UserInfoEndpoint: String
    ```
    
    ## How to fix?
    Add `addRuleAction()` to register an Action for a ListenerRule so that it can hold both config for `Listener` and config for `ListenerRule`. Also, separate `renderActions()` into one for the `Listener` (`defaultActions`) and one for the `ListenerRule` (`actions`) and have them use their own configs.
    
    This allows changes to be made without destroying existing published interfaces.
    
    Closes #12843, #21768.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/shared/listener-action.ts
+++ packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/shared/listener-action.ts
@@ -1,11 +1,15 @@
-import { CfnListener } from '../elasticloadbalancingv2.generated';
+import { CfnListener, CfnListenerRule } from '../elasticloadbalancingv2.generated';
 
 /**
  * Interface for listener actions
  */
 export interface IListenerAction {
   /**
-   * Render the actions in this chain
+   * Render the listener default actions in this chain
    */
   renderActions(): CfnListener.ActionProperty[];
+  /**
+   * Render the listener rule actions in this chain
+   */
+  renderRuleActions(): CfnListenerRule.ActionProperty[];
 }
