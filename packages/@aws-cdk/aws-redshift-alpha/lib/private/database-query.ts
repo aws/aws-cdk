6f7c4b595d4281a9f9bf19796d8d5ffa08c9d5e3
Mitchell Valine <mitchellvaline@yahoo.com>
2023-04-25 08:57:12 -0700

    feat: upgrade default CR runtime version (#24916)
    Adds a regional fact for the default custom resource NodeJS runtime version. Currently for 'aws' and 'aws-cn' partitions uses NodeJS 16.x and NodeJS 14.x for all other partitions. This should allow continued usage of our custom resources in gov cloud and other non-commercial regions while avoiding usage of Node14 which is EOL for others.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-redshift-alpha/lib/private/database-query.ts
+++ packages/@aws-cdk/aws-redshift-alpha/lib/private/database-query.ts
@@ -8,6 +8,7 @@ import { Construct } from 'constructs';
 import { DatabaseQueryHandlerProps } from './handler-props';
 import { Cluster } from '../cluster';
 import { DatabaseOptions } from '../database-options';
+import { builtInCustomResourceNodeRuntime } from 'aws-cdk-lib/custom-resources';
 
 export interface DatabaseQueryProps<HandlerProps> extends DatabaseOptions {
   readonly handler: string;
@@ -34,7 +35,7 @@ export class DatabaseQuery<HandlerProps> extends Construct implements iam.IGrant
       code: lambda.Code.fromAsset(path.join(__dirname, 'database-query-provider'), {
         exclude: ['*.ts'],
       }),
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: builtInCustomResourceNodeRuntime(this),
       handler: 'index.handler',
       timeout: cdk.Duration.minutes(1),
       uuid: '3de5bea7-27da-4796-8662-5efb56431b5f',
