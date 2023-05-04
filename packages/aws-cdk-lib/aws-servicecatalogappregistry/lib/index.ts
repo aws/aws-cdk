79a49d68c406ab3873b533e8467c9ecafb067558
yingdong chen <yingdon@amazon.com>
2023-05-03 23:28:27 -0700

    fix up the index.ts
    why?
    
    <why?>
    
    what?
    
    <what?>
    
    Testing done
    -------------------------------------
    *
    
    Related items
    ------------------------------------
    * SIM/auto-cut ticket

--- packages/aws-cdk-lib/aws-servicecatalogappregistry/lib/index.ts
+++ packages/aws-cdk-lib/aws-servicecatalogappregistry/lib/index.ts
@@ -3,5 +3,6 @@ export * from './attribute-group';
 export * from './application-associator';
 export * from './target-application';
 export * from './common';
+export * from './servicecatalogappregistry.generated';
 
 // AWS::ServiceCatalogAppRegistry CloudFormation Resources:
