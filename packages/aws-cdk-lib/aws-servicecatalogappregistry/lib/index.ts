76b3184f1f219b982c09df7322a8e023a1b818cb
yingdong chen <yingdon@amazon.com>
2023-05-04 13:08:00 -0700

    update the order to index.ts
    why?
    
    <why?>
    
    what?
    
    <what?>
    
    Testing done
    -------------------------------------
    *
    
    Related items
    ------------------------------------
    * SIM/auto-cut ticket

--- packages/aws-cdk-lib/aws-servicecatalogappregistry/lib/index.ts
+++ packages/aws-cdk-lib/aws-servicecatalogappregistry/lib/index.ts
@@ -1,8 +1,8 @@
+export * from './servicecatalogappregistry.generated';
 export * from './application';
 export * from './attribute-group';
 export * from './application-associator';
 export * from './target-application';
 export * from './common';
-export * from './servicecatalogappregistry.generated';
 
 // AWS::ServiceCatalogAppRegistry CloudFormation Resources:
