5bfeea119fb83c765468f65f2ca0461684fb8f92
yingdong chen <yingdon@amazon.com>
2023-05-03 22:48:54 -0700

    stablized aws-servicecatalogappregistry L2 constructs
    why?
    
    <why?>
    
    what?
    
    <what?>
    
    Testing done
    -------------------------------------
    *
    
    Related items
    ------------------------------------
    * SIM/auto-cut ticket

--- packages/aws-cdk-lib/aws-servicecatalogappregistry/lib/index.ts
+++ packages/aws-cdk-lib/aws-servicecatalogappregistry/lib/index.ts
@@ -1,1 +1,7 @@
-export * from './servicecatalogappregistry.generated';
+export * from './application';
+export * from './attribute-group';
+export * from './application-associator';
+export * from './target-application';
+export * from './common';
+
+// AWS::ServiceCatalogAppRegistry CloudFormation Resources:
