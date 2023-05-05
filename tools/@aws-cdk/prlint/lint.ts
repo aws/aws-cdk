f7435474bfb1c70938c4ab0c0d5edc0193094260
corymhall <43035978+corymhall@users.noreply.github.com>
2023-05-05 17:05:40 +0000

    removing logging statements

--- tools/@aws-cdk/prlint/lint.ts
+++ tools/@aws-cdk/prlint/lint.ts
@@ -342,8 +342,8 @@ export class PullRequestLinter {
     // COLLABORATOR = has been invited to collaborate on the repository
     const maintainerRequestedChanges = reviews.data.some(
       review => review.author_association === 'MEMBER'
-        && review.state === 'CHANGES_REQUESTED'
         && review.user?.login !== 'aws-cdk-automation'
+        && review.state === 'CHANGES_REQUESTED'
     );
     const prLinterFailed = reviews.data.find((review) => review.user?.login === 'aws-cdk-automation' && review.state !== 'DISMISSED') as Review;
     const userRequestsExemption = pr.labels.some(label => (label.name === Exemption.REQUEST_EXEMPTION || label.name === Exemption.REQUEST_CLARIFICATION));
@@ -453,10 +453,8 @@ export class PullRequestLinter {
       // always assess the review, even if the linter fails
     } finally {
       // also assess whether the PR needs review or not
-      console.log('HERE!!!!!!!');
       try {
         const state = await this.codeBuildJobSucceeded(sha);
-        console.log(state);
         if (state) {
           await this.assessNeedsReview(pr);
         }
