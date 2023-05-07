905d5eda211548c11cdec7ac29ac2ba34cb73e81
tmokmss <tomookam@live.jp>
2023-05-07 18:30:54 +0900

    Revert "revert"
    This reverts commit 458e6b4eb7dd201616dabfbf2c31adc68f163df4.

--- packages/aws-cdk-lib/core/lib/resolvable.ts
+++ packages/aws-cdk-lib/core/lib/resolvable.ts
@@ -158,6 +158,8 @@ export class DefaultTokenResolver implements ITokenResolver {
       // The token might have returned more values that need resolving, recurse
       resolved = context.resolve(resolved);
       resolved = postProcessor.postProcess(resolved, context);
+      // resolve again since postProcess might have added more tokens (e.g. overriding)
+      resolved = context.resolve(resolved);
       return resolved;
     } catch (e: any) {
       let message = `Resolution error: ${e.message}.`;
