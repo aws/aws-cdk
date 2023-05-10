3c6292e1e8ca0032d731c29ea7d151e1458cacdd
Momo Kornher <kornherm@amazon.co.uk>
2023-05-10 12:14:40 +0100

    use tsconfig.dev for eslint

--- packages/aws-cdk-lib/.eslintrc.js
+++ packages/aws-cdk-lib/.eslintrc.js
@@ -1,3 +1,3 @@
 const baseConfig = require('@aws-cdk/cdk-build-tools/config/eslintrc');
-baseConfig.parserOptions.project = __dirname + '/tsconfig.json';
+baseConfig.parserOptions.project = __dirname + '/tsconfig.dev.json';
 module.exports = baseConfig;
