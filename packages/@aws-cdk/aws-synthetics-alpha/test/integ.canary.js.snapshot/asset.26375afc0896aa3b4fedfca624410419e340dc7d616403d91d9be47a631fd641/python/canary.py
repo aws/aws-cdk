0e5b04185c35c315d79dd10d197243da1278c9b4
WinterYukky <49480575+WinterYukky@users.noreply.github.com>
2023-05-11 05:19:20 +0000

    feat(synthetics): support runtime puppeteer 4.0

--- packages/@aws-cdk/aws-synthetics-alpha/test/integ.canary.js.snapshot/asset.26375afc0896aa3b4fedfca624410419e340dc7d616403d91d9be47a631fd641/python/canary.py
+++ packages/@aws-cdk/aws-synthetics-alpha/test/integ.canary.js.snapshot/asset.26375afc0896aa3b4fedfca624410419e340dc7d616403d91d9be47a631fd641/python/canary.py
@@ -1,5 +1,6 @@
 # This example comes from the AWS Synthetics service console "API canary" blueprint
 
+import os
 import json
 import http.client
 import urllib.parse
@@ -11,51 +12,50 @@ def verify_request(method, url, post_data=None, headers={}):
     parsed_url = urllib.parse.urlparse(url)
     user_agent = str(syn_webdriver.get_canary_user_agent_string())
     if "User-Agent" in headers:
-        headers["User-Agent"] = " ".join([user_agent, headers["User-Agent"]])
+        headers["User-Agent"] = f"{user_agent} {headers['User-Agent']}"
     else:
-        headers["User-Agent"] = "{}".format(user_agent)
+        headers["User-Agent"] = user_agent
 
-    logger.info("Making request with Method: '%s' URL: %s: Data: %s Headers: %s" % (
-        method, url, json.dumps(post_data), json.dumps(headers)))
+    logger.info(f"Making request with Method: '{method}' URL: {url}: Data: {json.dumps(post_data)} Headers: {json.dumps(headers)}")
 
     if parsed_url.scheme == "https":
         conn = http.client.HTTPSConnection(parsed_url.hostname, parsed_url.port)
     else:
         conn = http.client.HTTPConnection(parsed_url.hostname, parsed_url.port)
 
-    conn.request(method, url, str(post_data), headers)
+    conn.request(method, url, post_data, headers)
     response = conn.getresponse()
-    logger.info("Status Code: %s " % response.status)
-    logger.info("Response Headers: %s" % json.dumps(response.headers.as_string()))
+    logger.info(f"Status Code: {response.status}")
+    logger.info(f"Response Headers: {json.dumps(response.headers.as_string())}")
 
     if not response.status or response.status < 200 or response.status > 299:
         try:
-            logger.error("Response: %s" % response.read().decode())
+            logger.error(f"Response: {response.read().decode()}")
         finally:
             if response.reason:
                 conn.close()
-                raise Exception("Failed: %s" % response.reason)
+                raise Exception(f"Failed: {response.reason}")
             else:
                 conn.close()
-                raise Exception("Failed with status code: %s" % response.status)
+                raise Exception(f"Failed with status code: {response.status}")
 
-    logger.info("Response: %s" % response.read().decode())
-    logger.info("HTTP request successfully executed")
+    logger.info(f"Response: {response.read().decode()}")
+    logger.info("HTTP request successfully executed.")
     conn.close()
 
 
 def main():
 
-    url = 'https://example.com/'
+    url = os.environ['URL']
     method = 'GET'
     postData = ""
     headers = {}
 
     verify_request(method, url, None, headers)
 
-    logger.info("Canary successfully executed")
+    logger.info("Canary successfully executed.")
 
 
 def handler(event, context):
-    logger.info("Selenium Python API canary")
+    logger.info("Selenium Python API canary.")
     main()
