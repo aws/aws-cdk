8c66597f92ec8c5732814a28856c38d1e84d1627
Momo Kornher <kornherm@amazon.co.uk>
2023-05-11 18:52:18 +0100

    generate new submodule during spec update

--- scripts/bump-cfnspec.sh
+++ scripts/bump-cfnspec.sh
@@ -10,22 +10,22 @@ pwd=$(pwd)
 
 ${pwd}/install.sh
 
-# Running the `@aws-cdk/cfnspec` update script requires both `cfn2ts` and
-# `ubergen` to be readily available. The dependency can however not be modeled
-# cleanly without introducing dependency cycles... This is due to how these
-# dependencies are in fact involved in the building of new construct libraries
-# created upon their introduction in the CFN Specification (they incur the
-# dependency, not `@aws-cdk/cfnspec` itself).
+# Running the `aws-cdk-lib/scripts/gen.ts` script requires `cfn2ts`
+# However we don't want to do a full build of `aws-cdk-lib``.
+# So we request `@aws-cdk/cfn2ts` explicitly.
 yarn lerna run build --stream     \
   --scope=@aws-cdk/cfnspec        \
   --scope=@aws-cdk/cfn2ts         \
-  --scope=@aws-cdk/ubergen        \
   --include-dependencies
 
 # Run the cfnspec update
 cd ${pwd}/packages/@aws-cdk/cfnspec
 yarn update
 version=$(cat cfn.version)
+
+# Generate L1s including new submodules
+cd ${pwd}/packages/aws-cdk-lib
+yarn gen
 
 # Come back to root, add all files to git and commit
 cd ${pwd}
