40658a19471e64546db235272974e76f28f985c9
Momo Kornher <kornherm@amazon.co.uk>
2023-05-11 18:26:20 +0100

    fix lazy index not updated

--- packages/aws-cdk-lib/scripts/gen.ts
+++ packages/aws-cdk-lib/scripts/gen.ts
@@ -6,6 +6,7 @@ import submodulesGen from './submodules';
 const awsCdkLibDir = path.join(__dirname, '..');
 const pkgJsonPath = path.join(awsCdkLibDir, 'package.json');
 const topLevelIndexFilePath = path.join(awsCdkLibDir, 'index.ts');
+const lazyExportsFilePath = path.join(awsCdkLibDir, 'lazy-index.ts');
 const scopeMapPath = path.join(__dirname, 'scope-map.json');
 
 main().catch(e => {
@@ -43,10 +44,15 @@ async function writeScopeMap(modules: ModuleMap) {
 async function updateExportsAndEntryPoints(modules: ModuleMap) {
   const pkgJson = await fs.readJson(pkgJsonPath);
 
-  const topLevelIndexFileEntries = new Array<string>();
+  const indexStatements = new Array<string>();
   if (fs.existsSync(topLevelIndexFilePath)) {
     const indexFile = await fs.readFile(topLevelIndexFilePath);
-    topLevelIndexFileEntries.push(...indexFile.toString('utf-8').split('\n').filter(Boolean));
+    indexStatements.push(...indexFile.toString('utf-8').split('\n').filter(Boolean));
+  }
+  const lazyExports = new Array<string>();
+  if (fs.existsSync(lazyExportsFilePath)) {
+    const lazExportsFile = await fs.readFile(lazyExportsFilePath);
+    lazyExports.push(...lazExportsFile.toString('utf-8').split('\n').filter(Boolean));
   }
 
   for (const [moduleName, { definition }] of Object.entries(modules)) {
@@ -55,14 +61,21 @@ async function updateExportsAndEntryPoints(modules: ModuleMap) {
       submodule: definition?.submoduleName ?? moduleName.replace(/-/g, '_'),
     };
 
-
     const exportName = `./${moduleConfig.name}`;
     if (!pkgJson.exports[exportName]) {
       pkgJson.exports[exportName] = `./${moduleConfig.name}/index.js`;
     }
 
-    if (!topLevelIndexFileEntries.find(e => e.includes(moduleConfig.name))) {
-      topLevelIndexFileEntries.push(`export * as ${moduleConfig.submodule} from './${moduleConfig.name}';`);
+    if (!indexStatements.find(e => e.includes(moduleConfig.name))) {
+      indexStatements.push(`export * as ${moduleConfig.submodule} from './${moduleConfig.name}';`);
+    }
+
+    if (!lazyExports.find(e => e.includes(moduleConfig.name))) {
+      if (moduleConfig.name === 'core') {
+        lazyExports.unshift(`export * from './${moduleConfig.name}';`);
+      } else {
+        lazyExports.push(`Object.defineProperty(exports, '${moduleConfig.submodule}', { get: function () { return require('${exportName}'); } });`);
+      }
     }
   }
 
@@ -70,5 +83,6 @@ async function updateExportsAndEntryPoints(modules: ModuleMap) {
   pkgJson.exports = Object.fromEntries(Object.entries(pkgJson.exports).sort(([e1], [e2]) => e1.localeCompare(e2)));
 
   await fs.writeJson(pkgJsonPath, pkgJson, { spaces: 2 });
-  await fs.writeFile(topLevelIndexFilePath, topLevelIndexFileEntries.sort().join('\n') + '\n');
+  await fs.writeFile(topLevelIndexFilePath, indexStatements.sort((l1, l2) => l1.localeCompare(l2)).join('\n') + '\n');
+  await fs.writeFile(lazyExportsFilePath, lazyExports.sort((l1, l2) => l1.localeCompare(l2)).join('\n') + '\n');
 }
