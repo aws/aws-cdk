d60b314a9308f4718299e024b6a518e197a4bde2
Momo Kornher <kornherm@amazon.co.uk>
2023-05-11 18:08:58 +0100

    refactor gen

--- packages/aws-cdk-lib/scripts/gen.ts
+++ packages/aws-cdk-lib/scripts/gen.ts
@@ -1,10 +1,12 @@
-import * as path from 'path';
+import * as path from 'node:path';
 import { generateAll, ModuleMap } from '@aws-cdk/cfn2ts';
 import * as fs from 'fs-extra';
+import submodulesGen from './submodules';
 
 const awsCdkLibDir = path.join(__dirname, '..');
 const pkgJsonPath = path.join(awsCdkLibDir, 'package.json');
 const topLevelIndexFilePath = path.join(awsCdkLibDir, 'index.ts');
+const scopeMapPath = path.join(__dirname, 'scope-map.json');
 
 main().catch(e => {
   // eslint-disable-next-line no-console
@@ -14,19 +16,21 @@ main().catch(e => {
 
 async function main() {
   // Generate all L1s based on config in scope-map.json
-  const scopeMapPath = path.join(__dirname, 'scope-map.json');
 
-  const generated = await generateAll(awsCdkLibDir, {
+  const generated = (await generateAll(awsCdkLibDir, {
     coreImport: '../../core',
     cloudwatchImport: '../../aws-cloudwatch',
     scopeMapPath,
-  });
+  }));
 
-  // Add any new cfn modules to exports in package.json and index.ts
-  await updatePackageJsonAndIndexFiles(generated);
+  await updateExportsAndEntryPoints(generated);
+  await writeScopeMap(generated);
+  await submodulesGen(generated, awsCdkLibDir);
+}
 
-  // Update scope-map config with any changes
-  const newScopeMap = Object.entries(generated)
+async function writeScopeMap(modules: ModuleMap) {
+  const newScopeMap = Object.entries(modules)
+    .sort(([modA], [modB]) => modA.localeCompare(modB))
     .reduce((accum, [moduleName, { scopes }]) => {
       return {
         ...accum,
@@ -34,63 +38,37 @@ async function main() {
       };
     }, {});
   await fs.writeJson(scopeMapPath, newScopeMap, { spaces: 2 });
-
-  // Call build-tools within modules for other codegen
-  // TODO: Move these up into aws-cdk-libs/scripts
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  require('../aws-events-targets/build-tools/gen.js');
-  await genCfnIncludeMap(generated);
 }
 
-async function updatePackageJsonAndIndexFiles(modules: ModuleMap) {
+async function updateExportsAndEntryPoints(modules: ModuleMap) {
   const pkgJson = await fs.readJson(pkgJsonPath);
 
   const topLevelIndexFileEntries = new Array<string>();
   if (fs.existsSync(topLevelIndexFilePath)) {
     const indexFile = await fs.readFile(topLevelIndexFilePath);
-    topLevelIndexFileEntries.push(...indexFile.toString('utf-8').split('\n'));
+    topLevelIndexFileEntries.push(...indexFile.toString('utf-8').split('\n').filter(Boolean));
   }
 
-  Object.entries(modules)
-    .forEach(([moduleName, { module }]) => {
-      let moduleConfig: { name: string, submodule: string };
-      if (module) {
-        moduleConfig = {
-          name: module.moduleName,
-          submodule: module.submoduleName,
-        };
-      } else {
-        moduleConfig = {
-          name: moduleName,
-          submodule: moduleName.replace(/-/g, '_'),
-        };
-      }
+  for (const [moduleName, { definition }] of Object.entries(modules)) {
+    const moduleConfig = {
+      name: definition?.moduleName ?? moduleName,
+      submodule: definition?.submoduleName ?? moduleName.replace(/-/g, '_'),
+    };
 
-      const exportName = `./${moduleConfig.name}`;
-      if (!pkgJson.exports[exportName]) {
-        pkgJson.exports[exportName] = `./${moduleConfig.name}/index.js`;
-      }
 
-      if (!topLevelIndexFileEntries.find(e => e.includes(moduleConfig.name))) {
-        topLevelIndexFileEntries.push(`export * as ${moduleConfig.submodule} from './${moduleConfig.name}';`);
-      }
-    });
+    const exportName = `./${moduleConfig.name}`;
+    if (!pkgJson.exports[exportName]) {
+      pkgJson.exports[exportName] = `./${moduleConfig.name}/index.js`;
+    }
+
+    if (!topLevelIndexFileEntries.find(e => e.includes(moduleConfig.name))) {
+      topLevelIndexFileEntries.push(`export * as ${moduleConfig.submodule} from './${moduleConfig.name}';`);
+    }
+  }
+
+  // sort exports
+  pkgJson.exports = Object.fromEntries(Object.entries(pkgJson.exports).sort(([e1], [e2]) => e1.localeCompare(e2)));
 
   await fs.writeJson(pkgJsonPath, pkgJson, { spaces: 2 });
-  await fs.writeFile(topLevelIndexFilePath, topLevelIndexFileEntries.join('\n'));
-}
-
-async function genCfnIncludeMap(generated: ModuleMap) {
-  const classMap: { [cfnType: string]: string } = {};
-  Object.entries(generated).forEach(([moduleName, { resources }]) => {
-    const modulePath = `aws-cdk-lib/${moduleName}`;
-    Object.entries(resources).forEach(([resourceName, resourceClassName]) => {
-      classMap[resourceName] = `${modulePath}.${resourceClassName}`;
-    });
-  });
-
-  const sortedClassMap = Object.fromEntries(Object.entries(classMap).sort(([resA], [resB]) => resA.localeCompare(resB)));
-
-  const filePath = path.join(__dirname, '..', 'cloudformation-include', 'cfn-types-2-classes.json');
-  await fs.writeJson(filePath, sortedClassMap, { spaces: 2 });
+  await fs.writeFile(topLevelIndexFilePath, topLevelIndexFileEntries.sort().join('\n') + '\n');
 }
