2490356f8889121cc2f13e3154b60556c7124f9b
Francis <colifran@amazon.com>
2023-05-15 09:32:09 -0700

    added unit test to verify correct helm chart version is set in template based on selected alb controller version
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/aws-eks/test/alb-controller.test.ts
+++ packages/aws-cdk-lib/aws-eks/test/alb-controller.test.ts
@@ -69,3 +69,37 @@ test('throws when a policy is not defined for a custom version', () => {
     version: AlbControllerVersion.of('custom'),
   })).toThrowError("'albControllerOptions.policy' is required when using a custom controller version");
 });
+
+test('correct helm chart version is set for selected alb controller version', () => {
+  const { stack } = testFixture();
+
+  const cluster = new Cluster(stack, 'Cluster', {
+    version: KubernetesVersion.V1_21,
+  });
+
+  AlbController.create(stack, {
+    cluster,
+    version: AlbControllerVersion.V2_5_1,
+    repository: 'custom',
+  });
+
+  Template.fromStack(stack).hasResourceProperties(HelmChart.RESOURCE_TYPE, {
+    Version: '1.5.2',
+    Values: {
+      'Fn::Join': [
+        '',
+        [
+          '{"clusterName":"',
+          {
+            Ref: 'Cluster9EE0221C',
+          },
+          '","serviceAccount":{"create":false,"name":"aws-load-balancer-controller"},"region":"us-east-1","vpcId":"',
+          {
+            Ref: 'ClusterDefaultVpcFA9F2722',
+          },
+          '","image":{"repository":"custom","tag":"v2.5.1"}}',
+        ],
+      ],
+    },
+  });
+});
