22bdcda5316c9b5e67b5d1b7deb80d162516affb
Austin Ely <austiely@amazon.com>
2023-05-19 15:19:18 +0000

    chore: fix integ test, remove unnecessary ec2 test

--- packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/ec2/integ.deployment-alarms.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/ec2/integ.deployment-alarms.ts
@@ -1,6 +1,7 @@
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as cdk from 'aws-cdk-lib';
 import * as integ from '@aws-cdk/integ-tests-alpha';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
@@ -15,6 +16,11 @@ const taskDefinition = new ecs.Ec2TaskDefinition(stack, 'TaskDef');
 taskDefinition.addContainer('web', {
   image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
   memoryReservationMiB: 256,
+  portMappings: [
+    {
+      containerPort: 80,
+    },
+  ],
 });
 
 const autoScalingGroup = new autoscaling.AutoScalingGroup(stack, 'ASG', {
@@ -35,15 +41,40 @@ const cluster = new ecs.Cluster(stack, 'EC2CPCluster', {
 });
 
 cluster.addAsgCapacityProvider(cp);
-const myAlarm = cloudwatch.Alarm.fromAlarmArn(stack, 'myAlarm', 'arn:aws:cloudwatch:us-east-1:372902930120:alarm:myAlarm');
+const lb = new elbv2.ApplicationLoadBalancer(stack, 'LB', { vpc, internetFacing: true });
+const listener = lb.addListener('PublicListener', { port: 80, open: true });
 
-new ecs.Ec2Service(stack, 'EC2Service', {
+const metric = lb.metrics.httpCodeTarget(elbv2.HttpCodeTarget.TARGET_5XX_COUNT, {
+  period: cdk.Duration.minutes(1),
+});
+const myAlarm = new cloudwatch.Alarm(lb, 'MyMetricAlarm', {
+  metric,
+  evaluationPeriods: 5,
+  threshold: 100,
+});
+
+const svc = new ecs.Ec2Service(stack, 'EC2Service', {
   cluster,
   taskDefinition,
-  deploymentAlarms: {
-    alarms: [myAlarm],
-  },
 });
+svc.enableDeploymentAlarms({
+  alarmNames: [myAlarm.alarmName],
+  behavior: ecs.AlarmBehavior.FAIL_ON_ALARM,
+});
+
+const tg = listener.addTargets('ECS', {
+  port: 80,
+  targets: [svc],
+});
+
+svc.createAlarm({
+  useAsDeploymentAlarm: true,
+  alarmName: 'TargetResponseTime',
+  evaluationPeriods: 5,
+  threshold: 80,
+  metric: tg.metrics.targetResponseTime(),
+});
+
 new integ.IntegTest(app, 'DeploymentAlarms', {
   testCases: [stack],
 });
