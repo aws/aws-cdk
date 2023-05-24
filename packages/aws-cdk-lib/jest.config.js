d00ba5f88f7ca8dfd0badb997a0b1917bdc0a5c9
Mitchell Valine <mitchellvaline@yahoo.com>
2023-05-24 15:03:22 -0700

    use base config

--- packages/aws-cdk-lib/jest.config.js
+++ packages/aws-cdk-lib/jest.config.js
@@ -2,6 +2,11 @@ const baseConfig = require('@aws-cdk/cdk-build-tools/config/jest.config');
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  ...baseConfig,
+  moduleFileExtensions: [
+    'js',
+    'ts',
+  ],
   maxWorkers: '50%',
   preset: 'ts-jest',
   testEnvironment: 'node',
