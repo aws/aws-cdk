382113e433ac9199418240900c3282e56fe92926
Mitchell Valine <mitchellvaline@yahoo.com>
2023-05-24 14:49:45 -0700

    add back coverage

--- packages/aws-cdk-lib/jest.config.js
+++ packages/aws-cdk-lib/jest.config.js
@@ -2,10 +2,16 @@ const baseConfig = require('@aws-cdk/cdk-build-tools/config/jest.config');
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  maxWorkers: '50%',
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
     '<rootDir>/**/test/**/?(*.)+(test).ts',
   ],
-  maxWorkers: '50%',
+   coverageThreshold: {
+    global: {
+      branches: 40,
+      statements: 55,
+    },
+  },
 };
