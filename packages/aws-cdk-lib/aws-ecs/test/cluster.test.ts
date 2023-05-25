a03f0e3888c6915c95ceed22c9e989757f42cbf8
Adithya Kolla <kolladit@dev-dsk-kolladit-2b-6ad49233.us-west-2.amazon.com>
2023-05-25 22:56:57 +0000

    remove extra space

--- packages/aws-cdk-lib/aws-ecs/test/cluster.test.ts
+++ packages/aws-cdk-lib/aws-ecs/test/cluster.test.ts
@@ -2722,7 +2722,7 @@ test('can add ASG capacity with autoscaling', () => {
 
   // Add Bottlerocket ASG Capacity Provider
   cluster.addAsgCapacityProvider(capacityProviderBottlerocket);
-  
+
   // THEN Bottlerocket LaunchConfiguration
   Template.fromStack(stack).hasResourceProperties('AWS::AutoScaling::LaunchConfiguration', {
     ImageId: {
