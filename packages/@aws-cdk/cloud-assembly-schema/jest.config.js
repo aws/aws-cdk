461cc44043f80caaf3dc013d84deca6222c3c2ea
Mitchell Valine <mitchellvaline@yahoo.com>
2023-05-25 12:23:21 -0700

    add ts-jest to copied packages

--- packages/@aws-cdk/cloud-assembly-schema/jest.config.js
+++ packages/@aws-cdk/cloud-assembly-schema/jest.config.js
@@ -1,6 +1,14 @@
 const baseConfig = require('@aws-cdk/cdk-build-tools/config/jest.config');
 module.exports = {
   ...baseConfig,
+  moduleFileExtensions: [
+    'js',
+    'ts',
+  ],
+  preset: 'ts-jest',
+  testMatch: [
+    '<rootDir>/**/test/**/?(*.)+(test).ts',
+  ],
   coverageThreshold: {
     global: {
       branches: 70,
