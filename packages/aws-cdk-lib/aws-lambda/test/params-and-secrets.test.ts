2745e6963ef3c0f48507d863a2a734e71421df73
Francis <colifran@amazon.com>
2023-05-26 12:37:18 -0700

    unit tests for fromVersion testing
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/aws-lambda/test/params-and-secrets.test.ts
+++ packages/aws-cdk-lib/aws-lambda/test/params-and-secrets.test.ts
@@ -140,8 +140,183 @@ describe('params and secrets', () => {
     });
 
     // THEN
-    /* eslint-disable no-console */
-    console.log(JSON.stringify(Template.fromStack(stack), null, 4));
+    Template.fromStack(stack).hasResourceProperties('AWS::Lambda::Function', {
+      Layers: [
+        {
+          'Fn::FindInMap': [
+            'ParamsandsecretslayerMap',
+            {
+              Ref: 'AWS::Region',
+            },
+            '4xx86x64',
+          ],
+        },
+      ],
+      Environment: {
+        Variables: {
+          PARAMETERS_AND_SECRETS_EXTENSION_CACHE_ENABLED: 'true',
+          PARAMETERS_AND_SECRETS_EXTENSION_CACHE_SIZE: '1000',
+          PARAMETERS_AND_SECRETS_EXTENSION_HTTP_PORT: '2773',
+          PARAMETERS_AND_SECRETS_EXTENSION_LOG_LEVEL: 'info',
+          PARAMETERS_AND_SECRETS_EXTENSION_MAX_CONNECTIONS: '3',
+          SECRETS_MANAGER_TIMEOUT_MILLIS: '0',
+          SECRETS_MANAGER_TTL: '300',
+          SSM_PARAMETER_STORE_TIMEOUT_MILLIS: '0',
+          SSM_PARAMETER_STORE_TTL: '300',
+        },
+      },
+    });
     expect(() => app.synth()).not.toThrow();
+  });
+
+  test('can enbale params and secrets from version in non-agnostic stack - arm64', () => {
+    // GIVEN
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, 'Stack', { env: { region: 'us-west-2' } });
+
+    // WHEN
+    new lambda.Function (stack, 'Function', {
+      functionName: 'lambda-function',
+      code: new lambda.InlineCode('foo'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_18_X,
+      architecture: lambda.Architecture.ARM_64,
+      paramsAndSecrets: {
+        layerVersion: lambda.ParamsAndSecretsLayerVersion.fromVersion(lambda.ParamsAndSecretsVersions.V4),
+      },
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::Lambda::Function', {
+      Layers: ['arn:aws:lambda:us-west-2:345057560386:layer:AWS-Parameters-and-Secrets-Lambda-Extension-Arm64:4'],
+      Environment: {
+        Variables: {
+          PARAMETERS_AND_SECRETS_EXTENSION_CACHE_ENABLED: 'true',
+          PARAMETERS_AND_SECRETS_EXTENSION_CACHE_SIZE: '1000',
+          PARAMETERS_AND_SECRETS_EXTENSION_HTTP_PORT: '2773',
+          PARAMETERS_AND_SECRETS_EXTENSION_LOG_LEVEL: 'info',
+          PARAMETERS_AND_SECRETS_EXTENSION_MAX_CONNECTIONS: '3',
+          SECRETS_MANAGER_TIMEOUT_MILLIS: '0',
+          SECRETS_MANAGER_TTL: '300',
+          SSM_PARAMETER_STORE_TIMEOUT_MILLIS: '0',
+          SSM_PARAMETER_STORE_TTL: '300',
+        },
+      },
+    });
+    expect(() => app.synth()).not.toThrow();
+  });
+
+  test('can enable params and secrets from version in non-agnostic stack - arm64', () => {
+    // GIVEN
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, 'Stack');
+
+    // WHEN
+    new lambda.Function (stack, 'Function', {
+      functionName: 'lambda-function',
+      code: new lambda.InlineCode('foo'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_18_X,
+      architecture: lambda.Architecture.ARM_64,
+      paramsAndSecrets: {
+        layerVersion: lambda.ParamsAndSecretsLayerVersion.fromVersion(lambda.ParamsAndSecretsVersions.V4),
+      },
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::Lambda::Function', {
+      Layers: [
+        {
+          'Fn::FindInMap': [
+            'ParamsandsecretslayerMap',
+            {
+              Ref: 'AWS::Region',
+            },
+            '4xarm64',
+          ],
+        },
+      ],
+      Environment: {
+        Variables: {
+          PARAMETERS_AND_SECRETS_EXTENSION_CACHE_ENABLED: 'true',
+          PARAMETERS_AND_SECRETS_EXTENSION_CACHE_SIZE: '1000',
+          PARAMETERS_AND_SECRETS_EXTENSION_HTTP_PORT: '2773',
+          PARAMETERS_AND_SECRETS_EXTENSION_LOG_LEVEL: 'info',
+          PARAMETERS_AND_SECRETS_EXTENSION_MAX_CONNECTIONS: '3',
+          SECRETS_MANAGER_TIMEOUT_MILLIS: '0',
+          SECRETS_MANAGER_TTL: '300',
+          SSM_PARAMETER_STORE_TIMEOUT_MILLIS: '0',
+          SSM_PARAMETER_STORE_TTL: '300',
+        },
+      },
+    });
+    expect(() => app.synth()).not.toThrow();
+  });
+
+  test('can enable params and secrets from version with non-default config options', () => {
+    // GIVEN
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, 'Stack', { env: { region: 'us-west-2' } });
+
+    // WHEN
+    new lambda.Function (stack, 'Function', {
+      functionName: 'lambda-function',
+      code: new lambda.InlineCode('foo'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_18_X,
+      paramsAndSecrets: {
+        layerVersion: lambda.ParamsAndSecretsLayerVersion.fromVersion(lambda.ParamsAndSecretsVersions.V4, {
+          cacheEnabled: false,
+          cacheSize: 500,
+          httpPort: 8080,
+          logLevel: lambda.ParamsAndSecretsLogLevel.DEBUG,
+          maxConnections: 1,
+          secretsManagerTimeout: cdk.Duration.seconds(10),
+          secretsManagerTtl: cdk.Duration.seconds(250),
+          parameterStoreTimeout: cdk.Duration.seconds(10),
+          parameterStoreTtl: cdk.Duration.seconds(250),
+        }),
+      },
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::Lambda::Function', {
+      Layers: ['arn:aws:lambda:us-west-2:345057560386:layer:AWS-Parameters-and-Secrets-Lambda-Extension:4'],
+      Environment: {
+        Variables: {
+          PARAMETERS_AND_SECRETS_EXTENSION_CACHE_ENABLED: 'false',
+          PARAMETERS_AND_SECRETS_EXTENSION_CACHE_SIZE: '500',
+          PARAMETERS_AND_SECRETS_EXTENSION_HTTP_PORT: '8080',
+          PARAMETERS_AND_SECRETS_EXTENSION_LOG_LEVEL: 'debug',
+          PARAMETERS_AND_SECRETS_EXTENSION_MAX_CONNECTIONS: '1',
+          SECRETS_MANAGER_TIMEOUT_MILLIS: '10000',
+          SECRETS_MANAGER_TTL: '250',
+          SSM_PARAMETER_STORE_TIMEOUT_MILLIS: '10000',
+          SSM_PARAMETER_STORE_TTL: '250',
+        },
+      },
+    });
+    expect(() => app.synth()).not.toThrow();
+  });
+
+  // x86_64 is supported in all regions - we're just checking for arm64
+  test('throws for unsupported architecture in region', () => {
+    // GIVEN
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, 'Stack', { env: { region: 'eu-central-2' } });
+
+    // WHEN/THEN
+    expect(() => {
+      new lambda.Function (stack, 'Function', {
+        functionName: 'lambda-function',
+        code: new lambda.InlineCode('foo'),
+        handler: 'index.handler',
+        runtime: lambda.Runtime.NODEJS_18_X,
+        architecture: lambda.Architecture.ARM_64,
+        paramsAndSecrets: {
+          layerVersion: lambda.ParamsAndSecretsLayerVersion.fromVersion(lambda.ParamsAndSecretsVersions.V4),
+        },
+      });
+    }).toThrow('Parameters and Secrets Extension is not supported in region eu-central-2 for arm64 architecture');
   });
 });
