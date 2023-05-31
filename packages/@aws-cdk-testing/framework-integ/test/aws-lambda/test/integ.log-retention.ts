cd691810aa7d400b2d4c35aeb6f77d9b6e4dd40c
Francis <colifran@amazon.com>
2023-05-31 11:01:05 -0700

    added new function to handle setting tags on log group
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk-testing/framework-integ/test/aws-lambda/test/integ.log-retention.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-lambda/test/integ.log-retention.ts
@@ -6,12 +6,15 @@ const app = new cdk.App();
 
 const stack = new cdk.Stack(app, 'aws-cdk-lambda-log-retention');
 
-new lambda.Function(stack, 'OneWeek', {
+const fn = new lambda.Function(stack, 'OneWeek', {
   code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
   handler: 'index.handler',
   runtime: lambda.Runtime.NODEJS_14_X,
   logRetention: logs.RetentionDays.ONE_WEEK,
 });
+cdk.Tags.of(fn).add('env', 'dev');
+cdk.Tags.of(fn).remove('dept');
+cdk.Tags.of(fn).add('key', 'value');
 
 new lambda.Function(stack, 'OneMonth', {
   code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
