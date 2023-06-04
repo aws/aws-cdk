b5c9a132a684b169c8866111cfdf279bd5df1640
fynnfluegge <fynnfluegge@gmx.de>
2023-06-04 12:49:18 +0200

    fix integration tests

--- packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ.dependencies.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ.dependencies.ts
@@ -30,7 +30,7 @@ class SdkV2TestStack extends Stack {
 class SdkV3TestStack extends Stack {
   public lambdaFunction: IFunction
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, useLambdaProvidedSdk = false, props?: StackProps) {
     super(scope, id, props);
 
     // This function uses @aws-sdk/* but it will not be included
@@ -38,7 +38,7 @@ class SdkV3TestStack extends Stack {
       entry: path.join(__dirname, 'integ-handlers/dependencies-sdk-v3.ts'),
       runtime: Runtime.NODEJS_18_X,
       bundling: {
-        bundleLambdaProvidedAwsSdk: true,
+        useLambdaProvidedAwsSdk: useLambdaProvidedSdk,
       },
     });
   }
@@ -47,12 +47,13 @@ class SdkV3TestStack extends Stack {
 const app = new App();
 const sdkV2testCase = new SdkV2TestStack(app, 'cdk-integ-lambda-nodejs-dependencies');
 const sdkV3testCase = new SdkV3TestStack(app, 'cdk-integ-lambda-nodejs-dependencies-for-sdk-v3');
+const sdkV3testCaseWithLambdaProvidedSdk = new SdkV3TestStack(app, 'cdk-integ-lambda-nodejs-dependencies-for-sdk-v3', true);
 
 const integ = new IntegTest(app, 'LambdaDependencies', {
-  testCases: [sdkV2testCase, sdkV3testCase],
+  testCases: [sdkV2testCase, sdkV3testCase, sdkV3testCaseWithLambdaProvidedSdk],
 });
 
-for (const testCase of [sdkV2testCase, sdkV3testCase]) {
+for (const testCase of [sdkV2testCase, sdkV3testCase, sdkV3testCaseWithLambdaProvidedSdk]) {
   const response = integ.assertions.invokeFunction({
     functionName: testCase.lambdaFunction.functionName,
   });
