22c9036bda9d3d248b2862712fb449292571b2ab
Francis <colifran@amazon.com>
2023-06-07 15:14:01 -0700

    integ tests and snapshots
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk-testing/framework-integ/test/aws-autoscaling/test/integ.asg-lt-from-config.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-autoscaling/test/integ.asg-lt-from-config.ts
@@ -1,51 +1,99 @@
+#!/usr/bin/env node
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as cdk from 'aws-cdk-lib';
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 import { AUTOSCALING_DISABLE_LAUNCH_CONFIG } from 'aws-cdk-lib/cx-api';
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'aws-cdk-asg-lg-from-config');
-stack.node.setContext(AUTOSCALING_DISABLE_LAUNCH_CONFIG, true);
+const stack = new cdk.Stack(app, 'aws-cdk-asg-integ');
+stack.node.setContext(AUTOSCALING_DISABLE_LAUNCH_CONFIG, false);
 
-const blockDevices = [{
-  deviceName: 'ebs',
-  mappingEnabled: true,
-  volume: autoscaling.BlockDeviceVolume.ebs(15, {
-    deleteOnTermination: true,
-    encrypted: true,
-    volumeType: autoscaling.EbsDeviceVolumeType.IO1,
-    iops: 5000,
+const lt = new ec2.LaunchTemplate(stack, 'MainLT', {
+  instanceType: new ec2.InstanceType('t3.micro'),
+  machineImage: new ec2.AmazonLinuxImage({
+    generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+    cpuType: ec2.AmazonLinuxCpuType.X86_64,
   }),
-}, {
-  deviceName: 'ebs-snapshot',
-  volume: autoscaling.BlockDeviceVolume.ebsFromSnapshot('snapshot-id', {
-    volumeSize: 500,
-    deleteOnTermination: false,
-    volumeType: autoscaling.EbsDeviceVolumeType.SC1,
-  }),
-}];
-
-const userData = ec2.UserData.forLinux();
-
-const vpc = new ec2.Vpc(stack, 'VPC');
-const securityGroup = new ec2.SecurityGroup(stack, 'IntegSg', {
-  vpc,
-  allowAllIpv6Outbound: true,
 });
 
-new autoscaling.AutoScalingGroup(stack, 'MyFleet', {
-  machineImage: new ec2.AmazonLinuxImage(),
-  keyName: 'ec2-key-pair',
-  instanceType: ec2.InstanceType.of(ec2.InstanceClass.M4, ec2.InstanceSize.MICRO),
-  instanceMonitoring: autoscaling.Monitoring.DETAILED,
-  securityGroup,
-  userData,
-  associatePublicIpAddress: true,
-  spotPrice: '0.05',
-  blockDevices,
+const ltOverrideT4g = new ec2.LaunchTemplate(stack, 'T4gLT', {
+  instanceType: new ec2.InstanceType('t4g.micro'),
+  machineImage: new ec2.AmazonLinuxImage({
+    generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+    cpuType: ec2.AmazonLinuxCpuType.ARM_64,
+  }),
+});
+
+const vpc = new ec2.Vpc(stack, 'VPC', {
+  restrictDefaultSecurityGroup: false,
+  maxAzs: 2,
+});
+
+new autoscaling.AutoScalingGroup(stack, 'AsgFromLT', {
   vpc,
-  vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+  launchTemplate: lt,
+  minCapacity: 0,
+  maxCapacity: 10,
+  desiredCapacity: 5,
+});
+
+new autoscaling.AutoScalingGroup(stack, 'AsgWithDefaultInstanceWarmup', {
+  vpc,
+  launchTemplate: lt,
+  defaultInstanceWarmup: cdk.Duration.seconds(5),
+});
+
+new autoscaling.AutoScalingGroup(stack, 'AsgFromMip', {
+  vpc,
+  mixedInstancesPolicy: {
+    instancesDistribution: {
+      onDemandPercentageAboveBaseCapacity: 50,
+    },
+    launchTemplate: lt,
+    launchTemplateOverrides: [
+      { instanceType: new ec2.InstanceType('t3.micro') },
+      { instanceType: new ec2.InstanceType('t3a.micro') },
+      { instanceType: new ec2.InstanceType('t4g.micro'), launchTemplate: ltOverrideT4g },
+    ],
+  },
+  minCapacity: 0,
+  maxCapacity: 10,
+  desiredCapacity: 5,
+});
+
+new autoscaling.AutoScalingGroup(stack, 'AsgFromMipWithoutDistribution', {
+  vpc,
+  mixedInstancesPolicy: {
+    launchTemplate: lt,
+    launchTemplateOverrides: [
+      { instanceType: new ec2.InstanceType('t3.micro') },
+      { instanceType: new ec2.InstanceType('t3a.micro') },
+      { instanceType: new ec2.InstanceType('t4g.micro'), launchTemplate: ltOverrideT4g },
+    ],
+  },
+  minCapacity: 0,
+  maxCapacity: 10,
+  desiredCapacity: 5,
+});
+
+new autoscaling.AutoScalingGroup(stack, 'AsgWithGp3Blockdevice', {
+  minCapacity: 0,
+  maxCapacity: 10,
+  desiredCapacity: 5,
+  instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+  machineImage: new ec2.AmazonLinuxImage(),
+  blockDevices: [{
+    deviceName: 'ebs',
+    mappingEnabled: true,
+    volume: autoscaling.BlockDeviceVolume.ebs(15, {
+      deleteOnTermination: true,
+      encrypted: true,
+      volumeType: autoscaling.EbsDeviceVolumeType.GP3,
+      throughput: 125,
+    }),
+  }],
+  vpc,
 });
 
 new IntegTest(app, 'LaunchTemplateFromLaunchConfigPropsTest', {
