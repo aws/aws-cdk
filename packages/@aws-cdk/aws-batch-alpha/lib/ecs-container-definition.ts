b0c672b7a131f49eab31ad08bd385ba86a97c7e6
Sumu Pitchayan <35242245+sumupitchayan@users.noreply.github.com>
2023-06-08 14:57:15 -0400

    Update packages/@aws-cdk/aws-batch-alpha/lib/ecs-container-definition.ts
    Co-authored-by: Calvin Combs <66279577+comcalvi@users.noreply.github.com>

--- packages/@aws-cdk/aws-batch-alpha/lib/ecs-container-definition.ts
+++ packages/@aws-cdk/aws-batch-alpha/lib/ecs-container-definition.ts
@@ -939,7 +939,7 @@ export class EcsFargateContainerDefinition extends EcsContainerDefinitionBase im
       if (props.ephemeralStorageSize.toGibibytes() > 200) {
         throw new Error(`ECS Fargate container '${id}' specifies 'ephemeralStorageSize' at ${props.ephemeralStorageSize.toGibibytes()} > 200 GB`);
       } else if (props.ephemeralStorageSize.toGibibytes() < 21) {
-        throw new Error(`ECS Fargate container ${id} specifies ephemeralStorageSize at ${props.ephemeralStorageSize.toGibibytes()} < 21 GB`);
+        throw new Error(`ECS Fargate container '${id}' specifies 'ephemeralStorageSize' at ${props.ephemeralStorageSize.toGibibytes()} < 21 GB`);
       }
     }
   }
