302ddd89948347f82d6fd31b4e478fad1303c7be
Sumu <sumughan@amazon.com>
2023-06-13 12:50:04 -0400

    edit integ test and snapshots, adding assertion on lambda function
    Signed-off-by: Sumu <sumughan@amazon.com>

--- packages/@aws-cdk-testing/framework-integ/test/aws-apigateway/test/integ.parametrized-swagger.api.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-apigateway/test/integ.parametrized-swagger.api.ts
@@ -1,48 +1,58 @@
 import * as path from 'path';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as cdk from 'aws-cdk-lib';
-import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { IntegTest, ExpectedResult } from '@aws-cdk/integ-tests-alpha';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { DeployTimeSubstitutedFile } from 'aws-cdk-lib/aws-s3-deployment';
 
+let lambdaFunctionArn: string = '';
 class Test extends cdk.Stack {
   constructor(scope: cdk.App, id: string) {
     super(scope, id);
 
-    const handler = new lambda.Function(this, 'MyHandler', {
+    const hello = new lambda.Function(this, 'Hello', {
       runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromInline(`exports.handler = ${handlerCode}`),
       handler: 'index.handler',
+      code: lambda.Code.fromInline(`exports.handler = ${helloCode}`),
     });
+
+    lambdaFunctionArn = hello.functionArn;
 
     const bucket = new Bucket(this, 'Bucket');
     const deployment = new DeployTimeSubstitutedFile(this, 'Deployment', {
       source: path.join(__dirname, 'sample-definition-substitution.yaml'),
       destinationBucket: bucket,
       substitutions: {
-        testMethod: handler.functionName,
+        testMethod: hello.functionArn,
+        xxxx: hello.functionArn,
       },
     });
 
     new apigateway.SpecRestApi(this, 'sample-api-definition', {
       apiDefinition: apigateway.ApiDefinition.fromBucket(deployment.bucket, deployment.objectKey),
     });
-
-    function handlerCode(event: any, _: any, callback: any) {
-      return callback(undefined, {
-        isBase64Encoded: false,
-        statusCode: 200,
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(event),
-      });
-    }
   }
 }
 
 const app = new cdk.App();
+new Test(app, 'my-stack');
 
 const testCase = new Test(app, 'test-apigateway-parameterized-swagger');
-new IntegTest(app, 'apigateway-parameterized-swagger', {
+const integ = new IntegTest(app, 'apigateway-parameterized-swagger', {
   testCases: [testCase],
 });
+
+const invoke = integ.assertions.invokeFunction({
+  functionName: lambdaFunctionArn,
+});
+invoke.expect(ExpectedResult.objectLike({
+  StatusCode: 200,
+}));
+
+function helloCode(_event: any, _context: any, callback: any) {
+  return callback(undefined, {
+    statusCode: 200,
+    body: 'hello, world!',
+  });
+}
