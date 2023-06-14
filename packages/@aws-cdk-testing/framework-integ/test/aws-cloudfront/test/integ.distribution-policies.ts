9d51ceca01767ad384e28a58ac1f6077adfe7f07
auvred <aauvred@gmail.com>
2023-06-14 12:05:46 +0000

    test: use denyList in distribution-policies integ test

--- packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront/test/integ.distribution-policies.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront/test/integ.distribution-policies.ts
@@ -12,7 +12,9 @@ const cachePolicy = new cloudfront.CachePolicy(stack, 'CachePolicy', {
 
 const originRequestPolicy = new cloudfront.OriginRequestPolicy(stack, 'OriginRequestPolicy', {
   originRequestPolicyName: 'ACustomOriginRequestPolicy',
+  cookieBehavior: cloudfront.OriginRequestCookieBehavior.allowList('cookie1'),
   headerBehavior: cloudfront.OriginRequestHeaderBehavior.all('CloudFront-Forwarded-Proto'),
+  queryStringBehavior: cloudfront.OriginRequestQueryStringBehavior.denyList('querystringparam'),
 });
 
 const responseHeadersPolicy = new cloudfront.ResponseHeadersPolicy(stack, 'ResponseHeadersPolicy', {
