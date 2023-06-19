c8c74e176be866ba644892d1430762e13cadc47d
Francis <colifran@amazon.com>
2023-06-19 13:41:17 -0700

    updated lambda log retention integ test to check for lambda custom resource provider being given correct permissions after it is already created
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk-testing/framework-integ/test/aws-lambda/test/integ.log-retention.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-lambda/test/integ.log-retention.ts
@@ -6,23 +6,25 @@ const app = new cdk.App();
 
 const stack = new cdk.Stack(app, 'aws-cdk-lambda-log-retention');
 
-const fn = new lambda.Function(stack, 'OneWeek', {
+new lambda.Function(stack, 'OneWeek', {
   code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
   handler: 'index.handler',
   runtime: lambda.Runtime.NODEJS_14_X,
   logRetention: logs.RetentionDays.ONE_WEEK,
   functionName: 'OneWeekFunction',
-  propagateTagsToLogGroup: true,
 });
-cdk.Tags.of(fn).add('env', 'beta');
-cdk.Tags.of(fn).add('dept', 'eng');
 
-new lambda.Function(stack, 'OneMonth', {
+// propagating tags to log group here is testing that the lambda resource provider is updated
+// with correct permissions - otherwise the integ test will fail here
+const fn = new lambda.Function(stack, 'OneMonth', {
   code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
   handler: 'index.handler',
   runtime: lambda.Runtime.NODEJS_14_X,
   logRetention: logs.RetentionDays.ONE_MONTH,
+  propagateTagsToLogGroup: true,
 });
+cdk.Tags.of(fn).add('env', 'beta');
+cdk.Tags.of(fn).add('dept', 'eng');
 
 new lambda.Function(stack, 'OneYear', {
   code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
