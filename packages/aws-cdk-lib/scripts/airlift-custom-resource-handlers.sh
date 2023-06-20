79318d03f4734fa367530b82da3687c8de03f199
Kaizen Conroy <zen.conroy@gmail.com>
2023-06-20 16:49:45 -0400

    update integ test for appstagingsynth

--- packages/aws-cdk-lib/scripts/airlift-custom-resource-handlers.sh
+++ packages/aws-cdk-lib/scripts/airlift-custom-resource-handlers.sh
@@ -6,7 +6,7 @@ awscdklibdir=${scriptdir}/..
 
 list_custom_resources() {
   for file in $customresourcedir/lib/*/*/index.js; do
-    echo $file | rev | cut -d "/" -f -4 | rev
+    echo $file | rev | cut -d "/" -f 2-4 | rev
   done
 }
 
@@ -19,5 +19,5 @@ mkdir -p $awscdklibdir/custom-resource-handlers
 
 for cr in $customresources; do
   mkdir -p $awscdklibdir/custom-resource-handlers/$cr
-  cp $customresourcedir/$cr $awscdklibdir/custom-resource-handlers/$cr
+  cp $customresourcedir/$cr/index.js $awscdklibdir/custom-resource-handlers/$cr
 done
