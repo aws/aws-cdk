4a569a0d4438949cfc22593703ac079163182a8c
watany <76135106+watany-dev@users.noreply.github.com>
2023-06-23 11:49:48 +0000

    oracle 19

--- packages/aws-cdk-lib/aws-rds/lib/instance-engine.ts
+++ packages/aws-cdk-lib/aws-rds/lib/instance-engine.ts
@@ -1449,6 +1449,8 @@ export class OracleEngineVersion {
   public static readonly VER_19_0_0_0_2022_10_R1 = OracleEngineVersion.of('19.0.0.0.ru-2022-10.rur-2022-10.r1', '19');
   /** Version "19.0.0.0.ru-2023-01.rur-2023-01.r1". */
   public static readonly VER_19_0_0_0_2023_01_R1 = OracleEngineVersion.of('19.0.0.0.ru-2023-01.rur-2023-01.r1', '19');
+  /** Version "19.0.0.0.ru-2023-04.rur-2023-04.r1". */
+  public static readonly VER_19_0_0_0_2023_04_R1 = OracleEngineVersion.of('19.0.0.0.ru-2023-04.rur-2023-04.r1', '19');
 
   /** Version "21" (only a major version, without a specific minor version). */
   public static readonly VER_21 = OracleEngineVersion.of('21', '21');
