e3b59ed7983a1e9616c769a26fbb3ee9346da963
Shailja Khurana <khushail@amazon.com>
2023-06-23 18:39:24 +0000

    added loadbalancer in test case

--- packages/aws-cdk-lib/aws-ec2/test/integ.vpc-endpoint-service.ts
+++ packages/aws-cdk-lib/aws-ec2/test/integ.vpc-endpoint-service.ts
@@ -1,14 +1,26 @@
 import * as cdk from '../../core';
 import * as ec2 from '../lib';
+import { IVpcEndpointServiceLoadBalancer } from '../lib';
 
 const app = new cdk.App();
+
+class DummyEndpointLoadBalacer implements IVpcEndpointServiceLoadBalancer {
+  /**
+   * The ARN of the load balancer that hosts the VPC Endpoint Service
+   */
+  public readonly loadBalancerArn: string;
+  constructor(arn: string) {
+    this.loadBalancerArn = arn;
+  }
+}
 
 class TestStack extends cdk.Stack {
   constructor(scope:cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const lb = new DummyEndpointLoadBalacer('arn:aws:elasticloadbalancing:us-east-1:384434886796:loadbalancer/net/Test/testLB');
     new ec2.VpcEndpointService(this, 'vpcEndpointService', {
-      vpcEndpointServiceLoadBalancers: [],
+      vpcEndpointServiceLoadBalancers: [lb],
       acceptanceRequired: true,
       contributorInsightsEnabled: true,
     });
