49484e3ee4d40a59e480ec5574a419f1d55d0aa8
Steven Tan <git@sktan.com>
2023-06-26 17:06:10 +0000

    Fixing tests

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-codebuild/test/integ.project-amazonlinux-image.lit.ts
@@ -0,0 +1,32 @@
+/// !cdk-integ *
+import { App, Stack } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { BuildSpec, LinuxBuildImage, Project } from 'aws-cdk-lib/aws-codebuild';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+class AmazonLinuxImageTestStack extends Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    /// !show
+    new Project(this, 'MyProject', {
+      buildSpec: BuildSpec.fromObject({
+        version: '0.2',
+      }),
+      environment: {
+        buildImage: LinuxBuildImage.AMAZON_LINUX_2_5,
+      },
+    });
+    /// !hide
+  }
+}
+
+const app = new App();
+
+const codebuildamazonlinux25 = new AmazonLinuxImageTestStack(app, 'codebuild-project-amazonlinux-2-5');
+
+// THEN
+new IntegTest(app, 'amazon-linux-2-5-codebuild', {
+  testCases: [codebuildamazonlinux25],
+  stackUpdateWorkflow: false,
+});
