319618abbdf45bcb608e5fb3e5e75683a5778458
Francis <colifran@amazon.com>
2023-07-05 12:15:49 -0700

    updated log-retention integ test to add assertions
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk-testing/framework-integ/test/aws-lambda/test/integ.log-retention.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-lambda/test/integ.log-retention.ts
@@ -1,39 +1,51 @@
 import * as logs from 'aws-cdk-lib/aws-logs';
+import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 
 const app = new cdk.App();
 
-const stack = new cdk.Stack(app, 'aws-cdk-lambda-log-retention');
+class TestStack extends cdk.Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-new lambda.Function(stack, 'OneWeek', {
-  code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
-  handler: 'index.handler',
-  runtime: lambda.Runtime.NODEJS_14_X,
-  logRetention: logs.RetentionDays.ONE_WEEK,
+    new lambda.Function(this, 'OneWeek', {
+      code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_14_X,
+      logRetention: logs.RetentionDays.ONE_WEEK,
+    });
+
+    // propagating tags to log group here is testing that the lambda resource provider is updated
+    // with correct permissions - otherwise the integ test will fail here
+    const oneMonthFunction = new lambda.Function(this, 'OneMonth', {
+      code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_14_X,
+      logRetention: logs.RetentionDays.ONE_MONTH,
+      functionName: 'OneMonthFunction',
+      propagateTagsToLogGroup: true,
+    });
+    cdk.Tags.of(oneMonthFunction).add('env', 'prod');
+    cdk.Tags.of(oneMonthFunction).add('dept', 'sales');
+
+    const oneYearFunction = new lambda.Function(this, 'OneYear', {
+      code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_14_X,
+      logRetention: logs.RetentionDays.ONE_YEAR,
+      functionName: 'OneYearFunction',
+      propagateTagsToLogGroup: true,
+    });
+    cdk.Tags.of(oneYearFunction).add('dept', 'eng');
+  }
+}
+
+const integTest = new IntegTest(app, 'aws-cdk-integ-lambda-log-retention', {
+  testCases: [new TestStack(app, 'aws-cdk-lambda-log-retention')],
 });
 
-// propagating tags to log group here is testing that the lambda resource provider is updated
-// with correct permissions - otherwise the integ test will fail here
-const oneMonthFunction = new lambda.Function(stack, 'OneMonth', {
-  code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
-  handler: 'index.handler',
-  runtime: lambda.Runtime.NODEJS_14_X,
-  logRetention: logs.RetentionDays.ONE_MONTH,
-  functionName: 'OneMonthFunction',
-  propagateTagsToLogGroup: true,
+integTest.assertions.awsApiCall('cloudwatch-logs', 'listTagsForResource', {
+  resourceArn: 'arn:aws:logs:us-east-1:586193817576:log-group:/aws/lambda/OneWeek',
 });
-cdk.Tags.of(oneMonthFunction).add('env', 'prod');
-cdk.Tags.of(oneMonthFunction).add('dept', 'sales', { excludeResourceTypes: ['AWS::Logs::LogGroup'] });
-
-const oneYearFunction = new lambda.Function(stack, 'OneYear', {
-  code: new lambda.InlineCode('exports.handler = (event) => console.log(JSON.stringify(event));'),
-  handler: 'index.handler',
-  runtime: lambda.Runtime.NODEJS_14_X,
-  logRetention: logs.RetentionDays.ONE_YEAR,
-  functionName: 'OneYearFunction',
-  propagateTagsToLogGroup: true,
-});
-cdk.Tags.of(oneYearFunction).add('dept', 'eng');
-
-app.synth();
