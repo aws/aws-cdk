d61adaeda4eeb112060bc6e68380c4a16554a4ab
Mitchell Valine <mitchellvaline@yahoo.com>
2023-07-06 14:17:25 -0700

    fix(integ-tests-alpha): incorrect sdk client resolution
    Fixes some issues with finding and calling v3 sdk clients.
    
    Exports some utilities from `aws-cdk-lib.AwsCustomResource` for finding
    and calling `@aws-sdk/client-*` modules to avoid duplicating this logic.

---
+++ packages/aws-cdk-lib/custom-resources/lib/aws-custom-resource/runtime/utils.ts
@@ -0,0 +1,2 @@
+export { getV3ClientPackageName } from './aws-sdk-v3-handler/v2-to-v3/get-v3-client-package-name';
+export { findV3ClientConstructor } from './aws-sdk-v3-handler/v2-to-v3/find-client-constructor';
