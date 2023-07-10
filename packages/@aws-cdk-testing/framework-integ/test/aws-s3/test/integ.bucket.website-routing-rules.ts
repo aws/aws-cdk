67870f3a95d2e7d834d5e3ce9458582cad4cbe8d
Momo Kornher <kornherm@amazon.co.uk>
2023-07-10 15:24:10 +0100

    Add integration test

--- packages/@aws-cdk-testing/framework-integ/test/aws-s3/test/integ.bucket.website-routing-rules.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-s3/test/integ.bucket.website-routing-rules.ts
@@ -5,16 +5,16 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 
 const app = new cdk.App();
 
-const stack = new cdk.Stack(app, 'aws-cdk-s3');
+const stack = new cdk.Stack(app, 's3-bucket-routing');
 
 new s3.Bucket(stack, 'MyBucketWithRoutingRules', {
   removalPolicy: cdk.RemovalPolicy.DESTROY,
-  websiteIndexDocument: "index.html",
+  websiteIndexDocument: 'index.html',
   websiteRoutingRules: [{
-    condition: { keyPrefixEquals: "" },
-    hostName: "example.com",
+    condition: { keyPrefixEquals: '' },
+    hostName: 'example.com',
     protocol: s3.RedirectProtocol.HTTPS,
-    replaceKey: "/",
+    replaceKey: s3.ReplaceKey.with('/'),
   }],
 });
 
