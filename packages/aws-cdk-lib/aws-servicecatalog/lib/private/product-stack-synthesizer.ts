9de2738ca65e15b148ab67cb508a92d03fb53ab4
Francis <colifran@amazon.com>
2023-07-09 17:58:14 -0700

    refactored addFileAsset method in product stack synthesizer to generate correct FileAssetLocation
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/aws-servicecatalog/lib/private/product-stack-synthesizer.ts
+++ packages/aws-cdk-lib/aws-servicecatalog/lib/private/product-stack-synthesizer.ts
@@ -22,16 +22,29 @@ export class ProductStackSynthesizer extends cdk.StackSynthesizer {
       throw new Error('An Asset Bucket must be provided to use Assets');
     }
 
-    const source = this.createSource(asset);
+    let source: ISource;
+    let objectKey: string;
+    if (asset.isTemplateFile) {
+      const location = this.parentDeployment.addFileAsset(asset);
+      if (!this.parentAssetBucket) {
+        this.parentAssetBucket = Bucket.fromBucketName(this.boundStack, 'ParentAssetBucket', location.bucketName);
+      }
+      objectKey = location.objectKey;
+      source = Source.bucket(this.parentAssetBucket, location.objectKey);
+    } else {
+      const outdir = cdk.App.of(this.boundStack)?.outdir ?? 'cdk.out';
+      const assetPath = `${outdir}/${asset.fileName}`;
+      if (!asset.fileName) {
+        throw new Error('Asset file name is undefined');
+      }
+      const assetFileBaseName = path.basename(asset.fileName);
+      const s3Filename = assetFileBaseName.split('.')[1] + '.zip';
+      objectKey = `${s3Filename}`;
+      source = Source.asset(assetPath);
+    }
     this.sources.push(source);
 
     const bucketName = this.physicalNameOfBucket(this.assetBucket);
-    if (!asset.fileName) {
-      throw new Error('Asset file name is undefined');
-    }
-    const assetFileBaseName = path.basename(asset.fileName);
-    const s3Filename = assetFileBaseName.split('.')[1] + '.zip';
-    const objectKey = `${s3Filename}`;
     const s3ObjectUrl = `s3://${bucketName}/${objectKey}`;
     const httpUrl = `https://s3.${bucketName}/${objectKey}`;
 
@@ -49,22 +62,6 @@ export class ProductStackSynthesizer extends cdk.StackSynthesizer {
       throw new Error('A bucketName must be provided to use Assets');
     }
     return resolvedName;
-  }
-
-  private createSource(asset: cdk.FileAssetSource) {
-    let source: ISource;
-    if (asset.isTemplateFile) {
-      const location = this.parentDeployment.addFileAsset(asset);
-      if (!this.parentAssetBucket) {
-        this.parentAssetBucket = Bucket.fromBucketName(this.boundStack, 'ParentAssetBucket', location.bucketName);
-      }
-      source = Source.bucket(this.parentAssetBucket, location.objectKey);
-    } else {
-      const outdir = cdk.App.of(this.boundStack)?.outdir ?? 'cdk.out';
-      const assetPath = `${outdir}/${asset.fileName}`;
-      source = Source.asset(assetPath);
-    }
-    return source;
   }
 
   private createDeploymentBucket() {
