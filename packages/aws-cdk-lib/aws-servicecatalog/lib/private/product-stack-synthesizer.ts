8ea7bcb7359664da3cd4f27691c9d144c75c2338
Francis <colifran@amazon.com>
2023-07-11 08:39:59 -0700

    minor formatting changes
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/aws-servicecatalog/lib/private/product-stack-synthesizer.ts
+++ packages/aws-cdk-lib/aws-servicecatalog/lib/private/product-stack-synthesizer.ts
@@ -16,13 +16,15 @@ export interface ProductStackSynthesizerProps {
 
   /**
    * A ServerSideEncryption can be enabled to encrypt assets that are put into assetBucket
-   * @default No encryption is used
+   *
+   * @default - No encryption is used
    */
   readonly serverSideEncryption? : ServerSideEncryption;
 
   /**
    * For AWS_KMS ServerSideEncryption a KMS KeyId must be provided which will be used to encrypt assets
-   * @default No KMS KeyId and SSE_KMS encryption cannot be used
+   *
+   * @default - No KMS KeyId and SSE_KMS encryption cannot be used
    */
   readonly serverSideEncryptionAwsKmsKeyId? : string;
 }
