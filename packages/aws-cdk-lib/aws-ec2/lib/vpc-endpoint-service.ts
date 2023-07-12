ddb3e70b7fef8973a64a03bf9f378fe794e8e1e3
Shailja Khurana <117320115+khushail@users.noreply.github.com>
2023-07-12 09:02:47 -0700

    Update packages/aws-cdk-lib/aws-ec2/lib/vpc-endpoint-service.ts
    Co-authored-by: Pahud Hsieh <pahudnet@gmail.com>

--- packages/aws-cdk-lib/aws-ec2/lib/vpc-endpoint-service.ts
+++ packages/aws-cdk-lib/aws-ec2/lib/vpc-endpoint-service.ts
@@ -101,7 +101,7 @@ export class VpcEndpointService extends Resource implements IVpcEndpointService 
 
     this.vpcEndpointServiceLoadBalancers = props.vpcEndpointServiceLoadBalancers;
     this.acceptanceRequired = props.acceptanceRequired ?? true;
-    this.contributorInsightsEnabled = props.contributorInsightsEnabled ?? true;
+    this.contributorInsightsEnabled = props.contributorInsights;
 
     if (props.allowedPrincipals && props.whitelistedPrincipals) {
       throw new Error('`whitelistedPrincipals` is deprecated; please use `allowedPrincipals` instead');
