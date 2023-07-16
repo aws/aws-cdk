402c7116d79b32b15806f03e05ccc6baae2fb377
Momo Kornher <kornherm@amazon.co.uk>
2023-07-16 14:58:28 +0100

    chore(cdk-lib): re-enable skipped tests (#26367)
    Some tests were previously skipped. Un-skip them and put a linter rule in place that prevents this.
    
    Closes #25137 
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-lambda-nodejs/test/util.test.ts
+++ packages/aws-cdk-lib/aws-lambda-nodejs/test/util.test.ts
@@ -12,64 +12,64 @@ describe('callsites', () => {
 });
 
 describe('findUp', () => {
-  test.skip('Starting at process.cwd()', () => {
+  test('Starting at process.cwd()', () => {
     expect(findUp('README.md')).toMatch(/aws-cdk-lib\/README.md$/);
   });
 
-  test.skip('Non existing file', () => {
+  test('Non existing file', () => {
     expect(findUp('non-existing-file.unknown')).toBe(undefined);
   });
 
-  test.skip('Starting at a specific path', () => {
+  test('Starting at a specific path', () => {
     expect(findUp('util.test.ts', path.join(__dirname, 'integ-handlers'))).toMatch(/aws-lambda-nodejs\/test\/util.test.ts$/);
   });
 
-  test.skip('Non existing file starting at a non existing relative path', () => {
+  test('Non existing file starting at a non existing relative path', () => {
     expect(findUp('not-to-be-found.txt', 'non-existing/relative/path')).toBe(undefined);
   });
 
-  test.skip('Starting at a relative path', () => {
+  test('Starting at a relative path', () => {
     expect(findUp('util.test.ts', 'aws-lambda-nodejs/test/integ-handlers')).toMatch(/aws-lambda-nodejs\/test\/util.test.ts$/);
   });
 });
 
 describe('findUpMultiple', () => {
-  test.skip('Starting at process.cwd()', () => {
+  test('Starting at process.cwd()', () => {
     const files = findUpMultiple(['README.md', 'package.json']);
     expect(files).toHaveLength(2);
     expect(files[0]).toMatch(/aws-cdk-lib\/README\.md$/);
     expect(files[1]).toMatch(/aws-cdk-lib\/package\.json$/);
   });
 
-  test.skip('Non existing files', () => {
+  test('Non existing files', () => {
     expect(findUpMultiple(['non-existing-file.unknown', 'non-existing-file.unknown2'])).toEqual([]);
   });
 
-  test.skip('Existing and non existing files', () => {
+  test('Existing and non existing files', () => {
     const files = findUpMultiple(['non-existing-file.unknown', 'README.md']);
     expect(files).toHaveLength(1);
     expect(files[0]).toMatch(/aws-cdk-lib\/README\.md$/);
   });
 
-  test.skip('Starting at a specific path', () => {
+  test('Starting at a specific path', () => {
     const files = findUpMultiple(['util.test.ts', 'function.test.ts'], path.join(__dirname, 'integ-handlers'));
     expect(files).toHaveLength(2);
     expect(files[0]).toMatch(/aws-lambda-nodejs\/test\/util\.test\.ts$/);
     expect(files[1]).toMatch(/aws-lambda-nodejs\/test\/function\.test\.ts$/);
   });
 
-  test.skip('Non existing files starting at a non existing relative path', () => {
+  test('Non existing files starting at a non existing relative path', () => {
     expect(findUpMultiple(['not-to-be-found.txt', 'not-to-be-found2.txt'], 'non-existing/relative/path')).toEqual([]);
   });
 
-  test.skip('Starting at a relative path', () => {
+  test('Starting at a relative path', () => {
     const files = findUpMultiple(['util.test.ts', 'function.test.ts'], 'aws-lambda-nodejs/test/integ-handlers');
     expect(files).toHaveLength(2);
     expect(files[0]).toMatch(/aws-lambda-nodejs\/test\/util\.test\.ts$/);
     expect(files[1]).toMatch(/aws-lambda-nodejs\/test\/function\.test\.ts$/);
   });
 
-  test.skip('Files on multiple levels', () => {
+  test('Files on multiple levels', () => {
     const files = findUpMultiple(['README.md', 'util.test.ts'], path.join(__dirname, 'integ-handlers'));
     expect(files).toHaveLength(1);
     expect(files[0]).toMatch(/aws-lambda-nodejs\/test\/util\.test\.ts$/);
@@ -77,7 +77,7 @@ describe('findUpMultiple', () => {
 });
 
 describe('exec', () => {
-  test.skip('normal execution', () => {
+  test('normal execution', () => {
     const spawnSyncMock = jest.spyOn(child_process, 'spawnSync').mockReturnValue({
       status: 0,
       stderr: Buffer.from('stderr'),
@@ -103,7 +103,7 @@ describe('exec', () => {
     spawnSyncMock.mockRestore();
   });
 
-  test.skip('non zero status', () => {
+  test('non zero status', () => {
     const spawnSyncMock = jest.spyOn(child_process, 'spawnSync').mockReturnValue({
       status: 999,
       stderr: Buffer.from('error occured'),
@@ -118,7 +118,7 @@ describe('exec', () => {
     spawnSyncMock.mockRestore();
   });
 
-  test.skip('with error', () => {
+  test('with error', () => {
     const spawnSyncMock = jest.spyOn(child_process, 'spawnSync').mockReturnValue({
       error: new Error('bad error'),
       status: 0,
@@ -136,7 +136,7 @@ describe('exec', () => {
 });
 
 describe('extractDependencies', () => {
-  test.skip('with dependencies referenced in package.json', () => {
+  test('with dependencies referenced in package.json', () => {
     const deps = extractDependencies(
       path.join(__dirname, 'testpackage.json'),
       ['@aws-cdk/aws-lambda', '@aws-cdk/core'],
@@ -147,7 +147,7 @@ describe('extractDependencies', () => {
     ]);
   });
 
-  test.skip('with transitive dependencies', () => {
+  test('with transitive dependencies', () => {
     expect(extractDependencies(
       path.join(__dirname, 'testpackage.json'),
       ['typescript'],
@@ -157,14 +157,14 @@ describe('extractDependencies', () => {
     });
   });
 
-  test.skip('with unknown dependency', () => {
+  test('with unknown dependency', () => {
     expect(() => extractDependencies(
       path.join(__dirname, 'testpackage.json'),
       ['unknown'],
     )).toThrow(/Cannot extract version for module 'unknown'/);
   });
 
-  test.skip('with file dependency', () => {
+  test('with file dependency', () => {
     const pkgPath = path.join(__dirname, 'package-file.json');
     fs.writeFileSync(pkgPath, JSON.stringify({
       dependencies: {
