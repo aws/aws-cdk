df2a50483c7c38a277b8961ab14c14b21b3e4a42
Momo Kornher <kornherm@amazon.co.uk>
2023-07-20 17:59:54 +0100

    chore: fix aws-lambda-nodejs test flakiness (#26448)
    Uses a fake file system in `aws-lambda-nodejs` tests to avoid intermittent test failures when attempting to access the real file system.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-lambda-nodejs/test/integ-handlers/dependencies-sdk-v3.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-import { S3Client } from '@aws-sdk/client-s3';
-
-const s3 = new S3Client({});
-
-export async function handler() {
-  console.log(s3);
-}
--- packages/aws-cdk-lib/aws-lambda-nodejs/test/integ-handlers/esm.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-console */
-import * as crypto from 'crypto';
-
-export async function handler() {
-  console.log(crypto.createHash('sha512').update('cdk').digest('hex'));
-}
