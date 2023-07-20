d5263752f93d841176bdca4b126fd1a7b028d2cb
Parker <69879391+scanlonp@users.noreply.github.com>
2023-07-20 15:31:53 -0700

    fix: add error check for too many scalingSteps

--- packages/aws-cdk-lib/aws-autoscaling/test/scaling.test.ts
+++ packages/aws-cdk-lib/aws-autoscaling/test/scaling.test.ts
@@ -5,6 +5,7 @@ import * as ec2 from '../../aws-ec2';
 import * as elbv2 from '../../aws-elasticloadbalancingv2';
 import * as cdk from '../../core';
 import * as autoscaling from '../lib';
+import { ScalingInterval } from '../lib';
 
 describe('scaling', () => {
   describe('target tracking policies', () => {
@@ -324,6 +325,61 @@ test('step scaling with evaluation period configured', () => {
     MetricName: 'Metric',
     Namespace: 'Test',
     Threshold: 100,
+  });
+});
+
+describe('step-scaling-policy scalingSteps length validation checks', () => {
+  test('scalingSteps must have at least 2 steps', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const fixture = new ASGFixture(stack, 'Fixture');
+
+    expect(() => {
+      fixture.asg.scaleOnMetric('Metric', {
+        metric: new cloudwatch.Metric({
+          metricName: 'Legs',
+          namespace: 'Henk',
+          dimensionsMap: { Mustache: 'Bushy' },
+        }),
+        estimatedInstanceWarmup: cdk.Duration.seconds(150),
+        // only one scaling step throws an error
+        scalingSteps: [
+          { lower: 0, upper: 2, change: +1 },
+        ],
+      });
+    }).toThrow(/must supply at least 2/);
+  });
+
+  test('scalingSteps has a maximum of 40 steps', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+    const fixture = new ASGFixture(stack, 'Fixture');
+
+    const numSteps = 41;
+    const messagesPerTask = 20;
+    let steps: ScalingInterval[] = [];
+
+    for (let i = 0; i < numSteps; ++i) {
+      const step: ScalingInterval = {
+        lower: i * messagesPerTask,
+        upper: i * (messagesPerTask + 1) - 1,
+        change: i + 1,
+      };
+      steps.push(step);
+    }
+
+    expect(() => {
+      fixture.asg.scaleOnMetric('Metric', {
+        metric: new cloudwatch.Metric({
+          metricName: 'Legs',
+          namespace: 'Henk',
+          dimensionsMap: { Mustache: 'Bushy' },
+        }),
+        estimatedInstanceWarmup: cdk.Duration.seconds(150),
+        // only one scaling step throws an error
+        scalingSteps: steps,
+      });
+    }).toThrow(/must have less than 40/);
   });
 });
 
