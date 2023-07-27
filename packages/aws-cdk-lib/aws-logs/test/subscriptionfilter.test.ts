7ae28ca835fc4511a298d8fe274225760659dab5
Tatsuya Mori <tamori@amazon.com>
2023-07-28 02:55:40 +0900

    feat(logs): configure custom subscription filter name (#26498)
    Currently, we can't set the subscription filter name as a prop for L2 SubscriptionFilter construct. This PR introduces the new prop `filterName`. This will let us set a specific name without requiring escape hatches.
    
    Closes #26485
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-logs/test/subscriptionfilter.test.ts
+++ packages/aws-cdk-lib/aws-logs/test/subscriptionfilter.test.ts
@@ -23,6 +23,28 @@ describe('subscription filter', () => {
       LogGroupName: { Ref: 'LogGroupF5B46931' },
     });
   });
+
+  test('specifying custom name', () => {
+    // GIVEN
+    const stack = new Stack();
+    const logGroup = new LogGroup(stack, 'LogGroup');
+
+    // WHEN
+    new SubscriptionFilter(stack, 'Subscription', {
+      logGroup,
+      destination: new FakeDestination(),
+      filterPattern: FilterPattern.literal('some pattern'),
+      filterName: 'CustomSubscriptionFilterName',
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::Logs::SubscriptionFilter', {
+      DestinationArn: 'arn:bogus',
+      FilterPattern: 'some pattern',
+      LogGroupName: { Ref: 'LogGroupF5B46931' },
+      FilterName: 'CustomSubscriptionFilterName',
+    });
+  });
 });
 
 class FakeDestination implements ILogSubscriptionDestination {
