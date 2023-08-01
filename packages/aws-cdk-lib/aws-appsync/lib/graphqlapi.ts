073316451895827cf58aff9e24e0b1701e530708
Brendan H <brendanhonea@gmail.com>
2023-08-01 18:01:23 -0400

    docs(appsync): fix outdated documentation (#26591)
    Documentation for appsync GraphqlApi schema property was still referring to older method of importing schema file. This small change updates the documentation to use the correct method
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-appsync/lib/graphqlapi.ts
+++ packages/aws-cdk-lib/aws-appsync/lib/graphqlapi.ts
@@ -314,7 +314,7 @@ export interface GraphqlApiProps {
   /**
    * GraphQL schema definition. Specify how you want to define your schema.
    *
-   * Schema.fromFile(filePath: string) allows schema definition through schema.graphql file
+   * SchemaFile.fromAsset(filePath: string) allows schema definition through schema.graphql file
    *
    * @default - schema will be generated code-first (i.e. addType, addObjectType, etc.)
    *
