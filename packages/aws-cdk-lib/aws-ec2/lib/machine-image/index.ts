61ae00b31710c3ccd83214b172cddc3a4348b652
Rizbir Khan <rizbir.khan@cgi.com>
2023-08-02 14:18:48 +0000

    reverting machine-image

--- packages/aws-cdk-lib/aws-ec2/lib/machine-image/index.ts
+++ packages/aws-cdk-lib/aws-ec2/lib/machine-image/index.ts
@@ -1,6 +1,5 @@
+export * from './common';
+export * from './amazon-linux2';
+export * from './machine-image';
 export * from './amazon-linux-2022';
 export * from './amazon-linux-2023';
-export * from './amazon-linux2';
-export * from './common';
-export * from './generic-machine-image';
-
