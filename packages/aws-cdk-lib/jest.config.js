0d3c71c3a03f601ab30a0ece11fed5b3ab7a3831
Rico Huijbers <rix0rrr@gmail.com>
2023-08-04 11:44:07 +0200

    Try to prevent ENOMEM on CodeBuild Projects by limiting workers

--- packages/aws-cdk-lib/jest.config.js
+++ packages/aws-cdk-lib/jest.config.js
@@ -1,5 +1,7 @@
 const baseConfig = require('@aws-cdk/cdk-build-tools/config/jest.config');
 
+const cpuCount = require('os').cpus().length;
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   ...baseConfig,
@@ -7,7 +9,9 @@ module.exports = {
     'js',
     'ts',
   ],
-  maxWorkers: '50%',
+  // Limit workers to a reasonable fixed number. If we scale in the number of available CPUs, we will explode
+  // our memory limit on the CodeBuild instance that has 72 CPUs.
+  maxWorkers: Math.min(16, cpuCount - 1),
   preset: 'ts-jest',
   testMatch: [
     '<rootDir>/**/test/**/?(*.)+(test).ts',
