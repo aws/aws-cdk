56bcc8a384e3451bdcdcb1e68151a44014f0f967
Will <waormd@gmail.com>
2023-08-09 17:07:28 +0100

    chore(ground-station): Scaffold AWS Ground Station alpha (#26683)
    As per title - adding the scaffold to begin work on Ground Station alpha constructs.
    This work has been discussed with bar raiser @TheRealAmazonKendra
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-groundstation-alpha/.eslintrc.js
@@ -0,0 +1,8 @@
+const baseConfig = require('@aws-cdk/cdk-build-tools/config/eslintrc');
+baseConfig.parserOptions.project = __dirname + '/tsconfig.json';
+
+baseConfig.rules['import/no-extraneous-dependencies'] = ['error', { devDependencies: true, peerDependencies: true } ];
+baseConfig.rules['import/order'] = 'off';
+baseConfig.rules['@aws-cdk/invalid-cfn-imports'] = 'off';
+
+module.exports = baseConfig;
