400a97b0ff5d3fee587ec6627453b4482f6594e6
RogerZhang <ruojiazh@amazon.com>
2023-08-11 23:58:42 +0000

    fix type

--- packages/aws-cdk-lib/aws-lambda/lib/snapstart-management.ts
+++ packages/aws-cdk-lib/aws-lambda/lib/snapstart-management.ts
@@ -1,7 +1,13 @@
-
+import { CfnFunction } from './lambda.generated';
 export class SnapStartConfig {
-  
-  public static readonly ON_PUBLISHED_VERSIONS = { applyOn: 'PublishedVersions' };
-  
-  
+
+  public static readonly ON_PUBLISHED_VERSIONS= new SnapStartConfig('PublishedVersions');
+
+  readonly snapConfig: CfnFunction.SnapStartProperty;
+
+  protected constructor(public readonly applyValue: string) {
+    this.snapConfig = {
+      applyOn: applyValue,
+    };
+  }
 }
