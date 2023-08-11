83093ab0548cac7d8afb663f029d5aabb3784103
Filipp Fediakov <fediakovphilip@gmail.com>
2023-08-11 10:04:58 +0000

    Add group to ISchedule

--- packages/@aws-cdk/aws-scheduler-alpha/lib/schedule.ts
+++ packages/@aws-cdk/aws-scheduler-alpha/lib/schedule.ts
@@ -1,9 +1,11 @@
 import { IResource } from 'aws-cdk-lib';
+import { IGroup } from './group';
 
 /**
  * Interface representing a created or an imported `Schedule`.
  */
 export interface ISchedule extends IResource {
   readonly scheduleName: string;
+  readonly group?: IGroup;
   readonly scheduleArn: string;
 }
