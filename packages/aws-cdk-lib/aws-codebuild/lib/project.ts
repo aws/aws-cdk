1e0fb9f12d421596f147fb35cba3cbd993750793
Pahud Hsieh <pahudnet@gmail.com>
2023-08-17 17:41:36 +0000

    update

--- packages/aws-cdk-lib/aws-codebuild/lib/project.ts
+++ packages/aws-cdk-lib/aws-codebuild/lib/project.ts
@@ -616,8 +616,16 @@ export interface CommonProjectProps {
   /**
    * Where to place the network interfaces within the VPC.
    *
-   * Only used if 'vpc' is supplied. If you have no PRIVATE_WITH_EGRESS subnets in the specified `vpc`, leaving this
-   * undefined will require VPC endpoints to access AWS services.
+   * To access AWS services, your CodeBuild project needs to be in one of the following types of subnets:
+   *
+   * 1. Subnets with access to the internet (of type PRIVATE_WITH_EGRESS).
+   * 2. Private subnets unconnected to the internet, but with [VPC endpoints](https://docs.aws.amazon.com/codebuild/latest/userguide/use-vpc-endpoints-with-codebuild.html) for the necessary services.
+   *
+   * If you don't specify a subnet selection, the default behavior is to use PRIVATE_WITH_EGRESS subnets first if they exist,
+   * then PRIVATE_WITHOUT_EGRESS, and finally PUBLIC subnets. If your VPC doesn't have PRIVATE_WITH_EGRESS subnets but you need
+   * AWS service access, add VPC Endpoints to your private subnets.
+   *
+   * @see https://docs.aws.amazon.com/codebuild/latest/userguide/vpc-support.html for more details.
    *
    * @default - private subnets if available else public subnets
    */
