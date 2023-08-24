5e2ccc2260802ea7890bd46bb417a5b3653835e6
Rico Huijbers <rix0rrr@gmail.com>
2023-08-24 15:00:44 +0200

    Harrumpf

--- tools/@aws-cdk/cdk-build-tools/config/jest.config.js
+++ tools/@aws-cdk/cdk-build-tools/config/jest.config.js
@@ -1,13 +1,33 @@
+const { cpus } = require('os');
+
 module.exports = {
   // The preset deals with preferring TS over JS
   moduleFileExtensions: [
     // .ts first to prefer a ts over a js if present
-    "ts",
-    "js",
+    'ts',
+    'js',
   ],
-  testMatch: ["<rootDir>/test/**/?(*.)+(test).ts"],
+  testMatch: [
+    '<rootDir>/test/**/?(*.)+(test).ts',
+  ],
 
-  testEnvironment: "node",
+  // Transform TypeScript using ts-jest. Use of this preset still requires the depending
+  // package to depend on `ts-jest` directly.
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        // Skips type checking
+        isolatedModules: true,
+      },
+    ],
+  },
+
+  // Limit workers to a reasonable fixed number. If we scale in the number of available CPUs, we will explode
+  // our memory limit on the CodeBuild instance that has 72 CPUs.
+  maxWorkers: Math.min(8, cpus().length - 1),
+
+  testEnvironment: 'node',
   coverageThreshold: {
     global: {
       branches: 80,
@@ -15,14 +35,14 @@ module.exports = {
     },
   },
   collectCoverage: true,
-  coverageReporters: ["lcov", "html", "text-summary"],
+  coverageReporters: ['lcov', 'html', 'text-summary'],
   coveragePathIgnorePatterns: [
-    "\\.generated\\.[jt]s$",
-    "<rootDir>/test/",
-    ".warnings.jsii.js$",
+    '\\.generated\\.[jt]s$',
+    '<rootDir>/test/',
+    '.warnings.jsii.js$',
   ],
   reporters: [
-    "default",
-    ["jest-junit", { suiteName: "jest tests", outputDirectory: "coverage" }],
+    'default',
+    ['jest-junit', { suiteName: 'jest tests', outputDirectory: 'coverage' }],
   ],
 };
