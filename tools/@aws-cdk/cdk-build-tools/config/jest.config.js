41557adf37748ef93feeac0de32aad1311a90f6a
Rico Huijbers <rix0rrr@gmail.com>
2023-08-24 14:16:02 +0200

    Where did my changes go

--- tools/@aws-cdk/cdk-build-tools/config/jest.config.js
+++ tools/@aws-cdk/cdk-build-tools/config/jest.config.js
@@ -1,26 +1,28 @@
 module.exports = {
-    // The preset deals with preferring TS over JS
-    preset: '@aws-cdk/cdk-build-tools/ts-jest-preset',
-    testEnvironment: "node",
-    coverageThreshold: {
-        global: {
-            branches: 80,
-            statements: 80,
-        },
+  // The preset deals with preferring TS over JS
+  moduleFileExtensions: [
+    // .ts first to prefer a ts over a js if present
+    "ts",
+    "js",
+  ],
+  testMatch: ["<rootDir>/test/**/?(*.)+(test).ts"],
+
+  testEnvironment: "node",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      statements: 80,
     },
-    collectCoverage: true,
-    coverageReporters: [
-        "lcov",
-        "html",
-        "text-summary",
-    ],
-    coveragePathIgnorePatterns: [
-        "\\.generated\\.[jt]s$",
-        "<rootDir>/test/",
-        ".warnings.jsii.js$",
-    ],
-	reporters: [
-        "default",
-          [ "jest-junit", { suiteName: "jest tests", outputDirectory: "coverage" } ]
-    ]
+  },
+  collectCoverage: true,
+  coverageReporters: ["lcov", "html", "text-summary"],
+  coveragePathIgnorePatterns: [
+    "\\.generated\\.[jt]s$",
+    "<rootDir>/test/",
+    ".warnings.jsii.js$",
+  ],
+  reporters: [
+    "default",
+    ["jest-junit", { suiteName: "jest tests", outputDirectory: "coverage" }],
+  ],
 };
