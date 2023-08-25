5227fe2eb6a0089abaee718fa5c7bff83a5bc855
Rico Huijbers <rix0rrr@gmail.com>
2023-08-25 08:19:17 +0200

    chore: generate coverage report in text
    For people working over SSH, that can't comfortably look at
    `coverage/index.html`, generate `coverage/coverage.txt` instead.

--- tools/@aws-cdk/pkglint/jest.config.js
+++ tools/@aws-cdk/pkglint/jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+        collectCoverage: true,
+    coverageReporters: [
+        "lcov",
+        "html",
+        "text-summary",
+        ['text', { file: 'coverage/coverage.txt' }]
+    ],
+    coveragePathIgnorePatterns: [
+        "<rootDir>/lib/.*\\.generated\\.[jt]s",
+        "<rootDir>/test/.*\\.[jt]s",
+    ],
+
   testMatch: [
     "**/?(*.)+(test).js",
   ],
