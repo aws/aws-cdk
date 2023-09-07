771feff8c6a679514b09cb23cb551224f3af6e5c
Rico Hermans <rix0rrr@gmail.com>
2023-09-07 17:45:59 +0200

    chore: update SDKv3 blob types model (#27043)
    Add a script that will automatically extract the blob parameter type data from the AWS SDKv3.
    
    `scripts/update-sdkv3-blobs-model.sh` will download the files and drive `scripts/update-sdkv3-blobs-model.ts`, which does the actual collection.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ scripts/update-sdkv3-parameters-model.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+scriptdir=$(cd $(dirname $0) && pwd)
+tmpdir=$(mktemp -d)
+zip=https://github.com/aws/aws-sdk-js-v3/archive/refs/heads/main.zip
+ziprootentry=aws-sdk-js-v3-main
+target_file=packages/@aws-cdk/sdk-v2-to-v3-adapter/lib/parameter-types.ts
+
+# Use the GitHub feature to download a zip archive of the current state of a branch
+# (Cloning --depth 1 takes about twice as long, cloning with full depth many minutes)
+echo "⬇️ Downloading..."
+curl -SfL -o $tmpdir/main.zip "$zip"
+(cd $tmpdir && unzip -q main.zip)
+
+echo "🖨️ Generating..."
+npx ts-node $scriptdir/update-sdkv3-parameters-model.ts $tmpdir/$ziprootentry > "$target_file"
+
+echo "🚮 Cleaning up..."
+rm -rf $tmpdir
+
+echo "✅ Done."
