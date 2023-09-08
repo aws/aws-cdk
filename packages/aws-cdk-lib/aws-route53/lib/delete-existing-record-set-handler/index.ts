aws/aws-cdk

commit 399b6bbdbfbf6440145e6d78e18898aae6261702

fix: all aws-cdk-lib custom resources now use Node18 (#26212)
Migrates all of the custom resources within aws-cdk-lib to use Node18 runtime. Also changes all handler code for these CRs to use aws-sdk v3 instead of v2 since Node18 lambda runtime ships sdk v3 in the environment.


----

*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

packages/aws-cdk-lib/aws-route53/lib/delete-existing-record-set-handler/index.ts
@@ -1,4 +1,4 @@
-import { Route53 } from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
+import { Route53, waitUntilResourceRecordSetsChanged } from '@aws-sdk/client-route-53'; // eslint-disable-line import/no-extraneous-dependencies
 
 interface ResourceProperties {
   HostedZoneId: string;
@@ -20,10 +20,10 @@ export async function handler(event: AWSLambda.CloudFormationCustomResourceEvent
     HostedZoneId: resourceProps.HostedZoneId,
     StartRecordName: resourceProps.RecordName,
     StartRecordType: resourceProps.RecordType,
-  }).promise();
+  });
 
   const existingRecord = listResourceRecordSets.ResourceRecordSets
-    .find(r => r.Name === resourceProps.RecordName && r.Type === resourceProps.RecordType);
+    ?.find(r => r.Name === resourceProps.RecordName && r.Type === resourceProps.RecordType);
 
   if (!existingRecord) {
     // There is no existing record, we can safely return
@@ -44,9 +44,9 @@ export async function handler(event: AWSLambda.CloudFormationCustomResourceEvent
         }),
       }],
     },
-  }).promise();
+  });
 
-  await route53.waitFor('resourceRecordSetsChanged', { Id: changeResourceRecordSets.ChangeInfo.Id }).promise();
+  await waitUntilResourceRecordSetsChanged({ client: route53, maxWaitTime: 60 }, { Id: changeResourceRecordSets?.ChangeInfo?.Id });
 
   return {
     PhysicalResourceId: `${existingRecord.Name}-${existingRecord.Type}`,

packages/@aws-cdk-testing/framework-integ/test/aws-dynamodb/test/integ.global-replicas-provisioned.js.snapshot/asset.dd1fc9a927cd28334a7c7f1625e7804720b41a7d1f429f1f72a7d71883e80118/index.ts
@@ -1,77 +1,0 @@
-/* eslint-disable no-console */
-import { DynamoDB } from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
-import type { IsCompleteRequest, IsCompleteResponse, OnEventRequest, OnEventResponse } from '../../../custom-resources/lib/provider-framework/types';
-
-export async function onEventHandler(event: OnEventRequest): Promise<OnEventResponse> {
-  console.log('Event: %j', { ...event, ResponseURL: '...' });
-
-  const dynamodb = new DynamoDB();
-
-  const tableName = event.ResourceProperties.TableName;
-  const region = event.ResourceProperties.Region;
-
-  let updateTableAction: 'Create' | 'Update' | 'Delete' | undefined;
-  if (event.RequestType === 'Create' || event.RequestType === 'Delete') {
-    updateTableAction = event.RequestType;
-  } else { // Update
-    // There are two cases where an Update can happen:
-    // 1. A table replacement. In that case, we need to create the replica in the new Table
-    // (the replica for the "old" Table will be deleted when CFN issues a Delete event on the old physical resource id).
-    // 2. A customer has changed one of the properties of the Custom Resource,
-    // like 'waitForReplicationToFinish'. In that case, we don't have to do anything.
-    // To differentiate the two cases, we make an API call to DynamoDB to check whether a replica already exists.
-    const describeTableResult = await dynamodb.describeTable({
-      TableName: tableName,
-    }).promise();
-    console.log('Describe table: %j', describeTableResult);
-    const replicaExists = describeTableResult.Table?.Replicas?.some(replica => replica.RegionName === region);
-    updateTableAction = replicaExists ? undefined : 'Create';
-  }
-
-  if (updateTableAction) {
-    const data = await dynamodb.updateTable({
-      TableName: tableName,
-      ReplicaUpdates: [
-        {
-          [updateTableAction]: {
-            RegionName: region,
-          },
-        },
-      ],
-    }).promise();
-    console.log('Update table: %j', data);
-  } else {
-    console.log("Skipping updating Table, as a replica in '%s' already exists", region);
-  }
-
-  return event.RequestType === 'Create' || event.RequestType === 'Update'
-    ? { PhysicalResourceId: `${tableName}-${region}` }
-    : {};
-}
-
-export async function isCompleteHandler(event: IsCompleteRequest): Promise<IsCompleteResponse> {
-  console.log('Event: %j', { ...event, ResponseURL: '...' });
-
-  const dynamodb = new DynamoDB();
-
-  const data = await dynamodb.describeTable({
-    TableName: event.ResourceProperties.TableName,
-  }).promise();
-  console.log('Describe table: %j', data);
-
-  const tableActive = data.Table?.TableStatus === 'ACTIVE';
-  const replicas = data.Table?.Replicas ?? [];
-  const regionReplica = replicas.find(r => r.RegionName === event.ResourceProperties.Region);
-  const replicaActive = regionReplica?.ReplicaStatus === 'ACTIVE';
-  const skipReplicationCompletedWait = event.ResourceProperties.SkipReplicationCompletedWait === 'true';
-
-  switch (event.RequestType) {
-    case 'Create':
-    case 'Update':
-      // Complete when replica is reported as ACTIVE
-      return { IsComplete: tableActive && (replicaActive || skipReplicationCompletedWait) };
-    case 'Delete':
-      // Complete when replica is gone
-      return { IsComplete: tableActive && regionReplica === undefined };
-  }
-}

packages/@aws-cdk-testing/framework-integ/test/aws-dynamodb/test/integ.global.js.snapshot/asset.dd1fc9a927cd28334a7c7f1625e7804720b41a7d1f429f1f72a7d71883e80118/index.ts
@@ -1,77 +1,0 @@
-/* eslint-disable no-console */
-import { DynamoDB } from 'aws-sdk'; // eslint-disable-line import/no-extraneous-dependencies
-import type { IsCompleteRequest, IsCompleteResponse, OnEventRequest, OnEventResponse } from '../../../custom-resources/lib/provider-framework/types';
-
-export async function onEventHandler(event: OnEventRequest): Promise<OnEventResponse> {
-  console.log('Event: %j', { ...event, ResponseURL: '...' });
-
-  const dynamodb = new DynamoDB();
-
-  const tableName = event.ResourceProperties.TableName;
-  const region = event.ResourceProperties.Region;
-
-  let updateTableAction: 'Create' | 'Update' | 'Delete' | undefined;
-  if (event.RequestType === 'Create' || event.RequestType === 'Delete') {
-    updateTableAction = event.RequestType;
-  } else { // Update
-    // There are two cases where an Update can happen:
-    // 1. A table replacement. In that case, we need to create the replica in the new Table
-    // (the replica for the "old" Table will be deleted when CFN issues a Delete event on the old physical resource id).
-    // 2. A customer has changed one of the properties of the Custom Resource,
-    // like 'waitForReplicationToFinish'. In that case, we don't have to do anything.
-    // To differentiate the two cases, we make an API call to DynamoDB to check whether a replica already exists.
-    const describeTableResult = await dynamodb.describeTable({
-      TableName: tableName,
-    }).promise();
-    console.log('Describe table: %j', describeTableResult);
-    const replicaExists = describeTableResult.Table?.Replicas?.some(replica => replica.RegionName === region);
-    updateTableAction = replicaExists ? undefined : 'Create';
-  }
-
-  if (updateTableAction) {
-    const data = await dynamodb.updateTable({
-      TableName: tableName,
-      ReplicaUpdates: [
-        {
-          [updateTableAction]: {
-            RegionName: region,
-          },
-        },
-      ],
-    }).promise();
-    console.log('Update table: %j', data);
-  } else {
-    console.log("Skipping updating Table, as a replica in '%s' already exists", region);
-  }
-
-  return event.RequestType === 'Create' || event.RequestType === 'Update'
-    ? { PhysicalResourceId: `${tableName}-${region}` }
-    : {};
-}
-
-export async function isCompleteHandler(event: IsCompleteRequest): Promise<IsCompleteResponse> {
-  console.log('Event: %j', { ...event, ResponseURL: '...' });
-
-  const dynamodb = new DynamoDB();
-
-  const data = await dynamodb.describeTable({
-    TableName: event.ResourceProperties.TableName,
-  }).promise();
-  console.log('Describe table: %j', data);
-
-  const tableActive = data.Table?.TableStatus === 'ACTIVE';
-  const replicas = data.Table?.Replicas ?? [];
-  const regionReplica = replicas.find(r => r.RegionName === event.ResourceProperties.Region);
-  const replicaActive = regionReplica?.ReplicaStatus === 'ACTIVE';
-  const skipReplicationCompletedWait = event.ResourceProperties.SkipReplicationCompletedWait === 'true';
-
-  switch (event.RequestType) {
-    case 'Create':
-    case 'Update':
-      // Complete when replica is reported as ACTIVE
-      return { IsComplete: tableActive && (replicaActive || skipReplicationCompletedWait) };
-    case 'Delete':
-      // Complete when replica is gone
-      return { IsComplete: tableActive && regionReplica === undefined };
-  }
-}

packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.alb-controller.js.snapshot/asset.baa290a08acb84544e3ce6cc3bd88d6689f5494eef7df73b7957fe9c4ef93e17/index.ts
@@ -1,67 +1,0 @@
-/* eslint-disable no-console */
-// eslint-disable-next-line import/no-extraneous-dependencies
-// eslint-disable-next-line import/no-extraneous-dependencies
-import * as aws from 'aws-sdk';
-import { ClusterResourceHandler } from './cluster';
-import { EksClient } from './common';
-import * as consts from './consts';
-import { FargateProfileResourceHandler } from './fargate';
-import { IsCompleteResponse } from '../../../custom-resources/lib/provider-framework/types';
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports, import/no-extraneous-dependencies
-const ProxyAgent = require('proxy-agent');
-
-aws.config.logger = console;
-aws.config.update({
-  httpOptions: { agent: new ProxyAgent() },
-});
-
-let eks: aws.EKS | undefined;
-
-const defaultEksClient: EksClient = {
-  createCluster: req => getEksClient().createCluster(req).promise(),
-  deleteCluster: req => getEksClient().deleteCluster(req).promise(),
-  describeCluster: req => getEksClient().describeCluster(req).promise(),
-  describeUpdate: req => getEksClient().describeUpdate(req).promise(),
-  updateClusterConfig: req => getEksClient().updateClusterConfig(req).promise(),
-  updateClusterVersion: req => getEksClient().updateClusterVersion(req).promise(),
-  createFargateProfile: req => getEksClient().createFargateProfile(req).promise(),
-  deleteFargateProfile: req => getEksClient().deleteFargateProfile(req).promise(),
-  describeFargateProfile: req => getEksClient().describeFargateProfile(req).promise(),
-  configureAssumeRole: req => {
-    console.log(JSON.stringify({ assumeRole: req }, undefined, 2));
-    const creds = new aws.ChainableTemporaryCredentials({
-      params: req,
-      stsConfig: { stsRegionalEndpoints: 'regional' },
-    });
-
-    eks = new aws.EKS({ credentials: creds });
-  },
-};
-
-function getEksClient() {
-  if (!eks) {
-    throw new Error('EKS client not initialized (call "configureAssumeRole")');
-  }
-
-  return eks;
-}
-
-export async function onEvent(event: AWSLambda.CloudFormationCustomResourceEvent) {
-  const provider = createResourceHandler(event);
-  return provider.onEvent();
-}
-
-export async function isComplete(event: AWSLambda.CloudFormationCustomResourceEvent): Promise<IsCompleteResponse> {
-  const provider = createResourceHandler(event);
-  return provider.isComplete();
-}
-
-function createResourceHandler(event: AWSLambda.CloudFormationCustomResourceEvent) {
-  switch (event.ResourceType) {
-    case consts.CLUSTER_RESOURCE_TYPE: return new ClusterResourceHandler(defaultEksClient, event);
-    case consts.FARGATE_PROFILE_RESOURCE_TYPE: return new FargateProfileResourceHandler(defaultEksClient, event);
-    default:
-      throw new Error(`Unsupported resource type "${event.ResourceType}`);
-  }
-}

commit 89c7f8ea1d1ad3a0664c9db1a63ddb5110459f84

Updates delete-existing-record-set-handler maxWaitTime to 30 * 60 seconds to regression caused by AWS SDK V3 migration Regression introduced in 399b6bbdbfbf6440145e6d78e18898aae6261702

packages/aws-cdk-lib/aws-route53/lib/delete-existing-record-set-handler/index.ts
@@ -46,7 +46,7 @@ export async function handler(event: AWSLambda.CloudFormationCustomResourceEvent
     },
   });
 
-  await waitUntilResourceRecordSetsChanged({ client: route53, maxWaitTime: 60 }, { Id: changeResourceRecordSets?.ChangeInfo?.Id });
+  await waitUntilResourceRecordSetsChanged({ client: route53, maxWaitTime: 1800 }, { Id: changeResourceRecordSets?.ChangeInfo?.Id });
 
   return {
     PhysicalResourceId: `${existingRecord.Name}-${existingRecord.Type}`,