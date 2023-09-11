2c04d3064b4c74843909baba59a5daf0cd19a786
Kaizen Conroy <zen.conroy@gmail.com>
2023-09-11 17:54:40 -0400

    scheduler

--- packages/@aws-cdk/aws-scheduler-alpha/lib/schedule-expression.ts
+++ packages/@aws-cdk/aws-scheduler-alpha/lib/schedule-expression.ts
@@ -1,5 +1,4 @@
-import * as events from 'aws-cdk-lib/aws-events';
-import { Duration, TimeZone } from 'aws-cdk-lib/core';
+import { Duration, TimeZone, Schedule, CronOptions } from 'aws-cdk-lib/core';
 
 /**
  * ScheduleExpression for EventBridge Schedule
@@ -9,7 +8,7 @@ import { Duration, TimeZone } from 'aws-cdk-lib/core';
  *
  * @see https://docs.aws.amazon.com/scheduler/latest/UserGuide/schedule-types.html
  */
-export abstract class ScheduleExpression {
+export abstract class ScheduleExpression extends Schedule {
   /**
    * Construct a one-time schedule from a date.
    *
@@ -17,15 +16,7 @@ export abstract class ScheduleExpression {
    * @param timeZone The time zone to use for interpreting the date. Default: - UTC
    */
   public static at(date: Date, timeZone?: TimeZone): ScheduleExpression {
-    try {
-      const literal = date.toISOString().split('.')[0];
-      return new LiteralScheduleExpression(`at(${literal})`, timeZone ?? TimeZone.ETC_UTC);
-    } catch (e) {
-      if (e instanceof RangeError) {
-        throw new Error('Invalid date');
-      }
-      throw e;
-    }
+    return super.protectedAt(date, timeZone);
   }
 
   /**
@@ -34,7 +25,7 @@ export abstract class ScheduleExpression {
    * @param timeZone The time zone to use for interpreting the expression. Default: - UTC
    */
   public static expression(expression: string, timeZone?: TimeZone): ScheduleExpression {
-    return new LiteralScheduleExpression(expression, timeZone ?? TimeZone.ETC_UTC);
+    return super.protectedExpression(expression, timeZone);
   }
 
   /**
@@ -43,53 +34,14 @@ export abstract class ScheduleExpression {
    * Rates may be defined with any unit of time, but when converted into minutes, the duration must be a positive whole number of minutes.
    */
   public static rate(duration: Duration): ScheduleExpression {
-    const schedule = events.Schedule.rate(duration);
-    return new LiteralScheduleExpression(schedule.expressionString);
+    return super.protectedRate(duration);
   }
 
   /**
    * Create a recurring schedule from a set of cron fields and time zone.
    */
-  public static cron(options: CronOptionsWithTimezone): ScheduleExpression {
-    const { timeZone, ...cronOptions } = options;
-    const schedule = events.Schedule.cron(cronOptions);
-    return new LiteralScheduleExpression(schedule.expressionString, timeZone);
-  }
-
-  /**
-   * Retrieve the expression for this schedule
-   */
-  public abstract readonly expressionString: string;
-
-  /**
-   * Retrieve the expression for this schedule
-   */
-  public abstract readonly timeZone?: TimeZone;
-
-  protected constructor() {}
-}
-
-/**
- * Options to configure a cron expression
- *
- * All fields are strings so you can use complex expressions. Absence of
- * a field implies '*' or '?', whichever one is appropriate.
- *
- * @see https://docs.aws.amazon.com/eventbridge/latest/userguide/scheduled-events.html#cron-expressions
- */
-export interface CronOptionsWithTimezone extends events.CronOptions {
-  /**
-   * The timezone to run the schedule in
-   *
-   * @default - TimeZone.ETC_UTC
-   */
-  readonly timeZone?: TimeZone;
-}
-
-const DEFAULT_TIMEZONE = TimeZone.ETC_UTC;
-
-class LiteralScheduleExpression extends ScheduleExpression {
-  constructor(public readonly expressionString: string, public readonly timeZone: TimeZone = DEFAULT_TIMEZONE) {
-    super();
+  public static cron(options: CronOptions): ScheduleExpression {
+    return super.protectedCron(options);
   }
 }
+
