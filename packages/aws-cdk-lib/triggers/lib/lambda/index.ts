d97dccdd32c3364e2539cadf77a974acd0ae618b
Momo Kornher <kornherm@amazon.co.uk>
2023-09-13 13:17:02 +0100

    fix(triggers): errors are shown as list of bytes (#27119)
    With Triggers a user can invoke a Lambda Function during every stack deployment.
    
    When we migrated the handler code to SDKv3, we overlooked that the return payload from the invoked lambda function is now a `Uint8Array`, and not a string anymore.
    In the error case, we attempt to `JSON.parse()` the error payload which then resulted in a list of bytes being returned to the user.
    
    Updated the handler test case to use correct mocks, including returning a payload that is a Uint8Array.
    
    Closes #27113
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/triggers/lib/lambda/index.ts
+++ packages/aws-cdk-lib/triggers/lib/lambda/index.ts
@@ -4,7 +4,19 @@
 import { Lambda, InvocationResponse } from '@aws-sdk/client-lambda';
 import { NodeHttpHandler } from '@aws-sdk/node-http-handler';
 
-export type InvokeFunction = (functionName: string, invocationType: string, timeout: number) => Promise<InvocationResponse>;
+export type DecodedInvocationResponse = Omit<InvocationResponse, 'Payload'> & {
+  Payload?: string
+}
+
+export type InvokeFunction = (functionName: string, invocationType: string, timeout: number) => Promise<DecodedInvocationResponse>;
+
+const decodePayload = (payload?: Uint8Array): string | undefined => {
+  if (!payload) {
+    return undefined;
+  }
+
+  return new TextDecoder().decode(Buffer.from(payload));
+};
 
 export const invoke: InvokeFunction = async (functionName, invocationType, timeout) => {
   const lambda = new Lambda({
@@ -22,10 +34,10 @@ export const invoke: InvokeFunction = async (functionName, invocationType, timeo
   let retryCount = 0;
   const delay = 5000;
 
-  let invokeResponse;
+  let rawResponse;
   while (true) {
     try {
-      invokeResponse = await lambda.invoke(invokeRequest);
+      rawResponse = await lambda.invoke(invokeRequest);
       break;
     } catch (error: any) {
       if (error.name === 'AccessDeniedException' && retryCount < 12) {
@@ -39,6 +51,11 @@ export const invoke: InvokeFunction = async (functionName, invocationType, timeo
       throw error;
     }
   }
+
+  const invokeResponse = {
+    ...rawResponse,
+    Payload: decodePayload(rawResponse.Payload),
+  };
 
   console.log({ invokeResponse });
   return invokeResponse;
@@ -67,7 +84,7 @@ export async function handler(event: AWSLambda.CloudFormationCustomResourceEvent
 
   const parsedTimeout = parseInt(timeout);
   if (isNaN(parsedTimeout)) {
-    throw new Error(`The "Timeout" property with value ${timeout} is not parseable to a number`);
+    throw new Error(`The "Timeout" property with value ${timeout} is not parsable to a number`);
   }
 
   const invokeResponse = await invoke(handlerArn, invocationType, parsedTimeout);
@@ -78,19 +95,22 @@ export async function handler(event: AWSLambda.CloudFormationCustomResourceEvent
 
   // if the lambda function throws an error, parse the error message and fail
   if (invokeResponse.FunctionError) {
-    throw new Error(parseError(invokeResponse.Payload?.toString()));
+    throw new Error(parseError(invokeResponse.Payload));
   }
 }
 
 /**
  * Parse the error message from the lambda function.
  */
-function parseError(payload: string | undefined): string {
-  console.log(`Error payload: ${payload}`);
+function parseError(payload?: string): string {
   if (!payload) {
     return 'unknown handler error';
   }
+
+  console.log(`Error payload: ${payload}`);
+
   try {
+
     const error = JSON.parse(payload);
     const concat = [error.errorMessage, error.trace].filter(x => x).join('\n');
     return concat.length > 0 ? concat : payload;
