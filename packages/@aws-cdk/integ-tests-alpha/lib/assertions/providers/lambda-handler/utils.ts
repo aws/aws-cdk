2098c18ef39496fe05056a50706594b1acff3c50
Francis <colifran@amazon.com>
2023-09-12 22:16:09 -0700

    remove unused parsePayload
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk/integ-tests-alpha/lib/assertions/providers/lambda-handler/utils.ts
+++ packages/@aws-cdk/integ-tests-alpha/lib/assertions/providers/lambda-handler/utils.ts
@@ -1,17 +1,3 @@
-export async function parsePayload(payload: any): Promise<any> {
-  try {
-    const buffer = Buffer.from(payload);
-    return JSON.parse(new TextDecoder().decode(buffer));
-  } catch {
-    try {
-      const transformedPayload = await payload.transformToString('utf-8');
-      return JSON.parse(transformedPayload);
-    } catch {
-      return payload;
-    }
-  }
-}
-
 export function parseJsonPayload(payload: any): any {
   // sdk v3 returns payloads in Uint8Array
   // If the payload is not convertible to a buffer or parsable as JSON, we don't touch it
