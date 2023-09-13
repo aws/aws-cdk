21077901758d1b95a23b09465511f4d3ef8d2728
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-09-14 00:36:02 +0900

    changed error massages

--- packages/aws-cdk-lib/aws-cloudwatch/lib/graph.ts
+++ packages/aws-cdk-lib/aws-cloudwatch/lib/graph.ts
@@ -243,7 +243,7 @@ export class GaugeWidget extends ConcreteWidget {
     this.copyMetricWarnings(...this.metrics);
 
     if (props.end !== undefined && props.start === undefined) {
-      throw new Error('You must also specify a start if you specify an end');
+      throw new Error('If you specify a value for end, you must also specify a value for start.');
     }
   }
 
@@ -436,7 +436,7 @@ export class GraphWidget extends ConcreteWidget {
     this.copyMetricWarnings(...this.leftMetrics, ...this.rightMetrics);
 
     if (props.end !== undefined && props.start === undefined) {
-      throw new Error('You must also specify a start if you specify an end');
+      throw new Error('If you specify a value for end, you must also specify a value for start.');
     }
   }
 
@@ -574,7 +574,7 @@ export class SingleValueWidget extends ConcreteWidget {
     }
 
     if (props.end !== undefined && props.start === undefined) {
-      throw new Error('You must also specify a start if you specify an end');
+      throw new Error('If you specify a value for end, you must also specify a value for start.');
     }
   }
 
