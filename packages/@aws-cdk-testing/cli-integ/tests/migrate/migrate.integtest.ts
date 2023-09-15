98092172eba49e042a488701cd5f176a0b07fab6
Hogan Bobertz <bobertzh@amazon.com>
2023-09-15 13:15:46 -0400

    dummify test even more

--- packages/@aws-cdk-testing/cli-integ/tests/migrate/migrate.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/migrate/migrate.integtest.ts
@@ -1,29 +1,6 @@
-import {
-  integTest,
-  withTemporaryDirectory,
-  withPackages,
-} from '../../lib';
+import { integTest, withTemporaryDirectory, withPackages } from "../../lib";
 
 integTest(
-  'cdk migrate typescript',
-  withTemporaryDirectory(
-    withPackages(async () => {
-      // const shell = ShellHelper.fromContext(context);
-      // await context.packages.makeCliAvailable();
-
-      // const tempPath = path.resolve(context.integTestDir);
-      // const inputFile = path.join(__dirname, 'template.yml');
-
-      // await shell.shell([
-      //   'cdk',
-      //   'migrate',
-      //   '-l',
-      //   'typescript',
-      //   '--from-path',
-      //   inputFile,
-      //   '--output-path',
-      //   tempPath,
-      // ]);
-    }),
-  ),
+  "cdk migrate typescript",
+  withTemporaryDirectory(withPackages(async () => {}))
 );
