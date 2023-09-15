c010e9cd1b5b664dcfda2463ce251260701e96ec
ndejaco2 <ndejaco123@gmail.com>
2023-09-15 06:56:46 -0700

    Attempt to fix build by setting flag to always be false in integ test

--- packages/@aws-cdk-testing/framework-integ/test/aws-appsync/test/integ.appsync-merged-api.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-appsync/test/integ.appsync-merged-api.ts
@@ -3,7 +3,11 @@ import * as cdk from 'aws-cdk-lib';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 import * as appsync from 'aws-cdk-lib/aws-appsync';
 
-const app = new cdk.App();
+const app = new cdk.App({
+  context: {
+    [cdk.cx_api.APPSYNC_ENABLE_USE_ARN_IDENTIFIER_SOURCE_API_ASSOCIATION]: false,
+  },
+});
 
 const stack = new cdk.Stack(app, 'stack');
 
