7fe5bd2daeb36d342a495b610269090becd8138e
Rico Huijbers <rix0rrr@gmail.com>
2023-09-20 15:07:43 +0200

    Remove accidentally checked-in files

--- tools/@aws-cdk/lazify/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-type LogFn = (...x: string[]) => void;
-export declare function transformFile(filename: string): Promise<void>;
-export declare function transformFileContents(filename: string, contents: string, progress?: LogFn): string;
-export {};
