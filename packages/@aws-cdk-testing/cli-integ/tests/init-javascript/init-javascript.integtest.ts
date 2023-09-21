bb0dfee1ae031eb1a4ee6124d20aa3f086aa193f
Rico Huijbers <rix0rrr@gmail.com>
2023-09-21 11:23:25 +0200

    Linterrr

--- packages/@aws-cdk-testing/cli-integ/tests/init-javascript/init-javascript.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/init-javascript/init-javascript.integtest.ts
@@ -1,5 +1,5 @@
-import * as fs from 'fs-extra';
 import * as path from 'path';
+import * as fs from 'fs-extra';
 import { integTest, withTemporaryDirectory, ShellHelper, withPackages } from '../../lib';
 
 ['app', 'sample-app'].forEach(template => {
