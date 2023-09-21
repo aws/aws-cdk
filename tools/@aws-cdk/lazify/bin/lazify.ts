807f2c3d7ba0dae8bce3d5f59cbffa77288a7d36
Rico Huijbers <rix0rrr@gmail.com>
2023-09-21 13:48:31 +0200

    Exclude custom resources

--- tools/@aws-cdk/lazify/bin/lazify.ts
+++ tools/@aws-cdk/lazify/bin/lazify.ts
@@ -7,25 +7,29 @@ async function main() {
   const verbose = !process.env.QUIET;
 
   for (const arg of args) {
-    await recurseJs(arg, async (f) => {
-      // Only if there's an accompanying .ts file
-      const tsFile = f.replace(/\.js$/, '.ts');
-      if (await fs.pathExists(tsFile)) {
-        await transformFile(f, verbose);
-      }
-    });
+    await recurseJs(
+      arg,
+      async (f) => {
+        // Only if there's an accompanying .ts file
+        const tsFile = f.replace(/\.js$/, '.ts');
+        if (await fs.pathExists(tsFile)) {
+          await transformFile(f, verbose);
+        }
+      },
+      // Skip directories marked as 'custom resource's, so we don't affect asset hashes
+      async (d) => path.basename(d) !== 'node_modules' && await fs.pathExists(path.join(d, '.is_custom_resource')));
   }
 }
 
-async function recurseJs(root: string, block: (x: string) => Promise<void>) {
+async function recurseJs(root: string, fileBlock: (x: string) => Promise<void>, dirBlock: (x: string) => Promise<boolean>) {
   return recurse(root);
 
   async function recurse(f: string) {
     const s = await fs.stat(f);
     if (s.isFile() && f.endsWith('.js')) {
-      await block(f);
+      await fileBlock(f);
     }
-    if (s.isDirectory() && path.basename(f) !== 'node_modules') {
+    if (s.isDirectory() && await dirBlock(f)) {
       for (const child of await fs.readdir(f)) {
         await recurse(path.join(f, child));
       }
