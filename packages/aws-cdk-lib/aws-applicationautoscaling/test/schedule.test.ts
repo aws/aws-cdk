37324bfbe95f8bed975ad180f8aee2b89acd5310
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2023-09-25 18:45:56 -0400

    revert: "feat(core): `Schedule` class" (#27284)
    Reverts aws/aws-cdk#27105
    
    This fixes #27259 by reverting the offending commit.

--- packages/aws-cdk-lib/aws-applicationautoscaling/test/schedule.test.ts
+++ packages/aws-cdk-lib/aws-applicationautoscaling/test/schedule.test.ts
@@ -1,17 +1,15 @@
-import { Duration, Stack, Lazy, TimeZone } from '../../core';
+import { Duration, Stack, Lazy } from '../../core';
 import * as appscaling from '../lib';
 
 describe('cron', () => {
   test('test utc cron, hour only', () => {
     expect(appscaling.Schedule.cron({ hour: '18', minute: '0' }).expressionString).toEqual('cron(0 18 * * ? *)');
+
   });
 
   test('test utc cron, hour and minute', () => {
     expect(appscaling.Schedule.cron({ hour: '18', minute: '24' }).expressionString).toEqual('cron(24 18 * * ? *)');
-  });
 
-  test('test europe/london cron', () => {
-    expect(appscaling.Schedule.cron({ hour: '18', minute: '0', timeZone: TimeZone.EUROPE_LONDON }).timeZone?.timezoneName).toEqual('Europe/London');
   });
 });
 
@@ -19,24 +17,28 @@ describe('rate', () => {
   test('rate must be whole number of minutes', () => {
     expect(() => {
       appscaling.Schedule.rate(Duration.minutes(0.13456));
-    }).toThrow(/0.13456 must be a whole number of minutes/);
+    }).toThrow(/'0.13456 minutes' cannot be converted into a whole number of seconds/);
+
   });
 
   test('rate can be in seconds', () => {
     const duration = appscaling.Schedule.rate(Duration.seconds(120));
     expect('rate(2 minutes)').toEqual(duration.expressionString);
+
   });
 
   test('rate must not be in seconds when specified as a token', () => {
     expect(() => {
       appscaling.Schedule.rate(Duration.seconds(Lazy.number({ produce: () => 5 })));
     }).toThrow(/Allowed units for scheduling/);
+
   });
 
   test('rate cannot be 0', () => {
     expect(() => {
       appscaling.Schedule.rate(Duration.days(0));
     }).toThrow(/Duration cannot be 0/);
+
   });
 
   test('rate can be token', () => {
@@ -44,17 +46,20 @@ describe('rate', () => {
     const lazyDuration = Duration.minutes(Lazy.number({ produce: () => 5 }));
     const rate = appscaling.Schedule.rate(lazyDuration);
     expect('rate(5 minutes)').toEqual(stack.resolve(rate).expressionString);
+
   });
 
   test('rate can be in allowed type hours', () => {
     expect('rate(1 hour)').toEqual(appscaling.Schedule.rate(Duration.hours(1))
       .expressionString);
+
   });
 });
 
 describe('expression', () => {
   test('test using a literal schedule expression', () => {
     expect(appscaling.Schedule.expression('cron(0 18 * * ? *)').expressionString).toEqual('cron(0 18 * * ? *)');
+
   });
 });
 
