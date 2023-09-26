1958df123f71a74938dc82918720dd01f83d6976
Tom Wright <tom.suffern.wright@gmail.com>
2023-09-26 12:23:40 +1000

    test: add basic literal expression evaluation tests

--- packages/aws-cdk/test/api/evalute-cloudformation-template.test.ts
+++ packages/aws-cdk/test/api/evalute-cloudformation-template.test.ts
@@ -6,7 +6,7 @@ import {
 } from '../../lib/api/evaluate-cloudformation-template';
 import { MockSdkProvider } from '../util/mock-sdk';
 
-const sdkProvider: MockSdkProvider = new MockSdkProvider();
+const sdkProvider = new MockSdkProvider();
 const sdk = sdkProvider.sdk;
 const listStackResources: ListStackResources = {
   listStackResources: jest.fn(),
@@ -22,19 +22,48 @@ const createEvaluateCloudFormationTemplate = (template: Template) => new Evaluat
   listStackResources,
 });
 
-test('resolves Fn::Join correctly', async () => {
-  // GIVEN
-  const template: Template = {};
-  const evaluateCfnTemplate = createEvaluateCloudFormationTemplate(template);
+describe('evaluateCfnExpression', () => {
+  describe('simple literal expressions', () => {
+    const template: Template = {};
+    const evaluateCfnTemplate = createEvaluateCloudFormationTemplate(template);
 
-  // WHEN
-  const result = await evaluateCfnTemplate.evaluateCfnExpression({
-    'Fn::Join': [':', ['a', 'b', 'c']],
+    test('resolves Fn::Join correctly', async () => {
+      // WHEN
+      const result = await evaluateCfnTemplate.evaluateCfnExpression({
+        'Fn::Join': [':', ['a', 'b', 'c']],
+      });
+
+      // THEN
+      expect(result).toEqual('a:b:c');
+    });
+
+    test('resolves Fn::Split correctly', async () => {
+      // WHEN
+      const result = await evaluateCfnTemplate.evaluateCfnExpression({ 'Fn::Split': ['|', 'a|b|c'] });
+
+      // THEN
+      expect(result).toEqual(['a', 'b', 'c']);
+    });
+
+    test('resolves Fn::Select correctly', async () => {
+      // WHEN
+      const result = await evaluateCfnTemplate.evaluateCfnExpression({ 'Fn::Select': ['1', ['apples', 'grapes', 'oranges', 'mangoes']] });
+
+      // THEN
+      expect(result).toEqual('grapes');
+    });
+
+    test('resolves Fn::Sub correctly', async () => {
+      // WHEN
+      const result = await evaluateCfnTemplate.evaluateCfnExpression({ 'Fn::Sub': ['Testing Fn::Sub Foo=${Foo} Bar=${Bar}', { Foo: 'testing', Bar: 1 }] });
+
+      // THEN
+      expect(result).toEqual('Testing Fn::Sub Foo=testing Bar=1');
+    });
+
   });
-
-  // THEN
-  expect(result).toEqual('a:b:c');
-});
+},
+);
 
 test('fails because Fn::ImportValue is unsupported', async () => {
   // GIVEN
@@ -48,5 +77,4 @@ test('fails because Fn::ImportValue is unsupported', async () => {
 
   // THEN
   await expect(evaluate).rejects.toBeInstanceOf(CfnEvaluationException);
-
 });
