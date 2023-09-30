f0c05bc11955854c5e9bc85fd1d46348da79b2a4
Rico Huijbers <rix0rrr@gmail.com>
2023-09-30 10:50:54 +0200

    Fix imports

--- packages/@aws-cdk-testing/cli-integ/tests/init-javascript/init-javascript.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/init-javascript/init-javascript.integtest.ts
@@ -1,3 +1,5 @@
+import * as path from 'path';
+import * as fs from 'fs-extra';
 import { integTest, withTemporaryDirectory, ShellHelper, withPackages } from '../../lib';
 
 ['app', 'sample-app'].forEach(template => {
