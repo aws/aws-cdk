fe93544e306fabefbd3371b89089b615e16f08e7
Justin Glommen <jglommen@gmail.com>
2023-10-04 18:46:55 -0500

    feat(s3-deployment): support Fn::Select in renderData() (#27237)
    Closes #25504 
    
    The reason for this change is to support more complex Cloudformation references used within `Source.data` in `aws-s3-deployment`. The objects today only support `Ref` or `Fn::GetAtt` Cfn references, which is limiting when it comes to attempting to manipulate Cfn references at deploy-time, such as via `Fn::Split` or `Fn::Select`. Many AWS CDK functions return tokens that must be evaluated using these complex Cfn functions (see [ApplicationTargetGroup's firstLoadBalancerFullName attribute](https://github.com/aws/aws-cdk/blob/3edd2400bc0c8a86366a29d3a7eef1ef4fa5e016/packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/alb/application-target-group.ts#L438)), but they are incompatible with `renderData`!
    
    This is a blocking issue for CDK projects which rely on generating S3 objects using `BucketDeployment`, wherein the rendered data is generated from native functions which utilize Cfn functions under-the-hood to dynamically construct values at deploy-time.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/aws-s3-deployment/test/integ.bucket-deployment-data.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-s3-deployment/test/integ.bucket-deployment-data.ts
@@ -11,6 +11,7 @@ const file1 = Source.data('file1.txt', 'boom');
 const file2 = Source.data('path/to/file2.txt', `bam! ${bucket.bucketName}`);
 const file3 = Source.jsonData('my-json/config.json', { website_url: bucket.bucketWebsiteUrl });
 const file4 = Source.yamlData('my-yaml/config.yaml', { website_url: bucket.bucketWebsiteUrl });
+const file5 = Source.jsonData('my-json/config2.json', { bucket_domain_name: bucket.bucketWebsiteDomainName });
 
 const deployment = new BucketDeployment(stack, 'DeployMeHere', {
   destinationBucket: bucket,
@@ -20,6 +21,7 @@ const deployment = new BucketDeployment(stack, 'DeployMeHere', {
 });
 deployment.addSource(file3);
 deployment.addSource(file4);
+deployment.addSource(file5);
 
 new CfnOutput(stack, 'BucketName', { value: bucket.bucketName });
 
