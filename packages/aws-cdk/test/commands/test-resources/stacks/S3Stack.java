b70f0d55fd0bff8460334ad533e96442d95518c5
Kendra Neil <53584728+TheRealAmazonKendra@users.noreply.github.com>
2023-10-05 14:41:02 -0700

    chore: upgrade cdk migrate version with bug fixes (#27406)
    This change contains bug fixes for all languages, some of which were caught in our current tests. 
    
    I'm continuing to list these changes as chores until we're ready for them to show up in our changelog.
    >
    > [CONTRIBUTING GUIDE]: https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md
    > [DESIGN GUIDELINES]: https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md
    
    Closes #<issue number here>.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk/test/commands/test-resources/stacks/S3Stack.java
+++ packages/aws-cdk/test/commands/test-resources/stacks/S3Stack.java
@@ -3,12 +3,12 @@ package com.myorg;
 import software.constructs.Construct;
 
 import java.util.*;
-import software.amazon.awscdk.*;
 import software.amazon.awscdk.CfnMapping;
 import software.amazon.awscdk.CfnTag;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 
+import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.s3.*;
 
 class GoodJavaStack extends Stack {
