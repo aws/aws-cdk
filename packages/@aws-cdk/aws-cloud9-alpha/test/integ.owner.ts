7962a83ef9417f44fca9d0bed715aa726481c3b7
Markus Ziller <web+github@markusziller.de>
2023-10-06 17:35:49 +0200

    Cleans up import in integration tests
    Co-authored-by: Luca Pizzini <lpizzini7@gmail.com>

--- packages/@aws-cdk/aws-cloud9-alpha/test/integ.owner.ts
+++ packages/@aws-cdk/aws-cloud9-alpha/test/integ.owner.ts
@@ -3,7 +3,6 @@ import { Stack, App, StackProps, CfnOutput } from 'aws-cdk-lib';
 import * as integ from '@aws-cdk/integ-tests-alpha';
 import { Construct } from 'constructs';
 import * as cloud9 from '../lib';
-import { ImageId, Owner } from '../lib';
 
 class Cloud9Env extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -18,8 +17,8 @@ class Cloud9Env extends Stack {
     // create a cloud9 ec2 environment in a new VPC
     const c9env = new cloud9.Ec2Environment(this, 'C9Env', {
       vpc,
-      imageId: ImageId.AMAZON_LINUX_2,
-      owner: Owner.assumedRole('807336301584', 'Admin/test'),
+      imageId: cloud9.ImageId.AMAZON_LINUX_2,
+      owner: cloud9.Owner.assumedRole('807336301584', 'Admin/test'),
     });
     new CfnOutput(this, 'URL', { value: c9env.ideUrl });
     new CfnOutput(this, 'ARN', { value: c9env.ec2EnvironmentArn });
