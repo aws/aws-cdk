1f9397a5567e942ce4e72108e526ec2336dd9154
nicolastremblay1 <141684369+nicolastremblay1@users.noreply.github.com>
2023-10-10 09:48:50 -0700

    Update packages/aws-cdk-lib/aws-codedeploy/test/server/deployment-group.test.ts
    Co-authored-by: Luca Pizzini <lpizzini7@gmail.com>

--- packages/@aws-cdk-testing/framework-integ/test/aws-codedeploy/test/server/integ.deployment-group.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-codedeploy/test/server/integ.deployment-group.ts
@@ -1,8 +1,8 @@
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
+import * as integ from '@aws-cdk/integ-tests-alpha';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as lb from 'aws-cdk-lib/aws-elasticloadbalancing';
-import * as lb2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as cdk from 'aws-cdk-lib';
 import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
 
@@ -23,29 +23,10 @@ elb.addListener({
   externalPort: 80,
 });
 
-const alb = new lb2.ApplicationLoadBalancer(stack, 'ALB', {
-  vpc: vpc,
-});
-
-const nlb = new lb2.NetworkLoadBalancer(stack, 'NLB', {
-  vpc: vpc,
-});
-
-const listener = alb.addListener('Listener', { protocol: lb2.ApplicationProtocol.HTTP });
-const targetGroup = listener.addTargets('Fleet', { protocol: lb2.ApplicationProtocol.HTTP });
-
-const nlbListener = nlb.addListener('Listener', { port: 80 });
-const nlbTargetGroup = nlbListener.addTargets('Fleet', { port: 80 });
-
 new codedeploy.ServerDeploymentGroup(stack, 'CodeDeployGroup', {
   deploymentConfig: codedeploy.ServerDeploymentConfig.ALL_AT_ONCE,
   autoScalingGroups: [asg],
   loadBalancer: codedeploy.LoadBalancer.classic(elb),
-  loadBalancers: [
-    codedeploy.LoadBalancer.classic(elb),
-    codedeploy.LoadBalancer.application(targetGroup),
-    codedeploy.LoadBalancer.network(nlbTargetGroup),
-  ],
   alarms: [
     new cloudwatch.Alarm(stack, 'Alarm1', {
       metric: new cloudwatch.Metric({
--- packages/aws-cdk-lib/aws-codedeploy/lib/server/deployment-group.ts
+++ packages/aws-cdk-lib/aws-codedeploy/lib/server/deployment-group.ts
@@ -168,6 +168,7 @@ export interface ServerDeploymentGroupProps {
    * or an Application Load Balancer / Network Load Balancer Target Group.
    *
    * @default - Deployment Group will not have a load balancer defined.
+   * @deprecated - Deprecated in favor of loadBalancers.
    */
   readonly loadBalancer?: LoadBalancer;
 
@@ -253,6 +254,7 @@ export class ServerDeploymentGroup extends DeploymentGroupBase implements IServe
   private readonly installAgent: boolean;
   private readonly codeDeployBucket: s3.IBucket;
   private readonly alarms: cloudwatch.IAlarm[];
+  private readonly loadBalancers?: LoadBalancer[];
 
   constructor(scope: Construct, id: string, props: ServerDeploymentGroupProps = {}) {
     super(scope, id, {
@@ -271,6 +273,12 @@ export class ServerDeploymentGroup extends DeploymentGroupBase implements IServe
     this._autoScalingGroups = props.autoScalingGroups || [];
     this.installAgent = props.installAgent ?? true;
     this.codeDeployBucket = s3.Bucket.fromBucketName(this, 'Bucket', `aws-codedeploy-${cdk.Stack.of(this).region}`);
+    this.loadBalancers = props.loadBalancers || (props.loadBalancer ? [props.loadBalancer]: undefined);
+
+    if (this.loadBalancers && this.loadBalancers.length === 0) {
+      throw new Error('Cannot set empty loadBalancers array');
+    }
+
     for (const asg of this._autoScalingGroups) {
       this.addCodeDeployAgentInstallUserData(asg);
     }
@@ -286,8 +294,8 @@ export class ServerDeploymentGroup extends DeploymentGroupBase implements IServe
       deploymentConfigName: props.deploymentConfig &&
         props.deploymentConfig.deploymentConfigName,
       autoScalingGroups: cdk.Lazy.list({ produce: () => this._autoScalingGroups.map(asg => asg.autoScalingGroupName) }, { omitEmpty: true }),
-      loadBalancerInfo: props.loadBalancers ? this.loadBalancersInfo(props.loadBalancers) : this.loadBalancerInfo(props.loadBalancer),
-      deploymentStyle: props.loadBalancer === undefined && props.loadBalancers === undefined
+      loadBalancerInfo: this.loadBalancersInfo(this.loadBalancers),
+      deploymentStyle: this.loadBalancers === undefined
         ? undefined
         : {
           deploymentOption: 'WITH_TRAFFIC_CONTROL',
@@ -378,28 +386,6 @@ export class ServerDeploymentGroup extends DeploymentGroupBase implements IServe
     }
   }
 
-  private loadBalancerInfo(loadBalancer?: LoadBalancer):
-  CfnDeploymentGroup.LoadBalancerInfoProperty | undefined {
-    if (!loadBalancer) {
-      return undefined;
-    }
-
-    switch (loadBalancer.generation) {
-      case LoadBalancerGeneration.FIRST:
-        return {
-          elbInfoList: [
-            { name: loadBalancer.name },
-          ],
-        };
-      case LoadBalancerGeneration.SECOND:
-        return {
-          targetGroupInfoList: [
-            { name: loadBalancer.name },
-          ],
-        };
-    }
-  }
-
   private loadBalancersInfo(loadBalancers?: LoadBalancer[]):
   CfnDeploymentGroup.LoadBalancerInfoProperty | undefined {
 
@@ -407,30 +393,23 @@ export class ServerDeploymentGroup extends DeploymentGroupBase implements IServe
       return undefined;
     }
 
-    // TODO, find a better type here
-    const loadBalancerInfo: {
-      elbInfoList: {name: string}[],
-      targetGroupInfoList: {name: string}[]
-    } = {
-      elbInfoList: [],
-      targetGroupInfoList: [],
-    };
-
-    loadBalancers.forEach(loadBalancer => {
-
+    return loadBalancers.reduce((accumulator : {
+      elbInfoList?: {name: string}[],
+      targetGroupInfoList?: {name: string}[]
+    }, loadBalancer: LoadBalancer) => {
       switch (loadBalancer.generation) {
         case LoadBalancerGeneration.FIRST:
-          loadBalancerInfo.elbInfoList.push({ name: loadBalancer.name });
-          break;
+          if (!accumulator.elbInfoList) accumulator.elbInfoList = [];
+          accumulator.elbInfoList.push({ name: loadBalancer.name });
+          return accumulator;
         case LoadBalancerGeneration.SECOND:
-          loadBalancerInfo.targetGroupInfoList.push({ name: loadBalancer.name });
-          break;
+          if (!accumulator.targetGroupInfoList) accumulator.targetGroupInfoList = [];
+          accumulator.targetGroupInfoList.push({ name: loadBalancer.name });
+          return accumulator;
         default:
-          throw new Error('Unknown load balancer generation');
+          return accumulator;
       }
-    });
-
-    return loadBalancerInfo;
+    }, {});
   }
 
   private ec2TagSet(tagSet?: InstanceTagSet):
