959688a7a50b5e7d2d1017118142499ea290ddb7
SankyRed <121981451+SankyRed@users.noreply.github.com>
2023-10-11 02:32:21 -0500

    feat(stepfunctions): adding custom state name prop (#27306)
    Closes https://github.com/aws/aws-cdk/issues/23532
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-stepfunctions/test/fail.test.ts
+++ packages/aws-cdk-lib/aws-stepfunctions/test/fail.test.ts
@@ -1,10 +1,67 @@
+import { render } from './private/render-util';
 import * as cdk from '../../core';
 import * as stepfunctions from '../lib';
 
 describe('Fail State', () => {
-  test('Props are optional', () => {
-    const stack = new cdk.Stack();
+  let stack: cdk.Stack;
+  let stateJson: any;
 
+  beforeEach(() => {
+    // GIVEN
+    stack = new cdk.Stack();
+    stateJson = {
+      Type: 'Task',
+      Resource: 'arn:aws:states:::dynamodb:putItem',
+      Parameters: {
+        TableName: 'MyTable',
+        Item: {
+          id: {
+            S: 'MyEntry',
+          },
+        },
+      },
+      ResultPath: null,
+    };
+  });
+
+  test('Props are optional', () => {
     new stepfunctions.Fail(stack, 'Fail');
+  });
+
+  test('can add a fail state to the chain with custom state name', () => {
+  // WHEN
+    const definition = new stepfunctions.CustomState(stack, 'Custom1', {
+      stateJson,
+    }).next(new stepfunctions.Pass(stack, 'MyPass'))
+      .next(new stepfunctions.Fail(stack, 'Fail', {
+        stateName: 'my-fail-state',
+        comment: 'failing state',
+        errorPath: stepfunctions.JsonPath.stringAt('$.error'),
+        causePath: stepfunctions.JsonPath.stringAt('$.cause'),
+      }));
+
+    // THEN
+    expect(render(stack, definition)).toStrictEqual(
+      {
+        StartAt: 'Custom1',
+        States: {
+          'Custom1': {
+            Next: 'MyPass',
+            Type: 'Task',
+            ...stateJson,
+          },
+          'MyPass': {
+            Type: 'Pass',
+            Next: 'my-fail-state',
+          },
+          'my-fail-state': {
+            Comment: 'failing state',
+            Type: 'Fail',
+            CausePath: '$.cause',
+            ErrorPath: '$.error',
+          },
+        },
+      },
+    );
   });
 });
