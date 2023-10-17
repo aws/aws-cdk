ffbad8cb70b6077f4b08aca63ca40daf1882a6bd
chrispidcock <chris.pidcock87@gmail.com>
2023-10-18 00:20:40 +1000

    tests: integration tests for storage mode

---
+++ packages/@aws-cdk/aws-msk-alpha/test/integ.cluster-storage-mode.ts
@@ -0,0 +1,38 @@
+import { App, RemovalPolicy, Stack } from 'aws-cdk-lib';
+import * as msk from '../lib/index';
+import { KafkaVersion } from '../lib/index';
+import { Construct } from 'constructs';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+
+const storageMode: msk.StorageMode[] = [
+  msk.StorageMode.TIERED,
+  msk.StorageMode.LOCAL,
+];
+
+class KafkaStorageModeTest extends Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    const vpc = new Vpc(this, 'Vpc', { maxAzs: 2, restrictDefaultSecurityGroup: false });
+
+    storageMode.forEach(sMode => {
+      const clusterMode = sMode.toLowerCase();
+      new msk.Cluster(this, `storageMode${clusterMode}`, {
+        clusterName: `${clusterMode}-cluster`,
+        kafkaVersion: KafkaVersion.V2_8_2_TIERED,
+        storageMode: sMode,
+        vpc,
+        removalPolicy: RemovalPolicy.DESTROY,
+      });
+    });
+  }
+}
+
+const app = new App();
+new IntegTest(app, 'KafkaVersionIntegTest', {
+  enableLookups: true,
+  testCases: [
+    new KafkaStorageModeTest(app, 'KafkaStorageModeTestStack'),
+  ],
+});
+app.synth();
