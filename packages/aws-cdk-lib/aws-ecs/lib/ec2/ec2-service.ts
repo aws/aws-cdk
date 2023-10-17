2d9ebaf79d33f3c1607dd4aafd5e98d3fcfed3b9
Mike Mwanje <mwanjemike767@gmail.com>
2023-10-17 09:51:44 +0000

    fix: remove omitEmptyArray default in Ec2Service constructor

--- packages/aws-cdk-lib/aws-ecs/lib/ec2/ec2-service.ts
+++ packages/aws-cdk-lib/aws-ecs/lib/ec2/ec2-service.ts
@@ -179,7 +179,7 @@ export class Ec2Service extends BaseService implements IEc2Service {
     {
       cluster: props.cluster.clusterName,
       taskDefinition: props.deploymentController?.type === DeploymentControllerType.EXTERNAL ? undefined : props.taskDefinition.taskDefinitionArn,
-      placementConstraints: Lazy.any({ produce: () => this.constraints }, { omitEmptyArray: true }),
+      placementConstraints: Lazy.any({ produce: () => this.constraints }),
       placementStrategies: Lazy.any({ produce: () => this.strategies }, { omitEmptyArray: true }),
       schedulingStrategy: props.daemon ? 'DAEMON' : 'REPLICA',
     }, props.taskDefinition);
