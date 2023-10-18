23e34222bd80f57c62341f95730503c4e3b04cd5
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-10-18 18:04:30 +0900

    change comments

--- packages/aws-cdk-lib/aws-secretsmanager/lib/rotation-schedule.ts
+++ packages/aws-cdk-lib/aws-secretsmanager/lib/rotation-schedule.ts
@@ -37,8 +37,6 @@ export interface RotationScheduleOptions {
    * Specifies the number of days after the previous rotation before
    * Secrets Manager triggers the next automatic rotation.
    *
-   * The maximum value is 1000 days.
-   *
    * A value of zero (`Duration.days(0)`) will not create RotationRules.
    *
    * @default Duration.days(30)
