8feb460228854912a86a29ac8ba2af2cff867607
Tatsuya Mori <tamori@amazon.com>
2023-10-20 02:57:37 +0900

    feat(opensearch): OpenSearch 2.9 engine version (#27612)
    [Amazon OpenSearch Service now supports OpenSearch version 2.9](https://aws.amazon.com/about-aws/whats-new/2023/10/amazon-opensearch-service-opensearch-version-2-9/). This PR adds the new engine version.
    
    Closes #27574
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/aws-opensearchservice/test/integ.opensearch.min.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-opensearchservice/test/integ.opensearch.min.ts
@@ -9,7 +9,7 @@ class TestStack extends Stack {
 
     // deploy the latest opensearch domain with minimal configuration
     const domainProps: opensearch.DomainProps = {
-      version: opensearch.EngineVersion.OPENSEARCH_2_7,
+      version: opensearch.EngineVersion.OPENSEARCH_2_9,
       removalPolicy: RemovalPolicy.DESTROY,
       capacity: {
         multiAzWithStandbyEnabled: false,
