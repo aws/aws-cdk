c4589cc964a12c059d9eaa700487e0b348753b10
Markus Ziller <zillerm@amazon.de>
2023-10-23 16:08:52 +0200

    Makes integration test independent of used AWS account by creating used role in Test Stack

--- packages/@aws-cdk/aws-cloud9-alpha/test/integ.owner.ts
+++ packages/@aws-cdk/aws-cloud9-alpha/test/integ.owner.ts
@@ -3,6 +3,7 @@ import { Stack, App, StackProps, CfnOutput } from 'aws-cdk-lib';
 import * as integ from '@aws-cdk/integ-tests-alpha';
 import { Construct } from 'constructs';
 import * as cloud9 from '../lib';
+import { AnyPrincipal, Role } from 'aws-cdk-lib/aws-iam';
 
 class Cloud9Env extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,11 +15,21 @@ class Cloud9Env extends Stack {
       natGateways: 1,
     });
 
+    const role = new Role(this, 'Cloud9OwnerRole', {
+      roleName: 'Cloud9OwnerRole',
+      description: 'Created as part of the integration tests for the Cloud9 CDK construct',
+      assumedBy: new AnyPrincipal(),
+    });
+
+    const accountId = Stack.of(this).account;
+    const roleSessionName = 'a-test-session';
+    const assumedRoleString = `${role.roleName}/${roleSessionName}`;
+
     // create a cloud9 ec2 environment in a new VPC
     const c9env = new cloud9.Ec2Environment(this, 'C9Env', {
       vpc,
       imageId: cloud9.ImageId.AMAZON_LINUX_2,
-      owner: cloud9.Owner.assumedRole('807336301584', 'Admin/test'),
+      owner: cloud9.Owner.assumedRole(accountId, assumedRoleString),
     });
     new CfnOutput(this, 'URL', { value: c9env.ideUrl });
     new CfnOutput(this, 'ARN', { value: c9env.ec2EnvironmentArn });
@@ -30,5 +41,3 @@ const app = new App();
 new integ.IntegTest(app, 'OwnerInteg', {
   testCases: [new Cloud9Env(app, 'cloud9-owner-integ')],
 });
-
-app.synth();
