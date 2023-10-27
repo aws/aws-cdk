bc5971de11a1df7504295247165c8eb658656bbf
Kaizen Conroy <zen.conroy@gmail.com>
2023-10-27 18:14:33 -0400

    tests with snappies

--- tools/@aws-cdk/spec2cdk/test/resources.test.ts
+++ tools/@aws-cdk/spec2cdk/test/resources.test.ts
@@ -1,151 +1,212 @@
-import { loadAwsServiceSpec } from '@aws-cdk/aws-service-spec';
-import { SpecDatabase } from '@aws-cdk/service-spec-types';
+import { Service, SpecDatabase, emptyDatabase } from '@aws-cdk/service-spec-types';
 import { TypeScriptRenderer } from '@cdklabs/typewriter';
 import { AstBuilder } from '../lib/cdk/ast';
 
 const renderer = new TypeScriptRenderer();
 let db: SpecDatabase;
+let service: Service;
 
-beforeAll(async () => {
-  db = await loadAwsServiceSpec();
-});
+beforeEach(async () => {
+  db = emptyDatabase();
 
-test('resource interface when primaryIdentifier is an attribute', () => {
-  const resource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::VoiceID::Domain').only();
-
-  const ast = AstBuilder.forResource(resource, { db });
-
-  const rendered = renderer.render(ast.module);
-
-  expect(rendered).toContain([
-    '/**',
-    ' * Attributes for `CfnDomain`.',
-    ' *',
-    ' * @stability external',
-    ' */',
-    'export interface ICfnDomain {',
-    '  /**',
-    '   * The identifier of the domain.',
-    '   *',
-    '   * @cloudformationAttribute DomainId',
-    '   */',
-    '  readonly attrDomainId: string;',
-    '}',
-  ].join('\n'));
+  service = db.allocate('service', {
+    name: 'aws-some',
+    shortName: 'some',
+    capitalized: 'Some',
+    cloudFormationNamespace: 'AWS::Some',
+  });
 });
 
 test('resource interface when primaryIdentifier is a property', () => {
-  const resource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::KinesisAnalyticsV2::Application').only();
+  // GIVEN
+  const resource = db.allocate('resource', {
+    name: 'Resource',
+    primaryIdentifier: ['Id'],
+    attributes: {},
+    properties: {
+      Id: {
+        type: { type: 'string' },
+        documentation: 'The identifier of the resource',
+      },
+    },
+    cloudFormationType: 'AWS::Some::Resource',
+  });
+  db.link('hasResource', service, resource);
 
-  const ast = AstBuilder.forResource(resource, { db });
+  // THEN
+  const foundResource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::Some::Resource').only();
+
+  const ast = AstBuilder.forResource(foundResource, { db });
 
   const rendered = renderer.render(ast.module);
 
-  expect(rendered).toContain([
-    '/**',
-    ' * Attributes for `CfnApplication`.',
-    ' *',
-    ' * @stability external',
-    ' */',
-    'export interface ICfnApplication {',
-    '  /**',
-    '   * The name of the application.',
-    '   *',
-    '   * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-kinesisanalyticsv2-application.html#cfn-kinesisanalyticsv2-application-applicationname',
-    '   */',
-    '  readonly attrApplicationName?: string;', // optional?
-    '}',
-  ].join('\n'));
+  expect(rendered).toMatchSnapshot();
+});
+
+test('resource interface when primaryIdentifier is an attribute', () => {
+  // GIVEN
+  const resource = db.allocate('resource', {
+    name: 'Resource',
+    primaryIdentifier: ['Id'],
+    properties: {},
+    attributes: {
+      Id: {
+        type: { type: 'string' },
+        documentation: 'The identifier of the resource',
+      },
+    },
+    cloudFormationType: 'AWS::Some::Resource',
+  });
+  db.link('hasResource', service, resource);
+
+  // THEN
+  const foundResource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::Some::Resource').only();
+
+  const ast = AstBuilder.forResource(foundResource, { db });
+
+  const rendered = renderer.render(ast.module);
+
+  expect(rendered).toMatchSnapshot();
 });
 
 test('resource interface with multiple primaryIdentifiers', () => {
-  const resource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::LakeFormation::DataCellsFilter').only();
+  // GIVEN
+  const resource = db.allocate('resource', {
+    name: 'Resource',
+    primaryIdentifier: ['Id', 'Another'],
+    properties: {},
+    attributes: {
+      Id: {
+        type: { type: 'string' },
+        documentation: 'The identifier of the resource',
+      },
+      Another: {
+        type: { type: 'string' },
+        documentation: 'Another identifier of the resource',
+      },
+    },
+    cloudFormationType: 'AWS::Some::Resource',
+  });
+  db.link('hasResource', service, resource);
 
-  const ast = AstBuilder.forResource(resource, { db });
+  // THEN
+  const foundResource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::Some::Resource').only();
+
+  const ast = AstBuilder.forResource(foundResource, { db });
 
   const rendered = renderer.render(ast.module);
 
-  expect(rendered).toContain([
-    '/**',
-    ' * Attributes for `CfnDataCellsFilter`.',
-    ' *',
-    ' * @stability external',
-    ' */',
-    'export interface ICfnDataCellsFilter {',
-    '  /**',
-    '   * Catalog id string, not less than 1 or more than 255 bytes long, matching the [single-line string pattern](https://docs.aws.amazon.com/lake-formation/latest/dg/aws-lake-formation-api-aws-lake-formation-api-common.html) .',
-    '   *',
-    '   * The ID of the catalog to which the table belongs.',
-    '   *',
-    '   * @cloudformationRef tableCatalogId',
-    '   *',
-    '   * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-lakeformation-datacellsfilter.html#cfn-lakeformation-datacellsfilter-tablecatalogid',
-    '   */',
-    '  readonly attrTableCatalogId: string;',
-    // '  /**',
-    // '   * UTF-8 string, not less than 1 or more than 255 bytes long, matching the [single-line string pattern](https://docs.aws.amazon.com/lake-formation/latest/dg/aws-lake-formation-api-aws-lake-formation-api-common.html) .',
-    // '   *',
-    // '   * A database in the Data Catalog .',
-    // '   *',
-    // '   * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-lakeformation-datacellsfilter.html#cfn-lakeformation-datacellsfilter-databasename',
-    // '   */',
-    // '  readonly attrDatabaseName: string;',
-    // '  /**',
-    // '   * UTF-8 string, not less than 1 or more than 255 bytes long, matching the [single-line string pattern](https://docs.aws.amazon.com/lake-formation/latest/dg/aws-lake-formation-api-aws-lake-formation-api-common.html) .',
-    // '   *',
-    // '   * A table in the database.',
-    // '   *',
-    // '   * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-lakeformation-datacellsfilter.html#cfn-lakeformation-datacellsfilter-tablename',
-    // '   */',
-    // '  readonly attrTableName: string;',
-    // '  /**',
-    // '   * UTF-8 string, not less than 1 or more than 255 bytes long, matching the [single-line string pattern](https://docs.aws.amazon.com/lake-formation/latest/dg/aws-lake-formation-api-aws-lake-formation-api-common.html) .',
-    // '   *',
-    // '   * The name given by the user to the data filter cell.',
-    // '   *',
-    // '   * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-lakeformation-datacellsfilter.html#cfn-lakeformation-datacellsfilter-name',
-    // '   */',
-    // '  readonly attrName: string;',
-    // '}',
-  ].join('\n'));
+  expect(rendered).toMatchSnapshot();
 });
 
 test('resource interface with "Arn"', () => {
-  const resource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::S3::Bucket').only();
+  // GIVEN
+  const resource = db.allocate('resource', {
+    name: 'Resource',
+    primaryIdentifier: ['Id', 'Another'],
+    properties: {},
+    attributes: {
+      Id: {
+        type: { type: 'string' },
+        documentation: 'The identifier of the resource',
+      },
+      Arn: {
+        type: { type: 'string' },
+        documentation: 'The arn for the resource',
+      },
+    },
+    cloudFormationType: 'AWS::Some::Resource',
+  });
+  db.link('hasResource', service, resource);
 
-  const ast = AstBuilder.forResource(resource, { db });
+  // THEN
+  const foundResource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::Some::Resource').only();
+
+  const ast = AstBuilder.forResource(foundResource, { db });
 
   const rendered = renderer.render(ast.module);
 
-  expect(rendered).toContain([
-    '/**',
-    ' * Attributes for `CfnBucket`.',
-    ' *',
-    ' * @stability external',
-    ' */',
-    'export interface ICfnBucket {',
-    '  /**',
-    '   * A name for the bucket.',
-    '   *',
-    '   * If you don\'t specify a name, AWS CloudFormation generates a unique ID and uses that ID for the bucket name. The bucket name must contain only lowercase letters, numbers, periods (.), and dashes (-) and must follow [Amazon S3 bucket restrictions and limitations](https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html) . For more information, see [Rules for naming Amazon S3 buckets](https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html#bucketnamingrules) in the *Amazon S3 User Guide* .',
-    '   *',
-    '   * > If you specify a name, you can\'t perform updates that require replacement of this resource. You can perform updates that require no or some interruption. If you need to replace the resource, specify a new name.',
-    '   *',
-    '   * @cloudformationRef bucketName',
-    '   *',
-    '   * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-s3-bucket.html#cfn-s3-bucket-bucketname',
-    '   */',
-    '  readonly attrBucketName?: string;',
-    // '  /**',
-    // '   * Returns the Amazon Resource Name (ARN) of the specified bucket.',
-    // '   *',
-    // '   * Example: `arn:aws:s3:::DOC-EXAMPLE-BUCKET`',
-    // '   *',
-    // '   * @cloudformationAttribute Arn',
-    // '   */',
-    // '   readonly attrArn: string;',
-    // '}',
-  ].join('\n'));
+  expect(rendered).toMatchSnapshot();
 });
 
-// TODO: test with ResourceArn and <Resource>Arn too
+test('resource interface with "ResourceArn"', () => {
+  // GIVEN
+  const resource = db.allocate('resource', {
+    name: 'Resource',
+    primaryIdentifier: ['Id'],
+    properties: {},
+    attributes: {
+      Id: {
+        type: { type: 'string' },
+        documentation: 'The identifier of the resource',
+      },
+      ResourceArn: {
+        type: { type: 'string' },
+        documentation: 'The arn for the resource',
+      },
+    },
+    cloudFormationType: 'AWS::Some::Resource',
+  });
+  db.link('hasResource', service, resource);
+
+  // THEN
+  const foundResource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::Some::Resource').only();
+
+  const ast = AstBuilder.forResource(foundResource, { db });
+
+  const rendered = renderer.render(ast.module);
+
+  expect(rendered).toMatchSnapshot();
+});
+
+test('resource interface with "<Resource>Arn"', () => {
+  // GIVEN
+  const resource = db.allocate('resource', {
+    name: 'Something',
+    primaryIdentifier: ['Id'],
+    properties: {},
+    attributes: {
+      Id: {
+        type: { type: 'string' },
+        documentation: 'The identifier of the resource',
+      },
+      SomethingArn: {
+        type: { type: 'string' },
+        documentation: 'The arn for something',
+      },
+    },
+    cloudFormationType: 'AWS::Some::Something',
+  });
+  db.link('hasResource', service, resource);
+
+  // THEN
+  const foundResource = db.lookup('resource', 'cloudFormationType', 'equals', 'AWS::Some::Something').only();
+
+  const ast = AstBuilder.forResource(foundResource, { db });
+
+  const rendered = renderer.render(ast.module);
+
+  expect(rendered).toMatchSnapshot();
+});
+
+// function getInterface(module: ResourceModule, intName: string) {
+//   const int = moduleInterfaces(module).find((i) => i.spec.name === intName);
+//   if (!int) throw new Error('wtf');
+//   return new InterfaceRenderer().renderInterface(int);
+// }
+
+// Should be in Module
+// function moduleInterfaces(module: ResourceModule) {
+//   return module.types.filter((t) => t instanceof InterfaceType).map((t) => t as InterfaceType);
+// }
+
+// class InterfaceRenderer extends TypeScriptRenderer {
+
+//   public render() {
+//     super.r
+//   }
+
+//   public renderInterface(interfaceType: InterfaceType) {
+//     super.renderInterface(interfaceType);
+//   }
+// }
