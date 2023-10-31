6fc3d6ce32c84695a3298ad86c5f4daee0cf2601
Hiroki Yamazaki <yhirokiy@amazon.com>
2023-10-31 15:00:09 +0000

    add unit tests for valid conditions

--- packages/@aws-cdk/aws-scheduler-targets-alpha/test/sns-publish.test.ts
+++ packages/@aws-cdk/aws-scheduler-targets-alpha/test/sns-publish.test.ts
@@ -1,6 +1,7 @@
 import * as scheduler from '@aws-cdk/aws-scheduler-alpha';
 import { App, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import { SnsPublish } from '../lib/sns-publish';
 
@@ -80,4 +81,234 @@ describe('sns topic schedule target', () => {
     });
   });
 
+  test('create IAM policy for provided IAM role', () => {
+    const targetExecutionRole = new iam.Role(stack, 'ProvidedTargetRole', {
+      assumedBy: new iam.AccountRootPrincipal(),
+    });
+
+    const target = new SnsPublish(topic, {
+      role: targetExecutionRole,
+    });
+
+    new scheduler.Schedule(stack, 'Schedule', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.hasResource('AWS::Scheduler::Schedule', {
+      Properties: {
+        Target: {
+          Arn: {
+            Ref: 'TopicBFC7AF6E',
+          },
+          RoleArn: {
+            'Fn::GetAtt': [
+              'ProvidedTargetRole8CFDD54A',
+              'Arn',
+            ],
+          },
+          RetryPolicy: {},
+        },
+      },
+    });
+
+    template.hasResourceProperties('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: 'sns:Publish',
+            Effect: 'Allow',
+            Resource: {
+              Ref: 'TopicBFC7AF6E',
+            },
+          },
+        ],
+      },
+      Roles: [{
+        Ref: 'ProvidedTargetRole8CFDD54A',
+      }],
+    });
+  });
+
+  test('reuse IAM role and IAM policy for two schedulers from the same account', () => {
+    const target = new SnsPublish(topic, {});
+
+    new scheduler.Schedule(stack, 'Schedule1', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    new scheduler.Schedule(stack, 'Schedule2', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::IAM::Policy', 1);
+    template.resourceCountIs('AWS::IAM::Role', 1);
+    template.resourceCountIs('AWS::Scheduler::Schedule', 2);
+
+    template.hasResourceProperties('AWS::IAM::Role', {
+      AssumeRolePolicyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'sts:AssumeRole',
+            Effect: 'Allow',
+            Condition: {
+              StringEquals: {
+                'aws:SourceAccount': '123456789012',
+              },
+            },
+            Principal: {
+              Service: 'scheduler.amazonaws.com',
+            },
+          },
+        ],
+      },
+    });
+
+    template.hasResourceProperties('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: 'sns:Publish',
+            Effect: 'Allow',
+            Resource: {
+              Ref: 'TopicBFC7AF6E',
+            },
+          },
+        ],
+      },
+      Roles: [{
+        Ref: 'SchedulerRoleForTarget1441a743A31888',
+      }],
+    });
+  });
+
+  test('create IAM policy for imported sns topic in the same account', () => {
+    const importedTopic = sns.Topic.fromTopicArn(stack, 'ImportedTopic', 'arn:aws:sns:us-east-1:123456789012:my-topic');
+
+    const target = new SnsPublish(importedTopic, {});
+
+    new scheduler.Schedule(stack, 'Schedule', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.hasResource('AWS::Scheduler::Schedule', {
+      Properties: {
+        Target: {
+          Arn: 'arn:aws:sns:us-east-1:123456789012:my-topic',
+          RoleArn: {
+            'Fn::GetAtt': [
+              'SchedulerRoleForTarget54a476A35D62A2', 'Arn',
+            ],
+          },
+          RetryPolicy: {},
+        },
+      },
+    });
+
+    template.hasResourceProperties('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: 'sns:Publish',
+            Effect: 'Allow',
+            Resource: 'arn:aws:sns:us-east-1:123456789012:my-topic',
+          },
+        ],
+      },
+      Roles: [{
+        Ref: 'SchedulerRoleForTarget54a476A35D62A2',
+      }],
+    });
+  });
+
+  test('create IAM policy for imported role for sns topic in the same account', () => {
+    const importedRole = iam.Role.fromRoleArn(stack, 'ImportedRole', 'arn:aws:iam::123456789012:role/my-role');
+
+    const target = new SnsPublish(topic, {
+      role: importedRole,
+    });
+
+    new scheduler.Schedule(stack, 'Schedule', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.hasResource('AWS::Scheduler::Schedule', {
+      Properties: {
+        Target: {
+          Arn: {
+            Ref: 'TopicBFC7AF6E',
+          },
+          RoleArn: 'arn:aws:iam::123456789012:role/my-role',
+          RetryPolicy: {},
+        },
+      },
+    });
+
+    template.hasResourceProperties('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: 'sns:Publish',
+            Effect: 'Allow',
+            Resource: {
+              Ref: 'TopicBFC7AF6E',
+            },
+          },
+        ],
+      },
+      Roles: ['my-role'],
+    });
+  });
+
+  test('create IAM policy for imported sns topic with imported IAM role in the same account', () => {
+    const importedTopic = sns.Topic.fromTopicArn(stack, 'ImportedTopic', 'arn:aws:sns:us-east-1:123456789012:my-topic');
+    const importedRole = iam.Role.fromRoleArn(stack, 'ImportedRole', 'arn:aws:iam::123456789012:role/my-role');
+
+    const target = new SnsPublish(importedTopic, {
+      role: importedRole,
+    });
+
+    new scheduler.Schedule(stack, 'Schedule', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.hasResource('AWS::Scheduler::Schedule', {
+      Properties: {
+        Target: {
+          Arn: 'arn:aws:sns:us-east-1:123456789012:my-topic',
+          RoleArn: 'arn:aws:iam::123456789012:role/my-role',
+          RetryPolicy: {},
+        },
+      },
+    });
+
+    template.hasResourceProperties('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: 'sns:Publish',
+            Effect: 'Allow',
+            Resource: 'arn:aws:sns:us-east-1:123456789012:my-topic',
+          },
+        ],
+      },
+      Roles: ['my-role'],
+    });
+  });
 });
