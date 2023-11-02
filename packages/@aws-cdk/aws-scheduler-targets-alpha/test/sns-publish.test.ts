7120ec40681d435effdde6e110bac8c424edc4cc
Hiroki Yamazaki <yhirokiy@amazon.com>
2023-11-02 13:27:53 +0000

    add unit tests

--- packages/@aws-cdk/aws-scheduler-targets-alpha/test/sns-publish.test.ts
+++ packages/@aws-cdk/aws-scheduler-targets-alpha/test/sns-publish.test.ts
@@ -1,8 +1,9 @@
 import * as scheduler from '@aws-cdk/aws-scheduler-alpha';
-import { App, Stack } from 'aws-cdk-lib';
+import { App, Duration, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as sns from 'aws-cdk-lib/aws-sns';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { SnsPublish } from '../lib/sns-publish';
 
 describe('sns topic schedule target', () => {
@@ -17,7 +18,7 @@ describe('sns topic schedule target', () => {
     topic = new sns.Topic(stack, 'Topic');
   });
 
-  test('create IAM role and IAM policy for SNS topic in the same account', () => {
+  test('creates IAM role and IAM policy for SNS topic in the same account', () => {
     const target = new SnsPublish(topic, {});
 
     new scheduler.Schedule(stack, 'Schedule', {
@@ -81,7 +82,7 @@ describe('sns topic schedule target', () => {
     });
   });
 
-  test('create IAM policy for provided IAM role', () => {
+  test('creates IAM policy for provided IAM role', () => {
     const targetExecutionRole = new iam.Role(stack, 'ProvidedTargetRole', {
       assumedBy: new iam.AccountRootPrincipal(),
     });
@@ -132,7 +133,7 @@ describe('sns topic schedule target', () => {
     });
   });
 
-  test('reuse IAM role and IAM policy for two schedulers from the same account', () => {
+  test('reuses IAM role and IAM policy for two schedulers from the same account', () => {
     const target = new SnsPublish(topic, {});
 
     new scheduler.Schedule(stack, 'Schedule1', {
@@ -189,7 +190,7 @@ describe('sns topic schedule target', () => {
     });
   });
 
-  test('create IAM policy for imported sns topic in the same account', () => {
+  test('creates IAM policy for imported sns topic in the same account', () => {
     const importedTopic = sns.Topic.fromTopicArn(stack, 'ImportedTopic', 'arn:aws:sns:us-east-1:123456789012:my-topic');
 
     const target = new SnsPublish(importedTopic, {});
@@ -231,7 +232,7 @@ describe('sns topic schedule target', () => {
     });
   });
 
-  test('create IAM policy for imported role for sns topic in the same account', () => {
+  test('creates IAM policy for imported role for sns topic in the same account', () => {
     const importedRole = iam.Role.fromRoleArn(stack, 'ImportedRole', 'arn:aws:iam::123456789012:role/my-role');
 
     const target = new SnsPublish(topic, {
@@ -273,7 +274,7 @@ describe('sns topic schedule target', () => {
     });
   });
 
-  test('create IAM policy for imported sns topic with imported IAM role in the same account', () => {
+  test('creates IAM policy for imported sns topic with imported IAM role in the same account', () => {
     const importedTopic = sns.Topic.fromTopicArn(stack, 'ImportedTopic', 'arn:aws:sns:us-east-1:123456789012:my-topic');
     const importedRole = iam.Role.fromRoleArn(stack, 'ImportedRole', 'arn:aws:iam::123456789012:role/my-role');
 
@@ -310,5 +311,209 @@ describe('sns topic schedule target', () => {
       },
       Roles: ['my-role'],
     });
+  });
+
+  test('throws when sns topic is imported from different account', () => {
+    const importedTopic = sns.Topic.fromTopicArn(stack, 'ImportedTopic', 'arn:aws:sns:us-east-1:234567890123:my-topic');
+
+    const target = new SnsPublish(importedTopic, {});
+
+    expect(() =>
+      new scheduler.Schedule(stack, 'Schedule', {
+        schedule: scheduleExpression,
+        target,
+      })).toThrow(/Both the schedule and the topic must be in the same account/);
+  });
+
+  test('throws when sns topic is imported from different region', () => {
+    const importedTopic = sns.Topic.fromTopicArn(stack, 'ImportedTopic', 'arn:aws:sns:us-west-2:123456789012:my-topic');
+
+    const target = new SnsPublish(importedTopic, {});
+
+    expect(() =>
+      new scheduler.Schedule(stack, 'Schedule', {
+        schedule: scheduleExpression,
+        target,
+      })).toThrow(/Both the schedule and the topic must be in the same region/);
+  });
+
+  test('throws when IAM role is imported from different account', () => {
+    const importedRole = iam.Role.fromRoleArn(stack, 'ImportedRole', 'arn:aws:iam::234567890123:role/my-role');
+
+    const target = new SnsPublish(topic, {
+      role: importedRole,
+    });
+
+    expect(() =>
+      new scheduler.Schedule(stack, 'Schedule', {
+        schedule: scheduleExpression,
+        target,
+      })).toThrow(/Both the target and the execution role must be in the same account/);
+  });
+
+  test('adds permissions to DLQ', () => {
+    const dlq = new sqs.Queue(stack, 'DeadLetterQueue');
+
+    const target = new SnsPublish(topic, {
+      deadLetterQueue: dlq,
+    });
+
+    new scheduler.Schedule(stack, 'Schedule', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::SQS::QueuePolicy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: 'sqs:SendMessage',
+            Effect: 'Allow',
+            Principal: {
+              Service: 'scheduler.amazonaws.com',
+            },
+            Resource: {
+              'Fn::GetAtt': ['DeadLetterQueue9F481546', 'Arn'],
+            },
+          },
+        ],
+      },
+      Queues: [
+        {
+          Ref: 'DeadLetterQueue9F481546',
+        },
+      ],
+    });
+  });
+
+  test('throws when adding permission to DLQ from a different region', () => {
+    const differentRegionStack = new Stack(app, 'Stack2', {
+      env: {
+        region: 'us-west-2',
+        account: '123456789012',
+      },
+    });
+    const queue = new sqs.Queue(differentRegionStack, 'DeadLetterQueue9F481546');
+
+    const target = new SnsPublish(topic, {
+      deadLetterQueue: queue,
+    });
+
+    expect(() =>
+      new scheduler.Schedule(stack, 'Schedule', {
+        schedule: scheduleExpression,
+        target,
+      })).toThrow(/Both the queue and the schedule must be in the same region/);
+  });
+
+  test('does not create a queue policy when DLQ is imported', () => {
+    const importedQueue = sqs.Queue.fromQueueArn(stack, 'ImportedQueue', 'arn:aws:sqs:us-east-1:123456789012:my-queue');
+
+    const target = new SnsPublish(topic, {
+      deadLetterQueue: importedQueue,
+    });
+
+    new scheduler.Schedule(stack, 'Schedule', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    const template = Template.fromStack(stack);
+    template.resourceCountIs('AWS::SQS::QueuePolicy', 0);
+  });
+
+  test('does not create a queue policy when DLQ is created in a different account', () => {
+    const differentAccountStack = new Stack(app, 'Stack2', {
+      env: {
+        region: 'us-east-1',
+        account: '234567890123',
+      },
+    });
+
+    const queue = new sqs.Queue(differentAccountStack, 'DeadLetterQueue', {
+      queueName: 'DummyDeadLetterQueue',
+    });
+
+    const target = new SnsPublish(topic, {
+      deadLetterQueue: queue,
+    });
+
+    new scheduler.Schedule(stack, 'Schedule', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::SQS::QueuePolicy', 0);
+  });
+
+  test('renders expected retry policy', () => {
+    const target = new SnsPublish(topic, {
+      retryAttempts: 5,
+      maxEventAge: Duration.hours(3),
+    });
+
+    new scheduler.Schedule(stack, 'Schedule', {
+      schedule: scheduleExpression,
+      target,
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.hasResource('AWS::Scheduler::Schedule', {
+      Properties: {
+        Target: {
+          Arn: {
+            Ref: 'TopicBFC7AF6E',
+          },
+          RoleArn: {
+            'Fn::GetAtt': ['SchedulerRoleForTarget1441a743A31888', 'Arn'],
+          },
+          RetryPolicy: {
+            MaximumEventAgeInSeconds: 3600 * 3,
+            MaximumRetryAttempts: 5,
+          },
+        },
+      },
+    });
+  });
+
+  test('throws when retry policy max age is more than 1 day', () => {
+    const target = new SnsPublish(topic, {
+      maxEventAge: Duration.days(3),
+    });
+
+    expect(() =>
+      new scheduler.Schedule(stack, 'Schedule', {
+        schedule: scheduleExpression,
+        target,
+      })).toThrow(/Maximum event age is 1 day/);
+  });
+
+  test('throws when retry policy max age is less than 15 minutes', () => {
+    const target = new SnsPublish(topic, {
+      maxEventAge: Duration.minutes(5),
+    });
+
+    expect(() =>
+      new scheduler.Schedule(stack, 'Schedule', {
+        schedule: scheduleExpression,
+        target,
+      })).toThrow(/Minimum event age is 15 minutes/);
+  });
+
+  test('throws when retry policy max retry attempts is out of the allowed limits', () => {
+    const target = new SnsPublish(topic, {
+      retryAttempts: 200,
+    });
+
+    expect(() =>
+      new scheduler.Schedule(stack, 'Schedule', {
+        schedule: scheduleExpression,
+        target,
+      })).toThrow(/Number of retry attempts should be less or equal than 185/);
   });
 });
