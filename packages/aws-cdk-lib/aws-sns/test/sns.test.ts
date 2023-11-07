0e2bafe0cac4ca772783cf897dd7e77a170a3433
Hiroki Yamazaki <yhirokiy@amazon.com>
2023-11-07 16:05:39 +0000

    update unit test

--- packages/aws-cdk-lib/aws-sns/test/sns.test.ts
+++ packages/aws-cdk-lib/aws-sns/test/sns.test.ts
@@ -349,17 +349,6 @@ describe('Topic', () => {
     expect(imported.fifo).toEqual(true);
   });
 
-  test('sets region for imported topic env', () => {
-    // GIVEN
-    const stack = new cdk.Stack();
-
-    // WHEN
-    const imported = sns.Topic.fromTopicArn(stack, 'Imported', 'arn:aws:sns:us-west-2:123456789012:my-topic');
-
-    // THEN
-    expect(imported.env.account).toEqual('es-west-2');
-  });
-
   test('sets account for imported topic env', () => {
     // GIVEN
     const stack = new cdk.Stack();
@@ -368,7 +357,18 @@ describe('Topic', () => {
     const imported = sns.Topic.fromTopicArn(stack, 'Imported', 'arn:aws:sns:us-west-2:123456789012:my-topic');
 
     // THEN
-    expect(imported.env.region).toEqual('123456789012');
+    expect(imported.env.account).toEqual('123456789012');
+  });
+
+  test('sets region for imported topic env', () => {
+    // GIVEN
+    const stack = new cdk.Stack();
+
+    // WHEN
+    const imported = sns.Topic.fromTopicArn(stack, 'Imported', 'arn:aws:sns:us-west-2:123456789012:my-topic');
+
+    // THEN
+    expect(imported.env.region).toEqual('es-west-2');
   });
 
   test('test metrics', () => {
