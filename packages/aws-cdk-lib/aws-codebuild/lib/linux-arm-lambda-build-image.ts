2c28108d7c924cc6fad89bd8a413837233e5d8b9
sakurai-ryo <sakurairyo.10do@gmail.com>
2023-11-11 03:37:19 +0900

    feat(codebuild): removed extra whitespace.

--- packages/aws-cdk-lib/aws-codebuild/lib/linux-arm-lambda-build-image.ts
+++ packages/aws-cdk-lib/aws-codebuild/lib/linux-arm-lambda-build-image.ts
@@ -22,7 +22,7 @@ export class LinuxArmLambdaBuildImage implements IBuildImage {
   /** Image "aws/codebuild/amazonlinux-aarch64-lambda-standard:nodejs18". */
   public static readonly AMAZON_LINUX_2_NODE_18 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:nodejs18');
   /** Image "aws/codebuild/amazonlinux-aarch64-lambda-standard:python3.11	". */
-  public static readonly AMAZON_LINUX_2_PYTHON_3_11 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:python3.11	');
+  public static readonly AMAZON_LINUX_2_PYTHON_3_11 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:python3.11');
   /** Image "aws/codebuild/amazonlinux-aarch64-lambda-standard:ruby3.2". */
   public static readonly AMAZON_LINUX_2_RUBY_3_2 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:ruby3.2');
   /** Image "	aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto17". */
