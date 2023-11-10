c363c0e3ee45cc99a1e3ef6abe730a8f3007e782
Colin Francis <131073567+colifran@users.noreply.github.com>
2023-11-10 12:13:56 -0800

    chore(s3-deployment): migrate bucket deployment handler (#27919)
    This PR moves the bucket deployment handler from aws-cdk-lib to our new centralized location for custom resource handlers in the [@aws-cdk](https://github.com/aws-cdk) package.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/custom-resource-handlers/test/aws-s3-deployment/bucket-deployment-handler/debug.sh
+++ packages/@aws-cdk/custom-resource-handlers/test/aws-s3-deployment/bucket-deployment-handler/debug.sh
@@ -9,4 +9,4 @@ $DOCKER_CMD build -f Dockerfile.debug -t $tag .
 echo "To iterate, run python3 ./test.py inside the container (source code is mapped into the container)."
 
 ln -fs /opt/lambda/index.py index.py
-$DOCKER_CMD run -v $PWD:/opt/awscli -v $PWD/../../lib/lambda:/opt/lambda --workdir /opt/awscli -it $tag
+$DOCKER_CMD run -v $PWD:/opt/awscli -v $PWD/../../../lib/aws-s3-deployment/bucket-deployment-handler:/opt/lambda --workdir /opt/awscli -it $tag
