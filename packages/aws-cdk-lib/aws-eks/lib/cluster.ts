66453a64cfab98eeba15bea9b356e4beba6a86f7
Pahud Hsieh <pahudnet@gmail.com>
2023-11-10 13:47:19 -0500

    feat(eks): support for Kubernetes version 1.28 (#27930)
    Similar to https://github.com/aws/aws-cdk/pull/25897, this PR adds eks with k8s 1.28 support.
    
    Closes #27500 
    
    <img width="792" alt="image" src="https://github.com/aws/aws-cdk/assets/278432/9cfec3f1-360d-4e7c-95fe-9a5177847f1d">
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-eks/lib/cluster.ts
+++ packages/aws-cdk-lib/aws-eks/lib/cluster.ts
@@ -926,6 +926,15 @@ export class KubernetesVersion {
   public static readonly V1_27 = KubernetesVersion.of('1.27');
 
   /**
+   * Kubernetes version 1.28
+   *
+   * When creating a `Cluster` with this version, you need to also specify the
+   * `kubectlLayer` property with a `KubectlV28Layer` from
+   * `@aws-cdk/lambda-layer-kubectl-v28`.
+   */
+  public static readonly V1_28 = KubernetesVersion.of('1.28');
+
+  /**
    * Custom cluster version
    * @param version custom version number
    */
