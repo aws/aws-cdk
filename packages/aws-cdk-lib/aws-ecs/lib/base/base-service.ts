22f3460dde8a119757f82f5a199492b09dc9312f
Jonathan Romano <jonathan@luxaritas.com>
2023-11-11 03:30:04 +0000

    Dont allow task definition revision to be overridden with CODE_DEPLOY deployment controller

--- packages/aws-cdk-lib/aws-ecs/lib/base/base-service.ts
+++ packages/aws-cdk-lib/aws-ecs/lib/base/base-service.ts
@@ -643,15 +643,21 @@ export abstract class BaseService extends Resource
       throw new Error('Deployment alarms requires the ECS deployment controller.');
     }
 
+    if (
+      props.deploymentController?.type === DeploymentControllerType.CODE_DEPLOY
+      && props.taskDefinitionRevision
+      && props.taskDefinitionRevision !== 'latest'
+    ) {
+      throw new Error('CODE_DEPLOY deploymentController cannot be used with a non-latest task definition revision');
+    }
+
     if (props.deploymentController?.type === DeploymentControllerType.CODE_DEPLOY) {
       // Strip the revision ID from the service's task definition property to
       // prevent new task def revisions in the stack from triggering updates
       // to the stack's ECS service resource
       this.resource.taskDefinition = taskDefinition.family;
       this.node.addDependency(taskDefinition);
-    }
-
-    if (props.taskDefinitionRevision) {
+    } else if (props.taskDefinitionRevision) {
       this.resource.taskDefinition = taskDefinition.family;
       if (props.taskDefinitionRevision !== 'latest') {
         this.resource.taskDefinition += `:${props.taskDefinitionRevision}`;
