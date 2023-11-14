8424ec8f37472fbf7b4758a5e9c99a8be4106336
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2023-11-14 09:28:26 +0000

    test

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,5 @@ module.exports = async ({github, context, core}) => {
     const fs = require('fs');
     const content = fs.readFileSync('./packages/aws-cdk-lib/region-info/build-tools/metadata.ts');
 
-    console.log(content);
+    console.log(content.toString());
 }
