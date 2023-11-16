e86afa537aa9928b49f77759c8a5c486303ba083
Francis <colifran@amazon.com>
2023-11-16 13:29:33 -0800

    added some deprecated runtime unit tests
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/aws-lambda/test/runtime.test.ts
+++ packages/aws-cdk-lib/aws-lambda/test/runtime.test.ts
@@ -55,3 +55,25 @@ describe('runtime', () => {
     expect(runtime.bundlingDockerImage.image).toEqual('my-docker-image');
   });
 });
+
+describe('deprecated runtimes', () => {
+  test('python 2.7 is deprecated', () => {
+    expect(lambda.Runtime.PYTHON_2_7.isDeprecated).toEqual(true);
+  });
+
+  test('python 3.6 is deprecated', () => {
+    expect(lambda.Runtime.PYTHON_3_6.isDeprecated).toEqual(true);
+  });
+
+  test('nodejs 10.x is deprecated', () => {
+    expect(lambda.Runtime.NODEJS_10_X.isDeprecated).toEqual(true);
+  });
+
+  test('nodejs 12.x is deprecated', () => {
+    expect(lambda.Runtime.NODEJS_12_X.isDeprecated).toEqual(true);
+  });
+
+  test('nodejs 14.x is deprecated', () => {
+    expect(lambda.Runtime.NODEJS_14_X.isDeprecated).toEqual(true);
+  });
+});
