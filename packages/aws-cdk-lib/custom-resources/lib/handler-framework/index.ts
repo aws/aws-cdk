e23503f61c7eded727a3574f44b7ac754c458a2d
Francis <colifran@amazon.com>
2023-11-17 12:24:34 -0800

    remove exports from index
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/custom-resources/lib/handler-framework/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './cdk-code';
-export * from './cdk-function';
-export * from './cdk-singleton-function';
--- packages/aws-cdk-lib/custom-resources/lib/helpers-internal/index.ts
+++ packages/aws-cdk-lib/custom-resources/lib/helpers-internal/index.ts
@@ -1,2 +1,1 @@
 export * from './sdk-info';
-export * from './runtime-determiner';
