04d5de84cc443648521ad42457c45b0f9db5984c
Francis <colifran@amazon.com>
2023-11-20 08:52:34 -0800

    renamed cdk-code.test to cdk-handler.test
    Signed-off-by: Francis <colifran@amazon.com>

---
+++ packages/aws-cdk-lib/custom-resources/test/handler-framework/cdk-handler.test.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+import { Runtime } from '../../../aws-lambda';
+import { CdkHandler } from '../../lib/handler-framework/cdk-handler';
+
+describe('code from asset', () => {
+  test('correctly sets compatibleRuntimes property', () => {
+    // GIVEN
+    const compatibleRuntimes = [Runtime.NODEJS_16_X, Runtime.NODEJS_18_X];
+
+    // WHEN
+    const handler = CdkHandler.fromAsset(path.join(__dirname, 'test-handler'), {
+      handler: 'index.handler',
+      compatibleRuntimes,
+    });
+
+    // THEN
+    expect(handler.compatibleRuntimes).toEqual(compatibleRuntimes);
+  });
+
+  test('correctly sets handler property', () => {
+    // GIVEN
+    const compatibleRuntimes = [Runtime.NODEJS_16_X, Runtime.NODEJS_18_X];
+
+    // WHEN
+    const handler = CdkHandler.fromAsset(path.join(__dirname, 'test-handler'), {
+      handler: 'index.handler',
+      compatibleRuntimes,
+    });
+
+    // THEN
+    expect(handler.handler).toEqual('index.handler');
+  });
+});
