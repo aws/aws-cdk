d783fad9883c7ec8dbbd8a4baec54c57a1f0af8e
Sumu <sumughan@amazon.com>
2023-11-21 15:37:10 -0500

    add generated file to lib/index.ts
    Signed-off-by: Sumu <sumughan@amazon.com>

--- packages/aws-cdk-lib/aws-apigatewayv2/lib/index.ts
+++ packages/aws-cdk-lib/aws-apigatewayv2/lib/index.ts
@@ -2,3 +2,5 @@ export * from './common';
 export * from './http';
 export * from './websocket';
 export * from './parameter-mapping';
+// AWS::Module CloudFormation Resources:
+export * from './apigatewayv2.generated';
