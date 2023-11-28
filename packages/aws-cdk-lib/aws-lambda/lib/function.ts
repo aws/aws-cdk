3589102666fc6511364b07ce612df4979bdbaf98
tomoish <tomo.i.120608@gmail.com>
2023-11-28 13:17:52 +0900

    fix logFormat type

--- packages/aws-cdk-lib/aws-lambda/lib/function.ts
+++ packages/aws-cdk-lib/aws-lambda/lib/function.ts
@@ -466,7 +466,7 @@ export interface FunctionOptions extends EventInvokeConfigOptions {
    * Sets the logFormat for the function.
    * @default Text format
    */
-  readonly logFormat?: LogFormat;
+  readonly logFormat?: LogFormat | keyof typeof LogFormat;
 
   /**
    * Sets the application log level for the function.
@@ -1085,6 +1085,10 @@ export class Function extends FunctionBase {
    * function and undefined if not.
    */
   private getLoggingConfig(props: FunctionProps): CfnFunction.LoggingConfigProperty | undefined {
+    if (props.logFormat !== undefined && !Object.values(LogFormat).includes(props.logFormat as LogFormat)) {
+      throw new TypeError(`${props.logFormat} is not a legal LogFormat type`);
+    }
+
     if ((props.applicationLogLevel || props.systemLogLevel) && props.logFormat !== LogFormat.JSON) {
       throw new Error(`To use ApplicationLogLevel and/or SystemLogLevel you must set LogFormat to '${LogFormat.JSON}', got '${props.logFormat}'.`);
     }
