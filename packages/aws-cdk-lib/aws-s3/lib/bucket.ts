a9ae1b1dd0f0484cf7c1ded7fa1be766c0556c4d
Kaizen Conroy <zen.conroy@gmail.com>
2023-11-28 17:45:35 -0500

    minor change

--- packages/aws-cdk-lib/aws-s3/lib/bucket.ts
+++ packages/aws-cdk-lib/aws-s3/lib/bucket.ts
@@ -1884,7 +1884,7 @@ export class Bucket extends BucketBase {
     this.encryptionKey = encryptionKey;
     this.eventBridgeEnabled = props.eventBridgeEnabled;
 
-    this.attrBucketName = this.getResourceNameAttribute(resource.ref);
+    this.attrBucketName = resource.attrBucketName;
     this.bucketName = this.attrBucketName;
     this.attrArn = this.getResourceArnAttribute(resource.attrArn, {
       region: '',
