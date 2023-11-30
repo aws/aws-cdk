ffa888dc08c2032beb99be6c3d44d256a869b224
Francis <colifran@amazon.com>
2023-11-29 18:42:07 -0800

    refactor deprecated runtimes unit tests to use test.each
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/aws-lambda/test/runtime.test.ts
+++ packages/aws-cdk-lib/aws-lambda/test/runtime.test.ts
@@ -57,67 +57,24 @@ describe('runtime', () => {
 });
 
 describe('deprecated runtimes', () => {
-  test('python 2.7 is deprecated', () => {
-    expect(lambda.Runtime.PYTHON_2_7.isDeprecated).toEqual(true);
-  });
-
-  test('python 3.6 is deprecated', () => {
-    expect(lambda.Runtime.PYTHON_3_6.isDeprecated).toEqual(true);
-  });
-
-  test('nodejs is deprecated', () => {
-    expect(lambda.Runtime.NODEJS.isDeprecated).toEqual(true);
-  });
-
-  test('nodejs 4.3 is deprecated', () => {
-    expect(lambda.Runtime.NODEJS_4_3.isDeprecated).toEqual(true);
-  });
-
-  test('nodejs 6.10 is deprecated', () => {
-    expect(lambda.Runtime.NODEJS_6_10.isDeprecated).toEqual(true);
-  });
-
-  test('nodejs 8.10 is deprecated', () => {
-    expect(lambda.Runtime.NODEJS_8_10.isDeprecated).toEqual(true);
-  });
-
-  test('nodejs 10.x is deprecated', () => {
-    expect(lambda.Runtime.NODEJS_10_X.isDeprecated).toEqual(true);
-  });
-
-  test('nodejs 12.x is deprecated', () => {
-    expect(lambda.Runtime.NODEJS_12_X.isDeprecated).toEqual(true);
-  });
-
-  test('nodejs 14.x is deprecated', () => {
-    expect(lambda.Runtime.NODEJS_14_X.isDeprecated).toEqual(true);
-  });
-
-  test('.net core 1.0 is deprecated', () => {
-    expect(lambda.Runtime.DOTNET_CORE_1.isDeprecated).toEqual(true);
-  });
-
-  test('.net core 2.0 is deprecated', () => {
-    expect(lambda.Runtime.DOTNET_CORE_2.isDeprecated).toEqual(true);
-  });
-
-  test('.net core 2.1 is deprecated', () => {
-    expect(lambda.Runtime.DOTNET_CORE_2_1.isDeprecated).toEqual(true);
-  });
-
-  test('.net core 3.1 is deprecated', () => {
-    expect(lambda.Runtime.DOTNET_CORE_3_1.isDeprecated).toEqual(true);
-  });
-
-  test('go 1.x is deprecated', () => {
-    expect(lambda.Runtime.GO_1_X.isDeprecated).toEqual(true);
-  });
-
-  test('ruby 2.5 is deprecated', () => {
-    expect(lambda.Runtime.RUBY_2_5.isDeprecated).toEqual(true);
-  });
-
-  test('custom provided runtime is deprecated', () => {
-    expect(lambda.Runtime.PROVIDED.isDeprecated).toEqual(true);
+  test.each([
+    [lambda.Runtime.PYTHON_2_7],
+    [lambda.Runtime.PYTHON_3_6],
+    [lambda.Runtime.NODEJS],
+    [lambda.Runtime.NODEJS_4_3],
+    [lambda.Runtime.NODEJS_6_10],
+    [lambda.Runtime.NODEJS_8_10],
+    [lambda.Runtime.NODEJS_10_X],
+    [lambda.Runtime.NODEJS_12_X],
+    [lambda.Runtime.NODEJS_14_X],
+    [lambda.Runtime.DOTNET_CORE_1],
+    [lambda.Runtime.DOTNET_CORE_2],
+    [lambda.Runtime.DOTNET_CORE_2_1],
+    [lambda.Runtime.DOTNET_CORE_3_1],
+    [lambda.Runtime.GO_1_X],
+    [lambda.Runtime.RUBY_2_5],
+    [lambda.Runtime.PROVIDED],
+  ])('%s is deprecated', (runtime) => {
+    expect(runtime.isDeprecated).toEqual(true);
   });
 });
