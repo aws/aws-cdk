5900e051c5816b532333e3e8474c48dbf1e17c98
rafaelrcamargo <rafaelrakochinski@gmail.com>
2023-12-01 15:39:23 -0300

    test: aliasArn should be a valid ARN

--- packages/aws-cdk-lib/aws-kms/test/alias.test.ts
+++ packages/aws-cdk-lib/aws-kms/test/alias.test.ts
@@ -2,7 +2,7 @@ import { Construct } from 'constructs';
 import { Template } from '../../assertions';
 import * as iam from '../../aws-iam';
 import { ArnPrincipal, PolicyStatement } from '../../aws-iam';
-import { App, Aws, CfnOutput, Stack } from '../../core';
+import { App, Arn, Aws, CfnOutput, Stack } from '../../core';
 import { KMS_ALIAS_NAME_REF } from '../../cx-api';
 import { Alias } from '../lib/alias';
 import { IKey, Key } from '../lib/key';
@@ -365,6 +365,20 @@ test('aliasArn and keyArn from alias should match', () => {
   const alias = new Alias(stack, 'Alias', { targetKey: key, aliasName: 'alias/foo' });
 
   expect(alias.aliasArn).toEqual(alias.keyArn);
+});
+
+test('aliasArn should be a valid ARN', () => {
+  const app = new App();
+  const stack = new Stack(app, 'Test');
+  const key = new Key(stack, 'Key');
+
+  const alias = new Alias(stack, 'Alias', { targetKey: key, aliasName: 'alias/foo' });
+
+  expect(alias.aliasArn).toEqual(Arn.format({
+    service: 'kms',
+    // aliasName already contains the '/'
+    resource: alias.aliasName,
+  }, stack));
 });
 
 class AliasOutputsConstruct extends Construct {
