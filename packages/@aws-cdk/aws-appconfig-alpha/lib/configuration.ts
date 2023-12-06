ec141b52394aa70c467c2ee984f9cc190540287f
Jane Chen <125300057+chenjane-dev@users.noreply.github.com>
2023-12-06 12:34:29 -0500

    feat(appconfig-alpha): add deploy method to configuration constructs (#28269)
    Adding a deploy method to the configuration construct to allow deployments on a config at any time after creation.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-appconfig-alpha/lib/configuration.ts
+++ packages/@aws-cdk/aws-appconfig-alpha/lib/configuration.ts
@@ -303,6 +303,24 @@ abstract class ConfigurationBase extends Construct implements IConfiguration, IE
     this.extensible.addExtension(extension);
   }
 
+  /**
+   * Deploys the configuration to the specified environment.
+   *
+   * @param environment The environment to deploy the configuration to
+   */
+  public deploy(environment: IEnvironment) {
+    const logicalId = `Deployment${this.getDeploymentHash(environment)}`;
+    new CfnDeployment(this, logicalId, {
+      applicationId: this.application.applicationId,
+      configurationProfileId: this.configurationProfileId,
+      deploymentStrategyId: this.deploymentStrategy!.deploymentStrategyId,
+      environmentId: environment.environmentId,
+      configurationVersion: this.versionNumber!,
+      description: this.description,
+      kmsKeyIdentifier: this.deploymentKey?.keyArn,
+    });
+  }
+
   protected addExistingEnvironmentsToApplication() {
     this.deployTo?.forEach((environment) => {
       if (!this.application.environments.includes(environment)) {
@@ -320,16 +338,7 @@ abstract class ConfigurationBase extends Construct implements IConfiguration, IE
       if ((this.deployTo && !this.deployTo.includes(environment))) {
         return;
       }
-      const logicalId = `Deployment${this.getDeploymentHash(environment)}`;
-      new CfnDeployment(this, logicalId, {
-        applicationId: this.application.applicationId,
-        configurationProfileId: this.configurationProfileId,
-        deploymentStrategyId: this.deploymentStrategy!.deploymentStrategyId,
-        environmentId: environment.environmentId,
-        configurationVersion: this.versionNumber!,
-        description: this.description,
-        kmsKeyIdentifier: this.deploymentKey?.keyArn,
-      });
+      this.deploy(environment);
     });
   }
 }
