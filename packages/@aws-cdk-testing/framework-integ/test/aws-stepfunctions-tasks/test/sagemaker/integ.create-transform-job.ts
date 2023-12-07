efe672f5becfba6f753f6a913e460553e1f0e802
Vinayak Kukreja <vinakuk@amazon.com>
2023-12-07 13:33:38 -0800

    run integ test
    Signed-off-by: Vinayak Kukreja <vinakuk@amazon.com>

--- packages/@aws-cdk-testing/framework-integ/test/aws-stepfunctions-tasks/test/sagemaker/integ.create-transform-job.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-stepfunctions-tasks/test/sagemaker/integ.create-transform-job.ts
@@ -2,6 +2,7 @@ import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { InstanceType, InstanceClass, InstanceSize } from 'aws-cdk-lib/aws-ec2';
 import { SageMakerCreateTransformJob, S3DataType } from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { StateMachine, IntegrationPattern, DefinitionBody } from 'aws-cdk-lib/aws-stepfunctions';
+import * as integ from '@aws-cdk/integ-tests-alpha';
 
 class StepFunctionsTaskCreateTransformJobIntegStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
@@ -35,5 +36,10 @@ class StepFunctionsTaskCreateTransformJobIntegStack extends Stack {
 }
 
 const app = new App();
-new StepFunctionsTaskCreateTransformJobIntegStack(app, 'aws-cdk-step-functions-task-create-transform-job-integ');
+const stack = new StepFunctionsTaskCreateTransformJobIntegStack(app, 'aws-cdk-step-functions-task-create-transform-job-integ');
+
+new integ.IntegTest(app, 'SqsTest', {
+  testCases: [stack],
+});
+
 app.synth();
