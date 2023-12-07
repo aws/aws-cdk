6712a4e7cc1b9cecc3250c1f94410444ee590e87
Francis <colifran@amazon.com>
2023-12-07 10:39:17 -0800

    merge conflicts
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/handler-framework/test/cdk-handler.test.ts
+++ packages/aws-cdk-lib/handler-framework/test/cdk-handler.test.ts
@@ -9,6 +9,20 @@ describe('cdk handler', () => {
     codeDirectory = path.join(__dirname, 'test-handler');
   });
 
+  test('code directory property is correctly set', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    const handler = new CdkHandler(stack, 'CdkHandler', {
+      codeDirectory,
+      compatibleRuntimes: [Runtime.NODEJS_16_X, Runtime.NODEJS_LATEST, Runtime.PYTHON_3_12],
+    });
+
+    // THEN
+    expect(handler.codeDirectory).toEqual(codeDirectory);
+  });
+
   test('runtime property is correctly set', () => {
     // GIVEN
     const stack = new Stack();
