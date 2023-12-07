efdef2ec595a759553007bbf61e73ac708fe30f9
Francis <colifran@amazon.com>
2023-12-07 01:07:14 -0800

    removed unneeded props and attributes
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/handler-framework/lib/cdk-handler.ts
+++ packages/aws-cdk-lib/handler-framework/lib/cdk-handler.ts
@@ -16,11 +16,6 @@ export interface CdkHandlerProps {
    * Runtimes that are compatible with the source code.
    */
   readonly compatibleRuntimes: Runtime[];
-
-  /**
-   * The name of the method within your code that Lambda calls to execute your function.
-   */
-  readonly entrypoint: string;
 }
 
 /**
@@ -33,19 +28,9 @@ export class CdkHandler extends Construct {
   private static readonly DEFAULT_RUNTIME = Runtime.NODEJS_LATEST;
 
   /**
-   * The source code loaded from a local disk path.
-   */
-  public readonly codeDirectory: string;
-
-  /**
    * The source code of your Lambda function.
    */
   public readonly code: Code;
-
-  /**
-   * The name of the method within your code that Lambda calls to execute your function.
-   */
-  public readonly entrypoint: string;
 
   /**
    * The latest runtime that is compatible with the source code.
@@ -54,9 +39,7 @@ export class CdkHandler extends Construct {
 
   public constructor(scope: Construct, id: string, props: CdkHandlerProps) {
     super(scope, id);
-    this.codeDirectory = props.codeDirectory;
     this.code = Code.fromAsset(props.codeDirectory);
-    this.entrypoint = props.entrypoint;
     this.runtime = RuntimeDeterminer.determineLatestRuntime(CdkHandler.DEFAULT_RUNTIME, props.compatibleRuntimes);
   }
 }
--- packages/aws-cdk-lib/handler-framework/test/cdk-handler.test.ts
+++ packages/aws-cdk-lib/handler-framework/test/cdk-handler.test.ts
@@ -9,21 +9,6 @@ describe('cdk handler', () => {
     codeDirectory = path.join(__dirname, 'test-handler');
   });
 
-  test('code directory property is correctly set', () => {
-    // GIVEN
-    const stack = new Stack();
-
-    // WHEN
-    const handler = new CdkHandler(stack, 'CdkHandler', {
-      codeDirectory,
-      entrypoint: 'index.handler',
-      compatibleRuntimes: [Runtime.NODEJS_LATEST],
-    });
-
-    // THEN
-    expect(handler.codeDirectory).toEqual(codeDirectory);
-  });
-
   test('runtime property is correctly set', () => {
     // GIVEN
     const stack = new Stack();
@@ -31,27 +16,10 @@ describe('cdk handler', () => {
     // WHEN
     const handler = new CdkHandler(stack, 'CdkHandler', {
       codeDirectory,
-      entrypoint: 'index.handler',
       compatibleRuntimes: [Runtime.NODEJS_16_X, Runtime.NODEJS_LATEST, Runtime.PYTHON_3_12],
     });
 
     // THEN
     expect(handler.runtime.runtimeEquals(Runtime.NODEJS_LATEST)).toBe(true);
-  });
-
-  test('entrypoint property is set correctly for non-default entrypoint', () => {
-    // GIVEN
-    const stack = new Stack();
-    const entrypoint = 'index.onEventHandler';
-
-    // WHEN
-    const handler = new CdkHandler(stack, 'CdkHandler', {
-      codeDirectory,
-      entrypoint,
-      compatibleRuntimes: [Runtime.NODEJS_LATEST],
-    });
-
-    // THEN
-    expect(handler.entrypoint).toEqual(entrypoint);
   });
 });
