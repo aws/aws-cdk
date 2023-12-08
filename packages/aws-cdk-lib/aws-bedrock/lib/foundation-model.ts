74c7757ebb1d8092c979e3aa1815121c0615adff
Clare Liguori <liguori@amazon.com>
2023-12-08 08:24:42 -0800

    Add missing readonly

--- packages/aws-cdk-lib/aws-bedrock/lib/foundation-model.ts
+++ packages/aws-cdk-lib/aws-bedrock/lib/foundation-model.ts
@@ -9,52 +9,52 @@ import { ArnFormat, Stack } from '../../core';
  */
 export class FoundationModelIdentifier {
   /** Base model "ai21.j2-mid-v1". */
-  public static AI21_LABS_JURASSIC_2_MID_V1 = new FoundationModelIdentifier('ai21.j2-mid-v1');
+  public static readonly AI21_LABS_JURASSIC_2_MID_V1 = new FoundationModelIdentifier('ai21.j2-mid-v1');
 
   /** Base model "ai21.j2-ultra-v1". */
-  public static AI21_LABS_JURASSIC_2_ULTRA_V1 = new FoundationModelIdentifier('ai21.j2-ultra-v1');
+  public static readonly AI21_LABS_JURASSIC_2_ULTRA_V1 = new FoundationModelIdentifier('ai21.j2-ultra-v1');
 
   /** Base model "amazon.titan-embed-text-v1". */
-  public static AMAZON_TITAN_EMBEDDINGS_G1_TEXT_V1 = new FoundationModelIdentifier('amazon.titan-embed-text-v1');
+  public static readonly AMAZON_TITAN_EMBEDDINGS_G1_TEXT_V1 = new FoundationModelIdentifier('amazon.titan-embed-text-v1');
 
   /** Base model "amazon.titan-text-express-v1". */
-  public static AMAZON_TITAN_TEXT_G1_EXPRESS_V1 = new FoundationModelIdentifier('amazon.titan-text-express-v1');
+  public static readonly AMAZON_TITAN_TEXT_G1_EXPRESS_V1 = new FoundationModelIdentifier('amazon.titan-text-express-v1');
 
   /** Base model "amazon.titan-embed-image-v1". */
-  public static AMAZON_TITAN_MULTIMODAL_EMBEDDINGS_G1_V1 = new FoundationModelIdentifier('amazon.titan-embed-image-v1');
+  public static readonly AMAZON_TITAN_MULTIMODAL_EMBEDDINGS_G1_V1 = new FoundationModelIdentifier('amazon.titan-embed-image-v1');
 
   /** Base model "amazon.titan-image-generator-v1". */
-  public static AMAZON_TITAN_IMAGE_GENERATOR_G1_V1 = new FoundationModelIdentifier('amazon.titan-image-generator-v1');
+  public static readonly AMAZON_TITAN_IMAGE_GENERATOR_G1_V1 = new FoundationModelIdentifier('amazon.titan-image-generator-v1');
 
   /** Base model "anthropic.claude-v1". */
-  public static ANTHROPIC_CLAUDE_V1 = new FoundationModelIdentifier('anthropic.claude-v1');
+  public static readonly ANTHROPIC_CLAUDE_V1 = new FoundationModelIdentifier('anthropic.claude-v1');
 
   /** Base model "anthropic.claude-v2". */
-  public static ANTHROPIC_CLAUDE_V2 = new FoundationModelIdentifier('anthropic.claude-v2');
+  public static readonly ANTHROPIC_CLAUDE_V2 = new FoundationModelIdentifier('anthropic.claude-v2');
 
   /** Base model "anthropic.claude-v2:1". */
-  public static ANTHROPIC_CLAUDE_V2_1 = new FoundationModelIdentifier('anthropic.claude-v2:1');
+  public static readonly ANTHROPIC_CLAUDE_V2_1 = new FoundationModelIdentifier('anthropic.claude-v2:1');
 
   /** Base model "anthropic.claude-instant-v1". */
-  public static ANTHROPIC_CLAUDE_INSTANT_V1 = new FoundationModelIdentifier('anthropic.claude-instant-v1');
+  public static readonly ANTHROPIC_CLAUDE_INSTANT_V1 = new FoundationModelIdentifier('anthropic.claude-instant-v1');
 
   /** Base model "cohere.command-text-v14". */
-  public static COHERE_COMMAND_V14 = new FoundationModelIdentifier('cohere.command-text-v14');
+  public static readonly COHERE_COMMAND_V14 = new FoundationModelIdentifier('cohere.command-text-v14');
 
   /** Base model "cohere.command-light-text-v14". */
-  public static COHERE_COMMAND_LIGHT_V14 = new FoundationModelIdentifier('cohere.command-light-text-v14');
+  public static readonly COHERE_COMMAND_LIGHT_V14 = new FoundationModelIdentifier('cohere.command-light-text-v14');
 
   /** Base model "cohere.embed-english-v3". */
-  public static COHERE_EMBED_ENGLISH_V3 = new FoundationModelIdentifier('cohere.embed-english-v3');
+  public static readonly COHERE_EMBED_ENGLISH_V3 = new FoundationModelIdentifier('cohere.embed-english-v3');
 
   /** Base model "cohere.embed-multilingual-v3". */
-  public static COHERE_EMBED_MULTILINGUAL_V3 = new FoundationModelIdentifier('cohere.embed-multilingual-v3');
+  public static readonly COHERE_EMBED_MULTILINGUAL_V3 = new FoundationModelIdentifier('cohere.embed-multilingual-v3');
 
   /** Base model "meta.llama2-13b-chat-v1". */
-  public static META_LLAMA_2_CHAT_13B_V1 = new FoundationModelIdentifier('meta.llama2-13b-chat-v1');
+  public static readonly META_LLAMA_2_CHAT_13B_V1 = new FoundationModelIdentifier('meta.llama2-13b-chat-v1');
 
   /** Base model "meta.llama2-70b-chat-v1". */
-  public static META_LLAMA_2_CHAT_70B_V1 = new FoundationModelIdentifier('meta.llama2-70b-chat-v1');
+  public static readonly META_LLAMA_2_CHAT_70B_V1 = new FoundationModelIdentifier('meta.llama2-70b-chat-v1');
 
   public constructor(public readonly modelId: string) { }
 }
