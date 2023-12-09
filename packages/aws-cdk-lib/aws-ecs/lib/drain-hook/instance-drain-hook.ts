7b8723104b5578be78e259e515bdad48238dbe0f
Francis <colifran@amazon.com>
2023-12-08 18:24:25 -0800

    drain hook revert
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/aws-cdk-lib/aws-ecs/lib/drain-hook/instance-drain-hook.ts
+++ packages/aws-cdk-lib/aws-ecs/lib/drain-hook/instance-drain-hook.ts
@@ -61,9 +61,9 @@ export class InstanceDrainHook extends Construct {
 
     // Invoke Lambda via SNS Topic
     const fn = new lambda.Function(this, 'Function', {
-      code: lambda.Code.inline(fs.readFileSync(path.join(__dirname, 'lambda-source', 'index.py'), { encoding: 'utf-8' })),
+      code: lambda.Code.fromInline(fs.readFileSync(path.join(__dirname, '..', '..', '..', 'custom-resource-handlers', 'dist', 'aws-ecs', 'lambda-source', 'index.py'), { encoding: 'utf-8' })),
       handler: 'index.lambda_handler',
-      runtime: lambda.Runtime.PYTHON_3_6,
+      runtime: lambda.Runtime.PYTHON_3_9,
       // Timeout: some extra margin for additional API calls made by the Lambda,
       // up to a maximum of 15 minutes.
       timeout: cdk.Duration.seconds(Math.min(drainTime.toSeconds() + 10, 900)),
