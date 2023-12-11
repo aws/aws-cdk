7a29244fbbc0407811adae2934d359b878a2c0d2
kazuho cryer-shinozuka <malaysia.cryer@gmail.com>
2023-12-12 01:09:02 +0900

    Update packages/aws-cdk-lib/aws-ecs-patterns/lib/base/queue-processing-service-base.ts
    Co-authored-by: Luca Pizzini <lpizzini7@gmail.com>

--- packages/aws-cdk-lib/aws-ecs-patterns/lib/base/queue-processing-service-base.ts
+++ packages/aws-cdk-lib/aws-ecs-patterns/lib/base/queue-processing-service-base.ts
@@ -40,7 +40,7 @@ export interface QueueProcessingServiceBaseProps {
    * The image used to start a container.
    *
    * For `QueueProcessingFargateService`, either `image` or `taskDefinition` must be specified, but not both.
-   * For QueueProcessingEc2Service, `image` is required.
+   * For `QueueProcessingEc2Service`, `image` is required.
    *
    * @default - the image of the task definition is used for Fargate, required otherwise
    */
