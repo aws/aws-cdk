0e9573f468825925d18c34ba4383215b74530b38
Mitchell Valine <mitchellvaline@yahoo.com>
2023-12-12 16:17:44 -0800

    Experiment with using  rule in eslint

--- packages/aws-cdk-lib/.eslintrc.js
+++ packages/aws-cdk-lib/.eslintrc.js
@@ -13,4 +13,37 @@ baseConfig.rules['import/no-extraneous-dependencies'] = [
   }
 ];
 
+// Ban construction of these classes for lambda handlers and point authors to use our new code generation tools
+const bannedConstructs = {
+  'aws-cdk-lib/aws-lambda': [ 'Function', 'SingletonFunction' ],
+  'aws-cdk-lib/core': [ 'CustomResourceProvider' ],
+}
+
+const restrictedConfig = Object.entries(bannedConstructs).reduce((accum, [module, types]) => {
+  return [
+    ...accum,
+    ...types.map((typeName) => {
+      return {
+        selector: `NewExpression[callee.name='${typeName}']`,
+        message: `Usage of ${module}.${typeName} in 'aws-cdk-lib' is disallowed. Instead, build and code generate a construct for your handler using @aws-cdk/custom-resource-framework`,
+      };
+    }),
+  ]
+}, [],)
+
+baseConfig.rules['no-restricted-syntax'] = [
+  'error',
+  ...restrictedConfig,
+]
+
+// Allow usage of disallowed handler constructs in test files
+baseConfig.overrides = [
+  {
+    files: ["*.test.ts"],
+    rules: {
+      'no-restricted-syntax': 'off',
+    }
+  }
+];
+
 module.exports = baseConfig;
