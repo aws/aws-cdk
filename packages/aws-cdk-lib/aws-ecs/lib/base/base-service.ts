0d00ca2bfd1c23ff9498a1404c131abb0625b3cf
Sumu <sumughan@amazon.com>
2023-12-14 16:58:11 -0500

    remove commented out default setting code
    Signed-off-by: Sumu <sumughan@amazon.com>

--- packages/aws-cdk-lib/aws-ecs/lib/base/base-service.ts
+++ packages/aws-cdk-lib/aws-ecs/lib/base/base-service.ts
@@ -709,13 +709,6 @@ export abstract class BaseService extends Resource
         rollback: props.deploymentAlarms.behavior !== AlarmBehavior.FAIL_ON_ALARM,
       };
     // CloudWatch alarms is only supported for Amazon ECS services that use the rolling update (ECS) deployment controller.
-    } else if ((!props.deploymentController ||
-      props.deploymentController?.type === DeploymentControllerType.ECS) && this.deploymentAlarmsAvailableInRegion()) {
-      // this.deploymentAlarms = {
-      //   alarmNames: [],
-      //   enable: false,
-      //   rollback: false,
-      // };
     }
 
     this.node.defaultChild = this.resource;
