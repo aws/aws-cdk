7b3e7c10eec9962c0297917c3d4c02bbff21435b
Francis <colifran@amazon.com>
2023-12-14 14:15:00 -0800

    name generator
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk/custom-resource-handlers/lib/custom-resources-framework/utils/framework-utils.ts
+++ packages/@aws-cdk/custom-resource-handlers/lib/custom-resources-framework/utils/framework-utils.ts
@@ -8,7 +8,7 @@ export function buildComponentName(fqn: string, type: ComponentType, entrypoint:
   )) + (handler.charAt(0).toUpperCase() + handler.slice(1));
   return name.charAt(0).toUpperCase()
     + name.slice(1)
-    + type === ComponentType.CUSTOM_RESOURCE_PROVIDER ? 'Provider' : 'Function';
+    + (type === ComponentType.CUSTOM_RESOURCE_PROVIDER ? 'Provider' : type);
 }
 
 export function toLambdaRuntime(runtime: Runtime) {
