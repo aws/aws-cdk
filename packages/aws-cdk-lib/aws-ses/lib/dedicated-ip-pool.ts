b0cd81b031cd8631489e3d0dec0e07b23da27afa
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2023-12-17 21:47:20 +0900

    fix: Responding to Reviews

--- packages/aws-cdk-lib/aws-ses/lib/dedicated-ip-pool.ts
+++ packages/aws-cdk-lib/aws-ses/lib/dedicated-ip-pool.ts
@@ -4,6 +4,7 @@ import { IResource, Resource } from '../../core';
 
 /**
  * What kind of scailing mode to use for this IP pool
+ *
  * @see https://docs.aws.amazon.com/ja_jp/ses/latest/dg/dedicated-ip.html
  */
 export enum ScalingMode {
@@ -11,6 +12,7 @@ export enum ScalingMode {
    * The customer controls which IPs are part of the dedicated IP pool.
    */
   STANDARD = 'STANDARD',
+
   /**
    * The reputation and number of IPs are automatically managed by Amazon SES.
    */
@@ -43,6 +45,10 @@ export interface DedicatedIpPoolProps {
 
   /**
    * The type of scailing mode to use for this IP pool
+   *
+   * Updating ScalingMode doesn't require a replacement if you're updating its value from `STANDARD` to `MANAGED`.
+   * However, updating ScalingMode from `MANAGED` to `STANDARD` is not supported.
+   *
    * @default ScalingMode.STANDARD
    */
   readonly scalingMode?: ScalingMode;
@@ -71,7 +77,7 @@ export class DedicatedIpPool extends Resource implements IDedicatedIpPool {
 
     const pool = new CfnDedicatedIpPool(this, 'Resource', {
       poolName: this.physicalName,
-      scalingMode: props.scalingMode ?? ScalingMode.STANDARD,
+      scalingMode: props.scalingMode,
     });
 
     this.dedicatedIpPoolName = pool.ref;
