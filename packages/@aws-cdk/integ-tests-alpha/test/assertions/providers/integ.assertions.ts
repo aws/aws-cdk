3d2a8dced07dfdcfa5c2bc71146781977ef5eb28
sakurai-ryo <sakurairyo.10do@gmail.com>
2023-12-17 19:52:40 +0900

    fix: add integ test

--- packages/@aws-cdk/integ-tests-alpha/test/assertions/providers/integ.assertions.ts
+++ packages/@aws-cdk/integ-tests-alpha/test/assertions/providers/integ.assertions.ts
@@ -12,6 +12,11 @@ const ssmParameter = new CfnResource(stack, 'Utf8Parameter', {
   },
 });
 
+const queue = new CfnResource(stack, 'Queue', {
+  type: 'AWS::SQS::Queue',
+  properties: {},
+});
+
 const integ = new IntegTest(app, 'AssertionsTest', {
   testCases: [stack],
 });
@@ -40,4 +45,34 @@ const secondAssertion = integ.assertions.awsApiCall('SSM', 'getParameter', {
   }),
 );
 
-firstAssertion.next(secondAssertion);
+const metricAssertion = integ.assertions.awsApiCall('CloudWatch', 'getMetricData', {
+  MetricDataQueries: [
+    {
+      Id: 'id1',
+      MetricStat: {
+        Metric: {
+          Namespace: 'AWS/SQS',
+          MetricName: 'NumberOfMessagesReceived',
+          Dimensions: [
+            {
+              Name: 'QueueName',
+              Value: queue.getAtt('QueueName').toString(),
+            },
+          ],
+        },
+        Period: 60,
+        Stat: 'Sum',
+      },
+      ReturnData: true,
+    },
+  ],
+  StartTime: new Date(Date.now() - 24 * 60 * 60 * 1000),
+  EndTime: new Date(),
+});
+metricAssertion.provider.addToRolePolicy({
+  Effect: 'Allow',
+  Action: ['cloudwatch:GetMetricData'],
+  Resource: ['*'],
+});
+
+firstAssertion.next(secondAssertion.next(metricAssertion));
