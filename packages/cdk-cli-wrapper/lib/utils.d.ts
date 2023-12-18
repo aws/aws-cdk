66200431fb17321b769bb0f5770c606b9cdb4608
Nico Schmidt <nico.schmidt@deutschebahn.com>
2023-12-18 15:31:42 +0100

    fix: removed cdk-cki-wrapper due to unknown origin. Fix formatting

--- packages/cdk-cli-wrapper/lib/utils.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Our own execute function which doesn't use shells and strings.
- */
-export declare function exec(commandLine: string[], options?: {
-    cwd?: string;
-    json?: boolean;
-    verbose?: boolean;
-    env?: any;
-}): any;
