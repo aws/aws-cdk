36582879945c32c14831b9f01c652e9a0ff4e898
Nico Schmidt <nico.schmidt@deutschebahn.com>
2023-12-19 10:05:48 +0100

    fix: test

--- packages/aws-cdk-lib/pipelines/test/blueprint/helpers-internal/pipeline-graph.test.ts
+++ packages/aws-cdk-lib/pipelines/test/blueprint/helpers-internal/pipeline-graph.test.ts
@@ -118,21 +118,21 @@ describe('blueprint with wave and stage', () => {
     // GIVEN
     let blueprintWithAllPrepareNodesFirst: Blueprint;
     beforeEach(() => {
-      blueprint = new Blueprint(app, 'Bp', {
+      blueprintWithAllPrepareNodesFirst = new Blueprint(app, 'Bp', {
         synth: new cdkp.ShellStep('Synth', {
           input: cdkp.CodePipelineSource.gitHub('test/test', 'main'),
           commands: ['build'],
         }),
       });
-    blueprintWithAllPrepareNodesFirst.addStage(new OneStackApp(app, 'CrossAccount', { env: { account: 'you' } }));
-    const appWithExposedStacks = new AppWithExposedStacks(app, 'Gamma');
+      blueprintWithAllPrepareNodesFirst.addStage(new OneStackApp(app, 'CrossAccount', { env: { account: 'you' } }));
+      const appWithExposedStacks = new AppWithExposedStacks(app, 'Gamma');
 
-    blueprintWithAllPrepareNodesFirst.waves[0].addStage(appWithExposedStacks, {
-      postPrepare: [
-        new cdkp.ManualApprovalStep('Step1'),
+      blueprintWithAllPrepareNodesFirst.waves[0].addStage(appWithExposedStacks, {
+        postPrepare: [
+          new cdkp.ManualApprovalStep('Step1'),
         // new cdkp.ManualApprovalStep('Step2'),
         // new cdkp.ManualApprovalStep('Step3'),
-      ],
+        ],
       // stackSteps: [
       //   {
       //     stack,
@@ -145,19 +145,21 @@ describe('blueprint with wave and stage', () => {
       //     post: [new cdkp.ManualApprovalStep('Post Approval')],
       //   },
       // ],
+      });
+
+      // WHEN
+      const graph = new PipelineGraph(blueprintWithAllPrepareNodesFirst).graph;
+      // THEN
+      expect(childrenAt(graph, 'Wave', 'Gamma', 'Stack1')).toEqual([
+        'Prepare-Gamma-Stack1',
+        'Step1',
+        'Step2',
+        'Step3',
+        'Deploy',
+
+      ]);
     });
 
-    // WHEN
-    const graph = new PipelineGraph(blueprintWithAllPrepareNodesFirst).graph;
-    // THEN
-    expect(childrenAt(graph, 'Wave', 'Gamma', 'Stack1')).toEqual([
-      'Prepare-Gamma-Stack1',
-      'Step1',
-      'Step2',
-      'Step3',
-      'Deploy',
-
-    ]);
   });
 
   test('pre, changeSet, and post are added correctly inside stack graph', () => {
