f133272e62361f3d2074eae5978f8a68b66fc621
kazuho cryer-shinozuka <malaysia.cryer@gmail.com>
2023-12-20 02:49:48 +0900

    feat(ses): add scalingMode property to dedicated ip pool (#28392)
    Closes #27418
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-ses/lib/dedicated-ip-pool.ts
+++ packages/aws-cdk-lib/aws-ses/lib/dedicated-ip-pool.ts
@@ -3,6 +3,23 @@ import { CfnDedicatedIpPool } from './ses.generated';
 import { IResource, Resource } from '../../core';
 
 /**
+ * Scaling mode to use for this IP pool.
+ *
+ * @see https://docs.aws.amazon.com/ses/latest/dg/dedicated-ip.html
+ */
+export enum ScalingMode {
+  /**
+   * The customer controls which IPs are part of the dedicated IP pool.
+   */
+  STANDARD = 'STANDARD',
+
+  /**
+   * The reputation and number of IPs are automatically managed by Amazon SES.
+   */
+  MANAGED = 'MANAGED',
+}
+
+/**
  * A dedicated IP pool
  */
 export interface IDedicatedIpPool extends IResource {
@@ -19,11 +36,21 @@ export interface IDedicatedIpPool extends IResource {
  */
 export interface DedicatedIpPoolProps {
   /**
-   * A name for the dedicated IP pool
+   * A name for the dedicated IP pool.
    *
    * @default - a CloudFormation generated name
    */
   readonly dedicatedIpPoolName?: string;
+
+  /**
+   * The type of scailing mode to use for this IP pool
+   *
+   * Updating ScalingMode doesn't require a replacement if you're updating its value from `STANDARD` to `MANAGED`.
+   * However, updating ScalingMode from `MANAGED` to `STANDARD` is not supported.
+   *
+   * @default ScalingMode.STANDARD
+   */
+  readonly scalingMode?: ScalingMode;
 }
 
 /**
@@ -49,6 +76,7 @@ export class DedicatedIpPool extends Resource implements IDedicatedIpPool {
 
     const pool = new CfnDedicatedIpPool(this, 'Resource', {
       poolName: this.physicalName,
+      scalingMode: props.scalingMode,
     });
 
     this.dedicatedIpPoolName = pool.ref;
