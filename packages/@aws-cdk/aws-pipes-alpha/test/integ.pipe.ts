a41dab4af600b4782cc3dc522167b4130cfe0654
Raphael Manke <github@manke.cloud>
2023-12-21 17:11:49 +0100

    feat(pipes): extend integ test

--- packages/@aws-cdk/aws-pipes-alpha/test/integ.pipe.ts
+++ packages/@aws-cdk/aws-pipes-alpha/test/integ.pipe.ts
@@ -1,12 +1,22 @@
 import { randomUUID } from 'crypto';
 import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
 import * as cdk from 'aws-cdk-lib';
-import { Pipe } from '../lib';
+import { Code } from 'aws-cdk-lib/aws-lambda';
+import { IncludeExecutionData, LogLevel, Pipe } from '../lib';
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'aws-cdk-pipes');
 const sourceQueue = new cdk.aws_sqs.Queue(stack, 'SourceQueue');
 const targetQueue = new cdk.aws_sqs.Queue(stack, 'TargetQueue');
+
+const enrichmentHandlerCode = 'exports.handler = async (event) => { return event.map( record => ({...record, body: record.body + "-enriched"}) ) };';
+const enrichmentLambda = new cdk.aws_lambda.Function(stack, 'EnrichmentLambda', {
+  code: Code.fromInline(enrichmentHandlerCode),
+  handler: 'index.handler',
+  runtime: cdk.aws_lambda.Runtime.NODEJS_LATEST,
+});
+
+const loggroup = new cdk.aws_logs.LogGroup(stack, 'LogGroup');
 
 new Pipe(stack, 'Pipe', {
   pipeName: 'BaseTestPipe',
@@ -22,6 +32,26 @@ new Pipe(stack, 'Pipe', {
       inputTemplate: '<$.body>',
     },
   },
+  enrichment: {
+    enrichmentArn: enrichmentLambda.functionArn,
+    enrichmentParameters: {},
+    grantInvoke: (role) => {
+      enrichmentLambda.grantInvoke(role);
+    },
+  },
+  logLevel: LogLevel.TRACE,
+  logIncludeExecutionData: [IncludeExecutionData.ALL],
+
+  logDestinations: [
+    {
+      parameters: {
+        cloudwatchLogsLogDestination: {
+          logGroupArn: loggroup.logGroupArn,
+        },
+      },
+      grantPush: (role) => loggroup.grantWrite(role),
+    },
+  ],
 });
 
 const test = new IntegTest(app, 'integtest-pipe-sqs-to-sqs', {
@@ -40,11 +70,11 @@ putMessageOnQueue.next(test.assertions.awsApiCall('SQS', 'receiveMessage',
   })).expect(ExpectedResult.objectLike({
   Messages: [
     {
-      Body: uniqueIdentifier,
+      Body: uniqueIdentifier+ '-enriched',
     },
   ],
 })).waitForAssertions({
-  totalTimeout: cdk.Duration.seconds(20),
+  totalTimeout: cdk.Duration.seconds(30),
 });
 // TODO add test for pipeName, pipeArn, pipeRole
 app.synth();
