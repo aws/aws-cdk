545b273ab1b0be385f6a28d7c0f2d537f34c2ae1
sakurai-ryo <58683719+sakurai-ryo@users.noreply.github.com>
2023-12-23 09:24:53 +0900

    feat(codebuild): Lambda compute for codebuild projects (#27934)
    CodeBuild has added support for Lambda compute.
    CloudFormation can be deployed as follows.
    ```yaml
    Resources:
      CodeBuildProject:
        Type: AWS::CodeBuild::Project
        Properties:
          Artifacts:
            Type: NO_ARTIFACTS
          ServiceRole: !GetAtt CodeBuildRole.Arn
          Source:
            # 
          Environment:
            Type: LINUX_LAMBDA_CONTAINER
            ComputeType: BUILD_LAMBDA_1GB
            Image: aws/codebuild/amazonlinux-x86_64-lambda-standard:go1.21
      CodeBuildRole:
        Type: AWS::IAM::Role
        Properties:
            # 
    ```
    https://aws.amazon.com/about-aws/whats-new/2023/11/aws-codebuild-lambda-compute
    
    This PR implements Lambda ComputeType by adding Classes (`LinuxArmLambdaBuildImage`, `LinuxLambdaBuildImage`) that extend the IBuildImage interface.
    
    Supported Docker Images and ComputeTypes are listed below.
    https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
    https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-compute-types.html#environment.types
    
    Also, Lambda compute has some limitations and I have added validation for them.
    https://docs.aws.amazon.com/codebuild/latest/userguide/lambda.html#lambda.limitations
    
    closes #28418
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/aws-cdk-lib/aws-codebuild/lib/linux-arm-lambda-build-image.ts
@@ -0,0 +1,85 @@
+import { BuildSpec } from './build-spec';
+import { ComputeType } from './compute-type';
+import { runScriptLinuxBuildSpec } from './private/run-script-linux-build-spec';
+import { BuildEnvironment, IBuildImage, isLambdaComputeType } from './project';
+
+/**
+ * Construction properties of `LinuxArmLambdaBuildImage`.
+ * Module-private, as the constructor of `LinuxArmLambdaBuildImage` is private.
+ */
+interface LinuxArmLambdaBuildImageProps {
+  readonly imageId: string;
+}
+
+/**
+ * A CodeBuild image running aarch64 Lambda.
+ *
+ * This class has a bunch of public constants that represent the CodeBuild aarch64 Lambda images.
+ *
+ * @see https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
+ */
+export class LinuxArmLambdaBuildImage implements IBuildImage {
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:nodejs18` build image. */
+  public static readonly AMAZON_LINUX_2_NODE_18 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:nodejs18');
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:python3.11` build image. */
+  public static readonly AMAZON_LINUX_2_PYTHON_3_11 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:python3.11');
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:ruby3.2` build image. */
+  public static readonly AMAZON_LINUX_2_RUBY_3_2 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:ruby3.2');
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto17` build image. */
+  public static readonly AMAZON_LINUX_2_CORRETTO_17 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto17');
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto11` build image. */
+  public static readonly AMAZON_LINUX_2_CORRETTO_11 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto11');
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:go1.21` build image. */
+  public static readonly AMAZON_LINUX_2_GO_1_21 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:go1.21');
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:dotnet6` build image. */
+  public static readonly AMAZON_LINUX_2_DOTNET_6 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:dotnet6');
+
+  /**
+   * Uses a Docker image provided by CodeBuild.
+   *
+   * NOTE: In Lambda compute, since only specified images can be used, this method is set to private.
+   *
+   * @see https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
+   *
+   * @param id The image identifier
+   * @example 'aws/codebuild/amazonlinux-aarch64-lambda-standard:nodejs18'
+   * @returns A Docker image provided by CodeBuild.
+   */
+  private static fromCodeBuildImageId(id: string): IBuildImage {
+    return new LinuxArmLambdaBuildImage({
+      imageId: id,
+    });
+  }
+
+  public readonly type = 'ARM_LAMBDA_CONTAINER';
+  public readonly defaultComputeType = ComputeType.LAMBDA_1GB;
+  public readonly imageId: string;
+
+  private constructor(props: LinuxArmLambdaBuildImageProps) {
+    this.imageId = props.imageId;
+  }
+
+  public validate(buildEnvironment: BuildEnvironment): string[] {
+    const errors = [];
+
+    if (buildEnvironment.privileged) {
+      errors.push('Lambda compute type does not support privileged mode');
+    }
+
+    if (buildEnvironment.computeType && !isLambdaComputeType(buildEnvironment.computeType)) {
+      errors.push([
+        'Lambda images only support Lambda ComputeTypes between',
+        `'${ComputeType.LAMBDA_1GB}'`,
+        'and',
+        `'${ComputeType.LAMBDA_10GB}',`,
+        `got '${buildEnvironment.computeType}'`,
+      ].join(' '));
+    }
+
+    return errors;
+  }
+
+  public runScriptBuildspec(entrypoint: string): BuildSpec {
+    return runScriptLinuxBuildSpec(entrypoint);
+  }
+}
---
+++ packages/aws-cdk-lib/aws-codebuild/lib/linux-lambda-build-image.ts
@@ -0,0 +1,85 @@
+import { BuildSpec } from './build-spec';
+import { ComputeType } from './compute-type';
+import { runScriptLinuxBuildSpec } from './private/run-script-linux-build-spec';
+import { BuildEnvironment, IBuildImage, isLambdaComputeType } from './project';
+
+/**
+ * Construction properties of `LinuxLambdaBuildImage`.
+ * Module-private, as the constructor of `LinuxLambdaBuildImage` is private.
+ */
+interface LinuxLambdaBuildImageProps {
+  readonly imageId: string;
+}
+
+/**
+ * A CodeBuild image running x86-64 Lambda.
+ *
+ * This class has a bunch of public constants that represent the CodeBuild Lambda x86-64 images.
+ *
+ * @see https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
+ */
+export class LinuxLambdaBuildImage implements IBuildImage {
+  /** The `aws/codebuild/amazonlinux-x86_64-lambda-standard:nodejs18` build image. */
+  public static readonly AMAZON_LINUX_2_NODE_18 = LinuxLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-x86_64-lambda-standard:nodejs18');
+  /** The `aws/codebuild/amazonlinux-x86_64-lambda-standard:python3.11` build image. */
+  public static readonly AMAZON_LINUX_2_PYTHON_3_11 = LinuxLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-x86_64-lambda-standard:python3.11');
+  /** The `aws/codebuild/amazonlinux-x86_64-lambda-standard:ruby3.2` build image. */
+  public static readonly AMAZON_LINUX_2_RUBY_3_2 = LinuxLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-x86_64-lambda-standard:ruby3.2');
+  /** The `aws/codebuild/amazonlinux-x86_64-lambda-standard:corretto17` build image. */
+  public static readonly AMAZON_LINUX_2_CORRETTO_17 = LinuxLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-x86_64-lambda-standard:corretto17');
+  /** The `aws/codebuild/amazonlinux-x86_64-lambda-standard:corretto11` build image. */
+  public static readonly AMAZON_LINUX_2_CORRETTO_11 = LinuxLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-x86_64-lambda-standard:corretto11');
+  /** The `aws/codebuild/amazonlinux-x86_64-lambda-standard:go1.21` build image. */
+  public static readonly AMAZON_LINUX_2_GO_1_21 = LinuxLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-x86_64-lambda-standard:go1.21');
+  /** The `aws/codebuild/amazonlinux-x86_64-lambda-standard:dotnet6` build image. */
+  public static readonly AMAZON_LINUX_2_DOTNET_6 = LinuxLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-x86_64-lambda-standard:dotnet6');
+
+  /**
+   * Uses a Docker image provided by CodeBuild.
+   *
+   * NOTE: In Lambda compute, since only specified images can be used, this method is set to private.
+   *
+   * @see https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
+   *
+   * @param id The image identifier
+   * @example 'aws/codebuild/amazonlinux-x86_64-lambda-standard:nodejs18'
+   * @returns A Docker image provided by CodeBuild.
+   */
+  private static fromCodeBuildImageId(id: string): IBuildImage {
+    return new LinuxLambdaBuildImage({
+      imageId: id,
+    });
+  }
+
+  public readonly type = 'LINUX_LAMBDA_CONTAINER';
+  public readonly defaultComputeType = ComputeType.LAMBDA_1GB;
+  public readonly imageId: string;
+
+  private constructor(props: LinuxLambdaBuildImageProps) {
+    this.imageId = props.imageId;
+  }
+
+  public validate(buildEnvironment: BuildEnvironment): string[] {
+    const errors = [];
+
+    if (buildEnvironment.privileged) {
+      errors.push('Lambda compute type does not support privileged mode');
+    }
+
+    if (buildEnvironment.computeType && !isLambdaComputeType(buildEnvironment.computeType)) {
+      errors.push([
+        'Lambda images only support Lambda ComputeTypes between',
+        `'${ComputeType.LAMBDA_1GB}'`,
+        'and',
+        `'${ComputeType.LAMBDA_10GB}',`,
+        `got '${buildEnvironment.computeType}'`,
+      ].join(' '));
+    }
+
+    return errors;
+  }
+
+  public runScriptBuildspec(entrypoint: string): BuildSpec {
+    return runScriptLinuxBuildSpec(entrypoint);
+  }
+}
