07c143961acb3e5991fd8e33b875064394e5a034
Parker Scanlon <69879391+scanlonp@users.noreply.github.com>
2023-12-23 12:30:50 -0800

    change stack name in integ test

--- packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.vpc-dual-stack.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.vpc-dual-stack.ts
@@ -2,10 +2,8 @@ import { App, Stack } from 'aws-cdk-lib/core';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 
-const salt = 'Deploy5';
-
 const app = new App();
-const stack = new Stack(app, `OneIPv6Block${salt}`);
+const stack = new Stack(app, 'DualStackVpc');
 
 new ec2.Vpc(stack, 'DualStackProtocolVpc', {
   vpcProtocol: ec2.VpcProtocol.DUAL_STACK,
