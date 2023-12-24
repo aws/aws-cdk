4fe30e5035c1f02a535eba7dc9a6d7f2a28956be
Ayush Shah <aayushshah896@gmail.com>
2023-12-24 11:08:53 +0530

    fix keypair support for ec2

--- packages/aws-cdk-lib/aws-ec2/lib/instance.ts
+++ packages/aws-cdk-lib/aws-ec2/lib/instance.ts
@@ -437,7 +437,7 @@ export class Instance extends Resource implements IInstance {
     // there is no need to configure them on the instance level
     this.instance = new CfnInstance(this, 'Resource', {
       imageId: imageConfig.imageId,
-      keyName: props.keyName,
+      keyName: props.keyPair?.keyPairName,
       instanceType: props.instanceType.toString(),
       subnetId: networkInterfaces ? undefined : subnet.subnetId,
       securityGroupIds: networkInterfaces ? undefined : securityGroupsToken,
