ae9a0c6d5d8dfd4b09e082c186689a24b4113d4b
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2023-12-25 23:11:44 +0900

    test: integ test

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-nvidia-bottlerocket-ami.ts
@@ -0,0 +1,26 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as integ from '@aws-cdk/integ-tests-alpha';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'aws-ecs-integ-nvidia-bottlerocket-ami');
+
+const vpc = new ec2.Vpc(stack, 'Vpc', { maxAzs: 2, restrictDefaultSecurityGroup: false });
+
+const cluster = new ecs.Cluster(stack, 'Cluster', {
+  vpc,
+});
+cluster.addCapacity('bottlerocket-asg', {
+  minCapacity: 2,
+  instanceType: new ec2.InstanceType('c5.large'),
+  machineImage: new ecs.BottleRocketImage({
+    variant: ecs.BottlerocketEcsVariant.AWS_ECS_1_NVIDIA,
+  }),
+});
+
+new integ.IntegTest(app, 'aws-ecs-nvidia-bottlerocket-ami', {
+  testCases: [stack],
+});
+
+app.synth();
