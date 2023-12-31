48be463a8f12a1390f86f6ea560b43b5b7205c18
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-12-31 18:50:36 +0900

    fix validation

--- packages/aws-cdk-lib/aws-codepipeline/lib/private/validation.ts
+++ packages/aws-cdk-lib/aws-codepipeline/lib/private/validation.ts
@@ -59,7 +59,7 @@ export function validateNamespaceName(namespaceName: string | undefined): void {
 }
 
 export function validatePipelineVariableName(variableName: string | undefined): void {
-  validateAgainstRegex(/^[A-Za-z0-9@-_]{1,128}$/, 'Variable', variableName);
+  validateAgainstRegex(/^[A-Za-z0-9@\-_]{1,128}$/, 'Variable', variableName);
 }
 
 function validateAgainstRegex(regex: RegExp, thing: string, name: string | undefined) {
