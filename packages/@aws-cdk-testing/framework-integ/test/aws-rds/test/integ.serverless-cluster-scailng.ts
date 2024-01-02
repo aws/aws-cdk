a5a98c80b0b2531f70b1ff6a8431401ec25b402b
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-01-02 15:54:04 +0900

    test: integ test

--- packages/@aws-cdk-testing/framework-integ/test/aws-rds/test/integ.serverless-cluster-scailng.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-rds/test/integ.serverless-cluster-scailng.ts
@@ -1,6 +1,7 @@
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as cdk from 'aws-cdk-lib';
 import * as rds from 'aws-cdk-lib/aws-rds';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'aws-cdk-rds-integ');
@@ -26,21 +27,18 @@ const cluster = new rds.ServerlessCluster(stack, 'Serverless Database', {
   vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
   subnetGroup,
   removalPolicy: cdk.RemovalPolicy.DESTROY,
+  scaling: {
+    autoPause: cdk.Duration.minutes(5),
+    minCapacity: rds.AuroraCapacityUnit.ACU_8,
+    maxCapacity: rds.AuroraCapacityUnit.ACU_32,
+    timeoutAction: rds.TimeoutAction.FORCE_APPLY_CAPACITY_CHANGE,
+    secondsBeforeTimeout: cdk.Duration.minutes(8),
+  },
 });
 cluster.connections.allowDefaultPortFromAnyIpv4('Open to the world');
 
-const noCopyTagsCluster = new rds.ServerlessCluster(stack, 'Serverless Database Without Copy Tags', {
-  engine: rds.DatabaseClusterEngine.AURORA_MYSQL,
-  credentials: {
-    username: 'admin',
-    password: cdk.SecretValue.unsafePlainText('7959866cacc02c2d243ecfe177464fe6'),
-  },
-  vpc,
-  vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
-  subnetGroup,
-  removalPolicy: cdk.RemovalPolicy.DESTROY,
-  copyTagsToSnapshot: false,
+new IntegTest(app, 'cluster-dual-test', {
+  testCases: [stack],
 });
-noCopyTagsCluster.connections.allowDefaultPortFromAnyIpv4('Open to the world');
 
 app.synth();
