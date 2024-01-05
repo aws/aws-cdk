5308b87cc83014426d9cd807b0b36fd83cd56d35
Parker Scanlon <69879391+scanlonp@users.noreply.github.com>
2024-01-04 16:08:06 -0800

    add ec2 integ test

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.vpc-dual-stack-ec2.ts
@@ -0,0 +1,87 @@
+/* eslint-disable no-console */
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cdk from 'aws-cdk-lib';
+import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+import * as path from 'path';
+
+const app = new cdk.App();
+
+class EC2DualStack extends cdk.Stack {
+
+  public readonly instancePublicIp: string;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'Ip6VpcDualStack', {
+      vpcProtocol: ec2.VpcProtocol.DUAL_STACK,
+      subnetConfiguration: [
+        {
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+        },
+      ],
+      flowLogs: {
+        cloudwatchLogs: {
+          destination: ec2.FlowLogDestination.toCloudWatchLogs(),
+        },
+      },
+    });
+
+    const role = new iam.Role(this, 'MyInstanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
+
+    const instance = new ec2.Instance(this, 'MyInstance', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2(),
+      vpc: vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      allowAllIpv6Outbound: true,
+      init: ec2.CloudFormationInit.fromConfigSets({
+        configSets: {
+          default: ['default'],
+        },
+        configs: {
+          default: new ec2.InitConfig([
+            ec2.InitFile.fromAsset('/app/webserver.zip', path.join(__dirname, 'integ.vpc-dual-stack-ec2.assets')),
+            ec2.InitCommand.shellCommand('unzip webserver.zip', { cwd: '/app' }),
+            ec2.InitService.systemdConfigFile('webserver', {
+              command: '/usr/bin/python3 web-server.py',
+              cwd: '/app',
+            }),
+            ec2.InitService.enable('webserver', {
+              serviceManager: ec2.ServiceManager.SYSTEMD,
+            }),
+          ]),
+        },
+      }),
+    });
+
+    this.instancePublicIp = instance.instancePublicIp;
+
+    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(22), 'Allow SSH access');
+    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(8000), 'HTTP traffic');
+    instance.connections.allowFromAnyIpv4(ec2.Port.icmpPing());
+    instance.connections.allowFrom(ec2.Peer.anyIpv6(), ec2.Port.allIcmpV6(), 'allow ICMP6');
+  }
+}
+
+const testCase = new EC2DualStack(app, 'vpc-dual-stack-ec2');
+
+const integ = new IntegTest(app, 'vpc-dual-stack-ec2-test', {
+  testCases: [testCase],
+});
+
+const ipv4ipv6Invoke = integ.assertions.httpApiCall(`http://${testCase.instancePublicIp}:8000`, {});
+ipv4ipv6Invoke.expect(ExpectedResult.objectLike({
+  status: 200,
+}));
