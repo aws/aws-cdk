bf391e4263c55f53f9a601fbacbf980b2e2460ec
go-to-k <24818752+go-to-k@users.noreply.github.com>
2024-01-05 21:45:48 +0900

    change integ test

--- packages/@aws-cdk-testing/framework-integ/test/custom-resources/test/provider-framework/integ.provider-with-waiter-state-machine.ts
+++ packages/@aws-cdk-testing/framework-integ/test/custom-resources/test/provider-framework/integ.provider-with-waiter-state-machine.ts
@@ -7,6 +7,7 @@ import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Provider } from 'aws-cdk-lib/custom-resources';
 import { LogLevel } from 'aws-cdk-lib/aws-stepfunctions';
 import { LogGroup } from 'aws-cdk-lib/aws-logs';
+import { WAITER_STATE_MACHINE_LOG_GROUP_NAME } from 'aws-cdk-lib/cx-api';
 
 class TestStack extends Stack {
   constructor(scope: Construct, id: string) {
@@ -37,10 +38,30 @@ class TestStack extends Stack {
         level: LogLevel.ALL,
       },
     });
+    new Provider(this, 'MyProviderWithDefaultLog', {
+      onEventHandler: new Function(this, 'OnEventWithDefaultLog', {
+        code: Code.fromInline('foo'),
+        handler: 'index.onEvent',
+        runtime: Runtime.NODEJS_LATEST,
+      }),
+      isCompleteHandler: new Function(this, 'IsCompleteWithDefaultLog', {
+        code: Code.fromInline('foo'),
+        handler: 'index.isComplete',
+        runtime: Runtime.NODEJS_LATEST,
+      }),
+      vpc: vpc,
+      vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+      securityGroups: [securityGroup],
+      waiterStateMachineLogOptions: {
+        includeExecutionData: true,
+        level: LogLevel.ALL,
+      },
+    });
   }
 }
 
 const app = new App();
+app.node.setContext(WAITER_STATE_MACHINE_LOG_GROUP_NAME, true);
 const stack = new TestStack(app, 'integ-provider-with-waiter-state-machine');
 
 new integ.IntegTest(app, 'IntegProviderWithWaiterStateMachine', {
