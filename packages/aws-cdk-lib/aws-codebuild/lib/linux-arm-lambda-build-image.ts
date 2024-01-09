96e3f4b62c3ad52cc8fd37bce5150e39f6c5cf03
Paavo Väänänen <paavo.vaananen@cgi.com>
2024-01-09 16:54:30 +0200

    feat(codebuild): new CodeBuild lambda compute images

--- packages/aws-cdk-lib/aws-codebuild/lib/linux-arm-lambda-build-image.ts
+++ packages/aws-cdk-lib/aws-codebuild/lib/linux-arm-lambda-build-image.ts
@@ -21,10 +21,16 @@ interface LinuxArmLambdaBuildImageProps {
 export class LinuxArmLambdaBuildImage implements IBuildImage {
   /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:nodejs18` build image. */
   public static readonly AMAZON_LINUX_2_NODE_18 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:nodejs18');
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:nodejs20` build image. */
+  public static readonly AMAZON_LINUX_2023_NODE_20 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:nodejs20');
   /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:python3.11` build image. */
   public static readonly AMAZON_LINUX_2_PYTHON_3_11 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:python3.11');
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:python3.12` build image. */
+  public static readonly AMAZON_LINUX_2023_PYTHON_3_12 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:python3.12');
   /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:ruby3.2` build image. */
   public static readonly AMAZON_LINUX_2_RUBY_3_2 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:ruby3.2');
+  /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto21` build image. */
+  public static readonly AMAZON_LINUX_2023_CORRETTO_21 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto21');
   /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto17` build image. */
   public static readonly AMAZON_LINUX_2_CORRETTO_17 = LinuxArmLambdaBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto17');
   /** The `aws/codebuild/amazonlinux-aarch64-lambda-standard:corretto11` build image. */
