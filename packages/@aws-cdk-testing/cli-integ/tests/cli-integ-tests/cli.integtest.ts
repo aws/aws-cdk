795f615114eb5a3b1b4f040a2bae14d8efd524b5
Kendra Neil <53584728+TheRealAmazonKendra@users.noreply.github.com>
2024-01-12 15:27:59 -0800

    chore(migrate): enable import of resources on apps created from cdk migrate (#28678)
    Apps generated from cdk migrate with resources that aren't already part of a stack will (soon) create a migrate.json file. This file contains the list of resources that should be imported upon creation of the new app.
    
    If this file is present and the source is either `localfile` or the ARN environment matches the deployment environment, running `cdk deploy` will:
    1. Create a new stack and import the resources listed in migrate.json.
    2. Apply outputs and CDKMetadata through a normal deployment.
    
    Note: `localfile` is a placeholder value so that we can run integration tests on this change. Once some of the other in-progress work is finished, this will be updated.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
@@ -1213,6 +1213,45 @@ integTest('test resource import', withDefaultFixture(async (fixture) => {
   }
 }));
 
+integTest('test migrate deployment for app with localfile source in migrate.json', withDefaultFixture(async (fixture) => {
+  const outputsFile = path.join(fixture.integTestDir, 'outputs', 'outputs.json');
+  await fs.mkdir(path.dirname(outputsFile), { recursive: true });
+
+  // Initial deploy
+  await fixture.cdkDeploy('migrate-stack', {
+    modEnv: { ORPHAN_TOPIC: '1' },
+    options: ['--outputs-file', outputsFile],
+  });
+
+  const outputs = JSON.parse((await fs.readFile(outputsFile, { encoding: 'utf-8' })).toString());
+  const stackName = fixture.fullStackName('migrate-stack');
+  const queueName = outputs[stackName].QueueName;
+  const queueUrl = outputs[stackName].QueueUrl;
+  const queueLogicalId = outputs[stackName].QueueLogicalId;
+  fixture.log(`Created queue ${queueUrl} in stack ${fixture.fullStackName}`);
+
+  // Write the migrate file based on the ID from step one, then deploy the app with migrate
+  const migrateFile = path.join(fixture.integTestDir, 'migrate.json');
+  await fs.writeFile(
+    migrateFile, JSON.stringify(
+      { Source: 'localfile', Resources: [{ ResourceType: 'AWS::SQS::Queue', LogicalResourceId: queueLogicalId, ResourceIdentifier: { QueueUrl: queueUrl } }] },
+    ),
+    { encoding: 'utf-8' },
+  );
+
+  await fixture.cdkDestroy('migrate-stack');
+  fixture.log(`Deleted stack ${fixture.fullStackName}, orphaning ${queueName}`);
+
+  // Create new stack from existing queue
+  try {
+    fixture.log(`Deploying new stack ${fixture.fullStackName}, migrating ${queueName} into stack`);
+    await fixture.cdkDeploy('migrate-stack');
+  } finally {
+    // Cleanup
+    await fixture.cdkDestroy('migrate-stack');
+  }
+}));
+
 integTest('hotswap deployment supports Lambda function\'s description and environment variables', withDefaultFixture(async (fixture) => {
   // GIVEN
   const stackArn = await fixture.cdkDeploy('lambda-hotswap', {
