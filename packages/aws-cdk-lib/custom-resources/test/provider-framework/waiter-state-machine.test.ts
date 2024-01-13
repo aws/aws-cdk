5782022f6502f193190e1924f440d4e177475a70
Mitchell Valine <mitchellvaline@yahoo.com>
2024-01-12 17:21:27 -0800

    revert: allowing log group config for SFN CR (#28699)
    Reverts #27310 which allows setting up LogGroups for the state machine configured by the waiter custom-resource.
    
    Refs: bfeef00
    
    This reverts commit bfeef00c5e1d99415ff2135054e35acc5e9f6b93.
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/custom-resources/test/provider-framework/waiter-state-machine.test.ts
+++ packages/aws-cdk-lib/custom-resources/test/provider-framework/waiter-state-machine.test.ts
@@ -2,8 +2,6 @@ import { Node } from 'constructs';
 import { Template } from '../../../assertions';
 import * as lambda from '../../../aws-lambda';
 import { Code, Function as lambdaFn } from '../../../aws-lambda';
-import { LogGroup, RetentionDays } from '../../../aws-logs';
-import { LogLevel } from '../../../aws-stepfunctions';
 import { Duration, Stack } from '../../../core';
 import { WaiterStateMachine } from '../../lib/provider-framework/waiter-state-machine';
 
@@ -26,9 +24,6 @@ describe('state machine', () => {
     const interval = Duration.hours(2);
     const maxAttempts = 2;
     const backoffRate = 5;
-    const logGroup = new LogGroup(stack, 'LogGroup', {
-      retention: RetentionDays.ONE_DAY,
-    });
 
     // WHEN
     new WaiterStateMachine(stack, 'statemachine', {
@@ -37,11 +32,6 @@ describe('state machine', () => {
       backoffRate,
       interval,
       maxAttempts,
-      logOptions: {
-        includeExecutionData: true,
-        level: LogLevel.ALL,
-        destination: logGroup,
-      },
     });
 
     // THEN
@@ -63,22 +53,6 @@ describe('state machine', () => {
       },
       RoleArn: {
         'Fn::GetAtt': [roleId, 'Arn'],
-      },
-      LoggingConfiguration: {
-        Destinations: [
-          {
-            CloudWatchLogsLogGroup: {
-              LogGroupArn: {
-                'Fn::GetAtt': [
-                  'LogGroupF5B46931',
-                  'Arn',
-                ],
-              },
-            },
-          },
-        ],
-        IncludeExecutionData: true,
-        Level: 'ALL',
       },
     });
     Template.fromStack(stack).hasResourceProperties('AWS::IAM::Role', {
@@ -117,147 +91,10 @@ describe('state machine', () => {
             Effect: 'Allow',
             Resource: stack.resolve(timeoutHandler.resourceArnsForGrantInvoke),
           },
-          {
-            Action: [
-              'logs:CreateLogDelivery',
-              'logs:CreateLogStream',
-              'logs:GetLogDelivery',
-              'logs:UpdateLogDelivery',
-              'logs:DeleteLogDelivery',
-              'logs:ListLogDeliveries',
-              'logs:PutLogEvents',
-              'logs:PutResourcePolicy',
-              'logs:DescribeResourcePolicies',
-              'logs:DescribeLogGroups',
-            ],
-            Effect: 'Allow',
-            Resource: '*',
-          },
         ],
         Version: '2012-10-17',
       },
       Roles: [{ Ref: roleId }],
     });
-    Template.fromStack(stack).hasResourceProperties('AWS::Logs::LogGroup', {
-      RetentionInDays: 1,
-    });
-  });
-
-  test('disable logging', () => {
-    // GIVEN
-    const stack = new Stack();
-    Node.of(stack).setContext('@aws-cdk/core:target-partitions', ['aws', 'aws-cn']);
-
-    const isCompleteHandler = new lambdaFn(stack, 'isComplete', {
-      code: Code.fromInline('foo'),
-      runtime: lambda.Runtime.NODEJS_LATEST,
-      handler: 'index.handler',
-    });
-    const timeoutHandler = new lambdaFn(stack, 'isTimeout', {
-      code: Code.fromInline('foo'),
-      runtime: lambda.Runtime.NODEJS_LATEST,
-      handler: 'index.handler',
-    });
-    const interval = Duration.hours(2);
-    const maxAttempts = 2;
-    const backoffRate = 5;
-
-    // WHEN
-    new WaiterStateMachine(stack, 'statemachine', {
-      isCompleteHandler,
-      timeoutHandler,
-      backoffRate,
-      interval,
-      maxAttempts,
-      disableLogging: true,
-    });
-
-    // THEN
-    Template.fromStack(stack).resourceCountIs('AWS::Logs::LogGroup', 0);
-  });
-
-  test('default logOptions and required policies will be created if logging is enabled without logOptions specified', () => {
-    // GIVEN
-    const stack = new Stack();
-    Node.of(stack).setContext('@aws-cdk/core:target-partitions', ['aws', 'aws-cn']);
-
-    const isCompleteHandler = new lambdaFn(stack, 'isComplete', {
-      code: Code.fromInline('foo'),
-      runtime: lambda.Runtime.NODEJS_LATEST,
-      handler: 'index.handler',
-    });
-    const timeoutHandler = new lambdaFn(stack, 'isTimeout', {
-      code: Code.fromInline('foo'),
-      runtime: lambda.Runtime.NODEJS_LATEST,
-      handler: 'index.handler',
-    });
-    const interval = Duration.hours(2);
-    const maxAttempts = 2;
-    const backoffRate = 5;
-
-    // WHEN
-    new WaiterStateMachine(stack, 'statemachine', {
-      isCompleteHandler,
-      timeoutHandler,
-      backoffRate,
-      interval,
-      maxAttempts,
-    });
-
-    // THEN
-    const roleId = 'statemachineRole52044F93';
-    Template.fromStack(stack).hasResourceProperties('AWS::StepFunctions::StateMachine', {
-      LoggingConfiguration: {
-        Destinations: [
-          {
-            CloudWatchLogsLogGroup: {
-              LogGroupArn: {
-                'Fn::GetAtt': [
-                  'statemachineLogGroupA08E43E4',
-                  'Arn',
-                ],
-              },
-            },
-          },
-        ],
-        IncludeExecutionData: false,
-        Level: 'ERROR',
-      },
-    });
-    Template.fromStack(stack).hasResourceProperties('AWS::IAM::Policy', {
-      PolicyDocument: {
-        Statement: [
-          {
-            Action: 'lambda:InvokeFunction',
-            Effect: 'Allow',
-            Resource: stack.resolve(isCompleteHandler.resourceArnsForGrantInvoke),
-          },
-          {
-            Action: 'lambda:InvokeFunction',
-            Effect: 'Allow',
-            Resource: stack.resolve(timeoutHandler.resourceArnsForGrantInvoke),
-          },
-          {
-            Action: [
-              'logs:CreateLogDelivery',
-              'logs:CreateLogStream',
-              'logs:GetLogDelivery',
-              'logs:UpdateLogDelivery',
-              'logs:DeleteLogDelivery',
-              'logs:ListLogDeliveries',
-              'logs:PutLogEvents',
-              'logs:PutResourcePolicy',
-              'logs:DescribeResourcePolicies',
-              'logs:DescribeLogGroups',
-            ],
-            Effect: 'Allow',
-            Resource: '*',
-          },
-        ],
-        Version: '2012-10-17',
-      },
-      Roles: [{ Ref: roleId }],
-    });
-    Template.fromStack(stack).resourceCountIs('AWS::Logs::LogGroup', 1);
   });
 });
