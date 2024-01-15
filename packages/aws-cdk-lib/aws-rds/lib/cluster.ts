db435079a0faba5f25b7c42a26e7e948828de1ef
Gavin Zhang <yuanhaoz@amazon.com>
2024-01-15 12:37:51 -0800

    Update doc strings

--- packages/aws-cdk-lib/aws-rds/lib/cluster.ts
+++ packages/aws-cdk-lib/aws-rds/lib/cluster.ts
@@ -503,8 +503,9 @@ abstract class DatabaseClusterNew extends DatabaseClusterBase {
   public readonly vpcSubnets?: ec2.SubnetSelection;
 
   /**
-   * The log groups created when cloudwatchLogsExports is set. Each export value
-   * will create a separate log group.
+   * The log group is created when `cloudwatchLogsExports` is set.
+   *
+   * Each export value will create a separate log group.
    */
   public readonly cloudwatchLogGroups: {[engine: string]: logs.ILogGroup};
 
--- packages/aws-cdk-lib/aws-rds/lib/instance.ts
+++ packages/aws-cdk-lib/aws-rds/lib/instance.ts
@@ -744,8 +744,9 @@ abstract class DatabaseInstanceNew extends DatabaseInstanceBase implements IData
   public readonly connections: ec2.Connections;
 
   /**
-   * The log groups created when cloudwatchLogsExports is set. Each export value
-   * will create a separate log group.
+   * The log group is created when `cloudwatchLogsExports` is set.
+   *
+   * Each export value will create a separate log group.
    */
   public readonly cloudwatchLogGroups: {[engine: string]: logs.ILogGroup};
 
