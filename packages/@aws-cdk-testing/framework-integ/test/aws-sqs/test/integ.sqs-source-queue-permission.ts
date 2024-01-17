60dd29286f0c1f9090b167d8f62dbc901d189840
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-01-18 03:29:08 +0900

    test: integ test

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-sqs/test/integ.sqs-source-queue-permission.ts
@@ -0,0 +1,24 @@
+
+import { App, Stack } from 'aws-cdk-lib';
+import * as integ from '@aws-cdk/integ-tests-alpha';
+import { Queue, RedrivePermission } from 'aws-cdk-lib/aws-sqs';
+
+const app = new App();
+
+const stack = new Stack(app, 'aws-cdk-sqs');
+
+const sourceQueue1 = new Queue(stack, 'SourceQueue1');
+const sourceQueue2 = new Queue(stack, 'SourceQueue2');
+
+new Queue(stack, 'DeadLetterQueue', {
+  deadLetterSourceQueuePermission: {
+    sourceQueues: [sourceQueue1, sourceQueue2],
+    redrivePermission: RedrivePermission.BY_QUEUE,
+  },
+});
+
+new integ.IntegTest(app, 'SqsTest', {
+  testCases: [stack],
+});
+
+app.synth();
