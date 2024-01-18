2fc776359d4789f689f739394c3428b7a6bee7ca
GZ <yuanhaoz@amazon.com>
2024-01-18 10:04:58 -0800

    feat(apigatewayv2): AWS type websocket api integration in http api (#28718)
    Currently, Amazon.CDK.AWS.Apigatewayv2 lacks support for AWS option as the [IntegrationType](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-apigatewayv2-integration.html#cfn-apigatewayv2-integration-integrationtype) for WebSocket Apigateway.
    
    Added the capability that allows user to create a WebSocket Apigateway that calls directly other AWS services without a Lambda function middleware.
    
    Closes https://github.com/aws/aws-cdk/issues/27164.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-apigatewayv2-integrations/test/websocket/integ.aws.ts
@@ -0,0 +1,67 @@
+import { HttpMethod, WebSocketApi, WebSocketStage } from 'aws-cdk-lib/aws-apigatewayv2';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { App, RemovalPolicy, Stack } from 'aws-cdk-lib';
+import { WebSocketAwsIntegration, WebSocketMockIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+/*
+ * Stack verification steps:
+ * 1. Verify manually that the integration has type "MOCK"
+ */
+
+const app = new App();
+const stack = new Stack(app, 'integ-aws-websocket-integration');
+
+const table = new dynamodb.Table(stack, 'MyTable', {
+  tableName: 'MyTable',
+  partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+  billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+  removalPolicy: RemovalPolicy.DESTROY,
+});
+
+// Create an IAM role for API Gateway
+const apiRole = new iam.Role(stack, 'ApiGatewayRole', {
+  assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+  managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess')],
+});
+
+const webSocketApi = new WebSocketApi(stack, 'mywsapi', {
+  defaultRouteOptions: { integration: new WebSocketMockIntegration('DefaultIntegration') },
+});
+
+// Optionally, create a WebSocket stage
+new WebSocketStage(stack, 'DevStage', {
+  webSocketApi: webSocketApi,
+  stageName: 'dev',
+  autoDeploy: true,
+});
+
+webSocketApi.addRoute('$connect', {
+  integration: new WebSocketAwsIntegration('DynamodbPutItem', {
+    integrationUri: `arn:aws:apigateway:${stack.region}:dynamodb:action/PutItem`,
+    integrationMethod: HttpMethod.POST,
+    credentialsRole: apiRole,
+    requestTemplates: {
+      'application/json': JSON.stringify({
+        TableName: table.tableName,
+        Item: {
+          id: {
+            S: '$context.requestId',
+          },
+        },
+      }),
+    },
+  }),
+});
+
+new IntegTest(app, 'apigatewayv2-aws-integration-integ-test', {
+  testCases: [stack],
+  cdkCommandOptions: {
+    deploy: {
+      args: {
+        rollback: true,
+      },
+    },
+  },
+});
