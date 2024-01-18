db4059d580314180b945bc882f4c708860f9b273
Paul Sun <paulhcs@amazon.com>
2024-01-17 17:16:59 -0800

    fix(redshift): enableRebootForParameterChanges fails synth with cannot find entry file...

---
+++ packages/@aws-cdk/aws-redshift-alpha/scripts/airlift-custom-resource-handlers.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+scriptdir=$(cd $(dirname $0) && pwd)
+customresourcedir=$(node -p "path.dirname(require.resolve('@aws-cdk/custom-resource-handlers/package.json'))")
+awscdklibdir=${scriptdir}/..
+
+function airlift() {
+  # core needs to be airlifted directly to core to prevent circular dependencies
+  if [[ $1 != dist/core/nodejs-entrypoint-handler && ($1 = dist/core || $1 = dist/core/*) ]];
+    then
+      mkdir -p $awscdklibdir/core/lib/$1
+      cp $customresourcedir/$2 $awscdklibdir/core/lib/$1
+    else
+      mkdir -p $awscdklibdir/custom-resource-handlers/$1
+      cp $customresourcedir/$2 $awscdklibdir/custom-resource-handlers/$1
+  fi
+}
+
+recurse() {
+  local dir=$1
+
+  for file in $dir/*; do
+    if [ -f $file ]; then
+      case $file in
+        $customresourcedir/dist/aws-redshift-alpha/*/index.*)
+          cr=$(echo $file | rev | cut -d "/" -f 2-4 | rev)
+          airlift $cr $cr/index.*
+          ;;
+      esac
+    fi
+
+    if [ -d $file ]; then
+      recurse $file
+    fi
+  done
+}
+
+recurse $customresourcedir/dist
