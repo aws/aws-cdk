ed00302e26bee60187b6d50dae2a28d8f7de4cbd
sakurai-ryo <sakurairyo.10do@gmail.com>
2024-01-20 07:37:44 +0900

    feat(dynamodb): import data from the bucket (#28610)
    This PR adds support for importing data from a bucket.
    This feature is not supported by `AWS::DynamoDB::GlobalTable`, so it is implemented only in the `Table` construct.
    
    ### Details of this feature
    Users can create DynamoDB table using data from the bucket.
    The data to be imported can be a single object or multiple objects.
    This can be selected by specifying the prefix of the key.
    
    Data format must be CSV, DynamoDB JSON, or Amazon Ion.
    ZSTD or GZIP compressed data can also be imported.
    
    For CSV files, delimiter and header can be specified as options.
    https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/S3forDynamoDB.html
    
    In CloudFormation, use this feat as follows.
    ```yaml
    AWSTemplateFormatVersion: "2010-09-09"
    Resources:
      MyTable:
        Type: AWS::DynamoDB::Table
        Properties:
          AttributeDefinitions:
            -
              AttributeName: "id"
              AttributeType: "S"
          KeySchema:
            -
              AttributeName: "id"
              KeyType: "HASH"
          TableName: "myTableName"
          ImportSourceSpecification:
            InputCompressionType: NONE # GZIP | ZSTD 
            InputFormat: DYNAMODB_JSON # ION | CSV
            S3BucketSource:
              S3Bucket: cloudfs-s3
              S3KeyPrefix: db
    ```
    https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-dynamodb-table-importsourcespecification.html
    
    Closes #21825
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-dynamodb/test/integ.import-source.ts
@@ -0,0 +1,122 @@
+import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Table, AttributeType, InputCompressionType, InputFormat } from 'aws-cdk-lib/aws-dynamodb';
+import { Bucket, IBucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Construct } from 'constructs';
+
+class S3Stack extends Stack {
+  public readonly bucket: Bucket;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    this.bucket = new Bucket(this, 'Bucket', {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    new BucketDeployment(this, 'BucketDeployment', {
+      destinationBucket: this.bucket,
+      sources: [
+        Source.jsonData('dynamodb-json-format.json', {
+          Item: {
+            id: {
+              S: '1',
+            },
+          },
+        }),
+        Source.data('ion-format.ion', `$ion_1_0 {
+            Item:{
+                id:"1",
+            }
+        }`),
+        Source.data('csv-format.csv', 'id\n1'),
+      ],
+    });
+  }
+}
+
+interface DynamoStackProps extends StackProps {
+  bucket: IBucket;
+}
+
+class DynamoStack extends Stack {
+  public readonly dynamoDBJsonTable: Table;
+  public readonly ionTable: Table;
+  public readonly csvTable: Table;
+
+  constructor(scope: Construct, id: string, props: DynamoStackProps) {
+    super(scope, id, props);
+
+    this.dynamoDBJsonTable = new Table(this, 'DynamoDBJsonTable', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+      importSource: {
+        compressionType: InputCompressionType.NONE,
+        inputFormat: InputFormat.dynamoDBJson(),
+        bucket: props.bucket,
+        keyPrefix: 'dynamodb-json',
+      },
+    });
+
+    this.ionTable = new Table(this, 'IonTable', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+      importSource: {
+        compressionType: InputCompressionType.NONE,
+        inputFormat: InputFormat.ion(),
+        bucket: props.bucket,
+        keyPrefix: 'ion',
+      },
+    });
+
+    this.csvTable = new Table(this, 'CsvTable', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+      importSource: {
+        compressionType: InputCompressionType.NONE,
+        inputFormat: InputFormat.csv(),
+        bucket: props.bucket,
+        keyPrefix: 'csv',
+      },
+    });
+  }
+}
+
+const app = new App();
+
+const s3Stack = new S3Stack(app, 'S3Stack');
+
+const dynamoStack = new DynamoStack(app, 'DynamoStack', {
+  bucket: s3Stack.bucket,
+});
+
+const integTest = new IntegTest(app, 'DynamoImportSourceIntegTest', {
+  testCases: [s3Stack, dynamoStack],
+  diffAssets: true,
+});
+
+// Assertions
+function getFromTable(tableName: string) {
+  return integTest.assertions.awsApiCall('DynamoDB', 'getItem', {
+    TableName: tableName,
+    Key: {
+      id: {
+        S: '1',
+      },
+    },
+  }).expect(ExpectedResult.objectLike({
+    Item: {
+      id: {
+        S: '1',
+      },
+    },
+  }));
+}
+
+const getFromDynamoDBJsonTable = getFromTable(dynamoStack.dynamoDBJsonTable.tableName);
+const getFromIonTable = getFromTable(dynamoStack.ionTable.tableName);
+const getFromCsvTable = getFromTable(dynamoStack.csvTable.tableName);
+
+getFromDynamoDBJsonTable.next(getFromIonTable).next(getFromCsvTable);
