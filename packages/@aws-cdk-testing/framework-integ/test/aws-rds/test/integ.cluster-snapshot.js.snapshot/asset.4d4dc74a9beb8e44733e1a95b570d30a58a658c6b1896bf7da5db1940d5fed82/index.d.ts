5c464dcf07b7bafa12a847686e9724a7c2d61294
Rico Huijbers <rix0rrr@gmail.com>
2024-01-26 09:58:43 +0000

    Snapshots

--- packages/@aws-cdk-testing/framework-integ/test/aws-rds/test/integ.cluster-snapshot.js.snapshot/asset.4d4dc74a9beb8e44733e1a95b570d30a58a658c6b1896bf7da5db1940d5fed82/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="aws-cdk-lib/custom-resources/lib/provider-framework/types" />
-export declare function onEventHandler(event: AWSCDKAsyncCustomResource.OnEventRequest): Promise<AWSCDKAsyncCustomResource.OnEventResponse>;
-export declare function isCompleteHandler(event: AWSCDKAsyncCustomResource.IsCompleteRequest): Promise<AWSCDKAsyncCustomResource.IsCompleteResponse>;
