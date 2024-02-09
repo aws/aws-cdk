bf5403b757212c27f06653a654075e6ed68feba0
Jane Chen <125300057+chenjane-dev@users.noreply.github.com>
2024-02-09 13:51:46 -0500

    chore(appconfig): refactor README and integ tests (#29017)
    ### Reason for this change
    
    Making changes after API review.
    
    ### Description of changes
    
    Refactor README and integ tests.
    
    ### Description of how you validated changes
    
    
    
    ### Checklist
    - [ ] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-appconfig-alpha/test/integ.application.ts
+++ packages/@aws-cdk/aws-appconfig-alpha/test/integ.application.ts
@@ -9,10 +9,15 @@ const stack = new Stack(app, 'aws-appconfig-application');
 
 new Application(stack, 'MyAppConfig', {
   description: 'This is my application for testing',
+  applicationName: 'MySampleApplication',
 });
 
 const taskDef = new FargateTaskDefinition(stack, 'MyTaskDef');
 Application.addAgentToEcs(taskDef);
+
+/* resource deployment alone is sufficient because we already have the
+   corresponding resource handler tests to assert that resources can be
+   used after created */
 
 new IntegTest(app, 'appconfig-application', {
   testCases: [stack],
