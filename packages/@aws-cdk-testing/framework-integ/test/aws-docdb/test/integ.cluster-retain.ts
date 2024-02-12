903ebc964a86ff82f09cd86b02caa86831c50d19
Jada Berenguer <jada.berenguer@slalom.com>
2024-02-12 10:53:29 -0800

    Update integration test to try to fix error

--- packages/@aws-cdk-testing/framework-integ/test/aws-docdb/test/integ.cluster-retain.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-docdb/test/integ.cluster-retain.ts
@@ -3,12 +3,17 @@ import * as cdk from "aws-cdk-lib";
 import { DatabaseCluster } from "aws-cdk-lib/aws-docdb";
 import * as integ from "@aws-cdk/integ-tests-alpha";
 
+/*
+ * Stack verification steps:
+ * * aws docdb describe-db-clusters --db-cluster-identifier <deployed db cluster identifier>
+ */
+
 const app = new cdk.App();
 
 const stack = new cdk.Stack(app, "aws-cdk-docdb-integ-cluster-retain");
 
 const vpc = ec2.Vpc.fromLookup(stack, "VPC", { isDefault: true });
-const db = new DatabaseCluster(stack, "Database", {
+new DatabaseCluster(stack, "Database", {
   masterUser: { username: "clusteradmin" },
   instanceType: ec2.InstanceType.of(
     ec2.InstanceClass.T3,
