4b6d41bc1181e02b764d88c255862ab87e6dbbbe
Jada Berenguer <jada.berenguer@slalom.com>
2024-02-12 11:22:05 -0800

    Fix syntax errors in integration test

--- packages/@aws-cdk-testing/framework-integ/test/aws-docdb/test/integ.cluster-retain.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-docdb/test/integ.cluster-retain.ts
@@ -1,7 +1,7 @@
-import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as cdk from "aws-cdk-lib";
-import { DatabaseCluster } from "aws-cdk-lib/aws-docdb";
-import * as integ from "@aws-cdk/integ-tests-alpha";
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as cdk from 'aws-cdk-lib';
+import { DatabaseCluster } from 'aws-cdk-lib/aws-docdb';
+import * as integ from '@aws-cdk/integ-tests-alpha';
 
 /*
  * Stack verification steps:
@@ -10,19 +10,19 @@ import * as integ from "@aws-cdk/integ-tests-alpha";
 
 const app = new cdk.App();
 
-const stack = new cdk.Stack(app, "aws-cdk-docdb-integ-cluster-retain");
+const stack = new cdk.Stack(app, 'aws-cdk-docdb-integ-cluster-retain');
 
-const vpc = ec2.Vpc.fromLookup(stack, "VPC", { isDefault: true });
-new DatabaseCluster(stack, "Database", {
-  masterUser: { username: "clusteradmin" },
+const vpc = ec2.Vpc.fromLookup(stack, 'VPC', { isDefault: true });
+new DatabaseCluster(stack, 'Database', {
+  masterUser: { username: 'clusteradmin' },
   instanceType: ec2.InstanceType.of(
     ec2.InstanceClass.T3,
-    ec2.InstanceSize.MEDIUM
+    ec2.InstanceSize.MEDIUM,
   ),
   vpc,
   removalPolicy: cdk.RemovalPolicy.RETAIN,
 });
 
-new integ.IntegTest(app, "DocdbTest", {
+new integ.IntegTest(app, 'DocdbTest', {
   testCases: [stack],
 });
