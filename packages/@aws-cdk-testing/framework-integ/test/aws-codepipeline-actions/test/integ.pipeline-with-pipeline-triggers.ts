dc74b70fcf1f11c7a0deaa766117b7ef22aeb7ac
k.goto <24818752+go-to-k@users.noreply.github.com>
2024-02-14 06:36:43 +0900

    feat(codepipeline): pipeline type v2 with pipeline-level variables and triggers (#28538)
    This PR supports pipeline type v2 with pipeline-level variables and triggers.
    
    When referring to a variable in pipeline actions, it must be specified according to the format `#{variables.variableName}`. In order to avoid the need to specify directly in this form, a new class `Variable` with a `reference()` method was created.
    
    ```ts
    const myVariable = new codepipeline.Variable({
      variableName: 'bucket-var',
      description: 'description',
      defaultValue: 'sample',
    });
    
    const pipeline = new codepipeline.Pipeline(stack, 'Pipeline', {
      artifactBucket: sourceBucket,
      pipelineType: codepipeline.PipelineType.V2,
      variables: [myVariable],
      stages: [
        {
          stageName: 'Source',
          actions: [sourceAction],
        },
        {
          stageName: 'Deploy',
          actions: [
            new S3DeployAction({
              actionName: 'DeployAction',
              extract: false,
              // objectKey: '#{variables.bucket-var}.txt',
              objectKey: `${myVariable.reference()}.txt`,
              input: sourceOutput,
              bucket: deployBucket,
            }),
          ],
        },
      ],
    });
    ```
    
    - user guide
      - https://docs.aws.amazon.com/codepipeline/latest/userguide/pipeline-types-planning.html
      - https://docs.aws.amazon.com/codepipeline/latest/userguide/reference-variables.html#reference-variables-workflow
      - https://docs.aws.amazon.com/codepipeline/latest/userguide/tutorials-github-tags.html
    - CloudFormation
      - https://docs.aws.amazon.com/ja_jp/AWSCloudFormation/latest/UserGuide/aws-resource-codepipeline-pipeline.html
      - https://docs.aws.amazon.com/ja_jp/AWSCloudFormation/latest/UserGuide/aws-properties-codepipeline-pipeline-variabledeclaration.html
      - https://docs.aws.amazon.com/ja_jp/AWSCloudFormation/latest/UserGuide/aws-properties-codepipeline-pipeline-pipelinetriggerdeclaration.html
    
    Closes #28476 #28694.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-codepipeline-actions/test/integ.pipeline-with-pipeline-triggers.ts
@@ -0,0 +1,70 @@
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as cdk from 'aws-cdk-lib';
+import * as cpactions from 'aws-cdk-lib/aws-codepipeline-actions';
+import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+const app = new cdk.App();
+
+const stack = new cdk.Stack(app, 'aws-cdk-codepipeline-with-pipeline-triggers');
+
+// Make sure you specify a valid connection ARN.
+const connectionArn = process.env.CONNECTION_ARN || 'MOCK';
+if (connectionArn === 'MOCK') {
+  cdk.Annotations.of(stack).addWarningV2('integ:connection-arn', 'You must specify a valid connection ARN in the CONNECTION_ARN environment variable');
+}
+
+const sourceOutput = new codepipeline.Artifact();
+const sourceAction = new cpactions.CodeStarConnectionsSourceAction({
+  actionName: 'CodeStarConnectionsSourceAction',
+  output: sourceOutput,
+  connectionArn,
+  owner: 'go-to-k',
+  repo: 'cdk-pipelines-demo',
+});
+
+new codepipeline.Pipeline(stack, 'Pipeline', {
+  pipelineName: 'my-pipeline',
+  pipelineType: codepipeline.PipelineType.V2,
+  crossAccountKeys: true,
+  stages: [
+    {
+      stageName: 'Source',
+      actions: [sourceAction],
+    },
+    {
+      stageName: 'Build',
+      actions: [
+        new cpactions.CodeBuildAction({
+          actionName: 'CodeBuild',
+          project: new codebuild.PipelineProject(stack, 'MyProject'),
+          input: sourceOutput,
+          outputs: [new codepipeline.Artifact()],
+          environmentVariables: {
+            CommitId: { value: sourceAction.variables.commitId },
+          },
+        }),
+      ],
+    },
+  ],
+  triggers: [{
+    providerType: codepipeline.ProviderType.CODE_STAR_SOURCE_CONNECTION,
+    gitConfiguration: {
+      sourceAction,
+      pushFilter: [{
+        tagsExcludes: ['exclude1', 'exclude2'],
+        tagsIncludes: ['include1', 'include2'],
+      }],
+    },
+  }],
+});
+
+const integrationTest = new IntegTest(app, 'codepipeline-with-pipeline-triggers-test', {
+  testCases: [stack],
+  diffAssets: true,
+});
+
+const awsApiCall = integrationTest.assertions.awsApiCall('CodePipeline', 'getPipeline', { name: 'my-pipeline' });
+awsApiCall.assertAtPath('pipeline.name', ExpectedResult.stringLikeRegexp('my-pipeline'));
+
+app.synth();
