27f8b19830fe2b46408df7e5c98113b5d1a28720
Raphael Manke <RaphaelManke@users.noreply.github.com>
2024-02-13 23:33:39 +0100

    feat(pipes-enrichments): new EventBridge Pipes enrichments alpha module (#29063)
    ### Issue # (if applicable)
    
    Closes #<issue number here>.
    
    ### Reason for this change
    
    This PR adds a new alpha module to for EvenBridge pipes enrichments.
    
    This is the base setup for future work and additional enrichments.
    
    ### Description of changes
    
    The initial enrichment is the Lambda enrichment.
    
    ### Description of how you validated changes
    
    - [x] Unittests
    - [x] Integration test
    
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-pipes-enrichments-alpha/lib/lambda.ts
@@ -0,0 +1,42 @@
+import { EnrichmentParametersConfig, IEnrichment, IPipe, InputTransformation } from '@aws-cdk/aws-pipes-alpha';
+import { IRole } from 'aws-cdk-lib/aws-iam';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
+
+/**
+ * Properties for a LambdaEnrichment
+ */
+export interface LambdaEnrichmentProps {
+  /**
+   * The input transformation for the enrichment
+   * @see https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-pipes-input-transformation.html
+   * @default - None
+   */
+  readonly inputTransformation?: InputTransformation;
+}
+
+/**
+ * A Lambda enrichment for a pipe
+ */
+export class LambdaEnrichment implements IEnrichment {
+  public readonly enrichmentArn: string;
+
+  private readonly inputTransformation?: InputTransformation;
+
+  constructor(private readonly lambda: IFunction, props?: LambdaEnrichmentProps) {
+    this.enrichmentArn = lambda.functionArn;
+    this.inputTransformation = props?.inputTransformation;
+  }
+
+  bind(pipe: IPipe): EnrichmentParametersConfig {
+    return {
+      enrichmentParameters: {
+        inputTemplate: this.inputTransformation?.bind(pipe).inputTemplate,
+      },
+    };
+  }
+
+  grantInvoke(pipeRole: IRole): void {
+    this.lambda.grantInvoke(pipeRole);
+  }
+}
+
