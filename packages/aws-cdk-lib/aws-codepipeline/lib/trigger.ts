1da6da4b5b61baf34f80e573a9b3afaa88320610
go-to-k <24818752+go-to-k@users.noreply.github.com>
2024-02-16 15:05:59 +0900

    feat(codepipeline): GitPullRequestFilter for pipeline trigger

--- packages/aws-cdk-lib/aws-codepipeline/lib/trigger.ts
+++ packages/aws-cdk-lib/aws-codepipeline/lib/trigger.ts
@@ -33,6 +33,91 @@ export interface GitPushFilter {
 }
 
 /**
+ * Git pull request filter for trigger.
+ */
+export interface GitPullRequestFilter {
+  /**
+   * The list of patterns of Git branches that, when pull request events occurs, are
+   * to be excluded from starting the pipeline.
+   *
+   * You can filter with glob patterns. The `branchesExcludes` takes priority
+   * over the `branchesIncludes`.
+   *
+   * Maximum length of this array is 8.
+   *
+   * @default - no branches.
+   */
+  readonly branchesExcludes?: string[];
+
+  /**
+   * The list of patterns of Git branches that, when pull request events occurs, are
+   * to be included as criteria that starts the pipeline.
+   *
+   * You can filter with glob patterns. The `branchesExcludes` takes priority
+   * over the `branchesIncludes`.
+   *
+   * Maximum length of this array is 8.
+   *
+   * @default - no branches.
+   */
+  readonly branchesIncludes?: string[];
+
+  /**
+   * The list of patterns of Git repository file paths that, when pull request events occurs,
+   * are to be excluded from starting the pipeline.
+   *
+   * You can filter with glob patterns. The `filePathsExcludes` takes priority
+   * over the `filePathsIncludes`.
+   *
+   * Maximum length of this array is 8.
+   *
+   * @default - no filePaths.
+   */
+  readonly filePathsExcludes?: string[];
+
+  /**
+   * The list of patterns of Git repository file paths that, when pull request events occurs,
+   * are to be included as criteria that starts the pipeline.
+   *
+   * You can filter with glob patterns. The `filePathsExcludes` takes priority
+   * over the `filePathsIncludes`.
+   *
+   * Maximum length of this array is 8.
+   *
+   * @default - no filePaths.
+   */
+  readonly filePathsIncludes?: string[];
+  
+  /**
+   * The field that specifies which pull request events to filter on (opened, updated, closed)
+   * for the trigger configuration.
+   *
+   * Maximum length of this array is 3.
+   *
+   * @default - no events.
+   */
+  readonly events?: GitPullRequestEvent[];
+}
+
+/**
+ * Event for trigger with pull request filter.
+ */
+export enum GitPullRequestEvent {
+  /**
+   * OPEN
+   */
+  OPEN = 'OPEN',
+  /**
+   * UPDATED
+   */
+  UPDATED = 'UPDATED',
+  /**
+   * CLOSED
+   */
+  CLOSED = 'CLOSED',
+}
+
+/**
  * Git configuration for trigger.
  */
 export interface GitConfiguration {
@@ -58,6 +143,16 @@ export interface GitConfiguration {
    * @default - no filter.
    */
   readonly pushFilter?: GitPushFilter[];
+
+  /**
+   * The field where the repository event that will start the pipeline
+   * is specified as pull requests.
+   *
+   * The length must be less than or equal to 3.
+   *
+   * @default - no filter.
+   */
+  readonly pullRequestFilter?: GitPullRequestFilter[];
 }
 
 /**
@@ -117,10 +212,33 @@ export class Trigger {
 
       pushFilter?.forEach(filter => {
         if (filter.tagsExcludes && filter.tagsExcludes.length > 8) {
-          throw new Error(`maximum length of tagsExcludes for sourceAction with name '${sourceAction.actionProperties.actionName}' is 8, got ${filter.tagsExcludes.length}`);
+          throw new Error(`maximum length of tagsExcludes in pushFilter for sourceAction with name '${sourceAction.actionProperties.actionName}' is 8, got ${filter.tagsExcludes.length}`);
         }
         if (filter.tagsIncludes && filter.tagsIncludes.length > 8) {
-          throw new Error(`maximum length of tagsIncludes for sourceAction with name '${sourceAction.actionProperties.actionName}' is 8, got ${filter.tagsIncludes.length}`);
+          throw new Error(`maximum length of tagsIncludes in pushFilter for sourceAction with name '${sourceAction.actionProperties.actionName}' is 8, got ${filter.tagsIncludes.length}`);
+        }
+      });
+
+      const pullRequestFilter = this.props.gitConfiguration.pullRequestFilter;
+      if (pullRequestFilter !== undefined && pullRequestFilter.length > 3) {
+        throw new Error(`length of pullRequestFilter for sourceAction with name '${sourceAction.actionProperties.actionName}' must be less than or equal to 3, got ${pullRequestFilter.length}`);
+      }
+
+      pullRequestFilter?.forEach(filter => {
+        if (filter.branchesExcludes && filter.branchesExcludes.length > 8) {
+          throw new Error(`maximum length of branchesExcludes in pullRequestFilter for sourceAction with name '${sourceAction.actionProperties.actionName}' is 8, got ${filter.branchesExcludes.length}`);
+        }
+        if (filter.branchesIncludes && filter.branchesIncludes.length > 8) {
+          throw new Error(`maximum length of branchesIncludes in pullRequestFilter for sourceAction with name '${sourceAction.actionProperties.actionName}' is 8, got ${filter.branchesIncludes.length}`);
+        }
+        if (filter.filePathsExcludes && filter.filePathsExcludes.length > 8) {
+          throw new Error(`maximum length of filePathsExcludes in pullRequestFilter for sourceAction with name '${sourceAction.actionProperties.actionName}' is 8, got ${filter.filePathsExcludes.length}`);
+        }
+        if (filter.filePathsIncludes && filter.filePathsIncludes.length > 8) {
+          throw new Error(`maximum length of filePathsIncludes in pullRequestFilter for sourceAction with name '${sourceAction.actionProperties.actionName}' is 8, got ${filter.filePathsIncludes.length}`);
+        }
+        if (filter.events && filter.events.length > 3) {
+          throw new Error(`maximum length of events in pullRequestFilter for sourceAction with name '${sourceAction.actionProperties.actionName}' is 3, got ${filter.events.length}`);
         }
       });
     }
@@ -136,6 +254,7 @@ export class Trigger {
     if (this.props.gitConfiguration) {
       const sourceAction = this.props.gitConfiguration.sourceAction;
       const pushFilter = this.props.gitConfiguration.pushFilter;
+      const pullRequestFilter = this.props.gitConfiguration.pullRequestFilter;
 
       const push: CfnPipeline.GitPushFilterProperty[] | undefined = pushFilter?.map(filter => {
         const tags: CfnPipeline.GitTagFilterCriteriaProperty | undefined = {
@@ -145,10 +264,27 @@ export class Trigger {
         };
         return { tags };
       });
+      const pullRequest: CfnPipeline.GitPullRequestFilterProperty[] | undefined = pullRequestFilter?.map(filter => {
+        const branches: CfnPipeline.GitBranchFilterCriteriaProperty | undefined = {
+          // set to undefined if empty array because CloudFormation does not accept empty array
+          excludes: filter.branchesExcludes?.length ? filter.branchesExcludes : undefined,
+          includes: filter.branchesIncludes?.length ? filter.branchesIncludes : undefined,
+        };
+        const filePaths: CfnPipeline.GitFilePathFilterCriteriaProperty | undefined = {
+          // set to undefined if empty array because CloudFormation does not accept empty array
+          excludes: filter.filePathsExcludes?.length ? filter.filePathsExcludes : undefined,
+          includes: filter.filePathsIncludes?.length ? filter.filePathsIncludes : undefined,
+        };
+        // set to undefined if empty array because CloudFormation does not accept empty array
+        const events: string[] | undefined = filter.events?.length ? filter.events : undefined;
+        return { branches, filePaths, events };
+      });
 
       gitConfiguration = {
         // set to undefined if empty array because CloudFormation does not accept empty array
         push: push?.length ? push : undefined,
+        // set to undefined if empty array because CloudFormation does not accept empty array
+        pullRequest: pullRequest?.length ? pullRequest : undefined,
         sourceActionName: sourceAction.actionProperties.actionName,
       };
     }
