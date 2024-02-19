c2ae59edd0e930a2e78bbb918c9d0cd5583d0fa4
Vandita2020 <vandita.vk20@gmail.com>
2024-02-19 10:47:55 -0800

    Fixing esbuildArgs

--- packages/aws-cdk-lib/aws-lambda-nodejs/lib/bundling.ts
+++ packages/aws-cdk-lib/aws-lambda-nodejs/lib/bundling.ts
@@ -418,10 +418,13 @@ function toTarget(runtime: Runtime): string {
 
 function toCliArgs(esbuildArgs: { [key: string]: string | boolean }): string {
   const args = new Array<string>();
+  const ls = ['--alias', '--drop', '--pure', '--log-override', '--out-extension'];
 
   for (const [key, value] of Object.entries(esbuildArgs)) {
     if (value === true || value === '') {
       args.push(key);
+    } else if (ls.includes(key)) {
+      args.push(`${key}:"${value}"`);
     } else if (value) {
       args.push(`${key}="${value}"`);
     }
