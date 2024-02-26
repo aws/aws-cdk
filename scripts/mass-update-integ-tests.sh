1333ad672832a199964e8e234843e6c072f55022
Rico Huijbers <rix0rrr@gmail.com>
2024-02-26 10:37:53 +0000

    Ignore things that aren't integ tests

--- scripts/mass-update-integ-tests.sh
+++ scripts/mass-update-integ-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -eu
-npx integ-runner --directory packages --update-on-failed --disable-update-workflow "$@"
+npx integ-runner --directory packages/@aws-cdk-testing/framework-integ --update-on-failed --disable-update-workflow "$@"
