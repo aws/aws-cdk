6d4f84dff666428d1102719bb50112ea4431082e
Michael Sambol <sambol.michael@gmail.com>
2024-02-29 16:07:30 -0700

    add cluster region

--- packages/aws-cdk-lib/aws-events-targets/lib/ecs-task.ts
+++ packages/aws-cdk-lib/aws-events-targets/lib/ecs-task.ts
@@ -288,7 +288,7 @@ export class EcsTask implements events.IRuleTarget {
       new iam.PolicyStatement({
         actions: ['ecs:TagResource'],
         // eslint-disable-next-line @aws-cdk/no-literal-partition
-        resources: [`arn:aws:ecs:*:*:task/${this.cluster.clusterName}/*`],
+        resources: [`arn:aws:ecs:${this.cluster.env.region}:*:task/${this.cluster.clusterName}/*`],
       }),
     ];
 
