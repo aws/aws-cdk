1451fc7549eaf80b1dc5b07f2f1e68b0fa650c34
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-03-09 03:16:19 +0900

    feat: updte create-schedule.ts

--- packages/aws-cdk-lib/aws-stepfunctions-tasks/lib/eventbridge-scheduler/create-schedule.ts
+++ packages/aws-cdk-lib/aws-stepfunctions-tasks/lib/eventbridge-scheduler/create-schedule.ts
@@ -1,7 +1,9 @@
 import { Construct } from 'constructs';
 import * as iam from '../../../aws-iam';
+import * as kms from '../../../aws-kms';
 import * as sfn from '../../../aws-stepfunctions';
-import { Stack } from '../../../core';
+import * as sqs from '../../../aws-sqs';
+import { Stack, Duration } from '../../../core';
 import { integrationResourceArn } from '../private/task-utils';
 
 export enum ActionAfterCompletion {
@@ -26,11 +28,100 @@ export interface CreateScheduleProps extends sfn.TaskStateBaseProps {
   readonly actionAfterCompletion?: ActionAfterCompletion;
 
   /**
+   * Unique, case-sensitive identifier to ensure the idempotency of the request.
+   */
+  readonly clientToken?: string;
+
+  /**
+   * The description for the schedule.
+   */
+  readonly description?: string;
+
+  /**
+   * The date, in UTC, before which the schedule can invoke its target.
+   * Depending on the schedule's recurrence expression, invocations might stop on, or before, the EndDate you specify. EventBridge Scheduler ignores EndDate for one-time schedules.
+   */
+  readonly endDate?: Date;
+
+  /**
+   * The maximum time window during which a schedule can be invoked.
+   */
+  readonly flexibleTimeWindow?: Duration;
+
+  /**
    * Schedule group name
    *
    * @default 'Default'
    */
-  readonly scheduleGroupName?: string;
+  readonly groupName?: string;
+
+  /**
+   * The customer managed KMS key that EventBridge Scheduler will use to encrypt and decrypt your data.
+   */
+  readonly kmsKey?: kms.IKey;
+
+  /**
+   * The expression that defines when the schedule runs.
+   *
+   * @see https://docs.aws.amazon.com/scheduler/latest/UserGuide/schedule-types.html
+   */
+  readonly scheduleExpression: string;
+
+  /**
+   * The timezone in which the scheduling expression is evaluated.
+   */
+  readonly timezone: string;
+
+  /**
+   * The date, in UTC, after which the schedule can begin invoking its target.
+   * Depending on the schedule's recurrence expression, invocations might occur on, or after, the StartDate you specify. EventBridge Scheduler ignores StartDate for one-time schedules.
+   */
+  readonly startDate?: Date;
+
+  /**
+   * Specifies whether the schedule is enabled or disabled.
+   */
+  readonly enabled?: boolean;
+
+  /**
+   * The Amazon Resource Name (ARN) of the target.
+   */
+  readonly targetArn: string;
+
+  /**
+   * The IAM role that EventBridge Scheduler will use for this target when the schedule is invoked.
+   */
+  readonly role: iam.IRole;
+
+  /**
+   * The input to the target.
+   */
+  readonly input?: string;
+
+  /**
+   * The retry policy settings
+   */
+  readonly retryPolicy?: RetryPolicy;
+
+  /**
+   * Dead letter queue for failed events
+   */
+  readonly deadLetterQueue?: sqs.IQueue;
+}
+
+/**
+ * The information about the retry policy settings
+ */
+export interface RetryPolicy {
+  /**
+   * The maximum number of retry attempts to make before the request fails.
+   */
+  readonly maximumRetryAttempts: number;
+
+  /**
+   * The maximum amount of time to continue to make retry attempts.
+   */
+  readonly maximumEventAge: Duration;
 }
 
 /**
@@ -54,7 +145,7 @@ export class CreateSchedule extends sfn.TaskStateBase {
         Stack.of(this).formatArn({
           service: 'scheduler',
           resource: 'schedule',
-          resourceName: `${this.props.scheduleGroupName ?? 'default'}/${this.props.scheduleName}`,
+          resourceName: `${this.props.groupName ?? 'default'}/${this.props.scheduleName}`,
         }),
       ],
       actions: [
@@ -70,9 +161,33 @@ export class CreateSchedule extends sfn.TaskStateBase {
     return {
       Resource: integrationResourceArn('aws-sdk:scheduler', 'createSchedule', this.integrationPattern),
       Parameters: {
-        ActionAfterCompletion: 'NONE',
+        ActionAfterCompletion: this.props.actionAfterCompletion,
+        ClientToken: this.props.clientToken,
+        Description: this.props.description,
+        EndDate: this.props.endDate ? Math.trunc(this.props.endDate.getTime() / 1000) : undefined,
+        FlexibleTimeWindow: {
+          Mode: this.props.flexibleTimeWindow ? 'FLEXIBLE' : 'OFF',
+          MaximumWindowInMinutes: this.props.flexibleTimeWindow?.toMinutes(),
+        },
+        GroupName: this.props.groupName,
+        KmsKeyArn: this.props.kmsKey?.keyArn,
         Name: this.props.scheduleName,
-        GroupName: this.props.scheduleGroupName,
+        ScheduleExpression: this.props.scheduleExpression,
+        ScheduleExpressionTimezone: this.props.timezone,
+        StartDate: this.props.startDate ? Math.trunc(this.props.startDate.getTime() / 1000) : undefined,
+        State: this.props.enabled ? 'ENABLED' : 'DISABLED',
+        Target: {
+          Arn: this.props.targetArn,
+          RoleArn: this.props.role.roleArn,
+          Input: this.props.input,
+          RetryPolicy: this.props.retryPolicy ? {
+            MaximumEventAgeInseconds: this.props.retryPolicy.maximumEventAge.toSeconds(),
+            MaximumRetryAttempts: this.props.retryPolicy.maximumRetryAttempts,
+          } : undefined,
+          DeadLetterConfig: this.props.deadLetterQueue ? {
+            Arn: this.props.deadLetterQueue.queueArn,
+          } : undefined,
+        },
       },
     };
   }
