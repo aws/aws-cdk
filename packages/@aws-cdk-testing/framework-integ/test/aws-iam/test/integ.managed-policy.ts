a5ced68c095216629ddefe169a568491366954f5
Kip Shields <kishiel@amazon.com>
2024-03-11 19:32:47 +0000

    update snapshots complaining about rules unrelated to this change

--- packages/@aws-cdk-testing/framework-integ/test/aws-iam/test/integ.managed-policy.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-iam/test/integ.managed-policy.ts
@@ -24,7 +24,8 @@ const policy3 = ManagedPolicy.fromAwsManagedPolicyName('SecurityAudit');
 user.addManagedPolicy(policy3);
 
 const role = new Role(stack, 'Role', { assumedBy: new AccountRootPrincipal() });
-role.grantAssumeRole(policy.grantPrincipal);
+role.grantAssumeRole(user);
+
 Grant.addToPrincipal({ actions: ['iam:*'], resourceArns: [role.roleArn], grantee: policy2 });
 
 policy.attachToRole(role);
