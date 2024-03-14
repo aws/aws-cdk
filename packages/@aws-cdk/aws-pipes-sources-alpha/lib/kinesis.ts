0bc01ed062324e2a005bc864a1888056ff2eb07b
Michael Sambol <sambol.michael@gmail.com>
2024-03-13 22:48:40 -0600

    add validation for parameters

--- packages/@aws-cdk/aws-pipes-sources-alpha/lib/kinesis.ts
+++ packages/@aws-cdk/aws-pipes-sources-alpha/lib/kinesis.ts
@@ -12,7 +12,7 @@ export interface KinesisSourceParameters {
     * The maximum number of records to include in each batch.
     *
     * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-pipes-pipe-pipesourcekinesisstreamparameters.html#cfn-pipes-pipe-pipesourcekinesisstreamparameters-batchsize
-    * @default undefined
+    * @default 1
     */
   readonly batchSize?: number;
 
@@ -20,7 +20,7 @@ export interface KinesisSourceParameters {
     * Define the target queue to send dead-letter queue events to.
     *
     * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-pipes-pipe-pipesourcekinesisstreamparameters.html#cfn-pipes-pipe-pipesourcekinesisstreamparameters-deadletterconfig
-    * @default undefined
+    * @default no dead letter config
     */
   readonly deadLetterConfig?: DeadLetterConfigParameters;
 
@@ -28,23 +28,23 @@ export interface KinesisSourceParameters {
    * The maximum length of a time to wait for events.
    *
    * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-pipes-pipe-pipesourcekinesisstreamparameters.html#cfn-pipes-pipe-pipesourcekinesisstreamparameters-maximumbatchingwindowinseconds
-   * @default undefined
+   * @default no batching window
    */
   readonly maximumBatchingWindow?: Duration;
 
   /**
    * (Streams only) Discard records older than the specified age. The default value is -1, which sets the maximum age to infinite. When the value is set to infinite, EventBridge never discards old records.
    *
-   * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-pipes-pipe-pipesourcekinesisstreamparameters.html#cfn-pipes-pipe-pipesourcekinesisstreamparameters-maximumbatchingwindowinseconds
-   * @default -1
+   * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-pipes-pipe-pipesourcekinesisstreamparameters.html#cfn-pipes-pipe-pipesourcekinesisstreamparameters-maximumrecordageinseconds
+   * @default -1 (infinite)
    */
-  readonly maximumRecordAge?: Duration;
+  readonly maximumRecordAgeInSeconds?: number;
 
   /**
    * (Streams only) Discard records after the specified number of retries. The default value is -1, which sets the maximum number of retries to infinite. When MaximumRetryAttempts is infinite, EventBridge retries failed records until the record expires in the event source.
    *
    * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-pipes-pipe-pipesourcekinesisstreamparameters.html#cfn-pipes-pipe-pipesourcekinesisstreamparameters-maximumretryattempts
-   * @default -1
+   * @default -1 (infinite)
    */
   readonly maximumRetryAttempts?: number;
 
@@ -52,7 +52,7 @@ export interface KinesisSourceParameters {
    * (Streams only) Define how to handle item process failures. AUTOMATIC_BISECT halves each batch and retry each half until all the records are processed or there is one failed message left in the batch.
    *
    * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-pipes-pipe-pipesourcekinesisstreamparameters.html#cfn-pipes-pipe-pipesourcekinesisstreamparameters-onpartialbatchitemfailure
-   * @default -1
+   * @default off
    */
   readonly onPartialBatchItemFailure?: OnPartialBatchItemFailure;
 
@@ -75,7 +75,7 @@ export interface KinesisSourceParameters {
    * With StartingPosition set to AT_TIMESTAMP, the time from which to start reading, in Unix time seconds.
    *
    * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-pipes-pipe-pipesourcekinesisstreamparameters.html#cfn-pipes-pipe-pipesourcekinesisstreamparameters-startingpositiontimestamp
-   * @default undefined
+   * @default no starting position timestamp
    */
   readonly startingPositionTimestamp?: string;
 }
@@ -97,7 +97,7 @@ export enum StartingPosition {
   /**
    * TRIM_HORIZON
    */
-  TRIM_HORIZON = 'AUTOMATIC_BISECT',
+  TRIM_HORIZON = 'TRIM_HORIZON',
   /**
    * LATEST
    */
@@ -115,24 +115,61 @@ export class KinesisSource implements ISource {
   private readonly stream: IStream;
   readonly sourceArn;
   private sourceParameters;
+  private batchSize;
+  private maximumBatchingWindowInSeconds;
+  private maximumRecordAgeInSeconds;
+  private maximumRetryAttempts;
+  private parallelizationFactor;
 
   constructor(stream: IStream, parameters: KinesisSourceParameters) {
     this.stream = stream;
     this.sourceArn = stream.streamArn;
     this.sourceParameters = parameters;
+    this.batchSize = this.sourceParameters.batchSize;
+    this.maximumBatchingWindowInSeconds = this.sourceParameters.maximumBatchingWindow?.toSeconds();
+    this.maximumRecordAgeInSeconds = this.sourceParameters.maximumRecordAgeInSeconds;
+    this.maximumRetryAttempts = this.sourceParameters.maximumRetryAttempts;
+    this.parallelizationFactor = this.sourceParameters.parallelizationFactor;
+
+    if (this.batchSize !== undefined) {
+      if (this.batchSize < 1 || this.batchSize > 10000) {
+        throw new Error(`Batch size must be between 1 and 10000, received ${this.batchSize}`);
+      }
+    }
+    if (this.maximumBatchingWindowInSeconds !== undefined) {
+      // only need to check upper bound since Duration amounts cannot be negative
+      if (this.maximumBatchingWindowInSeconds > 300) {
+        throw new Error(`Maximum batching window must be between 0 and 300, received ${this.maximumBatchingWindowInSeconds}`);
+      }
+    }
+    if (this.maximumRecordAgeInSeconds !== undefined) {
+      if (this.maximumRecordAgeInSeconds < -1 || this.maximumRecordAgeInSeconds > 604800) {
+        throw new Error(`Maximum record age in seconds must be between -1 and 604800, received ${this.maximumRecordAgeInSeconds}`);
+      }
+    }
+    if (this.maximumRetryAttempts !== undefined) {
+      if (this.maximumRetryAttempts < -1 || this.maximumRetryAttempts > 10000) {
+        throw new Error(`Maximum retry attempts must be between -1 and 10000, received ${this.maximumRetryAttempts}`);
+      }
+    }
+    if (this.parallelizationFactor !== undefined) {
+      if (this.parallelizationFactor < 1 || this.parallelizationFactor > 10) {
+        throw new Error(`Parallelization factor must be between 1 and 10, received ${this.parallelizationFactor}`);
+      }
+    }
   }
 
   bind(_pipe: IPipe): SourceConfig {
     return {
       sourceParameters: {
         kinesisStreamParameters: {
-          batchSize: this.sourceParameters.batchSize,
+          batchSize: this.batchSize,
           deadLetterConfig: this.sourceParameters.deadLetterConfig,
-          maximumBatchingWindowInSeconds: this.sourceParameters.maximumBatchingWindow?.toSeconds(),
-          maximumRecordAgeInSeconds: this.sourceParameters.maximumRecordAge?.toSeconds() ?? -1,
-          maximumRetryAttempts: this.sourceParameters.maximumRetryAttempts ?? -1,
+          maximumBatchingWindowInSeconds: this.maximumBatchingWindowInSeconds,
+          maximumRecordAgeInSeconds: this.maximumRecordAgeInSeconds,
+          maximumRetryAttempts: this.maximumRetryAttempts,
           onPartialBatchItemFailure: this.sourceParameters.onPartialBatchItemFailure,
-          parallelizationFactor: this.sourceParameters.parallelizationFactor ?? 1,
+          parallelizationFactor: this.sourceParameters.parallelizationFactor,
           startingPosition: this.sourceParameters.startingPosition,
           startingPositionTimestamp: this.sourceParameters.startingPositionTimestamp,
         },
--- packages/@aws-cdk/aws-pipes-sources-alpha/test/kinesis.test.ts
+++ packages/@aws-cdk/aws-pipes-sources-alpha/test/kinesis.test.ts
@@ -2,17 +2,18 @@ import { Pipe } from '@aws-cdk/aws-pipes-alpha';
 import { App, Duration, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { Stream } from 'aws-cdk-lib/aws-kinesis';
+import { Topic } from 'aws-cdk-lib/aws-sns';
 import { TestTarget } from './test-classes';
 import { KinesisSource, OnPartialBatchItemFailure, StartingPosition } from '../lib';
 
-describe('kinesis', () => {
-  it('should have only source arn', () => {
+describe('kinesis source', () => {
+  it('should have only source arn and starting position', () => {
     // ARRANGE
     const app = new App();
     const stack = new Stack(app, 'TestStack');
     const stream = new Stream(stack, 'MyStream', {});
     const source = new KinesisSource(stream, {
-      startingPosition: StartingPosition.LATEST,
+      startingPosition: StartingPosition.TRIM_HORIZON,
     });
 
     new Pipe(stack, 'MyPipe', {
@@ -33,7 +34,7 @@ describe('kinesis', () => {
       },
       SourceParameters: {
         KinesisStreamParameters: {
-          StartingPosition: 'LATEST',
+          StartingPosition: 'TRIM_HORIZON',
         },
       },
     });
@@ -44,13 +45,14 @@ describe('kinesis', () => {
     const app = new App();
     const stack = new Stack(app, 'TestStack');
     const stream = new Stream(stack, 'MyStream', {});
+    const topic = new Topic(stack, 'MyTopic', {});
     const source = new KinesisSource(stream, {
       batchSize: 10,
       deadLetterConfig: {
-        arn: 'FakeArn',
+        arn: topic.topicArn,
       },
       maximumBatchingWindow: Duration.seconds(10),
-      maximumRecordAge: Duration.seconds(10),
+      maximumRecordAgeInSeconds: 10,
       maximumRetryAttempts: 10,
       onPartialBatchItemFailure: OnPartialBatchItemFailure.AUTOMATIC_BISECT,
       parallelizationFactor: 10,
@@ -72,7 +74,9 @@ describe('kinesis', () => {
         KinesisStreamParameters: {
           BatchSize: 10,
           DeadLetterConfig: {
-            Arn: 'FakeArn',
+            Arn: {
+              Ref: 'MyTopic86869434',
+            },
           },
           MaximumBatchingWindowInSeconds: 10,
           MaximumRecordAgeInSeconds: 10,
@@ -106,5 +110,142 @@ describe('kinesis', () => {
     // ASSERT
     expect(template.findResources('AWS::IAM::Role')).toMatchSnapshot();
     expect(template.findResources('AWS::IAM::Policy')).toMatchSnapshot();
+  });
+});
+
+describe('kinesis source parameters validation', () => {
+  test('batch size > 10000 should throw', () => {
+    // GIVEN
+    const app = new App();
+    const stack = new Stack(app, 'demo-stack');
+    const stream = new Stream(stack, 'MyStream', {});
+
+    // WHEN
+    expect(() => {
+      new KinesisSource(stream, {
+        startingPosition: StartingPosition.LATEST,
+        batchSize: 10001,
+      });
+    }).toThrow('Batch size must be between 1 and 10000, received 10001');
+  });
+
+  test('batch size < 1 should throw', () => {
+    // GIVEN
+    const app = new App();
+    const stack = new Stack(app, 'demo-stack');
+    const stream = new Stream(stack, 'MyStream', {});
+
+    // WHEN
+    expect(() => {
+      new KinesisSource(stream, {
+        startingPosition: StartingPosition.LATEST,
+        batchSize: 0,
+      });
+    }).toThrow('Batch size must be between 1 and 10000, received 0');
+  });
+
+  test('maximum batching window > 300 should throw', () => {
+    // GIVEN
+    const app = new App();
+    const stack = new Stack(app, 'demo-stack');
+    const stream = new Stream(stack, 'MyStream', {});
+
+    // WHEN
+    expect(() => {
+      new KinesisSource(stream, {
+        startingPosition: StartingPosition.LATEST,
+        maximumBatchingWindow: Duration.seconds(301),
+      });
+    }).toThrow('Maximum batching window must be between 0 and 300, received 301');
+  });
+
+  test('maximum record age < -1 should throw', () => {
+    // GIVEN
+    const app = new App();
+    const stack = new Stack(app, 'demo-stack');
+    const stream = new Stream(stack, 'MyStream', {});
+
+    // WHEN
+    expect(() => {
+      new KinesisSource(stream, {
+        startingPosition: StartingPosition.LATEST,
+        maximumRecordAgeInSeconds: -2,
+      });
+    }).toThrow('Maximum record age in seconds must be between -1 and 604800, received -2');
+  });
+
+  test('maximum record age > 604800 should throw', () => {
+    // GIVEN
+    const app = new App();
+    const stack = new Stack(app, 'demo-stack');
+    const stream = new Stream(stack, 'MyStream', {});
+
+    // WHEN
+    expect(() => {
+      new KinesisSource(stream, {
+        startingPosition: StartingPosition.LATEST,
+        maximumRecordAgeInSeconds: 604801,
+      });
+    }).toThrow('Maximum record age in seconds must be between -1 and 604800, received 604801');
+  });
+
+  test('maximum retry attempts < -1 should throw', () => {
+    // GIVEN
+    const app = new App();
+    const stack = new Stack(app, 'demo-stack');
+    const stream = new Stream(stack, 'MyStream', {});
+
+    // WHEN
+    expect(() => {
+      new KinesisSource(stream, {
+        startingPosition: StartingPosition.LATEST,
+        maximumRetryAttempts: -2,
+      });
+    }).toThrow('Maximum retry attempts must be between -1 and 10000, received -2');
+  });
+
+  test('maximum retry attempts > 10000 should throw', () => {
+    // GIVEN
+    const app = new App();
+    const stack = new Stack(app, 'demo-stack');
+    const stream = new Stream(stack, 'MyStream', {});
+
+    // WHEN
+    expect(() => {
+      new KinesisSource(stream, {
+        startingPosition: StartingPosition.LATEST,
+        maximumRetryAttempts: 10001,
+      });
+    }).toThrow('Maximum retry attempts must be between -1 and 10000, received 10001');
+  });
+
+  test('parallelization factor < 1 should throw', () => {
+    // GIVEN
+    const app = new App();
+    const stack = new Stack(app, 'demo-stack');
+    const stream = new Stream(stack, 'MyStream', {});
+
+    // WHEN
+    expect(() => {
+      new KinesisSource(stream, {
+        startingPosition: StartingPosition.LATEST,
+        parallelizationFactor: 0,
+      });
+    }).toThrow('Parallelization factor must be between 1 and 10, received 0');
+  });
+
+  test('parallelization factor > 10 should throw', () => {
+    // GIVEN
+    const app = new App();
+    const stack = new Stack(app, 'demo-stack');
+    const stream = new Stream(stack, 'MyStream', {});
+
+    // WHEN
+    expect(() => {
+      new KinesisSource(stream, {
+        startingPosition: StartingPosition.LATEST,
+        parallelizationFactor: 11,
+      });
+    }).toThrow('Parallelization factor must be between 1 and 10, received 11');
   });
 });
