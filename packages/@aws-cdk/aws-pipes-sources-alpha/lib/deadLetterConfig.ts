1ff59abd0fef3d34eb189e7aead73542a683740d
Michael Sambol <sambol.michael@gmail.com>
2024-03-14 08:25:06 -0600

    change maximumRecordAge back to Duration

--- packages/@aws-cdk/aws-pipes-sources-alpha/lib/deadLetterConfig.ts
+++ packages/@aws-cdk/aws-pipes-sources-alpha/lib/deadLetterConfig.ts
@@ -8,7 +8,7 @@ export interface DeadLetterConfigParameters {
    * For Amazon Kinesis stream and Amazon DynamoDB stream sources, specify either an Amazon SNS topic or Amazon SQS queue ARN.
    *
    * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-pipes-pipe-deadletterconfig.html#cfn-pipes-pipe-deadletterconfig-arn
-   * @default no dead letter target queue
+   * @default no dead letter queue
    */
   readonly arn?: string;
 }
