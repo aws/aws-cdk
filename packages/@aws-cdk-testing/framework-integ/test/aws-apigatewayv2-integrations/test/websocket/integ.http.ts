c2e932f2c9c9e1f6af57dfc2c34c906bb12ac007
Jimmy Gaussen <jimmy.gaussen@gmail.com>
2024-03-22 17:02:40 +0100

    chore: integ tests (wip)

--- packages/@aws-cdk-testing/framework-integ/test/aws-apigatewayv2-integrations/test/websocket/integ.http.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-apigatewayv2-integrations/test/websocket/integ.http.ts
@@ -1,8 +1,10 @@
 import * as integ from '@aws-cdk/integ-tests-alpha';
 import { App, CfnOutput, Duration, Stack } from 'aws-cdk-lib';
 import { ContentHandling, HttpApi, WebSocketApi, WebSocketStage } from 'aws-cdk-lib/aws-apigatewayv2';
-import { HttpLambdaIntegration, WebSocketHttpProxyIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { HttpLambdaIntegration, WebSocketHttpIntegration, WebSocketHttpProxyIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+// import * as iam from 'aws-cdk-lib/aws-iam';
+// import * as logs from 'aws-cdk-lib/aws-logs';
 import * as assert from 'node:assert';
 
 /*
@@ -24,20 +26,30 @@ const httpHandler = new lambda.Function(stack, 'HttpHandler', {
 const httpApi = new HttpApi(stack, 'HttpApi', {
   defaultIntegration: new HttpLambdaIntegration('DefaultIntegration', httpHandler),
 });
-
 assert(httpApi.url, 'HTTP API URL is required');
 
-const websocketHttpIntegration = new WebSocketHttpProxyIntegration('WebsocketHttpIntegration', {
+const webSocketApi = new WebSocketApi(stack, 'WebSocketApi', {});
+
+const websocketHttpIntegration = new WebSocketHttpIntegration('WebsocketHttpIntegration', {
+  integrationMethod: 'GET',
+  integrationUri: httpApi.url,
+  timeout: Duration.seconds(10),
+  // contentHandling: ContentHandling.CONVERT_TO_BINARY,
+});
+webSocketApi.addRoute('http', {
+  integration: websocketHttpIntegration,
+  returnResponse: true,
+});
+
+const websocketHttpProxyIntegration = new WebSocketHttpProxyIntegration('WebsocketHttpIntegration', {
   integrationMethod: 'GET',
   integrationUri: httpApi.url,
   timeout: Duration.seconds(10),
   contentHandling: ContentHandling.CONVERT_TO_TEXT,
 });
-
-const webSocketApi = new WebSocketApi(stack, 'WebSocketApi', {
-  defaultRouteOptions: {
-    integration: websocketHttpIntegration,
-  },
+webSocketApi.addRoute('http-proxy', {
+  integration: websocketHttpProxyIntegration,
+  returnResponse: true,
 });
 
 const stage = new WebSocketStage(stack, 'mystage', {
@@ -45,6 +57,45 @@ const stage = new WebSocketStage(stack, 'mystage', {
   stageName: 'dev',
   autoDeploy: true,
 });
+
+// FIXME https://github.com/aws/aws-cdk/issues/11100
+/* const accessLogs = new logs.LogGroup(stack, 'APIGW-AccessLogs');
+(stage.node.defaultChild as CfnStage).accessLogSettings = {
+  destinationArn: accessLogs.logGroupArn,
+  format: JSON.stringify({
+    requestId: '$context.requestId',
+    userAgent: '$context.identity.userAgent',
+    sourceIp: '$context.identity.sourceIp',
+    requestTime: '$context.requestTime',
+    requestTimeEpoch: '$context.requestTimeEpoch',
+    httpMethod: '$context.httpMethod',
+    path: '$context.path',
+    status: '$context.status',
+    protocol: '$context.protocol',
+    responseLength: '$context.responseLength',
+    domainName: '$context.domainName',
+  }),
+};
+
+const role = new iam.Role(stack, 'ApiGWLogWriterRole', {
+  assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+});
+
+const policy = new iam.PolicyStatement({
+  actions: [
+    'logs:CreateLogGroup',
+    'logs:CreateLogStream',
+    'logs:DescribeLogGroups',
+    'logs:DescribeLogStreams',
+    'logs:PutLogEvents',
+    'logs:GetLogEvents',
+    'logs:FilterLogEvents',
+  ],
+  resources: ['*'],
+});
+
+role.addToPolicy(policy);
+accessLogs.grantWrite(role); */
 
 new CfnOutput(stack, 'ApiEndpoint', { value: stage.url });
 
