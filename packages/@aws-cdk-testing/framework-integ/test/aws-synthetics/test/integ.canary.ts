2bfe2aea31d6279d45d7f60800ea4bf8805a562d
Kip Shields <kishiel@amazon.com>
2024-03-30 23:18:03 +0000

    update synthetics runtime versions and snapshots

--- packages/@aws-cdk-testing/framework-integ/test/aws-synthetics/test/integ.canary.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-synthetics/test/integ.canary.ts
@@ -40,7 +40,7 @@ const inlineAsset = new Canary(stack, 'InlineAsset', {
   }),
   schedule: Schedule.rate(cdk.Duration.minutes(1)),
   artifactsBucketLocation: { bucket, prefix },
-  runtime: Runtime.SYNTHETICS_NODEJS_PUPPETEER_4_0,
+  runtime: Runtime.SYNTHETICS_NODEJS_PUPPETEER_7_0,
   cleanup: Cleanup.LAMBDA,
 });
 
@@ -49,7 +49,7 @@ const directoryAsset = new Canary(stack, 'DirectoryAsset', {
     handler: 'canary.handler',
     code: Code.fromAsset(path.join(__dirname, 'canaries')),
   }),
-  runtime: Runtime.SYNTHETICS_NODEJS_PUPPETEER_4_0,
+  runtime: Runtime.SYNTHETICS_NODEJS_PUPPETEER_7_0,
   environmentVariables: {
     URL: api.url,
   },
@@ -61,7 +61,7 @@ const folderAsset = new Canary(stack, 'FolderAsset', {
     handler: 'folder/canary.functionName',
     code: Code.fromAsset(path.join(__dirname, 'canaries')),
   }),
-  runtime: Runtime.SYNTHETICS_NODEJS_PUPPETEER_4_0,
+  runtime: Runtime.SYNTHETICS_NODEJS_PUPPETEER_7_0,
   environmentVariables: {
     URL: api.url,
   },
@@ -78,7 +78,7 @@ const zipAsset = new Canary(stack, 'ZipAsset', {
       expiration: cdk.Duration.days(30),
     },
   ],
-  runtime: Runtime.SYNTHETICS_NODEJS_PUPPETEER_4_0,
+  runtime: Runtime.SYNTHETICS_NODEJS_PUPPETEER_7_0,
   cleanup: Cleanup.LAMBDA,
 });
 
@@ -96,26 +96,24 @@ const createCanaryByRuntimes = (runtime: Runtime) =>
     cleanup: Cleanup.LAMBDA,
   });
 
-const puppeteer40 = createCanaryByRuntimes(Runtime.SYNTHETICS_NODEJS_PUPPETEER_4_0);
+const puppeteer70 = createCanaryByRuntimes(Runtime.SYNTHETICS_NODEJS_PUPPETEER_7_0);
 const puppeteer62 = createCanaryByRuntimes(Runtime.SYNTHETICS_NODEJS_PUPPETEER_6_2);
-const selenium13 = createCanaryByRuntimes(Runtime.SYNTHETICS_PYTHON_SELENIUM_1_3);
-const selenium20 = createCanaryByRuntimes(Runtime.SYNTHETICS_PYTHON_SELENIUM_2_0);
+const selenium30 = createCanaryByRuntimes(Runtime.SYNTHETICS_PYTHON_SELENIUM_3_0);
 const selenium21 = createCanaryByRuntimes(Runtime.SYNTHETICS_PYTHON_SELENIUM_2_1);
 
 const test = new IntegTest(app, 'IntegCanaryTest', {
   testCases: [stack],
 });
 
-// Assertion that all Canary's are Passed
+// Assertion that all Canaries are Passed
 [
   inlineAsset,
   directoryAsset,
   folderAsset,
   zipAsset,
-  puppeteer40,
+  puppeteer70,
   puppeteer62,
-  selenium13,
-  selenium20,
+  selenium30,
   selenium21,
 ].forEach((canary) => test.assertions
   .awsApiCall('Synthetics', 'getCanaryRuns', {
