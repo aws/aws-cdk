ea8d4de2fde55e415d719c7689ba9ca71467de9c
Francis <colifran@amazon.com>
2024-04-03 21:49:56 -0700

    integ test
    Signed-off-by: Francis <colifran@amazon.com>

---
+++ packages/@aws-cdk-testing/framework-integ/test/custom-resources/test/aws-custom-resource/integ.aws-custom-resource-ssm.ts
@@ -0,0 +1,64 @@
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { AwsCustomResource, AwsCustomResourcePolicy, Logging, PhysicalResourceId } from 'aws-cdk-lib/custom-resources';
+import { Construct } from 'constructs';
+
+const app = new App();
+
+class CreateParameterStack extends Stack {
+  public readonly parameterName: string;
+
+  public constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    const parameter = new StringParameter(this, 'Secret', {
+      parameterName: 'Password',
+      stringValue: 'ThisIsMyPassword',
+    });
+
+    this.parameterName = parameter.parameterName;
+  }
+}
+
+interface GetParameterStackProps extends StackProps {
+  readonly parameterName: string;
+}
+
+class GetParameterStack extends Stack {
+  public constructor(scope: Construct, id: string, props: GetParameterStackProps) {
+    super(scope, id, props);
+
+    const logging = Logging.selective({
+      logApiResponse: false,
+      logResponseObject: false,
+    });
+
+    new AwsCustomResource(this, 'GetSecret', {
+      onUpdate: {
+        service: 'SSM',
+        action: 'GetParameter',
+        parameters: {
+          Name: props.parameterName,
+          WithDecryption: true,
+        },
+        physicalResourceId: PhysicalResourceId.of(Date.now().toString()),
+      },
+      policy: AwsCustomResourcePolicy.fromSdkCalls({
+        resources: AwsCustomResourcePolicy.ANY_RESOURCE,
+      }),
+      logging,
+    });
+  }
+}
+
+const createParameterStack = new CreateParameterStack(app, 'CreateParameterStack');
+const getParameterStack = new GetParameterStack(app, 'GetParameterStack', {
+  parameterName: createParameterStack.parameterName,
+});
+
+getParameterStack.addDependency(createParameterStack);
+
+new IntegTest(app, 'AwsCustomResourceSsmInteg', {
+  testCases: [createParameterStack, getParameterStack],
+});
