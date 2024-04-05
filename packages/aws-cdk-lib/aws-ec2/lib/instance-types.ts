f6bb28f667dfa129a2af68d3ff4cce188f22c142
Tatsuya Mori <tamori@amazon.com>
2024-04-05 00:49:27 +0000

    feat(ec2): support for g6 and gr6 instance types in aws-ec2

--- packages/aws-cdk-lib/aws-ec2/lib/instance-types.ts
+++ packages/aws-cdk-lib/aws-ec2/lib/instance-types.ts
@@ -881,6 +881,26 @@ export enum InstanceClass {
   G5G = 'g5g',
 
   /**
+   * Graphics-optimized instances powered by NVIDIA L4 Tensor Core GPUs, 6th generation
+   */
+  GRAPHICS6 = 'graphics6',
+
+  /**
+   * Graphics-optimized instances powered by NVIDIA L4 Tensor Core GPUs, 6th generation
+   */
+  G6 = 'g6',
+
+  /**
+   * Graphics-optimized instances powered by NVIDIA L4 Tensor Core GPUs with higher memory ratio, 6th generation
+   */
+  GRAPHICS6_MEMORY = 'graphics6-memory',
+
+  /**
+   * Graphics-optimized instances powered by NVIDIA L4 Tensor Core GPUs with higher memory ratio, 6th generation
+   */
+  GR6 = 'gr6',
+
+  /**
    * Parallel-processing optimized instances, 2nd generation
    */
   PARALLEL2 = 'parallel2',
@@ -1556,6 +1576,10 @@ export class InstanceType {
       [InstanceClass.G5]: 'g5',
       [InstanceClass.GRAPHICS5_GRAVITON2]: 'g5g',
       [InstanceClass.G5G]: 'g5g',
+      [InstanceClass.GRAPHICS6]: 'g6',
+      [InstanceClass.G6]: 'g6',
+      [InstanceClass.GRAPHICS6_MEMORY]: 'gr6',
+      [InstanceClass.GR6]: 'gr6',
       [InstanceClass.PARALLEL2]: 'p2',
       [InstanceClass.P2]: 'p2',
       [InstanceClass.PARALLEL3]: 'p3',
