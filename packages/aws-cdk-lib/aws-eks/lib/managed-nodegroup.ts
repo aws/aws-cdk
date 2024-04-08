09cfe55c7068b26b43c39f0f2a4b993557a2845d
Tatsuya Mori <tamori@amazon.com>
2024-04-08 16:16:34 +0000

    Add g6 and g6r for isGpuInstanceType method

--- packages/aws-cdk-lib/aws-eks/lib/managed-nodegroup.ts
+++ packages/aws-cdk-lib/aws-eks/lib/managed-nodegroup.ts
@@ -597,6 +597,7 @@ function isGpuInstanceType(instanceType: InstanceType): boolean {
   //compare instanceType to known GPU InstanceTypes
   const knownGpuInstanceTypes = [InstanceClass.P2, InstanceClass.P3, InstanceClass.P3DN, InstanceClass.P4DE, InstanceClass.P4D,
     InstanceClass.G3S, InstanceClass.G3, InstanceClass.G4DN, InstanceClass.G4AD, InstanceClass.G5, InstanceClass.G5G,
+    InstanceClass.G6, InstanceClass.GR6,
     InstanceClass.INF1, InstanceClass.INF2];
   return knownGpuInstanceTypes.some((c) => instanceType.sameInstanceClassAs(InstanceType.of(c, InstanceSize.LARGE)));
 }
