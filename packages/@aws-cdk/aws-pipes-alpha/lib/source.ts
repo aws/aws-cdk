724ebfbde2bdfc50b385c223bf1c06cb6b8df860
Michael Sambol <sambol.michael@gmail.com>
2024-04-09 10:05:31 -0600

    Use deadLetterTarget not sourceArn

--- packages/@aws-cdk/aws-pipes-alpha/lib/source.ts
+++ packages/@aws-cdk/aws-pipes-alpha/lib/source.ts
@@ -112,7 +112,7 @@ export abstract class SourceWithDeadLetterTarget implements ISource {
    * Determines if the source is an instance of SourceWithDeadLetterTarget.
    */
   public static isSourceWithDeadLetterTarget(source: ISource): source is SourceWithDeadLetterTarget {
-    return (source as SourceWithDeadLetterTarget).sourceArn !== undefined;
+    return (source as SourceWithDeadLetterTarget).deadLetterTarget !== undefined;
   }
   /**
    * The ARN of the source resource.
