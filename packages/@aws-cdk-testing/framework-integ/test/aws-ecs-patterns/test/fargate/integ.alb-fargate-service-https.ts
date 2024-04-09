5b36ab50040de895b65b3daec745e459b6f9643c
wafuwafu13 <jaruwafu@gmail.com>
2024-04-09 21:18:12 +0100

    fix(ecs-patterns): integ test failed with certificate error

--- packages/@aws-cdk-testing/framework-integ/test/aws-ecs-patterns/test/fargate/integ.alb-fargate-service-https.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs-patterns/test/fargate/integ.alb-fargate-service-https.ts
@@ -6,6 +6,13 @@ import { App, Stack } from 'aws-cdk-lib';
 import * as integ from '@aws-cdk/integ-tests-alpha';
 import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
 
+const hostedZoneId = process.env.CDK_INTEG_HOSTED_ZONE_ID ?? process.env.HOSTED_ZONE_ID;
+if (!hostedZoneId) throw new Error('For this test you must provide your own HostedZoneId as an env var "HOSTED_ZONE_ID". See framework-integ/README.md for details.');
+const hostedZoneName = process.env.CDK_INTEG_HOSTED_ZONE_NAME ?? process.env.HOSTED_ZONE_NAME;
+if (!hostedZoneName) throw new Error('For this test you must provide your own HostedZoneName as an env var "HOSTED_ZONE_NAME". See framework-integ/README.md for details.');
+const domainName = process.env.CDK_INTEG_DOMAIN_NAME ?? process.env.DOMAIN_NAME;
+if (!domainName) throw new Error('For this test you must provide your own DomainName as an env var "DOMAIN_NAME". See framework-integ/README.md for details.');
+
 const app = new App();
 const stack = new Stack(app, 'aws-ecs-integ-alb-fg-https');
 const vpc = new Vpc(stack, 'Vpc', { maxAzs: 2, restrictDefaultSecurityGroup: false });
@@ -21,10 +28,10 @@ new ApplicationLoadBalancedFargateService(stack, 'myService', {
   protocol: ApplicationProtocol.HTTPS,
   enableECSManagedTags: true,
   enableExecuteCommand: true,
-  domainName: 'test.example.com',
+  domainName: domainName,
   domainZone: route53.HostedZone.fromHostedZoneAttributes(stack, 'HostedZone', {
-    hostedZoneId: 'fakeId',
-    zoneName: 'example.com.',
+    hostedZoneId: hostedZoneId,
+    zoneName: hostedZoneName,
   }),
   redirectHTTP: true,
 });
