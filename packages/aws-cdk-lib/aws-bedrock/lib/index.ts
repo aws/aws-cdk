425de6d279612d8135bfa8b026d784bf6db8cdf3
Hiroki Yamazaki <yhirokiy@amazon.co.jp>
2024-04-19 08:29:46 +0000

    support L1 Construct for Bedrock

--- packages/aws-cdk-lib/aws-bedrock/lib/index.ts
+++ packages/aws-cdk-lib/aws-bedrock/lib/index.ts
@@ -1,3 +1,6 @@
 export * from './foundation-model';
 export * from './model-base';
 export * from './provisioned-model';
+
+// AWS::Bedrock Cloudformation Resources
+export * from './bedrock.generated';
