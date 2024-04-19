469bae58397ebdabfa4e6ce7a051e401bcf273f7
k.goto <24818752+go-to-k@users.noreply.github.com>
2024-04-20 06:28:33 +0900

    fix(custom-resources): cannot set logging for state machine generated in CompleteHandler (#28706)
    This PR fixes the case that `StateMachine` generated for `CompleteHandler` in `Provider` cannot set logging.
    
    The default log group name of the state machine automatically created by the `Provider` with `isCompleteHandler` should start with `/aws/vendedlogs/states`. Otherwise, each time the state machine is created, the log resource policy size increases. 
    
    https://docs.aws.amazon.com/step-functions/latest/dg/bp-cwl.html
    
    Closes #27283
    Fixes #28577 #28744
    
    Related PR #27310(reverted by #28699), #28587
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk-testing/framework-integ/test/custom-resources/test/provider-framework/integ.provider-with-waiter-state-machine.ts
@@ -0,0 +1,60 @@
+/// !cdk-integ *
+import { App, Stack } from 'aws-cdk-lib';
+import * as integ from '@aws-cdk/integ-tests-alpha';
+import { Construct } from 'constructs';
+import { SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Provider } from 'aws-cdk-lib/custom-resources';
+import { LogLevel } from 'aws-cdk-lib/aws-stepfunctions';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
+
+class TestStack extends Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const vpc = new Vpc(this, 'Vpc');
+    const securityGroup = new SecurityGroup(this, 'SecurityGroup', { vpc });
+    const logGroup = new LogGroup(this, 'LogGroup');
+    const onEventHandler = new Function(this, 'OnEvent', {
+      code: Code.fromInline('foo'),
+      handler: 'index.onEvent',
+      runtime: Runtime.NODEJS_LATEST,
+    });
+    const isCompleteHandler = new Function(this, 'IsComplete', {
+      code: Code.fromInline('foo'),
+      handler: 'index.isComplete',
+      runtime: Runtime.NODEJS_LATEST,
+    });
+
+    new Provider(this, 'MyProvider', {
+      onEventHandler,
+      isCompleteHandler,
+      vpc: vpc,
+      vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+      securityGroups: [securityGroup],
+      waiterStateMachineLogOptions: {
+        destination: logGroup,
+        includeExecutionData: true,
+        level: LogLevel.ALL,
+      },
+    });
+
+    new Provider(this, 'MyProviderWithoutLogOptions', {
+      onEventHandler,
+      isCompleteHandler,
+      vpc: vpc,
+      vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+      securityGroups: [securityGroup],
+    });
+  }
+}
+
+const app = new App();
+const stack = new TestStack(app, 'integ-provider-with-waiter-state-machine');
+
+new integ.IntegTest(app, 'IntegProviderWithWaiterStateMachine', {
+  testCases: [stack],
+  diffAssets: true,
+});
+
+app.synth();
--- packages/aws-cdk-lib/custom-resources/lib/provider-framework/index.ts
+++ packages/aws-cdk-lib/custom-resources/lib/provider-framework/index.ts
@@ -1,1 +1,2 @@
 export * from './provider';
+export * from './waiter-state-machine';
