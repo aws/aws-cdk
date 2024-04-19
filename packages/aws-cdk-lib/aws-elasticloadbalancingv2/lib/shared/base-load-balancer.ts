9ad8a121b7eca001123282188b3f305f6ff1062c
pahud <pahud@pahud.cc>
2024-04-19 17:37:48 +0000

    minor update on doc string and logic

--- packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/shared/base-load-balancer.ts
+++ packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/shared/base-load-balancer.ts
@@ -52,6 +52,8 @@ export interface BaseLoadBalancerProps {
    * Indicates whether cross-zone load balancing is enabled.
    *
    * @default - false for Network Load Balancers and true for Application Load Balancers.
+   * This can not be `false` for Application Load Balancers.
+   * @see - https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-elasticloadbalancingv2-loadbalancer-loadbalancerattribute.html
    */
   readonly crossZoneEnabled?: boolean;
 
@@ -244,13 +246,17 @@ export abstract class BaseLoadBalancer extends Resource {
 
     this.setAttribute('deletion_protection.enabled', baseProps.deletionProtection ? 'true' : 'false');
 
-    if (baseProps.crossZoneEnabled === true) {
-      this.setAttribute('load_balancing.cross_zone.enabled', 'true');
+    if (baseProps.crossZoneEnabled !== undefined) {
+      this.setAttribute('load_balancing.cross_zone.enabled', baseProps.crossZoneEnabled === true ? 'true' : 'false');
     }
 
-    if (baseProps.crossZoneEnabled === false) {
-      this.setAttribute('load_balancing.cross_zone.enabled', 'false');
-    }
+    // if (baseProps.crossZoneEnabled === true) {
+    //   this.setAttribute('load_balancing.cross_zone.enabled', 'true');
+    // }
+
+    // if (baseProps.crossZoneEnabled === false) {
+    //   this.setAttribute('load_balancing.cross_zone.enabled', 'false');
+    // }
 
     if (baseProps.denyAllIgwTraffic !== undefined) {
       this.setAttribute('ipv6.deny_all_igw_traffic', baseProps.denyAllIgwTraffic.toString());
