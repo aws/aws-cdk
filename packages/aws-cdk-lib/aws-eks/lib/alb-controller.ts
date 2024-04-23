7e275ec8faecd907bc9e4a30bdebc59243696a55
Jimmy Gaussen <jimmy.gaussen@gmail.com>
2024-04-24 01:01:19 +0200

    chore(eks): update ALB controller versions (#29470)
    ### Issue # (if applicable)
    
    None as far as I could tell
    
    ### Reason for this change
    
    Update the CDK listed ALB controller versions to match the current availability
    
    ### Description of changes
    
    * Added missing controller versions
    * Updated the Helm version of existing controller versions
    
    ### Description of how you validated changes
    
    I listed the list of available versions by using the `ecr:ListImages` command on the `amazon/aws-load-balancer-controller` repository. I'm also filtering out tags that do not match a `v1.2.3` pattern, e.g. `v2.0.0-rc5`, `v2.0.0-test-linux_amd64`
    
    For the Helm chart version, I **initially** manually went through the blame history of https://github.com/aws/eks-charts/blob/master/stable/aws-load-balancer-controller/Chart.yaml. @guessi then recommended I use the [Helm CLI](https://helm.sh/) to obtain the corresponding versions, which worked a ton better and was easily integrated to my tool:
    
    ```sh
    helm repo add eks https://aws.github.io/eks-charts
    helm repo update
    helm search repo aws-load-balancer-controller --versions --output json
    ```
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-eks/lib/alb-controller.ts
+++ packages/aws-cdk-lib/aws-eks/lib/alb-controller.ts
@@ -20,67 +20,72 @@ export class AlbControllerVersion {
   /**
    * v2.0.0
    */
-  public static readonly V2_0_0 = new AlbControllerVersion('v2.0.0', '1.4.1', false);
+  public static readonly V2_0_0 = new AlbControllerVersion('v2.0.0', '1.0.6', false);
 
   /**
    * v2.0.1
    */
-  public static readonly V2_0_1 = new AlbControllerVersion('v2.0.1', '1.4.1', false);
+  public static readonly V2_0_1 = new AlbControllerVersion('v2.0.1', '1.0.8', false);
 
   /**
    * v2.1.0
    */
-  public static readonly V2_1_0 = new AlbControllerVersion('v2.1.0', '1.4.1', false);
+  public static readonly V2_1_0 = new AlbControllerVersion('v2.1.0', '1.1.1', false);
 
   /**
    * v2.1.1
    */
-  public static readonly V2_1_1 = new AlbControllerVersion('v2.1.1', '1.4.1', false);
+  public static readonly V2_1_1 = new AlbControllerVersion('v2.1.1', '1.1.3', false);
 
   /**
    * v2.1.2
    */
-  public static readonly V2_1_2 = new AlbControllerVersion('v2.1.2', '1.4.1', false);
+  public static readonly V2_1_2 = new AlbControllerVersion('v2.1.2', '1.1.4', false);
 
   /**
    * v2.1.3
    */
-  public static readonly V2_1_3 = new AlbControllerVersion('v2.1.3', '1.4.1', false);
+  public static readonly V2_1_3 = new AlbControllerVersion('v2.1.3', '1.1.6', false);
 
   /**
    * v2.0.0
    */
-  public static readonly V2_2_0 = new AlbControllerVersion('v2.2.0', '1.4.1', false);
+  public static readonly V2_2_0 = new AlbControllerVersion('v2.2.0', '1.2.2', false);
 
   /**
    * v2.2.1
    */
-  public static readonly V2_2_1 = new AlbControllerVersion('v2.2.1', '1.4.1', false);
+  public static readonly V2_2_1 = new AlbControllerVersion('v2.2.1', '1.2.3', false);
 
   /**
    * v2.2.2
    */
-  public static readonly V2_2_2 = new AlbControllerVersion('v2.2.2', '1.4.1', false);
+  public static readonly V2_2_2 = new AlbControllerVersion('v2.2.2', '1.2.5', false);
 
   /**
    * v2.2.3
    */
-  public static readonly V2_2_3 = new AlbControllerVersion('v2.2.3', '1.4.1', false);
+  public static readonly V2_2_3 = new AlbControllerVersion('v2.2.3', '1.2.6', false);
 
   /**
    * v2.2.4
    */
-  public static readonly V2_2_4 = new AlbControllerVersion('v2.2.4', '1.4.1', false);
+  public static readonly V2_2_4 = new AlbControllerVersion('v2.2.4', '1.2.7', false);
 
   /**
    * v2.3.0
    */
-  public static readonly V2_3_0 = new AlbControllerVersion('v2.3.0', '1.4.1', false);
+  public static readonly V2_3_0 = new AlbControllerVersion('v2.3.0', '1.3.2', false);
 
   /**
    * v2.3.1
    */
-  public static readonly V2_3_1 = new AlbControllerVersion('v2.3.1', '1.4.1', false);
+  public static readonly V2_3_1 = new AlbControllerVersion('v2.3.1', '1.3.3', false);
+
+  /**
+   * v2.4.0
+   */
+  public static readonly V2_4_0 = new AlbControllerVersion('v2.4.0', '1.4.0', false);
 
   /**
    * v2.4.1
@@ -156,6 +161,21 @@ export class AlbControllerVersion {
    * v2.6.2
    */
   public static readonly V2_6_2 = new AlbControllerVersion('v2.6.2', '1.6.2', false);
+
+  /**
+   * v2.7.0
+   */
+  public static readonly V2_7_0 = new AlbControllerVersion('v2.7.0', '1.7.0', false);
+
+  /**
+   * v2.7.1
+   */
+  public static readonly V2_7_1 = new AlbControllerVersion('v2.7.1', '1.7.1', false);
+
+  /**
+   * v2.7.2
+   */
+  public static readonly V2_7_2 = new AlbControllerVersion('v2.7.2', '1.7.2', false);
 
   /**
    * Specify a custom version and an associated helm chart version.
