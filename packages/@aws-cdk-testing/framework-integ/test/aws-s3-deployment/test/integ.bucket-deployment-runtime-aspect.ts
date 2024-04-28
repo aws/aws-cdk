e17116ee895b4632b8fd328c9129af45d5651c29
Francis <colifran@amazon.com>
2024-04-27 23:41:06 -0700

    integ test
    Signed-off-by: Francis <colifran@amazon.com>

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-s3-deployment/test/integ.bucket-deployment-runtime-aspect.ts
@@ -0,0 +1,46 @@
+
+import * as path from 'path';
+import { App, Aspects, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Construct } from 'constructs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Function, InlineCode, NodeRuntimeAspect, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+class TestStack extends Stack {
+  public readonly functionToIgnore: Function;
+
+  public constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const destinationBucket = new Bucket(this, 'Destination', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: false,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true, // needed for integration test cleanup
+    });
+
+    new BucketDeployment(this, 'DeployMe', {
+      sources: [Source.asset(path.join(__dirname, 'my-website'))],
+      destinationBucket,
+      retainOnDelete: false, // default is true, which will block the integration test cleanup
+    });
+
+    const fn = new Function(this, 'Lambda', {
+      code: new InlineCode('foo'),
+      handler: 'index.handler',
+      runtime: Runtime.NODEJS_18_X,
+    });
+    this.functionToIgnore = fn;
+  }
+}
+
+const app = new App();
+const testStack = new TestStack(app, 'TestStack');
+Aspects.of(testStack).add(NodeRuntimeAspect.nodeJs20({
+  functionsToIgnore: [testStack.functionToIgnore],
+}));
+
+new IntegTest(app, 'aws-cdk-bucket-deployment-runtime-aspect', {
+  testCases: [testStack],
+});
