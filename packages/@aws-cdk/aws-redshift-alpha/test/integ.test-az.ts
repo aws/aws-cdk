0b81b0ee78c7ff2de465ef543fe92d23c8f9fabd
go-to-k <24818752+go-to-k@users.noreply.github.com>
2024-04-28 20:40:00 +0900

    test2

--- packages/@aws-cdk/aws-redshift-alpha/test/integ.test-az.ts
+++ packages/@aws-cdk/aws-redshift-alpha/test/integ.test-az.ts
@@ -4,8 +4,13 @@ import * as integ from '@aws-cdk/integ-tests-alpha';
 import * as constructs from 'constructs';
 import * as redshift from '../lib';
 
-const app = new cdk.App();
 const accountId = process.env.CDK_DEFAULT_ACCOUNT || '123456789012';
+// const contextKey = `availability-zones:account=123456789012:region=eu-west-1`;
+const app = new cdk.App({
+  // context: {
+  //   'availability-zones:account=123456789012:region=us-east-1': ['us-east-1a, us-east-1b, us-east-1c'],
+  // },
+});
 const stack = new cdk.Stack(app, 'aws-cdk-redshift-cluster-database', {
   // Specify account and region to create 3 AZs
   env: {
