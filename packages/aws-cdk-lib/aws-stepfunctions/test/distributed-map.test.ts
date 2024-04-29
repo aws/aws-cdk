90b28c7ed6b6caf23b7c38a036485b3c4158c44b
James Funk <james.funk@maloufcompanies.com>
2024-04-29 16:31:11 -0600

    Fix mangling of a different test

--- packages/aws-cdk-lib/aws-stepfunctions/test/distributed-map.test.ts
+++ packages/aws-cdk-lib/aws-stepfunctions/test/distributed-map.test.ts
@@ -763,6 +763,9 @@ describe('Distributed Map State', () => {
       return map;
     });
 
+    expect(() => app.synth()).toThrow(/label cannot contain any whitespace or special characters/);
+  });
+
   test('does not fail in synthesis if label has `s`', () => {
     const app = createAppWithMap((stack) => {
       const map = new stepfunctions.DistributedMap(stack, 'Map State', {
