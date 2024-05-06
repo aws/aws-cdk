01c53a1a1eb109b848a8dd81b0b74ea79ef66c1b
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-05-06 14:55:59 +0900

    feat: add integ test

--- packages/@aws-cdk-testing/framework-integ/test/aws-ecs-patterns/test/fargate/integ.nlb-ipv6.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs-patterns/test/fargate/integ.nlb-ipv6.ts
@@ -6,34 +6,27 @@ import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'Ipv6NlbStack');
+const stack = new cdk.Stack(app, 'NlbIpv6Stack');
 
 const vpc = new ec2.Vpc(stack, 'Vpc', {
   maxAzs: 2,
   restrictDefaultSecurityGroup: false,
-});
-
-const ipv6Block = new ec2.CfnVPCCidrBlock(
-  stack,
-  'IPv6_Block',
-  {
-    vpcId: vpc.vpcId,
-    amazonProvidedIpv6CidrBlock: true,
-  },
-);
-const vpcIpv6CidrBlock = cdk.Fn.select(0, vpc.vpcIpv6CidrBlocks);
-const subnetIpv6CidrBlocks = cdk.Fn.cidr(vpcIpv6CidrBlock, 256, '64');
-
-vpc.publicSubnets.forEach((subnet, idx) => {
-  subnet.node.addDependency(ipv6Block);
-  const cfnSubnet = subnet.node.defaultChild as ec2.CfnSubnet;
-  cfnSubnet.ipv6CidrBlock = cdk.Fn.select(idx, subnetIpv6CidrBlocks);
-  cfnSubnet.assignIpv6AddressOnCreation = true;
+  ipProtocol: ec2.IpProtocol.DUAL_STACK,
 });
 
 const cluster = new ecs.Cluster(stack, 'FargateCluster', { vpc });
 
-new ecsPatterns.NetworkLoadBalancedFargateService(stack, 'NlbFargateService', {
+const securityGroupNlb = new ec2.SecurityGroup(stack, 'SecurityGroupNlb', {
+  vpc,
+  allowAllOutbound: true,
+});
+const securityGroupService = new ec2.SecurityGroup(stack, 'SecurityGroupService', {
+  vpc,
+  allowAllOutbound: true,
+});
+securityGroupService.addIngressRule(ec2.Peer.securityGroupId(securityGroupNlb.securityGroupId), ec2.Port.tcp(80));
+
+const nlbLoadbalancedFargateService = new ecsPatterns.NetworkLoadBalancedFargateService(stack, 'NlbFargateService', {
   cluster,
   memoryLimitMiB: 1024,
   cpu: 512,
@@ -41,8 +34,11 @@ new ecsPatterns.NetworkLoadBalancedFargateService(stack, 'NlbFargateService', {
     image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
   },
   ipAddressType: elbv2.IpAddressType.DUAL_STACK,
+  securityGroups: [securityGroupService],
 });
+nlbLoadbalancedFargateService.loadBalancer.connections.addSecurityGroup(securityGroupNlb);
+nlbLoadbalancedFargateService.loadBalancer.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
 
-new integ.IntegTest(app, 'Ipv6NlbTest', {
+new integ.IntegTest(app, 'NlbIpv6Test', {
   testCases: [stack],
 });
