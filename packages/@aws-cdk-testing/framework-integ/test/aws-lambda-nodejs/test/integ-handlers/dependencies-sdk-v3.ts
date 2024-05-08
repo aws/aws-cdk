5c78cf72022ad3dfeff7ea85a92f57040a10655d
Parker Scanlon <69879391+scanlonp@users.noreply.github.com>
2024-05-08 13:20:59 -0700

    chore(lambda-nodejs): clean up sdk v2 references and warn about runtime updates related to sdk bundling (#30099)
    Closes #29836.
    
    While sdk v2 was not being used directly in the construct, we had some remnants of its use. Added a warning for users of Node 16 who do not bundle the sdk on what it will take to update to newer node versions. Also updated integ tests to make actual sdk v3 calls as well as clean up false references to v2.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ-handlers/dependencies-sdk-v3.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ-handlers/dependencies-sdk-v3.ts
@@ -1,9 +1,13 @@
 /* eslint-disable no-console */
 // @ts-ignore
-import { S3Client } from '@aws-sdk/client-s3'; // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved
-
-const s3 = new S3Client({});
+import { DynamoDBClient, DescribeTableCommand } from '@aws-sdk/client-dynamodb'; // eslint-disable-line import/no-extraneous-dependencies
 
 export async function handler() {
-  console.log(s3);
+  const client = new DynamoDBClient();
+  const input = {
+    TableName: process.env.TABLE_NAME,
+  };
+  const command = new DescribeTableCommand(input);
+  const response = await client.send(command);
+  console.log(response);
 }
