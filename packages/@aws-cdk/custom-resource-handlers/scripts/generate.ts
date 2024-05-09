71579447d2f180aa379ffa66d2764255666ffb23
Francis <colifran@amazon.com>
2024-05-09 14:43:05 -0700

    remove test comment
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk/custom-resource-handlers/scripts/generate.ts
+++ packages/@aws-cdk/custom-resource-handlers/scripts/generate.ts
@@ -6,8 +6,6 @@ import { HandlerFrameworkModule } from '../lib/custom-resources-framework/framew
 
 const framework: { [fqn: string]: ComponentProps[] } = {};
 
-// test comment
-
 async function main() {
   recurse(config, []);
 
