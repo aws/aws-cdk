d43f883279ac2f47fc065d4fa198ed7f2d95b563
maz <yiyth.fcb6@gmail.com>
2024-05-15 23:34:37 +0900

    fix: update integ tests

--- packages/@aws-cdk/aws-neptune-alpha/test/integ.cluster-copy-tags-to-snapshot.ts
+++ packages/@aws-cdk/aws-neptune-alpha/test/integ.cluster-copy-tags-to-snapshot.ts
@@ -5,7 +5,7 @@ import { DatabaseCluster, InstanceType } from '../lib';
 
 const app = new cdk.App();
 
-const stack = new cdk.Stack(app, 'aws-cdk-neptune-integ');
+const stack = new cdk.Stack(app, 'aws-cdk-neptune-integ-copy-tags-to-snapshot');
 
 const vpc = new ec2.Vpc(stack, 'VPC', { maxAzs: 2, restrictDefaultSecurityGroup: false });
 
@@ -13,12 +13,14 @@ new DatabaseCluster(stack, 'DatabaseCopyTagsToSnapshotDisabled', {
   vpc,
   instanceType: InstanceType.R5_LARGE,
   copyTagsToSnapshot: false,
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
 });
 
 new DatabaseCluster(stack, 'DatabaseCopyTagsToSnapshotEnabled', {
   vpc,
   instanceType: InstanceType.R5_LARGE,
   copyTagsToSnapshot: true,
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
 });
 
 new integ.IntegTest(app, 'ClusterTest', {
