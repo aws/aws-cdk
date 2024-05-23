e7bd0e20495e07bffa1f38c4837600123be0015c
Francis <colifran@amazon.com>
2024-05-23 12:59:12 -0700

    migrate amplify handlers to framework and update snaps
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk/aws-amplify-alpha/scripts/airlift-custom-resource-handlers.sh
+++ packages/@aws-cdk/aws-amplify-alpha/scripts/airlift-custom-resource-handlers.sh
@@ -4,20 +4,32 @@ scriptdir=$(cd $(dirname $0) && pwd)
 customresourcedir=$(node -p "path.dirname(require.resolve('@aws-cdk/custom-resource-handlers/package.json'))")
 awscdklibdir=${scriptdir}/..
 
-list_custom_resources() {
-  for file in $customresourcedir/dist/aws-amplify-alpha/*/index.js; do
-    echo $file | rev | cut -d "/" -f 2-4 | rev
+function airlift() {
+  mkdir -p $awscdklibdir/custom-resource-handlers/$1
+  cp $customresourcedir/$2 $awscdklibdir/custom-resource-handlers/$1
+}
+
+function recurse() {
+  local dir=$1
+
+  for file in $dir/*; do
+    if [ -f $file ]; then
+      case $file in
+        $customresourcedir/dist/aws-amplify-alpha/*.generated.ts)
+          cr=$(echo $file | rev | cut -d "/" -f 2-3 | rev)
+          airlift $cr $cr/*.generated.ts
+          ;;
+        $customresourcedir/dist/aws-amplify-alpha/*/index.*)
+          cr=$(echo $file | rev | cut -d "/" -f 2-4 | rev)
+          airlift $cr $cr/index.*
+          ;;
+      esac
+    fi
+
+    if [ -d $file ]; then
+      recurse $file
+    fi
   done
 }
 
-customresources=$(list_custom_resources)
-
-echo $customresources
-
-cd $awscdklibdir
-mkdir -p $awscdklibdir/custom-resource-handlers
-
-for cr in $customresources; do
-  mkdir -p $awscdklibdir/custom-resource-handlers/$cr
-  cp $customresourcedir/$cr/index.js $awscdklibdir/custom-resource-handlers/$cr
-done
+recurse $customresourcedir/dist/aws-amplify-alpha
