0c412cb9d30f313f861a72e94b84ff21b5c10468
Janardhan (Janny) Molumuri <mjanardhan@gmail.com>
2024-05-28 14:24:49 +0000

    Update default python version

--- packages/@aws-cdk/aws-glue-alpha/lib/jobs/pyspark-etl-job.ts
+++ packages/@aws-cdk/aws-glue-alpha/lib/jobs/pyspark-etl-job.ts
@@ -111,7 +111,7 @@ export class PySparkEtlJob extends Job {
       command: {
         name: JobType.ETL,
         scriptLocation: this.codeS3ObjectUrl(props.script),
-        pythonVersion: PythonVersion.THREE_NINE,
+        pythonVersion: PythonVersion.THREE,
       },
       glueVersion: props.glueVersion ? props.glueVersion : GlueVersion.V4_0,
       workerType: props.workerType ? props.workerType : WorkerType.G_2X,
