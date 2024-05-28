17232c85791223614ca3c70ceb754ba414c8583e
Jakob Berg <bergjak@amazon.com>
2024-05-28 14:38:04 -0400

    nice comment

--- packages/@aws-cdk/cloudformation-diff/lib/diff/template-and-changeset-diff-merger.ts
+++ packages/@aws-cdk/cloudformation-diff/lib/diff/template-and-changeset-diff-merger.ts
@@ -88,6 +88,11 @@ export class TemplateAndChangeSetDiffMerger {
     return changeSetResources;
   }
 
+  /**
+   * This is us writing over the "ChangeImpact" that was computed from the template difference, and instead using the ChangeImpact that is included from the ChangeSet.
+   * Using the ChangeSet ChangeImpact is more accurate. The ChangeImpact tells us what the consequence is of changing the field. If changing the field causes resource
+   * replacement (e.g., changing the name of an IAM role requires deleting and replacing the role), then ChangeImpact is "Always".
+   */
   public overrideDiffResourceChangeImpactWithChangeSetChangeImpact(logicalId: string, change: types.ResourceDifference) {
     // resourceType getter throws an error if resourceTypeChanged
     if ((change.resourceTypeChanged === true) || change.resourceType?.includes('AWS::Serverless')) {
