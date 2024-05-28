5413e857e00032c577ed2239616b66646c804965
Janardhan (Janny) Molumuri <mjanardhan@gmail.com>
2024-05-28 15:10:53 +0000

    ETL jobs & tests

--- packages/@aws-cdk/aws-glue-alpha/lib/jobs/pyspark-etl-job.ts
+++ packages/@aws-cdk/aws-glue-alpha/lib/jobs/pyspark-etl-job.ts
@@ -115,7 +115,7 @@ export class PySparkEtlJob extends Job {
       },
       glueVersion: props.glueVersion ? props.glueVersion : GlueVersion.V4_0,
       workerType: props.workerType ? props.workerType : WorkerType.G_2X,
-      numberOfWorkers: props.numberOrWorkers,
+      numberOfWorkers: props.numberOrWorkers ? props.numberOrWorkers : 10,
       maxRetries: props.maxRetries,
       executionProperty: props.maxConcurrentRuns ? { maxConcurrentRuns: props.maxConcurrentRuns } : undefined,
       //notificationProperty: props.notifyDelayAfter ? { notifyDelayAfter: props.notifyDelayAfter.toMinutes() } : undefined,
