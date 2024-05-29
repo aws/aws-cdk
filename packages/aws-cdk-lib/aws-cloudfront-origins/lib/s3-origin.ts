bbf9fd1011a5ac29b7362a4c9952337347e075d8
gracelu0 <grace.r.luo@gmail.com>
2024-05-29 15:05:22 -0700

    refactor

--- packages/aws-cdk-lib/aws-cloudfront-origins/lib/s3-origin.ts
+++ packages/aws-cdk-lib/aws-cloudfront-origins/lib/s3-origin.ts
@@ -3,7 +3,7 @@ import { HttpOrigin } from './http-origin';
 import * as cloudfront from '../../aws-cloudfront';
 import * as iam from '../../aws-iam';
 import * as s3 from '../../aws-s3';
-import { Stack, Names, FeatureFlags, Aws, Lazy, CustomResource, Annotations } from '../../core';
+import { Stack, Names, FeatureFlags, Aws, CustomResource, Annotations } from '../../core';
 import { S3OriginAccessControlBucketPolicyProvider } from '../../custom-resource-handlers/dist/aws-cloudfront-origins/s3-origin-access-control-bucket-policy-provider.generated';
 import { S3OriginAccessControlKeyPolicyProvider } from '../../custom-resource-handlers/dist/aws-cloudfront-origins/s3-origin-access-control-key-policy-provider.generated';
 import * as cxapi from '../../cx-api';
@@ -39,11 +39,20 @@ export class S3Origin implements cloudfront.IOrigin {
   private readonly origin: cloudfront.IOrigin;
 
   constructor(bucket: s3.IBucket, props: S3OriginProps = {}) {
-    this.origin = bucket.isWebsite ?
-      new HttpOrigin(bucket.bucketWebsiteDomainName, {
+    if (props.originAccessControl && props.originAccessIdentity) {
+      throw new Error('Only one of originAccessControl or originAccessIdentity can be specified for an origin.');
+    }
+
+    if (bucket.isWebsite) {
+      this.origin = new HttpOrigin(bucket.bucketWebsiteDomainName, {
         protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY, // S3 only supports HTTP for website buckets
         ...props,
-      }) : new S3BucketOrigin(bucket, props);
+      });
+    } else if (props.originAccessIdentity || !FeatureFlags.of(bucket.stack).isEnabled(cxapi.CLOUDFRONT_USE_ORIGIN_ACCESS_CONTROL)) {
+      this.origin = new S3BucketOrigin(bucket, props);
+    } else {
+      this.origin = new S3BucketOacOrigin(bucket, props);
+    }
   }
 
   public bind(scope: Construct, options: cloudfront.OriginBindOptions): cloudfront.OriginBindConfig {
@@ -67,40 +76,6 @@ class S3BucketOacOrigin extends cloudfront.OriginBase {
   }
 
   public bind(scope: Construct, options: cloudfront.OriginBindOptions): cloudfront.OriginBindConfig {
-    if (!this.originAccessControl) {
-      this.originAccessControl = new cloudfront.OriginAccessControl(scope, options.originId);
-    }
-    return super.bind(scope, options);
-  }
-
-  /**
-   * If you're using origin access control (OAC) instead of origin access identity, specify an empty `OriginAccessIdentity` element.
-   * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-distribution-s3originconfig.html#cfn-cloudfront-distribution-s3originconfig-originaccessidentity
-   */
-  protected renderS3OriginConfig(): cloudfront.CfnDistribution.S3OriginConfigProperty | undefined {
-    return { originAccessIdentity: '' };
-  }
-}
-
-/**
- * An Origin specific to a S3 bucket (not configured for website hosting).
- *
- * Contains additional logic around bucket permissions and origin access identities.
- */
-class S3BucketOrigin extends cloudfront.OriginBase {
-  private originAccessIdentity?: cloudfront.IOriginAccessIdentity;
-  private originAccessControl?: cloudfront.IOriginAccessControl;
-
-  constructor(private readonly bucket: s3.IBucket, props: S3OriginProps) {
-    super(bucket.bucketRegionalDomainName, props);
-    if (props.originAccessControl && props.originAccessIdentity) {
-      throw new Error('Only one of originAccessControl or originAccessIdentity can be specified for an origin.');
-    }
-    this.originAccessControl = props.originAccessControl;
-    this.originAccessIdentity = props.originAccessIdentity;
-  }
-
-  private bindOAC(scope: Construct, options: cloudfront.OriginBindOptions): cloudfront.OriginBindConfig {
     if (!this.originAccessControl) {
       // Create a new origin access control if not specified
       this.originAccessControl = new cloudfront.OriginAccessControl(scope, 'S3OriginAccessControl');
@@ -183,10 +158,33 @@ class S3BucketOrigin extends cloudfront.OriginBase {
     };
   }
 
+  /**
+   * If you're using origin access control (OAC) instead of origin access identity, specify an empty `OriginAccessIdentity` element.
+   * @see http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-distribution-s3originconfig.html#cfn-cloudfront-distribution-s3originconfig-originaccessidentity
+   */
+  protected renderS3OriginConfig(): cloudfront.CfnDistribution.S3OriginConfigProperty | undefined {
+    return { originAccessIdentity: '' };
+  }
+}
+
+/**
+ * An Origin specific to a S3 bucket (not configured for website hosting).
+ *
+ * Contains additional logic around bucket permissions and origin access identities.
+ */
+class S3BucketOrigin extends cloudfront.OriginBase {
+  private originAccessIdentity!: cloudfront.IOriginAccessIdentity;
+
+  constructor(private readonly bucket: s3.IBucket, { originAccessIdentity, ...props }: S3OriginProps) {
+    super(bucket.bucketRegionalDomainName, props);
+
+    if (originAccessIdentity) {
+      this.originAccessIdentity = originAccessIdentity;
+    }
+  }
+
   public bind(scope: Construct, options: cloudfront.OriginBindOptions): cloudfront.OriginBindConfig {
-    if (this.originAccessControl || FeatureFlags.of(scope).isEnabled(cxapi.CLOUDFRONT_USE_ORIGIN_ACCESS_CONTROL)) {
-      return this.bindOAC(scope, options);
-    } else if (!this.originAccessIdentity && !(FeatureFlags.of(scope).isEnabled(cxapi.CLOUDFRONT_USE_ORIGIN_ACCESS_CONTROL))) {
+    if (!this.originAccessIdentity) {
       // Using a bucket from another stack creates a cyclic reference with
       // the bucket taking a dependency on the generated S3CanonicalUserId for the grant principal,
       // and the distribution having a dependency on the bucket's domain name.
@@ -215,9 +213,6 @@ class S3BucketOrigin extends cloudfront.OriginBase {
   }
 
   protected renderS3OriginConfig(): cloudfront.CfnDistribution.S3OriginConfigProperty | undefined {
-    if (this.originAccessIdentity) {
-      return { originAccessIdentity: `origin-access-identity/cloudfront/${this.originAccessIdentity.originAccessIdentityId}` };
-    }
-    return { originAccessIdentity: '' };
+    return { originAccessIdentity: `origin-access-identity/cloudfront/${this.originAccessIdentity.originAccessIdentityId}` };
   }
 }
