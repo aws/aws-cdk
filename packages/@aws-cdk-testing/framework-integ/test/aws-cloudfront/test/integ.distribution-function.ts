c0a105102002df5f6de466528b2e7f6e8ac06032
Leonardo Gama <leogama@amazon.com>
2024-05-30 11:46:56 -0700

    Update integ tests

--- packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront/test/integ.distribution-function.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront/test/integ.distribution-function.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { TestOrigin } from './test-origin';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'integ-distribution-function', { env: { region: 'eu-west-1' } });
@@ -22,5 +23,9 @@ new cloudfront.Distribution(stack, 'Dist', {
 
 new cdk.CfnOutput(stack, 'FunctionArn', { value: cfFunction.functionArn });
 new cdk.CfnOutput(stack, 'FunctionStage', { value: cfFunction.functionStage });
+
+new IntegTest(app, 'DistributionFunction', {
+  testCases: [stack],
+});
 
 app.synth();
