672dca5a56046757914e1dca20314012d9615727
Tim Brust <tim.brust@accenture.com>
2024-05-31 12:25:30 +0200

    feat(opensearch): adds OpenSearch 2.13 engine version

--- packages/@aws-cdk-testing/framework-integ/test/aws-opensearchservice/test/integ.opensearch.min.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-opensearchservice/test/integ.opensearch.min.ts
@@ -9,7 +9,7 @@ class TestStack extends Stack {
 
     // deploy the latest opensearch domain with minimal configuration
     const domainProps: opensearch.DomainProps = {
-      version: opensearch.EngineVersion.OPENSEARCH_2_11,
+      version: opensearch.EngineVersion.OPENSEARCH_2_13,
       removalPolicy: RemovalPolicy.DESTROY,
       capacity: {
         multiAzWithStandbyEnabled: false,
--- packages/aws-cdk-lib/aws-opensearchservice/lib/version.ts
+++ packages/aws-cdk-lib/aws-opensearchservice/lib/version.ts
@@ -90,14 +90,17 @@ export class EngineVersion {
   /** AWS OpenSearch 2.9 */
   public static readonly OPENSEARCH_2_9 = EngineVersion.openSearch('2.9');
 
-  /**
-   * AWS OpenSearch 2.10
-   * @deprecated use latest version of the OpenSearch engine
-   **/
+  /** AWS OpenSearch 2.10 */
   public static readonly OPENSEARCH_2_10 = EngineVersion.openSearch('2.10');
 
-  /** AWS OpenSearch 2.11 */
+  /**
+   * AWS OpenSearch 2.11
+   * @deprecated use latest version of the OpenSearch engine
+   **/
   public static readonly OPENSEARCH_2_11 = EngineVersion.openSearch('2.11');
+
+  /** AWS OpenSearch 2.13 */
+  public static readonly OPENSEARCH_2_13 = EngineVersion.openSearch('2.13');
 
   /**
    * Custom ElasticSearch version
