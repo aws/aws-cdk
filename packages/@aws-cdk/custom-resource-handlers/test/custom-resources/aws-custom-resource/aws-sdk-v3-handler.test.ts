eed768f0417b43286c4aac62eaadb933217bbe83
Francis <colifran@amazon.com>
2024-06-01 03:48:33 -0700

    add unit tests
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk/custom-resource-handlers/test/custom-resources/aws-custom-resource/aws-sdk-v3-handler.test.ts
+++ packages/@aws-cdk/custom-resource-handlers/test/custom-resources/aws-custom-resource/aws-sdk-v3-handler.test.ts
@@ -452,6 +452,38 @@ test('can specify apiVersion and region', async () => {
   expect(request.isDone()).toBeTruthy();
 });
 
+test('can specify logApiResponseData as false', async () => {
+  s3MockClient.on(S3.GetObjectCommand).resolves({});
+
+  const event: AWSLambda.CloudFormationCustomResourceCreateEvent = {
+    ...eventCommon,
+    RequestType: 'Create',
+    ResourceProperties: {
+      ServiceToken: 'token',
+      Create: JSON.stringify({
+        service: '@aws-sdk/client-s3',
+        action: 'GetObjectCommand',
+        parameters: {
+          Bucket: 'my-bucket',
+          Key: 'key',
+        },
+        physicalResourceId: { id: 'id' },
+        logApiResponseData: false,
+      } satisfies AwsSdkCall),
+    },
+  };
+
+  const request = createRequest(body =>
+    body.Status === 'SUCCESS' &&
+    body.Data!.apiVersion === '2010-03-31' &&
+    body.Data!.region === 'eu-west-1',
+  );
+
+  await handler(event, {} as AWSLambda.Context);
+
+  expect(request.isDone()).toBeTruthy();
+});
+
 test('installs the latest SDK', async () => {
   const tmpPath = '/tmp/node_modules/@aws-sdk/client-s3';
 
