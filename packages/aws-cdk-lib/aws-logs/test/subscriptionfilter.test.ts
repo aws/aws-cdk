d553627b02a83cafae1feb6ce2319757be637041
mazyu36 <yiyth.fcb6@gmail.com>
2024-06-04 05:27:08 +0900

    feat(logs): add distribution property to the Subscription class (#30423)
    ### Issue # (if applicable)
    
    Closes #30422
    
    ### Reason for this change
    Missing Property in the Subscription class.
    
    
    
    ### Description of changes
    Add destination property to the Subscription class.
    
    
    
    ### Description of how you validated changes
    Add unit tests and integ tests.
    
    
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-logs/test/subscriptionfilter.test.ts
+++ packages/aws-cdk-lib/aws-logs/test/subscriptionfilter.test.ts
@@ -1,7 +1,9 @@
 import { Construct } from 'constructs';
 import { Template } from '../../assertions';
+import { Stream } from '../../aws-kinesis';
+import { KinesisDestination } from '../../aws-logs-destinations';
 import { Stack } from '../../core';
-import { FilterPattern, ILogGroup, ILogSubscriptionDestination, LogGroup, SubscriptionFilter } from '../lib';
+import { Distribution, FilterPattern, ILogGroup, ILogSubscriptionDestination, LogGroup, SubscriptionFilter } from '../lib';
 
 describe('subscription filter', () => {
   test('trivial instantiation', () => {
@@ -44,6 +46,43 @@ describe('subscription filter', () => {
       LogGroupName: { Ref: 'LogGroupF5B46931' },
       FilterName: 'CustomSubscriptionFilterName',
     });
+  });
+
+  test('subscription filter with KinesisDestination can have distribution set.', () => {
+    // GIVEN
+    const stack = new Stack();
+    const logGroup = new LogGroup(stack, 'LogGroup');
+
+    const stream = new Stream(stack, 'Stream');
+
+    // WHEN
+    new SubscriptionFilter(stack, 'Subscription', {
+      logGroup,
+      destination: new KinesisDestination(stream),
+      filterPattern: FilterPattern.literal('some pattern'),
+      filterName: 'CustomSubscriptionFilterName',
+      distribution: Distribution.RANDOM,
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::Logs::SubscriptionFilter', {
+      Distribution: 'Random',
+    });
+  });
+
+  test('subscription filter with non-KinesisDestination can not have distribution set.', () => {
+    const stack = new Stack();
+    const logGroup = new LogGroup(stack, 'LogGroup');
+
+    expect(() => {
+      new SubscriptionFilter(stack, 'Subscription', {
+        logGroup,
+        destination: new FakeDestination(),
+        filterPattern: FilterPattern.literal('some pattern'),
+        filterName: 'CustomSubscriptionFilterName',
+        distribution: Distribution.RANDOM,
+      });
+    }).toThrow('distribution property can only be used with KinesisDestination.');
   });
 });
 
