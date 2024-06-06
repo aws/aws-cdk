31f4cc8c5bcc447880331a48e7c1a022c6000b0b
Francis <colifran@amazon.com>
2024-06-06 14:05:01 -0700

    docstrings for build script
    Signed-off-by: Francis <colifran@amazon.com>

--- packages/@aws-cdk/aws-amplify-alpha/scripts/airlift-custom-resource-handlers.sh
+++ packages/@aws-cdk/aws-amplify-alpha/scripts/airlift-custom-resource-handlers.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# This is a pre-build script that is run prior to building the aws-amplify-alpha module.
+# aws-amplify-alpha has a dev dependency on custom-resource-handlers which triggers that
+# module to build first. Once complete, this script will run to move all generated custom
+# resource code into a dist directory directly in aws-amplify-alpha.
+
 scriptdir=$(cd $(dirname $0) && pwd)
 customresourcedir=$(node -p "path.dirname(require.resolve('@aws-cdk/custom-resource-handlers/package.json'))")
 awscdklibdir=${scriptdir}/..
 
+# copy code generated custom resource files into aws-amplify-alpha/dist from
+# custom-resource-handlers/dist/aws-amplify-alpha
 function airlift() {
   mkdir -p $awscdklibdir/custom-resource-handlers/$1
   cp $customresourcedir/$2 $awscdklibdir/custom-resource-handlers/$1
 }
 
+# recursively iterates over directorys and files in custom-resource-handlers/dist/aws-amplify-alpha
+# and copys them into aws-amplify-alpha/dist
 function recurse() {
   local dir=$1
 
