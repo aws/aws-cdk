a992da9777c8addc23001d8508e388bc44e7df14
Jimmy Gaussen <jimmy.gaussen@gmail.com>
2024-06-10 16:12:32 +0200

    fix: unit test

--- packages/@aws-cdk/integ-tests-alpha/test/assertions/providers/lambda-handler/http.test.ts
+++ packages/@aws-cdk/integ-tests-alpha/test/assertions/providers/lambda-handler/http.test.ts
@@ -113,7 +113,7 @@ describe('HttpHandler', () => {
     const response = await processEvent({ parameters: { url: 'x' } });
 
     // THEN
-    expect(response.apiCallResponse).toEqual({ body: { key: 'value' } });
+    expect(response.apiCallResponse).toMatchObject({ body: { key: 'value' } });
   });
 
   test('Non-JSON is not parsed', async () => {
@@ -126,7 +126,7 @@ describe('HttpHandler', () => {
     const response = await processEvent({ parameters: { url: 'x' } });
 
     // THEN
-    expect(response.apiCallResponse).toEqual({ body: 'this is a string' });
+    expect(response.apiCallResponse).toMatchObject({ body: 'this is a string' });
   });
 });
 
