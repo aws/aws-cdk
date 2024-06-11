bdcf3c5a621c7b66bbbbf13b36857aaddaf8f13c
shikha372 <shikhaaggarwal1995@gmail.com>
2024-06-11 14:28:36 -0700

    barebone implementation of subnetV2

--- packages/@aws-cdk/aws-vpcv2-alpha/lib/subnet-v2.ts
+++ packages/@aws-cdk/aws-vpcv2-alpha/lib/subnet-v2.ts
@@ -1,63 +1,143 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 // /* eslint-disable @typescript-eslint/member-ordering */
-// import { Stack, ResourceEnvironment, RemovalPolicy, Resource } from 'aws-cdk-lib';
-// import { CfnVPCCidrBlock, INetworkAcl, IRouteTable, ISubnet } from 'aws-cdk-lib/aws-ec2';
-// import { IDependable, Node } from 'constructs';
-// import { IVpcV2 } from './vpc-v2';
+import { Resource, Names } from 'aws-cdk-lib';
+import { CfnRouteTable, CfnSubnet, CfnSubnetRouteTableAssociation, INetworkAcl, IRouteTable, ISubnet, NetworkAcl, SubnetNetworkAclAssociation } from 'aws-cdk-lib/aws-ec2';
+import { Construct, DependencyGroup, IDependable } from 'constructs';
+import { IVpcV2 } from './vpc-v2';
 
-// export interface SubnetPropsV2 {
-// /**
-//  * VPC Prop
-//  */
-//   vpc: IVpcV2;
+export interface ICidr {
+  readonly cidr: string;
+}
 
-//   /**
-//    * custom CIDR range
-//    * TODO: modify to Ipv4cidr class
-//    */
-//   cidrBlock: CfnVPCCidrBlock;
+export interface cidrBlockProps {
+  readonly cidrBlock: string;
+}
 
-//   /**
-//    * Custom AZ
-//    */
-//   availabilityZone: string;
+export class Ipv4Cidr implements ICidr {
 
-//   /**
-//    * Isolated subnet or not
-//    * @default true
-//    */
-//   isIsolated?: Boolean;
+  public readonly cidr: string;
+  constructor(props: cidrBlockProps ) {
+    this.cidr = props.cidrBlock;
+  }
+}
 
-// }
+export class Ipv6Cidr implements ICidr {
 
-// export class SubnetV2 extends Resource implements ISubnet {
-//   readonly availabilityZone: string;
-//   readonly subnetId: string;
-//   readonly internetConnectivityEstablished: IDependable;
-//   readonly ipv4CidrBlock: string;
-//   readonly routeTable: IRouteTable;
-//   associateNetworkAcl(id: string, acl: INetworkAcl): void {
-//     throw new Error('Method not implemented.');
-//   }
-//   readonly stack: Stack;
-//   readonly env: ResourceEnvironment;
-//   applyRemovalPolicy(policy: RemovalPolicy): void {
-//     throw new Error('Method not implemented.');
-//   }
-//   readonly node: Node;
+  public readonly cidr: string;
+  constructor(props: cidrBlockProps ) {
+    this.cidr = props.cidrBlock;
+  }
+}
 
-//   constructor(scope: Stack, id: string, props: any) {
-//     super(scope, id);
-//     this.stack = scope;
-//     this.node = Node.of(this);
-//     this.subnetId = id;
-//     this.availabilityZone = props.availabilityZone;
-//     this.ipv4CidrBlock = props.ipv4CidrBlock;
-//     this.routeTable = props.routeTable;
-//     this.internetConnectivityEstablished = props.internetConnectivityEstablished;
-//     this.env = {
-//       account: scope.account,
-//       region: scope.region,
-//     };
-//   }
+export interface SubnetPropsV2 {
+/**
+ * VPC Prop
+ */
+  vpc: IVpcV2;
 
-// }
+  /**
+   * custom CIDR range
+   * TODO: modify to Ipv4cidr class
+   */
+  cidrBlock: ICidr;
+
+  /**
+   * Custom AZ
+   */
+  availabilityZone: string;
+
+  /**
+   * Isolated subnet or not
+   * @default true
+   */
+  isIsolated?: Boolean;
+
+}
+
+export class SubnetV2 extends Resource implements ISubnet {
+
+  /**
+   * The Availability Zone the subnet is located in
+   */
+  public readonly availabilityZone: string;
+
+  /**
+   * The subnetId for this particular subnet
+   * @attribute
+   */
+  public readonly subnetId: string;
+
+  /**
+   * Dependable that can be depended upon to force internet connectivity established on the VPC
+   */
+  public readonly internetConnectivityEstablished: IDependable;
+
+  private readonly _internetConnectivityEstablished = new DependencyGroup();
+
+  /**
+   * The IPv4 CIDR block for this subnet
+   */
+  public readonly ipv4CidrBlock: string;
+
+  /**
+   * The route table for this subnet
+   */
+  public readonly routeTable: IRouteTable;
+
+  /**
+   *
+   * @param scope
+   * @param id
+   * @param props
+   */
+  private _networkAcl: INetworkAcl;
+
+  constructor(scope: Construct, id: string, props: SubnetPropsV2) {
+    super(scope, id);
+
+    const subnet = new CfnSubnet(this, 'Subnet', {
+      vpcId: props.vpc.vpcId,
+      cidrBlock: props.cidrBlock.cidr,
+      ipv6CidrBlock: props.cidrBlock.cidr,
+      availabilityZone: props.availabilityZone,
+    });
+
+    this.ipv4CidrBlock = subnet.cidrBlock?? '';
+    this.subnetId = subnet.ref;
+    const table = new CfnRouteTable(this, 'RouteTable', {
+      vpcId: props.vpc.vpcId,
+    });
+    this.routeTable = { routeTableId: table.ref };
+    this.availabilityZone = props.availabilityZone;
+
+    this._networkAcl = NetworkAcl.fromNetworkAclId(this, 'Acl', subnet.attrNetworkAclAssociationId);
+
+    const routeAssoc = new CfnSubnetRouteTableAssociation(this, 'RouteTableAssociation', {
+      subnetId: this.subnetId,
+      routeTableId: table.ref,
+    });
+    this._internetConnectivityEstablished.add(routeAssoc);
+
+    this.internetConnectivityEstablished = this._internetConnectivityEstablished;
+  }
+  /**
+   * Associate a Network ACL with this subnet
+   *
+   * @param acl The Network ACL to associate
+   */
+  public associateNetworkAcl(id: string, networkAcl: INetworkAcl) {
+    this._networkAcl = networkAcl;
+
+    const scope = networkAcl instanceof Construct ? networkAcl : this;
+    const other = networkAcl instanceof Construct ? this : networkAcl;
+    new SubnetNetworkAclAssociation(scope, id + Names.nodeUniqueId(other.node), {
+      networkAcl,
+      subnet: this,
+    });
+  }
+
+  public get networkAcl(): INetworkAcl {
+    return this._networkAcl;
+  }
+
+}
