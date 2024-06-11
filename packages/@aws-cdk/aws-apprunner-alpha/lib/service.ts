2d4e64e0be083a7bc9c561a6d95239189d018fdd
mazyu36 <yiyth.fcb6@gmail.com>
2024-06-11 09:04:14 +0900

    feat(apprunner): add ipAddressType property to the Service class (#30351)
    ### Issue # (if applicable)
    
    N/A
    
    ### Reason for this change
    AppRunner supported Dual Stack.
    
    https://aws.amazon.com/about-aws/whats-new/2023/11/aws-app-runner-supports-ipv6-public-inbound-traffic/?nc1=h_ls
    
    But current L2 Construct (alpha module) does not support it.
    
    
    ### Description of changes
    Add ipAddressType property to the Service class
    
    
    
    ### Description of how you validated changes
    Add unit tests and integ tests.
    
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-apprunner-alpha/lib/service.ts
+++ packages/@aws-cdk/aws-apprunner-alpha/lib/service.ts
@@ -723,6 +723,13 @@ export interface ServiceProps {
    * @default - Use an AWS managed key
    */
   readonly kmsKey?: kms.IKey;
+
+  /**
+   * The IP address type for your incoming public network configuration.
+   *
+   * @default - IpAddressType.IPV4
+   */
+  readonly ipAddressType?: IpAddressType;
 }
 
 /**
@@ -1005,6 +1012,21 @@ export class HealthCheck {
 }
 
 /**
+ * The IP address type for your incoming public network configuration.
+ */
+export enum IpAddressType {
+  /**
+   * IPV4
+   */
+  IPV4 = 'IPV4',
+
+  /**
+   * DUAL_STACK
+   */
+  DUAL_STACK = 'DUAL_STACK',
+}
+
+/**
  * Attributes for the App Runner Service
  */
 export interface ServiceAttributes {
@@ -1255,6 +1277,7 @@ export class Service extends cdk.Resource implements iam.IGrantable {
           egressType: this.props.vpcConnector ? 'VPC' : 'DEFAULT',
           vpcConnectorArn: this.props.vpcConnector?.vpcConnectorArn,
         },
+        ipAddressType: this.props.ipAddressType,
       },
       healthCheckConfiguration: this.props.healthCheck ?
         this.props.healthCheck.bind() :
