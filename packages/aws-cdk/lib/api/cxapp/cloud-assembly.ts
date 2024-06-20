2c9884f57424e665692f846efcebd2ff81ace924
Calvin Combs <comcalvi@amazon.com>
2024-06-20 11:12:14 -0700

    asm

--- packages/aws-cdk/lib/api/cxapp/cloud-assembly.ts
+++ packages/aws-cdk/lib/api/cxapp/cloud-assembly.ts
@@ -106,8 +106,17 @@ export class CloudAssembly {
     const allTopLevel = selector.allTopLevel ?? false;
     const patterns = sanitizePatterns(selector.patterns);
 
+    if (patterns.includes('Test-Stack-E')) {
+      // eslint-disable-next-line no-console
+      console.error(`WOW found stacks: ${stacks.map(stack => stack.stackName)}. They have heirarchical IDs: ${stacks.map(stack => stack.hierarchicalId)}`);
+    }
+
     if (stacks.length === 0) {
       if (options.ignoreNoStacks) {
+        if (patterns.includes('Test-Stack-E')) {
+          // eslint-disable-next-line no-console
+          console.error('no stacks found!');
+        }
         return new StackCollection(this, []);
       }
       throw new Error('This app contains no stacks');
@@ -116,10 +125,6 @@ export class CloudAssembly {
     if (allTopLevel) {
       return this.selectTopLevelStacks(stacks, topLevelStacks, options.extend);
     } else if (patterns.length > 0) {
-      if (patterns.includes('Test-Stack-E')) {
-        // eslint-disable-next-line no-console
-        console.error(`WOW found stacks: ${stacks.map(stack => stack.stackName)}. They have heirarchical IDs: ${stacks.map(stack => stack.hierarchicalId)}`);
-      }
       return this.selectMatchingStacks(stacks, patterns, options.extend);
     } else {
       return this.selectDefaultStacks(stacks, topLevelStacks, options.defaultBehavior);
