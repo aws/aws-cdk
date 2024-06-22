036c94ee3310d97af4937bc46803bbc5a13ce25c
Jim Nicholls <jim.nicholls@sydney.edu.au>
2024-06-22 17:49:03 +1000

    Test find/replace in CloudFront function code.

--- packages/aws-cdk-lib/aws-cloudfront/test/function.test.ts
+++ packages/aws-cdk-lib/aws-cloudfront/test/function.test.ts
@@ -294,4 +294,105 @@ describe('CloudFront Function', () => {
       });
     });
   });
+
+  describe('find/replace in function code', () => {
+    test('no-op when no instructions', () => {
+      const stack = new Stack();
+      new Function(stack, 'TestFn', {
+        code: FunctionCode.fromInline('code', {
+          findReplace: [],
+        }),
+      });
+      Template.fromStack(stack).hasResourceProperties('AWS::CloudFront::Function', {
+        FunctionCode: 'code',
+      });
+    });
+
+    test('replace first only', () => {
+      const stack = new Stack();
+      new Function(stack, 'TestFn', {
+        code: FunctionCode.fromInline('Some text replaced with other text', {
+          findReplace: [{ find: 'text', replace: '12', all: false }],
+        }),
+      });
+      Template.fromStack(stack).hasResourceProperties('AWS::CloudFront::Function', {
+        FunctionCode: 'Some 12 replaced with other text',
+      });
+    });
+
+    test('replace all', () => {
+      const stack = new Stack();
+      new Function(stack, 'TestFn', {
+        code: FunctionCode.fromInline('Some text replaced with other text', {
+          findReplace: [{ find: 'text', replace: '12', all: true }],
+        }),
+      });
+      Template.fromStack(stack).hasResourceProperties('AWS::CloudFront::Function', {
+        FunctionCode: 'Some 12 replaced with other 12',
+      });
+    });
+
+    test('replace first by default', () => {
+      const stack = new Stack();
+      new Function(stack, 'TestFn', {
+        code: FunctionCode.fromInline('abacda', {
+          findReplace: [{ find: 'a', replace: 'A' }],
+        }),
+      });
+      Template.fromStack(stack).hasResourceProperties('AWS::CloudFront::Function', {
+        FunctionCode: 'Abacda',
+      });
+    });
+
+    test('replace with multiple instructions', () => {
+      const stack = new Stack();
+      new Function(stack, 'TestFn', {
+        code: FunctionCode.fromInline('%TEXT%', {
+          findReplace: [
+            { find: '%', replace: '${' },
+            { find: '%', replace: '}' },
+            { find: 'T', replace: 't', all: true },
+            { find: '{tEXt}', replace: 'VAR' },
+          ],
+        }),
+      });
+      Template.fromStack(stack).hasResourceProperties('AWS::CloudFront::Function', {
+        FunctionCode: '$VAR',
+      });
+    });
+
+    test('replace with token', () => {
+      const stack = new Stack();
+      const keyValueStore = new KeyValueStore(stack, 'TestStore');
+      new Function(stack, 'TestFn', {
+        code: FunctionCode.fromInline('const kvsId = "%KVS_ID%";', {
+          findReplace: [{ find: '%KVS_ID%', replace: keyValueStore.keyValueStoreId }],
+        }),
+      });
+      Template.fromStack(stack).hasResourceProperties('AWS::CloudFront::Function', {
+        FunctionCode: {
+          'Fn::Join': ['', [
+            'const kvsId = "',
+            { 'Fn::GetAtt': ['TestStore8BB973CF', 'Id'] },
+            '";',
+          ]],
+        },
+      });
+    });
+
+    test('replace from external file', () => {
+      const stack = new Stack();
+      new Function(stack, 'CF2', {
+        code: FunctionCode.fromFile({
+          filePath: path.join(__dirname, 'function-code.js'),
+          findReplace: [
+            { find: 'event.request', replace: "{ body: 'hello world' }" },
+          ],
+        }),
+      });
+      Template.fromStack(stack).hasResourceProperties('AWS::CloudFront::Function', {
+        FunctionCode: "function handler(event) {\n  return { body: 'hello world' };\n}",
+      });
+    });
+  });
 });
