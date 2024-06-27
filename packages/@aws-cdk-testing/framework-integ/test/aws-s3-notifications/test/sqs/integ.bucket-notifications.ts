92a058e01c234961d2a9c1fd4d3e051e94508f39
GZ <yuanhaoz@amazon.com>
2024-06-27 16:11:14 -0700

    revert: fix(s3-notifications): unable to delete the existing S3 event notifications  (under feature flag) (#30699)
    Reverts aws/aws-cdk#30610 due to Feature flags issues

--- packages/@aws-cdk-testing/framework-integ/test/aws-s3-notifications/test/sqs/integ.bucket-notifications.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-s3-notifications/test/sqs/integ.bucket-notifications.ts
@@ -4,11 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as integ from '@aws-cdk/integ-tests-alpha';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 
-const app = new cdk.App({
-  postCliContext: {
-    '@aws-cdk/aws-s3:s3ExistingNotificationsDeleteEnabled': true,
-  },
-});
+const app = new cdk.App();
 
 const stack = new cdk.Stack(app, 'sqs-bucket-notifications');
 
