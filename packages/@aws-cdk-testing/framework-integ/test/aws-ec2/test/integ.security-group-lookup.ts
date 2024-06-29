db6985d38e621d6dd2b3bce7cded79e5e211bd20
John Dukewich <jdukewich@gmail.com>
2024-06-29 15:06:06 -0400

    Integ test updates, still cannot get it to work

--- packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.security-group-lookup.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.security-group-lookup.ts
@@ -1,18 +1,20 @@
+/// !cdk-integ pragma:enable-lookups
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
-import { EC2_RESTRICT_DEFAULT_SECURITY_GROUP } from 'aws-cdk-lib/cx-api';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+const env = {
+  account: process.env.CDK_INTEG_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_INTEG_REGION || process.env.CDK_DEFAULT_REGION,
+};
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'StackWithSg', {
-  env: {
-    region: 'eu-west-1',
-    account: '123456',
-  },
+  env,
 });
-stack.node.setContext(EC2_RESTRICT_DEFAULT_SECURITY_GROUP, false);
 
+/// !show
 const testVpc = new ec2.Vpc(stack, 'MyVpc', {
   vpcName: 'my-vpc-name',
 });
@@ -20,16 +22,18 @@ const testSgA = new ec2.SecurityGroup(stack, 'MySgA', { vpc: testVpc, descriptio
 new ec2.SecurityGroup(stack, 'MySgB', { vpc: testVpc, description: 'my-description' });
 cdk.Tags.of(testSgA).add('myTag', 'my-value');
 
-ec2.SecurityGroup.fromLookupByFilters(stack, 'SgFromLookup', {
+const lookupSg = ec2.SecurityGroup.fromLookupByFilters(stack, 'SgFromLookup', {
   description: 'my-description',
   tags: {
     myTag: ['my-value'],
   },
 });
+lookupSg.node.addDependency(testSgA);
+/// !hide
 
-new IntegTest(app, 'ArchiveTest', {
+new IntegTest(app, 'SgLookupTest', {
   testCases: [stack],
-  enableLookups: true,
 });
+
 app.synth();
 
