f276c6523bc34b744fdd793223a7874abe298668
Benoit Durand <benoit.durand.mail@gmail.com>
2024-07-01 18:48:57 +0200

    fix bootstrap test

--- packages/aws-cdk/test/cdk-toolkit.test.ts
+++ packages/aws-cdk/test/cdk-toolkit.test.ts
@@ -534,13 +534,13 @@ describe('deploy', () => {
       const toolkit = defaultToolkitSetup();
 
       // WHEN
-      await toolkit.bootstrap(['aws://56789/south-pole'], bootstrapper, {});
+      await toolkit.bootstrap(['aws://567890123456/south-pole'], bootstrapper, {});
 
       // THEN
       expect(bootstrapper.bootstrapEnvironment).toHaveBeenCalledWith({
-        account: '56789',
+        account: '567890123456',
         region: 'south-pole',
-        name: 'aws://56789/south-pole',
+        name: 'aws://567890123456/south-pole',
       }, expect.anything(), expect.anything());
       expect(bootstrapper.bootstrapEnvironment).toHaveBeenCalledTimes(1);
     });
