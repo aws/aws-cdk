c368955fe6b84968d1e19fb23f15dc9759d55143
Michael Sambol <sambol.michael@gmail.com>
2024-07-06 15:48:31 -0500

    Retry integration test

--- packages/@aws-cdk/aws-pipes-targets-alpha/test/integ.api-destination.ts
+++ packages/@aws-cdk/aws-pipes-targets-alpha/test/integ.api-destination.ts
@@ -36,7 +36,7 @@ class TestSource implements ISource {
 }
 
 const fn = new lambda.Function(stack, 'ConnectHandler', {
-  runtime: lambda.Runtime.NODEJS_20_X,
+  runtime: lambda.Runtime.NODEJS_18_X,
   handler: 'index.handler',
   code: new lambda.InlineCode('exports.handler = async function(event, context) { console.log(event); return { statusCode: 200, body: "connected" }; };'),
 });
