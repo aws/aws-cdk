ce799f86d0dd10af521e6dfd174b8b1793e5a035
Constantin <jaecktec@users.noreply.github.com>
2024-07-07 12:28:21 +0200

    chore(ecs): add integrationtests for ephemeral storage encryption

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-encrypt-ephemeral-storage.ts
@@ -0,0 +1,15 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as kms from 'aws-cdk-lib/aws-kms';
+import * as integ from '@aws-cdk/integ-tests-alpha';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'aws-ecs-integ');
+const key = new kms.Key(stack, 'key');
+new ecs.Cluster(stack, 'Cluster', { managedStorageConfiguration: { fargateEphemeralStorageKmsKey: key } });
+
+new integ.IntegTest(app, 'aws-ecs-cluster-encrypt-ephemeral-storage', {
+  testCases: [stack],
+});
+
+app.synth();
