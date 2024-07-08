6f6671754dc0e4983b920f50b1986190aa2b2b9a
sakurai-ryo <sakurairyo.10do@gmail.com>
2024-07-09 00:08:56 +0900

    fix: inline doc

--- packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/alb/application-load-balancer.ts
+++ packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/alb/application-load-balancer.ts
@@ -316,7 +316,9 @@ export class ApplicationLoadBalancer extends BaseLoadBalancer implements IApplic
    * Enable connection logging for this load balancer.
    *
    * A region must be specified on the stack containing the load balancer; you cannot enable logging on
-   * environment-agnostic stacks. See https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+   * environment-agnostic stacks.
+   *
+   * @see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
    */
   public logConnectionLogs(bucket: s3.IBucket, prefix?: string) {
     /**
