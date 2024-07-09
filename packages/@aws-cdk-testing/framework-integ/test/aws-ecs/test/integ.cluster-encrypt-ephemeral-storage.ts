37e5d0771e4e243ef2abafa24f003b0d81ba5a15
Constantin <jaecktec@users.noreply.github.com>
2024-07-09 08:32:48 +0200

    chore(ecs): add integration test for named cluster
    rename stack so it is unique for the test

--- packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-encrypt-ephemeral-storage.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-encrypt-ephemeral-storage.ts
@@ -4,9 +4,17 @@ import * as kms from 'aws-cdk-lib/aws-kms';
 import * as integ from '@aws-cdk/integ-tests-alpha';
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'aws-ecs-integ');
-const key = new kms.Key(stack, 'key');
-new ecs.Cluster(stack, 'Cluster', { managedStorageConfiguration: { fargateEphemeralStorageKmsKey: key } });
+const stack = new cdk.Stack(app, 'aws-ecs-ephemmeral-integ');
+const key = new kms.Key(stack, 'key', {
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+});
+new ecs.Cluster(stack, 'NamedCluster', {
+  clusterName: 'cluster-name',
+  managedStorageConfiguration: { fargateEphemeralStorageKmsKey: key },
+});
+new ecs.Cluster(stack, 'Cluster', {
+  managedStorageConfiguration: { fargateEphemeralStorageKmsKey: key },
+});
 
 new integ.IntegTest(app, 'aws-ecs-cluster-encrypt-ephemeral-storage', {
   testCases: [stack],
