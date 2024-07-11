f2a77ac0c57a0eb3ba2f945ae7989de43ff36c6b
Constantin <jaecktec@users.noreply.github.com>
2024-07-11 18:01:20 +0200

    disable vpc-default-handler lambda in ecs ephemal storage integration test

--- packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-encrypt-ephemeral-storage.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-encrypt-ephemeral-storage.ts
@@ -3,21 +3,31 @@ import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as kms from 'aws-cdk-lib/aws-kms';
 import * as integ from '@aws-cdk/integ-tests-alpha';
 
-const app = new cdk.App();
-const stack = new cdk.Stack(app, 'aws-ecs-ephemmeral-integ');
-const key = new kms.Key(stack, 'key', {
+const app = new cdk.App({context: {
+  "@aws-cdk/aws-ec2:restrictDefaultSecurityGroup": false,
+}});
+
+const stackUnnamed = new cdk.Stack(app, 'aws-ecs-ephemmeral-integ');
+const stackNamed = new cdk.Stack(app, 'aws-ecs-ephemmeral-integ-named-cluster');
+
+const keyForUnnamed = new kms.Key(stackUnnamed, 'key', {
   removalPolicy: cdk.RemovalPolicy.DESTROY,
 });
-new ecs.Cluster(stack, 'NamedCluster', {
-  clusterName: 'cluster-name',
-  managedStorageConfiguration: { fargateEphemeralStorageKmsKey: key },
+new ecs.Cluster(stackUnnamed, 'cluster', {
+  managedStorageConfiguration: { fargateEphemeralStorageKmsKey: keyForUnnamed },
 });
-new ecs.Cluster(stack, 'Cluster', {
-  managedStorageConfiguration: { fargateEphemeralStorageKmsKey: key },
+
+const keyForNamed = new kms.Key(stackNamed, 'key', {
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+});
+
+new ecs.Cluster(stackNamed, 'named-cluster', {
+  clusterName: 'cluster-name',
+  managedStorageConfiguration: { fargateEphemeralStorageKmsKey: keyForNamed },
 });
 
 new integ.IntegTest(app, 'aws-ecs-cluster-encrypt-ephemeral-storage', {
-  testCases: [stack],
+  testCases: [stackUnnamed],
 });
 
 app.synth();
