ee67521488bc5f9fa284c0b94f72e180f9190caf
Constantin <jaecktec@users.noreply.github.com>
2024-07-11 18:31:20 +0200

    fix linting problems

--- packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-encrypt-ephemeral-storage.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-encrypt-ephemeral-storage.ts
@@ -3,9 +3,11 @@ import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as kms from 'aws-cdk-lib/aws-kms';
 import * as integ from '@aws-cdk/integ-tests-alpha';
 
-const app = new cdk.App({context: {
-  "@aws-cdk/aws-ec2:restrictDefaultSecurityGroup": false,
-}});
+const app = new cdk.App({
+  context: {
+    '@aws-cdk/aws-ec2:restrictDefaultSecurityGroup': false,
+  },
+});
 
 const stackUnnamed = new cdk.Stack(app, 'aws-ecs-ephemmeral-integ');
 const stackNamed = new cdk.Stack(app, 'aws-ecs-ephemmeral-integ-named-cluster');
