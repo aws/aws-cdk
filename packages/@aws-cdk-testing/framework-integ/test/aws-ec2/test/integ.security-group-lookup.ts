586766852ad781367ebe7423e4104542e5beccbe
John Dukewich <jdukewich@gmail.com>
2024-07-17 16:16:56 -0400

    More coverage on integ test

--- packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.security-group-lookup.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.security-group-lookup.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
-import { EC2_RESTRICT_DEFAULT_SECURITY_GROUP } from 'aws-cdk-lib/cx-api';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 
 const app = new cdk.App();
@@ -11,33 +10,46 @@ const env = {
   region: process.env.CDK_INTEG_REGION || process.env.CDK_DEFAULT_REGION,
 };
 
-class SgLookupStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-    this.node.setContext(EC2_RESTRICT_DEFAULT_SECURITY_GROUP, false);
+// Deploy the security groups to lookup
+const stack = new cdk.Stack(app, 'StackWithSg', { env });
+const testVpc = new ec2.Vpc(stack, 'MyVpc', {
+  vpcName: 'my-vpc-name',
+  ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+  subnetConfiguration: [],
+  natGateways: 0,
+});
+const testSgA = new ec2.SecurityGroup(stack, 'MySgA', { vpc: testVpc });
+cdk.Tags.of(testSgA).add('myTag', 'my-value');
+const testSgB = new ec2.SecurityGroup(stack, 'MySgB', { vpc: testVpc });
+cdk.Tags.of(testSgB).add('myTagKey', 'true');
+new ec2.SecurityGroup(stack, 'MySgC', { vpc: testVpc, description: 'my-description' });
+new ec2.SecurityGroup(stack, 'MySgD', { vpc: testVpc, description: 'ownerId description' });
 
-    const testVpc = new ec2.Vpc(this, 'MyVpc', {
-      vpcName: 'my-vpc-name',
-      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
-      subnetConfiguration: [],
-      natGateways: 0,
-    });
-    const testSgA = new ec2.SecurityGroup(this, 'MySgA', { vpc: testVpc, description: 'my-description' });
-    new ec2.SecurityGroup(this, 'MySgB', { vpc: testVpc, description: 'my-description' });
-    cdk.Tags.of(testSgA).add('myTag', 'my-value');
+// Now perform the lookups
+const lookupStack = new cdk.Stack(app, 'LookupStack', { env });
+lookupStack.addDependency(stack);
+ec2.SecurityGroup.fromLookupByFilters(lookupStack, 'SgFromLookupTags', {
+  tags: {
+    myTag: ['my-value'],
+  },
+});
 
-    ec2.SecurityGroup.fromLookupByFilters(this, 'SgFromLookup', {
-      description: 'my-description',
-      tags: {
-        myTag: ['my-value'],
-      },
-    });
-  }
-}
+ec2.SecurityGroup.fromLookupByFilters(lookupStack, 'SgFromLookupTagKeys', {
+  tagKeys: ['myTagKey'],
+});
 
-const stack = new SgLookupStack(app, 'StackWithSg', { env });
+ec2.SecurityGroup.fromLookupByFilters(lookupStack, 'SgFromLookupDescription', {
+  description: 'my-description',
+});
+
+ec2.SecurityGroup.fromLookupByFilters(lookupStack, 'SgFromLookupOwnerId', {
+  description: 'ownerId description',
+  ownerId: process.env.CDK_INTEG_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+});
+
 new IntegTest(app, 'SgLookupTest', {
   testCases: [stack],
+  enableLookups: true,
 });
 
 app.synth();
