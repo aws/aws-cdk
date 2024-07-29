83d7608567a02a10157b083fcd3237a39f35cca8
watany <76135106+watany-dev@users.noreply.github.com>
2024-07-29 15:16:28 +0000

    adding snapshot

--- packages/@aws-cdk-testing/framework-integ/test/aws-stepfunctions/test/integ.state-machine-disable-xray.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-stepfunctions/test/integ.state-machine-disable-xray.ts
@@ -1,25 +1,14 @@
-import * as iam from 'aws-cdk-lib/aws-iam';
 import * as cdk from 'aws-cdk-lib';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
-/*
- * Stack verification steps:
- *
- * -- aws stepfunctions describe-state-machine --state-machine-arn <stack-output> has a status of `ACTIVE`
- * -- aws iam get-role-policy --role-name <role-name> --policy-name <policy-name> has all actions mapped to respective resources.
- */
+
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'aws-stepfunctions-integ');
 
-// const role = new iam.Role(stack, 'Role', {
-//   assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-// });
-
-const stateMachine = new sfn.StateMachine(stack, 'StateMachine', {
+new sfn.StateMachine(stack, 'StateMachine', {
   comment: 'a super cool state machine',
+  definition: new sfn.Pass(stack, 'StartState'),
   tracingEnabled: false,
 });
 
-// stateMachine.grantRead(role);
-// stateMachine.grant(role, 'states:SendTaskSuccess');
-
-app.synth();
+new IntegTest(app, 'StateMachineDisableXray', { testCases: [stack] });
