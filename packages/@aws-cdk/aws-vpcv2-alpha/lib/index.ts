3230063626c9d713273da83359373df2c07104d3
shikha372 <shikhaaggarwal1995@gmail.com>
2024-07-31 19:25:00 -0700

    correcting go repo name

--- packages/@aws-cdk/aws-vpcv2-alpha/lib/index.ts
+++ packages/@aws-cdk/aws-vpcv2-alpha/lib/index.ts
@@ -3,11 +3,8 @@
 // In general, all files including L2 classes will be listed here,
 // while all files including only utility functions will be omitted from here.
 
-// obviously, the ExampleResource L2 should be exported
 export * from './vpc-v2';
 export * from './ipam';
 export * from './vpc-v2-base';
 export * from './subnet-v2';
 export * from './route';
-
-// notice that private/example-resource-common.ts is not exported!
