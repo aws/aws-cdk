0ac34fb3c99f07d229007757591d66d0c7d574a4
gracelu0 <grassyful@gmail.com>
2024-08-14 16:59:54 -0700

    formatting

--- packages/aws-cdk-lib/aws-cloudfront-origins/lib/s3-static-website-origin.ts
+++ packages/aws-cdk-lib/aws-cloudfront-origins/lib/s3-static-website-origin.ts
@@ -16,9 +16,9 @@ export interface S3StaticWebsiteOriginProps extends HttpOriginProps {
  * An Origin for a S3 bucket configured as a website endpoint
  */
 export class S3StaticWebsiteOrigin extends HttpOrigin {
-  constructor(props: S3StaticWebsiteOriginProps){
+  constructor(props: S3StaticWebsiteOriginProps) {
     super(props.bucket.bucketWebsiteDomainName, {
-      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,// S3 only supports HTTP for website buckets
+      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY, // S3 only supports HTTP for website buckets
       ...props,
     });
   }
