aef7ad607f4bdbdbe1166505b45a12b828f3699b
alanxluc <alanxluc@amazon.com>
2024-08-23 18:09:03 -0400

    feat(codebuild): macOS codebuild support (#31203)
    ### Issue # (if applicable)
    
    Closes #31170.
    
    ### Reason for this change
    
    CodeBuild recently added support for Mac builds.
    https://aws.amazon.com/about-aws/whats-new/2024/08/aws-codebuild-mac-builds/
    
    The CDK can to be extended to support Mac CodeBuild fleets, as well as
    using those fleets in CodeBuild projects.
    
    ### Description of changes
    
    - Added `MAC_ARM` value to EnvironmentType
    [enum](https://github.com/aws/aws-cdk/blob/main/packages/aws-cdk-lib/aws-codebuild/lib/environment-type.ts)
    - Added `MacBuildImage` to
    [project.ts](https://github.com/aws/aws-cdk/blob/main/packages/aws-cdk-lib/aws-codebuild/lib/project.ts)
    
    All design decisions were made by following the previous precedent set
    by `LinuxBuildImage` and `WindowsBuildImage`.
    
    ### Description of how you validated changes
    
    I added unit tests and integration tests for `MacBuildImage`. I ensured
    that the CDK was able to build, and that the output CloudFormation stack
    had the correct resources. I also successfully built the Codebuild
    project that was deployed.
    
    ### Checklist
    - [X] My code adheres to the [CONTRIBUTING
    GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and
    [DESIGN
    GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache-2.0 license*
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/aws-cdk-lib/aws-codebuild/lib/project.ts
+++ packages/aws-cdk-lib/aws-codebuild/lib/project.ts
@@ -1749,8 +1749,9 @@ export interface IBindableBuildImage extends IBuildImage {
 
 /**
  * The options when creating a CodeBuild Docker build image
- * using `LinuxBuildImage.fromDockerRegistry`
- * or `WindowsBuildImage.fromDockerRegistry`.
+ * using `LinuxBuildImage.fromDockerRegistry`,
+ * `WindowsBuildImage.fromDockerRegistry`,
+ * or `MacBuildImage.fromDockerRegistry`
  */
 export interface DockerImageOptions {
   /**
@@ -2237,6 +2238,108 @@ export class WindowsBuildImage implements IBuildImage {
         },
       },
     });
+  }
+}
+
+/**
+ * Construction properties of `MacBuildImage`.
+ * Module-private, as the constructor of `MacBuildImage` is private.
+ */
+interface MacBuildImageProps {
+  readonly imageId: string;
+  readonly imagePullPrincipalType?: ImagePullPrincipalType;
+  readonly secretsManagerCredentials?: secretsmanager.ISecret;
+  readonly repository?: ecr.IRepository;
+}
+
+/**
+ * A CodeBuild image running ARM MacOS.
+ *
+ * This class has a bunch of public constants that represent the most popular images.
+ *
+ * You can also specify a custom image using one of the static methods:
+ *
+ * - MacBuildImage.fromDockerRegistry(image[, { secretsManagerCredentials }])
+ * - MacBuildImage.fromEcrRepository(repo[, tag])
+ * - MacBuildImage.fromAsset(parent, id, props)
+ *
+ *
+ * @see https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
+ */
+export class MacBuildImage implements IBuildImage {
+  /**
+   * Corresponds to the standard CodeBuild image `aws/codebuild/macos-arm-base:14`.
+   */
+  public static readonly BASE_14: IBuildImage = new MacBuildImage({
+    imageId: 'aws/codebuild/macos-arm-base:14',
+    imagePullPrincipalType: ImagePullPrincipalType.CODEBUILD,
+  });
+
+  /**
+   * Makes an ARM MacOS build image from a Docker Hub image.
+   */
+  public static fromDockerRegistry(name: string, options: DockerImageOptions = {}): IBuildImage {
+    return new MacBuildImage({
+      ...options,
+      imageId: name,
+      imagePullPrincipalType: ImagePullPrincipalType.SERVICE_ROLE,
+    });
+  }
+
+  /**
+   * Makes an ARM MacOS build image from an ECR repository.
+   */
+  public static fromEcrRepository(repository: ecr.IRepository, tagOrDigest: string = 'latest'): IBuildImage {
+    return new MacBuildImage({
+      imageId: repository.repositoryUriForTagOrDigest(tagOrDigest),
+      imagePullPrincipalType: ImagePullPrincipalType.SERVICE_ROLE,
+      repository,
+    });
+  }
+
+  /**
+   * Uses an Docker image asset as a ARM MacOS build image.
+   */
+  public static fromAsset(scope: Construct, id: string, props: DockerImageAssetProps): IBuildImage {
+    const asset = new DockerImageAsset(scope, id, props);
+    return new MacBuildImage({
+      imageId: asset.imageUri,
+      imagePullPrincipalType: ImagePullPrincipalType.SERVICE_ROLE,
+      repository: asset.repository,
+    });
+  }
+
+  public readonly type = EnvironmentType.MAC_ARM as string;
+  public readonly defaultComputeType = ComputeType.MEDIUM;
+  public readonly imageId: string;
+  public readonly imagePullPrincipalType?: ImagePullPrincipalType;
+  public readonly secretsManagerCredentials?: secretsmanager.ISecret;
+  public readonly repository?: ecr.IRepository;
+
+  private constructor(props: MacBuildImageProps) {
+    this.imageId = props.imageId;
+    this.imagePullPrincipalType = props.imagePullPrincipalType;
+    this.secretsManagerCredentials = props.secretsManagerCredentials;
+    this.repository = props.repository;
+  }
+
+  public validate(buildEnvironment: BuildEnvironment): string[] {
+    const errors: string[] = [];
+
+    if (buildEnvironment.computeType && isLambdaComputeType(buildEnvironment.computeType)) {
+      errors.push('Mac images do not support Lambda compute types');
+    }
+
+    if (!buildEnvironment.fleet) {
+      errors.push('Mac images must be used with a fleet');
+    }
+
+    return errors;
+  }
+
+  public runScriptBuildspec(entrypoint: string): BuildSpec {
+    // Reuse Linux BuildSpec, since it is compatible
+    return runScriptLinuxBuildSpec(entrypoint);
   }
 }
 
