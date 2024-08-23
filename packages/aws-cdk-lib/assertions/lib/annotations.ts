aad22824156ca325231b085a265e6eacc0a35515
MohanprasadKumar ST <67147891+stm29@users.noreply.github.com>
2024-08-23 17:59:39 +0530

    chore(assertions): misformatted doc string causes generated docs to cutoff information #27392 (#31199)
    ### Issue #27392
    
    Closes #27392
    
    ### Reason for this change
    Following Comment format,
    https://github.com/aws/aws-cdk/blob/aca90272ca65fce47d8de50dc1b2c3ab04132a19/packages/aws-cdk-lib/assertions/lib/template.ts#L183
    
    creates this following format in **.jsii** assembly
    ```
                  "docs": {
                    "remarks": "Provide '*' to match all parameters in the template.",
                    "summary": "the name of the parameter."
                  },
    ```
    **Remarks** section is created by **jsii**, when **`@remarks`** is mentioned or it takes anything after first paragraph. (after first **`dot`**)
    Reference:
    https://github.com/aws/jsii/blob/50e6c47fb873fab894e3779bafb0e84c31287839/packages/%40jsii/spec/src/assembly.ts#L313-L321
    
    ### Decision on approach
    1. fix it from **jsii / from our documentation portal side**, by modifying configuration to show remarks also in portal.
    2. convert this detail as summary by keeping it in first paragraph. [This PR implements this]
    
    ### Description of changes
    Changed 
    ```
    @param logicalId the name of the parameter. Provide `'*'` to match all parameters in the template.
    ```
    To, NOTE, **. P** --> **, p**
    ```
    @param logicalId the name of the parameter, provide `'*'` to match all parameters in the template.
    ```
    
    ### Description of how you validated changes
    1. `$ npx lerna run build --scope=aws-cdk-lib --skip-nx-cache`
    1. Ran build inside **aws-cdk-lib** --> `username@aws-cdk-lib$ yarn build`, this created .jsii file with compressed form.
    1. Now ran **yarn jsii** --> this uncompresses **.jsii** file and creates exact doc content inside **.jsii** file.
    1. Checked, everything is placed inside **summary**
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/assertions/lib/annotations.ts
+++ packages/aws-cdk-lib/assertions/lib/annotations.ts
@@ -25,7 +25,7 @@ export class Annotations {
   /**
    * Assert that an error with the given message exists in the synthesized CDK `Stack`.
    *
-   * @param constructPath the construct path to the error. Provide `'*'` to match all errors in the template.
+   * @param constructPath the construct path to the error, provide `'*'` to match all errors in the template.
    * @param message the error message as should be expected. This should be a string or Matcher object.
    */
   public hasError(constructPath: string, message: any): void {
@@ -38,7 +38,7 @@ export class Annotations {
   /**
    * Assert that an error with the given message does not exist in the synthesized CDK `Stack`.
    *
-   * @param constructPath the construct path to the error. Provide `'*'` to match all errors in the template.
+   * @param constructPath the construct path to the error, provide `'*'` to match all errors in the template.
    * @param message the error message as should be expected. This should be a string or Matcher object.
    */
   public hasNoError(constructPath: string, message: any): void {
@@ -51,7 +51,7 @@ export class Annotations {
   /**
    * Get the set of matching errors of a given construct path and message.
    *
-   * @param constructPath the construct path to the error. Provide `'*'` to match all errors in the template.
+   * @param constructPath the construct path to the error, provide `'*'` to match all errors in the template.
    * @param message the error message as should be expected. This should be a string or Matcher object.
    */
   public findError(constructPath: string, message: any): SynthesisMessage[] {
@@ -61,7 +61,7 @@ export class Annotations {
   /**
    * Assert that an warning with the given message exists in the synthesized CDK `Stack`.
    *
-   * @param constructPath the construct path to the warning. Provide `'*'` to match all warnings in the template.
+   * @param constructPath the construct path to the warning, provide `'*'` to match all warnings in the template.
    * @param message the warning message as should be expected. This should be a string or Matcher object.
    */
   public hasWarning(constructPath: string, message: any): void {
@@ -74,7 +74,7 @@ export class Annotations {
   /**
    * Assert that an warning with the given message does not exist in the synthesized CDK `Stack`.
    *
-   * @param constructPath the construct path to the warning. Provide `'*'` to match all warnings in the template.
+   * @param constructPath the construct path to the warning, provide `'*'` to match all warnings in the template.
    * @param message the warning message as should be expected. This should be a string or Matcher object.
    */
   public hasNoWarning(constructPath: string, message: any): void {
@@ -87,7 +87,7 @@ export class Annotations {
   /**
    * Get the set of matching warning of a given construct path and message.
    *
-   * @param constructPath the construct path to the warning. Provide `'*'` to match all warnings in the template.
+   * @param constructPath the construct path to the warning, provide `'*'` to match all warnings in the template.
    * @param message the warning message as should be expected. This should be a string or Matcher object.
    */
   public findWarning(constructPath: string, message: any): SynthesisMessage[] {
@@ -97,7 +97,7 @@ export class Annotations {
   /**
    * Assert that an info with the given message exists in the synthesized CDK `Stack`.
    *
-   * @param constructPath the construct path to the info. Provide `'*'` to match all info in the template.
+   * @param constructPath the construct path to the info, provide `'*'` to match all info in the template.
    * @param message the info message as should be expected. This should be a string or Matcher object.
    */
   public hasInfo(constructPath: string, message: any): void {
@@ -110,7 +110,7 @@ export class Annotations {
   /**
    * Assert that an info with the given message does not exist in the synthesized CDK `Stack`.
    *
-   * @param constructPath the construct path to the info. Provide `'*'` to match all info in the template.
+   * @param constructPath the construct path to the info, provide `'*'` to match all info in the template.
    * @param message the info message as should be expected. This should be a string or Matcher object.
    */
   public hasNoInfo(constructPath: string, message: any): void {
@@ -123,7 +123,7 @@ export class Annotations {
   /**
    * Get the set of matching infos of a given construct path and message.
    *
-   * @param constructPath the construct path to the info. Provide `'*'` to match all infos in the template.
+   * @param constructPath the construct path to the info, provide `'*'` to match all infos in the template.
    * @param message the info message as should be expected. This should be a string or Matcher object.
    */
   public findInfo(constructPath: string, message: any): SynthesisMessage[] {
