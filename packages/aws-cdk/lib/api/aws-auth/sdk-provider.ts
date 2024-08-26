3a7a948476bbc4ed0c5ab4a3d46896d72774c8f8
Sumu <sumughan@amazon.com>
2024-08-26 12:11:52 -0400

    add comment in sdk-provider.ts explaining why we use transitive tag keys
    Signed-off-by: Sumu <sumughan@amazon.com>

--- packages/aws-cdk/lib/api/aws-auth/sdk-provider.ts
+++ packages/aws-cdk/lib/api/aws-auth/sdk-provider.ts
@@ -373,6 +373,9 @@ export class SdkProvider {
             Value: value,
           })),
         } : {},
+        // We pass in the all Session Tags keys as TransitiveTagKeys so that
+        // they persist during role chaining. For more information, see:
+        // https://docs.aws.amazon.com/IAM/latest/UserGuide/id_session-tags.html#id_session-tags_role-chaining
         ...sessionTags ? { TransitiveTagKeys: Object.keys(sessionTags) } : {},
         RoleSessionName: `aws-cdk-${safeUsername()}`,
       },
