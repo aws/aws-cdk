463a1ec4c5701b3b663cc2b851cbb3d8f8fffdae
go-to-k <24818752+go-to-k@users.noreply.github.com>
2024-08-27 20:01:15 +0900

    add integ tests without snapshots

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/ec2/integ.task-definition-container-restart-policy.ts
@@ -0,0 +1,22 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'aws-ecs-task-definition-container-restart-policy');
+
+const taskDefinition = new ecs.Ec2TaskDefinition(stack, 'TaskDef', {});
+
+taskDefinition.addContainer('Container', {
+  image: ecs.ContainerImage.fromRegistry('public.ecr.aws/ecs-sample-image/amazon-ecs-sample:latest'),
+  restartPolicy: {
+    ignoredExitCodes: [1, 2, 3],
+    restartAttemptPeriod: cdk.Duration.seconds(360),
+  },
+});
+
+new IntegTest(app, 'TaskDefinitionContainerRestartPolicy', {
+  testCases: [stack],
+});
+
+app.synth();
