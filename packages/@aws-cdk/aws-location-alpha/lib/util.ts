3397333e5694002742148cfbe807ab2eb196785f
maz <yiyth.fcb6@gmail.com>
2024-08-28 09:04:57 +0900

    update

--- packages/@aws-cdk/aws-location-alpha/lib/util.ts
+++ packages/@aws-cdk/aws-location-alpha/lib/util.ts
@@ -1,6 +1,7 @@
 import { Names } from 'aws-cdk-lib/core';
+import { IConstruct } from 'constructs';
 
-export function generateUniqueId(context: any): string {
+export function generateUniqueId(context: IConstruct): string {
   const name = Names.uniqueId(context);
   if (name.length > 100) {
     return name.substring(0, 50) + name.substring(name.length - 50);
