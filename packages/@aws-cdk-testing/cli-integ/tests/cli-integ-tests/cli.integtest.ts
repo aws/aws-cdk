61b194dbb82b0fd5a8761ab4114510903c5d1032
Luke Guan <150387335+awslukeguan@users.noreply.github.com>
2024-08-30 09:52:09 -0400

    updated unread to unacknowledged

--- packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
@@ -2194,9 +2194,9 @@ async function listChildDirs(parent: string) {
 integTest(
   'cdk notices with --unacknowledged',
   withDefaultFixture(async (fixture) => {
-    const noticesUnread = await fixture.cdk(['notices', '--unacknowledged'], { verbose: false });
-    const noticesUnreadAlias = await fixture.cdk(['notices', '-u'], { verbose: false });
-    expect(noticesUnread).toEqual(expect.stringMatching(/There are \d{1,} unacknowledged notice\(s\)./));
-    expect(noticesUnread).toEqual(noticesUnreadAlias);
+    const noticesUnacknowledged = await fixture.cdk(['notices', '--unacknowledged'], { verbose: false });
+    const noticesUnacknowledgedAlias = await fixture.cdk(['notices', '-u'], { verbose: false });
+    expect(noticesUnacknowledged).toEqual(expect.stringMatching(/There are \d{1,} unacknowledged notice\(s\)./));
+    expect(noticesUnacknowledged).toEqual(noticesUnacknowledgedAlias);
   }),
 );
