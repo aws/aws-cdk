304dce69051ed2a0f639559edeecff0c2802d67a
maz <yiyth.fcb6@gmail.com>
2024-09-10 09:35:42 +0900

    support cache conifguration for app

---
+++ packages/@aws-cdk/aws-amplify-alpha/test/integ.app-cache-config.ts
@@ -0,0 +1,28 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import * as amplify from '../lib';
+
+class TestStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const amplifyApp = new amplify.App(this, 'App', {
+      // basicAuth: amplify.BasicAuth.fromGeneratedPassword('aws'),
+      // autoBranchCreation: {},
+      // platform: amplify.Platform.WEB_COMPUTE,
+      cacheConfigType: amplify.CacheConfigType.AMPLIFY_MANAGED_NO_COOKIES,
+    });
+
+    amplifyApp.addBranch('main');
+  }
+}
+
+const app = new App();
+const stack = new TestStack(app, 'cdk-amplify-app-cache-config');
+
+new IntegTest(app, 'amplify-app-cache-cofig-integ', {
+  testCases: [stack],
+  enableLookups: true,
+  stackUpdateWorkflow: false,
+});
