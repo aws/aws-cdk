3731370b9b8f8f6362f6d9b4b983108767f0dff2
watany <76135106+watany-dev@users.noreply.github.com>
2024-09-10 15:22:04 +0000

    nest the new tests under a new describe

--- packages/aws-cdk-lib/aws-cloudfront-origins/test/function-url-origin.test.ts
+++ packages/aws-cdk-lib/aws-cloudfront-origins/test/function-url-origin.test.ts
@@ -71,147 +71,149 @@ test('Correctly sets readTimeout and keepaliveTimeout', () => {
   });
 });
 
-test('Correctly adds permission to Lambda for CloudFront', () => {
-  const fn = new lambda.Function(stack, 'MyFunction', {
-    code: lambda.Code.fromInline('exports.handler = async () => {};'),
-    handler: 'index.handler',
-    runtime: lambda.Runtime.NODEJS_20_X,
-  });
+describe('FunctionUrlOriginAccessControl', () => {
+  test('Correctly adds permission to Lambda for CloudFront', () => {
+    const fn = new lambda.Function(stack, 'MyFunction', {
+      code: lambda.Code.fromInline('exports.handler = async () => {};'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_20_X,
+    });
 
-  const fnUrl = fn.addFunctionUrl({
-    authType: lambda.FunctionUrlAuthType.AWS_IAM,
-  });
+    const fnUrl = fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,
+    });
 
-  new cloudfront.Distribution(stack, 'MyDistribution', {
-    defaultBehavior: {
-      origin: FunctionUrlOrigin.withOriginAccessControl(fnUrl, {
-        originAccessControl: undefined,
-      }),
-    },
-  });
-
-  const template = Template.fromStack(stack);
-
-  template.hasResourceProperties('AWS::CloudFront::Distribution', {
-    DistributionConfig: {
-      Origins: Match.arrayWith([
-        Match.objectLike({
-          DomainName: {
-            'Fn::Select': [
-              2,
-              {
-                'Fn::Split': [
-                  '/',
-                  {
-                    'Fn::GetAtt': ['MyFunctionFunctionUrlFF6DE78C', 'FunctionUrl'],
-                  },
-                ],
-              },
-            ],
-          },
-          OriginAccessControlId: Match.objectLike({
-            'Fn::GetAtt': [
-              'MyDistributionOrigin1FunctionUrlOriginAccessControl0591AF75',
-              'Id',
-            ],
-          }),
+    new cloudfront.Distribution(stack, 'MyDistribution', {
+      defaultBehavior: {
+        origin: FunctionUrlOrigin.withOriginAccessControl(fnUrl, {
+          originAccessControl: undefined,
         }),
-      ]),
-    },
-  });
-});
+      },
+    });
 
-test('Correctly configures CloudFront Distribution with Origin Access Control', () => {
-  const fn = new lambda.Function(stack, 'MyFunction', {
-    code: lambda.Code.fromInline('exports.handler = async () => {};'),
-    handler: 'index.handler',
-    runtime: lambda.Runtime.NODEJS_20_X,
-  });
+    const template = Template.fromStack(stack);
 
-  const fnUrl = fn.addFunctionUrl({
-    authType: lambda.FunctionUrlAuthType.AWS_IAM,
-  });
-
-  new cloudfront.Distribution(stack, 'MyDistribution', {
-    defaultBehavior: {
-      origin: FunctionUrlOrigin.withOriginAccessControl(fnUrl, {
-        originAccessControl: undefined,
-      }),
-    },
-  });
-
-  const template = Template.fromStack(stack);
-
-  template.hasResourceProperties('AWS::CloudFront::Distribution', {
-    DistributionConfig: {
-      Origins: Match.arrayWith([
-        Match.objectLike({
-          DomainName: {
-            'Fn::Select': [
-              2,
-              {
-                'Fn::Split': [
-                  '/',
-                  {
-                    'Fn::GetAtt': ['MyFunctionFunctionUrlFF6DE78C', 'FunctionUrl'],
-                  },
-                ],
-              },
-            ],
-          },
-          OriginAccessControlId: Match.objectLike({
-            'Fn::GetAtt': [
-              'MyDistributionOrigin1FunctionUrlOriginAccessControl0591AF75',
-              'Id',
-            ],
+    template.hasResourceProperties('AWS::CloudFront::Distribution', {
+      DistributionConfig: {
+        Origins: Match.arrayWith([
+          Match.objectLike({
+            DomainName: {
+              'Fn::Select': [
+                2,
+                {
+                  'Fn::Split': [
+                    '/',
+                    {
+                      'Fn::GetAtt': ['MyFunctionFunctionUrlFF6DE78C', 'FunctionUrl'],
+                    },
+                  ],
+                },
+              ],
+            },
+            OriginAccessControlId: Match.objectLike({
+              'Fn::GetAtt': [
+                'MyDistributionOrigin1FunctionUrlOriginAccessControl0591AF75',
+                'Id',
+              ],
+            }),
           }),
+        ]),
+      },
+    });
+  });
+
+  test('Correctly configures CloudFront Distribution with Origin Access Control', () => {
+    const fn = new lambda.Function(stack, 'MyFunction', {
+      code: lambda.Code.fromInline('exports.handler = async () => {};'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_20_X,
+    });
+
+    const fnUrl = fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,
+    });
+
+    new cloudfront.Distribution(stack, 'MyDistribution', {
+      defaultBehavior: {
+        origin: FunctionUrlOrigin.withOriginAccessControl(fnUrl, {
+          originAccessControl: undefined,
         }),
-      ]),
-    },
+      },
+    });
+
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::CloudFront::Distribution', {
+      DistributionConfig: {
+        Origins: Match.arrayWith([
+          Match.objectLike({
+            DomainName: {
+              'Fn::Select': [
+                2,
+                {
+                  'Fn::Split': [
+                    '/',
+                    {
+                      'Fn::GetAtt': ['MyFunctionFunctionUrlFF6DE78C', 'FunctionUrl'],
+                    },
+                  ],
+                },
+              ],
+            },
+            OriginAccessControlId: Match.objectLike({
+              'Fn::GetAtt': [
+                'MyDistributionOrigin1FunctionUrlOriginAccessControl0591AF75',
+                'Id',
+              ],
+            }),
+          }),
+        ]),
+      },
+    });
+
+    template.hasResourceProperties('AWS::CloudFront::OriginAccessControl', {
+      OriginAccessControlConfig: {
+        OriginAccessControlOriginType: 'lambda',
+        SigningBehavior: 'always',
+        SigningProtocol: 'sigv4',
+      },
+    });
   });
 
-  template.hasResourceProperties('AWS::CloudFront::OriginAccessControl', {
-    OriginAccessControlConfig: {
-      OriginAccessControlOriginType: 'lambda',
-      SigningBehavior: 'always',
-      SigningProtocol: 'sigv4',
-    },
-  });
-});
+  test('Correctly configures CloudFront Distribution with a custom Origin Access Control', () => {
+    const fn = new lambda.Function(stack, 'MyFunction', {
+      code: lambda.Code.fromInline('exports.handler = async () => {};'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_20_X,
+    });
 
-test('Correctly configures CloudFront Distribution with a custom Origin Access Control', () => {
-  const fn = new lambda.Function(stack, 'MyFunction', {
-    code: lambda.Code.fromInline('exports.handler = async () => {};'),
-    handler: 'index.handler',
-    runtime: lambda.Runtime.NODEJS_20_X,
-  });
+    const fnUrl = fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,
+    });
 
-  const fnUrl = fn.addFunctionUrl({
-    authType: lambda.FunctionUrlAuthType.NONE,
-  });
+    // Custom OAC configuration
+    const oac = new cloudfront.FunctionUrlOriginAccessControl(stack, 'CustomOAC', {
+      originAccessControlName: 'CustomLambdaOAC',
+      signing: cloudfront.Signing.SIGV4_ALWAYS,
+    });
 
-  // Custom OAC configuration
-  const oac = new cloudfront.FunctionUrlOriginAccessControl(stack, 'CustomOAC', {
-    originAccessControlName: 'CustomLambdaOAC',
-    signing: cloudfront.Signing.SIGV4_ALWAYS,
-  });
+    new cloudfront.Distribution(stack, 'MyDistribution', {
+      defaultBehavior: {
+        origin: FunctionUrlOrigin.withOriginAccessControl(fnUrl, {
+          originAccessControl: oac,
+        }),
+      },
+    });
 
-  new cloudfront.Distribution(stack, 'MyDistribution', {
-    defaultBehavior: {
-      origin: FunctionUrlOrigin.withOriginAccessControl(fnUrl, {
-        originAccessControl: oac,
-      }),
-    },
-  });
+    const template = Template.fromStack(stack);
 
-  const template = Template.fromStack(stack);
-
-  template.hasResourceProperties('AWS::CloudFront::OriginAccessControl', {
-    OriginAccessControlConfig: {
-      Name: 'CustomLambdaOAC',
-      OriginAccessControlOriginType: 'lambda',
-      SigningBehavior: 'always',
-      SigningProtocol: 'sigv4',
-    },
+    template.hasResourceProperties('AWS::CloudFront::OriginAccessControl', {
+      OriginAccessControlConfig: {
+        Name: 'CustomLambdaOAC',
+        OriginAccessControlOriginType: 'lambda',
+        SigningBehavior: 'always',
+        SigningProtocol: 'sigv4',
+      },
+    });
   });
 });
