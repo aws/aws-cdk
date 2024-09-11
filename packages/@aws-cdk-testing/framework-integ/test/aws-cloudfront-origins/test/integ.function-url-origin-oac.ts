7f9ba73f62fe6bfd612c6d5708ec6a152f4db0d6
watany <76135106+watany-dev@users.noreply.github.com>
2024-09-11 07:06:47 +0000

    re integtest

--- packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront-origins/test/integ.function-url-origin-oac.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront-origins/test/integ.function-url-origin-oac.ts
@@ -1,7 +1,7 @@
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as cdk from 'aws-cdk-lib';
-import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { ExpectedResult, IntegTest, Match } from '@aws-cdk/integ-tests-alpha';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 const app = new cdk.App();
@@ -12,7 +12,7 @@ const stack = new cdk.Stack(app, 'integ-cloudfront-function-url-origin-oac');
 const fn = new lambda.Function(stack, 'MyFunction', {
   code: lambda.Code.fromInline('exports.handler = async () => {};'),
   handler: 'index.handler',
-  runtime: lambda.Runtime.NODEJS_20_X,
+  runtime: lambda.Runtime.NODEJS_LATEST,
 });
 
 // Add a Lambda Function URL
@@ -21,13 +21,50 @@ const fnUrl = fn.addFunctionUrl({
 });
 
 // Create CloudFront Distribution with OAC
-new cloudfront.Distribution(stack, 'Distribution', {
+const distribution = new cloudfront.Distribution(stack, 'Distribution', {
   defaultBehavior: {
     origin: origins.FunctionUrlOrigin.withOriginAccessControl(fnUrl),
   },
 });
 
 // Set up integration test
-new IntegTest(app, 'lambda-url-origin-oac', {
+const integ = new IntegTest(app, 'lambda-url-origin-oac', {
   testCases: [stack],
 });
+
+// Validate CloudFront distribution config for the Lambda Function URL origin
+integ.assertions.awsApiCall('CloudFront', 'getDistributionConfig', {
+  Id: distribution.distributionId,
+}).expect(ExpectedResult.objectLike({
+  DistributionConfig: Match.objectLike({
+    Origins: {
+      Quantity: 1,
+      Items: Match.arrayWith([
+        Match.objectLike({
+          CustomOriginConfig: {
+            OriginProtocolPolicy: 'https-only',
+          },
+          OriginAccessControlId: Match.stringLikeRegexp('^[A-Z0-9]+$'),
+        }),
+      ]),
+    },
+  }),
+}));
+
+// Fetch the OriginAccessControlId from the distribution config
+const originAccessControlId = integ.assertions.awsApiCall('CloudFront', 'getDistributionConfig', {
+  Id: distribution.distributionId,
+}).getAttString('DistributionConfig.Origins.Items.0.OriginAccessControlId');
+
+// Validate the Origin Access Control configuration
+integ.assertions.awsApiCall('CloudFront', 'getOriginAccessControlConfig', {
+  Id: originAccessControlId,
+}).expect(ExpectedResult.objectLike({
+  OriginAccessControlConfig: {
+    SigningProtocol: 'sigv4',
+    SigningBehavior: 'always',
+    OriginAccessControlOriginType: 'lambda',
+  },
+}));
+
+app.synth();
