c8f054015219d4fa430bb1c7f1d9c61160932175
Ari Palo <ari@aripalo.fi>
2024-09-15 19:35:59 +0000

    test: update snapshot

--- packages/@aws-cdk-testing/framework-integ/test/core/test/integ.stage-termination-protection.ts
+++ packages/@aws-cdk-testing/framework-integ/test/core/test/integ.stage-termination-protection.ts
@@ -9,4 +9,10 @@ const app = new cdk.App();
 const stage = new cdk.Stage(app, 'Stage', { terminationProtection: true });
 const stack = new cdk.Stack(stage, 'Stack');
 
+new cdk.CfnOutput(stack, 'StackTerminationProtection', { 
+    value: `${stack.terminationProtection}`,
+});
+
 new IntegTest(app, 'stack', { testCases: [stack] });
+
+app.synth();
