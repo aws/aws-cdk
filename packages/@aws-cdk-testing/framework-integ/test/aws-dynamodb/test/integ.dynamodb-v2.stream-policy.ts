5826eed01597c541730af8b5c247026f948198f8
Lee Hannigan <lhnng@amazon.com>
2024-09-20 18:20:08 +0100

    Adding integ testing

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-dynamodb/test/integ.dynamodb-v2.stream-policy.ts
@@ -0,0 +1,52 @@
+import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+const app = new App();
+
+class TestStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const docu = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['dynamodb:*'],
+          principals: [new iam.AccountRootPrincipal()],
+          resources: ['*'],
+        }),
+      ],
+    });
+
+    const streamDoc = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['dynamodb:GetRecords', 'dynamodb:DescribeStream'],
+          principals: [new iam.AccountRootPrincipal()],
+          resources: ['*'],
+        }),
+      ],
+    });
+
+    // table with resource policy
+    new dynamodb.TableV2(this, 'TableTestV2-1', {
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      },
+      removalPolicy: RemovalPolicy.DESTROY,
+      streamResourcePolicy: streamDoc,
+      resourcePolicy: docu,
+      dynamoStream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
+    });
+
+  }
+}
+
+const stack = new TestStack(app, 'StreamPolicy', { env: { region: 'eu-west-1' } });
+
+new IntegTest(app, 'table-v2-resource-policy-integ-test', {
+  testCases: [stack],
+});
