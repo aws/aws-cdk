164dd26dc68145c88f6a3b6a91eff6d889a68d0e
Lee Hannigan <lhnng@amazon.com>
2024-09-21 13:07:16 +0100

    integ test class fixed

--- packages/@aws-cdk-testing/framework-integ/test/aws-dynamodb/test/integ.dynamodb-v2.policy.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-dynamodb/test/integ.dynamodb-v2.policy.ts
@@ -4,7 +4,11 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 
-const app = new App();
+const app = new App({
+  postCliContext: {
+    '@aws-cdk/aws-dynamodb:resourcePolicyPerReplica': false,
+  },
+});
 
 class TestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
