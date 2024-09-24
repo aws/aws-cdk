6776a50392ad8b8d97a2e48396bec17f85b461a1
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2024-09-24 19:01:23 +0900

    Update cluster.ts

--- packages/aws-cdk-lib/aws-rds/lib/cluster.ts
+++ packages/aws-cdk-lib/aws-rds/lib/cluster.ts
@@ -701,7 +701,7 @@ abstract class DatabaseClusterNew extends DatabaseClusterBase {
       });
     }
 
-    // enableLocalWriteForwarding is not configurable for Aurora clusters other than MySQL.
+    // enableLocalWriteForwarding cannot be configured, including false, on Aurora clusters other than MySQL.
     if (props.enableLocalWriteForwarding !== undefined && !['aurora', 'aurora-mysql'].includes(props.engine.engineType)) {
       throw new Error(`\'enableLocalWriteForwarding\' is only supported for Aurora Mysql cluster engine type, got: ${props.engine.engineType}`);
     }
