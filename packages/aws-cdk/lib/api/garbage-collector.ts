796e8d24402fea45b726ca068d2522f4a75a76ee
Kaizen Conroy <zen.conroy@gmail.com>
2024-09-26 16:15:50 -0400

    tagging api in use now

--- packages/aws-cdk/lib/api/garbage-collector.ts
+++ packages/aws-cdk/lib/api/garbage-collector.ts
@@ -1,8 +1,12 @@
 import * as cxapi from '@aws-cdk/cx-api';
 import { ISDK, Mode, SdkProvider } from './aws-auth';
 import { ToolkitInfo } from './toolkit-info';
+import { print } from '../logging';
 import { CloudFormation, S3 } from 'aws-sdk';
-import path = require('path');
+import * as chalk from 'chalk';
+import * as path from 'path';
+
+const ISOLATED_TAG = 'awscdk.isolated';
 
 /**
  * Props for the Garbage Collector
@@ -58,19 +62,16 @@ interface GarbageCollectorProps {
  * A class to facilitate Garbage Collection of S3 and ECR assets
  */
 export class GarbageCollector {
-  private readonly inUseHashes: Set<string> = new Set();
-  private readonly objectHashes: Set<string> = new Set();
+  private readonly activeHashes = new Set<string>();
+  private readonly objects = new Map<string, S3Object>();
+  private bootstrapBucketName: string | undefined =  undefined;
 
   private garbageCollectS3Assets: boolean;
   private garbageCollectEcrAssets: boolean;
-  private resolvedEnvironment: cxapi.Environment;
-  private sdkProvider: SdkProvider;
 
-  public constructor(props: GarbageCollectorProps) {
+  public constructor(readonly props: GarbageCollectorProps) {
     this.garbageCollectS3Assets = props.type === 's3' || props.type === 'all';
     this.garbageCollectEcrAssets = props.type === 'ecr' || props.type === 'all';
-    this.resolvedEnvironment = props.resolvedEnvironment;
-    this.sdkProvider = props.sdkProvider;
 
     // TODO: ECR garbage collection
     if (this.garbageCollectEcrAssets) {
@@ -82,24 +83,40 @@ export class GarbageCollector {
    * Perform garbage collection on the resolved environment(s)
    */
   public async garbageCollect() {
+    print(chalk.green(`Garbage collecting for ${this.props.resolvedEnvironment.account} in ${this.props.resolvedEnvironment.region}`));
+  
     // set up the sdks used in garbage collection
-    const sdk = (await this.sdkProvider.forEnvironment(this.resolvedEnvironment, Mode.ForWriting)).sdk;
+    const sdk = (await this.props.sdkProvider.forEnvironment(this.props.resolvedEnvironment, Mode.ForWriting)).sdk;
     const { cfn, s3 } = this.setUpSDKs(sdk);
 
     if (this.garbageCollectS3Assets) {
+      print(chalk.green('Getting bootstrap bucket'));
       const bucket = await this.getBootstrapBucket(sdk);
-      console.log(bucket);
+      print(chalk.green(`Bootstrap Bucket ${bucket}`));
       await this.collectObjects(s3, bucket);
-      console.log(this.objectHashes);
+      print(chalk.blue(`Object hashes: ${this.objects.size}`));
     }
 
     await this.collectHashes(cfn);
-    console.log(this.inUseHashes);
+    print(chalk.green(`Hashes in use: ${this.activeHashes.size}`));
 
-    console.log(setDifference(this.objectHashes, this.inUseHashes));
+    for (const hash of this.activeHashes) {
+      const obj = this.objects.get(hash);
+      if (obj) {
+        obj.isolated = false;
+      }
+    }
+    const isolatedObjects = Array.from(this.objects.values())
+      .filter(obj => obj.isolated === true);
 
-    // TODO: match asset hashes with object keys
-    // TODO: tag isolated assets 
+    print(chalk.red(`Isolated hashes: ${isolatedObjects.length}`));
+
+    if (!this.props.dryRun) {
+      if (this.garbageCollectS3Assets) {
+        print(chalk.green('Tagging Isolated Objects'));
+        this.tagObjects(s3, await this.getBootstrapBucket(sdk));
+      }
+    }
   }
 
   private setUpSDKs(sdk: ISDK) {
@@ -119,7 +136,7 @@ export class GarbageCollector {
       return response.NextToken;
     });
 
-    console.log(`Parsing through ${stackNames.length} stacks`);
+    print(chalk.blue(`Parsing through ${stackNames.length} stacks`));
 
     for (const stack of stackNames) {
       const template = await cfn.getTemplate({
@@ -127,15 +144,18 @@ export class GarbageCollector {
       }).promise();
 
       const templateHashes = template.TemplateBody?.match(/[a-f0-9]{64}/g);
-      templateHashes?.forEach(this.inUseHashes.add, this.inUseHashes);
+      templateHashes?.forEach(this.activeHashes.add, this.activeHashes);
     }
 
-    console.log(`Found ${this.inUseHashes.size} unique hashes`);
+    print(chalk.blue(`Found ${this.activeHashes.size} unique hashes`));
   }
 
-  private async getBootstrapBucket(sdk: ISDK) {
-    const info = await ToolkitInfo.lookup(this.resolvedEnvironment, sdk, undefined);
-    return info.bucketName;
+  private async getBootstrapBucket(sdk: ISDK): Promise<string> {
+    if (!this.bootstrapBucketName) { 
+      const info = await ToolkitInfo.lookup(this.props.resolvedEnvironment, sdk, undefined);
+      this.bootstrapBucketName = info.bucketName;
+    }
+    return this.bootstrapBucketName;
   }
 
   private async collectObjects(s3: S3, bucket: string) {
@@ -145,10 +165,108 @@ export class GarbageCollector {
         ContinuationToken: nextToken,
       }).promise();
       response.Contents?.forEach((obj) => {
-        this.objectHashes.add(path.parse(obj.Key ?? '').name);
+        let key = obj.Key;
+        if (key) {
+          let hash = path.parse(key).name;
+          this.objects.set(hash, {
+            key,
+            hash,
+            isolated: true, // Default is isolated
+          });
+        }
       });
       return response.NextContinuationToken;
     });
+  }
+
+  private async tagObjects(s3: S3, bucket: string) {
+    let newlyTagged = 0;
+    let alreadyTagged = 0;
+    let newlyUntagged = 0;
+    let alreadyUntagged = 0;
+    for (const [_, obj] of this.objects) {
+      if (obj.isolated) {
+        const result = await this.tagIsolatedObject(s3, bucket, obj);
+        if (result) {
+          newlyTagged++;
+        } else {
+          alreadyTagged++;
+        }
+      } else {
+        const result = await this.untagActiveObject(s3, bucket, obj);
+        if (result) {
+          newlyUntagged++;
+        } else {
+          alreadyUntagged++;
+        }
+      }
+    }
+
+    print(chalk.white(`Newly Tagged: ${newlyTagged}\nAlready Tagged: ${alreadyTagged}\nNewly Untagged: ${newlyUntagged}\nAlready Untagged: ${alreadyUntagged}`));
+  }
+
+  /**
+   * Returns true if object gets tagged (was previously untagged but is now isolated)
+   */
+  private async tagIsolatedObject(s3: S3, bucket: string, object: S3Object) {
+    const response = await s3.getObjectTagging({
+      Bucket: bucket,
+      Key: object.key,
+    }).promise();
+    const isolatedTag = await this.getIsolatedTag(response.TagSet);
+  
+    // tag new objects with the current date
+    if (!isolatedTag) {
+      await s3.putObjectTagging({
+        Bucket: bucket,
+        Key: object.key,
+        Tagging: {
+          TagSet: [{
+            Key: ISOLATED_TAG,
+            Value: Date.now().toString(),
+          }],
+        },
+      }).promise();
+
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Returns true if object gets untagged (was previously tagged but is now active)
+   */
+  private async untagActiveObject(s3: S3, bucket: string, object: S3Object) {
+    const response = await s3.getObjectTagging({
+      Bucket: bucket,
+      Key: object.key,
+    }).promise();
+    const isolatedTag = await this.getIsolatedTag(response.TagSet);
+
+    // remove isolated tag and put any other tags back
+    if (isolatedTag) {
+      const newTags = response.TagSet?.filter(tag => tag.Key !== ISOLATED_TAG);
+      // TODO: double check what happens when newTags = []
+      await s3.putObjectTagging({
+        Bucket: bucket,
+        Key: object.key,
+        Tagging: {
+          TagSet: newTags,
+        },
+      }).promise();
+
+      return true;
+    }
+    return false;
+  }
+
+  private async getIsolatedTag(tags: S3.TagSet): Promise<string | undefined> {
+    for (const tag of tags) {
+      if (tag.Key === ISOLATED_TAG) {
+        return tag.Value;
+      }
+    }
+    return;
   }
 }
 
@@ -163,10 +281,8 @@ async function paginateSdkCall(cb: (nextToken?: string) => Promise<string | unde
   }
 }
 
-function setDifference<T>(setA: Set<T>, setB: Set<T>): Set<T> {
-  let difference = new Set(setA);
-  for (let elem of setB) {
-    difference.delete(elem);
-  }
-  return difference;
+interface S3Object {
+  readonly key: string;
+  readonly hash: string;
+  isolated: boolean;
 }
