8871e45f7b20a1bdc71075717986643e8326f744
Mohamed ElAsmar <melasmar@amazon.com>
2024-09-27 02:08:52 -0700

    rerun the integration testing

--- packages/@aws-cdk-testing/framework-integ/test/aws-appconfig/test/integ.configuration.js.snapshot/asset.c6358465bf49dfae556bb430bf9c81fa578c221b82c308e3707901b1dd654762/index.py
+++ packages/@aws-cdk-testing/framework-integ/test/aws-appconfig/test/integ.configuration.js.snapshot/asset.c6358465bf49dfae556bb430bf9c81fa578c221b82c308e3707901b1dd654762/index.py
@@ -64,6 +64,7 @@ def handler(event, context):
             exclude             = props.get('Exclude', [])
             include             = props.get('Include', [])
             sign_content        = props.get('SignContent', 'false').lower() == 'true'
+            output_object_keys  = props.get('OutputObjectKeys', 'true') == 'true'
 
             # backwards compatibility - if "SourceMarkers" is not specified,
             # assume all sources have an empty market map
@@ -135,7 +136,7 @@ def handler(event, context):
         cfn_send(event, context, CFN_SUCCESS, physicalResourceId=physical_id, responseData={
             # Passing through the ARN sequences dependencees on the deployment
             'DestinationBucketArn': props.get('DestinationBucketArn'),
-            'SourceObjectKeys': props.get('SourceObjectKeys'),
+            **({'SourceObjectKeys': props.get('SourceObjectKeys')} if output_object_keys else {'SourceObjectKeys': []})
         })
     except KeyError as e:
         cfn_error("invalid request. Missing key %s" % str(e))
@@ -333,4 +334,3 @@ def replace_markers(filename, markers):
     # # delete the original file and rename the new one to the original
     os.remove(filename)
     os.rename(outfile, filename)
-    
