fea5a32688e462e07dade9fa7210a1b15a2afc41
watany <76135106+watany-dev@users.noreply.github.com>
2024-09-28 03:11:48 +0000

    split integ

--- packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront-origins/test/integ.function-url-origin-oac-custom.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront-origins/test/integ.function-url-origin-oac-custom.ts
@@ -6,10 +6,10 @@ import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 const app = new cdk.App();
 
-const stack = new cdk.Stack(app, 'integ-cloudfront-function-url-origin-oac');
+const customOacStack = new cdk.Stack(app, 'integ-cloudfront-function-url-origin-custom-oac');
 
 // Create the Lambda function with a custom response
-const fn = new lambda.Function(stack, 'MyFunction', {
+const customOacFn = new lambda.Function(customOacStack, 'MyFunction', {
   code: lambda.Code.fromInline(`
     exports.handler = async () => ({
       statusCode: 200,
@@ -21,34 +21,41 @@ const fn = new lambda.Function(stack, 'MyFunction', {
 });
 
 // Add a Lambda Function URL
-const fnUrl = fn.addFunctionUrl({
+const customOacFnUrl = customOacFn.addFunctionUrl({
   authType: lambda.FunctionUrlAuthType.AWS_IAM,
 });
 
 // Optionally, test Lambda Function URL before creating CloudFront distribution
-const lambdaUrlHttpCall = new IntegTest(app, 'lambda-url-test', {
-  testCases: [stack],
+const customOacLambdaUrlHttpCall = new IntegTest(app, 'lambda-url-custom-oac-test', {
+  testCases: [customOacStack],
 });
-lambdaUrlHttpCall.assertions.httpApiCall(fnUrl.url).expect(ExpectedResult.objectLike({
+customOacLambdaUrlHttpCall.assertions.httpApiCall(customOacFnUrl.url).expect(ExpectedResult.objectLike({
   status: 200,
   body: 'Hello!!',
 }));
 
+const oac = new cloudfront.FunctionUrlOriginAccessControl(customOacStack, 'CustomOAC', {
+  originAccessControlName: 'CustomLambdaOAC',
+  signing: cloudfront.Signing.SIGV4_ALWAYS,
+});
+
 // Create CloudFront Distribution with OAC
-const distribution = new cloudfront.Distribution(stack, 'Distribution', {
+const customOacDistribution = new cloudfront.Distribution(customOacStack, 'Distribution', {
   defaultBehavior: {
-    origin: origins.FunctionUrlOrigin.withOriginAccessControl(fnUrl),
+    origin: origins.FunctionUrlOrigin.withOriginAccessControl(customOacFnUrl, {
+      originAccessControl: oac,
+    }),
   },
 });
 
 // Set up integration test
-const integ = new IntegTest(app, 'lambda-url-origin-oac', {
-  testCases: [stack],
+const customOacInteg = new IntegTest(app, 'lambda-url-origin-custom-oac', {
+  testCases: [customOacStack],
 });
 
 // Validate CloudFront distribution config for the Lambda Function URL origin
-integ.assertions.awsApiCall('CloudFront', 'getDistributionConfig', {
-  Id: distribution.distributionId,
+customOacInteg.assertions.awsApiCall('CloudFront', 'getDistributionConfig', {
+  Id: customOacDistribution.distributionId,
 }).expect(ExpectedResult.objectLike({
   DistributionConfig: Match.objectLike({
     Origins: {
@@ -66,15 +73,16 @@ integ.assertions.awsApiCall('CloudFront', 'getDistributionConfig', {
 }));
 
 // Fetch the OriginAccessControlId from the distribution config
-const originAccessControlId = integ.assertions.awsApiCall('CloudFront', 'getDistributionConfig', {
-  Id: distribution.distributionId,
+const customOacOriginAccessControlId = customOacInteg.assertions.awsApiCall('CloudFront', 'getDistributionConfig', {
+  Id: customOacDistribution.distributionId,
 }).getAttString('DistributionConfig.Origins.Items.0.OriginAccessControlId');
 
 // Validate the Origin Access Control configuration
-integ.assertions.awsApiCall('CloudFront', 'getOriginAccessControlConfig', {
-  Id: originAccessControlId,
+customOacInteg.assertions.awsApiCall('CloudFront', 'getOriginAccessControlConfig', {
+  Id: customOacOriginAccessControlId,
 }).expect(ExpectedResult.objectLike({
   OriginAccessControlConfig: {
+    Name: 'CustomLambdaOAC',
     SigningProtocol: 'sigv4',
     SigningBehavior: 'always',
     OriginAccessControlOriginType: 'lambda',
@@ -82,8 +90,8 @@ integ.assertions.awsApiCall('CloudFront', 'getOriginAccessControlConfig', {
 }));
 
 // Make an HTTP call to the CloudFront distribution and verify the response
-const httpCall = integ.assertions.httpApiCall(`https://${distribution.distributionDomainName}`);
-httpCall.expect(ExpectedResult.objectLike({
+const customOacHttpCall = customOacInteg.assertions.httpApiCall(`https://${customOacDistribution.distributionDomainName}`);
+customOacHttpCall.expect(ExpectedResult.objectLike({
   status: 200,
   body: 'Hello!!',
 }));
