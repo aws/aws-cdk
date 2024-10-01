377e715e9a850c6d47d63b14a2efd7ce6f92c38a
Sumu <sumughan@amazon.com>
2024-10-01 09:20:08 -0400

    rico comment: remove negative integ test assertions
    Signed-off-by: Sumu <sumughan@amazon.com>

--- packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
@@ -1049,11 +1049,6 @@ integTest(
       },
     });
 
-    // Assert that S3 upload and changeset creation error messages were not thrown:
-    expect(diff).not.toContain('Call failed: getBucketLocation');
-    expect(diff).not.toContain('not authorized to perform: s3:GetBucketLocation on resource');
-    expect(diff).not.toContain('Could not create a change set, will base the diff on template differences');
-
     // Assert that the CLI assumes the file publishing role:
     expect(diff).toMatch(/Assuming role .*file-publishing-role/);
     expect(diff).toContain('success: Published');
