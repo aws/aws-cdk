ae24b42279e79602fc68df0f9adae3f8d1c6aef5
Jonathan Colbert <jonathan.colbert033@gmail.com>
2024-10-06 15:19:29 -0400

    set cluster secret when using manageMasterUserPassword

--- packages/aws-cdk-lib/aws-rds/lib/cluster.ts
+++ packages/aws-cdk-lib/aws-rds/lib/cluster.ts
@@ -1166,6 +1166,7 @@ export class DatabaseCluster extends DatabaseClusterNew {
 
       this.clusterIdentifier = cluster.ref;
       this.clusterResourceIdentifier = cluster.attrDbClusterResourceId;
+      this.secret = secretsmanager.Secret.fromSecretCompleteArn(this, 'DbClusterSecret', cluster.attrMasterUserSecretSecretArn);
     }
 
     // create a number token that represents the port of the cluster
