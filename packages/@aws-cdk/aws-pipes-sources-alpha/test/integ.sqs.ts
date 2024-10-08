f04829d52f1bb684b1116c05d6ba43c52bcda779
Michael Sambol <sambol.michael@gmail.com>
2024-10-07 19:07:46 -0700

    Fix import

--- packages/@aws-cdk/aws-pipes-sources-alpha/test/integ.sqs.ts
+++ packages/@aws-cdk/aws-pipes-sources-alpha/test/integ.sqs.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from 'crypto';
 import { InputTransformation, Pipe } from '@aws-cdk/aws-pipes-alpha';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { SqsTarget } from '@aws-cdk/aws-pipes-targets-alpha';
 import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
 import * as cdk from 'aws-cdk-lib';
