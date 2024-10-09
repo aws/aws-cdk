fc09bc19166b2e4b76de6b836505c7b421111449
Pahud Hsieh <pahudnet@gmail.com>
2024-10-09 14:58:22 -0400

    feat(eks): support eks with k8s 1.31 (#31707)
    ### Issue # (if applicable)
    
    Closes https://github.com/aws/aws-cdk/issues/31695
    
    ### Reason for this change
    
    - Similar to https://github.com/aws/aws-cdk/pull/30454, this PR adds 1.31 support.
    - Update all existing integ tests for V1_30 and make sure they deploy with V1_31.
    
    
    ### Description of changes
    
    
    
    ### Description of how you validated changes
    
    
    ```ts
    import * as ec2 from 'aws-cdk-lib/aws-ec2';
    import * as iam from 'aws-cdk-lib/aws-iam';
    import { App, Stack, StackProps } from 'aws-cdk-lib';
    import { KubectlV31Layer } from '@aws-cdk/lambda-layer-kubectl-v31';
    import * as eks from 'aws-cdk-lib/aws-eks';
    import { Construct } from 'constructs';
    
    export class EksClusterLatestVersion extends Stack {
      constructor(scope: Construct, id: string, props: StackProps) {
        super(scope, id, props);
    
        const vpc = new ec2.Vpc(this, 'Vpc', { natGateways: 1 });
        const mastersRole = new iam.Role(this, 'Role', {
          assumedBy: new iam.AccountRootPrincipal(),
        });
    
        new eks.Cluster(this, 'Cluster', {
          vpc,
          mastersRole,
          version: eks.KubernetesVersion.V1_31,
          kubectlLayer: new KubectlV31Layer(this, 'KubectlLayer'),
          defaultCapacity: 1,
        });
      }
    }
    
    const app = new App();
    new EksClusterLatestVersion(app, 'v31-stack', {
      env: {
        account: process.env.CDK_DEFAULT_ACCOUNT,
        region: process.env.CDK_DEFAULT_REGION,
      },
    })
    ```
    
    verify
    
    ```sh
    $ kubectl get no
    NAME                           STATUS   ROLES    AGE     VERSION
    ip-10-0-183-173.ec2.internal   Ready    <none>   4m25s   v1.31.0-eks-a737599
    ```
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.eks-al2023-nodegroup.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.eks-al2023-nodegroup.ts
@@ -28,7 +28,7 @@ class EksClusterStack extends Stack {
       vpc: this.vpc,
       mastersRole,
       defaultCapacity: 0,
-      ...getClusterVersionConfig(this, eks.KubernetesVersion.V1_30),
+      ...getClusterVersionConfig(this, eks.KubernetesVersion.V1_31),
     });
 
     // create nodegroup with AL2023_X86_64_STANDARD
