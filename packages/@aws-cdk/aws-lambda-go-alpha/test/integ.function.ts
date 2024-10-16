6428b724935900fd4afb804a2568d53ae83e5cd3
Di Wu <diwud@amazon.com>
2024-10-16 12:18:37 -0700

    add a custom go function to test invocation reulst

--- packages/@aws-cdk/aws-lambda-go-alpha/test/integ.function.ts
+++ packages/@aws-cdk/aws-lambda-go-alpha/test/integ.function.ts
@@ -2,6 +2,8 @@ import * as path from 'path';
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from '../lib';
+import * as integ from "@aws-cdk/integ-tests-alpha";
+import { IFunction } from "aws-cdk-lib/aws-lambda";
 
 /*
  * Stack verification steps:
@@ -9,10 +11,11 @@ import * as lambda from '../lib';
  */
 
 class TestStack extends Stack {
+  public readonly lambdaFunction: IFunction
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new lambda.GoFunction(this, 'go-handler-docker', {
+    this.lambdaFunction = new lambda.GoFunction(this, 'go-handler-docker', {
       entry: path.join(__dirname, 'lambda-handler-vendor', 'cmd', 'api'),
       bundling: {
         forcedDockerBundling: true,
@@ -23,5 +26,14 @@ class TestStack extends Stack {
 }
 
 const app = new App();
-new TestStack(app, 'cdk-integ-lambda-golang');
-app.synth();
+const stack = new TestStack(app, 'cdk-integ-lambda-golang-test-stack');
+
+const integTest = new integ.IntegTest(app, 'cdk-integ-lambda-golang', {
+  testCases: [stack],
+});
+
+const response = integTest.assertions.invokeFunction({
+  functionName: stack.lambdaFunction.functionName,
+});
+
+response.expect(integ.ExpectedResult.exact(256.0));
--- packages/@aws-cdk/aws-lambda-go-alpha/test/lambda-handler-vendor/cmd/api/main.go
+++ packages/@aws-cdk/aws-lambda-go-alpha/test/lambda-handler-vendor/cmd/api/main.go
@@ -1,12 +1,33 @@
 package main
 
-// Intentionally empty. There were issues with 'gopkg.in', so:
-// - we cannot depend on 'github.com/aws/aws-lambda-go'
-//    - since: it has a dependency on 'gopkg.in/yaml.v3'
-// - therefore: we cannot type the handler properly here
-//
-// It doesn't matter that this isn't an actual Lambda handler, we
-// just need the test build to succeed.
+import (
+  "bytes"
+  "log"
+  "math"
+  "net/http"
+  "strconv"
+  "os"
+)
 
 func main() {
+  endpoint := "http://" + os.Getenv("AWS_LAMBDA_RUNTIME_API") + "/2018-06-01/runtime/invocation"
+
+  // Get requestId
+  headersResponse, err := http.Get(endpoint + "/next")
+  if err != nil {
+    log.Printf("failed to retrieve lambda requestId with error: %v", err)
+    return
+  }
+
+  requestId := headersResponse.Header.Get("Lambda-Runtime-Aws-Request-Id")
+  log.Printf("handling lambda request id: %s", requestId)
+
+  // Post response
+  body := []byte(strconv.FormatFloat(math.Pow(2, 8), 'f', 0, 64))
+  _, err = http.NewRequest("POST", endpoint + "/" + requestId + "/response", bytes.NewBuffer(body))
+
+  if err != nil {
+    log.Printf("failed to post response with error: %v", err)
+    return
+  }
 }
