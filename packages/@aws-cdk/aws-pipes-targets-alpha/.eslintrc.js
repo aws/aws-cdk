5724fe129619083e098e0a50e1c1a4be52ebdb96
Michael Sambol <sambol.michael@gmail.com>
2024-10-16 12:27:36 -0700

    fix defaults

--- packages/@aws-cdk/aws-pipes-targets-alpha/.eslintrc.js
+++ packages/@aws-cdk/aws-pipes-targets-alpha/.eslintrc.js
@@ -2,6 +2,5 @@ const baseConfig = require('@aws-cdk/cdk-build-tools/config/eslintrc');
 baseConfig.parserOptions.project = __dirname + '/tsconfig.json';
 
 baseConfig.rules['import/no-extraneous-dependencies'] = ['error', { devDependencies: true, peerDependencies: true }];
-baseConfig.rules['@aws-cdk/invalid-cfn-imports'] = 'off';
 
 module.exports = baseConfig;
