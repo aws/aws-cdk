0831ec1f9615e0b0ae2fa470c31200d8a0050558
Xia Zhao <78883180+xazhao@users.noreply.github.com>
2024-10-18 15:11:07 -0700

    fix(stepfunctions-tasks): stateMachine construct doesn't generate a valid policy for default StateMachineRole (#31801)
    ### Issue # (if applicable)
    
    Closes #31714.
    
    ### Reason for this change
    
    Currently, the step functions `runEcsTask()` will create an IAM policy. The `Resource` section is an ARN constructed by CDK with wildcard `*` appending at the end. However, CDK should `Ref` the resource directly instead of constructing the ARN, while keeping the revision number.
    
    ### Description of changes
    
    The same solution as https://github.com/aws/aws-cdk/pull/31615. However this change needs to behind a feature flag because it could be a breaking change.
    
    ### Description of how you validated changes
    
    Integration test. Also checked the synth template.
    
    ### Checklist
    - [ ] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/aws-stepfunctions-tasks/test/ecs/integ.ec2-run-task-ref-definition.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-stepfunctions-tasks/test/ecs/integ.ec2-run-task-ref-definition.ts
@@ -4,21 +4,11 @@ import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
 import * as cdk from 'aws-cdk-lib';
 import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
-import { EC2_RESTRICT_DEFAULT_SECURITY_GROUP, STEPFUNCTIONS_TASKS_FIX_RUN_ECS_TASK_POLICY } from 'aws-cdk-lib/cx-api';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { EC2_RESTRICT_DEFAULT_SECURITY_GROUP, STEPFUNCTIONS_TASKS_FIX_RUN_ECS_TASK_POLICY } from 'aws-cdk-lib/cx-api';
 
-/*
- * * Creates a state machine with a task state to run a job with ECS on EC2
- *
- * Stack verification steps:
- * The generated State Machine can be executed from the CLI (or Step Functions console)
- * and runs with an execution status of `Succeeded`.
- *
- * -- aws stepfunctions start-execution --state-machine-arn <state-machine-arn-from-output> provides execution arn
- * -- aws stepfunctions describe-execution --execution-arn <state-machine-arn-from-output> returns a status of `Succeeded`
- */
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'aws-sfn-tasks-ecs-run-task');
+const stack = new cdk.Stack(app, 'aws-sfn-tasks-ecs-run-task-ref-task-definition');
 stack.node.setContext(EC2_RESTRICT_DEFAULT_SECURITY_GROUP, false);
 stack.node.setContext(STEPFUNCTIONS_TASKS_FIX_RUN_ECS_TASK_POLICY, false);
 
@@ -33,7 +23,6 @@ const taskDefinition = new ecs.Ec2TaskDefinition(stack, 'TaskDef');
 const containerDefinition = taskDefinition.addContainer('Container', {
   image: ecs.ContainerImage.fromAsset(path.resolve(__dirname, 'eventhandler-image')),
   memoryLimitMiB: 256,
-  logging: new ecs.AwsLogDriver({ streamPrefix: 'EventDemo' }),
 });
 
 // Build state machine
@@ -47,25 +36,14 @@ const definition = new sfn.Pass(stack, 'Start', {
     containerOverrides: [
       {
         containerDefinition,
-        environment: [
-          {
-            name: 'SOME_KEY',
-            value: sfn.JsonPath.stringAt('$.SomeKey'),
-          },
-        ],
       },
     ],
     launchTarget: new tasks.EcsEc2LaunchTarget(),
-    enableExecuteCommand: true,
   }),
 );
 
-const sm = new sfn.StateMachine(stack, 'StateMachine', {
+new sfn.StateMachine(stack, 'StateMachine', {
   definition,
-});
-
-new cdk.CfnOutput(stack, 'stateMachineArn', {
-  value: sm.stateMachineArn,
 });
 
 new IntegTest(app, 'SfnTasksEcsEc2RunTaskTest', {
