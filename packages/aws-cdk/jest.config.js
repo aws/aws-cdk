2a78c1b082e7bc8b213ba3d5a78bdf6898caffb4
Momo Kornher <kornherm@amazon.co.uk>
2024-10-29 16:53:58 +0000

    add a warning

--- packages/aws-cdk/jest.config.js
+++ packages/aws-cdk/jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
     ...baseConfig,
     coverageThreshold: {
         global: {
+            // We want to improve our test coverage
+            // DO NOT LOWER THESE VALUES!
+            // If you need to break glass, open an issue to re-up the values with additional test coverage
             statements: 81,
             branches: 68,
             functions: 84,
