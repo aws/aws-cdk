c79a1e23778fa7331017ebdc4ac05ed0bdf3ac02
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-10-30 08:02:25 +0900

    temp update integ test

--- packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.eks-bootstrap-self-managed-addons.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.eks-bootstrap-self-managed-addons.ts
@@ -1,25 +1,55 @@
-import { App, Stack } from 'aws-cdk-lib';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
 import * as eks from 'aws-cdk-lib/aws-eks';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as integ from '@aws-cdk/integ-tests-alpha';
 import { getClusterVersionConfig } from './integ-tests-kubernetes-version';
+import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+interface EksClusterStackProps extends StackProps {
+  readonly bootstrapSelfManagedAddons: boolean;
+}
 
 class EksClusterStack extends Stack {
-  constructor(scope: App, id: string) {
+  public cluster: eks.Cluster;
+
+  constructor(scope: App, id: string, props: EksClusterStackProps) {
     super(scope, id);
 
     const vpc = new ec2.Vpc(this, 'Vpc', { natGateways: 1 });
-    new eks.Cluster(this, 'EksCluster', {
+    this.cluster = new eks.Cluster(this, 'Cluster', {
       vpc,
       ...getClusterVersionConfig(this, eks.KubernetesVersion.V1_30),
-      bootstrapSelfManagedAddons: true,
+      bootstrapSelfManagedAddons: props.bootstrapSelfManagedAddons,
     });
   }
 }
 
 const app = new App();
 
-const stack = new EksClusterStack(app, 'EksClusterWithSelfManagedAddonsStack');
-new integ.IntegTest(app, 'EksClusterWithSelfManagedAddons', {
-  testCases: [stack],
+const enabledStack = new EksClusterStack(app, 'EnabledEksClusterSstack', {
+  bootstrapSelfManagedAddons: true,
+});
+const disabledStack = new EksClusterStack(app, 'DisabledEksClusterStack', {
+  bootstrapSelfManagedAddons: false,
+});
+
+const integ = new IntegTest(app, 'EksClusterWithSelfManagedAddons', {
+  testCases: [enabledStack, disabledStack],
+});
+
+const assertion = integ.assertions.awsApiCall('EKS', 'ListAddons', {
+  name: disabledStack.cluster.clusterName,
+}).expect(ExpectedResult.objectLike({
+  addons: ['vpc-cni', 'kube-proxy', 'coredns'],
+}));
+
+integ.assertions.awsApiCall('EKS', 'ListAddons', {
+  name: enabledStack.cluster.clusterName,
+}).expect(ExpectedResult.objectLike({
+  addons: [],
+}));
+
+assertion.provider.addToRolePolicy({
+  Effects: 'Allow',
+  Action: ['eks:*'],
+  Resource: ['*'],
 });
