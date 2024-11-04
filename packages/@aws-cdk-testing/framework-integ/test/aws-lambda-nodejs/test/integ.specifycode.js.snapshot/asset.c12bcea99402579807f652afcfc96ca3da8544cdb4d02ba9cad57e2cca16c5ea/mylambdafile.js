14f80406af32a9f8ce59023fe6530d7b1fec0b62
Rico Hermans <rix0rrr@gmail.com>
2024-11-04 14:22:24 +0100

    chore: bump jsii (#31977)
    This includes a bugfix that produces an error when optional properties are not declared in an abstract class.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ.specifycode.js.snapshot/asset.c12bcea99402579807f652afcfc96ca3da8544cdb4d02ba9cad57e2cca16c5ea/mylambdafile.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-async function handler() {
-    return 'NICE';
-}
-exports.handler = handler;
