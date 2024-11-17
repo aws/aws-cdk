31e1d04d53721ea94b1d10056dfc2ce027510713
kdnakt <a.kid.1985@gmail.com>
2024-11-17 17:04:54 +0900

    fix(lambda): add @deprecated tag to python3.8

--- packages/aws-cdk-lib/aws-lambda/lib/runtime.ts
+++ packages/aws-cdk-lib/aws-lambda/lib/runtime.ts
@@ -147,6 +147,7 @@ export class Runtime {
 
   /**
    * The Python 3.8 runtime (python3.8)
+   * @deprecated Legacy runtime no longer supported by AWS Lambda. Migrate to the latest Python runtime.
    */
   public static readonly PYTHON_3_8 = new Runtime('python3.8', RuntimeFamily.PYTHON, {
     supportsInlineCode: true,
