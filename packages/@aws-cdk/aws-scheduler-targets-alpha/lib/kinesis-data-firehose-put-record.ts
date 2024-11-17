6c4d6220584098557b7b2c585a66662a33a363ab
Grace Luo <54298030+gracelu0@users.noreply.github.com>
2024-11-17 15:17:11 -0800

    fix(scheduler-targets-alpha): kinesis data firehose target uses l1 instead of l2 (#32150)
    ### Issue # (if applicable)
    
    Tracking #31785 
    
    ### Reason for this change
    
    Since the Kinesis Data Firehose Alpha module is in developer preview and contains the L2 construct for a Firehose Delivery Stream, we should make this upgrade from L1 to L2 now while the module is experimental instead of in the future, where we would need to add a V2 for this target (like for event targets, see https://github.com/aws/aws-cdk/pull/30189) to avoid breaking customers. 
    
    ### Description of changes
    
    Replace CfnDeliveryStream with IDeliveryStream. The L1 uses `S3DestinationConfiguration` property whereas the L2 uses `ExtendedS3DestinationConfiguration` property  (includes additional fields on top of S3DestinationConfiguration fields). According to 
     https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-kinesisfirehose-deliverystream.html, "If you change the delivery stream destination from an Amazon S3 destination to an Amazon ES destination, update requires [some interruptions](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-some-interrupt)."
    
    ### Description of how you validated changes
    
    - Updated unit tests and integration test
    - Deployed locally using CfnDeliveryStream then IDeliveryStream and verified the objects are written to the S3 bucket.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*
    
    BREAKING CHANGE: KinesisDataFirehosePutRecord scheduler target now accepts IDeliveryStream instead of CfnDeliveryStream.

--- packages/@aws-cdk/aws-scheduler-targets-alpha/lib/kinesis-data-firehose-put-record.ts
+++ packages/@aws-cdk/aws-scheduler-targets-alpha/lib/kinesis-data-firehose-put-record.ts
@@ -1,6 +1,6 @@
+import { IDeliveryStream } from '@aws-cdk/aws-kinesisfirehose-alpha';
 import { IScheduleTarget } from '@aws-cdk/aws-scheduler-alpha';
 import { IRole, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { CfnDeliveryStream } from 'aws-cdk-lib/aws-kinesisfirehose';
 import { ScheduleTargetBase, ScheduleTargetBaseProps } from './target';
 
 /**
@@ -8,17 +8,17 @@ import { ScheduleTargetBase, ScheduleTargetBaseProps } from './target';
  */
 export class KinesisDataFirehosePutRecord extends ScheduleTargetBase implements IScheduleTarget {
   constructor(
-    private readonly deliveryStream: CfnDeliveryStream,
+    private readonly deliveryStream: IDeliveryStream,
     props: ScheduleTargetBaseProps = {},
   ) {
-    super(props, deliveryStream.attrArn);
+    super(props, deliveryStream.deliveryStreamArn);
   }
 
   protected addTargetActionToRole(role: IRole): void {
 
     role.addToPrincipalPolicy(new PolicyStatement({
       actions: ['firehose:PutRecord'],
-      resources: [this.deliveryStream.attrArn],
+      resources: [this.deliveryStream.deliveryStreamArn],
     }));
   }
 }
--- packages/@aws-cdk/aws-scheduler-targets-alpha/test/integ.kinesis-data-firehose-put-record.ts
+++ packages/@aws-cdk/aws-scheduler-targets-alpha/test/integ.kinesis-data-firehose-put-record.ts
@@ -1,7 +1,8 @@
+import * as firehose from '@aws-cdk/aws-kinesisfirehose-alpha';
+import * as destinations from '@aws-cdk/aws-kinesisfirehose-destinations-alpha';
 import * as scheduler from '@aws-cdk/aws-scheduler-alpha';
 import { AwsApiCall, ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
 import * as cdk from 'aws-cdk-lib';
-import { CfnDeliveryStream } from 'aws-cdk-lib/aws-kinesisfirehose';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { KinesisDataFirehosePutRecord } from '../lib';
 
@@ -29,19 +30,16 @@ const deliveryStreamRole = new cdk.aws_iam.Role(stack, 'deliveryStreamRole', {
 
 destinationBucket.grantReadWrite(deliveryStreamRole);
 
-const firehose = new CfnDeliveryStream(stack, 'MyFirehose', {
-  s3DestinationConfiguration: {
-    bucketArn: destinationBucket.bucketArn,
-    roleArn: deliveryStreamRole.roleArn,
-    bufferingHints: {
-      intervalInSeconds: 60,
-    },
-  },
+const firehoseStream = new firehose.DeliveryStream(stack, 'MyFirehoseStream', {
+  destination: new destinations.S3Bucket(destinationBucket, {
+    role: deliveryStreamRole,
+    bufferingInterval: cdk.Duration.minutes(1),
+  }),
 });
 
 new scheduler.Schedule(stack, 'Schedule', {
   schedule: scheduler.ScheduleExpression.rate(cdk.Duration.minutes(1)),
-  target: new KinesisDataFirehosePutRecord(firehose, {
+  target: new KinesisDataFirehosePutRecord(firehoseStream, {
     input: scheduler.ScheduleTargetInput.fromObject(payload),
   }),
 });
