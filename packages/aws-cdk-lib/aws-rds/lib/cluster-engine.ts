c7685548b98edfeda99e13d1abc9bf7f48d26082
Ren Yamanashi <96362223+ren-yamanashi@users.noreply.github.com>
2024-11-21 07:40:10 +0900

    chore(rds): add support for 8.0.mysql_aurora.3.08.0 (#32180)
    (description based on https://github.com/aws/aws-cdk/pull/30479)
    
    ### Issue # (if applicable)
    
    n/A
    
    ### Reason for this change
    
    Add support for newly supported 8.0.mysql_aurora.3.08.0.  
    https://docs.aws.amazon.com/AmazonRDS/latest/AuroraMySQLReleaseNotes/AuroraMySQL.Updates.3080.html
    
    
    ### Description of changes
    
    Add a new version as a new property to AuroraMysqlEngineVersion class.
    
    
    ### Description of how you validated changes
    
    I used the AWS CLI to verify that the new version is available.
    
    ```sh
    $ aws rds describe-db-engine-versions --engine aurora-mysql --query "DBEngineVersions[?EngineVersion=='8.0.mysql_aurora.3.08.0']"
    
    [
        {
            "Engine": "aurora-mysql",
            "EngineVersion": "8.0.mysql_aurora.3.08.0",
            "DBParameterGroupFamily": "aurora-mysql8.0",
            "DBEngineDescription": "Aurora MySQL",
            "DBEngineVersionDescription": "Aurora MySQL 3.08.0 (compatible with MySQL 8.0.39)",
            "ValidUpgradeTarget": [],
            "ExportableLogTypes": [
                "audit",
                "error",
                "general",
                "slowquery"
            ],
            "SupportsLogExportsToCloudwatchLogs": true,
            "SupportsReadReplica": false,
            "SupportedEngineModes": [
                "provisioned"
            ],
            "SupportedFeatureNames": [
                "Bedrock"
            ],
            "Status": "available",
            "SupportsParallelQuery": true,
            "SupportsGlobalDatabases": true,
            "MajorEngineVersion": "8.0",
            "SupportsBabelfish": false,
            "SupportsCertificateRotationWithoutRestart": true,
            "SupportedCACertificateIdentifiers": [
                "rds-ca-ecc384-g1",
                "rds-ca-rsa4096-g1",
                "rds-ca-rsa2048-g1"
            ],
            "SupportsLocalWriteForwarding": true,
            "SupportsIntegrations": true
        }
    ]
    
    ```
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-rds/lib/cluster-engine.ts
+++ packages/aws-cdk-lib/aws-rds/lib/cluster-engine.ts
@@ -636,6 +636,8 @@ export class AuroraMysqlEngineVersion {
   public static readonly VER_3_07_0 = AuroraMysqlEngineVersion.builtIn_8_0('3.07.0');
   /** Version "8.0.mysql_aurora.3.07.1". */
   public static readonly VER_3_07_1 = AuroraMysqlEngineVersion.builtIn_8_0('3.07.1');
+  /** Version "8.0.mysql_aurora.3.08.0". */
+  public static readonly VER_3_08_0 = AuroraMysqlEngineVersion.builtIn_8_0('3.08.0');
 
   /**
    * Create a new AuroraMysqlEngineVersion with an arbitrary version.
