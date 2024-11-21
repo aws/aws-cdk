7ad82e26950de80488a8ec00ae134c13815b0a4b
go-to-k <24818752+go-to-k@users.noreply.github.com>
2024-11-21 20:15:41 +0900

    Revert "test for review comment"
    This reverts commit 5d64092a6efe9e7ef4c942187470bd5dc9948f3b.

--- packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-domain-cfdist.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-domain-cfdist.ts
@@ -1,12 +1,13 @@
 import { App, CfnOutput, RemovalPolicy, Stack } from 'aws-cdk-lib';
 import { UserPool } from 'aws-cdk-lib/aws-cognito';
+import { LOG_API_RESPONSE_DATA_PROPERTY_TRUE_DEFAULT } from 'aws-cdk-lib/cx-api';
 
 /*
  * Stack verification steps:
  * * Verify that the CloudFrontDistribution stack output is of the format 'xxxxxxxxxxxxxx.cloudfront.net'
  */
 
-const app = new App();
+const app = new App({ postCliContext: { [LOG_API_RESPONSE_DATA_PROPERTY_TRUE_DEFAULT]: false } });
 const stack = new Stack(app, 'integ-user-pool-domain-cfdist');
 
 const userpool = new UserPool(stack, 'UserPool', {
