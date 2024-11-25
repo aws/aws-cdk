390e99f0e391146c3e3a237a5454d49f8e9d2807
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2024-11-25 11:20:01 +0000

    Formatting

--- packages/@aws-cdk/cli-plugin-contract/lib/index.ts
+++ packages/@aws-cdk/cli-plugin-contract/lib/index.ts
@@ -33,19 +33,23 @@ export interface AwsCredentials {
    * AWS access key ID
    */
   readonly accessKeyId: string;
+
   /**
    * AWS secret access key
    */
   readonly secretAccessKey: string;
+
   /**
    * A security or session token to use with these credentials. Usually
    * present for temporary credentials.
    */
   readonly sessionToken?: string;
+
   /**
    * AWS credential scope for this set of credentials.
    */
   readonly credentialScope?: string;
+
   /**
    * AWS accountId.
    */
