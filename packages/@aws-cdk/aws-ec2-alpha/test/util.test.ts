5ac647bc1921fdb545a6858d1d81a4bb735a575c
Indranil Jana <indranij@amazon.com>
2024-11-25 05:14:03 +0000

    Do not use string comparison in rangesOverlap

---
+++ packages/@aws-cdk/aws-ec2-alpha/test/util.test.ts
@@ -0,0 +1,17 @@
+import { CidrBlock } from '../lib/util'
+
+describe('Tests for the CidrBlock.rangesOverlap method to check if IPv4 ranges overlap', () =>{
+    test('Should return false for non-overlapping IP ranges', () => {
+        const testCidr = new CidrBlock('10.0.0.0/16');
+        const range1 = ['10.0.0.0', '10.0.15.255'];
+        const range2 = ['10.0.128.0', '10.0.143.255'];
+        expect(testCidr.rangesOverlap(range1, range2)).toBe(false);
+    });
+
+    test('Should return true for overlapping IP ranges', () => {
+        const testCidr = new CidrBlock('54.0.0.0/17');
+        const range1 = ['54.0.0.0', '54.0.127.255'];
+        const range2 = ['54.0.100.0', '54.0.192.255'];
+        expect(testCidr.rangesOverlap(range1, range2)).toBe(true);
+    });
+});
