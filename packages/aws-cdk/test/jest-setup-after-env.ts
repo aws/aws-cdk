58b9c2f5fece1a5842db0fac0c8b6c7a154792b8
Rico Huijbers <rix0rrr@gmail.com>
2024-11-25 15:15:56 +0100

    Run afterAll in reverse order

--- packages/aws-cdk/test/jest-setup-after-env.ts
+++ packages/aws-cdk/test/jest-setup-after-env.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
+import { isPromise } from 'util/types';
 
 /**
  * Global test setup for Jest tests
@@ -38,6 +39,8 @@ beforeAll(() => {
   tmpDir = process.cwd(); // This will have resolved symlinks
 });
 
+const reverseAfterAll: Array<jest.ProvidesHookCallback> = [];
+
 /**
  * We need a cleanup here
  *
@@ -51,12 +54,32 @@ beforeAll(() => {
  * Jest will then try to write the `coverage` directory to the readonly directory,
  * and fail. Chdir back to the original dir.
  *
- * Only if we are still in the tempdir, because if not then some other temporary
- * directory cleanup block has already done the same and we shouldn't interfere
- * with that.
+ * If the test file has an `afterAll()` hook it installed as well, we need to run
+ * it before our cleanup, otherwise the wrong thing will happen (by default,
+ * all `afterAll()`s run in call order, but they should be run in reverse).
  */
-afterAll(() => {
+afterAll(async () => {
+  for (const aft of reverseAfterAll.reverse()) {
+    await new Promise<void>((resolve, reject) => {
+      const response = aft(resolve as any);
+      if (isPromise(response)) {
+        response.then(() => { return resolve(); }, reject);
+      } else {
+        resolve();
+      }
+    });
+  }
+
+  // eslint-disable-next-line no-console
+  process.stderr.write(`${process.cwd()}, ${tmpDir}\n`);
   if (process.cwd() === tmpDir) {
+    // eslint-disable-next-line no-console
+    process.stderr.write('chmod\n');
     process.chdir(oldDir);
   }
 });
+
+// Patch afterAll to make later-provided afterAll's run before us (in reverse order even).
+afterAll = (after: jest.ProvidesHookCallback) => {
+  reverseAfterAll.push(after);
+};
