4d4190696ed7bd0e4fcc5bda6a68790a04af7e5b
MasakiYamanaka <nes-m.yamanaka@nec.com>
2024-12-04 14:53:28 +0900

    chore(elasticloadbalancingv2): add ssl policy TLS13_12

--- packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/shared/enums.ts
+++ packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/shared/enums.ts
@@ -140,6 +140,11 @@ export enum SslPolicy {
   TLS13_11 = 'ELBSecurityPolicy-TLS13-1-1-2021-06',
 
   /**
+   * TLS1.2 only
+   */
+  TLS13_12 = 'ELBSecurityPolicy-TLS13-1-2-2021-06',
+
+  /**
    * TLS1.3 only
    */
   TLS13_13 = 'ELBSecurityPolicy-TLS13-1-3-2021-06',
