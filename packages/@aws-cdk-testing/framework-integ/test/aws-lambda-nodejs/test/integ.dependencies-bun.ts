c8e046120ee17bedd8abad092ef4fa5feef5bb56
Ben Limmer <630449+blimmer@users.noreply.github.com>
2024-12-05 12:41:08 -0700

    feat(lambda-nodejs): add bun support (#31770)
    ### Issue #31753
    
    Closes #31753.
    
    ### Reason for this change
    
    `bun` is rapidly gaining popularity because it's extremely performant compared to `node`. `bun` has its own lockfile, `bun.lockb`, which is not currently respected by AWS CDK when bundling `NodejsFunction`s.
    
    ### Description of changes
    
    This code is very well-structured, so it was simple to add bun support alongside `yarn`, `pnpm` and `npm`.
    
    ### Description of how you validated changes
    
    I linked this code up to a simple, sample CDK app with a `bun` lockfile. I wasn't able to `bun cdk synth` before my changes. Then, after linking up my local workspace, I was able to bundle with my `bun` lockfile.
    
    I also added unit tests similar to those that exist for the other packages managers.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ.dependencies-bun.ts
@@ -0,0 +1,38 @@
+import * as path from 'path';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as cdk from 'aws-cdk-lib';
+import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+
+const app = new cdk.App();
+
+const stack = new cdk.Stack(app, 'TestStack');
+
+const handler = new lambda.NodejsFunction(stack, 'Function', {
+  entry: path.join(__dirname, 'integ-handlers/bun/dependencies-bun.ts'),
+  runtime: Runtime.NODEJS_20_X,
+  bundling: {
+    minify: true,
+    // Will be installed, not bundled
+    // (axios is a package with sub-dependencies,
+    // will be used to ensure bun bundling works as expected)
+    nodeModules: ['axios'],
+    forceDockerBundling: true,
+  },
+  depsLockFilePath: path.join(__dirname, 'integ-handlers/bun/bun.lockb'),
+});
+
+const integ = new IntegTest(app, 'BunTest', {
+  testCases: [stack],
+  stackUpdateWorkflow: false, // this will tell the runner to not check in assets.
+});
+
+const response = integ.assertions.invokeFunction({
+  functionName: handler.functionName,
+});
+response.expect(ExpectedResult.objectLike({
+  // expect invoking without error
+  StatusCode: 200,
+  ExecutedVersion: '$LATEST',
+  Payload: 'null',
+}));
