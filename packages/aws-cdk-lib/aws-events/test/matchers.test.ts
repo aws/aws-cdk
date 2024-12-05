6fc2ef89d7e06a5fa7b921e8e8a4e534a3d78a25
John Walker <johnawalker@hotmail.com>
2024-12-05 20:08:14 +0100

    feat(events): add filter rules for prefixEqualsIgnoreCase, suffixEqualsIgnoreCase, wildcard, and anythingBut* matches (#32063)
    ### Issue # (if applicable)
    
    Closes #28462.
    
    ### Reason for this change
    
    Add support for:
    * Prefix matching while ignoring case
    * Suffix matching while ignoring case
    * Wildcard matching
    * Anything-but matching on suffixes
    * Anything-but matching using wildcards
    * Anything-but matching while ignoring case
    
    Extend anything-but matching on prefixes to support a list of prefix values.
    
    ### Description of changes
    
    Added functions on `Match` class:
    * `prefixEqualsIgnoreCase()`
    * `suffixEqualsIgnoreCase()`
    * `wildcard()`
    * `anythingButSuffix()`
    * `anythingButWildcard()`
    * `anythingButEqualsIgnoreCase()`
    
    Modified `anythingButPrefix()` to support rest parameters.
    
    ### Description of how you validated changes
    
    Added unit and integration tests for new and changed functions.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-events/test/matchers.test.ts
+++ packages/aws-cdk-lib/aws-events/test/matchers.test.ts
@@ -23,6 +23,48 @@ describe(Match, () => {
     expect(stack.resolve(Match.anythingButPrefix('foo'))).toEqual([
       { 'anything-but': { prefix: 'foo' } },
     ]);
+
+    expect(stack.resolve(Match.anythingButPrefix('foo', 'bar'))).toEqual([
+      { 'anything-but': { prefix: ['foo', 'bar'] } },
+    ]);
+
+    expect(() => stack.resolve(Match.anythingButPrefix())).toThrowError(/must be non-empty lists/);
+  });
+
+  test('anythingButSuffix', () => {
+    expect(stack.resolve(Match.anythingButSuffix('foo'))).toEqual([
+      { 'anything-but': { suffix: 'foo' } },
+    ]);
+
+    expect(stack.resolve(Match.anythingButSuffix('foo', 'bar'))).toEqual([
+      { 'anything-but': { suffix: ['foo', 'bar'] } },
+    ]);
+
+    expect(() => stack.resolve(Match.anythingButPrefix())).toThrowError(/must be non-empty lists/);
+  });
+
+  test('anythingButWildcard', () => {
+    expect(stack.resolve(Match.anythingButWildcard('*.txt'))).toEqual([
+      { 'anything-but': { wildcard: '*.txt' } },
+    ]);
+
+    expect(stack.resolve(Match.anythingButWildcard('*.txt', '*.json'))).toEqual([
+      { 'anything-but': { wildcard: ['*.txt', '*.json'] } },
+    ]);
+
+    expect(() => stack.resolve(Match.anythingButPrefix())).toThrowError(/must be non-empty lists/);
+  });
+
+  test('anythingButEqualsIgnoreCase', () => {
+    expect(stack.resolve(Match.anythingButEqualsIgnoreCase('foo'))).toEqual([
+      { 'anything-but': { 'equals-ignore-case': 'foo' } },
+    ]);
+
+    expect(stack.resolve(Match.anythingButEqualsIgnoreCase('foo', 'bar'))).toEqual([
+      { 'anything-but': { 'equals-ignore-case': ['foo', 'bar'] } },
+    ]);
+
+    expect(() => stack.resolve(Match.anythingButPrefix())).toThrowError(/must be non-empty lists/);
   });
 
   test('numeric', () => {
@@ -74,6 +116,24 @@ describe(Match, () => {
   test('suffix', () => {
     expect(stack.resolve(Match.suffix('foo'))).toEqual([
       { suffix: 'foo' },
+    ]);
+  });
+
+  test('prefixEqualsIgnoreCase', () => {
+    expect(stack.resolve(Match.prefixEqualsIgnoreCase('foo'))).toEqual([
+      { prefix: { 'equals-ignore-case': 'foo' } },
+    ]);
+  });
+
+  test('suffixEqualsIgnoreCase', () => {
+    expect(stack.resolve(Match.suffixEqualsIgnoreCase('foo'))).toEqual([
+      { suffix: { 'equals-ignore-case': 'foo' } },
+    ]);
+  });
+
+  test('wildcard', () => {
+    expect(stack.resolve(Match.wildcard('*.json'))).toEqual([
+      { wildcard: '*.json' },
     ]);
   });
 
