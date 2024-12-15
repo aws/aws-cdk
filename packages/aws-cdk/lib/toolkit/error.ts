96ae9a3621a55e069d85d33b71d248579c29c2e1
Sumu <sumughan@amazon.com>
2024-12-15 12:46:39 -0500

    WIP - initial toolkit error work
    Signed-off-by: Sumu <sumughan@amazon.com>

---
+++ packages/aws-cdk/lib/toolkit/error.ts
@@ -0,0 +1,38 @@
+// lib/toolkit/errors.ts
+
+/**
+ * Represents a general toolkit error in the AWS CDK CLI.
+ */
+class ToolkitError extends Error {
+  /**
+   * Determines if a given error is an instance of ToolkitError.
+   */
+  public static isToolkitError(error: any): error is ToolkitError {
+    return error instanceof ToolkitError;
+  }
+
+  /**
+   * The type of the error, defaults to "toolkit".
+   */
+  public readonly type: string;
+
+  constructor(message: string, type: string = 'toolkit') {
+    super(message);
+    Object.setPrototypeOf(this, ToolkitError.prototype);
+    this.name = new.target.name;
+    this.type = type;
+  }
+}
+
+/**
+ * Represents an authentication-specific error in the AWS CDK CLI.
+ */
+class AuthenticationError extends ToolkitError {
+  constructor(message: string) {
+    super(message, 'authentication');
+    Object.setPrototypeOf(this, AuthenticationError.prototype);
+  }
+}
+
+// Export classes for internal usage only
+export { ToolkitError, AuthenticationError };
