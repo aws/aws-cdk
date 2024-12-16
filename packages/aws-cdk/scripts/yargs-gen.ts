f9894d170cab85373446e60114ef73aaab5f22cc
Kaizen Conroy <zen.conroy@gmail.com>
2024-12-16 16:31:48 -0500

    initial codegen

--- packages/aws-cdk/scripts/yargs-gen.ts
+++ packages/aws-cdk/scripts/yargs-gen.ts
@@ -1,10 +1,11 @@
 import * as fs from 'fs';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { renderYargs } from '@aws-cdk/yargs-gen';
+import { renderYargs, renderCliType } from '@aws-cdk/yargs-gen';
 import { makeConfig } from '../lib/config';
 
 async function main() {
   fs.writeFileSync('./lib/parse-command-line-arguments.ts', await renderYargs(makeConfig()));
+  fs.writeFileSync('./lib/cli-type.ts', await renderCliType(makeConfig()));
 }
 
 main().then(() => {
