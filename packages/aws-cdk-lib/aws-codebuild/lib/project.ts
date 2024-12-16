13f31c492624a7b0d15f1fa36af31e2ab32ec55b
Matsuda <yiyth.fcb6@gmail.com>
2024-12-17 07:28:27 +0900

    feat(codebuild): support auto retry limit for Project (#32507)
    ### Issue # (if applicable)
    
    N/A
    
    ### Reason for this change
    To support auto retry limit in CodeBuild project.
    https://aws.amazon.com/about-aws/whats-new/2024/10/aws-codebuild-retrying-builds-automatically/
    
    **Note** 
    This feature is already in CFn and L1 Constructs.
    https://github.com/aws-cloudformation/cloudformation-coverage-roadmap/issues/2170
    #32446
    
    But not documented in CFn docs.
    
    
    
    ### Description of changes
    Add `autoRetryLimit` property.
    
    
    
    ### Description of how you validated changes
    Add an unit test and an integ test.
    
    
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-codebuild/lib/project.ts
+++ packages/aws-cdk-lib/aws-codebuild/lib/project.ts
@@ -734,6 +734,14 @@ export interface CommonProjectProps {
    * @default - no visibility is set
    */
   readonly visibility?: ProjectVisibility;
+
+  /**
+   * CodeBuild will automatically call retry build using the project's service role up to the auto-retry limit.
+   * `autoRetryLimit` must be between 0 and 10.
+   *
+   * @default - no retry
+   */
+  readonly autoRetryLimit?: number;
 }
 
 export interface ProjectProps extends CommonProjectProps {
@@ -1107,6 +1115,12 @@ export class Project extends ProjectBase {
       this.addFileSystemLocation(fileSystemLocation);
     }
 
+    if (!Token.isUnresolved(props.autoRetryLimit) && (props.autoRetryLimit !== undefined)) {
+      if (props.autoRetryLimit < 0 || props.autoRetryLimit > 10) {
+        throw new Error(`autoRetryLimit must be a value between 0 and 10, got ${props.autoRetryLimit}.`);
+      };
+    };
+
     const resource = new CfnProject(this, 'Resource', {
       description: props.description,
       source: {
@@ -1143,6 +1157,7 @@ export class Project extends ProjectBase {
           return config;
         },
       }),
+      autoRetryLimit: props.autoRetryLimit,
     });
 
     this.addVpcRequiredPermissions(props, resource);
