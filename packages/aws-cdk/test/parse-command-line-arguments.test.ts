38011d81492b08ce54d3e800daac1b61937f69c7
Momo Kornher <kornherm@amazon.co.uk>
2024-12-16 18:44:43 +0000

    chore(cli): simplify config with helper functions
    Now we either call helper functions at build time or declare the function call via a typewriter module proxy.

--- packages/aws-cdk/test/parse-command-line-arguments.test.ts
+++ packages/aws-cdk/test/parse-command-line-arguments.test.ts
@@ -1,8 +1,6 @@
 import { parseCommandLineArguments } from '../lib/parse-command-line-arguments';
-import { yargsNegativeAlias } from '../lib/util/yargs-helpers';
-
 test('cdk deploy -R sets rollback to false', async () => {
-  const argv = await parseCommandLineArguments(['deploy', '-R'], 'open', ['typescript'], ['typescript'], 'test', yargsNegativeAlias);
+  const argv = await parseCommandLineArguments(['deploy', '-R']);
 
   expect(argv.rollback).toBe(false);
 });
