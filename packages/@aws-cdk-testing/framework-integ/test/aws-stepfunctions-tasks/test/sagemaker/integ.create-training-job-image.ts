159fce74ddbe011ead57d1d662309d34f6a5fbea
phuhung273 <phuhung.tranpham@gmail.com>
2024-12-16 11:09:24 +0000

    integ

--- packages/@aws-cdk-testing/framework-integ/test/aws-stepfunctions-tasks/test/sagemaker/integ.create-training-job-image.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-stepfunctions-tasks/test/sagemaker/integ.create-training-job-image.ts
@@ -1,26 +1,34 @@
-import { App, RemovalPolicy, Stack } from 'aws-cdk-lib';
+/// !cdk-integ sfn-sm-training-job-image
+import { App, Duration, Stack } from 'aws-cdk-lib';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { S3Location, SageMakerCreateTrainingJob, InputMode, DockerImage } from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { StateMachine, IntegrationPattern } from 'aws-cdk-lib/aws-stepfunctions';
 import * as integ from '@aws-cdk/integ-tests-alpha';
-import { randomUUID } from 'crypto';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path';
 
 const app = new App();
-const stack = new Stack(app, 'step-functions-sagemaker-create-training-job-image');
+const stack = new Stack(app, 'sfn-sm-training-job-image');
 
-const trainingData = new Bucket(stack, 'TrainingData', {
-  removalPolicy: RemovalPolicy.DESTROY,
+// Please manually cleanup this bucket after testing.
+// We cannot put remove policy DESTROY here as objects cannot be automatically deleted
+const bucket = new Bucket(stack, 'Bucket');
+
+// Reference https://docs.aws.amazon.com/sagemaker/latest/dg/blazingtext.html#bt-inputoutput
+new BucketDeployment(stack, 'TrainSet', {
+  sources: [Source.asset(path.join(__dirname, 'blazingtext-dataset'))],
+  destinationBucket: bucket,
 });
 
-new StateMachine(stack, 'StateMachine', {
+const sm = new StateMachine(stack, 'StateMachine', {
   definition: new SageMakerCreateTrainingJob(stack, 'TrainTask', {
-    trainingJobName: `MyTrainingJob-${randomUUID()}`,
+    trainingJobName: 'MyBlazingTextTrainingJob',
     integrationPattern: IntegrationPattern.RUN_JOB,
     inputDataConfig: [{
       channelName: 'train',
       dataSource: {
         s3DataSource: {
-          s3Location: S3Location.fromBucket(trainingData, 'data/'),
+          s3Location: S3Location.fromBucket(bucket, 'data/'),
         },
       },
     }],
@@ -28,18 +36,34 @@ new StateMachine(stack, 'StateMachine', {
       mode: 'supervised',
     },
     algorithmSpecification: {
-      // Account for AWS builtin BlazingText image
-      trainingImage: DockerImage.fromRegistry(`811284229777.dkr.ecr.${stack.region}.amazonaws.com/blazingtext`), 
+      // AWS Account storing builtin BlazingText image
+      trainingImage: DockerImage.fromRegistry(`811284229777.dkr.ecr.${stack.region}.amazonaws.com/blazingtext`),
       trainingInputMode: InputMode.FAST_FILE,
     },
     outputDataConfig: {
-      s3OutputLocation: S3Location.fromBucket(trainingData, 'result/'),
+      s3OutputLocation: S3Location.fromBucket(bucket, 'result/'),
     },
   }),
 });
 
-new integ.IntegTest(app, 'StepFunctionSageMakerCreateTrainingJobImage', {
+const integTest = new integ.IntegTest(app, 'SfnSMTrainingJobImage', {
   testCases: [stack],
 });
+
+const startExecutionCall = integTest.assertions.awsApiCall('StepFunctions', 'startExecution', {
+  stateMachineArn: sm.stateMachineArn,
+});
+
+integTest.assertions.awsApiCall('StepFunctions', 'describeExecution', {
+  executionArn: startExecutionCall.getAttString('executionArn'),
+  includedData: 'METADATA_ONLY',
+})
+  .expect(integ.ExpectedResult.objectLike({
+    status: 'SUCCEEDED',
+  }))
+  .waitForAssertions({
+    totalTimeout: Duration.minutes(10),
+    interval: Duration.minutes(1),
+  });
 
 app.synth();
