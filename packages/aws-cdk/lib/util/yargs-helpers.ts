2f22887efb54b711b457a7834db02be8e437de9c
Momo Kornher <kornherm@amazon.co.uk>
2024-12-17 12:10:01 +0000

    chore(cli): simplify config with helper functions (#32547)
    ### Reason for this change
    
    Simplify the codegen for CLI config. Previously the generated function expected a number of arguments at call time.
    Instead we now require a single helpers object at build time. This holds typewriter references to any function called during config parsing.
    
    Additionally, some the helper functions for init and migrate are now just called at build time. This is fine because these values don't change dynamically unless a code change is made to the CLI.
    
    ### Description of changes
    
    - Moved all helper functions into a single file
    - Created a typewriter proxy class for that file
    - Use the module proxy instead passing arguments at runtime
    - This allows us to get rid of the `DynamicValue` stuff all-together and instead we can just check if a value is a typewriter expression.
    
    ### Description of how you validated changes
    
    Existing tests cover this. Run additional manual verification as well.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk/lib/util/yargs-helpers.ts
+++ packages/aws-cdk/lib/util/yargs-helpers.ts
@@ -1,10 +1,12 @@
+import * as version from '../../lib/version';
+
 /**
  * yargs middleware to negate an option if a negative alias is provided
  * E.g. `-R` will imply `--rollback=false`
  *
  * @param optionToNegate The name of the option to negate, e.g. `rollback`
  * @param negativeAlias The alias that should negate the option, e.g. `R`
- * @returns
+ * @returns a middleware function that can be passed to yargs
  */
 export function yargsNegativeAlias<T extends { [x in S | L]: boolean | undefined }, S extends string, L extends string>(
   negativeAlias: S,
@@ -19,3 +21,35 @@ export function yargsNegativeAlias<T extends { [x in S | L]: boolean | undefined
     return argv;
   };
 }
+
+/**
+ * Returns true if the current process is running in a CI environment
+ * @returns true if the current process is running in a CI environment
+ */
+export function isCI(): boolean {
+  return process.env.CI !== undefined;
+}
+
+/**
+ * Returns the current version of the CLI
+ * @returns the current version of the CLI
+ */
+export function cliVersion(): string {
+  return version.DISPLAY_VERSION;
+}
+
+/**
+ * Returns the default browser command for the current platform
+ * @returns the default browser command for the current platform
+ */
+export function browserForPlatform(): string {
+  switch (process.platform) {
+    case 'darwin':
+      return 'open %u';
+    case 'win32':
+      return 'start %u';
+    default:
+      return 'xdg-open %u';
+  }
+}
+
--- tools/@aws-cdk/yargs-gen/test/cli.test.ts
+++ tools/@aws-cdk/yargs-gen/test/cli.test.ts
@@ -1,4 +1,7 @@
-import { CliConfig, renderYargs } from '../lib';
+import { $E, expr, ThingSymbol } from '@cdklabs/typewriter';
+import { CliConfig, CliHelpers, renderYargs } from '../lib';
+
+const YARGS_HELPERS = new CliHelpers('./util/yargs-helpers');
 
 describe('render', () => {
   test('can generate global options', async () => {
@@ -20,23 +23,17 @@ describe('render', () => {
       commands: {},
     };
 
-    expect(await renderYargs(config)).toMatchInlineSnapshot(`
+    expect(await renderYargs(config, YARGS_HELPERS)).toMatchInlineSnapshot(`
       "// -------------------------------------------------------------------------------------------
       // GENERATED FROM packages/aws-cdk/lib/config.ts.
       // Do not edit by hand; all changes will be overwritten at build time from the config file.
       // -------------------------------------------------------------------------------------------
       /* eslint-disable @typescript-eslint/comma-dangle, comma-spacing, max-len, quotes, quote-props */
       import { Argv } from 'yargs';
+      import * as helpers from './util/yargs-helpers';
 
       // @ts-ignore TS6133
-      export function parseCommandLineArguments(
-        args: Array<string>,
-        browserDefault: string,
-        availableInitLanguages: Array<string>,
-        migrateSupportedLanguages: Array<string>,
-        version: string,
-        yargsNegativeAlias: any
-      ): any {
+      export function parseCommandLineArguments(args: Array<string>): any {
         return yargs
           .env('CDK')
           .usage('Usage: cdk -a <cdk-app> COMMAND')
@@ -57,7 +54,7 @@ describe('render', () => {
             nargs: 1,
             requiresArg: true,
           })
-          .version(version)
+          .version(helpers.cliVersion())
           .demandCommand(1, '')
           .recommendCommands()
           .help()
@@ -90,23 +87,17 @@ describe('render', () => {
       },
     };
 
-    expect(await renderYargs(config)).toMatchInlineSnapshot(`
+    expect(await renderYargs(config, YARGS_HELPERS)).toMatchInlineSnapshot(`
       "// -------------------------------------------------------------------------------------------
       // GENERATED FROM packages/aws-cdk/lib/config.ts.
       // Do not edit by hand; all changes will be overwritten at build time from the config file.
       // -------------------------------------------------------------------------------------------
       /* eslint-disable @typescript-eslint/comma-dangle, comma-spacing, max-len, quotes, quote-props */
       import { Argv } from 'yargs';
+      import * as helpers from './util/yargs-helpers';
 
       // @ts-ignore TS6133
-      export function parseCommandLineArguments(
-        args: Array<string>,
-        browserDefault: string,
-        availableInitLanguages: Array<string>,
-        migrateSupportedLanguages: Array<string>,
-        version: string,
-        yargsNegativeAlias: any
-      ): any {
+      export function parseCommandLineArguments(args: Array<string>): any {
         return yargs
           .env('CDK')
           .usage('Usage: cdk -a <cdk-app> COMMAND')
@@ -117,10 +108,10 @@ describe('render', () => {
                 alias: 'o',
                 desc: 'text for one',
               })
-              .middleware(yargsNegativeAlias('O', 'one'), true)
               .option('O', { type: 'boolean', hidden: true })
+              .middleware(helpers.yargsNegativeAlias('O', 'one'), true)
           )
-          .version(version)
+          .version(helpers.cliVersion())
           .demandCommand(1, '')
           .recommendCommands()
           .help()
@@ -133,5 +124,28 @@ describe('render', () => {
       const yargs = require('yargs');
       "
     `);
+  });
+
+  test('can pass-through expression unchanged', async () => {
+    const config: CliConfig = {
+      globalOptions: {},
+      commands: {
+        test: {
+          description: 'the action under test',
+          options: {
+            one: {
+              type: 'boolean',
+              default: $E(
+                expr
+                  .sym(new ThingSymbol('banana', YARGS_HELPERS))
+                  .call(expr.lit(1), expr.lit(2), expr.lit(3)),
+              ),
+            },
+          },
+        },
+      },
+    };
+
+    expect(await renderYargs(config, YARGS_HELPERS)).toContain('default: helpers.banana(1, 2, 3)');
   });
 });
