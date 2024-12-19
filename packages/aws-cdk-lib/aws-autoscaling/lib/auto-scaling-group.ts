bda5050b77800d46635684a55e914354ff590770
ren-yamanashi <pinokionigg@icloud.com>
2024-12-19 21:11:05 +0900

    feat: set default value

--- packages/aws-cdk-lib/aws-autoscaling/lib/auto-scaling-group.ts
+++ packages/aws-cdk-lib/aws-autoscaling/lib/auto-scaling-group.ts
@@ -1552,7 +1552,7 @@ export class AutoScalingGroup extends AutoScalingGroupBase implements
       autoScalingGroupName: this.physicalName,
       availabilityZoneDistribution: props.azCapacityDistributionStrategy
         ? { capacityDistributionStrategy: props.azCapacityDistributionStrategy }
-        : undefined,
+        : { capacityDistributionStrategy: CapacityDistributionStrategy.BALANCED_BEST_EFFORT },
       cooldown: props.cooldown?.toSeconds().toString(),
       minSize: Tokenization.stringifyNumber(minCapacity),
       maxSize: Tokenization.stringifyNumber(maxCapacity),
