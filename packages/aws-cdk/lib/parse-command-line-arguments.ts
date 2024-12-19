e4eeabae2e0378c2cd74f17551016abaae0669a9
Rico Huijbers <rix0rrr@gmail.com>
2024-12-19 13:35:38 +0100

    More linter problems in alpha packages

--- packages/aws-cdk/lib/parse-command-line-arguments.ts
+++ packages/aws-cdk/lib/parse-command-line-arguments.ts
@@ -2,7 +2,7 @@
 // GENERATED FROM packages/aws-cdk/lib/config.ts.
 // Do not edit by hand; all changes will be overwritten at build time from the config file.
 // -------------------------------------------------------------------------------------------
-/* eslint-disable @stylistic/comma-dangle, comma-spacing, max-len, quotes, quote-props */
+/* eslint-disable @stylistic/comma-dangle, @stylistic/comma-spacing, @stylistic/max-len, @stylistic/quotes, @stylistic/quote-props */
 import { Argv } from 'yargs';
 import * as helpers from './util/yargs-helpers';
 
