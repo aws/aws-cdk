b41c9d7f61636390f75e71eb15c2fd7b093c9acf
Mason <mpogbourne@gmail.com>
2024-12-21 01:25:06 -0600

    aws-ecs: integration update

--- packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-enhanced-container-insights.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecs/test/integ.cluster-enhanced-container-insights.ts
@@ -1,11 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as integ from '@aws-cdk/integ-tests-alpha';
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'integ-ecs-enhanced-container-insights');
+const vpc = new ec2.Vpc(stack, 'Vpc', { maxAzs: 2, restrictDefaultSecurityGroup: false });
 
 new ecs.Cluster(stack, 'Cluster', {
+  vpc,
   containerInsightsV2: ecs.ContainerInsights.ENHANCED,
 });
 
