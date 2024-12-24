b2ab31a794f39c6bf350ed40cc580c0e8599bf9d
Momo Kornher <kornherm@amazon.co.uk>
2024-12-24 02:56:21 +0100

    chore(toolkit): add proposed initial interfaces (#32643)
    ### Issue
    
    Close #32567 
    
    ### Reason for this change
    
    Add proposed interfaces and stub classes to the `@aws-cdk/toolkit` package according to RFC 300, so integrators can start designing their own solution.
    
    ### Describe any new or updated permissions being added
    
    n/a
    
    ### Description of how you validated changes
    
    n/a unused stub interfaces
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/toolkit/lib/index.ts
+++ packages/@aws-cdk/toolkit/lib/index.ts
@@ -1,1 +1,10 @@
-export class Toolkit {}
+export * from './toolkit';
+export * from './cloud-assembly-source';
+export * from './actions/deploy';
+export * from './actions/destroy';
+export * from './actions/import';
+export * from './actions/synth';
+export * from './actions/watch';
+
+export * from './io-host';
+export * from './types';
