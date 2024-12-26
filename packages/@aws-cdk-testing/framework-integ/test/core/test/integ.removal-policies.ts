467ebbde93e4eb13b194e9cd0090e697d445eea6
watany <76135106+watany-dev@users.noreply.github.com>
2024-12-26 14:28:12 +0000

    integ

--- packages/@aws-cdk-testing/framework-integ/test/core/test/integ.removal-policies.ts
+++ packages/@aws-cdk-testing/framework-integ/test/core/test/integ.removal-policies.ts
@@ -1,4 +1,4 @@
-import { App, RemovalPolicies, Stack } from 'aws-cdk-lib';
+import { App, RemovalPolicy, RemovalPolicies, Stack } from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
@@ -12,7 +12,8 @@ new s3.Bucket(stack, 'TestBucket');
 new dynamodb.Table(stack, 'TestTable', {
   partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
 });
-new iam.User(stack, 'TestUser');
+const user = new iam.User(stack, 'TestUser');
+user.applyRemovalPolicy(RemovalPolicy.RETAIN);
 
 // Apply different removal policies to demonstrate functionality
 RemovalPolicies.of(stack).destroy();
