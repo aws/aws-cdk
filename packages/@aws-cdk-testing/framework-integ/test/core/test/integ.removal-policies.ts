bf5eff9f6e8e2f10068f954c280b8645a0862559
watany <76135106+watany-dev@users.noreply.github.com>
2024-12-27 15:46:50 +0000

    integ

--- packages/@aws-cdk-testing/framework-integ/test/core/test/integ.removal-policies.ts
+++ packages/@aws-cdk-testing/framework-integ/test/core/test/integ.removal-policies.ts
@@ -7,15 +7,19 @@ import * as integ from '@aws-cdk/integ-tests-alpha';
 const app = new App();
 const stack = new Stack(app, 'TestStack');
 
-// Create resources
 new s3.Bucket(stack, 'TestBucket');
+
 new dynamodb.Table(stack, 'TestTable', {
   partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
 });
+
 const user = new iam.User(stack, 'TestUser');
 user.applyRemovalPolicy(RemovalPolicy.RETAIN);
 
-// Apply different removal policies to demonstrate functionality
+new s3.Bucket(stack, 'DestroyBucket', {
+  removalPolicy: RemovalPolicy.DESTROY,
+});
+
 RemovalPolicies.of(stack).destroy();
 
 new integ.IntegTest(app, 'RemovalPoliciesTest', {
