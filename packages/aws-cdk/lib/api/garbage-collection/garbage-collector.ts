a3ad8240f9498d026160d3dbbfdab15b37f9df7a
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2024-12-27 10:09:52 +0000

    feat(cli-plugin-contract): introduce a public contract between CLI and plugins (#32111)
    The contract between the CLI and credential provider plugins is not publicly defined.
    
    Extract the types involved in the CLI-plugin communication into a new package, `@aws-cdk/cli-plugin-contract`, and update all references in the CLI code.
    
    Closes https://github.com/aws/aws-cdk/issues/32099, closes https://github.com/aws/aws-cdk-cli/issues/6, closes https://github.com/aws/aws-cdk/issues/19564
    
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk/lib/api/garbage-collection/garbage-collector.ts
+++ packages/aws-cdk/lib/api/garbage-collection/garbage-collector.ts
@@ -9,7 +9,7 @@ import { DEFAULT_TOOLKIT_STACK_NAME, ToolkitInfo } from '../toolkit-info';
 import { ProgressPrinter } from './progress-printer';
 import { ActiveAssetCache, BackgroundStackRefresh, refreshStacks } from './stack-refresh';
 import { ToolkitError } from '../../toolkit/error';
-import { Mode } from '../plugin';
+import { Mode } from '../plugin/mode';
 
 // Must use a require() otherwise esbuild complains
 // eslint-disable-next-line @typescript-eslint/no-require-imports
