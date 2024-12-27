52cdadc8871fdf7e546dd03a3d55e9ec1231cc1c
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-12-27 10:04:09 +0900

    update s3encryption integ

--- packages/@aws-cdk-testing/framework-integ/test/aws-synthetics/test/integ.canary-artifact-s3-encryption.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-synthetics/test/integ.canary-artifact-s3-encryption.ts
@@ -1,4 +1,5 @@
 /// !cdk-integ canary-one
+import * as path from 'path';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib/core';
 import { ArtifactsEncryptionMode, Canary, Cleanup, Code, Runtime, Schedule, Test } from 'aws-cdk-lib/aws-synthetics';
@@ -15,7 +16,7 @@ const bucket = new s3.Bucket(stack, 'MyTestBucket', {
 });
 const prefix = 'integ';
 
-new Canary(stack, 'CanarySseS3', {
+new Canary(stack, 'CanarySseS3Puppeteer', {
   test: Test.custom({
     handler: 'index.handler',
     code: Code.fromInline(`
@@ -26,6 +27,18 @@ new Canary(stack, 'CanarySseS3', {
   schedule: Schedule.rate(cdk.Duration.minutes(1)),
   artifactsBucketLocation: { bucket, prefix },
   runtime: Runtime.SYNTHETICS_NODEJS_PUPPETEER_7_0,
+  cleanup: Cleanup.LAMBDA,
+  artifactS3EncryptionMode: ArtifactsEncryptionMode.S3_MANAGED,
+});
+
+new Canary(stack, 'CanarySseS3PlayWright', {
+  test: Test.custom({
+    handler: 'canary.handler',
+    code: Code.fromAsset(path.join(__dirname, 'canaries')),
+  }),
+  schedule: Schedule.rate(cdk.Duration.minutes(1)),
+  artifactsBucketLocation: { bucket, prefix },
+  runtime: Runtime.SYNTHETICS_NODEJS_PLAYWRIGHT_1_0,
   cleanup: Cleanup.LAMBDA,
   artifactS3EncryptionMode: ArtifactsEncryptionMode.S3_MANAGED,
 });
