548e5d6c3da838dae45113b0efd28c793a025f6d
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2025-01-04 21:18:14 +0900

    deletion protection check for an environment

--- packages/aws-cdk-lib/aws-appconfig/lib/environment.ts
+++ packages/aws-cdk-lib/aws-appconfig/lib/environment.ts
@@ -142,6 +142,32 @@ abstract class EnvironmentBase extends Resource implements IEnvironment, IExtens
 }
 
 /**
+ * The deletion protection check options.
+ */
+export enum DeletionProtectionCheck {
+  /**
+   * The default setting,
+   * which instructs AWS AppConfig to implement the deletion protection value specified in the UpdateAccountSettings API.
+   */
+  ACCOUNT_DEFAULT = 'ACCOUNT_DEFAULT',
+
+  /**
+   * Instructs the deletion protection check to run,
+   * even if deletion protection is disabled at the account level.
+   *
+   * APPLY also forces the deletion protection check to run against resources created in the past hour,
+   * which are normally excluded from deletion protection checks.
+   */
+  APPLY = 'APPLY',
+
+  /**
+   * Instructs AWS AppConfig to bypass the deletion protection check and delete a configuration profile
+   * even if deletion protection would have otherwise prevented it.
+   */
+  BYPASS = 'BYPASS',
+}
+
+/**
  * Options for the Environment construct.
  */
 export interface EnvironmentOptions {
@@ -165,6 +191,13 @@ export interface EnvironmentOptions {
    * @default - No monitors.
    */
   readonly monitors?: Monitor[];
+
+  /**
+   * A property to prevent accidental deletion of active environments.
+   *
+   * @default undefined - AppConfig default is ACCOUNT_DEFAULT
+   */
+  readonly deletionProtectionCheck?: DeletionProtectionCheck;
 }
 
 /**
@@ -309,6 +342,7 @@ export class Environment extends EnvironmentBase {
       applicationId: this.applicationId,
       name: this.name,
       description: this.description,
+      deletionProtectionCheck: props.deletionProtectionCheck,
       monitors: this.monitors?.map((monitor) => {
         return {
           alarmArn: monitor.alarmArn,
