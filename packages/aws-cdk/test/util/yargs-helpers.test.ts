5dfb8ff36f5fdfdaf859fdca120cf17b121f4135
Rico Huijbers <rix0rrr@gmail.com>
2025-01-06 11:11:19 +0100

    This needed to be 'true'

--- packages/aws-cdk/test/util/yargs-helpers.test.ts
+++ packages/aws-cdk/test/util/yargs-helpers.test.ts
@@ -7,7 +7,7 @@ test.each([
   ['0', false],
   // The following ones are unexpected but this is the legacy behavior we're preserving.
   ['banana', true],
-  ['', false],
+  ['', true],
 ])('test parsing of falsey CI values: %p parses as %p', (envvar, ci) => {
   process.env.CI = envvar;
   expect(isCI()).toEqual(ci);
