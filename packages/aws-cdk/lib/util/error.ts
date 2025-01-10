d51b3142984790f057a69e78749737e7a8b7762e
Sumu <sumughan@amazon.com>
2025-01-10 10:35:53 -0500

    WIP: add comment to formatErrorMessage function
    Signed-off-by: Sumu <sumughan@amazon.com>

--- packages/aws-cdk/lib/util/error.ts
+++ packages/aws-cdk/lib/util/error.ts
@@ -1,3 +1,11 @@
+/**
+ * Takes in an error and returns a correctly formatted string of its error message.
+ * If it is an AggregateError, it will return a string with all the inner errors
+ * formatted and separated by a newline.
+ *
+ * @param error The error to format
+ * @returns A string with the error message(s) of the error
+ */
 export function formatErrorMessage(error: any): string {
   if (error && Array.isArray(error.errors)) {
     const innerMessages = error.errors
