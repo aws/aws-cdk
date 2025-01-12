228177aa138895518361bc84772790fb5ec615a9
ren-yamanashi <pinokionigg@icloud.com>
2025-01-12 19:02:50 +0900

    feat: update jsDoc

--- packages/aws-cdk-lib/aws-cognito/lib/user-pool-client.ts
+++ packages/aws-cdk-lib/aws-cognito/lib/user-pool-client.ts
@@ -356,42 +356,45 @@ export interface UserPoolClientProps extends UserPoolClientOptions {
 
   /**
    * The analytics configuration for this client.
+   * @default - no analytics configuration
    */
   readonly analytics?: AnalyticsConfiguration;
 }
 
 /**
- * The settings for Amazon Pinpoint analytics configuration.
- *
- * With an analytics configuration, your application can collect user-activity metrics for user notifications with a Amazon Pinpoint campaign.
- *
+ * The settings for Amazon Pinpoint analytics configuration.\
+ * With an analytics configuration, your application can collect user-activity metrics for user notifications with a Amazon Pinpoint campaign.\
  * Amazon Pinpoint isn't available in all AWS Regions. For a list of available Regions, see [Amazon Cognito and Amazon Pinpoint Region availability](https://docs.aws.amazon.com/cognito/latest/developerguide/cognito-user-pools-pinpoint-integration.html#cognito-user-pools-find-region-mappings).
  */
 export interface AnalyticsConfiguration {
   /**
-   * The Amazon Resource Name (ARN) of an Amazon Pinpoint project that you want to connect to your user pool app client.
-   *
+   * The Amazon Resource Name (ARN) of an Amazon Pinpoint project that you want to connect to your user pool app client.\
    * Amazon Cognito publishes events to the Amazon Pinpoint project that `ApplicationArn` declares. You can also configure your application to pass an endpoint ID in the `AnalyticsMetadata` parameter of sign-in operations. The endpoint ID is information about the destination for push notifications
+   * @default - no applicationArn
    */
   readonly applicationArn?: string;
 
   /**
    * Your Amazon Pinpoint project ID.
+   * @default - no applicationId
    */
   readonly applicationId?: string;
 
   /**
    * The [external ID](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create_for-user_externalid.html) of the role that Amazon Cognito assumes to send analytics data to Amazon Pinpoint.
+   * @default - no externalId
    */
   readonly externalId?: string;
 
   /**
    * The ARN of an AWS Identity and Access Management role that has the permissions required for Amazon Cognito to publish events to Amazon Pinpoint analytics.
+   * @default - no roleArn
    */
   readonly roleArn?: string;
 
   /**
    * If `UserDataShared` is `true` , Amazon Cognito includes user data in the events that it publishes to Amazon Pinpoint analytics.
+   * @default - false
    */
   readonly userDataShared?: boolean;
 }
