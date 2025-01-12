415929df7b5c08ec80fb5240e63816536b324285
ren-yamanashi <pinokionigg@icloud.com>
2025-01-12 22:05:45 +0900

    feat: update integ test

--- packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-client-analytics.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-client-analytics.ts
@@ -1,8 +1,8 @@
 import { App, RemovalPolicy, Stack } from 'aws-cdk-lib';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
-import { Effect, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
-import * as pinpoint from 'aws-cdk-lib/aws-pinpoint';
+// import { Effect, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+// import * as pinpoint from 'aws-cdk-lib/aws-pinpoint';
 
 const app = new App();
 const stack = new Stack(app, 'UserPoolClientAnalytics');
@@ -11,36 +11,34 @@ const userPool = new UserPool(stack, 'Pool', {
   removalPolicy: RemovalPolicy.DESTROY,
 });
 
-const pinpointApp = new pinpoint.CfnApp(stack, 'PinpointApp', {
-  name: 'cognito-analytics-app',
-});
+// const pinpointApp = new pinpoint.CfnApp(stack, 'PinpointApp', {
+//   name: 'cognito-analytics-app',
+// });
 
-const pinpointRole = new Role(stack, 'PinpointRole', {
-  assumedBy: new ServicePrincipal('cognito-idp.amazonaws.com'),
-});
+// const pinpointRole = new Role(stack, 'PinpointRole', {
+//   assumedBy: new ServicePrincipal('cognito-idp.amazonaws.com'),
+// });
 
-pinpointRole.addToPolicy(new PolicyStatement({
-  effect: Effect.ALLOW,
-  actions: [
-    'mobiletargeting:UpdateEndpoint',
-    'mobiletargeting:PutEvents',
-  ],
-  resources: [pinpointApp.attrArn],
-}));
+// pinpointRole.addToPolicy(new PolicyStatement({
+//   effect: Effect.ALLOW,
+//   actions: [
+//     'mobiletargeting:UpdateEndpoint',
+//     'mobiletargeting:PutEvents',
+//   ],
+//   resources: [pinpointApp.attrArn],
+// }));
 
 new UserPoolClient(stack, 'Client', {
   userPool,
-  analytics: {
-    applicationArn: pinpointApp.attrArn,
-    applicationId: pinpointApp.ref,
-    externalId: 'test-external-id',
-    roleArn: pinpointRole.roleArn,
-    userDataShared: true,
-  },
+  // analytics: {
+  //   applicationArn: pinpointApp.attrArn,
+  //   applicationId: pinpointApp.ref,
+  //   externalId: 'test-external-id',
+  //   roleArn: pinpointRole.roleArn,
+  //   userDataShared: true,
+  // },
 });
 
 new IntegTest(app, 'integ-aws-cognito-user-pool-client-analytics', {
   testCases: [stack],
 });
-
-app.synth();
