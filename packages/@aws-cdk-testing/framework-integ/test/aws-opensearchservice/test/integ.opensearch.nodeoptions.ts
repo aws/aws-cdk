d2c9026e8f1187b47cac18eb6036ccd3f6a63266
Sarvesh Dubey <dubesar@amazon.com>
2025-01-16 00:52:02 +0530

    fixing build issue because of nodeoptions

--- packages/@aws-cdk-testing/framework-integ/test/aws-opensearchservice/test/integ.opensearch.nodeoptions.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-opensearchservice/test/integ.opensearch.nodeoptions.ts
@@ -1,5 +1,4 @@
 import { EbsDeviceVolumeType } from 'aws-cdk-lib/aws-ec2';
-import * as iam from 'aws-cdk-lib/aws-iam';
 import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as opensearch from 'aws-cdk-lib/aws-opensearchservice';
@@ -11,33 +10,11 @@ class TestStack extends Stack {
 
     const domainProps: opensearch.DomainProps = {
       removalPolicy: RemovalPolicy.DESTROY,
-      version: opensearch.EngineVersion.ELASTICSEARCH_7_1,
+      version: opensearch.EngineVersion.OPENSEARCH_2_15,
       ebs: {
         volumeSize: 10,
-        volumeType: EbsDeviceVolumeType.GENERAL_PURPOSE_SSD,
+        volumeType: EbsDeviceVolumeType.GP3,
       },
-      logging: {
-        slowSearchLogEnabled: true,
-        appLogEnabled: true,
-      },
-      nodeToNodeEncryption: true,
-      encryptionAtRest: {
-        enabled: true,
-      },
-      advancedOptions: {
-        'rest.action.multi.allow_explicit_index': 'false',
-        'indices.fielddata.cache.size': '25',
-        'indices.query.bool.max_clause_count': '2048',
-      },
-      // test the access policies custom resource works
-      accessPolicies: [
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: ['es:ESHttp*'],
-          principals: [new iam.AccountRootPrincipal()],
-          resources: ['*'],
-        }),
-      ],
       capacity: {
         multiAzWithStandbyEnabled: false,
         masterNodes: 3,
@@ -57,24 +34,16 @@ class TestStack extends Stack {
         enabled: true,
         availabilityZoneCount: 2,
       },
-      offPeakWindowEnabled: true,
-      offPeakWindowStart: {
-        hours: 20,
-        minutes: 0,
-      },
-      enableAutoSoftwareUpdate: true,
     };
 
-    // create 2 domains to ensure that Cloudwatch Log Group policy names dont conflict
     new opensearch.Domain(this, 'Domain1', domainProps);
-    new opensearch.Domain(this, 'Domain2', domainProps);
   }
 }
 
 const app = new App();
 const stack = new TestStack(app, 'cdk-integ-os-nodeoptions');
 
-new IntegTest(app, 'OpenSearchInteg', {
+new IntegTest(app, 'OpenSearchNodeOptionsInteg', {
   testCases: [stack],
   diffAssets: true,
 });
