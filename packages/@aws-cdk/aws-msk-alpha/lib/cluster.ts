39726e64a5470c8108e220787be507683457f999
Michael Sambol <sambol.michael@gmail.com>
2025-01-14 16:33:43 -0800

    rebase on main

--- packages/@aws-cdk/aws-msk-alpha/lib/cluster.ts
+++ packages/@aws-cdk/aws-msk-alpha/lib/cluster.ts
@@ -649,7 +649,7 @@ export class Cluster extends ClusterBase {
       );
     }
 
-    let clientAuthentication;
+    let clientAuthentication: CfnCluster.ClientAuthenticationProperty | undefined;
     if (props.clientAuthentication) {
       const { saslProps, tlsProps } = props.clientAuthentication;
       clientAuthentication = {
