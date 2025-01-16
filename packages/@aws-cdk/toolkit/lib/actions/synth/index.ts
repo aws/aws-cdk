059e0b4297c877d2743e6ebe69f925c021e11c38
Momo Kornher <kornherm@amazon.co.uk>
2025-01-16 14:58:22 +0000

    api changes

--- packages/@aws-cdk/toolkit/lib/actions/synth/index.ts
+++ packages/@aws-cdk/toolkit/lib/actions/synth/index.ts
@@ -4,7 +4,7 @@ export interface SynthOptions {
   /**
    * Select the stacks
    */
-  readonly stacks: StackSelector;
+  readonly stacks?: StackSelector;
 
   /**
    * After synthesis, validate stacks with the "validateOnSynth" attribute set (can also be controlled with CDK_VALIDATION)
