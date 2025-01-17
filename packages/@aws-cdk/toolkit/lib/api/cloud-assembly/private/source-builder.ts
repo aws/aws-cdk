392970667774c4b2d76ab1b7128a0083792213bb
Momo Kornher <kornherm@amazon.co.uk>
2025-01-17 16:03:33 +0000

    chore(toolkit): generate docs (#32991)
    In lack of a public docs page, use typedoc for now.
    
    ### Description of how you validated changes
    
    Docs only
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/toolkit/lib/api/cloud-assembly/private/source-builder.ts
+++ packages/@aws-cdk/toolkit/lib/api/cloud-assembly/private/source-builder.ts
@@ -19,6 +19,9 @@ export abstract class CloudAssemblySourceBuilder {
 
   /**
    * Create a Cloud Assembly from a Cloud Assembly builder function.
+   * @param builder the builder function
+   * @param props additional configuration properties
+   * @returns the CloudAssembly source
    */
   public async fromAssemblyBuilder(
     builder: AssemblyBuilder,
@@ -52,9 +55,8 @@ export abstract class CloudAssemblySourceBuilder {
 
   /**
    * Creates a Cloud Assembly from an existing assembly directory.
-   * @param directory the directory of the AWS CDK app. Defaults to the current working directory.
-   * @param props additional configuration properties
-   * @returns an instance of `AwsCdkCli`
+   * @param directory the directory of a already produced Cloud Assembly.
+   * @returns the CloudAssembly source
    */
   public async fromAssemblyDirectory(directory: string): Promise<ICloudAssemblySource> {
     const services: ToolkitServices = await this.toolkitServices();
@@ -78,9 +80,8 @@ export abstract class CloudAssemblySourceBuilder {
   }
   /**
    * Use a directory containing an AWS CDK app as source.
-   * @param directory the directory of the AWS CDK app. Defaults to the current working directory.
    * @param props additional configuration properties
-   * @returns an instance of `AwsCdkCli`
+   * @returns the CloudAssembly source
    */
   public async fromCdkApp(app: string, props: CdkAppSourceProps = {}): Promise<ICloudAssemblySource> {
     const services: ToolkitServices = await this.toolkitServices();
