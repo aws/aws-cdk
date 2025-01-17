a736a4ce8987c4330db6bdfd49207d90e4fd9e51
Kaizen Conroy <zen.conroy@gmail.com>
2025-01-17 16:45:59 -0500

    wip tests

--- packages/@aws-cdk/toolkit/test/actions/deploy.test.ts
+++ packages/@aws-cdk/toolkit/test/actions/deploy.test.ts
@@ -1,20 +1,23 @@
-import { RequireApproval } from '../../lib';
+import { RequireApproval, StackParameters } from '../../lib';
 import { Toolkit } from '../../lib/toolkit';
 import { builderFixture, TestIoHost } from '../_helpers';
 
 const ioHost = new TestIoHost();
 const toolkit = new Toolkit({ ioHost });
+const mockRollback = jest.spyOn(toolkit, 'rollback').mockResolvedValue({});
+
+let mockDeployStack = jest.fn().mockResolvedValue({
+  type: 'did-deploy-stack',
+  stackArn: 'arn:aws:cloudformation:region:account:stack/test-stack',
+  outputs: {},
+  noOp: false,
+});
 
 jest.mock('../../lib/api/aws-cdk', () => {
   return {
     ...jest.requireActual('../../lib/api/aws-cdk'),
     Deployments: jest.fn().mockImplementation(() => ({
-      deployStack: jest.fn().mockResolvedValue({
-        type: 'did-deploy-stack',
-        stackArn: 'arn:aws:cloudformation:region:account:stack/test-stack',
-        outputs: {},
-        noOp: false,
-      }),
+      deployStack: mockDeployStack,
       resolveEnvironment: jest.fn().mockResolvedValue({}),
       isSingleAssetPublished: jest.fn().mockResolvedValue(true),
       readCurrentTemplate: jest.fn().mockResolvedValue({ Resources: {} }),
@@ -35,11 +38,7 @@ describe('deploy', () => {
     await toolkit.deploy(cx);
 
     // THEN
-    expect(ioHost.notifySpy).toHaveBeenCalledWith(expect.objectContaining({
-      action: 'deploy',
-      level: 'info',
-      message: expect.stringContaining('Deployment time:'),
-    }));
+    successfulDeployment();
   });
 
   test('request response when require approval is set', async () => {
@@ -73,4 +72,112 @@ describe('deploy', () => {
       message: expect.stringContaining('Do you wish to deploy these changes'),
     }));
   });
+
+  describe('deployment options', () => {
+    test('parameters are passed in', async () => {
+      // WHEN
+      const cx = await builderFixture(toolkit, 'stack-with-role');
+      await toolkit.deploy(cx, {
+        parameters: StackParameters.exactly({
+          'my-param': 'my-value',
+        }),
+      });
+
+      // passed through correctly to Deployments
+      expect(mockDeployStack).toHaveBeenCalledWith(expect.objectContaining({
+        parameters: { 'my-param': 'my-value' },
+      }));
+
+      successfulDeployment();
+    });
+
+    test('notification arns are passed in', async () => {
+      // WHEN
+      const arn = 'arn:aws:sns:us-east-1:1111111111:resource';
+      const cx = await builderFixture(toolkit, 'stack-with-role');
+      await toolkit.deploy(cx, {
+        notificationArns: [arn],
+      });
+
+      // passed through correctly to Deployments
+      expect(mockDeployStack).toHaveBeenCalledWith(expect.objectContaining({
+        notificationArns: [arn],
+      }));
+
+      successfulDeployment();
+    });
+
+    test('notification arns from stack are passed in', async () => {
+      // WHEN
+      const arn = 'arn:aws:sns:us-east-1:222222222222:resource';
+      const cx = await builderFixture(toolkit, 'stack-with-notification-arns');
+      await toolkit.deploy(cx, {
+        notificationArns: [arn],
+      });
+
+      // passed through correctly to Deployments
+      expect(mockDeployStack).toHaveBeenCalledWith(expect.objectContaining({
+        notificationArns: [
+          arn,
+          'arn:aws:sns:us-east-1:1111111111:resource',
+          'arn:aws:sns:us-east-1:1111111111:other-resource',
+        ],
+      }));
+
+      successfulDeployment();
+    });
+
+    test('non sns notification arn results in error', async () => {
+      // WHEN
+      const arn = 'arn:aws:sqs:us-east-1:1111111111:resource';
+      const cx = await builderFixture(toolkit, 'stack-with-role');
+      await expect(async () => toolkit.deploy(cx, {
+        notificationArns: [arn],
+      })).rejects.toThrow(/Notification arn arn:aws:sqs:us-east-1:1111111111:resource is not a valid arn for an SNS topic/);
+    });
+  });
+
+  describe('deployment results', () => {
+    test('did-deploy-result', async () => {
+      // WHEN
+      const cx = await builderFixture(toolkit, 'stack-with-role');
+      await toolkit.deploy(cx);
+
+      // THEN
+      successfulDeployment();
+    });
+
+    test('failpaused-need-rollback-first', async () => {
+      // GIVEN
+      mockDeployStack = jest.fn().mockResolvedValue({
+        type: 'failpaused-need-rollback-first',
+        stackArn: 'arn:aws:cloudformation:region:account:stack/test-stack',
+        outputs: {},
+        noOp: false,
+      });
+
+      // WHEN
+      const cx = await builderFixture(toolkit, 'stack-with-role');
+      await toolkit.deploy(cx);
+
+      // THEN
+      noDeployment();
+    });
+  });
 });
+
+function successfulDeployment() {
+  expect(ioHost.notifySpy).toHaveBeenCalledWith(expect.objectContaining({
+    action: 'deploy',
+    level: 'info',
+    message: expect.stringContaining('Deployment time:'),
+  }));
+}
+
+function noDeployment() {
+  expect(ioHost.notifySpy).not.toHaveBeenCalledWith(expect.objectContaining({
+    action: 'deploy',
+    level: 'info',
+    message: expect.stringContaining('Deployment time:'),
+  }));
+}
