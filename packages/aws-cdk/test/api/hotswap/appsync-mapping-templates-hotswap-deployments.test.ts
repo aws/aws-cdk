f7b2d7374e791406ae5aa599a76999c01810b2c1
Rico Huijbers <rix0rrr@gmail.com>
2025-01-17 14:03:28 +0100

    chore: reduce the runtime of CLI unit tests by 2 minutes
    There was a test that was waiting a minute for some operation to time
    out after 7 retries.
    
    Make those retries happen faster by hijacking the timer system.

--- packages/aws-cdk/test/api/hotswap/appsync-mapping-templates-hotswap-deployments.test.ts
+++ packages/aws-cdk/test/api/hotswap/appsync-mapping-templates-hotswap-deployments.test.ts
@@ -1045,8 +1045,15 @@ describe.each([HotswapMode.FALL_BACK, HotswapMode.HOTSWAP_ONLY])('%p mode', (hot
   );
 
   silentTest(
-    'updateFunction() API fails if it recieves 7 failed attempts in a row - this is a long running test',
+    'updateFunction() API fails if it recieves 7 failed attempts in a row',
     async () => {
+      const realSetTimeout = setTimeout;
+      jest.useFakeTimers();
+
+      // Ignore the wait times that the SDK tries to impose and always set timers for 1 ms
+      jest.spyOn(global, 'setTimeout').mockImplementation((fn) => {
+        return realSetTimeout(fn, 1);
+      });
 
       // GIVEN
       mockAppSyncClient
@@ -1123,6 +1130,8 @@ describe.each([HotswapMode.FALL_BACK, HotswapMode.HOTSWAP_ONLY])('%p mode', (hot
         requestMappingTemplate: '## original request template',
         responseMappingTemplate: '## new response template',
       });
+
+      jest.useRealTimers();
     },
     320000,
   );
