a10d48659648ee910d52e92bdf9639d48cb6a4e9
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2025-01-19 11:43:24 +0900

    update integ

--- packages/@aws-cdk-testing/framework-integ/test/aws-synthetics/test/integ.canary-provisioned-resource-cleanup.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-synthetics/test/integ.canary-provisioned-resource-cleanup.ts
@@ -1,13 +1,15 @@
 import { App, Duration, Size, Stack, StackProps } from 'aws-cdk-lib/core';
-import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { AwsApiCall, ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
 import { Construct } from 'constructs';
 import * as synthetics from 'aws-cdk-lib/aws-synthetics';
 
 class TestStack extends Stack {
+  public cleanupCanary: synthetics.Canary;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new synthetics.Canary(this, 'CleanupCanary', {
+    this.cleanupCanary = new synthetics.Canary(this, 'CleanupCanary', {
       canaryName: 'cleanup',
       runtime: synthetics.Runtime.SYNTHETICS_NODEJS_PUPPETEER_7_0,
       test: synthetics.Test.custom({
@@ -21,26 +23,28 @@ class TestStack extends Stack {
       timeout: Duration.minutes(4),
       provisionedResourceCleanup: true,
     });
-
-    new synthetics.Canary(this, 'NormalCanary', {
-      canaryName: 'normal',
-      runtime: synthetics.Runtime.SYNTHETICS_NODEJS_PUPPETEER_7_0,
-      test: synthetics.Test.custom({
-        handler: 'index.handler',
-        code: synthetics.Code.fromInline(`
-          exports.handler = async () => {
-            console.log(\'hello world\');
-          };`),
-      }),
-      memory: Size.mebibytes(2048),
-      timeout: Duration.minutes(4),
-      provisionedResourceCleanup: false,
-    });
   }
 }
 
 const app = new App();
+const testStack = new TestStack(app, 'SyntheticsCanaryProvisionedResourceCleanupStack');
 
-new IntegTest(app, 'SyntheticsCanaryProvisionedResourceCleanup', {
-  testCases: [new TestStack(app, 'SyntheticsCanaryProvisionedResourceCleanupStack')],
+const integ = new IntegTest(app, 'SyntheticsCanaryProvisionedResourceCleanup', {
+  testCases: [testStack],
 });
+
+const getCleanupCanaryInfo = integ.assertions.awsApiCall('synthetics', 'getCanary', {
+  Name: testStack.cleanupCanary.canaryName,
+});
+const stopCanary = integ.assertions.awsApiCall('synthetics', 'stopCanary', {
+  Name: testStack.cleanupCanary.canaryName,
+}).waitForAssertions() as AwsApiCall;
+const deleteCanary = integ.assertions.awsApiCall('synthetics', 'deleteCanary', {
+  Name: testStack.cleanupCanary.canaryName,
+}).waitForAssertions();
+const getLayer = integ.assertions.awsApiCall('lambda', 'listLayers');
+getLayer.expect(ExpectedResult.objectLike({
+  Layers: [],
+}));
+
+getCleanupCanaryInfo.next(stopCanary).next(deleteCanary).next(getLayer).waitForAssertions();
