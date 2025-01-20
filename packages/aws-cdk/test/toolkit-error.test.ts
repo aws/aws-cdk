a6857378a113e9a686c1a124eef3ccdc5c3248d2
Momo Kornher <kornherm@amazon.co.uk>
2025-01-20 10:08:37 +0000

    feat(cli): throw typed errors (#33005)
    ### Reason for this change
    
    In #32548 we enabled typed errors for some places in the CLI.
    However many places were missed and the eslint rule wasn't enabled to
    enforce it in future.
    
    ### Description of changes
    
    Enforce by enabling the respective eslint rule.
    Also adds and implements the eslint rule in the toolkit.
    
    This has little functional effect since all new errors are still
    `Error`s. The printed output of an error will slightly change.
    
    ### Describe any new or updated permissions being added
    
    n/a
    
    ### Description of how you validated changes
    
    existing tests
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING
    GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and
    [DESIGN
    GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache-2.0 license*
    
    (cherry picked from commit bf81b3ce17941f1f84b386f1e935150893b58315)

--- packages/aws-cdk/test/toolkit-error.test.ts
+++ packages/aws-cdk/test/toolkit-error.test.ts
@@ -1,20 +1,23 @@
-import { AssemblyError, AuthenticationError, ToolkitError } from '../lib/toolkit/error';
+import { AssemblyError, AuthenticationError, ContextProviderError, ToolkitError } from '../lib/toolkit/error';
 
 describe('toolkit error', () => {
   let toolkitError = new ToolkitError('Test toolkit error');
   let authError = new AuthenticationError('Test authentication error');
   let assemblyError = new AssemblyError('Test authentication error');
+  let contextProviderError = new ContextProviderError('Test context provider error');
 
   test('types are correctly assigned', async () => {
     expect(toolkitError.type).toBe('toolkit');
     expect(authError.type).toBe('authentication');
     expect(assemblyError.type).toBe('assembly');
+    expect(contextProviderError.type).toBe('context-provider');
   });
 
   test('isToolkitError works', () => {
     expect(ToolkitError.isToolkitError(toolkitError)).toBe(true);
     expect(ToolkitError.isToolkitError(authError)).toBe(true);
     expect(ToolkitError.isToolkitError(assemblyError)).toBe(true);
+    expect(ToolkitError.isToolkitError(contextProviderError)).toBe(true);
   });
 
   test('isAuthenticationError works', () => {
@@ -26,5 +29,11 @@ describe('toolkit error', () => {
     expect(ToolkitError.isAssemblyError(assemblyError)).toBe(true);
     expect(ToolkitError.isAssemblyError(toolkitError)).toBe(false);
     expect(ToolkitError.isAssemblyError(authError)).toBe(false);
+  });
+
+  test('isContextProviderError works', () => {
+    expect(ToolkitError.isContextProviderError(contextProviderError)).toBe(true);
+    expect(ToolkitError.isContextProviderError(toolkitError)).toBe(false);
+    expect(ToolkitError.isContextProviderError(authError)).toBe(false);
   });
 });
