130ead90f3f63c993752d1db8387df14b8d477fd
Kaizen Conroy <zen.conroy@gmail.com>
2025-01-22 16:12:09 -0500

    chore(toolkit): rollback implementation that builds

--- packages/@aws-cdk/toolkit/lib/actions/rollback/index.ts
+++ packages/@aws-cdk/toolkit/lib/actions/rollback/index.ts
@@ -41,4 +41,18 @@ export interface RollbackOptions {
    * @default true
    */
   readonly validateBootstrapStackVersion?: boolean;
+
+  /**
+   * Name of the toolkit stack to use/deploy
+   *
+   * @default CDKToolkit
+   */
+  readonly toolkitStackName?: string;
+
+  /**
+   * Whether to force the rollback or not
+   *
+   * @default false
+   */
+  readonly force?: boolean;
 }
