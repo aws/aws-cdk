1b9707a6af5098c3a32082b678b96964e6738e7a
Zhou <zmengyi@amazon.com>
2025-01-21 20:19:07 -0800

    Address comments

--- packages/@aws-cdk/aws-applicationsignals-alpha/README.md
+++ packages/@aws-cdk/aws-applicationsignals-alpha/README.md
@@ -15,20 +15,79 @@
 
 <!--END STABILITY BANNER-->
 
+<!--BEGIN STABILITY BANNER-->
+
+---
+
+![cdk-constructs: Experimental](https://img.shields.io/badge/cdk--constructs-experimental-important.svg?style=for-the-badge)
+
+> The APIs of higher level constructs in this module are experimental and under active development.
+> They are subject to non-backward compatible changes or removal in any future version. These are
+> not subject to the [Semantic Versioning](https://semver.org/) model and breaking changes will be
+> announced in the release notes. This means that while you may use them, you may need to update
+> your source code when upgrading to a newer version of this package.
+
+---
+
+## Application Signals
+
+CloudWatch Application Signals is an auto-instrumentation solution built on OpenTelemetry that enables zero-code collection of monitoring data, such
+as traces and metrics, from applications running across multiple platforms. It also supports topology auto-discovery based on collected monitoring
+data and includes a new feature for managing service-level objectives (SLOs).
+
+It supports Java, Python, .NET, and Node.js on platforms including EKS (and native Kubernetes), Lambda, ECS, and EC2. For more details, visit
+[Application Signals](https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Application-Monitoring-Sections.html) on the AWS
+public website.
+
+## Application Signals L2 CDK for enablement
+
+This module is a collection of L2 constructs which leverages native L1 CFN resources, simplifying the enablement steps and the creation of Application
+Signals resources.
+
 This module is part of the [AWS Cloud Development Kit](https://github.com/aws/aws-cdk) project.
 
 ```ts nofixture
 import * as appsignals from '@aws-cdk/aws-applicationsignals-alpha';
 ```
 
-## Enable Application Signals on ECS with sidecar mode
+### ApplicationSignalsIntegration
+
+`ApplicationSignalsIntegration` aims to address key challenges in the current CDK enablement process, which requires complex manual configurations for
+ECS customers. Application Signals is designed to be flexible and is supported for other platforms as well. However, the initial focus is on supporting
+ECS, with plans to potentially extend support to other platforms in the future.
+
+#### 1. Enable Application Signals on ECS with sidecar mode
+
+##### 1.1. Create a TaskDefinition for application
+
+```ts
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+
+const fargateTaskDefinition = new ecs.FargateTaskDefinition(stack, 'FargateTaskDefinition', {
+  taskRole: taskRole,
+  cpu: 2048,
+  memoryLimitMiB: 4096
+});
+
+fargateTaskDefinition.addContainer('app', {
+  image: ...,
+  ...
+});
+```
+
+##### 1.2. Configure ApplicationSignalsIntegration
+
+Key steps:
+
+1. Configure `instrumentation` to instrument the application with the ADOT Java Agent.
+2. Setting `enableSidecar` to true to add the CloudWatch Agent as a sidecar container.
 
 ```ts
 new appsignals.ApplicationSignalsIntegration(this, 'ApplicationSignalsIntegration', {
   taskDefinition,
   instrumentation: {
     language: appsignals.InstrumentationLanguage.JAVA,
-    sdkVersion: appsignals.Instrumentation.JAVA_1_32_6
+    sdkVersion: appsignals.JavaInstrumentationVersion.V1_32_6
   },
   serviceName: 'sample-app',
   cloudWatchAgent: {
@@ -37,14 +96,20 @@ new appsignals.ApplicationSignalsIntegration(this, 'ApplicationSignalsIntegratio
 });
 ```
 
-## Enable Application Signals on ECS with daemon mode
+#### 2. Enable Application Signals on ECS with daemon mode
 
-Since the daemon deployment strategy is not supported on ECS Fargate, this mode is only supported on ECS on EC2.
+Note: Since the daemon deployment strategy is not supported on ECS Fargate, this mode is only supported on ECS on EC2.
+
+##### 2.1. Run CloudWatch Agent as a daemon service
 
 ```ts
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as logs from 'aws-cdk-lib/aws-logs';
 
-// create CloudWatch agent daemon service
+// Create a task definition for CloudWatch agent
+const cwAgentTaskDefinition = new ecs.Ec2TaskDefinition(this, 'CloudWatchAgentTaskDefinition');
+
+// Add CloudWatch agent container
 const cwAgentContainer = cwAgentTaskDefinition.addContainer('ecs-cwagent', {
     image: ecs.ContainerImage.fromRegistry("public.ecr.aws/cloudwatch-agent/cloudwatch-agent:latest"),
     cpu: 128,
@@ -63,7 +128,7 @@ const cwAgentContainer = cwAgentTaskDefinition.addContainer('ecs-cwagent', {
     ],
     logging: new ecs.AwsLogDriver({
         streamPrefix: 'ecs',
-        logGroup: new LogGroup(this, 'CwAgentLogGroup', {
+        logGroup: new logs.LogGroup(this, 'CwAgentLogGroup', {
             logGroupName: '/ecs/ecs-cwagent-cdk-daemon',
             removalPolicy: cdk.RemovalPolicy.DESTROY,
         }),
@@ -73,29 +138,55 @@ const cwAgentContainer = cwAgentTaskDefinition.addContainer('ecs-cwagent', {
     }
 });
 
-const cwAgentService = new ecs.Ec2Service(this, 'CwAgentDaemonService', {
-    cluster,
-    taskDefinition: cwAgentTaskDefinition,
-    daemon: true,
-    serviceConnectConfiguration: {
+// Create the CloudWatch agent daemon service
+new ecs.Ec2Service(this, 'CloudWatchAgentDaemon', {
+  cluster,
+  taskDefinition: cwAgentTaskDefinition,
+  daemon: true,  // Runs one container per EC2 instance
+  serviceConnectConfiguration: { // Create service connect
     namespace: namespace.namespaceArn,
     services: [{
-        portMappingName: 'cwagent-4316',
-        dnsName: 'cwagent-4316-http',
-        port: 4316
+      portMappingName: 'cwagent-4316',
+      dnsName: 'cwagent-4316-http',
+      port: 4316
     }, {
-        portMappingName: 'cwagent-2000',
-        dnsName: 'cwagent-2000-http',
-        port: 2000
+      portMappingName: 'cwagent-2000',
+      dnsName: 'cwagent-2000-http',
+      port: 2000
     }]
-    }
+  }
+});
+```
+
+##### 2.2. Create a TaskDefinition for application
+
+```ts
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+
+const ec2TaskDefinition = new ecs.Ec2TaskDefinition(this, 'EC2TaskDefinition', {
+  taskRole: taskRole,
+  ...
 });
 
-// configure ApplicationSignalsIntegration to use Service Connect to communicate to the CloudWatch agent server
-new appsignals.ApplicationSignalsIntegration(this, 'ApplicationSignalsIntegration', taskDefinition, {
+ec2TaskDefinition.addContainer('app', {
+  image: ...,
+  ...
+});
+```
+
+##### 2.3. Configure ApplicationSignalsIntegration
+
+Key steps:
+
+1. Configure `instrumentation` to instrument the application with the ADOT Python Agent.
+2. Set `enableSidecar` to false to disable running CloudWatch agent as a sidecar.
+3. Override environment variables by configuring `overrideEnvironments` to use Service Connect to communicate to the CloudWatch agent server
+
+```ts
+new appsignals.ApplicationSignalsIntegration(this, 'ApplicationSignalsIntegration', ec2TaskDefinition, {
   instrumentation: {
     language: appsignals.InstrumentationLanguage.PYTHON,
-    sdkVersion: appsignals.Instrumentation.PYTHON_V0_7_0
+    sdkVersion: appsignals.PythonInstrumentationVersion.V0_8_0
   },
   overrideEnvironments: [{
     name: appsignals.CommonExporting.OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT,
@@ -115,5 +206,5 @@ new appsignals.ApplicationSignalsIntegration(this, 'ApplicationSignalsIntegratio
     // disable sidecar mode
     enableSidecar: false,
   }
-})
+});
 ```
--- packages/@aws-cdk/aws-applicationsignals-alpha/test/ecs-enablement.test.ts
+++ packages/@aws-cdk/aws-applicationsignals-alpha/test/ecs-enablement.test.ts
@@ -2,7 +2,7 @@
 import { Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
-import { ApplicationSignalsIntegration, InstrumentationLanguage, DotnetInstrumentationVersion, PythonInstrumentationVersion, JavaInstrumentationVersion, NodeInstrumentationVersion } from '../lib';
+import { ApplicationSignalsIntegration, DotnetInstrumentationVersion, PythonInstrumentationVersion, JavaInstrumentationVersion, NodeInstrumentationVersion } from '../lib';
 
 describe('application signals integration', () => {
   test('should create basic application signals configurations with java', () => {
@@ -20,7 +20,6 @@ describe('application signals integration', () => {
     new ApplicationSignalsIntegration(stack, 'TestIntegration', {
       taskDefinition: fargateTaskDefinition,
       instrumentation: {
-        language: InstrumentationLanguage.JAVA,
         sdkVersion: JavaInstrumentationVersion.V1_32_6,
       },
     });
@@ -197,7 +196,6 @@ describe('application signals integration', () => {
       new ApplicationSignalsIntegration(stack, 'TestFailure', {
         taskDefinition: fargateTaskDefinition,
         instrumentation: {
-          language: InstrumentationLanguage.JAVA,
           sdkVersion: JavaInstrumentationVersion.V1_32_6,
         },
         serviceName: 'demo',
@@ -226,7 +224,6 @@ describe('application signals integration', () => {
     new ApplicationSignalsIntegration(stack, 'TestIntegration', {
       taskDefinition: ec2TaskDefinition,
       instrumentation: {
-        language: InstrumentationLanguage.PYTHON,
         sdkVersion: PythonInstrumentationVersion.V0_8_0,
       },
       serviceName: 'demo',
@@ -385,7 +382,6 @@ describe('application signals integration', () => {
     new ApplicationSignalsIntegration(stack, 'TestIntegration', {
       taskDefinition: ec2TaskDefinition,
       instrumentation: {
-        language: InstrumentationLanguage.DOTNET,
         sdkVersion: DotnetInstrumentationVersion.V1_6_0,
       },
       serviceName: 'demo',
@@ -539,7 +535,7 @@ describe('application signals integration', () => {
     });
   });
 
-  test('should create application signals configurations with dotnet on windows', () => {
+  test('should create application signals configurations with dotnet on windows2019', () => {
     // GIVEN
     const stack = new Stack();
 
@@ -556,7 +552,6 @@ describe('application signals integration', () => {
     new ApplicationSignalsIntegration(stack, 'TestIntegration', {
       taskDefinition: ec2TaskDefinition,
       instrumentation: {
-        language: InstrumentationLanguage.DOTNET,
         sdkVersion: DotnetInstrumentationVersion.V1_6_0_WINDOWS2019,
         runtimePlatform: {
           operatingSystemFamily: ecs.OperatingSystemFamily.WINDOWS_SERVER_2019_CORE,
@@ -721,6 +716,187 @@ describe('application signals integration', () => {
     });
   });
 
+  test('should create application signals configurations with dotnet on windows2022', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    const ec2TaskDefinition = new ecs.Ec2TaskDefinition(stack, 'TestTaskDefinition', {
+      networkMode: ecs.NetworkMode.HOST,
+    });
+    ec2TaskDefinition.addContainer('app', {
+      image: ecs.ContainerImage.fromRegistry('nathanpeck/name'),
+      cpu: 0,
+      memoryLimitMiB: 512,
+    });
+
+    new ApplicationSignalsIntegration(stack, 'TestIntegration', {
+      taskDefinition: ec2TaskDefinition,
+      instrumentation: {
+        sdkVersion: DotnetInstrumentationVersion.V1_6_0_WINDOWS2022,
+        runtimePlatform: {
+          operatingSystemFamily: ecs.OperatingSystemFamily.WINDOWS_SERVER_2022_CORE,
+        },
+      },
+      serviceName: 'demo',
+      cloudWatchAgent: {
+        enableSidecar: false,
+      },
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::ECS::TaskDefinition', {
+      ContainerDefinitions: [
+        {
+          Environment: [
+            {
+              Name: 'OTEL_LOGS_EXPORTER',
+              Value: 'none',
+            },
+            {
+              Name: 'OTEL_METRICS_EXPORTER',
+              Value: 'none',
+            },
+            {
+              Name: 'OTEL_EXPORTER_OTLP_PROTOCOL',
+              Value: 'http/protobuf',
+            },
+            {
+              Name: 'OTEL_AWS_APPLICATION_SIGNALS_ENABLED',
+              Value: 'true',
+            },
+            {
+              Name: 'OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT',
+              Value: 'http://localhost:4316/v1/metrics',
+            },
+            {
+              Name: 'OTEL_EXPORTER_OTLP_TRACES_ENDPOINT',
+              Value: 'http://localhost:4316/v1/traces',
+            },
+            {
+              Name: 'OTEL_TRACES_SAMPLER',
+              Value: 'xray',
+            },
+            {
+              Name: 'OTEL_TRACES_SAMPLER_ARG',
+              Value: 'endpoint=http://localhost:2000',
+            },
+            {
+              Name: 'OTEL_PROPAGATORS',
+              Value: 'tracecontext,baggage,b3,xray',
+            },
+            {
+              Name: 'OTEL_DOTNET_DISTRO',
+              Value: 'aws_distro',
+            },
+            {
+              Name: 'OTEL_DOTNET_CONFIGURATOR',
+              Value: 'aws_configurator',
+            },
+            {
+              Name: 'OTEL_DOTNET_AUTO_PLUGINS',
+              Value: 'AWS.Distro.OpenTelemetry.AutoInstrumentation.Plugin, AWS.Distro.OpenTelemetry.AutoInstrumentation',
+            },
+            {
+              Name: 'CORECLR_ENABLE_PROFILING',
+              Value: '1',
+            },
+            {
+              Name: 'CORECLR_PROFILER',
+              Value: '{918728DD-259F-4A6A-AC2B-B85E1B658318}',
+            },
+            {
+              Name: 'CORECLR_PROFILER_PATH',
+              Value: 'C:\\otel-auto-instrumentation-dotnet\\win-x64\\OpenTelemetry.AutoInstrumentation.Native.dll',
+            },
+            {
+              Name: 'DOTNET_STARTUP_HOOKS',
+              Value: 'C:\\otel-auto-instrumentation-dotnet\\net\\OpenTelemetry.AutoInstrumentation.StartupHook.dll',
+            },
+            {
+              Name: 'DOTNET_ADDITIONAL_DEPS',
+              Value: 'C:\\otel-auto-instrumentation-dotnet\\AdditionalDeps',
+            },
+            {
+              Name: 'OTEL_DOTNET_AUTO_HOME',
+              Value: 'C:\\otel-auto-instrumentation-dotnet',
+            },
+            {
+              Name: 'DOTNET_SHARED_STORE',
+              Value: 'C:\\otel-auto-instrumentation-dotnet\\store',
+            },
+            {
+              Name: 'OTEL_RESOURCE_ATTRIBUTES',
+              Value: 'service.name=demo',
+            },
+          ],
+          Essential: true,
+          Image: 'nathanpeck/name',
+          MountPoints: [
+            {
+              ContainerPath: 'C:\\otel-auto-instrumentation-dotnet',
+              ReadOnly: false,
+              SourceVolume: 'opentelemetry-auto-instrumentation',
+            },
+          ],
+          Cpu: 0,
+          Memory: 512,
+          Name: 'app',
+          DependsOn: [
+            {
+              Condition: 'SUCCESS',
+              ContainerName: 'adot-init',
+            },
+          ],
+        },
+        {
+          Command: [
+            'CMD',
+            '/c',
+            'xcopy',
+            '/e',
+            'C:\\autoinstrumentation\\*',
+            'C:\\otel-auto-instrumentation-dotnet',
+            '&&',
+            'icacls',
+            'C:\\otel-auto-instrumentation-dotnet',
+            '/grant',
+            '*S-1-1-0:R',
+            '/T',
+          ],
+          Essential: false,
+          Image: 'public.ecr.aws/aws-observability/adot-autoinstrumentation-dotnet:v1.6.0-windows2022',
+          MountPoints: [
+            {
+              ContainerPath: 'C:\\otel-auto-instrumentation-dotnet',
+              ReadOnly: false,
+              SourceVolume: 'opentelemetry-auto-instrumentation',
+            },
+          ],
+          Name: 'adot-init',
+          Cpu: 0,
+          Memory: 128,
+        },
+      ],
+      Family: 'TestTaskDefinition',
+      NetworkMode: 'host',
+      RequiresCompatibilities: [
+        'EC2',
+      ],
+      TaskRoleArn: {
+        'Fn::GetAtt': [
+          'TestTaskDefinitionTaskRole38EA0D26',
+          'Arn',
+        ],
+      },
+      Volumes: [
+        {
+          Name: 'opentelemetry-auto-instrumentation',
+        },
+      ],
+    });
+  });
+
   test('should create application signals configurations with node', () => {
     // GIVEN
     const stack = new Stack();
@@ -738,7 +914,6 @@ describe('application signals integration', () => {
     new ApplicationSignalsIntegration(stack, 'TestIntegration', {
       taskDefinition: ec2TaskDefinition,
       instrumentation: {
-        language: InstrumentationLanguage.NODEJS,
         sdkVersion: NodeInstrumentationVersion.V0_5_0,
 
       },
@@ -792,6 +967,144 @@ describe('application signals integration', () => {
             {
               Name: 'NODE_OPTIONS',
               Value: ' --require /otel-auto-instrumentation-nodejs/autoinstrumentation.js',
+            },
+            {
+              Name: 'OTEL_RESOURCE_ATTRIBUTES',
+              Value: 'service.name=demo',
+            },
+          ],
+          Essential: true,
+          Image: 'nathanpeck/name',
+          MountPoints: [
+            {
+              ContainerPath: '/otel-auto-instrumentation-nodejs',
+              ReadOnly: false,
+              SourceVolume: 'opentelemetry-auto-instrumentation',
+            },
+          ],
+          Cpu: 0,
+          Memory: 512,
+          Name: 'app',
+          DependsOn: [
+            {
+              Condition: 'SUCCESS',
+              ContainerName: 'adot-init',
+            },
+          ],
+        },
+        {
+          Command: [
+            'cp',
+            '-a',
+            '/autoinstrumentation/.',
+            '/otel-auto-instrumentation-nodejs',
+          ],
+          Essential: false,
+          Image: 'public.ecr.aws/aws-observability/adot-autoinstrumentation-node:v0.5.0',
+          MountPoints: [
+            {
+              ContainerPath: '/otel-auto-instrumentation-nodejs',
+              ReadOnly: false,
+              SourceVolume: 'opentelemetry-auto-instrumentation',
+            },
+          ],
+          Name: 'adot-init',
+          Cpu: 0,
+          Memory: 128,
+        },
+      ],
+      Family: 'TestTaskDefinition',
+      NetworkMode: 'host',
+      RequiresCompatibilities: [
+        'EC2',
+      ],
+      TaskRoleArn: {
+        'Fn::GetAtt': [
+          'TestTaskDefinitionTaskRole38EA0D26',
+          'Arn',
+        ],
+      },
+      Volumes: [
+        {
+          Name: 'opentelemetry-auto-instrumentation',
+        },
+      ],
+    });
+  });
+
+  test('should create application signals configurations with custom node options', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    const ec2TaskDefinition = new ecs.Ec2TaskDefinition(stack, 'TestTaskDefinition', {
+      networkMode: ecs.NetworkMode.HOST,
+    });
+    ec2TaskDefinition.addContainer('app', {
+      image: ecs.ContainerImage.fromRegistry('nathanpeck/name'),
+      cpu: 0,
+      memoryLimitMiB: 512,
+      environment: {
+        NODE_OPTIONS: '--max-old-space-size=4096',
+      },
+    });
+
+    new ApplicationSignalsIntegration(stack, 'TestIntegration', {
+      taskDefinition: ec2TaskDefinition,
+      instrumentation: {
+        sdkVersion: NodeInstrumentationVersion.V0_5_0,
+
+      },
+      serviceName: 'demo',
+      cloudWatchAgent: {
+        enableSidecar: false,
+      },
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::ECS::TaskDefinition', {
+      ContainerDefinitions: [
+        {
+          Environment: [
+            {
+              Name: 'NODE_OPTIONS',
+              Value: '--max-old-space-size=4096 --require /otel-auto-instrumentation-nodejs/autoinstrumentation.js',
+            },
+            {
+              Name: 'OTEL_LOGS_EXPORTER',
+              Value: 'none',
+            },
+            {
+              Name: 'OTEL_METRICS_EXPORTER',
+              Value: 'none',
+            },
+            {
+              Name: 'OTEL_EXPORTER_OTLP_PROTOCOL',
+              Value: 'http/protobuf',
+            },
+            {
+              Name: 'OTEL_AWS_APPLICATION_SIGNALS_ENABLED',
+              Value: 'true',
+            },
+            {
+              Name: 'OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT',
+              Value: 'http://localhost:4316/v1/metrics',
+            },
+            {
+              Name: 'OTEL_EXPORTER_OTLP_TRACES_ENDPOINT',
+              Value: 'http://localhost:4316/v1/traces',
+            },
+            {
+              Name: 'OTEL_TRACES_SAMPLER',
+              Value: 'xray',
+            },
+            {
+              Name: 'OTEL_TRACES_SAMPLER_ARG',
+              Value: 'endpoint=http://localhost:2000',
+            },
+            {
+              Name: 'OTEL_PROPAGATORS',
+              Value: 'tracecontext,baggage,b3,xray',
             },
             {
               Name: 'OTEL_RESOURCE_ATTRIBUTES',
