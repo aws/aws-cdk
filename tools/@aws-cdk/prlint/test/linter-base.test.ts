17b670224925d350fa9f6535057f851c74a09f00
Rico Hermans <rix0rrr@gmail.com>
2025-01-23 23:17:30 +0100

    chore: make PR linter overwrite previous reviews and delete old comments (#33105)
    Many changes here:
    
    - The PR linter now removes the text of previous reviews, so that they are not distracting.
    - The PR linter now deletes old comments; before the rewrite, it used to create both comments and reviews. The comments should be deleted.
    - There were a bunch of missing `await`s, add those.
    - The missing `await`s slipped in because in the past someone tried to turn off *some* linter rules, and in doing so disabled *all* linter rules. For now, just re-enable the ones that have to do with promises.
    - Fix a typo in a global constant
    - Only log the relevant details of reviews, instead of the entire GitHub object
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*
    
    (cherry picked from commit 7b9f6c839a76588773bcd0c9bd17340d4f629998)

---
+++ tools/@aws-cdk/prlint/test/linter-base.test.ts
@@ -0,0 +1,113 @@
+/**
+ * Tests for the "linter base" class
+ *
+ * This has generic GitHub functionality, and the ability to carry
+ * out a set of GitHub actions.
+ *
+ * None of this is CDK-specific business logic.
+ */
+
+import { PullRequestLinterBase } from "../linter-base";
+
+const octomock = {
+  pulls: {
+    get: jest.fn(),
+    listFiles: jest.fn(),
+    createReview: jest.fn(),
+    listReviews: jest.fn(),
+    dismissReview: jest.fn(),
+    updateReview: jest.fn(),
+    update: jest.fn(),
+  },
+  issues: {
+    createComment: jest.fn(),
+    deleteComment: jest.fn(),
+    listComments: jest.fn(),
+    removeLabel: jest.fn(),
+    addLabels: jest.fn(),
+  },
+  search: {
+    issuesAndPullRequests: jest.fn(),
+  },
+  repos: {
+    listCommitStatusesForRef: jest.fn(),
+  },
+  paginate: async (method: any, args: any) => { return (await method(args)).data; },
+};
+
+const linter = new PullRequestLinterBase({
+  client: octomock as any,
+  owner: 'test-owner',
+  repo: 'test-repo',
+  number: 123,
+  linterLogin: 'aws-cdk-automation',
+});
+
+beforeEach(() => {
+  jest.clearAllMocks();
+
+  octomock.pulls.get.mockReturnValue({
+    data: {
+      number: 123,
+      base: { ref: 'main' },
+      head: { sha: 'ABC' },
+    },
+  });
+  octomock.pulls.listReviews.mockReturnValue({ data: [] });
+  octomock.repos.listCommitStatusesForRef.mockReturnValue({ data: [] });
+});
+
+test('dismissing a review dismisses and changes the text of all previous reviews', async () => {
+  // GIVEN
+  octomock.pulls.listReviews.mockReturnValue({
+    data: [
+      {
+        id: 1111,
+        user: { login: 'aws-cdk-automation' },
+        state: 'CHANGES_REQUESTED',
+        body: 'some comment',
+      },
+      {
+        id: 2222,
+        user: { login: 'aws-cdk-automation' },
+        state: 'CHANGES_REQUESTED',
+        body: 'some other comment',
+      },
+      {
+        id: 3333,
+        user: { login: 'some-other-user' },
+        state: 'CHANGES_REQUESTED',
+        body: 'some other comment',
+      },
+    ],
+  });
+
+  // WHEN
+  await linter.executeActions({
+    dismissPreviousReview: true,
+  });
+
+  // THEN
+  expect(octomock.pulls.updateReview).toHaveBeenCalledWith(expect.objectContaining({
+    owner: 'test-owner',
+    repo: 'test-repo',
+    pull_number: 123,
+    review_id: 1111,
+  }));
+  expect(octomock.pulls.updateReview).toHaveBeenCalledWith(expect.objectContaining({
+    review_id: 2222,
+  }));
+  expect(octomock.pulls.updateReview).not.toHaveBeenCalledWith(expect.objectContaining({
+    review_id: 3333,
+  }));
+
+  expect(octomock.pulls.dismissReview).toHaveBeenCalledWith(expect.objectContaining({
+    review_id: 1111,
+  }));
+  expect(octomock.pulls.dismissReview).toHaveBeenCalledWith(expect.objectContaining({
+    review_id: 2222,
+  }));
+  expect(octomock.pulls.dismissReview).not.toHaveBeenCalledWith(expect.objectContaining({
+    review_id: 3333,
+  }));
+});
