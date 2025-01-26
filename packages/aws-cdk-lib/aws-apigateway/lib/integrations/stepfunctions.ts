6dc60c2cd86e80cfef53d278b6440b26eff05471
Hogan Bobertz <Bobertzx@gmail.com>
2025-01-26 18:09:23 -0500

    Update packages/aws-cdk-lib/aws-apigateway/lib/integrations/stepfunctions.ts
    Co-authored-by: Momo Kornher <kornherm@amazon.co.uk>

--- packages/aws-cdk-lib/aws-apigateway/lib/integrations/stepfunctions.ts
+++ packages/aws-cdk-lib/aws-apigateway/lib/integrations/stepfunctions.ts
@@ -151,7 +151,7 @@ class StepFunctionsExecutionIntegration extends AwsIntegration {
     if (this.stateMachine instanceof sfn.StateMachine) {
       const stateMachineType = (this.stateMachine as sfn.StateMachine).stateMachineType;
       if (stateMachineType !== sfn.StateMachineType.EXPRESS) {
-        throw new ValidationError('State Machine must be of type "EXPRESS". Please use StateMachineType.EXPRESS as the stateMachineType', this.stateMachine);
+        throw new ValidationError('State Machine must be of type "EXPRESS". Please use StateMachineType.EXPRESS as the stateMachineType', method);
       }
 
       // if not imported, extract the name from the CFN layer to reach the
