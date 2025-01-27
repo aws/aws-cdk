918a3a8a2ce07785af9275fc35aacf9a0148b6f7
yasuaki640 <yasuaki640@gmail.com>
2025-01-28 01:46:50 +0900

    chore(eslint): enforce no extra semicolons (#33095)
    ### Issue # (if applicable)
    
    Closes #<issue number here>.
    
    ### Reason for this change
    
    
    
    Enforced no extra semicolons by adding an eslint rule.
    
    ### Description of changes
    
    
    
    I would like to reduce noise during development by reducing the number of warnings output by the IDE.
    
    ### Describe any new or updated permissions being added
    
    
    
    
    ### Description of how you validated changes
    
    
    
    ### Checklist
    - [ ] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/toolkit/lib/actions/deploy/index.ts
+++ packages/@aws-cdk/toolkit/lib/actions/deploy/index.ts
@@ -82,21 +82,21 @@ export class StackParameters {
    */
   public static onlyExisting() {
     return new StackParameters({}, true);
-  };
+  }
 
   /**
    * Use exactly these parameters and remove any other existing parameters from the stack.
    */
   public static exactly(params: { [name: string]: string | undefined }) {
     return new StackParameters(params, false);
-  };
+  }
 
   /**
    * Define additional parameters for the stack, while keeping existing parameters for unspecified values.
    */
   public static withExisting(params: { [name: string]: string | undefined }) {
     return new StackParameters(params, true);
-  };
+  }
 
   public readonly parameters: Map<string, string | undefined>;
   public readonly keepExistingParameters: boolean;
