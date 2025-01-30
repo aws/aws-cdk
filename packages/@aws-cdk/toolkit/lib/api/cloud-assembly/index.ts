ff52425aa002cbd610540d4fc45810288e2fbfa6
Momo Kornher <kornherm@amazon.co.uk>
2025-01-30 19:20:57 +0000

    chore(toolkit): export source builder props (#33241)
    ### Description of changes
    
    Export the prop types from source-builder as intended.
    
    ### Describe any new or updated permissions being added
    
    n/a
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/toolkit/lib/api/cloud-assembly/index.ts
+++ packages/@aws-cdk/toolkit/lib/api/cloud-assembly/index.ts
@@ -1,5 +1,6 @@
 export * from './cached-source';
 export * from './identity-source';
+export * from './source-builder';
 export * from './stack-assembly';
 export * from './stack-selector';
 export * from './types';
