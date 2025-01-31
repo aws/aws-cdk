e6b5d85ae1e9c764bb863bbface5fb7355df8fdc
Put Cheung <cheungp@amazon.com>
2025-01-31 14:16:31 +0000

    [feat] Add aws-rds.DatabaseInstance.fromLookup

---
+++ packages/aws-cdk/lib/util/json.ts
@@ -0,0 +1,65 @@
+/**
+ * This gets the values of the jsonObject at the paths specified in propertiesToReturn.
+ *
+ * For example, jsonObject = {
+ *   key1: 'abc',
+ *   key2: {
+ *     foo: 'qwerty',
+ *     bar: 'data',
+ *   }
+ * }
+ *
+ * propertiesToReturn = ['key1', 'key2.foo'];
+ *
+ * The returned object is:
+ * {
+ *   key1: 'abc',
+ *   'key2.foo': 'qwerty',
+ * }
+ * @param propsObject
+ * @param propertiesToReturn
+ * @returns
+ */
+export function getResultObj(jsonObject: any, propertiesToReturn: string[]): {[key: string]: any} {
+  const propsObj = {};
+  propertiesToReturn.forEach((propName) => {
+    Object.assign(propsObj, { [propName]: findJsonValue(jsonObject, propName) });
+  });
+  return propsObj;
+}
+
+/**
+ * This finds the value of the jsonObject at the path.  Path is delimited by '.'.
+ *
+ * For example, jsonObject = {
+ *   key1: 'abc',
+ *   key2: {
+ *     foo: 'qwerty',
+ *     bar: 'data',
+ *   }
+ * }
+ *
+ * If path is 'key1', then it will return 'abc'.
+ * If path is 'key2.foo', then it will return 'qwerty'.
+ * If path is 'key2', then it will return the object:
+ * {
+ *   foo: 'qwerty',
+ *   bar: 'data',
+ * }
+ *
+ * If the path is not found, an Error will be thrown stating which token is missing.
+ *
+ * @param jsonObject
+ * @param path
+ */
+export function findJsonValue(jsonObject: any, path: string): any {
+  const paths = path.split('.');
+  let obj = jsonObject;
+  paths.forEach(p => {
+    obj = obj[p];
+    if (obj === undefined) {
+      throw new TypeError(`Cannot read field ${path}. ${p} is not found.`);
+    }
+  });
+  return obj;
+}
---
+++ packages/aws-cdk/test/context-providers/cc-api-provider.test.ts
@@ -0,0 +1,184 @@
+import { GetResourceCommand, ListResourcesCommand } from '@aws-sdk/client-cloudcontrol';
+import { CcApiContextProviderPlugin } from '../../lib/context-providers/cc-api-provider';
+import { mockCloudControlClient, MockSdkProvider, restoreSdkMocksToDefault } from '../util/mock-sdk';
+
+let provider: CcApiContextProviderPlugin;
+
+beforeEach(() => {
+  provider = new CcApiContextProviderPlugin(new MockSdkProvider());
+  restoreSdkMocksToDefault();
+});
+
+test('looks up RDS instance using CC API getResource', async () => {
+  // GIVEN
+  mockCloudControlClient.on(GetResourceCommand).resolves({
+    TypeName: 'AWS::RDS::DBInstance',
+    ResourceDescription: {
+      Identifier: 'my-db-instance-1',
+      Properties: '{"DBInstanceArn":"arn:aws:rds:us-east-1:123456789012:db:test-instance-1","StorageEncrypted":"true"}',
+    },
+  });
+
+  // WHEN
+  const result = await provider.getValue({
+    account: '123456789012',
+    region: 'us-east-1',
+    typeName: 'AWS::RDS::DBInstance',
+    exactIdentifier: 'my-db-instance-1',
+    propertiesToReturn: ['DBInstanceArn', 'StorageEncrypted'],
+  });
+
+  // THEN
+  const results = result[CcApiContextProviderPlugin.RESULTS];
+  const propsObj = results[0];
+  expect(propsObj.DBInstanceArn).toEqual('arn:aws:rds:us-east-1:123456789012:db:test-instance-1');
+  expect(propsObj.StorageEncrypted).toEqual('true');
+  expect(propsObj.Identifier).toEqual('my-db-instance-1');
+  expect(results.length).toEqual(1);
+});
+
+// In theory, this should never happen.  We ask for my-db-instance-1 but CC API returns ''.
+// Included this to test the code path.
+test('looks up RDS instance using CC API getResource - wrong match', async () => {
+  // GIVEN
+  mockCloudControlClient.on(GetResourceCommand).resolves({
+    TypeName: 'AWS::RDS::DBInstance',
+    ResourceDescription: {
+      Identifier: '',
+      Properties: '{"DBInstanceArn":"arn:aws:rds:us-east-1:123456789012:db:test-instance-1","StorageEncrypted":"true"}',
+    },
+  });
+
+  await expect(
+    // WHEN
+    provider.getValue({
+      account: '123456789012',
+      region: 'us-east-1',
+      typeName: 'AWS::RDS::DBInstance',
+      exactIdentifier: 'my-db-instance-1',
+      propertiesToReturn: ['DBInstanceArn', 'StorageEncrypted'],
+    }),
+  ).rejects.toThrow('Encountered CC API error while getting resource my-db-instance-1.'); // THEN
+});
+
+test('looks up RDS instance using CC API getResource - empty response', async () => {
+  // GIVEN
+  mockCloudControlClient.on(GetResourceCommand).resolves({
+  });
+
+  await expect(
+    // WHEN
+    provider.getValue({
+      account: '123456789012',
+      region: 'us-east-1',
+      typeName: 'AWS::RDS::DBInstance',
+      exactIdentifier: 'bad-identifier',
+      propertiesToReturn: ['DBInstanceArn', 'StorageEncrypted'],
+    }),
+  ).rejects.toThrow('Encountered CC API error while getting resource bad-identifier.'); // THEN
+});
+
+test('looks up RDS instance using CC API getResource - error in CC API', async () => {
+  // GIVEN
+  mockCloudControlClient.on(GetResourceCommand).rejects('No data found');
+
+  await expect(
+    // WHEN
+    provider.getValue({
+      account: '123456789012',
+      region: 'us-east-1',
+      typeName: 'AWS::RDS::DBInstance',
+      exactIdentifier: 'bad-identifier',
+      propertiesToReturn: ['DBInstanceArn', 'StorageEncrypted'],
+    }),
+  ).rejects.toThrow('Encountered CC API error while getting resource bad-identifier.'); // THEN
+});
+
+test('looks up RDS instance using CC API listResources', async () => {
+  // GIVEN
+  mockCloudControlClient.on(ListResourcesCommand).resolves({
+    ResourceDescriptions: [
+      {
+        Identifier: 'my-db-instance-1',
+        Properties: '{"DBInstanceArn":"arn:aws:rds:us-east-1:123456789012:db:test-instance-1","StorageEncrypted":"true","Endpoint":{"Address":"address1.amazonaws.com","Port":"5432"}}',
+      },
+      {
+        Identifier: 'my-db-instance-2',
+        Properties: '{"DBInstanceArn":"arn:aws:rds:us-east-1:123456789012:db:test-instance-2","StorageEncrypted":"false","Endpoint":{"Address":"address2.amazonaws.com","Port":"5432"}}',
+      },
+      {
+        Identifier: 'my-db-instance-3',
+        Properties: '{"DBInstanceArn":"arn:aws:rds:us-east-1:123456789012:db:test-instance-3","StorageEncrypted":"true","Endpoint":{"Address":"address3.amazonaws.com","Port":"6000"}}',
+      },
+    ],
+  });
+
+  // WHEN
+  const result = await provider.getValue({
+    account: '123456789012',
+    region: 'us-east-1',
+    typeName: 'AWS::RDS::DBInstance',
+    propertyMatch: {
+      StorageEncrypted: 'true',
+    },
+    propertiesToReturn: ['DBInstanceArn', 'StorageEncrypted', 'Endpoint.Port'],
+  });
+
+  // THEN
+  const results = result[CcApiContextProviderPlugin.RESULTS];
+  let propsObj = results[0];
+  expect(propsObj.DBInstanceArn).toEqual('arn:aws:rds:us-east-1:123456789012:db:test-instance-1');
+  expect(propsObj.StorageEncrypted).toEqual('true');
+  expect(propsObj['Endpoint.Port']).toEqual('5432');
+  expect(propsObj.Identifier).toEqual('my-db-instance-1');
+
+  propsObj = results[1];
+  expect(propsObj.DBInstanceArn).toEqual('arn:aws:rds:us-east-1:123456789012:db:test-instance-3');
+  expect(propsObj.StorageEncrypted).toEqual('true');
+  expect(propsObj['Endpoint.Port']).toEqual('6000');
+  expect(propsObj.Identifier).toEqual('my-db-instance-3');
+
+  expect(results.length).toEqual(2);
+});
+
+test('looks up RDS instance using CC API listResources - nested prop', async () => {
+  // GIVEN
+  mockCloudControlClient.on(ListResourcesCommand).resolves({
+    ResourceDescriptions: [
+      {
+        Identifier: 'my-db-instance-1',
+        Properties: '{"DBInstanceArn":"arn:aws:rds:us-east-1:123456789012:db:test-instance-1","StorageEncrypted":"true","Endpoint":{"Address":"address1.amazonaws.com","Port":"5432"}}',
+      },
+      {
+        Identifier: 'my-db-instance-2',
+        Properties: '{"DBInstanceArn":"arn:aws:rds:us-east-1:123456789012:db:test-instance-2","StorageEncrypted":"false","Endpoint":{"Address":"address2.amazonaws.com","Port":"5432"}}',
+      },
+      {
+        Identifier: 'my-db-instance-3',
+        Properties: '{"DBInstanceArn":"arn:aws:rds:us-east-1:123456789012:db:test-instance-3","StorageEncrypted":"true","Endpoint":{"Address":"address3.amazonaws.com","Port":"6000"}}',
+      },
+    ],
+  });
+
+  // WHEN
+  const result = await provider.getValue({
+    account: '123456789012',
+    region: 'us-east-1',
+    typeName: 'AWS::RDS::DBInstance',
+    propertyMatch: {
+      'StorageEncrypted': 'true',
+      'Endpoint.Port': '5432',
+    },
+    propertiesToReturn: ['DBInstanceArn', 'StorageEncrypted', 'Endpoint.Port'],
+  });
+
+  // THEN
+  const results = result[CcApiContextProviderPlugin.RESULTS];
+  let propsObj = results[0];
+  expect(propsObj.DBInstanceArn).toEqual('arn:aws:rds:us-east-1:123456789012:db:test-instance-1');
+  expect(propsObj.StorageEncrypted).toEqual('true');
+  expect(propsObj['Endpoint.Port']).toEqual('5432');
+  expect(propsObj.Identifier).toEqual('my-db-instance-1');
+
+  expect(results.length).toEqual(1);
+});
