92e1e1100f6d15dfd66d6d8aa435c95a1692e56d
Tietew <tietew@gmail.com>
2025-02-04 12:20:09 +0900

    add iteg test explicit disable choice-based authentication

--- packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-passwordless.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-passwordless.ts
@@ -1,6 +1,6 @@
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 import { App, CfnOutput, RemovalPolicy, Stack } from 'aws-cdk-lib';
-import { PasskeyUserVerification, UserPool } from 'aws-cdk-lib/aws-cognito';
+import { FeaturePlan, PasskeyUserVerification, UserPool } from 'aws-cdk-lib/aws-cognito';
 
 const app = new App();
 const stack = new Stack(app, 'integ-user-pool-passwordless');
@@ -9,6 +9,13 @@ const userpool = new UserPool(stack, 'myuserpool', {
   allowedFirstAuthFactors: { emailOtp: true, passkey: true },
   passkeyRelyingPartyId: 'example.com',
   passkeyUserVerification: PasskeyUserVerification.REQUIRED,
+  removalPolicy: RemovalPolicy.DESTROY,
+  deletionProtection: false,
+});
+
+new UserPool(stack, 'myuserpool-disabled-passwordless', {
+  allowedFirstAuthFactors: { password: true },
+  featurePlan: FeaturePlan.LITE,
   removalPolicy: RemovalPolicy.DESTROY,
   deletionProtection: false,
 });
