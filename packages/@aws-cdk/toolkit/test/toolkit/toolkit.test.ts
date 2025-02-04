e1de04cb5dc77ff7da6b6ff45a15c1fa469ae5c5
Momo Kornher <kornherm@amazon.co.uk>
2025-02-04 16:26:15 +0000

    chore(toolkit): trim whitespace from IoMessages (#33284)
    ### Reason for this change
    
    After removing emojis and ANSI colors, an IoMessage might end up with floating whitespace at either end. We want to trim this whitespace.
    
    ### Description of changes
    
    Always trim whitespace from IoMessages.
    
    ### Describe any new or updated permissions being added
    
    n/a
    
    ### Description of how you validated changes
    
    unit tests
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/toolkit/test/toolkit/toolkit.test.ts
+++ packages/@aws-cdk/toolkit/test/toolkit/toolkit.test.ts
@@ -9,42 +9,64 @@ import * as chalk from 'chalk';
 import { Toolkit } from '../../lib';
 import { TestIoHost } from '../_helpers';
 
-test('emojis can be stripped from message', async () => {
-  const ioHost = new TestIoHost();
-  const toolkit = new Toolkit({ ioHost, emojis: false });
+describe('message formatting', () => {
+  test('emojis can be stripped from message', async () => {
+    const ioHost = new TestIoHost();
+    const toolkit = new Toolkit({ ioHost, emojis: false });
 
-  await toolkit.ioHost.notify({
-    message: '💯Smile123😀',
-    action: 'deploy',
-    level: 'info',
-    code: 'CDK_TOOLKIT_I0000',
-    time: new Date(),
+    await toolkit.ioHost.notify({
+      message: '💯Smile123😀',
+      action: 'deploy',
+      level: 'info',
+      code: 'CDK_TOOLKIT_I0000',
+      time: new Date(),
+    });
+
+    expect(ioHost.notifySpy).toHaveBeenCalledWith(expect.objectContaining({
+      action: 'deploy',
+      level: 'info',
+      code: 'CDK_TOOLKIT_I0000',
+      message: 'Smile123',
+    }));
   });
 
-  expect(ioHost.notifySpy).toHaveBeenCalledWith(expect.objectContaining({
-    action: 'deploy',
-    level: 'info',
-    code: 'CDK_TOOLKIT_I0000',
-    message: 'Smile123',
-  }));
-});
+  test('color can be stripped from message', async () => {
+    const ioHost = new TestIoHost();
+    const toolkit = new Toolkit({ ioHost, color: false });
 
-test('color can be stripped from message', async () => {
-  const ioHost = new TestIoHost();
-  const toolkit = new Toolkit({ ioHost, color: false });
+    await toolkit.ioHost.notify({
+      message: chalk.red('RED') + chalk.bold('BOLD') + chalk.blue('BLUE'),
+      action: 'deploy',
+      level: 'info',
+      code: 'CDK_TOOLKIT_I0000',
+      time: new Date(),
+    });
 
-  await toolkit.ioHost.notify({
-    message: chalk.red('RED') + chalk.bold('BOLD') + chalk.blue('BLUE'),
-    action: 'deploy',
-    level: 'info',
-    code: 'CDK_TOOLKIT_I0000',
-    time: new Date(),
+    expect(ioHost.notifySpy).toHaveBeenCalledWith(expect.objectContaining({
+      action: 'deploy',
+      level: 'info',
+      code: 'CDK_TOOLKIT_I0000',
+      message: 'REDBOLDBLUE',
+    }));
   });
 
-  expect(ioHost.notifySpy).toHaveBeenCalledWith(expect.objectContaining({
-    action: 'deploy',
-    level: 'info',
-    code: 'CDK_TOOLKIT_I0000',
-    message: 'REDBOLDBLUE',
-  }));
+  test('whitespace is always trimmed from a message', async () => {
+    const ioHost = new TestIoHost();
+    const toolkit = new Toolkit({ ioHost, color: false });
+
+    await toolkit.ioHost.notify({
+      message: '   test message\n\n',
+      action: 'deploy',
+      level: 'info',
+      code: 'CDK_TOOLKIT_I0000',
+      time: new Date(),
+    });
+
+    expect(ioHost.notifySpy).toHaveBeenCalledWith(expect.objectContaining({
+      action: 'deploy',
+      level: 'info',
+      code: 'CDK_TOOLKIT_I0000',
+      message: 'test message',
+    }));
+  });
 });
