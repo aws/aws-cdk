2b086e9cbee7c0b105d9ecfd91e681d5d5b7bb4f
ren-yamanashi <pinokionigg@icloud.com>
2025-02-06 22:32:19 +0900

    test: add assertion test

--- packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-client-analytics.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-client-analytics.ts
@@ -1,6 +1,6 @@
-import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { IntegTest, ExpectedResult } from '@aws-cdk/integ-tests-alpha';
 import { App, Stack, RemovalPolicy } from 'aws-cdk-lib';
-import { UserPool } from 'aws-cdk-lib/aws-cognito';
+import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 import { PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { CfnApp } from 'aws-cdk-lib/aws-pinpoint';
 import { Construct } from 'constructs';
@@ -9,6 +9,8 @@ import { Construct } from 'constructs';
  * To set analytics config to UserPoolClient with Application ARN
  */
 class TestStack extends Stack {
+  public readonly userPool: UserPool;
+  public readonly client: UserPoolClient;
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
@@ -17,11 +19,11 @@ class TestStack extends Stack {
     });
     pinpointApp.applyRemovalPolicy(RemovalPolicy.DESTROY);
 
-    const userPool = new UserPool(this, 'Pool', {
+    this.userPool = new UserPool(this, 'Pool', {
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    userPool.addClient('Client', {
+    this.client = this.userPool.addClient('Client', {
       generateSecret: true,
       analytics: {
         application: pinpointApp,
@@ -34,6 +36,8 @@ class TestStack extends Stack {
  * To set analytics config to UserPoolClient with Application ID, External ID, and Role ARN
  */
 class TestStack2 extends Stack {
+  public readonly userPool: UserPool;
+  public readonly client: UserPoolClient;
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
@@ -50,11 +54,11 @@ class TestStack2 extends Stack {
       resources: ['*'],
     }));
 
-    const userPool = new UserPool(this, 'Pool', {
+    this.userPool = new UserPool(this, 'Pool', {
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    userPool.addClient('client', {
+    this.client = this.userPool.addClient('client', {
       generateSecret: true,
       analytics: {
         applicationId: pinpointApp.ref,
@@ -69,7 +73,22 @@ const app = new App();
 const testCase = new TestStack(app, 'IntegUserPoolClientAnalytics');
 const testCase2 = new TestStack2(app, 'IntegUserPoolClientAnalytics2');
 
-new IntegTest(app, 'integ-user-pool-client-test', {
+const integ = new IntegTest(app, 'integ-user-pool-client-test', {
   testCases: [testCase, testCase2],
   regions: ['us-east-1'],
 });
+
+// NOTE: assertion test for TestStack
+const resp1 = integ.assertions.awsApiCall('CognitoIdentityServiceProvider', 'describeUserPoolClient', {
+  UserPoolId: testCase.userPool.userPoolId,
+  ClientId: testCase.client.userPoolClientId,
+});
+resp1.assertAtPath('UserPoolClient.AnalyticsConfiguration.ApplicationArn', ExpectedResult.stringLikeRegexp('.*:apps/.*'));
+
+const resp2 = integ.assertions.awsApiCall('CognitoIdentityServiceProvider', 'describeUserPoolClient', {
+  UserPoolId: testCase2.userPool.userPoolId,
+  ClientId: testCase2.client.userPoolClientId,
+});
+resp2.assertAtPath('UserPoolClient.AnalyticsConfiguration.ApplicationId', ExpectedResult.stringLikeRegexp('.*'));
+resp2.assertAtPath('UserPoolClient.AnalyticsConfiguration.RoleArn', ExpectedResult.stringLikeRegexp('arn:aws:iam::.*:role/.*'));
+resp2.assertAtPath('UserPoolClient.AnalyticsConfiguration.ExternalId', ExpectedResult.stringLikeRegexp('.*'));
