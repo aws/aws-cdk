99c032fcc6c5f3919db43c8c92e1d3dec2b71c4e
Ian Hou <45278651+iankhou@users.noreply.github.com>
2025-02-11 12:20:04 -0500

    newline at end of file

--- packages/@aws-cdk-testing/cli-integ/resources/cdk-apps/app/lambda-node/index.ts
+++ packages/@aws-cdk-testing/cli-integ/resources/cdk-apps/app/lambda-node/index.ts
@@ -1,6 +1,7 @@
 export async function handler(event: any) {
     return {
-      statusCode: 200,
-      body: JSON.stringify('hello from nodejs lambda!'),
+        statusCode: 200,
+        body: JSON.stringify('hello from nodejs lambda!'),
     };
-  }
+}
+  
