aee42a032a570df1ee52e70813b077184396c451
Ian Hou <45278651+iankhou@users.noreply.github.com>
2025-02-11 17:29:16 -0500

    remove commented-out code

--- packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
@@ -1,9 +1,4 @@
-// import { execSync } from 'child_process';
-import {
-  existsSync,
-  // mkdtempSync,
-  promises as fs,
-} from 'fs';
+import { existsSync, promises as fs } from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import {
