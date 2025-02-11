48b928fac7b7950ee6289f9128dd2e7ce63c2c4d
Ian Hou <45278651+iankhou@users.noreply.github.com>
2025-02-10 20:35:14 -0500

    test to ensure asset metadata emitted from cdk import is consistent with other synth methods

--- packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cli.integtest.ts
@@ -2140,6 +2140,144 @@ integTest(
   }),
 );
 
+/**
+ * Ensure that CDK Import emits correct metadata, so that stack comparison during import succeeds
+ *
+ * https://github.com/aws/aws-cdk/issues/31999
+ *
+ */
+integTest(
+  'CDK import adds the metadata properties expected by sam, validates that bundling is not skipped',
+  withSamIntegrationFixture(async (fixture) => {
+    const randomPrefix = randomString();
+    const uniqueOutputsFileName = `${randomPrefix}Outputs.json`; // other tests use the outputs file. Make sure we don't collide.
+    const outputsFile = path.join(fixture.integTestDir, 'outputs', uniqueOutputsFileName);
+    await fs.mkdir(path.dirname(outputsFile), { recursive: true });
+
+    const mappingFile = path.join(fixture.integTestDir, 'outputs', `${randomPrefix}Mapping.json`);
+
+    // We don't need anything mapped, we are just testing metadata on import-synth
+    // --resource-mapping is required on cdk import when not run through the CLI
+    await fs.writeFile(mappingFile, JSON.stringify({}), { encoding: 'utf-8' });
+
+    await fixture.cdk(
+      ['import', '--resource-mapping', mappingFile], {});
+    const template = fixture.template('TestStack');
+
+    const expectedResources = [
+      {
+        // Python Layer Version
+        id: 'PythonLayerVersion39495CEF',
+        cdkId: 'PythonLayerVersion',
+        isBundled: true,
+        property: 'Content',
+      },
+      {
+        // Layer Version
+        id: 'LayerVersion3878DA3A',
+        cdkId: 'LayerVersion',
+        isBundled: false,
+        property: 'Content',
+      },
+      {
+        // Bundled layer version
+        id: 'BundledLayerVersionPythonRuntime6BADBD6E',
+        cdkId: 'BundledLayerVersionPythonRuntime',
+        isBundled: true,
+        property: 'Content',
+      },
+      {
+        // Python Function
+        id: 'PythonFunction0BCF77FD',
+        cdkId: 'PythonFunction',
+        isBundled: true,
+        property: 'Code',
+      },
+      {
+        // Log Retention Function
+        id: 'LogRetentionaae0aa3c5b4d4f87b02d85b201efdd8aFD4BFC8A',
+        cdkId: 'LogRetentionaae0aa3c5b4d4f87b02d85b201efdd8a',
+        isBundled: false,
+        property: 'Code',
+      },
+      {
+        // Function
+        id: 'FunctionPythonRuntime28CBDA05',
+        cdkId: 'FunctionPythonRuntime',
+        isBundled: false,
+        property: 'Code',
+      },
+      {
+        // Bundled Function
+        id: 'BundledFunctionPythonRuntime4D9A0918',
+        cdkId: 'BundledFunctionPythonRuntime',
+        isBundled: true,
+        property: 'Code',
+      },
+      {
+        // NodeJs Function
+        id: 'NodejsFunction09C1F20F',
+        cdkId: 'NodejsFunction',
+        isBundled: true,
+        property: 'Code',
+      },
+      {
+        // Go Function
+        id: 'GoFunctionCA95FBAA',
+        cdkId: 'GoFunction',
+        isBundled: true,
+        property: 'Code',
+      },
+      {
+        // Docker Image Function
+        id: 'DockerImageFunction28B773E6',
+        cdkId: 'DockerImageFunction',
+        dockerFilePath: 'Dockerfile',
+        property: 'Code.ImageUri',
+      },
+      {
+        // Spec Rest Api
+        id: 'SpecRestAPI7D4B3A34',
+        cdkId: 'SpecRestAPI',
+        property: 'BodyS3Location',
+      },
+    ];
+
+    for (const resource of expectedResources) {
+      fixture.output.write(`validate assets metadata for resource ${JSON.stringify(resource)}`);
+      expect(resource.id in template.Resources).toBeTruthy();
+      expect(template.Resources[resource.id]).toEqual(
+        expect.objectContaining({
+          Metadata: {
+            'aws:cdk:path': `${fixture.fullStackName('TestStack')}/${resource.cdkId}/Resource`,
+            'aws:asset:path': expect.stringMatching(/asset\.[0-9a-zA-Z]{64}/), // relative path, not absolute workspace path
+            'aws:asset:is-bundled': resource.isBundled,
+            'aws:asset:dockerfile-path': resource.dockerFilePath,
+            'aws:asset:property': resource.property,
+          },
+        }),
+      );
+    }
+
+    // Nested Stack
+    fixture.output.write('validate assets metadata for nested stack resource');
+    expect('NestedStackNestedStackNestedStackNestedStackResourceB70834FD' in template.Resources).toBeTruthy();
+    expect(template.Resources.NestedStackNestedStackNestedStackNestedStackResourceB70834FD).toEqual(
+      expect.objectContaining({
+        Metadata: {
+          'aws:cdk:path': `${fixture.fullStackName(
+            'TestStack',
+          )}/NestedStack.NestedStack/NestedStack.NestedStackResource`,
+          'aws:asset:path': expect.stringMatching(
+            `${fixture.stackNamePrefix.replace(/-/, '')}TestStackNestedStack[0-9A-Z]{8}\.nested\.template\.json`,
+          ),
+          'aws:asset:property': 'TemplateURL',
+        },
+      }),
+    );
+  }),
+);
+
 integTest(
   'CDK synth bundled functions as expected',
   withSamIntegrationFixture(async (fixture) => {
