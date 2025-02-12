24a48395eb7389a7535c1eb273ce3c24a1da6a41
Ian Hou <45278651+iankhou@users.noreply.github.com>
2025-02-12 13:42:43 -0500

    test(cli): cdk import works when used on a stack containing a nodejs lambda function (#33372)
    ### Improves testing for #33322.
    
    ### Reason for this change
    
    The original test (removed below) does not adequately test the CDK import fix from the above-mentioned PR. It calls CDK synth, but does not use the CLI and does not test `cdk import`.
    
    ### Description of changes
    
    The old test is removed.
    
    The new test ensures that `cdk import` works when an already-deployed stack contains a NodeJSFunction lambda. In our test, we deploy a stack containing a NodeJSFunction Lambda. We also create, orphan, and import an S3 bucket, which replicates the customer scenario in issue #31999.
    
    The test:
    1. Installs `esbuild` so that we can use it for bundling.
    2. Deploys a NodeJSFunction and an S3 bucket.
    3. Orphans the S3 bucket but allows the bucket to persist (not destroyed).
    4. Imports the S3 bucket into the template using its `BucketName` property.
    
    The test will fail if the import operation fails for any reason.
    
    The test does not assert any specific values or patterns for asset metadata keys. When investigating the issue, the metadata keys were different when using import-synth (skipped bundling) and deploy/diff/synth (did not skip bundling). The difference in the keys was another symptom of the same issue.
    
    ### Describe any new or updated permissions being added
    
    No permissions changes.
    
    
    ### Description of how you validated changes
    
    This PR tests the feature. I ensured that the test failed on code prior to the change in PR #33322, and passes on code following the change.
    
    ### Checklist
    - [X] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ.nodejs.build.images.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-lambda-nodejs/test/integ.nodejs.build.images.ts
@@ -1,8 +1,6 @@
 import * as path from 'path';
-import * as fs from 'fs';
-import { App, Stack, StackProps, ValidationError } from 'aws-cdk-lib';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs';
 import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
 import { IFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
@@ -50,40 +48,4 @@ stack.lambdaFunctions.forEach(func=> {
     StatusCode: 200,
     ExecutedVersion: '$LATEST',
   }));
-});
-
-// Ensure that the code is bundled
-const assembly = app.synth();
-
-stack.lambdaFunctions.forEach((func) => {
-  const template = assembly.getStackArtifact(stack.artifactId).template;
-  const resourceName = stack.getLogicalId(func.node.defaultChild as lambda.CfnFunction);
-  const resource = template.Resources[resourceName];
-
-  if (!resource || resource.Type !== 'AWS::Lambda::Function') {
-    throw new ValidationError(`Could not find Lambda function resource for ${func.functionName}`, stack);
-  }
-
-  const s3Bucket = resource.Properties.Code.S3Bucket;
-  const s3Key = resource.Properties.Code.S3Key;
-
-  if (!s3Bucket || !s3Key) {
-    throw new ValidationError(`Could not find S3 location for function ${func.functionName}`, stack);
-  }
-
-  const assetId = s3Key.split('.')[0]; // S3Key format is <hash>.zip"
-  const assetDir = path.join(assembly.directory, `asset.${assetId}`);
-
-  try {
-    if (!fs.existsSync(assetDir) || !fs.statSync(assetDir).isDirectory()) {
-      throw new ValidationError(`Asset directory does not exist for function ${func.functionName}: ${assetDir}`, stack);
-    }
-
-    const indexPath = path.join(assetDir, 'index.js');
-    if (!fs.existsSync(indexPath)) {
-      throw new ValidationError(`index.js not found in asset directory for function ${func.functionName}`, stack);
-    }
-  } catch (error) {
-    throw error;
-  }
 });
