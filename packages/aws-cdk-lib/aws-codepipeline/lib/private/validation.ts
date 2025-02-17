1344b0e8005daa38b81056baf791fd5d2f1be813
go-to-k <24818752+go-to-k@users.noreply.github.com>
2025-02-17 16:19:18 +0900

    feat(codepipeline-actions): add timeout for ManualApprovalAction

--- packages/aws-cdk-lib/aws-codepipeline/lib/private/validation.ts
+++ packages/aws-cdk-lib/aws-codepipeline/lib/private/validation.ts
@@ -1,5 +1,5 @@
 import * as cdk from '../../../core';
-import { ActionCategory } from '../action';
+import { ActionCategory, IAction, IAction } from '../action';
 import { Artifact } from '../artifact';
 
 /**
