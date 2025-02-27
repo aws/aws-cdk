dd0d62f84da06e2cafbe7a8bac80899d86b6f153
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2025-02-27 09:53:30 +0900

    feat(pipes-alpha): support for customer-managed KMS keys to encrypt pipe data (#33546)
    ### Issue # (if applicable)
    
    Closes #31453
    
    ### Reason for this change
    
    AWS Pipes supports for encrypting data by customer managed KMS key instead of Amazon managed key.
    
    https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-encryption-pipes-cmkey.html
    
    The L2 Pipe construct does not support this feature now.
    
    ### Description of changes
    
    - Add `kmsKey` prop to `PipeProps`
    - 
    ### Describe any new or updated permissions being added
    
    - Add KMS key policy which enables pipes to access to the key.
    
    https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-encryption-key-policy.html#eb-encryption-key-policy-pipe
    
    ### Description of how you validated changes
    
    Add both unit and integ tests.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-pipes-alpha/test/pipe.test.ts
+++ packages/@aws-cdk/aws-pipes-alpha/test/pipe.test.ts
@@ -1,6 +1,7 @@
 import { App, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import * as kms from 'aws-cdk-lib/aws-kms';
 import { Topic } from 'aws-cdk-lib/aws-sns';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { TestEnrichment, TestSource, TestSourceWithDeadLetterTarget, TestTarget } from './test-classes';
@@ -34,6 +35,107 @@ describe('Pipe', () => {
     // THEN
     template.resourceCountIs('AWS::Pipes::Pipe', 1);
     expect(template).toMatchSnapshot();
+  });
+
+  describe('kms key', () => {
+    test('specify a KMS key', () => {
+      // GIVEN
+      const kmsKey = new kms.Key(stack, 'Key');
+
+      // WHEN
+      new Pipe(stack, 'TestPipe', {
+        source,
+        target,
+        kmsKey,
+        pipeName: 'TestPipe',
+      });
+
+      // THEN
+      Template.fromStack(stack).hasResourceProperties('AWS::Pipes::Pipe', {
+        Name: 'TestPipe',
+        KmsKeyIdentifier: {
+          'Fn::GetAtt': [
+            'Key961B73FD',
+            'Arn',
+          ],
+        },
+      });
+
+      Template.fromStack(stack).hasResourceProperties('AWS::KMS::Key', {
+        KeyPolicy: {
+          Statement: [
+            {
+              Action: 'kms:*',
+              Effect: 'Allow',
+              Principal: {
+                AWS: {
+                  'Fn::Join': [
+                    '',
+                    [
+                      'arn:',
+                      {
+                        Ref: 'AWS::Partition',
+                      },
+                      ':iam::123456789012:root',
+                    ],
+                  ],
+                },
+              },
+              Resource: '*',
+            },
+            {
+              Action: [
+                'kms:Decrypt',
+                'kms:DescribeKey',
+                'kms:GenerateDataKey',
+              ],
+              Condition: {
+                'ArnLike': {
+                  'kms:EncryptionContext:aws:pipe:arn': {
+                    'Fn::Join': [
+                      '',
+                      [
+                        'arn:',
+                        {
+                          Ref: 'AWS::Partition',
+                        },
+                        ':pipes:us-east-1:123456789012:pipe/TestPipe',
+                      ],
+                    ],
+                  },
+                },
+                'ForAnyValue:StringEquals': {
+                  'kms:EncryptionContextKeys': [
+                    'aws:pipe:arn',
+                  ],
+                },
+              },
+              Effect: 'Allow',
+              Principal: {
+                AWS: {
+                  'Fn::GetAtt': [
+                    'TestPipeRole0FD00B2B',
+                    'Arn',
+                  ],
+                },
+              },
+              Resource: '*',
+            },
+          ],
+          Version: '2012-10-17',
+        },
+      });
+    });
+
+    test('throw error for not specifying pipe name', () => {
+      const kmsKey = new kms.Key(stack, 'Key');
+
+      expect(() => new Pipe(stack, 'TestPipe', {
+        source,
+        target,
+        kmsKey,
+      })).toThrow('`pipeName` is required when specifying a `kmsKey` prop.');
+    });
   });
 
   test('fromPipeName', () => {
