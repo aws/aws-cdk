6d600c74a4ef10f8297512b2f91cb4fe6fe7c835
Paul Sun <paulhcs@amazon.com>
2025-02-28 08:42:03 -0800

    extract code writing logic to enum-updater tool + add workflow file

--- tools/@aws-cdk/construct-metadata-updater/lib/index.ts
+++ tools/@aws-cdk/construct-metadata-updater/lib/index.ts
@@ -1,32 +1,20 @@
-// import { EnumLikeUpdater } from './metadata-updater';
-import { EnumLikeUpdater } from './metadata-updater';
-// TODO: Uncomment out the rest of the updaters and tweak the import before raising a PR.
-//       If you're reading this on GitHub, someone forgot to double-check!
+import { ConstructsUpdater, EnumLikeUpdater, EnumsUpdater, MethodsUpdater, PropertyUpdater } from './metadata-updater';
+
 export function main() {
   const dir = '../../../../packages/'
 
-  // new ConstructsUpdater(dir).execute();
-  // console.log('Constructs updater finished.');
-  // new ConstructsUpdater(dir).execute();
-  // console.log('Constructs updater finished.');
+  new ConstructsUpdater(dir).execute();
+  console.log('Constructs updater finished.');
 
-  // new PropertyUpdater(dir).execute();
-  // console.log('Property updater finished.')
-  // new PropertyUpdater(dir).execute();
-  // console.log('Property updater finished.')
+  new PropertyUpdater(dir).execute();
+  console.log('Property updater finished.')
 
-  // new EnumsUpdater(dir).execute();
-  // console.log('Enums updater finished.');
-  // new EnumsUpdater(dir).execute();
-  // console.log('Enums updater finished.');
-
-  // new MethodsUpdater(dir).execute();
-  // console.log('Methods updater finished.');
-  // new MethodsUpdater(dir).execute();
-  // console.log('Methods updater finished.');
-
-  // new EnumLikeUpdater(dir).TEST();
-  // // console.log('Enum-like updater finished.');
-
-  new EnumLikeUpdater(dir).TEST();
+  new EnumsUpdater(dir).execute();
+  console.log('Enums updater finished.');
+  
+  new EnumLikeUpdater(dir).execute();
+  console.log('Enums-like updater finished.');
+  
+  new MethodsUpdater(dir).execute();
+  console.log('Methods updater finished.');
 }
--- tools/@aws-cdk/enum-updater/lib/index.ts
+++ tools/@aws-cdk/enum-updater/lib/index.ts
@@ -1,5 +1,12 @@
 import { entryMethod } from './enum-updater';
+import { MissingEnumsUpdater } from './missing-enum-updater';
 
 export function main() {
     entryMethod();
+}
+
+export function updateMissingEnums() {
+    const dir = '../../../../packages/'
+  
+    new MissingEnumsUpdater(dir).execute();
 }
