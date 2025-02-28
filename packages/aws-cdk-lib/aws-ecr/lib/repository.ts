165436777ee8e0da51d6c6a028af648a638c8e7a
paulhcsun <paulhcsun@gmail.com>
2025-02-28 15:20:54 -0800

    chore(ecr): backfill missing enums for ecr

--- packages/aws-cdk-lib/aws-ecr/lib/repository.ts
+++ packages/aws-cdk-lib/aws-ecr/lib/repository.ts
@@ -1029,6 +1029,10 @@ export class RepositoryEncryption {
    * 'KMS'
    */
   public static readonly KMS = new RepositoryEncryption('KMS');
+  /**
+   * [PLACEHOLDER COMMENT: TO BE FILLED OUT]
+   */
+  public static readonly KMS_DSSE = new RepositoryEncryption('KMS_DSSE');
 
   /**
    * @param value the string value of the encryption
