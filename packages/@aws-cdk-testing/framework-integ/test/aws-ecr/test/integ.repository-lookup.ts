b1df6672fd0adecb9c31bdae3f76058f91a76462
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2025-03-01 16:23:56 +0900

    temp

--- packages/@aws-cdk-testing/framework-integ/test/aws-ecr/test/integ.repository-lookup.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ecr/test/integ.repository-lookup.ts
@@ -1,36 +1,43 @@
 
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
-import { App, RemovalPolicy, Stack } from 'aws-cdk-lib';
+import { App, CfnOutput, RemovalPolicy, Stack } from 'aws-cdk-lib';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 
 const app = new App();
 const repositoryName = 'my-repo';
 
-const stack = new Stack(app, 'Stack', {
+const setupStack = new Stack(app, 'EcrRepoStack', {
   env: {
     account: process.env.CDK_INTEG_ACCOUNT ?? process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_INTEG_REGION ?? process.env.CDK_DEFAULT_REGION,
   },
 });
 
-new ecr.Repository(stack, 'Repo', {
+new ecr.Repository(setupStack, 'Repo', {
   repositoryName,
   removalPolicy: RemovalPolicy.DESTROY,
   autoDeleteImages: true,
 });
 
-const lookupStack = new Stack(app, 'LookupStack', {
+const lookupStack = new Stack(app, 'EcrRepoLookupStack', {
   env: {
     account: process.env.CDK_INTEG_ACCOUNT ?? process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_INTEG_REGION ?? process.env.CDK_DEFAULT_REGION,
   },
 });
-
-ecr.Repository.fromLookup(lookupStack, 'LookupRepo', {
+new ecr.Repository(lookupStack, 'TestRepo', {
+  repositoryName: 'test-repo',
+  removalPolicy: RemovalPolicy.DESTROY,
+  autoDeleteImages: true,
+});
+const lookupRepo = ecr.Repository.fromLookup(lookupStack, 'LookupRepo', {
   repositoryName,
 });
 
+new CfnOutput(lookupStack, 'RepositoryUri', {
+  value: lookupRepo.repositoryUri,
+});
+
 new IntegTest(app, 'cdk-integ-auto-delete-images', {
-  enableLookups: true,
-  testCases: [stack],
+  testCases: [lookupStack],
 });
