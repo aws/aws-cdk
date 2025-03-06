3fabf43e69a5959187ee254551c1b0207622aae0
Matsuda <yiyth.fcb6@gmail.com>
2025-03-07 04:02:30 +0900

    chore(rds): add new MariaDB engine versions 10.5.28, 10.6.21, 10.11.11, and 11.4.5 (#33665)
    Ref: https://aws.amazon.com/about-aws/whats-new/2025/02/amazon-rds-for-mariadb-supports-minors/
    
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-rds/lib/instance-engine.ts
+++ packages/aws-cdk-lib/aws-rds/lib/instance-engine.ts
@@ -499,6 +499,8 @@ export class MariaDbEngineVersion {
   public static readonly VER_10_5_26 = MariaDbEngineVersion.of('10.5.26', '10.5');
   /** Version "10.5.27". */
   public static readonly VER_10_5_27 = MariaDbEngineVersion.of('10.5.27', '10.5');
+  /** Version "10.5.28". */
+  public static readonly VER_10_5_28 = MariaDbEngineVersion.of('10.5.28', '10.5');
 
   /** Version "10.6" (only a major version, without a specific minor version). */
   public static readonly VER_10_6 = MariaDbEngineVersion.of('10.6', '10.6');
@@ -548,6 +550,8 @@ export class MariaDbEngineVersion {
   public static readonly VER_10_6_19 = MariaDbEngineVersion.of('10.6.19', '10.6');
   /** Version "10.6.20". */
   public static readonly VER_10_6_20 = MariaDbEngineVersion.of('10.6.20', '10.6');
+  /** Version "10.6.21". */
+  public static readonly VER_10_6_21 = MariaDbEngineVersion.of('10.6.21', '10.6');
 
   /** Version "10.11" (only a major version, without a specific minor version). */
   public static readonly VER_10_11 = MariaDbEngineVersion.of('10.11', '10.11');
@@ -565,10 +569,14 @@ export class MariaDbEngineVersion {
   public static readonly VER_10_11_9 = MariaDbEngineVersion.of('10.11.9', '10.11');
   /** Version "10.11.10". */
   public static readonly VER_10_11_10 = MariaDbEngineVersion.of('10.11.10', '10.11');
+  /** Version "10.11.11". */
+  public static readonly VER_10_11_11 = MariaDbEngineVersion.of('10.11.11', '10.11');
   /** Version "11.4.3". */
   public static readonly VER_11_4_3 = MariaDbEngineVersion.of('11.4.3', '11.4');
   /** Version "11.4.4". */
   public static readonly VER_11_4_4 = MariaDbEngineVersion.of('11.4.4', '11.4');
+  /** Version "11.4.5". */
+  public static readonly VER_11_4_5 = MariaDbEngineVersion.of('11.4.5', '11.4');
 
   /**
    * Create a new MariaDbEngineVersion with an arbitrary version.
