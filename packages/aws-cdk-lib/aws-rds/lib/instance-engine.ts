1ed0d7606279ae36ec171aa24b6c712109238f56
Robert Djurasaj <robertd@users.noreply.github.com>
2025-03-10 12:01:54 -0600

    chore(rds): deprecate PostgreSQL minor versions 13.11, 13.12, 13.13, 13.14, 14.9, 14.10, 14.11, 15.4, 15.5, 15.6, 16.1, 16.2 (#33663)
    Deprecate PostgreSQL minor versions 13.11, 13.12, 13.13, 13.14, 14.9, 14.10, 14.11, 15.4, 15.5, 15.6, 16.1, 16.2
    
    ### Reason for this change
    
    Received this email few days ago:
    
    >You are receiving this message because you have one or more instances running Amazon RDS for PostgreSQL minor versions 13.11, 13.12, 13.13, 13.14, 14.9, 14.10, 14.11, 15.4, 15.5, 15.6, 16.1, or 16.2 that require your attention. Amazon RDS for PostgreSQL minor versions listed above will reach end of standard support on March 31, 2025 12:01 AM PDT. To learn more about the RDS policies related to major and minor version support, please refer to Database Engine Versions section in RDS FAQs [1].
    
    >Starting December 9, 2024, 12:01 AM PDT, you will not be able to create new RDS instances with PostgreSQL minor versions 13.11, 13.12, 13.13, 13.14, 14.9, 14.10, 14.11, 15.4, 15.5, 15.6, 16.1, or 16.2 from either the AWS Console or the CLI. RDS will upgrade your databases running versions listed above to PostgreSQL 13.18, 14.15, 15.10, 16.6, or higher during a scheduled maintenance window between March 31, 2025 12:01 AM PDT and April 30, 2025 12:01 AM PDT. Starting on April 30, 2025 12:01 AM PDT, any RDS for PostgreSQL databases still running in the affected minor versions will be upgraded to 13.18, 14.15, 15.10, 16.6, or higher regardless of instances scheduled maintenance window.
    
    >We recommend that you take action and upgrade [2] your Amazon RDS for PostgreSQL databases running minor versions 13.11 to 13.14, 14.9 to 14.11, 15.4 to 15.6, and 16.1 to 16.2 to versions 13.18, 14.15, 15.10, 16.6, or higher before March 31, 2025 12:01 AM PDT. Alternatively, you can enable Automatic Minor Version Upgrade [3] to allow Amazon RDS to upgrade your instances.
    
    >This is a mandatory engine version upgrade and it requires downtime. To minimize the downtime during the upgrade, you can now use the fully managed Amazon RDS Blue/Green Deployments that uses physical replication for minor version upgrades [4].
    
    >We recommend upgrading these instances to newer minor versions to benefit from patches for known security vulnerabilities, as well as bug fixes, performance improvements, and new functionalities added by the PostgreSQL community. To learn more about documented vulnerabilities of PostgreSQL versions, please visit the PostgreSQL security page [5].
    
    >Your Amazon RDS for PostgreSQL instances running minor versions mentioned above are listed in 'Affected Resources' tab of the AWS Health Dashboard.
    
    
    
    ### Description of changes
    
    Deprecated the following PostgreSQL minor versions: 13.11, 13.12, 13.13, 13.14, 14.9, 14.10, 14.11, 15.4, 15.5, 15.6, 16.1, 16.2
    
    ### Describe any new or updated permissions being added
    
    n/a
    
    
    ### Description of how you validated changes
    
    n/a
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-rds/lib/instance-engine.ts
+++ packages/aws-cdk-lib/aws-rds/lib/instance-engine.ts
@@ -1625,11 +1625,20 @@ export class PostgresEngineVersion {
    * @deprecated PostgreSQL 13.10 is no longer supported by Amazon RDS.
    */
   public static readonly VER_13_10 = PostgresEngineVersion.of('13.10', '13', { s3Import: true, s3Export: true });
-  /** Version "13.11". */
+  /**
+   * Version "13.11".
+   * @deprecated PostgreSQL 13.11 is no longer supported by Amazon RDS.
+   */
   public static readonly VER_13_11 = PostgresEngineVersion.of('13.11', '13', { s3Import: true, s3Export: true });
-  /** Version "13.12". */
+  /**
+   * Version "13.12".
+   * @deprecated PostgreSQL 13.12 is no longer supported by Amazon RDS.
+   */
   public static readonly VER_13_12 = PostgresEngineVersion.of('13.12', '13', { s3Import: true, s3Export: true });
-  /** Version "13.13". */
+  /**
+   * Version "13.13".
+   * @deprecated PostgreSQL 13.13 is no longer supported by Amazon RDS.
+   */
   public static readonly VER_13_13 = PostgresEngineVersion.of('13.13', '13', { s3Import: true, s3Export: true });
   /** Version "13.14". */
   public static readonly VER_13_14 = PostgresEngineVersion.of('13.14', '13', { s3Import: true, s3Export: true });
@@ -1688,11 +1697,20 @@ export class PostgresEngineVersion {
    * @deprecated PostgreSQL 14.8 is no longer supported by Amazon RDS.
    */
   public static readonly VER_14_8 = PostgresEngineVersion.of('14.8', '14', { s3Import: true, s3Export: true });
-  /** Version "14.9". */
+  /**
+   * Version "14.9".
+   * @deprecated PostgreSQL 14.9 is no longer supported by Amazon RDS.
+   */
   public static readonly VER_14_9 = PostgresEngineVersion.of('14.9', '14', { s3Import: true, s3Export: true });
-  /** Version "14.10". */
+  /**
+   * Version "14.10".
+   * @deprecated PostgreSQL 14.10 is no longer supported by Amazon RDS.
+   */
   public static readonly VER_14_10 = PostgresEngineVersion.of('14.10', '14', { s3Import: true, s3Export: true });
-  /** Version "14.11". */
+  /**
+   * Version "14.11".
+   * @deprecated PostgreSQL 14.11 is no longer supported by Amazon RDS.
+   */
   public static readonly VER_14_11 = PostgresEngineVersion.of('14.11', '14', { s3Import: true, s3Export: true });
   /** Version "14.12". */
   public static readonly VER_14_12 = PostgresEngineVersion.of('14.12', '14', { s3Import: true, s3Export: true });
@@ -1719,11 +1737,20 @@ export class PostgresEngineVersion {
    * @deprecated PostgreSQL 15.3 is no longer supported by Amazon RDS.
    */
   public static readonly VER_15_3 = PostgresEngineVersion.of('15.3', '15', { s3Import: true, s3Export: true });
-  /** Version "15.4". */
+  /**
+   * Version "15.4".
+   * @deprecated PostgreSQL 15.4 is no longer supported by Amazon RDS.
+   */
   public static readonly VER_15_4 = PostgresEngineVersion.of('15.4', '15', { s3Import: true, s3Export: true });
-  /** Version "15.5". */
+  /**
+   * Version "15.5".
+   * @deprecated PostgreSQL 15.5 is no longer supported by Amazon RDS
+   */
   public static readonly VER_15_5 = PostgresEngineVersion.of('15.5', '15', { s3Import: true, s3Export: true });
-  /** Version "15.6". */
+  /**
+   * Version "15.6".
+   * @deprecated PostgreSQL 15.6 is no longer supported by Amazon RDS
+   */
   public static readonly VER_15_6 = PostgresEngineVersion.of('15.6', '15', { s3Import: true, s3Export: true });
   /** Version "15.7". */
   public static readonly VER_15_7 = PostgresEngineVersion.of('15.7', '15', { s3Import: true, s3Export: true });
@@ -1740,9 +1767,15 @@ export class PostgresEngineVersion {
 
   /** Version "16" (only a major version, without a specific minor version). */
   public static readonly VER_16 = PostgresEngineVersion.of('16', '16', { s3Import: true, s3Export: true });
-  /** Version "16.1". */
+  /**
+   * Version "16.1".
+   * @deprecated PostgreSQL 16.1 is no longer supported by Amazon RDS
+   */
   public static readonly VER_16_1 = PostgresEngineVersion.of('16.1', '16', { s3Import: true, s3Export: true });
-  /** Version "16.2". */
+  /**
+   * Version "16.2".
+   * @deprecated PostgreSQL 16.2 is no longer supported by Amazon RDS
+   */
   public static readonly VER_16_2 = PostgresEngineVersion.of('16.2', '16', { s3Import: true, s3Export: true });
   /** Version "16.3". */
   public static readonly VER_16_3 = PostgresEngineVersion.of('16.3', '16', { s3Import: true, s3Export: true });
