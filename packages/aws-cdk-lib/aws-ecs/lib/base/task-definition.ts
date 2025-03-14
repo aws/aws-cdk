13694226ae0333bf60cfc1f273c1a1c6c7fa97cd
Murali Krishnan <110023377+mrlikl@users.noreply.github.com>
2025-03-14 23:07:01 +0530

    feat(ecs): add validation checks to memory cpu combinations of FARGATE compatible task definitions (#33608)
    ### Issue # (if applicable)
    
    Closes #22216
    
    ### Reason for this change
    
    
    
    ### Description of changes
    
    Trying to fix the cause of regression by adding a validation to see if values are tokens 
    https://github.com/aws/aws-cdk/issues/31106
    
    
    Added a validateFargateTaskDefinitionMemoryCpu method in the base task definition and run this validation when a task definition has FARGATE in requiresCompatibilities. 
    
    This is in line with the ECS requirement that even if a task definition has EC2 compatibility (along with FARGATE), FARGATE validations apply
    
    
    ### Description of how you validated changes
    
    unit and integ tests and also the [previous issue](https://github.com/aws/aws-cdk/issues/31106#issuecomment-2289166090)
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-ecs/lib/base/task-definition.ts
+++ packages/aws-cdk-lib/aws-ecs/lib/base/task-definition.ts
@@ -2,7 +2,7 @@ import { Construct } from 'constructs';
 import { ImportedTaskDefinition } from './_imported-task-definition';
 import * as ec2 from '../../../aws-ec2';
 import * as iam from '../../../aws-iam';
-import { IResource, Lazy, Names, PhysicalName, Resource } from '../../../core';
+import { IResource, Lazy, Names, PhysicalName, Resource, Token } from '../../../core';
 import { addConstructMetadata, MethodMetadata } from '../../../core/lib/metadata-resource';
 import { ContainerDefinition, ContainerDefinitionOptions, PortMapping, Protocol } from '../container-definition';
 import { CfnTaskDefinition } from '../ecs.generated';
@@ -443,22 +443,8 @@ export class TaskDefinition extends TaskDefinitionBase {
     }
 
     this.networkMode = props.networkMode ?? (this.isFargateCompatible ? NetworkMode.AWS_VPC : NetworkMode.BRIDGE);
-    if (this.isFargateCompatible && this.networkMode !== NetworkMode.AWS_VPC) {
-      throw new Error(`Fargate tasks can only have AwsVpc network mode, got: ${this.networkMode}`);
-    }
     if (props.proxyConfiguration && this.networkMode !== NetworkMode.AWS_VPC) {
       throw new Error(`ProxyConfiguration can only be used with AwsVpc network mode, got: ${this.networkMode}`);
-    }
-    if (props.placementConstraints && props.placementConstraints.length > 0 && this.isFargateCompatible) {
-      throw new Error('Cannot set placement constraints on tasks that run on Fargate');
-    }
-
-    if (this.isFargateCompatible && (!props.cpu || !props.memoryMiB)) {
-      throw new Error(`Fargate-compatible tasks require both CPU (${props.cpu}) and memory (${props.memoryMiB}) specifications`);
-    }
-
-    if (props.inferenceAccelerators && props.inferenceAccelerators.length > 0 && this.isFargateCompatible) {
-      throw new Error('Cannot use inference accelerators on tasks that run on Fargate');
     }
 
     if (this.isExternalCompatible && ![NetworkMode.BRIDGE, NetworkMode.HOST, NetworkMode.NONE].includes(this.networkMode)) {
@@ -467,6 +453,29 @@ export class TaskDefinition extends TaskDefinitionBase {
 
     if (!this.isFargateCompatible && props.runtimePlatform) {
       throw new Error('Cannot specify runtimePlatform in non-Fargate compatible tasks');
+    }
+
+    if (this.isFargateCompatible) {
+      if (this.networkMode !== NetworkMode.AWS_VPC) {
+        throw new Error(`Fargate tasks can only have AwsVpc network mode, got: ${this.networkMode}`);
+      }
+
+      if (props.placementConstraints && props.placementConstraints.length > 0) {
+        throw new Error('Cannot set placement constraints on tasks that run on Fargate');
+      }
+
+      if (!props.cpu || !props.memoryMiB) {
+        throw new Error(`Fargate-compatible tasks require both CPU (${props.cpu}) and memory (${props.memoryMiB}) specifications`);
+      }
+
+      if (props.inferenceAccelerators && props.inferenceAccelerators.length > 0) {
+        throw new Error('Cannot use inference accelerators on tasks that run on Fargate');
+      }
+
+      // Check the combination as per doc https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html
+      this.node.addValidation({
+        validate: () => this.validateFargateTaskDefinitionMemoryCpu(props.cpu!, props.memoryMiB!),
+      });
     }
 
     // https://docs.aws.amazon.com/AmazonECS/latest/developerguide/fault-injection.html
@@ -913,21 +922,58 @@ export class TaskDefinition extends TaskDefinitionBase {
   }
 
   private checkFargateWindowsBasedTasksSize(cpu: string, memory: string, runtimePlatform: RuntimePlatform) {
-    if (Number(cpu) === 1024) {
-      if (Number(memory) < 1024 || Number(memory) > 8192 || (Number(memory) % 1024 !== 0)) {
-        throw new Error(`If provided cpu is ${cpu}, then memoryMiB must have a min of 1024 and a max of 8192, in 1024 increments. Provided memoryMiB was ${Number(memory)}.`);
+    if (!Token.isUnresolved(cpu) && !Token.isUnresolved(memory)) {
+      if (Number(cpu) === 1024) {
+        if (Number(memory) < 1024 || Number(memory) > 8192 || (Number(memory) % 1024 !== 0)) {
+          throw new Error(`If provided cpu is ${cpu}, then memoryMiB must have a min of 1024 and a max of 8192, in 1024 increments. Provided memoryMiB was ${Number(memory)}.`);
+        }
+      } else if (Number(cpu) === 2048) {
+        if (Number(memory) < 4096 || Number(memory) > 16384 || (Number(memory) % 1024 !== 0)) {
+          throw new Error(`If provided cpu is ${cpu}, then memoryMiB must have a min of 4096 and max of 16384, in 1024 increments. Provided memoryMiB ${Number(memory)}.`);
+        }
+      } else if (Number(cpu) === 4096) {
+        if (Number(memory) < 8192 || Number(memory) > 30720 || (Number(memory) % 1024 !== 0)) {
+          throw new Error(`If provided cpu is ${cpu}, then memoryMiB must have a min of 8192 and a max of 30720, in 1024 increments.Provided memoryMiB was ${Number(memory)}.`);
+        }
+      } else {
+        throw new Error(`If operatingSystemFamily is ${runtimePlatform.operatingSystemFamily!._operatingSystemFamily}, then cpu must be in 1024 (1 vCPU), 2048 (2 vCPU), or 4096 (4 vCPU). Provided value was: ${cpu}`);
       }
-    } else if (Number(cpu) === 2048) {
-      if (Number(memory) < 4096 || Number(memory) > 16384 || (Number(memory) % 1024 !== 0)) {
-        throw new Error(`If provided cpu is ${cpu}, then memoryMiB must have a min of 4096 and max of 16384, in 1024 increments. Provided memoryMiB ${Number(memory)}.`);
-      }
-    } else if (Number(cpu) === 4096) {
-      if (Number(memory) < 8192 || Number(memory) > 30720 || (Number(memory) % 1024 !== 0)) {
-        throw new Error(`If provided cpu is ${cpu}, then memoryMiB must have a min of 8192 and a max of 30720, in 1024 increments.Provided memoryMiB was ${Number(memory)}.`);
-      }
-    } else {
-      throw new Error(`If operatingSystemFamily is ${runtimePlatform.operatingSystemFamily!._operatingSystemFamily}, then cpu must be in 1024 (1 vCPU), 2048 (2 vCPU), or 4096 (4 vCPU). Provided value was: ${cpu}`);
     }
+  }
+  private validateFargateTaskDefinitionMemoryCpu(cpu: string, memory: string): string[] {
+    const ret = new Array<string>();
+    if (Token.isUnresolved(cpu) || Token.isUnresolved(memory)) {
+      return ret;
+    }
+    const resolvedCpu = this.stack.resolve(cpu) as string;
+    const resolvedMemoryMiB = this.stack.resolve(memory) as string;
+    const validCpuMemoryCombinations = [
+      { cpu: '256', memory: ['512', '1024', '2048'] },
+      { cpu: '512', memory: this.range(1024, 4096, 1024) },
+      { cpu: '1024', memory: this.range(2048, 8192, 1024) },
+      { cpu: '2048', memory: this.range(4096, 16384, 1024) },
+      { cpu: '4096', memory: this.range(8192, 30720, 1024) },
+      { cpu: '8192', memory: this.range(16384, 61440, 4096) },
+      { cpu: '16384', memory: this.range(32768, 122880, 8192) },
+    ];
+
+    const isValidCombination = validCpuMemoryCombinations.some((combo) => {
+      return combo.cpu === resolvedCpu && combo.memory.includes(resolvedMemoryMiB);
+    });
+
+    if (!isValidCombination) {
+      ret.push(`Invalid CPU and memory combinations [${resolvedCpu} and ${resolvedMemoryMiB}] for FARGATE compatible task definition - https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html`);
+    }
+
+    return ret;
+  }
+
+  private range(start: number, end: number, step: number): string[] {
+    const result = [];
+    for (let i = start; i <= end; i += step) {
+      result.push(String(i));
+    }
+    return result;
   }
 }
 
