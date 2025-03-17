fef3da4a8c4a2f7b00b9bbecf6ad5be7a4903804
phuhung273 <phuhung.tranpham@gmail.com>
2025-03-17 15:42:20 +0000

    fix(cloudfront): use wildcard when grant `cloudfront:List*` permission

--- packages/aws-cdk-lib/aws-cloudfront/lib/private/utils.ts
+++ packages/aws-cdk-lib/aws-cloudfront/lib/private/utils.ts
@@ -1,4 +1,5 @@
 import { IDistribution } from '..';
+import * as iam from '../../../aws-iam';
 import { Stack } from '../../../core';
 
 /**
@@ -11,4 +12,47 @@ export function formatDistributionArn(dist: IDistribution) {
     resource: 'distribution',
     resourceName: dist.distributionId,
   });
+}
+
+/**
+ * Adds an IAM policy statement associated with this distribution to an IAM
+ * principal's policy.
+ */
+export function grant(dist: IDistribution, grantee: iam.IGrantable, ...actions: string[]): iam.Grant {
+  // cloudfront:List* can only use wildcard
+  // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/security_iam_service-with-iam.html#security_iam_service-with-iam-id-based-policies-resources
+  const listActions = [];
+  const otherActions = [];
+  let listGrant: iam.Grant;
+  let otherGrant: iam.Grant;
+
+  for (const action of actions) {
+    if (action.includes('cloudfront:List')) {
+      listActions.push(action);
+    } else {
+      otherActions.push(action);
+    }
+  }
+
+  if (listActions.length > 0) {
+    listGrant = iam.Grant.addToPrincipal({
+      grantee,
+      actions: listActions,
+      resourceArns: ['*'],
+    });
+  }
+
+  if (otherActions.length > 0) {
+    otherGrant = iam.Grant.addToPrincipal({
+      grantee,
+      actions: otherActions,
+      resourceArns: [formatDistributionArn(dist)],
+    });
+  }
+
+  if (listActions.length > 0 && otherActions.length > 0) {
+    return otherGrant!.combine(listGrant!);
+  }
+
+  return listActions.length > 0 ? listGrant! : otherGrant!;
 }
