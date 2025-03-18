e973cc67bb0374834d5b8b65072d865dafd4d679
Tietew <tietew@gmail.com>
2025-03-18 15:36:29 +0900

    integ test

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-sns-subscriptions/test/integ.firehose.ts
@@ -0,0 +1,55 @@
+import * as firehose from 'aws-cdk-lib/aws-kinesisfirehose';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import { AwsApiCall, ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+const app = new cdk.App();
+
+const stack = new cdk.Stack(app, 'aws-cdk-sns-firehose');
+
+const topic = new sns.Topic(stack, 'MyTopic');
+const deadLetterQueue = new sqs.Queue(stack, 'DeadLetterQueue');
+
+const bucket = new s3.Bucket(stack, 'Bucket', {
+  autoDeleteObjects: true,
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+});
+const stream = new firehose.DeliveryStream(stack, 'DeliveryStream', {
+  destination: new firehose.S3Bucket(bucket, {
+    bufferingInterval: cdk.Duration.seconds(30),
+    loggingConfig: { logging: false },
+  }),
+});
+
+topic.addSubscription(new subs.FirehoseSubscription(stream, {
+  deadLetterQueue,
+}));
+
+const integTest = new IntegTest(app, 'sns-firehose-integ', {
+  testCases: [stack],
+});
+
+integTest.assertions.awsApiCall('sns', 'Publish', {
+  TopicArn: topic.topicArn,
+  Message: 'Hello, world!',
+});
+const s3ApiCall = integTest.assertions.awsApiCall('s3', 'ListObjectsV2', {
+  Bucket: bucket.bucketName,
+  MaxKeys: 1,
+}).expect(ExpectedResult.objectLike({
+  KeyCount: 1,
+})).waitForAssertions({
+  interval: cdk.Duration.seconds(30),
+  totalTimeout: cdk.Duration.minutes(10),
+});
+
+if (s3ApiCall instanceof AwsApiCall) {
+  s3ApiCall.waiterProvider?.addToRolePolicy({
+    Effect: 'Allow',
+    Action: ['s3:GetObject', 's3:ListBucket'],
+    Resource: ['*'],
+  });
+}
