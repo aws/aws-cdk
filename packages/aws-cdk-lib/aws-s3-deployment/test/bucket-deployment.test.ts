58031816ccb892c236415e2b883092225f5a03d5
cedricfarinazzo <cedric.farinazzo@gmail.com>
2025-03-18 17:23:58 +0100

    feat(aws-s3-deployment): fix tests

--- packages/aws-cdk-lib/aws-s3-deployment/test/bucket-deployment.test.ts
+++ packages/aws-cdk-lib/aws-s3-deployment/test/bucket-deployment.test.ts
@@ -1361,6 +1361,7 @@ test('"SourceMarkers" is not included if none of the sources have markers', () =
     'DestinationBucketName',
     'Prune',
     'OutputObjectKeys',
+    'SyncWithSizeOnly'
   ]);
 });
 
