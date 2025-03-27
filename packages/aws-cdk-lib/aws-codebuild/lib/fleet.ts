ff510254c7ce0314872635ceeba5cf7745c01ebf
aws-cdk-automation <aws-cdk-automation@users.noreply.github.com>
2025-03-27 05:58:02 +0000

    chore(codebuild): add new enum values for codebuild

--- packages/aws-cdk-lib/aws-codebuild/lib/fleet.ts
+++ packages/aws-cdk-lib/aws-codebuild/lib/fleet.ts
@@ -315,4 +315,29 @@ export enum FleetComputeType {
    * @see https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-compute-types.html#environment-reserved-capacity.types
    */
   ATTRIBUTE_BASED = ComputeType.ATTRIBUTE_BASED,
+
+  /**
+   * PLACEHOLDER_COMMENT_TO_BE_FILLED_OUT
+   */
+  BUILD_LAMBDA_10GB = 'BUILD_LAMBDA_10GB',
+
+  /**
+   * PLACEHOLDER_COMMENT_TO_BE_FILLED_OUT
+   */
+  BUILD_LAMBDA_1GB = 'BUILD_LAMBDA_1GB',
+
+  /**
+   * PLACEHOLDER_COMMENT_TO_BE_FILLED_OUT
+   */
+  BUILD_LAMBDA_2GB = 'BUILD_LAMBDA_2GB',
+
+  /**
+   * PLACEHOLDER_COMMENT_TO_BE_FILLED_OUT
+   */
+  BUILD_LAMBDA_4GB = 'BUILD_LAMBDA_4GB',
+
+  /**
+   * PLACEHOLDER_COMMENT_TO_BE_FILLED_OUT
+   */
+  BUILD_LAMBDA_8GB = 'BUILD_LAMBDA_8GB',
 }
