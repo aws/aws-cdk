a9730bf5e4c0c5c25045fa531814215ac2c4b519
maz <13781813+mazyu36@users.noreply.github.com>
2025-03-28 22:08:53 +0900

    feat(amplify): add compute role support for Amplify app

---
+++ packages/@aws-cdk/aws-amplify-alpha/test/integ.app-compute-role.ts
@@ -0,0 +1,30 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import * as amplify from '../lib';
+
+class TestStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    const computeRole = new iam.Role(this, 'ComputeRole', {
+      assumedBy: new iam.ServicePrincipal('amplify.amazonaws.com'),
+    });
+
+    const amplifyApp = new amplify.App(this, 'App', {
+      cacheConfigType: amplify.CacheConfigType.AMPLIFY_MANAGED_NO_COOKIES,
+      computeRole,
+    });
+
+    amplifyApp.addBranch('main');
+  }
+}
+
+const app = new App();
+const stack = new TestStack(app, 'cdk-amplify-app-compute-role');
+
+new IntegTest(app, 'amplify-app-compute-role-integ', {
+  testCases: [stack],
+  enableLookups: true,
+  stackUpdateWorkflow: false,
+});
