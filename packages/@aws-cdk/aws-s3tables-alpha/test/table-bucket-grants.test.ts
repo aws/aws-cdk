235100a6bf8db9b01734bcfa05a6747585938e66
Soham Kulkarni <sohamkul@amazon.com>
2025-04-02 11:56:22 -0700

    Add test for IAM policy isolation on TableBucket

--- packages/@aws-cdk/aws-s3tables-alpha/test/table-bucket-grants.test.ts
+++ packages/@aws-cdk/aws-s3tables-alpha/test/table-bucket-grants.test.ts
@@ -13,11 +13,12 @@ describe('Access grant methods', () => {
   const DEFAULT_PROPS: s3tables.TableBucketProps = { tableBucketName: 'example-table-bucket' };
   const TABLE_BUCKET_ARN_SUB = { 'Fn::GetAtt': ['ExampleTableBucket9B5A2796', 'TableBucketARN'] };
   const TABLE_UUID = 'example-table-uuid';
-  const RESOURCES_WITH_TABLE_ARN = [
-    TABLE_BUCKET_ARN_SUB,
-    { 'Fn::Join': ['', [TABLE_BUCKET_ARN_SUB, `/table/${TABLE_UUID}`]] },
-  ];
   const EXISTING_ROLE_ARN = 'arn:aws:iam::123456789012:role/existing-role';
+  const getResourcesWithTablesArn = (tableId: string) => [
+    TABLE_BUCKET_ARN_SUB,
+    { 'Fn::Join': ['', [TABLE_BUCKET_ARN_SUB, `/table/${tableId}`]] },
+  ];
+  const RESOURCES_WITH_TABLE_ARN = getResourcesWithTablesArn(TABLE_UUID);
 
   let stack: core.Stack;
   let tableBucket: s3tables.TableBucket;
@@ -342,6 +343,79 @@ describe('Access grant methods', () => {
                 'AWS': EXISTING_ROLE_ARN,
               },
               'Resource': RESOURCES_WITH_TABLE_ARN,
+            },
+          ],
+        },
+      });
+    });
+  });
+
+  describe('Multiple permissions on same bucket', () => {
+    it('permissions are isolated per principal', () => {
+      const accountPrincipal = new iam.AccountPrincipal('123456789012');
+      const servicePrincipal = new iam.ServicePrincipal(PRINCIPAL);
+      tableBucket.grantRead(accountPrincipal, 'table1');
+      tableBucket.grantWrite(importedRole, 'table2');
+      tableBucket.grantRead(servicePrincipal, 'table3');
+
+      const statement = new iam.PolicyStatement({
+        effect: iam.Effect.DENY,
+        actions: ['s3tables:DeleteTable'],
+        principals: [new iam.ServicePrincipal('backup.amazonaws.com')],
+        resources: ['*'],
+      });
+      tableBucket.addToResourcePolicy(statement);
+
+      // tableBucketPolicy should have 4 different IAM policies
+      Template.fromStack(stack).hasResourceProperties(TABLE_BUCKET_POLICY_CFN_RESOURCE, {
+        'ResourcePolicy': {
+          'Statement': [
+            {
+              'Action': perms.TABLE_BUCKET_READ_ACCESS,
+              'Effect': 'Allow',
+              'Principal': {
+                'AWS': {
+                  'Fn::Join': ['', ['arn:', { 'Ref': 'AWS::Partition' }, ':iam::123456789012:root']],
+                },
+              },
+              'Resource': getResourcesWithTablesArn('table1'),
+            },
+            {
+              'Action': perms.TABLE_BUCKET_WRITE_ACCESS,
+              'Effect': 'Allow',
+              'Principal': {
+                'AWS': importedRole.roleArn,
+              },
+              'Resource': getResourcesWithTablesArn('table2'),
+            },
+            {
+              'Action': perms.TABLE_BUCKET_READ_ACCESS,
+              'Effect': 'Allow',
+              'Principal': {
+                'Service': servicePrincipal.service,
+              },
+              'Resource': getResourcesWithTablesArn('table3'),
+            },
+            {
+              'Action': 's3tables:DeleteTable',
+              'Effect': 'Deny',
+              'Principal': {
+                'Service': 'backup.amazonaws.com',
+              },
+              'Resource': '*',
+            },
+          ],
+        },
+      });
+
+      // Imported role policy should have write access to table2
+      Template.fromStack(stack).hasResourceProperties('AWS::IAM::Policy', {
+        'PolicyDocument': {
+          'Statement': [
+            {
+              'Action': perms.TABLE_BUCKET_WRITE_ACCESS,
+              'Effect': 'Allow',
+              'Resource': getResourcesWithTablesArn('table2'),
             },
           ],
         },
