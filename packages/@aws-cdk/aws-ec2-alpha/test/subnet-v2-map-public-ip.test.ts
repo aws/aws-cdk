42a13b2205cb66a32316d1a150c79d55e9b64413
shikha372 <shikagg@amazon.com>
2025-04-07 11:22:02 -0700

    feat(ec2-alpha): implement mapPublicIpOnLaunch prop in SubnetV2

--- packages/@aws-cdk/aws-ec2-alpha/test/subnet-v2-map-public-ip.test.ts
+++ packages/@aws-cdk/aws-ec2-alpha/test/subnet-v2-map-public-ip.test.ts
@@ -4,41 +4,6 @@ import { SubnetType } from 'aws-cdk-lib/aws-ec2';
 import { IpCidr, SubnetV2, VpcV2, IpAddresses } from '../lib';
 
 describe('SubnetV2 mapPublicIpOnLaunch', () => {
-  test('mapPublicIpOnLaunch defaults to false for all subnet types', () => {
-    // GIVEN
-    const stack = new Stack();
-    const vpc = new VpcV2(stack, 'VPC', {
-      primaryAddressBlock: IpAddresses.ipv4('10.0.0.0/16'),
-    });
-
-    // WHEN - public subnet
-    new SubnetV2(stack, 'PublicSubnet', {
-      vpc,
-      availabilityZone: 'us-east-1a',
-      ipv4CidrBlock: new IpCidr('10.0.0.0/24'),
-      subnetType: SubnetType.PUBLIC,
-    });
-
-    // WHEN - private subnet
-    new SubnetV2(stack, 'PrivateSubnet', {
-      vpc,
-      availabilityZone: 'us-east-1b',
-      ipv4CidrBlock: new IpCidr('10.0.1.0/24'),
-      subnetType: SubnetType.PRIVATE_WITH_EGRESS,
-    });
-
-    // THEN
-    Template.fromStack(stack).hasResourceProperties('AWS::EC2::Subnet', {
-      CidrBlock: '10.0.0.0/24',
-      MapPublicIpOnLaunch: false,
-    });
-
-    Template.fromStack(stack).hasResourceProperties('AWS::EC2::Subnet', {
-      CidrBlock: '10.0.1.0/24',
-      MapPublicIpOnLaunch: false,
-    });
-  });
-
   test('mapPublicIpOnLaunch can be explicitly set to true for public subnets', () => {
     // GIVEN
     const stack = new Stack();
