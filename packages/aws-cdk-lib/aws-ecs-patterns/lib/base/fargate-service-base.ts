3d31c49fdf3cd0dc8abf69aa1d2e7dc698c57231
Karthik Kumar Viswanathan <karthvif@amazon.com>
2025-04-09 08:59:00 -0700

    fix(ecs): Move AvailabilityZoneRebalancing into aws-ecs-patterns.

--- packages/aws-cdk-lib/aws-ecs-patterns/lib/base/fargate-service-base.ts
+++ packages/aws-cdk-lib/aws-ecs-patterns/lib/base/fargate-service-base.ts
@@ -1,4 +1,5 @@
 import { FargatePlatformVersion, FargateTaskDefinition, RuntimePlatform } from '../../../aws-ecs';
+import { AvailabilityZoneRebalancing } from '../availability-zone-rebalancing';
 
 export interface FargateServiceBaseProps {
   /**
@@ -89,4 +90,15 @@ export interface FargateServiceBaseProps {
    * @default - If the property is undefined, `operatingSystemFamily` is LINUX and `cpuArchitecture` is X86_64
    */
   readonly runtimePlatform?: RuntimePlatform;
+
+  /**
+   * Whether to use Availability Zone rebalancing for the service.
+   *
+   * If enabled, `maxHealthyPercent` must be greater than 100, and the service must not be a target
+   * of a Classic Load Balancer.
+   *
+   * @see https://docs.aws.amazon.com/AmazonECS/latest/developerguide/service-rebalancing.html
+   * @default AvailabilityZoneRebalancing.DISABLED
+   */
+  readonly availabilityZoneRebalancing?: AvailabilityZoneRebalancing;
 }
