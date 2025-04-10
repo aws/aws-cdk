c75c76d25d2313ede6cacc0af8c83ff28d3f4968
Hung Tran <40334379+phuhung273@users.noreply.github.com>
2025-04-11 02:24:34 +0700

    feat(apigatewayv2): `HttpStage` access logging (#33977)
    ### Issue # (if applicable)
    
    Closes #11100
    
    ### Description of changes
    - apigwv2 HttpStage support access logging to CloudWatch Logs https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-logging.html
    - Same logic as V1 without Firehose as not supported.
    
    ### Description of how you validated changes
    Unit + Integ
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-apigatewayv2/lib/common/base.ts
+++ packages/aws-cdk-lib/aws-apigatewayv2/lib/common/base.ts
@@ -1,9 +1,11 @@
 import { IApi } from './api';
 import { ApiMapping } from './api-mapping';
-import { DomainMappingOptions, IStage } from './stage';
+import { DomainMappingOptions, IAccessLogSettings, IStage } from './stage';
+import { AccessLogFormat } from '../../../aws-apigateway/lib';
 import * as cloudwatch from '../../../aws-cloudwatch';
-import { Resource } from '../../../core';
-import { UnscopedValidationError } from '../../../core/lib/errors';
+import { Resource, Token } from '../../../core';
+import { UnscopedValidationError, ValidationError } from '../../../core/lib/errors';
+import { CfnStage } from '../apigatewayv2.generated';
 
 /**
  * Base class representing an API
@@ -57,6 +59,27 @@ export abstract class StageBase extends Resource implements IStage {
     });
     // ensure the dependency
     this.node.addDependency(domainMapping.domainName);
+  }
+
+  /**
+   * @internal
+   */
+  protected _validateAccessLogSettings(props?: IAccessLogSettings): CfnStage.AccessLogSettingsProperty | undefined {
+    if (!props) return;
+
+    const format = props.format;
+    if (
+      format &&
+      !Token.isUnresolved(format.toString()) &&
+      !/\$context\.(?:requestId|extendedRequestId)\b/.test(format.toString())
+    ) {
+      throw new ValidationError('Access log must include either `AccessLogFormat.contextRequestId()` or `AccessLogFormat.contextExtendedRequestId()`', this);
+    }
+
+    return {
+      destinationArn: props.destination.bind(this).destinationArn,
+      format: format ? format.toString() : AccessLogFormat.clf().toString(),
+    };
   }
 
   public metric(metricName: string, props?: cloudwatch.MetricOptions): cloudwatch.Metric {
