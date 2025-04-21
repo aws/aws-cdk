ba9f1a88b9ceaac659a22eb2c0635b29e05b0973
dinsajwa <dinsajwa@amazon.com>
2025-04-21 15:47:38 -0400

    fix(bedrockl2):updated integ test

--- packages/@aws-cdk/aws-bedrock-alpha/test/bedrock/agents/integ.agent.ts
+++ packages/@aws-cdk/aws-bedrock-alpha/test/bedrock/agents/integ.agent.ts
@@ -10,12 +10,7 @@ import * as bedrock from '../../../lib';
 
 const app = new cdk.App();
 
-const stack = new cdk.Stack(app, 'aws-cdk-bedrock-agent-1', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+const stack = new cdk.Stack(app, 'aws-cdk-bedrock-agent-1');
 
 // Create a Bedrock Agent with default values
 new bedrock.Agent(stack, 'MyAgent', {
