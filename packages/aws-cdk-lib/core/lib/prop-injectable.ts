aa89620c48b10ac3828fa5bdf099e47dbf4fe9f2
Mohamed Elasmar <71043312+moelasmar@users.noreply.github.com>
2025-04-23 02:10:17 -0700

    Keep the static Class' name property

--- packages/aws-cdk-lib/core/lib/prop-injectable.ts
+++ packages/aws-cdk-lib/core/lib/prop-injectable.ts
@@ -24,7 +24,7 @@ export function propertyInjectable<T extends PropertyInjectableConstructConstruc
   //
   // I couldn't find a clear reference on what that error is trying to say, but it's possible
   // to cast it away, and the signature of the containing function seems to hold water.
-  return class extends (constructor as ArbitraryConstructor) {
+  const WrappedClass = class extends (constructor as ArbitraryConstructor) {
     constructor(scope: Construct, id: string, props: object, ...args: any[]) {
       // eslint-disable-next-line dot-notation
       const uniqueId = constructor.PROPERTY_INJECTION_ID;
@@ -35,5 +35,11 @@ export function propertyInjectable<T extends PropertyInjectableConstructConstruc
 
       super(scope, id, props, ...args);
     }
-  } as any;
+  };
+  // Preserve the static `name` property
+  Object.defineProperty(WrappedClass, 'name', {
+    value: constructor.name,
+    writable: false,
+  });
+  return WrappedClass as T;
 }
