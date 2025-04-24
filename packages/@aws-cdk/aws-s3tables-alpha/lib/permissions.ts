5b8574ea16963b014b14470d021de4d539040385
Soham Kulkarni <sohamkul@amazon.com>
2025-04-24 16:01:39 -0700

    Add KMS support for TableBucket

--- packages/@aws-cdk/aws-s3tables-alpha/lib/permissions.ts
+++ packages/@aws-cdk/aws-s3tables-alpha/lib/permissions.ts
@@ -20,3 +20,19 @@ export const TABLE_BUCKET_READ_WRITE_ACCESS = [...new Set([
   ...TABLE_BUCKET_READ_ACCESS,
   ...TABLE_BUCKET_WRITE_ACCESS,
 ])];
+
+// Permissions for user defined KMS Keys
+// https://docs.aws.amazon.com/AmazonS3/latest/userguide/s3-tables-kms-permissions.html
+export const KEY_READ_ACCESS = [
+  'kms:Decrypt',
+];
+
+export const KEY_WRITE_ACCESS = [
+  'kms:GenerateDataKey*',
+  'kms:Decrypt',
+];
+
+export const KEY_READ_WRITE_ACCESS = [...new Set([
+  ...KEY_READ_ACCESS,
+  ...KEY_WRITE_ACCESS,
+])];
