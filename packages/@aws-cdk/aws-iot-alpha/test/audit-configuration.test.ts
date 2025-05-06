8260681f7e75328d9142581e90651c611b4112ac
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2025-05-07 04:04:32 +0900

    feat(iot): device certificate age check audit configuration (#33816)
    ### Issue # (if applicable)
    
    None
    
    ### Reason for this change
    
    AWS IoT now supports for new audit configuration about device certificate age check.
    https://docs.aws.amazon.com/iot-device-defender/latest/devguide/device-certificate-age-check.html
    
    ### Description of changes
    
    - Add `deviceCertificateAgeCheck` to `CheckConfiguration`
      - To enable audit configuraiton
    - Add `deviceCertificateAgeCheckDuration` to `CheckConfiguration`
      - To configure threshold duration in days
    
    ### Describe any new or updated permissions being added
    
    None
    
    ### Description of how you validated changes
    
    Add both unit and integ tests
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    BREAKING CHANGE: By default, `deviceDertificateAgeCheck` is automatically enabled.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-iot-alpha/test/audit-configuration.test.ts
+++ packages/@aws-cdk/aws-iot-alpha/test/audit-configuration.test.ts
@@ -15,6 +15,12 @@ test('Default property', () => {
       CaCertificateExpiringCheck: { Enabled: true },
       CaCertificateKeyQualityCheck: { Enabled: true },
       ConflictingClientIdsCheck: { Enabled: true },
+      DeviceCertificateAgeCheck: {
+        Enabled: true,
+        Configuration: {
+          CertAgeThresholdInDays: '365',
+        },
+      },
       DeviceCertificateExpiringCheck: { Enabled: true },
       DeviceCertificateKeyQualityCheck: { Enabled: true },
       DeviceCertificateSharedCheck: { Enabled: true },
@@ -127,6 +133,29 @@ test('configure check configuration', () => {
       CaCertificateExpiringCheck: { Enabled: true },
     },
   });
+});
+
+test('throw error for configuring duration without enabling deviceCertificateAgeCheck', () => {
+  const stack = new cdk.Stack();
+  expect(() => new iot.AccountAuditConfiguration(stack, 'AccountAuditConfiguration', {
+    checkConfiguration: {
+      deviceCertificateAgeCheck: false,
+      deviceCertificateAgeCheckDuration: cdk.Duration.days(1229),
+    },
+  })).toThrow('You cannot specify a value for `deviceCertificateAgeCheckDuration` if `deviceCertificateAgeCheck` is set to `false`.');
+});
+
+test.each([
+  cdk.Duration.days(29),
+  cdk.Duration.days(3653),
+])('throw error for invalid duration %s', (duration) => {
+  const stack = new cdk.Stack();
+  expect(() => new iot.AccountAuditConfiguration(stack, 'AccountAuditConfiguration', {
+    checkConfiguration: {
+      deviceCertificateAgeCheck: true,
+      deviceCertificateAgeCheckDuration: duration,
+    },
+  })).toThrow(`The device certificate age check threshold must be between 30 and 3652 days. got: ${duration.toDays()} days.`);
 });
 
 test('import by Account ID', () => {
