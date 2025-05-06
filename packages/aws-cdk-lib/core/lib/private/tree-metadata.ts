90d0a7aa6bb55ddc033982227cb4f675358d77d8
Rico Hermans <rix0rrr@gmail.com>
2025-05-06 16:41:41 +0200

    refactor(trace): simplify how construct traces get built (#34355)
    This change is motivated by the tree metadata containing a `parent` pointer that used to be stripped before writing it to disk in `tree.json`. I removed the `parent` pointer because shouldn't be in this data structure at all.
    
    The TreeMetadata `parent` pointer was being used to build a `ConstructTrace` for the purposes of reporting template validation errors, so that code had to be refactored as well. It was doing a complicated dance that I honestly didn't quite understand going up and down the construct tree trace and holding cached copies of various data structures in memory.
    
    I removed the dependency of the trace reporting on `TreeMetadata` and replaced it with a path traversal from the root of the construct tree, and progressively building the `ConstructTrace` from the constructs we find along the way: first building a chain of `ConstructTrace` objects without `child` and `location` information, and then adding it on later.
    
    One note on what's left here: 
    
    - As far as I can tell we are peeling apart a stack trace from the lowest-level `CfnResource` into a different single frames, to be reported in the `ConstructTrace`. I *think* I did a faithful conversion, but what I have only works if child constructs are always created in constructors. Our counting of stack frames will be off by one for every method call involved (in other words, if more than 1 stack frame separates 2 levels in the construct tree).
    
    I'm not fixing this right now because that's not what I want to focus on, but this could/should probably be a backlog item?
    
    ------
    
    As an example of the latter, let's say this construct tree:
    
    ```
    StaticWebsite
      |
      +-- Bucket
            |
            +--- CfnResource   <-- stack trace only available here
    ```
    
    The way we will do this is we'll take the stack trace from `CfnResource`, and use it as follows:
    
    - `stackTrace[0]` -> that must be where CfnResource is created
    - `stackTrace[1]` -> that must be where Bucket is created
    - `stackTrace[2]` -> that must be where StaticWebsite is created
    
    The above assumptions hold for the following code:
    
    ```ts
    // static-website.ts
    class StaticWebsite {
      constructor() {
        new Bucket(this, 'Bucket');
       }
    }
    
    // app.ts
    new StaticWebsite();
    ```
    
    But *don't* hold for the following code:
    
    ```ts
    // static-website.ts
    class StaticWebsite {
      constructor() 
        this.createStaticHtmlBucket();
      }
      private createStaticHtmlBucket() {
        new Bucket(this, 'Bucket');
      }
    }
    
    // app.ts
    new StaticWebsite();
    ```
    
    Because the initialization of `StaticWebsite` now comprises 2 stack frames instead of 1.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/core/lib/private/tree-metadata.ts
+++ packages/aws-cdk-lib/core/lib/private/tree-metadata.ts
@@ -10,9 +10,7 @@ import { ISynthesisSession } from '../stack-synthesizers';
 import { IInspectable, TreeInspector } from '../tree';
 
 const FILE_PATH = 'tree.json';
-type Mutable<T> = {
-  -readonly [P in keyof T]: Mutable<T[P]>;
-};
+
 /**
  * Construct that is automatically attached to the top-level `App`.
  * This generates, as part of synthesis, a file containing the construct tree and the metadata for each node in the tree.
@@ -20,7 +18,6 @@ type Mutable<T> = {
  *
  */
 export class TreeMetadata extends Construct {
-  private _tree?: { [path: string]: Node };
   constructor(scope: Construct) {
     super(scope, 'Tree');
   }
@@ -45,15 +42,9 @@ export class TreeMetadata extends Construct {
         .filter((child) => child !== undefined)
         .reduce((map, child) => Object.assign(map, { [child!.id]: child }), {});
 
-      const parent = construct.node.scope;
       const node: Node = {
         id: construct.node.id || 'App',
         path: construct.node.path,
-        parent: parent && parent.node.path ? {
-          id: parent.node.id,
-          path: parent.node.path,
-          constructInfo: constructInfoFromConstruct(parent),
-        } : undefined,
         children: Object.keys(childrenMap).length === 0 ? undefined : childrenMap,
         attributes: this.synthAttributes(construct),
         constructInfo: constructInfoFromConstruct(construct),
@@ -68,16 +59,8 @@ export class TreeMetadata extends Construct {
       version: 'tree-0.1',
       tree: visit(this.node.root),
     };
-    this._tree = lookup;
-
     const builder = session.assembly;
-    fs.writeFileSync(path.join(builder.outdir, FILE_PATH), JSON.stringify(tree, (key: string, value: any) => {
-      // we are adding in the `parent` attribute for internal use
-      // and it doesn't make much sense to include it in the
-      // tree.json
-      if (key === 'parent') return undefined;
-      return value;
-    }), { encoding: 'utf-8' });
+    fs.writeFileSync(path.join(builder.outdir, FILE_PATH), JSON.stringify(tree), { encoding: 'utf-8' });
 
     builder.addArtifact('Tree', {
       type: ArtifactType.CDK_TREE,
@@ -85,76 +68,6 @@ export class TreeMetadata extends Construct {
         file: FILE_PATH,
       },
     });
-  }
-
-  /**
-   * Each node will only have 1 level up (node.parent.parent will always be undefined)
-   * so we need to reconstruct the node making sure the parents are set
-   */
-  private getNodeWithParents(node: Node): Node {
-    if (!this._tree) {
-      throw new Error(`attempting to get node branch for ${node.path}, but the tree has not been created yet!`);
-    }
-    let tree = node;
-    if (node.parent) {
-      tree = {
-        ...node,
-        parent: this.getNodeWithParents(this._tree[node.parent.path]),
-      };
-    }
-    return tree;
-  }
-
-  /**
-   * Construct a new tree with only the nodes that we care about.
-   * Normally each node can contain many child nodes, but we only care about the
-   * tree that leads to a specific construct so drop any nodes not in that path
-   *
-   * @param node Node the current tree node
-   * @returns Node the root node of the new tree
-   */
-  private renderTreeWithChildren(node: Node): Node {
-    /**
-     * @param currentNode - The current node being evaluated
-     * @param currentNodeChild - The previous node which should be the only child of the current node
-     * @returns The node with all children removed except for the path to the current node
-     */
-    function renderTreeWithSingleChild(currentNode: Mutable<Node>, currentNodeChild: Mutable<Node>) {
-      currentNode.children = {
-        [currentNodeChild.id]: currentNodeChild,
-      };
-      if (currentNode.parent) {
-        currentNode.parent = renderTreeWithSingleChild(currentNode.parent, currentNode);
-      }
-      return currentNode;
-    }
-
-    const currentNode = node.parent ? renderTreeWithSingleChild(node.parent, node) : node;
-    // now that we have the new tree we need to return the root node
-    let root = currentNode;
-    do {
-      if (root.parent) {
-        root = root.parent;
-      }
-    } while (root.parent);
-
-    return root;
-  }
-
-  /**
-   * This gets a specific "branch" of the tree for a given construct path.
-   * It will return the root Node of the tree with non-relevant branches filtered
-   * out (i.e. node children that don't traverse to the given construct path)
-   *
-   * @internal
-   */
-  public _getNodeBranch(constructPath: string): Node | undefined {
-    if (!this._tree) {
-      throw new Error(`attempting to get node branch for ${constructPath}, but the tree has not been created yet!`);
-    }
-    const tree = this._tree[constructPath];
-    const treeWithParents = this.getNodeWithParents(tree);
-    return this.renderTreeWithChildren(treeWithParents);
   }
 
   private synthAttributes(construct: IConstruct): { [key: string]: any } | undefined {
@@ -177,7 +90,6 @@ export class TreeMetadata extends Construct {
 export interface Node {
   readonly id: string;
   readonly path: string;
-  readonly parent?: Node;
   readonly children?: { [key: string]: Node };
   readonly attributes?: { [key: string]: any };
 
