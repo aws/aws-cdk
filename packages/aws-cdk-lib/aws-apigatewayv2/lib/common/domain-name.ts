dd632b57798e3a2e3274b80c737e58704d69393b
Mohamed Elasmar <71043312+moelasmar@users.noreply.github.com>
2025-05-07 09:40:27 -0700

    Revert "revert: "feat: property injection for 122 constructs" (#34370)"
    This reverts commit 2594f3cab68e130d0b9f62ae622c9bc7b92cd14e.

--- packages/aws-cdk-lib/aws-apigatewayv2/lib/common/domain-name.ts
+++ packages/aws-cdk-lib/aws-apigatewayv2/lib/common/domain-name.ts
@@ -5,6 +5,7 @@ import { IBucket } from '../../../aws-s3';
 import { IResource, Lazy, Resource, Token } from '../../../core';
 import { ValidationError } from '../../../core/lib/errors';
 import { addConstructMetadata, MethodMetadata } from '../../../core/lib/metadata-resource';
+import { propertyInjectable } from '../../../core/lib/prop-injectable';
 
 /**
  * The minimum version of the SSL protocol that you want API Gateway to use for HTTPS connections.
@@ -152,7 +153,13 @@ export interface MTLSConfig {
 /**
  * Custom domain resource for the API
  */
+@propertyInjectable
 export class DomainName extends Resource implements IDomainName {
+  /**
+   * Uniquely identifies this class.
+   */
+  public static readonly PROPERTY_INJECTION_ID: string = 'aws-cdk-lib.aws-apigatewayv2.DomainName';
+
   /**
    * Import from attributes
    */
