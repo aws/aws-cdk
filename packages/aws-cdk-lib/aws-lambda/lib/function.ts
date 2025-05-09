8d413c0bc9f31a25b0f2fc35b719fc56dcd351c7
Emma Patterson <emma.patterson@sonarsource.com>
2025-05-09 12:37:19 +0200

    Enforcing ssl by default on lambda dlqs

--- packages/aws-cdk-lib/aws-lambda/lib/function.ts
+++ packages/aws-cdk-lib/aws-lambda/lib/function.ts
@@ -1667,6 +1667,7 @@ Environment variables can be marked for removal when used in Lambda@Edge by sett
     } else {
       deadLetterQueue = props.deadLetterQueue || new sqs.Queue(this, 'DeadLetterQueue', {
         retentionPeriod: Duration.days(14),
+        enforceSSL: true,
       });
       this.addToRolePolicy(new iam.PolicyStatement({
         actions: ['sqs:SendMessage'],
