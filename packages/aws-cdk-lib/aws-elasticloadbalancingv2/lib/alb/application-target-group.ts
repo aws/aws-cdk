3034acbd088aebad16d51f08b574c853591daa96
kawabata-mcl <awsakira9989@gmail.com>
2025-05-10 06:30:44 +0900

    feat(elasticloadbalancingv2): add support for multi-value headers in Lambda target groups (#34298)
    ### Issue # (if applicable)
    
    None
    
    ### Reason for this change
    
    We can set a multi-value headers property for a lambda target group from cloudformation, but this was not supported in the AWS CDK L2 construct.
    
    ### Description of changes
    
    Add multiValueHeadersEnabled property to ApplicationTargetGroupProps and set it in the ApplicationTargetGroup constructor.
    
    ### Describe any new or updated permissions being added
    
    None
    
    ### Description of how you validated changes
    
    Added both unit and integration tests.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/alb/application-target-group.ts
+++ packages/aws-cdk-lib/aws-elasticloadbalancingv2/lib/alb/application-target-group.ts
@@ -107,6 +107,20 @@ export interface ApplicationTargetGroupProps extends BaseTargetGroupProps {
    * @see https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-target-groups.html#automatic-target-weights
    */
   readonly enableAnomalyMitigation?: boolean;
+
+  /**
+   * Indicates whether the target group supports multi-value headers.
+   *
+   * If the value is true, the request and response headers exchanged between
+   * the load balancer and the Lambda function include arrays of values or strings.
+   *
+   * Only applicable for Lambda targets.
+   *
+   * @default false
+   *
+   * @see https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-target-groups.html#target-group-attributes
+   */
+  readonly multiValueHeadersEnabled?: boolean;
 }
 
 /**
@@ -365,6 +379,15 @@ export class ApplicationTargetGroup extends TargetGroupBase implements IApplicat
       if (props.loadBalancingAlgorithmType) {
         this.setAttribute('load_balancing.algorithm.type', props.loadBalancingAlgorithmType);
       }
+
+      if (props.multiValueHeadersEnabled) {
+        if (this.targetType === TargetType.LAMBDA) {
+          this.setAttribute('lambda.multi_value_headers.enabled', 'true');
+        } else {
+          throw new ValidationError('multiValueHeadersEnabled is only supported for Lambda targets.', this);
+        }
+      }
+
       this.addTarget(...(props.targets || []));
 
       if (props.enableAnomalyMitigation !== undefined) {
