b37c803b032d4e90b6fc13c6e3a5d848c996d11e
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2025-05-11 21:31:07 +0900

    add integ test

--- packages/aws-cdk-lib/aws-appconfig/lib/index.ts
+++ packages/aws-cdk-lib/aws-appconfig/lib/index.ts
@@ -3,7 +3,6 @@ export * from './deployment-strategy';
 export * from './extension';
 export * from './application';
 export * from './configuration';
-export * from './util';
 
 // AWS::AppConfig CloudFormation Resources:
 export * from './appconfig.generated';
