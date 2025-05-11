f78fcb98b540ecc4d4b4957a022a5c632fb22fee
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2025-05-11 21:55:35 +0900

    updat ereadme

--- packages/aws-cdk-lib/aws-appconfig/lib/configuration.ts
+++ packages/aws-cdk-lib/aws-appconfig/lib/configuration.ts
@@ -16,30 +16,7 @@ import * as sm from '../../aws-secretsmanager';
 import * as ssm from '../../aws-ssm';
 import { PhysicalName, Stack, ArnFormat, Names, RemovalPolicy } from '../../core';
 import * as mimeTypes from 'mime-types';
-
-/**
- * Options for deletion protection check.
- */
-export enum DeletionProtectionCheck {
-  /**
-   * Instructs AWS AppConfig to bypass the deletion protection check and delete
-   * a configuration profile even if deletion protection would have otherwise prevented it.
-   */
-  BYPASS = 'BYPASS',
-
-  /**
-   * Instructs the deletion protection check to run, even if deletion protection
-   * is disabled at the account level. APPLY also forces the deletion protection check to run
-   * against resources created in the past hour, which are normally excluded from deletion protection checks.
-   */
-  APPLY = 'APPLY',
-
-  /**
-   * The default setting, which instructs AWS AppConfig to implement the deletion
-   * protection value specified in the UpdateAccountSettings API.
-   */
-  ACCOUNT_DEFAULT = 'ACCOUNT_DEFAULT',
-}
+import { DeletionProtectionCheck } from './util';
 
 /**
  * Options for the Configuration construct
