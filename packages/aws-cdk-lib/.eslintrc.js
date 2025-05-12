2f51d5a37cf877d230f9f6f311dc08722e377776
Momo Kornher <kornherm@amazon.co.uk>
2025-05-12 09:35:53 +0100

    feat(ecs): throw typed errors

--- packages/aws-cdk-lib/.eslintrc.js
+++ packages/aws-cdk-lib/.eslintrc.js
@@ -17,8 +17,6 @@ baseConfig.rules['import/no-extraneous-dependencies'] = [
 baseConfig.rules['@cdklabs/no-throw-default-error'] = ['error'];
 // not yet supported
 const noThrowDefaultErrorNotYetSupported = [
-  'aws-ecs-patterns',
-  'aws-ecs',
   'aws-elasticsearch',
   'aws-globalaccelerator',
   'aws-globalaccelerator-endpoints',
