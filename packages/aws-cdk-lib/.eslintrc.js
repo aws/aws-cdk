5ee092a5c3abfc25c5d9c02cd7086bbcaab01556
Momo Kornher <kornherm@amazon.co.uk>
2025-05-13 16:54:54 +0200

    feat(globalaccelerator): throw `ValidationError`s instead of untyped errors (#34426)
    ### Issue
    
    Relates to #32569
    
    ### Reason for this change
    
    untyped Errors are not recommended
    
    ### Description of changes
    
    `ValidationError`s everywhere
    
    ### Describe any new or updated permissions being added
    
    None
    
    ### Description of how you validated changes
    
    Existing tests. Exemptions granted as this is a refactor of existing code.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/.eslintrc.js
+++ packages/aws-cdk-lib/.eslintrc.js
@@ -17,8 +17,6 @@ baseConfig.rules['import/no-extraneous-dependencies'] = [
 baseConfig.rules['@cdklabs/no-throw-default-error'] = ['error'];
 // not yet supported
 const noThrowDefaultErrorNotYetSupported = [
-  'aws-globalaccelerator',
-  'aws-globalaccelerator-endpoints',
   'aws-iam',
   'aws-lambda-destinations',
   'aws-lambda-event-sources',
