084114959776cba9bd7feb421bbf5e83409166f0
Arman Tiwana <astiwana@amazon.com>
2025-05-13 11:25:26 -0700

    add unit test for cluster name exceeding 100 characters

--- packages/aws-cdk-lib/aws-eks/test/cluster.test.ts
+++ packages/aws-cdk-lib/aws-eks/test/cluster.test.ts
@@ -112,6 +112,32 @@ describe('cluster', () => {
     }).toThrow(/Cannot specify clusterHandlerSecurityGroup without placeClusterHandlerInVpc set to true/);
   });
 
+  test('cluster name is trimmed when it exceeds 100 characters', () => {
+    // GIVEN
+    const { stack } = testFixture();
+
+    // Create a cluster with a name longer than 100 characters
+    const longClusterName = 'a'.repeat(110);
+
+    // WHEN
+    const cluster = new eks.Cluster(stack, 'Cluster', {
+      version: CLUSTER_VERSION,
+      clusterName: longClusterName,
+      kubectlLayer: new KubectlV31Layer(stack, 'KubectlLayer'),
+    });
+
+    // THEN
+    // The cluster name should be <= 100 characters
+    expect(cluster.clusterName.length).toBeLessThanOrEqual(100);
+
+    // The original name should be passed to the resource
+    Template.fromStack(stack).hasResourceProperties('Custom::AWSCDK-EKS-Cluster', {
+      Config: {
+        name: longClusterName,
+      },
+    });
+  });
+
   describe('imported Vpc from unparseable list tokens', () => {
     let stack: cdk.Stack;
     let vpc: ec2.IVpc;
