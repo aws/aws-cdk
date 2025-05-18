4e1ca87e2331009f0c0348e43150aded0eca57db
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2025-05-19 00:37:07 +0900

    add dsql interface endpoint

--- packages/aws-cdk-lib/aws-ec2/lib/vpc-endpoint.ts
+++ packages/aws-cdk-lib/aws-ec2/lib/vpc-endpoint.ts
@@ -478,6 +478,8 @@ export class InterfaceVpcEndpointAwsService implements IInterfaceVpcEndpointServ
   public static readonly DEVOPS_GURU = new InterfaceVpcEndpointAwsService('devops-guru');
   public static readonly DIRECTORY_SERVICE = new InterfaceVpcEndpointAwsService('ds');
   public static readonly DIRECTORY_SERVICE_DATA = new InterfaceVpcEndpointAwsService('ds-data');
+  public static readonly DSQL = new InterfaceVpcEndpointAwsService('dsql');
+  public static readonly DSQL_FNH4 = new InterfaceVpcEndpointAwsService('dsql-fnh4');
   public static readonly DYNAMODB = new InterfaceVpcEndpointAwsService('dynamodb');
   public static readonly DYNAMODB_FIPS = new InterfaceVpcEndpointAwsService('dynamodb-fips');
   public static readonly EBS_DIRECT = new InterfaceVpcEndpointAwsService('ebs');
