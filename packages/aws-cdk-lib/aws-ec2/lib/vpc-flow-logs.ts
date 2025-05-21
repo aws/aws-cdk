c648625d51486f9bbce1916fb6b4f27364caf41f
Tietew <tietew@gmail.com>
2025-05-21 12:02:18 +0900

    simplify validations

--- packages/aws-cdk-lib/aws-ec2/lib/vpc-flow-logs.ts
+++ packages/aws-cdk-lib/aws-ec2/lib/vpc-flow-logs.ts
@@ -469,11 +469,8 @@ class FirehoseDestination extends FlowLogDestination {
   }
 
   public bind(scope: Construct, flowLog: FlowLog): FlowLogDestinationConfig {
-    if (this.props.deliveryStreamArn && this.props.deliveryStream) {
-      throw new ValidationError('Specify either deliveryStream or deliveryStreamArn, but not both.', scope);
-    }
-    if (!this.props.deliveryStreamArn && !this.props.deliveryStream) {
-      throw new ValidationError('Neither deliveryStream nor deliveryStreamArn is specified.', scope);
+    if (!!this.props.deliveryStreamArn === !!this.props.deliveryStream) {
+      throw new ValidationError('Specify exactly one of either deliveryStream or deliveryStreamArn.', scope);
     }
     if (this.props.deliveryStream) {
       const compareAccount = Token.compareStrings(this.props.deliveryStream.env.account, flowLog.env.account);
