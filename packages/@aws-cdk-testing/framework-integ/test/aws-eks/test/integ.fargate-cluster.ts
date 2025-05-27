8ad42e51e9736c43b155986d31debe40f83daf30
QuantumNeuralCoder <163458725+QuantumNeuralCoder@users.noreply.github.com>
2025-05-27 15:12:22 -0700

    fix some more tests for lambda loggroup ff

--- packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.fargate-cluster.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.fargate-cluster.ts
@@ -30,7 +30,8 @@ class EksFargateClusterStack extends Stack {
 
 const app = new App({
   postCliContext: {
-    '@aws-cdk/aws-lambda:useCdkManagedLogGroup': false,    '@aws-cdk/aws-lambda:createNewPoliciesWithAddToRolePolicy': false,
+    '@aws-cdk/aws-lambda:useCdkManagedLogGroup': false,
+    '@aws-cdk/aws-lambda:createNewPoliciesWithAddToRolePolicy': false,
   },
 });
 
