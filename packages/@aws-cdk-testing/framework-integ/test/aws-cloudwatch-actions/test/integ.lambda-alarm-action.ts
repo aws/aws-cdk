eeea778a5c62c6be0af91074793b46842f4416a9
QuantumNeuralCoder <163458725+QuantumNeuralCoder@users.noreply.github.com>
2025-05-27 16:39:48 -0700

    fix some more tests for lambda loggroup ff

--- packages/@aws-cdk-testing/framework-integ/test/aws-cloudwatch-actions/test/integ.lambda-alarm-action.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cloudwatch-actions/test/integ.lambda-alarm-action.ts
@@ -80,7 +80,10 @@ new integ.IntegTest(app, 'LambdaAlarmActionIntegrationTest', {
 app.synth();
 
 const appWithFeatureFlag = new App({
-  context: { [LAMBDA_PERMISSION_LOGICAL_ID_FOR_LAMBDA_ACTION]: true },
+  context: {
+    [LAMBDA_PERMISSION_LOGICAL_ID_FOR_LAMBDA_ACTION]: true,
+    '@aws-cdk/aws-lambda:useCdkManagedLogGroup': false,
+  },
 });
 const stackWithFeatureFlag = new LambdaAlarmActionIntegrationTestStack(appWithFeatureFlag, 'LambdaAlarmActionIntegrationTestStackWithFeatureFlag');
 new integ.IntegTest(appWithFeatureFlag, 'LambdaAlarmActionIntegrationTestWithFeatureFlag', {
