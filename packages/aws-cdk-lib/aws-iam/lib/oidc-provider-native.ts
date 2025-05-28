0b295e479e1d8cbd3020dc843b1463d877238c15
Deepu Mohan Puthrote <186013+WarFox@users.noreply.github.com>
2025-05-28 19:52:07 +0100

    feat(iam): introduce `OidcProviderNative` construct utilizing the native CloudFormation resource (#28634)
    IAM is stable in CDK, so we should not introduce breaking changes. This PR introduces a new version of OIDC provider without introducing breaking changes.
    
    Older `iam.OpenIdConnectProvider`, which uses custom resources with lambda, is marked as deprecated. 
    
    The newly introduced `OidcProviderNative` uses the native CloudFormation resource `AWS::IAM::OIDCProvider`
    
    ## ThumbprintList
    `ThumbprintList` must not be empty when using `AWS::IAM::OIDCProvider`
    https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_providers_create_oidc_verify-thumbprint.html
    https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-iam-oidcprovider.html
    https://docs.aws.amazon.com/IAM/latest/APIReference/API_CreateOpenIDConnectProvider.html
    
    Closes #21197
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/aws-cdk-lib/aws-iam/lib/oidc-provider-native.ts
@@ -0,0 +1,222 @@
+import { Construct } from 'constructs';
+import { CfnOIDCProvider } from './iam.generated';
+import { Arn, IResource, Resource, Token, ValidationError } from '../../core';
+
+/**
+ * Represents an IAM OpenID Connect provider.
+ *
+ */
+export interface IOidcProvider extends IResource {
+  /**
+   * The Amazon Resource Name (ARN) of the IAM OpenID Connect provider.
+   *
+   * @attribute
+   */
+  readonly oidcProviderArn: string;
+
+  /**
+   * The issuer for OIDC Provider
+   *
+   * @attribute
+   */
+  readonly oidcProviderIssuer: string;
+}
+
+/**
+ * Initialization properties for `OIDCProviderNative`.
+ */
+export interface OidcProviderNativeProps {
+  /**
+   * The name of the Native OIDC Provider.
+   *
+   * @default - A name is automatically generated.
+   */
+  readonly oidcProviderName?: string;
+
+  /**
+   * The URL of the identity provider. The URL must begin with https:// and
+   * should correspond to the iss claim in the provider's OpenID Connect ID
+   * tokens. Per the OIDC standard, path components are allowed but query
+   * parameters are not. Typically the URL consists of only a hostname, like
+   * https://server.example.org or https://example.com.
+   *
+   * You cannot register the same provider multiple times in a single AWS
+   * account. If you try to submit a URL that has already been used for an
+   * OpenID Connect provider in the AWS account, you will get an error.
+   *
+   * Warning: This URL cannot contain any port numbers
+   */
+  readonly url: string;
+
+  /**
+   * A list of client IDs (also known as audiences). When a mobile or web app
+   * registers with an OpenID Connect provider, they establish a value that
+   * identifies the application. (This is the value that's sent as the client_id
+   * parameter on OAuth requests.)
+   *
+   * You can register multiple client IDs with the same provider. For example,
+   * you might have multiple applications that use the same OIDC provider. You
+   * cannot register more than 100 client IDs with a single IAM OIDC provider.
+   *
+   * Client IDs are up to 255 characters long.
+   *
+   * @default - no clients are allowed
+   */
+  readonly clientIds?: string[];
+
+  /**
+   * A list of server certificate thumbprints for the OpenID Connect (OIDC)
+   * identity provider's server certificates.
+   *
+   * Typically this list includes only 1 entry or empty. However, IAM lets
+   * you have up to 5 thumbprints for an OIDC provider. This lets you maintain
+   * multiple thumbprints if the identity provider is rotating certificates.
+   *
+   * The server certificate thumbprint is the hex-encoded SHA-1 hash value of
+   * the X.509 certificate used by the domain where the OpenID Connect provider
+   * makes its keys available. It is always a 40-character string.
+   *
+   * For example, assume that the OIDC provider is server.example.com and the
+   * provider stores its keys at https://keys.server.example.com/openid-connect.
+   * In that case, the thumbprint string would be the hex-encoded SHA-1 hash
+   * value of the certificate used by https://keys.server.example.com.
+   *
+   * This property is optional. If it is not included, IAM will retrieve and use
+   * the top intermediate certificate authority (CA) thumbprint of the OpenID
+   * Connect identity provider server certificate.
+   *
+   * Obtain the thumbprint of the root certificate authority from the provider's
+   * server as described in https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_providers_create_oidc_verify-thumbprint.html
+   *
+   * @default - no thumbprints are allowed. IAM will retrieve and use thumbprint
+   * of idenity provider server cerctificate
+   */
+  readonly thumbprints?: string[];
+}
+
+/**
+ * IAM OIDC identity providers are entities in IAM that describe an external
+ * identity provider (IdP) service that supports the OpenID Connect (OIDC)
+ * standard, such as Google or Salesforce. You use an IAM OIDC identity provider
+ * when you want to establish trust between an OIDC-compatible IdP and your AWS
+ * account. This is useful when creating a mobile app or web application that
+ * requires access to AWS resources, but you don't want to create custom sign-in
+ * code or manage your own user identities.
+ *
+ * @see http://openid.net/connect
+ * @see https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_providers_oidc.html
+ *
+ * @resource AWS::IAM::OIDCProvider
+ */
+export class OidcProviderNative extends Resource implements IOidcProvider {
+  /**
+   * Imports an Open ID connect provider from an ARN.
+   * @param scope The definition scope
+   * @param id ID of the construct
+   * @param oidcProviderArn the ARN to import
+   */
+  public static fromOidcProviderArn(
+    scope: Construct,
+    id: string,
+    oidcProviderArn: string,
+  ): IOidcProvider {
+    const resourceName = Arn.extractResourceName(
+      oidcProviderArn,
+      'oidc-provider',
+    );
+
+    class Import extends Resource implements IOidcProvider {
+      public readonly oidcProviderArn = oidcProviderArn;
+      public readonly oidcProviderIssuer = resourceName;
+    }
+
+    return new Import(scope, id);
+  }
+
+  /**
+   * The Amazon Resource Name (ARN) of the Native IAM OpenID Connect provider.
+   *
+   * @attribute
+   */
+  public readonly oidcProviderArn: string;
+
+  /**
+   * The issuer for the Native OIDC Provider
+   *
+   * @attribute
+   */
+  public readonly oidcProviderIssuer: string;
+
+  /**
+   * The thumbprints configured for this provider.
+   *
+   * @attribute
+   */
+  public readonly oidcProviderThumbprints: string;
+
+  /**
+   * Defines a Native OpenID Connect provider.
+   * @param scope The definition scope
+   * @param id Construct ID
+   * @param props Initialization properties
+   */
+  public constructor(
+    scope: Construct,
+    id: string,
+    props: OidcProviderNativeProps,
+  ) {
+    super(scope, id, {
+      physicalName: props.oidcProviderName,
+    });
+
+    if (!props.url.startsWith('https://')) {
+      throw new ValidationError(
+        'The URL of the identity provider must start with https://', scope,
+      );
+    }
+
+    // maximum length of url is 255 characters
+    if (props.url.length > 255) {
+      throw new ValidationError('The maximum length allowed for url is 255 characters', scope);
+    }
+
+    // clientids cannot be more than 100
+    if (props.clientIds && props.clientIds.length > 100) {
+      throw new ValidationError('The maximum number of clients that can be registered is 100', scope);
+    }
+
+    // clientId max length is 255
+    if (props.clientIds?.some((clientId) => clientId.length > 255)) {
+      throw new ValidationError('The maximum length of a client ID is 255 characters', scope);
+    }
+
+    // thumbprints[] is optional, but if provided, must be 5 or less
+    if (props.thumbprints && props.thumbprints.length > 5) {
+      throw new ValidationError('The maximum number of thumbprints is 5', scope);
+    }
+
+    // thumbprint length is 40
+    if (props.thumbprints?.some((thumbprint) => thumbprint.length !== 40)) {
+      throw new ValidationError('The length of a thumbprint must be 40 characters', scope);
+    }
+
+    // thumbprint must be hex
+    if (props.thumbprints?.some((thumbprint) => !/^[0-9a-fA-F]+$/.test(thumbprint))) {
+      throw new ValidationError('All thumbprints must be in hexadecimal format', scope);
+    }
+
+    const resource = new CfnOIDCProvider(this, 'Resource', {
+      url: props.url,
+      clientIdList: props.clientIds,
+      thumbprintList: props.thumbprints,
+    });
+
+    this.oidcProviderArn = Token.asString(resource.ref);
+    this.oidcProviderIssuer = Arn.extractResourceName(
+      this.oidcProviderArn,
+      'oidc-provider',
+    );
+
+    this.oidcProviderThumbprints = Token.asString(props.thumbprints);
+  }
+}
---
+++ packages/aws-cdk-lib/aws-iam/test/oidc-provider-native.test.ts
@@ -0,0 +1,271 @@
+import { Template } from '../../assertions';
+import { Stack, Token } from '../../core';
+import * as iam from '../lib';
+
+const arnOfProvider =
+  'arn:aws:iam::1234567:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/someid';
+
+describe('OidcProvider resource', () => {
+  test('"OIDCProviderArn" resolves to the ref', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    const provider = new iam.OidcProviderNative(stack, 'MyProvider', {
+      url: 'https://openid-endpoint',
+      thumbprints: ['a909502dd82ae41433e6f83886b00d4277a32a7b'],
+    });
+
+    // THEN
+    expect(stack.resolve(provider.oidcProviderArn)).toStrictEqual({
+      Ref: 'MyProvider730BA1C8',
+    });
+  });
+
+  test('static fromOidcProviderArn can be used to import a provider', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    const provider = iam.OidcProviderNative.fromOidcProviderArn(
+      stack,
+      'MyProvider',
+      arnOfProvider,
+    );
+
+    // THEN
+    expect(stack.resolve(provider.oidcProviderArn)).toStrictEqual(
+      arnOfProvider,
+    );
+  });
+
+  test('thumbprint list and client ids can be specified', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    new iam.OidcProviderNative(stack, 'MyProvider', {
+      url: 'https://my-url',
+      clientIds: ['client1', 'client2'],
+      thumbprints: ['a909502dd82ae41433e6f83886b00d4277a32a7b'],
+    });
+
+    // THEN
+    Template.fromStack(stack).hasResourceProperties('AWS::IAM::OIDCProvider', {
+      Url: 'https://my-url',
+      ClientIdList: ['client1', 'client2'],
+      ThumbprintList: ['a909502dd82ae41433e6f83886b00d4277a32a7b'],
+    });
+  });
+});
+
+describe('OIDC issuer', () => {
+  test('extract issuer properly in the new provider', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    const provider = new iam.OidcProviderNative(stack, 'MyProvider', {
+      url: 'https://my-issuer',
+      thumbprints: ['a909502dd82ae41433e6f83886b00d4277a32a7b'],
+    });
+
+    // THEN
+    expect(stack.resolve(provider.oidcProviderIssuer)).toStrictEqual({
+      'Fn::Select': [
+        1,
+        { 'Fn::Split': [':oidc-provider/', { Ref: 'MyProvider730BA1C8' }] },
+      ],
+    });
+  });
+
+  test('extract issuer properly in a literal imported provider', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    const provider = iam.OidcProviderNative.fromOidcProviderArn(
+      stack,
+      'MyProvider',
+      arnOfProvider,
+    );
+
+    // THEN
+    expect(stack.resolve(provider.oidcProviderIssuer)).toStrictEqual(
+      'oidc.eks.us-east-1.amazonaws.com/id/someid',
+    );
+  });
+
+  test('extract issuer properly in a Token imported provider', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    // WHEN
+    const provider = iam.OidcProviderNative.fromOidcProviderArn(
+      stack,
+      'MyProvider',
+      Token.asString({ Ref: 'ARN' }),
+    );
+
+    // THEN
+    expect(stack.resolve(provider.oidcProviderIssuer)).toStrictEqual({
+      'Fn::Select': [1, { 'Fn::Split': [':oidc-provider/', { Ref: 'ARN' }] }],
+    });
+  });
+});
+
+describe('ValidationError checks', () => {
+  test('URL property must begin with https://', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    expect(() =>
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeProvider', {
+        url: 'https://example.com',
+      }),
+      // THEN
+    ).not.toThrow();
+
+    expect(() =>
+      new iam.OidcProviderNative(stack, 'SomeOtherProvider', {
+        url: 'http://example.com',
+      }),
+    ).toThrow('The URL of the identity provider must start with https://');
+  });
+
+  test('Maximum length of url is 255', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    expect(() =>
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeProvider', {
+        url: `https://${'a'.repeat(255 - 'https://'.length)}`,
+      }),
+      // THEN
+    ).not.toThrow();
+
+    expect(() =>
+      new iam.OidcProviderNative(stack, 'SomeOtherProvider', {
+        url: `https://${'a'.repeat(256 - 'https://'.length)}`,
+      }),
+    ).toThrow('The maximum length allowed for url is 255 characters');
+  });
+
+  test('maximum allowed number of clientIds is 100', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeProvider', {
+        url: 'https://example.com',
+        clientIds: new Array(100).fill('clientId'),
+      });
+      // THEN
+    }).not.toThrow();
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeOtherProvider', {
+        url: 'https://example.com',
+        clientIds: new Array(101).fill('clientId'),
+      });
+      // THEN
+    }).toThrow('The maximum number of clients that can be registered is 100');
+  });
+
+  test('clientId max length is 255', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeProvider', {
+        url: 'https://example.com',
+        clientIds: new Array(10).fill('c').map((x) => x.repeat(255)),
+      });
+      // THEN
+    }).not.toThrow();
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeOtherProvider', {
+        url: 'https://example.com',
+        clientIds: new Array(10).fill('c').map((x) => x.repeat(256)),
+      });
+      // THEN
+    }).toThrow('The maximum length of a client ID is 255 characters');
+  });
+
+  test('thumbprints[] is optional, but if provided, must be 5 or less', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeProvider', {
+        url: 'https://example.com',
+        thumbprints: new Array(5).fill('a909502dd82ae41433e6f83886b00d4277a32a7b'),
+      });
+      // THEN
+    }).not.toThrow();
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeOtherProvider', {
+        url: 'https://example.com',
+        thumbprints: new Array(6).fill('a909502dd82ae41433e6f83886b00d4277a32a7b'),
+      });
+      // THEN
+    }).toThrow('The maximum number of thumbprints is 5');
+  });
+
+  test('The length of a thumbprint must be 40 characters', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeProvider', {
+        url: 'https://example.com',
+        thumbprints: new Array(5).fill('a909502dd82ae41433e6f83886b00d4277a32a7b'),
+      });
+      // THEN
+    }).not.toThrow();
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeOtherProvider', {
+        url: 'https://otherexample.com',
+        thumbprints: new Array(5).fill('111'),
+      });
+      // THEN
+    }).toThrow('The length of a thumbprint must be 40 characters');
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'NewProvider', {
+        url: 'https://newexample.com',
+        thumbprints: new Array(5).fill('1').map((x) => x.repeat(41)),
+      });
+      // THEN
+    }).toThrow('The length of a thumbprint must be 40 characters');
+  });
+
+  test('All thumbprints must in hexadecimal format', () => {
+    // GIVEN
+    const stack = new Stack();
+
+    expect(() => {
+      // WHEN
+      new iam.OidcProviderNative(stack, 'SomeProvider', {
+        url: 'https://example.com',
+        thumbprints: new Array(5).fill('g909502dd82ae41433e6f83886b00d4277a32a7b'),
+      });
+
+      // THEN
+    }).toThrow('All thumbprints must be in hexadecimal format');
+  });
+});
