bcd1a5f178c43b6e7b472e30046871b81ffc45ed
Jonathan Romano <jonathan@luxaritas.com>
2025-05-30 21:50:52 -0400

    Restore accidentally removed docstring

--- packages/aws-cdk-lib/aws-ecs/lib/amis.ts
+++ packages/aws-cdk-lib/aws-ecs/lib/amis.ts
@@ -365,6 +365,25 @@ export interface BottleRocketImageProps {
    */
   readonly architecture?: ec2.InstanceArchitecture;
 
+  /**
+   * Whether the AMI ID is cached to be stable between deployments
+   *
+   * By default, the newest image is used on each deployment. This will cause
+   * instances to be replaced whenever a new version is released, and may cause
+   * downtime if there aren't enough running instances in the AutoScalingGroup
+   * to reschedule the tasks on.
+   *
+   * If set to true, the AMI ID will be cached in `cdk.context.json` and the
+   * same value will be used on future runs. Your instances will not be replaced
+   * but your AMI version will grow old over time. To refresh the AMI lookup,
+   * you will have to evict the value from the cache using the `cdk context`
+   * command. See https://docs.aws.amazon.com/cdk/latest/guide/context.html for
+   * more information.
+   *
+   * Can not be set to `true` in environment-agnostic stacks.
+   *
+   * @default false
+   */
   readonly cachedInContext?: boolean;
 
   /**
