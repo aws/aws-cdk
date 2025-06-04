79be2450bb1463d3421826be48e9e89a49f7a2ca
S Murali Krishnan <110023377+mrlikl@users.noreply.github.com>
2025-06-04 16:36:40 +0530

    fix unit tests

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.eks-custom-addons.ts
@@ -0,0 +1,44 @@
+/// !cdk-integ pragma:disable-update-workflow
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as integ from '@aws-cdk/integ-tests-alpha';
+import { getClusterVersionConfig } from './integ-tests-kubernetes-version';
+import * as eks from 'aws-cdk-lib/aws-eks';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { EC2_RESTRICT_DEFAULT_SECURITY_GROUP } from 'aws-cdk-lib/cx-api';
+
+class EksCustomAddOnClusterStack extends Stack {
+  public readonly vpc: ec2.IVpc;
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    this.node.setContext(EC2_RESTRICT_DEFAULT_SECURITY_GROUP, false);
+    this.vpc = this.createDummyVpc();
+    new eks.Cluster(this, 'Cluster', {
+      ...getClusterVersionConfig(this, eks.KubernetesVersion.V1_32),
+      bootstrapSelfManagedAddons: false,
+      vpc: this.vpc,
+    });
+  }
+  private createDummyVpc(): ec2.IVpc {
+    return new ec2.Vpc(this, 'DummyVpc', { maxAzs: 2, natGateways: 1, restrictDefaultSecurityGroup: false });
+  }
+}
+
+const app = new App({
+  postCliContext: {
+    '@aws-cdk/aws-lambda:useCdkManagedLogGroup': false,
+    '@aws-cdk/aws-lambda:createNewPoliciesWithAddToRolePolicy': false,
+  },
+});
+
+const stack = new EksCustomAddOnClusterStack(app, 'aws-cdk-eks-fargate-cluster-test-stack1', {
+  authMode: eks.AuthenticationMode.API,
+});
+
+new integ.IntegTest(app, 'aws-cdk-eks-fargate-cluster', {
+  testCases: [stack],
+  // Test includes assets that are updated weekly. If not disabled, the upgrade PR will fail.
+  diffAssets: false,
+});
+
+app.synth();
