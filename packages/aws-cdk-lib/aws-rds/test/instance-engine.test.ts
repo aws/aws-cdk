cff57a072f2b69583184ef78b93825f937bc21d4
Tietew <tietew@gmail.com>
2025-06-04 19:28:59 +0900

    chore(rds): add MariaDB versions 10.11.13 and 11.4.7 (#34623)
    ### Reason for this change
    
    New MariaDB versions are available.
    Note: Link to the release note will be available later.
    
    ### Description of changes
    
    ```console
    $ aws --region us-east-1 rds describe-db-engine-versions --engine mariadb --query 'DBEngineVersions[].EngineVersion'
    [
        "10.5.20",
        "10.5.21",
        "10.5.22",
        "10.5.23",
        "10.5.24",
        "10.5.25",
        "10.5.26",
        "10.5.27",
        "10.5.28",
        "10.5.29",
        "10.6.13",
        "10.6.14",
        "10.6.15",
        "10.6.16",
        "10.6.17",
        "10.6.18",
        "10.6.19",
        "10.6.20",
        "10.6.21",
        "10.6.22",
        "10.11.4",
        "10.11.5",
        "10.11.6",
        "10.11.7",
        "10.11.8",
        "10.11.9",
        "10.11.10",
        "10.11.11",
        "10.11.13",
        "11.4.3",
        "11.4.4",
        "11.4.5",
        "11.4.7"
    ]
    ```
    
    ### Describe any new or updated permissions being added
    
    N/A
    
    ### Description of how you validated changes
    
    Added versions to unit test.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-rds/test/instance-engine.test.ts
+++ packages/aws-cdk-lib/aws-rds/test/instance-engine.test.ts
@@ -301,9 +301,11 @@ describe('instance engine', () => {
       ['10.11.9', rds.MariaDbEngineVersion.VER_10_11_9],
       ['10.11.10', rds.MariaDbEngineVersion.VER_10_11_10],
       ['10.11.11', rds.MariaDbEngineVersion.VER_10_11_11],
+      ['10.11.13', rds.MariaDbEngineVersion.VER_10_11_13],
       ['11.4.3', rds.MariaDbEngineVersion.VER_11_4_3],
       ['11.4.4', rds.MariaDbEngineVersion.VER_11_4_4],
       ['11.4.5', rds.MariaDbEngineVersion.VER_11_4_5],
+      ['11.4.7', rds.MariaDbEngineVersion.VER_11_4_7],
     ])('is passed correctly for %s', (engineVersion, version) => {
       // WHEN
       const stack = new cdk.Stack();
