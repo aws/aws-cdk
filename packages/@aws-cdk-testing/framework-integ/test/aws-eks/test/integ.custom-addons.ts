1158858f4dd0b57f7a75b775870dde4f4579a52c
S Murali Krishnan <110023377+mrlikl@users.noreply.github.com>
2025-06-06 11:32:44 +0530

    fix integ test

--- packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.custom-addons.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.custom-addons.ts
@@ -34,7 +34,7 @@ const calico = new eks.HelmChart(stack, 'Calico', {
 const cnipatch = new eks.KubernetesPatch(stack, 'cnipatch', {
   cluster,
   resourceName: 'installation/default',
-  applyPatch: [{ op: 'replace', path: '/spec/cni', value: { 'type': 'Calico' } }],
+  applyPatch: [{ op: 'replace', path: '/spec/cni', value: { type: 'Calico' } }],
   restorePatch: [{ op: 'remove', path: '/spec/cni' }],
   patchType: eks.PatchType.JSON,
 });
