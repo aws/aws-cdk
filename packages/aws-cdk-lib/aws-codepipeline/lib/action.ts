4fefba52df04fcda65a2f06eed72d8310619521e
go-to-k <24818752+go-to-k@users.noreply.github.com>
2025-06-10 20:00:14 +0900

    import ordders

--- packages/aws-cdk-lib/aws-codepipeline/lib/action.ts
+++ packages/aws-cdk-lib/aws-codepipeline/lib/action.ts
@@ -1,11 +1,11 @@
 import { Construct } from 'constructs';
 import { Artifact } from './artifact';
+import { EnvironmentVariable, SecretsManagerEnvironmentVariable } from './environment-variable';
 import * as notifications from '../../aws-codestarnotifications';
 import * as events from '../../aws-events';
 import * as iam from '../../aws-iam';
 import * as s3 from '../../aws-s3';
 import { Duration, IResource, Lazy, UnscopedValidationError } from '../../core';
-import { EnvironmentVariable, SecretsManagerEnvironmentVariable } from './environment-variable';
 
 export enum ActionCategory {
   SOURCE = 'Source',
