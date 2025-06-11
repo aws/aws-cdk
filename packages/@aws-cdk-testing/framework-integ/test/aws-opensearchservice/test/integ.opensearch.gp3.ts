72ff0dc571cfba2750ca491fdd6f20d679d6037d
Pahud Hsieh <pahudnet@gmail.com>
2025-06-11 06:41:39 -0400

    fix(opensearch): opensearchservice: existing integ test is failing (#34680)
    ### Issue # (if applicable)
    
    Closes  https://github.com/aws/aws-cdk/issues/34678
    
    ### Reason for this change
    
    opensearchservice now has [enableOpensearchMultiAzWithStandby](https://github.com/aws/aws-cdk/blob/ef0272442d415ff7c6d5943b15b5833e0ae2e1bd/packages/aws-cdk-lib/cx-api/FEATURE_FLAGS.md?plain=1#L59) FF enabled by default, which means zoneAwareness has to be enabled across 3 AZs. This integ test is now failing without this PR and is blocking https://github.com/aws/aws-cdk/pull/34660
    
    We need first fix this integ test before https://github.com/aws/aws-cdk/pull/34660 can continue.
    
    
    ### Description of changes
    
    - enable `zoneAwareness`
    - set correct `AvailabilityZoneCount` and other required props
    
    ### Describe any new or updated permissions being added
    
    
    
    
    ### Description of how you validated changes
    
    
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/aws-opensearchservice/test/integ.opensearch.gp3.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-opensearchservice/test/integ.opensearch.gp3.ts
@@ -1,7 +1,7 @@
 import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as opensearch from 'aws-cdk-lib/aws-opensearchservice';
-import { EbsDeviceVolumeType } from 'aws-cdk-lib/aws-ec2';
+import { EbsDeviceVolumeType, SubnetType } from 'aws-cdk-lib/aws-ec2';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 
 class TestStack extends Stack {
@@ -18,6 +18,21 @@ class TestStack extends Stack {
         volumeType: EbsDeviceVolumeType.GP3,
         throughput: 125,
         iops: 3000,
+      },
+      zoneAwareness: {
+        enabled: true,
+        availabilityZoneCount: 3,
+      },
+      vpcSubnets: [
+        { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+      ],
+      capacity: {
+        dataNodeInstanceType: 'm7g.medium.search',
+        dataNodes: 3,
+        // Add dedicated master node - required for multi-AZ with standby
+        // m7g.large.search has 8GB RAM (minimum for 30 nodes/15K shards)
+        masterNodeInstanceType: 'm7g.large.search',
+        masterNodes: 3,
       },
     };
 
