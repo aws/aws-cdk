966055c3a532bd2f5f8ceca12f09fcedb859aea3
DaltJM <dalton@2pisoftware.com>
2025-06-29 05:53:26 +0000

    init fix

--- packages/aws-cdk-lib/aws-ec2/test/network-utils.test.ts
+++ packages/aws-cdk-lib/aws-ec2/test/network-utils.test.ts
@@ -1,3 +1,4 @@
+import { UnscopedValidationError } from '../../core';
 import {
   CidrBlock,
   InvalidCidrRangeError,
@@ -40,10 +41,10 @@ describe('network utils', () => {
     });
   });
   describe('CidrBlock', () => {
-    test('should return the next valid subnet from offset IP', () => {
+    test('throws on misaligned offset IP (numeric form)', () => {
       const num = NetworkUtils.ipToNum('10.0.1.255');
-      const newBlock = new CidrBlock(num, 24);
-      expect(newBlock.cidr).toEqual('10.0.2.0/24');
+      expect(() => new CidrBlock(num, 24))
+        .toThrow(UnscopedValidationError);
     });
     test('nextBlock() returns the next higher CIDR space', () => {
       const testValues = [
@@ -55,15 +56,15 @@ describe('network utils', () => {
         expect(block.nextBlock().cidr).toEqual(value[1]);
       }
     });
-    test('maxIp() should return the last usable IP from the CidrBlock', () => {
+    test('maxIp() should return the last usable IP from an aligned CidrBlock', () => {
       const testValues = [
         ['10.0.3.0/28', '10.0.3.15'],
         ['10.0.3.1/28', '10.0.3.31'],
         ['10.0.2.254/28', '10.0.3.15'],
       ];
-      for (const value of testValues) {
-        const block = new CidrBlock(value[0]);
-        expect(block.maxIp()).toEqual(value[1]);
+      for (const [cidr, expected] of testValues) {
+        const block = new CidrBlock(cidr);
+        expect(block.maxIp()).toEqual(expected);
       }
     });
     test('minIp() should return the first usable IP from the CidrBlock', () => {
@@ -89,6 +90,29 @@ describe('network utils', () => {
     test('calculateNetmask returns the ip string mask', () => {
       const netmask = CidrBlock.calculateNetmask(27);
       expect(netmask).toEqual('255.255.255.224');
+    });
+  });
+  describe('CidrBlock alignment validation', () => {
+    it('throws when you give a misaligned CIDR (string form)', () => {
+      expect(() => new CidrBlock('10.0.40.1/19'))
+        .toThrow(UnscopedValidationError);
+    });
+
+    it('throws when you give a misaligned CIDR (numeric form)', () => {
+      const num = NetworkUtils.ipToNum('10.0.40.0');
+      expect(() => new CidrBlock(num, 19))
+        .toThrow(UnscopedValidationError);
+    });
+
+    it('accepts a correctly aligned CIDR (string form)', () => {
+      expect(new CidrBlock('10.0.32.0/19').cidr)
+        .toBe('10.0.32.0/19');
+    });
+
+    it('accepts a correctly aligned CIDR (numeric form)', () => {
+      const num = NetworkUtils.ipToNum('10.0.32.0');
+      const block = new CidrBlock(num, 19);
+      expect(block.cidr).toBe('10.0.32.0/19');
     });
   });
   describe('NetworkBuilder', () => {
