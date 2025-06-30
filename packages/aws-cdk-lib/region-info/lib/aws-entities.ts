e215fecd516faba36beb6dea4df3856c41e42887
Pratik kumar <kumvprat@amazon.de>
2025-06-30 17:43:23 +0200

    feat: Add a new aws partition

--- packages/aws-cdk-lib/region-info/lib/aws-entities.ts
+++ packages/aws-cdk-lib/region-info/lib/aws-entities.ts
@@ -69,6 +69,7 @@ export const AWS_REGIONS_AND_RULES: readonly (string | symbol)[] = [
   'eu-isoe-west-1', // EU ISO-E West
   'us-isob-west-1', // US ISOB West
   'ap-east-2', // Asia Pacific (Taipei)
+  'eusc-de-east-1', // EU (Germany)
 ];
 
 /**
