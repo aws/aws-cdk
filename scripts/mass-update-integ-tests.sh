4cd0bdb7a631852e94a6ec8ae81d87844598605d
Rico Huijbers <rix0rrr@gmail.com>
2025-07-03 21:40:52 +0200

    Mass snapshot updates

--- scripts/mass-update-integ-tests.sh
+++ scripts/mass-update-integ-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-npx integ-runner --directory packages/@aws-cdk-testing/framework-integ --update-on-failed --disable-update-workflow "$@"
+npx integ-runner --unstable=toolkit-lib-engine --directory packages/@aws-cdk-testing/framework-integ --update-on-failed --disable-update-workflow "$@"
 for dir in packages/@aws-cdk/*; do
-    npx integ-runner --directory $dir --update-on-failed --disable-update-workflow "$@"
+    npx integ-runner --unstable=toolkit-lib-engine --directory $dir --update-on-failed --disable-update-workflow "$@"
 done
