715fe056716f3920ee7ec8605e034ddce371c8df
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2025-07-10 23:18:55 +0900

    fix

--- tools/@aws-cdk/node-bundle/lib/api/_shell.d.ts
+++
@@ -1,5 +0,0 @@
-export interface ShellOptions {
-    readonly cwd?: string;
-    readonly quiet?: boolean;
-}
-export declare function shell(command: string, options?: ShellOptions): string;
--- tools/@aws-cdk/node-bundle/lib/cli-main.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function cliMain(cliArgs: string[]): Promise<void>;
