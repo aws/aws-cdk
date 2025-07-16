e5b4e7ebc5b05c45259c10fd0c95b6e2a3544652
Abid Hasan <abidhsn@amazon.com>
2025-07-16 17:29:57 +0000

    add scoped error

--- packages/aws-cdk-lib/aws-cloudwatch/lib/alarm.ts
+++ packages/aws-cdk-lib/aws-cloudwatch/lib/alarm.ts
@@ -534,8 +534,8 @@ export class Alarm extends AlarmBase {
                   returnData,
                 };
               },
-              withSearchExpression(_searchExpr, _conf) {
-                throw new UnscopedValidationError('Search expressions are not supported in CloudWatch Alarms. Use search expressions only in dashboard graphs.');
+              withSearchExpression: (_searchExpr, _conf) => {
+                throw new ValidationError('Search expressions are not supported in CloudWatch Alarms. Use search expressions only in dashboard graphs.', this);
               },
             });
           }),
@@ -547,8 +547,8 @@ export class Alarm extends AlarmBase {
 
         return { props, primaryId };
       },
-      withSearchExpression() {
-        throw new UnscopedValidationError('Search expressions are not supported in CloudWatch Alarms. Use search expressions only in dashboard graphs.');
+      withSearchExpression: () => {
+        throw new ValidationError('Search expressions are not supported in CloudWatch Alarms. Use search expressions only in dashboard graphs.', this);
       },
     });
   }
