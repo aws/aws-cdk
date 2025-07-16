955030594326dee875f7a7029b467d5bfa49c638
A. Abdel-Rahman <abdlrmn@amazon.de>
2025-07-16 11:03:06 +0200

    fix(s3-deployment): broken cross stack reference when using Source.data (#34916)
    ### Issue #22843
    
    Closes #22843.
    
    ### Reason for this change
    
    The s3-deployment construct implementation had its own implmenetation of resolving references. This is unnecessary as these references should be resolved in the synthesis stage. Furthermore, this implementation did not support cross-stack references.
    
    ### Description of changes
    
    The construct will now pass the tokens as is and let the app synthesis take care of resolving the references instead as expected. Non-tokens will not use markers.
    
    Note: Credits to @smnrd who has written the reproduction steps here: https://github.com/aws/aws-cdk/issues/22843
    This has helped in writing the nested stack integration test.
    
    ### Describe any new or updated permissions being added
    
    No new permissions are added.
    
    ### Description of how you validated changes
    
    Unit tests, Integrations tests, and manually via the AWS console.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-s3-deployment/lib/render-data.ts
+++ packages/aws-cdk-lib/aws-s3-deployment/lib/render-data.ts
@@ -1,6 +1,4 @@
-import { Construct } from 'constructs';
-import { Stack } from '../../core';
-import { ValidationError } from '../../core/lib/errors';
+import { IResolvable, ITokenMapper, StringConcat, Tokenization } from '../../core';
 
 export interface Content {
   readonly text: string;
@@ -8,75 +6,39 @@ export interface Content {
 }
 
 /**
+ * Token mapper that transforms token elements into markers. This mapper saves the tokens
+ * it maps so that they can be retrieved later by renderData.
+ */
+class TokenToMarkerMapper implements ITokenMapper {
+  markers: Record<string, any>;
+
+  constructor() {
+    this.markers = {};
+  }
+
+  mapToken(token: IResolvable) {
+    const newMarker = `<<marker:0xbaba:${Object.keys(this.markers).length}>>`;
+    this.markers[newMarker] = token.toString();
+    return newMarker;
+  }
+}
+
+/**
  * Renders the given string data as deployable content with markers substituted
- * for all "Ref" and "Fn::GetAtt" objects.
+ * for all tokens.
  *
- * @param scope Construct scope
  * @param data The input data
  * @returns The markered text (`text`) and a map that maps marker names to their
  * values (`markers`).
  */
-export function renderData(scope: Construct, data: string): Content {
-  const obj = Stack.of(scope).resolve(data);
-  if (typeof(obj) === 'string') {
-    return { text: obj, markers: {} };
-  }
+export function renderData(data: string): Content {
+  const tokenMapper = new TokenToMarkerMapper();
 
-  if (typeof(obj) !== 'object') {
-    throw new ValidationError(`Unexpected: after resolve() data must either be a string or a CloudFormation intrinsic. Got: ${JSON.stringify(obj)}`, scope);
-  }
-
-  let markerIndex = 0;
-  const markers: Record<string, FnJoinPart> = {};
-  const result = new Array<string>();
-  const fnJoin: FnJoin | undefined = obj['Fn::Join'];
-
-  if (fnJoin) {
-    const sep = fnJoin[0];
-    const parts = fnJoin[1];
-
-    if (sep !== '') {
-      throw new ValidationError(`Unexpected "Fn::Join", expecting separator to be an empty string but got "${sep}"`, scope);
-    }
-
-    for (const part of parts) {
-      if (typeof (part) === 'string') {
-        result.push(part);
-        continue;
-      }
-
-      if (typeof (part) === 'object') {
-        addMarker(part);
-        continue;
-      }
-
-      throw new ValidationError(`Unexpected "Fn::Join" part, expecting string or object but got ${typeof (part)}`, scope);
-    }
-  } else if (obj.Ref || obj['Fn::GetAtt'] || obj['Fn::Select']) {
-    addMarker(obj);
-  } else {
-    throw new ValidationError('Unexpected: Expecting `resolve()` to return "Fn::Join", "Ref" or "Fn::GetAtt"', scope);
-  }
-
-  function addMarker(part: Ref | GetAtt | FnSelect) {
-    const keys = Object.keys(part);
-    const acceptedCfnFns = ['Ref', 'Fn::GetAtt', 'Fn::Select'];
-    if (keys.length !== 1 || !acceptedCfnFns.includes(keys[0])) {
-      const stringifiedAcceptedCfnFns = acceptedCfnFns.map((fn) => `"${fn}"`).join(' or ');
-      throw new ValidationError(`Invalid CloudFormation reference. Key must start with any of ${stringifiedAcceptedCfnFns}. Got ${JSON.stringify(part)}`, scope);
-    }
-
-    const marker = `<<marker:0xbaba:${markerIndex++}>>`;
-    result.push(marker);
-    markers[marker] = part;
-  }
-
-  return { text: result.join(''), markers };
+  return {
+    // Break down the string into its token fragments and replace each token with a marker.
+    // Then rejoin the fragments with basic string concatenation.
+    text: Tokenization.reverseString(data).mapTokens(tokenMapper).join(new StringConcat()),
+    // Return the result markers.
+    markers: tokenMapper.markers,
+  };
 }
-
-type FnJoin = [string, FnJoinPart[]];
-type FnJoinPart = string | Ref | GetAtt | FnSelect;
-type Ref = { Ref: string };
-type GetAtt = { 'Fn::GetAtt': [string, string] };
-type FnSplit = { 'Fn::Split': [string, string | Ref] };
-type FnSelect = { 'Fn::Select': [number, string[] | FnSplit] };
