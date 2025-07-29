aws/aws-cdk

commit fe5ef3a4d134acda8d4b624f75073b75c2e05f1c

fix(logs): exposed metric from the metric filter will now include the dimension map (#34648)
### Issue # (if applicable)

Closes #34643 

### Reason for this change

When the metric filter is created, you can specify the dimension map for the metric. When you extract the metric out of the metric filter, the extracted metric will not include the dimension map, and you will be forced to manually add it to the metric.

### Description of changes

This PR preserves the dimension map used when creating the metric filter and then applies it back when extracting the metric using the `MetricFilter.metric()` API. 

### Describe any new or updated permissions being added

N/A


### Description of how you validated changes

Added new unit and integration test that verify the extracted metric still includes the original dimension map used when creating the metric filter.

### Checklist
- [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)

----

*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

packages/aws-cdk-lib/aws-logs/lib/metric-filter.ts
@@ -25,6 +25,7 @@ export class MetricFilter extends Resource {
   public static readonly PROPERTY_INJECTION_ID: string = 'aws-cdk-lib.aws-logs.MetricFilter';
   private readonly metricName: string;
   private readonly metricNamespace: string;
+  private readonly metricDimensions?: Record<string, string>;
 
   constructor(scope: Construct, id: string, props: MetricFilterProps) {
     super(scope, id, {
@@ -35,6 +36,7 @@ export class MetricFilter extends Resource {
 
     this.metricName = props.metricName;
     this.metricNamespace = props.metricNamespace;
+    this.metricDimensions = props.dimensions;
 
     const numberOfDimensions = Object.keys(props.dimensions ?? {}).length;
     if (numberOfDimensions > 3) {
@@ -74,6 +76,7 @@ export class MetricFilter extends Resource {
     return new Metric({
       metricName: this.metricName,
       namespace: this.metricNamespace,
+      dimensionsMap: this.metricDimensions,
       statistic: 'avg',
       ...props,
     }).attachTo(this);

packages/@aws-cdk-testing/framework-integ/test/aws-logs/test/integ.expose-metric-with-dimensions.js.snapshot/cdk.out
@@ -1,0 +1,1 @@
+{"version":"44.0.0"}

packages/@aws-cdk-testing/framework-integ/test/aws-logs/test/integ.expose-metric-with-dimensions.ts
@@ -1,0 +1,47 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { FilterPattern, LogGroup, MetricFilter } from 'aws-cdk-lib/aws-logs';
+import { Dashboard, GraphWidget } from 'aws-cdk-lib/aws-cloudwatch';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+
+class ExposeMetricWithDimensions extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const logGroup = new LogGroup(this, 'LogGroup');
+
+    // Create a metric filter with dimensions
+    const mf = new MetricFilter(this, 'MetricFilter', {
+      logGroup,
+      metricName: 'testName',
+      metricNamespace: 'testNamespace',
+      filterPattern: FilterPattern.exists('$.latency'),
+      metricValue: '$.latency',
+      dimensions: {
+        Foo: 'Bar',
+        Bar: 'Baz',
+      },
+    });
+
+    // Expose the metric with dimensions
+    const metric = mf.metric();
+
+    new Dashboard(this, 'Dashboard', {
+      dashboardName: 'ExposeMetricWithDimensionsDashboard',
+      widgets: [[
+        new GraphWidget({
+          title: 'Latency with Dimensions',
+          left: [metric],
+        }),
+      ]],
+    });
+  }
+}
+
+const app = new App();
+const stack = new ExposeMetricWithDimensions(app, 'aws-cdk-expose-metric-with-dimensions-integ');
+
+new IntegTest(stack, 'ExposeMetricWithDimensionsTest', {
+  testCases: [stack],
+});
+
+app.synth();

packages/aws-cdk-lib/aws-logs/test/metricfilter.test.ts
@@ -112,6 +112,38 @@ describe('metric filter', () => {
     }));
   });
 
+  test('metric filter exposes metric with custom dimensions', () => {
+    // GIVEN
+    const stack = new Stack();
+    const logGroup = new LogGroup(stack, 'LogGroup');
+
+    // WHEN
+    const mf = new MetricFilter(stack, 'Subscription', {
+      logGroup,
+      metricNamespace: 'AWS/Test',
+      metricName: 'Latency',
+      metricValue: '$.latency',
+      filterPattern: FilterPattern.exists('$.latency'),
+      dimensions: {
+        Foo: 'Bar',
+        Bar: 'Baz',
+      },
+    });
+
+    const metric = mf.metric();
+
+    // THEN
+    expect(metric).toEqual(new Metric({
+      metricName: 'Latency',
+      namespace: 'AWS/Test',
+      dimensionsMap: {
+        Foo: 'Bar',
+        Bar: 'Baz',
+      },
+      statistic: 'avg',
+    }));
+  });
+
   test('metric filter exposes metric with custom statistic', () => {
     // GIVEN
     const stack = new Stack();

commit 09ad6f594d800889de09aec47e135dc4d9336fa0

fix(logs): revert "exposed metric from the metric filter will now include the dimension map (#34648)" (#35102)
### Issue # (if applicable)

Closes #35097 .

### Reason for this change

This reverts commit fe5ef3a4d134acda8d4b624f75073b75c2e05f1c which introduced a regression in the logs module. More details in the [reported issue](https://github.com/aws/aws-cdk/issues/35097) itself

### Description of changes

Reverting the changes of commit fe5ef3a4d134acda8d4b624f75073b75c2e05f1c 

### Describe any new or updated permissions being added
NA

### Description of how you validated changes

Ran integ tests for the module and verified they are now working as expected

### Checklist
- [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)

----

*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

packages/aws-cdk-lib/aws-logs/lib/metric-filter.ts
@@ -25,7 +25,6 @@ export class MetricFilter extends Resource {
   public static readonly PROPERTY_INJECTION_ID: string = 'aws-cdk-lib.aws-logs.MetricFilter';
   private readonly metricName: string;
   private readonly metricNamespace: string;
-  private readonly metricDimensions?: Record<string, string>;
 
   constructor(scope: Construct, id: string, props: MetricFilterProps) {
     super(scope, id, {
@@ -36,7 +35,6 @@ export class MetricFilter extends Resource {
 
     this.metricName = props.metricName;
     this.metricNamespace = props.metricNamespace;
-    this.metricDimensions = props.dimensions;
 
     const numberOfDimensions = Object.keys(props.dimensions ?? {}).length;
     if (numberOfDimensions > 3) {
@@ -76,7 +74,6 @@ export class MetricFilter extends Resource {
     return new Metric({
       metricName: this.metricName,
       namespace: this.metricNamespace,
-      dimensionsMap: this.metricDimensions,
       statistic: 'avg',
       ...props,
     }).attachTo(this);

packages/@aws-cdk-testing/framework-integ/test/aws-logs/test/integ.expose-metric-with-dimensions.js.snapshot/cdk.out
@@ -1,1 +1,0 @@
-{"version":"44.0.0"}

packages/@aws-cdk-testing/framework-integ/test/aws-logs/test/integ.expose-metric-with-dimensions.ts
@@ -1,47 +1,0 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
-import { FilterPattern, LogGroup, MetricFilter } from 'aws-cdk-lib/aws-logs';
-import { Dashboard, GraphWidget } from 'aws-cdk-lib/aws-cloudwatch';
-import { IntegTest } from '@aws-cdk/integ-tests-alpha';
-
-class ExposeMetricWithDimensions extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    const logGroup = new LogGroup(this, 'LogGroup');
-
-    // Create a metric filter with dimensions
-    const mf = new MetricFilter(this, 'MetricFilter', {
-      logGroup,
-      metricName: 'testName',
-      metricNamespace: 'testNamespace',
-      filterPattern: FilterPattern.exists('$.latency'),
-      metricValue: '$.latency',
-      dimensions: {
-        Foo: 'Bar',
-        Bar: 'Baz',
-      },
-    });
-
-    // Expose the metric with dimensions
-    const metric = mf.metric();
-
-    new Dashboard(this, 'Dashboard', {
-      dashboardName: 'ExposeMetricWithDimensionsDashboard',
-      widgets: [[
-        new GraphWidget({
-          title: 'Latency with Dimensions',
-          left: [metric],
-        }),
-      ]],
-    });
-  }
-}
-
-const app = new App();
-const stack = new ExposeMetricWithDimensions(app, 'aws-cdk-expose-metric-with-dimensions-integ');
-
-new IntegTest(stack, 'ExposeMetricWithDimensionsTest', {
-  testCases: [stack],
-});
-
-app.synth();

packages/aws-cdk-lib/aws-logs/test/metricfilter.test.ts
@@ -112,38 +112,6 @@ describe('metric filter', () => {
     }));
   });
 
-  test('metric filter exposes metric with custom dimensions', () => {
-    // GIVEN
-    const stack = new Stack();
-    const logGroup = new LogGroup(stack, 'LogGroup');
-
-    // WHEN
-    const mf = new MetricFilter(stack, 'Subscription', {
-      logGroup,
-      metricNamespace: 'AWS/Test',
-      metricName: 'Latency',
-      metricValue: '$.latency',
-      filterPattern: FilterPattern.exists('$.latency'),
-      dimensions: {
-        Foo: 'Bar',
-        Bar: 'Baz',
-      },
-    });
-
-    const metric = mf.metric();
-
-    // THEN
-    expect(metric).toEqual(new Metric({
-      metricName: 'Latency',
-      namespace: 'AWS/Test',
-      dimensionsMap: {
-        Foo: 'Bar',
-        Bar: 'Baz',
-      },
-      statistic: 'avg',
-    }));
-  });
-
   test('metric filter exposes metric with custom statistic', () => {
     // GIVEN
     const stack = new Stack();