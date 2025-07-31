8bc0a6747ff23cc394ef3f1e0adc79436025f45a
Pahud Hsieh <pahudnet@gmail.com>
2025-07-31 11:52:05 -0400

    update snapshots

--- packages/@aws-cdk-testing/framework-integ/test/core/test/integ.ecr-dualstack-synthesizer.js.snapshot/asset.4c0d45a729c001dbeb05244376c825fe108a988d5f3071345e8eacc64a000217/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Simple Node.js application for ECR dual-stack endpoint integration testing
- *
- * This is a minimal container application used to test Docker image asset
- * synthesis with both IPv4-only and dual-stack ECR endpoints.
- */
