ddee165ade15d3a8d8180e961ce5d4f64b350f62
Vishaal Mehrishi <mehrishi@amazon.nl>
2025-08-05 09:59:15 +0000

    chore: minor changes

--- packages/@aws-cdk/aws-vpclattice-alpha/lib/service-network.ts
+++ packages/@aws-cdk/aws-vpclattice-alpha/lib/service-network.ts
@@ -1,5 +1,5 @@
 import { CfnServiceNetwork } from 'aws-cdk-lib/aws-vpclattice';
-import { Stack, ArnFormat } from 'aws-cdk-lib/core';
+import { Stack, ArnFormat, ValidationError } from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import { IService } from './service';
 import { ServiceNetworkBase, IServiceNetwork } from './service-network-base';
@@ -47,7 +47,7 @@ export interface ServiceNetworkProps {
    *
    * @default - No tags
    */
-  readonly tags?: string;
+  readonly tags?: { [key: string]: string };
 
   /**
    * Services to associate with the service network
@@ -64,10 +64,9 @@ export interface ServiceNetworkAttributes {
   /**
    * The ARN of the service network
    */
-  readonly serviceNetworkArn: string;
+  readonly serviceNetworkArn?: string;
 
-  // TODO: restore ID? Constructing the ARN from the ID will need accountId, region, etc
-  // readonly serviceNetworkId?: string;
+  readonly serviceNetworkId?: string;
 }
 
 /**
@@ -86,6 +85,10 @@ export class ServiceNetwork extends ServiceNetworkBase {
     return ServiceNetwork.fromServiceNetworkAttributes(scope, id, { serviceNetworkArn: serviceNetworkArn });
   }
 
+  public static fromServiceNetworkId(scope: Construct, id: string, serviceNetworkId: string): IServiceNetwork {
+    return ServiceNetwork.fromServiceNetworkAttributes(scope, id, { serviceNetworkId: serviceNetworkId });
+  }
+
   /**
    * Import an existing VPC Lattice service network from its attributes
    *
@@ -96,11 +99,44 @@ export class ServiceNetwork extends ServiceNetworkBase {
    */
   public static fromServiceNetworkAttributes(scope: Construct, id: string, attrs: ServiceNetworkAttributes): IServiceNetwork {
     class UnownedServiceNetworkReference extends ServiceNetworkBase {
-      public readonly serviceNetworkArn: string = attrs.serviceNetworkArn;
-      // Extract the serviceNetworkId from the ARN, which has the format `arn:aws:vpc-lattice:<region>:<account>:servicenetwork/<service-network-id>`
-      public readonly serviceNetworkId: string = Stack.of(scope).splitArn(attrs.serviceNetworkArn, ArnFormat.SLASH_RESOURCE_NAME).resourceName!;
+      public readonly serviceNetworkArn: string;
+      public readonly serviceNetworkId: string;
+
+      constructor(serviceNetworkArn: string, serviceNetworkId: string) {
+        super(scope, id, { environmentFromArn: serviceNetworkArn });
+
+        this.serviceNetworkArn = serviceNetworkArn;
+        this.serviceNetworkId = serviceNetworkId;
+      }
     }
-    return new UnownedServiceNetworkReference(scope, id);
+
+    let serviceNetworkArn = attrs.serviceNetworkArn;
+    let serviceNetworkId = attrs.serviceNetworkId;
+
+    if (serviceNetworkArn === undefined && serviceNetworkId === undefined) {
+      throw new ValidationError('At least one of `serviceNetworkArn` or `serviceNetworkId` must be provided', scope);
+    } else if (serviceNetworkArn !== undefined && serviceNetworkId !== undefined) {
+      throw new ValidationError('Only one of `serviceNetworkArn` or `serviceNetworkId` must be provided', scope);
+    }
+
+    // Exactly one is provided - determine the value of one using the other
+    if (serviceNetworkArn) {
+      // Determine the serviceNetworkId from the ARN, which has the format `arn:aws:vpc-lattice:<region>:<account>:servicenetwork/<service-network-id>`
+      const parsed = Stack.of(scope).splitArn(serviceNetworkArn, ArnFormat.SLASH_RESOURCE_NAME);
+      if (!parsed.resourceName) {
+        throw new ValidationError('Invalid service network ARN format', scope);
+      }
+      serviceNetworkId = parsed.resourceName;
+    } else if (serviceNetworkId) {
+      // Build the serviceNetworkArn using the serviceNetworkId
+      serviceNetworkArn = Stack.of(scope).formatArn({
+        service: 'vpc-lattice',
+        resource: 'servicenetwork',
+        resourceName: serviceNetworkId,
+      });
+    }
+
+    return new UnownedServiceNetworkReference(serviceNetworkArn!, serviceNetworkId!);
   }
 
   /**
@@ -131,7 +167,7 @@ export class ServiceNetwork extends ServiceNetworkBase {
 
     // TODO: tags
 
-    const resource = new CfnServiceNetwork(this, id, {
+    const resource = new CfnServiceNetwork(this, 'Resource', {
       authType: props.authType,
       name: props.serviceNetworkName,
       sharingConfig: {
