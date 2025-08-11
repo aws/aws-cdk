88e736fdec2d8b56ef68fa66f1be465e73720afe
Abdelrahman Abdelrahman <abdlrmn@amazon.de>
2025-08-11 15:50:18 +0000

    fix(s3-deployment): intermittent CreateInvalidation and/or wait errors
    - Waiting for cache invalidation is redundant as it doesn't tell you if a cache invalidation fails anyway. Waiting is removed.
    - Modified retry strategy of cloudfront client to help with intermittent CreateInvalidation errors.
    
    Fixes #15891

--- packages/@aws-cdk-testing/framework-integ/test/aws-s3-deployment/test/integ.bucket-deployment-cloudfront.js.snapshot/asset.89b21dc08a652feeebf1f4fbda1fdace9783b1554718a01100d0a933d79ebe51/index.py
+++ packages/@aws-cdk-testing/framework-integ/test/aws-s3-deployment/test/integ.bucket-deployment-cloudfront.js.snapshot/asset.89b21dc08a652feeebf1f4fbda1fdace9783b1554718a01100d0a933d79ebe51/index.py
@@ -11,11 +11,17 @@ from uuid import uuid4
 from zipfile import ZipFile
 
 import boto3
+from botocore.config import Config
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-cloudfront = boto3.client('cloudfront')
+cloudfront = boto3.client('cloudfront', config=Config(
+    retries = {
+        'max_attempts': 9,
+        'mode': 'standard',
+    }
+))
 s3 = boto3.client('s3')
 
 CFN_SUCCESS = "SUCCESS"
@@ -233,10 +239,10 @@ def cloudfront_invalidate(distribution_id, distribution_paths):
             },
             'CallerReference': str(uuid4()),
         })
-    # by default, will wait up to 10 minutes
     cloudfront.get_waiter('invalidation_completed').wait(
         DistributionId=distribution_id,
         Id=invalidation_resp['Invalidation']['Id'])
+
 
 #---------------------------------------------------------------------------------------------------
 # set metadata
