0110b441bfddd0f3554be5cc6139f4d17e3a729c
hungshou <hungshou@amazon.com>
2025-08-17 05:17:03 -0700

    Updated archive cmkms integ test files

--- packages/@aws-cdk-testing/framework-integ/test/aws-events/test/integ.archive-customer-managed-key.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-events/test/integ.archive-customer-managed-key.ts
@@ -1,4 +1,5 @@
 import * as kms from 'aws-cdk-lib/aws-kms';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { App, RemovalPolicy, Stack } from 'aws-cdk-lib';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 import { Archive, EventBus } from 'aws-cdk-lib/aws-events';
@@ -8,6 +9,27 @@ const stack = new Stack(app, 'archive-customer-managed-key');
 
 const kmsKey = new kms.Key(stack, 'KmsKey', {
   removalPolicy: RemovalPolicy.DESTROY,
+  policy: new iam.PolicyDocument({
+    statements: [new iam.PolicyStatement({
+      resources: ['*'],
+      actions: ['kms:Decrypt', 'kms:GenerateDataKey', 'kms:DescribeKey', 'kms:ReEncrypt*'],
+      principals: [
+        new iam.ServicePrincipal('events.amazonaws.com'),
+        new iam.ServicePrincipal('events.aws.internal'),
+      ],
+      sid: 'Allow EventBridge in all stages',
+      effect: iam.Effect.ALLOW,
+    }),
+    new iam.PolicyStatement({
+      resources: ['*'],
+      actions: ['kms:*'],
+      principals: [
+        new iam.AccountPrincipal(stack.account),
+      ],
+      sid: 'Allow IAM User to modify the key',
+      effect: iam.Effect.ALLOW,
+    })],
+  }),
 });
 
 new Archive(stack, 'Archive', {
