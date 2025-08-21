edc863347e5f4ffa31873120d42a207f0d875c94
Radek Antoniuk <radek.antoniuk@gmail.com>
2025-08-21 14:03:03 +0200

    chore: assertion to check for absence of InferenceAccelerators

--- packages/aws-cdk-lib/aws-ecs/test/ec2/ec2-task-definition.test.ts
+++ packages/aws-cdk-lib/aws-ecs/test/ec2/ec2-task-definition.test.ts
@@ -21,6 +21,7 @@ describe('ec2 task definition', () => {
         Family: 'Ec2TaskDef',
         NetworkMode: ecs.NetworkMode.BRIDGE,
         RequiresCompatibilities: ['EC2'],
+        InferenceAccelerators: Match.absent(),
       });
 
       // test error if no container defs?
@@ -784,10 +785,10 @@ describe('ec2 task definition', () => {
       container.addContainerDependencies({
         container: dependency1,
       },
-      {
-        container: dependency2,
-        condition: ecs.ContainerDependencyCondition.SUCCESS,
-      });
+        {
+          container: dependency2,
+          condition: ecs.ContainerDependencyCondition.SUCCESS,
+        });
 
       // THEN
       Template.fromStack(stack).hasResourceProperties('AWS::ECS::TaskDefinition', {
