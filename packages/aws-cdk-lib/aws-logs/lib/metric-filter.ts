e286aab345f217da1f5a48e0d32f56ff311ee735
Antoine Dubé <antoine.dube@gmail.com>
2025-09-02 12:19:20 -0400

    Add test, integ test and edit README to take into account the new parameter in MetricFilter of aws-logs

--- packages/aws-cdk-lib/aws-logs/lib/metric-filter.ts
+++ packages/aws-cdk-lib/aws-logs/lib/metric-filter.ts
@@ -61,7 +61,7 @@ export class MetricFilter extends Resource {
         dimensions: props.dimensions ? Object.entries(props.dimensions).map(([key, value]) => ({ key, value })) : undefined,
         unit: props.unit,
       }],
-      applyOnTransformedLogs: props.applyOnTransformedLogs
+      applyOnTransformedLogs: props.applyOnTransformedLogs,
     });
   }
 
