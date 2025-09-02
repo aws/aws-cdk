f3c7efdffac88399a03d4d380578a33676f2f262
matoom-nomu <121716425+matoom-nomu@users.noreply.github.com>
2025-09-02 11:51:10 +0900

    fix(ecs): pass additionalCacheKey to EcsOptimizedImage constructor

--- packages/aws-cdk-lib/aws-ecs/test/amis.test.ts
+++ packages/aws-cdk-lib/aws-ecs/test/amis.test.ts
@@ -24,4 +24,26 @@ describe('amis', () => {
       ([k, v]) => k.startsWith(ssmKey) && (v as any).Default.includes(`/bottlerocket/${variant}/x86_64/`),
     )).toEqual(true);
   });
+
+  test.each([
+    ['amazonLinux2023', (opts: any) => ecs.EcsOptimizedImage.amazonLinux2023(ecs.AmiHardwareType.STANDARD, opts)],
+    ['amazonLinux2', (opts: any) => ecs.EcsOptimizedImage.amazonLinux2(ecs.AmiHardwareType.STANDARD, opts)],
+    ['amazonLinux', (opts: any) => ecs.EcsOptimizedImage.amazonLinux(opts)],
+    ['windows', (opts: any) => ecs.EcsOptimizedImage.windows(ecs.WindowsOptimizedVersion.SERVER_2022, opts)],
+  ])('EcsOptimizedImage %s accepts additionalCacheKey', (_, imageFactory) => {
+    // GIVEN
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, 'TestStack', {
+      env: { account: '123456789012', region: 'us-east-1' }
+    });
+    
+    // WHEN
+    const image = imageFactory({ 
+      cachedInContext: true, 
+      additionalCacheKey: 'test-key' 
+    });
+
+    //THEN
+    expect(() => image.getImage(stack)).not.toThrow();
+  });
 });
