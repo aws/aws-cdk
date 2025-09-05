5ba0ba9e147059951c64876b324d84cfedf05c49
Dhyan Gandhi <gandhya@amazon.com>
2025-09-05 11:55:38 -0400

    fix: add support for AWS::NoValue in non-string properties

---
+++ packages/@aws-cdk-testing/framework-integ/test/cloudformation-include/test/integ.novalue-nonstring.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+import * as cdk from 'aws-cdk-lib';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'integ-cfninclude-novalue-boolean');
+
+const templateFile = path.join(__dirname, 'test-templates', 'novalue-boolean.json');
+
+const cfnInclude = new CfnInclude(stack, 'Template', {
+  templateFile,
+});
+
+const bucket = cfnInclude.getResource('TestBucket');
+
+new cdk.CfnOutput(stack, 'BucketName', {
+  value: bucket.ref,
+});
+
+new IntegTest(app, 'CfnIncludeNoValueBooleanTest', {
+  testCases: [stack],
+});
