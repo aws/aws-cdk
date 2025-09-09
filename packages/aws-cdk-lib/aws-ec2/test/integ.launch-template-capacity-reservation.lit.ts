3bcec17ec4d5e4db78f9c2593e181488694f922c
Hung-Che Lo <hclo@snese.net>
2025-09-09 19:35:15 +0800

    feat(ec2): add literate integration test for CapacityReservationSpecification
    - Add integ.launch-template-capacity-reservation.lit.ts in main package
    - Follows existing pattern of .lit.ts integration tests
    - Tests both capacity reservation ID and resource group ARN scenarios

---
+++ packages/aws-cdk-lib/aws-ec2/test/integ.launch-template-capacity-reservation.lit.ts
@@ -0,0 +1,41 @@
+import * as cdk from '../../core';
+import * as ec2 from '../lib';
+
+class LaunchTemplateCapacityReservationStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    /// !show
+    // LaunchTemplate with capacity reservation specification
+    new ec2.LaunchTemplate(this, 'LaunchTemplateWithCapacityReservation', {
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      capacityReservationSpecification: {
+        capacityReservationPreference: ec2.CapacityReservationPreference.OPEN,
+        capacityReservationTarget: {
+          capacityReservationId: 'cr-1234567890abcdef0',
+        },
+      },
+    });
+
+    // LaunchTemplate with capacity reservation resource group
+    new ec2.LaunchTemplate(this, 'LaunchTemplateWithResourceGroup', {
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      capacityReservationSpecification: {
+        capacityReservationPreference: ec2.CapacityReservationPreference.NONE,
+        capacityReservationTarget: {
+          capacityReservationResourceGroupArn: 'arn:aws:resource-groups:us-west-2:123456789012:group/my-group',
+        },
+      },
+    });
+    /// !hide
+  }
+}
+
+const app = new cdk.App();
+new LaunchTemplateCapacityReservationStack(app, 'aws-cdk-launch-template-capacity-reservation', {
+  env: {
+    account: process.env.CDK_INTEG_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_INTEG_REGION || process.env.CDK_DEFAULT_REGION,
+  },
+});
+app.synth();
