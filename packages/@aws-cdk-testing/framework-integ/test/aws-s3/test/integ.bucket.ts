a4e0f36ee3784aff2200d96f8a15091d43296482
Abid Hasan <abidhsn@amazon.de>
2025-09-18 13:58:14 +0200

    updatesnapshots

--- packages/@aws-cdk-testing/framework-integ/test/aws-s3/test/integ.bucket.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-s3/test/integ.bucket.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as cdk from 'aws-cdk-lib';
 import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 import * as s3 from 'aws-cdk-lib/aws-s3';
@@ -23,6 +24,17 @@ const otherwiseEncryptedBucket = new s3.Bucket(stack, 'MyOtherBucket', {
 const user = new iam.User(stack, 'MyUser');
 bucket.grantReadWrite(user);
 otherwiseEncryptedBucket.grantRead(user);
+
+// Add VPC-dependent resource with non-existent VPC - will pass synth but fail deploy
+const vpc = ec2.Vpc.fromVpcAttributes(stack, 'InvalidVpc', {
+  vpcId: 'vpc-nonexistent123',
+  availabilityZones: ['us-east-1a', 'us-east-1b'],
+});
+
+new ec2.SecurityGroup(stack, 'TestSecurityGroup', {
+  vpc: vpc,
+  description: 'Test security group with invalid VPC',
+});
 
 new IntegTest(app, 'cdk-integ-s3-bucket', {
   testCases: [stack],
