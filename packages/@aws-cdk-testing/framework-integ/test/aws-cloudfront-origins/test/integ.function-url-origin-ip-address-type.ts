a3b622196294181adfa8e2fe44fbded5194417ee
matoom-nomu <121716425+matoom-nomu@users.noreply.github.com>
2025-09-30 11:45:40 +0900

    add Assertion to IntegTest

--- packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront-origins/test/integ.function-url-origin-ip-address-type.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront-origins/test/integ.function-url-origin-ip-address-type.ts
@@ -2,7 +2,7 @@ import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { FunctionUrlOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { App, Stack } from 'aws-cdk-lib';
-import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
 import { OriginIpAddressType } from 'aws-cdk-lib/aws-cloudfront';
 
 const app = new App();
@@ -28,7 +28,7 @@ new cloudfront.Distribution(stack, 'DistributionWithoutIpAddressTypeProp(IPv4)',
 });
 
 // CloudFront distribution with IPv4 IP address type
-new cloudfront.Distribution(stack, 'DistributionWithIPv4', {
+const distributionIPv4 = new cloudfront.Distribution(stack, 'DistributionWithIPv4', {
   defaultBehavior: {
     origin: FunctionUrlOrigin.withOriginAccessControl(fnUrl, {
       ipAddressType: OriginIpAddressType.IPV4,
@@ -37,7 +37,7 @@ new cloudfront.Distribution(stack, 'DistributionWithIPv4', {
 });
 
 // CloudFront distribution with IPv6 IP address type
-new cloudfront.Distribution(stack, 'DistributionWithIPv6', {
+const distributionIPv6 = new cloudfront.Distribution(stack, 'DistributionWithIPv6', {
   defaultBehavior: {
     origin: FunctionUrlOrigin.withOriginAccessControl(fnUrl, {
       ipAddressType: OriginIpAddressType.IPV6,
@@ -46,7 +46,7 @@ new cloudfront.Distribution(stack, 'DistributionWithIPv6', {
 });
 
 // CloudFront distribution with dualstack IP address type
-new cloudfront.Distribution(stack, 'DistributionWithDualstack', {
+const distributionDualstack = new cloudfront.Distribution(stack, 'DistributionWithDualstack', {
   defaultBehavior: {
     origin: FunctionUrlOrigin.withOriginAccessControl(fnUrl, {
       ipAddressType: OriginIpAddressType.DUALSTACK,
@@ -54,6 +54,19 @@ new cloudfront.Distribution(stack, 'DistributionWithDualstack', {
   },
 });
 
-new IntegTest(app, 'FunctionUrlOriginIpAddressTypeTest', {
+const integ = new IntegTest(app, 'FunctionUrlOriginIpAddressTypeTest', {
   testCases: [stack],
 });
+
+// Assert that distributions are created with expected IP address type settings
+integ.assertions.awsApiCall('CloudFront', 'getDistribution', {
+  Id: distributionIPv4.distributionId,
+}).assertAtPath('Distribution.DistributionConfig.IsIPV6Enabled', ExpectedResult.exact(false));
+
+integ.assertions.awsApiCall('CloudFront', 'getDistribution', {
+  Id: distributionIPv6.distributionId,
+}).assertAtPath('Distribution.DistributionConfig.IsIPV6Enabled', ExpectedResult.exact(true));
+
+integ.assertions.awsApiCall('CloudFront', 'getDistribution', {
+  Id: distributionDualstack.distributionId,
+}).assertAtPath('Distribution.DistributionConfig.IsIPV6Enabled', ExpectedResult.exact(true));
