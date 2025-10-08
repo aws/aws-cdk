4656e5c1e25cbecfcb787feb7f1c99c13777046a
Antoine Dubé <antoine.dube@gmail.com>
2025-10-08 15:26:57 -0400

    feat(logs): add parameter to allow metric filter on transformed logs (#35359)
    ### Issue # (if applicable)
    
    Not related to any issue.
    
    ### Reason for this change
    
    
    It was not possible to pass the argument 'applyOnTransformedLogs' to the class MetricFilter, in order to create MetricFilter, Filter and Alarm. 
    
    ### Description of changes
    
    Since MetricFilter makes use of CfnMetricFilter, which allows the parameter 'applyOnTransformedLogs', this change adds the parameter 'applyOnTransformedLogs' to MetricFilterOptions, which is directly passed to the CfnMetric class. 
    
    ### Describe any new or updated permissions being added
    
    No permission is added or updated.
    
    ### Description of how you validated changes
    
    I tested by hand. 
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-logs/test/integ.metricfilter-apply-on-transformed-logs.ts
@@ -0,0 +1,29 @@
+import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { FilterPattern, LogGroup, MetricFilter } from 'aws-cdk-lib/aws-logs';
+
+class TestStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const logGroup = new LogGroup(this, 'LogGroup', {
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    new MetricFilter(this, 'MetricFilter', {
+      logGroup,
+      metricNamespace: 'MyApp',
+      metricName: 'Latency',
+      filterPattern: FilterPattern.exists('$.latency'),
+      applyOnTransformedLogs: true,
+    });
+  }
+}
+
+const app = new App();
+const testCase = new TestStack(app, 'aws-cdk-metricfilter-apply-on-transformed-logs-integ');
+
+new IntegTest(app, 'metricfilter-apply-on-transformed-logs', {
+  testCases: [testCase],
+});
+app.synth();
