4f91ee415481c7207fb815d6c2c3da1e75d5ce67
Abid Hasan <abidhsn@amazon.de>
2025-10-09 10:05:35 +0200

    Revert "ci: add codebuild deployment integ runner PR workflow. (#35459)"
    This reverts commit 919b14b9b3b68ca88c9754aaf47b3c947b30a2dc.

--- tools/@aws-cdk/integration-test-deployment/test/git-mock.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-export const gitDiffMock = async (): Promise<string> => {
-  return readFileSync(join(__dirname, 'git-diff-snapshot.txt'), 'utf8');
-};
