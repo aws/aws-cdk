855d50780d9f4d5f908f8ce94d91fab602690908
Hung Tran <40334379+phuhung273@users.noreply.github.com>
2025-10-21 03:09:02 +0700

    chore(rds): sqlserver 13.00.6470.1.v1,14.00.3505.1.v1,15.00.4445.1.v1,16.00.4215.2.v1 (#35750)
    ### Reason for this change
    https://aws.amazon.com/about-aws/whats-new/2025/10/amazon-rds-supports-latest-cu-gdr-updates-for-microsoft-sql-server/
    
    ### Description of changes
    SqlServer add versions 13.00.6470.1.v1, 14.00.3505.1.v1, 15.00.4445.1.v1, 16.00.4215.2.v1
    
    ### Description of how you validated changes
    ```console
    $ aws rds describe-db-engine-versions --engine sqlserver-ee --output table --query 'DBEngineVersions[*].{Engine:Engine,EngineVersion:EngineVersion}'
    
    --------------------------------------
    |      DescribeDBEngineVersions      |
    +---------------+--------------------+
    |    Engine     |   EngineVersion    |
    +---------------+--------------------+
    ...
    |  sqlserver-ee |  13.00.6470.1.v1   |
    ...
    |  sqlserver-ee |  14.00.3505.1.v1   |
    ...
    |  sqlserver-ee |  15.00.4445.1.v1   |
    ...
    |  sqlserver-ee |  16.00.4215.2.v1   |
    +---------------+--------------------+
    ```
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-rds/lib/instance-engine.ts
+++ packages/aws-cdk-lib/aws-rds/lib/instance-engine.ts
@@ -2837,6 +2837,8 @@ export class SqlServerEngineVersion {
   public static readonly VER_13_00_6460_7_V1 = SqlServerEngineVersion.of('13.00.6460.7.v1', '13.00');
   /** Version "13.00.6465.1.v1". */
   public static readonly VER_13_00_6465_1_V1 = SqlServerEngineVersion.of('13.00.6465.1.v1', '13.00');
+  /** Version "13.00.6470.1.v1". */
+  public static readonly VER_13_00_6470_1_V1 = SqlServerEngineVersion.of('13.00.6470.1.v1', '13.00');
 
   /** Version "14.00" (only a major version, without a specific minor version). */
   public static readonly VER_14 = SqlServerEngineVersion.of('14.00', '14.00');
@@ -2900,6 +2902,8 @@ export class SqlServerEngineVersion {
   public static readonly VER_14_00_3495_9_V1 = SqlServerEngineVersion.of('14.00.3495.9.v1', '14.00');
   /** Version "14.00.3500.1.v1". */
   public static readonly VER_14_00_3500_1_V1 = SqlServerEngineVersion.of('14.00.3500.1.v1', '14.00');
+  /** Version "14.00.3505.1.v1". */
+  public static readonly VER_14_00_3505_1_V1 = SqlServerEngineVersion.of('14.00.3505.1.v1', '14.00');
 
   /** Version "15.00" (only a major version, without a specific minor version). */
   public static readonly VER_15 = SqlServerEngineVersion.of('15.00', '15.00');
@@ -2954,6 +2958,8 @@ export class SqlServerEngineVersion {
   public static readonly VER_15_00_4435_7_V1 = SqlServerEngineVersion.of('15.00.4435.7.v1', '15.00');
   /** Version "15.00.4440.1.v1". */
   public static readonly VER_15_00_4440_1_V1 = SqlServerEngineVersion.of('15.00.4440.1.v1', '15.00');
+  /** Version "15.00.4445.1.v1". */
+  public static readonly VER_15_00_4445_1_V1 = SqlServerEngineVersion.of('15.00.4445.1.v1', '15.00');
 
   /** Version "16.00" (only a major version, without a specific minor version). */
   public static readonly VER_16 = SqlServerEngineVersion.of('16.00', '16.00');
@@ -2989,6 +2995,8 @@ export class SqlServerEngineVersion {
   public static readonly VER_16_00_4205_1_V1 = SqlServerEngineVersion.of('16.00.4205.1.v1', '16.00');
   /** Version "16.00.4210.1.v1". */
   public static readonly VER_16_00_4210_1_V1 = SqlServerEngineVersion.of('16.00.4210.1.v1', '16.00');
+  /** Version "16.00.4215.2.v1". */
+  public static readonly VER_16_00_4215_2_V1 = SqlServerEngineVersion.of('16.00.4215.2.v1', '16.00');
 
   /**
    * Create a new SqlServerEngineVersion with an arbitrary version.
