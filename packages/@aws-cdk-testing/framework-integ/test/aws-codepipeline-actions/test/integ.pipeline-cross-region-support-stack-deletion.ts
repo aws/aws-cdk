ee5a8ae35f5f12190f33bc3535e8ac02e3980553
Jamie Yang <ychjamie@amazon.com>
2025-10-21 09:35:57 -0700

    fix: correct indentation in integ test to use 2 spaces

--- packages/@aws-cdk-testing/framework-integ/test/aws-codepipeline-actions/test/integ.pipeline-cross-region-support-stack-deletion.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-codepipeline-actions/test/integ.pipeline-cross-region-support-stack-deletion.ts
@@ -7,50 +7,50 @@ import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 const app = new cdk.App();
 
 const stack = new cdk.Stack(app, 'PipelineCrossRegionSupportStackDeletionTest', {
-    env: {
-        account: '123456789012',
-        region: 'us-east-1',
-    },
+  env: {
+    account: '123456789012',
+    region: 'us-east-1',
+  },
 });
 
 const bucket = new s3.Bucket(stack, 'SourceBucket', {
-    versioned: true,
-    removalPolicy: cdk.RemovalPolicy.DESTROY,
+  versioned: true,
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
 });
 
 const sourceOutput = new codepipeline.Artifact();
 const sourceAction = new cpactions.S3SourceAction({
-    actionName: 'S3Source',
-    bucketKey: 'path/to/artifact.zip',
-    bucket,
-    output: sourceOutput,
+  actionName: 'S3Source',
+  bucketKey: 'path/to/artifact.zip',
+  bucket,
+  output: sourceOutput,
 });
 
 // Create a pipeline with a cross-region action to trigger support stack creation
 new codepipeline.Pipeline(stack, 'Pipeline', {
-    artifactBucket: bucket,
-    stages: [
-        {
-            stageName: 'Source',
-            actions: [sourceAction],
-        },
-        {
-            stageName: 'Deploy',
-            actions: [
-                new cpactions.CloudFormationCreateUpdateStackAction({
-                    actionName: 'DeployCrossRegion',
-                    stackName: 'test-stack',
-                    templatePath: sourceOutput.atPath('template.yml'),
-                    adminPermissions: false,
-                    region: 'us-west-2', // Cross-region action triggers support stack
-                }),
-            ],
-        },
-    ],
+  artifactBucket: bucket,
+  stages: [
+    {
+      stageName: 'Source',
+      actions: [sourceAction],
+    },
+    {
+      stageName: 'Deploy',
+      actions: [
+        new cpactions.CloudFormationCreateUpdateStackAction({
+          actionName: 'DeployCrossRegion',
+          stackName: 'test-stack',
+          templatePath: sourceOutput.atPath('template.yml'),
+          adminPermissions: false,
+          region: 'us-west-2', // Cross-region action triggers support stack
+        }),
+      ],
+    },
+  ],
 });
 
 new IntegTest(app, 'PipelineCrossRegionSupportStackDeletionIntegTest', {
-    testCases: [stack],
+  testCases: [stack],
 });
 
 app.synth();
