8ee5d4b012b2d6a157b0e4915976e0a487cde115
Rico Hermans <rix0rrr@gmail.com>
2025-10-28 17:09:57 +0100

    feat(core): `IEnvironmentAware` interface to retrieve a construct's environment (#35817)
    Previously, `construct.env` was declared in the `IResource` interface.
    
    Downside is that `IResource` also:
    
    - requires providing `stack: Stack` (which is a bit pointless to begin with since we can always call `Stack.of(x)`); and
    - `applyRemovalPolicy()` which doesn't belong on this interface but now is required to implement.
    
    Therefore, we extract `IResource#env` out to its own interface, `IEnvironmentAware`; make `IXxxRef` extend that interface, simplify `IResourceWithPolicy` to only be `IEnvironmentAware` and not an entire `IResource`.
    
    Lessening the requirements on `IResourceWithPolicy` also lessens its guarantees, which makes that a breaking change. In order to make this code change, we do the following:
    
    - Introduce `IResourceWithPolicyV2` to be the version of `IResourceWithPolicy` we should have had.
    - Accept `IResourceWithPolicyV2` everywhere
    - Make `IResourceWithPolicy` extend `IResourcWithPolicyV2`
    
    ```
                       ┌──────────────────────────┐                     
                       │    IEnvironmentAware     │                     
                       └──────────────────────────┘                     
                                     ▲                                  
                 ┌───────────────────┴────────────────────┐             
                 │                                        │             
    ┌──────────────────────────┐            ┌──────────────────────────┐
    │  IResourceWithPolicyV2   │            │        IResource         │
    └──────────────────────────┘            └──────────────────────────┘
                  ▲                                       ▲             
                  └──────────────────┬────────────────────┘             
                                     │                                  
                       ┌──────────────────────────┐                     
                       │   IResourceWithPolicy    │                     
                       └──────────────────────────┘                     
    ```
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-opensearchservice/lib/domain.ts
+++ packages/aws-cdk-lib/aws-opensearchservice/lib/domain.ts
@@ -1328,7 +1328,6 @@ abstract class DomainBase extends cdk.Resource implements IDomain {
       grantee,
       actions: domainActions,
       resourceArns,
-      scope: this,
     });
 
     return grant;
