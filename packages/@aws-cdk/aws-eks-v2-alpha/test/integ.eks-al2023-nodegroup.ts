0ef6e5646bcb3bab045bc1c770ae065cfb8cf65b
harryfrzz <harikrishnac005@gmail.com>
2025-10-31 22:47:22 +0530

    update snapshot

--- packages/@aws-cdk/aws-eks-v2-alpha/test/integ.eks-al2023-nodegroup.ts
+++ packages/@aws-cdk/aws-eks-v2-alpha/test/integ.eks-al2023-nodegroup.ts
@@ -1,11 +1,11 @@
 /// !cdk-integ pragma:disable-update-workflow
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { App, Stack, StackProps } from 'aws-cdk-lib';
-import * as integ from '@aws-cdk/integ-tests-alpha';
-import * as eks from '../lib';
-import { NodegroupAmiType } from 'aws-cdk-lib/aws-eks';
-import { KubectlV33Layer } from '@aws-cdk/lambda-layer-kubectl-v33';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { App, Stack, StackProps } from "aws-cdk-lib";
+import * as integ from "@aws-cdk/integ-tests-alpha";
+import * as eks from "../lib";
+import { NodegroupAmiType } from "aws-cdk-lib/aws-eks";
+import { KubectlV34Layer } from "@aws-cdk/lambda-layer-kubectl-v34";
 
 class EksClusterStack extends Stack {
   private cluster: eks.Cluster;
@@ -15,42 +15,42 @@ class EksClusterStack extends Stack {
     super(scope, id, props);
 
     // allow all account users to assume this role in order to admin the cluster
-    const mastersRole = new iam.Role(this, 'AdminRole', {
+    const mastersRole = new iam.Role(this, "AdminRole", {
       assumedBy: new iam.AccountRootPrincipal(),
     });
 
     // just need one nat gateway to simplify the test
-    this.vpc = new ec2.Vpc(this, 'Vpc', { natGateways: 1, restrictDefaultSecurityGroup: false });
+    this.vpc = new ec2.Vpc(this, "Vpc", { natGateways: 1, restrictDefaultSecurityGroup: false });
 
     // create the cluster with no defaultCapacity, nodegroup will be created later
-    this.cluster = new eks.Cluster(this, 'Cluster', {
+    this.cluster = new eks.Cluster(this, "Cluster", {
       vpc: this.vpc,
       mastersRole,
       defaultCapacityType: eks.DefaultCapacityType.NODEGROUP,
       defaultCapacity: 0,
-      version: eks.KubernetesVersion.V1_33,
+      version: eks.KubernetesVersion.V1_34,
       kubectlProviderOptions: {
-        kubectlLayer: new KubectlV33Layer(this, 'kubectlLayer'),
+        kubectlLayer: new KubectlV34Layer(this, "kubectlLayer"),
       },
     });
 
     // create nodegroup with AL2023_X86_64_STANDARD
-    this.cluster.addNodegroupCapacity('MNG_AL2023_X86_64_STANDARD', {
+    this.cluster.addNodegroupCapacity("MNG_AL2023_X86_64_STANDARD", {
       amiType: NodegroupAmiType.AL2023_X86_64_STANDARD,
     });
 
     // create nodegroup with AL2023_ARM_64_STANDARD
-    this.cluster.addNodegroupCapacity('MNG_AL2023_ARM_64_STANDARD', {
+    this.cluster.addNodegroupCapacity("MNG_AL2023_ARM_64_STANDARD", {
       amiType: NodegroupAmiType.AL2023_ARM_64_STANDARD,
     });
 
     // create nodegroup with AL2023_X86_64_NEURON
-    this.cluster.addNodegroupCapacity('MNG_AL2023_X86_64_NEURON', {
+    this.cluster.addNodegroupCapacity("MNG_AL2023_X86_64_NEURON", {
       amiType: NodegroupAmiType.AL2023_X86_64_NEURON,
     });
 
     // create nodegroup with AL2023_X86_64_NVIDIA
-    this.cluster.addNodegroupCapacity('MNG_AL2023_X86_64_NVIDIA', {
+    this.cluster.addNodegroupCapacity("MNG_AL2023_X86_64_NVIDIA", {
       amiType: NodegroupAmiType.AL2023_X86_64_NVIDIA,
     });
   }
@@ -58,13 +58,13 @@ class EksClusterStack extends Stack {
 
 const app = new App({
   postCliContext: {
-    '@aws-cdk/aws-lambda:createNewPoliciesWithAddToRolePolicy': true,
-    '@aws-cdk/aws-lambda:useCdkManagedLogGroup': false,
+    "@aws-cdk/aws-lambda:createNewPoliciesWithAddToRolePolicy": true,
+    "@aws-cdk/aws-lambda:useCdkManagedLogGroup": false,
   },
 });
 
-const stack = new EksClusterStack(app, 'aws-cdk-eks-cluster-al2023-nodegroup-test');
-new integ.IntegTest(app, 'aws-cdk-eks-cluster-al2023-nodegroup', {
+const stack = new EksClusterStack(app, "aws-cdk-eks-cluster-al2023-nodegroup-test");
+new integ.IntegTest(app, "aws-cdk-eks-cluster-al2023-nodegroup", {
   testCases: [stack],
   // Test includes assets that are updated weekly. If not disabled, the upgrade PR will fail.
   diffAssets: false,
