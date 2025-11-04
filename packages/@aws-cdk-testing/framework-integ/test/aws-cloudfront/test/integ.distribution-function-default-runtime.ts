aee16a1abb9e4a063dda8b64d338e26f5ab82e14
Kazuho Cryer-Shinozuka <malaysia.cryer@gmail.com>
2025-11-04 22:56:20 +0900

    update integ

--- packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront/test/integ.distribution-function-default-runtime.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cloudfront/test/integ.distribution-function-default-runtime.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { TestOrigin } from './test-origin';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { ExpectedResult, IntegTest, Match } from '@aws-cdk/integ-tests-alpha';
 
-const app = new cdk.App();
-const stack = new cdk.Stack(app, 'integ-distribution-function', { env: { region: 'eu-west-1' } });
+const app = new cdk.App({
+  postCliContext: {
+    '@aws-cdk/aws-cloudfront:defaultFunctionRuntimeV2_0': true,
+  },
+});
+const stack = new cdk.Stack(app, 'integ-distribution-function-runtime');
 
 const cfFunction = new cloudfront.Function(stack, 'Function', {
   code: cloudfront.FunctionCode.fromInline('function handler(event) { return event.request }'),
 });
 
-new cloudfront.Distribution(stack, 'Dist', {
+new cloudfront.Distribution(stack, 'Distribution', {
   defaultBehavior: {
     origin: new TestOrigin('www.example.com'),
     cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
@@ -21,9 +25,18 @@ new cloudfront.Distribution(stack, 'Dist', {
   },
 });
 
-new cdk.CfnOutput(stack, 'FunctionArn', { value: cfFunction.functionArn });
-new cdk.CfnOutput(stack, 'FunctionStage', { value: cfFunction.functionStage });
-
-new IntegTest(app, 'DistributionFunction', {
+const integ = new IntegTest(app, 'DistributionFunction', {
   testCases: [stack],
 });
+
+integ.assertions.awsApiCall('CloudFront', 'describeFunction', {
+  Name: cfFunction.functionName,
+}).expect(
+  ExpectedResult.objectLike({
+    FunctionSummary: Match.objectLike({
+      FunctionConfig: Match.objectLike({
+        Runtime: 'cloudfront-js-2.0',
+      }),
+    }),
+  }),
+);
