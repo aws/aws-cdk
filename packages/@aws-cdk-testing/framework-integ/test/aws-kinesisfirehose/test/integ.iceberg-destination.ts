de1ccbde32a7ef05ac55554f9a5cb2e7f3bb2605
Konippi <konippi@amazon.co.jp>
2025-11-06 17:16:39 +0900

    fix: rm unnecessary a dependency and add actual cases to integ

--- packages/@aws-cdk-testing/framework-integ/test/aws-kinesisfirehose/test/integ.iceberg-destination.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-kinesisfirehose/test/integ.iceberg-destination.ts
@@ -4,7 +4,7 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib';
 import * as firehose from 'aws-cdk-lib/aws-kinesisfirehose';
-import { ExpectedResult, IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 
 const app = new cdk.App();
 
