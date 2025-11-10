b4466755c1aa642e77522826bedfd6f18d179461
Abdelrahman Abdelrahman <abdlrmn@amazon.de>
2025-11-10 14:37:29 +0000

    refactor and add batch size env option

--- tools/@aws-cdk/integration-test-deployment/test/integration-test-deployment.test.ts
+++ tools/@aws-cdk/integration-test-deployment/test/integration-test-deployment.test.ts
@@ -19,28 +19,34 @@ const atmosphereRoleArn = 'arn:atmopshere:role:mock';
 const endpoint = 'https://test-endpoint.com';
 const pool = 'test-pool';
 
-describe('Run Inegration Tests with Atmosphere', () => {
+describe('Run Integration Tests with Atmosphere', () => {
   let mockAtmosphereAllocation: AtmosphereAllocationMock;
   let changedSnapshots: Set<string>;
 
   const validateSnapshotRun = ({ batchSize }: {batchSize: number}) => {
+    // Test that git diff has only been called once to get the snapshots
     expect(utils.gitDiff).toHaveBeenCalledTimes(1);
 
+    // Test that the each Atmosphere acquire has been released.
     const numberOfCommands = Math.ceil(changedSnapshots.size/batchSize);
     expect(AtmosphereAllocation.acquire).toHaveBeenCalledTimes(numberOfCommands);
     expect(mockAtmosphereAllocation.release).toHaveBeenCalledTimes(numberOfCommands);
+
+    // Test that the role is assumed for every batch run.
     const assumeAtmosphereRoleCalls = (integRunner.assumeAtmosphereRole as jest.Mock).mock.calls;
     expect(assumeAtmosphereRoleCalls.length).toBe(numberOfCommands);
     for (const assumeAtmosphereRoleCall of assumeAtmosphereRoleCalls) {
       expect(assumeAtmosphereRoleCall[0]).toEqual(atmosphereRoleArn);
     }
 
+    // Test that bootstrap is invoked for every batch run.
     const bootstrapCalls = (integRunner.bootstrap as jest.Mock).mock.calls;
     expect(bootstrapCalls.length).toEqual(numberOfCommands);
     for (const call of bootstrapCalls) {
       expect(call[0]).toEqual(expect.objectContaining(env));
     }
 
+    // Test that all snapshots have run exactly once.
     const deployIntegrationTestCalls = (integRunner.deployIntegrationTest as jest.Mock).mock.calls;
     expect(deployIntegrationTestCalls.length).toEqual(numberOfCommands);
     const snapshotCalls = deployIntegrationTestCalls.map((call) => {
@@ -48,7 +54,7 @@ describe('Run Inegration Tests with Atmosphere', () => {
       return call[1];
     }).flat() as string[];
     expect(new Set(snapshotCalls).size).toEqual(snapshotCalls.length); // All snapshots are tested only once.
-    expect(new Set(snapshotCalls).size).toEqual(changedSnapshots.size); // All snapshots are tested.
+    expect(new Set(snapshotCalls)).toEqual(changedSnapshots); // All snapshots are tested.
   };
 
   beforeAll(async () => {
