e7826abf28337e7b2003b612a7cac356c26b556b
Momo Kornher <kornherm@amazon.co.uk>
2025-11-11 11:16:19 +0000

    fix(cdk-lib): multiple submodules use an incorrect namespace for .NET (#36002)
    ### Issue
    
    53 submodules of `aws-cdk-lib` used the wrong .NET package name.
    
    Example:
    ```
    # ❌  wrong
    Amazon.CDK.aws_bedrockagentcore
    
    # ✅ correct
    Amazon.CDK.AWS.BedrockAgentCore
    ```
    
    ❌  wrong
    <img width="514" height="150" alt="image" src="https://github.com/user-attachments/assets/97477ced-3bf0-4076-abfb-12e46ec55fa9" />
    
    ✅ correct
    <img width="453" height="123" alt="image" src="https://github.com/user-attachments/assets/8a76b027-faf4-47bd-aeea-7c1d7b4824db" />
    
    
    Full list of affected submodules:
    
    ```
    aws-aiops
    aws-applicationsignals
    aws-apptest
    aws-arcregionswitch
    aws-arczonalshift
    aws-b2bi
    aws-backupgateway
    aws-bcmdataexports
    aws-bedrock
    aws-bedrockagentcore
    aws-billing
    aws-cleanrooms
    aws-cleanroomsml
    aws-codeconnections
    aws-connectcampaignsv2
    aws-datazone
    aws-deadline
    aws-dsql
    aws-entityresolution
    aws-evs
    aws-gameliftstreams
    aws-healthimaging
    aws-invoicing
    aws-launchwizard
    aws-mediapackagev2
    aws-mpa
    aws-neptunegraph
    aws-notifications
    aws-notificationscontacts
    aws-observabilityadmin
    aws-odb
    aws-osis
    aws-paymentcryptography
    aws-pcaconnectorad
    aws-pcaconnectorscep
    aws-pcs
    aws-proton
    aws-qbusiness
    aws-rbin
    aws-route53profiles
    aws-rtbfabric
    aws-s3express
    aws-s3tables
    aws-s3vectors
    aws-securitylake
    aws-shield
    aws-smsvoice
    aws-ssmguiconnect
    aws-ssmquicksetup
    aws-verifiedpermissions
    aws-workspacesinstances
    aws-workspacesthinclient
    aws-workspacesweb
    ```
    
    ### Reason for this change
    
    The name of submodules is determined by a `.jsiirc.json‎` inside the submodule directory. If not present, the export name is appended to the name of the parent module. However for most jsii languages this convention is not great: JavaScript export names don't translate well into other ecosystems.
    
    For `aws-cdk-lib` we therefore have added `.jsiirc.json‎` files into each submodule. For new services, this file is generated when the service is first added. Due to a bug, this generated file was incorrectly adding the key `package` to the `dotnet` target configuration. It should have been `namespace` instead.
    
    Since this was a mistake, we are fixing this now. It means that the package names for the listed 53 submodules are changing for .NET users.
    
    ### Description of changes
    
    Update the package names of the listed 53 submodules to use the correct configuration.
    
    Adjusted the order of targets in other `.jsiirc.json` files to be the same as generated ones.
    
    ### Describe any new or updated permissions being added
    
    n/a
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/scripts/submodules/index.ts
+++ packages/aws-cdk-lib/scripts/submodules/index.ts
@@ -62,7 +62,7 @@ async function ensureSubmodule(submodule: ModuleMapEntry, modulePath: string) {
           package: submodule.definition.javaPackage,
         },
         dotnet: {
-          package: submodule.definition.dotnetPackage,
+          namespace: submodule.definition.dotnetPackage,
         },
         python: {
           module: submodule.definition.pythonModuleName,
