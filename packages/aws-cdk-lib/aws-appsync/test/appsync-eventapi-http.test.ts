25709652fb30c75ae7dc16b8ae10c75b798f979d
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2025-11-17 13:08:16 +0000

    Fix appsync tests

--- packages/aws-cdk-lib/aws-appsync/test/appsync-eventapi-http.test.ts
+++ packages/aws-cdk-lib/aws-appsync/test/appsync-eventapi-http.test.ts
@@ -118,7 +118,26 @@ describe('Http Data Source configuration', () => {
               'states:GetExecutionHistory',
             ],
             Effect: 'Allow',
-            Resource: 'arn:aws:states:us-east-1::stateMachine:hello:*',
+            Resource: {
+              'Fn::Join': [
+                '',
+                [
+                  'arn:',
+                  {
+                    Ref: 'AWS::Partition',
+                  },
+                  ':states:',
+                  {
+                    Ref: 'AWS::Region',
+                  },
+                  ':',
+                  {
+                    Ref: 'AWS::AccountId',
+                  },
+                  ':execution:hello:*',
+                ],
+              ],
+            },
           },
           {
             Action: [
