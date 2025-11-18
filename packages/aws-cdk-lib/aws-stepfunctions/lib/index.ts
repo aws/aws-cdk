41bc2b0b0f95cad1b8b67e8e24964390521f38d1
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2025-11-18 13:11:53 +0000

    feat(stepfunctions): add `StateMachineGrants`

--- packages/aws-cdk-lib/aws-stepfunctions/lib/index.ts
+++ packages/aws-cdk-lib/aws-stepfunctions/lib/index.ts
@@ -4,6 +4,7 @@ export * from './input';
 export * from './types';
 export * from './condition';
 export * from './state-machine';
+export * from './state-machine-grants';
 export * from './state-machine-fragment';
 export * from './state-transition-metrics';
 export * from './chain';
