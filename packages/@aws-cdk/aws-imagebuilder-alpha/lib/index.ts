341392e289c5316a38c520140b1dafe4424f075b
Alisen Ozel <74890690+ozelalisen@users.noreply.github.com>
2025-11-19 13:19:36 +0100

    revert(imagebuilder-alpha): add support for Distribution Configuration Construct (#36103)
    reverts aws/aws-cdk#36005 as it caused JSII errors that is coming from https://github.com/cdklabs/cdk-generate-synthetic-examples
    
    ```
    @aws-cdk.aws-imagebuilder-alpha.Repository-example.ts:18:58 - error TS2345: Argument of type 'Repository' is not assignable to parameter of type 'IRepository'.
    --
    Type 'Repository' is missing the following properties from type 'IRepository': repositoryArn, repositoryUri, registryUri, repositoryUriForTag, and 16 more.
     
    18 const repository = imagebuilder_alpha.Repository.fromEcr(repository);
    ~~~~~~~~~~
    @aws-cdk.aws-imagebuilder-alpha.Repository-example.ts:18:58 - error TS2448: Block-scoped variable 'repository' used before its declaration.
     
    18 const repository = imagebuilder_alpha.Repository.fromEcr(repository);
    ~~~~~~~~~~
     
    @aws-cdk.aws-imagebuilder-alpha.Repository-example.ts:18:7
    18 const repository = imagebuilder_alpha.Repository.fromEcr(repository);
    ~~~~~~~~~~
    'repository' is declared here.
    @aws-cdk.aws-imagebuilder-alpha.Repository-example.ts:18:58 - error TS2454: Variable 'repository' is used before being assigned.
     
    18 const repository = imagebuilder_alpha.Repository.fromEcr(repository);
    ```

--- packages/@aws-cdk/aws-imagebuilder-alpha/lib/index.ts
+++ packages/@aws-cdk/aws-imagebuilder-alpha/lib/index.ts
@@ -1,7 +1,6 @@
 // AWS::ImageBuilder CloudFormation Resources:
 
 export * from './component';
-export * from './distribution-configuration';
 export * from './infrastructure-configuration';
 
 export * from './os-version';
