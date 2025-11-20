acc1c9391e4dae4bbf4c126d5c189a6660774c1f
Tarun Belani <tarunb0612@gmail.com>
2025-11-20 15:40:06 -0800

    feat(imagebuilder-alpha): add support for Container Recipe Construct (#36091)
    ### Issue
    
    aws/aws-cdk-rfcs#789
    
    ### Reason for this change
    
    This change adds a new alpha module for EC2 Image Builder L2 Constructs (@aws-cdk/aws-imagebuilder-alpha), as outlined in aws/aws-cdk-rfcs#789. This PR specifically implements the ContainerRecipe construct.
    
    ### Description of changes
    
    This change implements the `ContainerRecipe` construct, which is a higher-level construct of [CfnContainerRecipe](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_imagebuilder.CfnContainerRecipe.html).
    
    #### Example
    
    ```ts
    const containerRecipe = new imagebuilder.ContainerRecipe(this, 'ContainerRecipe', {
      containerRecipeName: 'test-container-recipe',
      containerRecipeVersion: '1.0.0',
      description: 'A Container Recipe',
      baseImage: imagebuilder.BaseImage.fromEcr(
        this,
        'ECRImage',
        ecr.Repository.fromRepositoryName(this, 'SourceRepository', 'source-repository'),
        'latest',
      ),
      dockerfile: imagebuilder.DockerfileData.fromInline(`
        FROM {{{ imagebuilder:parentImage }}}
        CMD ["echo", "Hello, world!"]
        {{{ imagebuilder:environments }}}
        {{{ imagebuilder:components }}}
      `),
      targetRepository: imagebuilder.Repository.fromEcr(
        ecr.Repository.fromRepositoryName(this, 'TargetRepository', 'imagebuilder-repository'),
      ),
      // Use an AWS-managed component, shared component, and a self-owned component with parameters
      components: [
        {
          component: imagebuilder.AwsManagedComponent.fromAwsManagedComponentName(
            this,
            'update-linux-component',
            'update-linux',
          ),
        },
        {
          component: imagebuilder.Component.fromComponentArn(
            this,
            'ComplianceTestComponent',
            `arn:${this.partition}:imagebuilder:${this.region}:123456789012:component/compliance-test/2025.x.x.x`,
          ),
        },
        {
          component: imagebuilder.Component.fromComponentAttributes(this, 'CustomComponent', {
            componentName: 'custom-component',
          }),
          parameters: {
            CUSTOM_PARAMETER: imagebuilder.ComponentParameterValue.fromString('custom-parameter-value'),
          },
        },
      ],
      workingDirectory: '/var/tmp',
      // The image + block devices to use for the EC2 instance used for building the container image
      instanceImage: imagebuilder.ContainerInstanceImage.fromSsmParameterName(
        this,
        'ContainerInstanceImage',
        '/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended',
      ),
      instanceBlockDevices: [
        {
          deviceName: '/dev/sda1',
          mappingEnabled: true,
          volume: ec2.BlockDeviceVolume.ebs(50, {
            deleteOnTermination: true,
            iops: 1000,
            volumeType: ec2.EbsDeviceVolumeType.GP3,
            throughput: 1000,
            encrypted: true,
            kmsKey: kms.Key.fromLookup(this, 'VolumeKey', { aliasName: 'alias/volume-encryption-key' }),
          }),
        },
      ],
    });
    ```
    
    ### Describe any new or updated permissions being added
    
    N/A - new L2 construct in alpha module
    
    ### Description of how you validated changes
    
    Validated with unit tests and integration tests. Manually verified generated CFN templates as well.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ packages/@aws-cdk/aws-imagebuilder-alpha/test/integ.all-parameters.container-recipe.ts
@@ -0,0 +1,96 @@
+import * as integ from '@aws-cdk/integ-tests-alpha';
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as imagebuilder from '../lib';
+import { DockerfileData } from '../lib';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'aws-cdk-imagebuilder-container-recipe-all-parameters');
+
+const repository = new ecr.Repository(stack, 'Repository', { removalPolicy: cdk.RemovalPolicy.DESTROY });
+
+const parameterizedComponent = new imagebuilder.Component(stack, 'ParameterizedComponent', {
+  platform: imagebuilder.Platform.LINUX,
+  data: imagebuilder.ComponentData.fromComponentDocumentJsonObject({
+    name: 'ParameterizedComponent',
+    description: 'This is a parameterized component',
+    schemaVersion: imagebuilder.ComponentSchemaVersion.V1_0,
+    parameters: {
+      parameter1: {
+        type: imagebuilder.ComponentParameterType.STRING,
+        description: 'This is the first parameter',
+      },
+      parameter2: {
+        type: imagebuilder.ComponentParameterType.STRING,
+        description: 'This is the second parameter',
+      },
+    },
+    phases: [
+      {
+        name: imagebuilder.ComponentPhaseName.BUILD,
+        steps: [
+          {
+            name: 'step1',
+            action: imagebuilder.ComponentAction.EXECUTE_BASH,
+            inputs: imagebuilder.ComponentStepInputs.fromObject({
+              commands: ['echo ${{ parameter1 }}', 'echo ${{ parameter2 }}'],
+            }),
+          },
+        ],
+      },
+    ],
+  }),
+});
+
+const containerRecipe = new imagebuilder.ContainerRecipe(stack, 'ContainerRecipe', {
+  containerRecipeName: 'test-container-recipe',
+  containerRecipeVersion: '1.2.x',
+  description: 'A test container recipe',
+  baseImage: imagebuilder.BaseContainerImage.fromEcrPublic('amazonlinux', 'amazonlinux', 'latest'),
+  osVersion: imagebuilder.OSVersion.AMAZON_LINUX_2023,
+  targetRepository: imagebuilder.Repository.fromEcr(repository),
+  dockerfile: DockerfileData.fromInline(`FROM {{{ imagebuilder:parentImage }}}
+CMD ["echo", "Hello, world!"]
+{{{ imagebuilder:environments }}}
+{{{ imagebuilder:components }}}`),
+  components: [
+    {
+      component: imagebuilder.AwsManagedComponent.helloWorld(stack, 'Component2', {
+        platform: imagebuilder.Platform.LINUX,
+      }),
+    },
+    {
+      component: parameterizedComponent,
+      parameters: {
+        parameter1: imagebuilder.ComponentParameterValue.fromString('parameter-value-1'),
+        parameter2: imagebuilder.ComponentParameterValue.fromString('parameter-value-2'),
+      },
+    },
+  ],
+  workingDirectory: '/var/tmp',
+  instanceImage: imagebuilder.ContainerInstanceImage.fromSsmParameterName(
+    '/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended/image_id',
+  ),
+  tags: {
+    key1: 'value1',
+    key2: 'value2',
+  },
+});
+
+containerRecipe.addInstanceBlockDevice({
+  deviceName: '/dev/sda2',
+  volume: ec2.BlockDeviceVolume.ebsFromSnapshot('snap-123', {
+    volumeSize: 75,
+    deleteOnTermination: true,
+    iops: 1000,
+    volumeType: ec2.EbsDeviceVolumeType.GENERAL_PURPOSE_SSD_GP3,
+    throughput: 125,
+  }),
+});
+
+new cdk.CfnOutput(stack, 'ContainerRecipeVersion', { value: containerRecipe.containerRecipeVersion });
+
+new integ.IntegTest(app, 'ContainerRecipeTest-AllParameters', {
+  testCases: [stack],
+});
