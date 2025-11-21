a6c02883d4b6598a8a24d1bab85164a0c6cc6c05
A. Abdel-Rahman <abdlrmn@amazon.de>
2025-11-21 21:05:48 +0100

    ci(integ-runner): fix deployment to incorrect region (#36143)
    ### Reason for this change
    
    Integ runner does not deploy to the correct regions that have been set to be bootstrapped.
    
    ### Description of changes
    
    Updates the integ-runner version which fixes this bug. Fix is detailed in https://github.com/aws/aws-cdk-cli/pull/949
    
    As the new version changes the default CWD of integ tests, some integ tests had to be modified to use __dirname to lookup local assets files.
    
    ### Describe any new or updated permissions being added
    
    No new permissions are added.
    
    ### Description of how you validated changes
    
    Fix is confirmed with local fork run: https://github.com/Abogical/aws-cdk/actions/runs/19569552896/job/56039507543
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk-testing/framework-integ/test/cloudformation-include/test/integ.autoscaling-update-policy.ts
+++ packages/@aws-cdk-testing/framework-integ/test/cloudformation-include/test/integ.autoscaling-update-policy.ts
@@ -1,12 +1,13 @@
 import * as core from 'aws-cdk-lib';
 import * as inc from 'aws-cdk-lib/cloudformation-include';
 import * as integ from '@aws-cdk/integ-tests-alpha';
+import * as path from 'path';
 
 const app = new core.App();
 const stack = new core.Stack(app, 'Stack');
 
 new inc.CfnInclude(stack, 'Stack', {
-  templateFile: 'test-templates/autoscaling-update-policy.json',
+  templateFile: path.join(__dirname, 'test-templates/autoscaling-update-policy.json'),
 });
 
 new integ.IntegTest(app, 'AutoScalingUpdatePolicyTest', {
