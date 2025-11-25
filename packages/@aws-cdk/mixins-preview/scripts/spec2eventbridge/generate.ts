0ad520e63207dc51316d7245fa727b88b9fe5e5b
Rico Huijbers <rix0rrr@gmail.com>
2025-11-25 17:08:51 +0100

    Fix duplicate namespaces

--- packages/@aws-cdk/mixins-preview/scripts/spec2eventbridge/generate.ts
+++ packages/@aws-cdk/mixins-preview/scripts/spec2eventbridge/generate.ts
@@ -11,6 +11,7 @@ export async function generateAll(options: GenerateOptions): Promise<ModuleMap> 
   const services = await db.all('service');
   const moduleMap: ModuleMap = loadModuleMap({
     packageBases: MIXINS_PREVIEW_BASE_NAMES,
+    respectOverrides: false,
   });
   const moduleRequests: GenerateModuleMap = {};
 
