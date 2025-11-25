c6d8febc62e6fbd223c827c0cd590df4868fbc48
Gasser <gasolima@amazon.com>
2025-11-25 15:50:14 +0100

    fix l1-l2.test.ts import types

--- packages/@aws-cdk/mixins-preview/test/events/l1-l2.test.ts
+++ packages/@aws-cdk/mixins-preview/test/events/l1-l2.test.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @cdklabs/no-throw-default-error */
 import { Stack } from 'aws-cdk-lib';
-import { CfnRule, Rule, type EventPattern } from 'aws-cdk-lib/aws-events';
+import { CfnRule, Rule } from 'aws-cdk-lib/aws-events';
+import type { EventPattern } from 'aws-cdk-lib/aws-events';
 import { Construct } from 'constructs';
 import { BucketEvents } from '../../lib/services/aws-s3/events';
 import { Template } from 'aws-cdk-lib/assertions';
 import { AutoScalingGroupEvents } from '../../lib/services/aws-autoscaling/events.generated';
-import { AutoScalingGroupReference } from 'aws-cdk-lib/aws-autoscaling';
+import type { AutoScalingGroupReference } from 'aws-cdk-lib/aws-autoscaling';
 
 describe.each([
   ['CfnRule', (scope: Construct, eventPattern: EventPattern) => {
