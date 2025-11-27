deef50ea503c3ee3abf5e66f7231a832caba6492
Dennis Ruhe <dennis.ruhe@mybit.nl>
2025-11-27 13:03:04 +0100

    feat(aws-neptune-alpha): Add publiclyAccessible to cluster and instance props

--- packages/@aws-cdk/aws-neptune-alpha/lib/instance.ts
+++ packages/@aws-cdk/aws-neptune-alpha/lib/instance.ts
@@ -412,6 +412,13 @@ export interface DatabaseInstanceProps {
    * @default undefined
    */
   readonly autoMinorVersionUpgrade?: boolean;
+
+  /**
+   * Indicates whether the DB instance is publicly accessible.
+   *
+   * @default false
+   */
+  readonly publiclyAccessible?: boolean;
 }
 
 /**
@@ -513,6 +520,7 @@ export class DatabaseInstance extends DatabaseInstanceBase implements IDatabaseI
       availabilityZone: props.availabilityZone,
       dbInstanceIdentifier: props.dbInstanceName,
       dbParameterGroupName: props.parameterGroup?.parameterGroupName,
+      publiclyAccessible: props.publiclyAccessible,
     });
 
     this.cluster = props.cluster;
