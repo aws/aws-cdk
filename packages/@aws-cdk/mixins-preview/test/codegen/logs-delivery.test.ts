6338b3c25ad3adb42615370108e23f9ffcdaba2d
Leena <81589006+ShadowCat567@users.noreply.github.com>
2025-12-18 11:39:26 -0500

    chore(mixin-preview): temporary property while adjusting type of `vendedLogs` (#36431)
    ### Reason for this change
    
    
    We are switching `<Resource>.vendedLogs` to have a different type for better current and future compatibility. 
    ```
    OLD TYPE
    ...
      vendedLogs: VendedLog;
    }
    
    export interface VendedLog {
      readonly permissionsVersion: string;
      readonly logTypes: string[];
      readonly destinations: DestinationService[];
    }
    ```
    ```
    NEW TYPE
    ...
      vendedLogs: VendedLogs[];
    }
    
    export interface VendedLogs {
      readonly permissionsVersion: string;
      readonly logType: string;
      readonly destinations: DestinationService[];
    }
    ```
    
    ### Description of changes
    
    
    Since there is not an easy way to do this were we can keep using the `vendedLogs` property the whole time, we are going to temporarily use `vendedLogsConfig` and remove it in a later PR. 
    
    
    ### Description of how you validated changes
    
    
    Ensured generated Mixin still looks right
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/mixins-preview/test/codegen/logs-delivery.test.ts
+++ packages/@aws-cdk/mixins-preview/test/codegen/logs-delivery.test.ts
@@ -43,6 +43,38 @@ test('Logs Delivery Mixin for a resource', () => {
       logTypes: ['APPLICATION_LOGS', 'ACCESS_LOGS'],
       destinations: ['CWL', 'FH', 'S3'],
     },
+    vendedLogsConfig: [
+      {
+        permissionsVersion: 'V2',
+        logType: 'APPLICATION_LOGS',
+        destinations: [
+          {
+            destinationType: 'S3',
+          },
+          {
+            destinationType: 'CWL',
+          },
+          {
+            destinationType: 'FH',
+          },
+        ],
+      },
+      {
+        permissionsVersion: 'V2',
+        logType: 'ACCESS_LOGS',
+        destinations: [
+          {
+            destinationType: 'S3',
+          },
+          {
+            destinationType: 'CWL',
+          },
+          {
+            destinationType: 'FH',
+          },
+        ],
+      },
+    ],
   });
   db.link('hasResource', service, resource);
 
