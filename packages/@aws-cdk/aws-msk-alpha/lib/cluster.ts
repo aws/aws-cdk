c8b1c7549e01702fb02f22392ff49bf0cf032b82
gasolima <141347041+gasolima@users.noreply.github.com>
2025-12-19 13:30:59 +0100

    Update packages/@aws-cdk/aws-msk-alpha/lib/cluster.ts

--- packages/@aws-cdk/aws-msk-alpha/lib/cluster.ts
+++ packages/@aws-cdk/aws-msk-alpha/lib/cluster.ts
@@ -531,6 +531,7 @@ export class Cluster extends ClusterBase {
     if (isExpress) {
       // Validate Kafka version compatibility
       // msk service documentation for supported versions https://docs.aws.amazon.com/msk/latest/developerguide/supported-kafka-versions.html
+      // express broker documentation for supported versions https://docs.aws.amazon.com/msk/latest/developerguide/msk-broker-types-express.html#msk-broker-types-express-notes
       const supportedVersions = ['3.6', '3.8', '3.9'];
       const kafkaVersionString = props.kafkaVersion.version;
       const isCompatibleVersion = supportedVersions.some(version => kafkaVersionString.includes(version));
