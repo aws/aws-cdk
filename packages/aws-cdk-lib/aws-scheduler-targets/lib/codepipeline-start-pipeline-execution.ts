02f138e1638b4135ec1d261477760974fa95e061
Rico Hermans <rix0rrr@gmail.com>
2026-01-07 10:59:13 +0000

    Undo using new ARN handies, it won't work

--- packages/aws-cdk-lib/aws-scheduler-targets/lib/codepipeline-start-pipeline-execution.ts
+++ packages/aws-cdk-lib/aws-scheduler-targets/lib/codepipeline-start-pipeline-execution.ts
@@ -1,5 +1,5 @@
 import { ScheduleTargetBase, ScheduleTargetBaseProps } from './target';
-import { CfnPipeline } from '../../aws-codepipeline';
+import { CfnPipeline, IPipeline } from '../../aws-codepipeline';
 import { IRole, PolicyStatement } from '../../aws-iam';
 import { IScheduleTarget } from '../../aws-scheduler';
 import { IPipelineRef } from '../../interfaces/generated/aws-codepipeline-interfaces.generated';
@@ -12,13 +12,26 @@ export class CodePipelineStartPipelineExecution extends ScheduleTargetBase imple
     private readonly pipeline: IPipelineRef,
     props: ScheduleTargetBaseProps = {},
   ) {
-    super(props, CfnPipeline.arnForPipeline(pipeline));
+    super(props, pipelineArn(pipeline));
   }
 
   protected addTargetActionToRole(role: IRole): void {
     role.addToPrincipalPolicy(new PolicyStatement({
       actions: ['codepipeline:StartPipelineExecution'],
-      resources: [CfnPipeline.arnForPipeline(this.pipeline)],
+      resources: [pipelineArn(this.pipeline)],
     }));
   }
+}
+
+function pipelineArn(pipeline: IPipelineRef) : string {
+  // If the pipeline happens to be an IPipeline, it might already have a literal ARN, different
+  // than we one we construct from the `pipelineName` and the env.
+  //
+  // Because the Role logical ID is also hashed from this string, we have to keep on using
+  // it in order to not cause damage to existing templates.
+  return isIPipeline(pipeline) ? pipeline.pipelineArn : CfnPipeline.arnForPipeline(pipeline);
+}
+
+function isIPipeline(pipeline: IPipelineRef): pipeline is IPipeline {
+  return !!(pipeline as IPipeline).pipelineArn;
 }
