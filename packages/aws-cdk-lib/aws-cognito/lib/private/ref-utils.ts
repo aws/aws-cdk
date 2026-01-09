c6cb79c512d976540d25b3071a82affe5f44efef
Rico Huijbers <rix0rrr@gmail.com>
2026-01-09 10:55:00 +0100

    Fix type checkers

--- packages/aws-cdk-lib/aws-cognito/lib/private/ref-utils.ts
+++ packages/aws-cdk-lib/aws-cognito/lib/private/ref-utils.ts
@@ -7,13 +7,16 @@ import { IUserPoolIdentityProvider } from '../user-pool-idp';
  * Converts an IUserPoolRef to IUserPool, validating that it implements the full interface
  */
 export function toIUserPool(ref: IUserPoolRef): IUserPool {
-  const pool = ref as any;
-  if (typeof pool.userPoolId !== 'string' || typeof pool.userPoolArn !== 'string' || typeof pool.userPoolProviderName !== 'string') {
+  if (!isIUserPool(ref)) {
     throw new UnscopedValidationError(`'userPool' instance should implement IUserPool, but doesn't: ${ref.constructor.name}`);
   }
-  return ref as IUserPool;
+  return ref;
+}
+
+function isIUserPool(pool: IUserPoolRef): pool is IUserPool {
+  return ('userPoolId' in pool && 'userPoolArn' in pool && 'userPoolProviderName' in pool);
 }
 
 export function isIUserPoolIdentityProvider(x: IUserPoolIdentityProviderRef): x is IUserPoolIdentityProvider {
-  return !!(x as IUserPoolIdentityProvider).providerName;
+  return 'providerName' in x;
 }
