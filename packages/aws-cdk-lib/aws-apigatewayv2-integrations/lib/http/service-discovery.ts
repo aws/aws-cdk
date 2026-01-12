aws/aws-cdk

commit e36e49a66df1e078dcaa40a1427233bfbc717b1c

chore(servicediscovery): reference interfaces (#36494)
Reference interfaces for L2s, like https://github.com/aws/aws-cdk/pull/35271.

"chore" because I plan to make a lot of these PRs and they're not adding value to the changelog.

(Generated with AI)

packages/aws-cdk-lib/aws-apigatewayv2-integrations/lib/http/service-discovery.ts
@@ -1,8 +1,8 @@
 import { HttpPrivateIntegrationOptions } from './base-types';
 import { HttpPrivateIntegration } from './private/integration';
 import { HttpRouteIntegrationBindOptions, HttpRouteIntegrationConfig } from '../../../aws-apigatewayv2';
-import * as servicediscovery from '../../../aws-servicediscovery';
 import { ValidationError } from '../../../core/lib/errors';
+import { IServiceRef } from '../../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
 /**
  * Properties to initialize `HttpServiceDiscoveryIntegration`.
@@ -21,7 +21,7 @@ export class HttpServiceDiscoveryIntegration extends HttpPrivateIntegration {
    */
   constructor(
     id: string,
-    private readonly service: servicediscovery.IService,
+    private readonly service: IServiceRef,
     private readonly props: HttpServiceDiscoveryIntegrationProps = {}) {
     super(id);
   }
@@ -37,7 +37,7 @@ export class HttpServiceDiscoveryIntegration extends HttpPrivateIntegration {
       type: this.integrationType,
       connectionType: this.connectionType,
       connectionId: this.props.vpcLink.vpcLinkId,
-      uri: this.service.serviceArn,
+      uri: this.service.serviceRef.serviceArn,
       secureServerName: this.props.secureServerName,
       parameterMapping: this.props.parameterMapping,
     };

packages/aws-cdk-lib/aws-servicediscovery/lib/http-namespace.ts
@@ -5,9 +5,10 @@ import { CfnHttpNamespace } from './servicediscovery.generated';
 import { Resource } from '../../core';
 import { addConstructMetadata, MethodMetadata } from '../../core/lib/metadata-resource';
 import { propertyInjectable } from '../../core/lib/prop-injectable';
+import { HttpNamespaceReference, IHttpNamespaceRef } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
 export interface HttpNamespaceProps extends BaseNamespaceProps {}
-export interface IHttpNamespace extends INamespace { }
+export interface IHttpNamespace extends INamespace, IHttpNamespaceRef { }
 export interface HttpNamespaceAttributes {
   /**
    * A name for the Namespace.
@@ -39,6 +40,12 @@ export class HttpNamespace extends Resource implements IHttpNamespace {
       public namespaceId = attrs.namespaceId;
       public namespaceArn = attrs.namespaceArn;
       public type = NamespaceType.HTTP;
+      public get httpNamespaceRef(): HttpNamespaceReference {
+        return {
+          httpNamespaceId: attrs.namespaceId,
+          httpNamespaceArn: attrs.namespaceArn,
+        };
+      }
     }
     return new Import(scope, id);
   }
@@ -87,6 +94,13 @@ export class HttpNamespace extends Resource implements IHttpNamespace {
 
   /** @attribute */
   public get httpNamespaceId() { return this.namespaceId; }
+
+  public get httpNamespaceRef(): HttpNamespaceReference {
+    return {
+      httpNamespaceId: this.namespaceId,
+      httpNamespaceArn: this.namespaceArn,
+    };
+  }
 
   /**
    * Creates a service within the namespace

packages/aws-cdk-lib/aws-servicediscovery/lib/instance.ts
@@ -1,7 +1,8 @@
 import { IService } from './service';
 import { IResource, Names, Resource } from '../../core';
+import { IInstanceRef, InstanceReference } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
-export interface IInstance extends IResource {
+export interface IInstance extends IResource, IInstanceRef {
   /**
    * The id of the instance resource
    * @attribute
@@ -44,6 +45,12 @@ export abstract class InstanceBase extends Resource implements IInstance {
    * The Cloudmap service to which the instance is registered.
    */
   public abstract readonly service: IService;
+
+  public get instanceRef(): InstanceReference {
+    return {
+      instanceId: this.instanceId,
+    };
+  }
 
   /**
    * Generate a unique instance Id that is safe to pass to CloudMap

packages/aws-cdk-lib/aws-servicediscovery/lib/private-dns-namespace.ts
@@ -6,6 +6,7 @@ import * as ec2 from '../../aws-ec2';
 import { Resource, ValidationError } from '../../core';
 import { addConstructMetadata, MethodMetadata } from '../../core/lib/metadata-resource';
 import { propertyInjectable } from '../../core/lib/prop-injectable';
+import { IPrivateDnsNamespaceRef, PrivateDnsNamespaceReference } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
 export interface PrivateDnsNamespaceProps extends BaseNamespaceProps {
   /**
@@ -14,7 +15,7 @@ export interface PrivateDnsNamespaceProps extends BaseNamespaceProps {
   readonly vpc: ec2.IVpc;
 }
 
-export interface IPrivateDnsNamespace extends INamespace { }
+export interface IPrivateDnsNamespace extends INamespace, IPrivateDnsNamespaceRef { }
 
 export interface PrivateDnsNamespaceAttributes {
   /**
@@ -47,6 +48,12 @@ export class PrivateDnsNamespace extends Resource implements IPrivateDnsNamespac
       public namespaceId = attrs.namespaceId;
       public namespaceArn = attrs.namespaceArn;
       public type = NamespaceType.DNS_PRIVATE;
+      public get privateDnsNamespaceRef(): PrivateDnsNamespaceReference {
+        return {
+          privateDnsNamespaceId: attrs.namespaceId,
+          privateDnsNamespaceArn: attrs.namespaceArn,
+        };
+      }
     }
     return new Import(scope, id);
   }
@@ -105,6 +112,13 @@ export class PrivateDnsNamespace extends Resource implements IPrivateDnsNamespac
 
   /** @attribute */
   public get privateDnsNamespaceId() { return this.namespaceId; }
+
+  public get privateDnsNamespaceRef(): PrivateDnsNamespaceReference {
+    return {
+      privateDnsNamespaceId: this.namespaceId,
+      privateDnsNamespaceArn: this.namespaceArn,
+    };
+  }
 
   /**
    * Creates a service within the namespace

packages/aws-cdk-lib/aws-servicediscovery/lib/public-dns-namespace.ts
@@ -5,9 +5,10 @@ import { CfnPublicDnsNamespace } from './servicediscovery.generated';
 import { Resource } from '../../core';
 import { addConstructMetadata, MethodMetadata } from '../../core/lib/metadata-resource';
 import { propertyInjectable } from '../../core/lib/prop-injectable';
+import { IPublicDnsNamespaceRef, PublicDnsNamespaceReference } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
 export interface PublicDnsNamespaceProps extends BaseNamespaceProps {}
-export interface IPublicDnsNamespace extends INamespace { }
+export interface IPublicDnsNamespace extends INamespace, IPublicDnsNamespaceRef { }
 export interface PublicDnsNamespaceAttributes {
   /**
    * A name for the Namespace.
@@ -39,6 +40,12 @@ export class PublicDnsNamespace extends Resource implements IPublicDnsNamespace 
       public namespaceId = attrs.namespaceId;
       public namespaceArn = attrs.namespaceArn;
       public type = NamespaceType.DNS_PUBLIC;
+      public get publicDnsNamespaceRef(): PublicDnsNamespaceReference {
+        return {
+          publicDnsNamespaceId: attrs.namespaceId,
+          publicDnsNamespaceArn: attrs.namespaceArn,
+        };
+      }
     }
     return new Import(scope, id);
   }
@@ -93,6 +100,13 @@ export class PublicDnsNamespace extends Resource implements IPublicDnsNamespace 
 
   /** @attribute */
   public get publicDnsNamespaceId() { return this.namespaceId; }
+
+  public get publicDnsNamespaceRef(): PublicDnsNamespaceReference {
+    return {
+      publicDnsNamespaceId: this.namespaceId,
+      publicDnsNamespaceArn: this.namespaceArn,
+    };
+  }
 
   /**
    * Creates a service within the namespace

packages/aws-cdk-lib/aws-servicediscovery/lib/service.ts
@@ -11,8 +11,9 @@ import * as elbv2 from '../../aws-elasticloadbalancingv2';
 import { Duration, IResource, Resource, ValidationError } from '../../core';
 import { addConstructMetadata, MethodMetadata } from '../../core/lib/metadata-resource';
 import { propertyInjectable } from '../../core/lib/prop-injectable';
+import { IServiceRef, ServiceReference } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
-export interface IService extends IResource {
+export interface IService extends IResource, IServiceRef {
   /**
    * A name for the Cloudmap Service.
    * @attribute
@@ -152,6 +153,13 @@ abstract class ServiceBase extends Resource implements IService {
   public abstract routingPolicy: RoutingPolicy;
   public abstract readonly serviceName: string;
   public abstract discoveryType: DiscoveryType;
+
+  public get serviceRef(): ServiceReference {
+    return {
+      serviceId: this.serviceId,
+      serviceArn: this.serviceArn,
+    };
+  }
 }
 
 export interface ServiceAttributes {

commit 72ddad5c9b80f3c38ff020a8712ae69ec4c4318e

fix(servicediscovery): revert reference interfaces that broke type compatibility
Reverts commit e36e49a66d which introduced reference interfaces causing
INamespace to be incompatible with IPrivateDnsNamespace.

Fixes #36658

packages/aws-cdk-lib/aws-apigatewayv2-integrations/lib/http/service-discovery.ts
@@ -1,8 +1,8 @@
 import { HttpPrivateIntegrationOptions } from './base-types';
 import { HttpPrivateIntegration } from './private/integration';
 import { HttpRouteIntegrationBindOptions, HttpRouteIntegrationConfig } from '../../../aws-apigatewayv2';
+import * as servicediscovery from '../../../aws-servicediscovery';
 import { ValidationError } from '../../../core/lib/errors';
-import { IServiceRef } from '../../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
 /**
  * Properties to initialize `HttpServiceDiscoveryIntegration`.
@@ -21,7 +21,7 @@ export class HttpServiceDiscoveryIntegration extends HttpPrivateIntegration {
    */
   constructor(
     id: string,
-    private readonly service: IServiceRef,
+    private readonly service: servicediscovery.IService,
     private readonly props: HttpServiceDiscoveryIntegrationProps = {}) {
     super(id);
   }
@@ -37,7 +37,7 @@ export class HttpServiceDiscoveryIntegration extends HttpPrivateIntegration {
       type: this.integrationType,
       connectionType: this.connectionType,
       connectionId: this.props.vpcLink.vpcLinkId,
-      uri: this.service.serviceRef.serviceArn,
+      uri: this.service.serviceArn,
       secureServerName: this.props.secureServerName,
       parameterMapping: this.props.parameterMapping,
     };

packages/aws-cdk-lib/aws-servicediscovery/lib/http-namespace.ts
@@ -5,10 +5,9 @@ import { CfnHttpNamespace } from './servicediscovery.generated';
 import { Resource } from '../../core';
 import { addConstructMetadata, MethodMetadata } from '../../core/lib/metadata-resource';
 import { propertyInjectable } from '../../core/lib/prop-injectable';
-import { HttpNamespaceReference, IHttpNamespaceRef } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
 export interface HttpNamespaceProps extends BaseNamespaceProps {}
-export interface IHttpNamespace extends INamespace, IHttpNamespaceRef { }
+export interface IHttpNamespace extends INamespace { }
 export interface HttpNamespaceAttributes {
   /**
    * A name for the Namespace.
@@ -40,12 +39,6 @@ export class HttpNamespace extends Resource implements IHttpNamespace {
       public namespaceId = attrs.namespaceId;
       public namespaceArn = attrs.namespaceArn;
       public type = NamespaceType.HTTP;
-      public get httpNamespaceRef(): HttpNamespaceReference {
-        return {
-          httpNamespaceId: attrs.namespaceId,
-          httpNamespaceArn: attrs.namespaceArn,
-        };
-      }
     }
     return new Import(scope, id);
   }
@@ -94,13 +87,6 @@ export class HttpNamespace extends Resource implements IHttpNamespace {
 
   /** @attribute */
   public get httpNamespaceId() { return this.namespaceId; }
-
-  public get httpNamespaceRef(): HttpNamespaceReference {
-    return {
-      httpNamespaceId: this.namespaceId,
-      httpNamespaceArn: this.namespaceArn,
-    };
-  }
 
   /**
    * Creates a service within the namespace

packages/aws-cdk-lib/aws-servicediscovery/lib/instance.ts
@@ -1,8 +1,7 @@
 import { IService } from './service';
 import { IResource, Names, Resource } from '../../core';
-import { IInstanceRef, InstanceReference } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
-export interface IInstance extends IResource, IInstanceRef {
+export interface IInstance extends IResource {
   /**
    * The id of the instance resource
    * @attribute
@@ -45,12 +44,6 @@ export abstract class InstanceBase extends Resource implements IInstance {
    * The Cloudmap service to which the instance is registered.
    */
   public abstract readonly service: IService;
-
-  public get instanceRef(): InstanceReference {
-    return {
-      instanceId: this.instanceId,
-    };
-  }
 
   /**
    * Generate a unique instance Id that is safe to pass to CloudMap

packages/aws-cdk-lib/aws-servicediscovery/lib/private-dns-namespace.ts
@@ -6,7 +6,6 @@ import * as ec2 from '../../aws-ec2';
 import { Resource, ValidationError } from '../../core';
 import { addConstructMetadata, MethodMetadata } from '../../core/lib/metadata-resource';
 import { propertyInjectable } from '../../core/lib/prop-injectable';
-import { IPrivateDnsNamespaceRef, PrivateDnsNamespaceReference } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
 export interface PrivateDnsNamespaceProps extends BaseNamespaceProps {
   /**
@@ -15,7 +14,7 @@ export interface PrivateDnsNamespaceProps extends BaseNamespaceProps {
   readonly vpc: ec2.IVpc;
 }
 
-export interface IPrivateDnsNamespace extends INamespace, IPrivateDnsNamespaceRef { }
+export interface IPrivateDnsNamespace extends INamespace { }
 
 export interface PrivateDnsNamespaceAttributes {
   /**
@@ -48,12 +47,6 @@ export class PrivateDnsNamespace extends Resource implements IPrivateDnsNamespac
       public namespaceId = attrs.namespaceId;
       public namespaceArn = attrs.namespaceArn;
       public type = NamespaceType.DNS_PRIVATE;
-      public get privateDnsNamespaceRef(): PrivateDnsNamespaceReference {
-        return {
-          privateDnsNamespaceId: attrs.namespaceId,
-          privateDnsNamespaceArn: attrs.namespaceArn,
-        };
-      }
     }
     return new Import(scope, id);
   }
@@ -112,13 +105,6 @@ export class PrivateDnsNamespace extends Resource implements IPrivateDnsNamespac
 
   /** @attribute */
   public get privateDnsNamespaceId() { return this.namespaceId; }
-
-  public get privateDnsNamespaceRef(): PrivateDnsNamespaceReference {
-    return {
-      privateDnsNamespaceId: this.namespaceId,
-      privateDnsNamespaceArn: this.namespaceArn,
-    };
-  }
 
   /**
    * Creates a service within the namespace

packages/aws-cdk-lib/aws-servicediscovery/lib/public-dns-namespace.ts
@@ -5,10 +5,9 @@ import { CfnPublicDnsNamespace } from './servicediscovery.generated';
 import { Resource } from '../../core';
 import { addConstructMetadata, MethodMetadata } from '../../core/lib/metadata-resource';
 import { propertyInjectable } from '../../core/lib/prop-injectable';
-import { IPublicDnsNamespaceRef, PublicDnsNamespaceReference } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
 export interface PublicDnsNamespaceProps extends BaseNamespaceProps {}
-export interface IPublicDnsNamespace extends INamespace, IPublicDnsNamespaceRef { }
+export interface IPublicDnsNamespace extends INamespace { }
 export interface PublicDnsNamespaceAttributes {
   /**
    * A name for the Namespace.
@@ -40,12 +39,6 @@ export class PublicDnsNamespace extends Resource implements IPublicDnsNamespace 
       public namespaceId = attrs.namespaceId;
       public namespaceArn = attrs.namespaceArn;
       public type = NamespaceType.DNS_PUBLIC;
-      public get publicDnsNamespaceRef(): PublicDnsNamespaceReference {
-        return {
-          publicDnsNamespaceId: attrs.namespaceId,
-          publicDnsNamespaceArn: attrs.namespaceArn,
-        };
-      }
     }
     return new Import(scope, id);
   }
@@ -100,13 +93,6 @@ export class PublicDnsNamespace extends Resource implements IPublicDnsNamespace 
 
   /** @attribute */
   public get publicDnsNamespaceId() { return this.namespaceId; }
-
-  public get publicDnsNamespaceRef(): PublicDnsNamespaceReference {
-    return {
-      publicDnsNamespaceId: this.namespaceId,
-      publicDnsNamespaceArn: this.namespaceArn,
-    };
-  }
 
   /**
    * Creates a service within the namespace

packages/aws-cdk-lib/aws-servicediscovery/lib/service.ts
@@ -11,9 +11,8 @@ import * as elbv2 from '../../aws-elasticloadbalancingv2';
 import { Duration, IResource, Resource, ValidationError } from '../../core';
 import { addConstructMetadata, MethodMetadata } from '../../core/lib/metadata-resource';
 import { propertyInjectable } from '../../core/lib/prop-injectable';
-import { IServiceRef, ServiceReference } from '../../interfaces/generated/aws-servicediscovery-interfaces.generated';
 
-export interface IService extends IResource, IServiceRef {
+export interface IService extends IResource {
   /**
    * A name for the Cloudmap Service.
    * @attribute
@@ -153,13 +152,6 @@ abstract class ServiceBase extends Resource implements IService {
   public abstract routingPolicy: RoutingPolicy;
   public abstract readonly serviceName: string;
   public abstract discoveryType: DiscoveryType;
-
-  public get serviceRef(): ServiceReference {
-    return {
-      serviceId: this.serviceId,
-      serviceArn: this.serviceArn,
-    };
-  }
 }
 
 export interface ServiceAttributes {