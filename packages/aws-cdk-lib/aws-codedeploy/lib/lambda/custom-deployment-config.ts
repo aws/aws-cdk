61bd761cb9848e0a23a423dcb653fc68eeeac1f8
Rico Hermans <rix0rrr@gmail.com>
2026-01-13 12:10:42 +0100

    chore(codedeploy): reference interfaces (#36525)
    Reference interfaces for L2s, like https://github.com/aws/aws-cdk/pull/35271.
    
    "chore" because I plan to make a lot of these PRs and they're not adding value to the changelog.
    
    Slightly larger change here because I needed to get rid of a blanket 
    
    ```ts
        if (this.deploymentConfig instanceof Construct) {
          this.node.addDependency(this.deploymentConfig);
        }
    ```
    
    Predefined deployment configs now *do* implement `Construct` (via `DetachedConstruct`), but will throw when `node` is accessed so they cannot be used as the target of `addDependency`.
    
    Looks like this dependency feature was used for 2 places:
    
    - To add a dependency on a custom resource that would create a deployment config via side effect (still necessary).
    - To add a dependency on a deployment config that we already take the `config.deploymentConfigName` of. The usage of that name, which will be a `{ Ref }` or `{ Fn::GetAtt }`, will already be sufficient to sequence the deployment properly, so that's not necessary anymore.
    
    What I'm doing is making the `bindEnvironment()` facility public, which gives the `CustomDeploymentConfig` construct the ability to implement that and add the dependency there, for the single case where it's necessary.

--- packages/aws-cdk-lib/aws-codedeploy/lib/lambda/custom-deployment-config.ts
+++ packages/aws-cdk-lib/aws-codedeploy/lib/lambda/custom-deployment-config.ts
@@ -4,6 +4,8 @@ import { Duration, Names, Resource, ValidationError } from '../../../core';
 import { addConstructMetadata } from '../../../core/lib/metadata-resource';
 import { propertyInjectable } from '../../../core/lib/prop-injectable';
 import { AwsCustomResource, AwsCustomResourcePolicy, PhysicalResourceId } from '../../../custom-resources';
+import { IBindableDeploymentConfig } from '../base-deployment-config';
+import { DeploymentConfigReference, IDeploymentConfigRef, IDeploymentGroupRef } from '../codedeploy.generated';
 import { arnForDeploymentConfig, validateName } from '../private/utils';
 
 /**
@@ -67,7 +69,7 @@ export interface CustomLambdaDeploymentConfigProps {
  * @deprecated CloudFormation now supports Lambda deployment configurations without custom resources. Use `LambdaDeploymentConfig`.
  */
 @propertyInjectable
-export class CustomLambdaDeploymentConfig extends Resource implements ILambdaDeploymentConfig {
+export class CustomLambdaDeploymentConfig extends Resource implements ILambdaDeploymentConfig, IBindableDeploymentConfig {
   /** Uniquely identifies this class. */
   public static readonly PROPERTY_INJECTION_ID: string = 'aws-cdk-lib.aws-codedeploy.CustomLambdaDeploymentConfig';
   /**
@@ -83,6 +85,12 @@ export class CustomLambdaDeploymentConfig extends Resource implements ILambdaDep
    * @deprecated Use `LambdaDeploymentConfig`
    */
   public readonly deploymentConfigArn: string;
+
+  public get deploymentConfigRef(): DeploymentConfigReference {
+    return {
+      deploymentConfigName: this.deploymentConfigName,
+    };
+  }
 
   public constructor(scope: Construct, id: string, props: CustomLambdaDeploymentConfigProps) {
     super(scope, id);
@@ -162,6 +170,13 @@ export class CustomLambdaDeploymentConfig extends Resource implements ILambdaDep
     });
 
     this.node.addValidation({ validate: () => validateName('Deployment config', this.deploymentConfigName) });
+  }
+
+  public bindEnvironment(deploymentGroup: IDeploymentGroupRef): IDeploymentConfigRef {
+    // This construct creates a deployment config by side effect, so we should add a dependency
+    // to make sure this construct gets executed first.
+    deploymentGroup.node.addDependency(this);
+    return this;
   }
 
   // Validate the inputs. The percentage/interval limits come from CodeDeploy
