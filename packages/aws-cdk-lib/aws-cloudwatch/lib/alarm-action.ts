aad64142e5a61a9ae4c1e5e8652db0e34bec1a62
Rico Huijbers <rix0rrr@gmail.com>
2026-01-15 14:27:34 +0100

    Ignore in other place

--- packages/aws-cdk-lib/aws-cloudwatch/lib/alarm-action.ts
+++ packages/aws-cdk-lib/aws-cloudwatch/lib/alarm-action.ts
@@ -8,10 +8,8 @@ export interface IAlarmAction {
   /**
    * Return the properties required to send alarm actions to this CloudWatch alarm.
    *
-   * [disable-awslint:prefer-ref-interface]
-   *
    * @param scope root Construct that allows creating new Constructs
-   * @param alarm CloudWatch alarm that the action will target
+   * @param alarm CloudWatch alarm that the action will target [disable-awslint:prefer-ref-interface]
    */
   bind(scope: Construct, alarm: IAlarm): AlarmActionConfig;
 }
