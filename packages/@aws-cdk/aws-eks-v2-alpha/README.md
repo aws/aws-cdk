0cd1740b85d28b65480e510ff783250da0d96ff2
letsgomeow <h-hirano@nri.co.jp>
2026-01-24 17:28:48 +0900

    WIP

--- packages/@aws-cdk/aws-eks-v2-alpha/README.md
+++ packages/@aws-cdk/aws-eks-v2-alpha/README.md
@@ -1161,6 +1161,7 @@ const loadBalancerAddress = cluster.getServiceLoadBalancerAddress('my-service');
 
 ```ts
 declare const cluster: eks.Cluster;
+declare const role: iam.IRole;
 
 new eks.Addon(this, 'Addon', {
   cluster,
@@ -1171,6 +1172,14 @@ new eks.Addon(this, 'Addon', {
   configurationValues: {
     replicaCount: 2,
   },
+  namespace: 'kube-system',
+  podIdentityAssociations: [{
+    addonRole: role,
+    serviceAccount: 'coredns',
+  }],
+  resolveConflicts: eks.ResolveConflictsType.NONE,
+  // Add-ons support IAM via EKS Pod Identity or IRSA. While Pod Identity is recommended, please select and implement one.
+  // serviceAccountRole: role,
 });
 ```
 
--- packages/@aws-cdk/aws-eks-v2-alpha/test/integ.eks-addon.ts
+++ packages/@aws-cdk/aws-eks-v2-alpha/test/integ.eks-addon.ts
@@ -1,6 +1,6 @@
 import * as integ from '@aws-cdk/integ-tests-alpha';
 import { KubectlV33Layer } from '@aws-cdk/lambda-layer-kubectl-v33';
-import { App, Stack } from 'aws-cdk-lib';
+import { App, Stack, Fn } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as eks from '../lib';
@@ -8,10 +8,6 @@ import * as eks from '../lib';
 class EksClusterStack extends Stack {
   constructor(scope: App, id: string) {
     super(scope, id);
-
-    const testRole = new iam.Role(this, 'TestRole', {
-      assumedBy: new iam.ServicePrincipal('pods.eks.amazonaws.com'),
-    });
 
     const vpc = new ec2.Vpc(this, 'Vpc', { natGateways: 1 });
     const cluster = new eks.Cluster(this, 'Cluster', {
@@ -25,15 +21,54 @@ class EksClusterStack extends Stack {
     new eks.Addon(this, 'Addon', {
       addonName: 'coredns',
       cluster,
-      preserveOnDelete: false,
+      preserveOnDelete: true,
+      configurationValues: {
+        replicaCount: 2,
+      },
+    });
+
+    // test for additional Addon parameters(namespace, podIdentityAssociations, resolveConflicts)
+    const testRole = new iam.Role(this, 'TestRole', {
+      assumedBy: new iam.ServicePrincipal('pods.eks.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonEBSCSIDriverPolicy'),
+      ],
+    });
+
+    new eks.Addon(this, 'AddonPodIdentityAssociation', {
+      addonName: 'aws-ebs-csi-driver',
+      cluster,
+      preserveOnDelete: true,
       configurationValues: {
         replicaCount: 2,
       },
       namespace: 'kube-system',
       podIdentityAssociations: [{
         addonRole: testRole,
-        serviceAccount: 'coredns',
+        serviceAccount: 'ebs-csi-controller-sa',
       }],
+      resolveConflicts: eks.ResolveConflictsType.NONE,
+    });
+
+    // test for additional Addon parameters
+    const irsaRole = new iam.Role(this, 'IrsaTestRole', {
+      assumedBy: new iam.FederatedPrincipal(
+        cluster.openIdConnectProvider.openIdConnectProviderArn,
+        {
+          StringEquals: Fn.toJsonString({
+            [`${cluster.openIdConnectProvider.openIdConnectProviderIssuer}:sub`]: 'system:serviceaccount:kube-system:snapshot-controller',
+          }),
+        },
+        'sts:AssumeRoleWithWebIdentity',
+      ),
+    });
+    irsaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonEBSCSIDriverPolicy'),
+    );
+    new eks.Addon(this, 'AddonServiceAccountRole', {
+      addonName: 'aws-snapshot-controller',
+      cluster,
+      serviceAccountRole: irsaRole,
     });
   }
 }
