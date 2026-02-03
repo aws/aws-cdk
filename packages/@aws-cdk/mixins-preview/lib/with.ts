f2cc9c9f7290b073dc746e86b75de9f099a3025a
Momo Kornher <kornherm@amazon.co.uk>
2026-02-03 14:00:58 +0000

    fix(mixins-preview): apply mixins in order (#36847)
    ### Issue # (if applicable)
    
    Related RFC: https://github.com/aws/aws-cdk-rfcs/pull/824
    
    ### Reason for this change
    
    Aligns the implementation with the latest version of the [Mixins RFC](https://github.com/aws/aws-cdk-rfcs/pull/824).
    
    The `.with()` method previously iterated over constructs first, then mixins. This meant that mixins were not guaranteed to be applied in the order they were passed to the method. Additionally, if a mixin added new constructs to the tree, subsequent mixins in the same call could unexpectedly visit those newly added constructs.
    
    ### Description of changes
    
    Changed the loop order so that mixins are applied in order. The list of constructs is now captured at the start of the call, ensuring that constructs added by a mixin will not be visited by subsequent mixins. Users who need subsequent mixins to apply to added constructs can use multiple `.with()` calls.
    
    Also improved variable naming for clarity (`c` → `construct`, `m` → `mixin`).
    
    ### Describe any new or updated permissions being added
    
    N/A
    
    ### Description of how you validated changes
    
    Existing tests pass. This behavior is explicitly tested in https://github.com/aws/constructs/pull/2843.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/mixins-preview/lib/with.ts
+++ packages/@aws-cdk/mixins-preview/lib/with.ts
@@ -14,11 +14,18 @@ declare module 'constructs' {
 }
 
 // Hack the prototype to add .with() method
-(Construct.prototype as any).with = function(this: IConstruct, ...mixin: IMixin[]): IConstruct {
-  for (const c of this.node.findAll()) {
-    for (const m of mixin) {
-      if (m.supports(c)) {
-        applyMixin(c, m);
+// Changed the loop order so that mixins are applied in order.
+// The list of constructs is now captured at the start of the call,
+// ensuring that constructs added by a mixin will not be visited by subsequent mixins.
+//
+// This code is a hard copy of code from this PR https://github.com/aws/constructs/pull/2843
+// and is intended to be removed once the code is available in upstream.
+(Construct.prototype as any).with = function(this: IConstruct, ...mixins: IMixin[]): IConstruct {
+  const allConstructs = this.node.findAll();
+  for (const mixin of mixins) {
+    for (const construct of allConstructs) {
+      if (mixin.supports(construct)) {
+        applyMixin(construct, mixin);
       }
     }
   }
