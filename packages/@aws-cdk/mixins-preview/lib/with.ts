6b118f68e334cc95ffcda60d4a66956fab05cb9a
Momo Kornher <kornherm@amazon.co.uk>
2026-02-03 13:18:37 +0000

    add comment

--- packages/@aws-cdk/mixins-preview/lib/with.ts
+++ packages/@aws-cdk/mixins-preview/lib/with.ts
@@ -14,6 +14,12 @@ declare module 'constructs' {
 }
 
 // Hack the prototype to add .with() method
+// Changed the loop order so that mixins are applied in order.
+// The list of constructs is now captured at the start of the call,
+// ensuring that constructs added by a mixin will not be visited by subsequent mixins.
+//
+// This code is a hard copy of code from this PR https://github.com/aws/constructs/pull/2843
+// and is intended to be removed once the code is available in upstream.
 (Construct.prototype as any).with = function(this: IConstruct, ...mixins: IMixin[]): IConstruct {
   const allConstructs = this.node.findAll();
   for (const mixin of mixins) {
