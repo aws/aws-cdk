037e40a750b819e7422e087ddbcb4168298051d3
Ahmed Hamed <aemada@amazon.com>
2026-02-05 17:26:16 +0100

    remove '1' suffix'

--- packages/@aws-cdk/aws-apprunner-alpha/test/integ.service-vpc-connector.ts
+++ packages/@aws-cdk/aws-apprunner-alpha/test/integ.service-vpc-connector.ts
@@ -5,37 +5,36 @@ import { Service, Source, VpcConnector } from '../lib';
 
 const app = new cdk.App();
 
-// Stack 1: Service with newly created VPC Connector
-const stack1 = new cdk.Stack(app, 'integ-apprunner-vpc-connector-new');
+const stack = new cdk.Stack(app, 'integ-apprunner-vpc-connector-new');
 
-const vpc1 = new ec2.Vpc(stack1, 'Vpc', {
+const vpc = new ec2.Vpc(stack, 'Vpc', {
   restrictDefaultSecurityGroup: false,
   ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
 });
 
-const securityGroup1 = new ec2.SecurityGroup(stack1, 'SecurityGroup', { vpc: vpc1 });
+const securityGroup = new ec2.SecurityGroup(stack, 'SecurityGroup', { vpc });
 
-const vpcConnector1 = new VpcConnector(stack1, 'VpcConnector', {
-  vpc: vpc1,
-  vpcSubnets: vpc1.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
-  securityGroups: [securityGroup1],
+const vpcConnector = new VpcConnector(stack, 'VpcConnector', {
+  vpc,
+  vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
+  securityGroups: [securityGroup],
   vpcConnectorName: 'MyVpcConnector',
 });
 
-const service1 = new Service(stack1, 'Service', {
+const service = new Service(stack, 'Service', {
   source: Source.fromEcrPublic({
     imageConfiguration: {
       port: 8000,
     },
     imageIdentifier: 'public.ecr.aws/aws-containers/hello-app-runner:latest',
   }),
-  vpcConnector: vpcConnector1,
+  vpcConnector: vpcConnector,
 });
-new cdk.CfnOutput(stack1, 'ServiceURL', { value: `https://${service1.serviceUrl}` });
+new cdk.CfnOutput(stack, 'ServiceURL', { value: `https://${service.serviceUrl}` });
 
 // AppRunner is only available in specific regions
 new integ.IntegTest(app, 'AppRunnerVpcConnector', {
-  testCases: [stack1],
+  testCases: [stack],
   regions: ['ap-northeast-1', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'us-east-1', 'us-east-2', 'us-west-2'],
 });
 
