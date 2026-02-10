e354a064762a4565072fb4572ca90883905d3827
aemada-aws <157400220+aemada-aws@users.noreply.github.com>
2026-02-10 19:08:15 +0200

    chore(redshift-alpha): remove hardcoded account ids and deprecated node types from integ tests (#36842)
    ### Issue # (if applicable)
    
    Closes #<issue number here>.
    
    ### Reason for this change
    
    
    
    ### Description of changes
    
    This commit fixes 15 out of 15 failing Redshift integration tests by removing
    hardcoded account IDs and regions and making the tests region-agnostic.
    
    **Root Cause:**
    All tests were hardcoding `account: '123456789012'` and `region: 'us-east-1'`
    in their stack definitions, along with hardcoded availability zone context.
    
    Some tests were also using a deprecated node type (dc2.large).
    
    ### Describe any new or updated permissions being added
    
    None
    
    ### Description of how you validated changes
    
    Integ tests deployed
    
    ### Checklist
    - [X] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/@aws-cdk/aws-redshift-alpha/test/integ.cluster-iamrole.ts
+++ packages/@aws-cdk/aws-redshift-alpha/test/integ.cluster-iamrole.ts
@@ -74,17 +74,9 @@ const app = new App({
     '@aws-cdk/aws-lambda:createNewPoliciesWithAddToRolePolicy': true,
     '@aws-cdk/aws-lambda:useCdkManagedLogGroup': false,
   },
-  context: {
-    'availability-zones:account=123456789012:region=us-east-1': ['us-east-1a', 'us-east-1b', 'us-east-1c'],
-  },
 });
 
-const stack = new Stack(app, 'aws-cdk-redshift-cluster-database', {
-  env: {
-    account: '123456789012',
-    region: 'us-east-1',
-  },
-});
+const stack = new Stack(app, 'aws-cdk-redshift-cluster-database');
 
 const singleProviderRoleTestStack = new SingleProviderRoleStack(stack, 'single-provider-role-integ');
 
