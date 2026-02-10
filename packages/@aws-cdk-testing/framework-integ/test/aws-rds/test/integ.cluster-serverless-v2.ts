cf33788f24d3e9fcc915ad5b02488225952a049b
Ahmed Hamed <aemada@amazon.com>
2026-02-10 12:14:25 +0000

    rewrite byol oracle test

--- packages/@aws-cdk-testing/framework-integ/test/aws-rds/test/integ.cluster-serverless-v2.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-rds/test/integ.cluster-serverless-v2.ts
@@ -1,24 +1,73 @@
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { INTEG_TEST_LATEST_AURORA_MYSQL } from './db-versions';
-import * as cdk from 'aws-cdk-lib';
-import { IntegTestBaseStack } from './integ-test-base-stack';
-import * as integ from '@aws-cdk/integ-tests-alpha';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { App, Duration } from 'aws-cdk-lib';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
+import { ClusterInstance } from 'aws-cdk-lib/aws-rds';
+import { Construct } from 'constructs';
+import { INTEG_TEST_LATEST_AURORA_MYSQL } from './db-versions';
+import { IntegTestBaseStack } from './integ-test-base-stack';
 
-const app = new cdk.App();
+interface TestCaseProps extends Pick<rds.DatabaseClusterProps, 'writer' | 'readers' | 'vpc'> {
+}
 
-const stack = new IntegTestBaseStack(app, 'integ-aurora-serverlessv2-cluster');
+class TestCase extends Construct {
+  constructor(scope: Construct, id: string, props: TestCaseProps) {
+    super(scope, id);
+    const cluster = new rds.DatabaseCluster(this, 'Integ-Cluster', {
+      engine: rds.DatabaseClusterEngine.auroraMysql({ version: INTEG_TEST_LATEST_AURORA_MYSQL }),
+      writer: props.writer,
+      readers: props.readers,
+      vpc: props.vpc,
+    });
+    cluster.metricServerlessDatabaseCapacity({
+      period: Duration.minutes(10),
+    }).createAlarm(this, 'capacity', {
+      threshold: 1.5,
+      evaluationPeriods: 3,
+    });
+    cluster.metricACUUtilization({
+      period: Duration.minutes(10),
+    }).createAlarm(this, 'alarm', {
+      evaluationPeriods: 3,
+      threshold: 90,
+    });
+  }
+}
 
-const vpc = new ec2.Vpc(stack, 'Vpc', { maxAzs: 2, restrictDefaultSecurityGroup: false });
+const testCases: TestCaseProps[] = [
+  {
+    writer: ClusterInstance.serverlessV2('writer'),
+  },
+  {
+    writer: ClusterInstance.provisioned('writer'),
+    readers: [
+      ClusterInstance.serverlessV2('FailoverReader', { scaleWithWriter: true }),
+      ClusterInstance.serverlessV2('OtherReader'),
+    ],
+  },
+  {
+    writer: ClusterInstance.serverlessV2('writer'),
+    readers: [
+      ClusterInstance.provisioned('FailoverReader', { promotionTier: 1 }),
+      ClusterInstance.serverlessV2('OtherReader'),
+    ],
+  },
+];
 
-new rds.DatabaseCluster(stack, 'Serverless Database', {
-  engine: rds.DatabaseClusterEngine.auroraMysql({
-    version: INTEG_TEST_LATEST_AURORA_MYSQL,
-  }),
-  writer: rds.ClusterInstance.serverlessV2('writer'),
-  vpc,
-});
+export class TestStack extends IntegTestBaseStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    const vpc = new Vpc(this, 'Integ-VPC');
+    testCases.forEach((p: TestCaseProps, i) =>
+      new TestCase(this, `integ-aurora-serverlessv2-${i}`, {
+        ...p,
+        vpc,
+      }),
+    );
+  }
+}
 
-new integ.IntegTest(app, 'integ-test', {
-  testCases: [stack],
+const app = new App();
+new IntegTest(app, 'integ-test', {
+  testCases: [new TestStack(app, 'integ-aurora-serverlessv2-cluster')],
 });
--- packages/aws-cdk-lib/aws-rds/test/instance.lit.test.ts
+++ packages/aws-cdk-lib/aws-rds/test/instance.lit.test.ts
@@ -1,3 +1,9 @@
+/**
+ * NOTE: This is a unit test instead of an integration test because we cannot use
+ * "BRING_YOUR_OWN_LICENSE" in dev accounts
+ */
+
+import { Template } from '../../assertions';
 import * as cloudwatch from '../../aws-cloudwatch';
 import * as ec2 from '../../aws-ec2';
 import * as targets from '../../aws-events-targets';
@@ -7,25 +13,19 @@ import * as cdk from '../../core';
 import { RemovalPolicy } from '../../core';
 import * as rds from '../lib';
 
-const app = new cdk.App();
+describe('database instance with BYOL', () => {
+  test('create Oracle instance with parameter group, option group, and monitoring', () => {
+    const stack = new cdk.Stack();
+    const vpc = new ec2.Vpc(stack, 'VPC', { maxAzs: 2 });
 
-class DatabaseInstanceStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    const vpc = new ec2.Vpc(this, 'VPC', { maxAzs: 2 });
-
-    /// !show
-    // Set open cursors with parameter group
-    const parameterGroup = new rds.ParameterGroup(this, 'ParameterGroup', {
+    const parameterGroup = new rds.ParameterGroup(stack, 'ParameterGroup', {
       engine: rds.DatabaseInstanceEngine.oracleSe2({ version: rds.OracleEngineVersion.VER_19_0_0_0_2020_04_R1 }),
       parameters: {
         open_cursors: '2500',
       },
     });
 
-    /// Add XMLDB and OEM with option group
-    const optionGroup = new rds.OptionGroup(this, 'OptionGroup', {
+    const optionGroup = new rds.OptionGroup(stack, 'OptionGroup', {
       engine: rds.DatabaseInstanceEngine.oracleSe2({ version: rds.OracleEngineVersion.VER_19_0_0_0_2020_04_R1 }),
       configurations: [
         {
@@ -39,11 +39,9 @@ class DatabaseInstanceStack extends cdk.Stack {
       ],
     });
 
-    // Allow connections to OEM
     optionGroup.optionConnections.OEM.connections.allowDefaultPortFromAnyIpv4();
 
-    // Database instance with production values
-    const instance = new rds.DatabaseInstance(this, 'Instance', {
+    const instance = new rds.DatabaseInstance(stack, 'Instance', {
       engine: rds.DatabaseInstanceEngine.oracleSe2({ version: rds.OracleEngineVersion.VER_19_0_0_0_2020_04_R1 }),
       licenseModel: rds.LicenseModel.BRING_YOUR_OWN_LICENSE,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM),
@@ -63,27 +61,22 @@ class DatabaseInstanceStack extends cdk.Stack {
         'listener',
       ],
       cloudwatchLogsRetention: logs.RetentionDays.ONE_MONTH,
-      autoMinorVersionUpgrade: true, // required to be true if LOCATOR is used in the option group
+      autoMinorVersionUpgrade: true,
       optionGroup,
       parameterGroup,
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    // Allow connections on default port from any IPV4
     instance.connections.allowDefaultPortFromAnyIpv4();
-
-    // Rotate the master user password every 30 days
     instance.addRotationSingleUser();
 
-    // Add alarm for high CPU
-    new cloudwatch.Alarm(this, 'HighCPU', {
+    new cloudwatch.Alarm(stack, 'HighCPU', {
       metric: instance.metricCPUUtilization(),
       threshold: 90,
       evaluationPeriods: 1,
     });
 
-    // Trigger Lambda function on instance availability events
-    const fn = new lambda.Function(this, 'Function', {
+    const fn = new lambda.Function(stack, 'Function', {
       code: lambda.Code.fromInline('exports.handler = (event) => console.log(event);'),
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_20_X,
@@ -97,9 +90,47 @@ class DatabaseInstanceStack extends cdk.Stack {
         ],
       },
     });
-    /// !hide
-  }
-}
 
-new DatabaseInstanceStack(app, 'aws-cdk-rds-instance');
-app.synth();
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::RDS::DBInstance', {
+      Engine: 'oracle-se2',
+      LicenseModel: 'bring-your-own-license',
+      DBInstanceClass: 'db.t3.medium',
+      MultiAZ: true,
+      StorageType: 'io1',
+      StorageEncrypted: true,
+      BackupRetentionPeriod: 7,
+      MonitoringInterval: 60,
+      EnablePerformanceInsights: true,
+      EnableCloudwatchLogsExports: ['trace', 'audit', 'alert', 'listener'],
+      AutoMinorVersionUpgrade: true,
+    });
+
+    template.hasResourceProperties('AWS::RDS::DBParameterGroup', {
+      Parameters: {
+        open_cursors: '2500',
+      },
+    });
+
+    template.hasResourceProperties('AWS::RDS::OptionGroup', {
+      OptionConfigurations: [
+        { OptionName: 'LOCATOR' },
+        { OptionName: 'OEM', Port: 1158 },
+      ],
+    });
+
+    template.hasResourceProperties('AWS::CloudWatch::Alarm', {
+      Threshold: 90,
+      EvaluationPeriods: 1,
+    });
+
+    template.hasResourceProperties('AWS::Events::Rule', {
+      EventPattern: {
+        detail: {
+          EventCategories: ['availability'],
+        },
+      },
+    });
+  });
+});
