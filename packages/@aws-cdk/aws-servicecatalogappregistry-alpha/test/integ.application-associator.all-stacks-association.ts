a7310816335291829b0428a2dd6a7ea60de067a0
Ahmed Hamed <aemada@amazon.com>
2026-02-11 20:36:53 +0000

    fix(servicecatalogappregistry-alpha): remediate failing integration tests
    - Give each application-associator test unique stack/application names to
      prevent collisions during parallel execution
    - Modernize all-stacks-association from legacy /// !cdk-integ pragma to
      IntegTest construct
    - Add destroy.expectError for AppRegistry tests (applications cannot be
      deleted while resources are still associated)
    - Remove shareApplication/shareAttributeGroup from integ.application and
      integ.attribute-group (requires AWS Organization; covered by unit tests)
    - Regenerate all snapshots via integ-runner --update-on-failed

--- packages/@aws-cdk/aws-servicecatalogappregistry-alpha/test/integ.application-associator.all-stacks-association.ts
+++ packages/@aws-cdk/aws-servicecatalogappregistry-alpha/test/integ.application-associator.all-stacks-association.ts
@@ -1,18 +1,30 @@
-/// !cdk-integ integ-servicecatalogappregistry-application
+import * as integ from '@aws-cdk/integ-tests-alpha';
 import * as cdk from 'aws-cdk-lib';
 import * as appreg from '../lib';
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'integ-servicecatalogappregistry-application');
+const stack = new cdk.Stack(app, 'integ-appregistry-all-stacks');
 
 new appreg.ApplicationAssociator(app, 'RegisterCdkApplication', {
   applications: [appreg.TargetApplication.createApplicationStack({
-    applicationName: 'AppRegistryAssociatedApplication',
-    stackName: 'AppRegistryApplicationAssociatorStack',
-    stackId: 'AppRegistryApplicationStack',
+    applicationName: 'AppRegistryAllStacksApp',
+    stackName: 'AppRegistryAllStacksAppStack',
   })],
 });
 
 new cdk.Stack(stack, 'resourcesStack');
+
+new integ.IntegTest(app, 'ApplicationAssociatorTest', {
+  testCases: [stack],
+  cdkCommandOptions: {
+    destroy: {
+      // ApplicationAssociator associates stacks with the AppRegistry application.
+      // On teardown, CloudFormation tries to delete the application before
+      // disassociating the stacks, causing DELETE_FAILED. This is an
+      // AppRegistry service limitation, not a test defect.
+      expectError: true,
+    },
+  },
+});
 
 app.synth();
--- packages/@aws-cdk/aws-servicecatalogappregistry-alpha/test/integ.application-associator.disable-url.ts
+++ packages/@aws-cdk/aws-servicecatalogappregistry-alpha/test/integ.application-associator.disable-url.ts
@@ -3,11 +3,12 @@ import * as cdk from 'aws-cdk-lib';
 import * as appreg from '../lib';
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'integ-servicecatalogappregistry-application');
+const stack = new cdk.Stack(app, 'integ-appregistry-disable-url');
 
 new appreg.ApplicationAssociator(app, 'RegisterCdkApplication', {
   applications: [appreg.TargetApplication.createApplicationStack({
-    applicationName: 'AppRegistryAssociatedApplication',
+    applicationName: 'AppRegistryDisableUrlApp',
+    stackName: 'AppRegistryDisableUrlAppStack',
     emitApplicationManagerUrlAsOutput: false,
   })],
 });
@@ -16,6 +17,15 @@ new cdk.Stack(stack, 'resourcesStack');
 
 new integ.IntegTest(app, 'ApplicationAssociatorTest', {
   testCases: [stack],
+  cdkCommandOptions: {
+    destroy: {
+      // ApplicationAssociator associates stacks with the AppRegistry application.
+      // On teardown, CloudFormation tries to delete the application before
+      // disassociating the stacks, causing DELETE_FAILED. This is an
+      // AppRegistry service limitation, not a test defect.
+      expectError: true,
+    },
+  },
 });
 
 app.synth();
