8376a1a412fe0fa6da318d49c77e8c68db12d351
Ahmed Hamed <aemada@amazon.com>
2026-02-11 22:14:48 +0000

    fix(bedrock-alpha): add region constraints to agent integ tests
    - Add regions constraint to 5 agent integ tests (Bedrock CFN resources
      not available in all regions)
    - Regenerate snapshots

--- packages/@aws-cdk/aws-bedrock-alpha/test/bedrock/agents/integ.orchestration.ts
+++ packages/@aws-cdk/aws-bedrock-alpha/test/bedrock/agents/integ.orchestration.ts
@@ -58,6 +58,7 @@ new bedrock.Agent(stack, 'CustomOrchestrationAgent', {
 
 new integ.IntegTest(app, 'BedrockOrchestration', {
   testCases: [stack],
+  regions: ['ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2', 'ca-central-1', 'eu-central-1', 'eu-north-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'sa-east-1', 'us-east-1', 'us-east-2', 'us-west-2'],
 });
 
 app.synth();
