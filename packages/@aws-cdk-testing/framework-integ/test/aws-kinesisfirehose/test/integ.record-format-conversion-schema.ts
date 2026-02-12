bb2790e4025afe049789a6d57c05c437e3b8784c
Ahmed Hamed <aemada@amazon.com>
2026-02-12 06:59:39 +0000

    fix(kinesis): remediate failing integration tests
    - Add Glue Schema region constraint to firehose record-format-conversion test
    - Remove deprecated Flink runtimes (1_6, 1_8, 1_11, 1_13) from flink integ test
    - Regenerate snapshots for kinesis resource-policy test (transient failure)
    - Skip: cross-account-stream-consumption (requires cross-account setup)

--- packages/@aws-cdk-testing/framework-integ/test/aws-kinesisfirehose/test/integ.record-format-conversion-schema.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-kinesisfirehose/test/integ.record-format-conversion-schema.ts
@@ -120,6 +120,7 @@ class TestStack extends cdk.Stack {
 const stack = new TestStack(app, 'RecordFormatConversionSchema');
 const testCase = new integ.IntegTest(app, 'RecordFormatConversionSchemaTest', {
   testCases: [stack],
+  regions: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-central-1', 'eu-north-1', 'ap-northeast-1', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-south-1', 'sa-east-1', 'ca-central-1'],
 });
 
 const assertions = testCase.assertions;
