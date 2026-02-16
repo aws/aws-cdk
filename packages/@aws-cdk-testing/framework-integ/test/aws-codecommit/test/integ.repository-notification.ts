ffded947cd9846d04faf504d2f44ddf9c9233780
aemada-aws <157400220+aemada-aws@users.noreply.github.com>
2026-02-16 11:41:36 +0100

    Simplify regions in codecommit notification test
    Removed redundant regions from the integration test.

--- packages/@aws-cdk-testing/framework-integ/test/aws-codecommit/test/integ.repository-notification.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-codecommit/test/integ.repository-notification.ts
@@ -19,5 +19,4 @@ repository.notifyOnPullRequestMerged('NotifyOnPullRequestMerged', target);
 
 new IntegTest(app, 'codecommit-notification-test', {
   testCases: [stack],
-  regions: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-central-1', 'eu-north-1', 'ap-northeast-1', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-south-1', 'sa-east-1', 'ca-central-1'],
 });
