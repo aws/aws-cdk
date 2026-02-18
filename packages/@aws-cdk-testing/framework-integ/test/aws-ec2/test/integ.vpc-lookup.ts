7de599bf6efe4c3992aeac45b02ff0cadc2b6b38
Ahmed Hamed <aemada@amazon.com>
2026-02-18 18:18:20 +0000

    restore lookup test

--- packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.vpc-lookup.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.vpc-lookup.ts
@@ -12,10 +12,6 @@ const stack = new cdk.Stack(appWithVpc, 'StackWithVpc', {
 });
 stack.node.setContext(EC2_RESTRICT_DEFAULT_SECURITY_GROUP, false);
 
-const testVpc = new ec2.Vpc(stack, 'MyVpc', {
-  vpcName: 'my-vpc-name',
-});
-
 const appUnderTest = new cdk.App();
 const stackLookup = new cdk.Stack(appUnderTest, 'StackUnderTest', {
   env: {
@@ -24,19 +20,9 @@ const stackLookup = new cdk.Stack(appUnderTest, 'StackUnderTest', {
   },
 });
 
-const vpcFromVpcAttributes = ec2.Vpc.fromVpcAttributes(stackLookup, 'VpcFromVpcAttributes', {
-  region: 'eu-west-1',
-  availabilityZones: ['eu-west-1a'],
-  vpcId: testVpc.vpcId,
-});
-
 const vpcFromLookup = ec2.Vpc.fromLookup(stack, 'VpcFromLookup', {
   region: 'eu-west-1',
   vpcName: 'my-vpc-name',
-});
-
-new cdk.CfnOutput(stackLookup, 'OutputFromVpcAttributes', {
-  value: `Region fromVpcAttributes: ${vpcFromVpcAttributes.env.region}`,
 });
 
 new cdk.CfnOutput(stackLookup, 'OutputFromLookup', {
