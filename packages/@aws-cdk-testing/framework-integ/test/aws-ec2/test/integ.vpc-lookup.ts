7cf4f6746d7ac0bba3af3fa56f97239cdf30cbfc
Ahmed Hamed <aemada@amazon.com>
2026-02-19 11:16:57 +0000

    use fallback for pr build

--- packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.vpc-lookup.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-ec2/test/integ.vpc-lookup.ts
@@ -6,8 +6,8 @@ import { EC2_RESTRICT_DEFAULT_SECURITY_GROUP } from 'aws-cdk-lib/cx-api';
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'StackUnderTest', {
   env: {
-    account: process.env.AWS_ACCOUNT_ID ?? process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.AWS_REGION ?? process.env.CDK_DEFAULT_REGION,
+    account: process.env.AWS_ACCOUNT_ID ?? process.env.CDK_DEFAULT_ACCOUNT ?? 123456789012,
+    region: process.env.AWS_REGION ?? process.env.CDK_DEFAULT_REGION. ?? "us-east-1",
   },
 });
 stack.node.setContext(EC2_RESTRICT_DEFAULT_SECURITY_GROUP, false);
