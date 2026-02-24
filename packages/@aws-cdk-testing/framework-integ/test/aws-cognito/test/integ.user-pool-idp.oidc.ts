72e50d6c93f621cf5d3104a529cb89b4730e5a48
Miguel Merheb <miguel.r.merheb@gmail.com>
2026-02-24 20:26:02 +0000

    Update integration tests

--- packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-idp.oidc.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-cognito/test/integ.user-pool-idp.oidc.ts
@@ -1,3 +1,4 @@
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { App, CfnOutput, RemovalPolicy, Stack } from 'aws-cdk-lib';
 import { ProviderAttribute, UserPool, UserPoolIdentityProviderOidc } from 'aws-cdk-lib/aws-cognito';
 
@@ -5,16 +6,48 @@ import { ProviderAttribute, UserPool, UserPoolIdentityProviderOidc } from 'aws-c
  * Stack verification steps
  * * Visit the URL provided by stack output 'SignInLink' in a browser, and verify the 'cdk' sign in link shows up.
  */
+
 const app = new App();
-const stack = new Stack(app, 'integ-user-pool-idp-google');
+const stack = new Stack(app, 'integ-user-pool-idp-oidc');
 
 const userpool = new UserPool(stack, 'pool', {
   removalPolicy: RemovalPolicy.DESTROY,
 });
 
-new UserPoolIdentityProviderOidc(stack, 'cdk', {
+const secret = new Secret(stack, 'OidcClientSecretValue', {
+  secretName: 'OidcClientSecretValueName',
+  generateSecretString: {
+    excludePunctuation: true,
+    passwordLength: 20,
+  },
+});
+
+const clientSecret = Secret.fromSecretAttributes(stack, 'OidcClientSecretResolvedValue', {
+  secretCompleteArn: secret.secretArn,
+}).secretValue;
+
+new UserPoolIdentityProviderOidc(stack, 'provider-with-secret-value', {
   userPool: userpool,
-  name: 'cdk',
+  name: 'provider-with-secret-value',
+  clientId: 'client-id',
+  clientSecretValue: clientSecret,
+  issuerUrl: 'https://www.issuer-url.com',
+  endpoints: {
+    authorization: 'https://www.issuer-url.com/authorize',
+    token: 'https://www.issuer-url.com/token',
+    userInfo: 'https://www.issuer-url.com/userinfo',
+    jwksUri: 'https://www.issuer-url.com/jwks',
+  },
+  scopes: ['openid', 'phone'],
+  attributeMapping: {
+    phoneNumber: ProviderAttribute.other('phone_number'),
+    emailVerified: ProviderAttribute.other('email_verified'),
+  },
+});
+
+new UserPoolIdentityProviderOidc(stack, 'provider-with-secret-string', {
+  userPool: userpool,
+  name: 'provider-with-secret-string',
   clientId: 'client-id',
   clientSecret: 'client-secret',
   issuerUrl: 'https://www.issuer-url.com',
