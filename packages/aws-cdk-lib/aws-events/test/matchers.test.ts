8e7b75286efa882e550304d6e56f2b2dbaecd5e1
A. Abdelrahman <abdlrmn@amazon.de>
2026-03-04 16:07:34 +0100

    chore: migrate away from NodeJS 18 (#37095)
    ### Reason for this change
    
    Support for NodeJS 18 has officially ended back in November. 
    
    ### Description of changes
    
    This change replaces all references to the appropriate NodeJS version. This includes changes in the docs, unit tests and integration tests.
    
    The rationale for different NODEJS versions used is as followed:
    
    - For the docs, we want to encourage users to use the latest NodeJS version at any point in time, hence the change in the docs for NodeJS version is to set `NODEJS_LATEST`.
    - For default runtimes used within deployed constructs themselves, we expect them to be deployed in any region. Currently the NodeJS version supported in all region is set by NODEJS_LATEST, but some enums don't have the `NODEJS_LATEST` property. So at the moment, we will set those enum values to the actual mapped value at the time, which is NODEJS_22_X.
    - For integration tests, this is only used internally by contributors or developers. The version set is the latest NodeJS version available, NodeJS 24. We would like to use the latest LTS NodeJS version as much as possible. Setting to `NODEJS_LATEST` will force unneeded snapshot updates whenever it is updated. Furthermore, NodeJS 24 is currently newer than NODEJS_LATEST, so setting it as NODEJS_24 would make the integ test snapshot last a lot longer without re-updating.
    
    ### Describe any new or updated permissions being added
    
    No new permissions are added.
    
    ### Description of how you validated changes
    
    Build works. Snapshots are dry-runned and would need the integ test workflow to verify.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*
    
    BREAKING CHANGE: Node.js 20 or later is now required. Node.js 18 support ended in April 2025.

--- packages/aws-cdk-lib/aws-events/test/matchers.test.ts
+++ packages/aws-cdk-lib/aws-events/test/matchers.test.ts
@@ -265,7 +265,7 @@ describe('Match with Tokens', () => {
     bucket = new Bucket(stack, 'Bucket');
 
     lambdaFunction = new Function(stack, 'Function', {
-      runtime: Runtime.NODEJS_18_X,
+      runtime: Runtime.NODEJS_LATEST,
       handler: 'index.handler',
       code: Code.fromInline(`
         exports.handler = async (event) => {
