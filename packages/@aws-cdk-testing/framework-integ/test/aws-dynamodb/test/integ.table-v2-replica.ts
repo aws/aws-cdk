e6993aa9d80fe3971cc5b01b050905f35e44dc16
Ahmed Hamed <aemada@amazon.com>
2026-03-06 12:14:06 +0000

    fix: address review comments - use CDK_DEFAULT_REGION/ACCOUNT env vars

--- packages/@aws-cdk-testing/framework-integ/test/aws-dynamodb/test/integ.table-v2-replica.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-dynamodb/test/integ.table-v2-replica.ts
@@ -43,7 +43,7 @@ const app = new App({
 
 const integTest = new IntegTest(app, 'aws-cdk-global-table-replica-integ', {
   // Global tables with replicas require a region-aware stack
-  testCases: [new TestStack(app, 'BarStack', { env: { region: process.env.AWS_REGION || 'us-east-1', account: process.env.AWS_ACCOUNT_ID } })],
+  testCases: [new TestStack(app, 'BarStack', { env: { region: process.env.CDK_DEFAULT_REGION || 'us-east-1', account: process.env.CDK_DEFAULT_ACCOUNT } })],
   stackUpdateWorkflow: false,
 });
 
