4f5f60bfdc9b881880672f15203a1c26a300d084
Rico Huijbers <rix0rrr@gmail.com>
2026-03-06 16:58:15 +0100

    Undo this change

--- packages/aws-cdk-lib/aws-s3-assets/test/integ.assets.file.lit.ts
+++ packages/aws-cdk-lib/aws-s3-assets/test/integ.assets.file.lit.ts
@@ -7,27 +7,20 @@ class TestStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const user = new iam.User(this, 'MyUser');
-
     /// !show
-    for (let i = 0; i < 1000; i++) {
-      const asset = new assets.Asset(this, `SampleAsset${i}`, {
-        path: path.join(__dirname, 'file-asset.txt'),
+    const asset = new assets.Asset(this, 'SampleAsset', {
+      path: path.join(__dirname, 'file-asset.txt'),
 
-        // Optional: describe the purpose of the asset with a human-readable string
-        displayName: 'My file',
-      });
-      /// !hide
+      // Optional: describe the purpose of the asset with a human-readable string
+      displayName: 'My file',
+    });
+    /// !hide
 
-      asset.grantRead(user);
-    }
+    const user = new iam.User(this, 'MyUser');
+    asset.grantRead(user);
   }
 }
 
 const app = new cdk.App();
 new TestStack(app, 'aws-cdk-asset-file-test');
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-require('v8').writeHeapSnapshot();
-
 app.synth();
