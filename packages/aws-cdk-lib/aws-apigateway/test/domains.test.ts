be0549d0ef2ff67300dbc209b61943c43b737842
joachim.spalink <joachim.spalink@mimacom.com>
2026-03-09 10:19:17 +0100

    remove changed test

--- packages/aws-cdk-lib/aws-apigateway/test/domains.test.ts
+++ packages/aws-cdk-lib/aws-apigateway/test/domains.test.ts
@@ -72,14 +72,13 @@ describe('domains', () => {
     new apigw.DomainName(stack, 'my-domain', {
       domainName: 'old.example.com',
       certificate: cert,
-      securityPolicy: apigw.SecurityPolicy.TLS_1_2,
+      securityPolicy: apigw.SecurityPolicy.TLS_1_0,
     });
 
     new apigw.DomainName(stack, 'your-domain', {
       domainName: 'new.example.com',
       certificate: cert,
-      securityPolicy: apigw.SecurityPolicy.TLS13_1_3_2025_09,
-      endpointAccessMode: apigw.EndpointAccessMode.STRICT,
+      securityPolicy: apigw.SecurityPolicy.TLS_1_2,
     });
 
     new apigw.DomainName(stack, 'default-domain', {
@@ -92,14 +91,14 @@ describe('domains', () => {
       'DomainName': 'old.example.com',
       'EndpointConfiguration': { 'Types': ['REGIONAL'] },
       'RegionalCertificateArn': { 'Ref': 'Cert5C9FAEC1' },
-      'SecurityPolicy': 'TLS_1_2',
+      'SecurityPolicy': 'TLS_1_0',
     });
 
     Template.fromStack(stack).hasResourceProperties('AWS::ApiGateway::DomainName', {
       'DomainName': 'new.example.com',
       'EndpointConfiguration': { 'Types': ['REGIONAL'] },
       'RegionalCertificateArn': { 'Ref': 'Cert5C9FAEC1' },
-      'SecurityPolicy': 'SecurityPolicy_TLS13_1_3_2025_09',
+      'SecurityPolicy': 'TLS_1_2',
     });
 
     Template.fromStack(stack).hasResourceProperties('AWS::ApiGateway::DomainName', {
