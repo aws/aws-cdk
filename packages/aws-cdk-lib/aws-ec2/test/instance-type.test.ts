2c01ddb6429e07fb11ef43ef234742d261ebdc79
syukawa-gh <maguro470@gmail.com>
2026-03-09 11:10:01 +0900

    Update instance-type.test.ts
    feat(ec2): add C8a (InstanceClass.C8A) to available instance classes

--- packages/aws-cdk-lib/aws-ec2/test/instance-type.test.ts
+++ packages/aws-cdk-lib/aws-ec2/test/instance-type.test.ts
@@ -5,4 +5,14 @@ describe('InstanceType', () => {
     const instanceType = InstanceType.of(InstanceClass.MAC2_M1ULTRA, InstanceSize.METAL);
     expect(instanceType.toString()).toEqual('mac2-m1ultra.metal');
   });
+
+  test('c8a', () => {
+    const instanceType = InstanceType.of(InstanceClass.C8A, InstanceSize.XLARGE2);
+    expect(instanceType.toString()).toEqual('c8a.2xlarge');
+  });
+
+  test('compute8-amd', () => {
+    const instanceType = InstanceType.of(InstanceClass.COMPUTE8_AMD, InstanceSize.XLARGE2);
+    expect(instanceType.toString()).toEqual('c8a.2xlarge');
+  });
 });
