3707ca66f07e91a8b39b07e68e8d4ebefd381ca8
Saurav Sharma <saurav61091nra@gmail.com>
2026-03-13 18:15:47 +0530

    test(secretsmanager): add integration test for grantRead with KMS key
    Add integration test verifying that grantRead on a secret with a KMS
    encryption key correctly adds kms:Decrypt to the grantee's IAM policy.

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-secretsmanager/test/integ.secret-grant-read-kms.ts
@@ -0,0 +1,40 @@
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as kms from 'aws-cdk-lib/aws-kms';
+import * as cdk from 'aws-cdk-lib';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as integ from '@aws-cdk/integ-tests-alpha';
+
+class SecretGrantReadKmsStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string) {
+    super(scope, id);
+
+    // Create a KMS key
+    const key = new kms.Key(this, 'Key', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Create a secret encrypted with the KMS key
+    const secret = new secretsmanager.Secret(this, 'Secret', {
+      encryptionKey: key,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Create a role and grant it read access to the secret
+    const role = new iam.Role(this, 'TestRole', {
+      assumedBy: new iam.AccountRootPrincipal(),
+    });
+
+    // grantRead should also grant kms:Decrypt directly to the role
+    secret.grantRead(role);
+  }
+}
+
+const app = new cdk.App();
+
+const stack = new SecretGrantReadKmsStack(app, 'cdk-integ-secret-grant-read-kms');
+
+new integ.IntegTest(app, 'cdk-integ-secret-grant-read-kms-test', {
+  testCases: [stack],
+});
+
+app.synth();
