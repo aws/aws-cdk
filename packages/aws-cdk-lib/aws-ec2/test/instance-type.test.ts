066022803100c6842c5a1406382f739016cbde78
Shuto Yukawa <syukawa@amazon.com>
2026-03-30 08:59:04 +0900

    feat(ec2): add README example and unit tests for C8A instance class
    Add C8A (AMD EPYC 4th gen) usage example to the Instances section of
    the README and add unit tests verifying InstanceClass.C8A and
    InstanceClass.COMPUTE8_AMD produce the correct instance type strings.

--- packages/aws-cdk-lib/aws-ec2/test/instance-type.test.ts
+++ packages/aws-cdk-lib/aws-ec2/test/instance-type.test.ts
@@ -5,4 +5,14 @@ describe('InstanceType', () => {
     const instanceType = InstanceType.of(InstanceClass.MAC2_M1ULTRA, InstanceSize.METAL);
     expect(instanceType.toString()).toEqual('mac2-m1ultra.metal');
   });
+
+  test('c8a instance class', () => {
+    const instanceType = InstanceType.of(InstanceClass.C8A, InstanceSize.LARGE);
+    expect(instanceType.toString()).toEqual('c8a.large');
+  });
+
+  test('COMPUTE8_AMD instance class', () => {
+    const instanceType = InstanceType.of(InstanceClass.COMPUTE8_AMD, InstanceSize.XLARGE);
+    expect(instanceType.toString()).toEqual('c8a.xlarge');
+  });
 });
