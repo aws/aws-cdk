2ed7eedecbef0569ab93aa29e22fe24940a483e8
Rico Huijbers <rix0rrr@gmail.com>
2026-03-30 16:39:50 +0200

    fix(aws-cdk-lib): construct error stack trace hides namespaced package name
    We take efforts to condense a construct library stack trace to make it
    more comprehensible. This fixes two issues:
    
    - Only the first part of a namespaced package name would be captured
    - The stack trace looks confusing if it's too short to contain user
      code.
    
    Old:
    
    ```
        ...aws-cdk-lib, @aws-cdk...
    ```
    
    New:
    
    ```
        ...aws-cdk-lib, @aws-cdk/aws-lambda-python-alpha...
        (no user code in 10 frames, use --stack-trace-limit to capture more)
    ```

---
+++ packages/aws-cdk-lib/core/test/stack-trace.test.ts
@@ -0,0 +1,72 @@
+import { renderCallStackJustMyCode } from '../lib';
+
+describe('renderCallStackJustMyCode', () => {
+  test('renders namespaced packages correctly', () => {
+    const stack: Parameters<typeof renderCallStackJustMyCode>[0] = [
+      {
+        fileName: '/path/to/project/node_modules/aws-cdk-lib/core/lib/asset-staging.js',
+        functionName: 'AssetStaging.bundle',
+        sourceLocation: '2:512',
+      },
+      {
+        fileName: '/path/to/project/node_modules/aws-cdk-lib/aws-lambda/lib/function.js',
+        functionName: 'new Function2',
+        sourceLocation: '1:16236',
+      },
+      {
+        fileName: '/path/to/project/node_modules/aws-cdk-lib/core/lib/prop-injectable.js',
+        functionName: 'new Function2',
+        sourceLocation: '1:726',
+      },
+      {
+        fileName:
+          '/path/to/project/node_modules/@aws-cdk/aws-lambda-python-alpha/lib/function.ts',
+        functionName: 'new PythonFunction',
+        sourceLocation: '70:5',
+      },
+      {
+        fileName:
+          '/path/to/project/myfile.js',
+        functionName: 'new PythonFunction',
+        sourceLocation: '70:5',
+      },
+    ];
+
+    expect(renderCallStackJustMyCode(stack, false)).toEqual([
+      '...aws-cdk-lib, new PythonFunction in @aws-cdk/aws-lambda-python-alpha...',
+      'new PythonFunction (/path/to/project/myfile.js:70:5)',
+    ]);
+  });
+
+  test('adds a hint if there are not enough stack frames to see Just My Code', () => {
+    const stack: Parameters<typeof renderCallStackJustMyCode>[0] = [
+      {
+        fileName: '/path/to/project/node_modules/aws-cdk-lib/core/lib/asset-staging.js',
+        functionName: 'AssetStaging.bundle',
+        sourceLocation: '2:512',
+      },
+      {
+        fileName: '/path/to/project/node_modules/aws-cdk-lib/aws-lambda/lib/function.js',
+        functionName: 'new Function2',
+        sourceLocation: '1:16236',
+      },
+      {
+        fileName: '/path/to/project/node_modules/aws-cdk-lib/core/lib/prop-injectable.js',
+        functionName: 'new Function2',
+        sourceLocation: '1:726',
+      },
+      {
+        fileName:
+          '/path/to/project/node_modules/@aws-cdk/aws-lambda-python-alpha/lib/function.ts',
+        functionName: 'new PythonFunction',
+        sourceLocation: '70:5',
+      },
+    ];
+
+    expect(renderCallStackJustMyCode(stack, false)).toEqual([
+      '...aws-cdk-lib, new PythonFunction in @aws-cdk/aws-lambda-python-alpha...',
+      'new PythonFunction (/path/to/project/myfile.js:70:5)',
+      expect.stringContaining('use --stack-trace-limit to capture more'),
+    ]);
+  });
+});
