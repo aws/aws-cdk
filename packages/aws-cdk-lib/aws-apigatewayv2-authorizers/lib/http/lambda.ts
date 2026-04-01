2fb2f1650e957979e5ebf8292df3a95d41baa4ff
Elias Brange <elias.brange@gmail.com>
2026-04-01 12:57:37 +0200

    feat(apigatewayv2): add role support for lambda authorizers (#35706)
    ### Issue # (if applicable)
    
    Closes #35696.
    
    ### Reason for this change
    
    Added support for specifying the role to use to invoke an authorizer.
    
    ### Description of changes
    
    Added an optional `role: iam.IRole` to relevant interfaces and passing it down to the L1 construct.
    
    ### Description of how you validated changes
    
    Added unit tests and updated the integration test.
    
    ### Checklist
    - [x] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/aws-apigatewayv2-authorizers/lib/http/lambda.ts
+++ packages/aws-cdk-lib/aws-apigatewayv2-authorizers/lib/http/lambda.ts
@@ -9,6 +9,7 @@ import {
   HttpAuthorizerType,
   AuthorizerPayloadVersion,
 } from '../../../aws-apigatewayv2';
+import type { IRoleRef } from '../../../aws-iam';
 import { ServicePrincipal } from '../../../aws-iam';
 import type { IFunction } from '../../../aws-lambda';
 import { Stack, Duration, Names } from '../../../core';
@@ -63,6 +64,15 @@ export interface HttpLambdaAuthorizerProps {
    * @default [HttpLambdaResponseType.IAM]
    */
   readonly responseTypes?: HttpLambdaResponseType[];
+
+  /**
+   * The IAM role that the API Gateway service assumes while invoking the authorizer.
+   *
+   * Supported only for REQUEST authorizers.
+   *
+   * @default - No role
+   */
+  readonly role?: IRoleRef;
 }
 
 /**
@@ -123,17 +133,20 @@ export class HttpLambdaAuthorizer implements IHttpRouteAuthorizer {
         payloadFormatVersion: enableSimpleResponses ? AuthorizerPayloadVersion.VERSION_2_0 : AuthorizerPayloadVersion.VERSION_1_0,
         authorizerUri: lambdaAuthorizerArn(this.handler),
         resultsCacheTtl: this.props.resultsCacheTtl ?? Duration.minutes(5),
+        role: this.props.role,
       });
 
-      this.handler.addPermission(`${Names.nodeUniqueId(this.authorizer.node)}-Permission`, {
-        scope: options.scope,
-        principal: new ServicePrincipal('apigateway.amazonaws.com'),
-        sourceArn: Stack.of(options.route).formatArn({
-          service: 'execute-api',
-          resource: options.route.httpApi.apiId,
-          resourceName: `authorizers/${this.authorizer.authorizerId}`,
-        }),
-      });
+      if (!this.props.role) {
+        this.handler.addPermission(`${Names.nodeUniqueId(this.authorizer.node)}-Permission`, {
+          scope: options.scope,
+          principal: new ServicePrincipal('apigateway.amazonaws.com'),
+          sourceArn: Stack.of(options.route).formatArn({
+            service: 'execute-api',
+            resource: options.route.httpApi.apiId,
+            resourceName: `authorizers/${this.authorizer.authorizerId}`,
+          }),
+        });
+      }
     }
 
     return {
