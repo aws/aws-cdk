aws/aws-cdk

commit 408c12f0b00fede5c3a8b1d43024e961087adcfa

fix: all errors now have error codes (#36934)
We would like to build a knowledge base of common errors, so that resolution strategies can be described and shared. To that end, it's easier if all errors get an error code.

Use AI to invent an error code for all errors.

----

*By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

packages/aws-cdk-lib/aws-stepfunctions-tasks/lib/emr/private/cluster-utils.ts
@@ -93,7 +93,7 @@ export function EbsConfigurationPropertyToJson(property: EmrCreateCluster.EbsCon
  */
 export function InstanceTypeConfigPropertyToJson(property: EmrCreateCluster.InstanceTypeConfigProperty) {
   if (property.bidPrice && property.bidPriceAsPercentageOfOnDemandPrice) {
-    throw new UnscopedValidationError('Cannot specify both bidPrice and bidPriceAsPercentageOfOnDemandPrice');
+    throw new UnscopedValidationError('CannotSpecifyBothBidPriceOptions', 'Cannot specify both bidPrice and bidPriceAsPercentageOfOnDemandPrice');
   }
 
   return {
@@ -137,11 +137,11 @@ function SpotProvisioningSpecificationPropertyToJson(property?: EmrCreateCluster
   }
 
   if ((property.timeout && property.timeoutDurationMinutes) || (!property.timeout && !property.timeoutDurationMinutes)) {
-    throw new UnscopedValidationError('one of timeout and timeoutDurationMinutes must be specified');
+    throw new UnscopedValidationError('TimeoutOrTimeoutDurationMinutesRequired', 'one of timeout and timeoutDurationMinutes must be specified');
   }
   const timeout = property.timeout?.toMinutes() ?? property.timeoutDurationMinutes;
   if (timeout !== undefined && !cdk.Token.isUnresolved(timeout) && (timeout < 5 || timeout > 1440)) {
-    throw new UnscopedValidationError(`timeout must be between 5 and 1440 minutes, got ${timeout} minutes.`);
+    throw new UnscopedValidationError('TimeoutMustBeBetween5And1440Minutes', `timeout must be between 5 and 1440 minutes, got ${timeout} minutes.`);
   }
 
   return {
@@ -157,17 +157,17 @@ function SpotProvisioningSpecificationPropertyToJson(property?: EmrCreateCluster
  */
 export function InstanceFleetConfigPropertyToJson(property: EmrCreateCluster.InstanceFleetConfigProperty) {
   if (!property.targetSpotCapacity && !property.targetOnDemandCapacity) {
-    throw new UnscopedValidationError('At least one of targetSpotCapacity and targetOnDemandCapacity should be greater than 0');
+    throw new UnscopedValidationError('AtLeastOneTargetCapacityRequired', 'At least one of targetSpotCapacity and targetOnDemandCapacity should be greater than 0');
   }
   if (property.instanceFleetType === EmrCreateCluster.InstanceRoleType.MASTER) {
     if (property.targetSpotCapacity && property.targetOnDemandCapacity) {
-      throw new UnscopedValidationError('For a master instance fleet, only one of targetSpotCapacity and targetOnDemandCapacity can be specified');
+      throw new UnscopedValidationError('MasterInstanceFleetOnlyOneCapacityType', 'For a master instance fleet, only one of targetSpotCapacity and targetOnDemandCapacity can be specified');
     }
     if (property.targetSpotCapacity && property.targetSpotCapacity !== 1) {
-      throw new UnscopedValidationError(`For a master instance fleet, targetSpotCapacity cannot be a number other than 1, got ${property.targetSpotCapacity}`);
+      throw new UnscopedValidationError('MasterInstanceFleetSpotCapacityMustBeOne', `For a master instance fleet, targetSpotCapacity cannot be a number other than 1, got ${property.targetSpotCapacity}`);
     }
     if (property.targetOnDemandCapacity && property.targetOnDemandCapacity !== 1) {
-      throw new UnscopedValidationError(`For a master instance fleet, targetOnDemandCapacity cannot be a number other than 1, got ${property.targetOnDemandCapacity}`);
+      throw new UnscopedValidationError('MasterInstanceFleetOnDemandCapacityMustBeOne', `For a master instance fleet, targetOnDemandCapacity cannot be a number other than 1, got ${property.targetOnDemandCapacity}`);
     }
   }
   return {

commit ab520d7236c3f8b52db79d5e029c40f0ff9d3a78

fix(stepfunctions-tasks): add error codes to EMR priority validation errors
Align UnscopedValidationError calls with upstream error code pattern
introduced in #36934. Add lit-tagged error code as first argument to
three priority-related validation errors in cluster-utils.ts.

packages/aws-cdk-lib/aws-stepfunctions-tasks/lib/emr/private/cluster-utils.ts
@@ -100,6 +100,7 @@ export function InstanceTypeConfigPropertyToJson(property: EmrCreateCluster.Inst
   if (property.priority !== undefined && !cdk.Token.isUnresolved(property.priority)) {
     if (property.priority < 0) {
       throw new UnscopedValidationError(
+        lit`PriorityMustBeNonNegative`,
         `priority must be a non-negative number, got ${property.priority}. ` +
         'Priority values start at 0 (highest priority) and are used with OnDemandAllocationStrategy.PRIORITIZED.',
       );
@@ -192,6 +193,7 @@ export function InstanceFleetConfigPropertyToJson(property: EmrCreateCluster.Ins
 
   if (hasPriority && !isPrioritized) {
     throw new UnscopedValidationError(
+      lit`PriorityRequiresPrioritizedStrategy`,
       `Priority values are set on instance type configs, but allocation strategy is '${onDemandStrategy}'. ` +
       'Priority values only take effect with OnDemandAllocationStrategy.PRIORITIZED.',
     );
@@ -199,6 +201,7 @@ export function InstanceFleetConfigPropertyToJson(property: EmrCreateCluster.Ins
 
   if (isPrioritized && !hasPriority && property.instanceTypeConfigs?.length) {
     throw new UnscopedValidationError(
+      lit`PrioritizedStrategyRequiresPriorityValues`,
       'OnDemandAllocationStrategy.PRIORITIZED requires at least one instance type config to have a priority value set. ' +
       'See https://docs.aws.amazon.com/emr/latest/ManagementGuide/emr-instance-fleet.html',
     );