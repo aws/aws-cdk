7108bafe73539242d4986c07fe8e98bb4bb19355
Rico Huijbers <rix0rrr@gmail.com>
2026-05-11 11:24:17 +0200

    Do not accumulate tempdirs

--- packages/aws-cdk-lib/core/test/fs/fs-fingerprint.test.ts
+++ packages/aws-cdk-lib/core/test/fs/fs-fingerprint.test.ts
@@ -8,11 +8,11 @@ describe('fs fingerprint', () => {
   describe('files', () => {
     test('does not change with the file name', () => {
       // GIVEN
-      const workdir = fs.mkdtempSync(path.join(os.tmpdir(), 'hash-tests'));
+      using workdir = tempdir('hash-tests');
       const content = 'Hello, world!';
-      const input1 = path.join(workdir, 'input1.txt');
-      const input2 = path.join(workdir, 'input2.txt');
-      const input3 = path.join(workdir, 'input3.txt');
+      const input1 = path.join(workdir.dir, 'input1.txt');
+      const input2 = path.join(workdir.dir, 'input2.txt');
+      const input3 = path.join(workdir.dir, 'input3.txt');
       fs.writeFileSync(input1, content);
       fs.writeFileSync(input2, content);
       fs.writeFileSync(input3, content + '.'); // add one character, hash should be different
@@ -29,9 +29,9 @@ describe('fs fingerprint', () => {
 
     test('works on empty files', () => {
       // GIVEN
-      const workdir = fs.mkdtempSync(path.join(os.tmpdir(), 'hash-tests'));
-      const input1 = path.join(workdir, 'empty');
-      const input2 = path.join(workdir, 'empty');
+      using workdir = tempdir('hash-tests');
+      const input1 = path.join(workdir.dir, 'empty');
+      const input2 = path.join(workdir.dir, 'empty');
       fs.writeFileSync(input1, '');
       fs.writeFileSync(input2, '');
 
@@ -85,15 +85,15 @@ describe('fs fingerprint', () => {
     test('changes with file names', () => {
       // GIVEN
       const srcdir = path.join(__dirname, 'fixtures', 'symlinks');
-      const cpydir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      FileSystem.copyDirectory(srcdir, cpydir);
+      using cpydir = tempdir('fingerprint-tests');
+      FileSystem.copyDirectory(srcdir, cpydir.dir);
 
       // be careful not to break a symlink
-      fs.renameSync(path.join(cpydir, 'normal-dir', 'file-in-subdir.txt'), path.join(cpydir, 'move-me.txt'));
+      fs.renameSync(path.join(cpydir.dir, 'normal-dir', 'file-in-subdir.txt'), path.join(cpydir.dir, 'move-me.txt'));
 
       // WHEN
       const hashSrc = FileSystem.fingerprint(srcdir);
-      const hashCopy = FileSystem.fingerprint(cpydir);
+      const hashCopy = FileSystem.fingerprint(cpydir.dir);
 
       // THEN
       expect(hashSrc).not.toEqual(hashCopy);
@@ -102,17 +102,17 @@ describe('fs fingerprint', () => {
     test('changes with negated gitignore patterns inside ignored directories', () => {
       // GIVEN
       const srcdir = path.join(__dirname, 'fixtures', 'symlinks');
-      const cpydir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      FileSystem.copyDirectory(srcdir, cpydir);
+      using cpydir = tempdir('fingerprint-tests');
+      FileSystem.copyDirectory(srcdir, cpydir.dir);
 
       // Add a new file that is inside an ignored directory, but has a negated pattern that includes it.
-      const newFile = path.join(cpydir, 'ignored-dir', 'not-ignored-anymore.html');
+      const newFile = path.join(cpydir.dir, 'ignored-dir', 'not-ignored-anymore.html');
       fs.mkdirSync(path.dirname(newFile), { recursive: true });
       fs.writeFileSync(newFile, '<h1>Do not ignore me!</h1>');
 
       // WHEN
       const hashSrc = FileSystem.fingerprint(srcdir, { exclude: ['*', '!*.html', '!*/'], ignoreMode: IgnoreMode.GIT });
-      const hashCopy = FileSystem.fingerprint(cpydir, { exclude: ['*', '!*.html', '!*/'], ignoreMode: IgnoreMode.GIT });
+      const hashCopy = FileSystem.fingerprint(cpydir.dir, { exclude: ['*', '!*.html', '!*/'], ignoreMode: IgnoreMode.GIT });
 
       // THEN
       expect(hashSrc).not.toEqual(hashCopy);
@@ -122,25 +122,25 @@ describe('fs fingerprint', () => {
   describe('symlinks', () => {
     test('changes with the contents of followed symlink referent', () => {
       // GIVEN
-      const dir1 = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      const dir2 = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      const target = path.join(dir1, 'boom.txt');
+      using dir1 = tempdir('fingerprint-tests');
+      using dir2 = tempdir('fingerprint-tests');
+      const target = path.join(dir1.dir, 'boom.txt');
       const content = 'boom';
       fs.writeFileSync(target, content);
-      fs.symlinkSync(target, path.join(dir2, 'link-to-boom.txt'));
+      fs.symlinkSync(target, path.join(dir2.dir, 'link-to-boom.txt'));
 
       // now dir2 contains a symlink to a file in dir1
 
       // WHEN
-      const original = FileSystem.fingerprint(dir2);
+      const original = FileSystem.fingerprint(dir2.dir);
 
       // now change the contents of the target
       fs.writeFileSync(target, 'changning you!');
-      const afterChange = FileSystem.fingerprint(dir2);
+      const afterChange = FileSystem.fingerprint(dir2.dir);
 
       // revert the content to original and expect hash to be reverted
       fs.writeFileSync(target, content);
-      const afterRevert = FileSystem.fingerprint(dir2);
+      const afterRevert = FileSystem.fingerprint(dir2.dir);
 
       // THEN
       expect(original).not.toEqual(afterChange);
@@ -149,25 +149,25 @@ describe('fs fingerprint', () => {
 
     test('does not change with the contents of un-followed symlink referent', () => {
       // GIVEN
-      const dir1 = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      const dir2 = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      const target = path.join(dir1, 'boom.txt');
+      using dir1 = tempdir('fingerprint-tests');
+      using dir2 = tempdir('fingerprint-tests');
+      const target = path.join(dir1.dir, 'boom.txt');
       const content = 'boom';
       fs.writeFileSync(target, content);
-      fs.symlinkSync(target, path.join(dir2, 'link-to-boom.txt'));
+      fs.symlinkSync(target, path.join(dir2.dir, 'link-to-boom.txt'));
 
       // now dir2 contains a symlink to a file in dir1
 
       // WHEN
-      const original = FileSystem.fingerprint(dir2, { follow: SymlinkFollowMode.NEVER });
+      const original = FileSystem.fingerprint(dir2.dir, { follow: SymlinkFollowMode.NEVER });
 
       // now change the contents of the target
       fs.writeFileSync(target, 'changning you!');
-      const afterChange = FileSystem.fingerprint(dir2, { follow: SymlinkFollowMode.NEVER });
+      const afterChange = FileSystem.fingerprint(dir2.dir, { follow: SymlinkFollowMode.NEVER });
 
       // revert the content to original and expect hash to be reverted
       fs.writeFileSync(target, content);
-      const afterRevert = FileSystem.fingerprint(dir2, { follow: SymlinkFollowMode.NEVER });
+      const afterRevert = FileSystem.fingerprint(dir2.dir, { follow: SymlinkFollowMode.NEVER });
 
       // THEN
       expect(original).toEqual(afterChange);
@@ -176,18 +176,18 @@ describe('fs fingerprint', () => {
 
     test('changes when following directory symlinks with negated gitignore patterns', () => {
       // GIVEN
-      const srcdir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      fs.mkdirSync(path.join(srcdir, 'subdir'), { recursive: true });
-      fs.writeFileSync(path.join(srcdir, 'subdir', 'page.html'), '<h1>Hello, world!</h1>');
+      using srcdir = tempdir('fingerprint-tests');
+      fs.mkdirSync(path.join(srcdir.dir, 'subdir'), { recursive: true });
+      fs.writeFileSync(path.join(srcdir.dir, 'subdir', 'page.html'), '<h1>Hello, world!</h1>');
 
-      const cpydir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      FileSystem.copyDirectory(srcdir, cpydir);
-      fs.symlinkSync(path.join(cpydir, 'subdir'), path.join(cpydir, 'link-to-dir'));
+      using cpydir = tempdir('fingerprint-tests');
+      FileSystem.copyDirectory(srcdir.dir, cpydir.dir);
+      fs.symlinkSync(path.join(cpydir.dir, 'subdir'), path.join(cpydir.dir, 'link-to-dir'));
 
       // WHEN
       const options = { exclude: ['*', '!*.html', '!*/'], ignoreMode: IgnoreMode.GIT, follow: SymlinkFollowMode.ALWAYS };
-      const hashSrc = FileSystem.fingerprint(srcdir, options);
-      const hashCpy = FileSystem.fingerprint(cpydir, options);
+      const hashSrc = FileSystem.fingerprint(srcdir.dir, options);
+      const hashCpy = FileSystem.fingerprint(cpydir.dir, options);
 
       // THEN
       expect(hashSrc).not.toEqual(hashCpy);
@@ -195,18 +195,18 @@ describe('fs fingerprint', () => {
 
     test('does not change when not following directory symlinks with negated gitignore patterns', () => {
       // GIVEN
-      const srcdir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      fs.mkdirSync(path.join(srcdir, 'subdir'), { recursive: true });
-      fs.writeFileSync(path.join(srcdir, 'subdir', 'page.html'), '<h1>Hello, world!</h1>');
+      using srcdir = tempdir('fingerprint-tests');
+      fs.mkdirSync(path.join(srcdir.dir, 'subdir'), { recursive: true });
+      fs.writeFileSync(path.join(srcdir.dir, 'subdir', 'page.html'), '<h1>Hello, world!</h1>');
 
-      const cpydir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      FileSystem.copyDirectory(srcdir, cpydir);
-      fs.symlinkSync(path.join(cpydir, 'subdir'), path.join(cpydir, 'link-to-dir'));
+      using cpydir = tempdir('fingerprint-tests');
+      FileSystem.copyDirectory(srcdir.dir, cpydir.dir);
+      fs.symlinkSync(path.join(cpydir.dir, 'subdir'), path.join(cpydir.dir, 'link-to-dir'));
 
       // WHEN
       const options = { exclude: ['*', '!*.html', '!*/'], ignoreMode: IgnoreMode.GIT, follow: SymlinkFollowMode.NEVER };
-      const hashSrc = FileSystem.fingerprint(srcdir, options);
-      const hashCpy = FileSystem.fingerprint(cpydir, options);
+      const hashSrc = FileSystem.fingerprint(srcdir.dir, options);
+      const hashCpy = FileSystem.fingerprint(cpydir.dir, options);
 
       // THEN
       expect(hashSrc).toEqual(hashCpy);
@@ -214,29 +214,29 @@ describe('fs fingerprint', () => {
 
     test('dangling symlink is hashed as a link (not followed)', () => {
       // GIVEN
-      const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      fs.symlinkSync('/nonexistent/path/to/nowhere', path.join(dir, 'dangling'));
+      using dir = tempdir('fingerprint-tests');
+      fs.symlinkSync('/nonexistent/path/to/nowhere', path.join(dir.dir, 'dangling'));
 
       // WHEN/THEN — should not throw, should produce a stable hash
-      const hash1 = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.ALWAYS });
-      const hash2 = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.ALWAYS });
+      const hash1 = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.ALWAYS });
+      const hash2 = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.ALWAYS });
       expect(hash1).toEqual(hash2);
     });
 
     test('symlink to external directory is followed in EXTERNAL mode', () => {
       // GIVEN
-      const externalDir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-external'));
-      fs.writeFileSync(path.join(externalDir, 'file.txt'), 'external content');
+      using externalDir = tempdir('fingerprint-external');
+      fs.writeFileSync(path.join(externalDir.dir, 'file.txt'), 'external content');
 
-      const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      fs.symlinkSync(externalDir, path.join(dir, 'ext-link'));
+      using dir = tempdir('fingerprint-tests');
+      fs.symlinkSync(externalDir.dir, path.join(dir.dir, 'ext-link'));
 
       // WHEN
-      const hash1 = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL });
+      const hash1 = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL });
 
       // Change the external file — hash should change since we follow
-      fs.writeFileSync(path.join(externalDir, 'file.txt'), 'modified');
-      const hash2 = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL });
+      fs.writeFileSync(path.join(externalDir.dir, 'file.txt'), 'modified');
+      const hash2 = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL });
 
       // THEN
       expect(hash1).not.toEqual(hash2);
@@ -244,18 +244,18 @@ describe('fs fingerprint', () => {
 
     test('symlink to internal directory is NOT followed in EXTERNAL mode', () => {
       // GIVEN
-      const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      fs.mkdirSync(path.join(dir, 'subdir'));
-      fs.writeFileSync(path.join(dir, 'subdir', 'file.txt'), 'content');
-      fs.symlinkSync(path.join(dir, 'subdir'), path.join(dir, 'internal-link'));
+      using dir = tempdir('fingerprint-tests');
+      fs.mkdirSync(path.join(dir.dir, 'subdir'));
+      fs.writeFileSync(path.join(dir.dir, 'subdir', 'file.txt'), 'content');
+      fs.symlinkSync(path.join(dir.dir, 'subdir'), path.join(dir.dir, 'internal-link'));
 
       // WHEN
-      const hash1 = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL });
+      const hash1 = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL });
 
       // Change the file — hash should still change because we traverse subdir directly
       // But the internal-link should be hashed as a link, not followed
-      fs.writeFileSync(path.join(dir, 'subdir', 'file.txt'), 'modified');
-      const hash2 = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL });
+      fs.writeFileSync(path.join(dir.dir, 'subdir', 'file.txt'), 'modified');
+      const hash2 = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL });
 
       // THEN — hash changes because subdir/file.txt is traversed directly
       expect(hash1).not.toEqual(hash2);
@@ -263,55 +263,55 @@ describe('fs fingerprint', () => {
 
     test('absolute symlink targets are resolved correctly', () => {
       // GIVEN
-      const externalDir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-external'));
-      fs.writeFileSync(path.join(externalDir, 'target.txt'), 'hello');
+      using externalDir = tempdir('fingerprint-external');
+      fs.writeFileSync(path.join(externalDir.dir, 'target.txt'), 'hello');
 
-      const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
+      using dir = tempdir('fingerprint-tests');
       // Create an absolute symlink to a file
-      fs.symlinkSync(path.join(externalDir, 'target.txt'), path.join(dir, 'abs-link.txt'));
+      fs.symlinkSync(path.join(externalDir.dir, 'target.txt'), path.join(dir.dir, 'abs-link.txt'));
 
       // WHEN
-      const hash = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL });
+      const hash = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL });
 
       // THEN — should not throw and produce a deterministic hash
-      expect(hash).toEqual(FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL }));
+      expect(hash).toEqual(FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL }));
     });
 
     test('relative symlink targets are resolved correctly', () => {
       // GIVEN
-      const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
-      fs.mkdirSync(path.join(dir, 'subdir'));
-      fs.writeFileSync(path.join(dir, 'target.txt'), 'hello');
+      using dir = tempdir('fingerprint-tests');
+      fs.mkdirSync(path.join(dir.dir, 'subdir'));
+      fs.writeFileSync(path.join(dir.dir, 'target.txt'), 'hello');
       // Create a relative symlink: subdir/link -> ../target.txt
-      fs.symlinkSync('../target.txt', path.join(dir, 'subdir', 'rel-link.txt'));
+      fs.symlinkSync('../target.txt', path.join(dir.dir, 'subdir', 'rel-link.txt'));
 
       // WHEN — internal relative symlink, EXTERNAL mode should NOT follow
-      const hash = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL });
+      const hash = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL });
 
       // Change target — hash should NOT change since internal link is not followed
       const hashBefore = hash;
-      fs.writeFileSync(path.join(dir, 'target.txt'), 'changed');
+      fs.writeFileSync(path.join(dir.dir, 'target.txt'), 'changed');
       // But the direct file IS traversed, so hash changes from that
-      const hashAfter = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL });
+      const hashAfter = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL });
       expect(hashBefore).not.toEqual(hashAfter);
     });
 
     test('symlink pointing to another symlink is resolved transitively', () => {
       // GIVEN
-      const externalDir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-external'));
-      fs.writeFileSync(path.join(externalDir, 'real-file.txt'), 'the real content');
+      using externalDir = tempdir('fingerprint-external');
+      fs.writeFileSync(path.join(externalDir.dir, 'real-file.txt'), 'the real content');
       // First symlink: external dir has a symlink to the real file
-      fs.symlinkSync(path.join(externalDir, 'real-file.txt'), path.join(externalDir, 'link1.txt'));
+      fs.symlinkSync(path.join(externalDir.dir, 'real-file.txt'), path.join(externalDir.dir, 'link1.txt'));
 
-      const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'fingerprint-tests'));
+      using dir = tempdir('fingerprint-tests');
       // Second symlink: our dir has a symlink to the first symlink
-      fs.symlinkSync(path.join(externalDir, 'link1.txt'), path.join(dir, 'link2.txt'));
+      fs.symlinkSync(path.join(externalDir.dir, 'link1.txt'), path.join(dir.dir, 'link2.txt'));
 
       // WHEN — statSync follows the full chain, so content of real-file.txt is hashed
-      const hash1 = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL });
+      const hash1 = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL });
 
-      fs.writeFileSync(path.join(externalDir, 'real-file.txt'), 'modified content');
-      const hash2 = FileSystem.fingerprint(dir, { follow: SymlinkFollowMode.EXTERNAL });
+      fs.writeFileSync(path.join(externalDir.dir, 'real-file.txt'), 'modified content');
+      const hash2 = FileSystem.fingerprint(dir.dir, { follow: SymlinkFollowMode.EXTERNAL });
 
       // THEN — hash changes because we follow through to the real file
       expect(hash1).not.toEqual(hash2);
@@ -321,9 +321,14 @@ describe('fs fingerprint', () => {
   describe('eol', () => {
     test('normalizes line endings', () => {
       // GIVEN
-      const lf = path.join(__dirname, 'eol', 'lf.txt');
-      const crlf = path.join(__dirname, 'eol', 'crlf.txt');
-      fs.writeFileSync(crlf, fs.readFileSync(lf, 'utf8').replace(/\n/g, '\r\n'));
+      using tmpDir = tempdir('eol');
+      const message = 'hello word\nthis a new line!\n';
+
+      const lf = path.join(tmpDir.dir, 'lf.txt');
+      const crlf = path.join(tmpDir.dir, 'crlf.txt');
+
+      fs.writeFileSync(lf, message, 'utf-8');
+      fs.writeFileSync(crlf, message.replace(/\n/g, '\r\n'), 'utf-8');
 
       const lfStat = fs.statSync(lf);
       const crlfStat = fs.statSync(crlf);
@@ -335,8 +340,6 @@ describe('fs fingerprint', () => {
       // THEN
       expect(crlfStat.size).not.toEqual(lfStat.size); // Difference in size due to different line endings
       expect(crlfHash).toEqual(lfHash); // Same hash
-
-      fs.unlinkSync(crlf);
     });
   });
 
@@ -408,3 +411,14 @@ describe('fs fingerprint', () => {
     expect(hash1).toEqual(hash2);
   });
 });
+
+function tempdir(prefix: string) {
+  const dir = fs.mkdtempSync(path.join(os.tmpdir(), prefix));
+
+  return {
+    dir,
+    [Symbol.dispose]() {
+      fs.rmSync(dir, { force: true, recursive: true });
+    },
+  };
+}
