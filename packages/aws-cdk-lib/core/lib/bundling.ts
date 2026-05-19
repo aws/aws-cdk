606086c45c32acbfec44f21d30b09e96d6d9748f
Rico Hermans <rix0rrr@gmail.com>
2026-05-19 13:07:50 +0200

    feat(aws-cdk-lib): emits performance counters if synthesis is slow  (#37919)
    (This is a re-roll of #37843, to account for the warning emitted by `fs-extra` if it detects monkey patching)
    
    If the framework determines that synthesis is taking a long time (more than 10 seconds per stack, which is extremely unlikely high; we may adjust this threshold over time) it will emit a file with performance counters to the file indicated by the `$CDK_PERF_COUNTERS_FILE` environment variable.
    
    The CLI is responsible for sending this report in as telemetry, or not if disabled.
    
    The reporting can be disabled altogether by passing a property to `App`, or setting a context key in `cdk.json`:
    
    ```ts
    new App({
      performanceReporting: false,
    });
    ```
    
    ```js
    {
      "context": {
        "aws:cdk:performance-reporting": false,
      },
    }
    ```
    
    Performance profiling works by making functions emit `measure` events to the Node [performance measurement APIs](https://nodejs.org/api/perf_hooks.html). The new `perf.ts` file provides a number of helper functions to decorate classes and functions to make them emit measurement information. A number of CDK APIs that we suspect of being slow are annotated by default, and some global NodeJS APIs that are typical sources of slowness (filesystem access, subprocess spawns) are annotated as well.
    
    CDK users are free to use these functions as well and the `printPerfCounters()` function to do their own profiling.
    
    At the end of synthesis, only those counters annotated with `{ telemetry: true }` are written to the telemetry file and sent to the server.
    
    Here is an example of a set of perf counters (extracted from a test):
    
    ```js
    {
      'phase:Construction': 12154,
      'phase:Construction(cnt)': 1,
      'bundle:NodejsFunction': 12133,
      'bundle:NodejsFunction(cnt)': 2,
      'DockerImage.fromBuild': 10957,
      'DockerImage.fromBuild(cnt)': 1,
      'phase:Load': 3940,
      'phase:Load(cnt)': 1,
      'AssetBundlingBindMount.run': 1176,
      'AssetBundlingBindMount.run(cnt)': 1,
      'phase:Synthesis': 26,
      'phase:Synthesis(cnt)': 1,
      'Stack.resolve': 7,
      'Stack.resolve(cnt)': 47,
      'FileSystem.fingerprint': 1,
      'FileSystem.fingerprint(cnt)': 1,
      'FileSystem.isEmpty': 0,
      'FileSystem.isEmpty(cnt)': 1
    }
    ```
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*
    
    ### Issue # (if applicable)
    
    Closes #<issue number here>.
    
    ### Reason for this change
    
    
    
    ### Description of changes
    
    
    
    ### Describe any new or updated permissions being added
    
    
    
    
    ### Description of how you validated changes
    
    
    
    ### Checklist
    - [ ] My code adheres to the [CONTRIBUTING GUIDE](https://github.com/aws/aws-cdk/blob/main/CONTRIBUTING.md) and [DESIGN GUIDELINES](https://github.com/aws/aws-cdk/blob/main/docs/DESIGN_GUIDELINES.md)
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/core/lib/bundling.ts
+++ packages/aws-cdk-lib/core/lib/bundling.ts
@@ -7,6 +7,7 @@ import { FileSystem } from './fs';
 import { dockerExec } from './private/asset-staging';
 import { quiet, reset } from './private/jsii-deprecated';
 import { lit } from './private/literal-string';
+import { profileFn } from './private/perf';
 
 /**
  * Methods to build Docker CLI arguments for builds using secrets.
@@ -256,6 +257,7 @@ export class BundlingDockerImage {
   /**
    * Runs a Docker image
    */
+  @profileFn('BundlingDockerImage.run', { telemetry: true })
   public run(options: DockerRunOptions = {}) {
     const volumes = options.volumes || [];
     const environment = options.environment || {};
@@ -310,6 +312,7 @@ export class BundlingDockerImage {
    * @param outputPath the destination path for the copy operation
    * @returns the destination path
    */
+  @profileFn('BundlingDockerImage.cp', { telemetry: true })
   public cp(imagePath: string, outputPath?: string): string {
     const { stdout } = dockerExec(['create', this.image], {}); // Empty options to avoid stdout redirect here
     const match = stdout.toString().match(/([0-9a-f]{16,})/);
@@ -341,6 +344,7 @@ export class DockerImage extends BundlingDockerImage {
    * @param path The path to the directory containing the Docker file
    * @param options Docker build options
    */
+  @profileFn('DockerImage.fromBuild', { telemetry: true })
   public static fromBuild(path: string, options: DockerBuildOptions = {}) {
     const buildArgs = options.buildArgs || {};
 
@@ -689,3 +693,8 @@ function isSeLinux(): boolean {
     return false;
   }
 }
+
+/**
+ * If this symbol is present on the `BundlingOptions`, it will be used as the source of an additional timer measurement.
+ */
+export const PERF_BUNDLING_SRC_SYM = Symbol.for('@aws-cdk/core.bundlingSource');
