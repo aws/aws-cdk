d17a86a6450439a3cf6e91df8ff268f6e62a1c8d
nomu <>
2026-05-19 13:22:33 +0900

    linter fix

--- packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.eks-cluster-deletion-protection.ts
+++ packages/@aws-cdk-testing/framework-integ/test/aws-eks/test/integ.eks-cluster-deletion-protection.ts
@@ -1,12 +1,12 @@
 /// !cdk-integ pragma:disable-update-workflow
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as eks from 'aws-cdk-lib/aws-eks';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import { Template } from 'aws-cdk-lib/assertions';
 import type { StackProps } from 'aws-cdk-lib';
 import { App, CfnOutput, CfnResource, Stack } from 'aws-cdk-lib';
 import * as integ from '@aws-cdk/integ-tests-alpha';
-import { Template } from 'aws-cdk-lib/assertions';
 import { getClusterVersionConfig } from './integ-tests-kubernetes-version';
-import * as eks from 'aws-cdk-lib/aws-eks';
 
 const stackName = 'aws-cdk-eks-cluster-deletion-protection';
 
