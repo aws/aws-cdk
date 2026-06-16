e23694da0f2fd9200691ed62546144b1278ecbb1
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2026-06-16 14:42:24 +0100

    feat(core): add git source metadata to CloudFormation templates (#37368)
    Synthesized CloudFormation templates now include an `AWS::CDK::Source`
    entry in the top-level `Metadata` section containing the git remote repository URL and
    the latest commit hash. This helps trace deployed stacks back to their source code.
    
    Example output:
    
    ```yaml
    Metadata:
      AWS::CDK::Source:
        Repository: git@github.com:org/app.git
        Commit: 19e3e594a5512b75074526af89bf3b67a3164cc1
    ```
    
    The metadata is also added to the cloud assembly.
    
    This feature is disabled by default, and can be enabled by setting the context parameter 
    `@aws-cdk/core:enableGitSource` to true.
    
    If the current directory is not a git repository or git is not available, the metadata entry is
    silently omitted. Also, for security, only use the repository URL and commit hash (obtained
    by running `git` commands) if they match the expected format.
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- packages/aws-cdk-lib/core/lib/index.ts
+++ packages/aws-cdk-lib/core/lib/index.ts
@@ -68,6 +68,7 @@ export * from './cfn-capabilities';
 export * from './cloudformation.generated';
 
 export * from './feature-flags';
+export * from './git-source';
 export * from './permissions-boundary';
 
 export * from './validation';
