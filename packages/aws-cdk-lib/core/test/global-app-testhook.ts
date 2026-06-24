b1ce473f9a15e2e23358d9b7a6c09464bed8d6b2
Rico Huijbers <rix0rrr@gmail.com>
2026-06-24 13:55:55 +0200

    Unused mappings

--- packages/aws-cdk-lib/core/test/global-app-testhook.ts
+++ packages/aws-cdk-lib/core/test/global-app-testhook.ts
@@ -7,5 +7,6 @@ const APP_INIT_HOOK_SYMBOL = Symbol.for('@aws-cdk/core.App#initHook');
   cdk.Validations.of(app).acknowledge(
     // Many tests just create an empty stack to test some aspect of the CDK, and don't care about the fact that the synthesized template is empty.
     { id: 'CloudFormation-Validate::F0001', reason: 'Empty resource sections are expected in some tests' },
+    { id: 'CloudFormation-Validate::W7001', reason: 'Tests do not always reference mappings' },
   );
 };
