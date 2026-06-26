2d61195a899c23add1a61002973ede19d9bdb81a
Rico Huijbers <rix0rrr@gmail.com>
2026-06-26 09:43:38 +0200

    W8001

--- packages/aws-cdk-lib/testhelpers/jest-global-app-testhook.ts
+++ packages/aws-cdk-lib/testhelpers/jest-global-app-testhook.ts
@@ -20,5 +20,6 @@ const APP_INIT_HOOK_SYMBOL = Symbol.for('@aws-cdk/core.App#initHook');
     { id: 'CloudFormation-Validate::E3027', reason: 'Cron expressions are not valid' },
     { id: 'CloudFormation-Validate::F1020', reason: 'Invalid {Ref} targets in tests' },
     { id: 'CloudFormation-Validate::E1154', reason: 'Invalid subnet IDs in tests' },
+    { id: 'CloudFormation-Validate::W8001', reason: 'Conditions not used' },
   );
 };
