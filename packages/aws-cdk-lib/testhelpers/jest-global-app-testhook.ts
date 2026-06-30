c96cd0293778d4894828e3a4f47c4069e22cd40c
Rico Huijbers <rix0rrr@gmail.com>
2026-06-30 15:20:09 +0200

    More E3710

--- packages/aws-cdk-lib/testhelpers/jest-global-app-testhook.ts
+++ packages/aws-cdk-lib/testhelpers/jest-global-app-testhook.ts
@@ -27,5 +27,6 @@ const APP_INIT_HOOK_SYMBOL = Symbol.for('@aws-cdk/core.App#initHook');
     { id: 'CloudFormation-Validate::F3021', reason: 'Yes yes RDS passwords' },
     { id: 'CloudFormation-Validate::W2531', reason: 'There are many deprecated Lambda runtimes' },
     { id: 'CloudFormation-Validate::W9010', reason: 'Hardcoded AMI IDs are fine in tests' },
+    { id: 'CloudFormation-Validate::E3710', reason: 'We still have tests for shutdown services' },
   );
 };
