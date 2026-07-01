af42e6db65a53b6c25278fccf435b18661ced930
Rico Huijbers <rix0rrr@gmail.com>
2026-07-01 16:17:25 +0200

    DISABLE MEMORY TRACE COLLECTION IN BOXES

--- packages/aws-cdk-lib/core/lib/debug.ts
+++ packages/aws-cdk-lib/core/lib/debug.ts
@@ -1,16 +1,6 @@
-import { env } from 'process';
-
 export const CDK_DEBUG = 'CDK_DEBUG';
 
 export function debugModeEnabled(): boolean {
-  return isTruthy(env[CDK_DEBUG]);
+  return false;
 }
 
-const TRUTHY_VALUES = new Set(['1', 'on', 'true']);
-
-function isTruthy(value: string | undefined): boolean {
-  if (!value) {
-    return false;
-  }
-  return TRUTHY_VALUES.has(value.toLowerCase());
-}
