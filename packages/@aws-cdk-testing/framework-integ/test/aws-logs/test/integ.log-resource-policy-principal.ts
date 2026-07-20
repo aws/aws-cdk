c05d5367ff56d639d90f5c7998e41004901128f6
Wasim <wasim143mr@gmail.com>
2026-07-20 10:03:31 +0530

    test(logs): add integ test for resource policy canonical root arn

---
+++ packages/@aws-cdk-testing/framework-integ/test/aws-logs/test/integ.log-resource-policy-principal.ts
@@ -0,0 +1,20 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
+import { AccountPrincipal } from 'aws-cdk-lib/aws-iam';
+
+class LogGroupIntegStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const logGroup = new LogGroup(this, 'LogGroup', {});
+
+    logGroup.addToResourcePolicy(new AccountPrincipal(this.account), {
+      actions: ['logs:CreateLogStream'],
+    });
+  }
+}
+
+const app = new App();
+const stack = new LogGroupIntegStack(app, 'aws-cdk-log-group-resource-policy-principal-integ');
+new IntegTest(app, 'LogGroupResourcePolicyPrincipalInteg', { testCases: [stack] });
