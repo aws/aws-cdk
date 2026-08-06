c48dc9a5fefc098a0f94646bb355b13e483770fd
Matyas Boros <matboros@amazon.com>
2026-08-06 16:18:17 +0000

    chore(s3-deployment): increase default memory limit to 1024MB
    Update the BucketDeployment Lambda default memory from 512MB to 1024MB
    per benchmarking results showing 2x performance improvement at no extra
    cost (Lambda bills proportionally - double memory means double CPU but
    half the execution time).
    
    Updated all affected integration test snapshots across aws-s3-deployment,
    aws-bedrock-alpha, and other modules that use BucketDeployment without
    an explicit memoryLimit.
    
    Closes #35487.
    
    Co-authored-by: ychjamie <7179209+ychjamie@users.noreply.github.com>

--- packages/aws-cdk-lib/aws-s3-deployment/test/bucket-deployment.test.ts
+++ packages/aws-cdk-lib/aws-s3-deployment/test/bucket-deployment.test.ts
@@ -809,7 +809,7 @@ test('lambda execution role gets putObjectAcl permission when deploying with acc
   });
 });
 
-test('default memory limit is 512MB', () => {
+test('default memory limit is 1024MB', () => {
   // GIVEN
   const stack = new cdk.Stack();
   const bucket = new s3.Bucket(stack, 'Dest');
@@ -818,11 +818,11 @@ test('default memory limit is 512MB', () => {
   new s3deploy.BucketDeployment(stack, 'Deploy', {
     sources: [s3deploy.Source.asset(path.join(__dirname, 'my-website'))],
     destinationBucket: bucket,
-    // memoryLimit not specified - should default to 512MB
+    // memoryLimit not specified - should default to 1024MB
   });
 
   // THEN
-  Template.fromStack(stack).hasResourceProperties('AWS::Lambda::Function', { MemorySize: 512 });
+  Template.fromStack(stack).hasResourceProperties('AWS::Lambda::Function', { MemorySize: 1024 });
 });
 
 test('memoryLimit can be used to specify the memory limit for the deployment resource handler', () => {
