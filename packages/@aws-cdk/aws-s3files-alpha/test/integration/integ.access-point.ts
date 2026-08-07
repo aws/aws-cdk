d341072bd83d34b61d3b4d94318c6630aae0b701
Adonis Carvajal <caradoni@amazon.com>
2026-08-07 15:44:26 -0400

    rename dataExpiration, split tests, example uses proper default

--- packages/@aws-cdk/aws-s3files-alpha/test/integration/integ.access-point.ts
+++ packages/@aws-cdk/aws-s3files-alpha/test/integration/integ.access-point.ts
@@ -5,13 +5,14 @@ import * as core from 'aws-cdk-lib/core';
 import type { Construct } from 'constructs';
 import * as s3files from '../../lib';
 
-class BasicFileSystemStack extends core.Stack {
+class FileSystemWithAccessPointStack extends core.Stack {
   public readonly fileSystem: s3files.FileSystem;
+  public readonly accessPoint: s3files.AccessPoint;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 2 });
+    const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 1 });
     const bucket = new s3.Bucket(this, 'Bucket', {
       versioned: true,
       removalPolicy: core.RemovalPolicy.DESTROY,
@@ -26,24 +27,39 @@ class BasicFileSystemStack extends core.Stack {
       },
       removalPolicy: core.RemovalPolicy.DESTROY,
     });
+
+    this.accessPoint = this.fileSystem.addAccessPoint('AccessPoint', {
+      path: '/data',
+      createAcl: {
+        ownerUid: '1000',
+        ownerGid: '1000',
+        permissions: '755',
+      },
+      posixUser: {
+        uid: '1000',
+        gid: '1000',
+      },
+    });
   }
 }
 
 const app = new core.App();
 
-const stack = new BasicFileSystemStack(app, 'S3FilesBasicStack');
+const stack = new FileSystemWithAccessPointStack(app, 'S3FilesAccessPointStack');
 
-const integ = new IntegTest(app, 'S3FilesFileSystemIntegTest', {
+const integ = new IntegTest(app, 'S3FilesAccessPointIntegTest', {
   testCases: [stack],
 });
 
-// Assert the file system exists and is available
-const describeFs = integ.assertions.awsApiCall('@aws-sdk/client-s3files', 'GetFileSystemCommand', {
-  fileSystemId: stack.fileSystem.fileSystemId,
+// Assert the access point exists with the correct root directory
+const describeAp = integ.assertions.awsApiCall('@aws-sdk/client-s3files', 'GetAccessPointCommand', {
+  accessPointId: stack.accessPoint.accessPointId,
 });
 
-describeFs.expect(ExpectedResult.objectLike({
-  status: 'available',
+describeAp.expect(ExpectedResult.objectLike({
+  rootDirectory: {
+    path: '/data',
+  },
 }));
 
 app.synth();
