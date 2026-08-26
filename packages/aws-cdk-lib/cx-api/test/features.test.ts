132895ff4747c0e62431bcecf3612e13fed38636
tadurisaikiran <saitadur@amazon.com>
2026-08-26 15:11:03 +0000

    test(cx-api): register new feature flag in flag defaults guard test

--- packages/aws-cdk-lib/cx-api/test/features.test.ts
+++ packages/aws-cdk-lib/cx-api/test/features.test.ts
@@ -52,6 +52,7 @@ test('feature flag defaults may not be changed anymore', () => {
     [feats.EKS_DEFAULT_AL2023]: false,
     [feats.ANNOTATIONS_IN_VALIDATION_REPORT]: false,
     [feats.VALIDATE_AGAINST_DEFAULT_RULES]: false,
+    [feats.LOG_GROUP_GRANT_ENCRYPTION_KEY]: false,
 
   });
 });
