503666b6d7539b02a2ef8070c96e23a510afe03f
rajpreetcodes <rskkalt@gmail.com>
2026-08-29 15:37:07 +0530

    test(logs): add integration test for cross-account ARN principal canonical root ARN
    Fixes #37797 - Integration test verifying cross-account ARN principals are converted to canonical root ARN format to avoid CloudFormation drift detection false positives.

---
+++ packages/aws-cdk-lib/aws-logs/test/integ.cross-account-arn-principal.lit.ts
@@ -0,0 +1,26 @@
+﻿import type { StackProps } from '../../core';
+import { App, RemovalPolicy, Stack } from '../../core';
+import { ArnPrincipal, PolicyStatement } from '../../aws-iam';
+import { LogGroup } from '../lib';
+
+class CrossAccountArnPrincipalIntegStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const logGroup = new LogGroup(this, 'LogGroup', {
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    /// !show
+    logGroup.addToResourcePolicy(new PolicyStatement({
+      actions: ['logs:PutLogEvents'],
+      resources: ['*'],
+      principals: [new ArnPrincipal('arn:aws:iam::211125612616:role/Reader')],
+    }));
+    /// !hide
+  }
+}
+
+const app = new App();
+new CrossAccountArnPrincipalIntegStack(app, 'aws-cdk-cross-account-arn-principal-integ');
+app.synth();
