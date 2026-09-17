7337684fcd05e09036166d1a288968b198d13aaf
Shadowcat567 <lvielto@amazon.com>
2026-09-17 12:07:37 -0400

    improve language

--- packages/aws-cdk-lib/core/test/nested-stack.test.ts
+++ packages/aws-cdk-lib/core/test/nested-stack.test.ts
@@ -230,7 +230,7 @@ describe('nested-stack', () => {
  * A (cross-stack/cross region) reference that never gets resolved does not throw an error,
  * it silently renders as if its target were in the same template. This is a long-standing
  * issue. So we assert the assumption that all our cross-stack/cross-region references
- * should be resolved through tests.
+ * are resolved correctly through tests.
  */
 describe('nested-stack reference resolution', () => {
   const env = { account: '123456789012', region: 'us-east-1' };
